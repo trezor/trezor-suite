7c4d0fd6bd8ae959b40d0ae52c5787bb858872e2
Jan Pochyla <jpochyla@gmail.com>
2016-02-18 19:12:29 +0100

    test emitter

--- test/stream.js
+++ test/stream.js
@@ -1,6 +1,53 @@
 import test from 'tape';
 
-import { Stream } from '../lib/stream';
+import { Emitter, Stream } from '../lib/stream';
+
+test('Emitter.attach', (t) => {
+
+    // calls in correct order
+    let e1 = newEmitter();
+    let rec = [0, 0, 0];
+    e1.attach((v) => { t.deepEqual(rec, [0, 0, 0]); rec[0] = v; });
+    e1.attach((v) => { t.deepEqual(rec, [1, 0, 0]); rec[1] = v; });
+    e1.attach((v) => { t.deepEqual(rec, [1, 1, 0]); rec[2] = v; });
+    e1.emit(1);
+    t.deepEqual(rec, [1, 1, 1]);
+
+    // does not affect current .emit
+    let e2 = newEmitter();
+    let fn = called();
+    e2.attach((arg) => e2.attach(fn));
+    e2.emit(2);
+    t.deepEqual(fn.counter, 0);
+    e2.emit(3);
+    t.deepEqual(fn.counter, 1);
+    t.deepEqual(fn.args, [3]);
+
+    t.end();
+});
+
+test('Emitter.detach', (t) => {
+
+    // doesn't call after detach
+    let e1 = newEmitter();
+    let fn1 = called();
+    e1.attach(fn1);
+    e1.emit(1);
+    e1.detach(fn1);
+    e1.emit(2);
+    t.deepEqual(fn1.counter, 1);
+    t.deepEqual(fn1.args, [1]);
+
+    // does affect current .emit
+    let e2 = newEmitter();
+    let fn2 = called();
+    e2.attach(() => e2.detach(fn2));
+    e2.attach(fn2);
+    e2.emit(3);
+    t.deepEqual(fn2.counter, 0);
+
+    t.end();
+});
 
 test('Stream.combine', (t) => {
 
@@ -33,6 +80,10 @@ test('Stream.combine', (t) => {
     c.finish.emit();
 });
 
+function newEmitter() {
+    return new Emitter();
+}
+
 function newStream() {
     return new Stream(() => () => {});
 }
@@ -41,4 +92,13 @@ function calledWith(t, ...args) {
     return (arg) => {
         t.deepEqual(arg, args.shift());
     };
+}
+
+function called(t) {
+    let fn = (...args) => {
+        fn.args = args;
+        fn.counter++;
+    };
+    fn.counter = 0;
+    return fn;
 }
