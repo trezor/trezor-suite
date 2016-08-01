33143c3d8a7bded5f79b70216c94bebdd1881806
Karel Bilek <kb@karelbilek.com>
2016-08-01 14:03:52 +0200

    Adding forgotten monkey patch

--- trezor-link/src/index.js
+++ trezor-link/src/index.js
@@ -1,6 +1,8 @@
 /* @flow */
 
 import {Handler} from './handler';
+import {patch} from './protobuf/monkey_patch';
+patch();
 
 // not sure how to do this in ES6 syntax, so I won't
 module.exports = Handler;
