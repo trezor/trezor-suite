0a1783e307ea7e951ea1682e34657647fc8f3b7d
Karel Bilek <kb@karelbilek.com>
2016-08-03 15:29:17 +0200

    Build

--- trezor-link/lib/defered.js
+++ trezor-link/lib/defered.js
@@ -5,14 +5,14 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.create = create;
 function create() {
-  var _resolve = function _resolve() {};
-  var _reject = function _reject(e) {};
-  var promise = new Promise(function (resolve, reject) {
+  let _resolve = () => {};
+  let _reject = e => {};
+  const promise = new Promise((resolve, reject) => {
     _resolve = resolve;
     _reject = reject;
   });
-  var rejectingPromise = promise.then(function () {
-    throw new Error("Promise is always rejecting");
+  const rejectingPromise = promise.then(() => {
+    throw new Error(`Promise is always rejecting`);
   });
   return {
     promise: promise,
