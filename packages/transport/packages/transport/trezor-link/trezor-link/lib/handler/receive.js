5f77aaddc682e6406cf3f9c5f3d782395ae5ae82
Karel Bilek <kb@karelbilek.com>
2016-08-03 15:29:17 +0200

    Build

--- packages/transport/packages/transport/trezor-link/trezor-link/lib/handler/receive.js
+++ packages/transport/packages/transport/trezor-link/trezor-link/lib/handler/receive.js
@@ -6,26 +6,18 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 exports.receiveAndParse = receiveAndParse;
 
 var _message_decoder = require("../protobuf/message_decoder.js");
 
 var _protobufjs = require("protobufjs");
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var MESSAGE_HEADER_BYTE = 0x23;
+const MESSAGE_HEADER_BYTE = 0x23;
 
 // input that might or might not be fully parsed yet
-
-var PartiallyParsedInput = function () {
+class PartiallyParsedInput {
   // Expected length of the raq message, in bytes
-  function PartiallyParsedInput(typeNumber, length) {
-    _classCallCheck(this, PartiallyParsedInput);
-
+  constructor(typeNumber, length) {
     this.typeNumber = typeNumber;
     this.expectedLength = length;
     this.buffer = new _protobufjs.ByteBuffer(length);
@@ -35,49 +27,37 @@ var PartiallyParsedInput = function () {
 
   // Message type number
 
-
-  _createClass(PartiallyParsedInput, [{
-    key: "isDone",
-    value: function isDone() {
-      return this.buffer.offset >= this.expectedLength;
-    }
-  }, {
-    key: "append",
-    value: function append(buffer) {
-      this.buffer.append(buffer);
-    }
-  }, {
-    key: "arrayBuffer",
-    value: function arrayBuffer() {
-      var byteBuffer = this.buffer;
-      byteBuffer.reset();
-      return byteBuffer.toArrayBuffer();
-    }
-  }]);
-
-  return PartiallyParsedInput;
-}();
+  isDone() {
+    return this.buffer.offset >= this.expectedLength;
+  }
+  append(buffer) {
+    this.buffer.append(buffer);
+  }
+  arrayBuffer() {
+    const byteBuffer = this.buffer;
+    byteBuffer.reset();
+    return byteBuffer.toArrayBuffer();
+  }
+}
 
 // Parses first raw input that comes from Trezor and returns some information about the whole message.
-
-
 function parseFirstInput(bytes) {
   // convert to ByteBuffer so it's easier to read
-  var byteBuffer = _protobufjs.ByteBuffer.concat([bytes]);
+  const byteBuffer = _protobufjs.ByteBuffer.concat([bytes]);
 
   // checking first two bytes
-  var sharp1 = byteBuffer.readByte();
-  var sharp2 = byteBuffer.readByte();
+  const sharp1 = byteBuffer.readByte();
+  const sharp2 = byteBuffer.readByte();
   if (sharp1 !== MESSAGE_HEADER_BYTE || sharp2 !== MESSAGE_HEADER_BYTE) {
-    throw new Error("Didn't receive expected header signature.");
+    throw new Error(`Didn't receive expected header signature.`);
   }
 
   // reading things from header
-  var type = byteBuffer.readUint16();
-  var length = byteBuffer.readUint32();
+  const type = byteBuffer.readUint16();
+  const length = byteBuffer.readUint32();
 
   // creating a new buffer with the right size
-  var res = new PartiallyParsedInput(type, length);
+  const res = new PartiallyParsedInput(type, length);
   res.append(byteBuffer);
   return res;
 }
@@ -89,10 +69,10 @@ function receiveRest(parsedInput, receiver) {
     return Promise.resolve();
   }
 
-  return receiver().then(function (data) {
+  return receiver().then(data => {
     // sanity check
     if (data == null) {
-      throw new Error("Received no data.");
+      throw new Error(`Received no data.`);
     }
 
     parsedInput.append(data);
@@ -102,10 +82,10 @@ function receiveRest(parsedInput, receiver) {
 
 // Receives the whole message as a raw data buffer (but without headers or type info)
 function receiveBuffer(receiver) {
-  return receiver().then(function (data) {
-    var partialInput = parseFirstInput(data);
+  return receiver().then(data => {
+    const partialInput = parseFirstInput(data);
 
-    return receiveRest(partialInput, receiver).then(function () {
+    return receiveRest(partialInput, receiver).then(() => {
       return partialInput;
     });
   });
@@ -113,10 +93,10 @@ function receiveBuffer(receiver) {
 
 // Reads data from device and returns decoded message, that can be sent back to trezor.js
 function receiveAndParse(messages, receiver) {
-  return receiveBuffer(receiver).then(function (received) {
-    var typeId = received.typeNumber;
-    var buffer = received.arrayBuffer();
-    var decoder = new _message_decoder.MessageDecoder(messages, typeId, buffer);
+  return receiveBuffer(receiver).then(received => {
+    const typeId = received.typeNumber;
+    const buffer = received.arrayBuffer();
+    const decoder = new _message_decoder.MessageDecoder(messages, typeId, buffer);
     return {
       message: decoder.decodedJSON(),
       type: decoder.messageName()
--- packages/transport/packages/transport/trezor-link/trezor-link/lib/protobuf/monkey_patch.js
+++ packages/transport/packages/transport/trezor-link/trezor-link/lib/protobuf/monkey_patch.js
@@ -11,7 +11,7 @@ var ProtoBuf = _interopRequireWildcard(_protobufjs);
 
 function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
 
-var patched = false;
+let patched = false;
 
 // monkey-patching ProtoBuf,
 // so that bytes are loaded and decoded from hexadecimal
@@ -23,11 +23,11 @@ function patch() {
     // note: don't rewrite this function to arrow (value, skipRepeated) => ....
     // since I need `this` from the original context
     ProtoBuf.Reflect.Message.Field.prototype.verifyValue = function (value, skipRepeated) {
-      var newValue = value;
-      if (this.type === ProtoBuf.TYPES["bytes"]) {
+      let newValue = value;
+      if (this.type === ProtoBuf.TYPES[`bytes`]) {
         if (value != null) {
-          if (typeof value === "string") {
-            newValue = _protobufjs.ByteBuffer.wrap(value, "hex");
+          if (typeof value === `string`) {
+            newValue = _protobufjs.ByteBuffer.wrap(value, `hex`);
           }
         }
       }
