049e04ff2d429e40a0ae84b69cdb826035931afe
Karel Bilek <kb@karelbilek.com>
2016-08-10 03:09:11 +0200

    Build

--- packages/transport/lib/protobuf/to_json.js
+++ packages/transport/lib/protobuf/to_json.js
@@ -18,9 +18,129 @@ exports.protocolToJSON = protocolToJSON;
 
 var _object = require("object.values");
 
+Function.prototype.$asyncbind = function $asyncbind(self, catcher) {
+  var resolver = this;
+
+  if (catcher === true) {
+    if (!Function.prototype.$asyncbind.EagerThenable) Function.prototype.$asyncbind.EagerThenable = function factory(tick) {
+      var _tasks = [];
+
+      if (!tick) {
+        try {
+          tick = process.nextTick;
+        } catch (ex) {
+          tick = function tick(p) {
+            setTimeout(p, 0);
+          };
+        }
+      }
+
+      function _untask() {
+        for (var i = 0; i < _tasks.length; i += 2) {
+          var t = _tasks[i + 1],
+              r = _tasks[i];
+
+          for (var j = 0; j < t.length; j++) t[j].call(null, r);
+        }
+
+        _tasks = [];
+      }
+
+      function isThenable(obj) {
+        return obj && obj instanceof Object && typeof obj.then === "function";
+      }
+
+      function EagerThenable(resolver) {
+        function done(inline) {
+          var w;
+          if (_sync || phase < 0 || (w = _thens[phase]).length === 0) return;
+
+          _tasks.push(result, w);
+
+          _thens = [[], []];
+          if (_tasks.length === 2) inline ? _untask() : tick(_untask);
+        }
+
+        function resolveThen(x) {
+          if (isThenable(x)) return x.then(resolveThen, rejectThen);
+          phase = 0;
+          result = x;
+          done(true);
+        }
+
+        function rejectThen(x) {
+          if (isThenable(x)) return x.then(resolveThen, rejectThen);
+          phase = 1;
+          result = x;
+          done(true);
+        }
+
+        function settler(resolver, rejecter) {
+          _thens[0].push(resolver);
+
+          _thens[1].push(rejecter);
+
+          done();
+        }
+
+        function toString() {
+          return "EagerThenable{" + {
+            "-1": "pending",
+            0: "resolved",
+            1: "rejected"
+          }[phase] + "}=" + result.toString();
+        }
+
+        this.then = settler;
+        this.toString = toString;
+        var _thens = [[], []],
+            _sync = true,
+            phase = -1,
+            result;
+        resolver.call(null, resolveThen, rejectThen);
+        _sync = false;
+        done();
+      }
+
+      EagerThenable.resolve = function (v) {
+        return isThenable(v) ? v : {
+          then: function then(resolve, reject) {
+            return resolve(v);
+          }
+        };
+      };
+
+      return EagerThenable;
+    }();
+    return new Function.prototype.$asyncbind.EagerThenable(boundThen);
+  }
+
+  if (catcher) {
+    if (Function.prototype.$asyncbind.wrapAsyncStack) catcher = Function.prototype.$asyncbind.wrapAsyncStack(catcher);
+    return then;
+  }
+
+  function then(result, error) {
+    try {
+      return result && result instanceof Object && typeof result.then === "function" ? result.then(then, catcher) : resolver.call(self, result, error || catcher);
+    } catch (ex) {
+      return (error || catcher)(ex);
+    }
+  }
+
+  function boundThen(result, error) {
+    return resolver.call(self, result, error);
+  }
+
+  boundThen.then = boundThen;
+  return boundThen;
+};
+
 if (!Object.values) {
   (0, _object.shim)();
-}function protocolToJSON(p) {
+}
+
+function protocolToJSON(p) {
   // TODO: what if there are more files?
   const res = fileToJSON(p.file[2]);
   res.imports = [fileToJSON(p.file[1])];
