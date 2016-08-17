36ff59c2757f52d07ee993319ff2e536b9265eb5
Karel Bilek <kb@karelbilek.com>
2016-08-17 15:44:46 +0200

    Adding parralel transport

--- packages/transport/src/index.js
+++ packages/transport/src/index.js
@@ -4,5 +4,6 @@ import {patch} from './protobuf/monkey_patch';
 patch();
 
 export {LowlevelTransport} from './lowlevel-transport';
+export {ParallelTransport} from './parallel';
 export type {Transport, AcquireInput, TrezorDeviceInfoWithSession, MessageFromTrezor} from './transport';
 
