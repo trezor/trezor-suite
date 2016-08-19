3bc9b56be5f537aebb68e46c338e2975c0d2baab
Karel Bilek <kb@karelbilek.com>
2016-08-19 16:34:58 +0200

    Merging trezor-link packages together

--- packages/transport/src/index.js
+++ packages/transport/src/index.js
@@ -1,10 +1,6 @@
 /* @flow */
 
-import {patch} from './protobuf/monkey_patch';
-patch();
+// export is empty, you can import by "trezor-link/parallel", "trezor-link/lowlevel", "trezor-link/bridge"
 
-export {LowlevelTransport} from './lowlevel-transport';
-export {FallbackTransport} from './fallback';
-export {ParallelTransport} from './parallel';
 export type {Transport, AcquireInput, TrezorDeviceInfoWithSession, MessageFromTrezor} from './transport';
 
