786077cd9fd972f00be36cb899a4d54021153f84
Karel Bilek <kb@karelbilek.com>
2016-08-22 00:40:18 +0200

    target is not the class, rewriting

--- src/debug-decorator.js
+++ src/debug-decorator.js
@@ -4,24 +4,25 @@ export function debugInOut(target, name, descriptor) {
   const original = descriptor.value;
   descriptor.value = function () {
     const debug = this.debug || (name === `init` && arguments[0]);
+    const objName = this.constructor.name;
     const argsArr = Array.prototype.slice.call(arguments);
     if (debug) {
-      console.log(`[trezor-link] Calling ${target.name}.${name}(${argsArr.map(f => JSON.stringify(f)).join(`, `)})`);
+      console.log(`[trezor-link] Calling ${objName}.${name}(${argsArr.map(f => JSON.stringify(f)).join(`, `)})`);
     }
     // assuming that the function is a promise
     const resP = original.apply(this, arguments);
     return resP.then(function (res) {
       if (debug) {
         if (res == null) {
-          console.log(`[trezor-link] Done ${target.name}.${name}`);
+          console.log(`[trezor-link] Done ${objName}.${name}`);
         } else {
-          console.log(`[trezor-link] Done ${target.name}.${name}, result ${JSON.stringify(res)}`);
+          console.log(`[trezor-link] Done ${objName}.${name}, result ${JSON.stringify(res)}`);
         }
       }
       return res;
     }, function (err) {
       if (debug) {
-        console.error(`[trezor-link] Error in ${target.name}.${name}`, err);
+        console.error(`[trezor-link] Error in ${objName}.${name}`, err);
       }
       throw err;
     });
