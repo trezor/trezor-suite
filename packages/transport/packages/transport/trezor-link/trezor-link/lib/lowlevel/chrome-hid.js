cb3db2d6b33366ace91ca2745b5fc9f4cf8e813f
Karel Bilek <kb@karelbilek.com>
2016-08-22 01:15:08 +0200

    Build

--- packages/transport/packages/transport/trezor-link/trezor-link/lib/lowlevel/chrome-hid.js
+++ packages/transport/packages/transport/trezor-link/trezor-link/lib/lowlevel/chrome-hid.js
@@ -320,7 +320,7 @@ let ChromeHidPlugin = (_class = class ChromeHidPlugin {
   constructor() {
     this._hasReportId = {};
     this._udevError = false;
-    this.version = "0.2.6";
+    this.version = "0.2.7";
     this.debug = false;
   }
 
