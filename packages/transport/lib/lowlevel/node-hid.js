5dfb0aed6bffbaf43177b406a9e64945492652b3
Karel Bilek <kb@karelbilek.com>
2016-09-18 21:38:11 +0200

    Build

--- packages/transport/lib/lowlevel/node-hid.js
+++ packages/transport/lib/lowlevel/node-hid.js
@@ -174,7 +174,7 @@ let NodeHidPlugin = (_class = class NodeHidPlugin {
     this.name = `NodeHidPlugin`;
     this._sessionCounter = 0;
     this._devices = {};
-    this.version = "0.2.25";
+    this.version = "0.2.26";
     this.debug = false;
   }
 
