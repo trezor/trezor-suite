42e21c1b39e53811227f6574db9913bb45dfe6fc
Karel Bilek <kb@karelbilek.com>
2016-10-04 22:09:22 +0200

    More flow type fixes

--- src/index-browser.js
+++ src/index-browser.js
@@ -3,7 +3,6 @@
 // export is empty, you can import by "trezor-link/parallel", "trezor-link/lowlevel", "trezor-link/bridge"
 
 export type {Transport, AcquireInput, TrezorDeviceInfoWithSession, MessageFromTrezor} from './transport';
-export type {LowlevelTransportPlugin} from './lowlevel/plugin';
 
 import BridgeTransport from './bridge';
 import ExtensionTransport from './extension';
