8d4e1b2e379d71c725c1e6bed2f47181da805ace
Karel Bilek <kb@karelbilek.com>
2016-11-14 17:02:21 +0100

    Fixing errors with new Flow

--- src/lowlevel/plugin.js
+++ src/lowlevel/plugin.js
@@ -3,15 +3,15 @@
 import type {TrezorDeviceInfo} from '../transport';
 
 export type LowlevelTransportPlugin = {
-  enumerate: () => Promise<Array<TrezorDeviceInfo>>;
-  send: (path: string, session: string, data: ArrayBuffer) => Promise<void>;
-  receive: (path: string, session: string) => Promise<ArrayBuffer>;
-  connect: (path: string) => Promise<string>;
-  disconnect: (path: string, session: string) => Promise<void>;
+  +enumerate: () => Promise<Array<TrezorDeviceInfo>>;
+  +send: (path: string, session: string, data: ArrayBuffer) => Promise<void>;
+  +receive: (path: string, session: string) => Promise<ArrayBuffer>;
+  +connect: (path: string) => Promise<string>;
+  +disconnect: (path: string, session: string) => Promise<void>;
 
-  init: (debug: ?boolean) => Promise<void>;
-  version: string;
-  name: string;
+  +init: (debug: ?boolean) => Promise<void>;
+  +version: string;
+  +name: string;
 
   // in signal hid API, there is an issue that we cannot simultaneously
   // write and list devices.
