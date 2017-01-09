dde84286efb4c3243c306b18a156561e85bbab4f
Karel Bilek <kb@karelbilek.com>
2017-01-09 19:22:10 +0100

    Experimenting with synchronizing session state

--- packages/transport/trezor-link/trezor-link/src/lowlevel/webusb.js
+++ packages/transport/trezor-link/trezor-link/src/lowlevel/webusb.js
@@ -1,5 +1,16 @@
 /* @flow */
 
+type ChannelData = {
+    serialNumber: ?string;
+    session: ?string;
+};
+
+declare class BroadcastChannel {
+  constructor(name: string): void;
+  onmessage: ?((event: {data: ChannelData}) => void);
+  postMessage: (data: ChannelData) => void;
+}
+
 declare var __VERSION__: string;
 
 import {debugInOut} from '../debug-decorator';
@@ -25,9 +36,33 @@ export default class WebUsbPlugin {
   allowsWriteAndEnumerate: boolean = true;
 
   devices: {[path: string]:
-    USBDevice;
+    {
+      device: USBDevice;
+      session: ?string;
+    }
   } = {};
   lastPath: number = 0;
+  lastSession: number = 0;
+
+  channel: BroadcastChannel;
+
+  onChannelMessage(obj: ChannelData) {
+    this.lock(() => {
+      if (obj.session != null) {
+        this.lastSession = parseInt(obj.session);
+      }
+      Object.keys(this.devices).forEach(k => {
+        const d = this.devices[k];
+        if (
+          (obj.serialNumber == null && d.device.serialNumber == null) ||
+          (d.device.serialNumber === obj.serialNumber)
+        ) {
+          d.session = obj.session;
+        }
+      });
+      return Promise.resolve();
+    });
+  }
 
   @debugInOut
   init(debug: ?boolean): Promise<void> {
@@ -39,6 +74,10 @@ export default class WebUsbPlugin {
         return Promise.reject(new Error(`WebUSB is not available on this browser.`));
       } else {
         this.usb = usb;
+
+        this.channel = new BroadcastChannel(`trezor_webusb_sessions`);
+        this.channel.onmessage = (event) => { this.onChannelMessage(event.data); };
+
         return Promise.resolve();
       }
     } catch (e) {
@@ -57,14 +96,14 @@ export default class WebUsbPlugin {
           let isPresent = false;
           let path: string = ``;
           Object.keys(this.devices).forEach(kpath => {
-            if (this.devices[kpath] === dev) {
+            if (this.devices[kpath].device === dev) {
               isPresent = true;
               path = kpath;
             }
           });
           if (!isPresent) {
             this.lastPath++;
-            this.devices[this.lastPath.toString()] = dev;
+            this.devices[this.lastPath.toString()] = {device: dev, session: null};
             path = this.lastPath.toString();
           }
           res.push({path});
@@ -86,7 +125,7 @@ export default class WebUsbPlugin {
       return Promise.reject(new Error(`Device not found`));
     }
 
-    const uDevice: USBDevice = device;
+    const uDevice: USBDevice = device.device;
 
     const newArray: Uint8Array = new Uint8Array(64);
     newArray[0] = 63;
@@ -101,7 +140,7 @@ export default class WebUsbPlugin {
       return Promise.reject(new Error(`Device not found`));
     }
 
-    const uDevice: USBDevice = device;
+    const uDevice: USBDevice = device.device;
 
     return uDevice.transferIn(2, 64).then(result => {
       return result.data.buffer.slice(1);
@@ -110,40 +149,54 @@ export default class WebUsbPlugin {
 
   @debugInOut
   connect(path: string, previous: ?string): Promise<string> {
-    const device = this.devices[path];
-    if (device == null) {
-      return Promise.reject(new Error(`Device not present.`));
-    }
-
-    return device.open().then(() => {
-      return device.selectConfiguration(1);
-    }).then(() => {
-      if (previous != null) {
-        return device.reset();
+    return this.lock(() => {
+      const device = this.devices[path];
+      if (device == null) {
+        return Promise.reject(new Error(`Device not present.`));
       }
-    }).then(() => {
-      return device.claimInterface(2).catch((e) => {
-        if (typeof e === `object`) {
-          if (e.code) {
-            if (e.code === 19 && previous == null) {
-              throw new Error(`wrong previous session`);
+
+      return device.device.open().then(() => {
+        return device.device.selectConfiguration(1);
+      }).then(() => {
+        if (previous != null) {
+          return device.device.reset();
+        }
+      }).then(() => {
+        return device.device.claimInterface(2).catch((e) => {
+          if (typeof e === `object`) {
+            if (e.code) {
+              if (e.code === 19 && previous == null) {
+                throw new Error(`wrong previous session`);
+              }
             }
           }
-        }
-        throw e;
-      });
-    }).then(() => path); // path == session, why not?
+          throw e;
+        });
+      }).then(() => {
+        this.lastSession++;
+        const newSession = this.lastSession.toString();
+        device.session = newSession;
+        this.channel.postMessage({serialNumber: device.device.serialNumber, session: device.session});
+        return newSession;
+      }); // path == session, why not?
+    });
   }
 
   @debugInOut
   disconnect(path: string, session: string): Promise<void> {
-    const device = this.devices[path];
-    if (device == null) {
-      return Promise.reject(new Error(`Device not present.`));
-    }
+    return this.lock(() => {
+      const device = this.devices[path];
+      if (device == null) {
+        return Promise.reject(new Error(`Device not present.`));
+      }
 
-    return device.releaseInterface(2).then(() => {
-      return device.close();
+      return device.device.releaseInterface(2).then(() => {
+        return device.device.close();
+      }).then(() => {
+        device.session = null;
+        this.channel.postMessage({serialNumber: device.device.serialNumber, session: null});
+        return;
+      });
     });
   }
 
@@ -154,4 +207,10 @@ export default class WebUsbPlugin {
 
   requestNeeded: boolean = true;
 
+  _lock: Promise<any> = Promise.resolve();
+  lock<X>(fn: () => (Promise<X>)): Promise<X> {
+    const res = this._lock.then(() => fn());
+    this._lock = res.catch(() => {});
+    return res;
+  }
 }
