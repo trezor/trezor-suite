619f42173edaf1f1b992e4fec13db2c340954db8
Karel Bilek <kb@karelbilek.com>
2017-07-25 08:31:33 +0200

    Slight reorganization of fastxpub files

--- fastxpub/tests/benchmark.js
+++ fastxpub/tests/benchmark.js
@@ -1,4 +1,4 @@
-var fastxpub = require('./fastxpub');
+var fastxpub = require('../build/fastxpub');
 var bitcoin  = require('bitcoinjs-lib');
 
 var XPUB =
@@ -16,7 +16,7 @@ var nodeStruct = {
 var suite;
 var worker;
 
-var wasmBinaryFile = 'fastxpub.wasm';
+var wasmBinaryFile = '../build/fastxpub.wasm';
 
 if (typeof Worker !== 'undefined') {
     var promise = fetch(wasmBinaryFile, { credentials: 'same-origin' }).then(function(response) {
