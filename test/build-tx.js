e83b33ba6eb70cafd824fca558655eae266fde2f
Karel Bilek <kb@karelbilek.com>
2017-09-26 16:35:18 +0200

    Correcting test

--- test/build-tx.js
+++ test/build-tx.js
@@ -7,7 +7,7 @@ import bitcoin from 'bitcoinjs-lib-zcash';
 
 describe('build tx', () => {
     it('builds a simple tx with 1 output, no change', () => {
-        const utxos = {
+        const utxos = [{
             index: 0,
             transactionHash: 'fba6cbdccc01b85901f0372e1b0105586b2b503218f5aee432d2166fbeb6e35f',
             value: 102001,
@@ -18,12 +18,12 @@ describe('build tx', () => {
             vsize: 0,
             fee: 0,
             own: true,
-        };
-        const outputs = {
+        }];
+        const outputs = [{
             type: 'complete',
             address: '1BitcoinEaterAddressDontSendf59kuE',
             amount: 100000,
-        };
+        }];
         const height = 100;
         const feeRate = 10;
         const segwit = false;
