16634a11eb1974f719b5841b77ca090f9a23fc3b
Karel Bilek <kb@karelbilek.com>
2017-10-18 17:29:50 +0200

    Using own node4 install

--- test_helpers/install_nodejs4.sh
+++ test_helpers/install_nodejs4.sh
@@ -2,7 +2,8 @@
 
 # run as root; mainly for travis
 
-wget -qO- https://deb.nodesource.com/setup_4.x | sudo bash -
+#wget -qO- https://deb.nodesource.com/setup_4.x | sudo bash -
+sudo bash ./test_helpers/install_node_setup_4.sh
 sudo apt-get install nodejs
 sudo npm install -g yarn
 yarn
