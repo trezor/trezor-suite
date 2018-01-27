4d062af6d4cb3a25f36ee603505351001a5682a2
Karel Bilek <kb@karelbilek.com>
2018-01-28 00:02:26 +0100

    Removing deleted transports

--- packages/transport/packages/transport/trezor-link/trezor-link/src/flow-test/transports.js
+++ packages/transport/packages/transport/trezor-link/trezor-link/src/flow-test/transports.js
@@ -7,11 +7,7 @@ import BridgeTransport from '../bridge';
 import ExtensionTransport from '../extension';
 import ParallelTransport from '../parallel';
 import FallbackTransport from '../fallback';
-import LowlevelTransport from '../lowlevel';
 import LowlevelTransportWithSharedConnections from '../lowlevel/withSharedConnections';
-import ChromeHidPlugin from '../lowlevel/chrome-hid';
-import ChromeUdpPlugin from '../lowlevel/chrome-udp';
-import NodeHidPlugin from '../lowlevel/node-hid';
 import WebUsbPlugin from '../lowlevel/webusb';
 
 function acceptsTransport(t: Transport) {}
@@ -20,9 +16,5 @@ acceptsTransport(new BridgeTransport());
 acceptsTransport(new ExtensionTransport());
 acceptsTransport(new ParallelTransport({"a": new BridgeTransport()}));
 acceptsTransport(new FallbackTransport([new BridgeTransport()]));
-
-acceptsTransport(new LowlevelTransport(new ChromeHidPlugin()));
-acceptsTransport(new LowlevelTransport(new ChromeUdpPlugin()));
-acceptsTransport(new LowlevelTransport(new NodeHidPlugin()));
 
 acceptsTransport(new LowlevelTransportWithSharedConnections(new WebUsbPlugin(), () => new SharedWorker(`bla`)));
