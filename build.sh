0d1e49e1149a283095d1e430c817204f404096c2
Szymon Lesisz <sz.lesisz@gmail.com>
2018-05-09 16:35:40 +0200

    build to different address

--- build.sh
+++ build.sh
@@ -8,8 +8,8 @@ printf "\n-- COPYING FILES ----------------------\n"
 
 # cd build
 # rsync -avz --delete -e ssh . admin@dev.sldev.cz:~/experiments/www
-# rsync -avz --delete -e ssh ./build/* admin@dev.sldev.cz:~/mytrezor/ethereum/www
-rsync -avz --delete -e ssh ./build/* admin@dev.sldev.cz:~/experiments/www
+rsync -avz --delete -e ssh ./build/* admin@dev.sldev.cz:~/mytrezor/www/ethereum
+# rsync -avz --delete -e ssh ./build/* admin@dev.sldev.cz:~/experiments/www
 cd ../
 
 printf "\n-- COMPLETE ---------------------------\n"
