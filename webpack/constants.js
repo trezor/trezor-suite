2c84d69eabe7b94eaed12ffb29a7cb377c423b49
Szymon Lesisz <sz.lesisz@gmail.com>
2018-05-18 19:59:23 +0200

    add webpack/constants to git

---
+++ webpack/constants.js
@@ -0,0 +1,21 @@
+/* @flow */
+'use strict';
+
+import path from 'path';
+
+export const ABSOLUTE_BASE: string = path.normalize(path.join(__dirname, '..'));
+
+const constants: Object = Object.freeze({
+    BUILD: path.join(ABSOLUTE_BASE, 'build/'),
+    SRC: path.join(ABSOLUTE_BASE, 'src/'),
+    PORT: 8081,
+    INDEX: path.join(ABSOLUTE_BASE, 'src/index.html'),
+    TREZOR_CONNECT_ROOT: path.join(ABSOLUTE_BASE, '../trezor.js2/')
+});
+
+export const BUILD: string = constants.BUILD;
+export const SRC: string = constants.SRC;
+export const PORT: string = constants.PORT;
+export const INDEX: string = constants.INDEX;
+
+export default constants;
