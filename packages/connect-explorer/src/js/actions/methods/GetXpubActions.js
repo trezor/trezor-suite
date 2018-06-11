01e42b78a409fab4b9914d95fac58798d76af986
Szymon Lesisz <sz.lesisz@gmail.com>
2018-06-11 16:55:49 +0200

    clean GetXpubActions

--- packages/connect-explorer/src/js/actions/methods/GetXpubActions.js
+++ packages/connect-explorer/src/js/actions/methods/GetXpubActions.js
@@ -65,83 +65,13 @@ export function onResponseTabChange(tab: string): any {
 
 export function onGetXpub(params: any): any {
     return async function (dispatch, getState) {
-
-        
-                // var TREZOR_DESCS = [
-                //     // TREZOR v1
-                //     { vendorId: 0x534c, productId: 0x0001 },
-                //     // TREZOR v2 Bootloader
-                //     { vendorId: 0x1209, productId: 0x53c0 },
-                //     // TREZOR v2 Firmware
-                //     { vendorId: 0x1209, productId: 0x53c1 },
-                // ];
-        
-                // var usb = navigator.usb;
-                // try {
-                //     usb.requestDevice({filters: TREZOR_DESCS}).then(function(r){
-                //         console.log("REQ", r);
-                //     });
-        
-                //     //const devices = await usb.getDevices();
-                //     //postMessage(new UiMessage('WEBUSB'));
-                // } catch (error) {
-                //     //postMessage(new UiMessage('WEBUSB-ERROR'));
-                //     console.log("ERROR")
-                // }
-                // const response = 'A';
       
         const response = await TrezorConnect.getPublicKey({
             //...params,
             path: "m/46'/60'/0'",
-            confirmation: false,
             transformFormat: true,
-            useEmptyPassphrase: true,
+            useEmptyPassphrase: false,
         });
-
-        // const response = await TrezorConnect.requestDevice();
-
-
-        // const response = await TrezorConnect.cipherKeyValue({
-        //     path: "m/10016'/0",
-        //     key: "Activate TREZOR Password Manager?",
-        //     value: "2d650551248d792eabf628f451200d7f51cb63e46aadcbb1038aacb05e8c8aee2d650551248d792eabf628f451200d7f51cb63e46aadcbb1038aacb05e8c8aee",
-        //     encrypt: true,
-        //     askOnEncrypt: true,
-        //     askOnDecrypt: true,
-        //     useEmptyPassphrase: true,
-        //     override: true
-        // });
-
-        // const response = await TrezorConnect.ethereumGetAddress({
-        //     path: [],
-        //     // showOnTrezor: false,
-        //     useEmptyPassphrase: true
-        // });
-
-        // const response = await TrezorConnect.ethereumSignTransaction({
-        //     path: "m/44'/60'/0'/0",
-        //     nonce: "00",
-        //     gasPrice: '098bca5a00',
-        //     gasLimit: 'a43f',
-        //     to: 'e0b7927c4af23765cb51314a0e0521a9645f0e2a',
-        //     value: '010000000000000000',
-        //     chainId: 1
-        // });
-
-        // const response = await TrezorConnect.ethereumSignMessage({
-        //     path: "m/44'/60'/0'/0",
-        //     message: "Example message"
-        // });
-
-        // const response = await TrezorConnect.ethereumVerifyMessage({
-        //     address: "4c1686daf17e1b534def7dccd829a5163f72264c",
-        //     message: "Example message",
-        //     signature: "b885ec7f8ccc19d3d6ec0e145691c906420a6991f393ef77a841cc0702e5ad101d8308ef8058bcb1f29bb30ad6b8afc87f64a577bab06136fc6a3d9bf30703831b"
-        // });
-
-        // const response = await TrezorConnect.getFeatures({
-        //     useEmptyPassphrase: true
-        // });
 
         dispatch({
             type: GETXPUB_RESPONSE,
