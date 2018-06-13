6896b2a1049c244211b81e822b51ca108bdf6c3a
Szymon Lesisz <sz.lesisz@gmail.com>
2018-06-13 10:18:06 +0200

    GetXpub

--- src/js/actions/methods/GetXpubActions.js
+++ src/js/actions/methods/GetXpubActions.js
@@ -73,6 +73,56 @@ export function onGetXpub(params: any): any {
             useEmptyPassphrase: false,
         });
 
+        // const response = await TrezorConnect.nemGetAddress({
+        //     //...params,
+        //     path: "m/44'/43'/0'",
+        //     network:  0x68 // 98, 60
+        // });
+
+
+        /*
+        const response = await TrezorConnect.customMessage({
+            customFunction: function(sendMessage) {
+                return new Promise(function(resolve, reject) {
+                    //sendMessage('StellarGetPublicKey', { address_n: [2147483694, 2147483708, 2147483648] })
+                    // sendMessage('StellarGetPublicKey', { address_n: [44 | 0x80000000, 148 | 0x80000000, 0 | 0x80000000] })
+                    // sendMessage('StellarGetPublicKey', { address_n: [44 , 148 , 0 ] })
+                    //sendMessage('NEMGetAddress', { address_n: [2147483694, 2147483708, 2147483648], network: 0x68, show_display: true })
+                    // sendMessage('NEMGetAddress', { address_n: [1, 0, 0], network: 0x68, show_display: true })
+
+                    // protocol_version
+                    // network_passphrase
+                    // source_account
+                    // sendMessage('StellarSignTx', { address_n: [2147483694, 2147483708, 2147483648] })
+                    // .then(function(response) {
+
+                    //     if (response && response.type === 'StellarTxOpRequest') {
+                    //         sendMessage('StellarPaymentOp', {})
+                    //         .then(function(rr) {
+                    //             console.warn("RR", rr)
+
+                    //             resolve(response);
+                    //         })
+                    //     }
+
+                       
+                    // }).catch( function(error) {
+                    //     reject(error);
+                    // });
+
+
+
+                    sendMessage('StellarGetPublicKey', { address_n: [2147483694, 2147483708, 2147483648] })
+                    .then(function(response) {
+                        resolve(response);
+                    }).catch( function(error) {
+                        reject(error);
+                    });
+                });
+            }
+        });
+        */
+
         dispatch({
             type: GETXPUB_RESPONSE,
             response
