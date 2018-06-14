a620594333f4ada872d88e0cbae53fbe91091832
Szymon Lesisz <sz.lesisz@gmail.com>
2018-06-14 23:01:57 +0200

    EthereumGetAddress method

--- src/js/components/methods/index.js
+++ src/js/components/methods/index.js
@@ -2,7 +2,8 @@
 'use strict';
 
 export { default as GetPublicKey } from './GetPublicKey'; 
-export { default as NEMSignTx } from './NEMSignTx'; 
+export { default as EthereumGetAddress } from './EthereumGetAddress';
+export { default as NEMSignTx } from './NEMSignTx';
 export { default as NEMGetAddress } from './NEMGetAddress'; 
 export { default as ComposeTransaction } from './ComposeTransaction';
 export { default as CustomMessage } from './CustomMessage';
--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -11,6 +11,7 @@ import modal from './ModalReducer';
 import common from './methods/CommonReducer';
 import composeTx from './methods/ComposeTxReducer';
 import getxpub from './methods/GetXpubReducer';
+import ethgetaddress from './methods/EthereumGetAddressReducer';
 import nemgetaddress from './methods/NEMGetAddressReducer';
 import nemsigntx from './methods/NEMSignTxReducer';
 import custom from './methods/CustomMessageReducer';
@@ -24,6 +25,7 @@ export default combineReducers({
     common,
     composeTx,
     getxpub,
+    ethgetaddress,
     nemgetaddress,
     nemsigntx,
     custom,
