79359134766b1525483f2dde94e52025dbac1b71
Szymon Lesisz <sz.lesisz@gmail.com>
2018-06-14 22:17:53 +0200

    NEMGetAddressMethod

--- packages/connect-explorer/src/js/components/methods/index.js
+++ packages/connect-explorer/src/js/components/methods/index.js
@@ -3,5 +3,6 @@
 
 export { default as GetPublicKey } from './GetPublicKey'; 
 export { default as NEMSignTx } from './NEMSignTx'; 
+export { default as NEMGetAddress } from './NEMGetAddress'; 
 export { default as ComposeTransaction } from './ComposeTransaction';
 export { default as CustomMessage } from './CustomMessage';
