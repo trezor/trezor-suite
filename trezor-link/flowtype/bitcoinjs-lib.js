a98debd6d5597e7681c5bc30f58c93328b1b4fbf
Szymon Lesisz <sz.lesisz@gmail.com>
2018-07-13 13:50:44 +0200

    added bech32 to bitcoinjs-lib flowtype

--- trezor-link/flowtype/bitcoinjs-lib.js
+++ trezor-link/flowtype/bitcoinjs-lib.js
@@ -113,6 +113,7 @@ declare module 'bitcoinjs-lib-zcash' {
         scriptHash: number;
         wif: number;
         dustThreshold: number;
+        bech32: ?string;
     }
 
     declare type Output = {
