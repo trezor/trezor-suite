03af4fdfcec0a8474e066b3b683a950435fe499b
Vladimir Volek <vladimir.volek@satoshilabs.com>
2018-08-10 17:51:53 +0200

    Dashboard refactored

--- src/js/components/wallet/pages/Dashboard.js
+++ src/js/components/wallet/pages/Dashboard.js
@@ -1,20 +1,44 @@
-/* @flow */
-
-
 import React from 'react';
-import { H2 } from '~/js/components/common/Heading';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
+
+import { H2 } from '~/js/components/common/Heading';
 import DashboardImg from '~/images/dashboard.png';
 
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+`;
+
+const StyledH2 = styled(H2)`
+    padding: 24px 48px;
+`;
+
+const Row = styled.div`
+    flex: 1;
+    display: flex;
+    padding: 0px 48px;
+    
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const P = styled.p`
+    padding: 24px 0px;
+    text-align: center;
+`;
+
 const Dashboard = () => (
-    <section className="dashboard">
-        <H2>Dashboard</H2>
-        <div className="row">
+    <Wrapper>
+        <StyledH2>Dashboard</StyledH2>
+        <Row>
             <H2>Please select your coin</H2>
-            <p>You will gain access to recieving &amp; sending selected coin</p>
+            <P>You will gain access to recieving &amp; sending selected coin</P>
             <img src={DashboardImg} height="34" width="auto" alt="Dashboard" />
-        </div>
-    </section>
+        </Row>
+    </Wrapper>
 );
 
 export default connect(null, null)(Dashboard);
