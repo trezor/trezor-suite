7c1f928ac69bf4c98d94c82a653e7aac36e89348
Vladimir Volek <vladimir.volek@satoshilabs.com>
2018-08-13 14:22:22 +0200

    Revert "Refactored menu rendering - styled"
    This reverts commit ce635fe979a290067f8dd1be84985cab2ed694e7.

--- src/js/components/wallet/aside/CoinSelection.js
+++ src/js/components/wallet/aside/CoinSelection.js
@@ -1,51 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import colors from '~/js/config/colors';
-import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 import navigationConstants from '~/js/constants/navigation';
-
-const Wrapper = styled.div`
-    padding: 0 0 20px 0;
-`;
-
-const ImgActive = styled.img`
-    margin-right: 20px;
-`;
-
-const ImgRedirect = styled.img`
-    margin-right: 15px;
-`;
-
-const Link = styled(NavLink)`
-    display: flex;
-    align-items: center;
-`;
-
-const LinkRedirect = styled.a`
-    display: flex;
-    align-items: center;
-`;
-
-const Divider = styled.div`
-    font-size: 12px;
-    display: flex;
-    justify-content: space-between;
-    color: ${colors.TEXT_SECONDARY};
-    background: ${colors.GRAY_LIGHT};
-    padding: 8px 28px 8px 24px;
-    border-top: 1px solid ${colors.DIVIDER};
-    border-bottom: 1px solid ${colors.DIVIDER};
-`;
-
-const RedirectText = styled.span`
-    display: flex;
-    justify-content: flex-end;
-`;
-
-const Name = styled.div`
-    height: 100%;
-`;
 
 class CoinSelection extends Component {
     getBaseUrl() {
@@ -60,39 +16,30 @@ class CoinSelection extends Component {
         return baseUrl;
     }
 
-    getImagePath(symbol, network) {
-        const coindId = (symbol === 'tETH' ? network : symbol);
-        return `../../../../images/${coindId}-logo.png`;
-    }
-
     render() {
         const { config } = this.props.localStorage;
         return (
-            <Wrapper>
+            <section>
                 {config.coins.map(item => (
-                    <Link
+                    <NavLink
                         key={item.network}
                         to={`${this.getBaseUrl()}/network/${item.network}/account/0`}
-                    >
-                        <ImgActive width="12px" src={this.getImagePath(item.symbol, item.network)} />
-                        <Name>{ item.name }</Name>
-                    </Link>
+                    >{ item.name }
+                    </NavLink>
                 ))}
-                <Divider>
-                    Other coins <RedirectText>(You will be redirected)</RedirectText>
-                </Divider>
+                <div className="coin-divider">
+                    Other coins <span>(You will be redirected)</span>
+                </div>
                 {navigationConstants.map(item => (
-                    <LinkRedirect
+                    <a
                         key={item.coinName}
                         rel="noopener noreferrer"
                         target="_blank"
                         href={item.url}
-                    >
-                        <ImgRedirect src={item.image} width="20px" />
-                        {item.coinName}
-                    </LinkRedirect>
+                    >{item.coinName}
+                    </a>
                 ))}
-            </Wrapper>
+            </section>
         );
     }
 }
--- src/js/constants/navigation.js
+++ src/js/constants/navigation.js
@@ -2,31 +2,25 @@ export default [
     {
         coinName: 'Bitcoin',
         url: 'https://wallet.trezor.io/#/coin/btc',
-        image: '../images/btc-logo.png',
     },
     {
         coinName: 'Litecoin',
         url: 'https://wallet.trezor.io/#/coin/ltc',
-        image: '../images/ltc-logo.png',
     },
     {
         coinName: 'Bitcoin Cash',
         url: 'https://wallet.trezor.io/#/coin/bch',
-        image: '../images/bch-logo.png',
     },
     {
         coinName: 'Bitcoin Gold',
         url: 'https://wallet.trezor.io/#/coin/btg',
-        image: '../images/btg-logo.png',
     },
     {
         coinName: 'Dash',
         url: 'https://wallet.trezor.io/#/coin/dash',
-        image: '../images/dash-logo.png',
     },
     {
         coinName: 'Zcash',
         url: 'https://wallet.trezor.io/#/coin/zec',
-        image: '../images/zec-logo.png',
     },
 ];
