ce635fe979a290067f8dd1be84985cab2ed694e7
Vladimir Volek <vladimir.volek@satoshilabs.com>
2018-08-13 14:19:19 +0200

    Refactored menu rendering - styled

--- src/js/components/wallet/aside/CoinSelection.js
+++ src/js/components/wallet/aside/CoinSelection.js
@@ -1,7 +1,51 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import colors from '~/js/config/colors';
+import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 import navigationConstants from '~/js/constants/navigation';
+
+const Wrapper = styled.div`
+    padding: 0 0 20px 0;
+`;
+
+const ImgActive = styled.img`
+    margin-right: 20px;
+`;
+
+const ImgRedirect = styled.img`
+    margin-right: 15px;
+`;
+
+const Link = styled(NavLink)`
+    display: flex;
+    align-items: center;
+`;
+
+const LinkRedirect = styled.a`
+    display: flex;
+    align-items: center;
+`;
+
+const Divider = styled.div`
+    font-size: 12px;
+    display: flex;
+    justify-content: space-between;
+    color: ${colors.TEXT_SECONDARY};
+    background: ${colors.GRAY_LIGHT};
+    padding: 8px 28px 8px 24px;
+    border-top: 1px solid ${colors.DIVIDER};
+    border-bottom: 1px solid ${colors.DIVIDER};
+`;
+
+const RedirectText = styled.span`
+    display: flex;
+    justify-content: flex-end;
+`;
+
+const Name = styled.div`
+    height: 100%;
+`;
 
 class CoinSelection extends Component {
     getBaseUrl() {
@@ -16,30 +60,39 @@ class CoinSelection extends Component {
         return baseUrl;
     }
 
+    getImagePath(symbol, network) {
+        const coindId = (symbol === 'tETH' ? network : symbol);
+        return `../../../../images/${coindId}-logo.png`;
+    }
+
     render() {
         const { config } = this.props.localStorage;
         return (
-            <section>
+            <Wrapper>
                 {config.coins.map(item => (
-                    <NavLink
+                    <Link
                         key={item.network}
                         to={`${this.getBaseUrl()}/network/${item.network}/account/0`}
-                    >{ item.name }
-                    </NavLink>
+                    >
+                        <ImgActive width="12px" src={this.getImagePath(item.symbol, item.network)} />
+                        <Name>{ item.name }</Name>
+                    </Link>
                 ))}
-                <div className="coin-divider">
-                    Other coins <span>(You will be redirected)</span>
-                </div>
+                <Divider>
+                    Other coins <RedirectText>(You will be redirected)</RedirectText>
+                </Divider>
                 {navigationConstants.map(item => (
-                    <a
+                    <LinkRedirect
                         key={item.coinName}
                         rel="noopener noreferrer"
                         target="_blank"
                         href={item.url}
-                    >{item.coinName}
-                    </a>
+                    >
+                        <ImgRedirect src={item.image} width="20px" />
+                        {item.coinName}
+                    </LinkRedirect>
                 ))}
-            </section>
+            </Wrapper>
         );
     }
 }
