aea4732a7f6126629fe67e98e8aa5314af16f8fa
Szymon Lesisz <sz.lesisz@gmail.com>
2018-08-14 10:24:08 +0200

    StellarSignTx change address

--- packages/connect-explorer/src/js/reducers/methods/StellarSignTxReducer.js
+++ packages/connect-explorer/src/js/reducers/methods/StellarSignTxReducer.js
@@ -13,35 +13,9 @@ type MethodState = {
     transaction: string;
 }
 
-/*
-let message = {
-            protocol_version: 1,
-            source_account: '5d55642466b185b843152e9e219151dbc5892027ec40101a517bed5ca030c2e0',
-            fee: 100,
-            sequence_number: 0x100000000,
-            network_passphrase: 'Test SDF Network ; September 2015',
-            timebounds_start: null,
-            timebounds_end: null,
-            memo_type: 0,
-            memo_text: null,
-            memo_id: null,
-            memo_hash: null,
-
-            operations: [
-                {
-                    type: 'StellarPaymentOp',
-                    params: {
-                        source_account: '5d55642466b185b843152e9e219151dbc5892027ec40101a517bed5ca030c2e0',
-                        destination_account: '5d55642466b185b843152e9e219151dbc5892027ec40101a517bed5ca030c2e0',
-                        amount: 10000
-                    }
-                },
-            ]
-        };*/
-
 const defaultTx: string = 
 `{
-    source: "5d55642466b185b843152e9e219151dbc5892027ec40101a517bed5ca030c2e0",
+    source: "GAXSFOOGF4ELO5HT5PTN23T5XE6D5QWL3YBHSVQ2HWOFEJNYYMRJENBV",
     fee: 100,
     sequence: 4294967296,
     timebounds: {
@@ -57,8 +31,8 @@ const defaultTx: string =
     operations: [
       {
         type: "payment",
-        source: "5d55642466b185b843152e9e219151dbc5892027ec40101a517bed5ca030c2e0",
-        destination: "5d55642466b185b843152e9e219151dbc5892027ec40101a517bed5ca030c2e0",
+        source: "GAXSFOOGF4ELO5HT5PTN23T5XE6D5QWL3YBHSVQ2HWOFEJNYYMRJENBV",
+        destination: "GAXSFOOGF4ELO5HT5PTN23T5XE6D5QWL3YBHSVQ2HWOFEJNYYMRJENBV",
         asset: null,
         amount: "10000"
       }
