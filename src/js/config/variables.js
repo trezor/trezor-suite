82cbdcffdfead6abf8df3b6bf0c80614bc6cc12f
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-08-14 21:56:37 +0200

    Create file with global css variables

---
+++ src/js/config/variables.js
@@ -0,0 +1,13 @@
+export const FONT_SIZE = {
+    SMALLER: '12px',
+    SMALL: '14px',
+    BASE: '16px',
+};
+
+export const TRANSITION_TIME = {
+    BASE: '0.3s',
+}
+
+export const ICON_SIZE = {
+    BASE: '20px',
+}
