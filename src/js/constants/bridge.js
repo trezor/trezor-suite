509888fc6926b45965973946135e64a43ce2c76a
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-08-23 15:35:11 +0200

    Fix macOS name

--- src/js/constants/bridge.js
+++ src/js/constants/bridge.js
@@ -1,6 +1,6 @@
 export default [
     { id: 'Windows', value: 'trezor-bridge-2.0.11-win32-install.exe', label: 'Windows' },
-    { id: 'macOS', value: 'trezor-bridge-2.0.11.pkg', label: 'Mac OS X' },
+    { id: 'macOS', value: 'trezor-bridge-2.0.11.pkg', label: 'macOS' },
     { id: 'Linux', value: 'trezor-bridge_2.0.11_amd64.deb', label: 'Linux 64-bit (deb)' },
     { id: 'Linux-rpm', value: 'trezor-bridge_2.0.11_amd64.rpm', label: 'Linux 64-bit (rpm)' },
     { id: '01', value: 'trezor-bridge_2.0.11_amd32.deb', label: 'Linux 32-bit (deb)' },
