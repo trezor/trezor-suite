847f9817672798c20e5ce641a827499f9d6080ca
Vladimir Volek <vladimir.volek@satoshilabs.com>
2018-08-30 13:16:11 +0200

    Bootloader refactored

--- src/views/Wallet/views/Bootloader/index.js
+++ src/views/Wallet/views/Bootloader/index.js
@@ -1,17 +1,36 @@
-/* @flow */
-
 import React from 'react';
+import styled from 'styled-components';
 import { H2 } from 'components/Heading';
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+`;
+
+const Row = styled.div`
+    flex: 1;
+    display: flex;
+    padding: 0px 48px;
+    
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+const P = styled.p`
+    padding: 10px 0px;
+    text-align: center;
+`;
+
 const Bootloader = () => (
-    <section className="device-settings">
-        <div className="row">
+    <Wrapper>
+        <Row>
             <H2>Your device is in firmware update mode</H2>
-            <p>Please re-connect it</p>
-        </div>
-    </section>
+            <P>Please re-connect it</P>
+        </Row>
+    </Wrapper>
 );
 
 export default connect(null, null)(Bootloader);
