408d85f19af34fc3aa448f3396d79fbb6899d3ab
Szymon Lesisz <sz.lesisz@gmail.com>
2018-09-03 17:47:15 +0200

    logger is back

--- src/store.js
+++ src/store.js
@@ -37,10 +37,22 @@ if (process.env.NODE_ENV === 'development') {
     //     return pass.length === 0;
     // };
 
+    const logger = createLogger({
+        level: 'info',
+        predicate: excludeLogger,
+        collapsed: true,
+    });
+
+    const devToolsExtension: ?Function = window.devToolsExtension;
+    if (typeof devToolsExtension === 'function') {
+        enhancers.push(devToolsExtension());
+    }
+
     composedEnhancers = compose(
-        applyMiddleware(...middleware, ...services),
+        applyMiddleware(logger, ...middleware, ...services),
         ...enhancers,
     );
+    
 } else {
     composedEnhancers = compose(
         applyMiddleware(...middleware, ...services),
