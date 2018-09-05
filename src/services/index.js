0b3d6ef3120cd2568b14e370f3a2be3c8c69c443
Szymon Lesisz <sz.lesisz@gmail.com>
2018-09-05 15:44:47 +0200

    removed Web3Service (unused)

--- src/services/index.js
+++ src/services/index.js
@@ -4,7 +4,6 @@ import RouterService from './RouterService';
 import LocalStorageService from './LocalStorageService';
 import CoinmarketcapService from './CoinmarketcapService';
 import TrezorConnectService from './TrezorConnectService';
-import Web3Service from './Web3Service';
 
 export default [
     WalletService,
