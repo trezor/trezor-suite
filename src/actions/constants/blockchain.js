9293a33b57fbede3fb917e3d22da6a97e74833d2
Szymon Lesisz <sz.lesisz@gmail.com>
2018-09-13 14:38:52 +0200

    update constants

--- src/actions/constants/blockchain.js
+++ src/actions/constants/blockchain.js
@@ -1,7 +1,6 @@
 /* @flow */
 
-export const START: 'backend__start' = 'backend__start';
-export const STOP: 'backend__stop' = 'backend__stop';
+export const READY: 'backend__ready' = 'backend__ready';
 export const CONNECTING: 'backend__connecting' = 'backend__connecting';
 export const CONNECTED: 'backend__connected' = 'backend__connected';
 export const DISCONNECTED: 'backend__disconnected' = 'backend__disconnected';
