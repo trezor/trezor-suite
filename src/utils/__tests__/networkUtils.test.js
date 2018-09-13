fc9adc95a4cf2ed591d6477bd86935e9a31cf95f
Vladimir Volek <vladimir.volek@satoshilabs.com>
2018-09-13 13:34:31 +0200

    Added more tests

--- src/utils/__tests__/networkUtils.test.js
+++ src/utils/__tests__/networkUtils.test.js
@@ -5,15 +5,45 @@ describe('network utils', () => {
         fetch.resetMocks();
     });
 
-    it('httpRequest json ok response', async () => {
+    it('httpRequest json ok response json', async () => {
         fetch.mockResponse(JSON.stringify({ test_json: '12345' }));
-        const result = await networkUtils.httpRequest('/test/', 'json');
+        const result = await networkUtils.httpRequest('/http-request-test-response-json/', 'json');
         expect(result).toMatchSnapshot();
     });
 
-    it('JSONRequest ok', async () => {
+    it('httpRequest json ok response text', async () => {
+        fetch.mockResponse(JSON.stringify({ test_json: '12345' }));
+        const result = networkUtils.httpRequest('/http-request-test-response-text/', 'text');
+        expect(result).toMatchSnapshot();
+    });
+
+    it('httpRequest json ok response binary', async () => {
+        fetch.mockResponse(1);
+        const result = networkUtils.httpRequest('/http-request-test-response-binary/', 'binary');
+        expect(result).toMatchSnapshot();
+    });
+
+    it('httpRequest error (500)', async () => {
+        fetch.mockResponse(JSON.stringify({ test_json: '12345' }), { status: 500 });
+        try {
+            await networkUtils.httpRequest('/test/', 'json');
+        } catch (err) {
+            expect(err).toMatchSnapshot();
+        }
+    });
+
+    it('JSONRequest', async () => {
         fetch.mockResponse(JSON.stringify({ test_json: '01234' }));
         const result = await networkUtils.JSONRequest('/test/');
         expect(result).toMatchSnapshot();
+    });
+
+    it('JSONRequest error', async () => {
+        fetch.mockResponse(JSON.stringify({ test_json: '12345' }), { status: 500 });
+        try {
+            await networkUtils.JSONRequest('/test/', 'json');
+        } catch (err) {
+            expect(err).toMatchSnapshot();
+        }
     });
 });
