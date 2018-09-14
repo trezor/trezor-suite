ab2dddbf446ce3b821641477f8f5fc56db6d0fc2
Szymon Lesisz <sz.lesisz@gmail.com>
2018-09-14 16:44:24 +0200

    renamed blockchain constants

--- src/actions/constants/blockchain.js
+++ src/actions/constants/blockchain.js
@@ -1,6 +1,6 @@
 /* @flow */
 
-export const READY: 'backend__ready' = 'backend__ready';
-export const CONNECTING: 'backend__connecting' = 'backend__connecting';
-export const CONNECTED: 'backend__connected' = 'backend__connected';
-export const DISCONNECTED: 'backend__disconnected' = 'backend__disconnected';
+export const READY: 'blockchain__ready' = 'blockchain__ready';
+export const CONNECTING: 'blockchain__connecting' = 'blockchain__connecting';
+export const CONNECTED: 'blockchain__connected' = 'blockchain__connected';
+export const DISCONNECTED: 'blockchain__disconnected' = 'blockchain__disconnected';
