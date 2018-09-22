590e0ca642452cd665e60ac22c49af24953e0da2
Szymon Lesisz <sz.lesisz@gmail.com>
2018-09-22 19:21:33 +0200

    eslint fixes

--- src/actions/BlockchainActions.js
+++ src/actions/BlockchainActions.js
@@ -1,42 +1,28 @@
 /* @flow */
 
-import Web3 from 'web3';
-import HDKey from 'hdkey';
-
 import EthereumjsUtil from 'ethereumjs-util';
-import EthereumjsUnits from 'ethereumjs-units';
-import EthereumjsTx from 'ethereumjs-tx';
 import TrezorConnect from 'trezor-connect';
 import BigNumber from 'bignumber.js';
-
-import type { EstimateGasOptions } from 'web3';
-import type { TransactionStatus, TransactionReceipt } from 'web3';
-import { strip } from 'utils/ethUtils';
 import * as BLOCKCHAIN from 'actions/constants/blockchain';
-import * as WEB3 from 'actions/constants/web3';
 import * as PENDING from 'actions/constants/pendingTx';
-
-import * as AccountsActions from './AccountsActions';
-import * as Web3Actions from './Web3Actions';
 
 import type {
     TrezorDevice,
     Dispatch,
     GetState,
-    Action,
-    AsyncAction,
     PromiseAction,
-    ThunkAction,
 } from 'flowtype';
 import type { EthereumAccount } from 'trezor-connect';
 import type { Token } from 'reducers/TokensReducer';
 import type { NetworkToken } from 'reducers/LocalStorageReducer';
+import * as Web3Actions from './Web3Actions';
+import * as AccountsActions from './AccountsActions';
 
 export type BlockchainAction = {
     type: typeof BLOCKCHAIN.READY,
 }
 
-export const discoverAccount = (device: TrezorDevice, address: string, network: string): PromiseAction<EthereumAccount> => async (dispatch: Dispatch, getState: GetState): Promise<EthereumAccount> => {
+export const discoverAccount = (device: TrezorDevice, address: string, network: string): PromiseAction<EthereumAccount> => async (dispatch: Dispatch): Promise<EthereumAccount> => {
     // get data from connect
     // Temporary disabled, enable after trezor-connect@5.0.32 release
     const txs = await TrezorConnect.ethereumGetAccountInfo({
@@ -44,8 +30,8 @@ export const discoverAccount = (device: TrezorDevice, address: string, network: 
             address,
             block: 0,
             transactions: 0,
-            balance: "0",
-            nonce: 0
+            balance: '0',
+            nonce: 0,
         },
         coin: network,
     });
@@ -55,9 +41,9 @@ export const discoverAccount = (device: TrezorDevice, address: string, network: 
     }
 
     // blockbook web3 fallback
-    const web3account = await dispatch( Web3Actions.discoverAccount(address, network) );
+    const web3account = await dispatch(Web3Actions.discoverAccount(address, network));
     // return { transactions: txs.payload, ...web3account };
-    return { 
+    return {
         address,
         transactions: txs.payload.transactions,
         block: txs.payload.block,
@@ -66,22 +52,18 @@ export const discoverAccount = (device: TrezorDevice, address: string, network: 
     };
 };
 
-export const getTokenInfo = (input: string, network: string): PromiseAction<NetworkToken> => async (dispatch: Dispatch, getState: GetState): Promise<NetworkToken> => {
-    return await dispatch( Web3Actions.getTokenInfo(input, network) );
-}
+export const getTokenInfo = (input: string, network: string): PromiseAction<NetworkToken> => async (dispatch: Dispatch): Promise<NetworkToken> => dispatch(Web3Actions.getTokenInfo(input, network));
 
-export const getTokenBalance = (token: Token): PromiseAction<string> => async (dispatch: Dispatch, getState: GetState): Promise<string> => {
-    return await dispatch( Web3Actions.getTokenBalance(token) );
-}
+export const getTokenBalance = (token: Token): PromiseAction<string> => async (dispatch: Dispatch): Promise<string> => dispatch(Web3Actions.getTokenBalance(token));
 
-export const getGasPrice = (network: string, defaultGasPrice: number): PromiseAction<BigNumber> => async (dispatch: Dispatch, getState: GetState): Promise<BigNumber> => {
+export const getGasPrice = (network: string, defaultGasPrice: number): PromiseAction<BigNumber> => async (dispatch: Dispatch): Promise<BigNumber> => {
     try {
-        const gasPrice = await dispatch( Web3Actions.getCurrentGasPrice(network) );
+        const gasPrice = await dispatch(Web3Actions.getCurrentGasPrice(network));
         return new BigNumber(gasPrice);
     } catch (error) {
         return new BigNumber(defaultGasPrice);
     }
-}
+};
 
 const estimateProxy: Array<Promise<string>> = [];
 export const estimateGasLimit = (network: string, data: string, value: string, gasPrice: string): PromiseAction<string> => async (dispatch: Dispatch): Promise<string> => {
@@ -114,9 +96,9 @@ export const onBlockMined = (coinInfo: any): PromiseAction<void> => async (dispa
     const network: string = coinInfo.shortcut.toLowerCase();
 
     // try to resolve pending transactions
-    await dispatch( Web3Actions.resolvePendingTransactions(network) );
+    await dispatch(Web3Actions.resolvePendingTransactions(network));
 
-    await dispatch( Web3Actions.updateGasPrice(network) );
+    await dispatch(Web3Actions.updateGasPrice(network));
 
     const accounts: Array<any> = getState().accounts.filter(a => a.network === network);
     if (accounts.length > 0) {
@@ -125,39 +107,36 @@ export const onBlockMined = (coinInfo: any): PromiseAction<void> => async (dispa
             accounts,
             coin: network,
         });
-        
+
         if (response.success) {
             response.payload.forEach((a, i) => {
                 if (a.transactions > 0) {
                     // load additional data from Web3 (balance, nonce, tokens)
-                    dispatch( Web3Actions.updateAccount(accounts[i], a, network) )
+                    dispatch(Web3Actions.updateAccount(accounts[i], a, network));
                 } else {
                     // there are no new txs, just update block
-                    dispatch( AccountsActions.update( { ...accounts[i], block: a.block }) );
+                    dispatch(AccountsActions.update({ ...accounts[i], block: a.block }));
 
                     // HACK: since blockbook can't work with smart contracts for now
                     // try to update tokens balances added to this account using Web3
-                    dispatch( Web3Actions.updateAccountTokens(accounts[i]) );
+                    dispatch(Web3Actions.updateAccountTokens(accounts[i]));
                 }
             });
         }
     }
-}
+};
 
 
 // not used for now, waiting for fix in blockbook
 export const onNotification = (payload: any): PromiseAction<void> => async (dispatch: Dispatch, getState: GetState): Promise<void> => {
-
     // this event can be triggered multiple times
     // 1. check if pair [txid + address] is already in reducer
     const network: string = payload.coin.shortcut.toLowerCase();
     const address: string = EthereumjsUtil.toChecksumAddress(payload.tx.address);
-    const txInfo = await dispatch( Web3Actions.getPendingInfo(network, payload.tx.txid) );
+    const txInfo = await dispatch(Web3Actions.getPendingInfo(network, payload.tx.txid));
 
     // const exists = getState().pending.filter(p => p.id === payload.tx.txid && p.address === address);
-    const exists = getState().pending.filter(p => {
-        return p.address === address;
-    });
+    const exists = getState().pending.filter(p => p.address === address);
     if (exists.length < 1) {
         if (txInfo) {
             dispatch({
@@ -166,14 +145,14 @@ export const onNotification = (payload: any): PromiseAction<void> => async (disp
                     type: 'send',
                     id: payload.tx.txid,
                     network,
-                    currency: "tETH",
+                    currency: 'tETH',
                     amount: txInfo.value,
-                    total: "0",
+                    total: '0',
                     tx: {},
                     nonce: txInfo.nonce,
                     address,
-                    rejected: false
-                }
+                    rejected: false,
+                },
             });
         } else {
             // tx info not found (yet?)
@@ -186,18 +165,17 @@ export const onNotification = (payload: any): PromiseAction<void> => async (disp
             // });
         }
     }
-}
+};
 
 
 export const subscribe = (network: string): PromiseAction<void> => async (dispatch: Dispatch, getState: GetState): Promise<void> => {
-    const addresses: Array<string> = getState().accounts.filter(a => a.network === network).map(a => a.address);
-    // $FlowIssue: trezor-connect@5.0.32
-    return await TrezorConnect.blockchainSubscribe({
+    const addresses: Array<string> = getState().accounts.filter(a => a.network === network).map(a => a.address); // eslint-disable-line no-unused-vars
+    await TrezorConnect.blockchainSubscribe({
         // accounts: addresses,
         accounts: [],
-        coin: network
+        coin: network,
     });
-}
+};
 
 // Conditionally subscribe to blockchain backend
 // called after TrezorConnect.init successfully emits TRANSPORT.START event
@@ -207,26 +185,26 @@ export const init = (): PromiseAction<void> => async (dispatch: Dispatch, getSta
     if (getState().discovery.length > 0) {
         // get unique networks
         const networks: Array<string> = [];
-        getState().discovery.forEach(discovery => {
+        getState().discovery.forEach((discovery) => {
             if (networks.indexOf(discovery.network) < 0) {
                 networks.push(discovery.network);
             }
         });
 
         // subscribe
-        for (let i = 0; i < networks.length; i++) {
-            await dispatch( subscribe(networks[i]) );
-        }
+        const results = networks.map(n => dispatch(subscribe(n)));
+        // wait for all subscriptions
+        await Promise.all(results);
     }
 
     // continue wallet initialization
     dispatch({
-        type: BLOCKCHAIN.READY
+        type: BLOCKCHAIN.READY,
     });
-}
+};
 
 // Handle BLOCKCHAIN.ERROR event from TrezorConnect
 // disconnect and remove Web3 webscocket instance if exists
-export const error = (payload: any): PromiseAction<void> => async (dispatch: Dispatch, getState: GetState): Promise<void> => {
-    dispatch( Web3Actions.disconnect(payload.coin) );
-}
+export const error = (payload: any): PromiseAction<void> => async (dispatch: Dispatch): Promise<void> => {
+    dispatch(Web3Actions.disconnect(payload.coin));
+};
