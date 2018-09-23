1ea6d0be4cc87592fe929ddc5e5ee216c859132f
Szymon Lesisz <sz.lesisz@gmail.com>
2018-09-23 08:28:12 +0200

    eslint ./src/utils

--- src/utils/ethUtils.js
+++ src/utils/ethUtils.js
@@ -5,14 +5,11 @@ import EthereumjsUtil from 'ethereumjs-util';
 
 export const decimalToHex = (dec: number): string => new BigNumber(dec).toString(16);
 
-export const padLeftEven = (hex: string): string => {
-    hex = hex.length % 2 != 0 ? `0${hex}` : hex;
-    return hex;
-};
+export const padLeftEven = (hex: string): string => (hex.length % 2 !== 0 ? `0${hex}` : hex);
 
-export const sanitizeHex = (hex: number | string): ?string => {
-    if (typeof hex !== 'string') return null;
-    hex = hex.substring(0, 2) === '0x' ? hex.substring(2) : hex;
+export const sanitizeHex = ($hex: number | string): ?string => {
+    if (typeof $hex !== 'string') return null;
+    const hex = $hex.substring(0, 2) === '0x' ? $hex.substring(2) : $hex;
     if (hex === '') return '';
     return `0x${padLeftEven(hex)}`;
 };
@@ -35,10 +32,12 @@ export const validateAddress = (address: string): ?string => {
     const hasUpperCase = new RegExp('^(.*[A-Z].*)$');
     if (address.length < 1) {
         return 'Address is not set';
-    } else if (!EthereumjsUtil.isValidAddress(address)) {
+    }
+    if (!EthereumjsUtil.isValidAddress(address)) {
         return 'Address is not valid';
-    } else if (address.match(hasUpperCase) && !EthereumjsUtil.isValidChecksumAddress(address)) {
+    }
+    if (address.match(hasUpperCase) && !EthereumjsUtil.isValidChecksumAddress(address)) {
         return 'Address is not a valid checksum';
     }
     return null;
-}
+};
--- src/utils/promiseUtils.js
+++ src/utils/promiseUtils.js
@@ -1,12 +1,5 @@
 /* @flow */
 
-
-// import root from 'window-or-global';
-// import Promise from 'es6-promise';
-
-export async function resolveAfter(msec: number, value?: any): Promise<any> {
-    await new Promise((resolve) => {
-        //root.setTimeout(resolve, msec, value);
-        window.setTimeout(resolve, msec, value);
-    });
-}
+export const resolveAfter = (msec: number, value?: any): Promise<any> => new Promise((resolve) => {
+    window.setTimeout(resolve, msec, value);
+});
