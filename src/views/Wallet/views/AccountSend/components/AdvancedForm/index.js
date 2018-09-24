162ddebd6cdee5b9dfaaa370befbe2ffa84a2f8a
Vasek Mlejnsky <vasek.mlejnsky@gmail.com>
2018-09-24 10:45:40 +0200

    Fix AdvanceForm  composition for a smaller screen width

--- src/views/Wallet/views/AccountSend/components/AdvancedForm/index.js
+++ src/views/Wallet/views/AccountSend/components/AdvancedForm/index.js
@@ -13,6 +13,11 @@ import ICONS from 'config/icons';
 
 import type { Props } from '../../Container';
 
+// TODO: Decide on a small screen width for the whole app
+// and put it inside config/variables.js
+// same variable also in "AccountSend/index.js"
+const SmallScreenWidth = '850px';
+
 const InputRow = styled.div`
     margin-bottom: 20px;
 `;
@@ -38,11 +43,23 @@ const AdvancedSettingsWrapper = styled.div`
 const GasInputRow = styled(InputRow)`
     width: 100%;
     display: flex;
+
+    @media screen and (max-width: ${SmallScreenWidth}) {
+        flex-direction: column;
+    }
 `;
 
 const GasInput = styled(Input)`
+    min-height: 85px;
     &:first-child {
         padding-right: 20px;
+    }
+
+    @media screen and (max-width: ${SmallScreenWidth}) {
+        &:first-child {
+            padding-right: 0;
+            margin-bottom: 2px;
+        }
     }
 `;
 
@@ -204,7 +221,7 @@ const AdvancedForm = (props: Props) => {
                     </InputLabelWrapper>
                 )}
                 bottomText={errors.data || warnings.data || infos.data}
-                disabled={networkSymbol !== currency}
+                isDisabled={networkSymbol !== currency}
                 value={networkSymbol !== currency ? '' : data}
                 onChange={event => onDataChange(event.target.value)}
             />
