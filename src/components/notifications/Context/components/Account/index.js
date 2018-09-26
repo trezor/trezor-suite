25083b5f19ee45c7142c61a58880ba5ba0e0c7b4
Szymon Lesisz <sz.lesisz@gmail.com>
2018-09-26 14:11:54 +0200

    new components

---
+++ src/components/notifications/Context/components/Account/index.js
@@ -0,0 +1,19 @@
+/* @flow */
+import * as React from 'react';
+import { Notification } from 'components/Notification';
+
+import type { Props } from '../../index';
+
+// There could be only one account notification
+export default (props: Props) => {
+    const { notification } = props.selectedAccount;
+    if (notification) {
+        if (notification.type === 'backend') {
+            // special case: backend is down
+            // TODO: this is a different component with "auto resolve" button
+            return (<Notification type="error" title={notification.title} message={notification.message} />);
+        }
+        return (<Notification type={notification.type} title={notification.title} message={notification.message} />);
+    }
+    return null;
+};
---
+++ src/components/notifications/Context/components/Static/index.js
@@ -0,0 +1,17 @@
+/* @flow */
+import * as React from 'react';
+import { Notification } from 'components/Notification';
+
+import type { Props } from '../../index';
+
+export default (props: Props) => {
+    const { location } = props.router;
+    if (!location) return null;
+
+    const notifications: Array<Notification> = [];
+    if (location.state.device) {
+        notifications.push(<Notification key="example" type="info" title="Static example" />);
+    }
+
+    return notifications;
+};
