61d9713b0bc1568aeeab6aae190316ded759e31d
Szymon Lesisz <sz.lesisz@gmail.com>
2018-09-26 18:48:45 +0200

    removed comment

--- src/components/notifications/Context/components/Account/index.js
+++ src/components/notifications/Context/components/Account/index.js
@@ -11,6 +11,22 @@ export default (props: Props) => {
         if (notification.type === 'backend') {
             // special case: backend is down
             // TODO: this is a different component with "auto resolve" button
+            /*
+            return (
+                <Notification
+                    type="error"
+                    title="Backend not connected"
+                    actions={
+                        [{
+                            label: 'Try again',
+                            callback: async () => {
+                                await props.blockchainReconnect(network.network);
+                            },
+                        }]
+                    }
+                />
+            );
+            */
             return (<Notification type="error" title={notification.title} message={notification.message} />);
         }
         return (<Notification type={notification.type} title={notification.title} message={notification.message} />);
