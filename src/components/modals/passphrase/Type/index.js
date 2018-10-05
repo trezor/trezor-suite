5c9194e814f1e24b51edc8a299b72046559d2cba
Szymon Lesisz <sz.lesisz@gmail.com>
2018-10-05 17:49:51 +0200

    fixed flowtype in modals

--- src/components/modals/passphrase/Type/index.js
+++ src/components/modals/passphrase/Type/index.js
@@ -1,9 +1,14 @@
+/* @flow */
+
 import React from 'react';
 import Icon from 'components/Icon';
 import colors from 'config/colors';
 import icons from 'config/icons';
 import styled from 'styled-components';
 import { H3 } from 'components/Heading';
+import P from 'components/Paragraph';
+
+import type { Props } from 'components/modals/index';
 
 const Wrapper = styled.div`
     width: 360px;
@@ -12,7 +17,7 @@ const Wrapper = styled.div`
 
 const Header = styled.div``;
 
-const Confirmation = (props) => {
+const Confirmation = (props: Props) => {
     if (!props.modal.opened) return null;
     const { device } = props.modal;
 
@@ -21,6 +26,7 @@ const Confirmation = (props) => {
             <Header>
                 <Icon icon={icons.T1} size={60} color={colors.TEXT_SECONDARY} />
                 <H3>Complete the action on { device.label } device</H3>
+                <P isSmaller>TODO: add detailed text here. Explain passphrase/hidden wallet...</P>
             </Header>
         </Wrapper>
     );
