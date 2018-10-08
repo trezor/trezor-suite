d6af3d6dfa8eee6639d7cbe39ddef7f632089aab
Szymon Lesisz <sz.lesisz@gmail.com>
2018-10-08 12:17:31 +0200

    remove pendingTxs while switching between wallet types and forgeting device

--- src/actions/PendingTxActions.js
+++ src/actions/PendingTxActions.js
@@ -3,11 +3,7 @@
 
 import * as PENDING from 'actions/constants/pendingTx';
 
-import type {
-    Action, ThunkAction, GetState, Dispatch,
-} from 'flowtype';
 import type { State, PendingTx } from 'reducers/PendingTxReducer';
-
 
 export type PendingTxAction = {
     type: typeof PENDING.FROM_STORAGE,
@@ -26,28 +22,3 @@ export type PendingTxAction = {
     type: typeof PENDING.TX_TOKEN_ERROR,
     tx: PendingTx,
 }
-
-export const reject = (tx: PendingTx): ThunkAction => (dispatch: Dispatch, getState: GetState): void => {
-    /*
-            dispatch({
-                    type: NOTIFICATION.ADD,
-                    payload: {
-                        type: 'warning',
-                        title: 'Pending transaction rejected',
-                        message: `Transaction with id: ${tx.id} not found.`,
-                        cancelable: true,
-                        actions: [
-                            {
-                                label: 'OK',
-                                callback: () => {
-                                    dispatch({
-                                        type: PENDING.TX_RESOLVED,
-                                        tx,
-                                    });
-                                },
-                            },
-                        ],
-                    },
-                });
-            */
-};
