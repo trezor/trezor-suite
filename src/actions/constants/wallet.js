6a3e71f0a505d8e28c6ee3ed0c05b276a0fd337f
Szymon Lesisz <sz.lesisz@gmail.com>
2018-10-09 14:32:15 +0200

    remove unused constant WALLET.ON_BEFORE_UNLOAD

--- src/actions/constants/wallet.js
+++ src/actions/constants/wallet.js
@@ -1,7 +1,5 @@
 /* @flow */
 
-
-export const ON_BEFORE_UNLOAD: 'wallet__on_before_unload' = 'wallet__on_before_unload';
 export const TOGGLE_DEVICE_DROPDOWN: 'wallet__toggle_dropdown' = 'wallet__toggle_dropdown';
 export const SET_INITIAL_URL: 'wallet__set_initial_url' = 'wallet__set_initial_url';
 export const ONLINE_STATUS: 'wallet__online_status' = 'wallet__online_status';
