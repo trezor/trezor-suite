de34a8fe0400a0c55779e954213ab89d312bffa5
Vladimir Volek <vladimir.volek@satoshilabs.com>
2018-10-11 12:25:21 +0200

    comment out test data

--- src/components/notifications/Context/components/Action/components/NotificationsGroups/index.js
+++ src/components/notifications/Context/components/Action/components/NotificationsGroups/index.js
@@ -24,50 +24,50 @@ class NotificationsGroup extends PureComponent {
     }
 
     render() {
-        const { close } = this.props;
-        const notifications = [
-            {
-                key: 1,
-                title: 'this is a title of error notification',
-                type: 'error',
-                message: 'this is a message of error notification',
-            },
-            {
-                key: 2,
-                title: 'this is a title of warning notification',
-                type: 'warning',
-                message: 'this is a message of warning notification',
-            },
-            {
-                key: 3,
-                title: 'this is a title of warning notification',
-                type: 'warning',
-                message: 'this is a message of warning notification',
-            },
-            {
-                key: 4,
-                title: 'this is a title of warning notification sds d',
-                type: 'warning',
-                message: 'this is a message of warning notification',
-            },
-            {
-                key: 5,
-                title: 'this is a title of warning notification as',
-                type: 'info',
-                message: 'this is a message of warning notification',
-            },
-            {
-                key: 6,
-                title: 'this is a title of info notification s ',
-                type: 'info',
-                message: 'this is a message of info notification',
-                actions:
-                    [{
-                        label: 'Update',
-                        callback: 'props.routerActions.gotoBridgeUpdate',
-                    }],
-            },
-        ];
+        const { close, notifications } = this.props;
+        // const notifications = [
+        //     {
+        //         key: 1,
+        //         title: 'this is a title of error notification',
+        //         type: 'error',
+        //         message: 'this is a message of error notification',
+        //     },
+        //     {
+        //         key: 2,
+        //         title: 'this is a title of warning notification',
+        //         type: 'warning',
+        //         message: 'this is a message of warning notification',
+        //     },
+        //     {
+        //         key: 3,
+        //         title: 'this is a title of warning notification',
+        //         type: 'warning',
+        //         message: 'this is a message of warning notification',
+        //     },
+        //     {
+        //         key: 4,
+        //         title: 'this is a title of warning notification sds d',
+        //         type: 'warning',
+        //         message: 'this is a message of warning notification',
+        //     },
+        //     {
+        //         key: 5,
+        //         title: 'this is a title of warning notification as',
+        //         type: 'info',
+        //         message: 'this is a message of warning notification',
+        //     },
+        //     {
+        //         key: 6,
+        //         title: 'this is a title of info notification s ',
+        //         type: 'info',
+        //         message: 'this is a message of info notification',
+        //         actions:
+        //             [{
+        //                 label: 'Update',
+        //                 callback: 'props.routerActions.gotoBridgeUpdate',
+        //             }],
+        //     },
+        // ];
         const notificationGroups = this.groupNotifications(notifications);
         const sortedNotifications = this.sortByPriority(notificationGroups);
 
