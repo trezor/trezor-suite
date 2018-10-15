672429c62c201aa1d401af077d6e4dc16802daa1
Vladimir Volek <vladimir.volek@satoshilabs.com>
2018-10-15 14:36:56 +0200

    Fixed props warning

--- src/components/Link/index.js
+++ src/components/Link/index.js
@@ -37,7 +37,7 @@ const A = styled.a`
 
 const StyledNavLink = styled(NavLink)`
     ${props => props.isGreen && css`
-            color: ${colors.GREEN_PRIMARY};
+        color: ${colors.GREEN_PRIMARY};
     `}
 
     ${props => props.isGray && css`
@@ -51,12 +51,7 @@ class Link extends PureComponent {
         let LinkComponent;
         if (shouldRenderRouterLink) {
             LinkComponent = (
-                <StyledNavLink
-                    isGreen={this.props.isGreen}
-                    isGray={this.props.isGray}
-                    to={this.props.to}
-                >{this.props.children}
-                </StyledNavLink>);
+                <StyledNavLink {...this.props}>{this.props.children}</StyledNavLink>);
         } else {
             LinkComponent = (
                 <A
@@ -90,11 +85,6 @@ Link.propTypes = {
     onClick: PropTypes.func,
     isGreen: PropTypes.bool,
     isGray: PropTypes.bool,
-};
-
-Link.defaultProps = {
-    isGreen: false,
-    isGray: false,
 };
 
 export default Link;
