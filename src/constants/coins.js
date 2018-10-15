8753a043a733550bb5c346f99477a094bd3333ef
Vladimir Volek <vladimir.volek@satoshilabs.com>
2018-10-15 18:18:35 +0200

    Added urls for old wallet

--- src/constants/coins.js
+++ src/constants/coins.js
@@ -2,47 +2,47 @@ export default [
     {
         id: 'btc',
         coinName: 'Bitcoin',
-        url: 'https://wallet.trezor.io/#/coin/btc',
+        url: 'https://wallet.trezor.io/#/?coin=btc',
     },
     {
         id: 'bch',
         coinName: 'Bitcoin Cash',
-        url: 'https://wallet.trezor.io/#/coin/bch',
+        url: 'https://wallet.trezor.io/#/?coin=bch',
     },
     {
         id: 'btg',
         coinName: 'Bitcoin Gold',
-        url: 'https://wallet.trezor.io/#/coin/btg',
+        url: 'https://wallet.trezor.io/#/?coin=btg',
     },
     {
         id: 'dash',
         coinName: 'Dash',
-        url: 'https://wallet.trezor.io/#/coin/dash',
+        url: 'https://wallet.trezor.io/#/?coin=dash',
     },
     {
         id: 'doge',
         coinName: 'Dogecoin',
-        url: 'https://wallet.trezor.io/#/coin/doge',
+        url: 'https://wallet.trezor.io/#/?coin=doge',
     },
     {
         id: 'ltc',
         coinName: 'Litecoin',
-        url: 'https://wallet.trezor.io/#/coin/ltc',
+        url: 'https://wallet.trezor.io/#/?coin=ltc',
     },
     {
         id: 'nmc',
         coinName: 'Namecoin',
-        url: 'https://wallet.trezor.io/#/coin/nmc',
+        url: 'https://wallet.trezor.io/#/?coin=nmc',
     },
     {
         id: 'vtc',
         coinName: 'Vertcoin',
-        url: 'https://wallet.trezor.io/#/coin/vtc',
+        url: 'https://wallet.trezor.io/#/?coin=vtc',
     },
     {
         id: 'zec',
         coinName: 'Zcash',
-        url: 'https://wallet.trezor.io/#/coin/zec',
+        url: 'https://wallet.trezor.io/#/?coin=zec',
     },
     {
         id: 'xem',
