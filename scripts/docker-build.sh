15e4ee383fc827975ce3a40180af68d192a9d641
Vladimir Volek <vladimir.volek@satoshilabs.com>
2018-10-24 17:31:49 +0200

    Export resources while docker build - fix

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -4,8 +4,8 @@ cd "$(dirname "$0")"
 
 if [[ "$1" == "dev" || "$1" == 'beta' || "$1" == 'prod' ]]
     then
-        docker stop trezor-wallet
-        docker rm trezor-wallet
+        rm -rf ../build/$1
+        docker ps -q --filter "name=trezor-wallet" | grep -q . && docker stop trezor-wallet && docker rm -fv trezor-wallet
         docker build -t trezor-wallet ../ --build-arg BUILD_TYPE=$1
         docker run -p 8080:8080 -d --name trezor-wallet trezor-wallet:latest
         docker cp trezor-wallet:/trezor-wallet-app/build/$1 ../build/
