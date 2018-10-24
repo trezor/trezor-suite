1c0cf528e0e02f58716a37a86d82e73e93176d99
Szymon Lesisz <sz.lesisz@gmail.com>
2018-10-24 18:38:39 +0200

    docekrbuild script: add missing target directory

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -8,7 +8,7 @@ if [[ "$1" == "dev" || "$1" == 'beta' || "$1" == 'prod' ]]
         docker ps -q --filter "name=trezor-wallet" | grep -q . && docker stop trezor-wallet && docker rm -fv trezor-wallet
         docker build -t trezor-wallet ../ --build-arg BUILD_TYPE=$1
         docker run -p 8080:8080 -d --name trezor-wallet trezor-wallet:latest
-        docker cp trezor-wallet:/trezor-wallet-app/build/$1 ../build/
+        docker cp trezor-wallet:/trezor-wallet-app/build/$1 ../build/$1
         docker stop trezor-wallet
         docker rm trezor-wallet
     else
