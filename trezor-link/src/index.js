8cc00d30fbb3a08707e1a8ae91252ddc8b93a9e4
Karel Bilek <kb@karelbilek.com>
2018-11-08 13:26:55 +0700

    Remove bridgev1, extension

--- trezor-link/src/index.js
+++ trezor-link/src/index.js
@@ -4,11 +4,8 @@
 
 export type {Transport, AcquireInput, TrezorDeviceInfoWithSession, MessageFromTrezor} from './transport';
 
-import BridgeTransportV1 from './bridge/v1';
 import BridgeTransportV2 from './bridge/v2';
 import LowlevelTransportWithSharedConnections from './lowlevel/withSharedConnections';
-import ExtensionTransport from './extension';
-import ParallelTransport from './parallel';
 import FallbackTransport from './fallback';
 import WebUsbPlugin from './lowlevel/webusb';
 
@@ -17,18 +14,13 @@ import 'whatwg-fetch';
 if (typeof window === `undefined`) {
   // eslint-disable-next-line quotes
   const fetch = require('node-fetch');
-  BridgeTransportV1.setFetch(fetch, true);
   BridgeTransportV2.setFetch(fetch, true);
 } else {
-  BridgeTransportV1.setFetch(fetch, false);
   BridgeTransportV2.setFetch(fetch, false);
 }
 
 export default {
-  BridgeV1: BridgeTransportV1,
   BridgeV2: BridgeTransportV2,
-  Extension: ExtensionTransport,
-  Parallel: ParallelTransport,
   Fallback: FallbackTransport,
   Lowlevel: LowlevelTransportWithSharedConnections,
   WebUsb: WebUsbPlugin,
