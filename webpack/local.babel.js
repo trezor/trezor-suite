90b020e355d3037de1161aa804f5945598aa4baf
Szymon Lesisz <sz.lesisz@gmail.com>
2018-11-21 13:56:58 +0100

    exclude blockchain-link workers from webpack config

--- webpack/local.babel.js
+++ webpack/local.babel.js
@@ -50,7 +50,7 @@ module.exports = {
         rules: [
             {
                 test: /\.js?$/,
-                exclude: /node_modules/,
+                exclude: [/node_modules/, /blockchain-link\/build\/workers/],
                 use: ['babel-loader'],
             },
             {
