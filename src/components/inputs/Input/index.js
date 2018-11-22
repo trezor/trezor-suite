041c3ac42271d6b8335be51016510ff3eba81d62
Vladimir Volek <vladimir.volek@satoshilabs.com>
2018-11-22 16:25:03 +0100

    Revert "Turn off autocomplete"
    This reverts commit 8127a1a105f973d49286573727e16e16634ecdae.

--- src/components/inputs/Input/index.js
+++ src/components/inputs/Input/index.js
@@ -155,8 +155,12 @@ class Input extends PureComponent {
 
     render() {
         return (
-            <Wrapper className={this.props.className}>
-                {this.props.topLabel && <TopLabel>{this.props.topLabel}</TopLabel>}
+            <Wrapper
+                className={this.props.className}
+            >
+                {this.props.topLabel && (
+                    <TopLabel>{this.props.topLabel}</TopLabel>
+                )}
                 <InputWrapper>
                     <InputIconWrapper>
                         {this.props.state && (
@@ -168,9 +172,11 @@ class Input extends PureComponent {
                         <Overlay isPartiallyHidden={this.props.isPartiallyHidden} />
                         {this.props.icon}
                         <StyledInput
+                            trezorAction={this.props.trezorAction}
                             hasIcon={this.getIcon(this.props.state).length > 0}
                             innerRef={this.props.innerRef}
                             hasAddon={!!this.props.sideAddons}
+                            type={this.props.type}
                             color={this.getColor(this.props.state)}
                             placeholder={this.props.placeholder}
                             autocomplete={this.props.autocomplete}
@@ -178,13 +184,13 @@ class Input extends PureComponent {
                             autocapitalize={this.props.autocapitalize}
                             spellCheck={this.props.spellCheck}
                             value={this.props.value}
+                            readOnly={this.props.readOnly}
                             onChange={this.props.onChange}
                             onClick={this.props.autoSelect ? event => event.target.select() : null}
                             borderColor={this.getColor(this.props.state)}
                             disabled={this.props.isDisabled}
                             name={this.props.name}
                             data-lpignore="true"
-                            {...this.props}
                         />
                         <TrezorAction action={this.props.trezorAction}>
                             <ArrowUp />{this.props.trezorAction}
@@ -193,7 +199,9 @@ class Input extends PureComponent {
                     {this.props.sideAddons && this.props.sideAddons.map(sideAddon => sideAddon)}
                 </InputWrapper>
                 {this.props.bottomText && (
-                    <BottomText color={this.getColor(this.props.state)}>
+                    <BottomText
+                        color={this.getColor(this.props.state)}
+                    >
                         {this.props.bottomText}
                     </BottomText>
                 )}
@@ -229,7 +237,6 @@ Input.propTypes = {
 
 Input.defaultProps = {
     type: 'text',
-    autocomplete: false,
     autoSelect: false,
 };
 
