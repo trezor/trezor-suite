c1176d34de182abf007fadaa7cb58ea01e55dba4
Karel Bilek <kb@karelbilek.com>
2018-11-22 19:17:20 +0700

    Add webasm-disabled test

--- test/discover-account.js
+++ test/discover-account.js
@@ -1,4 +1,4 @@
-/* global it:false, describe:false */
+/* global it:false, describe:false, WebAssembly:true */
 
 import {MockBitcore} from '../src/mock-bitcore';
 import {WorkerDiscovery} from '../src/discovery/worker-discovery';
@@ -6,27 +6,56 @@ import fixtures from './fixtures/discover-account.json';
 
 import {discoveryWorkerFactory, xpubWorker, xpubFilePromise} from './_worker-helper';
 
-describe('discover account', () => {
-    fixtures.forEach(fixture => {
-        it(fixture.name, function (done) {
-            this.timeout(30 * 1000);
+discoverAccount(true);
+discoverAccount(false);
 
-            const blockchain = new MockBitcore(fixture.spec, done);
-            const discovery = new WorkerDiscovery(discoveryWorkerFactory, xpubWorker, xpubFilePromise, blockchain);
-            const stream = discovery.discoverAccount(
-                fixture.start,
-                fixture.xpub,
-                fixture.network,
-                fixture.segwit,
-                fixture.cashaddr,
-                fixture.gap,
-                fixture.timeOffset,
-            );
-            stream.ending.then((res) => {
-                if (!blockchain.errored) {
-                    if (JSON.stringify(res) !== JSON.stringify(fixture.end)) {
-                        console.log('Discovery result', JSON.stringify(res, null, 2));
-                        console.log('Fixture', JSON.stringify(fixture.end, null, 2));
+function discoverAccount(enableWebassembly) {
+    const desc = enableWebassembly ? ' wasm' : ' no wasm';
+    describe('discover account' + desc, () => {
+        fixtures.forEach(fixture => {
+            it(fixture.name, function (done_orig) {
+                this.timeout(30 * 1000);
+                let wasm_old;
+                if (!enableWebassembly) {
+                    wasm_old = WebAssembly;
+                    WebAssembly = undefined;
+                }
+                const done = (x) => {
+                    if (!enableWebassembly) {
+                        WebAssembly = wasm_old;
+                    }
+										done_orig(x);
+                };
+                const blockchain = new MockBitcore(fixture.spec, done);
+                const discovery = new WorkerDiscovery(discoveryWorkerFactory, xpubWorker, xpubFilePromise, blockchain);
+                const stream = discovery.discoverAccount(
+                    fixture.start,
+                    fixture.xpub,
+                    fixture.network,
+                    fixture.segwit,
+                    fixture.cashaddr,
+                    fixture.gap,
+                    fixture.timeOffset,
+                );
+                stream.ending.then((res) => {
+                    if (!blockchain.errored) {
+                        if (JSON.stringify(res) !== JSON.stringify(fixture.end)) {
+                            console.log('Discovery result', JSON.stringify(res, null, 2));
+                            console.log('Fixture', JSON.stringify(fixture.end, null, 2));
+                            done(new Error('Result not the same'));
+                        } else {
+                            if (blockchain.spec.length > 0) {
+                                console.log(JSON.stringify(blockchain.spec));
+                                done(new Error('Some spec left on end'));
+                            } else {
+                                done();
+                            }
+                        }
+                    }
+                }, (err) => {
+                    if (!(err.startsWith(fixture.endError))) {
+                        console.log('Discovery result', JSON.stringify(err, null, 2));
+                        console.log('Fixture', JSON.stringify(fixture.endError, null, 2));
                         done(new Error('Result not the same'));
                     } else {
                         if (blockchain.spec.length > 0) {
@@ -36,21 +65,8 @@ describe('discover account', () => {
                             done();
                         }
                     }
-                }
-            }, (err) => {
-                if (!(err.startsWith(fixture.endError))) {
-                    console.log('Discovery result', JSON.stringify(err, null, 2));
-                    console.log('Fixture', JSON.stringify(fixture.endError, null, 2));
-                    done(new Error('Result not the same'));
-                } else {
-                    if (blockchain.spec.length > 0) {
-                        console.log(JSON.stringify(blockchain.spec));
-                        done(new Error('Some spec left on end'));
-                    } else {
-                        done();
-                    }
-                }
+                });
             });
         });
     });
-});
+}
