5605f967701b36c41ef32a59e450ca6b19f41cd2
slowbackspace <slowbackspace@gmail.com>
2018-12-06 19:33:39 +0100

    pass isReceiveAddress to Input component to ignore readOnly styles

--- src/views/Wallet/views/Account/Receive/index.js
+++ src/views/Wallet/views/Account/Receive/index.js
@@ -120,6 +120,7 @@ const AccountReceive = (props: Props) => {
                     <Row>
                         <Input
                             type="text"
+                            isReceiveAddress
                             readOnly
                             autoSelect
                             value={address}
