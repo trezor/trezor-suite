21b1e9c93fae178512fa97ded6d3a2e4bccb27ee
Szymon Lesisz <sz.lesisz@gmail.com>
2018-12-06 10:23:50 +0100

    UI: new implementation

--- src/ui/index.ui.js
+++ src/ui/index.ui.js
@@ -17,51 +17,21 @@ const getInputValue = (id: string): string => {
     return value;
 }
 
-const instances = [
-    {
-        name: 'Ripple Testnet',
-        worker: 'js/ripple-worker.js',
-        server: [
-            'wss://foo1.bar',
-            'wss://foo2.bar',
-            'wss://s.altnet.rippletest.net',
-        ],
-        debug: true
-    },
-    // {
-    //     name: 'Ripple Mainnet',
-    //     worker: 'js/ripple-worker.js',
-    //     server: 'wss://s2.ripple.com:443',
-    // },
-    {
-        name: 'Bitcoin Testnet',
-        worker: 'js/blockbook-worker.js',
-        server: 'wss://blockbook-dev.corp:19136',
-        // server: 'https://testnet-bitcore1.trezor.io',
-        // server: 'wss://ropsten1.trezor.io/socket.io/?transport=websocket',
-        debug: true
-    },
-];
-
-BlockchainLink.create(instances);
-
-
-
 const handleClick = (event: MouseEvent) => {
     const target: HTMLElement = (event.target: any);
     if (target.nodeName !== 'BUTTON') return;
     const network: string = getInputValue('network-type');
 
     // const blockchain: Blockchain<Ripple> = BlockchainLink.get('Ripple Testnet');
-    const blockchain = BlockchainLink.get(network);
-    if (blockchain.listenerCount('block') < 1) {
-        blockchain.on('block', handleNotification);
-        blockchain.on('address', handleNotification);
-    }
+    const blockchain = instances.find(b => b.settings.name === network);
+    if (!blockchain) return;
 
     switch (target.id) {
         case 'get-info':
             blockchain.getInfo().then(handleResponse).catch(handleError);
+            break;
+        case 'disconnect':
+            blockchain.disconnect().then(handleResponse).catch(handleError);
             break;
 
         case 'get-account-info': {
@@ -96,14 +66,14 @@ const handleClick = (event: MouseEvent) => {
 
         case 'subscribe-address':
             blockchain.subscribe({
-                type: 'address',
+                type: 'notification',
                 addresses: getInputValue('subscribe-addresses').split(","),
             }).then(handleResponse).catch(handleError);
             break;
 
         case 'unsubscribe-address':
             blockchain.unsubscribe({
-                type: 'address',
+                type: 'notification',
                 addresses: getInputValue('subscribe-addresses').split(","),
             }).then(handleResponse).catch(handleError);
             break;
@@ -124,16 +94,20 @@ const handleResponse = (response: any) => {
     element.innerHTML = JSON.stringify(response, null , 2);
 }
 
+const handleError = (error: any) => {
+    const element = (document.getElementById('response'): any);
+    element.innerHTML = JSON.stringify(error.message, null , 2);
+}
+
 const handleNotification = (notification: any) => {
+    console.warn("HANLDE NOTIF", notification)
     const value = JSON.stringify(notification, null, 2);
     const element = (document.getElementById('notification'): any);
     element.innerHTML = `${value},\n${element.innerHTML}`;
 }
 
-const handleError = (error: any) => {
-    const element = (document.getElementById('response'): any);
-    element.innerHTML = JSON.stringify(error.message, null , 2);
-}
+
+// utils
 
 const clear = (id: string) => {
     const element = (document.getElementById(id): any);
@@ -150,6 +124,44 @@ const prepareNetworkSelect = (instances: Array<any>, selected: string) => {
         }
     })
 }
-prepareNetworkSelect(instances, 'Ripple Testnet');
+
+const config = [
+    {
+        name: 'Ripple Testnet',
+        worker: 'js/ripple-worker.js',
+        server: [
+            //'wss://foo1.bar',
+            //'wss://foo2.bar',
+            'wss://s.altnet.rippletest.net',
+        ],
+        debug: true
+    },
+    // {
+    //     name: 'Ripple Mainnet',
+    //     worker: 'js/ripple-worker.js',
+    //     server: 'wss://s2.ripple.com:443',
+    // },
+    {
+        name: 'Bitcoin Testnet',
+        worker: 'js/blockbook-worker.js',
+        server: ['wss://blockbook-dev.corp:19136'],
+        // server: 'https://testnet-bitcore1.trezor.io',
+        // server: 'wss://ropsten1.trezor.io/socket.io/?transport=websocket',
+        debug: true
+    },
+];
+
+prepareNetworkSelect(config, 'Ripple Testnet');
+
+const instances: Array<BlockchainLink> = [];
+config.forEach(i => {
+    const b = new BlockchainLink(i);
+    b.on('connected', handleNotification);
+    b.on('disconnected', () => console.log("---DISCONNECETD"));
+    b.on('error', handleNotification);
+    b.on('block', handleNotification);
+    b.on('notification', handleNotification);
+    instances.push(b);
+});
 
 document.addEventListener('click', handleClick);
