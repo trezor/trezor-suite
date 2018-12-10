5a066399a103bf2cf7ca5ab9035bd9921e858099
Vladimir Volek <vladimir.volek@satoshilabs.com>
2018-12-10 11:38:45 +0100

    Added lib and more tests

--- src/test/main.firmware.test.js
+++ src/test/main.firmware.test.js
@@ -1,7 +1,7 @@
 import { getLatestSafeFw } from 'main';
 
 describe('Get latest safe firmware version for T1 in normal mode', () => {
-    it('test firmware [10, 10, 10]', () => {
+    it('test firmware higher version bump', () => {
         const result = getLatestSafeFw({
             releasesList: [
                 { version: [3, 0, 0], min_firmware_version: [3, 0, 0] },
@@ -15,7 +15,7 @@ describe('Get latest safe firmware version for T1 in normal mode', () => {
         expect(result).toEqual([]);
     });
 
-    it('test firmware [2, 0, 0]', () => {
+    it('test single version bump', () => {
         const result = getLatestSafeFw({
             releasesList: [
                 { version: [3, 0, 0], min_firmware_version: [3, 0, 0] },
@@ -29,17 +29,18 @@ describe('Get latest safe firmware version for T1 in normal mode', () => {
         expect(result.version).toEqual([3, 0, 0]);
     });
 
-    it('test firmware [3, 0, 0]', () => {
+    it('test firmware multiple version bump', () => {
         const result = getLatestSafeFw({
             releasesList: [
-                { version: [3, 0, 0], min_firmware_version: [3, 0, 0] },
+                { version: [4, 0, 0], min_firmware_version: [3, 0, 0] },
+                { version: [3, 0, 0], min_firmware_version: [2, 0, 0] },
                 { version: [2, 0, 0], min_firmware_version: [2, 0, 0] },
                 { version: [1, 0, 0], min_firmware_version: [1, 0, 0] },
             ],
             isInBootloader: false,
-            firmwareVersion: [3, 0, 0],
+            firmwareVersion: [2, 0, 0],
         });
 
-        expect(result).toEqual([]);
+        expect(result.version).toEqual([4, 0, 0]);
     });
 });
