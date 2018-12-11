fcbf0d7aeea4fbc441b70141b9d32e924fb64c06
Szymon Lesisz <sz.lesisz@gmail.com>
2018-12-11 17:11:17 +0100

    add Tezos tx from example

--- packages/connect-explorer/src/js/reducers/methods/TezosSignTxReducer.js
+++ packages/connect-explorer/src/js/reducers/methods/TezosSignTxReducer.js
@@ -16,20 +16,14 @@ type MethodState = {
 const defaultOp: string = 
 `{
     transaction: {
-        source: {
-            tag: 0,
-            hash: "00001e65c88ae6317cd62a638c8abd1e71c83c8475",
-        },
-        fee: 0,
-        counter: 108925,
-        gas_limit: 200,
-        storage_limit: 0,
-        amount: 10000,
-        destination: {
-            tag: 0,
-            hash: "0004115bce5af2f977acbb900f449c14c53e1d89cf",
-        },
-    },
+        source: "tz1ckrgqGGGBt4jGDmwFhtXc1LNpZJUnA9F2",
+        destination: "tz1cTfmc5uuBr2DmHDgkXTAoEcufvXLwq5TP",
+        counter: 20449,
+        amount: 1000000000,
+        fee: 10000,
+        gas_limit: 11000,
+        storage_limit: 277
+    }
 }`;
 
 const initialState: MethodState = {
@@ -37,7 +31,7 @@ const initialState: MethodState = {
     fields: ['path', 'branch', 'operation'],
 
     path: "m/44'/1729'/0'",
-    branch: "f2ae0c72fdd41d7a89bebfe8d6dd6d38e0fcd0782adb8194717176eb70366f64",
+    branch: "BLHRTdZ5vUKSDbkp5vcG1m6ZTST4SRiHWUhGodysLTbvACwi77d",
     operation: defaultOp,
 };
 
