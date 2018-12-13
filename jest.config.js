4cccbb9daf641eb983c5c1a6feb2e1a3874f19f0
Vladimir Volek <vladimir.volek@satoshilabs.com>
2018-12-13 13:42:22 +0100

    coverage must be at least 100%

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,14 @@ module.exports = {
     transform: {
         '^.+\\.(js|jsx|ts)$': 'babel-jest',
     },
+    coverageThreshold: {
+        global: {
+            branches: 100,
+            functions: 100,
+            lines: 100,
+            statements: 100,
+        },
+    },
     automock: false,
     coverageDirectory: './coverage',
     collectCoverage: true,
