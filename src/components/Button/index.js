e4d2a5ec72dc55630e9ce9cf4436e8bd4a856186
slowbackspace <slowbackspace@gmail.com>
2018-12-13 13:29:46 +0100

    Revert "changed FONT_SIZE.BASE to 1rem"
    This reverts commit e4d2b8ec026cd1ddc8c995d93537208c8c63d1d8.

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -22,7 +22,7 @@ type Props = {
 const Wrapper = styled.button`
     padding: ${props => (props.icon ? '4px 24px 4px 15px' : '11px 24px')};
     border-radius: 3px;
-    font-size: ${FONT_SIZE.BASE};
+    font-size: ${FONT_SIZE.SMALL};
     font-weight: ${FONT_WEIGHT.LIGHT};
     cursor: pointer;
     background: ${colors.GREEN_PRIMARY};
