9d57587e9445aec0b0a601d5eef7e15a20bcfcbe
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-01-09 15:58:04 +0100

    Initial commit

---
+++ config/colors.js
@@ -0,0 +1,3 @@
+export default {
+    BLACK: '#000000'
+}
---
+++ stories/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Button } from '../components/Button';
+
+storiesOf('Button', module)
+    .add('with text', () => (
+        <Button>Hello Button</Button>
+    ))
