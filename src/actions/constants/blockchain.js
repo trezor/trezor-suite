1b1fbb45f5053dc63b52bc4ed0735d515e0a1262
slowbackspace <slowbackspace@gmail.com>
2019-01-14 02:46:41 +0100

    add connecting flag to blockchain's state

--- src/actions/constants/blockchain.js
+++ src/actions/constants/blockchain.js
@@ -1,4 +1,5 @@
 /* @flow */
 
+export const START_SUBSCRIBE: 'blockchain__start_subscribe' = 'blockchain__start_subscribe';
 export const READY: 'blockchain__ready' = 'blockchain__ready';
 export const UPDATE_FEE: 'blockchain__update_fee' = 'blockchain__update_fee';
