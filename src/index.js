15a1af2e54bbf58fa3f91db3614241dbfba86b34
Jan Czibula <>
2019-01-29 13:53:25 +0100

    add babel-preset-es2015

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 'use strict';
 
-exports.__esModule = true;
-
-import ButtonText from 'components/buttons/ButtonText';
+var ButtonText = require('./components/buttons/ButtonText');
 
 module.exports = {
     ButtonText,
