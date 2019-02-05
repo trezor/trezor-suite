99d61d66705a30bf0763c9d402e164cd24429ed6
Jan Czibula <>
2019-02-05 12:15:15 +0100

    Revert "build with webpack"
    This reverts commit 279005722c12d265b0b385d6d6934460a056e2fa.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import colors from './config/colors';
 import icons from './config/icons';
+import animations from './config/animations';
+import variables from './config/variables';
 
 import ButtonText from './components/buttons/ButtonText';
 import ButtonWebUSB from './components/buttons/ButtonWebUSB';
@@ -33,6 +35,8 @@ import P from './components/Paragraph';
 module.exports = {
 	colors,
     icons,
+    animations,
+	variables,
     ButtonText,
     ButtonWebUSB,
     Modal,
