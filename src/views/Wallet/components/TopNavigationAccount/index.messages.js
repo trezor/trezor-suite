1cd92a9872979cc0e8827be2f727ae8fd0a15396
slowbackspace <slowbackspace@gmail.com>
2019-02-22 13:06:35 +0100

    fix ids

--- src/views/Wallet/components/TopNavigationAccount/index.messages.js
+++ src/views/Wallet/components/TopNavigationAccount/index.messages.js
@@ -4,19 +4,19 @@ import type { Messages } from 'flowtype/npm/react-intl';
 
 const definedMessages: Messages = defineMessages({
     TR_NAV_SUMMARY: {
-        id: 'TR_SUMMARY',
+        id: 'TR_NAV_SUMMARY',
         defaultMessage: 'Summary',
     },
     TR_NAV_RECEIVE: {
-        id: 'TR_RECEIVE',
+        id: 'TR_NAV_RECEIVE',
         defaultMessage: 'Receive',
     },
     TR_NAV_SEND: {
-        id: 'TR_SEND',
+        id: 'TR_NAV_SEND',
         defaultMessage: 'Send',
     },
     TR_NAV_SIGN_AND_VERIFY: {
-        id: 'TR_SIGN_AND_VERIFY',
+        id: 'TR_NAV_SIGN_AND_VERIFY',
         defaultMessage: 'Sign & Verify',
     },
 });
