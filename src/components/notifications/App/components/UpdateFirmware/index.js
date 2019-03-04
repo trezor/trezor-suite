5b18a562f1d239af62dca3efdd3f31f4def353b4
slowbackspace <slowbackspace@gmail.com>
2019-03-04 16:55:40 +0100

    don't show fw update notification when user is on firmware update page

--- src/components/notifications/App/components/UpdateFirmware/index.js
+++ src/components/notifications/App/components/UpdateFirmware/index.js
@@ -2,15 +2,24 @@
 import * as React from 'react';
 import Notification from 'components/Notification';
 import l10nCommonMessages from 'views/common.messages';
+import { withRouter } from 'react-router-dom';
+import { matchPath } from 'react-router';
+import { getPattern } from 'support/routes';
 import type { Props } from '../../index';
 
 import l10nMessages from './index.messages';
 
-export default (props: Props) => {
+export default withRouter((props: Props & { location: any }) => {
     const { selectedDevice } = props.wallet;
     const outdated =
         selectedDevice && selectedDevice.features && selectedDevice.firmware === 'outdated';
+
     if (!outdated) return null;
+
+    // don't show notification when user is on firmware update page
+    if (matchPath(props.location.pathname, { path: getPattern('wallet-firmware-update') }))
+        return null;
+
     return (
         <Notification
             key="update-firmware"
@@ -27,4 +36,4 @@ export default (props: Props) => {
             ]}
         />
     );
-};
+});
