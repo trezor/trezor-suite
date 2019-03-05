1e28180d25f8714700e53acf729dae6166f60dbf
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-03-05 18:46:09 +0100

    cleanup

--- test/support/commands.js
+++ test/support/commands.js
@@ -1,5 +1,3 @@
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
 addMatchImageSnapshotCommand();
-
-Cypress.Commands.add('getTestElement', selector => cy.get(`[data-test="${selector}"]`));
--- test/support/index.js
+++ test/support/index.js
@@ -1,6 +1,1 @@
-import '@babel/polyfill';
 import './commands';
-
-beforeEach(() => {
-    window.localStorage.setItem('/betaModalPrivacy', true);
-});
