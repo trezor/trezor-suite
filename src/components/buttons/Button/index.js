7ffd6e44f4f0cb78c93d04d4496470c34e934d8b
slowbackspace <slowbackspace@gmail.com>
2019-03-07 14:00:22 +0100

    fix focus on disabled btn

--- src/components/buttons/Button/index.js
+++ src/components/buttons/Button/index.js
@@ -15,7 +15,7 @@ const Wrapper = styled.button`
     outline: none;
     background: ${colors.GREEN_PRIMARY};
     color: ${colors.WHITE};
-    border: 0;
+    border: 1px solid ${colors.GREEN_PRIMARY};
     transition: ${TRANSITION.HOVER};
 
     &:hover, &:focus {
@@ -26,22 +26,9 @@ const Wrapper = styled.button`
         box-shadow: ${colors.INPUT_FOCUS_SHADOW} 0px 0px 6px 0px;
     }
 
-    ${props => props.isInverse
-        && css`
-            background: transparent;
-            color: ${colors.GREEN_PRIMARY};
-            border: 1px solid ${colors.GREEN_PRIMARY};
-
-            &:hover, &:focus {
-                background: ${colors.GREEN_PRIMARY};
-                color: ${colors.WHITE};
-
-                &:before,
-                &:after {
-                    background: ${colors.WHITE};
-                }
-            }
-        `}
+    &:active {
+        background: ${colors.GREEN_TERTIARY};
+    }
 
     ${props => props.icon && css`
         position: relative;
@@ -57,19 +44,31 @@ const Wrapper = styled.button`
                 transition: ${TRANSITION.HOVER};   
             }
         }
+    `}
 
-        &:hover, &:focus {
-            svg {
-                path {
-                    fill: ${colors.WHITE};
+
+    ${props => props.isInverse && !props.isDisabled
+        && css`
+            background: transparent;
+            color: ${colors.GREEN_PRIMARY};
+            border: 1px solid ${colors.GREEN_PRIMARY};
+
+            &:hover, &:focus {
+                background: ${colors.GREEN_PRIMARY};
+                color: ${colors.WHITE};
+
+                &:before,
+                &:after {
+                    background: ${colors.WHITE};
+                }
+
+                svg {
+                    path {
+                        fill: ${colors.WHITE};
+                    }
                 }
             }
-        }
         `}
-
-    &:active {
-        background: ${colors.GREEN_TERTIARY};
-    }
 
     ${props => props.isDisabled
         && css`
@@ -77,6 +76,10 @@ const Wrapper = styled.button`
             color: ${colors.TEXT_SECONDARY};
             background: ${colors.GRAY_LIGHT};
             border: 1px solid ${colors.DIVIDER};
+
+            &:focus {
+                background: ${colors.GRAY_LIGHT};
+            }
 
             svg {
                 path {
@@ -117,7 +120,7 @@ const Wrapper = styled.button`
     ${props => props.isTransparent
         && css`
             background: transparent;
-            border: 0px;
+            border-color: transparent;
             color: ${colors.TEXT_SECONDARY};
 
             &:hover,
@@ -125,6 +128,7 @@ const Wrapper = styled.button`
             &:focus {
                 color: ${colors.TEXT_PRIMARY};
                 background: transparent;
+                box-shadow: none;
 
                 svg {
                     path {
