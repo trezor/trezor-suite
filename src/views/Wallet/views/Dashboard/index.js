25f1d4efcbfea521046bb96391283406567e094d
slowbackspace <slowbackspace@gmail.com>
2019-03-22 12:24:41 +0100

    auto generated clickable coinlogos in dashboard

--- src/views/Wallet/views/Dashboard/index.js
+++ src/views/Wallet/views/Dashboard/index.js
@@ -1,13 +1,13 @@
 /* @flow */
 import React from 'react';
 import styled from 'styled-components';
-import { connect } from 'react-redux';
 import Content from 'views/Wallet/components/Content';
-
+import { NavLink } from 'react-router-dom';
 import { CoinLogo, H4, P } from 'trezor-ui-components';
 
 import { FormattedMessage } from 'react-intl';
 import l10nMessages from './index.messages';
+import { Props } from './Container';
 
 const Wrapper = styled.div`
     display: flex;
@@ -30,17 +30,12 @@ const StyledP = styled(P)`
     }
 `;
 
-const Overlay = styled.div`
+const Coins = styled.div`
     display: flex;
-    width: 100%;
-    height: 40px;
-    justify-content: center;
-    align-items: center;
-    opacity: 0.2;
-    background: white;
+    flex-wrap: wrap;
 `;
 
-const StyledCoinLogo = styled(CoinLogo)`
+const StyledNavLink = styled(NavLink)`
     margin-right: 10px;
 
     &:last-child {
@@ -48,7 +43,27 @@ const StyledCoinLogo = styled(CoinLogo)`
     }
 `;
 
-const Dashboard = () => (
+const StyledCoinLogo = styled(CoinLogo)`
+    opacity: 0.7;
+    transition: opacity 0.2s ease-in-out;
+
+    &:hover {
+        opacity: 1;
+    }
+`;
+
+const getBaseUrl = device => {
+    let baseUrl = '';
+    if (device && device.features) {
+        baseUrl = `/device/${device.features.device_id}`;
+        if (device.instance) {
+            baseUrl += `:${device.instance}`;
+        }
+    }
+
+    return baseUrl;
+};
+const Dashboard = (props: Props) => (
     <Content>
         <Wrapper>
             <Row data-test="Dashboard__page__content">
@@ -58,16 +73,21 @@ const Dashboard = () => (
                 <StyledP>
                     <FormattedMessage {...l10nMessages.TR_YOU_WILL_GAIN_ACCESS} />
                 </StyledP>
-                <Overlay>
-                    <StyledCoinLogo network="eth" height={32} />
-                    <StyledCoinLogo network="xrp" height={32} />
-                </Overlay>
+                <Coins>
+                    {props.localStorage.config.networks.map(network => (
+                        <StyledNavLink
+                            key={network.shortcut}
+                            to={`${getBaseUrl(props.wallet.selectedDevice)}/network/${
+                                network.shortcut
+                            }/account/0`}
+                        >
+                            <StyledCoinLogo network={network.shortcut} height={32} />
+                        </StyledNavLink>
+                    ))}
+                </Coins>
             </Row>
         </Wrapper>
     </Content>
 );
 
-export default connect(
-    null,
-    null
-)(Dashboard);
+export default Dashboard
