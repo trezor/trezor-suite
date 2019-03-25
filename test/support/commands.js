ebcfdae680a90d6f34e4a1a6fb01e6d3f4556d81
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-03-25 16:09:56 +0100

    threshold adjust

--- test/support/commands.js
+++ test/support/commands.js
@@ -2,10 +2,8 @@
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
 addMatchImageSnapshotCommand({
-    failureThreshold: 0.03, // threshold for entire image
+    failureThreshold: 0.1, // threshold for entire image
     failureThresholdType: 'percent', // percent of image or number of pixels
-    customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
-    capture: 'viewport', // capture viewport in screenshot
 });
 
 Cypress.Commands.add('getTestElement', selector => cy.get(`[data-test="${selector}"]`));
