f9843a415b38664d81b0aeeb2752b327b4972597
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-03-25 12:57:03 +0100

    fix linting problems

--- scripts/build.js
+++ scripts/build.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import fs from 'fs-extra';
 import path from 'path';
 
--- test/support/commands.js
+++ test/support/commands.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 
 addMatchImageSnapshotCommand({
