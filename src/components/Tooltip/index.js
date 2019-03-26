5afd761152b71d952c0c178b231a5c5383e7d60d
slowbackspace <slowbackspace@gmail.com>
2019-03-26 09:52:59 +0100

    tippy tooltip

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -1,14 +1,10 @@
 import Link from 'components/Link';
 import PropTypes from 'prop-types';
-import RcTooltip from 'rc-tooltip';
+import { Tooltip as Tippy } from 'react-tippy';
 import React from 'react';
 import colors from 'config/colors';
+import { FONT_SIZE } from 'config/variables';
 import styled from 'styled-components';
-import tooltipStyles from './styles/Tooltip';
-
-const Wrapper = styled.div`
-    ${tooltipStyles};
-`;
 
 const Content = styled.div`
     max-width: ${props => `${props.maxWidth}px` || 'auto'};
@@ -16,6 +12,13 @@ const Content = styled.div`
 
 const ContentWrapper = styled.div`
     display: block;
+    margin: 4px;
+    background: ${colors.TOOLTIP_BACKGROUND};
+    color: white;
+    border-radius: 3px;
+    font-size: ${FONT_SIZE.SMALL};
+    /* min-height: 34px; */
+    padding: 8px 10px;
 `;
 
 const CTAWrapper = styled.div`
@@ -36,54 +39,24 @@ const StyledLink = styled(Link)`
     }
 `;
 
-class Tooltip extends React.Component {
-    constructor(props) {
-        super(props);
-        this.wrapper = null;
-    }
+const Tooltip = ({ maxWidth, placement, content, ctaText, ctaLink, children, ...rest }) => {
+    const Overlay = (
+        <ContentWrapper>
+            <Content maxWidth={maxWidth}>{content}</Content>
+            {ctaLink && (
+                <StyledLink isGray href={ctaLink}>
+                    <CTAWrapper>{ctaText}</CTAWrapper>
+                </StyledLink>
+            )}
+        </ContentWrapper>
+    );
 
-    render() {
-        const {
-            maxWidth,
-            className,
-            placement,
-            content,
-            ctaText,
-            ctaLink,
-            children,
-            ...rest
-        } = this.props;
-        const Overlay = (
-            <ContentWrapper>
-                <Content maxWidth={maxWidth}>{content}</Content>
-                {ctaLink && (
-                    <StyledLink isGray href={ctaLink}>
-                        <CTAWrapper>{ctaText}</CTAWrapper>
-                    </StyledLink>
-                )}
-            </ContentWrapper>
-        );
-
-        return (
-            <Wrapper
-                className={className}
-                ref={el => {
-                    this.wrapper = el;
-                }}
-            >
-                <RcTooltip
-                    getTooltipContainer={() => this.wrapper}
-                    arrowContent={<div className="rc-tooltip-arrow-inner" />}
-                    placement={placement}
-                    overlay={() => Overlay}
-                    {...rest}
-                >
-                    {children}
-                </RcTooltip>
-            </Wrapper>
-        );
-    }
-}
+    return (
+        <Tippy position={placement} html={Overlay} offset={4} interactive {...rest}>
+            {children}
+        </Tippy>
+    );
+};
 
 Tooltip.propTypes = {
     className: PropTypes.string,
