ec2fc258f40335a56f98f1e389d0d7fd73167baa
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-04-02 13:56:21 +0200

    Fix test

--- tests/index.js
+++ tests/index.js
@@ -34,7 +34,6 @@ describe('Ripple call', () => {
 
     it('Get fee', async () => {
         const result = await blockchain.estimateFee();
-        // expect(result).to.deep.equal({ id: 0, type: RESPONSES.GET_FEE, payload: '12' });
-        expect(result).to.equal('12');
+        expect(result).to.deep.equal([{ name: 'Normal', value: '12' }]);
     });
 });
