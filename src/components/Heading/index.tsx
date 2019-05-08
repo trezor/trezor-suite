6dcc804d94b85ae57860b05a2d7f478328981f63
slowbackspace <slowbackspace@gmail.com>
2019-05-08 18:24:34 +0200

    Heading ts

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -1,16 +1,14 @@
 import styled, { css } from 'styled-components';
-import PropTypes from 'prop-types';
 
 import { FONT_SIZE } from 'config/variables';
 import colors from 'config/colors';
 
-const Props = {
-    children: PropTypes.node,
-    textAlign: PropTypes.oneOf(['left', 'center', 'right', 'justify']),
-};
+interface Props {
+    textAlign: 'left' | 'center' | 'right' | 'justify';
+}
 
 const textAlignStyle = css`
-    text-align: ${props => props.textAlign};
+    text-align: ${(props: Props) => props.textAlign};
 `;
 
 const baseStyles = css`
@@ -20,50 +18,43 @@ const baseStyles = css`
     font-weight: bold;
     margin: 0;
     padding: 0;
-    ${props => props.textAlign && textAlignStyle}
+    ${(props: Props) => props.textAlign && textAlignStyle}
 `;
 
-const H1 = styled.h1`
+const H1 = styled.h1<Props>`
     ${baseStyles};
     font-size: ${FONT_SIZE.H1};
     padding-bottom: 10px;
 `;
 
-const H2 = styled.h2`
+const H2 = styled.h2<Props>`
     ${baseStyles};
     font-size: ${FONT_SIZE.H2};
     padding-bottom: 10px;
 `;
 
-const H3 = styled.h3`
+const H3 = styled.h3<Props>`
     ${baseStyles};
     font-size: ${FONT_SIZE.H3};
     padding-bottom: 10px;
 `;
 
-const H4 = styled.h4`
+const H4 = styled.h4<Props>`
     ${baseStyles};
     font-size: ${FONT_SIZE.H4};
     padding-bottom: 10px;
 `;
 
-const H5 = styled.h5`
+const H5 = styled.h5<Props>`
     ${baseStyles};
     font-size: ${FONT_SIZE.H5};
     padding-bottom: 10px;
 `;
 
-const H6 = styled.h6`
+const H6 = styled.h6<Props>`
     ${baseStyles};
     font-size: ${FONT_SIZE.H6};
     padding-bottom: 10px;
 `;
-
-H1.propTypes = Props;
-H2.propTypes = Props;
-H3.propTypes = Props;
-H4.propTypes = Props;
-H5.propTypes = Props;
-H6.propTypes = Props;
 
 export { H1, H2, H3, H4, H5, H6 };
