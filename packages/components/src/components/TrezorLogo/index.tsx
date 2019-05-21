c8d6577f5fcb7bf57712864a166a00a97c50a945
Jan Czibula <>
2019-05-21 13:21:08 +0200

    #24 add trezor-logos

---
+++ packages/components/src/components/TrezorLogo/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Img = styled.img<Props>``;
+
+interface Props extends React.ImgHTMLAttributes<HTMLImageElement> {
+    type: string;
+}
+
+const TrezorLogo = ({ type, ...rest }: Props) => {
+    // $FlowIssue: `require` must be a string literal.
+	const src = require(`../../images/trezor_logo_${type}_black.svg`); // eslint-disable-line
+    return <Img type={type} src={src} {...rest} />;
+};
+
+TrezorLogo.propTypes = {
+    type: PropTypes.oneOf(['horizontal', 'vertical']),
+};
+
+export default TrezorLogo;
