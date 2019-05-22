a4acde91af149ee7ae2b2475560d9a9795a76a53
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-05-22 15:30:43 +0200

    fix test url

--- packages/components/test/integration/TrezorLogo.test.js
+++ packages/components/test/integration/TrezorLogo.test.js
@@ -1,12 +1,10 @@
 describe('TrezorLogo', () => {
     beforeEach(() => {
         cy.viewport(1024, 768);
-        cy.visit(
-            'http://localhost:9001/iframe.html?selectedKind=Other&selectedStory=TrezorLogo&full=0'
-        );
+        cy.visit('/iframe.html?selectedKind=Other&selectedStory=TrezorLogo&full=0');
     });
 
-     it(`TrezorLogo`, () => {
+    it(`TrezorLogo`, () => {
         cy.getTestElement('trezor_logo')
             .should('be.visible')
             .matchImageSnapshot();
