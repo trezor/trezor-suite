b17052348f7e10275af874cac8377fa5c906d106
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-05-22 15:31:43 +0200

    fix test url

--- packages/components/test/integration/LanguagePicker.test.js
+++ packages/components/test/integration/LanguagePicker.test.js
@@ -1,9 +1,7 @@
 describe('LanguagePicker', () => {
     beforeEach(() => {
         cy.viewport(1024, 768);
-        cy.visit(
-            'http://localhost:9001/iframe.html?selectedKind=Other&selectedStory=LanguagePicker&full=0'
-        );
+        cy.visit('/iframe.html?selectedKind=Other&selectedStory=LanguagePicker&full=0');
     });
 
     it(`LanguagePicker`, () => {
