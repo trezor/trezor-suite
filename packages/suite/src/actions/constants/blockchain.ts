4151032fce02d737cebaf8d157f66c0c54c10700
Szymon Lesisz <sz.lesisz@gmail.com>
2019-05-28 14:00:52 +0200

    move constants from wallet

---
+++ packages/suite/src/actions/constants/blockchain.ts
@@ -0,0 +1,4 @@
+export const READY = '@blockchain/ready';
+export const START_SUBSCRIBE = '@blockchain/start_subscribe';
+export const FAIL_SUBSCRIBE = '@blockchain/fail_subscribe';
+export const UPDATE_FEE = '@blockchain/update_fee';
--- packages/wallet/src/actions/constants/localStorage.js
+++
@@ -1,5 +0,0 @@
-/* @flow */
-
-export const SAVE: 'storage__save' = 'storage__save';
-export const READY: 'storage__ready' = 'storage__ready';
-export const ERROR: 'storage__error' = 'storage__error';
