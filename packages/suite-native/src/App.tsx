134d1338563b3c961dd08205ed3dd0f0ae7fcf49
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-05-28 17:05:34 +0200

    add more config

--- packages/suite-native/src/App.tsx
+++ packages/suite-native/src/App.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { Platform } from 'react-native';
 import { Provider } from 'react-redux';
 import './support/global';
+import Preloader from '@suite/components/Preloader';
 import RouterHandler from './support/RouterHandler';
 
 import { initStore } from './reducers/store';
-import Preloader from '@suite/components/Preloader';
 
 const TrezorSuite = (props: any) => {
     process.env.RN_EMULATOR = props.isEmulator;
