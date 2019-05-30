28f8174bf3efddcf200cbbc48a65ea2e1ee0a367
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-05-30 12:39:24 +0200

    improve snapshot testing

--- packages/components/test/support/commands.js
+++ packages/components/test/support/commands.js
@@ -4,6 +4,8 @@ import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command';
 addMatchImageSnapshotCommand({
     failureThreshold: 0.01, // threshold for entire image
     failureThresholdType: 'percent', // percent of image or number of pixels
+    customSnapshotsDir: './test/integration/snapshots',
+    customDiffDir: './test/integration/diffs',
 });
 
 Cypress.Commands.add('getTestElement', selector => cy.get(`[data-test="${selector}"]`));
