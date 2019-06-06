958ba237a59c206613edb433713c87159b590142
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-06-06 13:08:00 +0200

    load version page in preloader

--- packages/suite/src/components/Preloader.tsx
+++ packages/suite/src/components/Preloader.tsx
@@ -4,11 +4,14 @@ import { State, Dispatch } from '@suite/types';
 import { Loader } from '@trezor/components';
 import { View, StyleSheet } from 'react-native';
 import { SUITE } from '@suite/actions/constants';
+import VersionPage from '@suite/views/version';
+import Layout from '@suite/components/Layout';
 import SuiteWrapper from '../views';
 
 interface Props {
     loaded: State['suite']['loaded'];
     dispatch: Dispatch;
+    router: State['router'];
 }
 
 const styles = StyleSheet.create({
@@ -20,23 +23,35 @@ const styles = StyleSheet.create({
 });
 
 const Preloader: React.FunctionComponent<Props> = props => {
-    const { loaded, dispatch } = props;
+    const { loaded, dispatch, router } = props;
     useEffect(() => {
-        if (!loaded) {
+        if (router.pathname !== '/version' || !loaded) {
             dispatch({ type: SUITE.INIT });
         }
-    }, [dispatch, loaded]);
-    return !loaded ? (
-        <View style={styles.wrapper}>
-            <Loader text="Loading" size={100} strokeWidth={1} />
-        </View>
-    ) : (
-        <SuiteWrapper>{props.children}</SuiteWrapper>
-    );
+    }, [dispatch, loaded, router.pathname]);
+
+    if (router.pathname === '/version') {
+        return (
+            <Layout isLanding>
+                <VersionPage />
+            </Layout>
+        );
+    }
+
+    if (!loaded) {
+        return (
+            <View style={styles.wrapper}>
+                <Loader text="Loading" size={100} strokeWidth={1} />
+            </View>
+        );
+    }
+
+    return <SuiteWrapper>{props.children}</SuiteWrapper>;
 };
 
 const mapStateToProps = (state: State) => ({
     loaded: state.suite.loaded,
+    router: state.router,
 });
 
 export default connect(mapStateToProps)(Preloader);
