04984d560d818f1bbe6160da3b8d790aa4a6723c
slowbackspace <slowbackspace@gmail.com>
2019-06-07 12:11:48 +0200

    add getRoute to utils

--- packages/suite/src/utils/router.ts
+++ packages/suite/src/utils/router.ts
@@ -16,3 +16,30 @@ export const getParams = (url: string) => {
     });
     return params;
 };
+
+export interface Route {
+    name: string;
+    pattern: string;
+    fields?: string[];
+}
+
+export const routes: Route[] = [
+    {
+        name: 'suite-device-settings',
+        pattern: '/settings',
+    },
+    {
+        name: 'wallet-settings',
+        pattern: '/wallet/settings',
+    },
+];
+
+export const getRoute = (name: string): string => {
+    const entry = routes.find(r => r.name === name);
+    if (!entry) {
+        // eslint-disable-next-line no-console
+        console.error(`Route for ${name} not found`);
+        return '/';
+    }
+    return entry.pattern;
+};
