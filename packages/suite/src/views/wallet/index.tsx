5bbedb12691b28f2c4de226814746c9899ea72bc
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-06-07 17:12:30 +0200

    add basic components

--- packages/suite/src/views/wallet/index.tsx
+++ packages/suite/src/views/wallet/index.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import { connect } from 'react-redux';
 
 import { Text } from 'react-native';
 import { Button } from '@trezor/components';
 import { goto } from '@suite-actions/routerActions';
 
+import CoinMenu from '@wallet-components/CoinMenu';
+
+const Wrapper = styled.div``;
+
 const Wallet = () => {
     return (
-        <>
+        <Wrapper>
+            <CoinMenu />
             <Text>Wallet homepage</Text>
             <Button variant="success" onClick={() => goto('/wallet/account#/eth/1')}>
                 Ethereum
@@ -19,7 +25,7 @@ const Wallet = () => {
             <Button variant="success" onClick={() => goto('/wallet/settings')}>
                 Settings
             </Button>
-        </>
+        </Wrapper>
     );
 };
 
