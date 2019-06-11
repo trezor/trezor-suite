ce8f5432ea513dab2c52359eff575b7fc17fc736
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-06-11 14:33:40 +0200

    remove duplicated prop

--- packages/suite/src/components/wallet/TopNavigation/index.tsx
+++ packages/suite/src/components/wallet/TopNavigation/index.tsx
@@ -14,7 +14,6 @@ const { FONT_WEIGHT, FONT_SIZE, SCREEN_SIZE } = variables;
 const Wrapper = styled.div`
     display: flex;
     width: 100%;
-    height: 100%;
     align-items: center;
     overflow-y: hidden;
     overflow-x: auto;
