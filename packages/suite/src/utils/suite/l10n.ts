a28472d57710e06be5f20784f2133741f00364e0
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-06-12 18:33:19 +0200

    fix unit test

--- packages/suite/src/utils/suite/l10n.ts
+++ packages/suite/src/utils/suite/l10n.ts
@@ -1,11 +1,10 @@
-import { LANGUAGES } from '@suite-config/app';
-import { Language } from '@suite-types/languages';
+import suiteConfig from '@suite-config/index';
 
 export const getInitialLocale = (navigatorLanguage: string, defaultLocale: string = 'en') => {
     if (!navigatorLanguage) return defaultLocale;
 
     const browserLocale = navigatorLanguage.split('-')[0];
-    if (LANGUAGES.some((e: Language) => e.code === browserLocale)) {
+    if (suiteConfig.languages.some(e => e.code === browserLocale)) {
         // Array of supported languages contains the locale we're looking for
         return browserLocale;
     }
