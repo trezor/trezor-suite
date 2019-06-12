928ff1efc74a0ee0800a02bd622591243ea85b62
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-06-12 18:23:03 +0200

    remove leftover

--- packages/suite/src/support/suite/nextjs.ts
+++
@@ -1,9 +0,0 @@
-import { join } from 'path';
-
-export const resolveStaticPath = (path: string) => {
-    if (process.env.assetPrefix) {
-        return join(process.env.assetPrefix, path);
-    }
-
-    return path;
-};
