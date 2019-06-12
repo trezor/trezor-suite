434f68e23e34abebf5f421c8604f26cd7b5fb8f4
slowbackspace <slowbackspace@gmail.com>
2019-06-12 15:19:02 +0200

    layout with coinmenu

--- packages/suite/src/components/wallet/Layout/index.tsx
+++ packages/suite/src/components/wallet/Layout/index.tsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { State } from '@suite-types/index';
 import styled from 'styled-components';
 import TopNavigation from '@wallet-components/TopNavigation';
+import CoinMenu from '@wallet-components/CoinMenu';
 
 interface Props {
     router: State['router'];
@@ -10,15 +11,46 @@ interface Props {
     children: React.ReactNode;
 }
 
-const WalletWrapper = styled.div`
+const Wrapper = styled.div`
+    display: flex;
+    width: 100%;
+    max-width: 1170px;
+    flex-direction: row;
+    flex: 1 1 0%;
+`;
+
+const Content = styled.div`
     padding: 20px 35px;
 `;
 
+const Sidebar = styled.div`
+    display: flex;
+    width: 320px;
+    position: relative;
+    overflow-y: auto;
+    border-top-left-radius: 4px;
+    overflow-x: hidden;
+    background: rgb(251, 251, 251);
+    border-right: 1px solid rgb(227, 227, 227);
+`;
+
+const ContentWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    flex: 1 1 0%;
+    overflow: auto;
+`;
+
 const Layout = (props: Props) => (
-    <>
-        <TopNavigation />
-        <WalletWrapper>{props.children}</WalletWrapper>
-    </>
+    <Wrapper>
+        <Sidebar>
+            <CoinMenu />
+        </Sidebar>
+        <ContentWrapper>
+            <TopNavigation />
+            <Content>{props.children}</Content>
+        </ContentWrapper>
+    </Wrapper>
 );
 
 const mapStateToProps = (state: State) => ({
