5ad69249d5d3d161417cd91a95958b861c09c59d
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-06-13 13:44:29 +0200

    disable web security in dev

--- packages/suite-desktop/src/electron.js
+++ packages/suite-desktop/src/electron.js
@@ -1,4 +1,9 @@
-const { app, session, BrowserWindow, ipcMain } = require('electron');
+const {
+    app,
+    session,
+    BrowserWindow,
+    ipcMain
+} = require('electron');
 const isDev = require('electron-is-dev');
 const prepareNext = require('electron-next');
 const path = require('path');
@@ -13,18 +18,20 @@ const init = async () => {
         width: 800,
         height: 600,
         webPreferences: {
+            webSecurity: !isDev,
+            allowRunningInsecureContent: isDev,
             nodeIntegration: false,
             preload: path.join(__dirname, 'preload.js'),
         },
     });
 
-    const src = isDev
-        ? 'http://localhost:8000/'
-        : url.format({
-              pathname: path.join(__dirname, '../build/index.html'),
-              protocol: 'file:',
-              slashes: true,
-          });
+    const src = isDev ?
+        'http://localhost:8000/' :
+        url.format({
+            pathname: path.join(__dirname, '../build/index.html'),
+            protocol: 'file:',
+            slashes: true,
+        });
 
     mainWindow.loadURL(src);
 };
@@ -32,7 +39,7 @@ const init = async () => {
 app.on('ready', init);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
+app.on('window-all-closed', function () {
     // On OS X it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
@@ -40,7 +47,7 @@ app.on('window-all-closed', function() {
     }
 });
 
-app.on('activate', function() {
+app.on('activate', function () {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
@@ -48,7 +55,7 @@ app.on('activate', function() {
     }
 });
 
-app.on('browser-window-focus', function(event, win) {
+app.on('browser-window-focus', function (event, win) {
     if (!win.isDevToolsOpened()) {
         win.openDevTools();
     }
