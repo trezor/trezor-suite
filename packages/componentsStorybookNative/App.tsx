1fb30c6b26d392beaca2a87b1e2041c956c31d17
Jan Czibula <>
2019-06-14 15:27:32 +0200

    add Checkbox native, fix svg library in iOS

--- packages/componentsStorybookNative/App.tsx
+++ packages/componentsStorybookNative/App.tsx
@@ -8,7 +8,7 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import { ButtonPin } from '@trezor/components';
+import { Checkbox } from '@trezor/components';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -22,7 +22,9 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <ButtonPin onClick={() => {}}>+</ButtonPin>
+        <Checkbox onClick={() => {}} isChecked>
+          Checkbox
+        </Checkbox>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
