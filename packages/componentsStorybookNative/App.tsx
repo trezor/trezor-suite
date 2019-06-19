51815aad70ec54df7269ecf3674a968a20c8cab7
Jan Czibula <>
2019-06-19 09:38:59 +0200

    app update

--- packages/componentsStorybookNative/App.tsx
+++ packages/componentsStorybookNative/App.tsx
@@ -6,48 +6,50 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import { Checkbox } from '@trezor/components';
+import React, { Component } from 'react';
+import styled from 'styled-components/native';
+import { H1, H2, H3, H4, H5, H6, P, Link, TrezorImage } from '@trezor/components';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+const Container = styled.ScrollView.attrs({
+  contentContainerStyle: {
+    justifyContent: 'center',
+    paddingTop: 100,
+  }
+})``;
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Checkbox onClick={() => {}} isChecked>
-          Checkbox
-        </Checkbox>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Container>
+        <H1>Heading 1 with <Link href="https://trezor.io">link</Link></H1>
+        <H2>Heading 2</H2>
+        <H3>Heading 3</H3>
+        <H4>Heading 4</H4>
+        <H5>Heading 5</H5>
+        <H6>Heading 6</H6>
+        <Link href="https://trezor.io">Trezor.io</Link>
+        <P size='small'>
+          This is an paragraph small
+        </P>
+        <P size='medium'>
+          This is an paragraph medium
+        </P>
+        <P size='large'>
+          This is an paragraph large
+        </P>
+        <P size='xlarge'>
+          This is an paragraph xlarge
+        </P>
+        <TrezorImage
+          height={310}
+          model={1}
+        />
+        <TrezorImage
+          height={310}
+          model={2}
+        />
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
