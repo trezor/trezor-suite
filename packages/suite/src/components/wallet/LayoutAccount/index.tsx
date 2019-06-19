6ff8084b67e42d59f5727ac535d352d400fca36a
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-06-19 17:45:00 +0200

    fix circular dependency

--- packages/suite/src/components/wallet/LayoutAccount/index.tsx
+++ packages/suite/src/components/wallet/LayoutAccount/index.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { State } from '@suite-types/index';
 import TopNavigation from '@wallet-components/TopNavigation';
+import Layout from '@wallet-components/Layout';
 
 interface Props {
     children: React.ReactNode;
 }
 
-const Layout = (props: Props) => (
+const LayoutAccount = (props: Props) => (
     <Layout>
         <TopNavigation />
         {props.children}
@@ -19,4 +20,4 @@ const mapStateToProps = (state: State) => ({
     suite: state.suite,
 });
 
-export default connect(mapStateToProps)(Layout);
+export default connect(mapStateToProps)(LayoutAccount);
--- packages/suite/src/views/wallet/account/summary/index.tsx
+++ packages/suite/src/views/wallet/account/summary/index.tsx
@@ -1,26 +1,9 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-import { Text } from 'react-native';
-import { Button } from '@trezor/components';
-import { State } from '@suite-types/index';
-import { goto } from '@suite-actions/routerActions';
 import LayoutAccount from '@wallet-components/LayoutAccount';
 
-interface Props {
-    suite: State['suite'];
-    router: State['router'];
-}
-
-const Wallet = (props: Props) => {
-    const { pathname, params } = props.router;
-    const baseUrl = `${pathname}#/${params.coin}/`;
-    return <LayoutAccount>aaa</LayoutAccount>;
+const Summary = () => {
+    return <LayoutAccount>summary</LayoutAccount>;
 };
 
-const mapStateToProps = (state: State) => ({
-    suite: state.suite,
-    router: state.router,
-});
-
-export default connect(mapStateToProps)(Wallet);
+export default Summary;
