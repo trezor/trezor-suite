9968d0a61663d79190bcf29ce29ddb3dacc0235b
Szymon Lesisz <sz.lesisz@gmail.com>
2019-06-20 14:33:45 +0200

    update build.lib script

--- scripts/build.lib.js
+++ scripts/build.lib.js
@@ -1,28 +1,46 @@
-import fs from 'fs-extra';
+import fs from 'fs-extra'; // eslint-disable-line import/no-extraneous-dependencies
 import { resolve } from 'path';
-import { collectImportsSync } from 'babel-collect-imports';
 import packageJSON from '../package.json';
 
-const { internal } = collectImportsSync(resolve(__dirname, '../src/index.js'));
+// const { internal } = collectImportsSync(resolve(__dirname, '../src/index.js'));
 
 const src = resolve(__dirname, '../src');
-const npm = resolve(__dirname, '../build');
-const lib = resolve(__dirname, '../build/lib');
+const npm = resolve(__dirname, '../');
+const lib = resolve(__dirname, '../lib');
 
-internal.forEach(file => {
-    const libFile = file.replace(src, lib);
-    fs.copySync(file, libFile);
-    fs.copySync(file, libFile + '.flow');
-});
+try {
+    fs.remove(lib);
+} catch (err) {}
 
-delete packageJSON.devDependencies;
-delete packageJSON.scripts;
-delete packageJSON.bin;
+const ignore = ['.DS_Store', 'ui', '_old', 'ws.js'];
 
-packageJSON.main = 'lib/index.js';
-fs.writeFileSync(resolve(npm, 'package.json'), JSON.stringify(packageJSON, null, '  '), 'utf-8');
+const copySrc = dir => {
+    const files = fs.readdirSync(dir, 'utf8');
+    for (const file of files) {
+        if (ignore.indexOf(file) < 0) {
+            const filePath = resolve(dir, file);
+            console.log('F', filePath);
+            if (fs.statSync(filePath).isDirectory()) {
+                copySrc(filePath);
+            } else {
+                const libFile = filePath.replace(src, lib);
+                fs.copySync(filePath, libFile);
+                fs.copySync(filePath, `${libFile}.flow`);
+            }
+        }
+    }
+};
 
-fs.copySync(resolve(npm, '../README.md'), resolve(npm, 'README.md'));
-fs.copySync(resolve(npm, '../LICENSE.md'), resolve(npm, 'LICENSE.md'));
-fs.copySync(resolve(npm, '../CHANGELOG.md'), resolve(npm, 'CHANGELOG.md'));
-fs.copySync(resolve(src, 'utils/ws.js'), resolve(lib, 'utils/ws.js'));
+copySrc(src);
+
+// delete packageJSON.devDependencies;
+// delete packageJSON.scripts;
+// delete packageJSON.bin;
+
+// packageJSON.main = 'lib/index.js';
+// fs.writeFileSync(resolve(npm, 'package.json'), JSON.stringify(packageJSON, null, '  '), 'utf-8');
+
+// fs.copySync(resolve(npm, '../README.md'), resolve(npm, 'README.md'));
+// fs.copySync(resolve(npm, '../LICENSE.md'), resolve(npm, 'LICENSE.md'));
+// fs.copySync(resolve(npm, '../CHANGELOG.md'), resolve(npm, 'CHANGELOG.md'));
+// fs.copySync(resolve(src, 'utils/ws.js'), resolve(lib, 'utils/ws.js'));
