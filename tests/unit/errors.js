d71e314379b9f760d68884a9909a09f36e81dece
Szymon Lesisz <sz.lesisz@gmail.com>
2019-06-20 11:25:17 +0200

    spearate unit tests

--- tests/unit/errors.js
+++ tests/unit/errors.js
@@ -1,51 +1,51 @@
 /* @flow */
 
-import { describe, it } from 'mocha';
-import { expect } from 'chai';
-import { CustomError } from '../src/constants/errors';
+// import { describe, it } from 'mocha';
+// import { expect } from 'chai';
+import { CustomError } from '../../src/constants/errors';
 
 describe('Custom errors', () => {
     it('Error with predefined code and predefined message', async () => {
         const error = new CustomError('worker_not_found');
-        expect(error.code).equals('blockchain_link/worker_not_found');
-        expect(error.message).equals('Worker not found');
+        expect(error.code).toBe('blockchain_link/worker_not_found');
+        expect(error.message).toBe('Worker not found');
     });
 
     it('Error with predefined code and custom message', async () => {
         const error = new CustomError('worker_not_found', 'Custom message');
-        expect(error.code).equals('blockchain_link/worker_not_found');
-        expect(error.message).equals('Custom message');
+        expect(error.code).toBe('blockchain_link/worker_not_found');
+        expect(error.message).toBe('Custom message');
     });
 
     it('Error already prefixed code', async () => {
         const error = new CustomError('blockchain_link/worker_not_found');
-        expect(error.code).equals('blockchain_link/worker_not_found');
-        expect(error.message).equals('Worker not found');
+        expect(error.code).toBe('blockchain_link/worker_not_found');
+        expect(error.message).toBe('Worker not found');
     });
 
     it('Error with custom code and custom message', async () => {
         const error = new CustomError('blockchain_link/custom', 'Custom message');
-        expect(error.code).equals('blockchain_link/custom');
-        expect(error.message).equals('Custom message');
+        expect(error.code).toBe('blockchain_link/custom');
+        expect(error.message).toBe('Custom message');
     });
 
     it('Error with custom code and without message', async () => {
         const error = new CustomError('custom');
-        expect(error.code).equals('blockchain_link/custom');
-        expect(error.message).equals('Message not set');
+        expect(error.code).toBe('blockchain_link/custom');
+        expect(error.message).toBe('Message not set');
     });
 
     it('Error without code and with custom message', async () => {
         // $FlowIssue
         const error = new CustomError(null, 'Custom message');
-        expect(error.code).equals(undefined);
-        expect(error.message).equals('Custom message');
+        expect(error.code).toBe(undefined);
+        expect(error.message).toBe('Custom message');
     });
 
     it('Error without code and without message', async () => {
         // $FlowIssue
         const error = new CustomError();
-        expect(error.code).equals(undefined);
-        expect(error.message).equals('Message not set');
+        expect(error.code).toBe(undefined);
+        expect(error.message).toBe('Message not set');
     });
 });
--- tests/unit/utils.js
+++ tests/unit/utils.js
@@ -1,12 +1,10 @@
 /* @flow */
 
-import { describe, it } from 'mocha';
-import { expect } from 'chai';
 import {
     filterTargets,
     filterTokenTransfers,
     transformTransaction,
-} from '../src/workers/blockbook/utils';
+} from '../../src/workers/blockbook/utils';
 
 import fixtures from './fixtures/utils';
 
@@ -14,7 +12,7 @@ describe('blockbook/utils', () => {
     describe('filterTargets', () => {
         it('addresses as string', async () => {
             const t = filterTargets('A', [{ addresses: ['A'], n: 0 }, { addresses: ['B'], n: 1 }]);
-            expect(t).to.deep.equal([{ addresses: ['A'], n: 0 }]);
+            expect(t).toEqual([{ addresses: ['A'], n: 0 }]);
         });
 
         it('addresses as array of strings', async () => {
@@ -22,7 +20,7 @@ describe('blockbook/utils', () => {
                 ['A'],
                 [{ addresses: ['A'], n: 0 }, { addresses: ['B'], n: 1 }]
             );
-            expect(t).to.deep.equal([{ addresses: ['A'], n: 0 }]);
+            expect(t).toEqual([{ addresses: ['A'], n: 0 }]);
         });
 
         it('addresses as array of mixed objects', async () => {
@@ -31,38 +29,38 @@ describe('blockbook/utils', () => {
                 ['A', 1, undefined, 'C', { address: 'B', path: '', transfers: 0, decimal: 0 }],
                 [{ addresses: ['A'], n: 0 }, { addresses: ['B'], n: 1 }]
             );
-            expect(t).to.deep.equal([{ addresses: ['A'], n: 0 }, { addresses: ['B'], n: 1 }]);
+            expect(t).toEqual([{ addresses: ['A'], n: 0 }, { addresses: ['B'], n: 1 }]);
         });
 
         it('targets not found', async () => {
             const t = filterTargets('A', [{ addresses: ['B'], n: 0 }, { addresses: ['C'], n: 1 }]);
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
         });
 
         it('addresses as unexpected object', async () => {
             // $FlowIssue
             let t = filterTargets(1, [{ addresses: ['A'], n: 0 }]);
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
             // $FlowIssue
             t = filterTargets(null, [{ addresses: ['A'], n: 0 }]);
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
             // $FlowIssue
             t = filterTargets([1], [{ addresses: ['A'], n: 0 }]);
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
             // $FlowIssue
             t = filterTargets([{ foo: 'bar' }], [{ addresses: ['A'], n: 0 }]);
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
         });
 
         it('targets as unexpected object', async () => {
             let t = filterTargets('A', 'A');
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
 
             t = filterTargets('A', ['A', null, 1, {}]);
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
 
             t = filterTargets('A', null);
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
         });
     });
 
@@ -70,12 +68,12 @@ describe('blockbook/utils', () => {
         const tokens = [{ from: 'A', to: 'B' }, { from: 'C', to: 'D' }, { from: 'X', to: 'X' }];
         it('addresses as string', async () => {
             const t = filterTokenTransfers('A', tokens);
-            expect(t).to.deep.equal([tokens[0]]);
+            expect(t).toEqual([tokens[0]]);
         });
 
         it('addresses as array of strings', async () => {
             const t = filterTokenTransfers(['A'], tokens);
-            expect(t).to.deep.equal([tokens[0]]);
+            expect(t).toEqual([tokens[0]]);
         });
 
         it('addresses as array of mixed objects', async () => {
@@ -84,7 +82,7 @@ describe('blockbook/utils', () => {
                 ['A', 1, undefined, 'X', { address: 'D' }, 'NOT_FOUND'],
                 tokens
             );
-            expect(t).to.deep.equal(tokens);
+            expect(t).toEqual(tokens);
         });
 
         it('addresses as Address object', async () => {
@@ -92,57 +90,57 @@ describe('blockbook/utils', () => {
                 [{ address: 'A', path: '', transfers: 0, decimal: 0 }],
                 tokens
             );
-            expect(t).to.deep.equal([tokens[0]]);
+            expect(t).toEqual([tokens[0]]);
         });
 
         it('addresses as unexpected object', async () => {
             // $FlowIssue
             let t = filterTokenTransfers(1, tokens);
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
             // $FlowIssue
             t = filterTokenTransfers(null, tokens);
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
             // $FlowIssue
             t = filterTokenTransfers([1], tokens);
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
             // $FlowIssue
             t = filterTokenTransfers([{ foo: 'bar' }], tokens);
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
         });
 
         it('transfers sent', async () => {
             let t = filterTokenTransfers('A', tokens);
-            expect(t).to.deep.equal([tokens[0]]);
+            expect(t).toEqual([tokens[0]]);
 
             t = filterTokenTransfers(['C'], tokens);
-            expect(t).to.deep.equal([tokens[1]]);
+            expect(t).toEqual([tokens[1]]);
         });
 
         it('transfers recv', async () => {
             let t = filterTokenTransfers('B', tokens);
-            expect(t).to.deep.equal([tokens[0]]);
+            expect(t).toEqual([tokens[0]]);
 
             t = filterTokenTransfers(['D'], tokens);
-            expect(t).to.deep.equal([tokens[1]]);
+            expect(t).toEqual([tokens[1]]);
         });
 
         it('transfers self', async () => {
             const t = filterTokenTransfers('X', tokens);
-            expect(t).to.deep.equal([tokens[2]]);
+            expect(t).toEqual([tokens[2]]);
         });
 
         it('transfers as unexpected object', async () => {
             let t = filterTokenTransfers('A', 'A');
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
 
             t = filterTokenTransfers('A', ['A', null, 1, {}]);
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
 
             t = filterTokenTransfers('A', null);
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
 
             t = filterTokenTransfers('A', []);
-            expect(t).to.deep.equal([]);
+            expect(t).toEqual([]);
         });
     });
 
@@ -150,7 +148,7 @@ describe('blockbook/utils', () => {
         fixtures.transformTransaction.forEach(f => {
             it(f.description, () => {
                 const tx = transformTransaction(f.descriptor, f.addresses, f.tx);
-                expect(tx).to.deep.equal({
+                expect(tx).toEqual({
                     txid: undefined,
                     blockTime: undefined,
                     blockHeight: undefined,
@@ -160,7 +158,6 @@ describe('blockbook/utils', () => {
                     tokens: [],
                     ...f.parsed,
                 });
-                // expect(tx.tokens).to.deep.equal(f.parsed.tokens || []);
             });
         });
     });
