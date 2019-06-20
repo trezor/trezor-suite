5c44461d7e14c25fdf5fff77e548892daebf5fce
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-06-20 22:09:12 +0200

    rename reducers and add modal reducer

--- packages/suite/src/reducers/suite/index.ts
+++ packages/suite/src/reducers/suite/index.ts
@@ -1,11 +1,13 @@
-import router from './router';
-import suite from './suite';
-import devices from './devices';
+import router from './routerReducer';
+import suite from './suiteReducer';
+import devices from './devicesReducer';
 import log from './logReducer';
+import modal from './modalReducer';
 
 export default {
     suite,
     router,
     devices,
     log,
+    modal,
 };
