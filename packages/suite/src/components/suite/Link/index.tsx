78fe34ed233922b256ed5f03f1e68e293b689de9
slowbackspace <slowbackspace@gmail.com>
2019-06-21 10:44:02 +0200

    wrap Link from components with nextjs Link

--- packages/suite/src/components/suite/Link/index.tsx
+++ packages/suite/src/components/suite/Link/index.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import NextLink, { LinkProps } from 'next/link';
+import { Link as TLink } from '@trezor/components';
 import { routes } from '@suite-utils/router';
 import { getPrefixedURL } from '@suite-utils/nextjs';
 
-const Link = ({ children, href, ...rest }: LinkProps) => {
+interface Props extends LinkProps {
+    className?: string;
+}
+
+const Link = ({ children, href, className, ...rest }: Props) => {
     // if href prop refers to internal url puts assetPrefix in front and
     // pass the prefixed value in 'as' prop to prevent refreshing the page
     const isInternalLink = routes.find(r => r.pattern === href) || false;
@@ -12,8 +17,10 @@ const Link = ({ children, href, ...rest }: LinkProps) => {
     };
 
     return (
-        <NextLink href={href} {...overrideAsProp} {...rest}>
-            {children}
+        <NextLink href={href} passHref {...overrideAsProp} {...rest}>
+            <TLink target="_self" {...rest} className={className}>
+                {children}
+            </TLink>
         </NextLink>
     );
 };
