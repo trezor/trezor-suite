affb4ff4465abc890a8ec9f1217cd2e4cd7d7dfd
slowbackspace <slowbackspace@gmail.com>
2019-06-25 16:13:19 +0200

    add route for wallet import

--- packages/suite/src/utils/suite/router.ts
+++ packages/suite/src/utils/suite/router.ts
@@ -26,6 +26,10 @@ export const routes: Route[] = [
         pattern: '/wallet/account/',
     },
     {
+        name: 'wallet-import',
+        pattern: '/wallet/import/',
+    },
+    {
         name: 'wallet-account-summary',
         pattern: '/wallet/account/',
     },
