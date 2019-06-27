f6c10f283684c530c01284b47498dadb53592cc0
slowbackspace <slowbackspace@gmail.com>
2019-06-27 13:37:39 +0200

    connect reducer to store

--- packages/suite/src/reducers/wallet/index.ts
+++ packages/suite/src/reducers/wallet/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import signVerifyReducer from './signVerifyReducer';
 import settingsReducer from './settingsReducer';
+import fiatRateReducer from './fiatRateReducer.ts';
 
 const WalletReducers = combineReducers({
     signVerify: signVerifyReducer,
+    fiat: settingsReducer,
     settings: settingsReducer,
 });
 
