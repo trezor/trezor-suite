fc0c54c03a151e854203758fcbcfb372a187c7fe
Szymon Lesisz <sz.lesisz@gmail.com>
2019-07-02 09:58:18 +0200

    unify test websocket

--- tests/websocket/fixtures/blockbook/index.js
+++ tests/websocket/fixtures/blockbook/index.js
@@ -2,7 +2,55 @@
 // mocked blockbook responses
 
 export default {
-    getInfo: require('./getInfo'),
-    getAccountInfo: require('./getAccountInfo'),
-    subscribe: require('./estimateFee'),
+    getInfo: {
+        data: {
+            name: 'Test',
+            shortcut: 'test',
+            decimals: 9,
+            bestHeight: 1,
+        },
+    },
+    // getAccountInfo: require('./getAccountInfo'),
+    // subscribe: require('./estimateFee'),
+
+    subscribeNewBlock: {
+        data: {
+            subscribed: true,
+        },
+    },
+    unsubscribeNewBlock: {
+        data: {
+            subscribed: false,
+        },
+    },
+    subscribeAddresses: {
+        data: {
+            subscribed: true,
+        },
+    },
+    unsubscribeAddresses: {
+        data: {
+            subscribed: false,
+        },
+    },
+
+    getAccountInfo: {
+        data: {
+            address: '0',
+            balance: '0',
+            txs: 0,
+            unconfirmedBalance: 0,
+            unconfirmedTxs: 0,
+            // eth stuff
+            nonce: '0',
+            // btc stuff
+            totalReceived: '0',
+            totalSent: '0',
+            usedTokens: 0, // how many adresses are used
+        },
+    },
+
+    getAccountUtxo: {
+        data: [],
+    },
 };
--- tests/websocket/fixtures/ripple/index.js
+++ tests/websocket/fixtures/ripple/index.js
@@ -3,8 +3,41 @@
 // copied from: https://github.com/ripple/ripple-lib/tree/develop/test/fixtures/rippled
 
 export default {
+    server_info: {
+        status: 'success',
+        type: 'response',
+        result: {
+            info: {
+                build_version: '0.24.0-rc1',
+                hostid: 'localhost',
+                validated_ledger: {
+                    base_fee_xrp: 0.00001,
+                    reserve_base_xrp: 20,
+                    reserve_inc_xrp: 5,
+                    hash: '1',
+                    seq: 1,
+                },
+            },
+        },
+    },
     subscribe: require('./subscribe'),
+    unsubscribe: {
+        result: {},
+        status: 'success',
+        type: 'response',
+    },
     serverInfo: {
         normal: require('./serverInfo'),
     },
+    account_info: {
+        status: 'error',
+        type: 'response',
+        error: 'actNotFound',
+        error_code: 19,
+        error_message: 'Account not found.',
+        ledger_hash: 'FDFA9E459F54932F2713F6728A6102E8255C3C8441C717CD7689AD0C38F228AD',
+        ledger_index: 48356306,
+        validated: true,
+    },
+    account_tx: require('./get-account-tx'),
 };
