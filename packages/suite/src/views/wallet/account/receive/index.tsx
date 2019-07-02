7dea7abc9ce0b8c12bdd4ebb7429f97343d6a390
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-07-02 16:33:45 +0200

    send and receive basic skeleton

--- packages/suite/src/views/wallet/account/receive/index.tsx
+++ packages/suite/src/views/wallet/account/receive/index.tsx
@@ -1,53 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
 
-import { Text } from 'react-native';
-import { Button } from '@trezor/components';
-import Content from '@wallet-components/Content';
-import { AppState } from '@suite-types/index';
-import { goto } from '@suite-actions/routerActions';
 import LayoutAccount from '@wallet-components/LayoutAccount';
 
-interface Props {
-    suite: AppState['suite'];
-    router: AppState['router'];
-}
+import EthereumTypeReceiveForm from './ethereum';
+import RippleTypeReceiveForm from './ripple';
+import BitcoinTypeReceiveForm from './bitcoin';
 
-const Wallet = (props: Props) => {
-    const { pathname, params } = props.router;
-    const baseUrl = `${pathname}#/${params.coin}/`;
+const AccountReceive = () => {
+    const network = { type: 'bitcoin' };
     return (
         <LayoutAccount>
-            <Text>
-                {params.coin} Account {params.accountId} Receive Page
-            </Text>
-            <Text>Other accounts</Text>
-            <Button variant="success" onClick={() => goto(`${baseUrl}1`)}>
-                Account#1
-            </Button>
-            <Button variant="success" onClick={() => goto(`${baseUrl}2`)}>
-                Account#2
-            </Button>
-            <Text>Account subpages</Text>
-            <Button variant="success" onClick={() => goto('/wallet/account', true)}>
-                Dashboard
-            </Button>
-            <Button variant="success" onClick={() => goto('/wallet/account/send', true)}>
-                Send
-            </Button>
-            <Button variant="success" onClick={() => goto('/wallet/account/receive', true)}>
-                Recv
-            </Button>
-            <Button variant="success" onClick={() => goto('/wallet/account/sign-verify', true)}>
-                Sign verify
-            </Button>
+            {network.type === 'bitcoin' && <BitcoinTypeReceiveForm />}
+            {network.type === 'ethereum' && <EthereumTypeReceiveForm />}
+            {network.type === 'ripple' && <RippleTypeReceiveForm />}
         </LayoutAccount>
     );
 };
 
-const mapStateToProps = (state: AppState) => ({
-    suite: state.suite,
-    router: state.router,
-});
-
-export default connect(mapStateToProps)(Wallet);
+export default AccountReceive;
---
+++ packages/suite/src/views/wallet/account/send/ethereum/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styled from 'styled-components';
+import Title from '@wallet-components/Title';
+
+const Wrapper = styled.div``;
+
+const EthereumSend = () => (
+    <Wrapper>
+        <Title>Send Ethereum</Title>
+    </Wrapper>
+);
+
+export default EthereumSend;
