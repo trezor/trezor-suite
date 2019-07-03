0d6b3f4d518d93b09cc6f4780034ebd0061a71ba
Szymon Lesisz <sz.lesisz@gmail.com>
2019-07-03 14:00:59 +0200

    copy ./src/utils/ws.js to lib

--- scripts/build.lib.js
+++ scripts/build.lib.js
@@ -1,36 +1,42 @@
-import fs from 'fs-extra'; // eslint-disable-line import/no-extraneous-dependencies
-import { resolve } from 'path';
-import packageJSON from '../package.json';
+const fs = require('fs');
+const path = require('path');
+
+fs.copyFile(path.resolve(__dirname, '../src/utils/ws.js'), path.resolve(__dirname, '../lib/utils/ws.js'), () => {});
+
+
+// import fs from 'fs'; // lint-disable-line import/no-extraneous-dependencies
+// import { resolve } from 'path';
+// import packageJSON from '../package.json';
 
 // const { internal } = collectImportsSync(resolve(__dirname, '../src/index.js'));
 
-const src = resolve(__dirname, '../src');
-const npm = resolve(__dirname, '../');
-const lib = resolve(__dirname, '../lib');
+// const src = resolve(__dirname, '../src');
+// const npm = resolve(__dirname, '../');
+// const lib = resolve(__dirname, '../lib');
 
-try {
-    fs.remove(lib);
-} catch (err) {}
+// try {
+//     fs.remove(lib);
+// } catch (err) {}
 
-const ignore = ['.DS_Store', 'ui', '_old', 'ws.js'];
+// const ignore = ['.DS_Store', 'ui', '_old', 'ws.js'];
 
-const copySrc = dir => {
-    const files = fs.readdirSync(dir, 'utf8');
-    for (const file of files) {
-        if (ignore.indexOf(file) < 0) {
-            const filePath = resolve(dir, file);
-            if (fs.statSync(filePath).isDirectory()) {
-                copySrc(filePath);
-            } else {
-                const libFile = filePath.replace(src, lib);
-                fs.copySync(filePath, libFile);
-                fs.copySync(filePath, `${libFile}.flow`);
-            }
-        }
-    }
-};
+// const copySrc = dir => {
+//     const files = fs.readdirSync(dir, 'utf8');
+//     for (const file of files) {
+//         if (ignore.indexOf(file) < 0) {
+//             const filePath = resolve(dir, file);
+//             if (fs.statSync(filePath).isDirectory()) {
+//                 copySrc(filePath);
+//             } else {
+//                 const libFile = filePath.replace(src, lib);
+//                 fs.copySync(filePath, libFile);
+//                 fs.copySync(filePath, `${libFile}.flow`);
+//             }
+//         }
+//     }
+// };
 
-copySrc(src);
+// copySrc(src);
 
 // delete packageJSON.devDependencies;
 // delete packageJSON.scripts;
