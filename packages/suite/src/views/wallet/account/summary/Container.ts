b22faea3716886481902f1787feafa658ce3d244
slowbackspace <slowbackspace@gmail.com>
2019-07-04 10:25:53 +0200

    add components

---
+++ packages/suite/src/views/wallet/account/summary/Container.ts
@@ -0,0 +1,76 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { injectIntl } from 'react-intl';
+
+// import * as TokenActions from 'actions/TokenActions';
+
+import { AppState, Dispatch } from '@suite-types/index';
+import Summary from './index';
+
+const mapStateToProps = (state: AppState) => ({
+    // TODO get selectedAccount from reducers
+    selectedAccount: {
+        tokens: [],
+        pending: [],
+        network: {
+            order: 2,
+            type: 'ethereum',
+            name: 'Ethereum',
+            symbol: 'ETH',
+            shortcut: 'eth',
+            bip44: "m/44'/60'/0'/0",
+            chainId: 1,
+            defaultGasPrice: 64,
+            defaultGasLimit: 21000,
+            defaultGasLimitTokens: 200000,
+            decimals: 18,
+            tokens: './data/ethereumTokens.json',
+            web3: ['wss://eth2.trezor.io/geth'],
+            explorer: {
+                tx: 'https://etherscan.io/tx/',
+                address: 'https://etherscan.io/address/',
+            },
+            hasSignVerify: true,
+        },
+        shouldRender: true,
+        account: {
+            imported: false,
+            index: 0,
+            network: 'eth',
+            deviceID: '4C7098C3D916B10FBCDCE8A0',
+            deviceState: '7dcccffe70d8bb8bb28a2185daac8e05639490eee913b326097ae1d73abc8b4f',
+            accountPath: [2147483692, 2147483792, 2147483648, 0, 0],
+            descriptor: 'rNaqKtKrMSwpwZSzRckPf7S96DkimjkF4H',
+            balance: '0',
+            availableBalance: '0',
+            block: 47570989,
+            transactions: 0,
+            empty: true,
+            networkType: 'ethereum',
+            sequence: 0,
+            reserve: '20',
+        },
+    },
+    summary: state.wallet.summary,
+    wallet: state.wallet,
+    device: state.suite.device,
+    tokens: state.wallet.tokens,
+    fiat: state.wallet.fiat,
+    localStorage: state.wallet.localStorage,
+});
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({
+    addToken: () => {},
+    loadTokens: () => {},
+    removeToken: () => {},
+    // addToken: bindActionCreators(TokenActions.add, dispatch),
+    // loadTokens: bindActionCreators(TokenActions.load, dispatch),
+    // removeToken: bindActionCreators(TokenActions.remove, dispatch),
+});
+
+export default injectIntl(
+    connect(
+        mapStateToProps,
+        mapDispatchToProps,
+    )(Summary),
+);
--- packages/suite/src/views/wallet/account/summary/index.tsx
+++ packages/suite/src/views/wallet/account/summary/index.tsx
@@ -1,9 +1,55 @@
 import React from 'react';
-
+import { AppState } from '@suite-types/index';
+import Content from '@wallet-components/Content';
 import LayoutAccount from '@wallet-components/LayoutAccount';
+import Tokens from './components/Tokens';
+import AccountHeader from './components/AccountHeader';
 
-const Summary = () => {
-    return <LayoutAccount>summary</LayoutAccount>;
+interface Props {
+    selectedAccount: AppState['wallet']['selectedAccount'];
+    summary: AppState['wallet']['summary'];
+    wallet: AppState['wallet'];
+    device: AppState['suite']['device'];
+    tokens: AppState['wallet']['tokens'];
+    fiat: AppState['wallet']['fiat'];
+    localStorage: AppState['wallet']['localStorage'];
+    // addToken: typeof TokenActions.add,
+    // loadTokens: typeof TokenActions.load,
+    // removeToken: typeof TokenActions.remove,
+}
+
+const AccountSummary = (props: Props) => {
+    const { device } = props;
+    const { account, network, tokens, pending, shouldRender } = props.selectedAccount;
+
+    if (!device || !account || !network || !shouldRender) {
+        const { loader, exceptionPage } = props.selectedAccount;
+        return <Content loader={loader} exceptionPage={exceptionPage} isLoading />;
+    }
+
+    return (
+        <LayoutAccount>
+            <AccountHeader
+                account={account}
+                network={network}
+                pending={pending}
+                fiatRates={props.fiat}
+                localCurrency={props.wallet.settings.localCurrency}
+                isHidden={props.wallet.settings.hideBalance}
+            />
+            {network.type === 'ethereum' ? (
+                <Tokens
+                    account={account}
+                    loadTokens={props.loadTokens}
+                    addToken={props.addToken}
+                    removeToken={props.removeToken}
+                    tokens={tokens}
+                    pending={pending}
+                    isBalanceHidden={props.wallet.settings.hideBalance}
+                />
+            ) : null}
+        </LayoutAccount>
+    );
 };
 
-export default Summary;
+export default AccountSummary;
