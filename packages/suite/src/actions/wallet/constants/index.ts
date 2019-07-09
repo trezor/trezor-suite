c57ae1a178a096b361560678969d955a8d7c2c21
slowbackspace <slowbackspace@gmail.com>
2019-07-09 13:21:14 +0200

    fix import

--- packages/suite/src/actions/wallet/constants/index.ts
+++ packages/suite/src/actions/wallet/constants/index.ts
@@ -3,6 +3,5 @@ import * as SETTINGS from './settingsConstants';
 import * as NOTIFICATION from './notification';
 import * as SUMMARY from './summary';
 import * as TOKEN from './token';
-import * as WALLET from './wallet';
 
-export { SIGN_VERIFY, SETTINGS, NOTIFICATION, SUMMARY, TOKEN, WALLET };
+export { SIGN_VERIFY, SETTINGS, NOTIFICATION, SUMMARY, TOKEN };
