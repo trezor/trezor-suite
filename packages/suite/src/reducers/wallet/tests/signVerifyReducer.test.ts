8dc1d45676e6176dee69d5b94baa4cc34fd158b5
slowbackspace <slowbackspace@gmail.com>
2019-07-09 17:05:14 +0200

    remove unused import

--- packages/suite/src/reducers/wallet/tests/signVerifyReducer.test.ts
+++ packages/suite/src/reducers/wallet/tests/signVerifyReducer.test.ts
@@ -1,5 +1,4 @@
 import reducer from '@wallet-reducers/settingsReducer';
-import * as types from '@wallet-actions/constants/signVerifyConstants';
 
 describe('sign verify reducer', () => {
     it('test initial state', () => {
