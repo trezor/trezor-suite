e359ab808c5f54bd91fe1d24f17e45979f5be74a
slowbackspace <slowbackspace@gmail.com>
2019-07-19 14:40:28 +0200

    install dep

--- packages/suite/src/storage/index.ts
+++ packages/suite/src/storage/index.ts
@@ -2,6 +2,14 @@ const VERSION = 1;
 // react-native https://facebook.github.io/react-native/docs/asyncstorage.html
 
 // next.js https://medium.com/@filipvitas/indexeddb-with-promises-and-async-await-3d047dddd313
+
+const isIndexedDBAvailable = () => {
+    if (!('indexedDB' in window)) {
+        return true;
+    }
+    return false;
+};
+
 // TODO: implement upgrade
 const onUpgrade = (db: IDBDatabase) => {
     db.createObjectStore('devices', { keyPath: 'id', autoIncrement: true });
@@ -20,6 +28,11 @@ const openDB = async () => {
 };
 
 const load = async () => {
+    if (!isIndexedDBAvailable()) {
+        console.warn('IndexedDB not supported');
+        return;
+    }
+
     await openDB();
 
     // db.close();
