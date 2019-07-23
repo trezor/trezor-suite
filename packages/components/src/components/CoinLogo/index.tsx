b4285e79172692c9bf63f8bb49f2916f8020e5dc
Jan Czibula <>
2019-07-23 17:23:56 +0200

    add vector coin logos as components, remove svg files

--- packages/components/src/components/CoinLogo/index.tsx
+++ packages/components/src/components/CoinLogo/index.tsx
@@ -3,7 +3,51 @@ import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
-const Logo = styled.img``;
+import AdaCoin from './coins/ada';
+import BchCoin from './coins/bch';
+import BtcCoin from './coins/btc';
+import BtgCoin from './coins/btg';
+import DashCoin from './coins/dash';
+import DgbCoin from './coins/dgb';
+import DogeCoin from './coins/doge';
+import EtcCoin from './coins/etc';
+import EthCoin from './coins/eth';
+import LtcCoin from './coins/ltc';
+import NemCoin from './coins/nem';
+import NmcCoin from './coins/nmc';
+import RinkebyCoin from './coins/rinkeby';
+import TropCoin from './coins/trop';
+import TxrpCoin from './coins/txrp';
+import VtcCoin from './coins/vtc';
+import XemCoin from './coins/xem';
+import XlmCoin from './coins/xlm';
+import XrpCoin from './coins/xrp';
+import XtzCoin from './coins/xtz';
+import ZecCoin from './coins/zec';
+
+const LOGOS: { [key: string]: any } = {
+    ada: AdaCoin,
+    bch: BchCoin,
+    btc: BtcCoin,
+    btg: BtgCoin,
+    dash: DashCoin,
+    dgb: DgbCoin,
+    doge: DogeCoin,
+    etc: EtcCoin,
+    eth: EthCoin,
+    ltc: LtcCoin,
+    nem: NemCoin,
+    nmc: NmcCoin,
+    rinkeby: RinkebyCoin,
+    trop: TropCoin,
+    txrp: TxrpCoin,
+    vtc: VtcCoin,
+    xem: XemCoin,
+    xlm: XlmCoin,
+    xrp: XrpCoin,
+    xtz: XtzCoin,
+    zec: ZecCoin,
+};
 
 interface Props extends React.ImgHTMLAttributes<HTMLImageElement> {
     network: string;
@@ -11,10 +55,8 @@ interface Props extends React.ImgHTMLAttributes<HTMLImageElement> {
 }
 
 const CoinLogo = ({ network, className, ...rest }: Props) => {
-    return (
-        // eslint-disable-next-line import/no-dynamic-require
-        <Logo className={className} src={require(`../../images/coins/${network}.png`)} {...rest} />
-    );
+    const Component = LOGOS[network];
+    return <Component width={32} height={32} />;
 };
 
 CoinLogo.propTypes = {
