f1aa42dbc24ed7a46d56bfb78baac4adc196a3c8
slowbackspace <slowbackspace@gmail.com>
2019-07-30 13:45:14 +0200

    add NoSSR component

---
+++ packages/suite/src/support/suite/NoSSR.tsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from 'react';
+
+interface Props {
+    children: React.ReactNode;
+    fallbackComponent?: any;
+}
+
+const NoSSR = ({ children, fallbackComponent = null }: Props) => {
+    const [shouldRender, setShouldRender] = useState(false);
+
+    useEffect(() => {
+        // should fire on equivalent of componentDidMount
+        setShouldRender(true);
+    }, []);
+
+    return shouldRender ? children : fallbackComponent;
+};
+
+export default NoSSR;
