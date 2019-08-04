b9a8099ad351fd6e04b90854b60eb0f4672baf34
Szymon Lesisz <sz.lesisz@gmail.com>
2019-08-04 12:30:33 +0200

    move @wallet-services > @wallet-middlewares

--- packages/suite/src/middlewares/wallet/coingeckoMiddleware.ts
+++ packages/suite/src/middlewares/wallet/coingeckoMiddleware.ts
@@ -1,14 +1,9 @@
+import { MiddlewareAPI } from 'redux';
+import { AppState, Action, Dispatch } from '@suite-types/index';
+import { SUITE } from '@suite-actions/constants';
 import { httpRequest } from '@wallet-utils/networkUtils';
 import { resolveAfter } from '@wallet-utils/promiseUtils';
 import fiatConfig from '@suite-config/fiat';
-import { SUITE } from '@suite-actions/constants';
-// import { READY } from 'actions/constants/localStorage';
-// import * as TOKEN from 'actions/constants/token';
-// import { Token } from 'reducers/TokensReducer';
-import { MiddlewareAPI } from 'redux';
-import { AppState, Action, Dispatch } from '@suite-types';
-
-// const BASE_URL = 'https://api.coingecko.com/';
 
 export const RATE_UPDATE = '@rate/update';
 
@@ -22,18 +17,6 @@ export interface FiatRateActions {
     network: string;
     rates: { [key: string]: number };
 }
-
-// interface CoinGeckoToken {
-//     name: string;
-//     symbol: string;
-//     id: string;
-// }
-
-// const getSupportedCurrencies = async () => {
-//     const url = 'https://api.coingecko.com/api/v3/simple/supported_vs_currencies';
-//     const res = await httpRequest(url, 'json');
-//     return res;
-// };
 
 const loadRateAction = () => async (dispatch: Dispatch): Promise<void> => {
     // const { config } = getState().localStorage;
@@ -59,6 +42,41 @@ const loadRateAction = () => async (dispatch: Dispatch): Promise<void> => {
 
     await resolveAfter(50000);
 };
+
+const coingeckoMiddleware = (api: MiddlewareAPI<Dispatch, AppState>) => (next: Dispatch) => (
+    action: Action,
+): Action => {
+    // pass action
+    next(action);
+
+    // TODO: change to WALLET.READY
+    if (action.type === SUITE.READY) {
+        api.dispatch(loadRateAction());
+    }
+
+    // if (action.type === TOKEN.ADD) {
+    //     api.dispatch(loadTokenRateAction(action.payload));
+    // }
+    return action;
+};
+
+export default coingeckoMiddleware;
+
+// ----- old CoingeckoService
+
+// const BASE_URL = 'https://api.coingecko.com/';
+
+// interface CoinGeckoToken {
+//     name: string;
+//     symbol: string;
+//     id: string;
+// }
+
+// const getSupportedCurrencies = async () => {
+//     const url = 'https://api.coingecko.com/api/v3/simple/supported_vs_currencies';
+//     const res = await httpRequest(url, 'json');
+//     return res;
+// };
 
 // const fetchCoinRate = async (id: string): Promise<any> => {
 //     const url = new URL(`/api/v3/coins/${id}`, BASE_URL);
@@ -103,26 +121,3 @@ const loadRateAction = () => async (dispatch: Dispatch): Promise<void> => {
 //         console.log(err);
 //     }
 // };
-
-/**
- * Middleware
- */
-const CoingeckoService = (api: MiddlewareAPI<Dispatch, AppState>) => (next: Dispatch) => (
-    action: Action,
-): Action => {
-    next(action);
-
-    // TODO: change to WALLET.READY
-    if (action.type === SUITE.READY) {
-        console.log('suite ready');
-        api.dispatch(loadRateAction());
-    }
-
-    // if (action.type === TOKEN.ADD) {
-    //     api.dispatch(loadTokenRateAction(action.payload));
-    // }
-
-    return action;
-};
-
-export default CoingeckoService;
