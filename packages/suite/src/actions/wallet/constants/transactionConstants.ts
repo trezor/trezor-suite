52ca7ad57446e148c4402a26981de1cf079f82d0
slowbackspace <slowbackspace@gmail.com>
2019-08-06 15:04:13 +0200

    remove comment

--- packages/suite/src/actions/wallet/constants/transactionConstants.ts
+++ packages/suite/src/actions/wallet/constants/transactionConstants.ts
@@ -2,4 +2,3 @@ export const ADD = '@transaction/add';
 export const REMOVE = '@transaction/remove';
 export const UPDATE = '@transaction/update';
 export const FROM_STORAGE = '@transaction/from-storage';
-// export const FETCH = '@token/remove';
