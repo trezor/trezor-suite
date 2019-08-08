03abc92905739d7cde8e1c69cf6fe4fceafbf499
martin <mvarmuza@gmail.com>
2019-08-08 16:07:35 +0200

    fix healht test. wallet.trezor.io/data returns 403 (no listing allowed) so it cant be in constants

--- packages/suite/src/constants/urls.ts
+++ packages/suite/src/constants/urls.ts
@@ -2,4 +2,3 @@ export const TREZOR_URL = 'https://trezor.io';
 export const SUPPORT_URL = 'https://trezor.io/support/';
 export const WIKI_URL = 'https://wiki.trezor.io/';
 export const BLOG_URL = 'https://blog.trezor.io/';
-export const TREZOR_DATA_URL = 'https://wallet.trezor.io/data/';
