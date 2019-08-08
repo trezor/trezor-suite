393f2d61d05b2f97d613284fc95dc583b69c64bc
Szymon Lesisz <sz.lesisz@gmail.com>
2019-08-08 14:25:25 +0200

    remove trezor-connect-lib dir

--- packages/trezor-connect-lib/lib/types/uiRequest.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
-
-var POPUP = _interopRequireWildcard(require("../constants/popup"));
-
-var IFRAME = _interopRequireWildcard(require("../constants/iframe"));
-
-var UI = _interopRequireWildcard(require("../constants/ui"));
--- packages/trezor-connect-lib/lib/types/uiResponse.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-
-var _interopRequireWildcard = require("@babel/runtime/helpers/interopRequireWildcard");
-
-var UI = _interopRequireWildcard(require("../constants/ui"));
