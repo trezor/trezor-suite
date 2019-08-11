d855e0dad1ef8634391bb75a096941623431cc63
martin <30367552+mroz22@users.noreply.github.com>
2019-08-11 20:51:36 +0200

    redirect uninitialized device to onboarding automatically (#364)
    * redirect uninitialized device to onboarding automatically
    
    * better types and use getRoute
    
    * make typescript happy

--- packages/suite/src/utils/suite/router.ts
+++ packages/suite/src/utils/suite/router.ts
@@ -1,6 +1,8 @@
 import { routes } from '@suite-constants/routes';
 
-const PARAMS = ['coin', 'accountId'];
+type AnyRouteName = typeof routes[number]['name'];
+
+const PARAMS = ['coin', 'accountId'] as const;
 
 // Prefix a url with assetPrefix (eg. name of the branch in CI)
 // Useful with NextJS's Router.push() that accepts `as` prop as second arg
@@ -28,7 +30,7 @@ export const getParams = (url: string) => {
     return params;
 };
 
-export const getRoute = (name: string, params?: { [key: string]: string }) => {
+export const getRoute = (name: AnyRouteName | string, params?: { [key: string]: string }) => {
     const entry = routes.find(r => r.name === name);
     if (!entry) {
         // eslint-disable-next-line no-console
@@ -77,7 +79,7 @@ export const isInternalRoute = (route: string) => {
     return !!routes.find(r => r.pattern === toInternalRoute(route));
 };
 
-export const isStatic = (route: string) => {
+export const isStatic = (route: AnyRouteName | string) => {
     const routeFound = routes.find(r => r.pattern === route);
     return routeFound ? !!routeFound.isStatic : true; // 404 page act as a static
 };
