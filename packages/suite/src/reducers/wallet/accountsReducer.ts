8c7ee52c1d422ea4a4bb83688127e5949aafc1ec
Szymon Lesisz <sz.lesisz@gmail.com>
2019-08-12 10:34:21 +0200

    eslint fix in accountsReducer

--- packages/suite/src/reducers/wallet/accountsReducer.ts
+++ packages/suite/src/reducers/wallet/accountsReducer.ts
@@ -23,18 +23,18 @@ export interface Account {
 export const initialState: Account[] = [];
 
 // const create = (state: Account[], action: Action) => {
-    // const { network, rates } = action;
-    // const affected = state.find(f => f.network === network);
-    // Object.keys(rates).map(k => rates[k].toFixed(2));
-    // if (!affected) {
-    //     state.push({
-    //         network,
-    //         rates,
-    //     });
-    // } else {
-    //     affected.network = network;
-    //     affected.rates = rates;
-    // }
+// const { network, rates } = action;
+// const affected = state.find(f => f.network === network);
+// Object.keys(rates).map(k => rates[k].toFixed(2));
+// if (!affected) {
+//     state.push({
+//         network,
+//         rates,
+//     });
+// } else {
+//     affected.network = network;
+//     affected.rates = rates;
+// }
 // };
 
 export default (state: Account[] = initialState, action: Action): Account[] => {
