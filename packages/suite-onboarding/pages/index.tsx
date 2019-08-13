869f8d698255edf250e829d056b4798a861febc1
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-08-13 10:33:29 +0200

    Propagate static files across the monorepo, remove build target for suite-onboarding (#387)
    * add data package
    
    * add translations
    
    * rename package
    
    * refactor code
    
    * remove static folders from packages
    
    * ignore static folder
    
    * remove duplicated files, add scripts
    
    * remove suite-onbaording
    
    * lerna bootstrap after install in every package
    
    * remove lerna postinstall
    
    * move deps
    
    * fix react-native
    
    * fix install script
    
    * remove suite-onboarding from CI
    
    * install missing dep
    
    * removed locks - use just yarn root lock

--- packages/suite-onboarding/pages/index.tsx
+++
@@ -1,3 +0,0 @@
-import Index from '@suite/views/onboarding';
-
-export default Index;
--- packages/suite/src/actions/suite/suiteActions.ts
+++ packages/suite/src/actions/suite/suiteActions.ts
@@ -26,7 +26,7 @@ export const updateOnlineStatus = () => (dispatch: Dispatch) => {
     };
     statusHandler();
     // TODO: not working in react-native
-    if (typeof window !== 'undefined') {
+    if (typeof window !== 'undefined' && window.addEventListener) {
         window.addEventListener('online', statusHandler);
         window.addEventListener('offline', statusHandler);
     }
