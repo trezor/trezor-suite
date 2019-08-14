27f3ba82c6b76e047f5dcbd0f50258c6bcc41f0f
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-08-14 15:38:15 +0200

    Unify global styles for onboarding

--- packages/suite/src/support/suite/styles/animations.ts
+++ packages/suite/src/support/suite/styles/animations.ts
@@ -1,5 +1,5 @@
 import { css } from 'styled-components';
-// for react-transition used in coint/account menu
+import { STEP_ANIMATION_DURATION } from '@onboarding-constants/constants';
 
 const animationStyles = css`
     .slide-left-enter {
@@ -38,6 +38,50 @@ const animationStyles = css`
     .slide-right-exit.slide-right-exit-active {
         transform: translate(-200%);
         transition: transform 300ms ease-in-out;
+    }
+
+    .step-transition-enter {
+        opacity: 0;
+        transform: translateX(+20%);
+        position: absolute;
+    }
+
+    .step-transition-enter-active {
+        opacity: 1;
+        transform: translateX(0);
+        transition: opacity ${STEP_ANIMATION_DURATION}ms cubic-bezier(1, -0.01, 1, -0.02),
+            transform ${STEP_ANIMATION_DURATION}ms linear;
+    }
+
+    .step-transition-exit {
+        opacity: 1;
+        transform: translateX(0);
+    }
+
+    .step-transition-exit-active {
+        opacity: 0;
+        transform: translateX(-20%);
+        transition: opacity ${STEP_ANIMATION_DURATION}ms cubic-bezier(0, 1.01, 0, 1),
+            transform ${STEP_ANIMATION_DURATION}ms linear;
+    }
+
+    .fade-out-enter {
+        opacity: 0;
+    }
+
+    .fade-out-enter-active {
+        opacity: 1;
+        transition: opacity 1s;
+    }
+
+    .fade-out-exit {
+        opacity: 1;
+    }
+
+    .fade-out-exit-active {
+        /* transform: translateY(-20%); */
+        opacity: 0;
+        transition: opacity 1s;
     }
 `;
 
