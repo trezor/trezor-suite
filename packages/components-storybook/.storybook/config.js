b403d5a22cc61a3bf44140de90c3975e199bf3f9
Jan Czibula <>
2019-08-15 15:03:40 +0200

    fix storybook 5 theme

--- packages/components-storybook/.storybook/config.js
+++ packages/components-storybook/.storybook/config.js
@@ -1,42 +1,11 @@
-import { addDecorator, configure } from '@storybook/react';
-import { withOptions } from '@storybook/addon-options';
-import { withInfo } from '@storybook/addon-info';
-import { configureViewport } from '@storybook/addon-viewport';
-import { colors } from '@trezor/components';
+import { addParameters, configure } from '@storybook/react';
+import Theme from './theme';
 
-addDecorator(
-    withOptions({
-        name: 'Trezor UI Components',
-        url: '#',
-        addonPanelInRight: true,
-        theme: {
-            mainBackground: colors.BACKGROUND,
-            mainBorder: `1px solid ${colors.DIVIDER}`,
-            mainFill: colors.WHITE,
-            barFill: colors.GRAY_LIGHT,
-            mainTextFace: 'Roboto',
-            mainTextColor: colors.TEXT_PRIMARY,
-            layoutMargin: 10,
-            mainTextSize: 14,
-            treeMenuHeader: {
-                color: colors.TEXT_PRIMARY,
-                lineHeight: '1.4rem',
-            },
-            menuLink: {
-                color: colors.TEXT_SECONDARY,
-                fontSize: '.9rem',
-                lineHeight: '1.2rem',
-                marginLeft: 0,
-            },
-            activeMenuLink: {
-                color: colors.GREEN_PRIMARY,
-                background: 'none',
-            },
-        },
-    }),
-);
-
-configureViewport();
+addParameters({
+    options: {
+        theme: Theme
+    }
+});
 
 function loadStories() {
     require('../src/stories/components/text.tsx');
