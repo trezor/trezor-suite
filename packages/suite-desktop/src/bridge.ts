e49f74881385eca1695194bd030ef8892a8779e2
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-08-23 16:12:46 +0200

    eslit and ts fixes

--- packages/suite-desktop/src/bridge.ts
+++ packages/suite-desktop/src/bridge.ts
@@ -2,24 +2,22 @@ const psList = require('ps-list');
 const os = require('os');
 const { spawn } = require('child_process');
 
+const TREZOR_PROCESS_NAME = 'trezord';
+
 const processes = await psList();
-const bridgeProcess = processes.find(ps => ps.name.includes('trezord'));
 const platform = os.platform();
+const bridgeProcess = processes.find(ps => ps.name.includes(TREZOR_PROCESS_NAME));
 
 console.log({
     platform,
     bridgeProcess,
 });
 
-console.log('bridgeProcess', bridgeProcess);
-
 // bridge is not running, run
-// if(!bridgeProcess) {
-
+if(!bridgeProcess) {
     switch(platform) {
         case "darwin": {
             spawn('ls', ['-lh', '/usr']);
         }
     }
-
-// }
+}
