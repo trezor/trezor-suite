e6cca4a2ff7c9a6e211a6ddcce027584798262b0
Jan Czibula <jan.czibula@gmail.com>
2019-08-23 12:20:50 +0200

    add PinInvalid modal

---
+++ packages/suite/src/components/suite/modals/PinInvalid/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import styled from 'styled-components';
+import { FormattedMessage } from 'react-intl';
+
+import { P, H5 } from '@trezor/components';
+
+import { TrezorDevice } from '@suite-types';
+import l10nMessages from './messages';
+
+interface Props {
+    device: TrezorDevice;
+}
+
+const Wrapper = styled.div`
+    padding: 30px 48px;
+`;
+
+const InvalidPin = (props: Props) => (
+    <Wrapper>
+        <H5>
+            <FormattedMessage
+                {...l10nMessages.TR_ENTERED_PIN_NOT_CORRECT}
+                values={{ deviceLabel: props.device.label }}
+            />
+        </H5>
+        <P size="small">
+            <FormattedMessage {...l10nMessages.TR_RETRYING_DOT_DOT} />
+        </P>
+    </Wrapper>
+);
+
+export default InvalidPin;
