b70231a1c6ccaf038c72893a2cfc7f7903c2b775
Jan Czibula <jan.czibula@gmail.com>
2019-08-26 13:29:09 +0200

    Add ConfirmNoBackup modal

---
+++ packages/suite/src/components/suite/modals/confirm/NoBackup/index.tsx
@@ -0,0 +1,59 @@
+import React, { FunctionComponent } from 'react';
+import styled from 'styled-components';
+import { FormattedMessage } from 'react-intl';
+
+import { H5, P, Icon, Button, Link, colors } from '@trezor/components';
+import l10nCommonMessages from '../../messages';
+
+import { TrezorDevice } from '@suite-types';
+
+const Wrapper = styled.div`
+    max-width: 370px;
+    padding: 30px 48px;
+`;
+
+const BackupButton = styled(Button)`
+    width: 100%;
+    margin-bottom: 10px;
+`;
+
+const StyledP = styled(P)`
+    /* boost-specificity hack to override P base styling */
+    && {
+        padding-bottom: 20px;
+    }
+`;
+
+const Row = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+interface Props {
+    onReceiveConfirmation: (confirmed: boolean) => void;
+    device: TrezorDevice;
+}
+
+const Confirmation: FunctionComponent<Props> = ({ device, onReceiveConfirmation }) => (
+    <Wrapper>
+        <H5>
+            <FormattedMessage {...l10nCommonMessages.TR_YOUR_TREZOR_IS_NOT_BACKED_UP} />
+        </H5>
+        <Icon size={32} color={colors.WARNING_PRIMARY} icon="WARNING" />
+        <StyledP size="small">
+            <FormattedMessage {...l10nCommonMessages.TR_IF_YOUR_DEVICE_IS_EVER_LOST} />
+        </StyledP>
+        <Row>
+            <Link href={`/?backup#${device.path}`} target="_self">
+                <BackupButton onClick={() => onReceiveConfirmation(false)}>
+                    <FormattedMessage {...l10nCommonMessages.TR_CREATE_BACKUP_IN_3_MINUTES} />
+                </BackupButton>
+            </Link>
+            <Button isInverse variant="warning" onClick={() => onReceiveConfirmation(true)}>
+                <FormattedMessage {...l10nCommonMessages.TR_SHOW_ADDRESS_I_WILL_TAKE_THE_RISK} />
+            </Button>
+        </Row>
+    </Wrapper>
+);
+
+export default Confirmation;
--- packages/suite/src/components/suite/modals/messages.ts
+++ packages/suite/src/components/suite/modals/messages.ts
@@ -14,6 +14,23 @@ const definedMessages = defineMessages({
         defaultMessage: 'Account #{number}',
         description: 'Used in auto-generated account label',
     },
+    TR_YOUR_TREZOR_IS_NOT_BACKED_UP: {
+        id: 'TR_YOUR_TREZOR_IS_NOT_BACKED_UP',
+        defaultMessage: 'Your Trezor is not backed up',
+    },
+    TR_IF_YOUR_DEVICE_IS_EVER_LOST: {
+        id: 'TR_IF_YOUR_DEVICE_IS_EVER_LOST',
+        defaultMessage:
+            'If your device is ever lost or damaged, your funds will be lost. Backup your device first, to protect your coins against such events.',
+    },
+    TR_CREATE_BACKUP_IN_3_MINUTES: {
+        id: 'TR_CREATE_BACKUP_IN_3_MINUTES',
+        defaultMessage: 'Create a backup in 3 minutes',
+    },
+    TR_SHOW_ADDRESS_I_WILL_TAKE_THE_RISK: {
+        id: 'TR_SHOW_ADDRESS_I_WILL_TAKE_THE_RISK',
+        defaultMessage: 'Show address, I will take the risk',
+    },
 });
 
 export default definedMessages;
