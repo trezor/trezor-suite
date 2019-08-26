c9d5cb639c5a90f2d94f40f046da30814cbc5beb
Jan Czibula <jan.czibula@gmail.com>
2019-08-26 11:45:35 +0200

    add ConfirmAction modal

---
+++ packages/suite/src/components/suite/modals/confirm/Action/messages.ts
@@ -0,0 +1,10 @@
+import { defineMessages } from 'react-intl';
+
+const definedMessages = defineMessages({
+    TR_CONFIRM_ACTION_ON_YOUR: {
+        id: 'TR_CONFIRM_ACTION_ON_YOUR',
+        defaultMessage: 'Confirm action on your "{deviceLabel}" device.',
+    },
+});
+
+export default definedMessages;
