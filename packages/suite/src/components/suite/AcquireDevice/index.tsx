75f9cb25a735c54a4521bb11489a7a0fbeabd4b4
Szymon Lesisz <sz.lesisz@gmail.com>
2019-08-27 15:14:41 +0200

    update AcquireDevice component

--- packages/suite/src/components/suite/AcquireDevice/index.tsx
+++ packages/suite/src/components/suite/AcquireDevice/index.tsx
@@ -1,34 +1,21 @@
 import React, { FunctionComponent } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import TrezorConnect from 'trezor-connect';
 import { injectIntl, InjectedIntlProps } from 'react-intl';
 
 import { Notification } from '@trezor/components';
-import { selectDevice } from '@suite-actions/suiteActions';
+import { acquireDevice } from '@suite-actions/suiteActions';
 import { AppState } from '@suite-types';
 import l10nMessages from './index.messages';
 
 interface Props extends InjectedIntlProps {
     device: AppState['suite']['device'];
-    selectDevice: typeof selectDevice;
+    acquireDevice: typeof acquireDevice;
 }
 
-const Selection: FunctionComponent<Props> = props => {
+const Acquire: FunctionComponent<Props> = props => {
     const { device } = props;
-
     if (!device) return null;
-
-    const onClick = async () => {
-        const resp = await TrezorConnect.getFeatures({
-            device,
-        });
-
-        if (resp.success) {
-            // acquire complete!
-        }
-    };
-
     return (
         <Notification
             title={props.intl.formatMessage(l10nMessages.TR_DEVICE_USED_IN_OTHER)}
@@ -41,9 +28,7 @@ const Selection: FunctionComponent<Props> = props => {
             actions={[
                 {
                     label: props.intl.formatMessage(l10nMessages.TR_ACQUIRE_DEVICE),
-                    callback: () => {
-                        onClick();
-                    },
+                    callback: props.acquireDevice,
                 },
             ]}
         />
@@ -58,7 +43,7 @@ export default injectIntl(
     connect(
         mapStateToProps,
         dispatch => ({
-            selectDevice: bindActionCreators(selectDevice, dispatch),
+            acquireDevice: bindActionCreators(acquireDevice, dispatch),
         }),
-    )(Selection),
+    )(Acquire),
 );
