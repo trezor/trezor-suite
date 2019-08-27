f487af7f45bd20adaf0394745af7ffd7d2ca28e8
Jan Czibula <jan.czibula@gmail.com>
2019-08-27 13:30:44 +0200

    fix #440 added react-focus-lock, added useKeyPress hook

--- packages/suite/src/components/suite/modals/Passphrase/index.tsx
+++ packages/suite/src/components/suite/modals/Passphrase/index.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 import { H5, P, Button, Input, Checkbox, colors } from '@trezor/components';
 import { FormattedMessage } from 'react-intl';
+import { useKeyPress } from '@suite-utils/hooks';
 
 import modalsMessages from '../messages';
 import messages from './messages';
@@ -52,13 +53,24 @@ interface State {
 interface Props {
     device: TrezorDevice;
     onEnterPassphrase: (device: TrezorDevice, value: string) => void;
+    shouldShowSingleInput?: boolean;
 }
 
-const Passphrase: FunctionComponent<Props> = ({ device, onEnterPassphrase }) => {
+const Passphrase: FunctionComponent<Props> = ({
+    device,
+    onEnterPassphrase,
+    shouldShowSingleInput,
+}) => {
     const [value, setValue] = useState<State['value']>('');
     const [valueAgain, setValueAgain] = useState<State['valueAgain']>('');
     const [showPassword, setShowPassword] = useState(false);
     const type: State['type'] = showPassword ? 'text' : 'password';
+    const passwordsMatch = shouldShowSingleInput || showPassword ? true : value === valueAgain;
+    const enterPressed = useKeyPress('Enter');
+
+    if (enterPressed) {
+        onEnterPassphrase(device, value);
+    }
 
     return (
         <Wrapper>
@@ -82,15 +94,18 @@ const Passphrase: FunctionComponent<Props> = ({ device, onEnterPassphrase }) => 
                     value={value}
                 />
             </FormRow>
-            <FormRow>
-                <Input
-                    onChange={event => setValueAgain(event.target.value)}
-                    placeholder=""
-                    topLabel="Confirm Passphrase"
-                    type={type}
-                    value={valueAgain}
-                />
-            </FormRow>
+            {!shouldShowSingleInput && (
+                <FormRow>
+                    <Input
+                        onChange={event => setValueAgain(event.target.value)}
+                        placeholder=""
+                        topLabel="Confirm Passphrase"
+                        type={type}
+                        value={showPassword ? value : valueAgain}
+                        disabled={!!showPassword}
+                    />
+                </FormRow>
+            )}
             <FormRow>
                 <Checkbox onClick={() => setShowPassword(!showPassword)} isChecked={showPassword}>
                     Show passphrase
@@ -99,11 +114,11 @@ const Passphrase: FunctionComponent<Props> = ({ device, onEnterPassphrase }) => 
             <Column>
                 <Button
                     onClick={() => onEnterPassphrase(device, value)}
-                    isDisabled={value !== valueAgain}
+                    isDisabled={!passwordsMatch}
                 >
                     <FormattedMessage {...messages.TR_ENTER_PASSPHRASE} />
                 </Button>
-                <ErrorMessage size="small" show={value !== valueAgain}>
+                <ErrorMessage size="small" show={!passwordsMatch}>
                     <FormattedMessage {...messages.PASSPHRASE_DO_NOT_MATCH} />
                 </ErrorMessage>
                 <BottomMessage size="small">
