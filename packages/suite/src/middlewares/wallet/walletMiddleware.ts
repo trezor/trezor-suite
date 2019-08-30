fa606ea23768be85f31d1a55cb7cd2ecd60c4423
Szymon Lesisz <sz.lesisz@gmail.com>
2019-08-30 10:18:53 +0200

    Update walletMiddleware.ts

--- packages/suite/src/middlewares/wallet/walletMiddleware.ts
+++ packages/suite/src/middlewares/wallet/walletMiddleware.ts
@@ -1,35 +1,48 @@
 import { MiddlewareAPI } from 'redux';
-import { LOCATION_CHANGE } from '@suite/actions/suite/routerActions';
+import { LOCATION_CHANGE } from '@suite-actions/routerActions';
+import * as suiteActions from '@suite-actions/suiteActions';
 import * as selectedAccountActions from '@wallet-actions/selectedAccountActions';
 import { loadStorage } from '@wallet-actions/storageActions';
 import * as walletActions from '@wallet-actions/walletActions';
+import * as discoveryActions from '@wallet-actions/discoveryActions';
 import { SUITE } from '@suite/actions/suite/constants';
 import { WALLET, DISCOVERY } from '@wallet-actions/constants';
 import { AppState, Action, Dispatch } from '@suite-types';
 
 // Flow: LOCATION.CHANGE -> WALLET.INIT -> load storage -> WALLET.INIT_SUCCESS
-const walletMiddleware = (api: MiddlewareAPI<Dispatch, AppState>) => (next: Dispatch) => (
+const walletMiddleware = (api: MiddlewareAPI<Dispatch, AppState>) => (next: Dispatch) => async (
     action: Action,
-): Action => {
+): Promise<Action> => {
     const prevState = api.getState();
     // pass action
-    next(action);
+    await next(action);
 
     // runs only in wallet app
-    if (prevState.router.app !== 'wallet' && action.type !== LOCATION_CHANGE) return action;
+    if (api.getState().router.app !== 'wallet') return action;
+
+    if (action.type === SUITE.SELECT_DEVICE || action.type === WALLET.INIT_SUCCESS) {
+        // TODO: if device have a state, run discovery
+        api.dispatch(suiteActions.requestPassphraseMode());
+    }
+    if (action.type === SUITE.RECEIVE_PASSPHRASE_MODE) {
+        api.dispatch(suiteActions.authorizeDevice());
+    }
+    if (action.type === SUITE.AUTH_DEVICE) {
+        api.dispatch(discoveryActions.start());
+    }
+
     switch (action.type) {
         case DISCOVERY.UPDATE:
             // update discovery in selectedAccount
             api.dispatch(selectedAccountActions.observe(prevState, action));
             break;
-        case LOCATION_CHANGE: {
+        case LOCATION_CHANGE:
             // update selected account if needed
             api.dispatch(selectedAccountActions.observe(prevState, action));
 
             // dispatch wallet init, then load storage
             api.dispatch(walletActions.init());
             break;
-        }
 
         case WALLET.INIT:
             api.dispatch(loadStorage());
