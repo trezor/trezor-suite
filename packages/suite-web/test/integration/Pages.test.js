a8dd90bdfc66b7189ca23cd1453314efe3b4ed0c
Jan Czibula <jan.czibula@gmail.com>
2019-09-02 15:34:07 +0200

    try run just single test

--- packages/suite-web/test/integration/Pages.test.js
+++ packages/suite-web/test/integration/Pages.test.js
@@ -17,27 +17,27 @@ describe('Pages', () => {
             .should('match', /onboarding/);
     });
 
-    it(`test / page is online after initial run`, () => {
-        cy.viewport(1024, 768)
-            .visit('/', {
-                onBeforeLoad: win => {
-                    win.initialState = {
-                        suite: {
-                            initialRun: false,
-                        },
-                    };
-                },
-            })
-            .get('html')
-            .should('contain', 'Connect Trezor to continue');
-    });
+    // it(`test / page is online after initial run`, () => {
+    //     cy.viewport(1024, 768)
+    //         .visit('/', {
+    //             onBeforeLoad: win => {
+    //                 win.initialState = {
+    //                     suite: {
+    //                         initialRun: false,
+    //                     },
+    //                 };
+    //             },
+    //         })
+    //         .get('html')
+    //         .should('contain', 'Connect Trezor to continue');
+    // });
 
-    it(`test /version page is online`, () => {
-        cy.viewport(1024, 768)
-            .visit('/version')
-            .get('html')
-            .should('contain', 'version');
-    });
+    // it(`test /version page is online`, () => {
+    //     cy.viewport(1024, 768)
+    //         .visit('/version')
+    //         .get('html')
+    //         .should('contain', 'version');
+    // });
 
     // todo: 404 also requires device now
     // it('should render a nice 404 page', () => {
