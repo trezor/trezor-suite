b329478b105aa8d62a3f176ef1969d94c0ce4771
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-09-02 16:54:10 +0200

    add review

--- packages/suite/src/utils/wallet/reducerUtils.ts
+++ packages/suite/src/utils/wallet/reducerUtils.ts
@@ -120,8 +120,8 @@ export const getSelectedAccount = (
     return accounts.find(a => a.index === index && a.network === routerParams.coin) || null;
 };
 
-export const getSelectedNetwork = (networks: Network[], networkSymbol: string) => {
-    return networks.find(c => c.symbol === networkSymbol) || null;
+export const getSelectedNetwork = (networks: Network[], symbol: string) => {
+    return networks.find(c => c.symbol === symbol) || null;
 };
 
 export const getDiscoveryProcess = (
