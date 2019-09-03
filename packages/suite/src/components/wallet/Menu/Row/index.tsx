6fa05bd97bbb9fc2dcd7498fb0e4ff7957de8065
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-09-03 19:31:18 +0200

    split components

---
+++ packages/suite/src/components/wallet/Menu/Row/index.tsx
@@ -0,0 +1,155 @@
+import React from 'react';
+import { CoinLogo, colors, variables } from '@trezor/components';
+import styled from 'styled-components';
+import { getRoute } from '@suite-utils/router';
+import { Link } from '@suite-components';
+import { NETWORKS } from '@suite-config';
+
+const Wrapper = styled.div`
+    padding: 0 15px;
+    display: flex;
+    height: 55px;
+    cursor: pointer;
+    transition: background-color 0.3s, color 0.3s;
+    justify-content: space-between;
+
+    &:hover {
+        background-color: ${colors.GRAY_LIGHT};
+    }
+
+    &:first-child {
+        padding-top: 0;
+    }
+`;
+
+const CoinName = styled.div``;
+
+const LogoWrapper = styled.div`
+    min-width: 40px;
+    display: flex;
+    align-items: center;
+`;
+
+const AccountIndex = styled.div`
+    display: flex;
+    align-items: center;
+    font-size: ${variables.FONT_SIZE.SMALL};
+`;
+
+const BalanceValue = styled.div`
+    text-transform: uppercase;
+    font-size: ${variables.FONT_SIZE.BIG};
+`;
+
+const Left = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+`;
+
+const Name = styled.div`
+    display: flex;
+    flex-direction: column;
+    font-size: ${variables.FONT_SIZE.BIG};
+    color: ${colors.TEXT_PRIMARY};
+`;
+
+const Right = styled.div`
+    display: flex;
+    flex: 1;
+    align-items: flex-end;
+    justify-content: center;
+    flex-direction: column;
+`;
+
+const Label = styled.span`
+    display: flex;
+    justify-content: center;
+    text-transform: uppercase;
+    padding-right: 3px;
+    font-size: ${variables.FONT_SIZE.COUNTER};
+    color: ${colors.TEXT_SECONDARY};
+`;
+
+const Balance = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const Transactions = styled.div`
+    display: flex;
+    font-size: ${variables.FONT_SIZE.COUNTER};
+    align-items: center;
+`;
+
+const TransactionsValue = styled.div`
+    display: flex;
+    padding-right: 2px;
+    align-items: center;
+`;
+
+// todo make no style link component
+const StyledLink = styled(Link)`
+    display: flex;
+    flex-direction: column;
+    color: ${colors.TEXT_PRIMARY};
+
+    &:focus,
+    &:hover,
+    &:visited,
+    &:link,
+    &:active {
+        color: ${colors.TEXT_PRIMARY};
+        text-decoration: none;
+    }
+`;
+
+const getCoinName = (symbol: string, accountType: string) => {
+    const result = NETWORKS.filter(
+        network => network.symbol === symbol && (network.accountType || 'normal') === accountType,
+    );
+    return result[0].name;
+};
+
+const getCoinLogo = (network: string) => (network === 'test' ? 'btc' : network);
+
+const Row = ({ account }) => (
+    <StyledLink
+        key={`${account.network}-${account.descriptor}`}
+        href={getRoute('wallet-account', {
+            accountId: account.index,
+            symbol: account.network,
+            accountType: account.accountType,
+        })}
+    >
+        <Wrapper>
+            <Left>
+                <LogoWrapper>
+                    <CoinLogo size={25} network={getCoinLogo(account.network)} />
+                </LogoWrapper>
+                <Name>
+                    <CoinName>{getCoinName(account.network, account.accountType)}</CoinName>
+                    <AccountIndex>
+                        <Label>account</Label>
+                        {`#${account.index + 1}`}
+                    </AccountIndex>
+                </Name>
+            </Left>
+            <Right>
+                <Balance>
+                    <BalanceValue>
+                        {account.balance} {account.network}
+                    </BalanceValue>
+                </Balance>
+                {account.history.total !== -1 && (
+                    <Transactions>
+                        <Label>transactions</Label>
+                        <TransactionsValue>{account.history.total}</TransactionsValue>
+                    </Transactions>
+                )}
+            </Right>
+        </Wrapper>
+    </StyledLink>
+);
+
+export default Row;
--- packages/suite/src/components/wallet/Menu/index.tsx
+++ packages/suite/src/components/wallet/Menu/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { CoinLogo, colors, variables, Loader } from '@trezor/components';
+import { colors, variables, Loader } from '@trezor/components';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
-import { getRoute } from '@suite-utils/router';
-import { Link } from '@suite-components';
 import { AppState } from '@suite-types';
-import { NETWORKS } from '@suite-config';
+import Row from './Row';
 
 import ProgressBar from './ProgressBar';
 
@@ -25,105 +23,6 @@ const LoadingText = styled.div`
     padding-left: 10px;
 `;
 
-const CoinName = styled.div``;
-
-const Row = styled.div`
-    padding: 0 15px;
-    display: flex;
-    height: 55px;
-    cursor: pointer;
-    transition: background-color 0.3s, color 0.3s;
-    justify-content: space-between;
-
-    &:hover {
-        background-color: ${colors.GRAY_LIGHT};
-    }
-
-    &:first-child {
-        padding-top: 0;
-    }
-`;
-
-const LogoWrapper = styled.div`
-    min-width: 40px;
-    display: flex;
-    align-items: center;
-`;
-
-const AccountIndex = styled.div`
-    display: flex;
-    align-items: center;
-    font-size: ${variables.FONT_SIZE.SMALL};
-`;
-
-const BalanceValue = styled.div`
-    text-transform: uppercase;
-    font-size: ${variables.FONT_SIZE.BIG};
-`;
-
-const Left = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-`;
-
-const Name = styled.div`
-    display: flex;
-    flex-direction: column;
-    font-size: ${variables.FONT_SIZE.BIG};
-    color: ${colors.TEXT_PRIMARY};
-`;
-
-const Right = styled.div`
-    display: flex;
-    flex: 1;
-    align-items: flex-end;
-    justify-content: center;
-    flex-direction: column;
-`;
-
-const Label = styled.span`
-    display: flex;
-    justify-content: center;
-    text-transform: uppercase;
-    padding-right: 3px;
-    font-size: ${variables.FONT_SIZE.COUNTER};
-    color: ${colors.TEXT_SECONDARY};
-`;
-
-const Balance = styled.div`
-    display: flex;
-    align-items: center;
-`;
-
-const Transactions = styled.div`
-    display: flex;
-    font-size: ${variables.FONT_SIZE.COUNTER};
-    align-items: center;
-`;
-
-const TransactionsValue = styled.div`
-    display: flex;
-    padding-right: 2px;
-    align-items: center;
-`;
-
-// todo make no style link component
-const StyledLink = styled(Link)`
-    display: flex;
-    flex-direction: column;
-    color: ${colors.TEXT_PRIMARY};
-
-    &:focus,
-    &:hover,
-    &:visited,
-    &:link,
-    &:active {
-        color: ${colors.TEXT_PRIMARY};
-        text-decoration: none;
-    }
-`;
-
 interface Props {
     suite: AppState['suite'];
     wallet: AppState['wallet'];
@@ -139,15 +38,6 @@ const getLoadingProgress = (discovery: AppState['wallet']['discovery']) => {
     return 0;
 };
 
-const getCoinName = (symbol: string, accountType: string) => {
-    const result = NETWORKS.filter(
-        network => network.symbol === symbol && (network.accountType || 'normal') === accountType,
-    );
-    return result[0].name;
-};
-
-const getCoinLogo = (network: string) => (network === 'test' ? 'btc' : network); // TODO add icon for testnet
-
 const Menu = (props: Props) => (
     <Wrapper>
         <ProgressBar progress={getLoadingProgress(props.discovery)} />
@@ -158,46 +48,9 @@ const Menu = (props: Props) => (
             </LoadingWrapper>
         )}
         {props.wallet.accounts
-            // .filter(account => !account.empty)
+            .filter(account => !account.empty)
             .map(account => (
-                <StyledLink
-                    key={`${account.network}-${account.descriptor}`}
-                    href={getRoute('wallet-account', {
-                        accountId: account.index,
-                        symbol: account.network,
-                        accountType: account.accountType,
-                    })}
-                >
-                    <Row>
-                        <Left>
-                            <LogoWrapper>
-                                <CoinLogo size={25} network={getCoinLogo(account.network)} />
-                            </LogoWrapper>
-                            <Name>
-                                <CoinName>
-                                    {getCoinName(account.network, account.accountType)}
-                                </CoinName>
-                                <AccountIndex>
-                                    <Label>account</Label>
-                                    {`#${account.index + 1}`}
-                                </AccountIndex>
-                            </Name>
-                        </Left>
-                        <Right>
-                            <Balance>
-                                <BalanceValue>
-                                    {account.balance} {account.network}
-                                </BalanceValue>
-                            </Balance>
-                            {account.history.total !== -1 && (
-                                <Transactions>
-                                    <Label>transactions</Label>
-                                    <TransactionsValue>{account.history.total}</TransactionsValue>
-                                </Transactions>
-                            )}
-                        </Right>
-                    </Row>
-                </StyledLink>
+                <Row account={account} />
             ))}
     </Wrapper>
 );
