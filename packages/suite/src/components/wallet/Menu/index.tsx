9dd90c952e05474fd2c3cde85662af620af05e27
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-09-04 12:48:53 +0200

    Menu account type fix + ts in console (#479)
    * fix coin discovery by network
    
    * add typecheck to console
    
    * add account type param
    
    * split components
    
    * better progress bar
    
    * move to components
    
    * fix unit tests

--- packages/suite/src/components/wallet/Menu/index.tsx
+++ packages/suite/src/components/wallet/Menu/index.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
-import { CoinLogo, colors, variables, Loader } from '@trezor/components';
+import { colors, variables, Loader } from '@trezor/components';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
-import { getRoute } from '@suite-utils/router';
 import * as discoveryActions from '@wallet-actions/discoveryActions';
 import { Discovery } from '@wallet-reducers/discoveryReducer';
-import { Link } from '@suite-components';
 import { AppState, Dispatch } from '@suite-types';
-import { NETWORKS } from '@suite-config';
 
-import ProgressBar from './ProgressBar';
+import ProgressBar from './components/ProgressBar';
+import Row from './components/Row';
 
 const Wrapper = styled.div``;
 
@@ -25,105 +23,6 @@ const LoadingText = styled.div`
     font-size: ${variables.FONT_SIZE.SMALL};
     color: ${colors.TEXT_SECONDARY};
     padding-left: 10px;
-`;
-
-const CoinName = styled.div``;
-
-const Row = styled.div`
-    padding: 0 15px;
-    display: flex;
-    height: 55px;
-    cursor: pointer;
-    transition: background-color 0.3s, color 0.3s;
-    justify-content: space-between;
-
-    &:hover {
-        background-color: ${colors.GRAY_LIGHT};
-    }
-
-    &:first-child {
-        padding-top: 0;
-    }
-`;
-
-const LogoWrapper = styled.div`
-    min-width: 40px;
-    display: flex;
-    align-items: center;
-`;
-
-const AccountIndex = styled.div`
-    display: flex;
-    align-items: center;
-    font-size: ${variables.FONT_SIZE.SMALL};
-`;
-
-const BalanceValue = styled.div`
-    text-transform: uppercase;
-    font-size: ${variables.FONT_SIZE.BIG};
-`;
-
-const Left = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-`;
-
-const Name = styled.div`
-    display: flex;
-    flex-direction: column;
-    font-size: ${variables.FONT_SIZE.BIG};
-    color: ${colors.TEXT_PRIMARY};
-`;
-
-const Right = styled.div`
-    display: flex;
-    flex: 1;
-    align-items: flex-end;
-    justify-content: center;
-    flex-direction: column;
-`;
-
-const Label = styled.span`
-    display: flex;
-    justify-content: center;
-    text-transform: uppercase;
-    padding-right: 3px;
-    font-size: ${variables.FONT_SIZE.COUNTER};
-    color: ${colors.TEXT_SECONDARY};
-`;
-
-const Balance = styled.div`
-    display: flex;
-    align-items: center;
-`;
-
-const Transactions = styled.div`
-    display: flex;
-    font-size: ${variables.FONT_SIZE.COUNTER};
-    align-items: center;
-`;
-
-const TransactionsValue = styled.div`
-    display: flex;
-    padding-right: 2px;
-    align-items: center;
-`;
-
-// todo make no style link component
-const StyledLink = styled(Link)`
-    display: flex;
-    flex-direction: column;
-    color: ${colors.TEXT_PRIMARY};
-
-    &:focus,
-    &:hover,
-    &:visited,
-    &:link,
-    &:active {
-        color: ${colors.TEXT_PRIMARY};
-        text-decoration: none;
-    }
 `;
 
 const mapStateToProps = (state: AppState) => ({
@@ -146,15 +45,6 @@ const getLoadingProgress = (discovery?: Discovery) => {
     }
     return 0;
 };
-
-const getCoinName = (symbol: string, accountType: string) => {
-    const result = NETWORKS.filter(
-        network => network.symbol === symbol && (network.accountType || 'normal') === accountType,
-    );
-    return result[0].name;
-};
-
-const getCoinLogo = (network: string) => (network === 'test' ? 'btc' : network); // TODO add icon for testnet
 
 const Menu = ({ device, accounts, getDiscoveryForDevice }: Props) => {
     const discovery = getDiscoveryForDevice();
@@ -180,43 +70,7 @@ const Menu = ({ device, accounts, getDiscoveryForDevice }: Props) => {
                 </LoadingWrapper>
             )}
             {list.map(account => (
-                <StyledLink
-                    key={`${account.network}-${account.descriptor}`}
-                    href={getRoute('wallet-account', {
-                        coin: account.network,
-                        accountId: account.index,
-                    })}
-                >
-                    <Row>
-                        <Left>
-                            <LogoWrapper>
-                                <CoinLogo size={25} network={getCoinLogo(account.network)} />
-                            </LogoWrapper>
-                            <Name>
-                                <CoinName>
-                                    {getCoinName(account.network, account.accountType)}
-                                </CoinName>
-                                <AccountIndex>
-                                    <Label>account</Label>
-                                    {`#${account.index + 1}`}
-                                </AccountIndex>
-                            </Name>
-                        </Left>
-                        <Right>
-                            <Balance>
-                                <BalanceValue>
-                                    {account.balance} {account.network}
-                                </BalanceValue>
-                            </Balance>
-                            {account.history.total !== -1 && (
-                                <Transactions>
-                                    <Label>transactions</Label>
-                                    <TransactionsValue>{account.history.total}</TransactionsValue>
-                                </Transactions>
-                            )}
-                        </Right>
-                    </Row>
-                </StyledLink>
+                <Row account={account} />
             ))}
         </Wrapper>
     );
--- packages/suite/src/utils/suite/__tests__/router.test.ts
+++ packages/suite/src/utils/suite/__tests__/router.test.ts
@@ -43,14 +43,14 @@ describe('router', () => {
         it('should object of params parsed from the url', () => {
             expect(getParams('/suite-web/wallet/account/receive/#/trop/0')).toEqual({
                 accountId: '0',
-                coin: 'trop',
+                symbol: 'trop',
             });
             expect(getParams('/suite-web/wallet/account/receive/#/eth/1')).toEqual({
                 accountId: '1',
-                coin: 'eth',
+                symbol: 'eth',
             });
             expect(getParams('/suite-web/wallet/account/receive/#/eth')).toEqual({
-                coin: 'eth',
+                symbol: 'eth',
             });
         });
     });
@@ -61,13 +61,14 @@ describe('router', () => {
             expect(getRoute('wallet-account-transactions')).toEqual('/wallet/account/transactions');
             expect(
                 getRoute('wallet-account', {
-                    coin: 'eth',
+                    symbol: 'eth',
                     accountId: '0',
+                    accountType: 'segwit',
                 }),
-            ).toEqual('/wallet/account#/eth/0');
+            ).toEqual('/wallet/account#/eth/0/segwit');
             expect(
                 getRoute('wallet-account', {
-                    coin: 'eth',
+                    symbol: 'eth',
                 }),
             ).toEqual('/wallet/account#/eth');
         });
