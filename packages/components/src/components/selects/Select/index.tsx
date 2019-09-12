f89f3c78732deb354f7781c8647f4d9a2a3d57f5
Zuzana Jeschke <zuz@zuzjes.com>
2019-09-12 15:27:02 +0200

    Add label to select with some mistake

--- packages/components/src/components/selects/Select/index.tsx
+++ packages/components/src/components/selects/Select/index.tsx
@@ -3,20 +3,55 @@ import React from 'react';
 import ReactSelect from 'react-select';
 import { Props as SelectProps } from 'react-select/lib/Select';
 import { OptionProps } from 'react-select/lib/types';
+import styled, { css } from 'styled-components';
 import styles from '../styles';
+import colors from '../../../config/colors';
+
+const Wrapper = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+`;
+
+const TopLabel = styled.span`
+    padding-bottom: 10px;
+    color: ${colors.TEXT_SECONDARY};
+`;
 
 interface Props extends SelectProps<OptionProps> {
     isSearchable?: boolean;
     withDropdownIndicator?: boolean;
+    topLabel: React.ReactNode;
+    wrapperProps?: Record<string, any>;
 }
-
-const Select = ({ isSearchable = true, withDropdownIndicator = true, ...rest }: Props) => (
-    <ReactSelect
-        styles={styles(isSearchable, withDropdownIndicator)}
-        isSearchable={isSearchable}
-        {...rest}
-    />
-);
+// const Select = ({ isSearchable = true, withDropdownIndicator = true, ...rest }: Props) => (
+//     <ReactSelect
+//         styles={styles(isSearchable, withDropdownIndicator)}
+//         isSearchable={isSearchable}
+//         {...rest}
+//     />
+// );
+const Select = ({
+    isSearchable = true,
+    withDropdownIndicator = true,
+    className,
+    wrapperProps,
+    topLabel,
+    ...rest
+}: Props) => {
+    return (
+        <Wrapper className={className} {...wrapperProps}>
+            {topLabel && <TopLabel>{topLabel}</TopLabel>}
+            <ReactSelect
+                styles={styles(isSearchable, withDropdownIndicator)}
+                isSearchable={isSearchable}
+                {...rest}
+                ƒ
+            />
+        </Wrapper>
+    );
+};
 
 Select.propTypes = {
     isAsync: PropTypes.bool,
@@ -27,7 +62,8 @@ Select.propTypes = {
     options: PropTypes.array,
     // TODO
     // eslint-disable-next-line react/forbid-prop-types
-    value: PropTypes.object,
+    topLabel: PropTypes.node,
+    value: PropTypes.string,
     onChange: PropTypes.func,
 };
 
