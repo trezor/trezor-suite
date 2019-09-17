db7e14668a676a8998b982e54660fca7d1d070d6
martin <30367552+mroz22@users.noreply.github.com>
2019-09-17 09:15:54 +0200

    use url constants in pin modal component (#475)
    * use url constants in pin modal component
    
    * point imports to @suite-constats
    
    * lint fix

---
+++ packages/suite/src/constants/onboarding/index.ts
@@ -0,0 +1,7 @@
+import BIP_39 from './bip39';
+import * as CONSTANTS from './constants';
+import * as STEPS from './steps';
+import * as TREZOR from './trezor';
+import * as URLS from './urls';
+
+export { BIP_39, CONSTANTS, STEPS, TREZOR, URLS };
