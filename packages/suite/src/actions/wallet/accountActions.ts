0dcb79ebad9ca127a19ae12c4298c7e5d87f2596
slowbackspace <slowbackspace@gmail.com>
2019-09-17 09:53:08 +0200

    fix type

--- packages/suite/src/actions/wallet/accountActions.ts
+++ packages/suite/src/actions/wallet/accountActions.ts
@@ -1,9 +1,9 @@
 import { Dispatch, GetState } from '@suite-types/index';
-import { AccountInfo } from 'trezor-connect';
 import { ACCOUNT } from './constants';
+import { Account } from '@wallet-types';
 
 export type AccountActions =
-    | { type: typeof ACCOUNT.CREATE; payload: AccountInfo } // TODO types
+    | { type: typeof ACCOUNT.CREATE; payload: Account } // TODO types
     | { type: typeof ACCOUNT.UPDATE; payload: any }; // TODO types
 
 export const create = () => async (_dispatch: Dispatch, _getState: GetState): Promise<void> => {};
