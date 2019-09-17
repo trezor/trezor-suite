314f2915d2e29944c0cfe2111bd703a83e549701
Szymon Lesisz <sz.lesisz@gmail.com>
2019-09-17 15:04:54 +0200

    toggle legacy accounts (#520)

--- packages/suite/src/components/wallet/Menu/index.tsx
+++ packages/suite/src/components/wallet/Menu/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { FormattedMessage } from 'react-intl';
 import styled from 'styled-components';
@@ -6,8 +6,10 @@ import { colors, variables, Loader } from '@trezor/components';
 import * as discoveryActions from '@wallet-actions/discoveryActions';
 import { sortByCoin } from '@wallet-utils/accountUtils';
 import { AppState, Dispatch } from '@suite-types';
+import { Account } from '@wallet-types';
 import Row from './components/Row';
 import AddAccountButton from './components/AddAccount';
+import ToggleLegacyAccounts from './components/ToggleLegacyAccounts';
 import l10nMessages from './index.messages';
 
 const Wrapper = styled.div``;
@@ -29,6 +31,7 @@ const LoadingText = styled.div`
 const mapStateToProps = (state: AppState) => ({
     device: state.suite.device,
     accounts: state.wallet.accounts,
+    selectedAccount: state.wallet.selectedAccount,
     hideBalance: state.wallet.settings.hideBalance,
     discovery: state.wallet.discovery,
     router: state.router,
@@ -39,6 +42,9 @@ const mapDispatchToProps = (dispatch: Dispatch) => ({
 });
 
 type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
+interface State {
+    legacyVisible: boolean;
+}
 
 const DiscoveryStatus = () => (
     <Wrapper>
@@ -51,20 +57,36 @@ const DiscoveryStatus = () => (
     </Wrapper>
 );
 
-const Menu = ({ device, accounts, hideBalance, getDiscoveryForDevice }: Props) => {
+const Menu = ({ device, accounts, hideBalance, getDiscoveryForDevice, selectedAccount }: Props) => {
+    const [legacyVisibleState, setLegacyVisibleState] = useState<State['legacyVisible']>(false);
     const discovery = getDiscoveryForDevice();
     if (!device || !discovery) {
         return <DiscoveryStatus />;
     }
     const discoveryIsRunning = discovery.status <= 2;
 
-    const list = sortByCoin(
-        accounts.filter(
-            a => a.deviceState === device.state && (!a.empty || (a.empty && a.visible)),
-        ),
+    const list = sortByCoin(accounts.filter(a => a.deviceState === device.state));
+    // always show first "normal" account even if they are empty
+    const normalAccounts = list.filter(
+        a => a.accountType === 'normal' && (a.index === 0 || !a.empty || (a.empty && a.visible)),
     );
-    const normalAccounts = list.filter(a => a.accountType === 'normal');
-    const legacyAccounts = list.filter(a => a.accountType !== 'normal');
+    const legacyAccounts = list.filter(
+        a => a.accountType !== 'normal' && (!a.empty || (a.empty && a.visible)),
+    );
+
+    const isSelected = (account: Account) => {
+        if (selectedAccount.account && selectedAccount.account === account) {
+            return true;
+        }
+        return false;
+    };
+
+    let legacyVisible = discoveryIsRunning || legacyVisibleState;
+    if (!legacyVisible) {
+        const legacyAccountIsSelected = legacyAccounts.find(a => isSelected(a));
+        legacyVisible = !!legacyAccountIsSelected;
+    }
+
     return (
         <Wrapper>
             {discoveryIsRunning && list.length === 0 && <DiscoveryStatus />}
@@ -72,20 +94,7 @@ const Menu = ({ device, accounts, hideBalance, getDiscoveryForDevice }: Props) =
                 <Row
                     account={account}
                     hideBalance={hideBalance}
-                    key={`${account.descriptor}-${account.symbol}`}
-                />
-            ))}
-            {legacyAccounts.length > 0 && (
-                <LoadingWrapper>
-                    <LoadingText>
-                        <FormattedMessage {...l10nMessages.TR_LEGACY_ACCOUNTS} />
-                    </LoadingText>
-                </LoadingWrapper>
-            )}
-            {legacyAccounts.map(account => (
-                <Row
-                    account={account}
-                    hideBalance={hideBalance}
+                    selected={isSelected(account)}
                     key={`${account.descriptor}-${account.symbol}`}
                 />
             ))}
@@ -95,6 +104,21 @@ const Menu = ({ device, accounts, hideBalance, getDiscoveryForDevice }: Props) =
                     tooltipContent={<FormattedMessage {...l10nMessages.TR_ADD_ACCOUNT} />}
                 />
             )}
+            {legacyAccounts.length > 0 && (
+                <ToggleLegacyAccounts
+                    onToggle={() => setLegacyVisibleState(!legacyVisible)}
+                    isOpen={legacyVisible}
+                />
+            )}
+            {legacyVisible &&
+                legacyAccounts.map(account => (
+                    <Row
+                        account={account}
+                        hideBalance={hideBalance}
+                        selected={isSelected(account)}
+                        key={`${account.descriptor}-${account.symbol}`}
+                    />
+                ))}
         </Wrapper>
     );
 };
