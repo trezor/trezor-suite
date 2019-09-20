69b9795142863ed794f8686773005e4ae0e604d1
martin <mvarmuza@gmail.com>
2019-09-20 08:53:42 +0200

    text all in one file

--- packages/suite-web/test/integration/static-pages.test.ts
+++ packages/suite-web/test/integration/static-pages.test.ts
@@ -19,7 +19,7 @@ const fixtures = [
 
 describe('Static pages', () => {
     beforeEach(() => {
-        cy.viewport(1024, 768);
+        cy.viewport(1024, 768).resetDb();
     });
 
     fixtures.forEach(f => {
@@ -27,6 +27,23 @@ describe('Static pages', () => {
             cy.visit(f.visit)
                 .get('html')
                 .should(f.should[0], f.should[1]);
+        });
+    });
+
+    it(`on first page load, user should be redirected to onboarding page, then he clicks 'use wallet now' and connects device`, () => {
+        cy.dispatchDeviceConnect().then(dispatch => {
+            cy.visit('/')
+                .get('html')
+                .should('contain', 'Welcome to Trezor')
+                .window()
+                .its('store')
+                .invoke('dispatch', dispatch)
+                .getTestElement('button-use-wallet')
+                .click()
+                // todo: add snapshots in distance future when everything is stable
+                // .matchImageSnapshot()
+                .get('html')
+                .should('contain', 'Please select your coin');
         });
     });
 });
