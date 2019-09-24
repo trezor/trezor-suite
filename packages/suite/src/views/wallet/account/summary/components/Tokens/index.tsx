5b0e06fa1fccdce99e0dbac5b37e325e27e2a6fa
slowbackspace <slowbackspace@gmail.com>
2019-09-24 12:21:08 +0200

    remove borders from token item

--- packages/suite/src/views/wallet/account/summary/components/Tokens/index.tsx
+++ packages/suite/src/views/wallet/account/summary/components/Tokens/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { H5 } from '@trezor/components';
+import { H5, colors } from '@trezor/components';
 import { FormattedMessage } from 'react-intl';
 import l10nMessages from './index.messages';
 import TokenItem from './components/TokenItem';
@@ -9,6 +9,12 @@ import { Account } from '@wallet-types';
 const TokensHeadingWrapper = styled.div`
     display: flex;
     padding: 20px 0;
+`;
+
+const NoTokens = styled.div`
+    display: flex;
+    justify-content: center;
+    color: ${colors.TEXT_SECONDARY};
 `;
 
 const AddedTokensWrapper = styled.div``;
@@ -27,6 +33,7 @@ const Tokens = ({ tokens, hideBalance }: Props) => {
                 </H5>
             </TokensHeadingWrapper>
             <AddedTokensWrapper>
+                {tokens.length === 0 && <NoTokens>No tokens</NoTokens>}
                 {tokens.map(token => (
                     <TokenItem key={token.symbol} token={token} hideBalance={hideBalance} />
                 ))}
