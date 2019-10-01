5cb8097ee5a7f33c95c5a375328b4e958494ab3b
martin <mvarmuza@gmail.com>
2019-10-01 18:38:04 +0200

    add longer 'happy path' test suite

--- packages/suite-web/test/integration/connect-device.test.ts
+++ packages/suite-web/test/integration/connect-device.test.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { DEVICE } from 'trezor-connect';
+import { getConnectDevice, getDeviceFeatures } from '../../../suite/src/support/tests/setupJest';
+
 describe('Connect device', () => {
     beforeEach(() => {
         cy.viewport(1024, 768).resetDb();
@@ -18,5 +22,63 @@ describe('Connect device', () => {
                 .get('html')
                 .should('contain', 'Please select your coin');
         });
+    });
+
+    it(`onboarding shit`, () => {
+        cy.visit('/')
+            .get('html')
+            .should('contain', 'Welcome to Trezor')
+            .getTestElement('button-path-create')
+            .click()
+            // todo: add snapshots in distance future when everything is stable
+            // .matchImageSnapshot()
+            .get('html')
+            .should('contain', 'New device')
+            .getTestElement('button-new-path')
+            .click()
+            .get('html')
+            .should('contain', 'Select your device')
+            .getTestElement('option-model-t-path')
+            .click()
+            .get('html')
+            .should('contain', 'Hologram check')
+            .getTestElement('button-continue')
+            .click()
+            .get('html')
+            .should('contain', 'Pair device')
+            .window()
+            .its('store')
+            .invoke('dispatch', {
+                type: DEVICE.CONNECT,
+                payload: getConnectDevice({
+                    mode: 'initialize',
+                    firmware: 'none',
+                    features: getDeviceFeatures({
+                        firmware_present: false,
+                        bootloader_mode: true,
+                        initialized: false,
+                    }),
+                }),
+            })
+            .getTestElement('button-continue')
+            .click()
+            .get('html')
+            .should('contain', 'Pair device')
+            .window()
+            .its('store')
+            .invoke('dispatch', {
+                type: DEVICE.CHANGED,
+                payload: getConnectDevice({
+                    mode: 'initialize',
+                    firmware: 'valid',
+                    features: getDeviceFeatures({
+                        firmware_present: true,
+                        bootloader_mode: false,
+                        initialized: false,
+                    }),
+                }),
+            })
+            .getTestElement('button-continue')
+            .click()
     });
 });
