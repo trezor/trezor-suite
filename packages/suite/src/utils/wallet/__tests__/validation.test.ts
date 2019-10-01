99dd067d40f2edd2741f8e3e052ba03df145e6a1
slowbackspace <slowbackspace@gmail.com>
2019-10-01 19:04:51 +0200

    add more test

--- packages/suite/src/utils/wallet/__tests__/validation.test.ts
+++ packages/suite/src/utils/wallet/__tests__/validation.test.ts
@@ -7,11 +7,19 @@ describe('validation', () => {
         expect(isAddressValid('1F1tAaz5x1HUXrCNLbsMDqcw6o5GNn4xqX0', 'btc')).toEqual(false);
         expect(isAddressValid('mtXWDB6k5yC5v7TcwKZHB89SUp85yCKshy', 'test')).toEqual(true);
         expect(isAddressValid('mtXWDB6k5yC5v7TcwKZHB89SUp85yCKshy0', 'test')).toEqual(false);
+        // trop
+        expect(isAddressValid('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', 'trop')).toEqual(true);
+        expect(isAddressValid('0x32Be343B94f860124dC4fEe278FDCBD38C102D880', 'trop')).toEqual(
+            false,
+        );
         // eth
         expect(isAddressValid('0x32Be343B94f860124dC4fEe278FDCBD38C102D88', 'eth')).toEqual(true);
         expect(isAddressValid('0x32Be343B94f860124dC4fEe278FDCBD38C102D880', 'eth')).toEqual(false);
         // xrp
         expect(isAddressValid('r3kmLJN5D28dHuH8vZNUZpMC43pEHpaocV', 'xrp')).toEqual(true);
         expect(isAddressValid('r3kmLJN5D28dHuH8vZNUZpMC43pEHpaocV0', 'xrp')).toEqual(false);
+        // txrp
+        expect(isAddressValid('r3kmLJN5D28dHuH8vZNUZpMC43pEHpaocV', 'txrp')).toEqual(true);
+        expect(isAddressValid('r3kmLJN5D28dHuH8vZNUZpMC43pEHpaocV0', 'txrp')).toEqual(false);
     });
 });
