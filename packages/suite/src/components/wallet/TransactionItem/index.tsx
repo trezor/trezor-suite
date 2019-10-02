efa4de396f5b9874a09d5105e942082fd0575803
slowbackspace <slowbackspace@gmail.com>
2019-10-02 12:24:58 +0200

    remove space

--- packages/suite/src/components/wallet/TransactionItem/index.tsx
+++ packages/suite/src/components/wallet/TransactionItem/index.tsx
@@ -170,7 +170,7 @@ const TransactionItem = React.memo(
                         {!blockHeight && <Target>(pending)</Target>}
                         {targets &&
                             targets.map((target, i) => (
-                                // It   is ok to ignore eslint. the list is never reordered/filtered, items have no ids, list/items do not change
+                                // It is ok to ignore eslint. the list is never reordered/filtered, items have no ids, list/items do not change
                                 // eslint-disable-next-line react/no-array-index-key
                                 <Target key={i}>
                                     {target.addresses &&
