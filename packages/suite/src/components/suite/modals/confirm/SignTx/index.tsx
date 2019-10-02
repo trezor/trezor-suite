5b727c846e6c144cd9e8fcf9901fec5e096955d2
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-10-02 18:15:03 +0200

    Styled confirm modal (#614)
    * styled a bit
    
    * tweak more styles

--- packages/suite/src/components/suite/modals/confirm/SignTx/index.tsx
+++ packages/suite/src/components/suite/modals/confirm/SignTx/index.tsx
@@ -12,7 +12,7 @@ import l10nMessages from './messages';
 const { LINE_HEIGHT, FONT_SIZE, FONT_WEIGHT } = variables;
 
 const Wrapper = styled.div`
-    max-width: 450px;
+    max-width: 480px;
 `;
 
 const Header = styled.div`
@@ -22,13 +22,13 @@ const Header = styled.div`
 const Content = styled.div`
     border-top: 1px solid ${colors.DIVIDER};
     background: ${colors.MAIN};
-    padding: 30px 48px;
     border-radius: 4px;
+    padding: 15px 0;
 `;
 
 const StyledP = styled(P)`
-    padding-bottom: 20px;
-    text-align: center;
+    display: flex;
+    justify-content: left;
     color: ${colors.TEXT};
     font-size: ${FONT_SIZE.BASE};
     &:last-child {
@@ -42,17 +42,42 @@ const Address = styled(StyledP)`
 `;
 
 const Label = styled.div`
-    padding-bottom: 6px;
+    padding: 0 3px 6px 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     font-weight: ${FONT_WEIGHT.MEDIUM};
-    font-size: ${FONT_SIZE.SMALL};
+    font-size: ${FONT_SIZE.BIG};
     color: ${colors.TEXT_SECONDARY};
 `;
 
-const FeeLevelName = styled(StyledP)`
-    padding-bottom: 0px;
+const Symbol = styled.div`
+    text-transform: uppercase;
+    font-size: ${FONT_SIZE.BIG};
 `;
 
-const OutputWrapper = styled.div``;
+const FeeLevelName = styled.div`
+    padding: 20px 0;
+`;
+
+const Value = styled.div`
+    font-size: ${FONT_SIZE.BIG};
+    font-weight: ${FONT_WEIGHT.MEDIUM};
+    padding: 0 5px 0 0;
+`;
+
+const OutputWrapper = styled.div`
+    display: flex;
+    padding: 15px 48px;
+    flex-direction: column;
+    border-bottom: 1px solid ${colors.DIVIDER};
+`;
+
+const Row = styled.div`
+    display: flex;
+    justify-content: ${(props: { centered?: boolean }) =>
+        props.centered ? 'center' : 'flex-start'};
+`;
 
 interface Props {
     device: TrezorDevice;
@@ -63,7 +88,6 @@ interface Props {
 const ConfirmSignTx = ({ device, sendForm, account }: Props) => {
     if (!account || !sendForm || !sendForm.networkTypeBitcoin.transactionInfo) return null;
     const { outputs } = sendForm;
-    const selectedFeeLevel = sendForm.selectedFee;
     const majorVersion = device.features ? device.features.major_version : 2;
 
     return (
@@ -75,7 +99,7 @@ const ConfirmSignTx = ({ device, sendForm, account }: Props) => {
                         values={{ deviceLabel: device.label }}
                     />
                 </Prompt>
-                <P textAlign="center" size="small">
+                <P>
                     <FormattedMessage {...l10nMessages.TR_DETAILS_ARE_SHOWN_ON} />
                 </P>
             </Header>
@@ -85,25 +109,33 @@ const ConfirmSignTx = ({ device, sendForm, account }: Props) => {
                 </Label>
                 {outputs.map(output => (
                     <OutputWrapper key={output.id}>
-                        <StyledP>{`${output.amount.value || '0'} ${account.symbol}`}</StyledP>
-                        <Label>
-                            <FormattedMessage {...l10nMessages.TR_TO_LABEL} />
-                        </Label>
-                        <Address>{output.address.value}</Address>
-                        <Label>
-                            <FormattedMessage {...l10nMessages.TR_FEE_LABEL} />
-                        </Label>
-                        {/* TODO add more coins */}
-                        <FeeLevelName>
-                            {formatNetworkAmount(
-                                // @ts-ignore
-                                sendForm.networkTypeBitcoin.transactionInfo.fee,
-                                account.symbol,
-                            )}
-                        </FeeLevelName>
-                        <StyledP>{selectedFeeLevel.label}</StyledP>
+                        <Row>
+                            <Value>{`${output.amount.value || '0'} `}</Value>
+                            <Symbol>{account.symbol}</Symbol>
+                        </Row>
+                        <Row>
+                            <Label>
+                                <FormattedMessage {...l10nMessages.TR_TO_LABEL} />
+                            </Label>
+                            <Address>{output.address.value}</Address>
+                        </Row>
                     </OutputWrapper>
                 ))}
+                <FeeLevelName>
+                    <Label>
+                        <FormattedMessage {...l10nMessages.TR_FEE_LABEL} />
+                    </Label>
+                    <Row centered>
+                        <Value>
+                            {sendForm.networkTypeBitcoin.transactionInfo.type !== 'error' &&
+                                formatNetworkAmount(
+                                    sendForm.networkTypeBitcoin.transactionInfo.fee,
+                                    account.symbol,
+                                )}
+                        </Value>
+                        <Symbol>{account.symbol}</Symbol>
+                    </Row>
+                </FeeLevelName>
             </Content>
         </Wrapper>
     );
