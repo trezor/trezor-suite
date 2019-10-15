79e820a3ab5578e0293b42b0848a9d79af16832d
slowbackspace <slowbackspace@gmail.com>
2019-10-15 16:36:16 +0200

    reuse clear action

--- packages/suite/src/actions/wallet/sendFormSpecific/bitcoinActions.ts
+++ packages/suite/src/actions/wallet/sendFormSpecific/bitcoinActions.ts
@@ -8,6 +8,7 @@ import * as accountActions from '@wallet-actions/accountActions';
 import { Dispatch, GetState } from '@suite-types';
 import { Account } from '@wallet-types';
 import * as sendFormCacheActions from '../sendFormCacheActions';
+import * as sendFormActions from '../sendFormActions';
 
 export type SendFormBitcoinActions =
     | { type: typeof SEND.BTC_ADD_RECIPIENT; newOutput: Output }
@@ -86,7 +87,7 @@ export const send = () => async (dispatch: Dispatch, getState: GetState) => {
     });
 
     if (resp.success) {
-        dispatch({ type: SEND.CLEAR });
+        dispatch(sendFormActions.clear());
         dispatch(
             notificationActions.add({
                 variant: 'success',
