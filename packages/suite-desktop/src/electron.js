65c76f85a8170b134a7e1873b0614ac7c71b9f45
slowbackspace <slowbackspace@gmail.com>
2019-10-16 17:08:11 +0200

    use keyup listener instead of globalShortcut

--- packages/suite-desktop/src/electron.js
+++ packages/suite-desktop/src/electron.js
@@ -1,13 +1,5 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const {
-    app,
-    session,
-    BrowserWindow,
-    ipcMain,
-    globalShortcut,
-    protocol,
-    shell,
-} = require('electron');
+const { app, session, BrowserWindow, ipcMain, protocol, shell } = require('electron');
 const isDev = require('electron-is-dev');
 const prepareNext = require('electron-next');
 const path = require('path');
@@ -80,12 +72,17 @@ const init = async () => {
             callback({ path: url });
         });
 
-        globalShortcut.register('f5', () => {
-            mainWindow.loadURL(src);
-        });
-        globalShortcut.register('CommandOrControl+R', () => {
-            mainWindow.loadURL(src);
-        });
+        // TODO: maybe we should just use mousetrap https://electronjs.org/docs/tutorial/keyboard-shortcuts#shortcuts-within-a-browserwindow
+        window.addEventListener(
+            'keyup',
+            e => {
+                if (e.keyCode === 82 || e.keyCode === 116) {
+                    // cmd + r or f5
+                    mainWindow.loadURL(src);
+                }
+            },
+            true,
+        );
     }
 
     mainWindow.loadURL(src);
