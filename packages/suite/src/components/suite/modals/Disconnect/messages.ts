140da49137cc21832dc334dad1162ad76cb70691
martin <mvarmuza@gmail.com>
2019-10-17 14:09:10 +0200

    show disconnect device modal after wipe

---
+++ packages/suite/src/components/suite/modals/Disconnect/messages.ts
@@ -0,0 +1,15 @@
+import { defineMessages } from 'react-intl';
+
+const definedMessages = defineMessages({
+    TR_DISCONNECT_DEVICE_HEADER: {
+        id: 'TR_DISCONNECT_DEVICE_HEADER',
+        defaultMessage: 'Disconnect {label} device',
+    },
+    TR_DISCONNECT_DEVICE_TEXT: {
+        id: 'TR_DISCONNECT_DEVICE_TEXT',
+        defaultMessage:
+            'Your device is wiped and might be safely sold on ebay. No one will ever be able to steal your funds.',
+    },
+});
+
+export default definedMessages;
