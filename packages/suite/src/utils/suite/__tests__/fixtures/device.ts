89200065a5db83d2b1686dc460b0eddf16cba69e
slowbackspace <slowbackspace@gmail.com>
2019-11-06 14:20:51 +0100

    add device test

--- packages/suite/src/utils/suite/__tests__/fixtures/device.ts
+++ packages/suite/src/utils/suite/__tests__/fixtures/device.ts
@@ -332,6 +332,19 @@ const sortByTimestamp = {
     result: [{ id: 5, ts: 3 }, { id: 4, ts: 2 }, { id: 1, ts: 1 }, { id: 2 }, { id: 3 }],
 };
 
+const isDeviceRemembered = [
+    {
+        description: 'acquired non remembered device',
+        device: getSuiteDevice({ type: 'acquired', remember: true }),
+        result: true,
+    },
+    {
+        description: 'acquired remembered device',
+        device: getSuiteDevice({ type: 'acquired', remember: false }),
+        result: false,
+    },
+];
+
 const d = (obj: any) => ({
     path: obj.path ? obj.path : obj.id,
     features: obj.id
@@ -464,4 +477,5 @@ export default {
     sortByTimestamp,
     getOtherDevices,
     getDeviceInstances,
+    isDeviceRemembered,
 };
