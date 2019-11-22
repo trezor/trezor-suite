fd7f5892890685352a6a86ae3d5ed36efe0ac251
Jan Czibula <jan.czibula@gmail.com>
2019-11-22 11:31:02 +0100

    Components v2 icons (#800)
    * add icons
    
    * add Icon component, fix icon svg files
    
    * add icons to storybook
    
    * add tests, add arrow_right and arrow_left, fix center icons
    
    * fix svg's
    
    * add snapshots
    
    * add icon story
    
    * remove android project files

--- packages/components-v2/src/index.ts
+++ packages/components-v2/src/index.ts
@@ -3,5 +3,7 @@ import * as variables from './config/variables';
 
 import { Button } from './components/buttons/Button';
 import { Input } from './components/inputs/Input';
+import { Icon } from './components/Icon';
+import * as types from './support/types';
 
-export { colors, variables, Button, Input };
+export { colors, variables, Button, Input, Icon, types };
