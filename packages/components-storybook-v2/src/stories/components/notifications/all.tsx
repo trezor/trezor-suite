340e22e4156ceb56674fa54d91796d6a276ef34b
Jan Czibula <jan.czibula@gmail.com>
2019-11-22 11:00:29 +0100

    add tests

--- packages/components-storybook-v2/src/stories/components/notifications/all.tsx
+++ packages/components-storybook-v2/src/stories/components/notifications/all.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Notification, colors } from '@trezor/components-v2';
+import { Notification, H1, colors } from '@trezor/components-v2';
 import { storiesOf } from '@storybook/react';
 
 const Wrapper = styled.div`
@@ -15,68 +15,102 @@ const NotificationWrapper = styled.div`
 storiesOf('Notifications', module).add(
     'All',
     () => {
+        const states = ['success', 'info', 'warning', 'error'];
+        type StateType = 'success' | 'info' | 'warning' | 'error';
+
         return (
             <Wrapper>
-                <NotificationWrapper>
-                    <Notification
-                        title="The main message"
-                        description="Something else that needs to be told. This short message is optional and also not needed."
-                    />
-                </NotificationWrapper>
-                <NotificationWrapper>
-                    <Notification
-                        title="The main message"
-                        description="Something else that needs to be told. This short message is optional and also not needed."
-                        state="warning"
-                    />
-                </NotificationWrapper>
-                <NotificationWrapper>
-                    <Notification
-                        title="The main message"
-                        description="Something else that needs to be told. This short message is optional and also not needed."
-                        mainCta={{
-                            callback: () => {},
-                            label: 'CTA',
-                        }}
-                    />
-                </NotificationWrapper>
-                <NotificationWrapper>
-                    <Notification
-                        title="The main message"
-                        description="Something else that needs to be told. This short message is optional and also not needed."
-                        mainCta={{
-                            callback: () => {},
-                            label: 'Main CTA',
-                        }}
-                        secondCta={{
-                            callback: () => {},
-                            label: 'Second CTA',
-                        }}
-                    />
-                </NotificationWrapper>
-                <NotificationWrapper>
-                    <Notification
-                        title="The main message"
-                        description="Something else that needs to be told. This short message is optional and also not needed."
-                        isLoading
-                    />
-                </NotificationWrapper>
-                <NotificationWrapper>
-                    <Notification title="The main message" isLoading />
-                </NotificationWrapper>
-                <NotificationWrapper>
-                    <Notification
-                        title="The main message"
-                        mainCta={{
-                            callback: () => {},
-                            label: 'Main CTA',
-                        }}
-                        secondCta={{
-                            callback: () => {},
-                            label: 'Second CTA',
-                        }}
-                    />
-                </NotificationWrapper>
+                {states.map(state => (
+                    <>
+                        <H1>{state}</H1>
+                        <NotificationWrapper>
+                            <Notification
+                                title="The main message"
+                                state={state as StateType}
+                                dataTest={`notification-${state}`}
+                            />
+                        </NotificationWrapper>
+                        <NotificationWrapper>
+                            <Notification
+                                title="The main message"
+                                mainCta={{
+                                    callback: () => {},
+                                    label: 'CTA',
+                                }}
+                                state={state as StateType}
+                                dataTest={`notification-cta-${state}`}
+                            />
+                        </NotificationWrapper>
+                        <NotificationWrapper>
+                            <Notification
+                                title="The main message"
+                                mainCta={{
+                                    callback: () => {},
+                                    label: 'Main CTA',
+                                }}
+                                secondCta={{
+                                    callback: () => {},
+                                    label: 'Second CTA',
+                                }}
+                                state={state as StateType}
+                                dataTest={`notification-second-cta-${state}`}
+                            />
+                        </NotificationWrapper>
+                        <NotificationWrapper>
+                            <Notification
+                                title="The main message"
+                                isLoading
+                                state={state as StateType}
+                                dataTest={`notification-loading-${state}`}
+                            />
+                        </NotificationWrapper>
+                        <NotificationWrapper>
+                            <Notification
+                                title="The main message"
+                                description="Something else that needs to be told. This short message is optional and also not needed."
+                                state={state as StateType}
+                                dataTest={`notification-desc-${state}`}
+                            />
+                        </NotificationWrapper>
+                        <NotificationWrapper>
+                            <Notification
+                                title="The main message"
+                                description="Something else that needs to be told. This short message is optional and also not needed."
+                                mainCta={{
+                                    callback: () => {},
+                                    label: 'CTA',
+                                }}
+                                state={state as StateType}
+                                dataTest={`notification-desc-cta-${state}`}
+                            />
+                        </NotificationWrapper>
+                        <NotificationWrapper>
+                            <Notification
+                                title="The main message"
+                                description="Something else that needs to be told. This short message is optional and also not needed."
+                                mainCta={{
+                                    callback: () => {},
+                                    label: 'Main CTA',
+                                }}
+                                secondCta={{
+                                    callback: () => {},
+                                    label: 'Second CTA',
+                                }}
+                                state={state as StateType}
+                                dataTest={`notification-desc-second-cta-${state}`}
+                            />
+                        </NotificationWrapper>
+                        <NotificationWrapper>
+                            <Notification
+                                title="The main message"
+                                description="Something else that needs to be told. This short message is optional and also not needed."
+                                isLoading
+                                state={state as StateType}
+                                dataTest={`notification-desc-loading-${state}`}
+                            />
+                        </NotificationWrapper>
+                    </>
+                ))}
             </Wrapper>
         );
     },
