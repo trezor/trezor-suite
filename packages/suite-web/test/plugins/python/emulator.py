6d3b8f9601580bed41116216d30203bb4ccc2d3d
martin <mvarmuza@gmail.com>
2019-11-27 19:01:24 +0100

    add python scripts

---
+++ packages/suite-web/test/plugins/python/emulator.py
@@ -0,0 +1,89 @@
+import sys
+import os
+import signal
+import time
+from subprocess import Popen
+
+from trezorlib import device, debuglink, log
+from trezorlib.debuglink import DebugLink, TrezorClientDebugLink
+from trezorlib.client import TrezorClient
+from trezorlib.transport import enumerate_devices, get_transport
+from trezorlib.transport.udp import UdpTransport
+from trezorlib.transport.bridge import BridgeTransport
+
+proc = None
+# log.enable_debug_output()
+
+def start():
+    global proc
+    if proc is None:
+        # TODO:
+        # - check if emulator process is already running and kill it if so
+        # - detect if Popen process starts without error (if udp port is listening)
+        # - run custom firmware
+        # - run T1 emulator
+        # - run T1 & T2 emulator at once
+        # - run two T2/T1 emulators
+        proc = Popen(
+            # ["./emulator", "-O0", "-X", "heapsize=20M", "-m", "main"],
+            # "./packages/suite-web/test/trezor-emu-core-v2.0.10 -O0 -X heapsize=20M -m main",
+            "./test/trezor-emu-core-v2.0.10 -O0 -X heapsize=20M -m main",
+            # "TREZOR_OLED_SCALE=2 ./packages/suite-web/test/trezor-emu-legacy-v1.6.2 -O0",
+            
+            shell=True,
+            preexec_fn=os.setsid
+        )
+        print("the commandline is {}".format(proc.args))
+        
+        time.sleep(3) # replace this with "wait_for_emulator" process
+
+def stop():
+    global proc
+    if proc is not None:
+        os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
+        proc = None
+
+def get_udp_device():
+    devices = UdpTransport.enumerate()
+    for d in devices:
+        # debugBridge = d.find_debug()
+        return d
+    raise RuntimeError("No debuggable udp device found")
+
+def get_bridge_device():
+    devices = BridgeTransport.enumerate()
+    print(devices);
+    for d in devices:
+        debugBridge = d.find_debug()
+        
+        return d
+    raise RuntimeError("No debuggable bridge device found")
+
+def setup_device(mnemonic, pin, passphrase_protection, label):
+    # Setup link
+    # transport = get_udp_device()
+    # TODO:
+    # - "path" parameter to work with correct device
+    # - check if device is acquired otherwise throws "wrong previous session" from bridge
+    transport = get_bridge_device()
+    print(transport)
+    client = TrezorClientDebugLink(transport)
+    client.open()
+    device.wipe(client)
+    debuglink.load_device_by_mnemonic(client, mnemonic=mnemonic, pin=pin, passphrase_protection=passphrase_protection, label=label)
+    client.close()
+    # time.sleep(1)
+
+def decision():
+    # TODO:
+    # - "path" parameter to work with correct device, keep transport with device
+    # Setup link
+    transport = get_bridge_device()
+    print(transport)
+    client = DebugLink(transport.find_debug())
+
+    client.open()
+    time.sleep(0.6) # trezord needs time to populate changes
+    # client.swipe_down()
+    client.press_yes()
+    client.close()
---
+++ packages/suite-web/test/plugins/python/main.py
@@ -0,0 +1,74 @@
+import sys
+import os
+import json
+import atexit
+from termcolor import colored
+
+from websocket_server import WebsocketServer
+import emulator
+import bridge
+
+def cleanup():
+    emulator.stop()
+    bridge.stop()
+
+atexit.register(cleanup)
+
+# Called for every client connecting (after handshake)
+def new_client(client, server):
+    welcome = json.dumps({"type": "client", "id": client['id']})
+    server.send_message_to_all(welcome)
+
+# Called for every client disconnecting
+def client_left(client, server):
+    print(colored("Client(%d) disconnected" % client['id'], "blue"))
+
+# Called when a client sends a message
+def message_received(client, server, message):
+    print("Client(%d) request: %s" % (client['id'], message))
+    try:
+        cmd = json.loads(message)
+    except:
+        server.send_message(client, json.dumps({"success": False, "error": "Invalid json message"}))
+        return
+
+    cmdId = cmd["id"]
+    cmdType = cmd["type"]
+    response = None
+    try:
+        if cmdType == "ping":
+            server.send_message(client, "pong")
+        elif cmdType == "emulator-start":
+            emulator.start()
+            response = { "success": True }
+        elif cmdType == "emulator-stop":
+            emulator.stop()
+            response = { "success": True }
+        elif cmdType == "emulator-setup":
+            emulator.setup_device(cmd["mnemonic"], cmd["pin"], cmd["passphrase_protection"], cmd["label"])
+            response = { "success": True }
+        elif cmdType == "emulator-decision":
+            emulator.decision()
+            response = { "success": True }
+        elif cmdType == "bridge-start":
+            bridge.start()
+            response = { "success": True }
+        elif cmdType == "bridge-stop":
+            bridge.stop()
+            response = { "success": True }
+        elif cmdType == "exit":
+            emulator.stop()
+            bridge.stop()
+            os._exit(1)
+        print("Client(%d) response: %s" % (client['id'], str(response)))
+        if response is not None:
+            server.send_message(client, json.dumps(dict(response, id=cmdId, success=True)))
+    except Exception as e:
+        print("Client(%d) response: %s" % (client['id'], str(e)))
+        server.send_message(client, json.dumps({"id": cmdId, "success": False, "error": str(e)}))
+
+server = WebsocketServer(9001)
+server.set_fn_new_client(new_client)
+server.set_fn_client_left(client_left)
+server.set_fn_message_received(message_received)
+server.run_forever()
