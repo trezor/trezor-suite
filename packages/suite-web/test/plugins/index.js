c5e95d51352886cb5c15e526d77379ffb14a86f9
martin <mvarmuza@gmail.com>
2019-11-27 19:18:39 +0100

    tests setup

--- packages/suite-web/test/plugins/index.js
+++ packages/suite-web/test/plugins/index.js
@@ -5,6 +5,22 @@
 
 const { addMatchImageSnapshotPlugin } = require('cypress-image-snapshot/plugin');
 const cypressTypeScriptPreprocessor = require('./ts-preprocessor');
+const path = require('path');
+const { spawn } = require('child_process');
+
+const { Controller } = require('./python/websocket-client');
+
+const controller = new Controller({ url: 'ws://localhost:9001/' });
+
+// let pythonProcess;
+// const spawnProcess = () => {
+//     const src = path.resolve(__dirname, './python/main.py');
+//     const child = spawn('python3', [src], {
+//         detached: true,
+//         stdio: ['ignore', 'ignore', 'ignore'],
+//     });
+//     return child;
+// };
 
 module.exports = on => {
     addMatchImageSnapshotPlugin(on);
@@ -15,5 +31,27 @@ module.exports = on => {
             return args;
         }
         return args;
+    });
+
+    on('task', {
+        // startPython: async () => {
+        //     pythonProcess = spawnProcess();
+        //     return new Promise((resolve, reject) => {
+        //         setTimeout(() => resolve(null), 3000);
+        //         pythonProcess.on('error', err => {
+        //             reject();
+        //         });
+        //     });
+        // },
+        startBridge: async () => {
+            await controller.connect();
+            const response = await controller.send({ type: 'bridge-start' });
+            return null;
+        },
+        startEmu: async () => {
+            await controller.connect();
+            const response = await controller.send({ type: 'emulator-start' });
+            return null;
+        },
     });
 };
