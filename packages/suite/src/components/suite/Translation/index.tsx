4fcfa6468e03ee9a1b2e5c37f71f0764ee8efc39
slowbackspace <slowbackspace@gmail.com>
2019-11-28 11:52:01 +0100

    fix ts

--- packages/suite/src/components/suite/Translation/index.tsx
+++ packages/suite/src/components/suite/Translation/index.tsx
@@ -3,7 +3,7 @@ import { FormattedMessage, MessageDescriptor } from 'react-intl';
 import { ExtendedMessageDescriptor } from '@suite-types';
 
 export const isChildrenMessageDescriptor = (
-    message: React.ReactNode | ExtendedMessageDescriptor,
+    message: null | React.ReactNode | ExtendedMessageDescriptor,
 ): message is ExtendedMessageDescriptor => {
     return (
         typeof message === 'object' &&
@@ -12,14 +12,27 @@ export const isChildrenMessageDescriptor = (
     );
 };
 
-interface Props extends Partial<ExtendedMessageDescriptor> {
-    children?: React.ReactNode | ExtendedMessageDescriptor;
+export const isMsgType = (props: MsgType | ChildrenType): props is MsgType => {
+    return (
+        typeof props === 'object' &&
+        props !== null &&
+        (props as MsgType).defaultMessage !== undefined
+    );
+};
+
+export const isChildrenType = (props: MsgType | ChildrenType): props is ChildrenType => {
+    return typeof props === 'object' && props !== null && props.children !== null;
+};
+
+interface ChildrenType {
+    children: React.ReactChild | ExtendedMessageDescriptor;
+}
+
+interface MsgType extends ExtendedMessageDescriptor {
+    children?: undefined;
 }
 
 type PrimitiveType = string | number | boolean | Date | null | undefined;
-
-// TODO: Problems
-// 1. all props are optional so we are losing strict check if all necessary props are passed
 
 /**
  * Util component that helps with rendering react-intl messages.
@@ -29,32 +42,10 @@ type PrimitiveType = string | number | boolean | Date | null | undefined;
  * If children prop is an object, it assumes that it is ExtendedMessageDescriptor
  * and renders FormattedMessage with the message. Otherwise it just simply render passed children.
  */
-const Translation = ({ children, ...props }: Props) => {
+const Translation = (props: ChildrenType | MsgType) => {
     const values: Record<string, PrimitiveType | React.ReactNode> = {};
-
-    // passed children prop (component or ExtendedMessageDescriptor obj)
-    if (isChildrenMessageDescriptor(children)) {
-        if (children.values) {
-            // Message with variables passed via 'values' prop.
-            // Value entry can also contain a MessageDescriptor.
-            // Copy values and extract necessary messages to a new 'values' object
-            Object.keys(children.values).forEach(key => {
-                values[key] = <Translation>{children.values![key]}</Translation>;
-            });
-        }
-
-        // pass undefined to a 'values' prop in case of an empty values object
-        return (
-            <FormattedMessage
-                {...children}
-                values={Object.keys(values).length === 0 ? undefined : values}
-            />
-        );
-    }
-
     // message passed via props (id, defaultMessage, values)
-    // TODO: type guard?
-    if (props.id && props.defaultMessage) {
+    if (isMsgType(props)) {
         Object.keys(props.values || []).forEach(key => {
             values[key] = <Translation>{props.values![key]}</Translation>;
         });
@@ -65,7 +56,28 @@ const Translation = ({ children, ...props }: Props) => {
             />
         );
     }
-    return <>{children}</>;
+    // passed children prop (component or ExtendedMessageDescriptor obj)
+    if (isChildrenType(props) && isChildrenMessageDescriptor(props.children)) {
+        if (props.children.values) {
+            // Message with variables passed via 'values' prop.
+            // Value entry can also contain a MessageDescriptor.
+            // Copy values and extract necessary messages to a new 'values' object
+            Object.keys(props.children.values).forEach(key => {
+                // @ts-ignore
+                values[key] = <Translation>{props.children.values[key]}</Translation>;
+            });
+        }
+
+        // pass undefined to a 'values' prop in case of an empty values object
+        return (
+            <FormattedMessage
+                {...props.children}
+                values={Object.keys(values).length === 0 ? undefined : values}
+            />
+        );
+    }
+
+    return <>{props.children}</>;
 };
 
 export { Translation };
