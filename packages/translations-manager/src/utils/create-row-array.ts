09e14d44025f4102e54fb0025adae7cd42dc3fc2
slowbackspace <slowbackspace@gmail.com>
2019-12-02 20:52:09 +0100

    rewrite to TS

--- packages/translations-manager/src/utils/create-row-array.ts
+++ packages/translations-manager/src/utils/create-row-array.ts
@@ -1,4 +1,12 @@
-const createRowArray = (masterKey, masterValue, occurrences = false) => {
+export interface MessageObject {
+    source: string;
+    meta?: {
+        comment?: string;
+        occurrences?: string[];
+    };
+}
+
+const createRowArray = (masterKey: string, masterValue: MessageObject, occurrences = false) => {
     const context = [];
     if (masterValue.meta) {
         if (masterValue.meta.comment) {
@@ -12,11 +20,7 @@ const createRowArray = (masterKey, masterValue, occurrences = false) => {
         }
     }
 
-    return [
-        masterKey,
-        masterValue.source,
-        context.join(' '),
-    ];
+    return [masterKey, masterValue.source, context.join(' ')];
 };
 
 export default createRowArray;
