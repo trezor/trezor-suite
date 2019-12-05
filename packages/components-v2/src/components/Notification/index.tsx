35692631beaf854ecfc8d263870148dad025d709
Jan Czibula <jan.czibula@gmail.com>
2019-12-05 13:35:38 +0100

    fix ts

--- packages/components-v2/src/components/Notification/index.tsx
+++ packages/components-v2/src/components/Notification/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import styled, { css } from 'styled-components';
 import { FeedbackState } from '../../support/types';
 import colors from '../../config/colors';
-import { getStateColor } from '../../utils/colors';
+import { getFeedbackColor } from '../../utils/colors';
 
 const Wrapper = styled.div<WrapperProps>`
     display: flex;
@@ -20,7 +20,7 @@ const State = styled.div<StateProps>`
     display: flex;
     width: 4px;
     height: 100%;
-    background: ${props => getStateColor(props.state || 'success')};
+    background: ${props => getFeedbackColor(props.state || 'success')};
     border-radius: 4px;
 `;
 
