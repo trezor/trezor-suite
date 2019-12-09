86f769b217aee9284c4096b4282cc0fbfb3a2441
Tomas Zorvan <tomas.zorvan@gmail.com>
2019-12-09 13:36:46 +0100

    Add timezones support for tx history. Fix for #705

--- packages/suite/src/utils/suite/__tests__/date.test.ts
+++ packages/suite/src/utils/suite/__tests__/date.test.ts
@@ -11,4 +11,17 @@ describe('Date utils', () => {
         expect(utils.formatDuration(63671184000)).toBe('over 2017 years'); // jesus was born
         expect(utils.formatDuration(99999999999)).toBe('almost 3169 years');
     });
+
+    test('get date with timezone', () => {
+        const dateInMs = 1565797979000;
+        const dateInIso = '2014-06-25T10:00:00.000Z';
+
+        expect(utils.getDateWithTimeZone(dateInMs, 'Asia/Tokyo')).toEqual(
+            new Date('2019-08-14T22:52:59.000Z'),
+        );
+        expect(utils.getDateWithTimeZone(dateInMs)).toEqual(new Date('2019-08-14T15:52:59.000Z'));
+        expect(utils.getDateWithTimeZone(dateInIso, 'America/New_York')).toEqual(
+            new Date('2014-06-25T04:00:00.000Z'),
+        );
+    });
 });
