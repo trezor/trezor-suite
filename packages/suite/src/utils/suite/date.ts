b4ac4a62f787d2bcc1b0549920e5c33e197f2243
Tomas Zorvan <tomas.zorvan@gmail.com>
2019-12-10 17:11:39 +0100

    Fix/timezones tx history (#902)
    * Add timezones support for tx history. Fix for #705
    
    * Fix tests
    
    * Set UTC timezone for suite tests

--- packages/suite/src/utils/suite/date.ts
+++ packages/suite/src/utils/suite/date.ts
@@ -1,4 +1,12 @@
 import { formatDistance } from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 
 export const formatDuration = (seconds: number) =>
     formatDistance(0, seconds * 1000, { includeSeconds: true });
+
+export const getDateWithTimeZone = (date: number | string, timeZone?: string): Date => {
+    const isoDate = new Date(date).toISOString();
+    const tz = timeZone || Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+    return utcToZonedTime(isoDate, tz);
+};
