924acf858ffe84f1e946f6729651881f9dc09b51
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-12-10 15:27:00 +0100

    fix types

--- packages/suite/src/types/wallet/sendForm/index.ts
+++ packages/suite/src/types/wallet/sendForm/index.ts
@@ -1,17 +1,22 @@
-import { Actions } from './actions';
-import { Output as Output$ } from './output';
-import { PrecomposedTransactionXrp as PrecomposedTransactionXrp$ } from './transactions';
 import {
-    State as State$,
-    InitialState as InitialState$,
-    FeeLevel as FeeLevel$,
-    FeeInfo as FeeInfo$,
-} from './state';
+    SendFormActions,
+    SendFormBtcActions,
+    SendFormXrpActions,
+    SendFormEthActions,
+} from './actions';
 
-export type SendFormActions = Actions;
-export type Output = Output$;
-export type FeeInfo = FeeInfo$;
-export type InitialState = InitialState$;
-export type State = State$;
-export type FeeLevel = FeeLevel$;
-export type PrecomposedTransactionXrp = PrecomposedTransactionXrp$;
+import { Output } from './output';
+import { PrecomposedTransactionXrp, PrecomposedTransactionEth } from './transactions';
+import { State, InitialState, FeeLevel, FeeInfo } from './state';
+
+export type SendFormActions = SendFormActions;
+export type SendFormBtcActions = SendFormBtcActions;
+export type SendFormXrpActions = SendFormXrpActions;
+export type SendFormEthActions = SendFormEthActions;
+export type Output = Output;
+export type FeeInfo = FeeInfo;
+export type InitialState = InitialState;
+export type State = State;
+export type FeeLevel = FeeLevel;
+export type PrecomposedTransactionXrp = PrecomposedTransactionXrp;
+export type PrecomposedTransactionEth = PrecomposedTransactionEth;
