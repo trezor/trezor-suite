9ece3a1c79e456b962827763dc8bc6c97586f95e
Vladimir Volek <vladimir.volek@satoshilabs.com>
2019-12-11 13:48:16 +0100

    fix path

--- packages/suite/src/utils/suite/nextjs.ts
+++ packages/suite/src/utils/suite/nextjs.ts
@@ -1,7 +1,7 @@
 import { join } from 'path';
 
 export const resolveStaticPath = (path: string) => {
-    const staticPath = join('static', path);
+    const staticPath = join('/static', path);
     if (process.env.assetPrefix) {
         return join(process.env.assetPrefix, staticPath);
     }
