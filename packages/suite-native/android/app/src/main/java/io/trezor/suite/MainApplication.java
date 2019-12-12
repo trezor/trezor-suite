c8cf398c54b573d7b5fb19ea72d6f22691af6484
Szymon Lesisz <sz.lesisz@gmail.com>
2019-12-12 11:09:21 +0100

    clean MainApplication.java

--- packages/suite-native/android/app/src/main/java/io/trezor/suite/MainApplication.java
+++ packages/suite-native/android/app/src/main/java/io/trezor/suite/MainApplication.java
@@ -1,55 +1,45 @@
 package io.trezor.suite;
 
-import android.app.Application;
-import android.content.Context;
-import com.facebook.react.PackageList;
-import com.facebook.hermes.reactexecutor.HermesExecutorFactory;
-import com.facebook.react.bridge.JavaScriptExecutorFactory;
+import java.util.List;
 
+import android.app.Application;
+
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-// import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import java.lang.reflect.InvocationTargetException;
-
-// import java.util.Arrays;
-import java.util.List;
-
-// https://github.com/react-native-community/react-native-svg#android
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            List<ReactPackage> packages = new PackageList(this).getPackages();
+            // Packages that cannot be auto linked yet can be added manually here:
+            packages.add(new BridgePackage());
+            return packages;
+        }
+
+        @Override
+        protected String getJSMainModuleName() {
+            return "packages/suite-native/index";
+        }
+    };
+
     @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
     }
 
-
     @Override
-    protected List<ReactPackage> getPackages() {
-      @SuppressWarnings("UnnecessaryLocalVariable")
-      List<ReactPackage> packages = new PackageList(this).getPackages();
-      // Packages that cannot be auto linked yet can be added manually here:
-      packages.add(new BridgePackage());
-      return packages;
+    public void onCreate() {
+        super.onCreate();
+        SoLoader.init(this, /* native exopackage */ false);
     }
-
-    @Override
-    protected String getJSMainModuleName() {
-      return "packages/suite-native/index";
-    }
-  };
-
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
 }
