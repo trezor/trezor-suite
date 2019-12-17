b7d16c0ed84a027f88e514e223c6590af1b19596
martin <30367552+mroz22@users.noreply.github.com>
2019-12-17 19:35:30 +0100

    implement update based on timestamp (#946)
    * implement update based on timestamp
    
    * add persistence
    
    * review changes
    
    * bump version
    
    * fix max age
    
    * update yarn.lock

--- packages/suite/src/reducers/wallet/fiatRateReducer.ts
+++ packages/suite/src/reducers/wallet/fiatRateReducer.ts
@@ -1,35 +1,42 @@
-import { RATE_UPDATE, FiatRateActions } from '@wallet-middlewares/coingeckoMiddleware';
 import produce from 'immer';
+import { Action } from '@suite-types';
+import { RATE_UPDATE } from '@wallet-actions/constants/fiatRatesConstants';
+import { STORAGE } from '@suite-actions/constants';
+import { Network } from '@wallet-types';
 
 export interface Fiat {
-    symbol: string;
+    symbol: Network['symbol'];
     rates: { [key: string]: number };
+    timestamp: number;
 }
 
 export const initialState: Fiat[] = [];
 
-const update = (state: Fiat[], action: FiatRateActions) => {
-    const { symbol, rates } = action;
+const update = (state: Fiat[], fiat: Fiat) => {
+    const { symbol, rates, timestamp } = fiat;
     const affected = state.find(f => f.symbol === symbol);
-
     Object.keys(rates).map(k => rates[k].toFixed(2));
     if (!affected) {
         state.push({
             symbol,
             rates,
+            timestamp,
         });
     } else {
         affected.symbol = symbol;
         affected.rates = rates;
+        affected.timestamp = timestamp;
     }
 };
 
-export default (state: Fiat[] = initialState, action: FiatRateActions) => {
+export default (state: Fiat[] = initialState, action: Action): Fiat[] => {
     return produce(state, draft => {
         switch (action.type) {
             case RATE_UPDATE:
-                update(draft, action);
+                update(draft, action.payload);
                 break;
+            case STORAGE.LOADED:
+                return action.payload.wallet.fiat;
             // no default
         }
     });
