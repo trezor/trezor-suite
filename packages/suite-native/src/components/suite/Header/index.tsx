e53985a09fe2c90b4ad32f5ca564f4d456f95ba3
Szymon Lesisz <sz.lesisz@gmail.com>
2019-12-20 15:02:31 +0100

    lint fix

--- packages/suite-native/src/components/suite/Header/index.tsx
+++ packages/suite-native/src/components/suite/Header/index.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Image, TouchableOpacity } from 'react-native';
 import { HeaderBackButtonProps } from 'react-navigation-stack';
 
+/* eslint-disable global-require */
+
 export default (props: HeaderBackButtonProps) => {
     return (
         <TouchableOpacity onPress={() => props.scene.descriptor.navigation.openDrawer()}>
