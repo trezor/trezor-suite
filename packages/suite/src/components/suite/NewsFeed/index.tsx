4115a868fd49eb11d640e162311d4dd07be85d05
slowbackspace <slowbackspace@gmail.com>
2020-01-02 18:03:30 +0100

    add bottom action

--- packages/suite/src/components/suite/NewsFeed/index.tsx
+++ packages/suite/src/components/suite/NewsFeed/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import Card from '@suite-components/Card';
 import { colors, Button, variables } from '@trezor/components-v2';
+// import parser from 'fast-xml-parser';
 
 const StyledCard = styled(Card)`
     flex-direction: column;
@@ -20,6 +21,8 @@ const SectionHeader = styled.div`
     display: flex;
     padding: 12px 0px;
     flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
 `;
 
 const SectionTitle = styled.div`
@@ -29,6 +32,21 @@ const SectionTitle = styled.div`
     font-weight: 600;
     text-transform: uppercase;
     color: ${colors.BLACK50};
+`;
+
+const SectionAction = styled.div`
+    font-size: 12px;
+    font-weight: 500;
+    color: ${colors.BLACK25};
+`;
+
+const BottomAction = styled.div`
+    display: flex;
+    margin-top: 13px;
+    font-size: 12px;
+    font-weight: 500;
+    justify-content: center;
+    color: ${colors.BLACK25};
 `;
 
 const NewsItem = styled.div`
@@ -95,15 +113,79 @@ const CTAButton = styled(Button)``;
 
 export type Props = React.HTMLAttributes<HTMLDivElement>;
 
+// const stripHTML = (html: string) => {
+//     const doc = new DOMParser().parseFromString(html, 'text/html');
+//     return doc.body.textContent || '';
+// };
+
+// TODO: state logic for read/unread articles
 // TODO: Parse real RSS feed https://blog.trezor.io/feed
-const NewsFeed = ({ ...rest }: Props) => {
+// 0. convert xml to json (fast-xml-parser) works fine
+// 1. download the rss feed using a proxy server because of cors
+// 2. images for articles are not provided (could be parsed from the article's html)
+// 3. content of an article is provided only as html, need to strip html tags to show something reasonable
+// 4. content of an article includes the author and and date which will need to be removed
+const NewsFeed = React.memo(({ ...rest }: Props) => {
+    // const [items, setItems] = useState<any[]>([]);
+    // const [error, setError] = useState<string | null>(null);
+
+    // useEffect(() => {
+    //     const fetchBlogFeed = async () => {
+    //         const response = await fetch(
+    //             'https://cors-anywhere.herokuapp.com/https://blog.trezor.io/feed',
+    //         ); // TODO: custom proxy
+    //         if (!response.ok) setError(response.statusText);
+    //         if (response.body) {
+    //             const xmlData = await response.text();
+    //             const result = parser.parse(xmlData);
+    //             console.log(result);
+    //             setItems(result.rss.channel.item);
+    //         }
+    //     };
+    //     fetchBlogFeed();
+    // }, []);
+
     return (
         <Section {...rest}>
             <SectionHeader>
                 <SectionTitle>What's new</SectionTitle>
+                <SectionAction>
+                    <Button
+                        variant="tertiary"
+                        size="small"
+                        icon="CHECK"
+                        onClick={() => {
+                            console.log('do something');
+                        }}
+                    >
+                        Mark all as read
+                    </Button>
+                </SectionAction>
             </SectionHeader>
             <Content>
                 <StyledCard>
+                    {/* {items.map(item => (
+                        <NewsItem>
+                            <Left>
+                                <NewsImage />
+                            </Left>
+                            <Outline>
+                                <OutlineIcon show />
+                            </Outline>
+                            <Right>
+                                <NewsTitle visited={false}>{item.title}</NewsTitle>
+                                <Timestamp visited={false}>{item.pubDate}</Timestamp>
+                                <Description visited={false}>
+                                    {stripHTML(item['content:encoded'])}
+                                </Description>
+                                <CTAWrapper>
+                                    <CTAButton size="small" variant="tertiary">
+                                        Read more
+                                    </CTAButton>
+                                </CTAWrapper>
+                            </Right>
+                        </NewsItem>
+                    ))} */}
                     <NewsItem>
                         <Left>
                             <NewsImage />
@@ -126,33 +208,22 @@ const NewsFeed = ({ ...rest }: Props) => {
                             </CTAWrapper>
                         </Right>
                     </NewsItem>
-
-                    <NewsItem>
-                        <Left>
-                            <NewsImage />
-                        </Left>
-                        <Outline>
-                            <OutlineIcon show={false} />
-                        </Outline>
-                        <Right>
-                            <NewsTitle visited>Blog: Where in the world are my coins?</NewsTitle>
-                            <Timestamp visited>today</Timestamp>
-                            <Description visited>
-                                Your coins aren’t in your hardware wallet. But don’t panic! By the
-                                time you finish reading this article, you’ll have a better
-                                understanding of where your coins are…
-                            </Description>
-                            <CTAWrapper>
-                                <CTAButton size="small" variant="tertiary">
-                                    Read more
-                                </CTAButton>
-                            </CTAWrapper>
-                        </Right>
-                    </NewsItem>
                 </StyledCard>
             </Content>
+            <BottomAction>
+                <Button
+                    variant="tertiary"
+                    size="small"
+                    icon="ARROW_DOWN"
+                    onClick={() => {
+                        console.log('do something');
+                    }}
+                >
+                    Show older news
+                </Button>
+            </BottomAction>
         </Section>
     );
-};
+});
 
 export default NewsFeed;
