d5448248a24f51de00a4e43f2a76837046cda676
slowbackspace <slowbackspace@gmail.com>
2020-01-04 16:07:55 +0100

    remove old lang files, update url

--- packages/suite/src/actions/suite/languageActions.useNative.ts
+++ packages/suite/src/actions/suite/languageActions.useNative.ts
@@ -4,7 +4,7 @@ import { Dispatch } from '@suite-types';
 
 export const fetchLocale = (locale: string) => async (dispatch: Dispatch) => {
     try {
-        const response = await fetch(resolveStaticPath(`translations/l10n/${locale}.json`));
+        const response = await fetch(resolveStaticPath(`translations/${locale}.json`));
         if (!response.ok) throw Error(response.statusText);
         const messages: { [key: string]: string } = await response.json();
         dispatch({
