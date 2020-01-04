5812584e042010bbeb9f3b9e1750c15e21b328e0
slowbackspace <slowbackspace@gmail.com>
2020-01-04 17:56:35 +0100

    fix android build

---
+++ packages/suite-native/src/actions/suite/languageActions.ts
@@ -0,0 +1,22 @@
+/* eslint-disable global-require */
+
+import { SUITE } from '@suite-actions/constants';
+import { Dispatch } from '@suite-types';
+
+// TODO: copy files from suite-data and then require from this package just to be consistent?
+export const fetchLocale = (locale: string) => (dispatch: Dispatch) => {
+    const messages: { [key: string]: any } = {
+        en: require('@trezor/suite-data/files/translations/en'),
+        cs: require('@trezor/suite-data/files/translations/cs'),
+        de: require('@trezor/suite-data/files/translations/de'),
+        es: require('@trezor/suite-data/files/translations/es'),
+        fr: require('@trezor/suite-data/files/translations/fr'),
+        ru: require('@trezor/suite-data/files/translations/ru'),
+    };
+
+    dispatch({
+        type: SUITE.SET_LANGUAGE,
+        locale,
+        messages: messages[locale],
+    });
+};
