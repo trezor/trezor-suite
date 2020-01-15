b469e642b40b02aec7f10b1267a6abb4bb4323e5
martin <mvarmuza@gmail.com>
2020-01-15 11:49:54 +0100

    even longer

--- packages/integration-tests/projects/suite-web/tests/onboarding/create-new-wallet.test.ts
+++ packages/integration-tests/projects/suite-web/tests/onboarding/create-new-wallet.test.ts
@@ -2,7 +2,7 @@
 
 describe('Onboarding happy paths', () => {
     beforeEach(() => {
-        cy.wait(500);
+        cy.wait(1000);
         cy.task('startBridge');
         cy.task('startEmu');
         cy.task('wipeEmu');
