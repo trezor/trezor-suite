ea991497090070fa18b29704b77cc183695faac3
martin <mvarmuza@gmail.com>
2020-01-21 13:16:34 +0100

    rebased

--- packages/suite/src/types/settings/index.ts
+++ packages/suite/src/types/settings/index.ts
@@ -2,10 +2,6 @@ import { FirmwareActions } from '@settings-actions/firmwareActions';
 import { WalletSettingsActions } from '@settings-actions/walletSettingsActions';
 import { RecoveryActions } from '@settings-actions/recoveryActions';
 
-export type SettingsActions =
-    | FirmwareActions
-    | DeviceSettingsActions
-    | WalletSettingsActions
-    | RecoveryActions;
+export type SettingsActions = FirmwareActions | WalletSettingsActions | RecoveryActions;
 
 export type WordCount = 12 | 18 | 24;
