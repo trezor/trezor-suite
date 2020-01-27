c15cbfc26a0a9943d7ce5139148894dcbe036a34
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-01-27 17:00:32 +0100

    style tooltips

--- packages/components-storybook-v2/src/stories/components/tooltip/all.tsx
+++ packages/components-storybook-v2/src/stories/components/tooltip/all.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Tooltip, H2 } from '@trezor/components-v2';
+import { Tooltip } from '@trezor/components-v2';
 import { storiesOf } from '@storybook/react';
+import { StoryColumn } from '../../../components/Story';
 
 const Center = styled.div`
     display: flex;
@@ -12,6 +13,7 @@ const TooltipWrapper = styled.div`
     width: 400px;
     text-align: center;
     padding: 5rem 0px;
+    margin: 5px;
 `;
 
 storiesOf('Tooltip', module).add(
@@ -19,38 +21,34 @@ storiesOf('Tooltip', module).add(
     () => {
         return (
             <>
-                <H2>Top</H2>
-                <Center>
+                <StoryColumn>
                     <TooltipWrapper data-test="tooltip-top">
                         <Tooltip content="Tooltip text" visible>
                             <span>Tooltip top</span>
                         </Tooltip>
                     </TooltipWrapper>
-                </Center>
-                <H2>Bottom</H2>
-                <Center>
+                </StoryColumn>
+                <StoryColumn>
                     <TooltipWrapper data-test="tooltip-bottom">
                         <Tooltip content="Tooltip text" visible placement="bottom">
                             <span>Tooltip bottom</span>
                         </Tooltip>
                     </TooltipWrapper>
-                </Center>
-                <H2>Left</H2>
-                <Center>
+                </StoryColumn>
+                <StoryColumn>
                     <TooltipWrapper data-test="tooltip-left">
                         <Tooltip content="Tooltip text" visible placement="left">
                             <span>Tooltip left</span>
                         </Tooltip>
                     </TooltipWrapper>
-                </Center>
-                <H2>Right</H2>
-                <Center>
+                </StoryColumn>
+                <StoryColumn>
                     <TooltipWrapper data-test="tooltip-right">
                         <Tooltip content="Tooltip text" visible placement="right">
                             <span>Tooltip right</span>
                         </Tooltip>
                     </TooltipWrapper>
-                </Center>
+                </StoryColumn>
             </>
         );
     },
