4c9a299695d38d2d58f25591d2b20b6efbd6a470
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-01-28 17:28:04 +0100

    Remove sign & verify (#1106)
    * remove sign verify
    
    * fix ts
    
    * fix ts

--- packages/suite/src/config/wallet/menu.tsx
+++ packages/suite/src/config/wallet/menu.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Translation } from '@suite-components/Translation';
-import { NETWORKS } from '@wallet-config';
+// import { NETWORKS } from '@wallet-config';
 import messages from '@suite/support/messages';
 
 export const ITEMS = [
@@ -25,13 +25,13 @@ export const ITEMS = [
 ] as const;
 
 export const HIDDEN_ITEMS = [
-    {
-        route: 'wallet-sign-verify',
-        title: <Translation>{messages.TR_NAV_SIGN_AND_VERIFY}</Translation>,
-        icon: 'SIGN',
-        isHidden: (networkType: string) => {
-            const network = NETWORKS.find(c => c.symbol === networkType);
-            return network ? !network.hasSignVerify : false;
-        },
-    },
+    // {
+    //     route: 'wallet-sign-verify',
+    //     title: <Translation>{messages.TR_NAV_SIGN_AND_VERIFY}</Translation>,
+    //     icon: 'SIGN',
+    //     isHidden: (networkType: string) => {
+    //         const network = NETWORKS.find(c => c.symbol === networkType);
+    //         return network ? !network.hasSignVerify : false;
+    //     },
+    // },
 ] as const;
