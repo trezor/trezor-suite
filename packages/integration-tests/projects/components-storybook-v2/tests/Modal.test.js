670bfcd17cfc11f14e429e24fb807155fc1256d5
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-01-28 16:23:48 +0100

    fix tests

--- packages/integration-tests/projects/components-storybook-v2/tests/Modal.test.js
+++ packages/integration-tests/projects/components-storybook-v2/tests/Modal.test.js
@@ -3,11 +3,6 @@ describe('Modal', () => {
         cy.viewport(1024, 500);
         cy.loadContent('iframe.html?id=modals--default');
         cy.getTestElement('modal')
-            .find('svg')
-            .each(el => {
-                cy.get(el).should('be.visible');
-            });
-        cy.getTestElement('modal')
             .should('be.visible')
             .matchImageSnapshot();
     });
