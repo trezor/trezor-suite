b3aa94927fa755e2fa1f365a0e09a344137c1d84
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-01-29 18:16:45 +0100

    Add notification library and wrapper (#1058)
    * add main notification component
    
    * style notifications
    
    * add notifications to desktop
    
    * change notification position
    
    * finalize notifications
    
    * move styles to global
    
    * remove default export

--- packages/suite-desktop/pages/_app.tsx
+++ packages/suite-desktop/pages/_app.tsx
@@ -4,7 +4,6 @@ import { Store } from 'redux';
 import { Provider as ReduxProvider } from 'react-redux';
 import withRedux from 'next-redux-wrapper';
 import * as Sentry from '@sentry/browser';
-
 import { initStore } from '@suite/reducers/store';
 import Preloader from '@suite-components/Preloader';
 import Router from '@suite-support/Router';
@@ -37,13 +36,11 @@ class TrezorSuiteApp extends App<Props> {
                     <Resize />
                     <OnlineStatus />
                     <IntlProvider>
-                        <>
-                            <Router />
-                            <BridgeStatus />
-                            <Preloader>
-                                <Component {...pageProps} />
-                            </Preloader>
-                        </>
+                        <Router />
+                        <BridgeStatus />
+                        <Preloader>
+                            <Component {...pageProps} />
+                        </Preloader>
                     </IntlProvider>
                 </ReduxProvider>
             </ErrorBoundary>
--- packages/suite/src/components/suite/Preloader/index.tsx
+++ packages/suite/src/components/suite/Preloader/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { ToastContainer as NotificationsContainer } from 'react-toastify';
 import { bindActionCreators } from 'redux';
 import { SUITE } from '@suite-actions/constants';
 import { Modal as ModalComponent } from '@trezor/components';
@@ -178,6 +179,7 @@ const Preloader = (props: Props) => {
     // everything is set. action modals will use own ModalComponent wrapper
     return (
         <>
+            <NotificationsContainer />
             <Modals />
             {props.children}
         </>
