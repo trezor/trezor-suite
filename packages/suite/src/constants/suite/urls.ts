680ef93b8bc645f2046c8b0a1865e9fb673e08dc
Szymon Lesisz <sz.lesisz@gmail.com>
2020-02-04 15:50:09 +0100

    styled AccountAnnoucements for LTC, BCH, XRP (#1138)
    * styled AccountAnnoucements for LTC, BCH, XRP
    
    * Link Learn more with variant="nostyle"

--- packages/suite/src/constants/suite/urls.ts
+++ packages/suite/src/constants/suite/urls.ts
@@ -25,6 +25,9 @@ export const SATOSHILABS_URL = 'http://satoshilabs.com';
 export const COIN_GECKO_URL = 'https://www.coingecko.com';
 export const TRANSLATIONS_MANUAL_URL =
     'https://wiki.trezor.io/Developers_guide:CrowdIn.com_-_A_tool_for_translation';
-export const RIPPLE_MANUAL_URL = 'https://wiki.trezor.io/Ripple_(XRP)';
+export const XRP_MANUAL_URL = 'https://wiki.trezor.io/Ripple_(XRP)';
+export const BCH_ADDRESS_TOOL = 'https://cashaddr.bitcoincash.org/';
+export const LTC_ADDRESS_INFO_URL =
+    'https://blog.trezor.io/litecoins-new-p2sh-segwit-addresses-843633e3e707';
 export const BRIDGE_CHANGELOG_URL = 'https://github.com/trezor/trezord-go/blob/master/CHANGELOG.md';
 export const WIKI_QR_CODE = 'https://wiki.trezor.io/QR_code';
