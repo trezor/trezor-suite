1fcca2b86da7ea656174b5ef94f413a0583d1bc9
martin <mvarmuza@gmail.com>
2020-02-10 12:46:15 +0100

    missing stop emu task

--- packages/integration-tests/projects/suite-web/tests/suite/initial-run.test.ts
+++ packages/integration-tests/projects/suite-web/tests/suite/initial-run.test.ts
@@ -1,6 +1,7 @@
 describe('Suite initial run: when user opens suite for the first time, welcome and analytics modals appear', () => {
     beforeEach(() => {
         cy.viewport(1024, 768).resetDb();
+        cy.task('stopEmu');
     });
 
     it('Still reloads to welcome screen before initialRun flags is set to false', () => {
