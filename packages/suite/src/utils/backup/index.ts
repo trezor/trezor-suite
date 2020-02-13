df4e7ea0f553bb1a952827445aaa5abab92459fa
martin <30367552+mroz22@users.noreply.github.com>
2020-02-13 17:46:19 +0100

    Imporve backup (#1204)
    * add loading to backup modal
    
    * reorganize backup tests
    
    * Lock type
    
    * failed case test, set pin directly from success screen
    
    * add close button to connect device fallback
    
    * remove button

--- packages/suite/src/utils/backup/index.ts
+++ packages/suite/src/utils/backup/index.ts
@@ -1,12 +1,12 @@
 import { ConfirmKey } from '@backup-actions/backupActions';
-
+import { Lock } from '@suite-types';
 /**
  * Utility function used to disable backup start button
  */
-export const canStart = (userConfirmed: ConfirmKey[]) =>
+export const canStart = (userConfirmed: ConfirmKey[], locks: Lock[]) =>
     (['has-enough-time', 'is-in-private', 'understands-what-seed-is'] as const).every(e =>
         userConfirmed.includes(e),
-    );
+    ) && !locks.includes(2);
 
 /**
  * Utility function used to disable exit button after successful backup
