c34757990fe9361d2dd0e38271d0337810abd692
martin <mvarmuza@gmail.com>
2020-02-18 11:01:35 +0100

    experimenting with react-custom-scrollbars lib

--- packages/suite/src/components/onboarding/Wrapper/StepWrapper.tsx
+++ packages/suite/src/components/onboarding/Wrapper/StepWrapper.tsx
@@ -1,14 +1,8 @@
-import styled from 'styled-components';
+import React from 'react';
+import { Scrollbars } from 'react-custom-scrollbars';
 
-const StepWrapper = styled.div`
-    align-items: center;
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    height: 100%;
-    /* justify-content: space-around; */
-    padding-left: 10%;
-    padding-right: 10%;
-`;
-
-export default StepWrapper;
+interface Props {
+    children: React.ReactNode;
+    ['data-test']?: string;
+}
+export default ({ children, ...props }: Props) => <Scrollbars {...props}>{children}</Scrollbars>;
