fb95437eebbf268c1f0d6385565426c1d223e365
Szymon Lesisz <sz.lesisz@gmail.com>
2020-02-19 15:18:26 +0100

    fix for macos close

--- packages/suite-desktop/src-electron/electron.ts
+++ packages/suite-desktop/src-electron/electron.ts
@@ -63,10 +63,14 @@ const init = async () => {
     });
     mainWindow.removeMenu();
 
-    mainWindow.on('close', () => {
-        // store window bounds
-        store.setWinBounds(mainWindow);
-    });
+    if (process.platform === 'darwin') {
+        // On OS X it is common for applications and their menu bar
+        // to stay active until the user quits explicitly with Cmd + Q (onBeforeQuit = true)
+        mainWindow.on('close', event => {
+            event.preventDefault();
+            mainWindow.hide();
+        });
+    }
 
     // open external links in default browser
     const handleExternalLink = (event: Event, url: string) => {
@@ -110,10 +114,16 @@ app.on('ready', init);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
+    app.quit();
+    mainWindow = undefined;
+});
+
+app.on('before-quit', () => {
+    if (mainWindow) {
+        // remove onclose listener
+        mainWindow.removeAllListeners();
+        // store window bounds
+        store.setWinBounds(mainWindow);
     }
 });
 
@@ -129,7 +139,9 @@ app.on('will-quit', () => {
 app.on('activate', () => {
     // On OS X it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
+    if (mainWindow) {
+        mainWindow.show();
+    } else {
         init();
     }
 });
