8a06cfaa49d5bf69edee1c9647cacc5a272a090c
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-02-20 15:02:35 +0100

    remove prop

--- packages/suite/src/components/suite/HiddenPlaceholder/index.tsx
+++ packages/suite/src/components/suite/HiddenPlaceholder/index.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 
 interface WrapperProps {
     intensity: number;
-    showOnHover: boolean;
 }
 
 const Wrapper = styled.span<WrapperProps>`
@@ -16,17 +15,12 @@ const Wrapper = styled.span<WrapperProps>`
 `;
 
 interface Props {
-    showOnHover: boolean;
     children: ReactNode;
     discreetMode: boolean;
     intensity: number;
 }
 
-export default ({ showOnHover = true, children, discreetMode, intensity }: Props) => {
+export default ({ children, discreetMode, intensity }: Props) => {
     if (!discreetMode) return children;
-    return (
-        <Wrapper intensity={intensity} showOnHover={showOnHover}>
-            {children}
-        </Wrapper>
-    );
+    return <Wrapper intensity={intensity}>{children}</Wrapper>;
 };
