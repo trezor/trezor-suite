997196ab70419e9525f0c40fde347ea66ba73411
Szymon Lesisz <sz.lesisz@gmail.com>
2020-02-21 16:25:13 +0100

    remove toastMiddleware from index

--- packages/suite/src/middlewares/suite/index.ts
+++ packages/suite/src/middlewares/suite/index.ts
@@ -3,6 +3,5 @@ import suite from './suiteMiddleware';
 import redirect from './redirectMiddleware';
 import analytics from './analyticsMiddleware';
 import buttonRequest from './buttonRequestMiddleware';
-import toast from './toastMiddleware';
 
-export default [log, redirect, suite, analytics, buttonRequest, toast];
+export default [log, redirect, suite, analytics, buttonRequest];
