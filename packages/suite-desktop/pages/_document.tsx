431d7ec317ecdd654e03af69882f1cb2eb037b86
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-02-25 19:59:00 +0100

    Revert "fix ssr styled components (#1246)"
    This reverts commit c89efea356354d89227b3e4a15c8b68a012d2cc9.

--- packages/suite-desktop/pages/_document.tsx
+++ packages/suite-desktop/pages/_document.tsx
@@ -1,58 +1,44 @@
-import React from 'react';
-import Document, { DocumentContext, Head, Main, NextScript } from 'next/document';
-import { resolveStaticPath } from '@suite-utils/nextjs';
+import Document, { Head, Main, DocumentContext, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
+import { resolveStaticPath } from '@suite-utils/nextjs';
+import React from 'react';
+import { AppRegistry } from 'react-native';
 import globalStyles from '@suite-support/styles/global';
 
-export default class MyDocument extends Document {
-    static async getInitialProps(ctx: DocumentContext) {
+interface Props {
+    styleTags: any;
+}
+
+export default class MyDocument extends Document<Props> {
+    static async getInitialProps({ renderPage }: DocumentContext) {
+        AppRegistry.registerComponent('Main', () => Main);
+        // @ts-ignore getApplication is React Native Web addition for SSR.
+        const { getStyleElement } = AppRegistry.getApplication('Main');
         const sheet = new ServerStyleSheet();
-        const originalRenderPage = ctx.renderPage;
-        try {
-            ctx.renderPage = () =>
-                originalRenderPage({
-                    enhanceApp: App => props =>
-                        sheet.collectStyles(
-                            <>
-                                <App {...props} />
-                            </>,
-                        ),
-                });
-            const initialProps = await Document.getInitialProps(ctx);
-            return {
-                ...initialProps,
-                styles: (
-                    <>
-                        {initialProps.styles}
-                        {sheet.getStyleElement()}
-                        {/* eslint-disable-next-line react/no-danger */}
-                        <style dangerouslySetInnerHTML={{ __html: globalStyles }} key="styles" />
-                    </>
-                ),
-            };
-        } finally {
-            sheet.seal();
-        }
+        const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
+        const styleTags = sheet.getStyleElement();
+        const styles = [
+            // eslint-disable-next-line react/no-danger
+            <style dangerouslySetInnerHTML={{ __html: globalStyles }} key="styles" />,
+            getStyleElement(),
+        ];
+        return { ...page, styleTags, styles: React.Children.toArray(styles) };
     }
 
     render() {
         return (
-            <html lang="en">
+            <html lang="en" style={{ height: '100%' }}>
                 <Head>
                     <meta charSet="utf-8" />
-                    <script
-                        type="text/javascript"
-                        src={resolveStaticPath('browser-detection/index.js')}
-                    />
                     <link
                         media="all"
                         rel="stylesheet"
                         href={resolveStaticPath('fonts/fonts.css')}
                     />
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
-                    {this.props.styles}
+                    {this.props.styleTags}
                 </Head>
-                <body>
+                <body style={{ height: '100%' }}>
                     <Main />
                     <NextScript />
                 </body>
