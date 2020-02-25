c89efea356354d89227b3e4a15c8b68a012d2cc9
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-02-25 18:07:53 +0100

    fix ssr styled components (#1246)

--- packages/suite-web/pages/_document.tsx
+++ packages/suite-web/pages/_document.tsx
@@ -1,33 +1,43 @@
-import Document, { Head, Main, DocumentContext, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components';
-import { resolveStaticPath } from '@suite-utils/nextjs';
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import Document, { DocumentContext, Head, Main, NextScript } from 'next/document';
+import { resolveStaticPath } from '@suite-utils/nextjs';
+import { ServerStyleSheet } from 'styled-components';
 import globalStyles from '@suite-support/styles/global';
 
-interface Props {
-    styleTags: any;
-}
-
-export default class MyDocument extends Document<Props> {
-    static async getInitialProps({ renderPage }: DocumentContext) {
-        AppRegistry.registerComponent('Main', () => Main);
-        // @ts-ignore getApplication is React Native Web addition for SSR.
-        const { getStyleElement } = AppRegistry.getApplication('Main');
+export default class MyDocument extends Document {
+    static async getInitialProps(ctx: DocumentContext) {
         const sheet = new ServerStyleSheet();
-        const page = renderPage(App => props => sheet.collectStyles(<App {...props} />));
-        const styleTags = sheet.getStyleElement();
-        const styles = [
-            // eslint-disable-next-line react/no-danger
-            <style dangerouslySetInnerHTML={{ __html: globalStyles }} key="styles" />,
-            getStyleElement(),
-        ];
-        return { ...page, styleTags, styles: React.Children.toArray(styles) };
+        const originalRenderPage = ctx.renderPage;
+        try {
+            ctx.renderPage = () =>
+                originalRenderPage({
+                    enhanceApp: App => props =>
+                        sheet.collectStyles(
+                            <>
+                                <App {...props} />
+                            </>,
+                        ),
+                });
+            const initialProps = await Document.getInitialProps(ctx);
+            return {
+                ...initialProps,
+                styles: (
+                    <>
+                        {initialProps.styles}
+                        {sheet.getStyleElement()}
+                        {/* eslint-disable-next-line react/no-danger */}
+                        <style dangerouslySetInnerHTML={{ __html: globalStyles }} key="styles" />
+                    </>
+                ),
+            };
+        } finally {
+            sheet.seal();
+        }
     }
 
     render() {
         return (
-            <html lang="en" style={{ height: '100%' }}>
+            <html lang="en">
                 <Head>
                     <meta charSet="utf-8" />
                     <script
@@ -40,9 +50,9 @@ export default class MyDocument extends Document<Props> {
                         href={resolveStaticPath('fonts/fonts.css')}
                     />
                     <meta name="viewport" content="width=device-width, initial-scale=1" />
-                    {this.props.styleTags}
+                    {this.props.styles}
                 </Head>
-                <body style={{ height: '100%' }}>
+                <body>
                     <Main />
                     <NextScript />
                 </body>
