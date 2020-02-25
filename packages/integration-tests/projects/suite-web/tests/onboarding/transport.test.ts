bce8a7a43a8889a2a6d08a8916c2896419cd0f5f
martin <mvarmuza@gmail.com>
2020-02-25 15:39:10 +0100

    fix e2e

--- packages/integration-tests/projects/suite-web/tests/onboarding/transport.test.ts
+++ packages/integration-tests/projects/suite-web/tests/onboarding/transport.test.ts
@@ -11,26 +11,20 @@ describe('Transport webusb/bridge', () => {
     });
 
     it('Offer webusb as primary choice on web, but allow user to disable it and fallback to bridge', () => {
-        cy.getTestElement('@onboarding/button-begin')
-            .click()
-            .getTestElement('@onboarding/button-path-create')
-            .click()
-            .getTestElement('@onboarding/button-used-path')
-            .click()
-            .getTestElement('@onboarding/try-bridge-button')
-            .click()
-            .getTestElement('@onboarding/bridge');
+        cy.getTestElement('@onboarding/begin-button').click();
+        cy.getTestElement('@onboarding/path-create-button').click();
+        cy.getTestElement('@onboarding/path-used-button').click();
+        cy.getTestElement('@onboarding/try-bridge-button').click();
+        cy.getTestElement('@onboarding/bridge');
     });
 
     it('user selects new device -> user selects model one -> in this case we know that he can not use webusb (unreadable device) so we disable webusb and offer bridge download', () => {
-        cy.getTestElement('@onboarding/button-begin')
-            .click()
-            .getTestElement('@onboarding/button-path-create')
-            .click()
-            .getTestElement('@onboarding/path-new-button')
-            .click()
-            .getTestElement('@onboarding/option-model-one-path')
-            .click()
-            .getTestElement('@onboarding/bridge');
+        cy.getTestElement('@onboarding/begin-button').click();
+        cy.getTestElement('@onboarding/path-create-button').click();
+        cy.getTestElement('@onboarding/path-new-button').click();
+        cy.getTestElement('@onboarding/option-model-one-path').click();
+        cy.getTestElement('@onboarding/continue-button').click();
+        // see, no try bridge button, we already know we can not use webusb
+        cy.getTestElement('@onboarding/bridge');
     });
 });
