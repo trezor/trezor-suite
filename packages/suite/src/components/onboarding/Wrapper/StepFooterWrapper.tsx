4daae2b117cfdbbe12afd1b69b60807e68be42ee
martin <mvarmuza@gmail.com>
2020-02-26 11:29:04 +0100

    1px

--- packages/suite/src/components/onboarding/Wrapper/StepFooterWrapper.tsx
+++ packages/suite/src/components/onboarding/Wrapper/StepFooterWrapper.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const StepFooterWrapper = styled.div`
     display: flex;
     margin-top: auto;
-    min-height: 35px;
+    min-height: 36px;
     text-align: center;
     justify-content: center;
     flex: 1;
