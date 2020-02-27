8104f8a81ef4d453e55ea9a72cc26d48f57b8a59
slowbackspace <slowbackspace@gmail.com>
2020-02-27 14:26:17 +0100

    fix typo

--- packages/suite/src/support/messages.ts
+++ packages/suite/src/support/messages.ts
@@ -1774,9 +1774,9 @@ const definedMessages = defineMessages({
         defaultMessage: '(Unknown transaction)',
         id: 'TR_UNKNOWN_TRANSACTION',
     },
-    TR_UNKOWN_ERROR_SEE_CONSOLE: {
+    TR_UNKNOWN_ERROR_SEE_CONSOLE: {
         defaultMessage: 'Unknown error. See console logs for details.',
-        id: 'TR_UNKOWN_ERROR_SEE_CONSOLE',
+        id: 'TR_UNKNOWN_ERROR_SEE_CONSOLE',
     },
     TR_UNLOCK: {
         defaultMessage: 'Unlock',
