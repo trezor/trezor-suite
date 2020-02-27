511b5d7ed49599e5364be3c5413cc1aa25c27fe3
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-02-27 16:38:51 +0100

    fix path

--- packages/components/src/components/Tooltip/all.stories.tsx
+++ packages/components/src/components/Tooltip/all.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Tooltip } from '../../';
+import { Tooltip } from './index';
 import { storiesOf } from '@storybook/react';
 
 const TooltipWrapper = styled.div`
