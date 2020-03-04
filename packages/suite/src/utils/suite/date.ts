159c0f52a28a63963e5404550d293a47eee8af3c
slowbackspace <slowbackspace@gmail.com>
2020-03-04 11:44:17 +0100

    remove try catch

--- packages/suite/src/utils/suite/date.ts
+++ packages/suite/src/utils/suite/date.ts
@@ -5,12 +5,7 @@ export const formatDuration = (seconds: number) =>
     formatDistance(0, seconds * 1000, { includeSeconds: true });
 
 export const getDateWithTimeZone = (date: number, timeZone?: string) => {
-    try {
-        const unixDate = fromUnixTime(date / 1000);
-        const tz = timeZone || Intl.DateTimeFormat().resolvedOptions().timeZone;
-        return utcToZonedTime(unixDate, tz);
-    } catch (err) {
-        console.error(err);
-        return null;
-    }
+    const unixDate = fromUnixTime(date / 1000);
+    const tz = timeZone || Intl.DateTimeFormat().resolvedOptions().timeZone;
+    return utcToZonedTime(unixDate, tz);
 };
