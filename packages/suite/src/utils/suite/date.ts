fba7438ad5ef1e9870072a89483dec83df47a803
slowbackspace <slowbackspace@gmail.com>
2020-03-04 11:39:32 +0100

    fix parsing date key for grouped transactions

--- packages/suite/src/utils/suite/date.ts
+++ packages/suite/src/utils/suite/date.ts
@@ -1,12 +1,16 @@
-import { formatDistance } from 'date-fns';
+import { formatDistance, fromUnixTime } from 'date-fns';
 import { utcToZonedTime } from 'date-fns-tz';
 
 export const formatDuration = (seconds: number) =>
     formatDistance(0, seconds * 1000, { includeSeconds: true });
 
-export const getDateWithTimeZone = (date: number | string, timeZone?: string): Date => {
-    const isoDate = new Date(date).toISOString();
-    const tz = timeZone || Intl.DateTimeFormat().resolvedOptions().timeZone;
-
-    return utcToZonedTime(isoDate, tz);
+export const getDateWithTimeZone = (date: number, timeZone?: string) => {
+    try {
+        const unixDate = fromUnixTime(date / 1000);
+        const tz = timeZone || Intl.DateTimeFormat().resolvedOptions().timeZone;
+        return utcToZonedTime(unixDate, tz);
+    } catch (err) {
+        console.error(err);
+        return null;
+    }
 };
