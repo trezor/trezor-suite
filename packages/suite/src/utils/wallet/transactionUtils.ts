6d8b38fab72ef2eb57f4871134864660bd3effea
Szymon Lesisz <sz.lesisz@gmail.com>
2020-03-06 10:50:41 +0100

    create analyzeTransactions utility

--- packages/suite/src/utils/wallet/transactionUtils.ts
+++ packages/suite/src/utils/wallet/transactionUtils.ts
@@ -1,3 +1,4 @@
+import { AccountTransaction } from 'trezor-connect';
 import { WalletAccountTransaction } from '@wallet-reducers/transactionReducer';
 import { getDateWithTimeZone } from '../suite/date';
 import BigNumber from 'bignumber.js';
@@ -62,3 +63,77 @@ export const parseKey = (key: string) => {
 
 export const findTransaction = (txid: string, transactions: WalletAccountTransaction[]) =>
     transactions.find(t => t && t.txid === txid);
+
+export const analyzeTransactions = (
+    fresh: AccountTransaction[],
+    known: WalletAccountTransaction[],
+) => {
+    let addTxs: AccountTransaction[] = [];
+    const newTxs: AccountTransaction[] = [];
+    let firstKnownIndex = 0;
+    let sliceIndex = 0;
+
+    if (fresh.length < 1) {
+        return {
+            newTransactions: [],
+            add: [],
+            remove: known,
+        };
+    }
+
+    // If there are no known confirmed txs
+    // just remove all known and add all fresh
+    const gotConfirmedTxs = known.some(tx => tx.blockHeight);
+    if (!gotConfirmedTxs) {
+        return {
+            newTransactions: fresh.filter(tx => tx.blockHeight),
+            add: fresh,
+            remove: known,
+        };
+    }
+
+    fresh.forEach((tx, i) => {
+        const height = tx.blockHeight;
+        const isLast = i + 1 === fresh.length;
+        if (!height) {
+            addTxs.push(tx);
+        } else {
+            let index = firstKnownIndex;
+            const len = known.length;
+            for (index; index < len; index++) {
+                const kTx = known[index];
+                if (!kTx.blockHeight) {
+                    firstKnownIndex = index + 1;
+                    sliceIndex = index + 1;
+                }
+                if (kTx.blockHeight && kTx.blockHeight < height) {
+                    sliceIndex = isLast ? len : index;
+                    // console.warn("-lower", kTx, tx, isLast)
+                    addTxs = fresh.slice(0, i + 1);
+                    newTxs.push(tx);
+                    break;
+                }
+                if (kTx.blockHeight === height) {
+                    firstKnownIndex = index + 1;
+                    // console.warn("-equal", kTx, tx)
+                    if (kTx.blockHash !== tx.blockHash) {
+                        sliceIndex = isLast ? len : index + 1;
+                        // console.warn("+++setslice", index, isLast);
+                        addTxs.push(tx);
+                    }
+                    break;
+                }
+                // console.warn("-default", kTx, tx)
+            }
+            // if (changed > 0 && validIndex < index - changed) validIndex = index - changed;
+            // if (validIndex < lastValidIndex - changed) validIndex = lastValidIndex - changed;
+            // if (validIndex < index - changed) validIndex = index - changed;
+            // console.log("Final index", index firstKnownIndex, sliceIndex);
+        }
+    });
+    return {
+        newTransactions: newTxs,
+        add: addTxs,
+        remove: known.slice(0, sliceIndex),
+    };
+};
