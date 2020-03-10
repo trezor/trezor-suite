444431b948ff5b94b1d8f99bfb16376f22201c80
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-03-10 16:13:10 +0100

    implement coin balance

--- packages/suite/src/components/wallet/CoinBalance/index.tsx
+++ packages/suite/src/components/wallet/CoinBalance/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import BigNumber from 'bignumber.js';
 import styled from 'styled-components';
 import { Account } from '@wallet-types';
+import { formatCoinBalance } from '@wallet-utils/balanceUtils';
 import { HiddenPlaceholder } from '@suite-components';
 
 const Wrapper = styled.div`
@@ -14,40 +14,16 @@ const Symbol = styled.span`
     text-indent: 3px;
 `;
 
-const MAX_NUMBERS = 8;
-
 interface Props {
     value: string;
     symbol: Account['symbol'];
 }
 
-const getBalance = (value: string) => {
-    const balanceBig = new BigNumber(value);
-
-    if (balanceBig.isZero() || !balanceBig.isNaN()) return '0';
-
-    const fixedBalance = new BigNumber(balanceBig.toFixed(MAX_NUMBERS, 1));
-    if (fixedBalance.isZero()) {
-        return '~0';
-    }
-
-    const balanceWithPrecision = fixedBalance.precision(MAX_NUMBERS).toFixed();
-    const balanceWithPrecisionLength = balanceWithPrecision.length;
-    let result = balanceWithPrecision;
-
-    if (balanceWithPrecisionLength < MAX_NUMBERS) {
-        const zerosToFill = MAX_NUMBERS - balanceWithPrecisionLength + 1;
-        [...Array(zerosToFill)].map(() => (result += '0'));
-    }
-
-    return result;
-};
-
 export default ({ value, symbol }: Props) => {
     return (
         <HiddenPlaceholder>
             <Wrapper>
-                <Value>{getBalance(value)}</Value>
+                <Value>{formatCoinBalance(value)}</Value>
                 <Symbol>{symbol ? symbol.toUpperCase() : symbol}</Symbol>
             </Wrapper>
         </HiddenPlaceholder>
--- packages/suite/src/utils/wallet/balanceUtils.ts
+++ packages/suite/src/utils/wallet/balanceUtils.ts
@@ -1,3 +1,21 @@
+import BigNumber from 'bignumber.js';
+
 export const formatCoinBalance = (value: string) => {
-    return 'aaa';
+    const MAX_NUMBERS = 8;
+    const balanceBig = new BigNumber(value);
+
+    if (balanceBig.isZero()) return '0';
+
+    const parts = value.split('.');
+    const hasDecimals = parts.length > 1;
+
+    if (hasDecimals) {
+        const DOT_CHAR_LENGTH = 1;
+        const parts = value.split('.');
+        return balanceBig.toFixed(MAX_NUMBERS - parts[0].length || 1 - DOT_CHAR_LENGTH, 1);
+    }
+
+    if (balanceBig.isGreaterThanOrEqualTo(100000000)) return '100M+';
+
+    return balanceBig.toFixed();
 };
