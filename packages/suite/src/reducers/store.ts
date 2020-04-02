4c25e36220a04f15b267ec9a173747d8f779c24b
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-04-02 15:20:58 +0200

    use redux toolkit in store

--- packages/suite/src/reducers/store.ts
+++ packages/suite/src/reducers/store.ts
@@ -1,21 +1,20 @@
-import { createStore, applyMiddleware, combineReducers, compose } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
-
 import * as buildUtils from '@suite-utils/build';
+import { createLogger } from 'redux-logger';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import thunkMiddleware from 'redux-thunk';
 import suiteMiddlewares from '@suite-middlewares';
 import walletMiddlewares from '@wallet-middlewares';
 import onboardingMiddlewares from '@onboarding-middlewares';
 import firmwareMiddlewares from '@firmware-middlewares';
 import backupMiddlewares from '@backup-middlewares';
 import recoveryMiddlewares from '@recovery-middlewares';
-
 import suiteReducers from '@suite-reducers';
 import walletReducers from '@wallet-reducers';
 import onboardingReducers from '@onboarding-reducers';
 import recoveryReducers from '@recovery-reducers';
 import firmwareReducers from '@firmware-reducers';
 import backupReducers from '@backup-reducers';
+
 // toastMiddleware can be used only in suite-desktop and suite-web
 // it's not included into `@suite-middlewares` index
 import toastMiddleware from '@suite-middlewares/toastMiddleware';
@@ -42,7 +41,6 @@ const middlewares = [
     ...recoveryMiddlewares,
 ];
 
-const enhancers: any[] = [];
 if (buildUtils.isDev()) {
     const excludeLogger = (_getState: any, action: any): boolean => {
         // '@@router/LOCATION_CHANGE'
@@ -56,20 +54,8 @@ if (buildUtils.isDev()) {
         predicate: excludeLogger,
         collapsed: true,
     });
-    middlewares.push(logger);
 
-    /* eslint-disable no-underscore-dangle */
-    if (
-        typeof window !== 'undefined' &&
-        typeof (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ === 'function'
-    ) {
-        enhancers.push((window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__());
-    }
-    /* eslint-enable no-underscore-dangle */
+    middlewares.push(logger);
 }
 
-const composedEnhancers = compose(applyMiddleware(...middlewares), ...enhancers);
-
-export const initStore = () => {
-    return createStore(rootReducer, composedEnhancers);
-};
+export const initStore = () => configureStore({ reducer: rootReducer, middleware: middlewares });
