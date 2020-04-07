d9deeb2baecbe21edb7b0e0c2b398846796f49f7
Szymon Lesisz <sz.lesisz@gmail.com>
2020-04-07 16:45:49 +0200

    update Wallet menu

--- packages/suite/src/config/wallet/menu.tsx
+++ packages/suite/src/config/wallet/menu.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import { Translation } from '@suite-components/Translation';
-// import { NETWORKS } from '@wallet-config';
 import { Account } from '@wallet-types';
+
+export const VISIBLE_ITEMS_LIMIT = 3;
 
 export const ITEMS = [
     {
@@ -28,16 +29,16 @@ export const ITEMS = [
         icon: 'INFO',
         isHidden: (networkType: Account['networkType']) => networkType !== 'bitcoin',
     },
-] as const;
-
-export const HIDDEN_ITEMS = [
+    {
+        route: 'wallet-sign-verify',
+        title: <Translation id="TR_NAV_SIGN_AND_VERIFY" />,
+        icon: 'SIGN',
+        isHidden: () => false,
+    },
     // {
-    //     route: 'wallet-sign-verify',
-    //     title: <Translation id="TR_NAV_SIGN_AND_VERIFY" />,
+    //     route: 'wallet-index',
+    //     title: <Translation id="TR_BUY" />,
     //     icon: 'SIGN',
-    //     isHidden: (networkType: string) => {
-    //         const network = NETWORKS.find(c => c.symbol === networkType);
-    //         return network ? !network.hasSignVerify : false;
-    //     },
+    //     isHidden: () => false,
     // },
 ] as const;
--- packages/suite/src/support/messages.ts
+++ packages/suite/src/support/messages.ts
@@ -1002,6 +1002,10 @@ const definedMessages = defineMessages({
         description: 'Link to Trezor wiki.',
         id: 'TR_LEARN_MORE_LINK',
     },
+    TR_SEGWIT_ACCOUNTS: {
+        defaultMessage: 'Segwit accounts',
+        id: 'TR_SEGWIT_ACCOUNTS',
+    },
     TR_LEGACY_ACCOUNTS: {
         defaultMessage: 'Legacy accounts',
         id: 'TR_LEGACY_ACCOUNTS',
