efca48e5510faa6db8e88b2040406b8afc43c117
slowbackspace <slowbackspace@gmail.com>
2020-04-09 14:12:42 +0200

    fix settings rows

--- packages/suite/src/components/suite/Settings/components/Row.tsx
+++ packages/suite/src/components/suite/Settings/components/Row.tsx
@@ -4,22 +4,26 @@ import styled from 'styled-components';
 
 const { SCREEN_SIZE } = variables;
 
-const Wrapper = styled.div`
-    display: flex;
-    flex: 1;
-    padding: 0 24px;
-`;
-
 const Content = styled.div`
     display: flex;
     justify-content: space-between;
     flex: 1;
-
-    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
     padding: 16px 0;
 
     @media all and (max-width: ${SCREEN_SIZE.SM}) {
         flex-direction: column;
+    }
+`;
+
+const Wrapper = styled.div`
+    display: flex;
+    flex: 1;
+    padding: 0 24px;
+
+    & + & {
+        ${Content} {
+            border-top: 1px solid rgba(0, 0, 0, 0.1);
+        }
     }
 `;
 
