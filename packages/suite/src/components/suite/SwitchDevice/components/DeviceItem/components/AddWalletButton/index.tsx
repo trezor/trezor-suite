552c6cafe7686aa344ef0898bd2041b4f04f6186
Maroš <slowbackspace@gmail.com>
2020-04-09 10:11:04 +0200

    Select undiscovered wallet on "add wallet" click (#1468)
    * select undiscovered wallet on add wallet btn
    
    * remove unused msg
    
    * remove unnecessary async/await
    
    * Revert "remove unnecessary async/await"
    
    This reverts commit 62f565ddd1cec10bf2afdfb6efd71e6945c57cb5.
    
    * try with return
    
    * tests don't lie :D
    
    * review fixes
    
    Co-authored-by: mroz22 <mvarmuza@gmail.com>

--- packages/suite/src/components/suite/SwitchDevice/components/DeviceItem/components/AddWalletButton/index.tsx
+++ packages/suite/src/components/suite/SwitchDevice/components/DeviceItem/components/AddWalletButton/index.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { Button, Tooltip } from '@trezor/components';
 import { Translation } from '@suite-components';
 import { TrezorDevice, AcquiredDevice } from '@suite-types';
+import { useDeviceActionLocks } from '@suite-utils/hooks';
 
 const AddWallet = styled.div`
     display: flex;
@@ -13,32 +14,26 @@ interface Props {
     device: TrezorDevice;
     instances: AcquiredDevice[];
     addDeviceInstance: (instance: TrezorDevice) => Promise<void>;
+    selectDeviceInstance: (instance: TrezorDevice) => Promise<void>;
 }
 
-const AddWalletButton = ({ device, instances, addDeviceInstance }: Props) => {
+const AddWalletButton = ({ device, instances, addDeviceInstance, selectDeviceInstance }: Props) => {
     const hasAtLeastOneWallet = instances.find(d => d.state);
-
-    let tooltipMsg: string | JSX.Element = '';
-    if (!hasAtLeastOneWallet) {
-        tooltipMsg = <Translation id="TR_TO_ACCESS_OTHER_WALLETS_DISCOVER" />;
-    }
-    if (!device.connected) {
-        tooltipMsg = <Translation id="TR_TO_ACCESS_OTHER_WALLETS" />;
-    }
-
+    const tooltipMsg = <Translation id="TR_TO_ACCESS_OTHER_WALLETS" />;
+    const [actionEnabled] = useDeviceActionLocks();
     return (
         <AddWallet>
-            <Tooltip
-                enabled={!device.connected || !hasAtLeastOneWallet}
-                placement="top"
-                content={tooltipMsg}
-            >
+            <Tooltip enabled={!device.connected} placement="top" content={tooltipMsg}>
                 <Button
                     data-test="@switch-device/add-wallet-button"
                     variant="tertiary"
                     icon="PLUS"
-                    isDisabled={!device.connected || !hasAtLeastOneWallet}
-                    onClick={async () => addDeviceInstance(device)}
+                    isDisabled={!actionEnabled || !hasAtLeastOneWallet}
+                    onClick={async () =>
+                        hasAtLeastOneWallet
+                            ? addDeviceInstance(device)
+                            : selectDeviceInstance(instances[0])
+                    }
                 >
                     <Translation id="TR_ADD_WALLET" />
                 </Button>
