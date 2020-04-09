6e2a2d8763055db8a98cbf4c7fc2bd4a2af699eb
Szymon Lesisz <sz.lesisz@gmail.com>
2020-04-09 18:08:30 +0200

    reexport trezor-connect.BLOCKCHAIN from @wallet-actions/contants

--- packages/suite/src/actions/wallet/constants/blockchainConstants.ts
+++ packages/suite/src/actions/wallet/constants/blockchainConstants.ts
@@ -1,4 +1,13 @@
+import { BLOCKCHAIN } from 'trezor-connect';
 export const READY = '@blockchain/ready';
+export const SET_RECONNECT_TIMEOUT = '@blockchain/set-reconnect-timeout';
 export const START_SUBSCRIBE = '@blockchain/start-subscribe';
 export const FAIL_SUBSCRIBE = '@blockchain/fail-subscribe';
 export const UPDATE_FEE = '@blockchain/update-fee';
+
+// reexport from trezor-connect
+export const CONNECT = BLOCKCHAIN.CONNECT;
+export const ERROR = BLOCKCHAIN.ERROR;
+export const BLOCK = BLOCKCHAIN.BLOCK;
+export const NOTIFICATION = BLOCKCHAIN.NOTIFICATION;
+export const FIAT_RATES_UPDATE = BLOCKCHAIN.FIAT_RATES_UPDATE;
