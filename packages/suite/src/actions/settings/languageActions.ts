5cf8507edc2f46fc5aa8f1f545fd1953e82766da
slowbackspace <slowbackspace@gmail.com>
2020-04-14 09:00:27 +0200

    bake en translations into the app

--- packages/suite/src/actions/settings/languageActions.ts
+++ packages/suite/src/actions/settings/languageActions.ts
@@ -2,24 +2,34 @@ import { resolveStaticPath } from '@suite-utils/nextjs';
 import { SUITE } from '@suite-actions/constants';
 import { Dispatch } from '@suite-types';
 import { LANGUAGES } from '@suite-config';
+import enLocale from '@trezor/suite-data/files/translations/en.json';
 
 export const fetchLocale = (locale: typeof LANGUAGES[number]['code']) => async (
     dispatch: Dispatch,
 ) => {
-    try {
-        const response = await fetch(resolveStaticPath(`translations/${locale}.json`));
-        if (!response.ok) throw Error(response.statusText);
-        const messages: { [key: string]: string } = await response.json();
+    if (locale === 'en') {
         dispatch({
             type: SUITE.SET_LANGUAGE,
             locale,
-            messages,
+            messages: enLocale,
         });
-    } catch (error) {
-        dispatch({
-            type: SUITE.SET_LANGUAGE,
-            locale: 'en',
-            messages: {},
-        });
+    } else {
+        try {
+            const response = await fetch(resolveStaticPath(`translations/${locale}.json`));
+            if (!response.ok) throw Error(response.statusText);
+
+            const messages: { [key: string]: string } = await response.json();
+            dispatch({
+                type: SUITE.SET_LANGUAGE,
+                locale,
+                messages,
+            });
+        } catch (error) {
+            dispatch({
+                type: SUITE.SET_LANGUAGE,
+                locale: 'en',
+                messages: enLocale,
+            });
+        }
     }
 };
