e2fe03c8aaed2555de7f972950ab16edc80955a6
slowbackspace <slowbackspace@gmail.com>
2020-04-16 12:39:57 +0200

    fetch graph data on discovery complete

--- packages/suite/src/middlewares/wallet/graphMiddleware.ts
+++ packages/suite/src/middlewares/wallet/graphMiddleware.ts
@@ -1,6 +1,5 @@
 import { MiddlewareAPI } from 'redux';
-// import { SUITE } from '@suite-actions/constants';
-import { ACCOUNT } from '@wallet-actions/constants';
+import { DISCOVERY } from '@wallet-actions/constants';
 import { WALLET_SETTINGS } from '@settings-actions/constants';
 import * as graphActions from '@wallet-actions/graphActions';
 import { AppState, Action, Dispatch } from '@suite-types';
@@ -8,24 +7,25 @@ import { AppState, Action, Dispatch } from '@suite-types';
 const graphMiddleware = (api: MiddlewareAPI<Dispatch, AppState>) => (next: Dispatch) => (
     action: Action,
 ): Action => {
+    const prevAccounts = api.getState().wallet.accounts;
     next(action);
+    const currentAccounts = api.getState().wallet.accounts;
 
     switch (action.type) {
-        // case STORAGE.LOADED:
-        //     api.dispatch(graphActions.updateGraphData(action.payload.wallet.accounts));
-        //     break;
-
-        // case DISCOVERY.COMPLETE:
-        //     // todo: fetch data for added accounts only
-        //     api.dispatch(graphActions.updateGraphData(api.getState().wallet.accounts));
-        //     break;
-
-        case ACCOUNT.CREATE:
-            api.dispatch(graphActions.updateGraphData([action.payload]));
+        case DISCOVERY.COMPLETE: {
+            // todo: fetch data for added accounts only
+            // refetch the data only if on accounts change or if is a first fetch (handles also accounts loaded from storage)
+            if (
+                prevAccounts.length !== currentAccounts.length ||
+                api.getState().wallet.graph.data.length === 0
+            ) {
+                api.dispatch(graphActions.updateGraphData(currentAccounts));
+            }
             break;
+        }
 
         case WALLET_SETTINGS.CHANGE_NETWORKS:
-            api.dispatch(graphActions.updateGraphData(api.getState().wallet.accounts));
+            api.dispatch(graphActions.updateGraphData(currentAccounts));
             break;
 
         default:
