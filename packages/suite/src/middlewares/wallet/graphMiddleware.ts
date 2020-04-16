f35ddf45782b9a64637326f4134862932f7ab264
slowbackspace <slowbackspace@gmail.com>
2020-04-16 11:50:54 +0200

    react to network change

--- packages/suite/src/middlewares/wallet/graphMiddleware.ts
+++ packages/suite/src/middlewares/wallet/graphMiddleware.ts
@@ -1,17 +1,31 @@
 import { MiddlewareAPI } from 'redux';
+// import { SUITE } from '@suite-actions/constants';
 import { ACCOUNT } from '@wallet-actions/constants';
+import { WALLET_SETTINGS } from '@settings-actions/constants';
 import * as graphActions from '@wallet-actions/graphActions';
 import { AppState, Action, Dispatch } from '@suite-types';
 
 const graphMiddleware = (api: MiddlewareAPI<Dispatch, AppState>) => (next: Dispatch) => (
     action: Action,
 ): Action => {
-    // pass action
     next(action);
 
     switch (action.type) {
+        // case STORAGE.LOADED:
+        //     api.dispatch(graphActions.updateGraphData(action.payload.wallet.accounts));
+        //     break;
+
+        // case DISCOVERY.COMPLETE:
+        //     // todo: fetch data for added accounts only
+        //     api.dispatch(graphActions.updateGraphData(api.getState().wallet.accounts));
+        //     break;
+
         case ACCOUNT.CREATE:
             api.dispatch(graphActions.updateGraphData([action.payload]));
+            break;
+
+        case WALLET_SETTINGS.CHANGE_NETWORKS:
+            api.dispatch(graphActions.updateGraphData(api.getState().wallet.accounts));
             break;
 
         default:
