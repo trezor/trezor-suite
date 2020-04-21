a87cf7d1343beef920713be84909f37297b6ef29
slowbackspace <slowbackspace@gmail.com>
2020-04-21 19:13:05 +0200

    add error icon to confirm address modal on disconnected device

--- packages/suite/src/components/suite/modals/confirm/Address/components/DeviceDisconnected.tsx
+++ packages/suite/src/components/suite/modals/confirm/Address/components/DeviceDisconnected.tsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import styled from 'styled-components';
 import { P, colors } from '@trezor/components';
-import { Translation } from '@suite-components';
+import { Translation, Image } from '@suite-components';
 
 const Wrapper = styled.div`
     display: flex;
-    flex-direction: column;
-    align-items: start;
-    padding: 16px;
     margin: 0px 32px;
     margin-bottom: 40px;
     text-align: left;
     background: ${colors.BLACK96};
+    align-items: center;
+`;
+
+const Col = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: start;
+    text-align: left;
+`;
+
+const ContentCol = styled(Col)`
+    padding: 16px;
+`;
+const ImageCol = styled(Col)`
+    padding: 4px;
 `;
 
 interface Props {
@@ -20,11 +32,19 @@ interface Props {
 
 export default ({ label }: Props) => (
     <Wrapper>
-        <P size="small">
-            <Translation id="TR_DEVICE_LABEL_IS_NOT_CONNECTED" values={{ deviceLabel: label }} />
-        </P>
-        <P size="tiny">
-            <Translation id="TR_PLEASE_CONNECT_YOUR_DEVICE" />
-        </P>
+        <ContentCol>
+            <P size="small">
+                <Translation
+                    id="TR_DEVICE_LABEL_IS_NOT_CONNECTED"
+                    values={{ deviceLabel: label }}
+                />
+            </P>
+            <P size="tiny">
+                <Translation id="TR_PLEASE_CONNECT_YOUR_DEVICE" />
+            </P>
+        </ContentCol>
+        <ImageCol>
+            <Image image="UNI_ERROR" height={70} />
+        </ImageCol>
     </Wrapper>
 );
