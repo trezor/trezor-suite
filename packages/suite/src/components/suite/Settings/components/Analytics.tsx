285b1d6c79e5e9d9fb6c8f0b8c145c180595f7b4
mroz22 <mvarmuza@gmail.com>
2020-04-24 17:24:12 +0200

    extended useAnalytics

--- packages/suite/src/components/suite/Settings/components/Analytics.tsx
+++ packages/suite/src/components/suite/Settings/components/Analytics.tsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { Switch } from '@trezor/components';
 import { Translation } from '@suite-components/Translation';
-
-import { SUITE } from '@suite-actions/constants';
 import { SectionItem, ActionColumn, TextColumn } from '@suite-components/Settings';
-import { AppState, Dispatch } from '@suite-types';
-import * as analyticsActions from '@suite-actions/analyticsActions';
+import { useAnalytics, useDeviceActionLocks } from '@suite-hooks';
 
-const mapStateToProps = (state: AppState) => ({
-    locks: state.suite.locks,
-    enabled: state.analytics.enabled,
-});
-
-const mapDispatchToProps = (dispatch: Dispatch) => ({
-    init: bindActionCreators(analyticsActions.init, dispatch),
-    dispose: bindActionCreators(analyticsActions.dispose, dispatch),
-});
-
-export type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
-
-const Analytics = ({ locks, enabled, init, dispose }: Props) => {
-    const uiLocked = locks.includes(SUITE.LOCK_TYPE.DEVICE) || locks.includes(SUITE.LOCK_TYPE.UI);
-
+const Analytics = () => {
+    const [isEnabled] = useDeviceActionLocks();
+    const { init, dispose, enabled } = useAnalytics();
     return (
         <SectionItem>
             <TextColumn
@@ -41,11 +24,11 @@ const Analytics = ({ locks, enabled, init, dispose }: Props) => {
                         }
                         init();
                     }}
-                    isDisabled={uiLocked}
+                    isDisabled={!isEnabled}
                 />
             </ActionColumn>
         </SectionItem>
     );
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Analytics);
+export default Analytics;
--- packages/suite/src/hooks/suite/useAnalytics.ts
+++ packages/suite/src/hooks/suite/useAnalytics.ts
@@ -1,18 +1,26 @@
-import { useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+import { useMemo } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
 import * as analyticsActions from '@suite-actions/analyticsActions';
+import { AppState } from '@suite-types';
 
 export const useAnalytics = () => {
     const dispatch = useDispatch();
 
-    const report = useCallback(
-        (analyticsPayload: analyticsActions.Payload) => {
-            dispatch(analyticsActions.report(analyticsPayload));
-        },
+    const analytics = useSelector<AppState, AppState['analytics']>(state => state.analytics);
+
+    const actions = useMemo(
+        () => ({
+            report: bindActionCreators(analyticsActions.report, dispatch),
+            init: bindActionCreators(analyticsActions.init, dispatch),
+            dispose: bindActionCreators(analyticsActions.dispose, dispatch),
+        }),
         [dispatch],
     );
 
     return {
-        report,
+        ...analytics,
+        ...actions,
     };
 };
