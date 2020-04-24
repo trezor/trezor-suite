41b5ba4b34158c57e495849485887941a95ef5f6
slowbackspace <slowbackspace@gmail.com>
2020-04-24 19:07:56 +0200

    update wipe modal

--- packages/suite/src/components/suite/modals/WipeDevice/index.tsx
+++ packages/suite/src/components/suite/modals/WipeDevice/index.tsx
@@ -2,19 +2,11 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import { P, H2, Button } from '@trezor/components';
+import { Modal, Button } from '@trezor/components';
 import { Translation, CheckItem, Image } from '@suite-components';
-
 import * as deviceSettingsActions from '@settings-actions/deviceSettingsActions';
-import ModalWrapper from '@suite-components/ModalWrapper';
 import { Dispatch, AppState } from '@suite-types';
 import { SUITE } from '@suite-actions/constants';
-
-const Wrapper = styled(ModalWrapper)`
-    flex-direction: column;
-    max-width: 80vw;
-    min-height: 60vh;
-`;
 
 const Row = styled.div`
     display: flex;
@@ -24,11 +16,6 @@ const Row = styled.div`
 const Col = styled.div`
     display: flex;
     flex-direction: column;
-`;
-
-const Texts = styled.div`
-    max-width: 600px;
-    margin-bottom: 24px;
 `;
 
 const CheckItems = styled(Row)`
@@ -64,15 +51,13 @@ const WipeDevice = ({ locks, wipeDevice, onCancel }: Props) => {
     const uiLocked = locks.includes(SUITE.LOCK_TYPE.DEVICE) || locks.includes(SUITE.LOCK_TYPE.UI);
 
     return (
-        <Wrapper>
-            <Texts>
-                <H2>
-                    <Translation id="TR_WIPE_DEVICE_HEADING" />
-                </H2>
-                <P size="tiny">
-                    <Translation id="TR_WIPE_DEVICE_TEXT" />
-                </P>
-            </Texts>
+        <Modal
+            size="small"
+            cancelable
+            onCancel={onCancel}
+            heading={<Translation id="TR_WIPE_DEVICE_HEADING" />}
+            description={<Translation id="TR_WIPE_DEVICE_TEXT" />}
+        >
             <Image image="UNI_ERROR" />
             <CheckItems>
                 <Col>
@@ -108,7 +93,7 @@ const WipeDevice = ({ locks, wipeDevice, onCancel }: Props) => {
                     </StyledButton>
                 </Col>
             </Buttons>
-        </Wrapper>
+        </Modal>
     );
 };
 
