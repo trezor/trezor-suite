6654ca632157a521454d0a6524ba98cbb6cd0e93
slowbackspace <slowbackspace@gmail.com>
2020-04-24 19:50:45 +0200

    fix confirm action modal

--- packages/suite/src/components/suite/modals/confirm/Action/index.tsx
+++ packages/suite/src/components/suite/modals/confirm/Action/index.tsx
@@ -1,17 +1,8 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Modal } from '@trezor/components';
 import { Translation } from '@suite-components/Translation';
 import DeviceConfirmImage from '@suite-components/images/DeviceConfirmImage';
 import { TrezorDevice } from '@suite-types';
-
-const Wrapper = styled.div`
-    max-width: 360px;
-    flex-direction: column;
-    text-align: center;
-    align-items: center;
-    margin: auto;
-`;
 
 interface Props {
     device: TrezorDevice;
@@ -20,17 +11,16 @@ interface Props {
 const ConfirmAction = ({ device }: Props) => {
     return (
         <Modal
-            useFixedWidth={false}
+            size="tiny"
             heading={
                 <Translation
                     id="TR_CONFIRM_ACTION_ON_YOUR"
                     values={{ deviceLabel: device.label }}
                 />
             }
+            data-test="@suite/modal/confirm-action-on-device"
         >
-            <Wrapper data-test="@suite/modal/confirm-action-on-device">
-                <DeviceConfirmImage device={device} />
-            </Wrapper>
+            <DeviceConfirmImage device={device} />
         </Modal>
     );
 };
