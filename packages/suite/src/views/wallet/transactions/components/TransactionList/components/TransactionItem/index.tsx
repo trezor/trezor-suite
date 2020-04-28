f7dadd06988985291befacd6d33cd06ccf363901
slowbackspace <slowbackspace@gmail.com>
2020-04-28 12:34:56 +0200

    fix n addresses msg

--- packages/suite/src/views/wallet/transactions/components/TransactionList/components/TransactionItem/index.tsx
+++ packages/suite/src/views/wallet/transactions/components/TransactionList/components/TransactionItem/index.tsx
@@ -267,7 +267,10 @@ const TransactionItem = (props: Props) => {
                                     e.stopPropagation();
                                 }}
                             >
-                                {targets.length} <Translation id="TR_ADDRESSES" />
+                                <Translation
+                                    id="TR_N_ADDRESSES"
+                                    values={{ count: targets.length }}
+                                />
                             </ExpandButton>
                         </>
                     )}
