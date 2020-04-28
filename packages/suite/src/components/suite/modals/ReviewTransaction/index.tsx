d7b68f788733a03d0a122a4a4c887473e4774bd9
slowbackspace <slowbackspace@gmail.com>
2020-04-28 11:41:35 +0200

    fix ts

--- packages/suite/src/components/suite/modals/ReviewTransaction/index.tsx
+++ packages/suite/src/components/suite/modals/ReviewTransaction/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { Translation, AccountLabeling } from '@suite-components';
 import { getTransactionInfo } from '@wallet-utils/sendFormUtils';
