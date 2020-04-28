a3a7246fd7b9718fbbeb04bc4f987b97be15c525
slowbackspace <slowbackspace@gmail.com>
2020-04-28 11:52:25 +0200

    update VersionCheck modal

--- packages/suite-desktop/src/support/VersionCheck.tsx
+++ packages/suite-desktop/src/support/VersionCheck.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Loading, ModalWrapper, Translation } from '@suite-components';
-import { Modal as ModalComponent, Button, H2, P, Link, colors } from '@trezor/components';
+import { Loading, Translation } from '@suite-components';
+import { Modal, Button, P, Link, colors } from '@trezor/components';
 import { isDev } from '@suite-utils/build';
 
 interface Props {
@@ -17,7 +17,6 @@ interface State {
 const Content = styled.div`
     display: flex;
     flex-direction: column;
-    max-width: 500px;
 `;
 
 const Span = styled.span`
@@ -39,7 +38,8 @@ const ChangesSummary = styled.div`
 
 const Actions = styled.div`
     display: flex;
-    padding: 20px;
+    width: 100%;
+    padding: 0px 20px;
     flex-direction: column;
     button + button {
         margin-top: 12px;
@@ -96,39 +96,39 @@ export default ({ children }: Props) => {
     // display warning message
     if (state.outdated && !checked) {
         return (
-            <ModalComponent>
-                <ModalWrapper>
-                    <Content>
-                        <H2>
-                            <Translation id="DESKTOP_OUTDATED_TITLE" />
-                        </H2>
-                        <P>
-                            <Translation
-                                id="DESKTOP_OUTDATED_MESSAGE"
-                                values={{
-                                    currentVersion: <Span>{process.env.COMMITHASH}</Span>,
-                                    newVersion: <Span>{state.sha}</Span>,
-                                }}
-                            />
-                        </P>
-                        <ChangesSummary>{state.message}</ChangesSummary>
-                        <Actions>
-                            <Button icon="EXTERNAL_LINK" alignIcon="right">
-                                <Link
-                                    variant="nostyle"
-                                    href="https://gitlab.com/satoshilabs/trezor/trezor-suite/pipelines"
-                                >
-                                    <Translation id="DESKTOP_OUTDATED_BUTTON_DOWNLOAD" />
-                                </Link>
-                            </Button>
+            <Modal
+                size="small"
+                heading={<Translation id="DESKTOP_OUTDATED_TITLE" />}
+                description={
+                    <Translation
+                        id="DESKTOP_OUTDATED_MESSAGE"
+                        values={{
+                            currentVersion: <Span>{process.env.COMMITHASH}</Span>,
+                            newVersion: <Span>{state.sha}</Span>,
+                        }}
+                    />
+                }
+                bottomBar={
+                    <Actions>
+                        <Button icon="EXTERNAL_LINK" alignIcon="right">
+                            <Link
+                                variant="nostyle"
+                                href="https://gitlab.com/satoshilabs/trezor/trezor-suite/pipelines"
+                            >
+                                <Translation id="DESKTOP_OUTDATED_BUTTON_DOWNLOAD" />
+                            </Link>
+                        </Button>
 
-                            <Button onClick={() => setChecked(true)} variant="secondary">
-                                <Translation id="DESKTOP_OUTDATED_BUTTON_CANCEL" />
-                            </Button>
-                        </Actions>
-                    </Content>
-                </ModalWrapper>
-            </ModalComponent>
+                        <Button onClick={() => setChecked(true)} variant="secondary">
+                            <Translation id="DESKTOP_OUTDATED_BUTTON_CANCEL" />
+                        </Button>
+                    </Actions>
+                }
+            >
+                <Content>
+                    <ChangesSummary>{state.message}</ChangesSummary>
+                </Content>
+            </Modal>
         );
     }
 
