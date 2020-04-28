04c4c99301b4df4d17680f6728682f8503f0f647
slowbackspace <slowbackspace@gmail.com>
2020-04-28 08:23:56 +0200

    remove unused test

--- packages/integration-tests/projects/components-storybook/tests/Modal.test.js
+++ packages/integration-tests/projects/components-storybook/tests/Modal.test.js
@@ -2,21 +2,6 @@ describe('Modal', () => {
     it('modal', () => {
         cy.viewport(1024, 500);
         cy.loadContent('iframe.html?id=modals--default');
-        cy.getTestElement('modal')
-            .should('be.visible')
-            .matchImageSnapshot();
-    });
-
-    it('modal-with-cancel', () => {
-        cy.viewport(1024, 500);
-        cy.loadContent('/iframe.html?id=modals--padding-with-cancel');
-        cy.getTestElement('modal-with-cancel')
-            .find('svg')
-            .each(el => {
-                cy.get(el).should('be.visible');
-            });
-        cy.getTestElement('modal-with-cancel')
-            .should('be.visible')
-            .matchImageSnapshot();
+        cy.getTestElement('modal').should('be.visible').matchImageSnapshot();
     });
 });
