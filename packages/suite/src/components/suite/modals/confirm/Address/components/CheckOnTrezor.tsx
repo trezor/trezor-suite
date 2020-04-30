1b5e128e1ca5600f11f36c2c946ecd9492657a16
slowbackspace <slowbackspace@gmail.com>
2020-04-30 10:47:45 +0200

    make receive modal usable on lower height screens

--- packages/suite/src/components/suite/modals/confirm/Address/components/CheckOnTrezor.tsx
+++ packages/suite/src/components/suite/modals/confirm/Address/components/CheckOnTrezor.tsx
@@ -9,8 +9,7 @@ const Wrapper = styled.div`
     flex-direction: column;
     align-items: start;
     padding: 16px;
-    margin: 0px 32px;
-    margin-bottom: 40px;
+    margin: 0px 32px 32px 32px;
     text-align: left;
     background: ${colors.BLACK96};
     position: relative;
