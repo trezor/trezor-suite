bd495499f043872b901ae59691944baa67eed940
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-05-07 17:05:31 +0200

    Add landing page skeleton (#1713)
    * add landing page skeleton
    
    * add eslint

--- packages/suite-data/config.ts
+++ packages/suite-data/config.ts
@@ -2,4 +2,5 @@ export default {
     web: ['fonts', 'images', 'videos', 'translations', 'browser-detection'],
     desktop: ['fonts', 'images', 'videos', 'translations', 'connect', 'bridge'],
     native: ['fonts', 'images', 'videos'],
+    landing: ['fonts'],
 };
