5c555e0ec4fe18e230f6d87322513ec0cdeda435
mroz22 <mvarmuza@gmail.com>
2020-05-07 09:06:29 +0200

    wip

--- packages/suite/src/views/onboarding/unexpected-states/components/IsSameDevice.tsx
+++ packages/suite/src/views/onboarding/unexpected-states/components/IsSameDevice.tsx
@@ -1,11 +1,17 @@
-import { OnboardingButton, Text, Wrapper } from '@onboarding-components';
-import { Translation } from '@suite-components';
-import { Dispatch } from '@suite-types';
-import { resetOnboarding } from '@onboarding-actions/onboardingActions';
 
 import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import styled from 'styled-components';
+
+import { OnboardingButton, Text, Wrapper } from '@onboarding-components';
+import { Translation, Image } from '@suite-components';
+import { Dispatch } from '@suite-types';
+import { resetOnboarding } from '@onboarding-actions/onboardingActions';
+
+const StyledImage = styled(Image)`
+    flex: 1;
+`;
 
 const mapDispatchToProps = (dispatch: Dispatch) => ({
     actionAlt: bindActionCreators(resetOnboarding, dispatch),
@@ -15,6 +21,7 @@ type Props = ReturnType<typeof mapDispatchToProps>;
 
 const IsSameDevice = ({ actionAlt }: Props) => (
     <>
+        <StyledImage image="UNI_WARNING" />
         <Text>
             <Translation id="TR_DEVICE_YOU_RECONNECTED_IS_DIFFERENT" />
         </Text>
