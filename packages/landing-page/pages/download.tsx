75680f49b4163677152dca16fef7d73112523da7
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-05-07 19:34:30 +0200

    style download page

---
+++ packages/landing-page/pages/download.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import styled from 'styled-components';
+import Layout from '../components/Layout';
+import { H2, Button, P, Select } from '@trezor/components';
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+const Item = styled.div``;
+const Row = styled.div`
+    margin: 30px 0 0 0;
+    display: flex;
+`;
+
+const ButtonContinue = styled(Button)`
+    margin: 60px 0 0 0;
+`;
+
+const ButtonDownload = styled(Button)`
+    margin: 30px 0 0 20px;
+`;
+
+const Index = () => (
+    <Layout>
+        <Wrapper>
+            <H2>Download Trezor Suite (beta) desktop app</H2>
+            <P size="tiny">
+                For testing purpouses only. Please keep in mind this is a beta version.
+            </P>
+            <Row>
+                <Select
+                    variant="small"
+                    topLabel="Choose your platform"
+                    width={240}
+                    options={{ label: 'a', value: 'a' }}
+                />
+                <Item>
+                    <ButtonDownload variant="primary">Download</ButtonDownload>
+                </Item>
+            </Row>
+            <Item>
+                <ButtonContinue variant="tertiary" icon="ARROW_RIGHT" alignIcon="right">
+                    Continue in browser
+                </ButtonContinue>
+            </Item>
+        </Wrapper>
+    </Layout>
+);
+
+export default Index;
