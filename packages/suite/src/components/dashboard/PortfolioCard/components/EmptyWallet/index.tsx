ab7587d6b038190b98d39811a6f8556be298676f
slowbackspace <slowbackspace@gmail.com>
2020-05-11 21:06:59 +0200

    fix ts

--- packages/suite/src/components/dashboard/PortfolioCard/components/EmptyWallet/index.tsx
+++ packages/suite/src/components/dashboard/PortfolioCard/components/EmptyWallet/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { colors, Button, variables } from '@trezor/components';
+import { colors, variables } from '@trezor/components';
 import { Translation, Image } from '@suite-components';
 
 const Wrapper = styled.div`
