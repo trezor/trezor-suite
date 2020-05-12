21eee15f7ff008a8f25f265265189c523797ff8b
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-05-12 12:56:59 +0200

    Calendar Versioning (#1748)
    * prepare release version, add settings link, rename desktop apps
    
    * propagate version name
    
    * fix paths
    
    * fix paths more
    
    * add wildcard
    
    * fix version name
    
    * fix version
    
    * fix version in root
    
    * edit app name

--- packages/suite/src/support/messages.ts
+++ packages/suite/src/support/messages.ts
@@ -2071,7 +2071,7 @@ const definedMessages = defineMessages({
         id: 'TR_YOUR_CURRENT_FIRMWARE',
     },
     TR_YOUR_CURRENT_VERSION: {
-        defaultMessage: 'Your current Suite version is "{version}"',
+        defaultMessage: 'Your current Suite version is',
         id: 'TR_YOUR_CURRENT_VERSION',
     },
     TR_YOUR_TREZOR_IS_NOT_BACKED_UP: {
