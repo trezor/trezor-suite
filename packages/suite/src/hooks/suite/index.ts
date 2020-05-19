5bd28dddf64fe45ce76b21345b5f397a4d36de9d
slowbackspace <slowbackspace@gmail.com>
2020-05-19 19:57:12 +0200

    move typed useSelector to hooks/suite/index

--- packages/suite/src/hooks/suite/index.ts
+++ packages/suite/src/hooks/suite/index.ts
@@ -1,5 +1,14 @@
+import { AppState } from '@suite-types';
+import { useSelector as useReduxSelector, TypedUseSelectorHook } from 'react-redux';
+
 export { useDevice } from './useDevice';
 export { useDeviceActionLocks } from './useDeviceActionLocks';
 export { useDiscovery } from './useDiscovery';
 export { useScrollRef } from './useScrollRef';
 export { useAnalytics } from './useAnalytics';
+
+/**
+ * Properly typed useSelector hook, use this one instead of directly importing it from react-redux.
+ * https://react-redux.js.org/using-react-redux/static-typing#typing-the-useselector-hook
+ */
+export const useSelector: TypedUseSelectorHook<AppState> = useReduxSelector;
