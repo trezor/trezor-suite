b815d04128acb554affdbe8c676f87f7cee3a75b
slowbackspace <slowbackspace@gmail.com>
2020-05-20 20:40:48 +0200

    fix missing intl in graph tooltip

--- packages/suite/src/support/messages.ts
+++ packages/suite/src/support/messages.ts
@@ -3412,6 +3412,14 @@ const definedMessages = defineMessages({
         defaultMessage:
             'Try a different date range.{newLine}Still nothing? Go ahead and send a transaction.',
     },
+    TR_RECEIVED_AMOUNT: {
+        id: 'TR_RECEIVED_AMOUNT',
+        defaultMessage: 'Received {amount}',
+    },
+    TR_SENT_AMOUNT: {
+        id: 'TR_SENT_AMOUNT',
+        defaultMessage: 'Sent {amount}',
+    },
 } as const);
 
 export default definedMessages;
