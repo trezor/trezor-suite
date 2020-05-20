3fd41f659e6af88331c53abb86da7dd98033fdc1
Szymon Lesisz <sz.lesisz@gmail.com>
2020-05-20 19:55:44 +0200

    ETH serialization tests with real values

--- packages/suite/src/utils/wallet/__tests__/sendFormUtils.test.ts
+++ packages/suite/src/utils/wallet/__tests__/sendFormUtils.test.ts
@@ -1,3 +1,5 @@
+import { FakeTransaction } from 'ethereumjs-tx';
+import { sha3 } from 'web3-utils';
 import * as fixtures from '../__fixtures__/sendFormFixtures';
 import {
     getOutput,
@@ -5,6 +7,7 @@ import {
     shouldComposeBy,
     getInputState,
     prepareEthereumTransaction,
+    serializeEthereumTx,
 } from '../sendFormUtils';
 
 const { OUTPUTS } = fixtures;
@@ -88,6 +91,21 @@ describe('sendForm utils', () => {
     fixtures.prepareEthereumTransaction.forEach(f => {
         it(`prepareEthereumTransaction: ${f.description}`, () => {
             expect(prepareEthereumTransaction(f.txInfo)).toEqual(f.result);
+        });
+    });
+
+    fixtures.serializeEthereumTx.forEach(f => {
+        it(`serializeEthereumTx: ${f.description}`, () => {
+            const serialized = serializeEthereumTx(f.tx);
+            // verify hash using 2 different tools
+            if (f.tx.chainId !== 61) {
+                // ETC is not supported
+                const tx = new FakeTransaction(serialized);
+                const hash1 = tx.hash().toString('hex');
+                expect(`0x${hash1}`).toEqual(f.result);
+            }
+            const hash2 = sha3(serialized);
+            expect(hash2).toEqual(f.result);
         });
     });
 });
