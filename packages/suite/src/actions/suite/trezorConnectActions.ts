6ecd5fbdef54a96c8813fd0096151a9e34672990
slowbackspace <slowbackspace@gmail.com>
2020-05-21 13:25:16 +0200

    set dev blockbooks url. REVERT BEFORE MERGING

--- packages/suite/src/actions/suite/trezorConnectActions.ts
+++ packages/suite/src/actions/suite/trezorConnectActions.ts
@@ -82,6 +82,34 @@ export const init = () => async (dispatch: Dispatch, getState: GetState) => {
                 appUrl: '@trezor/suite',
             },
         });
+        TrezorConnect.blockchainSetCustomBackend({
+            coin: 'btc',
+            blockchainLink: {
+                type: 'blockbook',
+                url: ['https://blockbook-dev.corp.sldev.cz:9130'],
+            },
+        });
+        TrezorConnect.blockchainSetCustomBackend({
+            coin: 'eth',
+            blockchainLink: {
+                type: 'blockbook',
+                url: ['https://blockbook-dev.corp.sldev.cz:9136'],
+            },
+        });
+        TrezorConnect.blockchainSetCustomBackend({
+            coin: 'trop',
+            blockchainLink: {
+                type: 'blockbook',
+                url: ['https://blockbook-dev.corp.sldev.cz:19136'],
+            },
+        });
+        TrezorConnect.blockchainSetCustomBackend({
+            coin: 'test',
+            blockchainLink: {
+                type: 'blockbook',
+                url: ['https://blockbook-dev.corp.sldev.cz:19130'],
+            },
+        });
         dispatch({
             type: SUITE.CONNECT_INITIALIZED,
         });
