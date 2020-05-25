46b243b5cef83d550df1800975c8bfd417ca88ef
Szymon Lesisz <sz.lesisz@gmail.com>
2020-05-25 11:46:51 +0200

    remove unused import in useActions hook

--- packages/suite/src/hooks/suite/useActions.ts
+++ packages/suite/src/hooks/suite/useActions.ts
@@ -1,7 +1,7 @@
 import { useMemo } from 'react';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators, ActionCreatorsMapObject } from 'redux';
-import { Action, Dispatch } from '@suite-types';
+import { Dispatch } from '@suite-types';
 
 export const useActions = <M extends ActionCreatorsMapObject<any>>(actions: M) => {
     const dispatch = useDispatch<Dispatch>();
