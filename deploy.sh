67d2810d934973f3d19b61c41842ffc18fb14a06
mroz22 <mvarmuza@gmail.com>
2020-05-27 09:49:33 +0200

    experiment

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
 
-# suite-desktop linux build
-# https://gitlab.com/satoshilabs/trezor/trezor-suite/-/jobs/artifacts/develop/download?job=suite-desktop%20build%20linux
+rm -rf combined-build
+mkdir combined-build
+wget -P ./combined-build "https://gitlab.com/satoshilabs/trezor/trezor-suite/-/jobs/artifacts/develop/download?job=suite-desktop%20build%20linux"
+
+ls -la combined-build
