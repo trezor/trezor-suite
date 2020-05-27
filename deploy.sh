2f8b3f22d7781e6e86fdbdcc1721d5c29b35024c
mroz22 <mvarmuza@gmail.com>
2020-05-27 10:03:56 +0200

    wexperimitns

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+
 rm -rf combined-build
 mkdir combined-build
-wget -P ./combined-build "https://gitlab.com/satoshilabs/trezor/trezor-suite/-/jobs/artifacts/develop/download?job=suite-desktop%20build%20linux"
+wget "https://gitlab.com/satoshilabs/trezor/trezor-suite/-/jobs/artifacts/develop/download?job=suite-desktop%20build%20linux" -O ./combined-build/desktop-linux.zip
+unzip ./combined-build/desktop-linux.zip -d ./combined-build
+rm ./combined-build/desktop-linux.zip
+
 
 ls -la combined-build
