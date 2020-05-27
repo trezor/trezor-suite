8a52396deb9a0072dad97ce2cb0904d27c74ac34
slowbackspace <slowbackspace@gmail.com>
2020-05-27 11:00:03 +0200

    test

--- packages/suite-data/files/browser-detection/index.js
+++ packages/suite-data/files/browser-detection/index.js
@@ -84,3 +84,36 @@ window.onload = function() {
         setBody(unsupportedBrowser);
     }
 };
+
+// https://github.com/facebook/react/issues/11538
+if (typeof Node === 'function' && Node.prototype) {
+    const originalRemoveChild = Node.prototype.removeChild
+    Node.prototype.removeChild = function (child) {
+      if (child.parentNode !== this) {
+        if (console) {
+          console.error(
+            'Cannot remove a child from a different parent',
+            child,
+            this
+          )
+        }
+        return child
+      }
+      return originalRemoveChild.apply(this, arguments)
+    }
+  
+    const originalInsertBefore = Node.prototype.insertBefore
+    Node.prototype.insertBefore = function (newNode, referenceNode) {
+      if (referenceNode && referenceNode.parentNode !== this) {
+        if (console) {
+          console.error(
+            'Cannot insert before a reference node from a different parent',
+            referenceNode,
+            this
+          )
+        }
+        return newNode
+      }
+      return originalInsertBefore.apply(this, arguments)
+    }
+  }
