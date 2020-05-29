2841fa3193d89cefc2bc0e3f394fcbfd66580929
slowbackspace <slowbackspace@gmail.com>
2020-05-29 12:14:22 +0200

    formatCoinBalance: fix negative arg in toFixed()

--- packages/suite/src/utils/wallet/__tests__/balanceUtils.test.ts
+++ packages/suite/src/utils/wallet/__tests__/balanceUtils.test.ts
@@ -22,4 +22,5 @@ test('formatBalanceUtils', () => {
     expect(formatCoinBalance('2900000')).toEqual('2900000');
     expect(formatCoinBalance('100000000')).toEqual('100m');
     expect(formatCoinBalance('100000001')).toEqual('100m+');
+    expect(formatCoinBalance('0099999999.999999999999')).toEqual('99999999');
 });
--- packages/suite/src/utils/wallet/balanceUtils.ts
+++ packages/suite/src/utils/wallet/balanceUtils.ts
@@ -9,12 +9,13 @@ export const formatCoinBalance = (value: string) => {
     if (balanceBig.isEqualTo(MAX_CRYPTO_VALUE)) return '100m';
     if (balanceBig.isGreaterThan(MAX_CRYPTO_VALUE)) return '100m+';
 
-    const parts = value.split('.');
+    // instead of splitting on input `value`, do it on "normalized" BN string (it strips leading zeros)
+    const parts = balanceBig.toFixed().split('.');
     const hasDecimals = parts.length > 1;
 
     if (hasDecimals) {
         const firstPart = parts[0].length || 1;
-        const fixCount = MAX_NUMBERS - firstPart;
+        const fixCount = Math.max(MAX_NUMBERS - firstPart, 0); // don't go lower than 0
         // fix to max visible numbers with decimals
         const fixedBalance = balanceBig.toFixed(fixCount, 1);
         const fixedBalanceBig = new BigNumber(fixedBalance);
