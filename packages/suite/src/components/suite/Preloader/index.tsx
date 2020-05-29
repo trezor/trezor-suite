93172fc2dd89b0c1a9cc32cc810c70378545a937
slowbackspace <slowbackspace@gmail.com>
2020-05-29 12:18:09 +0200

    fix ts

--- packages/suite/src/components/suite/Preloader/index.tsx
+++ packages/suite/src/components/suite/Preloader/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import FocusLock from 'react-focus-lock';
