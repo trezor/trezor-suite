dc537856dd2c4207e4a6f477e192b6eabfb55002
slowbackspace <slowbackspace@gmail.com>
2020-06-01 14:37:16 +0200

    fix multiline wrapping for accounts labels in translator debug mode

--- packages/suite/src/components/suite/Translation/index.tsx
+++ packages/suite/src/components/suite/Translation/index.tsx
@@ -4,7 +4,7 @@ import { ExtendedMessageDescriptor } from '@suite-types';
 import HelperTooltip from './components/HelperTooltip';
 import messages from '@suite/support/messages';
 
-type FormattedMessageProps = { tagName?: React.ElementType<any> };
+type FormattedMessageProps = { isNested?: boolean };
 type MsgType = FormattedMessageProps & ExtendedMessageDescriptor;
 
 export const isMsgType = (props: MsgType | React.ReactNode): props is MsgType => {
@@ -20,19 +20,15 @@ const Translation = (props: MsgType) => {
         // Iterates through all values. The entry may also contain a MessageDescriptor.
         // If so, Renders MessageDescriptor by passing it to `Translation` component
         const maybeMsg = props.values![key];
-        values[key] = isMsgType(maybeMsg) ? (
-            <Translation {...maybeMsg} tagName={undefined} />
-        ) : (
-            maybeMsg
-        );
+        values[key] = isMsgType(maybeMsg) ? <Translation {...maybeMsg} isNested /> : maybeMsg;
     });
 
     // pass undefined to a 'values' prop in case of an empty values object
     return (
-        <HelperTooltip messageId={props.id}>
+        <HelperTooltip isNested={props.isNested} messageId={props.id}>
             <FormattedMessage
                 id={props.id}
-                tagName="span"
+                tagName={props.isNested ? undefined : 'span'}
                 {...messages[props.id]}
                 {...props}
                 values={Object.keys(values).length === 0 ? undefined : values}
