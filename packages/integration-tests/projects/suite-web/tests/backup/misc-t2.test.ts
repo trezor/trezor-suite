9bcbfc767dd39cae00b69fd95955c186af4210a7
mroz22 <mvarmuza@gmail.com>
2020-06-02 23:28:26 +0200

    split backup

--- packages/integration-tests/projects/suite-web/tests/backup/misc-t2.test.ts
+++ packages/integration-tests/projects/suite-web/tests/backup/misc-t2.test.ts
@@ -11,38 +11,6 @@ describe('Backup', () => {
         cy.passThroughInitialRun();
     });
 
-    it('Successful backup happy path', () => {
-        // access from notification
-        cy.getTestElement('@notification/no-backup/button').click();
-
-        cy.getTestElement('@backup').matchImageSnapshot('backup')
-        
-        cy.getTestElement('@backup/check-item/understands-what-seed-is').click();
-        cy.getTestElement('@backup/check-item/has-enough-time').click();
-        cy.getTestElement('@backup/check-item/is-in-private').click();
-
-        cy.log('Create backup on device');
-        cy.getTestElement('@backup/start-button').click();
-        cy.getConfirmActionOnDeviceModal();
-        
-        cy.task('sendDecision');
-        cy.task('swipeEmu', 'up');
-        cy.task('swipeEmu', 'up');
-        cy.task('sendDecision');
-        cy.task('inputEmu', 'all');
-        cy.task('inputEmu', 'all');
-        cy.task('inputEmu', 'all');
-        cy.task('sendDecision');
-        cy.task('sendDecision');
-
-        cy.log('click all after checkboxes and close backup modal');
-        cy.getTestElement('@backup/continue-to-pin-button').should('be.disabled');
-        cy.getTestElement('@backup/check-item/wrote-seed-properly').click();
-        cy.getTestElement('@backup/check-item/made-no-digital-copy').click();
-        cy.getTestElement('@backup/check-item/will-hide-seed').click();
-        cy.getTestElement('@backup/continue-to-pin-button').should('not.be.disabled');
-    });
-
     it('Backup failed', () => {
         cy.getTestElement('@notification/no-backup/button').click();
         cy.getTestElement('@backup/check-item/understands-what-seed-is').click();
