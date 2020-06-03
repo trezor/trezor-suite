0d37b5da4dad1e5448b4af070d0e6789b583dda5
slowbackspace <slowbackspace@gmail.com>
2020-06-03 17:39:59 +0200

    Revert "enable experimental nextjs source maps"
    This reverts commit a182b06753d5f35acf74470a9094b7950dad5c8c.

--- packages/suite-web/next.config.js
+++ packages/suite-web/next.config.js
@@ -6,6 +6,7 @@ const withCustomBabelConfig = require('next-plugin-custom-babel-config');
 const withTranspileModules = require('next-transpile-modules');
 const withImages = require('next-images');
 const withWorkers = require('@zeit/next-workers');
+const withSourceMaps = require('@zeit/next-source-maps');
 
 const GitRevisionPlugin = require('git-revision-webpack-plugin');
 const webpack = require('webpack');
@@ -14,44 +15,45 @@ const packageJson = require('./package.json');
 const gitRevisionPlugin = new GitRevisionPlugin();
 module.exports = withBundleAnalyzer(
     withCustomBabelConfig(
-        withTranspileModules(
-            withImages(
-                withWorkers({
-                    typescript: {
-                        ignoreDevErrors: true,
-                    },
-                    inlineImageLimit: 0,
-                    babelConfigFile: path.resolve('babel.config.js'),
-                    // https://github.com/zeit/next.js/issues/6219
-                    // target: 'serverless',
-                    transpileModules: [
-                        '@trezor',
-                        '../packages/suite/src', // issue: https://github.com/zeit/next.js/issues/5666
-                    ],
-                    exportTrailingSlash: true,
-                    assetPrefix: process.env.assetPrefix || '',
-                    workerLoaderOptions: {
-                        name: 'static/[hash].worker.js',
-                        publicPath: '/_next/',
-                    },
-                    experimental: {
-                        productionBrowserSourceMaps: true,
-                    },
-                    webpack: (config, options) => {
-                        config.plugins.push(
-                            new webpack.DefinePlugin({
-                                'process.env.SUITE_TYPE': JSON.stringify('web'),
-                                'process.env.VERSION': JSON.stringify(packageJson.version),
-                                'process.env.assetPrefix': JSON.stringify(process.env.assetPrefix),
-                                'process.env.COMMITHASH': JSON.stringify(
-                                    gitRevisionPlugin.commithash(),
-                                ),
-                            }),
-                        );
+        withSourceMaps(
+            withTranspileModules(
+                withImages(
+                    withWorkers({
+                        typescript: {
+                            ignoreDevErrors: true,
+                        },
+                        inlineImageLimit: 0,
+                        babelConfigFile: path.resolve('babel.config.js'),
+                        // https://github.com/zeit/next.js/issues/6219
+                        // target: 'serverless',
+                        transpileModules: [
+                            '@trezor',
+                            '../packages/suite/src', // issue: https://github.com/zeit/next.js/issues/5666
+                        ],
+                        exportTrailingSlash: true,
+                        assetPrefix: process.env.assetPrefix || '',
+                        workerLoaderOptions: {
+                            name: 'static/[hash].worker.js',
+                            publicPath: '/_next/',
+                        },
+                        webpack: (config, options) => {
+                            config.plugins.push(
+                                new webpack.DefinePlugin({
+                                    'process.env.SUITE_TYPE': JSON.stringify('web'),
+                                    'process.env.VERSION': JSON.stringify(packageJson.version),
+                                    'process.env.assetPrefix': JSON.stringify(
+                                        process.env.assetPrefix,
+                                    ),
+                                    'process.env.COMMITHASH': JSON.stringify(
+                                        gitRevisionPlugin.commithash(),
+                                    ),
+                                }),
+                            );
 
-                        return config;
-                    },
-                }),
+                            return config;
+                        },
+                    }),
+                ),
             ),
         ),
     ),
