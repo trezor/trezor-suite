5de2e34e45e436e588e366600b7bcbeb5b10b454
slowbackspace <slowbackspace@gmail.com>
2020-06-05 16:04:08 +0200

    remove debug option

--- packages/suite/src/config/suite/sentry.ts
+++ packages/suite/src/config/suite/sentry.ts
@@ -2,7 +2,6 @@ import { CaptureConsole } from '@sentry/integrations';
 
 export default {
     dsn: 'https://6d91ca6e6a5d4de7b47989455858b5f6@o117836.ingest.sentry.io/5193825',
-    debug: true,
     normalizeDepth: 10,
     integrations: [
         new CaptureConsole({
