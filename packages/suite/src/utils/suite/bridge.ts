77b0757181ab62f122feb38e224625b3f9487033
mroz22 <mvarmuza@gmail.com>
2020-06-08 12:14:54 +0200

    one last fix

--- packages/suite/src/utils/suite/bridge.ts
+++ packages/suite/src/utils/suite/bridge.ts
@@ -1,5 +1,6 @@
 export const getLinuxPackage = () => {
-    if (typeof navigator === 'undefined' || !navigator.appVersion.includes('Linux')) return;
+    if (typeof navigator === 'undefined') return;
+    if (!navigator.appVersion.includes('Linux') || navigator.appVersion.includes('Android')) return;
     return navigator.appVersion.match(/CentOS|Fedora|Mandriva|Mageia|Red Hat|Scientific|SUSE/)
         ? 'rpm'
         : 'deb';
