352857089b2109db61deed4ea8ad79af7b0a2030
slowbackspace <slowbackspace@gmail.com>
2020-06-22 14:46:23 +0200

    revert changes in wrong component

--- packages/suite/src/components/suite/modals/AddAccount/components/AddAccountButton.tsx
+++ packages/suite/src/components/suite/modals/AddAccount/components/AddAccountButton.tsx
@@ -53,7 +53,7 @@ export default (props: Props) => {
 
     if (tooltip) {
         return (
-            <Tooltip placement="bottom" content={tooltip}>
+            <Tooltip maxWidth={285} placement="top" content={tooltip}>
                 {addButton}
             </Tooltip>
         );
