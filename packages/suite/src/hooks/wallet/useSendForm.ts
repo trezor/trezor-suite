760796723051022c10f5112b2679ccd7e4e31e7e
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-07-01 12:00:25 +0200

    prepare reducer and dispatch

--- packages/suite/src/hooks/wallet/useSendForm.ts
+++ packages/suite/src/hooks/wallet/useSendForm.ts
@@ -1,5 +1,7 @@
 import { createContext, useContext, useCallback, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { useFormContext } from 'react-hook-form';
+// import { SEND } from '@wallet-actions/constants';
 import { useActions } from '@suite-hooks';
 import * as sendFormActions from '@wallet-actions/sendFormActions';
 import { FormState, ContextStateValues, ContextState } from '@wallet-types/sendForm';
@@ -37,26 +39,37 @@ export const useSendForm = (defaultValues: ContextStateValues) => {
 // Used across send form components
 // Provide combined FormState from `react-hook-form` and ContextState from `SendContext`
 export const useSendFormContext = () => {
+    const dispatch = useDispatch();
     const sendContext = useContext(SendContext) as ContextState;
     const formContext = useFormContext<FormState>();
-    const { saveDraft } = useActions({ saveDraft: sendFormActions.saveDraft });
+    const { saveDraft, composeTransaction, removeDraft } = useActions({
+        removeDraft: sendFormActions.removeDraft,
+        saveDraft: sendFormActions.saveDraft,
+        composeTransaction: sendFormActions.composeTransactionNew,
+    });
 
     const updateDraft = useCallback(() => {
         saveDraft(formContext.getValues({ nest: true }), sendContext);
     }, [sendContext, formContext, saveDraft]);
 
-    const { composeTransaction } = useActions({
-        composeTransaction: sendFormActions.composeTransactionNew,
-    });
+    const clearDraft = useCallback(() => {
+        removeDraft();
+    }, [removeDraft]);
 
     const compose = useCallback(async () => {
-        const result = await composeTransaction(sendContext, formContext.getValues());
-    }, [sendContext, formContext, composeTransaction]);
+        const result = await composeTransaction(sendContext, formContext.getValues({ nest: true }));
+        console.log(result);
+        if (result) {
+            // save precomposed tx to reducer
+            // dispatch({ type: SEND.SAVE_PRECOMPOSED_TX, precomposedTx: result });
+        }
+    }, [sendContext, formContext, composeTransaction, dispatch]);
 
     return {
         sendContext,
         formContext,
         updateDraft,
+        clearDraft,
         composeTransaction: compose,
     };
 };
