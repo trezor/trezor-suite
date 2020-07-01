9ed5934e7641296ff6a59630b16d8f60a90b9576
Szymon Lesisz <sz.lesisz@gmail.com>
2020-07-01 15:51:42 +0200

    Update useActions.ts

--- packages/suite/src/hooks/suite/useActions.ts
+++ packages/suite/src/hooks/suite/useActions.ts
@@ -1,11 +1,12 @@
-import { useMemo } from 'react';
+import { useMemo, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators, ActionCreatorsMapObject } from 'redux';
 import { Dispatch } from '@suite-types';
 
 export const useActions = <M extends ActionCreatorsMapObject<any>>(actions: M) => {
     const dispatch = useDispatch<Dispatch>();
+    const ref = useRef(actions);
     return useMemo(() => {
-        return bindActionCreators(actions, dispatch);
-    }, [dispatch, actions]);
+        return bindActionCreators(ref.current, dispatch);
+    }, [dispatch, ref]);
 };
