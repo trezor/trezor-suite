a9fd189948a95f139edfedf6da2a2d7a04373f58
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-07-02 11:05:24 +0200

    add select bar

---
+++ packages/components/src/components/form/SelectBar/index.stories.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import SelectBar from './index';
+import { storiesOf } from '@storybook/react';
+import { StoryColumn } from '../../../support/Story';
+
+storiesOf('Form', module).add(
+    'Select Bar',
+    () => {
+        const options = [
+            { label: 'low', value: 'low' },
+            { label: 'medium', value: 'medium' },
+            { label: 'high', value: 'high' },
+            { label: 'custom', value: 'custom' },
+        ];
+
+        return (
+            <SelectBar
+                label="fee"
+                selectedOption={{ label: 'low', value: 'low' }}
+                options={options}
+            />
+        );
+    },
+    {
+        options: {
+            showPanel: false,
+        },
+    }
+);
---
+++ packages/components/src/components/form/SelectBar/index.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import { colors, variables } from '../../../config';
+
+interface Option {
+    label: string;
+    value: string;
+}
+
+interface Props {
+    label?: string;
+    selectedOption?: Option;
+    options: Option[];
+}
+
+const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const Options = styled.div`
+    display: flex;
+    padding: 2px;
+    border-radius: 4px;
+    background: ${colors.NEUE_BG_GRAY};
+`;
+
+const Label = styled.div`
+    padding-right: 20px;
+    text-transform: capitalize;
+    font-size: ${variables.FONT_SIZE.NORMAL};
+    color: ${colors.NEUE_TYPE_DARK_GREY};
+`;
+
+const Option = styled.div<{ isSelected: boolean }>`
+    padding: 0 14px;
+    border-radius: 4px;
+    font-size: ${variables.FONT_SIZE.SMALL};
+    text-transform: capitalize;
+    color: ${colors.NEUE_TYPE_LIGHT_GREY};
+    cursor: pointer;
+
+    ${props =>
+        props.isSelected &&
+        css`
+            background: white;
+        `}
+`;
+
+export default ({ options, selectedOption, label }: Props) => {
+    const [selectedOptionIn, setSelected] = useState<Option | null>(selectedOption || null);
+
+    return (
+        <Wrapper>
+            {label && <Label>{label}</Label>}
+            <Options>
+                {options.map(option => (
+                    <Option
+                        onClick={() => setSelected(option)}
+                        isSelected={
+                            selectedOptionIn ? selectedOptionIn.value === option.value : false
+                        }
+                        key={option.value}
+                    >
+                        {option.label}
+                    </Option>
+                ))}
+            </Options>
+        </Wrapper>
+    );
+};
