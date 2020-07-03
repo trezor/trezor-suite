17ec86eae75f7ac91759c255bbe92e339cf56704
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-07-03 10:42:11 +0200

    add missing param

--- packages/suite/src/views/wallet/send/components/AdvancedForm/components/Fee/index.tsx
+++ packages/suite/src/views/wallet/send/components/AdvancedForm/components/Fee/index.tsx
@@ -190,7 +190,7 @@ export default () => {
                     variant="small"
                     onChange={([selected]) => {
                         // preserve fee level for next transaction
-                        setLastUsedFeeLevel(selected.label);
+                        setLastUsedFeeLevel(selected.label, symbol);
                         return { ...selected };
                     }}
                     options={feeInfo.levels}
