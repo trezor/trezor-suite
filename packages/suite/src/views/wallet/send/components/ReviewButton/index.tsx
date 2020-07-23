8956ead220d7593c3da2e59fdaf3a2810411e957
Szymon Lesisz <sz.lesisz@gmail.com>
2020-07-23 09:43:22 +0200

    cleanup in ReviewButton

--- packages/suite/src/views/wallet/send/components/ReviewButton/index.tsx
+++ packages/suite/src/views/wallet/send/components/ReviewButton/index.tsx
@@ -1,12 +1,9 @@
-import { SUITE } from '@suite-actions/constants';
-import { Translation } from '@suite-components/Translation';
-import { useActions } from '@suite-hooks';
-import { AppState, TrezorDevice } from '@suite-types';
-import { useSendFormContext } from '@wallet-hooks';
-import { Button, colors } from '@trezor/components';
 import React from 'react';
 import styled from 'styled-components';
-import { SendContextState } from '@wallet-types/sendForm';
+import { Button, colors } from '@trezor/components';
+import { useDevice } from '@suite-hooks';
+import { useSendFormContext } from '@wallet-hooks';
+import { Translation } from '@suite-components/Translation';
 
 const Wrapper = styled.div`
     display: flex;
@@ -32,72 +29,13 @@ const Row = styled.div`
     }
 `;
 
-const isDisabled = (
-    errors: SendContextState['errors'],
-    locks: AppState['suite']['locks'],
-    device: TrezorDevice,
-    online: AppState['suite']['online'],
-    outputs: SendContextState['outputs'],
-    getValues: SendContextState['getValues'],
-) => {
-    // any form error
-    if (Object.entries(errors).length > 0) {
-        return true;
-    }
-
-    let filledAddress = 0;
-    let filledAmounts = 0;
-
-    outputs.forEach(output => {
-        const { address, amount } = output;
-        if (address && address.length > 0) {
-            filledAddress++;
-        }
-        if (amount && amount.length > 0) {
-            filledAmounts++;
-        }
-    });
-
-    if (filledAddress !== outputs.length || filledAmounts !== outputs.length) {
-        return true;
-    }
-
-    // locks
-    if (locks.includes(SUITE.LOCK_TYPE.DEVICE)) {
-        return true;
-    }
-
-    // device disconnected and not available
-    if (!device.connected || !device.available) {
-        return true;
-    }
-
-    // offline
-    if (!online) {
-        return true;
-    }
-
-    return false;
-};
-
 export default () => {
-    const {
-        account,
-        online,
-        locks,
-        device,
-        token,
-        isLoading,
-        transactionInfo,
-
-        signTransaction,
-        getValues,
-        composedLevels,
-    } = useSendFormContext();
+    const { device, isLocked } = useDevice();
+    const { online, isLoading, signTransaction, getValues, composedLevels } = useSendFormContext();
 
     const values = getValues();
     const composedTx = composedLevels ? composedLevels[values.selectedFee || 'normal'] : undefined;
-    const isDisabled = !composedTx;
+    const isDisabled = !composedTx || isLocked() || (device && !device.available) || !online;
     return (
         <Wrapper>
             <Row>
