e5ae1716fb2c3ee4a1b96d17b83ffa96a75954f7
slowbackspace <slowbackspace@gmail.com>
2020-08-03 11:55:25 +0200

    update list of tokens design

--- packages/suite/src/views/wallet/transactions/components/TokenList/index.tsx
+++ packages/suite/src/views/wallet/transactions/components/TokenList/index.tsx
@@ -1,15 +1,19 @@
 import React, { Fragment } from 'react';
 import styled, { css } from 'styled-components';
 import { Account } from '@wallet-types';
-import { FiatValue, HiddenPlaceholder, FormattedCryptoAmount } from '@suite-components';
+import { FiatValue, FormattedCryptoAmount, Translation } from '@suite-components';
+import { Section } from '@dashboard-components';
 import { variables, colors, Icon, Link, Card } from '@trezor/components';
 import { CARD_PADDING_SIZE } from '@suite-constants/layout';
+
+const StyledSection = styled(Section)`
+    margin-bottom: 30px;
+`;
 
 const Wrapper = styled(Card)`
     display: grid;
     grid-template-columns: ${(props: { isTestnet?: boolean }) =>
         props.isTestnet ? '4fr 1fr 44px' : '4fr 1fr 1fr 44px'};
-    margin-bottom: 30px;
     padding: 12px ${CARD_PADDING_SIZE};
 `;
 
@@ -20,7 +24,8 @@ interface ColProps {
 }
 
 const TokenSymbol = styled.div`
-    font-weight: ${variables.FONT_WEIGHT.DEMI_BOLD};
+    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
+    font-size: ${variables.FONT_SIZE.NORMAL};
     padding-right: 2px;
 `;
 
@@ -28,7 +33,7 @@ const Col = styled.div<ColProps>`
     display: flex;
     align-items: center;
     padding: 10px 12px 10px 0px;
-    color: ${colors.BLACK0};
+    color: ${colors.NEUE_TYPE_DARK_GREY};
     font-size: ${variables.FONT_SIZE.SMALL};
     border-top: 1px solid ${colors.BLACK96};
 
@@ -58,15 +63,28 @@ const TokenName = styled.div`
     flex: 1;
     overflow: hidden;
     text-overflow: ellipsis;
+    align-items: center;
 `;
 
 const TokenValue = styled.div`
     display: flex;
+    font-size: ${variables.FONT_SIZE.NORMAL};
+    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
+    color: ${colors.NEUE_TYPE_DARK_GREY};
 `;
 
 const FiatWrapper = styled.div`
     display: flex;
-    font-size: ${variables.FONT_SIZE.TINY};
+    font-size: ${variables.FONT_SIZE.SMALL};
+    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
+    color: ${colors.NEUE_TYPE_LIGHT_GREY};
+`;
+
+const Divider = styled.div`
+    width: 100%;
+    height: 1px;
+    background: ${colors.NEUE_STROKE_GREY};
+    margin: 24px 0px;
 `;
 
 interface Props {
@@ -78,18 +96,18 @@ interface Props {
 const TokenList = ({ tokens, explorerUrl, isTestnet }: Props) => {
     if (!tokens || tokens.length === 0) return null;
     return (
-        <Wrapper isTestnet={isTestnet} noPadding>
-            {tokens.map(t => {
-                return (
-                    <Fragment key={t.address}>
-                        <Col isTestnet={isTestnet}>
-                            <TokenName>
-                                <TokenSymbol>{t.symbol?.toUpperCase()}</TokenSymbol>
-                                {` - ${t.name}`}
-                            </TokenName>
-                        </Col>
-                        <Col isTestnet={isTestnet} justify="right">
-                            <HiddenPlaceholder>
+        <StyledSection heading={<Translation id="TR_TOKENS" />}>
+            <Wrapper isTestnet={isTestnet} noPadding>
+                {tokens.map(t => {
+                    return (
+                        <Fragment key={t.address}>
+                            <Col isTestnet={isTestnet}>
+                                <TokenName>
+                                    <TokenSymbol>{t.symbol?.toUpperCase()}</TokenSymbol>
+                                    {` - ${t.name}`}
+                                </TokenName>
+                            </Col>
+                            <Col isTestnet={isTestnet} justify="right">
                                 <TokenValue>
                                     {t.balance && (
                                         <FormattedCryptoAmount
@@ -98,30 +116,31 @@ const TokenList = ({ tokens, explorerUrl, isTestnet }: Props) => {
                                         />
                                     )}
                                 </TokenValue>
-                            </HiddenPlaceholder>
-                        </Col>
-                        {!isTestnet && (
-                            <Col isTestnet={isTestnet} justify="right">
-                                <FiatWrapper>
-                                    {t.balance && t.symbol && (
-                                        <FiatValue
-                                            amount={t.balance}
-                                            symbol={t.symbol}
-                                            badge={{ color: 'blue' }}
-                                        />
-                                    )}
-                                </FiatWrapper>
                             </Col>
-                        )}
-                        <Col isTestnet={isTestnet} justify="right">
-                            <Link href={`${explorerUrl}${t.address}`}>
-                                <Icon icon="EXTERNAL_LINK" size={16} color={colors.BLACK25} />
-                            </Link>
-                        </Col>
-                    </Fragment>
-                );
-            })}
-        </Wrapper>
+                            {!isTestnet && (
+                                <Col isTestnet={isTestnet} justify="right">
+                                    <FiatWrapper>
+                                        {t.balance && t.symbol && (
+                                            <FiatValue amount={t.balance} symbol={t.symbol} />
+                                        )}
+                                    </FiatWrapper>
+                                </Col>
+                            )}
+                            <Col isTestnet={isTestnet} justify="right">
+                                <Link href={`${explorerUrl}${t.address}`}>
+                                    <Icon
+                                        icon="EXTERNAL_LINK"
+                                        size={16}
+                                        color={colors.NEUE_TYPE_LIGHT_GREY}
+                                    />
+                                </Link>
+                            </Col>
+                        </Fragment>
+                    );
+                })}
+            </Wrapper>
+            <Divider />
+        </StyledSection>
     );
 };
 
