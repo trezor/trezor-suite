b539064432240cbb0debb5c275f84397f0d4f596
slowbackspace <slowbackspace@gmail.com>
2020-08-05 20:56:34 +0200

    improve fw changelog formatting

--- packages/suite/src/views/firmware/index.tsx
+++ packages/suite/src/views/firmware/index.tsx
@@ -56,6 +56,7 @@ const StyledButton = styled(Button)`
 
 const ChangesSummary = styled.div`
     width: 100%;
+    text-align: left;
     background-color: ${colors.BLACK96};
     border: 1px solid ${colors.BLACK80};
     padding: 20px;
@@ -126,6 +127,20 @@ const StyledH2 = styled(H2)`
     ::first-letter {
         text-transform: capitalize;
     }
+`;
+
+const ChangelogGroup = styled.div`
+    margin-bottom: 20px;
+    color: ${colors.NEUE_TYPE_DARK_GREY};
+    font-size: ${FONT_SIZE.SMALL};
+`;
+
+const ChangelogHeading = styled.div`
+    font-weight: ${FONT_WEIGHT.MEDIUM};
+`;
+
+const ChangelogPre = styled.pre`
+    white-space: pre-wrap;
 `;
 
 const CloseButton = (props: ButtonProps) => (
@@ -391,10 +406,12 @@ const Firmware = ({
                                 {device.firmwareRelease.changelog?.length > 0 && (
                                     <ChangesSummary>
                                         {device.firmwareRelease.changelog.map((c: any) => (
-                                            <div key={c.url}>
-                                                <P>{c.version.join('.')}</P>
-                                                <P size="small">{c.changelog}</P>
-                                            </div>
+                                            <ChangelogGroup key={c.url}>
+                                                <ChangelogHeading>
+                                                    {c.version.join('.')}
+                                                </ChangelogHeading>
+                                                <ChangelogPre>{c.changelog}</ChangelogPre>
+                                            </ChangelogGroup>
                                         ))}
                                     </ChangesSummary>
                                 )}
