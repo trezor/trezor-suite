d1942bd6ff02046017990f075794794189fdfc16
slowbackspace <slowbackspace@gmail.com>
2020-08-10 18:35:50 +0200

    don't force minimal height for bars representing data point with zero amount

--- packages/suite/src/components/suite/TransactionsGraph/components/CustomBar/index.tsx
+++ packages/suite/src/components/suite/TransactionsGraph/components/CustomBar/index.tsx
@@ -6,13 +6,10 @@ interface CustomBarProps {
 }
 
 const CustomBar = (props: CustomBarProps) => {
-    const { fill, x, y, width, height, payload, variant } = props;
+    const { fill, x, y, width, height } = props;
     let forcedHeightChange = false;
     let minHeight = height;
-    if (
-        (variant === 'sent' && Math.abs(height) < 1 && payload.sent !== '0') ||
-        (variant === 'received' && Math.abs(height) < 1 && payload.received !== '0')
-    ) {
+    if (Math.abs(height) < 1 && props.value !== 0) {
         // make sure small amounts are visible by forcing minHeight of 2 if abs(amount) < 1
         // minHeight = variant === 'sent' ? -2 : 2; // useful if we want to show sent bars below the y = 0
         minHeight = 2;
