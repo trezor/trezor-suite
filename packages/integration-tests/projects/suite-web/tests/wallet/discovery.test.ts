ecc0133cb14fc39a64761026643675c1f9f13d58
mroz22 <mvarmuza@gmail.com>
2020-08-26 23:13:57 +0200

    ci: use eval

--- packages/integration-tests/projects/suite-web/tests/wallet/discovery.test.ts
+++ packages/integration-tests/projects/suite-web/tests/wallet/discovery.test.ts
@@ -1,17 +1,15 @@
 import { NETWORKS } from '@wallet-config';
-
+ 
 describe.skip('Discovery', () => {
     beforeEach(() => {
         cy.viewport(1024, 768).resetDb();
-        cy.task('stopEmu');
-        cy.task('startEmu');
+        cy.task('startEmu', { wipe: true });
         cy.task('setupEmu');
+        cy.task('startGoogle');
         cy.prefixedVisit('/settings/wallet');
         cy.passThroughInitialRun();
-        cy.getTestElement('@suite/loading').should('not.be.visible');
     });
 
-    // todo: this test is potentially flaky. I maybe want to introduce two category of tests.
     it('go to wallet settings page, activate all coins and see that there is equal number of records on dashboard', () => {
         cy.getTestElement('@settings/wallet/coins-group/mainnet/activate-all').click();
         cy.getTestElement('@settings/wallet/coins-group/testnet/activate-all').click({
@@ -27,21 +25,21 @@ describe.skip('Discovery', () => {
     });
 
     // todo: I ll need to do some fixes to tests, but in next PR
-    it(`
-        1. connect model 2
-        2. go to settings/coins
-        3. disable all networks, enable XRP
-        4. go back to wallet
-        5. connect model 1
-    `, () => {
-        cy.getTestElement('@settings/wallet/coins-group/mainnet/deactivate-all').click();
-        cy.getTestElement('@settings/wallet/coins-group/testnet/deactivate-all').click({
-            force: true,
-        });
-        cy.getTestElement('@settings/wallet/network/xrp').click({ force: true });
-        cy.task('stopEmu');
-        cy.task('startEmu', '1.8.3');
-        cy.task('setupEmu');
-        cy.getTestElement('@settings/wallet/network/xrp').should('not.exist');
-    });
+    // it(`
+    //     1. connect model 2
+    //     2. go to settings/coins
+    //     3. disable all networks, enable XRP
+    //     4. go back to wallet
+    //     5. connect model 1
+    // `, () => {
+    //     cy.getTestElement('@settings/wallet/coins-group/mainnet/deactivate-all').click();
+    //     cy.getTestElement('@settings/wallet/coins-group/testnet/deactivate-all').click({
+    //         force: true,
+    //     });
+    //     cy.getTestElement('@settings/wallet/network/xrp').click({ force: true });
+    //     cy.task('stopEmu');
+    //     cy.task('startEmu', '1.8.3');
+    //     cy.task('setupEmu');
+    //     cy.getTestElement('@settings/wallet/network/xrp').should('not.exist');
+    // });
 });
