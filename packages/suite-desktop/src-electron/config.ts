f451c8e96f6c5e88479cb7a514c17362e460c7ec
Rafael Keramidas <rafael@keramid.as>
2020-08-31 19:31:29 +0200

    Fixed incorrect new window handling (variable already used in parent scope). Added confirmation dialog for unknown URLs before opening them in the browser.

---
+++ packages/suite-desktop/src-electron/config.ts
@@ -0,0 +1,18 @@
+export const oauthUrls = [
+    'https://accounts.google.com',
+    'https://www.dropbox.com/oauth2/authorize',
+];
+
+export const allowedExternalUrls = [
+    // SatoshiLabs
+    'https://trezor.io/support',
+    'https://wiki.trezor.io',
+    'https://blog.trezor.io',
+    // External Services
+    'https://github.com/trezor/',
+    // External wallets
+    'https://nemplatform.com/wallets', // NEM
+    'https://trezor.io/stellar', // Stellar
+    'https://adalite.io/app', // Ada
+    'https://wallet.simplestaking.com/tezos/wallet/start', // Tezos
+];
