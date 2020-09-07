b7bdaffda7732345fac9a4db60449221d07bcfce
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-09-07 13:47:32 +0200

    add timeout

--- packages/suite/src/views/wallet/coinmarket/buy/components/BuyForm/index.tsx
+++ packages/suite/src/views/wallet/coinmarket/buy/components/BuyForm/index.tsx
@@ -23,7 +23,7 @@ const BuyForm = () => {
 
     useEffect(() => {
         if (accountHasCachedRequest && cachedAccountInfo.shouldSubmit) {
-            // handleSubmit(onSubmit)();
+            setTimeout(() => handleSubmit(onSubmit)(), 3000);
         }
     });
 
