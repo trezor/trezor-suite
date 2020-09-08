16f95716eed1692e994eadeab03b44a325f920a4
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-09-08 19:51:21 +0200

    Refactor redirect to hooks

---
+++ packages/suite/src/hooks/suite/useRedirect.ts
@@ -0,0 +1,80 @@
+import { Account } from '@wallet-types';
+import { BuyTradeQuoteRequest } from 'invity-api';
+import { useActions } from '@suite/hooks/suite';
+import * as routerActions from '@suite-actions/routerActions';
+import * as coinmarketBuyActions from '@wallet-actions/coinmarketBuyActions';
+
+export const useRedirect = () => {
+    const { goto } = useActions({ goto: routerActions.goto });
+    const { saveQuoteRequest, saveCachedAccountInfo } = useActions({
+        saveQuoteRequest: coinmarketBuyActions.saveQuoteRequest,
+        saveCachedAccountInfo: coinmarketBuyActions.saveCachedAccountInfo,
+    });
+
+    interface OfferRedirectParams {
+        symbol: Account['symbol'];
+        index: Account['index'];
+        accountType: Account['accountType'];
+        wantCrypto: boolean;
+        fiatCurrency: string;
+        receiveCurrency: string;
+        amount: string;
+        country: string;
+    }
+
+    const redirectWithQuotes = async (params: OfferRedirectParams) => {
+        const {
+            symbol,
+            index,
+            accountType,
+            wantCrypto,
+            fiatCurrency,
+            receiveCurrency,
+            amount,
+            country,
+        } = params;
+        let request: BuyTradeQuoteRequest;
+        const commonParams = { fiatCurrency, receiveCurrency, country };
+
+        if (wantCrypto) {
+            request = {
+                ...commonParams,
+                wantCrypto,
+                cryptoStringAmount: amount,
+            };
+        } else {
+            request = {
+                ...commonParams,
+                wantCrypto,
+                fiatStringAmount: amount,
+            };
+        }
+        await saveQuoteRequest(request);
+        await saveCachedAccountInfo(symbol, index, accountType, true);
+        goto('wallet-coinmarket-buy', { symbol, accountIndex: index, accountType });
+    };
+
+    interface DetailRedirectParams {
+        symbol: Account['symbol'];
+        index: Account['index'];
+        accountType: Account['accountType'];
+        transactionId: string;
+    }
+
+    const redirectToDetail = async (params: DetailRedirectParams) => {
+        const { transactionId } = params;
+
+        console.log('transactionId', transactionId);
+
+        goto('wallet-coinmarket-buy-detail', {
+            symbol: params.symbol,
+            accountIndex: params.index,
+            accountType: params.accountType,
+        });
+    };
+
+    return {
+        redirectWithQuotes,
+        redirectToDetail,
+    };
+};
--- packages/suite/src/views/suite/coinmarket-redirect/index.tsx
+++ packages/suite/src/views/suite/coinmarket-redirect/index.tsx
@@ -1,11 +1,9 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import { variables } from '@trezor/components';
-import { BuyTradeQuoteRequest } from 'invity-api';
+import { useSelector } from '@suite/hooks/suite';
 import { Account } from '@wallet-types';
-import { useSelector, useActions } from '@suite/hooks/suite';
-import * as routerActions from '@suite-actions/routerActions';
-import * as coinmarketBuyActions from '@wallet-actions/coinmarketBuyActions';
+import { variables } from '@trezor/components';
+import { useRedirect } from '@suite-hooks/useRedirect';
 
 const Wrapper = styled.div`
     display: flex;
@@ -18,63 +16,34 @@ const Wrapper = styled.div`
 `;
 
 const CoinmarketRedirect = () => {
-    const { goto } = useActions({ goto: routerActions.goto });
-    const { saveQuoteRequest, saveCachedAccountInfo } = useActions({
-        saveQuoteRequest: coinmarketBuyActions.saveQuoteRequest,
-        saveCachedAccountInfo: coinmarketBuyActions.saveCachedAccountInfo,
-    });
+    const { redirectWithQuotes, redirectToDetail } = useRedirect();
     const router = useSelector(state => state.router);
-    const params = router?.hash?.split('/');
 
     useEffect(() => {
+        const params = router?.hash?.split('/');
         if (!params) return;
 
-        const redirectParams = {
-            routeType: params[0] as 'offers',
+        const redirectCommonParams = {
+            routeType: params[0] as 'detail' | 'offers',
             symbol: params[1] as Account['symbol'],
             accountType: params[2] as Account['accountType'],
             index: parseInt(params[3], 10),
         };
 
-        const redirectWithQuotes = async () => {
-            if (redirectParams.routeType === 'offers') {
-                const wantCrypto = params[4] === 'qc';
-                let request: BuyTradeQuoteRequest;
-                const commonParams = {
-                    fiatCurrency: params[6],
-                    receiveCurrency: params[8],
-                    country: params[5],
-                };
+        if (redirectCommonParams.routeType === 'offers') {
+            redirectWithQuotes({
+                ...redirectCommonParams,
+                wantCrypto: params[4] === 'qc',
+                fiatCurrency: params[6],
+                amount: params[7],
+                receiveCurrency: params[8],
+                country: params[5],
+            });
+        }
 
-                if (wantCrypto) {
-                    request = {
-                        ...commonParams,
-                        wantCrypto,
-                        cryptoStringAmount: params[7],
-                    };
-                } else {
-                    request = {
-                        ...commonParams,
-                        wantCrypto,
-                        fiatStringAmount: params[7],
-                    };
-                }
-                await saveQuoteRequest(request);
-                await saveCachedAccountInfo(
-                    redirectParams.symbol,
-                    redirectParams.index,
-                    redirectParams.accountType,
-                    true,
-                );
-
-                goto('wallet-coinmarket-buy', {
-                    symbol: redirectParams.symbol,
-                    accountIndex: redirectParams.index,
-                    accountType: redirectParams.accountType,
-                });
-            }
-        };
-        redirectWithQuotes();
+        if (redirectCommonParams.routeType === 'detail') {
+            redirectToDetail({ ...redirectCommonParams, transactionId: params[4] });
+        }
     });
 
     return <Wrapper>Redirecting ...</Wrapper>;
