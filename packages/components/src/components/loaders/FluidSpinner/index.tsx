2d87962562c0af22a650eb0ef35e580d49b77079
slowbackspace <slowbackspace@gmail.com>
2020-09-21 14:20:47 +0200

    perf: optimalize css spinner perf

--- packages/components/src/components/loaders/FluidSpinner/index.tsx
+++ packages/components/src/components/loaders/FluidSpinner/index.tsx
@@ -23,18 +23,22 @@ const Wrapper = styled.div<Props>`
         animation: ${animations.SPIN} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
         border-color: #fff transparent transparent transparent;
         border-top-color: ${props => (props.color ? props.color : 'inherit')};
+        will-change: transform;
     }
 
     div:nth-child(1) {
         animation-delay: -0.45s;
+        will-change: transform;
     }
 
     div:nth-child(2) {
         animation-delay: -0.3s;
+        will-change: transform;
     }
 
     div:nth-child(3) {
         animation-delay: -0.15s;
+        will-change: transform;
     }
 `;
 
