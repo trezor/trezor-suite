0d1a69618f838d850f7fa644a6659edeadbbe0ac
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-09-23 13:08:47 +0200

    bump db version

--- packages/suite/src/storage/index.ts
+++ packages/suite/src/storage/index.ts
@@ -11,7 +11,7 @@ import { GraphData } from '@wallet-types/graph';
 import { BuyTrade, ExchangeTrade } from 'invity-api';
 import { migrate } from './migrations';
 
-const VERSION = 16; // don't forget to add migration and CHANGELOG when changing versions!
+const VERSION = 17; // don't forget to add migration and CHANGELOG when changing versions!
 
 export interface DBWalletAccountTransaction {
     tx: WalletAccountTransaction;
