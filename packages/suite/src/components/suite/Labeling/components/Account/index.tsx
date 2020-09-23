fd9dabc848031867711db62da48419fdab7016da
robertbrada <bert.blader@gmail.com>
2020-09-23 15:01:05 +0200

    style: hid account type in transaction list

--- packages/suite/src/components/suite/Labeling/components/Account/index.tsx
+++ packages/suite/src/components/suite/Labeling/components/Account/index.tsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import * as accountUtils from '@wallet-utils/accountUtils';
 import * as deviceUtils from '@suite-utils/device';
 import { Translation } from '@suite-components';
-import messages from '@suite/support/messages';
 import { Props } from './Container';
 import WalletLabel from '../Wallet';
 
@@ -19,34 +18,32 @@ const Account = (props: Props) => {
 
     if (accounts.length < 1) return null;
 
-    let accountLabel: JSX.Element;
-    const networkName = accountUtils.getTitleForNetwork(accounts[0].symbol).defaultMessage; // Bitcoin, Ethereum, ...
+    const accountLabel: JSX.Element = (
+        <Translation
+            id="LABELING_ACCOUNT"
+            values={{
+                networkName: accountUtils.getTitleForNetwork(accounts[0].symbol).defaultMessage, // Bitcoin, Ethereum, ...
+                index: accounts[0].index + 1, // this is the number which shows after hash, e.g. Ethereum #3
+            }}
+        />
+    );
 
-    if (accounts[0].accountType !== 'normal') {
-        accountLabel = (
-            <Translation
-                id="LABELING_ACCOUNT_WITH_TYPE"
-                values={{
-                    networkName,
-                    index: accounts[0].index + 1,
-                    type:
-                        accounts[0].accountType === 'segwit'
-                            ? messages.TR_ACCOUNT_TYPE_SEGWIT
-                            : messages.TR_ACCOUNT_TYPE_LEGACY,
-                }}
-            />
-        );
-    } else {
-        accountLabel = (
-            <Translation
-                id="LABELING_ACCOUNT"
-                values={{
-                    networkName,
-                    index: accounts[0].index + 1, // this is the number which shows after hash, e.g. Account #3
-                }}
-            />
-        );
-    }
+    // display account type (e.g.'segwit') in the brackets after the account label
+    // if (accounts[0].accountType !== 'normal') {
+    //     accountLabel = (
+    //         <Translation
+    //             id="LABELING_ACCOUNT_WITH_TYPE"
+    //             values={{
+    //                 networkName,
+    //                 index: accounts[0].index + 1,
+    //                 type:
+    //                     accounts[0].accountType === 'segwit'
+    //                         ? messages.TR_ACCOUNT_TYPE_SEGWIT
+    //                         : messages.TR_ACCOUNT_TYPE_LEGACY,
+    //             }}
+    //         />
+    //     );
+    // }
 
     if (device && !accounts.find(a => a.deviceState === device.state)) {
         // account is not associated with selected device, add wallet label
