9f86cd44d10206664dbc1c0f802d9c1dbb963166
robertbrada <bert.blader@gmail.com>
2020-09-25 21:24:32 +0200

    fix: layout fix for more fee options

--- packages/suite/src/views/wallet/send/components/Fees/index.tsx
+++ packages/suite/src/views/wallet/send/components/Fees/index.tsx
@@ -8,24 +8,29 @@ import { getFeeUnits } from '@wallet-utils/sendFormUtils';
 import EstimatedMiningTime from './components/EstimatedMiningTime';
 import CustomFee from './components/CustomFee';
 import { useSendFormContext } from '@wallet-hooks';
+import { useLayoutSize } from '@suite-hooks';
 
 const StyledCard = styled(Card)`
     margin-bottom: 25px;
     padding: 32px 42px;
-    display: grid;
-    grid-gap: 5px;
-    grid-template-columns: 40px 1fr 1fr; /* First column contains "Fee" title */
+    display: flex;
+    flex-direction: row;
+    @media all and (max-width: ${variables.SCREEN_SIZE.SM}) {
+        flex-direction: column;
+    }
 `;
 
 const Label = styled.div`
-    padding-top: 5px;
+    padding: 5px 20px 10px 0;
     font-weight: ${variables.FONT_WEIGHT.MEDIUM};
     text-transform: capitalize;
     font-size: ${variables.FONT_SIZE.NORMAL};
     color: ${colors.NEUE_TYPE_DARK_GREY};
 `;
 
-const FeeSetupWrapper = styled.div``;
+const FeeSetupWrapper = styled.div`
+    width: 100%;
+`;
 
 const SelectBarWrapper = styled.div`
     display: flex; /* necessary for the <SelectBar> not to be stretched over full column width */
@@ -51,6 +56,7 @@ const FeeInfo = styled.div`
     display: flex;
     align-items: baseline;
     flex-wrap: wrap;
+    min-width: 150px;
 `;
 
 const FeeUnits = styled.span`
@@ -69,11 +75,15 @@ const EstimatedMiningTimeWrapper = styled.span`
 
 const FeeAmount = styled.div`
     display: flex;
-    justify-content: flex-start;
     flex-direction: column;
     align-items: flex-end;
     text-align: right;
     padding-top: 5px;
+    width: 100%; /* stretch to all available width so that the Fee amount is aligned all the way to the right */
+`;
+
+const FeeInfoWrapper = styled.div`
+    display: flex;
 `;
 
 interface Option {
@@ -90,6 +100,18 @@ const buildFeeOptions = (levels: FeeLevel[]) => {
     return result;
 };
 
+// Compute if mobile layout should be used.
+// The mobile layout is displayed based on the screen size and the size (=number of options) of <SelectBar>
+const getIfMobileLayout = (selectBarOptionsCount: number, layoutSize: string) => {
+    if (layoutSize === 'TINY') {
+        return true;
+    }
+    if (selectBarOptionsCount >= 5 && (layoutSize === 'NORMAL' || layoutSize === 'SMALL')) {
+        return true;
+    }
+    return false;
+};
+
 const Fees = () => {
     const {
         feeInfo,
@@ -104,20 +126,42 @@ const Fees = () => {
     const selectedLevel = feeInfo.levels.find(level => level.label === selectedLabel)!;
     const transactionInfo = composedLevels ? composedLevels[selectedLabel] : undefined;
     const isCustomLevel = selectedLabel === 'custom';
+    const feeOptions = buildFeeOptions(feeInfo.levels);
+    const { layoutSize } = useLayoutSize();
+    const useMobileLayout = getIfMobileLayout(feeOptions.length, layoutSize);
+
+    // declare feeAmountJSX code before rendering since we will use it at two different locations based on the layout setup (useMobileLayout?)
+    const feeAmountJSX =
+        transactionInfo !== undefined && transactionInfo.type !== 'error' ? (
+            <FeeAmount>
+                <CoinAmount>
+                    <FormattedCryptoAmount
+                        value={formatNetworkAmount(transactionInfo.fee, symbol)}
+                        symbol={symbol}
+                    />
+                </CoinAmount>
+                <FiatAmount>
+                    <FiatValue
+                        amount={formatNetworkAmount(transactionInfo.fee, symbol)}
+                        symbol={symbol}
+                    />
+                </FiatAmount>
+            </FeeAmount>
+        ) : (
+            <></>
+        );
 
     return (
         <StyledCard>
-            {/* FIRST COLUMN - Title */}
             <Label>
                 <Translation id="FEE" />
             </Label>
 
-            {/* SECOND COLUMN - Fee selector, Custom fee Input, Mining time message */}
             <FeeSetupWrapper>
                 <SelectBarWrapper>
                     <SelectBar
                         selectedOption={selectedLabel}
-                        options={buildFeeOptions(feeInfo.levels)}
+                        options={feeOptions}
                         onChange={value => {
                             // changeFeeLevel will decide if composeTransaction in needed or not
                             const shouldCompose = changeFeeLevel(
@@ -129,46 +173,32 @@ const Fees = () => {
                     />
                 </SelectBarWrapper>
 
-                <FeeInfo>
-                    {isCustomLevel && <CustomFee />}
-                    {networkType === 'bitcoin' && !isCustomLevel && (
-                        <EstimatedMiningTimeWrapper>
-                            <EstimatedMiningTime
-                                seconds={feeInfo.blockTime * selectedLevel.blocks * 60}
-                            />
-                        </EstimatedMiningTimeWrapper>
-                    )}
-                    <FeeUnits>
-                        {!isCustomLevel
-                            ? `${selectedLevel.feePerUnit} ${getFeeUnits(networkType)}`
-                            : ' '}
-                    </FeeUnits>
-                    {networkType === 'bitcoin' &&
-                        !isCustomLevel &&
-                        transactionInfo &&
-                        transactionInfo.type !== 'error' && (
-                            <TxSize>({transactionInfo.bytes} B)</TxSize>
+                <FeeInfoWrapper>
+                    <FeeInfo>
+                        {isCustomLevel && <CustomFee />}
+                        {networkType === 'bitcoin' && !isCustomLevel && (
+                            <EstimatedMiningTimeWrapper>
+                                <EstimatedMiningTime
+                                    seconds={feeInfo.blockTime * selectedLevel.blocks * 60}
+                                />
+                            </EstimatedMiningTimeWrapper>
                         )}
-                </FeeInfo>
+                        <FeeUnits>
+                            {!isCustomLevel
+                                ? `${selectedLevel.feePerUnit} ${getFeeUnits(networkType)}`
+                                : ' '}
+                        </FeeUnits>
+                        {networkType === 'bitcoin' &&
+                            !isCustomLevel &&
+                            transactionInfo &&
+                            transactionInfo.type !== 'error' && (
+                                <TxSize>({transactionInfo.bytes} B)</TxSize>
+                            )}
+                    </FeeInfo>
+                    {useMobileLayout && feeAmountJSX}
+                </FeeInfoWrapper>
             </FeeSetupWrapper>
-
-            {/* THIRD COLUMN - Fee amount in crypto and fiat */}
-            {transactionInfo && transactionInfo.type !== 'error' && (
-                <FeeAmount>
-                    <CoinAmount>
-                        <FormattedCryptoAmount
-                            value={formatNetworkAmount(transactionInfo.fee, symbol)}
-                            symbol={symbol}
-                        />
-                    </CoinAmount>
-                    <FiatAmount>
-                        <FiatValue
-                            amount={formatNetworkAmount(transactionInfo.fee, symbol)}
-                            symbol={symbol}
-                        />
-                    </FiatAmount>
-                </FeeAmount>
-            )}
+            {!useMobileLayout && feeAmountJSX}
         </StyledCard>
     );
 };
