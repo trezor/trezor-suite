e66bcc5962d330e0eb0d9ceac7a3d7ec742935f6
slowbackspace <slowbackspace@gmail.com>
2020-09-30 15:01:56 +0200

    fix: disabled spinners on dashboard

--- packages/components/src/components/loaders/FluidSpinner/index.tsx
+++ packages/components/src/components/loaders/FluidSpinner/index.tsx
@@ -28,17 +28,14 @@ const Wrapper = styled.div<Props>`
 
     div:nth-child(1) {
         animation-delay: -0.45s;
-        will-change: transform;
     }
 
     div:nth-child(2) {
         animation-delay: -0.3s;
-        will-change: transform;
     }
 
     div:nth-child(3) {
         animation-delay: -0.15s;
-        will-change: transform;
     }
 `;
 
