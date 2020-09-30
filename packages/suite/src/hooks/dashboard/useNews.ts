725965616cf469d41322b1c0460b4eb09cb26a3b
Pavol Rusnak <pavol@rusnak.io>
2020-09-30 17:39:39 +0200

    fix(suite): use only production news API

--- packages/suite/src/hooks/dashboard/useNews.ts
+++ packages/suite/src/hooks/dashboard/useNews.ts
@@ -1,9 +1,6 @@
 import { useState, useEffect } from 'react';
-import { isDev } from '@suite-utils/build';
 
-const NEWS_API_STAGING_URL = 'https://staging-news.trezor.io';
-const NEWS_API_PRODUCTION_URL = 'https://news.trezor.io';
-// const LOCAL_DEV = 'http://localhost:3003';
+const NEWS_API_URL = 'https://news.trezor.io';
 
 export function useFetchNews() {
     const [posts, setPosts] = useState<any[]>([]);
@@ -12,10 +9,8 @@ export function useFetchNews() {
 
     useEffect(() => {
         const abortController = new AbortController();
-        // const origin = LOCAL_DEV;
-        const origin = isDev() ? NEWS_API_STAGING_URL : NEWS_API_PRODUCTION_URL;
 
-        fetch(`${origin}/posts?limit=${fetchCount}`, { signal: abortController.signal })
+        fetch(`${NEWS_API_URL}/posts?limit=${fetchCount}`, { signal: abortController.signal })
             .then(response => response.json())
             .then(response => {
                 if (response.length > 1) {
