444b4b4df14d3a37bc4bd0e24010bdee6da76130
slowbackspace <slowbackspace@gmail.com>
2020-10-01 13:11:49 +0200

    chore: review fixes

--- packages/suite/src/components/wallet/AccountAnnouncement/XRPReserve.tsx
+++ packages/suite/src/components/wallet/AccountAnnouncement/XRPReserve.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import { NotificationCard, Translation } from '@suite-components';
-import { Link } from '@trezor/components';
-import { URLS } from '@suite-constants';
-
+import { NotificationCard, Translation, ReadMoreLink } from '@suite-components';
 import { formatNetworkAmount } from '@wallet-utils/accountUtils';
 
 interface Props {
@@ -13,18 +10,13 @@ const XRPReserve = ({ reserve }: Props) => (
     <NotificationCard
         variant="info"
         button={{
-            children: (
-                <Link variant="nostyle" href={URLS.XRP_MANUAL_URL}>
-                    <Translation id="TR_LEARN_MORE" />
-                </Link>
-            ),
+            children: <ReadMoreLink url="XRP_MANUAL_URL" />,
         }}
     >
         <Translation
             id="TR_XRP_RESERVE_INFO"
             values={{
                 minBalance: formatNetworkAmount(reserve, 'xrp'),
-                TR_LEARN_MORE: undefined,
             }}
         />
     </NotificationCard>
