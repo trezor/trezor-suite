e4e8e0238a0d0222bea7cd572bc4ae1270d13dde
Rafael Keramidas <rafael@keramid.as>
2020-10-02 16:33:05 +0300

    fix: gloal definition

--- packages/suite/global.d.ts
+++ packages/suite/global.d.ts
@@ -1,7 +1,14 @@
+// Interface for exposed Electron API (ipcRenderer)
 export interface DesktopApi {
     send: (channel: string, data?: any) => void;
     on: (channel: string, func: Function) => void;
     off: (channel: string, func: Function) => void;
+    // App Ready
+    ready: () => void;
+    // Auto Updater
+    checkForUpdates: () => void;
+    downloadUpdate: () => void;
+    installUpdate: () => void;
 }
 
 declare global {
