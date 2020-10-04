77dacb03e79caeea60804b17caf023be121bb46b
Pavol Rusnak <pavol@rusnak.io>
2020-10-04 20:07:24 +0200

    fix(suite-desktop): don't try to run bridge on unsupported systems

--- packages/suite-desktop/src-electron/bridge.ts
+++ packages/suite-desktop/src-electron/bridge.ts
@@ -31,14 +31,9 @@ const getArch = () => {
 const getOS = () => {
     const platform = os.platform();
     switch (platform) {
-        case 'aix':
-        case 'freebsd':
         case 'linux':
-        case 'openbsd':
-        case 'android':
             return 'linux';
         case 'darwin':
-        case 'sunos':
             return 'mac';
         case 'win32':
             return 'win';
