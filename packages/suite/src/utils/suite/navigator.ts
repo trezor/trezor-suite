d5d71dcaf7ec77fb8de89f1661b761729332bc7a
Tomas Listiak <tomexx@tomexx.net>
2020-10-05 10:00:23 +0200

    style: fix eslint issues in navigator util

--- packages/suite/src/utils/suite/navigator.ts
+++ packages/suite/src/utils/suite/navigator.ts
@@ -1,14 +1,16 @@
-import { Platform } from '@suite-types'
+import { Platform } from '@suite-types';
 
 export const getPlatform = (navigator: Navigator): Platform => {
-    const macPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K']
-    const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE']
+    const macPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
+    const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
     if (macPlatforms.includes(navigator.platform)) {
-        return 'mac'
-    } else if (windowsPlatforms.includes(navigator.platform)) {
-        return 'windows'
-    } else if (/Linux/.test(navigator.platform)) {
-        return 'linux'
+        return 'mac';
     }
-    return undefined
-}
+    if (windowsPlatforms.includes(navigator.platform)) {
+        return 'windows';
+    }
+    if (/Linux/.test(navigator.platform)) {
+        return 'linux';
+    }
+    return undefined;
+};
