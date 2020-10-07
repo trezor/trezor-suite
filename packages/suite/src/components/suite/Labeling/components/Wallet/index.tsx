8d536a1d9979316caa5fb7aa4a74b954813a1435
slowbackspace <slowbackspace@gmail.com>
2020-10-07 16:40:37 +0200

    fix: don't render wallet name for device without state

--- packages/suite/src/components/suite/Labeling/components/Wallet/index.tsx
+++ packages/suite/src/components/suite/Labeling/components/Wallet/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Translation } from '@suite-components';
-import { TrezorDevice } from '@suite-types';
+import { TrezorDevice, ExtendedMessageDescriptor } from '@suite-types';
 import messages from '@suite/support/messages';
 
 interface Props {
@@ -10,17 +10,19 @@ interface Props {
 
 const Wallet = (props: Props) => {
     const { device } = props;
-    const walletLabel = device.useEmptyPassphrase
-        ? messages.TR_NO_PASSPHRASE_WALLET
-        : {
-              ...messages.TR_PASSPHRASE_WALLET,
-              values: {
-                  id: device.instance,
-              },
-          };
+    let walletLabel: ExtendedMessageDescriptor | undefined;
+    if (device.state) {
+        walletLabel = device.useEmptyPassphrase
+            ? messages.TR_NO_PASSPHRASE_WALLET
+            : {
+                  ...messages.TR_PASSPHRASE_WALLET,
+                  values: {
+                      id: device.instance,
+                  },
+              };
+    }
 
-    const label =
-        typeof walletLabel === 'string' ? <>{walletLabel}</> : <Translation {...walletLabel} />;
+    const label = walletLabel ? <Translation {...walletLabel} /> : null;
 
     if (props.useDeviceLabel) {
         return (
