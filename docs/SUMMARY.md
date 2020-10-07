0d33c15327d4486bab396f7a1b2c80392fa11771
Tomas Susanka <tsusanka@gmail.com>
2020-10-07 22:36:55 +0200

    docs: typos

--- docs/SUMMARY.md
+++ docs/SUMMARY.md
@@ -1,6 +1,8 @@
 # Summary
 
 - [Introduction](index.md)
+- [Deployment](deployment/index.md)
+  - [Web](deployment/web.md)
 - [Features](features/index.md)
   - [Metadata](features/metadata.md)
 - [Miscellaneous](misc/index.md)
--- shell.nix
+++ shell.nix
@@ -4,6 +4,7 @@ stdenv.mkDerivation {
   name = "trezor-suite-dev";
   buildInputs = [
     nodejs-12_x
+    mdbook
     (yarn.override { nodejs = nodejs-12_x; })
   ] ++ lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
     Cocoa
