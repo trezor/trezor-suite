893c3091947e6db4de4040ce88565f4bc51cb6fd
Rafael Keramidas <rafael@keramid.as>
2020-10-07 17:21:32 +0200

    test: add mock flags for features test

--- packages/suite/src/utils/suite/__tests__/features.test.ts
+++ packages/suite/src/utils/suite/__tests__/features.test.ts
@@ -1,19 +1,56 @@
 import * as features from '../features';
 
+const MOCK_FLAGS = {
+    FLAG: false,
+};
+
+jest.mock('@suite-config/features', () => ({
+    get FLAGS() {
+        return MOCK_FLAGS;
+    },
+    get FLAGS_WEB() {
+        return {
+            ...MOCK_FLAGS,
+            FLAG: true,
+        };
+    },
+    get FLAGS_DESKTOP() {
+        return {
+            ...MOCK_FLAGS,
+            FLAG: false,
+        };
+    },
+    get FLAGS_LANDING() {
+        return {
+            ...MOCK_FLAGS,
+            FLAG: true,
+        };
+    },
+}));
+
 describe('Features utils', () => {
-    test('example flag should be enabled', () => {
-        expect(features.isEnabled('EXAMPLE')).toBe(true);
+    test('mock flag should be disabled', () => {
+        // @ts-ignore Mocked flag
+        expect(features.isEnabled('FLAG')).toBe(false);
     });
 
-    test('example flag should be enabled for web', () => {
-        expect(features.isEnabled('EXAMPLE', 'web')).toBe(true);
+    test('mock flag should be enabled for web', () => {
+        // @ts-ignore Mocked flag
+        expect(features.isEnabled('FLAG', 'web')).toBe(true);
     });
 
-    test('example flag should be disabled for desktop', () => {
-        expect(features.isEnabled('EXAMPLE', 'desktop')).toBe(false);
+    test('mock flag should be disabled for desktop', () => {
+        // @ts-ignore Mocked flag
+        expect(features.isEnabled('FLAG', 'desktop')).toBe(false);
     });
 
-    test('example flag should be enabled for landing', () => {
-        expect(features.isEnabled('EXAMPLE', 'landing')).toBe(true);
+    test('mock flag should be enabled for landing', () => {
+        // @ts-ignore Mocked flag
+        expect(features.isEnabled('FLAG', 'landing')).toBe(true);
+    });
+
+    test('Unknown flag should always default to false', () => {
+        // @ts-ignore Mocked flag
+        expect(features.isEnabled('UNKNOWN')).toBe(false);
     });
 });
