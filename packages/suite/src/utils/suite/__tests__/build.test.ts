063d600ce88793a20eaaf351db8be17b23bbe50c
mroz22 <mvarmuza@gmail.com>
2020-10-12 16:34:44 +0200

    chore: remove unused isBeta() util

--- packages/suite/src/utils/suite/__tests__/build.test.ts
+++ packages/suite/src/utils/suite/__tests__/build.test.ts
@@ -1,23 +1,8 @@
-import { isDev, isBeta, normalizeVersion } from '../build';
+import { isDev, normalizeVersion } from '../build';
 
 const OLD_ENV = { ...process.env };
 
 describe('build', () => {
-    describe('dev true by BUILD variable', () => {
-        beforeEach(() => {
-            jest.resetModules();
-            process.env.BUILD = 'development';
-        });
-
-        afterEach(() => {
-            process.env = OLD_ENV;
-        });
-
-        it('isDev', () => {
-            expect(isDev()).toEqual(true);
-        });
-    });
-
     describe('dev true by NODE_ENV variable', () => {
         beforeEach(() => {
             jest.resetModules();
@@ -30,36 +15,6 @@ describe('build', () => {
 
         it('isDev', () => {
             expect(isDev()).toEqual(true);
-        });
-    });
-
-    describe('beta true by BUILD variable', () => {
-        beforeEach(() => {
-            jest.resetModules();
-            process.env.BUILD = 'beta';
-        });
-
-        afterEach(() => {
-            process.env = OLD_ENV;
-        });
-
-        it('isDev', () => {
-            expect(isBeta()).toEqual(true);
-        });
-    });
-
-    describe('should NOT be beta', () => {
-        beforeEach(() => {
-            jest.resetModules();
-            process.env.BUILD = 'something';
-        });
-
-        afterEach(() => {
-            process.env = OLD_ENV;
-        });
-
-        it('isDev', () => {
-            expect(isBeta()).toEqual(false);
         });
     });
 
--- packages/suite/src/utils/suite/build.ts
+++ packages/suite/src/utils/suite/build.ts
@@ -1,10 +1,6 @@
-// process.env.BUILD is set by package.json
 // process.env.NODE_ENV is set by webpack.mode
 
-export const isDev = (): boolean =>
-    process.env.BUILD === 'development' || process.env.NODE_ENV === 'development';
-
-export const isBeta = (): boolean => process.env.BUILD === 'beta';
+export const isDev = () => process.env.NODE_ENV === 'development';
 
 export const normalizeVersion = (version: string) => {
     // remove any zeros that are not preceded by Latin letters, decimal digits, underscores
