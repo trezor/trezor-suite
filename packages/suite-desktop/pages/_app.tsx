29ff5091218c9ac83e4bd77046fdcea53cc8a48c
Tomas Listiak <tomexx@tomexx.net>
2020-10-19 12:14:19 +0200

    refactor(suite-desktop): custom titlebar

--- packages/suite-desktop/pages/_app.tsx
+++ packages/suite-desktop/pages/_app.tsx
@@ -15,43 +15,12 @@ import ErrorBoundary from '@suite-support/ErrorBoundary';
 import DesktopUpdater from '@desktop/support/DesktopUpdater';
 import { SENTRY_CONFIG } from '@suite-config';
 import Resize from '@suite-support/Resize/Container';
-import NoSSR from '@suite-support/NoSSR';
 import { isDev } from '@suite-utils/build';
-import styled from 'styled-components';
-import DesktopTitlebar from '@desktop/support/DesktopTitlebar';
+import DesktopTitlebarWrapper from '@desktop/support/DesktopTitlebar';
 
 interface Props {
     store: Store;
 }
-
-const titlebarHeight = 40;
-const resizeHandlerPadding = 4;
-const TitlebarWrapper = styled.div`
-    display: block;
-    position: fixed;
-    z-index: 1000000;
-    height: ${titlebarHeight}px;
-    width: 100%;
-
-    &:after {
-        position: fixed;
-        width: calc(100% - ${resizeHandlerPadding * 2}px);
-        height: ${titlebarHeight - resizeHandlerPadding}px;
-        content: '';
-        display: block;
-        top: ${resizeHandlerPadding}px;
-        left: ${resizeHandlerPadding}px;
-        z-index: 0;
-        -webkit-user-select: none;
-        -webkit-app-region: drag;
-    }
-`;
-
-const MainWrapper = styled.div`
-    height: calc(100% - ${titlebarHeight}px);
-    margin-top: ${titlebarHeight}px;
-    overflow-y: auto;
-`;
 
 class TrezorSuiteApp extends App<Props> {
     componentDidMount() {
@@ -68,30 +37,23 @@ class TrezorSuiteApp extends App<Props> {
         const { Component, pageProps, store } = this.props;
 
         return (
-            <>
-                <NoSSR>
-                    <TitlebarWrapper>
-                        <DesktopTitlebar />
-                    </TitlebarWrapper>
-                </NoSSR>
-                <MainWrapper>
-                    <ReduxProvider store={store}>
-                        <ErrorBoundary>
-                            <Resize />
-                            <OnlineStatus />
-                            <IntlProvider>
-                                <DesktopUpdater />
-                                <Router />
-                                <BridgeStatus />
-                                <ToastContainer />
-                                <Preloader>
-                                    <Component {...pageProps} />
-                                </Preloader>
-                            </IntlProvider>
-                        </ErrorBoundary>
-                    </ReduxProvider>
-                </MainWrapper>
-            </>
+            <DesktopTitlebarWrapper>
+                <ReduxProvider store={store}>
+                    <ErrorBoundary>
+                        <Resize />
+                        <OnlineStatus />
+                        <IntlProvider>
+                            <DesktopUpdater />
+                            <Router />
+                            <BridgeStatus />
+                            <ToastContainer />
+                            <Preloader>
+                                <Component {...pageProps} />
+                            </Preloader>
+                        </IntlProvider>
+                    </ErrorBoundary>
+                </ReduxProvider>
+            </DesktopTitlebarWrapper>
         );
     }
 }
