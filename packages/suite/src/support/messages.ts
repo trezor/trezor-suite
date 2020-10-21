19f27cc70ed7a1b4be03e5157822d20953b93868
Martin Boehm <martin.boehm@1mbsoftware.net>
2020-10-21 14:05:21 +0200

    fix: translations for buy crypto

--- packages/suite/src/support/messages.ts
+++ packages/suite/src/support/messages.ts
@@ -285,8 +285,8 @@ const definedMessages = defineMessages({
         id: 'TR_BUY_OFFERS_REFRESH',
     },
     TR_BUY_VALIDATION_ERROR_EMPTY: {
-        defaultMessage: 'TR_BUY_ERROR_EMPTY',
-        id: 'TR_BUY_ERROR_EMPTY',
+        defaultMessage: 'Required',
+        id: 'TR_BUY_VALIDATION_ERROR_EMPTY',
     },
     TR_BUY_VALIDATION_ERROR_MINIMUM_CRYPTO: {
         defaultMessage: 'Minimum is {minimum} {currency}',
@@ -305,8 +305,8 @@ const definedMessages = defineMessages({
         id: 'TR_BUY_VALIDATION_ERROR_MAXIMUM_FIAT',
     },
     TR_BUY_VALIDATION_ERROR_NOT_NUMBER: {
-        defaultMessage: 'TR_BUY_VALIDATION_ERROR_NO_NUMBER',
-        id: 'TR_BUY_VALIDATION_ERROR_NO_NUMBER',
+        defaultMessage: 'Not a number',
+        id: 'TR_BUY_VALIDATION_ERROR_NOT_NUMBER',
     },
     TR_BUY_BUY_AGAIN: {
         defaultMessage: 'Buy again',
@@ -3009,12 +3009,12 @@ const definedMessages = defineMessages({
         defaultMessage: 'Hide',
     },
     TR_BUY_RECEIVE_ACCOUNT_QUESTION_TOOLTIP: {
-        id: 'TR_BUY_RECEIVE_QUESTION_TOOLTIP',
-        defaultMessage: 'TR_BUY_RECEIVE_QUESTION_TOOLTIP',
+        id: 'TR_BUY_RECEIVE_ACCOUNT_QUESTION_TOOLTIP',
+        defaultMessage: 'Account to which the coins will be sent',
     },
     TR_BUY_RECEIVE_ADDRESS_QUESTION_TOOLTIP: {
-        id: 'TR_BUY_RECEIVE_QUESTION_TOOLTIP',
-        defaultMessage: 'TR_BUY_RECEIVE_QUESTION_TOOLTIP',
+        id: 'TR_BUY_RECEIVE_ADDRESS_QUESTION_TOOLTIP',
+        defaultMessage: 'Address from your account to which the coins will be sent',
     },
     TR_COINMARKET_BUYS: {
         id: 'TR_COINMARKET_BUYS',
