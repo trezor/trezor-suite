b7f37024411058725a2b1a1fa34fafbf6236a233
mroz22 <mvarmuza@gmail.com>
2020-10-27 11:11:23 +0100

    feat(metadata): display address label as a fallback in tx lists if tx label does not exist yet

--- packages/suite/src/views/wallet/transactions/components/TransactionList/components/TransactionItem/components/TargetAddressLabel/index.tsx
+++ packages/suite/src/views/wallet/transactions/components/TransactionList/components/TransactionItem/components/TargetAddressLabel/index.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { WalletAccountTransaction } from '@wallet-types';
 import { ArrayElement } from '@suite/types/utils';
 import { Translation, AddressLabeling } from '@suite-components';
+import { AccountMetadata } from '@suite-types/metadata';
 
 const TruncatedSpan = styled.span`
     overflow: hidden;
@@ -12,19 +13,35 @@ const TruncatedSpan = styled.span`
 interface Props {
     target: ArrayElement<WalletAccountTransaction['targets']>;
     type: WalletAccountTransaction['type'];
+    accountMetadata?: AccountMetadata;
 }
-const TargetAddressLabel = ({ target, type }: Props) => {
+const TargetAddressLabel = ({ target, type, accountMetadata }: Props) => {
     const isLocalTarget = (type === 'sent' || type === 'self') && target.isAccountTarget;
-    const addressLabel = isLocalTarget ? (
-        <Translation id="TR_SENT_TO_SELF" />
-    ) : (
-        target.addresses?.map((a, i) =>
-            // Using index as a key is safe as the array doesn't change (no filter/reordering, pushing new items)
-            // eslint-disable-next-line react/no-array-index-key
-            type === 'sent' ? <AddressLabeling key={i} address={a} /> : <span key={i}>{a}</span>,
-        )
+
+    if (isLocalTarget) {
+        return (
+            <TruncatedSpan>
+                <Translation id="TR_SENT_TO_SELF" />
+            </TruncatedSpan>
+        );
+    }
+
+    return (
+        <TruncatedSpan>
+            {target.addresses?.map((a, i) =>
+                // either it may be AddressLabeling - sent to another account associated with this device, e.g: "Bitcoin #2"
+                // or it may show address metadata label added from receive tab e.g "My address for illegal things"
+                type === 'sent' ? (
+                    // Using index as a key is safe as the array doesn't change (no filter/reordering, pushing new items)
+                    // eslint-disable-next-line react/no-array-index-key
+                    <AddressLabeling key={i} address={a} />
+                ) : (
+                    // eslint-disable-next-line react/no-array-index-key
+                    <span key={i}>{accountMetadata?.addressLabels[a] || a}</span>
+                ),
+            )}
+        </TruncatedSpan>
     );
-    return <TruncatedSpan>{addressLabel}</TruncatedSpan>;
 };
 
 export default TargetAddressLabel;
