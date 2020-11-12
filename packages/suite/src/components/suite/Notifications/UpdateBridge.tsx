66ef529f082a2c4603525a16c92d5b02a93a5f45
Pavol Rusnak <pavol@rusnak.io>
2020-11-12 09:51:02 +0100

    fix(suite-desktop): don't trigger bridge update dialog for bridge 2.0.27 and newer

--- packages/suite/src/components/suite/Notifications/UpdateBridge.tsx
+++ packages/suite/src/components/suite/Notifications/UpdateBridge.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, colors } from '@trezor/components';
 import { Translation } from '@suite-components';
+import { isDesktop } from '@suite-utils/env';
 
 import Wrapper from './components/Wrapper';
 import { Props as BaseProps } from './index';
@@ -12,6 +13,11 @@ interface Props {
 
 const UpdateBridge = ({ transport, goto }: Props) => {
     if (!transport || !transport.outdated) return null;
+    if (isDesktop()) {
+        // we can work with these versions of bridge too
+        if (transport.version && ['2.0.27', '2.0.28', '2.0.29'].includes(transport.version))
+            return null;
+    }
     return (
         <Wrapper variant="info">
             <Translation id="TR_NEW_TREZOR_BRIDGE_IS_AVAILABLE" />
