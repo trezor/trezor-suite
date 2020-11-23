e8f863e907035d66c4eaf4f287a2902076b5452a
martin <30367552+mroz22@users.noreply.github.com>
2020-11-23 10:11:20 +0100

    chore: rework static notifications panel

---
+++ packages/suite/src/components/suite/Banners/index.tsx
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import styled from 'styled-components';
+import { isDesktop } from '@suite-utils/env';
+import { useSelector } from '@suite-hooks';
+
+import OnlineStatus from './OnlineStatus';
+import UpdateBridge from './UpdateBridge';
+import UpdateFirmware from './UpdateFirmware';
+import NoBackup from './NoBackup';
+import FailedBackup from './FailedBackup';
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    z-index: 3;
+`;
+
+const Banners = () => {
+    const transport = useSelector(state => state.suite.transport);
+    const online = useSelector(state => state.suite.online);
+    const device = useSelector(state => state.suite.device);
+
+    let Banner;
+
+    const showUpdateBridge = () => {
+        if (
+            isDesktop() &&
+            transport?.version &&
+            ['2.0.27', '2.0.28', '2.0.29'].includes(transport.version)
+        ) {
+            return false;
+        }
+        return transport?.outdated;
+    };
+
+    if (device?.features?.unfinished_backup) {
+        Banner = <FailedBackup />;
+    } else if (device?.features?.needs_backup) {
+        Banner = <NoBackup />;
+    } else if (showUpdateBridge()) {
+        Banner = <UpdateBridge />;
+    } else if (
+        device?.connected &&
+        device?.features &&
+        device?.mode !== 'bootloader' &&
+        ['outdated'].includes(device.firmware)
+    ) {
+        Banner = <UpdateFirmware />;
+    }
+
+    return (
+        <Wrapper>
+            <OnlineStatus isOnline={online} />
+            {Banner}
+            {/* TODO: add Pin not set */}
+        </Wrapper>
+    );
+};
+
+export default Banners;
