152011093de780eef33680bdec22ce9a46249938
Vladimir Volek <vladimir.volek@satoshilabs.com>
2020-12-03 18:58:11 +0100

    fixup! fixup! fix: long tx id

--- packages/suite/src/components/wallet/CoinmarketTransactionId/index.tsx
+++ packages/suite/src/components/wallet/CoinmarketTransactionId/index.tsx
@@ -1,4 +1,4 @@
-import React, { createRef } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { variables, Button } from '@trezor/components';
 import { copyToClipboard } from '@suite-utils/dom';
