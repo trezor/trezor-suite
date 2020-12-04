057ba64d0dc4d0199b84930b7c3295904e61b264
Tomas Listiak <tomexx@tomexx.net>
2020-12-04 17:03:27 +0100

    fix(desktop): Missing offset for notifications [RELEASE ONLY]

--- packages/suite/src/constants/suite/layout.ts
+++ packages/suite/src/constants/suite/layout.ts
@@ -2,3 +2,4 @@ export const MAX_WIDTH = '1020px';
 export const MAX_WIDTH_WALLET_CONTENT = '768px';
 export const CARD_PADDING_SIZE = '20px';
 export const CARD_PADDING_SIZE_LARGE = '26px';
+export const DESKTOP_TITLEBAR_HEIGHT = '28px';
