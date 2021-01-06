79fd47ca183b17dc2801f2851a3e701c432be1b9
Szymon Lesisz <sz.lesisz@gmail.com>
2021-01-06 12:09:17 +0100

    chore: enable rbf as default option

--- packages/suite/src/hooks/wallet/useSendForm.ts
+++ packages/suite/src/hooks/wallet/useSendForm.ts
@@ -11,7 +11,7 @@ import {
     SendContextValues,
     Output,
 } from '@wallet-types/sendForm';
-// import { isEnabled as isFeatureEnabled } from '@suite-utils/features';
+import { isEnabled as isFeatureEnabled } from '@suite-utils/features';
 
 import { getFeeLevels } from '@wallet-utils/sendFormUtils';
 import { useSendFormOutputs } from './useSendFormOutputs';
@@ -25,16 +25,14 @@ SendContext.displayName = 'SendContext';
 
 const getDefaultValues = (
     currency: Output['currency'],
-    _network: UseSendFormState['network'],
+    network: UseSendFormState['network'],
 ): FormState => {
     return {
         ...DEFAULT_VALUES,
-        // TEMP: rbf option is disabled by default
-        // options:
-        //     isFeatureEnabled('RBF') && network.features?.includes('rbf')
-        //         ? ['bitcoinRBF', 'broadcast']
-        //         : ['broadcast'],
-        options: ['broadcast'],
+        options:
+            isFeatureEnabled('RBF') && network.features?.includes('rbf')
+                ? ['bitcoinRBF', 'broadcast']
+                : ['broadcast'],
         outputs: [{ ...DEFAULT_PAYMENT, currency }],
     };
 };
