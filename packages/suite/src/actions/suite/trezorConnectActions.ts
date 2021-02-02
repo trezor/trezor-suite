99e614038c0e9b7af2896963ce916f7478188dca
Szymon Lesisz <sz.lesisz@gmail.com>
2021-02-02 17:47:26 +0100

    fixup! fixup! fixup! chore: update trezor-connect@8.1.20

--- packages/suite/src/actions/suite/trezorConnectActions.ts
+++ packages/suite/src/actions/suite/trezorConnectActions.ts
@@ -11,7 +11,7 @@ import { Dispatch, GetState } from '@suite-types';
 import { isDesktop, isWeb } from '@suite-utils/env';
 import { toTorUrl } from '@suite-utils/tor';
 
-const CONNECT_URL = 'https://connect.corp.sldev.cz/feat/new-signtx-process/';
+const CONNECT_URL = 'https://connect.trezor.io/8/';
 
 export const init = () => async (dispatch: Dispatch, getState: GetState) => {
     // set event listeners and dispatch as
