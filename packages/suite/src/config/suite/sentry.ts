986a9c3fc51691134e08dfc1929d9d8a7be7b720
slowbackspace <slowbackspace@gmail.com>
2021-02-09 14:24:03 +0100

    fix(suite): throw custom error on non-200 status codes from fiat rates provider, don't send the errors to sentry

--- packages/suite/src/config/suite/sentry.ts
+++ packages/suite/src/config/suite/sentry.ts
@@ -1,4 +1,6 @@
 import { CaptureConsole } from '@sentry/integrations';
+import { BrowserOptions } from '@sentry/browser';
+import { TOR_DOMAIN } from '@suite-constants/urls';
 
 export default {
     dsn: 'https://6d91ca6e6a5d4de7b47989455858b5f6@o117836.ingest.sentry.io/5193825',
@@ -10,4 +12,13 @@ export default {
     ],
     release: process.env.COMMITHASH,
     environment: process.env.SUITE_TYPE,
-};
+    beforeSend(event, hint) {
+        const error = hint?.syntheticException;
+        const re = new RegExp(`FiatRatesFetchError.*${TOR_DOMAIN}`, 'gmi');
+        if (error?.message?.match(re)) {
+            // discard failed fiat rate fetch on TOR
+            event.fingerprint = ['FiatRatesFetchError'];
+            return null;
+        }
+    },
+} as BrowserOptions;
