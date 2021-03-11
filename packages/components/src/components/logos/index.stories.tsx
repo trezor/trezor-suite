576ff623ca3b158c1b2dff0726fd0cde0c037437
slowbackspace <slowbackspace@gmail.com>
2021-03-11 18:19:37 +0100

    fix(components): TrezorLogo dark mode adjustments

--- packages/components/src/components/logos/index.stories.tsx
+++ packages/components/src/components/logos/index.stories.tsx
@@ -14,7 +14,6 @@ interface WrapperProps {
 }
 
 const LogoWrapper = styled.div<WrapperProps>`
-    background: ${props => (props.isDark ? 'black' : 'none')};
     display: flex;
     min-height: 100px;
     flex-direction: column;
@@ -55,57 +54,18 @@ storiesOf('Logos', module).add(
                 <LogoWrapper>
                     <TrezorLogo
                         type="horizontal"
-                        variant="black"
                         width="200px"
                         data-test="trezor-logo-horizontal-black"
                     />
                     <TrezorLogo
                         type="vertical"
-                        variant="black"
                         width="120px"
                         data-test="trezor-logo-vertical-black"
                     />
-                    <TrezorLogo
-                        type="symbol"
-                        variant="black"
-                        width="50px"
-                        data-test="trezor-logo-symbol-black"
-                    />
-                    <TrezorLogo
-                        type="suite"
-                        variant="black"
-                        width="200px"
-                        data-test="trezor-suite-logo-black"
-                    />
+                    <TrezorLogo type="symbol" width="50px" data-test="trezor-logo-symbol-black" />
+                    <TrezorLogo type="suite" width="200px" data-test="trezor-suite-logo-black" />
                     <TrezorLogo
                         type="suite_compact"
-                        variant="black"
-                        width="200px"
-                        data-test="trezor-suite-compact-logo-black"
-                    />
-                </LogoWrapper>
-                <LogoWrapper isDark>
-                    <TrezorLogo
-                        type="horizontal"
-                        variant="white"
-                        width="200px"
-                        data-test="trezor-logo-horizontal-white"
-                    />
-                    <TrezorLogo
-                        type="vertical"
-                        variant="white"
-                        width="120px"
-                        data-test="trezor-logo-vertical-white"
-                    />
-                    <TrezorLogo
-                        type="symbol"
-                        variant="white"
-                        width="50px"
-                        data-test="trezor-logo-symbol-white"
-                    />
-                    <TrezorLogo
-                        type="suite_compact"
-                        variant="white"
                         width="200px"
                         data-test="trezor-suite-compact-logo-black"
                     />
