1e788a279904d051bd0105e4166ac8b583dc38ae
slowbackspace <slowbackspace@gmail.com>
2021-03-11 18:19:37 +0100

    fix(components): TrezorLogo dark mode adjustments

--- packages/components/src/components/logos/TrezorLogo/index.tsx
+++ packages/components/src/components/logos/TrezorLogo/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { ReactSVG } from 'react-svg';
 import styled from 'styled-components';
 import { LOGOS } from './logos';
+import { useTheme } from '../../../utils/hooks';
 import { TrezorLogoType, TrezorLogoVariant } from '../../../support/types';
 
 const SvgWrapper = styled.div<Omit<Props, 'type'>>`
@@ -16,34 +17,31 @@ const SvgWrapper = styled.div<Omit<Props, 'type'>>`
 
 interface Props extends React.ImgHTMLAttributes<HTMLImageElement> {
     type: TrezorLogoType;
-    variant?: TrezorLogoVariant;
+    fillColor?: string;
     width?: string | number;
     height?: string | number;
 }
 
-const TrezorLogo = ({
-    type,
-    variant = 'black',
-    width = 'auto',
-    height = 'auto',
-    ...rest
-}: Props) => (
-    <SvgWrapper
-        width={typeof width === 'number' ? `${width}px` : width}
-        height={typeof height === 'number' ? `${height}px` : height}
-        {...rest}
-    >
-        <ReactSVG
-            src={LOGOS[type.toUpperCase()]}
-            beforeInjection={svg => {
-                if (typeof height === 'number') {
-                    svg.setAttribute('height', `${height}px`);
-                }
-                svg.setAttribute('fill', variant);
-            }}
-            loading={() => <span className="loading" />}
-        />
-    </SvgWrapper>
-);
+const TrezorLogo = ({ type, fillColor, width = 'auto', height = 'auto', ...rest }: Props) => {
+    const theme = useTheme();
+    return (
+        <SvgWrapper
+            width={typeof width === 'number' ? `${width}px` : width}
+            height={typeof height === 'number' ? `${height}px` : height}
+            {...rest}
+        >
+            <ReactSVG
+                src={LOGOS[type.toUpperCase()]}
+                beforeInjection={svg => {
+                    if (typeof height === 'number') {
+                        svg.setAttribute('height', `${height}px`);
+                    }
+                    svg.setAttribute('fill', fillColor ?? theme.TYPE_DARK_GREY);
+                }}
+                loading={() => <span className="loading" />}
+            />
+        </SvgWrapper>
+    );
+};
 
 export { TrezorLogo, Props as TrezorLogoProps };
