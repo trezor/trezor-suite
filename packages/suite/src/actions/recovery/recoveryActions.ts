9748f45c623b1db5642d8ae34f3b8fc678c75bcd
slowbackspace <slowbackspace@gmail.com>
2021-04-08 19:58:30 +0200

    feat(onboarding): recovery step

--- packages/suite/src/actions/recovery/recoveryActions.ts
+++ packages/suite/src/actions/recovery/recoveryActions.ts
@@ -12,6 +12,7 @@ export type SeedInputStatus =
     | 'initial'
     | 'select-word-count'
     | 'select-recovery-type'
+    | 'waiting-for-confirmation'
     | 'in-progress'
     | 'finished';
 
@@ -55,7 +56,12 @@ const checkSeed = () => async (dispatch: Dispatch, getState: GetState) => {
     const { device } = getState().suite;
     if (!device || !device.features) return;
     dispatch(setError(''));
-    dispatch(setStatus('in-progress'));
+
+    if (device.features.major_version === 1) {
+        dispatch(setStatus('waiting-for-confirmation'));
+    } else {
+        dispatch(setStatus('in-progress'));
+    }
 
     const response = await TrezorConnect.recoveryDevice({
         dry_run: true,
@@ -84,7 +90,12 @@ const recoverDevice = () => async (dispatch: Dispatch, getState: GetState) => {
     const { device } = getState().suite;
     if (!device || !device.features) return;
     dispatch(setError(''));
-    dispatch(setStatus('in-progress'));
+
+    if (device.features.major_version === 1) {
+        dispatch(setStatus('waiting-for-confirmation'));
+    } else {
+        dispatch(setStatus('in-progress'));
+    }
 
     const params: RecoveryDevice = {
         type: advancedRecovery ? 1 : 0,
--- packages/suite/src/components/recovery/SelectWordCount.tsx
+++ packages/suite/src/components/recovery/SelectWordCount.tsx
@@ -1,50 +1,47 @@
 import React from 'react';
-
-import { P } from '@trezor/components';
-import { Option, Wrapper } from '@onboarding-components';
+import styled from 'styled-components';
+import { NeueOption, OptionsWrapper, OptionsDivider } from '@onboarding-components';
 
 import { WordCount } from '@recovery-types';
 import { Translation } from '@suite-components';
+
+const StyledOption = styled(NeueOption)`
+    justify-content: center;
+`;
 
 interface Props {
     onSelect: (number: WordCount) => void;
 }
 
 const SelectWordCount = ({ onSelect }: Props) => (
-    <>
-        <P size="small">
+    <OptionsWrapper>
+        {/* <P size="small">
             <Translation id="TR_RECOVER_SUBHEADING" />
-        </P>
-        <Wrapper.Options>
-            <Option
-                variant={3}
-                action={() => {
-                    onSelect(12);
-                }}
-                button={<Translation id="TR_WORDS" values={{ count: '12' }} />}
-                imgSrc="images/svg/12-words.svg"
-                data-test="@recover/select-count/12"
-            />
-            <Option
-                variant={3}
-                action={() => {
-                    onSelect(18);
-                }}
-                button={<Translation id="TR_WORDS" values={{ count: '18' }} />}
-                imgSrc="images/svg/18-words.svg"
-                data-test="@recover/select-count/18"
-            />
-            <Option
-                variant={3}
-                action={() => {
-                    onSelect(24);
-                }}
-                button={<Translation id="TR_WORDS" values={{ count: '24' }} />}
-                imgSrc="images/svg/24-words.svg"
-                data-test="@recover/select-count/24"
-            />
-        </Wrapper.Options>
-    </>
+        </P> */}
+        <StyledOption
+            onClick={() => {
+                onSelect(12);
+            }}
+            heading={<Translation id="TR_WORDS" values={{ count: '12' }} />}
+            data-test="@recover/select-count/12"
+        />
+        <OptionsDivider />
+        <StyledOption
+            onClick={() => {
+                onSelect(18);
+            }}
+            heading={<Translation id="TR_WORDS" values={{ count: '18' }} />}
+            data-test="@recover/select-count/18"
+        />
+        <OptionsDivider />
+        <StyledOption
+            onClick={() => {
+                onSelect(24);
+            }}
+            heading={<Translation id="TR_WORDS" values={{ count: '24' }} />}
+            data-test="@recover/select-count/24"
+        />
+    </OptionsWrapper>
 );
 
 export default SelectWordCount;
