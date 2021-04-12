333de993bad05c548a1ea504de6a624a64181428
slowbackspace <slowbackspace@gmail.com>
2021-04-12 13:27:27 +0200

    feat(onboarding): Add Coins (basic settings) step

---
+++ packages/suite/src/views/onboarding/steps/BasicSettings/BasicSettingsStepBox.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { OnboardingStepBox, OnboardingStepBoxProps } from '@firmware-components';
+
+const BasicSettingsStepBox = (props: OnboardingStepBoxProps) => {
+    return <OnboardingStepBox image="COINS" {...props} />;
+};
+
+export default BasicSettingsStepBox;
