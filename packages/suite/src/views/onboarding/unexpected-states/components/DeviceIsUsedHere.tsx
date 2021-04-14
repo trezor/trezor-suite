c6383cd19ec60b7a90bee84a62eb200eefc37b0b
slowbackspace <slowbackspace@gmail.com>
2021-04-14 21:17:02 +0200

    feat(onboarding): handling various not-so-ideal device states

--- packages/suite/src/views/onboarding/unexpected-states/components/DeviceIsUsedHere.tsx
+++ packages/suite/src/views/onboarding/unexpected-states/components/DeviceIsUsedHere.tsx
@@ -1,29 +1,25 @@
 import React from 'react';
-
-import { H2, P } from '@trezor/components';
 import { Translation } from '@suite-components';
-import { Wrapper, OnboardingButton } from '@onboarding-components';
+import { OnboardingButton } from '@onboarding-components';
 
 import * as suiteActions from '@suite-actions/suiteActions';
 import { useActions } from '@suite-hooks';
+import { OnboardingStepBox } from '@suite/components/firmware';
 
 const DeviceIsUsedHere = () => {
     const { actionCta } = useActions({ actionCta: suiteActions.acquireDevice });
 
     return (
-        <>
-            <H2>
-                <Translation id="TR_DEVICE_IS_USED_IN_OTHER_WINDOW_HEADING" />
-            </H2>
-            <P size="small">
-                <Translation id="TR_DEVICE_IS_USED_IN_OTHER_WINDOW_TEXT" />
-            </P>
-            <Wrapper.Controls>
+        <OnboardingStepBox
+            disableConfirmWrapper
+            heading={<Translation id="TR_DEVICE_IS_USED_IN_OTHER_WINDOW_HEADING" />}
+            description={<Translation id="TR_DEVICE_IS_USED_IN_OTHER_WINDOW_TEXT" />}
+            innerActions={
                 <OnboardingButton.Cta onClick={() => actionCta()}>
                     <Translation id="TR_DEVICE_IS_USED_IN_OTHER_WINDOW_BUTTON" />
                 </OnboardingButton.Cta>
-            </Wrapper.Controls>
-        </>
+            }
+        />
     );
 };
 
--- packages/suite/src/views/onboarding/unexpected-states/components/Reconnect.tsx
+++ packages/suite/src/views/onboarding/unexpected-states/components/Reconnect.tsx
@@ -1,60 +1,33 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { Button } from '@trezor/components';
-import { Translation, WebusbButton, Image } from '@suite-components';
-import { Wrapper, Text } from '@onboarding-components';
-
-const ButtonWrapper = styled.div`
-    display: flex;
-    width: 100%;
-    justify-content: center;
-    margin-top: 20px;
-`;
+import React from 'react';
+import { Translation } from '@suite-components/Translation';
+import { TroubleshootingTips } from '@onboarding-components';
 
 interface Props {
     showWebUsb: boolean;
 }
 
-const Reconnect = ({ showWebUsb }: Props) => {
-    // we need imageLoaded here so that we can position webusb button properly.
-    const [imageLoaded, setImageLoaded] = useState(false);
-    return (
-        <Wrapper.Step data-test="@onboarding/unexpected-state/reconnect">
-            <Wrapper.StepHeading>
-                <Translation id="TR_RECONNECT_HEADER" />
-            </Wrapper.StepHeading>
-            <Wrapper.StepBody>
-                <Image
-                    width="360"
-                    image="CONNECT_DEVICE"
-                    onLoad={() => setImageLoaded(true)}
-                    onError={() => setImageLoaded(true)}
-                />
-                <Text>
-                    <Translation id="TR_RECONNECT_TEXT" />
-                </Text>
-                <Text>---</Text>
-                <Text>
-                    <Translation id="TR_RECONNECT_TROUBLESHOOT_CONNECTION" />
-                </Text>
-                <Text>
-                    <Translation id="TR_RECONNECT_TROUBLESHOOT_CABLE" />
-                </Text>
-                <Text>
-                    <Translation id="TR_RECONNECT_TROUBLESHOOT_BRIDGE" />
-                </Text>
+const tips = [
+    {
+        key: '1',
+        heading: <Translation id="TR_RECONNECT_TEXT" />,
+        description: <Translation id="TR_RECONNECT_TROUBLESHOOT_CONNECTION" />,
+    },
+    {
+        key: '3',
+        heading: <Translation id="TR_RECONNECT_TROUBLESHOOT_CABLE" />,
+        description: 'Connect it directly to computer, no hubs.',
+    },
+    {
+        key: '4',
+        heading: <Translation id="TR_RECONNECT_TROUBLESHOOT_BRIDGE" />,
+    },
+];
 
-                {showWebUsb && (
-                    <ButtonWrapper>
-                        <WebusbButton ready={imageLoaded}>
-                            <Button icon="PLUS">
-                                <Translation id="TR_CHECK_FOR_DEVICES" />
-                            </Button>
-                        </WebusbButton>
-                    </ButtonWrapper>
-                )}
-            </Wrapper.StepBody>
-        </Wrapper.Step>
-    );
-};
+const Reconnect = ({ showWebUsb }: Props) => (
+    <TroubleshootingTips
+        label={<Translation id="TR_STILL_DONT_SEE_YOUR_TREZOR" />}
+        items={tips}
+        offerWebUsb={showWebUsb}
+    />
+);
 export default Reconnect;
