e6c354f0928dd96d0df9d9727d39b6b8f5ab4ec2
slowbackspace <slowbackspace@gmail.com>
2021-04-23 11:26:02 +0200

    fix(news-api): return proper error statusCode, change user agent

--- packages/news-api/src/config.ts
+++ packages/news-api/src/config.ts
@@ -1,3 +1,4 @@
 export const MEDIUM_FEED_URL = 'https://cdn.trezor.io/dynamic/medium/feed/trezor-security-blog';
 export const MEDIUM_CDN_BASE = 'https://cdn-images-1.medium.com';
 export const TREZOR_CDN_BASE = 'https://cdn.trezor.io/static/medium/images';
+export const USER_AGENT = 'trezor/news-api';
