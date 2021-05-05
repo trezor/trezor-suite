4b2348fbb200c3437f51a73e4547f66de16e79ae
Jen Tak <jentak@hey.com>
2021-05-06 01:10:27 +0200

    chore(tor): Refactor tor electron module
    - Get rid of the global mutable `torSettings` object. Use the store as
      the single source of truth.
    - Fix an issue where the tor process wouldn't be stopped if it was
      already running and the address was changed to some other than the
      default one.
    - Put the decision on when the process should run to a single place.
    - Document semantics of the `TorSettings` fields which might not be
      obvious from their names.

--- packages/suite-desktop/src-electron/modules/tor.ts
+++ packages/suite-desktop/src-electron/modules/tor.ts
@@ -3,93 +3,85 @@
  */
 import { app, session, ipcMain } from 'electron';
 import TorProcess, { DEFAULT_ADDRESS } from '@desktop-electron/libs/processes/TorProcess';
-import { b2t } from '@desktop-electron/libs/utils';
-
 import { onionDomain } from '../config';
-
-const torFlag = app.commandLine.hasSwitch('tor');
 
 const init = async ({ mainWindow, store }: Dependencies) => {
     const { logger } = global;
     const tor = new TorProcess();
-    const torSettings = store.getTorSettings();
 
-    const toggleTor = async (start: boolean) => {
-        // Only start/stop the process if address is the default one.
+    /**
+     * Merges given TorSettings with settings already present in the store,
+     * persists the result and returns it.
+     */
+    const persistSettings = (options: Partial<TorSettings>): TorSettings => {
+        const newSettings = { ...store.getTorSettings(), ...options };
+        store.setTorSettings(newSettings);
+        return newSettings;
+    };
+
+    const setProxy = (rule: string) => {
+        logger.info('tor', `Setting proxy rules to "${rule}"`);
+        session.defaultSession.setProxy({
+            proxyRules: rule,
+        });
+    };
+
+    const setupTor = async (settings: TorSettings) => {
+        // Start (or stop) the bundled tor only if address is the default one.
         // Otherwise the user must run the process themselves.
-        if(torSettings.address === DEFAULT_ADDRESS) {
-            if (start) {
-                if (torSettings.running) {
-                    logger.info('tor', 'Restarting');
-                    await tor.restart();
-                } else {
-                    logger.info('tor', 'Starting');
-                    await tor.start();
-                }
+        const shouldRunBundledTor = settings.running && settings.address === DEFAULT_ADDRESS;
+        if (shouldRunBundledTor !== (await tor.status()).process) {
+            if (shouldRunBundledTor === true) {
+                await tor.start();
             } else {
-                logger.info('tor', 'Stopping');
                 await tor.stop();
             }
         }
 
-        torSettings.running = start;
-        store.setTorSettings(torSettings);
+        // Start (or stop) routing all communication through tor.
+        if (settings.running) {
+            setProxy(`socks5://${settings.address}`);
+        } else {
+            setProxy('');
+        }
 
-        mainWindow.webContents.send('tor/status', start);
-
-        const proxy = start ? `socks5://${torSettings.address}` : '';
-        logger.info('tor', `Setting proxy to "${proxy}"`);
-        session.defaultSession.setProxy({
-            proxyRules: proxy,
-        });
+        // Notify the renderer.
+        mainWindow.webContents.send('tor/status', settings.running);
     };
-
-    if (torFlag || torSettings.running) {
-        logger.info('tor', [
-            'Auto starting:',
-            `- Running with flag: ${b2t(torFlag)}`,
-            `- Running with settings: ${b2t(torSettings.running)}`,
-        ]);
-        await toggleTor(true);
-    }
 
     ipcMain.on('tor/toggle', async (_, start: boolean) => {
         logger.info('tor', `Toggling ${start ? 'ON' : 'OFF'}`);
-        await toggleTor(start);
+        const settings = persistSettings({ running: start });
+        await setupTor(settings);
     });
 
     ipcMain.on('tor/set-address', async (_, address: string) => {
-        if (torSettings.address !== address) {
-            logger.debug('tor', [
-                'Updating address:',
-                `- From: ${torSettings.address}`,
-                `- To: ${address}`,
-            ]);
-
-            torSettings.address = address;
-            store.setTorSettings(torSettings);
-
-            if (torSettings.running) {
-                await toggleTor(true);
-            }
+        if (store.getTorSettings().address !== address) {
+            logger.debug('tor', `Changed address to ${address}`);
+            const settings = persistSettings({ address });
+            await setupTor(settings);
         }
     });
 
     ipcMain.on('tor/get-status', () => {
-        logger.debug('tor', `Getting status (${torSettings.running ? 'ON' : 'OFF'})`);
-        mainWindow.webContents.send('tor/status', torSettings.running);
+        logger.debug('tor', `Getting status (${store.getTorSettings().running ? 'ON' : 'OFF'})`);
+        mainWindow.webContents.send('tor/status', store.getTorSettings().running);
     });
 
     ipcMain.handle('tor/get-address', () => {
-        logger.debug('tor', `Getting address (${torSettings.address})`);
-        return torSettings.address;
+        logger.debug('tor', `Getting address (${store.getTorSettings().address})`);
+        return store.getTorSettings().address;
     });
 
     session.defaultSession.webRequest.onBeforeRequest({ urls: ['*://*/*'] }, (details, cb) => {
         const { hostname, protocol } = new URL(details.url);
 
         // Redirect outgoing trezor.io requests to .onion domain
-        if (torSettings.running && hostname.endsWith('trezor.io') && protocol === 'https:') {
+        if (
+            store.getTorSettings().running &&
+            hostname.endsWith('trezor.io') &&
+            protocol === 'https:'
+        ) {
             logger.info('tor', `Rewriting ${details.url} to .onion URL`);
             cb({
                 redirectURL: details.url.replace(
@@ -107,6 +99,13 @@ const init = async ({ mainWindow, store }: Dependencies) => {
         logger.info('tor', 'Stopping (app quit)');
         tor.stop();
     });
+
+    if (app.commandLine.hasSwitch('tor')) {
+        logger.info('tor', 'Tor enabled by command line option.');
+        persistSettings({ running: true });
+    }
+
+    await setupTor(store.getTorSettings());
 };
 
 export default init;
