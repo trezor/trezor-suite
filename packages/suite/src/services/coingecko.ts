e36d67c3d0fb5f2acf16fd3b993df8ae5fb764cb
slowbackspace <slowbackspace@gmail.com>
2021-05-11 13:45:47 +0200

    fix(suite): slow down coingecko requests

--- packages/suite/src/services/coingecko.ts
+++ packages/suite/src/services/coingecko.ts
@@ -1,11 +1,55 @@
+/* eslint-disable max-classes-per-file */
 import { LastWeekRates, TickerId } from '@wallet-types/fiatRates';
 import FIAT_CONFIG from '@suite-config/fiat';
+import { differenceInMilliseconds } from 'date-fns';
 
+// a proxy for https://api.coingecko.com/api/v3
 const COINGECKO_API_BASE_URL = 'https://cdn.trezor.io/dynamic/coingecko/api/v3';
 
 interface HistoricalResponse extends LastWeekRates {
     symbol: string;
 }
+
+class RateLimiter {
+    // Poor man's coingecko rate limiter
+    // Slows down requests so there is `delayMs` gap between them
+    private delayMs: number; // gap between each request
+    private lastFetchTimestamp: number;
+    private queued: number; // how many requests are waiting to be fired
+    private totalDelay: number; // by how much time are we gonna slown down next request
+
+    constructor(delayMs: number) {
+        this.delayMs = delayMs;
+
+        this.lastFetchTimestamp = 0;
+        this.queued = 0;
+        this.totalDelay = 0;
+    }
+
+    async limit<T>(fn: () => Promise<T>): Promise<T> {
+        const msSinceLastFetch = differenceInMilliseconds(
+            new Date().getTime(),
+            this.lastFetchTimestamp,
+        );
+        if (msSinceLastFetch < this.delayMs) {
+            this.queued += 1;
+            this.totalDelay += this.delayMs;
+            // dummy wait for this.totalDelay before we fire next request
+            await new Promise(resolve => setTimeout(resolve, this.totalDelay)); // slow down firing next request
+        }
+
+        this.lastFetchTimestamp = new Date().getTime();
+        const results = await fn();
+        this.queued -= 1;
+        if (this.queued === 0) {
+            // if all queued requests were fired, we need to reset totalDelay to properly delay next batch of requests
+            this.totalDelay = 0;
+        }
+        return results;
+    }
+}
+
+const rateLimiter = new RateLimiter(1000);
 
 class FiatRatesFetchError extends Error {
     constructor(message: string) {
@@ -20,7 +64,7 @@ class FiatRatesFetchError extends Error {
 
 const fetchCoinGecko = async (url: string) => {
     try {
-        const res = await fetch(url);
+        const res = await rateLimiter.limit(() => fetch(url));
         if (!res.ok) {
             throw new FiatRatesFetchError(`${res.status}: ${url}`);
         }
