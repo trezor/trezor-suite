d6f05ba20fbb9805e1839fe58b5bd58fe9f7b1c6
Szymon Lesisz <sz.lesisz@gmail.com>
2021-05-18 13:21:20 +0200

    fix: change suite-web dev server port
    to satisfy trezord CORS check

--- packages/suite-web/scripts/httpsServer.js
+++ packages/suite-web/scripts/httpsServer.js
@@ -16,8 +16,8 @@ app.prepare().then(() => {
     createServer(httpsOptions, (req, res) => {
         const parsedUrl = parse(req.url, true);
         handle(req, res, parsedUrl);
-    }).listen(3000, err => {
+    }).listen(8000, err => {
         if (err) throw err;
-        console.log('> Ready on https://localhost:3000');
+        console.log('> Ready on https://localhost:8000');
     });
 });
