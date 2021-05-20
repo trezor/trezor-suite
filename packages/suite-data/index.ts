a41bc8a0fc5d570301eb5d6b01ecd2bab24751a7
Jen Tak <jentak@hey.com>
2021-05-20 12:14:50 +0200

    chore: Refactor copy-static-files script
    Keep the semantics untouched and
    - Move the config into the script to reduce indirection.
    - Merge several config objects into a single one.
    - Order config objects alphabetically and add trailing commas.
    - Use idiomatic JS (`Object.entries`)to traverse an object.
    - Deduplicate rimraf invocations.

--- packages/suite-data/index.ts
+++ packages/suite-data/index.ts
@@ -1,34 +1,54 @@
-import config from './config';
 import * as fs from 'fs-extra';
 import * as rm from 'rimraf';
 import { resolve, join } from 'path';
 
-const projectRoot = resolve(__dirname);
-const packagesRoot = resolve(projectRoot, '../');
-
-const paths = {
-    web: join(packagesRoot, 'suite-web', 'public', 'static'),
-    desktop: join(packagesRoot, 'suite-desktop', 'public', 'static'),
-    native: join(packagesRoot, 'suite-native', 'public', 'static'),
-    landing: join(packagesRoot, 'landing-page', 'public', 'static'),
-    'web-landing': join(packagesRoot, 'suite-web-landing', 'public', 'static'),
+const config = {
+    'landing-page': [
+        'fonts',
+        'images/icons/favicon',
+        'images/landing',
+    ],
+    'suite-desktop': [
+        'bin',
+        'connect',
+        'fonts',
+        'images',
+        'message-system',
+        'translations',
+        'videos',
+    ],
+    'suite-native': [
+        'fonts',
+        'images',
+        'message-system',
+        'videos',
+    ],
+    'suite-web': [
+        'browser-detection',
+        'connect',
+        'fonts',
+        'images',
+        'message-system',
+        'oauth',
+        'translations',
+        'videos',
+    ],
+    'suite-web-landing': [
+        'fonts',
+        'images/icons/favicon',
+        'images/suite-web-landing',
+    ],
 };
 
-const copyFiles = (from: string, to: string) => {
-    rm.sync(join(packagesRoot, 'suite-web', 'public'));
-    rm.sync(join(packagesRoot, 'suite-desktop', 'public'));
-    rm.sync(join(packagesRoot, 'suite-native', 'public'));
-    rm.sync(join(packagesRoot, 'landing-page', 'public'));
-    rm.sync(join(packagesRoot, 'suite-web-landing', 'public'));
-
-    fs.copy(from, to, err => {
-        if (err) return console.error(err);
-        console.log('copied', from, to);
-    });
-};
-
-Object.keys(config).map((project: string) =>
-    config[project].map((path: string) =>
-        copyFiles(join(projectRoot, 'files', path), join(paths[project], path)),
-    ),
-);
+Object.entries(config).forEach(([project, paths]) => {
+    const destination = join(resolve(__dirname, '../'), project, 'public');
+    rm.sync(destination);
+    paths.forEach(path => {
+        const from = join(resolve(__dirname), 'files', path);
+        const to = join(destination, 'static', path);
+        fs.copy(from, to, err => {
+            if (err) return console.error(err);
+            console.log('copied', from, to);
+        });
+    })
+});
