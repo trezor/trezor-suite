aa8bcb321aed9c4f66d949b895569c904f60aecb
Jen Tak <jentak@hey.com>
2021-05-28 15:42:23 +0200

    feat: #3713 Index Guide Content
    Traverse the GitBook source and construct its index including all
    locales. This index will be used in following commits to transform the
    markdown content to a form that can be mounted in the Suite app at
    runtime.

--- packages/suite-data/src/guide/index.ts
+++ packages/suite-data/src/guide/index.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs-extra';
 import simpleGit from 'simple-git';
 import { TMP, GITBOOK_SOURCE, GITBOOK_REVISION } from './constants';
+import { Parser } from './parser';
 
 /**
  * Clears the @param destination directory and then populates it with
@@ -15,4 +16,14 @@ const fetchSource = async (repository: string, revision: string, destination: st
     await git.checkout(revision);
 };
 
-fetchSource(GITBOOK_SOURCE, GITBOOK_REVISION, TMP);
+const main = async () => {
+    // Fetch content from GitBook mirror.
+    await fetchSource(GITBOOK_SOURCE, GITBOOK_REVISION, TMP);
+    // Parse content tree.
+    const parser = new Parser(TMP);
+    const index = parser.parse();
+
+    console.log(JSON.stringify(index, undefined, 2));
+};
+
+main();
