e8addfbf26af9caf636babe9c3e5a5024ef860e2
Jen Tak <jentak@hey.com>
2021-06-15 12:25:00 +0200

    feat(guide): update guide source

--- packages/suite-data/src/guide/constants.ts
+++ packages/suite-data/src/guide/constants.ts
@@ -1,7 +1,7 @@
 import { resolve, join } from 'path';
 
 export const GITBOOK_SOURCE = 'https://github.com/trezor/trezor-suite-guide.git';
-export const GITBOOK_REVISION = '867eda00926bc065c3ec7eaee26832d08549ecfe';
+export const GITBOOK_REVISION = '7cc4ae126002d30dc247f95073d37baa4ff297c4';
 export const TMP = join(resolve(__dirname, '../..'), 'tmp', 'guide');
 // Path to the GitBook assets. Relative to TMP.
 export const ASSETS_DIR = join('.gitbook', 'assets');
