26b2386d1f2225cbe3ffee12807a7c21c6b5c0dd
Marek Polak <marek.polak@satoshilabs.com>
2021-07-08 14:44:20 +0200

    feat(suite): #1871 add menu item for custom firmware It's currently in the danger area, under the Wipe item. The translated strings are improvised for now.

--- packages/suite/src/views/firmware/FirmwareCustom.tsx
+++ packages/suite/src/views/firmware/FirmwareCustom.tsx
@@ -1,3 +1,18 @@
-const FirmwareCustom = () => null;
+import React from 'react';
+import * as routerActions from '@suite-actions/routerActions';
+import { useActions } from '@suite-hooks';
+import { Modal } from '@suite-components';
+
+const FirmwareCustom = () => {
+    const { closeModalApp } = useActions({
+        closeModalApp: routerActions.closeModalApp,
+    });
+
+    const onCancel = () => {
+        closeModalApp();
+    };
+
+    return <Modal cancelable onCancel={onCancel} />;
+};
 
 export default FirmwareCustom;
