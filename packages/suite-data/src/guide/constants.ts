23e184a020a96d81f30a011c4ae7a3d8ff76ba5f
Jen Tak <jentak@hey.com>
2021-07-12 14:10:33 +0200

    feat(guide): Update GitBook revision

--- packages/suite-data/src/guide/constants.ts
+++ packages/suite-data/src/guide/constants.ts
@@ -1,7 +1,7 @@
 import { resolve, join } from 'path';
 
 export const GITBOOK_SOURCE = 'https://github.com/trezor/trezor-suite-guide.git';
-export const GITBOOK_REVISION = 'c9cbce8250014539f74bd78516f7cc827a63bf5d';
+export const GITBOOK_REVISION = '7defb3d8fd61bd7cc6d7602955f54b95698f5ed2';
 export const TMP = join(resolve(__dirname, '../..'), 'tmp', 'guide');
 // Path to the GitBook assets. Relative to TMP.
 export const ASSETS_DIR = join('.gitbook', 'assets');
