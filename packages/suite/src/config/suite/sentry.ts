33a70d4f46117a4f01b370066f6222b24843c939
Matej Kriz <matej@matejkriz.cz>
2021-07-12 16:49:37 +0200

    fix(suite): 3925 stop firing FiatRatesFetchError completely
    - There is not much we could do about it, it's handled in UI and it eats up the Sentry limit.

--- packages/suite/src/config/suite/sentry.ts
+++ packages/suite/src/config/suite/sentry.ts
@@ -1,8 +1,5 @@
 import { CaptureConsole } from '@sentry/integrations';
 import { BrowserOptions } from '@sentry/browser';
-import { TOR_DOMAIN } from '@suite-constants/urls';
-
-const fiatRatesRe = new RegExp(`FiatRatesFetchError.*${TOR_DOMAIN}`, 'gmi');
 
 export default {
     dsn: 'https://6d91ca6e6a5d4de7b47989455858b5f6@o117836.ingest.sentry.io/5193825',
@@ -15,13 +12,4 @@ export default {
     ],
     release: process.env.COMMITHASH,
     environment: process.env.SUITE_TYPE,
-    beforeSend: (event, hint) => {
-        const error = hint?.syntheticException;
-        if (error?.message?.match(fiatRatesRe)) {
-            // discard failed fiat rate fetch on TOR
-            event.fingerprint = ['FiatRatesFetchError'];
-            return null;
-        }
-        return event;
-    },
 } as BrowserOptions;
