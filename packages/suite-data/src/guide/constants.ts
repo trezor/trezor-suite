6ce967a47dc7e2119f4fae81b5fc6c6984df5f75
Tomas Susanka <tsusanka@gmail.com>
2021-07-14 10:21:16 +0200

    feat(guide): Update GitBook revision

--- packages/suite-data/src/guide/constants.ts
+++ packages/suite-data/src/guide/constants.ts
@@ -1,7 +1,7 @@
 import { resolve, join } from 'path';
 
 export const GITBOOK_SOURCE = 'https://github.com/trezor/trezor-suite-guide.git';
-export const GITBOOK_REVISION = '7defb3d8fd61bd7cc6d7602955f54b95698f5ed2';
+export const GITBOOK_REVISION = '4b4a84ff90608d1cb34d44aeaa3d5e0b5c6a608d';
 export const TMP = join(resolve(__dirname, '../..'), 'tmp', 'guide');
 // Path to the GitBook assets. Relative to TMP.
 export const ASSETS_DIR = join('.gitbook', 'assets');
