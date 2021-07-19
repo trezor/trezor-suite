93d019ff1111be35f26caed7c00bce0e641be388
Marek Polak <marek.polak@satoshilabs.com>
2021-07-19 15:33:38 +0200

    feat(suite): #4039 fix acquire desktop subtitles

--- packages/suite/src/components/suite/PrerequisitesGuide/components/DeviceAcquire.tsx
+++ packages/suite/src/components/suite/PrerequisitesGuide/components/DeviceAcquire.tsx
@@ -6,6 +6,7 @@ import { Button } from '@trezor/components';
 import { Translation, TroubleshootingTips } from '@suite-components';
 import { useDevice, useActions } from '@suite-hooks';
 import * as suiteActions from '@suite-actions/suiteActions';
+import { isDesktop } from '@suite-utils/env';
 
 const Wrapper = styled.div`
     display: flex;
@@ -36,12 +37,28 @@ const DeviceAcquire = () => {
                     {
                         key: 'device-acquire',
                         heading: <Translation id="TR_TROUBLESHOOTING_CLOSE_TABS" />,
-                        description: <Translation id="TR_TROUBLESHOOTING_CLOSE_TABS_DESCRIPTION" />,
+                        description: (
+                            <Translation
+                                id={
+                                    isDesktop()
+                                        ? 'TR_TROUBLESHOOTING_CLOSE_TABS_DESCRIPTION_DESKTOP'
+                                        : 'TR_TROUBLESHOOTING_CLOSE_TABS_DESCRIPTION'
+                                }
+                            />
+                        ),
                     },
                     {
                         key: 'device-reconnect',
                         heading: <Translation id="TR_RECONNECT_YOUR_DEVICE" />,
-                        description: <Translation id="TR_RECONNECT_DEVICE_DESCRIPTION" />,
+                        description: (
+                            <Translation
+                                id={
+                                    isDesktop()
+                                        ? 'TR_RECONNECT_DEVICE_DESCRIPTION_DESKTOP'
+                                        : 'TR_RECONNECT_DEVICE_DESCRIPTION'
+                                }
+                            />
+                        ),
                     },
                 ]}
             />
