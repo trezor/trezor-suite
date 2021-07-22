894aaf1e95e07df962913cbdf6838303df194890
tomasklima <tomas.klim@gmail.com>
2021-07-22 10:38:07 +0200

    feat(sentry): increase sample rate + track all non-duplicated issues

--- packages/suite/src/config/suite/sentry.ts
+++ packages/suite/src/config/suite/sentry.ts
@@ -1,14 +1,15 @@
-import { CaptureConsole } from '@sentry/integrations';
+import { CaptureConsole, Dedupe } from '@sentry/integrations';
 import { BrowserOptions } from '@sentry/browser';
 
 export default {
     dsn: 'https://6d91ca6e6a5d4de7b47989455858b5f6@o117836.ingest.sentry.io/5193825',
     normalizeDepth: 10,
-    sampleRate: 0.1,
+    sampleRate: 0.5,
     integrations: [
         new CaptureConsole({
             levels: ['error'],
         }),
+        new Dedupe(),
     ],
     release: process.env.COMMITHASH,
     environment: process.env.SUITE_TYPE,
