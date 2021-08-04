805a2ab150a178f32e4032bb460050b53e2df5e5
grdddj <jiri.musil06@seznam.cz>
2021-08-04 18:13:05 +0200

    feat(tests): option to use custom emulator version in local e2e tests

--- docker/docker-suite-test.sh
+++ docker/docker-suite-test.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+# Usage for custom emulators: docker/docker-suite-test.sh -o /tmp/1-custom -t /tmp/2-custom
+# -o being for T-One location and -t for T-T location, both arguments being optional
+
+DIR=$(dirname "$0")
+
 set -e
 
 # todo: resolve selective xhost permissions
@@ -6,5 +11,14 @@ set -e
 
 xhost +
 export LOCAL_USER_ID=`id -u $USER`
+
+# When supplied with custom emulator(s), copying them into tenv container
+# Forwarding all script arguments (-o and -t) into the script and running it on
+#   the background to be able to wait until the container is running
+if [[ $# -ne 0 ]]
+then
+    echo "Copying custom emulators into the tenv container"
+    ${DIR}/copy-custom-emulators-into-tenv.sh $@ &
+fi
 
 docker-compose -f ./docker/docker-compose.suite-test.yml up --build --abort-on-container-exit
