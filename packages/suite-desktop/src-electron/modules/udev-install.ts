698e15d09252ddd5b7618f334d1ad444069dbb92
Szymon Lesisz <sz.lesisz@gmail.com>
2021-08-10 14:51:38 +0200

    feat(suite): add udev rules to suite-desktop files
    and create RPC function do be called from suite UI

---
+++ packages/suite-desktop/src-electron/modules/udev-install.ts
@@ -0,0 +1,74 @@
+import fs from 'fs';
+import path from 'path';
+import { app, ipcMain } from 'electron';
+import { spawn } from 'child_process';
+
+const FILE_NAME = '51-trezor.rules';
+
+const fileExists = async (path: string) => {
+    try {
+        await fs.promises.stat(path);
+        return true;
+    } catch (error) {
+        // file is not present
+    }
+    return false;
+};
+
+type Response = ReturnType<NonNullable<typeof window.desktopApi>['installUdevRules']>;
+
+const init = () => {
+    ipcMain.handle('udev/install', async (): Response => {
+        const { logger, resourcesPath } = global;
+        const resourceRules = path.join(resourcesPath, `bin/udev/${FILE_NAME}`);
+        const userRules = `${app.getPath('userData')}/${FILE_NAME}`;
+        const distRules = `/etc/udev/rules.d/${FILE_NAME}`;
+
+        logger.info('udev', `Installing ${resourceRules} > ${userRules} > ${distRules}`);
+
+        if (await fileExists(distRules)) {
+            logger.error('udev', `/etc/udev rules already installed: ${distRules}`);
+
+            // /etc/udev already exists, break here.
+            // TODO: should override anyway?
+            return { success: false, error: `File ${distRules} already exists` };
+        }
+
+        if (!(await fileExists(userRules))) {
+            try {
+                logger.info('udev', `Create user data rules: ${userRules}`);
+                // copy rules from app resources (/tmp/...) to user data files (~/.cache/...)
+                // this step is required. `pkexec` returns "Permission denied" error when copying directly from app resources.
+                await fs.promises.copyFile(resourceRules, userRules);
+                // chmod to read-only
+                await fs.promises.chmod(userRules, 0o444);
+            } catch (error) {
+                logger.error('udev', `User data rules error ${error}`);
+                return { success: false, error: `${error}` };
+            }
+        }
+
+        return new Promise(resolve => {
+            logger.info('udev', `Copy rules from ${userRules} to ${distRules}`);
+            // request superuser permissions and copy from user data files to /etc/udev
+            // NOTE: https://github.blog/2021-06-10-privilege-escalation-polkit-root-on-linux-with-bug/
+            const process = spawn('pkexec', ['cp', userRules, distRules]);
+
+            process.on('error', error => {
+                logger.error('udev', `pkexec error ${error}`);
+                resolve({ success: false, error: `pkexec error ${error.message}` });
+            });
+
+            process.on('exit', code => {
+                logger.debug('udev', `pkexec exit with code ${code}`);
+                if (code === 0) {
+                    resolve({ success: true });
+                } else {
+                    resolve({ success: false, error: `pkexec exit with code ${code}` });
+                }
+            });
+        });
+    });
+};
+
+export default init;
