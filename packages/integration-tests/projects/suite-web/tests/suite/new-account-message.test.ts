b21ae6ff99ff6a888caa21701491982733dddf87
grdddj <jiri.musil06@seznam.cz>
2021-08-23 14:13:02 +0200

    fix(tests): updating expected message in new account e2e test

--- packages/integration-tests/projects/suite-web/tests/suite/new-account-message.test.ts
+++ packages/integration-tests/projects/suite-web/tests/suite/new-account-message.test.ts
@@ -2,24 +2,24 @@
 // @retry=2
 
 describe('New accounts', () => {
-        beforeEach(() => {
-             // Launches Trezor emulator
-            cy.task('startEmu', { wipe: true });
-            // Initializes Trezor (random seed)
-            cy.task('setupEmu', { needs_backup: false });
-            cy.task('startBridge');
-            cy.viewport(1024, 768).resetDb();
-            cy.prefixedVisit('/');
-            cy.passThroughInitialRun();
-            cy.discoveryShouldFinish();
-        });
-    
+    beforeEach(() => {
+        // Launches Trezor emulator
+        cy.task('startEmu', { wipe: true });
+        // Initializes Trezor (random seed)
+        cy.task('setupEmu', { needs_backup: false });
+        cy.task('startBridge');
+        cy.viewport(1024, 768).resetDb();
+        cy.prefixedVisit('/');
+        cy.passThroughInitialRun();
+        cy.discoveryShouldFinish();
+    });
 
-    it('Goes to accounts and verifies that the "New default Accounts natively.." is displayed:', () => {
+    const expectedAccountMessage = 'New to Trezor Suite: BTC Bech32 accounts!';
+    it(`Goes to accounts and verifies that the "${expectedAccountMessage}" is displayed:`, () => {
         cy.getTestElement('@suite/menu/wallet-index').click();
         cy.getTestElement('@accounts/empty-account/receive');
         cy.getTestElement('@accounts/empty-account/default-native-account/close');
-        cy.contains('New default Accounts natively in Trezor Suite!');
+        cy.contains(expectedAccountMessage);
         // Clicks on Got it!
         cy.getTestElement('@accounts/empty-account/default-native-account/close').click({
             scrollBehavior: 'bottom',
@@ -28,13 +28,13 @@ describe('New accounts', () => {
         cy.getTestElement('@accounts/empty-account/receive');
         cy.getTestElement('@accounts/empty-account/buy');
         cy.getTestElement('@accounts/empty-account/default-native-account/close').should('not.exist');
-        cy.contains('New default Accounts natively in Trezor Suite!').should('not.exist');
+        cy.contains(expectedAccountMessage).should('not.exist');
         // Reload page.
         cy.reload();
         // Makes sure it is still gone.
         cy.getTestElement('@accounts/empty-account/receive');
         cy.getTestElement('@accounts/empty-account/buy');
         cy.getTestElement('@accounts/empty-account/default-native-account/close').should('not.exist');
-        cy.contains('New default Accounts natively in Trezor Suite!').should('not.exist');
+        cy.contains(expectedAccountMessage).should('not.exist');
     });
 });
