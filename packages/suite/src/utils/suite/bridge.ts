b9bd124d458e51bb7dd7647bc3f4b26738fccaa7
Szymon Lesisz <sz.lesisz@gmail.com>
2021-08-25 16:12:49 +0200

    feat(suite): use udev installers provided by trezor-connect

--- packages/suite/src/utils/suite/bridge.ts
+++
@@ -1,7 +0,0 @@
-export const getLinuxPackage = () => {
-    if (typeof navigator === 'undefined') return;
-    if (!navigator.appVersion.includes('Linux') || navigator.appVersion.includes('Android')) return;
-    return navigator.appVersion.match(/CentOS|Fedora|Mandriva|Mageia|Red Hat|Scientific|SUSE/)
-        ? 'rpm'
-        : 'deb';
-};
