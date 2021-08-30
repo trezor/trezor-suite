trezor/trezor-suite

commit dc4555740c911e29e1858e7e1b45b6cb25a3358f

feat(suite): use ButtonRequest object
ButtonRequests are stored in device object.
since FW 2.4.1 requests are extended by "page" and "pages" fields and they will be extended more in the future.

Allow suite to store whole object instead of only ButtonRequest.code (string).

packages/suite/src/components/suite/PinMatrix/index.tsx
@@ -39,11 +39,12 @@ const PinMatrix = ({ device, hideExplanation, invalid }: Props) => {
 
     useEffect(() => {
         if (
+            pinRequestType.code &&
             [
                 'PinMatrixRequestType_NewFirst',
                 'PinMatrixRequestType_NewSecond',
                 'PinMatrixRequestType_Current',
-            ].includes(pinRequestType)
+            ].includes(pinRequestType.code)
         ) {
             setSubmitted(false);
         }

packages/suite/src/components/suite/modals/Pin/index.tsx
@@ -13,7 +13,8 @@ type Props = OwnProps;
 
 const Pin = ({ device, cancelable, noBackground, noPadding, ...rest }: Props) => {
     const pinRequestType = device.buttonRequests[device.buttonRequests.length - 1];
-    const invalidCounter = device.buttonRequests.filter(r => r === 'ui-invalid_pin').length || 0;
+    const invalidCounter =
+        device.buttonRequests.filter(r => r.code === 'ui-invalid_pin').length || 0;
 
     if (!device.features) return null;
 
@@ -21,9 +22,11 @@ const Pin = ({ device, cancelable, noBackground, noPadding, ...rest }: Props) =>
     // 1) and 2) - Setting a new pin: 1 entry, 2nd (confirmation) entry
     // 3) Invalid pin (It doesn't seem to work anymore) instead separate PinMismatch modal is shown
     const isExtended =
-        ['PinMatrixRequestType_NewFirst', 'PinMatrixRequestType_NewSecond'].includes(
-            pinRequestType,
-        ) || invalidCounter > 0;
+        (pinRequestType.code &&
+            ['PinMatrixRequestType_NewFirst', 'PinMatrixRequestType_NewSecond'].includes(
+                pinRequestType.code,
+            )) ||
+        invalidCounter > 0;
 
     return (
         <Modal

packages/suite/src/components/suite/modals/ReviewTransaction/index.tsx
@@ -109,7 +109,7 @@ const ReviewTransaction = ({ decision }: Props) => {
 
     // omit other button requests (like passphrase)
     const buttonRequests = device.buttonRequests.filter(
-        r => r === 'ButtonRequest_ConfirmOutput' || r === 'ButtonRequest_SignTx',
+        r => r.code === 'ButtonRequest_ConfirmOutput' || r.code === 'ButtonRequest_SignTx',
     );
 
     // NOTE: T1 edge-case
@@ -118,7 +118,7 @@ const ReviewTransaction = ({ decision }: Props) => {
     if (
         typeof decreaseOutputId === 'number' &&
         device.features?.major_version === 1 &&
-        buttonRequests.filter(r => r === 'ButtonRequest_ConfirmOutput').length > 1
+        buttonRequests.filter(r => r.code === 'ButtonRequest_ConfirmOutput').length > 1
     ) {
         buttonRequests.splice(-1, 1);
     }

packages/suite/src/views/onboarding/steps/Pin/index.tsx
@@ -34,8 +34,9 @@ const SetPinStep = () => {
             // enter-pin and repeat-pin" states are set only while working with T1 (TT sends different request ButtonRequest_PinEntry and everything is done in touchscreen).
             // They are used to show better context-aware UI/texts (Right now it only changes a header from "Set a new PIN" to "Confirm PIN").
             // As the whole process on TT is done via touchscreen we don't really need to track anything besides 'initial' and 'success' states.
-            if (device.buttonRequests.includes('PinMatrixRequestType_NewFirst')) {
-                if (device.buttonRequests.includes('PinMatrixRequestType_NewSecond')) {
+            const buttonRequests = device.buttonRequests.map(r => r.code);
+            if (buttonRequests.includes('PinMatrixRequestType_NewFirst')) {
+                if (buttonRequests.includes('PinMatrixRequestType_NewSecond')) {
                     setStatus('repeat-pin');
                 } else {
                     setStatus('enter-pin');
@@ -58,7 +59,7 @@ const SetPinStep = () => {
     // First button request that will pop out of the device is "ButtonRequest_ProtectCall" (T1) or "ButtonRequest_Other" (T2), requesting us to confirm enabling PIN
     // buttonRequests will be cleared on cancelling the confirmation prompt on the device, turning this condition to false.
     const showConfirmationPrompt = device.buttonRequests.find(
-        b => b === 'ButtonRequest_Other' || b === 'ButtonRequest_ProtectCall',
+        b => b.code === 'ButtonRequest_Other' || b.code === 'ButtonRequest_ProtectCall',
     );
 
     if (showPinMismatch) {

packages/suite/src/views/onboarding/steps/ResetDevice/index.tsx
@@ -30,7 +30,7 @@ const ResetDeviceStep = () => {
     const isShamirBackupAvailable = device.features?.capabilities?.includes('Capability_Shamir');
     const isWaitingForConfirmation =
         device.buttonRequests.some(
-            r => r === 'ButtonRequest_ResetDevice' || r === 'ButtonRequest_ProtectCall',
+            r => r.code === 'ButtonRequest_ResetDevice' || r.code === 'ButtonRequest_ProtectCall',
         ) && !submitted; // ButtonRequest_ResetDevice is for TT, ButtonRequest_ProtectCall for T1
 
     // eslint-disable-next-line camelcase

packages/suite/src/actions/firmware/__fixtures__/firmwareActions.ts
@@ -211,7 +211,7 @@ export const reducerActions = [
         initialState: {},
         action: {
             type: SUITE.ADD_BUTTON_REQUEST,
-            payload: 'ButtonRequest_FirmwareUpdate',
+            payload: { code: 'ButtonRequest_FirmwareUpdate' },
         },
         result: {
             state: {

packages/suite/src/actions/suite/suiteActions.ts
@@ -7,16 +7,17 @@ import * as storageActions from '@suite-actions/storageActions';
 import { getOsTheme } from '@suite-utils/env';
 import { SUITE, METADATA } from './constants';
 import type { Locale } from '@suite-config/languages';
-import {
+import type {
     Action,
     Dispatch,
     GetState,
     TrezorDevice,
+    ButtonRequest,
     AppState,
     SuiteThemeVariant,
     SuiteThemeColors,
 } from '@suite-types';
-import { DebugModeOptions } from '@suite-reducers/suiteReducer';
+import type { DebugModeOptions } from '@suite-reducers/suiteReducer';
 
 export type SuiteAction =
     | { type: typeof SUITE.INIT }
@@ -65,7 +66,7 @@ export type SuiteAction =
     | {
           type: typeof SUITE.ADD_BUTTON_REQUEST;
           device: TrezorDevice | undefined;
-          payload?: string;
+          payload?: ButtonRequest;
       }
     | {
           type: typeof SUITE.SET_PROCESS_MODE;
@@ -83,7 +84,7 @@ export const removeButtonRequests = (device: TrezorDevice | undefined) => ({
     device,
 });
 
-export const addButtonRequest = (device: TrezorDevice | undefined, payload: string) => ({
+export const addButtonRequest = (device: TrezorDevice | undefined, payload: ButtonRequest) => ({
     type: SUITE.ADD_BUTTON_REQUEST,
     device,
     payload,

packages/suite/src/components/suite/modals/ReviewTransaction/components/OutputList.tsx
@@ -10,8 +10,9 @@ import Detail from './Detail';
 import Indicator from './Indicator';
 import Output, { OutputProps } from './Output';
 import OutputElement from './OutputElement';
-import { Account } from '@wallet-types';
-import { FormState, PrecomposedTransactionFinal } from '@wallet-types/sendForm';
+import type { ButtonRequest } from '@suite-types';
+import type { Account } from '@wallet-types';
+import type { FormState, PrecomposedTransactionFinal } from '@wallet-types/sendForm';
 
 const Content = styled.div`
     display: flex;
@@ -72,7 +73,7 @@ interface Props {
     decision?: { resolve: (success: boolean) => void }; // dfd
     detailsOpen: boolean;
     outputs: OutputProps[];
-    buttonRequests: string[];
+    buttonRequests: ButtonRequest[];
     isRbfAction: boolean;
     toggleDetails: () => void;
 }

commit 1427dd39695e862be2e89bfee513630cd68d2243

fix(suite): Pin request code detection
Bug introduced by dc4555740c911e29e1858e7e1b45b6cb25a3358f
ButtonRequest may be undefined at first render, it will by added after "SUITE.ADD_BUTTON_REQUEST" action

packages/suite/src/components/suite/PinMatrix/index.tsx
@@ -39,7 +39,7 @@ const PinMatrix = ({ device, hideExplanation, invalid }: Props) => {
 
     useEffect(() => {
         if (
-            pinRequestType.code &&
+            pinRequestType?.code &&
             [
                 'PinMatrixRequestType_NewFirst',
                 'PinMatrixRequestType_NewSecond',

packages/suite/src/components/suite/modals/Pin/index.tsx
@@ -22,7 +22,7 @@ const Pin = ({ device, cancelable, noBackground, noPadding, ...rest }: Props) =>
     // 1) and 2) - Setting a new pin: 1 entry, 2nd (confirmation) entry
     // 3) Invalid pin (It doesn't seem to work anymore) instead separate PinMismatch modal is shown
     const isExtended =
-        (pinRequestType.code &&
+        (pinRequestType?.code &&
             ['PinMatrixRequestType_NewFirst', 'PinMatrixRequestType_NewSecond'].includes(
                 pinRequestType.code,
             )) ||