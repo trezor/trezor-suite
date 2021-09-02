649f60740a2a762b4608b28a376dfa10d47073a9
Matej Kriz <matej@matejkriz.cz>
2021-09-02 11:53:32 +0200

    fix(suite,landing): fix resolution of static path

--- packages/suite/src/utils/suite/__tests__/build.test.ts
+++ packages/suite/src/utils/suite/__tests__/build.test.ts
@@ -64,9 +64,16 @@ describe('build', () => {
             expect(resolveStaticPath('mypath')).toBe('/static/mypath');
         });
 
+        it('should return static path even if ASSET_PREFIX is undefined', () => {
+            process.env.ASSET_PREFIX = undefined;
+            expect(resolveStaticPath('mypath')).toBe('/static/mypath');
+            expect(resolveStaticPath('/mypath')).toBe('/static/mypath');
+        });
+
         it('should return static path prefixed with ASSET_PREFIX', () => {
             process.env.ASSET_PREFIX = 'brachName';
             expect(resolveStaticPath('mypath')).toBe('brachName/static/mypath');
+            expect(resolveStaticPath('/mypath')).toBe('brachName/static/mypath');
         });
     });
 });
