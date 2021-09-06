dbcd2ab2aa5c0b269a1ea2cc9afc788e7f278299
pingpwned <42174681+pingpwned@users.noreply.github.com>
2021-09-06 11:49:17 +0200

    fix(suite): fix firmware version & changelog UI

--- packages/suite/src/components/firmware/FirmwareOffer.tsx
+++ packages/suite/src/components/firmware/FirmwareOffer.tsx
@@ -98,7 +98,10 @@ const FirmwareOffer = ({ device, customFirmware }: Props) => {
     ) : (
         getFwUpdateVersion(device)
     );
-    const parsedChangelog = !customFirmware && parseFirmwareChangelog(device.firmwareRelease);
+
+    const parsedChangelog =
+        !customFirmware && parseFirmwareChangelog(device.features, device.firmwareRelease);
+
     const bitcoinOnlyVersion = isBitcoinOnly(device) && ' (bitcoin-only)';
 
     return (
@@ -132,37 +135,35 @@ const FirmwareOffer = ({ device, customFirmware }: Props) => {
                             dashed
                             content={
                                 <Changelog>
-                                    {parsedChangelog.slice(0, 1).map(log => (
-                                        <ChangelogGroup key={log.versionString}>
-                                            <ChangelogHeading>
-                                                <Translation
-                                                    id="TR_VERSION"
-                                                    values={{ version: log.versionString }}
-                                                />
-                                                <TrezorLink
-                                                    size="small"
-                                                    variant="nostyle"
-                                                    href={CHANGELOG_URL}
+                                    <ChangelogGroup key={parsedChangelog.versionString}>
+                                        <ChangelogHeading>
+                                            <Translation
+                                                id="TR_VERSION"
+                                                values={{ version: parsedChangelog.versionString }}
+                                            />
+                                            <TrezorLink
+                                                size="small"
+                                                variant="nostyle"
+                                                href={CHANGELOG_URL}
+                                            >
+                                                <Button
+                                                    variant="tertiary"
+                                                    icon="EXTERNAL_LINK"
+                                                    alignIcon="right"
                                                 >
-                                                    <Button
-                                                        variant="tertiary"
-                                                        icon="EXTERNAL_LINK"
-                                                        alignIcon="right"
-                                                    >
-                                                        View all
-                                                    </Button>
-                                                </TrezorLink>
-                                            </ChangelogHeading>
-                                            <ChangesUl>
-                                                {/* render individual changes for a given version */}
-                                                {log.changelog.map(
-                                                    change =>
-                                                        // return only if change is not an empty array
-                                                        change && <li key={change}>{change}</li>,
-                                                )}
-                                            </ChangesUl>
-                                        </ChangelogGroup>
-                                    ))}
+                                                    View all
+                                                </Button>
+                                            </TrezorLink>
+                                        </ChangelogHeading>
+                                        <ChangesUl>
+                                            {/* render individual changes for a given version */}
+                                            {parsedChangelog.changelog.map(
+                                                change =>
+                                                    // return only if change is not an empty array
+                                                    change && <li key={change}>{change}</li>,
+                                            )}
+                                        </ChangesUl>
+                                    </ChangelogGroup>
                                 </Changelog>
                             }
                             placement="top"
