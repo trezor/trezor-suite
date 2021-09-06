6d69a796a980d91836abc5338a75ec00a9ba1d6f
tomasklima <tomas.klim@gmail.com>
2021-09-06 10:23:46 +0200

    chore(suite-desktop): update electron-builder and handle -x64 extension
    electron-builder from version 22.11.1 adds -x64 to 64-bit win/mac installation files

--- packages/suite-web-landing/utils/navigator.ts
+++ packages/suite-web-landing/utils/navigator.ts
@@ -1,15 +1,15 @@
-export type Platform = 'linux-arm64' | 'linux-x86_64' | 'mac' | 'mac-arm64' | 'win';
+export type Platform = 'linux-arm64' | 'linux-x86_64' | 'mac-x64' | 'mac-arm64' | 'win-x64';
 
 export const getPlatform = (navigator: Navigator): Platform => {
     const macPlatforms = ['Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'];
     const windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'];
     if (macPlatforms.includes(navigator.platform)) {
         // navigator.platform returns "MacIntel" on Apple Silicon too
-        // so let's always return "mac" for now
-        return 'mac';
+        // so let's always return "mac-x64" for now
+        return 'mac-x64';
     }
     if (windowsPlatforms.includes(navigator.platform)) {
-        return 'win';
+        return 'win-x64';
     }
     if (navigator.platform.includes('aarch64')) {
         return 'linux-arm64';
