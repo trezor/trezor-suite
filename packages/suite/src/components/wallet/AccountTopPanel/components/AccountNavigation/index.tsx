9e2b171f359f4ad5e4e7379464aedfd1b0d0de88
Marek Polak <marek.polak@satoshilabs.com>
2021-09-21 16:42:02 +0200

    feat(suite): add sign&verify option to menu

--- packages/suite/src/components/wallet/AccountTopPanel/components/AccountNavigation/index.tsx
+++ packages/suite/src/components/wallet/AccountTopPanel/components/AccountNavigation/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Account } from '@wallet-types';
-import AppNavigation, { AppNavigationItem } from '@suite/components/suite/AppNavigation';
+import AppNavigation, { AppNavigationItem } from '@suite-components/AppNavigation';
 import { Translation } from '@suite-components/Translation';
 import { useActions } from '@suite-hooks';
 import * as routerActions from '@suite-actions/routerActions';
 import * as modalActions from '@suite-actions/modalActions';
+import { hasSignVerify } from '@wallet-utils/accountUtils';
 
 interface Props {
     account?: Account;
@@ -94,7 +95,7 @@ const AccountNavigation = (props: Props) => {
             icon: 'SIGN',
             position: 'secondary',
             extra: true,
-            isHidden: () => true,
+            isHidden: () => !account || !hasSignVerify(account),
         },
     ];
 
--- packages/suite/src/config/wallet/networks.ts
+++ packages/suite/src/config/wallet/networks.ts
@@ -8,7 +8,6 @@ const networks = [
         networkType: 'bitcoin',
         symbol: 'btc',
         bip44: "m/84'/0'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://btc1.trezor.io/tx/',
@@ -22,7 +21,6 @@ const networks = [
         accountType: 'segwit',
         symbol: 'btc',
         bip44: "m/49'/0'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://btc1.trezor.io/tx/',
@@ -36,7 +34,6 @@ const networks = [
         accountType: 'legacy',
         symbol: 'btc',
         bip44: "m/44'/0'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://btc1.trezor.io/tx/',
@@ -50,7 +47,6 @@ const networks = [
         networkType: 'bitcoin',
         symbol: 'ltc',
         bip44: "m/84'/2'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://ltc1.trezor.io/tx/',
@@ -64,7 +60,6 @@ const networks = [
         accountType: 'segwit',
         symbol: 'ltc',
         bip44: "m/49'/2'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://ltc1.trezor.io/tx/',
@@ -78,7 +73,6 @@ const networks = [
         accountType: 'legacy',
         symbol: 'ltc',
         bip44: "m/44'/2'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://ltc1.trezor.io/tx/',
@@ -93,7 +87,6 @@ const networks = [
         symbol: 'eth',
         chainId: 1,
         bip44: "m/44'/60'/0'/0/i",
-        hasSignVerify: true,
         decimals: 18,
         explorer: {
             tx: 'https://eth1.trezor.io/tx/',
@@ -109,7 +102,6 @@ const networks = [
         symbol: 'etc',
         chainId: 61,
         bip44: "m/44'/61'/0'/0/i",
-        hasSignVerify: true,
         decimals: 18,
         explorer: {
             tx: 'https://etc1.trezor.io/tx/',
@@ -146,7 +138,6 @@ const networks = [
         networkType: 'bitcoin',
         symbol: 'btg',
         bip44: "m/49'/156'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://btg1.trezor.io/tx/',
@@ -160,7 +151,6 @@ const networks = [
         accountType: 'legacy',
         symbol: 'btg',
         bip44: "m/44'/156'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://btg1.trezor.io/tx/',
@@ -173,7 +163,6 @@ const networks = [
         networkType: 'bitcoin',
         symbol: 'dash',
         bip44: "m/44'/5'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://dash1.trezor.io/tx/',
@@ -186,7 +175,6 @@ const networks = [
         networkType: 'bitcoin',
         symbol: 'dgb',
         bip44: "m/49'/20'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://dgb1.trezor.io/tx/',
@@ -200,7 +188,6 @@ const networks = [
         accountType: 'legacy',
         symbol: 'dgb',
         bip44: "m/44'/20'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://dgb1.trezor.io/tx/',
@@ -213,7 +200,6 @@ const networks = [
         networkType: 'bitcoin',
         symbol: 'doge',
         bip44: "m/44'/3'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://doge1.trezor.io/tx/',
@@ -226,7 +212,6 @@ const networks = [
         networkType: 'bitcoin',
         symbol: 'nmc',
         bip44: "m/44'/7'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://nmc1.trezor.io/tx/',
@@ -239,7 +224,6 @@ const networks = [
         networkType: 'bitcoin',
         symbol: 'vtc',
         bip44: "m/49'/28'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://vtc1.trezor.io/tx/',
@@ -253,7 +237,6 @@ const networks = [
         accountType: 'legacy',
         symbol: 'vtc',
         bip44: "m/44'/28'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://vtc1.trezor.io/tx/',
@@ -266,7 +249,6 @@ const networks = [
         networkType: 'bitcoin',
         symbol: 'zec',
         bip44: "m/44'/133'/i'",
-        hasSignVerify: true,
         decimals: 8,
         explorer: {
             tx: 'https://zec1.trezor.io/tx/',
@@ -280,7 +262,6 @@ const networks = [
         networkType: 'bitcoin',
         symbol: 'test',
         bip44: "m/84'/1'/i'",
-        hasSignVerify: true,
         decimals: 8,
         testnet: true,
         label: 'TR_TESTNET_COINS_LABEL',
@@ -296,7 +277,6 @@ const networks = [
         accountType: 'segwit',
         symbol: 'test',
         bip44: "m/49'/1'/i'",
-        hasSignVerify: true,
         decimals: 8,
         testnet: true,
         label: 'TR_TESTNET_COINS_LABEL',
@@ -312,7 +292,6 @@ const networks = [
         accountType: 'legacy',
         symbol: 'test',
         bip44: "m/44'/1'/i'",
-        hasSignVerify: true,
         decimals: 8,
         testnet: true,
         label: 'TR_TESTNET_COINS_LABEL',
@@ -327,7 +306,6 @@ const networks = [
         networkType: 'ethereum',
         symbol: 'trop',
         bip44: "m/44'/1'/0'/0/i",
-        hasSignVerify: true,
         chainId: 3,
         decimals: 18,
         testnet: true,
@@ -358,7 +336,6 @@ type Network = {
     testnet?: boolean;
     isHidden?: boolean;
     chainId?: number;
-    hasSignVerify?: boolean;
     features?: string[];
     label?: ExtendedMessageDescriptor['id'];
     tooltip?: ExtendedMessageDescriptor['id'];
