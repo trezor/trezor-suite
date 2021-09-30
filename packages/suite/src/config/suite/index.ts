263d3edb28080fb05fa107f161a54a9ad9500171
tomasklima <tomas.klim@gmail.com>
2021-09-30 14:53:22 +0200

    chore(suite): remove unused supported browsers object

--- packages/suite/src/config/suite/index.ts
+++ packages/suite/src/config/suite/index.ts
@@ -1,8 +1,7 @@
 import LANGUAGES from './languages';
 import FIAT from './fiat';
 import SETTINGS from './settings';
-import SUPPORTED_BROWSERS from './supportedBrowsers';
 import SENTRY_CONFIG from './sentry';
 import ANIMATION from './animation';
 
-export { LANGUAGES, FIAT, SETTINGS, SUPPORTED_BROWSERS, SENTRY_CONFIG, ANIMATION };
+export { LANGUAGES, FIAT, SETTINGS, SENTRY_CONFIG, ANIMATION };
