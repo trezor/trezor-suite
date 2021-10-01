10f7e27eb0251a88d892294496b082b9d8891c31
Szymon Lesisz <sz.lesisz@gmail.com>
2021-10-01 13:41:52 +0200

    fix(components): use ts-loader@8.3.0 for storybook builds

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -1,7 +1,15 @@
+const path = require('path');
+
 module.exports = {
     stories: ['../src/**/*.stories.*'],
     logLevel: 'debug',
     webpackFinal: async config => {
+        // for typescript use ts-loader@8.3.0 compatible with webpack@4 (storybook dependency)
+        // probably may be removed after storybook@6.4.0 with webpack@5 support
+        config.module.rules.unshift({
+            test: /\.tsx?$/,
+            loader: path.resolve('./node_modules/ts-loader/index.js'),
+        })
         return config;
     },
     addons: [
