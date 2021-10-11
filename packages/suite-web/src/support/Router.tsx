2791ffe0bddb95f022b2c8a9a6cd90d1ef6826d8
tomasklima <tomas.klim@gmail.com>
2021-10-11 16:06:15 +0200

    chore(suite): do not generate chunk for each view

--- packages/suite-web/src/support/Router.tsx
+++ packages/suite-web/src/support/Router.tsx
@@ -5,36 +5,71 @@ import routes from '@suite-constants/routes';
 import { BundleLoader } from '@suite-components';
 
 const components: { [key: string]: React.LazyExoticComponent<any> } = {
-    'suite-index': lazy(() => import('@dashboard-views')),
-    'notifications-index': lazy(() => import('@suite-views/notifications')),
+    'suite-index': lazy(() => import(/* webpackChunkName: "dashboard" */ '@dashboard-views')),
+    'notifications-index': lazy(
+        () => import(/* webpackChunkName: "notifications" */ '@suite-views/notifications'),
+    ),
 
-    'wallet-index': lazy(() => import('@wallet-views/transactions')),
-    'wallet-receive': lazy(() => import('@wallet-views/receive')),
-    'wallet-details': lazy(() => import('@wallet-views/details')),
-    'wallet-tokens': lazy(() => import('@wallet-views/tokens')),
-    'wallet-send': lazy(() => import('@wallet-views/send')),
-    'wallet-sign-verify': lazy(() => import('@wallet-views/sign-verify')),
+    // wallet
+    'wallet-index': lazy(
+        () => import(/* webpackChunkName: "wallet" */ '@wallet-views/transactions'),
+    ),
+    'wallet-receive': lazy(() => import(/* webpackChunkName: "wallet" */ '@wallet-views/receive')),
+    'wallet-details': lazy(() => import(/* webpackChunkName: "wallet" */ '@wallet-views/details')),
+    'wallet-tokens': lazy(() => import(/* webpackChunkName: "wallet" */ '@wallet-views/tokens')),
+    'wallet-send': lazy(() => import(/* webpackChunkName: "wallet" */ '@wallet-views/send')),
+    'wallet-sign-verify': lazy(
+        () => import(/* webpackChunkName: "wallet" */ '@wallet-views/sign-verify'),
+    ),
 
-    'wallet-coinmarket-buy': lazy(() => import('@wallet-views/coinmarket/buy')),
-    'wallet-coinmarket-buy-detail': lazy(() => import('@wallet-views/coinmarket/buy/detail')),
-    'wallet-coinmarket-buy-offers': lazy(() => import('@wallet-views/coinmarket/buy/offers')),
-    'wallet-coinmarket-sell': lazy(() => import('@wallet-views/coinmarket/sell')),
-    'wallet-coinmarket-sell-detail': lazy(() => import('@wallet-views/coinmarket/sell/detail')),
-    'wallet-coinmarket-sell-offers': lazy(() => import('@wallet-views/coinmarket/sell/offers')),
-    'wallet-coinmarket-exchange': lazy(() => import('@wallet-views/coinmarket/exchange')),
+    // coinmarket
+    'wallet-coinmarket-buy': lazy(
+        () => import(/* webpackChunkName: "coinmarket" */ '@wallet-views/coinmarket/buy'),
+    ),
+    'wallet-coinmarket-buy-detail': lazy(
+        () => import(/* webpackChunkName: "coinmarket" */ '@wallet-views/coinmarket/buy/detail'),
+    ),
+    'wallet-coinmarket-buy-offers': lazy(
+        () => import(/* webpackChunkName: "coinmarket" */ '@wallet-views/coinmarket/buy/offers'),
+    ),
+    'wallet-coinmarket-sell': lazy(
+        () => import(/* webpackChunkName: "coinmarket" */ '@wallet-views/coinmarket/sell'),
+    ),
+    'wallet-coinmarket-sell-detail': lazy(
+        () => import(/* webpackChunkName: "coinmarket" */ '@wallet-views/coinmarket/sell/detail'),
+    ),
+    'wallet-coinmarket-sell-offers': lazy(
+        () => import(/* webpackChunkName: "coinmarket" */ '@wallet-views/coinmarket/sell/offers'),
+    ),
+    'wallet-coinmarket-exchange': lazy(
+        () => import(/* webpackChunkName: "coinmarket" */ '@wallet-views/coinmarket/exchange'),
+    ),
     'wallet-coinmarket-exchange-detail': lazy(
-        () => import('@wallet-views/coinmarket/exchange/detail'),
+        () =>
+            import(/* webpackChunkName: "coinmarket" */ '@wallet-views/coinmarket/exchange/detail'),
     ),
     'wallet-coinmarket-exchange-offers': lazy(
-        () => import('@wallet-views/coinmarket/exchange/offers'),
+        () =>
+            import(/* webpackChunkName: "coinmarket" */ '@wallet-views/coinmarket/exchange/offers'),
     ),
-    'wallet-coinmarket-spend': lazy(() => import('@wallet-views/coinmarket/spend')),
-    'wallet-coinmarket-redirect': lazy(() => import('@wallet-views/coinmarket/redirect')),
+    'wallet-coinmarket-spend': lazy(
+        () => import(/* webpackChunkName: "coinmarket" */ '@wallet-views/coinmarket/spend'),
+    ),
+    'wallet-coinmarket-redirect': lazy(
+        () => import(/* webpackChunkName: "coinmarket" */ '@wallet-views/coinmarket/redirect'),
+    ),
 
-    'settings-index': lazy(() => import('@settings-views')),
-    'settings-coins': lazy(() => import('@settings-views/coins')),
-    'settings-debug': lazy(() => import('@settings-views/debug')),
-    'settings-device': lazy(() => import('@settings-views/device')),
+    // settings
+    'settings-index': lazy(() => import(/* webpackChunkName: "settings" */ '@settings-views')),
+    'settings-coins': lazy(
+        () => import(/* webpackChunkName: "settings" */ '@settings-views/coins'),
+    ),
+    'settings-debug': lazy(
+        () => import(/* webpackChunkName: "settings" */ '@settings-views/debug'),
+    ),
+    'settings-device': lazy(
+        () => import(/* webpackChunkName: "settings" */ '@settings-views/device'),
+    ),
 };
 
 const AppRouter = () => (
