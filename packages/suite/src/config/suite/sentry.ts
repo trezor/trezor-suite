b542214fc9db69f043e1e97edb09868049392a73
Matej Kriz <matej@matejkriz.cz>
2021-10-29 17:11:22 +0200

    feat(sentry): update sentry config to disable sample rating and keep normalizeDepth on default 3

--- packages/suite/src/config/suite/sentry.ts
+++ packages/suite/src/config/suite/sentry.ts
@@ -6,8 +6,6 @@ export const allowReportTag = 'allowReport';
 export default {
     dsn: 'https://6d91ca6e6a5d4de7b47989455858b5f6@o117836.ingest.sentry.io/5193825',
     autoSessionTracking: false, // do not send analytical data to Sentry
-    normalizeDepth: 10,
-    sampleRate: 0.5,
     integrations: [
         new CaptureConsole({
             levels: ['error'],
