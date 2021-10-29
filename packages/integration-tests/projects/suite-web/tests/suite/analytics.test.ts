b9c1586cf114021fe3cc2e0fadeceaf3c3b81f46
Matej Kriz <matej@matejkriz.cz>
2021-10-29 17:09:33 +0200

    fix(analytics): skip report until user confirm analytics reporting
    - stronger and more safe condition than initialRun

--- packages/integration-tests/projects/suite-web/tests/suite/analytics.test.ts
+++ packages/integration-tests/projects/suite-web/tests/suite/analytics.test.ts
@@ -37,17 +37,18 @@ describe('Analytics', () => {
         cy.getTestElement('@onboarding/continue-button').click();
         cy.getTestElement('@onboarding/exit-app-button').click();
 
-        // assert that only 1 request was fired
+        // assert that only 2 requests was fired
         cy.wait('@data-fetch');
-        cy.wrap(requests).its(0).its('c_session_id').as('request0');
-        cy.wrap(requests).its(0).should('have.property', 'c_type', 'initial-run-completed');
+        cy.wrap(requests).its(0).should('have.property', 'c_type', 'analytics/dispose');
+        cy.wrap(requests).its(1).its('c_session_id').as('request0');
+        cy.wrap(requests).its(1).should('have.property', 'c_type', 'initial-run-completed');
         cy.wrap(requests)
-            .its(0)
+            .its(1)
             .should('have.property', 'c_timestamp')
             .should('match', timestamp)
             .as('timestamp0');
-        cy.wrap(requests).its(0).should('have.property', 'analytics', 'false');
-        cy.wrap(requests).its(1).should('equal', undefined);
+        cy.wrap(requests).its(1).should('have.property', 'analytics', 'false');
+        cy.wrap(requests).its(2).should('equal', undefined);
 
         // important, suite needs time to save initialRun flag into storage
         cy.getTestElement('@suite/loading').should('not.exist');
@@ -62,18 +63,19 @@ describe('Analytics', () => {
 
         // analytics is not enabled and no additional requests were fired
         cy.getTestElement('@analytics/toggle-switch').should('not.be.checked');
-        cy.wrap(requests).its(0).should('have.property', 'c_type', 'initial-run-completed');
-        cy.wrap(requests).its(0).should('have.property', 'analytics', 'false');
-        cy.wrap(requests).its(1).should('equal', undefined);
+        cy.wrap(requests).its(0).should('have.property', 'c_type', 'analytics/dispose');
+        cy.wrap(requests).its(1).should('have.property', 'c_type', 'initial-run-completed');
+        cy.wrap(requests).its(1).should('have.property', 'analytics', 'false');
+        cy.wrap(requests).its(2).should('equal', undefined);
 
         // enable it and reload page
         cy.log('enable it again, reload and see it remains checked');
         cy.getTestElement('@analytics/toggle-switch').click({ force: true });
         cy.getTestElement('@analytics/toggle-switch').should('be.checked');
         cy.wait('@data-fetch');
-        cy.wrap(requests).its(1).should('have.property', 'c_type', 'analytics/enable');
+        cy.wrap(requests).its(2).should('have.property', 'c_type', 'analytics/enable');
         cy.wrap(requests)
-            .its(1)
+            .its(2)
             .should('have.property', 'c_timestamp')
             .should('match', timestamp)
             .as('timestamp1');
@@ -91,10 +93,10 @@ describe('Analytics', () => {
         // check that fiat change got logged.
 
         cy.wait('@data-fetch');
-        cy.wrap(requests).its(2).its('c_session_id').as('request1');
-        cy.wrap(requests).its(2).should('have.property', 'c_type', 'settings/general/change-fiat');
-        cy.wrap(requests).its(2).should('have.property', 'fiat', 'huf');
-        cy.wrap(requests).its(2).should('have.property', 'c_instance_id').should('match', instance);
+        cy.wrap(requests).its(3).its('c_session_id').as('request1');
+        cy.wrap(requests).its(3).should('have.property', 'c_type', 'settings/general/change-fiat');
+        cy.wrap(requests).its(3).should('have.property', 'fiat', 'huf');
+        cy.wrap(requests).its(3).should('have.property', 'c_instance_id').should('match', instance);
         // and check that session ids changed after reload;
         cy.get('@request0').then(r0 => {
             cy.get('@request1').then(r1 => {
@@ -105,13 +107,13 @@ describe('Analytics', () => {
         // opening device modal
         cy.getTestElement('@menu/switch-device').click();
         cy.wait('@data-fetch');
-        cy.wrap(requests).its(4).should('have.property', 'c_type', 'menu/goto/switch-device');
+        cy.wrap(requests).its(5).should('have.property', 'c_type', 'menu/goto/switch-device');
 
         // adding wallet
         cy.getTestElement('@switch-device/add-hidden-wallet-button').click();
         cy.wait('@data-fetch');
         cy.wrap(requests)
-            .its(5)
+            .its(6)
             .should('have.property', 'c_type', 'switch-device/add-hidden-wallet');
     });
 });
--- packages/integration-tests/projects/suite-web/tests/suite/initial-run.test.ts
+++ packages/integration-tests/projects/suite-web/tests/suite/initial-run.test.ts
@@ -11,9 +11,14 @@ describe('Suite initial run', () => {
 
     it('Until user passed through initial run, it will be there after reload', () => {
         cy.prefixedVisit('/');
+        cy.getTestElement('@analytics/toggle-switch').should('be.visible');
+        cy.reload();
+        // analytics screen is there until user confirms his choice
+        cy.getTestElement('@analytics/toggle-switch').should('be.visible');
         cy.getTestElement('@onboarding/continue-button').click();
         cy.reload();
-        cy.getTestElement('@onboarding/continue-button').click();
+        cy.getTestElement('@analytics/toggle-switch').should('not.exist');
+        cy.getTestElement('@onboarding/exit-app-button').should('be.visible');
     });
 
     it('Once user passed trough, skips initial run and shows connect-device modal', () => {
