bdee9d1d857b893cd7d8f573c6af8d6e80a9a532
tomasklima <tomas.klim@gmail.com>
2021-11-03 17:19:57 +0100

    fix(suite-desktop): do not launch dev build before app is built

--- packages/suite-build/plugins/shell-spawn-plugin.ts
+++ packages/suite-build/plugins/shell-spawn-plugin.ts
@@ -1,9 +1,10 @@
 import webpack from 'webpack';
-import { spawn } from 'child_process';
+import { spawn, spawnSync } from 'child_process';
 
 interface Command {
     command: string;
     args: string[];
+    isSync?: boolean;
 }
 
 interface Options {
@@ -28,8 +29,12 @@ class ShellSpawnPlugin {
 
     apply(compiler: webpack.Compiler) {
         compiler.hooks.afterEmit.tap('ShellSpawnPlugin', (_: webpack.Compilation) => {
-            const execute = ({ command, args }: Command) =>
-                spawn(command, args, { stdio: 'inherit', cwd: this.options.cwd });
+            const execute = ({ command, args, isSync }: Command) => {
+                if (isSync) {
+                    return spawnSync(command, args, { stdio: 'inherit', cwd: this.options.cwd });
+                }
+                return spawn(command, args, { stdio: 'inherit', cwd: this.options.cwd });
+            };
 
             if (!this.initialRun) {
                 this.initialRun = true;
