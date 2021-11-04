d873a92afcbc9c523ca2e374cde09bdf91f1dd06
pingpwned <pingpwned@nblx.me>
2021-11-04 09:51:15 +0100

    fix: inverse favicon if system mode changed

---
+++ packages/suite-web-landing/utils/switchFavicon.ts
@@ -0,0 +1,22 @@
+import { resolveStaticPath } from '@suite-utils/build';
+// switch favicon if dark mode
+export const switchFavicon = () => {
+    const icon = document.querySelector('link[rel=icon]');
+    const appleIcon = document.querySelector('link[rel=apple-touch-icon]');
+    const switchIcons = (isDarkMode: boolean) => {
+        if (isDarkMode) {
+            icon?.setAttribute('href', resolveStaticPath('/images/icons/favicon/favicon_dm.png'));
+            appleIcon?.setAttribute(
+                'href',
+                resolveStaticPath('/images/icons/favicon/favicon_dm.png'),
+            );
+        } else {
+            icon?.setAttribute('href', resolveStaticPath('/images/icons/favicon/favicon.png'));
+            appleIcon?.setAttribute('href', resolveStaticPath('/images/icons/favicon/favicon.png'));
+        }
+    };
+    window
+        .matchMedia('(prefers-color-scheme: dark)')
+        .addEventListener('change', e => switchIcons(e.matches));
+    switchIcons(window.matchMedia('(prefers-color-scheme: dark)').matches);
+};
