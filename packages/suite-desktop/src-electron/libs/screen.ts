e324bbbdd6139c32c29d6c0b62364fa70ac3dfa2
tomasklima <tomas.klim@gmail.com>
2021-11-08 09:14:18 +0100

    fix(suite-desktop): round window width/height to avoid default sizes

--- packages/suite-desktop/src-electron/libs/screen.ts
+++ packages/suite-desktop/src-electron/libs/screen.ts
@@ -1,29 +1,22 @@
 import { screen } from 'electron';
 
-// Constants
 export const MIN_WIDTH = 770;
 export const MIN_HEIGHT = 700;
 export const MAX_WIDTH = 1920;
 export const MAX_HEIGHT = 1080;
-export const BUFFER = 0.2;
+export const WINDOW_SIZE_FACTOR = 0.8;
 
-// Functions
 export const getInitialWindowSize = () => {
     const { bounds } = screen.getPrimaryDisplay();
 
-    const buffer = {
-        width: bounds.width * BUFFER,
-        height: bounds.height * BUFFER,
-    };
-
-    let width = bounds.width - buffer.width;
+    let width = Math.floor(bounds.width * WINDOW_SIZE_FACTOR);
     if (width <= MIN_WIDTH) {
         width = MIN_WIDTH;
     } else if (width >= MAX_WIDTH) {
         width = MAX_WIDTH;
     }
 
-    let height = bounds.height - buffer.height;
+    let height = Math.floor(bounds.height * WINDOW_SIZE_FACTOR);
     if (height <= MIN_HEIGHT) {
         height = MIN_HEIGHT;
     } else if (height >= MAX_HEIGHT) {
