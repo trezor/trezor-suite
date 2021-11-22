092ebe4ffb4901f80e5d0c892ab16f5b647ac349
mroz <mvarmuza@gmail.com>
2021-11-22 16:46:17 +0100

    test: desktop e2e

---
+++ packages/integration-tests/projects/suite-desktop/playwright.config.ts
@@ -0,0 +1,6 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+    testDir: 'tests',
+};
+export default config;
---
+++ packages/integration-tests/projects/suite-desktop/tests/first.spec.ts
@@ -0,0 +1,52 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { _electron: electron } = require('playwright');
+const { test, expect } = require('@playwright/test');
+const { Controller } = require('../../../websocket-client');
+
+let window;
+let electronApp;
+
+test.beforeAll(async () => {
+    // Launch Electron app.
+    electronApp = await electron.launch({
+        cwd: '../suite-desktop',
+        args: ['./dist/app.js'],
+    });
+});
+
+const getTestElement = async (window, dataTest) => {
+    const selector = `[data-test="${dataTest}"]`;
+    const el = await window.locator(selector);
+    if (!el) {
+        // todo: normally I would retry here. Is there built in retryability in playwright?
+        throw new Error(`element: ${selector} does not exist`);
+    }
+    await expect(el).toBeVisible();
+    return el;
+};
+
+test('The first test', async () => {
+    window = await electronApp.firstWindow();
+    const title = await window.title();
+    expect(title).toBe('Trezor Suite');
+
+    const controller = new Controller({ url: 'ws://localhost:9001/' });
+    await controller.connect();
+    // todo: this shouldn't be needed. desktop should run its own bridge on localhost?
+    await controller.send({ type: 'bridge-start' });
+
+    await getTestElement(window, '@welcome/title');
+    await window.screenshot({ path: './projects/suite-desktop/screenshots/intro.png' });
+
+    await controller.send({
+        type: 'emulator-start',
+    });
+
+    await getTestElement(window, '@onboarding/continue-button');
+    await window.screenshot({ path: './projects/suite-desktop/screenshots/analytics.png' });
+});
+
+test.afterAll(async () => {
+    // Exit app.
+    await electronApp.close();
+});
