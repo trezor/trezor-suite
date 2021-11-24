db7157312b6b6fb2322ebe3415eb001557cf862e
martin <30367552+mroz22@users.noreply.github.com>
2021-11-24 16:23:53 +0100

    upgrade to typescript and refactor protobuf layer (#44)

--- packages/transport/shell.nix
+++ packages/transport/shell.nix
@@ -1,8 +1,8 @@
-# the last successful build of nixos-20.09 (stable) as of 2020-10-11
+# the last successful build of nixos-21.05 (stable) as of 2021-08-02
 with import
   (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/0b8799ecaaf0dc6b4c11583a3c96ca5b40fcfdfb.tar.gz";
-    sha256 = "11m4aig6cv0zi3gbq2xn9by29cfvnsxgzf9qsvz67qr0yq29ryyz";
+    url = "https://github.com/NixOS/nixpkgs/archive/16bf3980bfa0d8929639be93fa8491ebad9d61ec.tar.gz";
+    sha256 = "0azsnd2pjg53siv97n5l62j0c8b5whi5bd5a2wqz1sphkirf3cgq";
   })
 { };
 
@@ -11,12 +11,11 @@ stdenv.mkDerivation {
   buildInputs = [
     autoPatchelfHook
     git
-    nodejs-12_x
-    (yarn.override { nodejs = nodejs-12_x; })
+    nodejs
+    yarn
   ];
   shellHook = ''
     export HISTFILE=".nix_bash_history"
     export PATH="$PATH:$(pwd)/node_modules/.bin"
-    autoPatchelf $(pwd)/node_modules/flow-bin/
   '';
 }
