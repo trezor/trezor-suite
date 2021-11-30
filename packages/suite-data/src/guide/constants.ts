389a53ba0be6de827c9b607a8f33412ea9e89530
tomasklima <tomas.klim@gmail.com>
2021-11-30 16:33:00 +0100

    feat(guide): update articles

--- packages/suite-data/src/guide/constants.ts
+++ packages/suite-data/src/guide/constants.ts
@@ -1,7 +1,7 @@
 import { resolve, join } from 'path';
 
 export const GITBOOK_SOURCE = 'https://github.com/trezor/trezor-suite-guide.git';
-export const GITBOOK_REVISION = '4b4a84ff90608d1cb34d44aeaa3d5e0b5c6a608d';
+export const GITBOOK_REVISION = 'f2be6e51228685627d7cc71237dad655d524e8c1';
 export const TMP = join(resolve(__dirname, '../..'), 'tmp', 'guide');
 // Path to the GitBook assets. Relative to TMP.
 export const ASSETS_DIR = join('.gitbook', 'assets');
