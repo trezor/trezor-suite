5015c4f5df2826a8473bdc8c82c6425bc0eabb1b
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2021-12-01 10:08:34 +0100

    fixup! fix(icons): change name from hologram to universal error

--- packages/suite/src/components/firmware/index.tsx
+++ packages/suite/src/components/firmware/index.tsx
@@ -5,17 +5,8 @@ export { FirmwareInstallation } from './FirmwareInstallation';
 export { FirmwareInitial } from './FirmwareInitial';
 export { Fingerprint } from './Fingerprint';
 export { SelectCustomFirmware } from './SelectCustomFirmware';
-export { RetryButton, ContinueButton, InstallButton, CloseButton, ConfirmButton } from './Button';
-export {
-    InitImg,
-    SuccessImg,
-    ErrorImg,
-    DisconnectImg,
-    WarningImg,
-    ConnectInNormalImg,
-    ConnectInBootloaderImg,
-    SeedImg,
-} from './Img';
+export { RetryButton, ContinueButton, InstallButton, CloseButton } from './Button';
+
 // todo: this should be unified in @trezor/components.
 //  When done, implementations of this export should be changed to {P, H1, H2} from @trezor/components
 export { P, H1, H2 } from './Typography';
