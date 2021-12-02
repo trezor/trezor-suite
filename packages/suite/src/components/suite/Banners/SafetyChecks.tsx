a1ff4e8d6715a1c11bbe576b32c46a5a52b25842
Szymon Lesisz <sz.lesisz@gmail.com>
2021-12-02 10:11:05 +0100

    fix(suite): open safety-checks modal from banner
    Open modal from any route instead of navigating back to settings/device.

--- packages/suite/src/components/suite/Banners/SafetyChecks.tsx
+++ packages/suite/src/components/suite/Banners/SafetyChecks.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Translation } from '@suite-components';
 import { useActions } from '@suite-hooks';
+import * as modalActions from '@suite-actions/modalActions';
 import Wrapper from './components/Wrapper';
-import * as routerActions from '@suite/actions/suite/routerActions';
 
 interface Props {
     onDismiss?: () => void;
 }
 
 const SafetyChecksBanner = (props: Props) => {
-    const { goto } = useActions({
-        goto: routerActions.goto,
+    const { openModal } = useActions({
+        openModal: modalActions.openModal,
     });
 
     return (
@@ -19,8 +19,7 @@ const SafetyChecksBanner = (props: Props) => {
             body={<Translation id="TR_SAFETY_CHECKS_DISABLED_WARNING" />}
             action={{
                 label: <Translation id="TR_SAFETY_CHECKS_BANNER_CHANGE" />,
-                // TODO: Use anchor to bring the user to the appropriate section of settings.
-                onClick: () => goto('settings-device'),
+                onClick: () => openModal({ type: 'safety-checks' }),
                 'data-test': '@banner/safety-checks/button',
             }}
             dismissal={
