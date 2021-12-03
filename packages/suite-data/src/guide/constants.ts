65c834ada1fa75a5f9787535d5d23fe6aee29f08
tomasklima <tomas.klim@gmail.com>
2021-12-03 15:47:29 +0100

    chore(guide): update Suite Guide
    (cherry picked from commit 05f704824ac9e1bae3bc0a31a033ad29bcca9e2d)

--- packages/suite-data/src/guide/constants.ts
+++ packages/suite-data/src/guide/constants.ts
@@ -1,7 +1,7 @@
 import { resolve, join } from 'path';
 
 export const GITBOOK_SOURCE = 'https://github.com/trezor/trezor-suite-guide.git';
-export const GITBOOK_REVISION = '49f3d8959369e7c38ca76d6db22330fbc6108c7e';
+export const GITBOOK_REVISION = 'cc7f8ea101beb22cbc40564397c1061932c8dbc2';
 export const TMP = join(resolve(__dirname, '../..'), 'tmp', 'guide');
 // Path to the GitBook assets. Relative to TMP.
 export const ASSETS_DIR = join('.gitbook', 'assets');
