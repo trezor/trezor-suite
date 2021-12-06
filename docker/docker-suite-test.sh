17e2cb3e1fc3686e47e59408fda3cc3c3fa45167
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2021-12-06 12:10:11 +0100

    ci: add bitcoind regtest configs

--- docker/docker-suite-test.sh
+++ docker/docker-suite-test.sh
@@ -7,4 +7,4 @@ set -e
 xhost +
 export LOCAL_USER_ID=`id -u $USER`
 
-docker-compose -f ./docker/docker-compose.suite-test.yml up --build --abort-on-container-exit
+docker-compose -f ./docker/docker-compose.suite-test.yml up --build --abort-on-container-exit --force-recreate
