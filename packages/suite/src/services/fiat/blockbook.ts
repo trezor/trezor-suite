c78522813dfbe2fae44d290d0f69b226831f6cf5
Marek Polak <marek.polak@satoshilabs.com>
2021-12-10 17:38:30 +0100

    feat(suite): fallback to blockbook fiat rates

---
+++ packages/suite/src/services/fiat/blockbook.ts
@@ -0,0 +1,81 @@
+import { RateLimiter } from './limiter';
+import type { LastWeekRates, TimestampedRates, TickerId } from '@wallet-types/fiatRates';
+
+const ENDPOINTS = {
+    btc: ['btc1', 'btc2', 'btc3', 'btc4', 'btc5'],
+};
+
+type Ticker = keyof typeof ENDPOINTS;
+
+const randomEndpoint = (ticker: Ticker) =>
+    ENDPOINTS[ticker][Math.floor(Math.random() * ENDPOINTS[ticker].length)];
+
+const getQuery = (query?: { currency?: string; timestamp?: number | string }) =>
+    Object.entries(query || {})
+        .filter(([, val]) => val !== undefined)
+        .map(([key, val]) => `${key}=${val}`)
+        .join('&');
+
+const getApiUrl = (ticker: Ticker) => `https://${randomEndpoint(ticker)}.trezor.io/api/v2`;
+
+const limiter = new RateLimiter(500);
+
+const request = <T>(url: string): Promise<T | null> =>
+    limiter
+        .limit(() => fetch(url))
+        .then(res =>
+            res.ok
+                ? res.json()
+                : Promise.reject(new Error(`Fiat rates failed to fetch: ${res.status}`)),
+        )
+        .catch(err => {
+            console.warn(err);
+            return null;
+        });
+
+const getTickers = (ticker: Ticker, timestamp?: number, currency?: string) => {
+    const url = `${getApiUrl(ticker)}/tickers/?${getQuery({
+        timestamp,
+        currency,
+    })}`;
+
+    return request<TimestampedRates>(url);
+};
+
+const getMultiTickers = async (
+    ticker: Ticker,
+    timestamps: number[],
+    currency?: string,
+): Promise<LastWeekRates | null> => {
+    const url = `${getApiUrl(ticker)}/multi-tickers/?${getQuery({
+        timestamp: timestamps.join(','),
+        currency,
+    })}`;
+
+    const rates = !timestamps.length ? [] : await request<TimestampedRates[]>(url);
+
+    return (
+        rates && {
+            ts: new Date().getTime(),
+            symbol: ticker,
+            tickers: rates.map((rate, i) => ({ ...rate, ts: timestamps[i] })),
+        }
+    );
+};
+
+const getLastWeekTimestamps = () =>
+    Array.from(Array(7).keys()).map(i => {
+        const date = new Date();
+        date.setDate(date.getDate() - 7 + i);
+        return Math.floor(date.getTime() / 1000);
+    });
+
+export const isTickerSupported = (ticker: TickerId): ticker is TickerId & { symbol: Ticker } =>
+    !!ENDPOINTS[ticker.symbol as Ticker];
+
+export const fetchCurrentFiatRates = getTickers;
+
+export const getFiatRatesForTimestamps = getMultiTickers;
+
+export const fetchLastWeekRates = (ticker: Ticker, currency: string) =>
+    getMultiTickers(ticker, getLastWeekTimestamps(), currency);
--- packages/suite/src/services/fiat/index.ts
+++ packages/suite/src/services/fiat/index.ts
@@ -1,9 +1,30 @@
-import * as coingecko from './coingecko';
+import * as cg from './coingecko';
+import * as bb from './blockbook';
+import type { TickerId, TimestampedRates, LastWeekRates } from '@wallet-types/fiatRates';
 
-export const {
-    getTickerConfig,
-    fetchCurrentTokenFiatRates,
-    fetchCurrentFiatRates,
-    fetchLastWeekRates,
-    getFiatRatesForTimestamps,
-} = coingecko;
+export const { getTickerConfig, fetchCurrentTokenFiatRates } = cg;
+
+export const fetchCurrentFiatRates = async (ticker: TickerId): Promise<TimestampedRates | null> => {
+    const res = bb.isTickerSupported(ticker) ? await bb.fetchCurrentFiatRates(ticker.symbol) : null;
+    return res ?? cg.fetchCurrentFiatRates(ticker);
+};
+
+export const fetchLastWeekRates = async (
+    ticker: TickerId,
+    currency: string,
+): Promise<LastWeekRates | null> => {
+    const res = bb.isTickerSupported(ticker)
+        ? await bb.fetchLastWeekRates(ticker.symbol, currency)
+        : null;
+    return res ?? cg.fetchLastWeekRates(ticker, currency);
+};
+
+export const getFiatRatesForTimestamps = async (
+    ticker: TickerId,
+    timestamps: number[],
+): Promise<LastWeekRates | null> => {
+    const res = bb.isTickerSupported(ticker)
+        ? await bb.getFiatRatesForTimestamps(ticker.symbol, timestamps)
+        : null;
+    return res ?? cg.getFiatRatesForTimestamps(ticker, timestamps);
+};
