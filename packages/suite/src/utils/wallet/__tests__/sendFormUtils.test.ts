4d5f3d215e817c8ebcbfc03f3c4f54d81d8ba25b
tomasklima <tomas.klim@gmail.com>
2021-12-16 16:22:03 +0100

    chore(suite): update ethereumjs libs

--- packages/suite/src/utils/wallet/__tests__/sendFormUtils.test.ts
+++ packages/suite/src/utils/wallet/__tests__/sendFormUtils.test.ts
@@ -1,4 +1,4 @@
-import { FakeTransaction } from 'ethereumjs-tx';
+import { Transaction } from '@ethereumjs/tx';
 import { sha3 } from 'web3-utils';
 import * as fixtures from '../__fixtures__/sendFormUtils';
 import {
@@ -27,14 +27,14 @@ describe('sendForm utils', () => {
 
     fixtures.serializeEthereumTx.forEach(f => {
         it(`serializeEthereumTx: ${f.description}`, () => {
-            const serialized = serializeEthereumTx(f.tx);
             // verify hash using 2 different tools
             if (f.tx.chainId !== 61) {
                 // ETC is not supported
-                const tx = new FakeTransaction(serialized);
+                const tx = Transaction.fromTxData(f.tx);
                 const hash1 = tx.hash().toString('hex');
                 expect(`0x${hash1}`).toEqual(f.result);
             }
+            const serialized = serializeEthereumTx(f.tx);
             const hash2 = sha3(serialized);
             expect(hash2).toEqual(f.result);
         });
