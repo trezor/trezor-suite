f89e7b68464eb8e8eacd953e28f3782dc3eae72d
Szymon Lesisz <sz.lesisz@gmail.com>
2022-01-11 21:29:02 +0100

    chore(blockchain-link): update eslint rules

--- packages/blockchain-link/.eslintrc.js
+++ packages/blockchain-link/.eslintrc.js
@@ -3,4 +3,18 @@ module.exports = {
     parserOptions: {
         project: ['./tsconfig.json'],
     },
+    rules: {
+        'prefer-object-spread': 'off', // prefer Object.assign
+        'no-underscore-dangle': 'off',
+        'camelcase': 'off',
+        'no-console': 'warn',
+    },
+    overrides: [
+        {
+            files: ['**/webpack/*.js'],
+            rules: {
+                '@typescript-eslint/no-var-requires': 'off'
+            }
+        },
+    ],
 };
