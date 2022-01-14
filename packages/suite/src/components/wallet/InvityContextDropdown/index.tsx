b8aff1413ef3a75b41559f6a8c4338668671cfe4
Lukas Rada <lukas.rada@invity.io>
2022-01-14 11:32:57 +0100

    feat(coinmarket): savings Invity context dropdown button colors improvement

--- packages/suite/src/components/wallet/InvityContextDropdown/index.tsx
+++ packages/suite/src/components/wallet/InvityContextDropdown/index.tsx
@@ -17,29 +17,27 @@ interface StyledButtonProps extends ButtonProps {
 }
 
 // TODO: extract somewhere?
-const InvityPrimaryColor = '#00BFD9';
+const InvityPrimaryColor = 'rgb(0, 191, 217)';
+const InvityPrimaryBackbgroundColor = 'rgba(0, 191, 217, 0.05)';
 
 // TODO: The button is not properly designed. Need to wait until designer designs the button by design manual correctly.
 const StyledButton = styled(Button)<StyledButtonProps>`
     cursor: pointer;
     font-size: ${variables.FONT_SIZE.SMALL};
-    color: ${props => props.theme.BG_WHITE};
+    color: ${InvityPrimaryColor};
     font-weight: ${variables.FONT_WEIGHT.MEDIUM};
     display: flex;
     align-items: center;
     white-space: nowrap;
     margin-left: 10px;
     height: 32px;
-    background: ${props => darken(props.theme.HOVER_DARKEN_FILTER, InvityPrimaryColor)};
-    img {
-        filter: brightness(4);
-    }
+    background: ${InvityPrimaryBackbgroundColor};
     &:hover,
     &:visited,
     &:focus,
     &:active {
-        color: ${props => props.theme.BG_WHITE};
-        background: ${props => darken(props.theme.HOVER_DARKEN_FILTER, InvityPrimaryColor)};
+        background: ${props =>
+            darken(props.theme.HOVER_DARKEN_FILTER, InvityPrimaryBackbgroundColor)};
     }
 `;
 
@@ -51,9 +49,9 @@ const Note = styled.div`
 `;
 
 const Image = styled.img`
-    height: 24px;
+    height: 12px;
     object-fit: contain;
-    margin-right: -8px;
+    margin-right: 8px;
 `;
 
 const InvityContextDropdown = () => {
@@ -126,6 +124,7 @@ const InvityContextDropdown = () => {
                 ]}
             >
                 <StyledButton size={14} type="button" isAuthenticated={isAuthenticated}>
+                    <Image src={resolveStaticPath('/images/svg/invity-symbol.svg')} />
                     <Translation
                         id={
                             isAuthenticated
@@ -133,7 +132,6 @@ const InvityContextDropdown = () => {
                                 : 'TR_INVITY_SIGNIN_BUTTON'
                         }
                     />
-                    <Image src={resolveStaticPath('/images/svg/invity-logo.svg')} />
                 </StyledButton>
             </Dropdown>
         </Wrapper>
