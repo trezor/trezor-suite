04b72eb763ff29d7a66ddaed5532146962d3b227
Daniel Suchý <suchydan@gmail.com>
2022-01-17 16:57:25 +0100

    feat(native): Init RN Transport Native package

---
+++ packages/transport-native/ios/TrezorTransportNative.swift
@@ -0,0 +1,8 @@
+@objc(TrezorTransportNative)
+class TrezorTransportNative: NSObject {
+
+    @objc(multiply:withB:withResolver:withRejecter:)
+    func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        resolve(a*b)
+    }
+}
