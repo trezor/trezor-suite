12f235dfc8f6b4ae1bd56e3b19eed042c66686a2
Szymon Lesisz <sz.lesisz@gmail.com>
2022-01-21 10:59:00 +0100

    chore(suite-desktop): remove unused trezord filter

--- packages/suite-desktop/src-electron/modules/bridge.ts
+++ packages/suite-desktop/src-electron/modules/bridge.ts
@@ -1,26 +1,15 @@
 /**
  * Bridge runner
  */
-import { app, session } from 'electron';
+import { app } from 'electron';
 import BridgeProcess from '@desktop-electron/libs/processes/BridgeProcess';
 import { b2t } from '@desktop-electron/libs/utils';
-
-const filter = {
-    urls: ['http://127.0.0.1:21325/*'],
-};
 
 const bridgeDev = app.commandLine.hasSwitch('bridge-dev');
 
 const init = async () => {
     const { logger } = global;
     const bridge = new BridgeProcess();
-
-    session.defaultSession.webRequest.onBeforeSendHeaders(filter, (details, callback) => {
-        // @ts-ignore electron declares requestHeaders as an empty interface
-        details.requestHeaders.Origin = 'https://electron.trezor.io';
-        logger.debug('bridge', `Setting header for ${details.url}`);
-        callback({ cancel: false, requestHeaders: details.requestHeaders });
-    });
 
     try {
         logger.info('bridge', `Starting (Dev: ${b2t(bridgeDev)})`);
