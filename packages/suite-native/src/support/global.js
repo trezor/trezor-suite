6d3e0f19051ac8f9ec4a880d997b790355e6cb7c
Daniel Suchý <suchydan@gmail.com>
2022-01-23 17:39:42 +0100

    chore(code): refactor eslint for suite-native package

--- packages/suite-native/src/support/global.js
+++ packages/suite-native/src/support/global.js
@@ -5,6 +5,6 @@
 global.Buffer = require('buffer').Buffer;
 global.process = require('process');
 global.Intl = require('intl');
-global.IntlLocales = require('intl/locale-data/jsonp/en.js');
+global.IntlLocales = require('intl/locale-data/jsonp/en');
 
 global.process.env.NODE_ENV = __DEV__ ? 'development' : 'production';
