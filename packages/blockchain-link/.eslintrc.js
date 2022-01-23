bd50ec89327d56b396ff1ba2724edce758d764f7
Daniel Suchý <suchydan@gmail.com>
2022-01-23 17:34:19 +0100

    chore(code): refactor eslint for blockchain link

--- packages/blockchain-link/.eslintrc.js
+++ packages/blockchain-link/.eslintrc.js
@@ -1,20 +1,6 @@
 module.exports = {
-    extends: '../../.eslintrc.js',
-    parserOptions: {
-        project: ['./tsconfig.json'],
-    },
     rules: {
-        'prefer-object-spread': 'off', // prefer Object.assign
         'no-underscore-dangle': 'off',
-        'camelcase': 'off',
         'no-console': 'warn',
     },
-    overrides: [
-        {
-            files: ['**/webpack/*.js'],
-            rules: {
-                '@typescript-eslint/no-var-requires': 'off'
-            }
-        },
-    ],
 };
