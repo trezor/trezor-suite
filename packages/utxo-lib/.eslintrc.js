f91330e900a053ff74a613b586bb1296e0bc8563
Daniel Suchý <suchydan@gmail.com>
2022-01-23 17:37:15 +0100

    chore(code): refactor eslint for utxo-lib

--- packages/utxo-lib/.eslintrc.js
+++ packages/utxo-lib/.eslintrc.js
@@ -1,8 +1,4 @@
 module.exports = {
-    extends: '../../.eslintrc.js',
-    parserOptions: {
-        project: './tsconfig.json',
-    },
     rules: {
         'no-bitwise': 'off', // airbnb-base: used in hardending
         'prefer-object-spread': 'off', // prefer Object.assign
