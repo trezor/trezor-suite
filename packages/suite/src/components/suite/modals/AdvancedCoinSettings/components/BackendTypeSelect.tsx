e3b7c279ec6712340f95b1a43409044d89ac6d16
Marek Polak <marek.polak@satoshilabs.com>
2022-01-25 16:39:26 +0100

    feat(suite): allow electrum in desktop Suite

--- packages/suite/src/components/suite/modals/AdvancedCoinSettings/components/BackendTypeSelect.tsx
+++ packages/suite/src/components/suite/modals/AdvancedCoinSettings/components/BackendTypeSelect.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import styled from 'styled-components';
 import { Select } from '@trezor/components';
 import { Translation } from '@suite-components';
+import { isDesktop } from '@suite-utils/env';
+import { useSelector } from '@suite-hooks';
 import type { Network } from '@wallet-types';
 import type { BackendOption } from '@settings-hooks/backends';
 
@@ -9,23 +11,31 @@ const Capitalize = styled.span`
     text-transform: capitalize;
 `;
 
-const getBackendOptions = (network: Network) => {
-    const backends: BackendOption[] = [];
-    if (network.symbol !== 'regtest') backends.push('default');
-    if (network.networkType !== 'ripple') backends.push('blockbook');
-    // if (network.symbol === 'btc') backends.push('electrum');
-    return backends.map(backend => ({
-        label:
-            backend === 'default' ? (
-                <Translation id="TR_BACKEND_DEFAULT_SERVERS" />
-            ) : (
-                <Translation
-                    id="TR_BACKEND_CUSTOM_SERVERS"
-                    values={{ type: <Capitalize>{backend}</Capitalize> }}
-                />
-            ),
-        value: backend,
-    }));
+const useBackendOptions = (network: Network) => {
+    const debug = useSelector(state => state.suite.settings.debug.showDebugMenu);
+
+    const options = useMemo(() => {
+        const backends: BackendOption[] = [];
+        if (network.symbol !== 'regtest') backends.push('default');
+        if (network.networkType !== 'ripple') backends.push('blockbook');
+        if (['btc', 'regtest'].includes(network.symbol) && isDesktop() && debug)
+            backends.push('electrum');
+
+        return backends.map(backend => ({
+            label:
+                backend === 'default' ? (
+                    <Translation id="TR_BACKEND_DEFAULT_SERVERS" />
+                ) : (
+                    <Translation
+                        id="TR_BACKEND_CUSTOM_SERVERS"
+                        values={{ type: <Capitalize>{backend}</Capitalize> }}
+                    />
+                ),
+            value: backend,
+        }));
+    }, [network, debug]);
+
+    return options;
 };
 
 type BackendTypeSelectProps = {
@@ -35,7 +45,7 @@ type BackendTypeSelectProps = {
 };
 
 export const BackendTypeSelect = ({ network, value, onChange }: BackendTypeSelectProps) => {
-    const backendOptions = getBackendOptions(network);
+    const backendOptions = useBackendOptions(network);
 
     const changeType = (option: { value: BackendOption }) => onChange(option.value);
 
