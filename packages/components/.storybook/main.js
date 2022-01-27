d00870ad8dd8b946e924c10fa71faeca6b5b5ca4
mroz <mvarmuza@gmail.com>
2022-01-27 09:31:45 +0100

    fix: storybook build

--- packages/components/.storybook/main.js
+++ packages/components/.storybook/main.js
@@ -3,15 +3,6 @@ const path = require('path');
 module.exports = {
     stories: ['../src/**/*.stories.*'],
     logLevel: 'debug',
-    webpackFinal: async config => {
-        // for typescript use ts-loader@8.3.0 compatible with webpack@4 (storybook dependency)
-        // probably may be removed after storybook@6.4.0 with webpack@5 support
-        config.module.rules.unshift({
-            test: /\.tsx?$/,
-            loader: path.resolve('./node_modules/ts-loader/index.js'),
-        })
-        return config;
-    },
     addons: [
         '@storybook/addon-links',
         '@storybook/addon-knobs', // DEPRECATED
@@ -24,5 +15,8 @@ module.exports = {
         // reactDocgenTypescriptOptions: {
         //   propFilter: (prop) => ['label', 'disabled'].includes(prop.name),
         // },
+    },
+    core: {
+        builder: 'webpack5',
     },
 };
