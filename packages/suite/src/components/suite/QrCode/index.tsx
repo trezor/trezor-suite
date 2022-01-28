194e861f569c32eb8dd9fb3e04614f94e14ae759
Tomáš Klíma <tomas.klim@gmail.com>
2022-01-28 21:47:35 +0100

    fix(qr-code): width on retina displays

--- packages/suite/src/components/suite/QrCode/index.tsx
+++ packages/suite/src/components/suite/QrCode/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import QRCode from 'qrcode.react';
 import styled from 'styled-components';
 
-import { colors } from '@trezor/components';
+import { colors, variables } from '@trezor/components';
 
 export const QRCODE_SIZE = 384;
 export const QRCODE_PADDING = 12;
@@ -13,6 +13,11 @@ const Wrapper = styled.div`
     /* some qr code scanners can't recognize qr codes on dark background, having white border around helps with this */
     padding: ${QRCODE_PADDING}px;
     background: ${colors.BG_WHITE};
+    width: ${QRCODE_SIZE}px;
+
+    @media all and (max-width: ${variables.SCREEN_SIZE.SM}) {
+        width: 100%;
+    }
 `;
 
 interface QrCodeProps {
