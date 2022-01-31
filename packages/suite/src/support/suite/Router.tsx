cf82a04b0abbe5048a44917cdc8ac0497fb0b250
Tomáš Klíma <tomas.klim@gmail.com>
2022-01-31 13:27:19 +0100

    fix(router): prevent double location change on hash change

--- packages/suite/src/support/suite/Router.tsx
+++ packages/suite/src/support/suite/Router.tsx
@@ -1,20 +1,14 @@
 import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+
 import { useActions } from '@suite-hooks/useActions';
 import { onLocationChange, onBeforePopState } from '@suite-actions/routerActions';
 import history from '@suite/support/history';
 
 const RouterComponent = () => {
-    const location = useLocation();
     const actions = useActions({
         onLocationChange,
         onBeforePopState,
     });
-
-    useEffect(() => {
-        const url = location.pathname + location.hash;
-        actions.onLocationChange(url);
-    }, [location.pathname, location.hash, actions]);
 
     useEffect(() => {
         const onPopState = () => {
