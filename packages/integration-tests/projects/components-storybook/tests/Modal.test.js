61935622361361f5afc2067f7a0dffb31ff99535
mroz <mvarmuza@gmail.com>
2022-02-02 20:10:25 +0100

    chore: remove dead tests (components-storybook)

--- packages/integration-tests/projects/components-storybook/tests/Modal.test.js
+++
@@ -1,7 +0,0 @@
-describe('Modal', () => {
-    it('modal', () => {
-        cy.viewport(1024, 500);
-        cy.loadContent('iframe.html?id=modals--default');
-        cy.getTestElement('modal').should('be.visible').matchImageSnapshot();
-    });
-});
