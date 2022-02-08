d9b885fcbd1608f37ea85f7d062b04feb33d2f71
Marek Polak <marek.polak@satoshilabs.com>
2022-02-08 14:00:50 +0100

    chore(suite): add proper typing to useActions hook

--- packages/suite/src/hooks/suite/useActions.ts
+++ packages/suite/src/hooks/suite/useActions.ts
@@ -1,9 +1,9 @@
 import { useMemo, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import { bindActionCreators, ActionCreatorsMapObject } from 'redux';
-import { Dispatch } from '@suite-types';
+import type { Action, ThunkAction, Dispatch } from '@suite-types';
 
-export const useActions = <M extends ActionCreatorsMapObject<any>>(actions: M) => {
+export const useActions = <M extends ActionCreatorsMapObject<Action | ThunkAction>>(actions: M) => {
     const dispatch = useDispatch<Dispatch>();
     const ref = useRef(actions);
     return useMemo(() => bindActionCreators(ref.current, dispatch), [dispatch, ref]);
