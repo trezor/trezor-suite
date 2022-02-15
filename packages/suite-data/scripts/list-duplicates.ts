630169e5830bd1643a12ee2221bb640fec85c08a
Daniel Suchý <suchydan@gmail.com>
2022-02-15 20:02:25 +0100

    chore(suite-data): fix import for list-duplicated messages script

--- packages/suite-data/scripts/list-duplicates.ts
+++ packages/suite-data/scripts/list-duplicates.ts
@@ -1,4 +1,8 @@
-import messages from '../../suite/src/support/messages';
+// @trezor/suite-data -> @trezor/suite -> @trezor/suite-data
+// this is causing module cyclic dependency!!! it will work since this dep is not specified
+// in package.json or refs, and also these files must be excluded from TS check
+// TODO: intl or scripts should have dedicated package on that could both suite-data and suite depends
+import messages from '@trezor/suite/src/support/messages';
 
 type Message = {
     [key: string]: {
