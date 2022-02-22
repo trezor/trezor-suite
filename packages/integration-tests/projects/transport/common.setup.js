b25307e01d48d01af55177f9a43e6055d7f6472a
mroz <mvarmuza@gmail.com>
2022-02-22 10:18:40 +0100

    test(transport): bridge post and read endpoints

--- packages/integration-tests/projects/transport/common.setup.js
+++ packages/integration-tests/projects/transport/common.setup.js
@@ -1,31 +1,11 @@
 const { Controller } = require('../../websocket-client');
 
-const mnemonicAll = 'all all all all all all all all all all all all';
-
-const emulatorSetupOpts = {
-    mnemonic: mnemonicAll,
-    pin: '',
-    passphrase_protection: false,
-    label: 'TrezorT',
-    needs_backup: false,
-};
-
-const emulatorStartOpts = { version: '2-master', wipe: true };
-
 const wait = ms =>
     new Promise(resolve => {
         setTimeout(resolve, ms);
     });
 
-const setup = async controller => {
-    await controller.connect();
-    await controller.send({ type: 'bridge-stop' });
-    await controller.send({ type: 'emulator-start', ...emulatorStartOpts });
-    await controller.send({ type: 'emulator-setup', ...emulatorSetupOpts });
-};
-
 global.Trezor = {
     wait,
-    setup,
     Controller,
 };
