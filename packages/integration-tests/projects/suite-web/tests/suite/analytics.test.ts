065a57a8cb7c383edc4f3fd82bced65f20c70296
Tomáš Klíma <tomas.klim@gmail.com>
2022-02-23 15:01:20 +0100

    chore(e2e): analytics

--- packages/integration-tests/projects/suite-web/tests/suite/analytics.test.ts
+++ packages/integration-tests/projects/suite-web/tests/suite/analytics.test.ts
@@ -4,7 +4,7 @@
 import { urlSearchParams } from '../../../../../suite/src/utils/suite/metadata';
 
 type Requests = ReturnType<typeof urlSearchParams>[];
-const requests: Requests = [];
+let requests: Requests;
 const instance = new RegExp(/^[A-Za-z0-9]{10,10}$/);
 const timestamp = new RegExp(/^[0-9]{13,16}$/);
 
@@ -14,15 +14,11 @@ describe('Analytics', () => {
         cy.task('setupEmu');
         cy.task('startBridge');
         cy.viewport(1024, 768).resetDb();
+
+        requests = [];
     });
 
-    it('Analytics should be enabled on initial run, then user may disable it and this option should be respected on subsequent reloads', () => {
-        /*
-         * Skip test on localhost as analytics is disabled on location.hostname === "localhost". See getUrl function.
-         * TODO: Build app with test flag, modify getUrl function to return test url and mock this test url.
-         */
-        cy.skipOn('localhost');
-
+    it('should respect disabled analytics in onboarding with following enabling in settings', () => {
         cy.intercept({ hostname: 'data.trezor.io', url: '/suite/log/**' }, req => {
             const params = urlSearchParams(req.url);
             requests.push(params);
@@ -30,54 +26,50 @@ describe('Analytics', () => {
 
         cy.prefixedVisit('/');
 
-        // pass through initial run
+        // pass through onboarding with disabled analytics
         cy.getTestElement('@analytics/toggle-switch').should('be.checked');
         cy.getTestElement('@analytics/toggle-switch').click({ force: true });
         cy.getTestElement('@analytics/toggle-switch').should('not.be.checked');
         cy.getTestElement('@onboarding/continue-button').click();
-        cy.getTestElement('@onboarding/exit-app-button').click();
 
-        // assert that only 2 requests was fired
+        // assert that only "analytics/dispose" event was fired
         cy.wait('@data-fetch');
         cy.wrap(requests).its(0).should('have.property', 'c_type', 'analytics/dispose');
-        cy.wrap(requests).its(1).its('c_session_id').as('request0');
-        cy.wrap(requests).its(1).should('have.property', 'c_type', 'initial-run-completed');
+        cy.wrap(requests).its(0).its('c_session_id').as('request0');
+        cy.wrap(requests)
+            .its(0)
+            .should('have.property', 'c_timestamp')
+            .should('match', timestamp)
+            .as('timestamp0');
+        cy.wrap(requests).should('have.length', 1);
+
+        // finish onboarding
+        cy.getTestElement('@onboarding/exit-app-button').click();
+
+        // reload app (important, app needs time to save initialRun flag into storage) to change session id
+        cy.getTestElement('@suite/loading').should('not.exist');
+        cy.discoveryShouldFinish();
+        cy.reload();
+        cy.discoveryShouldFinish();
+
+        // go to settings, analytics should not enabled and no additional analytics requests should be fired
+        cy.getTestElement('@suite/menu/settings').click();
+        cy.getTestElement('@analytics/toggle-switch').should('not.be.checked');
+        cy.wrap(requests).should('have.length', 1);
+
+        // enable analytics and check "analytics/enable" event was fired
+        cy.getTestElement('@analytics/toggle-switch').click({ force: true });
+        cy.getTestElement('@analytics/toggle-switch').should('be.checked');
+        cy.wait('@data-fetch');
+        cy.wrap(requests).its(1).should('have.property', 'c_type', 'analytics/enable');
+        cy.wrap(requests).its(1).its('c_session_id').as('request1');
         cy.wrap(requests)
             .its(1)
             .should('have.property', 'c_timestamp')
             .should('match', timestamp)
-            .as('timestamp0');
-        cy.wrap(requests).its(1).should('have.property', 'analytics', 'false');
-        cy.wrap(requests).its(2).should('equal', undefined);
-
-        // important, suite needs time to save initialRun flag into storage
-        cy.getTestElement('@suite/loading').should('not.exist');
-        cy.discoveryShouldFinish();
-
-        // go to settings
-        cy.prefixedVisit('/');
-        cy.task('startEmu', { wipe: false });
-        cy.discoveryShouldFinish();
-        cy.getTestElement('@suite/menu/settings').click();
-
-        // analytics is not enabled and no additional requests were fired
-        cy.getTestElement('@analytics/toggle-switch').should('not.be.checked');
-        cy.wrap(requests).its(0).should('have.property', 'c_type', 'analytics/dispose');
-        cy.wrap(requests).its(1).should('have.property', 'c_type', 'initial-run-completed');
-        cy.wrap(requests).its(1).should('have.property', 'analytics', 'false');
-        cy.wrap(requests).its(2).should('equal', undefined);
-
-        // enable it and reload page
-        cy.log('enable it again, reload and see it remains checked');
-        cy.getTestElement('@analytics/toggle-switch').click({ force: true });
-        cy.getTestElement('@analytics/toggle-switch').should('be.checked');
-        cy.wait('@data-fetch');
-        cy.wrap(requests).its(2).should('have.property', 'c_type', 'analytics/enable');
-        cy.wrap(requests)
-            .its(2)
-            .should('have.property', 'c_timestamp')
-            .should('match', timestamp)
             .as('timestamp1');
+        cy.wrap(requests).should('have.length', 2);
+
         // check that timestamp changes between requests
         cy.get('@timestamp0').then(t0 => {
             cy.get('@timestamp1').then(t1 => {
@@ -85,34 +77,87 @@ describe('Analytics', () => {
             });
         });
 
-        // change fiat
-        cy.getTestElement('@settings/fiat-select/input').click({ force: true });
-        cy.getTestElement('@settings/fiat-select/option/huf').click({ force: true });
-
-        // check that fiat change got logged.
-
-        cy.wait('@data-fetch');
-        cy.wrap(requests).its(3).its('c_session_id').as('request1');
-        cy.wrap(requests).its(3).should('have.property', 'c_type', 'settings/general/change-fiat');
-        cy.wrap(requests).its(3).should('have.property', 'fiat', 'huf');
-        cy.wrap(requests).its(3).should('have.property', 'c_instance_id').should('match', instance);
-        // and check that session ids changed after reload;
+        // check that session ids changed after reload;
         cy.get('@request0').then(r0 => {
             cy.get('@request1').then(r1 => {
                 expect(r1).not.to.equal(r0);
             });
         });
 
-        // opening device modal
+        // change fiat and check that it was logged
+        cy.getTestElement('@settings/fiat-select/input').click({ force: true });
+        cy.getTestElement('@settings/fiat-select/option/huf').click({ force: true });
+        cy.wait('@data-fetch');
+        cy.wrap(requests).its(2).should('have.property', 'c_type', 'settings/general/change-fiat');
+        cy.wrap(requests).its(2).should('have.property', 'fiat', 'huf');
+        cy.wrap(requests).its(2).should('have.property', 'c_instance_id').should('match', instance);
+        cy.wrap(requests).should('have.length', 3);
+
+        // open device modal and check that it was logged
         cy.getTestElement('@menu/switch-device').click();
         cy.wait('@data-fetch');
-        cy.wrap(requests).its(5).should('have.property', 'c_type', 'menu/goto/switch-device');
+        cy.wrap(requests).its(3).should('have.property', 'c_type', 'router/location-change');
+        cy.wrap(requests).its(4).should('have.property', 'c_type', 'menu/goto/switch-device');
+        cy.wrap(requests).should('have.length', 5);
 
-        // adding wallet
+        // add hidden wallet and check that it was logged
         cy.getTestElement('@switch-device/add-hidden-wallet-button').click();
         cy.wait('@data-fetch');
         cy.wrap(requests)
-            .its(6)
+            .its(5)
             .should('have.property', 'c_type', 'switch-device/add-hidden-wallet');
+    });
+
+    it('should respect enabled analytics in onboarding with following disabling in settings', () => {
+        cy.intercept({ hostname: 'data.trezor.io', url: '/suite/log/**' }, req => {
+            const params = urlSearchParams(req.url);
+            requests.push(params);
+        }).as('data-fetch');
+
+        cy.prefixedVisit('/');
+
+        // pass through onboarding with enabled analytics
+        cy.getTestElement('@analytics/toggle-switch').should('be.checked');
+        cy.getTestElement('@onboarding/continue-button').click();
+
+        // assert that more than 1 event was fired and it was "suite/ready" and "analytics/enable" for sure
+        cy.wait('@data-fetch');
+        cy.wrap(requests).its('length').should('be.gt', 1);
+        cy.wrap(requests)
+            .then(list => Cypress._.map(list, 'c_type'))
+            .should('include', 'suite-ready');
+        cy.wrap(requests)
+            .then(list => Cypress._.map(list, 'c_type'))
+            .should('include', 'analytics/enable');
+
+        // finish onboarding
+        cy.getTestElement('@onboarding/exit-app-button').click();
+
+        // go to settings, analytics should be enabled
+        cy.getTestElement('@suite/menu/settings').click();
+        cy.getTestElement('@analytics/toggle-switch').should('be.checked');
+
+        // disable analytics
+        cy.getTestElement('@analytics/toggle-switch').click({ force: true });
+        cy.getTestElement('@analytics/toggle-switch').should('not.be.checked');
+
+        // change fiat
+        cy.getTestElement('@settings/fiat-select/input').click({ force: true });
+        cy.getTestElement('@settings/fiat-select/option/huf').click({ force: true });
+        cy.wait('@data-fetch');
+        cy.wrap(requests).its('length').as('length1');
+
+        // check that analytics disable event was fired
+        cy.wrap(requests)
+            .then(list => Cypress._.map(list, 'c_type'))
+            .should('include', 'analytics/dispose');
+        cy.wrap(requests).its('length').as('length0');
+
+        // check that "settings/general/change-fiat" event was not fired
+        cy.get('@length0').then(l0 => {
+            cy.get('@length1').then(l1 => {
+                expect(l1).to.equal(l0);
+            });
+        });
     });
 });
