d1c024914c7d4bda40e66bc2bf68410d7dee6195
mroz <mvarmuza@gmail.com>
2022-03-04 09:04:58 +0100

    test(popup): various cases of popup closing

---
+++ docker/wait-for-200.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+echo "Waiting for $1 to load up..."
+counter=0
+max_attempts=60
+
+curl -i -s -I $1
+
+until (curl -i -s -I --insecure  $1 | grep '200 OK'); do
+  if [ ${counter} -eq ${max_attempts} ]; then
+    echo "$1 is not running. exiting"
+    exit 1
+  fi
+  counter=$(($counter+1))
+  printf "."
+  sleep 1
+done
+
+echo "$1 loaded up"
