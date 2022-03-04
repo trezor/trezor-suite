e7ce8dd250086703a44cddef117ec88feacd9680
mroz <mvarmuza@gmail.com>
2022-03-04 08:51:57 +0100

    test: disable parallelism in popup playwright tests

--- packages/integration-tests/projects/connect-explorer/playwright.config.ts
+++ packages/integration-tests/projects/connect-explorer/playwright.config.ts
@@ -2,7 +2,9 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
     testDir: 'tests',
-    retries: 3,
+    retries: 0,
+    workers: 1, // to disable parallelism between test files
+    timeout: 30000,
     use: {
         headless: process.env.HEADLESS === 'true',
         ignoreHTTPSErrors: true,
