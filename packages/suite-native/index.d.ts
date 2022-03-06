66f4e0aa7ae0105a75d787cadb734d87452adec8
Daniel Suchy <suchydan@gmail.com>
2022-03-06 16:19:41 +0100

    App vol 2

--- packages/suite-native/index.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../suite/global.d.ts" />
-
-declare module '*.svg';
-
-// declare module '*.svg' {
-//     const content: any;
-//     export const ReactComponent: any;
-//     export default content;
-// }
--- packages/suite-native/index.js
+++ packages/suite-native/index.js
@@ -2,5 +2,4 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
-// TODO: https://www.fabrizioduroni.it/2017/12/08/react-native-multiple-instance-rctrootview.html
 AppRegistry.registerComponent(appName, () => App);
