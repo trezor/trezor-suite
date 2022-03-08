6b7e7fa25de1c50814fe3bfcd889cf147c93ef24
alex-jerechinsky <75733878+alex-jerechinsky@users.noreply.github.com>
2022-03-08 16:19:16 +0100

    feat(guide): update articles
    Adding 'Cardano (ADA) - Staking in Trezor Suite' and 'Self-Hosted Full Node via Electrum Server' article
    
    (cherry picked from commit 2adc29dd0c82046f81052341ec82c79fe75e446d)

--- packages/suite-data/src/guide/constants.ts
+++ packages/suite-data/src/guide/constants.ts
@@ -1,7 +1,7 @@
 import { resolve, join } from 'path';
 
 export const GITBOOK_SOURCE = 'https://github.com/trezor/trezor-suite-guide.git';
-export const GITBOOK_REVISION = 'ffb6ff212e515e20a65fefc2b21d4475dec6bbe0';
+export const GITBOOK_REVISION = '45cb71c2476819367df98bbbc1cac5f2764cfd7c';
 export const TMP = join(resolve(__dirname, '../..'), 'tmp', 'guide');
 // Path to the GitBook assets. Relative to TMP.
 export const ASSETS_DIR = join('.gitbook', 'assets');
