0b08f92a2d9cc95da4871696c078fcc05492ec4c
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2022-03-08 10:17:46 +0100

    feat: script for regtest electrum

---
+++ docker/docker-regtest-electrum.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+set -e
+
+export LOCAL_USER_ID=`id -u $USER`
+
+docker-compose -f ./docker/docker-compose.regtest-electrum.yml up --build --abort-on-container-exit --force-recreate
