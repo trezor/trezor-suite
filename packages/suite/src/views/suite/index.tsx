7e601287bffcfdf1e8b2b9faf8ac7aa141bfbe3b
Marek Polak <marek.polak@satoshilabs.com>
2022-03-09 15:52:25 +0100

    refactor(suite): remove unused device views

--- packages/suite/src/views/suite/index.tsx
+++ packages/suite/src/views/suite/index.tsx
@@ -1,29 +1,9 @@
 import Bridge from './bridge';
 import DeviceAcquire from './device-acquire';
-import DeviceBootloader from './device-bootloader';
-import DeviceInitialize from './device-initialize';
-import DeviceNoFirmware from './device-no-firmware';
-import DeviceSeedless from './device-seedless';
 import DeviceUnknown from './device-unknown';
 import DeviceUnreadable from './device-unreadable';
-import DeviceUpdateRequired from './device-update-required';
-import DeviceRecoveryMode from './device-recovery-mode';
 import SwitchDevice from './switch-device';
 import Udev from './udev';
 import Version from './version';
 
-export {
-    Bridge,
-    DeviceAcquire,
-    DeviceBootloader,
-    DeviceInitialize,
-    DeviceNoFirmware,
-    DeviceSeedless,
-    DeviceUnknown,
-    DeviceUnreadable,
-    DeviceUpdateRequired,
-    DeviceRecoveryMode,
-    SwitchDevice,
-    Udev,
-    Version,
-};
+export { Bridge, DeviceAcquire, DeviceUnknown, DeviceUnreadable, SwitchDevice, Udev, Version };
