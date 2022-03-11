fc102959fd73e6cf6d5371b58fd762fae895f410
Pavol Rusnak <pavol@rusnak.io>
2022-03-11 18:38:00 +0100

    fix(connect-explorer): testnet is 0x98

--- packages/connect-explorer/src/js/data/methods/nem/common.ts
+++ packages/connect-explorer/src/js/data/methods/nem/common.ts
@@ -1,5 +1,5 @@
 export const networks = [
     { value: 'Mainnet', label: 'Mainnet', affectedValue: 0x68 },
-    { value: 'Testnet', label: 'Testnet', affectedValue: 0x96 },
+    { value: 'Testnet', label: 'Testnet', affectedValue: 0x98 },
     { value: 'Mijin', label: 'Mijin', affectedValue: 0x60 },
 ];
