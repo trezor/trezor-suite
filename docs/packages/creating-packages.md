b5f3b76868991190fbc179462c08b73b69c57703
Daniel Suchý <suchydan@gmail.com>
2022-03-14 21:02:09 +0100

    chore(scripts): Add package generator script (#5045)

---
+++ docs/packages/creating-packages.md
@@ -0,0 +1,9 @@
+# How to create new package?
+
+1. Use `yarn generate-package your-package-name` - it will generate package boilerplate in `packages/your-package-name`.
+
+## How to use this new package?
+
+1. Place this package to dependency field to package.json in package where you want to use it.
+2. Run `yarn refs` to generate tsconfig refs.
+3. Run `yarn` to let yarn symlink this package.
---
+++ jest.config.base.js
@@ -0,0 +1,34 @@
+const babelConfig = {
+    presets: [
+        ['@babel/preset-env', { targets: { node: 'current' }, modules: 'commonjs' }],
+        '@babel/preset-typescript',
+        '@babel/preset-react',
+    ],
+};
+
+module.exports = {
+    // An array of file extensions your modules use
+    moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
+
+    // The glob patterns Jest uses to detect test files
+    testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+
+    // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+    testPathIgnorePatterns: [
+        '/node_modules/',
+        '/libDev/',
+        '/lib/',
+        '/dist/',
+        '/build/',
+        '/build-electron/',
+        '/coverage/',
+        '/public/',
+    ],
+
+    transform: {
+        '\\.(js|jsx|ts|tsx)$': ['babel-jest', babelConfig],
+    },
+
+    // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
+    watchPathIgnorePatterns: ['libDev', 'lib'],
+};
