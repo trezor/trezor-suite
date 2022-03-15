f147eae5ea0cf12bd6acf0bc11b5db5530030018
Vitalij Dovhanyc <45185420+vdovhanych@users.noreply.github.com>
2022-03-15 15:51:07 +0100

    test: add script for generating regtest to suite electrum backend (#5100)
    Co-authored-by: jakubondracek <ondracekjakub@gmail.com>

---
+++ docker/create-funds-on-regtest.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+set -e
+shopt -s expand_aliases
+
+BTC_REC_ADDR=$1
+
+if [ -z "$BTC_REC_ADDR" ]
+then
+      echo "Please fill in the recieving BTC address as the first arg."
+else
+  # Get unspent transactions
+  UNSENT_TR=$( /usr/bin/bitcoin-cli -regtest -datadir=/root/.bitcoin --rpccookiefile=/root/.cookie -rpcport=18021 listunspent)
+  # Get trxId of the last transaction
+  TRX_ID=$(echo $UNSENT_TR | jq -r '.[-1].txid')
+
+  # Create a btc transaction
+  TRX=$(/usr/bin/bitcoin-cli -regtest -datadir=/root/.bitcoin --rpccookiefile=/root/.cookie -rpcport=18021 createrawtransaction \
+  "[{
+  \"txid\" : \""$TRX_ID"\",
+  \"vout\" : 0
+  }]" \
+  "{\""$BTC_REC_ADDR"\": 49.99999}")
+
+  # Sign the transaction
+  TX_OUTPUT=$(/usr/bin/bitcoin-cli -regtest -datadir=/root/.bitcoin --rpccookiefile=/root/.cookie -rpcport=18021 signrawtransactionwithwallet $TRX)
+
+  # Broadcast the tx to the network
+  HEX_ID=$(echo $TX_OUTPUT | jq -r '.hex')
+  /usr/bin/bitcoin-cli -regtest -datadir=/root/.bitcoin --rpccookiefile=/root/.cookie -rpcport=18021 sendrawtransaction $HEX_ID &>/dev/null
+
+  # Mine a new block
+  ADDR=$(/usr/bin/bitcoin-cli -regtest -datadir=/root/.bitcoin --rpccookiefile=/root/.cookie -rpcport=18021 -rpcwallet=tenv-test getnewaddress)
+  /usr/bin/bitcoin-cli -regtest -datadir=/root/.bitcoin --rpccookiefile=/root/.cookie -rpcport=18021 -rpcwallet=tenv-test generatetoaddress 150 $ADDR &>/dev/null
+fi
--- docker/docker-regtest-electrum.sh
+++ docker/docker-regtest-electrum.sh
@@ -4,3 +4,4 @@ set -e
 export LOCAL_USER_ID=`id -u $USER`
 
 docker-compose -f ./docker/docker-compose.regtest-electrum.yml up --build --abort-on-container-exit --force-recreate
+
