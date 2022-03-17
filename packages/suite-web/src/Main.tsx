c91b39c9d32497a7a4b2e211c550f6cd4ffe2964
Szymon Lesisz <sz.lesisz@gmail.com>
2022-03-17 18:50:43 +0100

    chore(suite-web): group cypress exports
    (cherry picked from commit 49bef32d4296c77ad022755d67a18fe03ae0e2a6)

--- packages/suite-web/src/Main.tsx
+++ packages/suite-web/src/Main.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
 import { Router as RouterProvider } from 'react-router-dom';
-import TrezorConnect from '@trezor/connect';
 import { init as initSentry } from '@sentry/browser';
 
 import { store } from '@suite/reducers/store';
@@ -24,46 +23,40 @@ import history from '@suite/support/history';
 import { ModalContextProvider } from '@suite-support/ModalContext';
 
 import AppRouter from './support/Router';
-import { CypressExportStore } from './support/CypressExportStore';
+import { useCypress } from './support/useCypress';
 
 const Main = () => {
+    useCypress();
     useEffect(() => {
         if (!window.Cypress && !isDev) {
             initSentry(SENTRY_CONFIG);
         }
-        if (window.Cypress) {
-            // exposing ref to TrezorConnect allows us to mock its methods in cypress tests
-            window.TrezorConnect = TrezorConnect;
-        }
     }, []);
 
     return (
-        <>
-            <CypressExportStore store={store} />
-            <ReduxProvider store={store}>
-                <ThemeProvider>
-                    <RouterProvider history={history}>
-                        <ModalContextProvider>
-                            <ErrorBoundary>
-                                <Autodetect />
-                                <Resize />
-                                <Tor />
-                                <Protocol />
-                                <OnlineStatus />
-                                <RouterHandler />
-                                <IntlProvider>
-                                    <Metadata />
-                                    <ToastContainer />
-                                    <Preloader>
-                                        <AppRouter />
-                                    </Preloader>
-                                </IntlProvider>
-                            </ErrorBoundary>
-                        </ModalContextProvider>
-                    </RouterProvider>
-                </ThemeProvider>
-            </ReduxProvider>
-        </>
+        <ReduxProvider store={store}>
+            <ThemeProvider>
+                <RouterProvider history={history}>
+                    <ModalContextProvider>
+                        <ErrorBoundary>
+                            <Autodetect />
+                            <Resize />
+                            <Tor />
+                            <Protocol />
+                            <OnlineStatus />
+                            <RouterHandler />
+                            <IntlProvider>
+                                <Metadata />
+                                <ToastContainer />
+                                <Preloader>
+                                    <AppRouter />
+                                </Preloader>
+                            </IntlProvider>
+                        </ErrorBoundary>
+                    </ModalContextProvider>
+                </RouterProvider>
+            </ThemeProvider>
+        </ReduxProvider>
     );
 };
 
---
+++ packages/suite-web/src/support/useCypress.ts
@@ -0,0 +1,22 @@
+import { useEffect } from 'react';
+import TrezorConnect from '@trezor/connect';
+import { useStore } from 'react-redux';
+
+/**
+ * Utility for running tests in Cypress.
+ * Used to augment window object with redux store and TrezorConnect instance to make it accessible in tests
+ */
+export const useCypress = () => {
+    const store = useStore();
+
+    useEffect(() => {
+        if (typeof window !== 'undefined' && window.Cypress) {
+            window.store = store;
+            window.TrezorConnect = TrezorConnect;
+            return () => {
+                delete window.store;
+                delete window.TrezorConnect;
+            };
+        }
+    }, [store]);
+};
