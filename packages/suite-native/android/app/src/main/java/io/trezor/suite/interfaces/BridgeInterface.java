3152c65b63ce0929d516c7e4b2e6364f1d011b14
Daniel Suchý <suchydan@gmail.com>
2022-03-21 14:35:27 +0100

    chore(native): remove old native app

--- packages/suite-native/android/app/src/main/java/io/trezor/suite/interfaces/BridgeInterface.java
+++
@@ -1,9 +0,0 @@
-package io.trezor.suite.interfaces;
-
-import java.util.List;
-
-public interface BridgeInterface {
-    public List<TrezorInterface> enumerate();
-    public TrezorInterface getDeviceByPath(String path);
-    public void findAlreadyConnectedDevices();
-}
--- packages/suite-native/index.js
+++
@@ -1,6 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './src/App';
-import { name as appName } from './app.json';
-
-// TODO: https://www.fabrizioduroni.it/2017/12/08/react-native-multiple-instance-rctrootview.html
-AppRegistry.registerComponent(appName, () => App);
