32c0b62393699b780020643df74f08cf04e8b62c
Daniel Suchy <suchydan@gmail.com>
2022-03-25 11:18:59 +0100

    chore(suite-native): config and structure cleanup
    chore(suite-native): config and structure cleanup

--- packages/suite-native/metro.config.js
+++ packages/suite-native/metro.config.js
@@ -2,23 +2,23 @@ const path = require('path');
 const nodejs = require('node-libs-browser');
 
 module.exports = {
-  projectRoot: path.resolve(__dirname, '../../'),
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-  resolver: {
-    blockList: [/libDev/],
-    extraNodeModules: {
-      // modules needed for trezor-connect
-      crypto: nodejs.crypto,
-      stream: nodejs.stream,
-      https: nodejs.https,
-      http: nodejs.http,
+    projectRoot: path.resolve(__dirname, '../../'),
+    transformer: {
+        getTransformOptions: async () => ({
+            transform: {
+                experimentalImportSupport: false,
+                inlineRequires: true,
+            },
+        }),
     },
-  },
+    resolver: {
+        blockList: [/libDev/, /news-api\/build/],
+        extraNodeModules: {
+            // modules needed for trezor-connect
+            crypto: nodejs.crypto,
+            stream: nodejs.stream,
+            https: nodejs.https,
+            http: nodejs.http,
+        },
+    },
 };
