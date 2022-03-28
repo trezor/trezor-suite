1fe88d4e5022c799f7656a0b43f65bc3f524473b
slowbackspace <slowbackspace@gmail.com>
2022-03-28 16:11:29 +0200

    feat(blockchain-link): add withdrawal and deposits amounts to transaction

--- packages/blockchain-link/tests/unit/fixtures/blockfrost/utils.ts
+++ packages/blockchain-link/tests/unit/fixtures/blockfrost/utils.ts
@@ -438,6 +438,8 @@ export default {
                             tokens: [],
                             cardanoSpecific: {
                                 subtype: 'withdrawal',
+                                withdrawal: '730065',
+                                deposit: undefined,
                             },
                             details: {
                                 vin: [
@@ -1165,6 +1167,345 @@ export default {
                     size: 289,
                     totalInput: '40515743',
                     totalOutput: '40347470',
+                },
+            },
+        },
+        {
+            description: 'Transform transaction (self, stake registration)',
+            descriptor:
+                '6d17587575a3b4f0f86ebad3977e8f7e4981faa863eccf5c1467065c74fe3435943769446dd290d103fb3d360128e86de4b47faea73ffb0900c94c6a61ef9ea2',
+            accountAddress: {
+                change: [
+                    {
+                        address:
+                            'addr1qy2mdeuujt253nzxz2v669nn8q0nlmsp7hx7e7qm73nwlpxhh5j3f89h33sgtjs8azqcwr9zmlu49plr870dalvmruxslxqfwt',
+                        path: "m/1852'/1815'/i'/1/0",
+                        transfers: 1,
+                        received: '2825699',
+                        sent: '0',
+                    },
+                    {
+                        address:
+                            'addr1qyx542efp7w244zpjkf2gj805leqls76hjyn7cpn8jw4rdkhh5j3f89h33sgtjs8azqcwr9zmlu49plr870dalvmruxsn7vajz',
+                        path: "m/1852'/1815'/i'/1/1",
+                        transfers: 0,
+                        received: '0',
+                        sent: '0',
+                    },
+                ],
+                used: [
+                    {
+                        address:
+                            'addr1qx6jxvhq2pnusc6xh0m9pt7pyrjyxy2rtpsx7lnsc325907hh5j3f89h33sgtjs8azqcwr9zmlu49plr870dalvmruxs9zkqe2',
+                        path: "m/1852'/1815'/i'/0/0",
+                        transfers: 2,
+                        received: '2400000',
+                        sent: '2400000',
+                    },
+                    {
+                        address:
+                            'addr1q8dsx5dtjd9rjuv78l5rn8827nw9hg7rj42q8gaakzg29g7hh5j3f89h33sgtjs8azqcwr9zmlu49plr870dalvmruxs0hzyx8',
+                        path: "m/1852'/1815'/i'/0/1",
+                        transfers: 2,
+                        received: '5000000',
+                        sent: '5000000',
+                    },
+                ],
+                unused: [
+                    {
+                        address:
+                            'addr1qy84tj63d8323q5rs3e7e3ds274h6zpx8ktuw3twl9yjr6khh5j3f89h33sgtjs8azqcwr9zmlu49plr870dalvmruxs530lvf',
+                        path: "m/1852'/1815'/i'/0/2",
+                        transfers: 0,
+                        received: '0',
+                        sent: '0',
+                    },
+                ],
+            },
+            data: {
+                txData: {
+                    hash: 'ae5d36efa351a9e4dbe3ae11ac3c0be65a710c2338874e5613aca5c22bd91cfe',
+                    block: '4701eb448ccd0c976b01221235c35271d9c41cb0c6fb021d0c5e77ec81e34901',
+                    block_height: 7004268,
+                    block_time: 1647445260,
+                    slot: 55878969,
+                    index: 15,
+                    output_amount: [
+                        {
+                            unit: 'lovelace',
+                            quantity: '2825699',
+                            decimals: 6,
+                        },
+                    ],
+                    fees: '174301',
+                    deposit: '2000000',
+                    size: 425,
+                    invalid_before: null,
+                    invalid_hereafter: '55885702',
+                    utxo_count: 2,
+                    withdrawal_count: 0,
+                    mir_cert_count: 0,
+                    delegation_count: 1,
+                    stake_cert_count: 1,
+                    pool_update_count: 0,
+                    pool_retire_count: 0,
+                    asset_mint_or_burn_count: 0,
+                    redeemer_count: 0,
+                    valid_contract: true,
+                },
+                txUtxos: {
+                    hash: 'ae5d36efa351a9e4dbe3ae11ac3c0be65a710c2338874e5613aca5c22bd91cfe',
+                    inputs: [
+                        {
+                            address:
+                                'addr1q8dsx5dtjd9rjuv78l5rn8827nw9hg7rj42q8gaakzg29g7hh5j3f89h33sgtjs8azqcwr9zmlu49plr870dalvmruxs0hzyx8',
+                            amount: [
+                                {
+                                    unit: 'lovelace',
+                                    quantity: '5000000',
+                                    decimals: 6,
+                                },
+                            ],
+                            tx_hash:
+                                '6d88e622cb69163eca9df8ebda0a3e92fa5084dbcd2b6adfa8746841c31e87e9',
+                            output_index: 0,
+                            collateral: false,
+                            data_hash: null,
+                        },
+                    ],
+                    outputs: [
+                        {
+                            address:
+                                'addr1qy2mdeuujt253nzxz2v669nn8q0nlmsp7hx7e7qm73nwlpxhh5j3f89h33sgtjs8azqcwr9zmlu49plr870dalvmruxslxqfwt',
+                            amount: [
+                                {
+                                    unit: 'lovelace',
+                                    quantity: '2825699',
+                                    decimals: 6,
+                                },
+                            ],
+                            output_index: 0,
+                            data_hash: null,
+                        },
+                    ],
+                },
+                address:
+                    'addr1q8dsx5dtjd9rjuv78l5rn8827nw9hg7rj42q8gaakzg29g7hh5j3f89h33sgtjs8azqcwr9zmlu49plr870dalvmruxs0hzyx8',
+                txHash: 'ae5d36efa351a9e4dbe3ae11ac3c0be65a710c2338874e5613aca5c22bd91cfe',
+            },
+            result: {
+                type: 'self',
+                txid: 'ae5d36efa351a9e4dbe3ae11ac3c0be65a710c2338874e5613aca5c22bd91cfe',
+                blockTime: 1647445260,
+                blockHeight: 7004268,
+                blockHash: '4701eb448ccd0c976b01221235c35271d9c41cb0c6fb021d0c5e77ec81e34901',
+                amount: '174301',
+                fee: '174301',
+                totalSpent: '174301',
+                targets: [],
+                tokens: [],
+                cardanoSpecific: {
+                    subtype: 'stake_registration',
+                    deposit: '2000000',
+                },
+                details: {
+                    vin: [
+                        {
+                            n: 0,
+                            addresses: [
+                                'addr1q8dsx5dtjd9rjuv78l5rn8827nw9hg7rj42q8gaakzg29g7hh5j3f89h33sgtjs8azqcwr9zmlu49plr870dalvmruxs0hzyx8',
+                            ],
+                            isAddress: true,
+                            value: '5000000',
+                        },
+                    ],
+                    vout: [
+                        {
+                            n: 0,
+                            addresses: [
+                                'addr1qy2mdeuujt253nzxz2v669nn8q0nlmsp7hx7e7qm73nwlpxhh5j3f89h33sgtjs8azqcwr9zmlu49plr870dalvmruxslxqfwt',
+                            ],
+                            isAddress: true,
+                            value: '2825699',
+                        },
+                    ],
+                    size: 425,
+                    totalInput: '5000000',
+                    totalOutput: '2825699',
+                },
+            },
+        },
+        {
+            description: 'Transform transaction (self, stake delegation)',
+            descriptor:
+                '6d17587575a3b4f0f86ebad3977e8f7e4981faa863eccf5c1467065c74fe3435943769446dd290d103fb3d360128e86de4b47faea73ffb0900c94c6a61ef9ea2',
+            accountAddress: {
+                change: [
+                    {
+                        address:
+                            'addr1q93ut5vk0tdq9jzzxal0xtz836yptp6spyt4rdxdpk9lvstvuu7shup00h3xnnv47g0klxlf7svhkz7vcmflneehhmashp5mvn',
+                        path: "m/1852'/1815'/i'/1/0",
+                        transfers: 11,
+                        received: '41329048',
+                        sent: '41329048',
+                    },
+                    {
+                        address:
+                            'addr1qx72y832hl98wfrq4949785w53f9n66434qxq0w2fwvundtvuu7shup00h3xnnv47g0klxlf7svhkz7vcmflneehhmaslep6vx',
+                        path: "m/1852'/1815'/i'/1/1",
+                        transfers: 1,
+                        received: '1083241',
+                        sent: '0',
+                    },
+                ],
+                used: [
+                    {
+                        address:
+                            'addr1qxhdfak0gu9tm6qunj5l4mlcncg6yktznsxtk6dmee8krvtvuu7shup00h3xnnv47g0klxlf7svhkz7vcmflneehhmas5m4rfw',
+                        path: "m/1852'/1815'/i'/0/0",
+                        transfers: 5,
+                        received: '7121215',
+                        sent: '7121215',
+                    },
+                    {
+                        address:
+                            'addr1qy06cynvyj70wskrjpu4lntnr2fky4uh4tdt83qly6zs2fnvuu7shup00h3xnnv47g0klxlf7svhkz7vcmflneehhmasa59ma3',
+                        path: "m/1852'/1815'/i'/0/1",
+                        transfers: 9,
+                        received: '5000000',
+                        sent: '5000000',
+                    },
+                    {
+                        address:
+                            'addr1q8clk3mtn8s0tl3ge8gg23ejajwkyndnyyswvufpmh6h72nvuu7shup00h3xnnv47g0klxlf7svhkz7vcmflneehhmasqnks2g',
+                        path: "m/1852'/1815'/i'/0/2",
+                        transfers: 6,
+                        received: '32000000',
+                        sent: '32000000',
+                    },
+                ],
+                unused: [
+                    {
+                        address:
+                            'addr1qy0vma2ykn42vcqj69aky7j9ahja98063ejxh9vv5axdhhnvuu7shup00h3xnnv47g0klxlf7svhkz7vcmflneehhmas4f4pef',
+                        path: "m/1852'/1815'/i'/0/3",
+                        transfers: 0,
+                        received: '0',
+                        sent: '0',
+                    },
+                ],
+            },
+            data: {
+                txData: {
+                    hash: 'c5dd23c59aee8688fe068c7c25ebb18612cf3da0b04fce9f0d2293247421b18c',
+                    block: '8e5015a14576e1ef2ce85440b2b39396626262223a4f1a3c0de0a575b0c6af5c',
+                    block_height: 7012003,
+                    block_time: 1647606759,
+                    slot: 56040468,
+                    index: 9,
+                    output_amount: [
+                        {
+                            unit: 'lovelace',
+                            quantity: '1083241',
+                            decimals: 6,
+                        },
+                    ],
+                    fees: '172805',
+                    deposit: '0',
+                    size: 391,
+                    invalid_before: null,
+                    invalid_hereafter: '56047354',
+                    utxo_count: 2,
+                    withdrawal_count: 0,
+                    mir_cert_count: 0,
+                    delegation_count: 1,
+                    stake_cert_count: 0,
+                    pool_update_count: 0,
+                    pool_retire_count: 0,
+                    asset_mint_or_burn_count: 0,
+                    redeemer_count: 0,
+                    valid_contract: true,
+                },
+                txUtxos: {
+                    hash: 'c5dd23c59aee8688fe068c7c25ebb18612cf3da0b04fce9f0d2293247421b18c',
+                    inputs: [
+                        {
+                            address:
+                                'addr1q93ut5vk0tdq9jzzxal0xtz836yptp6spyt4rdxdpk9lvstvuu7shup00h3xnnv47g0klxlf7svhkz7vcmflneehhmashp5mvn',
+                            amount: [
+                                {
+                                    unit: 'lovelace',
+                                    quantity: '1256046',
+                                    decimals: 6,
+                                },
+                            ],
+                            tx_hash:
+                                '40009d6d0a1901dff4722a78d23c448ccda3efe07ce0d06aeca4d7d29e53ea43',
+                            output_index: 1,
+                            collateral: false,
+                            data_hash: null,
+                        },
+                    ],
+                    outputs: [
+                        {
+                            address:
+                                'addr1qx72y832hl98wfrq4949785w53f9n66434qxq0w2fwvundtvuu7shup00h3xnnv47g0klxlf7svhkz7vcmflneehhmaslep6vx',
+                            amount: [
+                                {
+                                    unit: 'lovelace',
+                                    quantity: '1083241',
+                                    decimals: 6,
+                                },
+                            ],
+                            output_index: 0,
+                            data_hash: null,
+                        },
+                    ],
+                },
+                address:
+                    'addr1q93ut5vk0tdq9jzzxal0xtz836yptp6spyt4rdxdpk9lvstvuu7shup00h3xnnv47g0klxlf7svhkz7vcmflneehhmashp5mvn',
+                txHash: 'c5dd23c59aee8688fe068c7c25ebb18612cf3da0b04fce9f0d2293247421b18c',
+            },
+            result: {
+                type: 'self',
+                txid: 'c5dd23c59aee8688fe068c7c25ebb18612cf3da0b04fce9f0d2293247421b18c',
+                blockTime: 1647606759,
+                blockHeight: 7012003,
+                blockHash: '8e5015a14576e1ef2ce85440b2b39396626262223a4f1a3c0de0a575b0c6af5c',
+                amount: '172805',
+                fee: '172805',
+                totalSpent: '172805',
+                targets: [],
+                tokens: [],
+                cardanoSpecific: {
+                    subtype: 'stake_delegation',
+                    deposit: undefined,
+                    withdrawal: undefined,
+                },
+                details: {
+                    vin: [
+                        {
+                            n: 1,
+                            addresses: [
+                                'addr1q93ut5vk0tdq9jzzxal0xtz836yptp6spyt4rdxdpk9lvstvuu7shup00h3xnnv47g0klxlf7svhkz7vcmflneehhmashp5mvn',
+                            ],
+                            isAddress: true,
+                            value: '1256046',
+                        },
+                    ],
+                    vout: [
+                        {
+                            n: 0,
+                            addresses: [
+                                'addr1qx72y832hl98wfrq4949785w53f9n66434qxq0w2fwvundtvuu7shup00h3xnnv47g0klxlf7svhkz7vcmflneehhmaslep6vx',
+                            ],
+                            isAddress: true,
+                            value: '1083241',
+                        },
+                    ],
+                    size: 391,
+                    totalInput: '1256046',
+                    totalOutput: '1083241',
                 },
             },
         },
