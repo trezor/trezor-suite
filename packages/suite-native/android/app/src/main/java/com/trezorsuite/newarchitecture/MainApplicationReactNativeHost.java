50d174f1e3838c10e6533f91ed7df38aee9bd124
Daniel Suchy <suchydan@gmail.com>
2022-03-28 15:31:38 +0200

    chore(suite-native): enable new Fabric architecture

--- packages/suite-native/android/app/src/main/java/com/trezorsuite/newarchitecture/MainApplicationReactNativeHost.java
+++ packages/suite-native/android/app/src/main/java/com/trezorsuite/newarchitecture/MainApplicationReactNativeHost.java
@@ -56,7 +56,7 @@ public class MainApplicationReactNativeHost extends ReactNativeHost {
 
   @Override
   protected String getJSMainModuleName() {
-    return "index";
+    return "packages/suite-native/index";
   }
 
   @NonNull
