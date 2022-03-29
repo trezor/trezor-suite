4f836de7c021e3de70891b738a6f45081fd52b8e
alex-jerechinsky <75733878+alex-jerechinsky@users.noreply.github.com>
2022-03-29 10:49:49 +0200

    feat(guide): update articles
    - Adding 'Sign & Verify' and 'Electrum compatible signatures' articles
    - Adding 'Early Access Program' article
    - Edited 'Full node via Electrum Server'

--- packages/suite-data/src/guide/constants.ts
+++ packages/suite-data/src/guide/constants.ts
@@ -1,7 +1,7 @@
 import { resolve, join } from 'path';
 
 export const GITBOOK_SOURCE = 'https://github.com/trezor/trezor-suite-guide.git';
-export const GITBOOK_REVISION = '45cb71c2476819367df98bbbc1cac5f2764cfd7c';
+export const GITBOOK_REVISION = '067fc85ea90ef29303c4e8c948fde1f1708929fa';
 export const TMP = join(resolve(__dirname, '../..'), 'tmp', 'guide');
 // Path to the GitBook assets. Relative to TMP.
 export const ASSETS_DIR = join('.gitbook', 'assets');
