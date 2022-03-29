8d4e1338a4cf2cbb71d26c0b4725e6236badb477
mroz <mvarmuza@gmail.com>
2022-03-29 15:44:11 +0200

    feat: add @trezor/connect package

---
+++ packages/connect/.eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    rules: {
+        'no-bitwise': 'off', // airbnb-base: used in hardending
+        'no-underscore-dangle': 'off', // underscore is used
+        camelcase: 'off', // camelcase is used
+        'no-console': 'warn',
+    },
+};
---
+++ packages/connect/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    preset: '../../jest.config.base.js',
+    // REF-TODO: should be node (default?) testEnvironment: 'node',
+    testEnvironment: 'jsdom',
+};
