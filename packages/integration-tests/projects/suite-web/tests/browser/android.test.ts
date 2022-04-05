2fae0b6ebb59c276addf83f66265172cabb481cd
Tomáš Klíma <tomas.klim@gmail.com>
2022-04-05 11:18:02 +0200

    feat(e2e): update unsupported browser tests

--- packages/integration-tests/projects/suite-web/tests/browser/android.test.ts
+++ packages/integration-tests/projects/suite-web/tests/browser/android.test.ts
@@ -1,30 +1,28 @@
 // @group:suite
 // @retry=2
-// @user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36
+// @user-agent=Mozilla/5.0 (Linux; U; Android 2.3.6; en-us; Nexus S Build/GRK39F) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1
 
 // note that running tests in /browser folder will not work in 'debug local setup'
 
-describe('Windows 7 with outdated chrome ', () => {
+describe('Android with non-chrome browser', () => {
     before(() => {
-        cy.viewport(1080, 1440);
+        cy.viewport('samsung-note9');
         cy.resetDb();
     });
 
-    it('Should just display outdated browser', () => {
+    it('Should display unsupported browsers page', () => {
         cy.prefixedVisit('/');
         cy.document().its('fonts.status').should('equal', 'loaded');
-        cy.get('html').should('contain.text', 'Your browser is outdated');
+        cy.get('html').should('contain.text', 'Your browser is not supported');
         cy.get('img')
             .should('be.visible')
-            .should('have.length', 2)
+            .should('have.length', 1)
             .and($img => {
                 // "naturalWidth" and "naturalHeight" are set when the image loads
                 expect($img[0].naturalWidth).to.be.greaterThan(0);
-                expect($img[1].naturalWidth).to.be.greaterThan(0);
             });
-
         cy.wait(500); // wait for text rendering to finish before taking a screenshot
-        cy.matchImageSnapshot('chrome is supported but outdated');
+        cy.matchImageSnapshot('android browser is not supported');
 
         cy.get('html').should('contain.text', 'Continue at my own risk');
         cy.getTestElement('@continue-to-suite').click();
--- packages/integration-tests/projects/suite-web/tests/browser/edge.test.ts
+++ packages/integration-tests/projects/suite-web/tests/browser/edge.test.ts
@@ -16,12 +16,19 @@ describe('Windows 10 with edge browser ', () => {
         cy.get('html').should('contain.text', 'Your browser is not supported');
         cy.get('img')
             .should('be.visible')
+            .should('have.length', 3)
             .and($img => {
                 // "naturalWidth" and "naturalHeight" are set when the image loads
                 expect($img[0].naturalWidth).to.be.greaterThan(0);
                 expect($img[1].naturalWidth).to.be.greaterThan(0);
+                expect($img[2].naturalWidth).to.be.greaterThan(0);
             });
         cy.wait(500); // wait for text rendering to finish before taking a screenshot
-        cy.matchImageSnapshot('browser-not-supported');
+        cy.matchImageSnapshot('browser is not supported at all');
+
+        cy.get('html').should('contain.text', 'Continue at my own risk');
+        cy.getTestElement('@continue-to-suite').click();
+
+        cy.getTestElement('@welcome/title');
     });
 });
