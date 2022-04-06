64b143fb616ce7848acc38dacba8556568c61250
Calodzero <calodzero@Calodzeros-MacBook-Pro.local>
2022-04-06 17:31:13 +0200

    Hi, MichalKaraba here

--- packages/integration-tests/projects/suite-web/tests/assignment/assignment.test.ts
+++ packages/integration-tests/projects/suite-web/tests/assignment/assignment.test.ts
@@ -14,8 +14,8 @@ describe('Assignment test', () => {
         cy.passThroughInitialRun();
 
         /**
-         * I am adding shoud for visibility check, as sometimes test or rather env started to be flaky.
-         * Had various results with it but seems it helped.
+         * I am adding should for visibility check. Sometimes run or rather env started to be flaky.
+         * I Had various results with it but mostly it helped and test was more stable.
          */
         cy.getTestElement('@suite/menu/settings').should('be.visible').click();
         cy.getTestElement('@settings/menu/device').click();
@@ -29,8 +29,9 @@ describe('Assignment test', () => {
             /**
              * Focus with type and invoke somehow work together. On my local run, when I used only invoke('val', '')
              * in theory it suppose to be enough, but wasn't. Once I added those other parts it was doing what it suppose to.
-             * Havent figured out why standalone clear() nor type('{selectall}{backspace}') have not worked. I have noticed
-             * my focuse was behind upper tab where Dashboeard, Device and other remains, but dont know what is behind it.
+             * Haven't figured out why standalone clear() nor type('{selectall}{backspace}') have not worked. I have noticed
+             * my focus was behind upper tab where Dashboard, Device and other tabs remains, but don't know what is behind it.
+             * All in all focus().clear().type() should be working but didn't.
              */
             .focus()
             .type('{selectall}{backspace}', { force: true })
@@ -39,7 +40,7 @@ describe('Assignment test', () => {
             .type('Done!');
         /**
          * Somehow focus gets off without force:true and click wont work.
-         * Same issue as upwards with focuse. Here force is enough to click.
+         * Same issue as upwards with focus. Here force is enough to click.
          */
         cy.getTestElement('@settings/device/label-submit').click({ force: true });
         cy.getTestElement('@suite/modal/confirm-action-on-device');
@@ -47,14 +48,19 @@ describe('Assignment test', () => {
         cy.getTestElement('@suite/modal/confirm-action-on-device').should('not.exist');
 
         /**
-         * This is not implemented. Setting phasphrase settings by my self always resulted in various weird behaviors
+         * This is not implemented. Setting phassphrase settings by my self always resulted in various weird behaviors
          * and test was very unstable. I tried it with  cy.task('setupEmu', { passphrase_protection: true }); And steps to click passphrase.
          * I believe there is some auto config somewhere but I was not able to find it.
          */
         // cy.getTestElement('@suite/menu/suite-index').click();
+        /**
+         * Here maybe putting whole line 57 inside block for the box in which this phrase suppose to be would be even more precise.
+         * I saw it is possible. Feel more clear on what I intent to assert with it.
+         */
         // cy.get('Passphrase enabled');
 
+        // Last check of renamed Trezor
         cy.getTestElement('@menu/switch-device').click();
-        cy.getTestElement('Done!');
+        cy.get('Done!');
     });
 });
