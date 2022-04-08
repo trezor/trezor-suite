959a1101514ddb4dff35f5417f11557119560357
alex-jerechinsky <75733878+alex-jerechinsky@users.noreply.github.com>
2022-04-08 13:19:14 +0200

    Update constants.ts
    - Update to 'Supported coins' section
    - Updating 'Staking ADA' article
    - Sign & Verify - Remove images
    - Electrum signatures - Removing images
    
    (cherry picked from commit 8263cd668d5ff26d50fc81f65a98680dea06b5ce)

--- packages/suite-data/src/guide/constants.ts
+++ packages/suite-data/src/guide/constants.ts
@@ -1,7 +1,7 @@
 import { resolve, join } from 'path';
 
 export const GITBOOK_SOURCE = 'https://github.com/trezor/trezor-suite-guide.git';
-export const GITBOOK_REVISION = 'f4af9a0b24f4a8ef8768d61c8dd2f4ad9a817d1e';
+export const GITBOOK_REVISION = '89e34a47e0cb6baeffcee628a2b0f58044622613';
 export const TMP = join(resolve(__dirname, '../..'), 'tmp', 'guide');
 // Path to the GitBook assets. Relative to TMP.
 export const ASSETS_DIR = join('.gitbook', 'assets');
