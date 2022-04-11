618233553e2bb22e1b505022f7c019ca1d5dda69
mroz <mvarmuza@gmail.com>
2022-04-12 00:09:06 +0200

    chore: move ci

--- docker/docker-connect-test.sh
+++ docker/docker-connect-test.sh
@@ -1,6 +1,134 @@
 #!/usr/bin/env bash
-set -e
 
-xhost +
+# This script runs @trezor/connect tests.
+# It spins up trezor-user-env and sets required evironment variables.
 
-docker-compose -f ./docker/docker-compose.connect-test.yml up --build --abort-on-container-exit
+set -euo pipefail
+
+# Running standalone instance of trezor-user-env
+# docker run -it -e SDL_VIDEODRIVER="dummy" -p "9001:9001" -p "21326:21326" -p "21325:21326" registry.gitlab.com/satoshilabs/trezor/trezor-user-env/trezor-user-env
+
+# Tweaking trezor-user-env locally
+# docker run -it -e SDL_VIDEODRIVER="dummy" -p "9001:9001" -p "21326:21326" -p "21325:21326" registry.gitlab.com/satoshilabs/trezor/trezor-user-env/trezor-user-env nix-shell
+# do your changes using `vi` and run:
+# [nix-shell:/trezor-user-env]# ./run.sh
+
+# Ports
+# 9001  - websocket server, communication test case > user-env (setup etc...)
+# 21326 - trezord proxy. beacuse of trezord CORS check
+# 21325 - original trezord port redirected to trezor-user-env proxy
+
+ENVIRONEMT=$1
+
+if [[ "$ENVIRONEMT" != "web" && "$ENVIRONEMT" != "node" ]];
+    then
+        echo "either 'web' or 'node' must be specified as the first argument"
+        exit 1
+fi
+
+show_usage() {
+  echo "Usage: run [OPTIONS] [ARGS]"
+  echo ""
+  echo "Options:"
+  echo "  -c       Disable backend cache. default: enabled"
+  echo "  -d       Disable docker. Useful when running own instance of trezor-user-env. default: enabled"
+  echo "  -D PATH  Set path to docker executable. Can be replaced with `podman`. default: docker"
+  echo "  -e       All methods except excluded, example: applySettings,signTransaction"
+  echo "  -f       Use specific firmware version, example: 2.1.4, 1.8.0 default: 2-master"
+  echo "  -i       Included methods only, example: applySettings,signTransaction"
+  echo "  -s       actual test script. default: 'yarn test:integration'"
+}
+
+# default options
+FIRMWARE=""
+INCLUDED_METHODS=""
+EXCLUDED_METHODS=""
+DOCKER=true
+DOCKER_PATH="docker"
+USE_TX_CACHE=true
+USE_WS_CACHE=true
+PATTERN=""
+
+# user options
+OPTIND=2
+while getopts ":p:i:e:f:D:hdc" opt; do
+  case $opt in
+  d)
+    DOCKER=false
+    ;;
+  D)
+    DOCKER_PATH="$OPTARG"
+    ;;
+  c)
+    USE_TX_CACHE=false
+    USE_WS_CACHE=false
+    ;;
+  f)
+    FIRMWARE=$OPTARG
+    ;;
+  i)
+    INCLUDED_METHODS=$OPTARG
+    ;;
+  e)
+    EXCLUDED_METHODS=$OPTARG
+    ;;
+  p)
+    PATTERN=$OPTARG
+    ;;
+  h) # Script usage
+    show_usage
+    exit 0
+    ;;
+  \?)
+    echo "invalid option $OPTARG"
+    exit 1
+    ;;
+  esac
+done
+shift $((OPTIND - 1))
+
+if [[ $ENVIRONEMT == "node" ]];
+  then
+    SCRIPT="yarn workspace @trezor/integration-tests test:connect:node"
+  else 
+    SCRIPT="yarn workspace @trezor/integration-tests test:connect:web"
+fi
+
+# export essential process.env variables
+export TESTS_FIRMWARE=$FIRMWARE
+export TESTS_INCLUDED_METHODS=$INCLUDED_METHODS
+export TESTS_EXCLUDED_METHODS=$EXCLUDED_METHODS
+export TESTS_USE_TX_CACHE=$USE_TX_CACHE
+export TESTS_USE_WS_CACHE=$USE_WS_CACHE
+export TESTS_PATTERN=$PATTERN
+export TESTS_SCRIPT=$SCRIPT
+
+runDocker() {
+  docker-compose -f ./docker/docker-compose.connect-test.yml up --abort-on-container-exit
+}
+
+getLatestFirmware() {
+  TESTS_FIRMWARE=$(node ./packages/integration-tests/get-latest-firmware.js)
+}
+
+
+run() {
+
+  echo "Testing env: ${ENVIRONEMT}. Using: ${SCRIPT} ${PATTERN}"
+  echo "  Firmware: ${FIRMWARE}"
+  echo "  Included methods: ${INCLUDED_METHODS}"
+  echo "  Excluded methods: ${EXCLUDED_METHODS}"
+  echo "  TxCache: ${USE_TX_CACHE}"
+  echo "  WsCache: ${USE_WS_CACHE}"
+
+  if [ $DOCKER = true ]; then
+    runDocker
+  else
+    getLatestFirmware
+    $SCRIPT
+  fi
+  
+}
+
+run
+
