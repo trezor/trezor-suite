2d7748400868b552ffddf11560b0c4d35e8b2888
Marek Polak <marek.polak@satoshilabs.com>
2022-04-20 13:15:21 +0200

    feat(utils): improved and typed deep merge util

--- packages/utils/src/mergeObject.ts
+++ packages/utils/src/mergeObject.ts
@@ -1,13 +1,29 @@
-export const mergeObject = (
-    target: Record<string, any>,
-    source: Record<string, any> | string | boolean | number,
-) => {
-    Object.keys(source).forEach(key => {
-        if (source instanceof Object && source[key] instanceof Object) {
-            Object.assign(source[key], mergeObject(target[key], source[key]));
-        }
-    });
-    // Join `target` and modified `source`
-    Object.assign(target || {}, source);
-    return target;
+// Merge only objects, not arrays
+type Mergable = {
+    [key: string]: any;
+    [index: number]: never;
 };
+
+type Merge<A, B> = A extends Mergable ? (B extends Mergable ? MergeResult<A, B> : B) : B;
+
+type MergeResult<A, B> = { [K in Exclude<keyof A, keyof B>]: A[K] } & {
+    [K in Exclude<keyof B, keyof A>]: B[K];
+} & { [K in keyof A & keyof B]: Merge<A[K], B[K]> };
+
+const isMergable = (value: any): value is Mergable =>
+    value && typeof value === 'object' && !Array.isArray(value);
+
+export const mergeObject = <A extends Mergable, B extends Mergable>(
+    a: A,
+    b: B,
+): MergeResult<A, B> =>
+    Object.entries(b).reduce((current, [key, newValue]) => {
+        const currentValue = current[key];
+        return {
+            ...current,
+            [key]:
+                isMergable(currentValue) && isMergable(newValue)
+                    ? mergeObject(currentValue, newValue)
+                    : newValue,
+        };
+    }, a) as any;
