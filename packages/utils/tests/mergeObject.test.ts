030b309fdbf550a62ac5aa174c610d762b93c8a4
Marek Polak <marek.polak@satoshilabs.com>
2022-04-20 13:15:21 +0200

    feat(utils): improved and typed deep merge util

--- packages/utils/tests/mergeObject.test.ts
+++ packages/utils/tests/mergeObject.test.ts
@@ -1,27 +1,23 @@
 import { mergeObject } from '../src/mergeObject';
 
+const fixtures = [
+    ['simple merge', { a: 1, b: { a: 1 } }, { c: { a: 1 } }, { a: 1, b: { a: 1 }, c: { a: 1 } }],
+    ['second override first', { a: 'a', b: 'x' }, { c: 'c', b: 'y' }, { a: 'a', b: 'y', c: 'c' }],
+    ['object with array', { a: [1, 2], d: 4 }, { a: { b: 'c' }, d: [] }, { a: { b: 'c' }, d: [] }],
+    [
+        'nulls and undefineds',
+        { a: 1, b: undefined, d: 2 },
+        { a: null, b: null, c: undefined },
+        { a: null, b: null, c: undefined, d: 2 },
+    ],
+] as const;
+
 describe('mergeObject', () => {
-    it('should deep merge two objects', () => {
-        const target = {
-            a: 1,
-            b: {
-                a: 1,
-            },
-        };
-        const source = {
-            c: {
-                a: 1,
-            },
-        };
-        const expected = {
-            a: 1,
-            b: {
-                a: 1,
-            },
-            c: {
-                a: 1,
-            },
-        };
-        expect(mergeObject(target, source)).toEqual(expected);
+    describe('should deep merge two objects', () => {
+        fixtures.forEach(([description, target, source, expected]) =>
+            it(description, () => {
+                expect(mergeObject(target, source)).toEqual(expected);
+            }),
+        );
     });
 });
