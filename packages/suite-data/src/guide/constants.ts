2d8eb7deacf9e4d241ee84ac24c0b17af3cb9224
alex-jerechinsky <75733878+alex-jerechinsky@users.noreply.github.com>
2022-04-22 11:18:33 +0200

    feat(guide): add images to articles
    - Images added to 'Electrum compatible signatures' article
    - Image added to 'Sign & Verify' article
    - Minor typo fixed, image included in 'EAP' article

--- packages/suite-data/src/guide/constants.ts
+++ packages/suite-data/src/guide/constants.ts
@@ -1,7 +1,7 @@
 import { resolve, join } from 'path';
 
 export const GITBOOK_SOURCE = 'https://github.com/trezor/trezor-suite-guide.git';
-export const GITBOOK_REVISION = '89e34a47e0cb6baeffcee628a2b0f58044622613';
+export const GITBOOK_REVISION = '4ea34d3bb07036a747baa329065eba1a519c7698';
 export const TMP = join(resolve(__dirname, '../..'), 'tmp', 'guide');
 // Path to the GitBook assets. Relative to TMP.
 export const ASSETS_DIR = join('.gitbook', 'assets');
