9336822c946aeacd377bcb04e3837b1733d7f528
wendys-cats <simona.skytova@satoshilabs.com>
2022-04-29 10:51:14 +0200

    test/early-access-test

--- packages/integration-tests/projects/suite-web/tests/suite/early-access.test.ts
+++ packages/integration-tests/projects/suite-web/tests/suite/early-access.test.ts
@@ -1,57 +1,52 @@
 // @group:suite
 // @retry=2
 
-//steps:
+// steps:
 // seeded trezor
 // 1 navigate to settings
 // 2 scroll down to Experimental features
 // log version you are using right now
-   
-describe('early-access', function () {
-    beforeEach(function() {
+// TODO: make some real use of this test
+
+describe('early-access', () => {
+    beforeEach(() => {
         cy.task('startBridge');
         cy.task('startEmu', { wipe: true, version: '2.4.3' });
-        cy.task('setupEmu', { passphrase_protection: true, mnemonic: 'all all all all all all all all all all all all'  });
-        
+        cy.task('setupEmu', {
+            passphrase_protection: true,
+            mnemonic: 'all all all all all all all all all all all all',
+        });
 
-    
         cy.task('applySettings', { passphrase_always_on_device: false });
 
         cy.viewport(1080, 1440).resetDb();
         cy.prefixedVisit('/');
-        cy.wait( 6000 );
+        cy.wait(6000);
         cy.passThroughInitialRun();
     });
 
-    it('early access', function () {
+    afterEach(() => {
+        cy.task('stopEmu');
+    });
+
+    it('early access', () => {
         const passphraseToType = 'taxation is theft{enter}';
 
-
         cy.getTestElement('@passphrase/input', { timeout: 60000 }).type(passphraseToType);
 
         cy.task('pressYes');
         cy.task('pressYes');
 
-        cy.getTestElement('@passphrase/input', { timeout: 60000 }).type(passphraseToType);
-        cy.getTestElement('@passphrase/confirm-checkbox').click();
-        cy.getTestElement('@passphrase/hidden/submit-button').click();
-
-        cy.task('pressYes');
-        cy.task('pressYes');
-        
         cy.getTestElement('@dashboard/loading', { timeout: 30000 });
         cy.getTestElement('@dashboard/loading', { timeout: 30000 }).should('not.exist');
-        
 
         cy.getTestElement('@suite/menu/settings').click();
         cy.getTestElement('@settings/version').should('exist');
-       
-        
-        cy.get('a[href="https://github.com/trezor/trezor-suite/releases/tag/v22.5.0"]')
-        .invoke('text')
-        .then( function (readText) {
-            cy.log(readText)
-              .invoke('log');
-        })
+
+        cy.get('a[href^="https://github.com/trezor/trezor-suite/releases/tag/"]')
+            .invoke('text')
+            .then(readText => {
+                cy.log(readText);
+            });
     });
 });
