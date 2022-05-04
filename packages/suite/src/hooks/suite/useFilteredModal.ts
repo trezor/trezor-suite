d00fc83bcf008f429bbb8f06d76c6d89053cd4de
Marek Polak <marek.polak@satoshilabs.com>
2022-05-04 16:49:38 +0200

    feat(suite): add useFilteredModal hook

---
+++ packages/suite/src/hooks/suite/useFilteredModal.ts
@@ -0,0 +1,36 @@
+import { useSelector } from '@suite-hooks';
+import { MODAL } from '@suite-actions/constants';
+import type { State as ModalState } from '@suite-reducers/modalReducer';
+import type { UserContextPayload } from '@suite-actions/modalActions';
+
+type ModalContext = ModalState['context'];
+type UserModalType = UserContextPayload['type'];
+type UserModalState = Extract<ModalState, { context: typeof MODAL.CONTEXT_USER }>;
+
+const hasGivenContext = <T extends ModalContext>(
+    modal: ModalState,
+    context: T[],
+): modal is Extract<ModalState, { context: T }> => context.includes(modal.context as any);
+
+const hasGivenUserModalType = <T extends UserModalType>(
+    modal: UserModalState,
+    type: T[],
+): modal is Extract<UserModalState, { payload: { type: T } }> =>
+    type.includes(modal.payload.type as any);
+
+export const useFilteredModal = <T extends ModalContext>(
+    context: T[],
+    type?: T extends typeof MODAL.CONTEXT_USER ? UserModalType[] : never,
+): Extract<ModalState, { context: T }> | null => {
+    const modal = useSelector(state => state.modal);
+
+    if (!hasGivenContext(modal, context)) {
+        return null;
+    }
+
+    if (modal.context === MODAL.CONTEXT_USER && type && !hasGivenUserModalType(modal, type)) {
+        return null;
+    }
+
+    return modal;
+};
