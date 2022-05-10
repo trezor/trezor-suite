be560deb7171f3d58853a6d38bc00183b9b45486
Tomáš Klíma <tomas.klim@gmail.com>
2022-05-10 13:57:18 +0200

    feat(suite-analytics): suite analytics package

---
+++ packages/suite-analytics/src/index.ts
@@ -0,0 +1,12 @@
+import { Analytics } from '@trezor/analytics';
+
+import { VERSION } from './constants';
+
+import type { SuiteAnalyticsEvent } from './types/events';
+
+const analytics = new Analytics<SuiteAnalyticsEvent>(VERSION, 'suite');
+
+export { analytics };
+export * from './types/definitions';
+export * from './types/events';
+export * from './constants';
