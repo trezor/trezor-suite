2cdfa920fb243444c73c662653fed9256f202ed3
Tomáš Klíma <tomas.klim@gmail.com>
2022-05-11 11:16:50 +0200

    feat(news-api): return empty array instead of posts, deprecate package

--- packages/news-api/README.md
+++ packages/news-api/README.md
@@ -1,1 +1,5 @@
 # @trezor/news-api
+
+May 2022
+Package got deprecated.
+An empty array instead of news is returned to hide News section in Suite app and to avoid fetching errors.
--- packages/news-api/src/server.ts
+++ packages/news-api/src/server.ts
@@ -1,8 +1,8 @@
 import express, { Application } from 'express';
-import fetcher from './index';
 import cors from 'cors';
-import pkg from '../package.json';
 import childProcess from 'child_process';
+
+import pkg from '../package.json';
 
 const app: Application = express();
 const PORT = process.env.PORT || 3003;
@@ -11,53 +11,28 @@ const options: cors.CorsOptions = {
     origin: '*',
 };
 
-const invalidLimitRes = JSON.stringify({
-    status: 'error',
-    errorMsg: 'Invalid limit param',
-});
-
 app.use(cors(options));
 
-app.get('/', (_req, res) => {
-    res.send(`trezor news api`);
+app.get('/', (_, res) => {
+    res.send(`Deprecated Trezor news api.`);
 });
 
-app.get('/posts', (req, res) => {
-    let limitCount;
-    const { limit } = req.query;
-
-    if (limit) {
-        if (typeof limit !== 'string') {
-            return res.status(400).end(invalidLimitRes);
-        }
-        limitCount = parseInt(limit, 10);
-        if (Number.isNaN(limitCount)) {
-            return res.status(400).end(invalidLimitRes);
-        }
-    }
-
-    fetcher((statusCode, data: string | null, errorMsg) => {
-        res.setHeader('Content-Type', 'application/json');
-        if (statusCode !== 200) {
-            res.statusCode = statusCode;
-            res.end(JSON.stringify({ status: 'error', errorMsg }));
-        } else {
-            res.end(data);
-        }
-    }, limitCount);
+app.get('/posts', (_, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify([]));
 });
 
 app.get('/status', (_req, res) => {
-    res.setHeader('Content-Type', 'application/json');
     const revision = childProcess.execSync('git rev-parse HEAD').toString().trim();
 
+    res.setHeader('Content-Type', 'application/json');
     res.end(
         JSON.stringify({ status: 'OK', app: pkg.name, version: pkg.version, commit: revision }),
     );
 });
 
 app.get('*', (_req, res) => {
-    res.send(`not found`);
+    res.send(`Not found. Deprecated Trezor news api.`);
 });
 
 app.listen(PORT, () => {
