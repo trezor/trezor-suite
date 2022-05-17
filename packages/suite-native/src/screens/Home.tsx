eaac8403a16f80355730fbc9ff58c68827a401aa
Daniel Suchý <suchydan@gmail.com>
2022-05-17 19:44:22 +0200

    chore: remove connect showcase

--- packages/suite-native/src/screens/Home.tsx
+++ packages/suite-native/src/screens/Home.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { Platform, SafeAreaView, ScrollView, StatusBar, useColorScheme, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView, ScrollView, StatusBar, useColorScheme, View } from 'react-native';
 
-import { Text, Box, Button, Icon } from '@trezor/atoms';
+import { Box, Button, Icon, Text } from '@trezor/atoms';
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
 
 const backgroundStyle = prepareNativeStyle<{ isDarkMode: boolean }>(
@@ -12,48 +12,9 @@ const backgroundStyle = prepareNativeStyle<{ isDarkMode: boolean }>(
     }),
 );
 
-const connectOptions = {
-    transportReconnect: true,
-    debug: true,
-    manifest: {
-        email: 'info@trezor.io',
-        appUrl: '@trezor/suite',
-    },
-};
-
 export const Home = () => {
     const isDarkMode = useColorScheme() === 'dark';
-    const [connectStatus, setConnectStatus] = useState<any>('');
-    const [deviceState, setDeviceState] = useState<any>('');
     const { applyStyle } = useNativeStyles();
-
-    const getFeatures = useCallback(() => {
-        console.log('getting features');
-        TrezorConnect.getDeviceState()
-            .then(state => {
-                setDeviceState(`${Math.random()} ${JSON.stringify(state)}`);
-            })
-            .catch(error => {
-                console.log(`get device state failed ${JSON.stringify(error)}}`);
-                setDeviceState(`${Math.random()}`);
-            });
-    }, []);
-
-    useEffect(() => {
-        // prevent errors spam on iOS
-        if (Platform.OS === 'android') {
-            TrezorConnect.init(connectOptions)
-                .then(result => {
-                    setConnectStatus('Init OK');
-                    console.log(result);
-                    getFeatures();
-                })
-                .catch(error => {
-                    setConnectStatus(`Init failed ${JSON.stringify(error.code)}`);
-                });
-        }
-    }, [getFeatures]);
-    console.log('rerender Home');
 
     return (
         <SafeAreaView style={applyStyle(backgroundStyle, { isDarkMode })}>
@@ -92,20 +53,10 @@ export const Home = () => {
                         <Icon type="warningCircle" size="big" color="black" />
                     </Box>
                     <Box marginVertical="md">
-                        <Button onPress={getFeatures} size="md" colorScheme="primary">
+                        <Button onPress={() => {}} size="md" colorScheme="primary">
                             My Fancy Button
                         </Button>
                     </Box>
-
-                    <Box>
-                        <Text variant="highlight">Connect status:</Text>
-                        <Text>{connectStatus}</Text>
-                    </Box>
-
-                    <Button onPress={getFeatures} size="md" colorScheme="primary">
-                        get device state
-                    </Button>
-                    <Text>Device features: {deviceState}</Text>
                 </View>
             </ScrollView>
         </SafeAreaView>
