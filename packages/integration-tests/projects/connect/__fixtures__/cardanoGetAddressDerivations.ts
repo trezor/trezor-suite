ca515f256b2b51e77e56c05099892f1e84e9acda
mroz <mvarmuza@gmail.com>
2022-05-19 08:54:28 +0200

    test(connect): add tests for cardano derivations

---
+++ packages/integration-tests/projects/connect/__fixtures__/cardanoGetAddressDerivations.ts
@@ -0,0 +1,43 @@
+import commonFixtures from '../../../../../submodules/trezor-common/tests/fixtures/cardano/get_base_address.derivations.json';
+
+import { CardanoAddressType, CardanoDerivationType } from '@trezor/transport/lib/types/messages';
+
+export default {
+    method: 'cardanoGetAddress',
+    setup: {
+        mnemonic: commonFixtures.setup.mnemonic,
+    },
+    tests: commonFixtures.tests.flatMap(({ name, parameters, result }) => ({
+        description: name,
+        params: {
+            addressParameters: {
+                // @ts-ignore loading untyped json
+                addressType: CardanoAddressType[parameters.address_type.toUpperCase()],
+                path: parameters.path,
+                stakingPath: parameters.staking_path,
+            },
+            // @ts-ignore loading untyped json
+            derivationType: CardanoDerivationType[parameters.derivation_type],
+            networkId: parameters.network_id,
+            protocolMagic: parameters.protocol_magic,
+        },
+        result: {
+            address: result.expected_address,
+        },
+        legacyResults: [
+            {
+                // before derivations were implemented
+                rules: ['<2.4.3', '1'],
+                payload: {
+                    address:
+                        'addr1q8g9th06vccxzl96nr905al0vgg2t0fqfxrxmv7ecye3x2e66sl9kvzd905ad5natzd5wghy2w3a9lm0y7u7c5sv0c2snql3c4',
+                },
+            },
+            {
+                // cardanoGetAddress not supported below this version
+                rules: ['<2.3.2', '1'],
+                success: false,
+            },
+        ],
+    })),
+};
