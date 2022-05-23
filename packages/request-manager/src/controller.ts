dfaedac4fb89d2c272a9d3f643c2996c2105c01c
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2022-05-23 14:17:35 +0200

    fix(request-manager): catch errors

--- packages/request-manager/src/controller.ts
+++ packages/request-manager/src/controller.ts
@@ -17,21 +17,32 @@ export class TorController extends EventEmitter {
         this.controlPort = new TorControlPort(options);
     }
 
-    waitUntilAlive() {
-        const waitUntilResponse = async (triesCount: number) => {
+    waitUntilAlive(): Promise<void> {
+        const errorMessages: string[] = [];
+        const waitUntilResponse = async (triesCount: number): Promise<void> => {
             if (triesCount >= this.maxTriesWaiting) {
-                throw new Error('Timeout waiting for TOR control port');
+                throw new Error(
+                    `Timeout waiting for TOR control port: \n${errorMessages.join('\n')}`,
+                );
             }
-            const isAlive = await this.controlPort
-                .connect()
-                .then(() => this.controlPort.ping())
-                .catch(() => false);
-            if (isAlive) {
-                // It is running so let's not wait anymore.
-                return;
+            try {
+                const isConnected = await this.controlPort.connect();
+                const isAlive = this.controlPort.ping();
+                if (isConnected && isAlive) {
+                    // It is running so let's not wait anymore.
+                    return;
+                }
+            } catch (error) {
+                // Some error here is expected when waiting but
+                // we do not want to throw untill maxTriesWaiting is reach.
+                // Instead we want to log it to know what causes the error.
+                if (error && error.message) {
+                    console.warn('request-manager:', error.message);
+                    errorMessages.push(error.message);
+                }
             }
             await createTimeoutPromise(this.waitingTime);
-            await waitUntilResponse(triesCount + 1);
+            return waitUntilResponse(triesCount + 1);
         };
         return waitUntilResponse(1);
     }
