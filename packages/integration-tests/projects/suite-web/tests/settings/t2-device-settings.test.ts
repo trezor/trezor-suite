d72ff4c5b9f3cbe2ccb4be47170f88c08e9271d1
ondracja <9817938+ondracja@users.noreply.github.com>
2022-05-24 16:11:40 +0200

    test/changeDeviceName (#5477)
    * expanding current t2 device test

--- packages/integration-tests/projects/suite-web/tests/settings/t2-device-settings.test.ts
+++ packages/integration-tests/projects/suite-web/tests/settings/t2-device-settings.test.ts
@@ -6,34 +6,59 @@ describe('T2 - Device settings', () => {
         cy.viewport(1080, 1440).resetDb();
         cy.task('startBridge');
     });
-    // cypress open todo: seems like entering urls (/settings/device) directly does not work anymore?
+    // TODO: cypress open: seems like entering urls (/settings/device) directly does not work anymore?
+    /*
+     * Test case:
+     * 1. Navigate to settings/device screen and wait for it to load
+     * 2. open the firmware update modal
+     * 3. verify it by clicking on the close btn
+     * 4. change the trezor's name via its input
+     * 5. verify the name from top left wallet overview btn
+     * 6. enable the passphrase protection
+     * 7. verify that the passphrase input is now enabled
+     * 8. change the device's background
+     * 9. hange the device's rotation
+     */
     it('change all possible device settings', () => {
+        //
+        // Test preparation & constants
+        //
+        const newDeviceName = 'TREVOR!';
+        const editNameBtn = '@settings/device/label-submit';
+
         cy.task('startEmu', { wipe: true });
         cy.task('setupEmu');
-
         // navigate to device settings page
         cy.prefixedVisit('/');
         cy.passThroughInitialRun();
         cy.getTestElement('@suite/menu/settings').click();
         cy.getTestElement('@settings/menu/device').click();
 
-        // firmware modal
+        //
+        // Test execution
+        //
+
+        // verify firmware modal
         cy.log('open firmware modal and close it again');
-        cy.getTestElement('@settings/device/update-button').click({ scrollBehavior: false });
+        cy.getTestElement('@settings/device/update-button')
+            .should('be.visible')
+            .click({ scrollBehavior: false });
         cy.getTestElement('@modal/close-button').click();
 
-        // change label
-        cy.log('change label');
-        cy.getTestElement('@settings/device/label-input')
-            .should('have.value', 'My Trevor')
-            .clear()
-            .type('My Tenzor');
-        cy.getTestElement('@settings/device/label-submit').click();
+        // change device's name
+        cy.log(`-> Filling in ${newDeviceName} as new trezor's name.`);
+        cy.getTestElement('@settings/device/label-input').clear().type(newDeviceName);
+        cy.getTestElement(editNameBtn).should('be.enabled');
+        cy.getTestElement(editNameBtn).click();
         cy.getConfirmActionOnDeviceModal();
         cy.task('pressYes');
         cy.getConfirmActionOnDeviceModal().should('not.exist');
+        cy.log('-> Done.');
 
-        // passphrase protection
+        // verify the name change
+        cy.getTestElement('@menu/switch-device').contains(newDeviceName);
+
+        // enable passphrase protection
         cy.log('turn on passphrase protection');
         cy.getTestElement('@settings/device/passphrase-switch')
             .click({ force: true })
@@ -41,7 +66,10 @@ describe('T2 - Device settings', () => {
         cy.task('pressYes');
         cy.getConfirmActionOnDeviceModal().should('not.exist');
 
-        // background
+        // verify enabling
+        cy.getTestElement('@settings/device/passphrase-switch').find('input').should('be.checked');
+
+        // change background
         cy.log('change background');
         cy.getTestElement('@settings/device/select-from-gallery')
             .click()
@@ -51,7 +79,7 @@ describe('T2 - Device settings', () => {
         cy.task('pressYes');
         cy.getConfirmActionOnDeviceModal().should('not.exist');
 
-        // display rotation
+        // change display rotation
         cy.log('change display rotation');
         cy.getTestElement('@settings/device/rotation-button/90')
             .click()
