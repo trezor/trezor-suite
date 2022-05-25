23ff350a325082043a6c4604c9d6e900b3345489
Marek Polak <marek.polak@satoshilabs.com>
2022-05-25 16:00:28 +0200

    chore(urls): add @trezor/urls package

---
+++ packages/urls/src/urls.ts
@@ -0,0 +1,83 @@
+export const TREZOR_URL = 'https://trezor.io';
+export const TREZOR_FORUM_URL = 'https://forum.trezor.io/';
+export const TREZOR_BLOG_URL = 'https://blog.trezor.io/';
+export const TREZOR_SHOP_URL = 'https://shop.trezor.io/';
+export const TREZOR_START_URL = 'https://trezor.io/start';
+export const TREZOR_COINS_URL = 'https://trezor.io/coins/';
+export const TREZOR_SUPPORT_URL = 'https://trezor.io/support/';
+export const TREZOR_RESELLERS_URL = 'https://trezor.io/resellers/';
+export const TREZOR_PASSWORDS_URL = 'https://trezor.io/passwords/';
+export const TREZOR_SIGNING_KEY_URL = 'https://trezor.io/security/satoshilabs-2021-signing-key.asc';
+export const TREZOR_TROUBLESHOOTING_URL = 'https://trezor.io/support/#technical-technical-issues';
+export const TREZOR_WALLET_ACCESS_URL =
+    'https://trezor.io/support/technical/i-cannot-access-the-wallet/';
+
+export const DATA_URL = 'https://data.trezor.io';
+export const DATA_TOS_URL = 'https://data.trezor.io/legal/wallet-terms.pdf';
+export const DATA_TOS_INVITY_URL = 'https://data.trezor.io/legal/invity-terms-of-use.pdf';
+
+export const DOCS_ANALYTICS_URL = 'https://docs.trezor.io/trezor-suite/analytics/';
+
+export const SUITE_URL = 'https://suite.trezor.io';
+export const SUITE_BACKUP_URL = 'https://suite.trezor.io/web/backup/';
+export const SUITE_BRIDGE_URL = 'https://suite.trezor.io/web/bridge/';
+export const SUITE_FIRMWARE_URL = 'https://suite.trezor.io/web/firmware/';
+export const SUITE_UDEV_URL = 'https://suite.trezor.io/web/udev/';
+
+export const WIKI_URL = 'https://wiki.trezor.io/';
+export const WIKI_PIN_URL = 'https://wiki.trezor.io/User_manual:Entering_PIN';
+export const WIKI_DRY_RUN_URL = 'https://wiki.trezor.io/User_manual:Dry-run_recovery';
+export const WIKI_PASSPHRASE_URL = 'https://wiki.trezor.io/Passphrase';
+export const WIKI_USER_MANUAL_URL = 'https://wiki.trezor.io/User_manual';
+export const WIKI_RECOVERY_SEED_URL = 'https://wiki.trezor.io/Recovery_seed';
+export const WIKI_PACKAGING_URL = 'https://wiki.trezor.io/Trezor_packaging_timeline';
+export const WIKI_RECOVERY_T1_URL = 'https://wiki.trezor.io/User_manual:Recovery__T1';
+export const WIKI_XRP_URL = 'https://wiki.trezor.io/Ripple_(XRP)';
+export const WIKI_CASHADDR_URL = 'https://wiki.trezor.io/CashAddr#cashaddr';
+export const WIKI_QR_CODE = 'https://wiki.trezor.io/QR_code';
+export const WIKI_BIP84_URL = 'https://wiki.trezor.io/Bech32';
+export const WIKI_BIP86_URL = 'https://wiki.trezor.io/Taproot';
+export const WIKI_BIP49_URL = 'https://wiki.trezor.io/P2SH';
+export const WIKI_BIP44_URL = 'https://wiki.trezor.io/P2PKH';
+export const WIKI_UDEV_URL = 'https://wiki.trezor.io/Udev_rules';
+export const WIKI_TOR_URL = 'https://wiki.trezor.io/Glossary:Tor';
+export const WIKI_HOW_TO_RUN_URL = 'https://wiki.trezor.io/Apps:Trezor_Suite#How_to_run';
+export const WIKI_FW_DOWNGRADE_URL = 'https://wiki.trezor.io/Firmware_downgrade';
+export const WIKI_FAILED_BACKUP_URL = 'https://wiki.trezor.io/Recovery_seed_backup_fail';
+export const WIKI_ADVANCED_RECOVERY_URL = 'https://wiki.trezor.io/User_manual:Advanced_recovery';
+export const WIKI_XPUB_URL =
+    'https://wiki.trezor.io/Suite_manual:Displaying_account_public_key_(XPUB)';
+
+export const GITHUB_ROADMAP_URL = 'https://github.com/orgs/trezor/projects/28?fullscreen=true';
+export const GITHUB_FW_COMMIT_URL = 'https://github.com/trezor/trezor-firmware/commit/';
+export const GITHUB_FW_CHANGELOG_URL =
+    'https://github.com/trezor/trezor-firmware/blob/master/core/CHANGELOG.md';
+export const GITHUB_FW_BINARIES_URL =
+    'https://github.com/trezor/webwallet-data/tree/master/firmware';
+export const GITHUB_BRIDGE_CHANGELOG_URL =
+    'https://github.com/trezor/trezord-go/blob/master/CHANGELOG.md';
+
+export const SOCIAL_TWITTER_URL = 'https://twitter.com/trezor';
+export const SOCIAL_FACEBOOK_URL = 'https://www.facebook.com/trezor.io';
+
+export const SATOSHILABS_URL = 'https://satoshilabs.com';
+export const INVITY_URL = 'https://invity.io/';
+export const HOMESCREEN_EDITOR_URL = 'https://trezor.github.io/homescreen-editor/';
+export const LTC_ADDRESS_INFO_URL =
+    'https://blog.trezor.io/litecoins-new-p2sh-segwit-addresses-843633e3e707';
+
+export const CARDANO_STAKE_POOL_MAINNET_URL =
+    'https://trezor-cardano-mainnet.blockfrost.io/api/v0/pools/';
+export const CARDANO_STAKE_POOL_TESTNET_URL =
+    'https://trezor-cardano-testnet.blockfrost.io/api/v0/pools/';
+
+export const CHROME_URL = 'https://www.google.com/chrome/';
+export const CHROME_UPDATE_URL = 'https://support.google.com/chrome/answer/95414';
+export const CHROME_ANDROID_URL =
+    'https://play.google.com/store/apps/details?id=com.android.chrome';
+export const FIREFOX_URL = 'https://www.mozilla.org/firefox/new/';
+export const FIREFOX_UPDATE_URL =
+    'https://support.mozilla.org/en-US/kb/update-firefox-latest-release';
+export const CHROMIUM_URL = 'https://www.chromium.org/';
+export const ELECTRON_URL = 'https://www.electronjs.org/';
+export const TOR_PROJECT_URL = 'https://www.torproject.org/';
