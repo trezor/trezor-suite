4298c31c0b72efb28430e8d0b902984e891a241c
Marek Polak <marek.polak@satoshilabs.com>
2022-05-25 16:46:32 +0200

    chore(connect-popup): use urls package

--- packages/connect-popup/src/view/firmwareRequiredUpdate.ts
+++ packages/connect-popup/src/view/firmwareRequiredUpdate.ts
@@ -1,8 +1,8 @@
 // origin: https://github.com/trezor/connect/blob/develop/src/js/popup/view/firmwareRequiredUpdate.js
 
 import { UiRequestUnexpectedDeviceMode } from '@trezor/connect';
+import { SUITE_FIRMWARE_URL } from '@trezor/urls';
 import { showView } from './common';
-import { SUITE_FIRMWARE } from '../urls';
 
 export const firmwareRequiredUpdate = (device: UiRequestUnexpectedDeviceMode['payload']) => {
     const view = showView('firmware-update');
@@ -11,5 +11,5 @@ export const firmwareRequiredUpdate = (device: UiRequestUnexpectedDeviceMode['pa
 
     const button = view.getElementsByClassName('confirm')[0];
 
-    button.setAttribute('href', SUITE_FIRMWARE);
+    button.setAttribute('href', SUITE_FIRMWARE_URL);
 };
