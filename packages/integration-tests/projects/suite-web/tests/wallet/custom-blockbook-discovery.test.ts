d51ba897e13be4bdfa598dc4f6415310a51dc3ef
wendys-cats <simona.skytova@satoshilabs.com>
2022-05-31 14:53:09 +0200

    test(e2e): btc custom blockbook discovery

---
+++ packages/integration-tests/projects/suite-web/tests/wallet/custom-blockbook-discovery.test.ts
@@ -0,0 +1,56 @@
+// @group:suite
+// @retry=2
+
+describe('Custom-blockbook-discovery', () => {
+    beforeEach(() => {
+        cy.task('startEmu', { wipe: true });
+        cy.task('setupEmu', {
+            needs_backup: true,
+            mnemonic: 'all all all all all all all all all all all all',
+        });
+        cy.task('startBridge');
+
+        cy.viewport(1080, 1440).resetDb();
+        cy.prefixedVisit('/settings/coins');
+        cy.getTestElement('@suite/menu/settings').click();
+        cy.getTestElement('@settings/menu/wallet').click();
+    });
+
+    afterEach(() => {
+        cy.task('stopEmu');
+    });
+
+    /* Test case
+    1. Go to Crypto Settings
+    2. Change backend to custom blockbook 
+    3. Go to Dashboard
+    4. Pass discovery
+    5. Assert discovery is success (graph render)
+    */
+    it('BTC-custom-blockbook-discovery', () => {
+        //
+        // Test preparation
+        //
+        const customBTCblockbook = 'https://btc1.trezor.io';
+
+        //
+        // Test execution
+        //
+        cy.getTestElement('@settings/wallet/network/btc', { timeout: 30000 })
+            .should('exist')
+            .trigger('mouseover');
+        cy.getTestElement('@settings/wallet/network/btc/advance').click();
+        cy.getTestElement('@modal').should('exist');
+        cy.getTestElement('@settings/advance/select-type/input').click();
+        cy.getTestElement('@settings/advance/select-type/option/blockbook').click();
+        cy.getTestElement('@settings/advance/url').type(customBTCblockbook);
+        cy.getTestElement('@settings/advance/button/save').click();
+        cy.getTestElement('@settings/menu/close').click();
+
+        //
+        // Assert
+        //
+        // when graph becomes visible, discovery was finished
+        cy.getTestElement('@dashboard/graph', { timeout: 30000 }).should('exist');
+    });
+});
