7d4f2636a27dd56645a2b4897204febeb87714ec
jakub.ondracek <jakub.ondracek@satoshilabs.com>
2022-05-31 11:40:23 +0200

    feat(e2e): updating the e2e documentation and fixing the path bug

---
+++ packages/integration-tests/projects/suite-web/tests/wallet/check-xpub-btc.test.ts
@@ -0,0 +1,47 @@
+// @group:suite
+// @retry=2
+
+describe('Check Bitcoin XPUB', () => {
+    beforeEach(() => {
+        cy.task('startEmu', { wipe: true });
+        cy.task('setupEmu', {
+            needs_backup: false,
+            mnemonic: 'all all all all all all all all all all all all',
+        });
+        cy.task('startBridge');
+
+        cy.viewport(1080, 1440).resetDb();
+        cy.prefixedVisit('/');
+        cy.passThroughInitialRun();
+        cy.discoveryShouldFinish();
+    });
+
+    afterEach(() => {
+        cy.task('stopEmu');
+    });
+
+    /* Test case
+    1. Pass onboarding
+    2. Navigate to Accounts/BTC/Account/
+    3. Click on @wallets/details/show-xpub-button
+    4. Check that @xpub-modal/xpub-field is present
+    */
+    it('Check Bitcoin XPUB', () => {
+        //
+        // Test preparation
+        //
+
+        //
+        // Test execution
+        //
+        cy.getTestElement('@suite/menu/wallet-index').click();
+        cy.getTestElement('@wallet/menu/wallet-details').click();
+        cy.getTestElement('@wallets/details/show-xpub-button').click();
+        cy.getTestElement('@xpub-modal/xpub-field').should('exist');
+
+        //
+        // Assert
+        //
+        cy.getTestElement('@xpub-modal/xpub-field').should('be.visible');
+    });
+});
