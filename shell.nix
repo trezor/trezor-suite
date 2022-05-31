4855b50800d93ab1ff078a1fc5d5230966a2796a
vdovhanych <dovhanych@me.com>
2022-05-31 16:39:55 +0200

    chore(all): update to nodejs version 16

--- shell.nix
+++ shell.nix
@@ -1,13 +1,15 @@
-# the last successful build of nixpkgs-unstable as of 2021-11-16
+# the last successful build of nixpkgs-unstable as of 2022-05-31
 with import
   (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/5cb226a06c49f7a2d02863d0b5786a310599df6b.tar.gz";
-    sha256 = "0dzz207swwm5m0dyibhxg5psccrcqfh1lzkmzzfns27wc4ria6z3";
+    url = "https://github.com/NixOS/nixpkgs/archive/b62ada430501de88dfbb08cea4eb98ead3a5e3e7.tar.gz";
+    sha256 = "1ppaixbncqyvy2ixskwvzggjsw20j77iy3aqyk4749dvkx0ml27f";
   })
 { };
 
 let
-  electron = electron_15;  # use the same version as defined in packages/suite-desktop/package.json
+  # unstable packages
+  electron = electron_18;  # use the same version as defined in packages/suite-desktop/package.json
+  nodejs = nodejs-16_x;
 in
   stdenv.mkDerivation {
     name = "trezor-suite-dev";
