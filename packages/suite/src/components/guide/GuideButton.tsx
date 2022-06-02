90e17aa3eb9b5023580ffef754b5753b0ea34986
Jan Komarek <jan.komarek@satoshilabs.com>
2022-06-02 14:00:22 +0200

    fix(suite): fix GuidePanel position

--- packages/suite/src/components/guide/GuideButton.tsx
+++ packages/suite/src/components/guide/GuideButton.tsx
@@ -6,7 +6,7 @@ import { resolveStaticPath } from '@trezor/utils';
 import { useGuide } from '@guide-hooks';
 import { FreeFocusInside } from 'react-focus-lock';
 
-const Wrapper = styled.button`
+const Wrapper = styled.button<{ $isGuideOpen: boolean }>`
     display: flex;
     justify-content: center;
     align-items: center;
@@ -23,18 +23,23 @@ const Wrapper = styled.button`
     box-shadow: 0 2px 7px 0 ${({ theme }) => theme.BOX_SHADOW_BLACK_15},
         0 2px 3px 0 ${({ theme }) => theme.BOX_SHADOW_BLACK_5};
     transition: opacity 0.3s ease 0.3s;
+    opacity: ${({ $isGuideOpen }) => ($isGuideOpen ? 0 : 1)};
 
-    & > img {
+    :focus {
+        transition: opacity 0.1s ease; /* hide button faster on guide open to prevent overlap */
+    }
+
+    > img {
         display: block;
     }
 `;
 
 export const GuideButton = () => {
-    const { openGuide } = useGuide();
+    const { openGuide, isGuideOpen } = useGuide();
 
     return (
         <FreeFocusInside>
-            <Wrapper data-test="@guide/button-open" onClick={openGuide}>
+            <Wrapper data-test="@guide/button-open" onClick={openGuide} $isGuideOpen={isGuideOpen}>
                 <img
                     src={resolveStaticPath('/images/suite/lightbulb.svg')}
                     width="18"
