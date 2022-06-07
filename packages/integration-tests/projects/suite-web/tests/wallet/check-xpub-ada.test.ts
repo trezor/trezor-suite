344bcbab7fafd6f15d7bb71ae89cdeacc441a82e
testing <bosomt2@gmail.com>
2022-06-07 13:28:57 +0200

    test(e2e): check-xpub-ADA

---
+++ packages/integration-tests/projects/suite-web/tests/wallet/check-xpub-ada.test.ts
@@ -0,0 +1,55 @@
+// @group:suite
+// @retry=2
+
+describe('Check Cardano XPUB', () => {
+    beforeEach(() => {
+        cy.task('startEmu', { wipe: true });
+        cy.task('setupEmu', {
+            needs_backup: false,
+            mnemonic: 'all all all all all all all all all all all all',
+        });
+        cy.task('startBridge');
+
+        cy.viewport(1080, 1440).resetDb();
+        cy.prefixedVisit('/settings/coins');
+        cy.passThroughInitialRun();
+    });
+
+    afterEach(() => {
+        cy.task('stopEmu');
+    });
+
+    /* Test case
+     * 1. Pass onboarding
+     * 2. Disable BTC coin to speedup discovery process
+     * 3. Enable Ada coin
+     * 4. Navigate to Accounts/ADA/Account/
+     * 5. Click on @wallets/details/show-xpub-button
+     * 6. Check that @xpub-modal/xpub-field is present
+     */
+    it('Check Cardano XPUB', () => {
+        //
+        // Test preparation
+        //
+        cy.getTestElement('@settings/menu/wallet').click();
+        cy.getTestElement('@settings/wallet/network/btc').click({ force: true });
+        cy.getTestElement('@settings/wallet/network/ada').click({ force: true });
+        //
+        // Test execution
+        //
+        cy.getTestElement('@suite/menu/wallet-index').click();
+        cy.discoveryShouldFinish();
+        cy.getTestElement('@account-menu/ada/normal/0/label').click();
+        cy.getTestElement('@wallet/menu/wallet-details').click();
+        cy.getTestElement('@wallets/details/show-xpub-button').click();
+        cy.getTestElement('@xpub-modal/xpub-field').should('exist');
+
+        //
+        // Assert
+        //
+        cy.getTestElement('@xpub-modal/xpub-field')
+            .should('be.visible')
+            .invoke('text')
+            .should('not.be.empty');
+    });
+});
