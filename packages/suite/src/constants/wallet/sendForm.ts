b0399db03a0488568761ef4c7b6635c9dd11703e
Szymon Lesisz <sz.lesisz@gmail.com>
2022-06-07 13:20:55 +0200

    chore(suite): remove redundant zcash enhancement
    if will be provided automatically by connect signTransaction

--- packages/suite/src/constants/wallet/sendForm.ts
+++ packages/suite/src/constants/wallet/sendForm.ts
@@ -8,13 +8,6 @@ export const U_INT_32 = 0xffffffff;
 export const ETH_DEFAULT_GAS_PRICE = '1';
 export const ETH_DEFAULT_GAS_LIMIT = '21000';
 
-export const ZEC_SIGN_ENHANCEMENT = {
-    overwintered: true,
-    version: 4,
-    versionGroupId: 0x892f2085,
-    branchId: 0xc2d6d0b4,
-};
-
 export const ERC20_TRANSFER = 'a9059cbb'; // 4 bytes function signature of solidity erc20 `transfer(address,uint256)`
 export const ERC20_GAS_LIMIT = '200000';
 
