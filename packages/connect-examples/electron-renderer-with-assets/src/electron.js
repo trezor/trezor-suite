311b07c8b2ab1c508a8a22756b716153ad83c92a
Tomáš Klíma <tomas.klim@gmail.com>
2022-06-08 14:10:21 +0200

    chore(connect-examples): update electron in electron-renderer-wth-assets

--- packages/connect-examples/electron-renderer-with-assets/src/electron.js
+++ packages/connect-examples/electron-renderer-with-assets/src/electron.js
@@ -47,7 +47,7 @@ app.on('ready', init);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On OS X it is common for applications and their menu bar
+    // On macOS it is common for applications and their menu bar
     // to stay active until the user quits explicitly with Cmd + Q
     if (process.platform !== 'darwin') {
         app.quit();
@@ -55,7 +55,7 @@ app.on('window-all-closed', () => {
 });
 
 app.on('activate', () => {
-    // On OS X it's common to re-create a window in the app when the
+    // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (mainWindow === null) {
         init();
