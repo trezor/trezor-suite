c899e663a0dcccd1ef830571bf201fb0cc81dbdb
mroz <mvarmuza@gmail.com>
2022-06-15 13:43:17 +0200

    test(suite-web): extend passphrase legacy test

--- packages/suite/src/views/dashboard/components/PortfolioCard/components/Header.tsx
+++ packages/suite/src/views/dashboard/components/PortfolioCard/components/Header.tsx
@@ -71,7 +71,11 @@ export const Header = (props: HeaderProps) => {
     if (!props.isWalletLoading && !props.isWalletError) {
         if (props.isWalletEmpty) {
             actions = (
-                <ActionButton variant="primary" onClick={props.receiveClickHandler}>
+                <ActionButton
+                    variant="primary"
+                    onClick={props.receiveClickHandler}
+                    data-test="@dashboard/receive-button"
+                >
                     <Translation id="TR_RECEIVE" />
                 </ActionButton>
             );
