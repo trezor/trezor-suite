3dad072606b56b2e6c3e2484846edf76c68e2a16
Jan Bouchner <jan.bouchner@gmail.com>
2022-06-16 18:13:30 +0200

    chore: Use io.trezor.suite instead of com.trezorsuite (iOS, Android) (#5631)

--- packages/suite-native/android/app/src/main/java/io/trezor/suite/MainActivity.java
+++ packages/suite-native/android/app/src/main/java/io/trezor/suite/MainActivity.java
@@ -1,4 +1,4 @@
-package com.trezorsuite;
+package io.trezor.suite;
 
 import android.os.Bundle;
 import com.facebook.react.ReactActivity;
--- packages/suite-native/android/app/src/main/java/io/trezor/suite/newarchitecture/components/MainComponentsRegistry.java
+++ packages/suite-native/android/app/src/main/java/io/trezor/suite/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.trezorsuite.newarchitecture.components;
+package io.trezor.suite.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
