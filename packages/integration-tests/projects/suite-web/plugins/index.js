2f6a1eb0020978f3933364ec66662d1716c59a91
stepan.janda <stepan.janda@satoshilabs.com>
2022-06-22 21:24:17 +0200

    custom-firmware

--- packages/integration-tests/projects/suite-web/plugins/index.js
+++ packages/integration-tests/projects/suite-web/plugins/index.js
@@ -302,5 +302,20 @@ module.exports = on => {
             await controller.disconnect();
             return null;
         },
+        csvToJson(data) {
+            const lines = data.split('\n');
+            const result = [];
+            const headers = lines[0].split(',');
+            for (let i = 1; i < lines.length; i++) {
+                const obj = {};
+                const currentline = lines[i].split(',');
+
+                for (let j = 0; j < headers.length; j++) {
+                    obj[headers[j]] = currentline[j];
+                }
+                result.push(obj);
+            }
+            return result;
+        },
     });
 };
---
+++ packages/integration-tests/projects/suite-web/tests/wallet/export-btc-csv.test.ts
@@ -0,0 +1,63 @@
+// @group:settings
+
+import { invoke } from 'cypress/types/lodash';
+
+// @retry=2
+describe('Import a BTC csv file', () => {
+    beforeEach(() => {
+        cy.task('startEmu', { wipe: true });
+        cy.task('setupEmu');
+        cy.task('startBridge');
+        cy.viewport(1080, 1440).resetDb();
+        cy.prefixedVisit('/');
+        cy.passThroughInitialRun();
+        cy.discoveryShouldFinish();
+    });
+    /** 1. click on `Accounts`
+    2. select a BTC account if it’s not selected
+    3. click on `Send`
+    4. click on `...` in the form header
+    5. click on `Import addresses from CSV`
+    6. add the test data csv file 
+  */
+
+    it('Go to BTC send form and import a csv', () => {
+        //
+        // Test preparation
+        //
+        cy.getTestElement('@suite/menu/wallet-index').click();
+        cy.getTestElement('@account-menu/btc/normal/0').click();
+        cy.getTestElement('@wallet/menu/wallet-send').click();
+
+        //
+        // Test execution
+        //
+        cy.getTestElement('@send/header-dropdown').click();
+        cy.contains('Import').click();
+        cy.getTestElement('@modal').then(fileUploadModal => {
+            cy.wrap(fileUploadModal)
+                .find('input[type=file]')
+                .attachFile('btctest.csv', { subjectType: 'drag-n-drop' });
+        });
+
+        //
+        // Assert
+        //
+        cy.readFile('fixtures/btctest.csv').then(data => {
+            cy.task('csvToJson', data).then(processedData => {
+                const firstAddress: string = processedData[0].address;
+                const secondAddress: string = processedData[1].address;
+
+                // process data
+                cy.getTestElement('outputs[0].address')
+                    .should('be.visible')
+                    .invoke('val')
+                    .should('equal', firstAddress);
+                cy.getTestElement('outputs[1].address')
+                    .should('be.visible')
+                    .invoke('val')
+                    .should('equal', secondAddress);
+            });
+        });
+    });
+});
