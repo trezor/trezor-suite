87ab29921ef5ebaa525e6e7c3766023da6107781
mroz <mvarmuza@gmail.com>
2022-06-22 11:45:17 +0200

    chore(connect): rmeove webusb dead code

--- packages/connect-web/src/webusb/button.ts
+++ packages/connect-web/src/webusb/button.ts
@@ -1,6 +1,6 @@
 // origin: https://github.com/trezor/connect/blob/develop/src/js/webusb/button.js
 
-const render = (className = '', url: string, origin = '') => {
+const render = (className = '', url: string) => {
     const query = className || '.trezor-webusb-button';
     const buttons = document.querySelectorAll(query);
     const src = `${url}?${Date.now()}`;
@@ -19,16 +19,6 @@ const render = (className = '', url: string, origin = '') => {
             // btnIframe.style.opacity = '0'; // this makes click impossible on cross-origin
             btnIframe.setAttribute('allow', 'usb');
             btnIframe.setAttribute('scrolling', 'no');
-            btnIframe.onload = () => {
-                btnIframe.contentWindow!.postMessage(
-                    {
-                        // style: JSON.stringify( window.getComputedStyle(b) ),
-                        // outer: b.outerHTML,
-                        // inner: b.innerHTML
-                    },
-                    origin,
-                );
-            };
             btnIframe.src = src;
 
             // inject iframe into button
