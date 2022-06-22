8f02c146e0b6381abf402c188b0087ee2aa118a7
testing <bosomt2@gmail.com>
2022-06-22 10:03:45 +0200

    test(e2e) toast notification rendering

---
+++ packages/integration-tests/projects/suite-web/tests/settings/notification.toast.test.ts
@@ -0,0 +1,42 @@
+// @group:suite
+// @retry=2
+
+describe('Check notification toast', () => {
+    beforeEach(() => {
+        cy.task('startBridge');
+        cy.task('startEmu', { version: Cypress.env('emuVersionT1'), wipe: true });
+        cy.task('setupEmu', { needs_backup: false });
+        cy.viewport(1080, 1440).resetDb();
+        cy.prefixedVisit('/settings/device');
+        cy.passThroughInitialRun();
+    });
+
+    afterEach(() => {
+        cy.task('stopEmu');
+    });
+
+    /* Test case
+     * 1. Pass onboarding.
+     * 2. Navigate to Settings/device.
+     * 3. Change passphrase settings.
+     * 4. Confirm on device.
+     * 5. Check that toast notification is rendered.
+     */
+    it('Check notification toast', () => {
+        //
+        // Test preparation
+        //
+        // Test execution
+        //
+        cy.log('turn on passphrase protection');
+        cy.getTestElement('@settings/device/passphrase-switch')
+            .click({ force: true })
+            .getConfirmActionOnDeviceModal();
+        cy.task('pressYes');
+        cy.getConfirmActionOnDeviceModal().should('not.exist');
+        //
+        // Assert
+        //
+        cy.getTestElement('@toast/settings-applied').should('be.visible');
+    });
+});
