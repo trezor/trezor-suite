43762af42298b590272110ad7872d7978f2d0d19
bouchja1 <jan.bouchner@gmail.com>
2022-06-29 21:50:47 +0200

    feat(atom): Fix gesture handler - working on Android as well

--- suite-native/atoms/src/Modal/BottomModalContainer.tsx
+++ suite-native/atoms/src/Modal/BottomModalContainer.tsx
@@ -1,8 +1,6 @@
 import React, { ReactNode } from 'react';
 import { Modal as RNModal } from 'react-native';
-import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
-
-import { Box } from '../Box';
+import { BottomModalGestureHandler } from './BottomModalGestureHandler';
 
 type ModalProps = {
     children: ReactNode;
@@ -10,18 +8,8 @@ type ModalProps = {
     onClose: () => void;
 };
 
-const modalWithOverlayStyle = prepareNativeStyle(utils => ({
-    flex: 1,
-    justifyContent: 'flex-end',
-    backgroundColor: utils.transparentize(0.3, utils.colors.black),
-}));
-
-export const BottomModalContainer = ({ children, isVisible, onClose }: ModalProps) => {
-    const { applyStyle } = useNativeStyles();
-
-    return (
-        <RNModal transparent visible={isVisible} onRequestClose={onClose} animationType="fade">
-            <Box style={applyStyle(modalWithOverlayStyle)}>{children}</Box>
-        </RNModal>
-    );
-};
+export const BottomModalContainer = ({ children, isVisible, onClose }: ModalProps) => (
+    <RNModal transparent visible={isVisible} onRequestClose={onClose} animationType="fade">
+        <BottomModalGestureHandler>{children}</BottomModalGestureHandler>
+    </RNModal>
+);
