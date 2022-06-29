a6b5f7630b432d461a88f40f47393056763e525b
stepan.janda <stepan.janda@satoshilabs.com>
2022-06-29 21:25:21 +0200

    test (e2e):look-up-account

---
+++ packages/integration-tests/projects/suite-web/tests/wallet/look-up-an-account.test.ts
@@ -0,0 +1,43 @@
+// @group:suite
+// @retry=2
+
+describe('Look up a BTC account', () => {
+    beforeEach(() => {
+        cy.task('startEmu', { wipe: true });
+        cy.task('setupEmu', {
+            needs_backup: false,
+            mnemonic: 'cancel solid bulb sample fury scrap whale ranch raven razor sight skin',
+        });
+        cy.task('startBridge');
+
+        cy.viewport(1080, 1440).resetDb();
+        cy.prefixedVisit('/settings/coins');
+        cy.passThroughInitialRun();
+    });
+
+    afterEach(() => {
+        cy.task('stopEmu');
+    });
+
+    /** Test case
+     * 1. navigate to the `Accounts`
+     * 2. in the left side bar search panel, type in `bitcoin`
+     */
+    it('Search for bitcoin in accounts', () => {
+        //
+        // Test preparation
+        //
+        cy.getTestElement('@settings/menu/wallet').click();
+        cy.getTestElement('@settings/wallet/network/ltc').click({ force: true });
+        //
+        // Test execution
+        //
+        cy.getTestElement('@suite/menu/wallet-index').click();
+        cy.discoveryShouldFinish();
+        cy.getTestElement('@account-menu/search-input').type('bitcoin');
+        //
+        // Assert
+        //
+        cy.getTestElement('@account-menu/ltc/normal/0').should('not.exist');
+    });
+});
