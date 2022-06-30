c3c455c6cc35ea5c23d40bb71319891c48f25e62
vdovhanych <dovhanych@me.com>
2022-06-30 20:00:42 +0200

    fix(ci): set longer wait time for tenv launch

--- docker/wait-for-env.sh
+++ docker/wait-for-env.sh
@@ -3,8 +3,8 @@
 echo "Waiting for trezor-user-env to load up..."
 counter=0
 max_attempts=60
-  # there is no official support for websockets in curl
-  # trezor-user-env websocket server will return HTTP/1.1 426 Upgrade Required error with "Upgrade: websocket" header
+# there is no official support for websockets in curl
+# trezor-user-env websocket server will return HTTP/1.1 426 Upgrade Required error with "Upgrade: websocket" header
 
 # this is a workaround to add curl that has been removed from cypress docker image since version 9.5.4
 # TODO: remove when issue https://github.com/cypress-io/cypress-docker-images/issues/642 is solved
@@ -14,7 +14,7 @@ until (curl -i -s -I http://localhost:9001 | grep 'websocket'); do
     echo "trezor-user-env is not running. exiting"
     exit 1
   fi
-  counter=$(($counter+1))
+  counter=$(($counter + 1))
   printf "."
   sleep 1
 done
