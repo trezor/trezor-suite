9200a792539560777386fb859c49f4cab7d1ef35
bouchja1 <jan.bouchner@gmail.com>
2022-07-04 00:41:52 +0200

    feat(suite-native): Fix trezor/connect RN network utils

---
+++ packages/connect/src/utils/assetUtils.ts
@@ -0,0 +1,19 @@
+/* eslint-disable global-require */
+
+export const getAssetByUrl = (url: string) => {
+    const fileUrl = url.split('?')[0];
+    switch (fileUrl) {
+        case './data/coins.json':
+            return require('@trezor/connect-common/files/coins.json');
+        case './data/bridge/releases.json':
+            return require('@trezor/connect-common/files/bridge/releases.json');
+        case './data/firmware/1/releases.json':
+            return require('@trezor/connect-common/files/firmware/1/releases.json');
+        case './data/firmware/2/releases.json':
+            return require('@trezor/connect-common/files/firmware/2/releases.json');
+        case './data/messages/messages.json':
+            return require('@trezor/transport/messages.json');
+        default:
+            return null;
+    }
+};
