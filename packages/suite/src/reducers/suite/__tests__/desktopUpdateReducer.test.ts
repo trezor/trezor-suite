e2f536bdad9b14bbdd115dfb66d8ddd57148467e
Marek Polak <marek.polak@satoshilabs.com>
2022-07-12 10:13:29 +0200

    test(suite): desktop reducer unit test

---
+++ packages/suite/src/reducers/suite/__tests__/desktopUpdateReducer.test.ts
@@ -0,0 +1,60 @@
+import desktopUpdateReducer, { State, UpdateState } from '../desktopUpdateReducer';
+import { DESKTOP_UPDATE, SUITE } from '@suite-actions/constants';
+import type { Action } from '@suite-types';
+
+const createUpdateInfo = (salt: string) => ({
+    releaseDate: `releaseDate-${salt}`,
+    version: `version-${salt}`,
+});
+
+const fixtures: [Action, Partial<State>][] = [
+    [
+        {
+            type: SUITE.DESKTOP_HANDSHAKE,
+            // @ts-ignore
+            payload: {
+                desktopUpdate: {
+                    allowPrerelease: true,
+                },
+            },
+        },
+        { enabled: true, allowPrerelease: true },
+    ],
+    [{ type: DESKTOP_UPDATE.ALLOW_PRERELEASE, payload: false }, { allowPrerelease: false }],
+    [{ type: DESKTOP_UPDATE.CHECKING }, { state: UpdateState.Checking }],
+    [
+        { type: DESKTOP_UPDATE.AVAILABLE, payload: createUpdateInfo('a') },
+        { state: UpdateState.Available, latest: createUpdateInfo('a') },
+    ],
+    [
+        { type: DESKTOP_UPDATE.NOT_AVAILABLE, payload: createUpdateInfo('b') },
+        { state: UpdateState.NotAvailable, latest: createUpdateInfo('b') },
+    ],
+    [{ type: DESKTOP_UPDATE.DOWNLOAD }, { state: UpdateState.Downloading }],
+    [{ type: DESKTOP_UPDATE.DOWNLOADING, payload: { percent: 42 } }, { progress: { percent: 42 } }],
+    [
+        { type: DESKTOP_UPDATE.READY, payload: createUpdateInfo('c') },
+        { state: UpdateState.Ready, latest: createUpdateInfo('c') },
+    ],
+    [
+        { type: DESKTOP_UPDATE.OPEN_EARLY_ACCESS_ENABLE },
+        { state: UpdateState.EarlyAccessEnable, window: 'maximized' },
+    ],
+    [{ type: DESKTOP_UPDATE.WINDOW, payload: 'minimized' }, { window: 'minimized' }],
+
+    [
+        { type: DESKTOP_UPDATE.OPEN_EARLY_ACCESS_DISABLE },
+        { state: UpdateState.EarlyAccessDisable, window: 'maximized' },
+    ],
+];
+
+describe('desktopUpdateReducer', () => {
+    it('DESKTOP_UPDATE actions', () => {
+        let lastState: State | undefined;
+        fixtures.forEach(([action, state]) => {
+            // @ts-ignore
+            lastState = desktopUpdateReducer(lastState, action);
+            expect(lastState).toMatchObject(state);
+        });
+    });
+});
