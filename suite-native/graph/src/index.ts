c355f61dd9a6f6102e2ae59c5f083035c0b4de73
juriczech <bohdan.juricek@gmail.com>
2022-07-25 15:22:28 +0200

    chore(suite-native): Add @dahaca to @suite-native codeowners

---
+++ suite-native/graph/src/index.ts
@@ -0,0 +1,2 @@
+export * from './components/Graph';
+export * from './appGraphReducer';
--- suite-native/state/src/store.ts
+++ suite-native/state/src/store.ts
@@ -1,7 +1,7 @@
 import { configureStore, Store } from '@reduxjs/toolkit';
 
 import { appSettingsReducer } from '@suite-native/module-settings';
-import { appGraphReducer } from '@suite-native/home-graph';
+import { appGraphReducer } from '@suite-native/graph';
 
 export const store: Store = configureStore({
     reducer: {
