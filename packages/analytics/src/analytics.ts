e922b4621866892b64be0e560240267edbdfd450
Tomáš Klíma <tomas.klim@gmail.com>
2022-07-25 18:07:30 +0200

    fix(analytics): retry analytics report if the previous one fails

--- packages/analytics/src/analytics.ts
+++ packages/analytics/src/analytics.ts
@@ -1,4 +1,5 @@
-import { encodeDataToQueryString, getRandomId, getUrl } from './utils';
+import { encodeDataToQueryString, getRandomId, getUrl, reportEvent } from './utils';
+
 import type { InitOptions, Event, App } from './types';
 
 export class Analytics<T extends Event> {
@@ -61,12 +62,16 @@ export class Analytics<T extends Event> {
             data,
         );
 
-        try {
-            fetch(`${this.url}?${qs}`, {
+        // try to report analytics event once again if the previous one fails
+        // easy solution which should solve missing events on the launch of the app
+        // if it does not help, then queue or more sophisticated solution should be implemented
+        reportEvent({
+            type: data.type,
+            url: `${this.url}?${qs}`,
+            options: {
                 method: 'GET',
-            });
-        } catch (err) {
-            console.error('Failed to log analytics', err);
-        }
+            },
+            retry: true,
+        });
     };
 }
