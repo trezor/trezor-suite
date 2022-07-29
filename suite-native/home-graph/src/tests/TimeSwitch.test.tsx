a4b07ea80c1e5d93fbc9cd4e694089fbcda25ca2
juriczech <bohdan.juricek@gmail.com>
2022-07-29 08:59:12 +0200

    fix(suite-native): Circular deps

--- suite-native/home-graph/src/tests/TimeSwitch.test.tsx
+++ suite-native/home-graph/src/tests/TimeSwitch.test.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
-import { store } from '@suite-native/state';
 import { fireEvent, render } from '@suite-native/test-utils';
 
+import { graphStore } from '../slice';
 import { TimeSwitch, timeSwitchItems } from '../components/TimeSwitch';
 
 test('time switch correctly changes values in redux from component', () => {
-    const { getByTestId } = render(<TimeSwitch />);
+    const { getByTestId } = render(
+        <Provider store={graphStore}>
+            <TimeSwitch />
+        </Provider>,
+    );
 
     const all = getByTestId(timeSwitchItems.all.value);
     fireEvent.press(all);
 
-    const { selectedTimeFrame } = store.getState().appGraph;
+    const { selectedTimeFrame } = graphStore.getState().appGraph;
     expect(selectedTimeFrame).toBe(timeSwitchItems.all.value);
 });
--- suite-native/test-utils/src/Provider.tsx
+++ suite-native/test-utils/src/Provider.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Provider as ReduxProvider } from 'react-redux';
 
 import { createRenderer, StylesProvider } from '@trezor/styles';
 import { prepareNativeTheme } from '@trezor/theme';
-import { store } from '@suite-native/state';
 
 type ProviderProps = {
     children: React.ReactNode;
@@ -12,9 +10,7 @@ const renderer = createRenderer();
 const theme = prepareNativeTheme({ colorVariant: 'chill' });
 
 export const Provider = ({ children }: ProviderProps) => (
-    <ReduxProvider store={store}>
-        <StylesProvider theme={theme} renderer={renderer}>
-            {children}
-        </StylesProvider>
-    </ReduxProvider>
+    <StylesProvider theme={theme} renderer={renderer}>
+        {children}
+    </StylesProvider>
 );
