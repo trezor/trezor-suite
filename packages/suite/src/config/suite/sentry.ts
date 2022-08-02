c206c732b56b90bb0517d2fa6e48538d633ccd73
Matej Kriz <matej@matejkriz.cz>
2022-08-02 16:01:59 +0200

    feat(sentry): stop reporting some errors that are not relevant to save internet traffic

--- packages/suite/src/config/suite/sentry.ts
+++ packages/suite/src/config/suite/sentry.ts
@@ -4,6 +4,17 @@ import { beforeSend, beforeBreadcrumb } from '@suite-utils/sentry';
 
 import type { Options } from '@sentry/types';
 
+const ignoreErrors = [
+    'ERR_INTERNET_DISCONNECTED',
+    'ERR_NETWORK_IO_SUSPENDED',
+    'ERR_NETWORK_CHANGED',
+    'Error: HTTP Error',
+    'other call in progress',
+    'Action cancelled by user',
+    'ResizeObserver loop limit exceeded',
+    'device disconnected during action',
+];
+
 const config: Options = {
     dsn: 'https://6d91ca6e6a5d4de7b47989455858b5f6@o117836.ingest.sentry.io/5193825',
     autoSessionTracking: false, // do not send analytical data to Sentry
@@ -20,6 +31,7 @@ const config: Options = {
     normalizeDepth: 4,
     maxBreadcrumbs: 40,
     beforeBreadcrumb,
+    ignoreErrors,
     initialScope: {
         tags: {
             version: process.env.VERSION || 'undefined',
