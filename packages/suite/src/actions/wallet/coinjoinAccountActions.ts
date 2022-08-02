changliang0931/jubiter-suite

feat(suite): create Coinjoin account using non-standard discovery process

packages/suite/src/actions/wallet/coinjoinAccountActions.ts
@@ -1,0 +1,167 @@
+import TrezorConnect from '@trezor/connect';
+import * as COINJOIN from './constants/coinjoinConstants';
+import { goto } from '../suite/routerActions';
+import { addToast } from '../suite/notificationActions';
+import { add as addTransaction } from './transactionActions';
+import {
+    create as createAccount,
+    update as updateAccountInfo,
+    updateAccount,
+} from './accountActions';
+import { CoinjoinBackend } from '@suite/services/coinjoin/coinjoinBackend';
+import type { Dispatch, GetState } from '@suite-types';
+import type { Account, Network } from '@wallet-types';
+
+export type CoinjoinAccountAction = {
+    type: typeof COINJOIN.ACCOUNT_CREATE;
+    payload: Account;
+};
+
+export const fetchAndUpdateAccount =
+    (account: Account) => async (dispatch: Dispatch, getState: GetState) => {
+        if (account.backendType !== 'coinjoin') return;
+
+        const lastKnownState = {
+            time: Date.now(),
+            blockHash: account.lastKnownState?.blockHash || '',
+            progress: 0,
+        };
+
+        try {
+            const api = CoinjoinBackend.getInstance(account.symbol);
+            const accountInfo = await api.getAccountInfo({
+                descriptor: account.descriptor,
+                lastKnownState: {
+                    balance: account.balance,
+                    blockHash: lastKnownState.blockHash,
+                },
+                onProgress: (progressState: any) => {
+                    dispatch(
+                        updateAccount({
+                            ...account,
+                            lastKnownState: {
+                                ...progressState,
+                                time: Date.now(),
+                            },
+                        }),
+                    );
+                },
+                symbol: account.symbol,
+            });
+
+            if (accountInfo) {
+                // get fresh info from reducer
+                const updatedAccount = getState().wallet.accounts.find(a => a.key === account.key);
+                if (updatedAccount && updatedAccount.lastKnownState) {
+                    // finalize
+                    dispatch(
+                        updateAccountInfo(
+                            {
+                                ...updatedAccount,
+                                lastKnownState: {
+                                    time: Date.now(),
+                                    blockHash: updatedAccount.lastKnownState.blockHash,
+                                },
+                            },
+                            accountInfo,
+                        ),
+                    );
+                    // add account transactions
+                    if (accountInfo.history.transactions) {
+                        dispatch(addTransaction(accountInfo.history.transactions, account));
+                    }
+                }
+            } else {
+                // TODO: no accountInfo
+            }
+        } catch (error) {
+            // TODO
+            console.warn('fetchAndUpdateAccount', error);
+        }
+    };
+
+export const createCoinjoinAccount =
+    (network: Network) => async (dispatch: Dispatch, getState: GetState) => {
+        if (network.accountType !== 'coinjoin') return;
+        const { device } = getState().suite;
+
+        // TODO: Disable safety_checks until Trezor FW will implement slip-0025
+        // const safety =
+        //     device?.features?.safety_checks !== 'PromptTemporarily'
+        //         ? 'PromptTemporarily'
+        //         : undefined;
+        const experimentalFeatures = await TrezorConnect.applySettings({
+            device,
+            experimental_features: true,
+            // safety_checks: safety,
+        });
+        if (!experimentalFeatures.success) {
+            dispatch(addToast({ type: 'error', error: 'Experimental features not enabled' }));
+            return;
+        }
+
+        // temporary hardcoded until Trezor FW will implement slip-0025
+        const PATH = `m/86'/1'/25'`; // `m/10025'/1'/0'`  network.bip43Path
+
+        // get coinjoin account xpub
+        const publicKey = await TrezorConnect.getPublicKey({
+            device,
+            useEmptyPassphrase: device?.useEmptyPassphrase,
+            path: PATH,
+            coin: network.symbol,
+        });
+        if (!publicKey.success) {
+            dispatch(addToast({ type: 'error', error: 'Public key not given' }));
+            return;
+        }
+
+        // create empty account
+        const account = dispatch(
+            createAccount(
+                device!.state!,
+                {
+                    index: 0,
+                    path: PATH,
+                    accountType: network.accountType,
+                    networkType: network.networkType,
+                    backendType: 'coinjoin',
+                    coin: network.symbol,
+                    derivationType: 0,
+                    lastKnownState: {
+                        time: 0,
+                        blockHash: '',
+                        progress: 0,
+                    },
+                },
+                {
+                    addresses: { change: [], used: [], unused: [] },
+                    availableBalance: '0',
+                    balance: '0',
+                    descriptor: publicKey.payload.xpubSegwit || publicKey.payload.xpub,
+                    empty: true,
+                    history: { total: 0, unconfirmed: 0 },
+                    legacyXpub: publicKey.payload.xpub,
+                    page: { index: 1, size: 25, total: 1 },
+                    utxo: [],
+                },
+            ),
+        );
+        dispatch({
+            type: COINJOIN.ACCOUNT_CREATE,
+            payload: account.payload,
+        });
+
+        // switch to account
+        dispatch(
+            goto('wallet-index', {
+                params: {
+                    symbol: network.symbol,
+                    accountType: network.accountType,
+                    accountIndex: 0,
+                },
+            }),
+        );
+
+        // start discovery
+        dispatch(fetchAndUpdateAccount(account.payload));
+    };

packages/suite/src/middlewares/wallet/coinjoinMiddleware.ts
@@ -1,0 +1,47 @@
+import type { MiddlewareAPI } from 'redux';
+import { ROUTER } from '@suite-actions/constants';
+import { BLOCKCHAIN, DISCOVERY } from '@wallet-actions/constants';
+import * as coinjoinAccountActions from '@wallet-actions/coinjoinAccountActions';
+import { CoinjoinBackend } from '@suite/services/coinjoin/coinjoinBackend';
+import type { AppState, Action, Dispatch } from '@suite-types';
+
+export const coinjoinMiddleware =
+    (api: MiddlewareAPI<Dispatch, AppState>) =>
+    (next: Dispatch) =>
+    (action: Action): Action => {
+        // cancel discovery for each CoinjoinBackend
+        if (action.type === ROUTER.LOCATION_CHANGE && action.payload.app !== 'wallet') {
+            CoinjoinBackend.getInstances().forEach(b => b.cancel());
+        }
+
+        // propagate action to reducers
+        next(action);
+
+        if (action.type === DISCOVERY.START) {
+            // find all coinjoin accounts
+            const coinjoinAccounts = api
+                .getState()
+                .wallet.accounts.filter(a => a.accountType === 'coinjoin');
+            if (coinjoinAccounts.length > 0) {
+                coinjoinAccounts.forEach(a =>
+                    api.dispatch(coinjoinAccountActions.fetchAndUpdateAccount(a)),
+                );
+            }
+        }
+
+        if (action.type === BLOCKCHAIN.SYNCED) {
+            // find all coinjoin accounts for network
+            const coinjoinAccounts = api
+                .getState()
+                .wallet.accounts.filter(
+                    a => a.accountType === 'coinjoin' && a.symbol === action.payload.symbol,
+                );
+            if (coinjoinAccounts.length > 0) {
+                coinjoinAccounts.forEach(a =>
+                    api.dispatch(coinjoinAccountActions.fetchAndUpdateAccount(a)),
+                );
+            }
+        }
+
+        return action;
+    };

packages/suite/src/services/coinjoin/coinjoinBackend.ts
@@ -1,0 +1,97 @@
+import TrezorConnect from '@trezor/connect';
+
+// NOTE: function below will be replaced by @trezor/coinjoin implementation
+type GetAccounInfo = {
+    descriptor: string;
+    lastKnownState: {
+        balance: string;
+        blockHash: string;
+    };
+    symbol: string;
+    onProgress: (state: any) => void;
+};
+
+const getCoinjoinAccountInfo = async (
+    { descriptor, symbol, onProgress, lastKnownState }: GetAccounInfo,
+    abortSignal: AbortSignal,
+) => {
+    const accountInfo = await TrezorConnect.getAccountInfo({
+        descriptor,
+        coin: symbol,
+        details: 'txs',
+    });
+
+    if (!accountInfo.success) {
+        console.log('No accountInfo');
+        return;
+    }
+
+    if (lastKnownState.blockHash === '11') {
+        return accountInfo.payload;
+    }
+
+    return new Promise<typeof accountInfo.payload>(resolve => {
+        // Temporary simulate account discovery by block filters
+        let i = !lastKnownState.blockHash ? 0 : 70;
+        let timeout: ReturnType<typeof setTimeout>;
+        const tick = () => {
+            i += 10;
+            if (i < 100) {
+                onProgress({
+                    blockHash: `0${i}`,
+                    progress: i,
+                    progressMessage:
+                        i > 60 ? 'Checking transaction history' : 'Loading block filters',
+                });
+                timeout = setTimeout(tick, 1000);
+            } else {
+                // Finish loading
+                onProgress({
+                    blockHash: '11',
+                });
+                resolve(accountInfo.payload);
+            }
+        };
+        tick();
+
+        abortSignal.addEventListener('abort', () => {
+            console.warn('ABORTED!');
+            clearTimeout(timeout);
+        });
+    });
+};
+
+export class CoinjoinBackend {
+    private static instances: Record<string, CoinjoinBackend> = {};
+    settings: Readonly<{
+        network: string;
+    }>;
+    private abortController: AbortController | undefined;
+
+    constructor(network: string) {
+        this.settings = Object.freeze({
+            network,
+        });
+    }
+
+    getAccountInfo(params: GetAccounInfo) {
+        this.abortController = new AbortController();
+        return getCoinjoinAccountInfo(params, this.abortController.signal);
+    }
+
+    cancel() {
+        this.abortController?.abort();
+        this.abortController = undefined;
+    }
+
+    static getInstance(network: string) {
+        if (!this.instances[network]) {
+            this.instances[network] = new CoinjoinBackend(network);
+        }
+        return this.instances[network];
+    }
+
+    static getInstances() {
+        return Object.keys(this.instances).map(key => this.instances[key]);
+    }
+}

trezor/trezor-suite

packages/suite/src/actions/wallet/coinjoinAccountActions.ts
@@ -1,0 +1,167 @@
+import TrezorConnect from '@trezor/connect';
+import * as COINJOIN from './constants/coinjoinConstants';
+import { goto } from '../suite/routerActions';
+import { addToast } from '../suite/notificationActions';
+import { add as addTransaction } from './transactionActions';
+import {
+    create as createAccount,
+    update as updateAccountInfo,
+    updateAccount,
+} from './accountActions';
+import { CoinjoinBackendService } from '@suite/services/coinjoin/coinjoinBackend';
+import type { Dispatch, GetState } from '@suite-types';
+import type { Account, Network } from '@wallet-types';
+
+export type CoinjoinAccountAction = {
+    type: typeof COINJOIN.ACCOUNT_CREATE;
+    payload: Account;
+};
+
+export const fetchAndUpdateAccount =
+    (account: Account) => async (dispatch: Dispatch, getState: GetState) => {
+        if (account.backendType !== 'coinjoin') return;
+
+        const lastKnownState = {
+            time: Date.now(),
+            blockHash: account.lastKnownState?.blockHash || '',
+            progress: 0,
+        };
+
+        try {
+            const api = CoinjoinBackendService.getInstance(account.symbol);
+            const accountInfo = await api.getAccountInfo({
+                descriptor: account.descriptor,
+                lastKnownState: {
+                    balance: account.balance,
+                    blockHash: lastKnownState.blockHash,
+                },
+                onProgress: (progressState: any) => {
+                    dispatch(
+                        updateAccount({
+                            ...account,
+                            lastKnownState: {
+                                ...progressState,
+                                time: Date.now(),
+                            },
+                        }),
+                    );
+                },
+                symbol: account.symbol,
+            });
+
+            if (accountInfo) {
+                // get fresh info from reducer
+                const updatedAccount = getState().wallet.accounts.find(a => a.key === account.key);
+                if (updatedAccount && updatedAccount.lastKnownState) {
+                    // finalize
+                    dispatch(
+                        updateAccountInfo(
+                            {
+                                ...updatedAccount,
+                                lastKnownState: {
+                                    time: Date.now(),
+                                    blockHash: updatedAccount.lastKnownState.blockHash,
+                                },
+                            },
+                            accountInfo,
+                        ),
+                    );
+                    // add account transactions
+                    if (accountInfo.history.transactions) {
+                        dispatch(addTransaction(accountInfo.history.transactions, account));
+                    }
+                }
+            } else {
+                // TODO: no accountInfo
+            }
+        } catch (error) {
+            // TODO
+            console.warn('fetchAndUpdateAccount', error);
+        }
+    };
+
+export const createCoinjoinAccount =
+    (network: Network) => async (dispatch: Dispatch, getState: GetState) => {
+        if (network.accountType !== 'coinjoin') return;
+        const { device } = getState().suite;
+
+        // TODO: Disable safety_checks until Trezor FW will implement slip-0025
+        // const safety =
+        //     device?.features?.safety_checks !== 'PromptTemporarily'
+        //         ? 'PromptTemporarily'
+        //         : undefined;
+        const experimentalFeatures = await TrezorConnect.applySettings({
+            device,
+            experimental_features: true,
+            // safety_checks: safety,
+        });
+        if (!experimentalFeatures.success) {
+            dispatch(addToast({ type: 'error', error: 'Experimental features not enabled' }));
+            return;
+        }
+
+        // temporary hardcoded until Trezor FW will implement slip-0025
+        const PATH = `m/86'/1'/25'`; // `m/10025'/1'/0'`  network.bip43Path
+
+        // get coinjoin account xpub
+        const publicKey = await TrezorConnect.getPublicKey({
+            device,
+            useEmptyPassphrase: device?.useEmptyPassphrase,
+            path: PATH,
+            coin: network.symbol,
+        });
+        if (!publicKey.success) {
+            dispatch(addToast({ type: 'error', error: 'Public key not given' }));
+            return;
+        }
+
+        // create empty account
+        const account = dispatch(
+            createAccount(
+                device!.state!,
+                {
+                    index: 0,
+                    path: PATH,
+                    accountType: network.accountType,
+                    networkType: network.networkType,
+                    backendType: 'coinjoin',
+                    coin: network.symbol,
+                    derivationType: 0,
+                    lastKnownState: {
+                        time: 0,
+                        blockHash: '',
+                        progress: 0,
+                    },
+                },
+                {
+                    addresses: { change: [], used: [], unused: [] },
+                    availableBalance: '0',
+                    balance: '0',
+                    descriptor: publicKey.payload.xpubSegwit || publicKey.payload.xpub,
+                    empty: true,
+                    history: { total: 0, unconfirmed: 0 },
+                    legacyXpub: publicKey.payload.xpub,
+                    page: { index: 1, size: 25, total: 1 },
+                    utxo: [],
+                },
+            ),
+        );
+        dispatch({
+            type: COINJOIN.ACCOUNT_CREATE,
+            payload: account.payload,
+        });
+
+        // switch to account
+        dispatch(
+            goto('wallet-index', {
+                params: {
+                    symbol: network.symbol,
+                    accountType: network.accountType,
+                    accountIndex: 0,
+                },
+            }),
+        );
+
+        // start discovery
+        dispatch(fetchAndUpdateAccount(account.payload));
+    };

packages/suite/src/middlewares/wallet/coinjoinMiddleware.ts
@@ -1,0 +1,47 @@
+import type { MiddlewareAPI } from 'redux';
+import { ROUTER } from '@suite-actions/constants';
+import { BLOCKCHAIN, DISCOVERY } from '@wallet-actions/constants';
+import * as coinjoinAccountActions from '@wallet-actions/coinjoinAccountActions';
+import { CoinjoinBackendService } from '@suite/services/coinjoin/coinjoinBackend';
+import type { AppState, Action, Dispatch } from '@suite-types';
+
+export const coinjoinMiddleware =
+    (api: MiddlewareAPI<Dispatch, AppState>) =>
+    (next: Dispatch) =>
+    (action: Action): Action => {
+        // cancel discovery for each CoinjoinBackend
+        if (action.type === ROUTER.LOCATION_CHANGE && action.payload.app !== 'wallet') {
+            CoinjoinBackendService.getInstances().forEach(b => b.cancel());
+        }
+
+        // propagate action to reducers
+        next(action);
+
+        if (action.type === DISCOVERY.START) {
+            // find all coinjoin accounts
+            const coinjoinAccounts = api
+                .getState()
+                .wallet.accounts.filter(a => a.accountType === 'coinjoin');
+            if (coinjoinAccounts.length > 0) {
+                coinjoinAccounts.forEach(a =>
+                    api.dispatch(coinjoinAccountActions.fetchAndUpdateAccount(a)),
+                );
+            }
+        }
+
+        if (action.type === BLOCKCHAIN.SYNCED) {
+            // find all coinjoin accounts for network
+            const coinjoinAccounts = api
+                .getState()
+                .wallet.accounts.filter(
+                    a => a.accountType === 'coinjoin' && a.symbol === action.payload.symbol,
+                );
+            if (coinjoinAccounts.length > 0) {
+                coinjoinAccounts.forEach(a =>
+                    api.dispatch(coinjoinAccountActions.fetchAndUpdateAccount(a)),
+                );
+            }
+        }
+
+        return action;
+    };

packages/suite/src/services/coinjoin/coinjoinBackend.ts
@@ -1,0 +1,97 @@
+import TrezorConnect from '@trezor/connect';
+
+// NOTE: function below will be replaced by @trezor/coinjoin implementation
+type GetAccountInfoParams = {
+    descriptor: string;
+    lastKnownState?: {
+        balance: string;
+        blockHash: string;
+    };
+    symbol: string;
+    onProgress: (state: any) => void;
+};
+
+const getCoinjoinAccountInfo = async (
+    { descriptor, symbol, onProgress, lastKnownState }: GetAccountInfoParams,
+    abortSignal: AbortSignal,
+) => {
+    const accountInfo = await TrezorConnect.getAccountInfo({
+        descriptor,
+        coin: symbol,
+        details: 'txs',
+    });
+
+    if (!accountInfo.success) {
+        console.log('No accountInfo');
+        return;
+    }
+
+    if (lastKnownState?.blockHash === '11') {
+        return accountInfo.payload;
+    }
+
+    return new Promise<typeof accountInfo.payload>(resolve => {
+        // Temporary simulate account discovery by block filters
+        let i = !lastKnownState?.blockHash ? 0 : 70;
+        let timeout: ReturnType<typeof setTimeout>;
+        const tick = () => {
+            i += 10;
+            if (i < 100) {
+                onProgress({
+                    blockHash: `0${i}`,
+                    progress: i,
+                    progressMessage:
+                        i > 60 ? 'Checking transaction history' : 'Loading block filters',
+                });
+                timeout = setTimeout(tick, 1000);
+            } else {
+                // Finish loading
+                onProgress({
+                    blockHash: '11',
+                });
+                resolve(accountInfo.payload);
+            }
+        };
+        tick();
+
+        abortSignal.addEventListener('abort', () => {
+            console.warn('ABORTED!');
+            clearTimeout(timeout);
+        });
+    });
+};
+
+export class CoinjoinBackendService {
+    private static instances: Record<string, CoinjoinBackendService> = {};
+    settings: Readonly<{
+        network: string;
+    }>;
+    private abortController: AbortController | undefined;
+
+    constructor(network: string) {
+        this.settings = Object.freeze({
+            network,
+        });
+    }
+
+    getAccountInfo(params: GetAccountInfoParams) {
+        this.abortController = new AbortController();
+        return getCoinjoinAccountInfo(params, this.abortController.signal);
+    }
+
+    cancel() {
+        this.abortController?.abort();
+        this.abortController = undefined;
+    }
+
+    static getInstance(network: string) {
+        if (!this.instances[network]) {
+            this.instances[network] = new CoinjoinBackendService(network);
+        }
+        return this.instances[network];
+    }
+
+    static getInstances() {
+        return Object.keys(this.instances).map(key => this.instances[key]);
+    }
+}