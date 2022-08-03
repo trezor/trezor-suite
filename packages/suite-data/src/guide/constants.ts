cacec68dcc95e296b73f0b896e429744194e886e
alex-jerechinsky <75733878+alex-jerechinsky@users.noreply.github.com>
2022-08-03 15:21:45 +0200

    feat(guide): update articles

--- packages/suite-data/src/guide/constants.ts
+++ packages/suite-data/src/guide/constants.ts
@@ -1,7 +1,7 @@
 import { resolve, join } from 'path';
 
 export const GITBOOK_SOURCE = 'https://github.com/trezor/trezor-suite-guide.git';
-export const GITBOOK_REVISION = '4ea34d3bb07036a747baa329065eba1a519c7698';
+export const GITBOOK_REVISION = '9d15bee695a446f729d6aeb0b3aa34ad7bb6c0e6';
 export const TMP = join(resolve(__dirname, '../..'), 'tmp', 'guide');
 // Path to the GitBook assets. Relative to TMP.
 export const GITBOOK_ASSETS_DIR_PREFIX = '.gitbook';
