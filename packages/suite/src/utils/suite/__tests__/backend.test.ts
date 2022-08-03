changliang0931/jubiter-suite

feat(suite): exclude non-standard backendTypes from various accounts actions
accounts with non-standard `backendType` are not using `TrezorConnect` api:
- are not automaticaly discovered
- are not subscribed/unsubscribed
- are not auto updated
- are not fetching account graph data

packages/suite/src/utils/suite/__tests__/backend.test.ts
@@ -17,4 +17,15 @@ describe('backend utils', () => {
         expect(utils.isElectrumUrl('https://google.com')).toBe(false);
         expect(utils.isElectrumUrl('')).toBe(false);
     });
+
+    test('isStandardBackendType', () => {
+        const { isStandardBackendType } = utils;
+        expect(isStandardBackendType()).toBe(true);
+        expect(isStandardBackendType('blockbook')).toBe(true);
+        expect(isStandardBackendType('coinjoin')).toBe(false);
+        // @ts-expect-error
+        expect(isStandardBackendType('gibberish')).toBe(false);
+        // @ts-expect-error
+        expect(isStandardBackendType({})).toBe(false);
+    });
 });

trezor/trezor-suite

packages/suite/src/utils/suite/__tests__/backend.test.ts
@@ -17,4 +17,15 @@ describe('backend utils', () => {
         expect(utils.isElectrumUrl('https://google.com')).toBe(false);
         expect(utils.isElectrumUrl('')).toBe(false);
     });
+
+    test('isTrezorConnectBackendType', () => {
+        const { isTrezorConnectBackendType } = utils;
+        expect(isTrezorConnectBackendType()).toBe(true);
+        expect(isTrezorConnectBackendType('blockbook')).toBe(true);
+        expect(isTrezorConnectBackendType('coinjoin')).toBe(false);
+        // @ts-expect-error
+        expect(isTrezorConnectBackendType('gibberish')).toBe(false);
+        // @ts-expect-error
+        expect(isTrezorConnectBackendType({})).toBe(false);
+    });
 });