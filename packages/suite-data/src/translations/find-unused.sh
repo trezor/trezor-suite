18326c95d404d45fb8a420f84887222dbdd47f25
mroz <mvarmuza@gmail.com>
2022-08-08 07:59:51 +0200

    ci: fix translation unused script

--- packages/suite-data/src/translations/find-unused.sh
+++
@@ -1,9 +0,0 @@
-find . $1 \
- -not -path "**/node_modules/*" \
- -not -path "**/build/*" \
- -not -path "**/lib/*" \
- -not -path "**/libDev/*" \
- \( -name "*.ts" -or -name "*.tsx" \) \
- -not -name "messages.ts" \
- -not -name "*.test.*" \
- -exec grep -nH --color=auto $2 {} ';' -quit;
