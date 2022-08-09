8e03186dc0f8cfa0503e22b81b54d8a72d9d7a79
mroz <mvarmuza@gmail.com>
2022-08-09 13:28:40 +0200

    wip

--- packages/integration-tests/projects/suite-desktop/playwright.config.ts
+++ packages/integration-tests/projects/suite-desktop/playwright.config.ts
@@ -3,5 +3,12 @@ import { PlaywrightTestConfig } from '@playwright/test';
 const config: PlaywrightTestConfig = {
     testDir: 'tests',
     timeout: 60000,
+    retries: 0,
+    workers: 1, // to disable parallelism between test files
+    use: {
+        headless: process.env.HEADLESS === 'true',
+        ignoreHTTPSErrors: true,
+        trace: 'retain-on-failure',
+    },
 };
 export default config;
