a15e781db3a53bd5b3cc2ecce4ce49a7be10befb
mroz <mvarmuza@gmail.com>
2022-08-11 15:48:00 +0200

    wait

--- packages/integration-tests/projects/suite-desktop/playwright.config.ts
+++ packages/integration-tests/projects/suite-desktop/playwright.config.ts
@@ -2,7 +2,7 @@ import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
     testDir: 'tests',
-    timeout: 60000,
+    timeout: 60000 * 5,
     retries: 0,
     workers: 1, // to disable parallelism between test files
     use: {
