626362f175e523828207d4f7806de570b698b2a5
mroz <mvarmuza@gmail.com>
2022-08-15 15:19:42 +0200

    ci: fix connect-bump script

--- packages/connect/scripts/bump-version.js
+++ packages/connect/scripts/bump-version.js
@@ -1,7 +1,7 @@
 const child_process = require('child_process');
 const path = require('path');
 
-const PACKAGE_PATH = path.join(__dirname, '..');
+const REPO_ROOT = path.join(__dirname, '..', '..', '..');
 
 const args = process.argv.slice(2);
 
@@ -27,27 +27,35 @@ const cmd = [
     semver,
 
     // files with version
-    './package.json',
-    'README.md',
-    './src/data/version.ts',
-    '../connect-web/package.json',
-    '../connect-web/src/webextension/trezor-usb-permissions.js',
-    '../connect-popup/package.json',
-    '../connect-iframe/package.json',
-    '../connect-explorer/package.json',
-    '../suite-common/connect-init/package.json',
-    '../suite-common/wallet-utils/package.json',
-    '../suite-common/wallet-types/package.json',
-    '../suite-common/test-utils/package.json',
-    '../suite-common/suite-types/package.json',
-    '../suite-common/suite-desktop/package.json',
-    '../suite-common/suite/package.json',
-    '../suite-native/app/package.json',
+    './packages/connect/package.json',
+    './packages/connect/README.md',
+    './packages/connect/src/data/version.ts',
+    './packages/connect-web/package.json',
+    './packages/connect-web/src/webextension/trezor-usb-permissions.js',
+    './packages/connect-popup/package.json',
+    './packages/suite-desktop/package.json',
+    './packages/suite/package.json',
+    './packages/connect-iframe/package.json',
+    './packages/connect-explorer/package.json',
+
+    './suite-common/connect-init/package.json',
+    './suite-common/wallet-utils/package.json',
+    './suite-common/wallet-types/package.json',
+    './suite-common/test-utils/package.json',
+    './suite-common/suite-types/package.json',
+    './suite-native/app/package.json',
 
     // todo: add examples package.jsons
 ];
 
 const res = child_process.spawnSync('yarn', cmd, {
     encoding: 'utf-8',
-    cwd: PACKAGE_PATH,
-}).stdout;
+    cwd: REPO_ROOT,
+});
+
+if (res.stderr) {
+    console.log(res.stderr);
+    process.exit(1);
+} else {
+    console.log(res.stdout);
+}
