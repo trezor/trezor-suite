359aa61a495bbe461b8c9e700665804065c0cb54
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2022-08-16 09:49:07 +0200

    feat: intercept requests from nodejs main process

--- packages/request-manager/src/types.ts
+++ packages/request-manager/src/types.ts
@@ -9,3 +9,8 @@ export type BootstrapEvent = {
     progress: string | undefined;
     summary: string | undefined;
 };
+
+export interface InterceptedEvent {
+    method: string;
+    details: string;
+}
