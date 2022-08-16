7828b61539d3e7e4f799f803dc8cdd8e57713327
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2022-08-16 14:32:07 +0200

    feat: tor identities manager

--- packages/request-manager/src/interceptor.ts
+++ packages/request-manager/src/interceptor.ts
@@ -3,28 +3,45 @@ import http from 'http';
 import https from 'https';
 import tls from 'tls';
 import { InterceptedEvent } from './types';
+import { TorIdentities } from './torIdentities';
 
-type Listener = (event: InterceptedEvent) => void;
+type InterceptorOptions = {
+    handler: (event: InterceptedEvent) => void;
+    getIsTorEnabled: () => boolean;
+};
 
-const interceptNetSocketConnect = (listener: Listener) => {
+const getIdentityName = (userAgent: string): string | undefined => {
+    const identity = userAgent[0];
+    if (Array.isArray(userAgent)) {
+        const identityName = identity.match(/identity:(.*)/);
+        // Only return identity name if it is explicitly defined.
+        return identityName ? identityName[1] : undefined;
+    }
+    return undefined;
+};
+
+const getAgent = (identityName: string | undefined) =>
+    TorIdentities.getIdentity(identityName || 'default');
+
+const interceptNetSocketConnect = (interceptorOptions: InterceptorOptions) => {
     const originalSocketConnect = net.Socket.prototype.connect;
 
     net.Socket.prototype.connect = function (...args) {
         const [options, connectionListener] = args as any;
         let details;
         if (Array.isArray(options) && options.length > 0 && options[0].href) {
-            // When websockets in clearnet options contains array where first element is networkOptions.
+            // When websockets in clear-net options contains array where first element is networkOptions.
             details = options[0].href;
         } else if (typeof options === 'object' && options.host && options.port) {
             // When Tor is used options is object with host and port that is used to connect to SocksPort.
             details = `${options.host}:${options.port}`;
         } else if (typeof options === 'number') {
-            // When stablishing conneciton to Tor control port `connectionListener` is Tor control port and
+            // When establishing connection to Tor control port `connectionListener` is Tor control port and
             // `options` is Tor control port host, most likely 127.0.0.1.
             details = `${connectionListener}:${options}`;
         }
 
-        listener({
+        interceptorOptions.handler({
             method: 'net.Socket.connect',
             details,
         });
@@ -33,54 +50,125 @@ const interceptNetSocketConnect = (listener: Listener) => {
     };
 };
 
-const interceptNetConnect = (listener: Listener) => {
+const interceptNetConnect = (interceptorOptions: InterceptorOptions) => {
     const originalConnect = net.connect;
 
     net.connect = function (...args) {
         const [connectArguments] = args;
-        listener({ method: 'net.connect', details: (connectArguments as any).host });
+        interceptorOptions.handler({
+            method: 'net.connect',
+            details: (connectArguments as any).host,
+        });
+
         // @ts-expect-error
         return originalConnect.apply(this, args);
     };
 };
 
-const interceptHttp = (listener: Listener) => {
+const interceptHttp = (interceptorOptions: InterceptorOptions) => {
     const originalHttpRequest = http.request;
 
     http.request = function (...args) {
-        const [options] = args as any;
-        listener({ method: 'http.request', details: options.href });
+        const isTorEnabled = interceptorOptions.getIsTorEnabled();
+        // eslint-disable-next-line prefer-const
+        let [url, options, callback] = args as any;
+        // http.request could have different arguments according to it's types definition,
+        // but we only care when second argument is object containing RequestOptions.
+        if (!callback && typeof url === 'object' && (!options || typeof options === 'function')) {
+            const overloadedOptions = url;
+            const overloadedCallback = options;
+
+            const overloadedOptionsUrl = new URL(overloadedOptions.href);
+            const userAgent = overloadedOptions.headers['User-Agent'];
+            // Requests to localhost should not use the proxy.
+            const shouldIntercept = overloadedOptionsUrl.hostname !== '127.0.0.1';
+            const identityName = getIdentityName(userAgent);
+            const agent = isTorEnabled && shouldIntercept ? getAgent(identityName) : undefined;
+            if (agent) {
+                // In the case that `User-Agent` was used for identity information we send empty string.
+                // TODO: this could be changed to use a common `User-Agent` instead of empty string,
+                // but consider the common `User-Agent` will be changing over time.
+                overloadedOptions.headers['User-Agent'] = '';
+            }
+
+            interceptorOptions.handler({ method: 'http.request', details: overloadedOptions.href });
+            return originalHttpRequest.call(
+                this,
+                {
+                    ...overloadedOptions,
+                    agent,
+                },
+                overloadedCallback as any,
+            );
+        }
+
+        // In cases that are not considered above we pass the args as they came.
         // @ts-expect-error
         return originalHttpRequest.apply(this, args);
     };
 };
 
-const interceptHttps = (listener: Listener) => {
+const interceptHttps = (interceptorOptions: InterceptorOptions) => {
     const originalHttpsRequest = https.request;
 
     https.request = function (...args) {
-        const [options] = args as any;
-        listener({ method: 'https.request', details: options.href });
+        const isTorEnabled = interceptorOptions.getIsTorEnabled();
+        const [url, options, callback] = args as any;
+        // https.request could have different arguments according to it's types definition,
+        // but we only care when second argument is object containing RequestOptions.
+        if (!callback && typeof url === 'object' && (!options || typeof options === 'function')) {
+            const overloadedOptions = url;
+            const overloadedCallback = options;
+
+            const overloadedOptionsUrl = new URL(overloadedOptions.href);
+            const userAgent = overloadedOptions.headers['User-Agent'];
+            // Requests to localhost should not use the proxy.
+            const shouldIntercept = overloadedOptionsUrl.hostname !== '127.0.0.1';
+            const identityName = getIdentityName(userAgent);
+            const agent = isTorEnabled && shouldIntercept ? getAgent(identityName) : undefined;
+            if (agent) {
+                // In the case that `User-Agent` was used for identity information we send empty string.
+                // TODO: this could be changed to use a common `User-Agent` instead of empty string,
+                // but consider the common `User-Agent` will be changing over time.
+                overloadedOptions.headers['User-Agent'] = '';
+            }
+
+            interceptorOptions.handler({
+                method: 'https.request',
+                details: overloadedOptions.href,
+            });
+
+            return originalHttpsRequest.call(
+                this,
+                {
+                    ...overloadedOptions,
+                    agent,
+                },
+                overloadedCallback as any,
+            );
+        }
+
+        // In cases that are not considered above we pass the args as they came.
         // @ts-expect-error
         return originalHttpsRequest.apply(this, args);
     };
 };
 
-const interceptTlsConnect = (listener: Listener) => {
-    const orginalTlsConnect = tls.connect;
+const interceptTlsConnect = (interceptorOptions: InterceptorOptions) => {
+    const originalTlsConnect = tls.connect;
 
     tls.connect = function (...args) {
         const [options] = args as any;
-        listener({ method: 'tls.connect', details: options.servername });
+        interceptorOptions.handler({ method: 'tls.connect', details: options.servername });
         // @ts-expect-error
-        return orginalTlsConnect.apply(this, args);
+        return originalTlsConnect.apply(this, args);
     };
 };
 
-export const createInterceptor = (listener: Listener) => {
-    interceptNetSocketConnect(listener);
-    interceptNetConnect(listener);
-    interceptHttp(listener);
-    interceptHttps(listener);
-    interceptTlsConnect(listener);
+export const createInterceptor = (interceptorOptions: InterceptorOptions) => {
+    interceptNetSocketConnect(interceptorOptions);
+    interceptNetConnect(interceptorOptions);
+    interceptHttp(interceptorOptions);
+    interceptHttps(interceptorOptions);
+    interceptTlsConnect(interceptorOptions);
 };
