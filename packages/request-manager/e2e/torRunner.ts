ec72016e70b1db353fd555718ecd1d3987e481bb
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2022-08-16 14:32:57 +0200

    test: integration tests for tor identities

---
+++ packages/request-manager/e2e/torRunner.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+import { spawn } from 'child_process';
+
+const customTorProcessDir = process.env.TOR_BINARY_PATH;
+const processDir = path.join(__dirname, './../../suite-data/files/bin/tor/linux-x64/');
+const processPath = customTorProcessDir || path.join(processDir, 'tor');
+
+export const torRunner = ({ torParams = [] }: { torParams: string[] }) => {
+    const process = spawn(processPath, torParams, {
+        cwd: processDir,
+        stdio: ['ignore', 'ignore', 'ignore'],
+    });
+    return process;
+};
