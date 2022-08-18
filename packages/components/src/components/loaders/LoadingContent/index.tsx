13d35bb5f2b8dc002835033ffc6581c3468f8367
dahaca <dahaca17@gmail.com>
2022-08-18 19:18:10 +0200

    feat(suite): improve tor loading indicator in settings

--- packages/components/src/components/loaders/LoadingContent/index.tsx
+++ packages/components/src/components/loaders/LoadingContent/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled, { css } from 'styled-components';
+import { useTheme } from '@trezor/components';
 import { Icon } from '../../Icon';
 import { Loader } from '../Loader';
 
@@ -8,34 +9,51 @@ const LoadingWrapper = styled.div`
     align-items: center;
 `;
 
-export type LoadingContentProps = {
-    isLoading?: boolean;
-    size?: number;
-};
-
-const LoaderCell = styled.div<Required<LoadingContentProps>>`
+const LoaderCell = styled.div<Required<Omit<LoadingContentProps, 'isSuccessful'>>>`
     width: ${({ size }) => 1.5 * size}px;
     transition: all 0.25s ease-out 0.5s;
-    svg {
-        fill: ${({ theme }) => theme.TYPE_GREEN};
-    }
+
     ${({ isLoading }) =>
         !isLoading &&
         css`
             width: 0;
             opacity: 0;
         `}
+
+    svg {
+        fill: ${({ theme }) => theme.TYPE_GREEN};
+    }
 `;
+
+export type LoadingContentProps = {
+    isLoading?: boolean;
+    size?: number;
+    isSuccessful?: boolean;
+};
 
 export const LoadingContent: React.FC<LoadingContentProps> = ({
     children,
     isLoading = false,
     size = 20,
-}) => (
-    <LoadingWrapper>
-        <LoaderCell isLoading={isLoading} size={size}>
-            {isLoading ? <Loader size={size} /> : <Icon icon="CHECK" size={size} />}
-        </LoaderCell>
-        {children}
-    </LoadingWrapper>
-);
+    isSuccessful = true,
+}) => {
+    const theme = useTheme();
+
+    return (
+        <LoadingWrapper>
+            <LoaderCell isLoading={isLoading} size={size}>
+                {isLoading ? (
+                    <Loader size={size} />
+                ) : (
+                    <Icon
+                        icon={isSuccessful ? 'CHECK' : 'CROSS'}
+                        size={size}
+                        color={isSuccessful ? theme.TYPE_GREEN : theme.TYPE_RED}
+                    />
+                )}
+            </LoaderCell>
+
+            {children}
+        </LoadingWrapper>
+    );
+};
