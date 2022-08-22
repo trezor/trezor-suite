ff0e7c30e4039ba64481f35c12ce887bd3460441
Marek Polak <marek.polak@satoshilabs.com>
2022-08-22 17:00:55 +0200

    feat(coinjoin): coinjoin discovery logic

---
+++ packages/coinjoin/src/backend/types.ts
@@ -0,0 +1,114 @@
+import type { Network } from '@trezor/utxo-lib';
+import type { AccountInfo, Address, Transaction } from '@trezor/blockchain-link/src/types';
+import type { Transaction as BlockbookTransaction } from '@trezor/blockchain-link/src/types/blockbook';
+
+import type { CoinjoinBackendClient } from './CoinjoinBackendClient';
+import type { MempoolController } from './CoinjoinMempoolController';
+
+export type { BlockbookTransaction };
+export type { AccountInfo, Address, Transaction };
+
+export type BlockbookBlock = {
+    height: number;
+    txs: BlockbookTransaction[];
+};
+
+export type BlockFilter = {
+    blockHeight: number;
+    blockHash: string;
+    filter: string;
+    prevHash: string;
+    blockTime: number;
+};
+
+export type BlockFilterResponse = {
+    bestHeight: number;
+    filters: BlockFilter[];
+};
+
+type MethodContext = {
+    client: CoinjoinBackendClient;
+    network: Network;
+    abortSignal?: AbortSignal;
+};
+
+type ScanContext<T> = MethodContext & {
+    filters: FilterController;
+    mempool: MempoolController;
+    onProgress: (progress: T) => void;
+};
+
+export type ScanAddressContext = ScanContext<ScanAddressProgress>;
+
+export type ScanAccountContext = ScanContext<ScanAccountProgress>;
+
+export type ScanAddressCheckpoint = {
+    blockHash: string;
+    blockHeight: number;
+};
+
+export type ScanAccountCheckpoint = ScanAddressCheckpoint & {
+    receiveCount: number;
+    changeCount: number;
+};
+
+export type ScanProgressInfo = {
+    progress?: number;
+    message?: string;
+};
+
+type ScanProgress<T> = {
+    checkpoint: T;
+    transactions: Transaction[];
+    info?: ScanProgressInfo;
+};
+
+export type ScanAddressProgress = ScanProgress<ScanAddressCheckpoint>;
+
+export type ScanAccountProgress = ScanProgress<ScanAccountCheckpoint>;
+
+export type ScanAccountParams = {
+    descriptor: string;
+    checkpoint?: ScanAccountCheckpoint;
+};
+
+export type ScanAddressParams = {
+    descriptor: string;
+    checkpoint?: ScanAddressCheckpoint;
+};
+
+export type ScanAccountResult = {
+    pending: Transaction[];
+    checkpoint: ScanAccountCheckpoint;
+};
+
+export type ScanAddressResult = {
+    pending: Transaction[];
+    checkpoint: ScanAddressCheckpoint;
+};
+
+export type FilterControllerParams = {
+    fromHash?: string;
+    batchSize?: number;
+};
+
+export type FilterControllerContext = {
+    abortSignal?: AbortSignal;
+};
+
+export interface FilterController {
+    get bestBlockHeight(): number;
+    getFilterIterator(
+        params?: FilterControllerParams,
+        context?: FilterControllerContext,
+    ): AsyncGenerator<BlockFilter>;
+}
+
+export type FilterClient = Pick<CoinjoinBackendClient, 'fetchFilters'>;
+
+export type MempoolClient = Pick<CoinjoinBackendClient, 'fetchMempoolTxids' | 'fetchTransaction'>;
+
+export type AccountAddress = {
+    address: string;
+    script: Buffer;
+};
