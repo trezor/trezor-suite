b5555cdd8895b70d4fbc34de5878a25fd636306b
bouchja1 <jan.bouchner@gmail.com>
2022-08-31 17:20:33 +0200

    feat(native): Add support for debugging redux in RN

--- suite-native/state/src/store.ts
+++ suite-native/state/src/store.ts
@@ -1,10 +1,18 @@
-import { configureStore, Store } from '@reduxjs/toolkit';
+import { configureStore, Store, Middleware } from '@reduxjs/toolkit';
+import createDebugger from 'redux-flipper';
 
 import { appSettingsReducer } from '@suite-native/module-settings';
 import { appGraphReducer } from '@suite-native/home-graph';
 import { onboardingReducer } from '@suite-native/module-onboarding';
 
 import { extraDependencies } from './extraDependencies';
+
+const middlewares: Middleware[] = [];
+
+if (__DEV__) {
+    const reduxFlipperDebugger = createDebugger();
+    middlewares.push(reduxFlipperDebugger);
+}
 
 export const store: Store = configureStore({
     reducer: {
@@ -17,7 +25,7 @@ export const store: Store = configureStore({
             thunk: {
                 extraArgument: extraDependencies,
             },
-        }),
+        }).concat(middlewares),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
