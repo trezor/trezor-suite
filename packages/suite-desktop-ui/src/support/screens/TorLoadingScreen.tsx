0caa295768b7ec51cc3c9c9670cd17d816ff8507
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2022-09-20 13:13:04 +0200

    fix: use Tor loader component in TorLoadingScreen

--- packages/suite-desktop-ui/src/support/screens/TorLoadingScreen.tsx
+++ packages/suite-desktop-ui/src/support/screens/TorLoadingScreen.tsx
@@ -3,9 +3,9 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { ThemeProvider } from '@suite-support/ThemeProvider';
 import { TorStatus } from '@suite-types';
-import { Translation } from '@suite-components/Translation';
+import { Translation, TorLoader } from '@suite-components';
 
-import { Button, Progress, Image, variables, Modal } from '@trezor/components';
+import { Button, Modal } from '@trezor/components';
 import { desktopApi, BootstrapTorEvent } from '@trezor/suite-desktop-api';
 
 const Wrapper = styled.div`
@@ -19,73 +19,6 @@ const Wrapper = styled.div`
 
 const StyledModal = styled(Modal)`
     max-width: 600px;
-`;
-
-const StyledImage = styled(Image)`
-    margin-bottom: 28px;
-`;
-
-const Text = styled.h2`
-    font-size: ${variables.FONT_SIZE.H2};
-    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
-    color: ${({ theme }) => theme.TYPE_DARK_GREY};
-`;
-
-const InfoWrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-`;
-
-const MessageWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    margin-bottom: 28px;
-`;
-
-const DisableButton = styled(Button)`
-    margin-left: 10px;
-    border: 1px solid ${({ theme }) => theme.STROKE_GREY};
-    background: ${({ theme }) => theme.BG_WHITE};
-    color: ${({ theme }) => theme.TYPE_LIGHT_GREY};
-
-    :hover {
-        background: ${({ theme }) => theme.BG_LIGHT_RED};
-    }
-`;
-
-const Percentage = styled.span`
-    display: flex;
-    align-items: center;
-    font-variant-numeric: tabular-nums;
-    height: 24px;
-`;
-
-const StyledProgress = styled(Progress)`
-    display: flex;
-    margin: 0 20px;
-    border-radius: 5px;
-    flex: 1;
-
-    ${Progress.Value} {
-        position: relative;
-        border-radius: 5px;
-    }
-`;
-
-const ProgressWrapper = styled.div`
-    display: flex;
-    align-items: center;
-    border-radius: 8px;
-    width: 100%;
-    min-height: 45px;
-`;
-
-const ProgressMessage = styled.div`
-    margin-right: 16px;
-    color: ${({ theme }) => theme.TYPE_LIGHT_GREY};
-    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
 `;
 
 const StyledButton = styled(Button)`
@@ -125,14 +58,6 @@ export const TorLoadingScreen = ({ callback }: TorLoadingScreenProps) => {
         return () => desktopApi.removeAllListeners('tor/bootstrap');
     }, [torStatus, callback]);
 
-    let message: 'TR_ENABLING_TOR' | 'TR_ENABLING_TOR_FAILED' | 'TR_DISABLING_TOR' =
-        'TR_ENABLING_TOR';
-    if (torStatus === TorStatus.Error) {
-        message = 'TR_ENABLING_TOR_FAILED';
-    } else if (torStatus === TorStatus.Disabling) {
-        message = 'TR_DISABLING_TOR';
-    }
-
     const tryAgain = async () => {
         setProgress(0);
         setTorStatus(TorStatus.Enabling);
@@ -152,7 +77,7 @@ export const TorLoadingScreen = ({ callback }: TorLoadingScreenProps) => {
         desktopApi.removeAllListeners('tor/bootstrap');
         desktopApi.toggleTor(false);
 
-        // This is a total fake progress, otherwise it would be to fast for user.
+        // This is a total fake progress, otherwise it would be too fast for user.
         await new Promise(resolve => {
             const interval = setInterval(() => {
                 if (fakeProgress === 100) {
@@ -184,39 +109,7 @@ export const TorLoadingScreen = ({ callback }: TorLoadingScreenProps) => {
                         )
                     }
                 >
-                    <MessageWrapper>
-                        <StyledImage width={130} height={130} image="TOR_ENABLING" />
-                        <Text>
-                            <Translation id={message} />
-                        </Text>
-                    </MessageWrapper>
-
-                    <InfoWrapper>
-                        <ProgressWrapper>
-                            <StyledProgress
-                                isRed={torStatus === TorStatus.Error}
-                                value={torStatus === TorStatus.Error ? 100 : progress}
-                            />
-
-                            <ProgressMessage>
-                                {torStatus === TorStatus.Error ? (
-                                    <Translation id="TR_FAILED" />
-                                ) : (
-                                    <Percentage>{progress} %</Percentage>
-                                )}
-                            </ProgressMessage>
-                        </ProgressWrapper>
-
-                        {torStatus !== TorStatus.Disabling && (
-                            <DisableButton
-                                data-test="@tor-loading-screen/disable-button"
-                                variant="secondary"
-                                onClick={disableTor}
-                            >
-                                <Translation id="TR_TOR_DISABLE" />
-                            </DisableButton>
-                        )}
-                    </InfoWrapper>
+                    <TorLoader torStatus={torStatus} progress={progress} disableTor={disableTor} />
                 </StyledModal>
             </Wrapper>
         </ThemeProvider>
