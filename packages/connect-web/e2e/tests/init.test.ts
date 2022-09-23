ed516c0dc0714e97f676b6125285bb74ff82d435
mroz <mvarmuza@gmail.com>
2022-09-23 12:59:59 +0200

    test(connect-web): TrezorConnect.init connectSrc test

---
+++ packages/connect-web/e2e/tests/init.test.ts
@@ -0,0 +1,53 @@
+import { test, expect } from '@playwright/test';
+
+test.beforeAll(async () => {});
+
+const inlineScriptUrl = process.env.URL
+    ? // url of build from CI
+      `${process.env.URL}/trezor-connect.js`
+    : // for testing out on localhost
+      'https://connect.trezor.io/9/trezor-connect.js';
+
+const fixtures = [
+    {
+        params: {
+            connectSrc: '',
+        },
+        result: 'https://connect.trezor.io/9/iframe.html',
+    },
+    {
+        params: {
+            versionChannel: undefined,
+            connectSrc: 'https://connect.trezor.io/9.0.1/',
+        },
+        result: 'https://connect.trezor.io/9.0.1/iframe.html',
+    },
+    {
+        // it is even possible to load old connect version 8
+        params: {
+            connectSrc: 'https://connect.trezor.io/8/',
+        },
+        result: 'https://connect.trezor.io/8/iframe.html',
+    },
+];
+
+fixtures.forEach(f => {
+    test(`TrezorConnect.init ${JSON.stringify(f.params)} => ${f.result}`, async ({ page }) => {
+        await page.addScriptTag({ url: inlineScriptUrl });
+        await page.addScriptTag({
+            content: `
+                window.TrezorConnect.init({
+                    lazyLoad: false,
+                    manifest: {
+                        email: 'developer@xyz.com',
+                        appUrl: 'http://your.application.com',
+                    },
+                    versionChannel: '${f.params.versionChannel}',
+                    connectSrc: '${f.params.connectSrc}'
+                });
+        `,
+        });
+        const iframeSrc = await page.locator('iframe').getAttribute('src');
+        expect(iframeSrc).toContain(f.result);
+    });
+});
