1ec30afe2731d09d882818010c55162e434535e4
Jan Bouchner <jan.bouchner@gmail.com>
2022-09-26 13:42:03 +0200

    fix(suite-common): Fix intl init in formatters (#6391)

--- packages/suite/src/hooks/suite/useFormattersConfig.ts
+++ packages/suite/src/hooks/suite/useFormattersConfig.ts
@@ -1,15 +1,12 @@
 import { useSelector } from '@suite-hooks/useSelector';
-import { FormatterConfig } from '@suite-common/formatters';
-import { useIntl } from 'react-intl';
+import { FormatterProviderConfig } from '@suite-common/formatters';
 
-export const useFormattersConfig = (): FormatterConfig => {
-    const intl = useIntl();
+export const useFormattersConfig = (): FormatterProviderConfig => {
     const locale = useSelector(state => state.suite.settings.language);
     const bitcoinAmountUnit = useSelector(state => state.wallet.settings.bitcoinAmountUnit);
 
     return {
         locale,
         bitcoinAmountUnit,
-        intl,
     };
 };
