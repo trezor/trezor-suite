6e0c014dd9ca99621944d717ab3f68f30c8d766a
mroz <mvarmuza@gmail.com>
2022-09-27 14:53:43 +0200

    chore: improve types in jest tests

--- packages/connect/src/global.d.ts
+++ packages/connect/src/global.d.ts
@@ -7,9 +7,3 @@ declare namespace globalThis {
 interface Window {
     __TREZOR_CONNECT_SRC?: string;
 }
-
-type TestFixtures<TestedMethod extends (...args: any) => any> = {
-    description: string;
-    input: Parameters<TestedMethod>;
-    output: ReturnType<TestedMethod>;
-}[];
