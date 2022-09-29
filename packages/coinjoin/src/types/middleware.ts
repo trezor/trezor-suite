2598853a57ae32a320b7603201a3a7c67e1522b9
Szymon Lesisz <sz.lesisz@gmail.com>
2022-09-29 15:21:34 +0200

    feat(coinjoin): add WabiSabiClientLibrary requests module
    http requests to WalletWasabi.WabiSabiClientLibrary binary (middleware)

---
+++ packages/coinjoin/src/types/middleware.ts
@@ -0,0 +1,40 @@
+import { AllowedScriptTypes } from './coordinator';
+
+export interface Credentials {
+    value: number;
+    randomness: string;
+    mac: {
+        t: string;
+    };
+}
+
+export type UtxoForRound = {
+    outpoint: string;
+    amount: number;
+    scriptType: AllowedScriptTypes;
+    anonymitySet: number;
+};
+
+export interface AnalyzeInternalVinVout {
+    address: string;
+    value: number;
+}
+
+export interface AnalyzeExternalVinVout {
+    scriptPubKey: string;
+    value: number;
+}
+
+export interface AnalyzeTransactionDetails {
+    internalInputs: AnalyzeInternalVinVout[];
+    internalOutputs: AnalyzeInternalVinVout[];
+    externalInputs: AnalyzeExternalVinVout[];
+    externalOutputs: AnalyzeExternalVinVout[];
+}
+
+export interface AnalyzeResult {
+    results: {
+        address: string;
+        anonymitySet: number;
+    }[];
+}
