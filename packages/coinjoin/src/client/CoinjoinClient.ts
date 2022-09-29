092282a7ac76b104ed884a2276498778cb4a8206
Szymon Lesisz <sz.lesisz@gmail.com>
2022-09-29 16:15:59 +0200

    feat(coinjoin): implement CoinjoinClient module

--- packages/coinjoin/src/client/CoinjoinClient.ts
+++ packages/coinjoin/src/client/CoinjoinClient.ts
@@ -1,10 +1,25 @@
 import { EventEmitter } from 'events';
 
-import type { CoinjoinClientSettings } from '../types';
-import type { CoinjoinStatusEvent, CoinjoinStatus, RegisterAccountParams } from '../types/client';
+import { Status, OnStatusUpdate } from './Status';
+import { selectRound, finishCurrentProcess, processRounds, resolveRequests } from './phase';
+import { registerAccount } from './account';
+import {
+    CoinjoinClientSettings,
+    CoinjoinStatusEvent,
+    CoinjoinClientEvent,
+    RegisterAccountParams,
+    RegisteredAccount,
+    ActiveRound,
+    RequestEvent,
+    RoundPhase,
+} from '../types';
 
 interface Events {
     status: CoinjoinStatusEvent;
+    request: RequestEvent[];
+    log: any[];
+    event: CoinjoinClientEvent;
+    error: any;
 }
 
 export declare interface CoinjoinClient {
@@ -16,23 +31,223 @@ export declare interface CoinjoinClient {
 
 export class CoinjoinClient extends EventEmitter {
     readonly settings: CoinjoinClientSettings;
+    private abortController: AbortController; // used for immediate interruption
+    private accounts: RegisteredAccount[]; // list of registered accounts
+    private activeRounds: ActiveRound[]; // list of active rounds
+    private usedAddresses: any[]; // temporary list of addresses used in active round, may be modified after account update
+    private bannedUtxos: any[]; // list of banned utxos, may be modified after account update
+    // private bannedOutputs: any[]; // list of banned outputs
+    private status: Status;
 
     constructor(settings: CoinjoinClientSettings) {
         super();
         this.settings = Object.freeze(settings);
+        this.abortController = new AbortController();
+        this.accounts = [];
+        this.activeRounds = [];
+        this.usedAddresses = [];
+        this.bannedUtxos = [];
+        // this.bannedOutputs = [];
+        this.status = new Status(settings);
+        this.status.on('update', event => {
+            this.onStatusUpdate(event);
+            if (event.changed.length > 0) {
+                this.emit('status', event);
+            }
+        });
+    }
+
+    private log(...args: any[]) {
+        if (this.listenerCount('log') > 0) {
+            this.emit('log', ...args);
+        }
+    }
+
+    private async onStatusUpdate({ changed, rounds }: OnStatusUpdate) {
+        // try to finish/interrupt current running process on changed round (if exists)
+        await finishCurrentProcess(changed);
+
+        // find all ActiveRounds affected by changed Status/Rounds
+        const roundsToProcess = changed.reduce((result, round) => {
+            const active = this.activeRounds.find(r => r.id === round.id);
+            return active
+                ? result.concat({
+                      ...active,
+                      phase: round.phase,
+                      coinjoinState: round.coinjoinState,
+                  }) // update ActiveRound data from Status
+                : result;
+        }, [] as ActiveRound[]);
+
+        const options = {
+            signal: this.abortController.signal,
+            coordinatorName: this.settings.coordinatorName,
+            coordinatorUrl: this.settings.coordinatorUrl,
+            middlewareUrl: this.settings.middlewareUrl,
+            log: (...args: any[]) => this.log(...args),
+        };
+
+        // there are no ActiveRounds to process. try to create new ActiveRound
+        if (roundsToProcess.length === 0) {
+            const round = await selectRound(this.accounts, rounds, this.activeRounds, options);
+            if (round) {
+                roundsToProcess.push(round);
+            }
+        }
+
+        // notify wallet
+        roundsToProcess.forEach(round => {
+            this.emit('event', {
+                type: 'round-change',
+                payload: round,
+            });
+        });
+
+        if (roundsToProcess.length > 0) {
+            const {
+                rounds: processedRounds,
+                requests,
+                failed,
+            } = await processRounds(roundsToProcess, this.accounts, options);
+
+            processedRounds.forEach(round => {
+                if (round.addresses) {
+                    this.usedAddresses.concat(round.addresses);
+                }
+            });
+
+            // store banned utxos
+            failed.forEach(utxo => {
+                // TODO: get real message
+                if (utxo.error && utxo.error.message.includes('ban')) {
+                    this.bannedUtxos.push(utxo);
+                }
+            });
+
+            this.updateActiveRounds(processedRounds);
+            if (requests.length > 0) {
+                this.emit('request', requests);
+            }
+        }
+    }
+
+    // handle the result of the Status update > processRounds
+    private updateActiveRounds(rounds: ActiveRound[]) {
+        if (rounds.length < 1) return;
+        rounds.forEach(round => {
+            this.emit('event', {
+                type: 'round-change',
+                payload: round,
+            });
+        });
+        this.activeRounds = this.activeRounds
+            .filter(
+                active =>
+                    !rounds.find(round => round.id === active.id) &&
+                    active.phase !== RoundPhase.Ended,
+            )
+            .concat(rounds.filter(r => r.phase !== RoundPhase.Ended));
     }
 
     enable() {
-        return Promise.resolve<CoinjoinStatus>({ rounds: [{ id: '00' }] });
+        if (this.abortController.signal.aborted) {
+            this.abortController = new AbortController();
+        }
+        return this.status.start();
     }
 
-    disable() {}
+    disable() {
+        this.removeAllListeners();
+        this.abortController.abort();
+        this.status.stop();
+    }
 
-    registerAccount(_account: RegisterAccountParams) {}
+    registerAccount(account: RegisterAccountParams) {
+        if (this.accounts.find(a => a.descriptor === account.descriptor)) {
+            this.updateAccount(account);
+            return;
+        }
 
-    updateAccount(_account: RegisterAccountParams) {}
+        this.accounts.push(registerAccount(account));
 
-    unregisterAccount(_descriptor: string) {}
+        // TODO: only temporary, to trigger registration immediately without waiting for status change
+        this.onStatusUpdate({ rounds: this.status.rounds, changed: [], feeRatesMedians: [] });
+    }
 
-    resolveRequest() {}
+    updateAccount(account: RegisterAccountParams) {
+        const accountToUpdate = this.accounts.find(a => a.descriptor === account.descriptor);
+        if (accountToUpdate) {
+            // TODO: check if account/utxos is not registered in any ActiveRound
+            // TODO: update usedAddresses and bannedUtxos related to this account
+            this.accounts = this.accounts
+                .filter(a => a.descriptor !== account.descriptor)
+                .concat([registerAccount(account)]);
+
+            const activeRounds = this.activeRounds.filter(round =>
+                Object.keys(round.accounts).includes(account.descriptor),
+            );
+            if (activeRounds.length === 0) {
+                // TODO: check Status next tick (possible race condition)
+                // TODO: only temporary, to trigger registration immediately without waiting for status change
+                this.onStatusUpdate({
+                    rounds: this.status.rounds,
+                    changed: [],
+                    feeRatesMedians: [],
+                });
+            }
+        }
+    }
+
+    unregisterAccount(descriptor: string) {
+        // TODO: check if account/utxos is not registered in any ActiveRound
+        // TODO: update usedAddresses and bannedUtxos related to this account
+        const activeRounds = this.activeRounds.filter(round =>
+            Object.keys(round.accounts).includes(descriptor),
+        );
+        if (activeRounds.length > 0) {
+            const { rounds } = this.status;
+            const changed = rounds.filter(round =>
+                activeRounds.find(active => active.id === round.id),
+            );
+            finishCurrentProcess(changed);
+
+            const roundsToUpdate = activeRounds.map(r => {
+                const roundAccounts = r.accounts;
+                delete roundAccounts[descriptor];
+                // TODO: unregister if it's possible
+                return {
+                    ...r,
+                    accounts: roundAccounts,
+                };
+            });
+            this.updateActiveRounds(roundsToUpdate);
+        }
+
+        this.accounts = this.accounts.filter(a => a.descriptor !== descriptor);
+    }
+
+    resolveRequest(response: RequestEvent[]) {
+        // TODO: validate errors or missing but expected fields (ownership, witness)
+        const roundsToUpdate = resolveRequests(this.activeRounds, response);
+        // const acctoupdate = response.flatMap(event => {
+        //     if (event.type === 'witness') {
+        //         Object.keys(event.accounts).forEach(key => {
+        //             const aa = this.accounts.find(a => a.descriptor === key);
+        //             return aa || [];
+        //         });
+        //     }
+        //     return [];
+        // });
+        // if (acctoupdate.length) {
+        //     // this.accounts = this.accounts.map(a => {
+        //     // })
+        // }
+        this.updateActiveRounds(roundsToUpdate);
+
+        const { rounds } = this.status;
+        const changed = rounds.filter(round =>
+            roundsToUpdate.find(active => active.id === round.id),
+        );
+        this.onStatusUpdate({ rounds, changed, feeRatesMedians: [] });
+    }
 }
--- packages/coinjoin/src/types/client.ts
+++ packages/coinjoin/src/types/client.ts
@@ -1,3 +1,19 @@
+import {
+    RegisteredAccountUtxo,
+    RegisteredAccountAddress,
+    AccountUtxo,
+    AccountAddress,
+    AllowedNetworks,
+} from './account';
+import {
+    Round,
+    RoundPhase,
+    AllowedScriptTypes,
+    CoinjoinStateEvent,
+    TxPaymentRequest,
+} from './coordinator';
+import { Credentials } from './middleware';
+
 export type CoinjoinRound = {
     id: string;
 };
@@ -8,6 +24,114 @@ export interface CoinjoinStatusEvent {
     feeRatesMedians: any[];
 }
 
-export type RegisterAccountParams = {
-    descriptor: string;
+export type CoinjoinClientEvent = {
+    type: 'round-change';
+    payload: ActiveRound;
 };
+
+export type RegisterAccountParams = {
+    type: AllowedScriptTypes;
+    symbol: AllowedNetworks;
+    descriptor: string;
+    utxos: AccountUtxo[];
+    addresses: AccountAddress[];
+    anonymityLevel: number;
+    maxRounds: number;
+    maxFeePerKvbyte: number;
+    maxCoordinatorFeeRate: number;
+};
+
+// data sent as request to wallet (witness request)
+// contains data required to sign transaction and provide witnesses in return
+
+type TxInputs = {
+    path?: string;
+    outpoint: string;
+    index: number;
+    hash: string;
+    amount: number;
+    commitmentData: string;
+    scriptPubKey: string;
+    ownershipProof: string;
+    witness?: string;
+    witnessIndex?: number;
+};
+
+interface TxOutputs {
+    path?: string;
+    address: string;
+    amount: number;
+}
+
+export interface TransactionData {
+    inputs: TxInputs[];
+    outputs: TxOutputs[];
+    paymentRequest: TxPaymentRequest; // TODO
+}
+
+// wasabi Arena
+export interface ActiveRound {
+    id: string;
+    phase: RoundPhase;
+    accounts: Record<string, ActiveRoundAccount>; // key(string) = account.descriptor
+    commitmentData: string; // commitment data used for ownership proof and witness requests
+    amountCredentialIssuerParameters: Round['amountCredentialIssuerParameters'];
+    vsizeCredentialIssuerParameters: Round['vsizeCredentialIssuerParameters'];
+    roundParameters: Extract<CoinjoinStateEvent, { Type: 'RoundCreated' }>['roundParameters'] & {
+        inputRegistrationEnd: string;
+    };
+    coinjoinState: Round['coinjoinState'];
+    addresses?: RegisteredAccountAddress[]; // list of addresses (outputs) used in this round in outputRegistration phase
+    transactionData?: TransactionData; // transaction to sign
+}
+
+export interface ActiveRoundOptions {
+    signal: AbortSignal;
+    coordinatorName: string;
+    coordinatorUrl: string;
+    middlewareUrl: string;
+    log: (...args: any[]) => any;
+}
+
+export interface ActiveRoundAccount {
+    type: AllowedScriptTypes;
+    utxos: RegisteredAccountUtxo[]; // wasabi Alice
+    addresses: RegisteredAccountAddress[]; // wasabi Bob
+    // amounts: number[];
+    // amountCredentials: Credentials[];
+    // vsizeCredentials: Credentials[];
+}
+
+export interface DecomposedOutputs {
+    outputSize: number;
+    amounts: number[];
+    addresses: RegisteredAccountAddress[];
+    amountCredentials: Credentials[];
+    vsizeCredentials: Credentials[];
+}
+
+export type RequestedAccount = {
+    utxos: {
+        path: string;
+        outpoint: string;
+        ownershipProof?: string;
+        witness?: string;
+        witnessIndex?: number;
+        error?: string; // error from the wallet
+    }[];
+    addresses: RegisteredAccountAddress[];
+};
+
+export type RequestEvent =
+    | {
+          type: 'ownership';
+          round: string;
+          accounts: Record<string, RequestedAccount>;
+          commitmentData: string;
+      }
+    | {
+          type: 'witness';
+          round: string;
+          accounts: Record<string, RequestedAccount>;
+          transaction: TransactionData;
+      };
