3322c743604e2348e7c855f7701276acfa00fa79
mroz <mvarmuza@gmail.com>
2022-10-04 12:40:12 +0200

    chore(connect-popup): remove cookie fallback

--- packages/connect-common/src/storage/index.test.ts
+++ packages/connect-common/src/storage/index.test.ts
@@ -22,29 +22,6 @@ describe('storage', () => {
         expect(storage.load(storage.BROWSER_KEY)).toBe(false);
     });
 
-    test('window.document.cookie', () => {
-        const org = window.localStorage;
-        // @ts-expect-error, deactivate localStorage
-        delete window.localStorage;
-
-        expect(storage.load(storage.PERMISSIONS_KEY)).toBe(undefined);
-        storage.save(storage.BROWSER_KEY, true);
-        storage.save(storage.PERMISSIONS_KEY, [{ type: 'read' }]);
-
-        expect(storage.load(storage.PERMISSIONS_KEY)).toEqual([{ type: 'read' }]);
-        expect(storage.load(storage.BROWSER_KEY)).toBe(true);
-
-        // @ts-expect-error
-        storage.save(storage.PERMISSIONS_KEY, 'not-array');
-        expect(storage.load(storage.PERMISSIONS_KEY)).toEqual(undefined);
-        // @ts-expect-error
-        storage.save(storage.BROWSER_KEY, 'not-boolean');
-        expect(storage.load(storage.BROWSER_KEY)).toBe(false);
-
-        // restore localStorage
-        window.localStorage = org;
-    });
-
     test('memoryStorage', () => {
         expect(storage.load(storage.PERMISSIONS_KEY, true)).toBe(undefined);
         storage.save(storage.PERMISSIONS_KEY, [], true);
