4bddd57a6504d12d361d7729d23630b836d38fcb
Matej Kriz <matej@matejkriz.cz>
2022-10-12 16:49:04 +0200

    fix(suite): use JS implementation of bcrypto instead of native binaries to be able to run on arm mac

--- packages/suite-desktop/scripts/build-inject.ts
+++ packages/suite-desktop/scripts/build-inject.ts
@@ -2,3 +2,6 @@ import fetch from 'node-fetch';
 
 // @ts-expect-error
 global.fetch = fetch;
+
+// fix bcrypto - use JS implementaion to prevent using binaries for wrong architecture
+process.env.NODE_BACKEND = 'js';
