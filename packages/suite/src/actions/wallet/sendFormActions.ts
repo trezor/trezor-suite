13b25a066999f10359f1c82c2a82507110c64d52
dahaca <dahaca17@gmail.com>
2022-10-12 16:14:03 +0200

    fix(suite): fix sats appearing for unsupported networks in toasts

--- packages/suite/src/actions/wallet/sendFormActions.ts
+++ packages/suite/src/actions/wallet/sendFormActions.ts
@@ -225,7 +225,10 @@ const pushTransaction = () => async (dispatch: Dispatch, getState: GetState) => 
         ? new BigNumber(precomposedTx.totalSpent).minus(precomposedTx.fee).toString()
         : '0';
 
-    const areSatoshisUsed = getAreSatoshisUsed(getState().wallet.settings.bitcoinAmountUnit);
+    const areSatoshisUsed = getAreSatoshisUsed(
+        getState().wallet.settings.bitcoinAmountUnit,
+        account,
+    );
 
     // get total amount without fee OR token amount
     const formattedAmount = token
