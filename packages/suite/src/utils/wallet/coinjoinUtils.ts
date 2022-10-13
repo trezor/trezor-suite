be193149c6a5f2ab42b10c73f898b822e99b0abd
Szymon Lesisz <sz.lesisz@gmail.com>
2022-10-13 21:13:43 +0200

    feat(suite): handle CoinjoinClient status update

--- packages/suite/src/utils/wallet/coinjoinUtils.ts
+++ packages/suite/src/utils/wallet/coinjoinUtils.ts
@@ -1,4 +1,5 @@
 import BigNumber from 'bignumber.js';
+import { CoinjoinStatusEvent } from '@trezor/coinjoin';
 import { getUtxoOutpoint } from '@suite-common/wallet-utils';
 import { Account } from '@suite-common/wallet-types';
 
@@ -54,3 +55,29 @@ export const breakdownCoinjoinBalance = ({
 
     return balanceBreakdown;
 };
+
+/**
+ * Transform from coordinator format to coinjoinReducer format `CoinjoinClientFeeRatesMedians`
+ * array => object { name: value-in-vBytes }
+ */
+export const transformFeeRatesMedians = (medians: CoinjoinStatusEvent['feeRatesMedians']) => {
+    const [fast, recommended, slow] = medians.map(m => m.medianFeeRate);
+    // convert from kvBytes (kilo virtual bytes) to vBytes
+    const kvB2vB = (v: number) => (v ? Math.round(v / 1000) : 1);
+    return {
+        fast: kvB2vB(fast) * 2, // NOTE: this calculation will be smarter once have enough data
+        recommended: kvB2vB(recommended),
+        slow: kvB2vB(slow),
+    };
+};
+
+/**
+ * Transform from coordinator format to coinjoinReducer format `CoinjoinClientInstance`
+ * - coordinatorFeeRate: multiplied by 10. representation of percentage value
+ * - feeRatesMedians: array => object with values in vBytes
+ */
+export const transformCoinjoinStatus = (event: CoinjoinStatusEvent) => ({
+    rounds: event.rounds.map(r => ({ id: r.id, phase: r.phase })),
+    coordinatorFeeRate: event.coordinatorFeeRate * 10,
+    feeRatesMedians: transformFeeRatesMedians(event.feeRatesMedians),
+});
