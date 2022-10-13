9342cda6c06da179b3747f2ced91043af0abf320
Szymon Lesisz <sz.lesisz@gmail.com>
2022-10-13 17:42:08 +0200

    feat(coinjoin): implement Status in CoinjoinClient

--- packages/coinjoin/README.md
+++ packages/coinjoin/README.md
@@ -1,3 +1,27 @@
 # @trezor/coinjoin
 
 Coinjoin client library.
+
+## CoinjoinClient class
+
+Usage:
+
+```
+const client = new CoinjoinClient(settings);
+const status = await client.enable();
+```
+
+Once enabled it periodically sync with coordinator `/status` using random proxy (TOR) identities and time frequency until `client.disable();` is called.
+
+Status changes are emitted as `status` event:
+
+```
+client.on('status', event => {});
+
+{
+    rounds: Round[]; // current list of rounds
+    changes: Round[]: // list of changed rounds since recent update
+    feeRatesMedians: Array<{ timeFrame: string; medianFeeRate: number; }>, // timeFrame format: "0d 0h 0m 0s"
+    coordinatorFeeRate: action.status.coordinatorFeeRate, // current coordinatorFeeRate
+}
+```
--- packages/coinjoin/src/client/CoinjoinClient.ts
+++ packages/coinjoin/src/client/CoinjoinClient.ts
@@ -1,10 +1,12 @@
 import { EventEmitter } from 'events';
 
+import { Status } from './Status';
 import type { CoinjoinClientSettings } from '../types';
-import type { CoinjoinStatusEvent, CoinjoinStatus, RegisterAccountParams } from '../types/client';
+import type { CoinjoinStatusEvent, RegisterAccountParams } from '../types/client';
 
 interface Events {
     status: CoinjoinStatusEvent;
+    exception: string;
 }
 
 export declare interface CoinjoinClient {
@@ -16,23 +18,51 @@ export declare interface CoinjoinClient {
 
 export class CoinjoinClient extends EventEmitter {
     readonly settings: CoinjoinClientSettings;
+    private abortController: AbortController; // used for interruption
+    private status: Status;
 
     constructor(settings: CoinjoinClientSettings) {
         super();
         this.settings = Object.freeze(settings);
+        this.abortController = new AbortController();
+
+        this.status = new Status(settings);
+        this.status.on('update', event => {
+            this.onStatusUpdate(event);
+            if (event.changed.length > 0) {
+                this.emit('status', event);
+            }
+        });
+        this.status.on('exception', event => this.emit('exception', event));
     }
 
     enable() {
-        return Promise.resolve<CoinjoinStatus>({ rounds: [{ id: '00' }] });
+        if (this.abortController.signal.aborted) {
+            this.abortController = new AbortController();
+        }
+        return this.status.start();
     }
 
-    disable() {}
+    disable() {
+        this.removeAllListeners();
+        this.abortController.abort();
+        this.status.stop();
+    }
 
-    registerAccount(_account: RegisterAccountParams) {}
+    registerAccount(_account: RegisterAccountParams) {
+        this.status.setMode('enabled');
+    }
 
     updateAccount(_account: RegisterAccountParams) {}
 
-    unregisterAccount(_descriptor: string) {}
+    unregisterAccount(_descriptor: string) {
+        this.status.setMode('idle');
+    }
 
     resolveRequest() {}
+
+    private onStatusUpdate({ changed, rounds }: Pick<CoinjoinStatusEvent, 'changed' | 'rounds'>) {
+        // To be implemented soon
+        if (changed && rounds) return Promise.resolve();
+    }
 }
