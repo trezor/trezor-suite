f986af5b4c141789456534593aaadb5cd8392c0e
Marek Polak <marek.polak@satoshilabs.com>
2022-10-14 12:18:58 +0200

    feat(suite): improved tx IO details

--- packages/suite/src/components/suite/modals/TransactionDetail/components/IODetails.tsx
+++ packages/suite/src/components/suite/modals/TransactionDetail/components/IODetails.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Icon, useTheme, variables } from '@trezor/components';
 import { WalletAccountTransaction } from '@suite-common/wallet-types';
-import { formatNetworkAmount, getNetwork } from '@suite-common/wallet-utils';
+import { formatNetworkAmount } from '@suite-common/wallet-utils';
 import { FormattedCryptoAmount, HiddenPlaceholder, Translation } from '@suite-components';
+
+export const blurFix = css`
+    margin-left: -10px;
+    margin-right: -10px;
+    padding-left: 10px;
+    padding-right: 10px;
+`;
 
 const Wrapper = styled.div`
     text-align: left;
     margin-top: 25px;
-`;
-
-const IOWrapper = styled.div`
     overflow: auto;
-    padding: 0 1px;
+    ${blurFix}
 `;
 
 const IconWrapper = styled.div`
@@ -23,96 +27,122 @@ const IconWrapper = styled.div`
     margin-bottom: 16px;
 `;
 
-const IORowTitle = styled.div`
+const IOGridTitle = styled.div`
     margin-bottom: 4px;
     color: ${({ theme }) => theme.TYPE_LIGHT_GREY};
     font-size: ${variables.NEUE_FONT_SIZE.TINY};
 `;
 
-const IORow = styled.div<{ isAccountOwned?: boolean }>`
-    display: flex;
+const IOGrid = styled.div`
+    display: grid;
+    gap: 0 16px;
     line-height: 1.9;
-    color: ${({ theme, isAccountOwned }) =>
-        isAccountOwned ? theme.TYPE_DARK_GREY : theme.TYPE_LIGHT_GREY};
     font-weight: ${variables.FONT_WEIGHT.MEDIUM};
     font-size: ${variables.NEUE_FONT_SIZE.SMALL};
-`;
-
-const CryptoAmountWrapper = styled.div`
-    flex: 0 0 auto;
-`;
-
-const Circle = styled.div`
-    display: inline-flex;
-    margin-left: 5px;
-    margin-right: 5px;
     color: ${({ theme }) => theme.TYPE_LIGHT_GREY};
+    grid-template-columns: auto auto auto; /* address, extra, amount */
+    ${blurFix}
 `;
+
+const IOGridCell = styled.div<{ isAccountOwned?: boolean }>`
+    white-space: nowrap;
+    overflow: hidden;
+    ${blurFix}
+
+    ${({ theme, isAccountOwned }) =>
+        isAccountOwned &&
+        css`
+            color: ${theme.TYPE_DARK_GREY};
+        `}
+
+    :nth-child(3n + 1) {
+        /* address */
+        text-overflow: ellipsis;
+    }
+
+    :nth-child(3n + 2) {
+        /* extra */
+    }
+
+    :nth-child(3n + 3) {
+        /* amount */
+        text-align: right;
+    }
+`;
+
+type EnhancedVinVout = WalletAccountTransaction['details']['vin'][number];
+
+const IOGridRow = ({
+    tx: { symbol },
+    vinvout: { isAccountOwned, addresses, value },
+}: {
+    tx: WalletAccountTransaction;
+    vinvout: EnhancedVinVout;
+}) => (
+    <>
+        <IOGridCell isAccountOwned={isAccountOwned}>
+            <HiddenPlaceholder>{addresses}</HiddenPlaceholder>
+        </IOGridCell>
+        <IOGridCell isAccountOwned={isAccountOwned} />
+        <IOGridCell isAccountOwned={isAccountOwned}>
+            {value && (
+                <FormattedCryptoAmount value={formatNetworkAmount(value, symbol)} symbol={symbol} />
+            )}
+        </IOGridCell>
+    </>
+);
+
+type IOSectionProps = {
+    tx: WalletAccountTransaction;
+    inputs: EnhancedVinVout[];
+    outputs: EnhancedVinVout[];
+};
+
+const IOSection = ({ tx, inputs, outputs }: IOSectionProps) => {
+    const theme = useTheme();
+    const hasInputs = !!inputs?.length;
+    const hasOutputs = !!outputs?.length;
+    return (
+        <>
+            {hasInputs && (
+                <IOGridTitle>
+                    <Translation id="TR_INPUTS" />
+                </IOGridTitle>
+            )}
+            {hasInputs && (
+                <IOGrid>
+                    {inputs.map(input => (
+                        <IOGridRow key={input.n} tx={tx} vinvout={input} />
+                    ))}
+                </IOGrid>
+            )}
+            {hasInputs && hasOutputs && (
+                <IconWrapper>
+                    <Icon icon="ARROW_DOWN" size={17} color={theme.TYPE_LIGHT_GREY} />
+                </IconWrapper>
+            )}
+            {hasOutputs && (
+                <IOGridTitle>
+                    <Translation id="TR_OUTPUTS" />
+                </IOGridTitle>
+            )}
+            {hasOutputs && (
+                <IOGrid>
+                    {outputs.map(output => (
+                        <IOGridRow key={output.n} tx={tx} vinvout={output} />
+                    ))}
+                </IOGrid>
+            )}
+        </>
+    );
+};
 
 interface IODetailsProps {
     tx: WalletAccountTransaction;
 }
 
-export const IODetails = ({ tx }: IODetailsProps) => {
-    const theme = useTheme();
-    const network = getNetwork(tx.symbol);
-
-    return (
-        <Wrapper>
-            {tx.details.vin && tx.details.vout && (
-                <IOWrapper>
-                    <div>
-                        <IORowTitle>
-                            <Translation id="TR_INPUTS" />
-                        </IORowTitle>
-
-                        {tx.details.vin.map(input => (
-                            <IORow key={input.n} isAccountOwned={input.isAccountOwned}>
-                                {network?.networkType !== 'ethereum' && (
-                                    // don't show input amount in ethereum
-                                    // consider faking it by showing the same value os the output
-                                    <CryptoAmountWrapper>
-                                        <FormattedCryptoAmount
-                                            value={
-                                                input.value &&
-                                                formatNetworkAmount(input.value, tx.symbol)
-                                            }
-                                            symbol={tx.symbol}
-                                        />
-                                        <Circle>&bull;</Circle>
-                                    </CryptoAmountWrapper>
-                                )}
-                                <HiddenPlaceholder>{input.addresses}</HiddenPlaceholder>
-                            </IORow>
-                        ))}
-                    </div>
-
-                    <IconWrapper>
-                        <Icon icon="ARROW_DOWN" size={17} color={theme.TYPE_LIGHT_GREY} />
-                    </IconWrapper>
-
-                    <div>
-                        <IORowTitle>
-                            <Translation id="TR_OUTPUTS" />
-                        </IORowTitle>
-                        {tx.details.vout.map(output => (
-                            <IORow key={output.n} isAccountOwned={output.isAccountOwned}>
-                                <CryptoAmountWrapper>
-                                    <FormattedCryptoAmount
-                                        value={
-                                            output.value &&
-                                            formatNetworkAmount(output.value, tx.symbol)
-                                        }
-                                        symbol={tx.symbol}
-                                    />
-                                    <Circle>&bull;</Circle>
-                                </CryptoAmountWrapper>
-                                <HiddenPlaceholder>{output.addresses}</HiddenPlaceholder>
-                            </IORow>
-                        ))}
-                    </div>
-                </IOWrapper>
-            )}
-        </Wrapper>
-    );
-};
+export const IODetails = ({ tx }: IODetailsProps) => (
+    <Wrapper>
+        <IOSection tx={tx} inputs={tx.details.vin} outputs={tx.details.vout} />
+    </Wrapper>
+);
