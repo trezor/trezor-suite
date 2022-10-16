ff447f9de79f395cf14d8c907d4ee155201252b4
Szymon Lesisz <sz.lesisz@gmail.com>
2022-10-16 13:07:23 +0200

    ci: add coinjoin-backend docker service

---
+++ docker/docker-coinjoin-backend.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -e
+
+LOCAL_USER_ID="$(id -u "$USER")"
+export LOCAL_USER_ID
+
+docker-compose -f ./docker/docker-compose.coinjoin-backend.yml up --build --abort-on-container-exit --force-recreate
