b98b84ba691444e92b0496e8e6e42ccaa6f1473f
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2022-10-18 18:41:04 +0800

    fix(suite-common): Immutable array for sorting (#6583)

--- suite-common/wallet-utils/src/transactionUtils.ts
+++ suite-common/wallet-utils/src/transactionUtils.ts
@@ -46,26 +46,31 @@ export const groupTransactionsByDate = (
     transactions: WalletAccountTransaction[],
 ): { [key: string]: WalletAccountTransaction[] } => {
     const r: { [key: string]: WalletAccountTransaction[] } = {};
-    transactions.sort(sortByBlockHeight).forEach(item => {
-        let key = 'pending';
-        if (item.blockHeight && item.blockHeight > 0 && item.blockTime && item.blockTime > 0) {
-            const t = item.blockTime * 1000;
-            const d = new Date(t);
-            if (d) {
-                // YYYY-MM-DD format
-                key = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
-            } else {
-                // eslint-disable-next-line no-console
-                console.log(
-                    `Error during grouping transaction by date. Failed timestamp conversion (${t})`,
-                );
+    // Note: We should use ts-belt for sorting this array but currently, there can be undefined inside
+    // Built-in sort doesn't include undefined elements but ts-belt does so there will be some refactoring involved.
+    transactions
+        .slice()
+        .sort(sortByBlockHeight)
+        .forEach(item => {
+            let key = 'pending';
+            if (item.blockHeight && item.blockHeight > 0 && item.blockTime && item.blockTime > 0) {
+                const t = item.blockTime * 1000;
+                const d = new Date(t);
+                if (d) {
+                    // YYYY-MM-DD format
+                    key = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`;
+                } else {
+                    // eslint-disable-next-line no-console
+                    console.log(
+                        `Error during grouping transaction by date. Failed timestamp conversion (${t})`,
+                    );
+                }
             }
-        }
-        if (!r[key]) {
-            r[key] = [];
-        }
-        r[key].push(item);
-    });
+            if (!r[key]) {
+                r[key] = [];
+            }
+            r[key].push(item);
+        });
     return r;
 };
 
