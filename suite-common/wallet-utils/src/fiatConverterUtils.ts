0b72dd9484392b00ba2da84fcff7bb3a87368db0
juriczech <bohdan.juricek@gmail.com>
2022-10-20 11:14:40 +0800

    fix(suite-common): Revert refactor

--- suite-common/wallet-utils/src/fiatConverterUtils.ts
+++ suite-common/wallet-utils/src/fiatConverterUtils.ts
@@ -16,16 +16,21 @@ export const toFiatCurrency = (
         return null;
     }
 
-    const formattedAmount = amount.replace(',', '.');
-    const fiatAmount = new BigNumber(formattedAmount).times(rate);
-    if (fiatAmount.isNaN()) {
+    let formattedAmount = amount;
+    if (typeof amount === 'string') {
+        formattedAmount = amount.replace(',', '.');
+    }
+
+    const localAmount = new BigNumber(formattedAmount).times(rate);
+    if (localAmount.isNaN()) {
         return null;
     }
-    return decimals === -1 ? fiatAmount.toFixed() : fiatAmount.toFixed(decimals);
+
+    return decimals === -1 ? localAmount.toFixed() : localAmount.toFixed(decimals);
 };
 
 export const fromFiatCurrency = (
-    fiatAmount: string,
+    localAmount: string,
     fiatCurrency: string,
     networkRates: FiatRates | undefined,
     decimals: number,
@@ -35,7 +40,10 @@ export const fromFiatCurrency = (
         return null;
     }
 
-    const formattedLocalAmount = fiatAmount.replace(',', '.');
+    let formattedLocalAmount = localAmount;
+    if (typeof localAmount === 'string') {
+        formattedLocalAmount = localAmount.replace(',', '.');
+    }
 
     const amount = new BigNumber(formattedLocalAmount).div(rate);
     const amountStr = amount.isNaN() ? null : amount.toFixed(decimals);
