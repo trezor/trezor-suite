d313a2da2cd5ff7c7dbe243a2c6d75d67643efb7
dahaca <dahaca17@gmail.com>
2022-10-21 17:58:39 +0200

    feat(suite): add phase countdown to the critical phases modal

--- packages/suite/src/components/suite/CountdownTimer.tsx
+++ packages/suite/src/components/suite/CountdownTimer.tsx
@@ -4,23 +4,28 @@ import { formatTimeLeft } from '@suite-utils/formatTimeLeft';
 
 interface TimerProps {
     deadline: number | string;
+    format?: Array<keyof Duration>;
     className?: string;
 }
 
-export const CountdownTimer = ({ deadline, className }: TimerProps) => {
+export const CountdownTimer = ({
+    deadline,
+    format = ['hours', 'minutes'],
+    className,
+}: TimerProps) => {
     const locale = useLocales();
 
     const [timeLeftString, setTimeLeftString] = useState(
-        formatTimeLeft(new Date(deadline), locale),
+        formatTimeLeft(new Date(deadline), locale, format),
     );
 
     useEffect(() => {
         const interval = setInterval(() => {
-            setTimeLeftString(formatTimeLeft(new Date(deadline), locale));
+            setTimeLeftString(formatTimeLeft(new Date(deadline), locale, format));
         }, 100);
 
         return () => clearInterval(interval);
-    }, [deadline, locale]);
+    }, [deadline, locale, format]);
 
     return <span className={className}>{timeLeftString}</span>;
 };
