d30150138a6052c35d9152be5afdb149ed6ea7c1
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2022-10-21 14:36:22 +0200

    feat(suite): Add goerli coin to coinmarket

--- packages/suite/src/views/wallet/coinmarket/exchange/components/ExchangeForm/Inputs/SendCryptoInput/SendCryptoSelect/index.tsx
+++ packages/suite/src/views/wallet/coinmarket/exchange/components/ExchangeForm/Inputs/SendCryptoInput/SendCryptoSelect/index.tsx
@@ -48,7 +48,12 @@ const SendCryptoSelect = () => {
                         setValue(CRYPTO_INPUT, '');
                         setValue(FIAT_INPUT, '');
                         const token = selected.value;
-                        if (token === 'ETH' || token === 'TROP' || token === 'ETC') {
+                        if (
+                            token === 'ETH' ||
+                            token === 'TROP' ||
+                            token === 'TGOR' ||
+                            token === 'ETC'
+                        ) {
                             setValue(CRYPTO_TOKEN, undefined);
                             // set own account for non ERC20 transaction
                             setValue('outputs[0].address', account.descriptor);
