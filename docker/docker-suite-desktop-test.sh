76f44a241b3fa33b145c20205d662af1d311ab9f
mroz <mvarmuza@gmail.com>
2022-10-25 10:31:53 +0200

    test(suite-desktop): add option to run single test locally

--- docker/docker-suite-desktop-test.sh
+++ docker/docker-suite-desktop-test.sh
@@ -7,5 +7,6 @@ set -e
 xhost +
 LOCAL_USER_ID="$(id -u "$USER")"
 export LOCAL_USER_ID
+export TEST_FILE=$1
 
 docker-compose -f ./docker/docker-compose.suite-desktop-test.yml up --build --abort-on-container-exit --force-recreate
