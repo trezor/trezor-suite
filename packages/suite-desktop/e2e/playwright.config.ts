39ed0ec2ba1e52b59e7599a387c02510894351ef
mroz <mvarmuza@gmail.com>
2022-10-25 21:56:23 +0200

    test(suite-deskto): fine-tune tor test

--- packages/suite-desktop/e2e/playwright.config.ts
+++ packages/suite-desktop/e2e/playwright.config.ts
@@ -2,7 +2,6 @@ import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
     testDir: 'tests',
-    timeout: 360000, // tests can take long, especially due to Tor
     workers: 1, // to disable parallelism between test files
     use: {
         headless: true,
