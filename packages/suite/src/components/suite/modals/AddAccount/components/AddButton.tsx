745c4f7cb816624c16915a5f69cef9c5c9d5ed0b
Matej Kriz <matej@matejkriz.cz>
2022-10-25 17:06:18 +0200

    feat(coinjoin): add loading state on Add CoinJoin account button

--- packages/suite/src/components/suite/modals/AddAccount/components/AddButton.tsx
+++ packages/suite/src/components/suite/modals/AddAccount/components/AddButton.tsx
@@ -1,18 +1,19 @@
 import React from 'react';
-import { TooltipButton } from '@trezor/components';
+import { TooltipButton, ButtonProps } from '@trezor/components';
 import { Translation } from '@suite-components';
 
-interface ButtonProps {
+interface AddButtonProps extends ButtonProps {
     disabledMessage: React.ReactNode;
     handleClick: () => void;
 }
 
-export const AddButton = ({ disabledMessage, handleClick }: ButtonProps) => (
+export const AddButton = ({ disabledMessage, handleClick, ...buttonProps }: AddButtonProps) => (
     <TooltipButton
         tooltipContent={disabledMessage}
         isDisabled={!!disabledMessage}
         onClick={handleClick}
         data-test="@add-account"
+        {...buttonProps}
     >
         <Translation id="TR_ADD_ACCOUNT" />
     </TooltipButton>
