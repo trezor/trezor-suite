aca9690e76051a4313c0d2f31168d0ff849c0e79
juriczech <bohdan.juricek@gmail.com>
2022-10-26 19:29:52 +0800

    chore(suite-native): Remove settings sections

--- suite-native/module-settings/src/index.ts
+++ suite-native/module-settings/src/index.ts
@@ -2,3 +2,5 @@ export * from './navigation/SettingsStackNavigator';
 export * from './slice';
 export * from './types';
 export * from './components/ColorSchemePicker';
+export * from './components/CoinsSettings';
+export * from './components/DeviceActionButtons';
