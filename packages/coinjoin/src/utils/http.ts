b779440ca8b56d1499e9e83ea48588b643134fe9
Marek Polak <marek.polak@satoshilabs.com>
2022-11-01 09:29:23 +0100

    feat(coinjoin): more precise http request control

--- packages/coinjoin/src/utils/http.ts
+++ packages/coinjoin/src/utils/http.ts
@@ -1,5 +1,7 @@
 import fetch from 'cross-fetch';
 
+import { throwError } from '@trezor/utils';
+
 export interface RequestOptions {
     method?: 'POST' | 'GET';
     baseUrl?: string;
@@ -15,25 +17,59 @@ const parseResult = (text: string, json = true) => {
     return JSON.parse(text);
 };
 
-const requestDelay = async (options: RequestOptions) => {
-    const delay = options.delay || 0;
-    if (delay > 0) {
+type ControlRequestParams = {
+    delay?: number; // How many ms wait before calling action (default = none)
+    timeout?: number; // How many ms wait for a single action attempt (default = indefinitely)
+    deadline?: number; // How many ms wait for all action attempts (default = indefinitely)
+    attempts?: number; // How many attempts before failure (default = one, or infinite when deadline is set)
+    signal?: AbortSignal;
+};
+
+const controlRequest = async <T>(
+    request: (signal?: AbortSignal) => Promise<T>,
+    { delay, timeout, deadline, attempts, signal }: ControlRequestParams,
+) => {
+    if (delay) {
         await new Promise<void>((resolve, reject) => {
-            let timeout: ReturnType<typeof setTimeout> | undefined;
-            const abortHandler = () => {
-                if (timeout) {
-                    clearTimeout(timeout);
-                    reject(new Error('The user aborted a request.'));
-                }
+            let delayTimeout: ReturnType<typeof setTimeout>;
+            const abortDelay = () => {
+                clearTimeout(delayTimeout);
+                reject(new Error('The user aborted a request.'));
             };
-            options.signal?.addEventListener('abort', abortHandler);
-            timeout = setTimeout(() => {
-                timeout = undefined;
-                options.signal?.removeEventListener('abort', abortHandler);
+            signal?.addEventListener('abort', abortDelay);
+            delayTimeout = setTimeout(() => {
+                signal?.removeEventListener('abort', abortDelay);
                 resolve();
             }, delay);
         });
     }
+
+    const deadlineAborter = new AbortController();
+    const abortDeadline = () => deadlineAborter.abort();
+    if (signal?.aborted) abortDeadline();
+    const deadlineTimeout = deadline ? setTimeout(abortDeadline, deadline) : undefined;
+    signal?.addEventListener('abort', abortDeadline);
+
+    const attempt = () => {
+        const attemptAborter = new AbortController();
+        const abortAttempt = () => attemptAborter.abort();
+        const attemptTimeout = timeout ? setTimeout(abortAttempt, timeout) : undefined;
+        deadlineAborter.signal.addEventListener('abort', abortAttempt);
+        return request(attemptAborter.signal).finally(() => {
+            deadlineAborter.signal.removeEventListener('abort', abortAttempt);
+            clearTimeout(attemptTimeout);
+        });
+    };
+
+    const attemptsRecursive = (attemptsLeft: number): ReturnType<typeof attempt> =>
+        attemptsLeft && !deadlineAborter.signal.aborted
+            ? attempt().catch(() => attemptsRecursive(attemptsLeft - 1))
+            : throwError('The user aborted a request');
+
+    return attemptsRecursive(attempts ?? (deadline ? Infinity : 1)).finally(() => {
+        signal?.removeEventListener('abort', abortDeadline);
+        clearTimeout(deadlineTimeout);
+    });
 };
 
 const createHeaders = (options: RequestOptions) => {
@@ -54,13 +90,8 @@ const createHeaders = (options: RequestOptions) => {
     return headers;
 };
 
-export const httpGet = async (
-    url: string,
-    query?: Record<string, any>,
-    options: RequestOptions = {},
-) => {
+export const httpGet = (url: string, query?: Record<string, any>, options: RequestOptions = {}) => {
     const queryString = query ? `?${new URLSearchParams(query)}` : '';
-    await requestDelay(options);
     return fetch(`${url}${queryString}`, {
         method: 'GET',
         signal: options.signal,
@@ -68,19 +99,13 @@ export const httpGet = async (
     });
 };
 
-export const httpPost = async (
-    url: string,
-    body?: Record<string, any>,
-    options: RequestOptions = {},
-) => {
-    await requestDelay(options);
-    return fetch(url, {
+export const httpPost = (url: string, body?: Record<string, any>, options: RequestOptions = {}) =>
+    fetch(url, {
         method: 'POST',
         body: JSON.stringify(body),
         signal: options.signal,
         headers: createHeaders(options),
     });
-};
 
 // Requests to wasabi coordinator and middleware (CoinjoinClientLibrary bin)
 export const coordinatorRequest = async <R = void>(
@@ -90,9 +115,14 @@ export const coordinatorRequest = async <R = void>(
 ): Promise<R> => {
     const baseUrl = options.baseUrl || '';
 
-    const response = await httpPost(`${baseUrl}${url}`, body, options);
+    const request = async (signal?: AbortSignal) => {
+        const response = await httpPost(`${baseUrl}${url}`, body, { ...options, signal });
+        const text = await response.text();
+        return { response, text };
+    };
 
-    const text = await response.text();
+    const { response, text } = await controlRequest(request, options);
+
     if (response.ok) {
         const json = typeof options.parseJson === 'boolean' ? options.parseJson : true;
         return parseResult(text, json);
