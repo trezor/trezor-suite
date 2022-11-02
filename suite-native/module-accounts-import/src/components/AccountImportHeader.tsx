f2ffe33d48dc2dfb99bebe9a8822e67c57f7e75a
juriczech <bohdan.juricek@gmail.com>
2022-11-02 17:46:19 +0800

    feat(suite-native): Add option to exit onboarding if user already finished it before

--- suite-native/module-accounts-import/src/components/AccountImportHeader.tsx
+++ suite-native/module-accounts-import/src/components/AccountImportHeader.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-import { Box, StepsProgressBar } from '@suite-native/atoms';
+import { useNavigation } from '@react-navigation/core';
+
+import { Box, IconButton, StepsProgressBar } from '@suite-native/atoms';
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
+import { selectIsOnboardingFinished } from '@suite-native/module-settings';
+import {
+    AccountsImportStackParamList,
+    AccountsImportStackRoutes,
+    AppTabsRoutes,
+    HomeStackRoutes,
+    RootStackParamList,
+    RootStackRoutes,
+    StackToTabCompositeProps,
+} from '@suite-native/navigation';
 
 type AccountImportHeaderProps = {
     activeStep: 1 | 2 | 3;
@@ -10,19 +23,51 @@ const accountImportHeaderStyle = prepareNativeStyle(_ => ({
     width: '100%',
 }));
 
+const closeButtonSizeStyle = prepareNativeStyle(_ => ({
+    width: 48,
+    height: 48,
+}));
+
+type NavigationProp = StackToTabCompositeProps<
+    AccountsImportStackParamList,
+    AccountsImportStackRoutes.AccountImport,
+    RootStackParamList
+>;
+
 export const AccountImportHeader = ({ activeStep }: AccountImportHeaderProps) => {
     const { applyStyle } = useNativeStyles();
+    const navigation = useNavigation<NavigationProp>();
+    const isOnboardingFinished = useSelector(selectIsOnboardingFinished);
+
+    const handleCloseOnboarding = () =>
+        navigation.navigate(RootStackRoutes.AppTabs, {
+            screen: AppTabsRoutes.HomeStack,
+            params: {
+                screen: HomeStackRoutes.Home,
+            },
+        });
+
     return (
         <Box
             flexDirection="row"
-            justifyContent="center"
+            justifyContent={isOnboardingFinished ? 'space-between' : 'center'}
             alignItems="center"
             marginBottom="small"
             style={applyStyle(accountImportHeaderStyle)}
         >
-            <Box>
-                <StepsProgressBar activeStep={activeStep} numberOfSteps={3} />
-            </Box>
+            {isOnboardingFinished && <Box style={applyStyle(closeButtonSizeStyle)} />}
+            <StepsProgressBar activeStep={activeStep} numberOfSteps={3} />
+            {isOnboardingFinished && (
+                <Box style={applyStyle(closeButtonSizeStyle)}>
+                    <IconButton
+                        iconName="close"
+                        colorScheme="gray"
+                        onPress={handleCloseOnboarding}
+                        size="large"
+                        isRounded
+                    />
+                </Box>
+            )}
         </Box>
     );
 };
