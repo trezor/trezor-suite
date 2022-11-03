eb7185f4c460ad056586ebdd3046f301cd74c3b8
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2022-11-03 17:58:31 +0800

    feat(suite-native): Add option to exit onboarding if user already fin… (#6755)
    * feat(suite-native): Add option to exit onboarding if user already finished it before
    
    * chore(suite-native): Improve styling issues

--- suite-native/module-accounts-import/src/components/AccountImportHeader.tsx
+++ suite-native/module-accounts-import/src/components/AccountImportHeader.tsx
@@ -1,28 +1,74 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-import { Box, StepsProgressBar } from '@suite-native/atoms';
+import { useNavigation } from '@react-navigation/core';
+
+import { Box, IconButton, StepsProgressBar } from '@suite-native/atoms';
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
+import { selectIsOnboardingFinished } from '@suite-native/module-settings';
+import {
+    AccountsImportStackParamList,
+    AccountsImportStackRoutes,
+    AppTabsRoutes,
+    HomeStackRoutes,
+    RootStackParamList,
+    RootStackRoutes,
+    StackToTabCompositeProps,
+} from '@suite-native/navigation';
 
 type AccountImportHeaderProps = {
     activeStep: 1 | 2 | 3;
 };
+
+const CLOSE_BUTTON_SIZE = 48;
+
 const accountImportHeaderStyle = prepareNativeStyle(_ => ({
+    position: 'relative',
     width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: CLOSE_BUTTON_SIZE,
 }));
+
+const closeButtonStyle = prepareNativeStyle(_ => ({
+    position: 'absolute',
+    right: 0,
+}));
+
+type NavigationProp = StackToTabCompositeProps<
+    AccountsImportStackParamList,
+    AccountsImportStackRoutes.AccountImport,
+    RootStackParamList
+>;
 
 export const AccountImportHeader = ({ activeStep }: AccountImportHeaderProps) => {
     const { applyStyle } = useNativeStyles();
+    const navigation = useNavigation<NavigationProp>();
+    const isOnboardingFinished = useSelector(selectIsOnboardingFinished);
+
+    const handleCloseOnboarding = () =>
+        navigation.navigate(RootStackRoutes.AppTabs, {
+            screen: AppTabsRoutes.HomeStack,
+            params: {
+                screen: HomeStackRoutes.Home,
+            },
+        });
+
     return (
-        <Box
-            flexDirection="row"
-            justifyContent="center"
-            alignItems="center"
-            marginBottom="small"
-            style={applyStyle(accountImportHeaderStyle)}
-        >
-            <Box>
-                <StepsProgressBar activeStep={activeStep} numberOfSteps={3} />
-            </Box>
+        <Box style={applyStyle(accountImportHeaderStyle)}>
+            <StepsProgressBar activeStep={activeStep} numberOfSteps={3} />
+            {isOnboardingFinished && (
+                <Box style={applyStyle(closeButtonStyle)}>
+                    <IconButton
+                        iconName="close"
+                        colorScheme="gray"
+                        onPress={handleCloseOnboarding}
+                        size="large"
+                        isRounded
+                    />
+                </Box>
+            )}
         </Box>
     );
 };
