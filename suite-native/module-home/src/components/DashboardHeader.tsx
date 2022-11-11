e9467b602ab79ac9b23e48ff54eb8d46e7c05210
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2022-11-11 18:51:56 +0800

    chore(suite-native): Remove icons (#6882)

--- suite-native/module-home/src/components/DashboardHeader.tsx
+++ suite-native/module-home/src/components/DashboardHeader.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
-import { Icon } from '@trezor/icons';
 import { Box, Text } from '@suite-native/atoms';
 
 const headerStyle = prepareNativeStyle(() => ({
@@ -12,21 +11,10 @@ const headerStyle = prepareNativeStyle(() => ({
     alignItems: 'center',
 }));
 
-const controlButtonsStyle = prepareNativeStyle(() => ({
-    position: 'absolute',
-    left: 0,
-}));
-
 export const DashboardHeader = () => {
     const { applyStyle } = useNativeStyles();
     return (
         <Box style={applyStyle(headerStyle)}>
-            <Box flexDirection="row" style={applyStyle(controlButtonsStyle)}>
-                <Box marginRight="medium">
-                    <Icon name="eyeSlash" color="gray600" />
-                </Box>
-                <Icon name="notifications" color="gray600" />
-            </Box>
             <Text>Home</Text>
         </Box>
     );
