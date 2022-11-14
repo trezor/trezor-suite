8ddf0a70855adfd6dcfa4b0509296669f9559023
Szymon Lesisz <sz.lesisz@gmail.com>
2022-11-14 18:06:20 +0100

    fix(connect): setBusy reload features

--- packages/connect/e2e/tests/device/setBusy.test.ts
+++ packages/connect/e2e/tests/device/setBusy.test.ts
@@ -8,6 +8,10 @@ describe('TrezorConnect.setBusy', () => {
     beforeAll(async () => {
         await setup(controller, {
             mnemonic: 'mnemonic_all',
+            pin: '1234',
+            settings: {
+                auto_lock_delay_ms: 10000,
+            },
         });
         await initTrezorConnect(controller);
     });
@@ -18,22 +22,24 @@ describe('TrezorConnect.setBusy', () => {
     });
 
     conditionalTest(['1', '<2.5.3'], 'setBusy timeout', async () => {
-        const busy = await TrezorConnect.setBusy({
-            expiry_ms: 5000,
+        let busy = false;
+        TrezorConnect.on('DEVICE_EVENT', event => {
+            if ('features' in event.payload) {
+                busy = event.payload.features!.busy!;
+            }
         });
-        if (!busy.success) throw new Error(busy.payload.error);
+        const setBusy = await TrezorConnect.setBusy({
+            expiry_ms: 3000,
+            keepSession: true, // keep session
+        });
+        if (!setBusy.success) throw new Error(setBusy.payload.error);
 
-        let features: Awaited<ReturnType<typeof TrezorConnect.getFeatures>>;
-
-        features = await TrezorConnect.getFeatures();
-        if (!features.success) throw new Error(features.payload.error);
-        // is busy
-        expect(features.payload.busy).toBe(true);
+        expect(busy).toBe(true);
 
         // wait for expiry
-        await new Promise(resolve => setTimeout(resolve, 5000));
+        await new Promise(resolve => setTimeout(resolve, 3000));
 
-        features = await TrezorConnect.getFeatures();
+        const features = await TrezorConnect.getFeatures();
         if (!features.success) throw new Error(features.payload.error);
         expect(features.payload.busy).toBe(false);
     });
@@ -57,5 +63,31 @@ describe('TrezorConnect.setBusy', () => {
         if (!features.success) throw new Error(features.payload.error);
         // not busy
         expect(features.payload.busy).toBe(false);
+    });
+
+    conditionalTest(['1', '<2.5.3'], 'setBusy with autolock', async () => {
+        await new Promise(resolve => setTimeout(resolve, 11000)); // wait for auto-lock
+
+        let busy = false;
+        TrezorConnect.on('DEVICE_EVENT', event => {
+            if ('features' in event.payload) {
+                busy = event.payload.features!.busy!;
+            }
+        });
+
+        const setBusy = await TrezorConnect.setBusy({
+            expiry_ms: 15000,
+            keepSession: true, // keep session
+        });
+        if (!setBusy.success) throw new Error(setBusy.payload.error);
+
+        expect(busy).toBe(true);
+
+        await new Promise(resolve => setTimeout(resolve, 1000)); // cool off
+
+        // reset expiry
+        await TrezorConnect.setBusy({});
+
+        expect(busy).toBe(false);
     });
 });
--- packages/connect/src/api/setBusy.ts
+++ packages/connect/src/api/setBusy.ts
@@ -1,4 +1,5 @@
 import { AbstractMethod } from '../core/AbstractMethod';
+import { DEVICE, createDeviceMessage } from '../events';
 import { PROTO } from '../constants';
 import { getFirmwareRange, validateParams } from './common/paramsValidator';
 
@@ -6,7 +7,6 @@ export default class SetBusy extends AbstractMethod<'setBusy', PROTO.SetBusy> {
     init() {
         this.useDeviceState = false;
         this.requiredPermissions = ['management'];
-        this.keepSession = true; // TODO: device-changed will not be emitted. followup: https://github.com/trezor/trezor-suite/issues/6446
 
         const { payload } = this;
 
@@ -25,6 +25,14 @@ export default class SetBusy extends AbstractMethod<'setBusy', PROTO.SetBusy> {
     async run() {
         const cmd = this.device.getCommands();
         const { message } = await cmd.typedCall('SetBusy', 'Success', this.params);
+        if (this.keepSession && !!this.params.expiry_ms) {
+            // NOTE: DEVICE.CHANGED will not be emitted because session is not released
+            // change device features and trigger event manually
+            // followup: https://github.com/trezor/trezor-suite/issues/6446
+            this.device.features.busy = true;
+            this.postMessage(createDeviceMessage(DEVICE.CHANGED, this.device.toMessageObject()));
+        }
+
         return message;
     }
 }
