7857d94526d08f4fb1b75031b2bc7a5744c2e511
juriczech <bohdan.juricek@gmail.com>
2022-11-22 08:43:34 +0100

    feat(suite-native): Add option to add more xpubs

--- suite-native/module-accounts-import/src/components/XpubScanHeader.tsx
+++ suite-native/module-accounts-import/src/components/XpubScanHeader.tsx
@@ -1,63 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
 
-import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
-import { Box, Chip, Text } from '@suite-native/atoms';
-import { CryptoIcon } from '@trezor/icons';
+import { Box, Select, Text } from '@suite-native/atoms';
 import { networks, NetworkSymbol } from '@suite-common/wallet-config';
-import { enabledNetworks } from '@suite-native/config';
+import { CryptoIconName } from '@trezor/icons';
 
 type XpubScanHeaderProps = {
     onSelectCurrency: (currencySymbol: NetworkSymbol) => void;
     selectedCurrencySymbol: NetworkSymbol;
 };
 
-const chipsWrapperStyle = prepareNativeStyle(utils => ({
-    flexDirection: 'row',
-    borderRadius: utils.borders.radii.large,
-    backgroundColor: utils.colors.gray200,
-}));
-
-const chipStyle = prepareNativeStyle<{ isSelected: boolean }>((utils, { isSelected }) => ({
-    ...utils.typography.label,
-    flex: 1,
-    backgroundColor: 'transparent',
-    borderColor: 'transparent',
-    borderRadius: utils.borders.radii.large,
-    extend: {
-        condition: isSelected,
-        style: {
-            backgroundColor: utils.colors.gray0,
-        },
-    },
-}));
-
 export const XpubScanHeader = ({
     onSelectCurrency,
     selectedCurrencySymbol,
 }: XpubScanHeaderProps) => {
-    const { applyStyle } = useNativeStyles();
+    const selectNetworkItems = Object.keys(networks).map(networkSymbol => ({
+        value: networkSymbol as NetworkSymbol,
+        label: networks[networkSymbol as NetworkSymbol].name,
+        iconName: networkSymbol as CryptoIconName,
+    }));
+
     return (
         <Box alignItems="center" marginBottom="large">
             <Box marginBottom="medium">
                 <Text variant="titleMedium">XPUB Import</Text>
             </Box>
-            <View style={applyStyle(chipsWrapperStyle)}>
-                {/* TODO enable more coins */}
-                {enabledNetworks.map(network => (
-                    <Chip
-                        key={network}
-                        icon={<CryptoIcon name={network} />}
-                        title={networks[network].name}
-                        titleColor="gray600"
-                        onSelect={() => onSelectCurrency(network)}
-                        style={applyStyle(chipStyle, {
-                            isSelected: selectedCurrencySymbol === network,
-                        })}
-                        isSelected={selectedCurrencySymbol === network}
-                    />
-                ))}
-            </View>
+            <Box style={{ width: '100%' }}>
+                <Select<NetworkSymbol>
+                    selectLabel="Select coin"
+                    onSelectItem={onSelectCurrency}
+                    selectValue={selectedCurrencySymbol}
+                    items={selectNetworkItems}
+                />
+            </Box>
         </Box>
     );
 };
--- suite-native/module-accounts-import/src/screens/XpubScanScreen.tsx
+++ suite-native/module-accounts-import/src/screens/XpubScanScreen.tsx
@@ -24,6 +24,7 @@ import { AccountImportHeader } from '../components/AccountImportHeader';
 const devXpubs: Partial<Record<NetworkSymbol, string>> & Record<'btc' | 'test', string> = {
     btc: 'zpub6rjNNddoAVvuYaD6WPdxiqFEToQHgrERjWMg7kM9gGGk6rhPMWNEmL5X745FGqBq8Wp136LfA3A7UjRGEYdJrf8dUfshzNrb5rvaryNfVJf',
     test: 'vpub5ZjRPuuMiEQnbwEDi9jtH1FaJMajZW78uZ1t3RJXKhxyMoTnPraKwGxiDo9SguDYvSieqjoLJxW5n2t9156RR1oeqRnURuftNZTzejBc4pa',
+    doge: 'dgub8sbe5Mi8LA4dXB9zPfLZW8armvaRkaqT37y6xfT1QA12ErATDf8iGEG8y7kamAnjLuEk9AMpTMMp6EK9Vjp2HHx91xdDEmWYpmD49fpoUYF',
 };
 
 const cameraStyle = prepareNativeStyle(_ => ({
@@ -37,7 +38,7 @@ const devXpubButtonStyle = prepareNativeStyle(utils => ({
     borderRadius: utils.borders.radii.round,
 }));
 
-const DEFAULT_CURRENCY_SYMBOL = 'btc';
+const DEFAULT_CURRENCY_SYMBOL = 'doge';
 
 const xpubFormValidationSchema = yup.object({
     xpubAddress: yup.string().required(),
@@ -92,6 +93,7 @@ export const XpubScanScreen = ({
     }, [handleXpubResult, route.params]);
 
     const handleSelectCurrency = (currencySymbol: NetworkSymbol) => {
+        console.log(currencySymbol, 'handle select');
         setSelectedCurrencySymbol(currencySymbol);
     };
 
