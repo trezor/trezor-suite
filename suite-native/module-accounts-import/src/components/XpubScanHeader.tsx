7ce62a4336c9877f141d84c4293eaa6e0b469f44
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2022-11-23 14:00:41 +0100

    feat(suite-native): Add option to add more xpubs (#6970)
    * feat(suite-native): Add option to add more xpubs
    
    * fix(suite-native): Logs and fixes
    
    * fix(suite-native): Return btc as default currency
    
    * chore(suite-native): Use navigate instead of goBack
    
    * chore(suite-native): Remove reset
    
    * chore(suite-native): Fix casting
    
    * fix(suite-native): Enabled networks used
    
    * fixup! fix(suite-native): Enabled networks used
    
    * feat(suite-native): Add option to add more xpubs
    
    * fix(suite-native): Logs and fixes
    
    * fix(suite-native): Return btc as default currency
    
    * chore(suite-native): Remove reset
    
    * chore(suite-native): Fix casting
    
    * fix(suite-native): Enabled networks used
    
    * fixup! fix(suite-native): Enabled networks used

--- suite-native/module-accounts-import/src/components/XpubScanHeader.tsx
+++ suite-native/module-accounts-import/src/components/XpubScanHeader.tsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import { View } from 'react-native';
 
-import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
-import { Box, Chip, Text } from '@suite-native/atoms';
-import { CryptoIcon } from '@trezor/icons';
+import { Box, Select, Text } from '@suite-native/atoms';
 import { networks, NetworkSymbol } from '@suite-common/wallet-config';
+import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
 import { enabledNetworks } from '@suite-native/config';
 
 type XpubScanHeaderProps = {
@@ -12,24 +10,8 @@ type XpubScanHeaderProps = {
     selectedCurrencySymbol: NetworkSymbol;
 };
 
-const chipsWrapperStyle = prepareNativeStyle(utils => ({
-    flexDirection: 'row',
-    borderRadius: utils.borders.radii.large,
-    backgroundColor: utils.colors.gray200,
-}));
-
-const chipStyle = prepareNativeStyle<{ isSelected: boolean }>((utils, { isSelected }) => ({
-    ...utils.typography.label,
-    flex: 1,
-    backgroundColor: 'transparent',
-    borderColor: 'transparent',
-    borderRadius: utils.borders.radii.large,
-    extend: {
-        condition: isSelected,
-        style: {
-            backgroundColor: utils.colors.gray0,
-        },
-    },
+const selectWrapper = prepareNativeStyle(() => ({
+    width: '100%',
 }));
 
 export const XpubScanHeader = ({
@@ -37,27 +19,26 @@ export const XpubScanHeader = ({
     selectedCurrencySymbol,
 }: XpubScanHeaderProps) => {
     const { applyStyle } = useNativeStyles();
+
+    const selectNetworkItems = enabledNetworks.map(networkSymbol => ({
+        value: networkSymbol,
+        label: networks[networkSymbol].name,
+        iconName: networkSymbol,
+    }));
+
     return (
         <Box alignItems="center" marginBottom="large">
             <Box marginBottom="medium">
                 <Text variant="titleMedium">XPUB Import</Text>
             </Box>
-            <View style={applyStyle(chipsWrapperStyle)}>
-                {/* TODO enable more coins */}
-                {enabledNetworks.map(network => (
-                    <Chip
-                        key={network}
-                        icon={<CryptoIcon name={network} />}
-                        title={networks[network].name}
-                        titleColor="gray600"
-                        onSelect={() => onSelectCurrency(network)}
-                        style={applyStyle(chipStyle, {
-                            isSelected: selectedCurrencySymbol === network,
-                        })}
-                        isSelected={selectedCurrencySymbol === network}
-                    />
-                ))}
-            </View>
+            <Box style={applyStyle(selectWrapper)}>
+                <Select<NetworkSymbol>
+                    selectLabel="Select coin"
+                    onSelectItem={onSelectCurrency}
+                    selectValue={selectedCurrencySymbol}
+                    items={selectNetworkItems}
+                />
+            </Box>
         </Box>
     );
 };
