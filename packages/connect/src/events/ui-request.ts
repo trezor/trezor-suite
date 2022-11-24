8ebc38e20c4d1a071d29271ed72e7fa6e961ff92
mroz <mvarmuza@gmail.com>
2022-11-24 16:17:07 +0100

    tmp: remove analytics from connect core

--- packages/connect/src/events/ui-request.ts
+++ packages/connect/src/events/ui-request.ts
@@ -1,7 +1,7 @@
 /*
  * messages to UI emitted as UI_EVENT
  */
-import type { EventTypeDeviceSelected } from '@trezor/connect-analytics';
+// import type { EventTypeDeviceSelected } from '@trezor/connect-analytics';
 
 import type { PROTO } from '../constants';
 import type { TransportDisableWebUSB } from './transport';
@@ -173,7 +173,8 @@ export interface UiRequestConfirmation {
             className: string;
             label: string;
         };
-        analytics?: EventTypeDeviceSelected;
+        // analytics?: EventTypeDeviceSelected;
+        analytics?: any;
     };
 }
 
