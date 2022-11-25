b6258eabce6bae7963481164acc14fb79c007667
Jan Komarek <jan.komarek@satoshilabs.com>
2022-11-25 12:24:48 +0100

    e2e(suite-web): test doge account spending UTXO with amount above MAX_SAFE_INTEGER

--- packages/suite-web/e2e/tests/wallet/send-form-doge.test.ts
+++ packages/suite-web/e2e/tests/wallet/send-form-doge.test.ts
@@ -6,7 +6,8 @@ describe('Doge send form with mocked blockbook', () => {
         cy.task('startEmu', { wipe: true });
         cy.task('setupEmu', {
             needs_backup: false,
-            mnemonic: 'access juice claim special truth ugly swarm rabbit hair man error bar',
+            mnemonic:
+                'fantasy auto fancy access ring spring patrol expect common tape talent annual',
         });
         cy.task('startBridge');
 
@@ -20,47 +21,44 @@ describe('Doge send form with mocked blockbook', () => {
         cy.task('stopBlockbookMock');
     });
 
-    // todo: unskip
-    it.skip('spend an output with amount over MAX_SAFE_INTEGER', () => {
+    it('spend an output with amount over MAX_SAFE_INTEGER', () => {
         //
         // Test preparation
         //
 
-        // todo: pass fixtures file for doe
-        cy.task('startBlockbookMock', { endpointsFile: 'send-form-ltc-mimble-wimble' }).then(
-            port => {
-                const customBlockbook = `ws://localhost:${port}`;
-                cy.log(customBlockbook);
+        cy.task('startBlockbookMock', { endpointsFile: 'send-form-doge' }).then(port => {
+            const customBlockbook = `ws://localhost:${port}`;
+            cy.log(customBlockbook);
 
-                //
-                // Test execution
-                //
-                cy.getTestElement('@settings/wallet/network/btc').click();
-                cy.getTestElement('@settings/wallet/network/doge', { timeout: 30000 })
-                    .should('exist')
-                    .click()
-                    .trigger('mouseover');
-                cy.getTestElement('@settings/wallet/network/doge/advance').click();
-                cy.getTestElement('@modal').should('exist');
-                cy.getTestElement('@settings/advance/select-type/input').click();
-                cy.getTestElement('@settings/advance/select-type/option/blockbook').click();
-                cy.getTestElement('@settings/advance/url').type(customBlockbook);
-                cy.getTestElement('@settings/advance/button/save').click();
+            //
+            // Test execution
+            //
+            cy.getTestElement('@settings/wallet/network/btc').click();
+            cy.getTestElement('@settings/wallet/network/doge', { timeout: 30000 })
+                .should('exist')
+                .click()
+                .trigger('mouseover');
+            cy.getTestElement('@settings/wallet/network/doge/advance').click();
+            cy.getTestElement('@modal').should('exist');
+            cy.getTestElement('@settings/advance/select-type/input').click();
+            cy.getTestElement('@settings/advance/select-type/option/blockbook').click();
+            cy.getTestElement('@settings/advance/url').type(customBlockbook);
+            cy.getTestElement('@settings/advance/button/save').click();
 
-                cy.prefixedVisit('/accounts/send#/doge/0');
-                cy.discoveryShouldFinish();
+            cy.prefixedVisit('/accounts/send#/doge/0');
+            cy.discoveryShouldFinish();
 
-                cy.getTestElement('broadcast-button').click();
+            cy.getTestElement('broadcast-button').click();
 
-                // cy.getTestElement('outputs[0].address').type(
-                //     'ltc1q0lqwsyygg9frql6ujjfhevfculsxwledvv6yzc',
-                // );
-                // cy.getTestElement('outputs[0].setMax').click({ force: true });
-
-                // cy.pause();
-                // TDD TODO: click on review button, now it fails "slice out of bounds "
-            },
-        );
+            cy.getTestElement('outputs[0].address').type('DJk8vtoEuNGtT4YRNoqVxWyRh6kM3s8bzc');
+            cy.getTestElement('outputs[0].amount').type('115568568500');
+            cy.getTestElement('@send/review-button').click();
+            cy.getTestElement('@prompts/confirm-on-device');
+            cy.task('pressYes');
+            cy.task('pressYes');
+            cy.task('pressYes');
+            cy.getTestElement('@send/copy-raw-transaction');
+        });
     });
 });
 
