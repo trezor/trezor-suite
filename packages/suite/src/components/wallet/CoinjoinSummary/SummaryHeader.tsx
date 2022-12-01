037d62c253433d90b58d1892a1bc4587a41e87b2
dahaca <dahaca17@gmail.com>
2022-12-01 17:21:58 +0100

    feat(suite): remove the anonymity level indicator

--- packages/suite/src/components/wallet/CoinjoinSummary/SummaryHeader.tsx
+++ packages/suite/src/components/wallet/CoinjoinSummary/SummaryHeader.tsx
@@ -2,21 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import { H3 } from '@trezor/components';
 import { Translation } from '@suite-components/Translation';
-import { AnonymityLevelSetup } from '@wallet-components/PrivacyAccount/AnonymityLevelSetup';
 
-const Row = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+const Heading = styled(H3)`
     margin-bottom: 28px;
 `;
 
 export const SummaryHeader = () => (
-    <Row>
-        <H3>
-            <Translation id="TR_MY_COINS" />
-        </H3>
-
-        <AnonymityLevelSetup />
-    </Row>
+    <Heading>
+        <Translation id="TR_MY_COINS" />
+    </Heading>
 );
