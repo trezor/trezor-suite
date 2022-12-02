6724c004bbe57c33ac5ea115e77ac8793c60ec8a
bouchja1 <jan.bouchner@gmail.com>
2022-12-02 13:09:11 +0100

    feat(react-native): Remove console.log from production

--- suite-native/app/babel.config.js
+++ suite-native/app/babel.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+    env: {
+        production: {
+            plugins: ['transform-remove-console'],
+        },
+    },
     presets: ['module:metro-react-native-babel-preset'],
     plugins: [['react-native-reanimated/plugin', { globals: ['__scanCodes'] }]],
 };
