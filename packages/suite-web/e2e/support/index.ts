90d0cef8165b51851cf4d6543836069fd9be56d6
jakub.ondracek <jakub.ondracek@satoshilabs.com>
2022-12-05 15:09:24 +0100

    test(e2e): hiding xhr requests

--- packages/suite-web/e2e/support/index.ts
+++ packages/suite-web/e2e/support/index.ts
@@ -1,2 +1,11 @@
 import './commands';
 import 'cypress-file-upload';
+
+// Hide fetch/XHR requests
+const app: any = window.top;
+if (!app.document.head.querySelector('[data-hide-command-log-request]')) {
+    const style = app.document.createElement('style');
+    style.innerHTML = '.command-name-request, .command-name-xhr { display: none }';
+    style.setAttribute('data-hide-command-log-request', '');
+    app.document.head.appendChild(style);
+}
