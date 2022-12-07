d4e72dfcf5f3f71838014d22e7a4af304b1ba8fc
Daniel Suchy <suchydan@gmail.com>
2022-12-07 10:33:34 +0100

    chore: graph improvements

--- suite-common/graph/src/hooks.ts
+++ suite-common/graph/src/hooks.ts
@@ -7,24 +7,44 @@ import { FiatCurrencyCode } from '@suite-common/suite-config';
 import { selectAccountByKey, selectAccounts } from '@suite-common/wallet-core';
 import { AccountKey } from '@suite-common/wallet-types';
 
-import { getMultipleAccountBalanceHistoryWithFiat } from './graphThunks';
+import { getMultipleAccountBalanceHistoryWithFiat } from './graphDataFetching';
 import { AccountItem, FiatGraphPoint, FiatGraphPointWithCryptoBalance } from './types';
 
-type UseGraphParams = {
+const getTimeFrameForHistoryHours = (hoursToHistory: number | null) => {
+    const endOfTimeFrameDate = roundToNearestMinutes(new Date(), {
+        nearestTo: 10,
+        roundingMethod: 'floor',
+    });
+    const startOfTimeFrameDate = hoursToHistory
+        ? subHours(endOfTimeFrameDate, hoursToHistory)
+        : null;
+
+    return { startOfTimeFrameDate, endOfTimeFrameDate };
+};
+
+type CommonUseGraphParams = {
     fiatCurrency: FiatCurrencyCode;
 };
+
+// if start date is null we are fetching all data till first account movement
+type StartOfTimeFrameDate = Date | null;
 
 export const useGraphForAccounts = ({
     accounts,
     fiatCurrency,
-}: UseGraphParams & { accounts: AccountItem[] }) => {
+    endOfTimeFrameDate,
+    startOfTimeFrameDate,
+}: CommonUseGraphParams & {
+    accounts: AccountItem[];
+    endOfTimeFrameDate: Date;
+    startOfTimeFrameDate: StartOfTimeFrameDate;
+}) => {
     const [graphPoints, setGraphPoints] = useState<
         FiatGraphPointWithCryptoBalance[] | FiatGraphPoint[]
     >([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState<string | null>(null);
     const [refetchToken, setRefetchToken] = useState(0);
-    const [hoursToHistory, setHoursToHistory] = useState<number | null>(24);
 
     const refetch = useCallback(() => {
         // this is used to trigger to force re-run of useEffect lower
@@ -40,19 +60,11 @@ export const useGraphForAccounts = ({
             setIsLoading(true);
 
             try {
-                const endOfTimeFrameDate = roundToNearestMinutes(new Date(), {
-                    nearestTo: 10,
-                    roundingMethod: 'floor',
-                });
-                const startOfTimeFrameDate = hoursToHistory
-                    ? subHours(endOfTimeFrameDate, hoursToHistory)
-                    : null;
-
                 const points = await getMultipleAccountBalanceHistoryWithFiat({
                     accounts,
+                    fiatCurrency,
                     startOfTimeFrameDate,
                     endOfTimeFrameDate,
-                    fiatCurrency,
                 });
 
                 if (shouldSetPoints) {
@@ -71,16 +83,21 @@ export const useGraphForAccounts = ({
         return () => {
             shouldSetPoints = false;
         };
-    }, [accounts, hoursToHistory, fiatCurrency, refetchToken]);
+    }, [accounts, fiatCurrency, refetchToken, endOfTimeFrameDate, startOfTimeFrameDate]);
 
-    return { graphPoints, isLoading, error, refetch, setHoursToHistory, hoursToHistory };
+    return { graphPoints, isLoading, error, refetch };
 };
 
 export const useGraphForSingleAccount = ({
     accountKey,
     fiatCurrency,
-}: UseGraphParams & { accountKey: AccountKey }) => {
+}: CommonUseGraphParams & { accountKey: AccountKey }) => {
     const account = useSelector((state: any) => selectAccountByKey(state, accountKey));
+    const [hoursToHistory, setHoursToHistory] = useState<number | null>(24);
+    const { startOfTimeFrameDate, endOfTimeFrameDate } = useMemo(
+        () => getTimeFrameForHistoryHours(hoursToHistory),
+        [hoursToHistory],
+    );
 
     const accounts = useMemo(() => {
         if (!account) return [];
@@ -92,14 +109,25 @@ export const useGraphForSingleAccount = ({
         ] as AccountItem[];
     }, [account]);
 
-    return useGraphForAccounts({
-        accounts,
-        fiatCurrency,
-    });
+    return {
+        ...useGraphForAccounts({
+            accounts,
+            fiatCurrency,
+            startOfTimeFrameDate,
+            endOfTimeFrameDate,
+        }),
+        hoursToHistory,
+        setHoursToHistory,
+    };
 };
 
-export const useGraphForAllAccounts = ({ fiatCurrency }: UseGraphParams) => {
+export const useGraphForAllAccounts = ({ fiatCurrency }: CommonUseGraphParams) => {
     const accounts = useSelector(selectAccounts);
+    const [hoursToHistory, setHoursToHistory] = useState<number | null>(24);
+    const { startOfTimeFrameDate, endOfTimeFrameDate } = useMemo(
+        () => getTimeFrameForHistoryHours(hoursToHistory),
+        [hoursToHistory],
+    );
 
     const accountItems = useMemo(
         () =>
@@ -110,8 +138,14 @@ export const useGraphForAllAccounts = ({ fiatCurrency }: UseGraphParams) => {
         [accounts],
     );
 
-    return useGraphForAccounts({
-        accounts: accountItems,
-        fiatCurrency,
-    });
+    return {
+        ...useGraphForAccounts({
+            accounts: accountItems,
+            fiatCurrency,
+            startOfTimeFrameDate,
+            endOfTimeFrameDate,
+        }),
+        hoursToHistory,
+        setHoursToHistory,
+    };
 };
