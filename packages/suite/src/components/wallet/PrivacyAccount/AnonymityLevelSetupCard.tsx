4247b0cfd2f1f8193d4c466f44b64d12ea85a566
dahaca <dahaca17@gmail.com>
2022-12-13 20:39:43 +0100

    feat(suite): add a number input to the anonymity slider

--- packages/suite/src/components/wallet/PrivacyAccount/AnonymityLevelSetupCard.tsx
+++ packages/suite/src/components/wallet/PrivacyAccount/AnonymityLevelSetupCard.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback, useRef } from 'react';
 import styled from 'styled-components';
-import { Card, P, variables } from '@trezor/components';
+import { Card, Input, P, variables } from '@trezor/components';
 import { Translation } from '@suite-components';
 import { useSelector } from '@suite-hooks/useSelector';
 import { selectCurrentTargetAnonymity } from '@wallet-reducers/coinjoinReducer';
 
-import { AnonymityLevelSlider } from './AnonymityLevelSlider';
+import { AnonymityLevelSlider, getPosition } from './AnonymityLevelSlider';
+import { useDispatch } from 'react-redux';
+import { selectSelectedAccount } from '@wallet-reducers/selectedAccountReducer';
+import { coinjoinAccountUpdateAnonymity } from '@wallet-actions/coinjoinAccountActions';
 
 const SetupCard = styled(Card)`
     position: relative;
@@ -13,19 +16,16 @@ const SetupCard = styled(Card)`
     overflow: hidden;
 `;
 
-const Level = styled.div`
+const Level = styled(Input)`
     position: absolute;
-    right: 20px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 64px;
+    right: 0;
+    width: 68px;
     height: 42px;
+    padding: 1px 12px 0 12px;
     border: 1.5px solid ${({ theme }) => theme.STROKE_GREY};
-    border-radius: 8px;
     color: ${({ theme }) => theme.TYPE_GREEN};
     font-size: ${variables.FONT_SIZE.H2};
-    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
+    text-align: center;
 `;
 
 const Text = styled.div`
@@ -34,13 +34,78 @@ const Text = styled.div`
 `;
 
 export const AnonymityLevelSetupCard = () => {
+    const currentAccount = useSelector(selectSelectedAccount);
     const targetAnonymity = useSelector(selectCurrentTargetAnonymity) || 1;
+    const [inputValue, setInputValue] = useState<number | ''>(targetAnonymity);
+    const [sliderPosition, setSliderPosition] = useState(getPosition(targetAnonymity || 1));
+
+    const dispatch = useDispatch();
+    const previousValue = useRef(inputValue);
+
+    useEffect(() => {
+        if (targetAnonymity !== inputValue) {
+            setInputValue(targetAnonymity);
+        }
+    }, [targetAnonymity]); // eslint-disable-line react-hooks/exhaustive-deps
+
+    const setAnonymity = useCallback(
+        (number: number) => {
+            if (Number.isNaN(number)) {
+                return;
+            }
+
+            previousValue.current = number;
+            dispatch(coinjoinAccountUpdateAnonymity(currentAccount?.key ?? '', number));
+            setSliderPosition(getPosition(number));
+        },
+        [currentAccount?.key, dispatch],
+    );
+
+    const handleChange = ({ target }: React.ChangeEvent<HTMLInputElement>) => {
+        if (target.value === '') {
+            setInputValue('');
+
+            return;
+        }
+
+        const number = Number(target.value);
+        if (Number.isNaN(number)) {
+            return;
+        }
+
+        previousValue.current = number;
+        setInputValue(number);
+    };
+
+    const handleFocus = () => {
+        setInputValue('');
+    };
+
+    const handleBlur = () => {
+        let formattedNumber = Number(inputValue);
+
+        if ((!formattedNumber || formattedNumber < 1) && previousValue.current !== '') {
+            formattedNumber = previousValue.current;
+        }
+
+        if (formattedNumber > 100) {
+            formattedNumber = 100;
+        }
+
+        setInputValue(formattedNumber);
+        setAnonymity(formattedNumber);
+    };
 
     return (
         <SetupCard>
-            <Level>
-                <span>{targetAnonymity}</span>
-            </Level>
+            <Level
+                noError
+                noTopLabel
+                value={String(inputValue)}
+                onChange={handleChange}
+                onFocus={handleFocus}
+                onBlur={handleBlur}
+            />
 
             <Text>
                 <P weight="medium">
@@ -51,7 +116,7 @@ export const AnonymityLevelSetupCard = () => {
                 </P>
             </Text>
 
-            <AnonymityLevelSlider />
+            <AnonymityLevelSlider position={sliderPosition} setAnonymity={setAnonymity} />
         </SetupCard>
     );
 };
