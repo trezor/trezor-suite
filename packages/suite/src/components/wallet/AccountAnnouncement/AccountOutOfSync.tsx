b6d3f7dffdb22a8d6766d8ea502925b93861d7a6
Marek Polak <marek.polak@satoshilabs.com>
2022-12-13 14:26:06 +0100

    refactor(suite): move logic inside announcements

--- packages/suite/src/components/wallet/AccountAnnouncement/AccountOutOfSync.tsx
+++ packages/suite/src/components/wallet/AccountAnnouncement/AccountOutOfSync.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { NotificationCard, Translation } from '@suite-components';
+import type { Account } from '@wallet-types/index';
 
-const AccountOutOfSync = () => (
-    <NotificationCard variant="warning">
-        <Translation id="TR_ACCOUNT_OUT_OF_SYNC" />
-    </NotificationCard>
-);
+type AccountOutOfSyncProps = {
+    account: Account | undefined;
+};
+
+const AccountOutOfSync = ({ account }: AccountOutOfSyncProps) =>
+    account?.backendType === 'coinjoin' && account.status === 'out-of-sync' ? (
+        <NotificationCard variant="warning">
+            <Translation id="TR_ACCOUNT_OUT_OF_SYNC" />
+        </NotificationCard>
+    ) : null;
 
 export default AccountOutOfSync;
--- packages/suite/src/components/wallet/AccountAnnouncement/XRPReserve.tsx
+++ packages/suite/src/components/wallet/AccountAnnouncement/XRPReserve.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
+import Bignumber from 'bignumber.js';
 import { NotificationCard, Translation, ReadMoreLink } from '@suite-components';
 import { formatNetworkAmount } from '@suite-common/wallet-utils';
+import type { Account } from '@wallet-types/index';
 
 interface Props {
-    reserve: string;
+    account: Account | undefined;
 }
 
-const XRPReserve = ({ reserve }: Props) => (
-    <NotificationCard
-        variant="info"
-        button={{
-            children: <ReadMoreLink url="HELP_CENTER_XRP_URL" />,
-        }}
-    >
-        <Translation
-            id="TR_XRP_RESERVE_INFO"
-            values={{
-                minBalance: formatNetworkAmount(reserve, 'xrp'),
+const XRPReserve = ({ account }: Props) => {
+    if (account?.networkType !== 'ripple') return null;
+    const bigBalance = new Bignumber(account.balance);
+    const bigReserve = new Bignumber(account.misc.reserve);
+    return bigBalance.isLessThan(bigReserve) ? (
+        <NotificationCard
+            variant="info"
+            button={{
+                children: <ReadMoreLink url="HELP_CENTER_XRP_URL" />,
             }}
-        />
-    </NotificationCard>
-);
+        >
+            <Translation
+                id="TR_XRP_RESERVE_INFO"
+                values={{
+                    minBalance: formatNetworkAmount(account.misc.reserve, 'xrp'),
+                }}
+            />
+        </NotificationCard>
+    ) : null;
+};
 
 export default XRPReserve;
