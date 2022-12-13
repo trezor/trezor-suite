381247bc566eb8d8b19b85d895652a2535310ed8
Marek Polak <marek.polak@satoshilabs.com>
2022-12-13 15:16:55 +0100

    refactor(suite): cleaned BackendDisconnected

--- packages/suite/src/components/wallet/AccountMode/BackendDisconnected.tsx
+++ packages/suite/src/components/wallet/AccountMode/BackendDisconnected.tsx
@@ -3,24 +3,22 @@ import { NotificationCard, Translation } from '@suite-components';
 import { useSelector, useActions } from '@suite-hooks';
 import { reconnectBlockchainThunk } from '@suite-common/wallet-core';
 import { isTrezorConnectBackendType } from '@suite-common/wallet-utils';
+import type { NetworkSymbol } from '@suite-common/wallet-config';
 
-const Disconnected = () => {
+const DisconnectedNotification = ({
+    symbol,
+    resolveTime = 0,
+}: {
+    symbol: NetworkSymbol;
+    resolveTime: number | undefined;
+}) => {
+    const [progress, setProgress] = useState(false);
+    const [time, setTime] = useState<number>();
+
     const { reconnect } = useActions({
         reconnect: reconnectBlockchainThunk,
     });
-    const { blockchain, selectedAccount, online } = useSelector(state => ({
-        blockchain: state.wallet.blockchain,
-        selectedAccount: state.wallet.selectedAccount,
-        online: state.suite.online,
-    }));
-    const [progress, setProgress] = useState(false);
-    const [time, setTime] = useState<number | null>(null);
-    const symbol = selectedAccount.status === 'loaded' ? selectedAccount.network.symbol : undefined;
-    const reconnection =
-        symbol && blockchain[symbol] && blockchain[symbol].reconnection
-            ? blockchain[symbol].reconnection
-            : undefined;
-    const resolveTime = reconnection ? reconnection.time : 0;
+
     useEffect(() => {
         const interval = setInterval(() => {
             const secToResolve = Math.round((resolveTime - new Date().getTime()) / 1000);
@@ -31,16 +29,9 @@ const Disconnected = () => {
         };
     }, [resolveTime]);
 
-    if (selectedAccount.status !== 'loaded') return null;
-    // TODO handle non-standard backends differently
-    if (!isTrezorConnectBackendType(selectedAccount.account.backendType)) return null;
-    if (symbol && blockchain[symbol]?.connected) return null;
-    if (!online) return null;
-
-    const { network } = selectedAccount;
     const click = async () => {
         setProgress(true);
-        const r: any = await reconnect(network.symbol);
+        const r: any = await reconnect(symbol);
         if (!r.success) {
             setProgress(false);
         }
@@ -65,6 +56,27 @@ const Disconnected = () => {
             {displayTime}
         </NotificationCard>
     );
+};
+
+const Disconnected = () => {
+    const { blockchain, selectedAccount, online } = useSelector(state => ({
+        blockchain: state.wallet.blockchain,
+        selectedAccount: state.wallet.selectedAccount,
+        online: state.suite.online,
+    }));
+
+    if (!online) return null;
+
+    if (selectedAccount.status !== 'loaded') return null;
+
+    // TODO handle non-standard backends differently
+    if (!isTrezorConnectBackendType(selectedAccount.account.backendType)) return null;
+
+    const { symbol } = selectedAccount.network;
+    const chain = blockchain[symbol];
+    if (!chain || chain.connected) return null;
+
+    return <DisconnectedNotification symbol={symbol} resolveTime={chain.reconnection?.time} />;
 };
 
 export default Disconnected;
