5cc52bda5d0a6e3057c65477ed64f9bfbe4f2c8c
Szymon Lesisz <sz.lesisz@gmail.com>
2022-12-14 18:16:36 +0100

    chore(suite): remove duplicated coinjoin enums

--- packages/suite/src/types/wallet/coinjoin.ts
+++ packages/suite/src/types/wallet/coinjoin.ts
@@ -1,5 +1,11 @@
 import { NetworkSymbol } from '@suite-common/wallet-config';
 
+// @trezor/coinjoin package is meant to be imported dynamically
+// importing types is safe, but importing an enum thru index will bundle whole lib
+import { RoundPhase } from '@trezor/coinjoin/src/enums';
+
+export { RoundPhase } from '@trezor/coinjoin/src/enums';
+
 export type CoinjoinServerEnvironment = 'localhost' | 'public';
 
 export interface CoinjoinSessionParameters {
@@ -8,17 +14,6 @@ export interface CoinjoinSessionParameters {
     skipRounds?: [number, number];
     maxFeePerKvbyte: number;
     maxCoordinatorFeeRate: number;
-}
-
-// this is a duplicate of @trezor/coinjoin enum.
-// @trezor/coinjoin package is meant to be imported dynamically
-// importing types is safe, but importing an enum will bundle whole lib
-export enum RoundPhase {
-    InputRegistration = 0,
-    ConnectionConfirmation = 1,
-    OutputRegistration = 2,
-    TransactionSigning = 3,
-    Ended = 4,
 }
 
 export interface CoinjoinSession extends CoinjoinSessionParameters {
