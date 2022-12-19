d112786d5a431a38aec8a437b097e859ea9f4341
Marek Polak <marek.polak@satoshilabs.com>
2022-12-19 15:45:15 +0100

    feat(utils): arrayPartition type predicate support

--- packages/utils/src/arrayPartition.ts
+++ packages/utils/src/arrayPartition.ts
@@ -1,12 +1,17 @@
+type ArrayPartition = {
+    <T, S extends T>(array: T[], condition: (elem: T) => elem is S): [S[], Exclude<T, S>[]];
+    <T>(array: T[], condition: (elem: T) => boolean): [T[], T[]];
+};
+
 /**
  *
  * @param array Array to be divided into two parts.
  * @param condition Condition for inclusion in the first part.
  * @returns Array of two arrays - the items in the first array satisfy the condition and the rest is in the second array. Preserving original order.
  */
-export const arrayPartition = <T = []>(array: T[], condition: (elem: T) => boolean): [T[], T[]] =>
-    array.reduce(
+export const arrayPartition: ArrayPartition = <T>(array: T[], condition: (elem: T) => boolean) =>
+    array.reduce<[T[], T[]]>(
         ([pass, fail], elem) =>
             condition(elem) ? [[...pass, elem], fail] : [pass, [...fail, elem]],
-        [[], []] as [T[], T[]],
-    );
+        [[], []],
+    ) as any;
