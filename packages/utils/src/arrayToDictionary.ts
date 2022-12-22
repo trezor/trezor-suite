bd585aa41255d3b1400c88e0e421c9b0d0cf69ce
Szymon Lesisz <sz.lesisz@gmail.com>
2022-12-22 13:50:56 +0100

    feat(utils): arrayToDictionary strongly typed keys, optional keys

--- packages/utils/src/arrayToDictionary.ts
+++ packages/utils/src/arrayToDictionary.ts
@@ -1,32 +1,50 @@
+type DictionaryKey = string | number;
+type GetKey<T> = (item: T) => unknown;
+type Key<Fn extends GetKey<any>, R = ReturnType<Fn>> = R extends DictionaryKey ? R : never;
 type ArrayToDictionary = {
-    <T>(array: T[], getKey: (item: T) => string | number, multiple?: false): { [key: string]: T };
-    <T>(array: T[], getKey: (item: T) => string | number, multiple: true): { [key: string]: T[] };
+    <T, Fn extends GetKey<T>>(array: T[], getKey: Fn, multiple?: false): Record<Key<Fn>, T>;
+    <T, Fn extends GetKey<T>>(array: T[], getKey: Fn, multiple: true): Record<Key<Fn>, T[]>;
 };
 
 /**
  * @param array Array to be converted to dictionary
  * @param getKey Function extracting string from an array item T, which will become its
  * key in the dictionary (if not unique, latter item could replace the former one)
+ * Item will not be added to dictionary if key is not defined
  * @param multiple If true, dictionary values are arrays of all items with the given key
  * @returns Dictionary object with array items as values
  */
-export const arrayToDictionary: ArrayToDictionary = <T>(
+
+const validateKey = (key: unknown): key is DictionaryKey => {
+    if (['string', 'number'].includes(typeof key)) {
+        return true;
+    }
+    return false;
+};
+
+export const arrayToDictionary: ArrayToDictionary = <T, Fn extends GetKey<T>>(
     array: T[],
-    getKey: (item: T) => string | number,
+    getKey: Fn,
     multiple?: boolean,
 ) =>
     multiple
-        ? array.reduce<{ [key: string]: T[] }>(
-              (prev, cur) => ({
-                  ...prev,
-                  [getKey(cur)]: [...(prev[getKey(cur)] ?? []), cur],
-              }),
-              {},
-          )
-        : array.reduce<{ [key: string]: T }>(
-              (prev, cur) => ({
-                  ...prev,
-                  [getKey(cur)]: cur,
-              }),
-              {},
-          );
+        ? array.reduce<Record<DictionaryKey, T[]>>((prev, cur) => {
+              const key = getKey(cur);
+              if (validateKey(key)) {
+                  return {
+                      ...prev,
+                      [key]: [...(prev[key] ?? []), cur],
+                  };
+              }
+              return prev;
+          }, {})
+        : array.reduce<Record<DictionaryKey, T>>((prev, cur) => {
+              const key = getKey(cur);
+              if (validateKey(key)) {
+                  return {
+                      ...prev,
+                      [key]: cur,
+                  };
+              }
+              return prev;
+          }, {});
--- packages/utils/tests/arrayToDictionary.test.ts
+++ packages/utils/tests/arrayToDictionary.test.ts
@@ -52,6 +52,76 @@ describe('arrayToDictionary', () => {
         });
     });
 
+    it('array to dictionary with strongly typed optional number keys', () => {
+        const array = [
+            { value: 1, name: 'a' } as const,
+            { value: 2, name: 'b' } as const,
+            { value: 3, name: 'c' } as const,
+            { value: 4, name: 'b' } as const,
+        ];
+        const dictionary = arrayToDictionary(
+            array,
+            e => (e.value !== 2 ? e.value : undefined),
+            true,
+        );
+        expect(dictionary).toStrictEqual({
+            1: [{ value: 1, name: 'a' }],
+            3: [{ value: 3, name: 'c' }],
+            4: [{ value: 4, name: 'b' }],
+        });
+
+        // @ts-expect-error "2" key is not expected (skipped)
+        expect(dictionary[2]).toBe(undefined);
+        // @ts-expect-error string key is not expected
+        expect(dictionary.a).toBe(undefined);
+    });
+
+    it('array to multidictionary with strongly typed optional string keys', () => {
+        const array = [
+            { value: 1, name: 'a' } as const,
+            { value: 2, name: 'b' } as const,
+            { value: 3, name: 'skip' } as const,
+            { value: 4, name: 'b' } as const,
+            { value: 5, name: 'skip' } as const,
+        ];
+        const dictionary = arrayToDictionary(
+            array,
+            e => (e.name !== 'skip' ? e.name : undefined),
+            true,
+        );
+        expect(dictionary).toStrictEqual({
+            a: [{ value: 1, name: 'a' }],
+            b: [
+                { value: 2, name: 'b' },
+                { value: 4, name: 'b' },
+            ],
+        });
+
+        // @ts-expect-error "skip" key is not expected (skipped)
+        expect(dictionary.skip).toBe(undefined);
+        // @ts-expect-error number key is not expected
+        expect(dictionary[1]).toBe(undefined);
+    });
+
+    it('array with mixed keys', () => {
+        const array = ['a', 'b', 0, 1];
+        const dictionary = arrayToDictionary(array, e => e);
+        expect(dictionary).toStrictEqual({
+            a: 'a',
+            b: 'b',
+            0: 0,
+            1: 1,
+        });
+        expect(dictionary.a).toEqual('a');
+        expect(dictionary[0]).toEqual(0);
+    });
+
+    it('array with invalid keys returned from getKey callback', () => {
+        const array = ['a', 'b', 'c'];
+        const dictionary = arrayToDictionary(array, e => (e === 'a' ? { foo: 1 } : null));
+        expect(dictionary).toStrictEqual({});
+    });
+
     it('array with calculated keys', () => {
         const array = ['aalpha', 'bbeta', 'ggamma', 'ddelta'];
         const dictionary = arrayToDictionary(array, e => e.toUpperCase().slice(1));
