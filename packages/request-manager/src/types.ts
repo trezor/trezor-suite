2107670fa93fc08507ac9009c8494d73855d6a83
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2022-12-30 11:02:03 +0100

    feat(request-manager): httpPool to intercept http responses

--- packages/request-manager/src/types.ts
+++ packages/request-manager/src/types.ts
@@ -5,12 +5,37 @@ export interface TorConnectionOptions {
     torDataDir: string;
 }
 
-export type BootstrapEvent = {
-    progress: string | undefined;
-    summary: string | undefined;
-};
+export type BootstrapEvent =
+    | {
+          type: 'slow';
+      }
+    | {
+          type: 'progress';
+          progress: string;
+          summary?: string;
+      };
 
-export interface InterceptedEvent {
-    method: string;
-    details: string;
-}
+export type InterceptedEvent =
+    | {
+          type: 'INTERCEPTED_REQUEST';
+          method: string;
+          details: string;
+      }
+    | {
+          type: 'INTERCEPTED_RESPONSE';
+          host: string;
+          time: number;
+          statusCode: number | undefined;
+      }
+    | {
+          type: 'NETWORK_MISBEHAVING';
+      }
+    | {
+          type: 'ERROR' | 'ERROR_PROXY_TIMEOUT' | 'ERROR_PROXY_REJECTED';
+      };
+
+export type InterceptorOptions = {
+    handler: (event: InterceptedEvent) => void;
+    getIsTorEnabled: () => boolean;
+    isDevEnv?: boolean;
+};
