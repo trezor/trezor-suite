7ec0500bd8773422bb72b2b43ba89c3b6684f73f
juriczech <bohdan.juricek@gmail.com>
2023-01-04 11:30:24 +0100

    fix(suite-native): Pending txs

--- suite-native/transactions/src/components/TransactionDetail/TransactionDetailData.tsx
+++ suite-native/transactions/src/components/TransactionDetail/TransactionDetailData.tsx
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import { Box, Card, Divider, Text, VStack } from '@suite-native/atoms';
 import { WalletAccountTransaction } from '@suite-common/wallet-types';
 import { Icon } from '@trezor/icons';
-import { formatNetworkAmount, toFiatCurrency } from '@suite-common/wallet-utils';
+import { formatNetworkAmount, isPending, toFiatCurrency } from '@suite-common/wallet-utils';
 import { useFormatters } from '@suite-common/formatters';
 import { selectFiatCurrency } from '@suite-native/module-settings';
 
@@ -26,8 +26,11 @@ export const TransactionDetailData = ({ transaction }: TransactionDetailDataProp
         return new Date(transaction.blockTime * 1000).toLocaleTimeString();
     };
 
+    // Only one input and output address for now until UX comes up with design to support multiple outputs
     const transactionOriginAddresses = transaction.details.vin[0].addresses;
     const transactionTargetAddresses = transaction.targets[0].addresses;
+
+    const isTransactionPending = isPending(transaction);
 
     return (
         <>
@@ -46,7 +49,7 @@ export const TransactionDetailData = ({ transaction }: TransactionDetailDataProp
                 <TransactionDetailSummary
                     origin={transactionOriginAddresses && transactionOriginAddresses[0]}
                     target={transactionTargetAddresses && transactionTargetAddresses[0]}
-                    transactionStatus="pending"
+                    transactionStatus={isTransactionPending ? 'pending' : 'confirmed'}
                 />
                 <Card>
                     <Box flexDirection="row" justifyContent="space-between" alignItems="center">
