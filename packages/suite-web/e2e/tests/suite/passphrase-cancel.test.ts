6414dedc1a845c6320c07ac3218d27fe0ea4afe1
mroz <mvarmuza@gmail.com>
2023-01-05 09:07:28 +0100

    test(suite-web): add Passphrase cancel test and small improvements

---
+++ packages/suite-web/e2e/tests/suite/passphrase-cancel.test.ts
@@ -0,0 +1,33 @@
+// @group:passphrase
+// @retry=2
+
+const testedVersions = ['2-latest', '1-latest'];
+
+describe('Passphrase cancel', () => {
+    beforeEach(() => {
+        cy.viewport(1080, 1440).resetDb();
+    });
+
+    testedVersions.forEach(version => {
+        it(version, () => {
+            cy.task('startEmu', { wipe: true, version });
+            cy.task('setupEmu', {
+                mnemonic: 'all all all all all all all all all all all all',
+                passphrase_protection: true,
+            });
+            cy.task('startBridge');
+            cy.prefixedVisit('/');
+            cy.passThroughInitialRun();
+
+            cy.getTestElement('@passphrase/input').type('abc');
+            cy.getTestElement('@passphrase/hidden/submit-button').click();
+            cy.getTestElement('@suite/modal/confirm-action-on-device');
+
+            cy.getTestElement('@modal/close-button').click();
+            cy.getTestElement('@toast/auth-failed');
+            // todo: interesting fact is that model One and model T have different text in the toast notification
+        });
+    });
+});
+
+export {};
