dbdef65ae0066364796bdb9763c2a464ce72c66c
Szymon Lesisz <sz.lesisz@gmail.com>
2023-01-17 14:01:31 +0100

    chore(coinjoin): add scriptPubKey to select-inputs-for-round

--- packages/coinjoin/src/client/Account.ts
+++ packages/coinjoin/src/client/Account.ts
@@ -1,10 +1,27 @@
 import { Network } from '@trezor/utxo-lib';
 
-import { getScriptPubKeyFromAddress } from '../utils/coordinatorUtils';
+import { getScriptPubKeyFromAddress, prefixScriptPubKey } from '../utils/coordinatorUtils';
 import { AllowedScriptTypes } from '../types/coordinator';
 import { RawLiquidityClue } from '../types/middleware';
 import { RegisterAccountParams } from '../types';
 import { AccountUtxo, AccountAddress } from '../types/account';
+
+const enhanceAccountUtxo = (
+    utxos: Omit<AccountUtxo, 'scriptPubKey'>[],
+    network: Network,
+    scriptType: AllowedScriptTypes,
+): AccountUtxo[] =>
+    utxos.flatMap(address => ({
+        path: address.path,
+        address: address.address,
+        scriptPubKey: prefixScriptPubKey(
+            getScriptPubKeyFromAddress(address.address, network, scriptType),
+            true,
+        ),
+        outpoint: address.outpoint,
+        amount: address.amount,
+        anonymityLevel: address.anonymityLevel,
+    }));
 
 const enhanceAccountAddresses = (
     addresses: Omit<AccountAddress, 'scriptPubKey'>[],
@@ -36,7 +53,7 @@ export class Account {
         this.accountKey = account.accountKey;
         this.network = network;
         this.scriptType = account.scriptType;
-        this.utxos = account.utxos;
+        this.utxos = enhanceAccountUtxo(account.utxos, network, account.scriptType);
         this.changeAddresses = enhanceAccountAddresses(
             account.changeAddresses,
             network,
@@ -51,7 +68,7 @@ export class Account {
     }
 
     update(account: RegisterAccountParams) {
-        this.utxos = account.utxos;
+        this.utxos = enhanceAccountUtxo(account.utxos, this.network, account.scriptType);
         this.changeAddresses = enhanceAccountAddresses(
             account.changeAddresses,
             this.network,
