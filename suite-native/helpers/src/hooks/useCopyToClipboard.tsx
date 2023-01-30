77d4e4ea2c93e6b53fcc0d93760eefe009d86010
Petr Knetl <petr.knetl@satoshilabs.com>
2023-01-30 12:56:53 +0100

    fix(suite-native): clipboard toast message shown only on ios
    Clipboard toast message displayed only on iOS.
    Since Android shows toast message on copy-to-clipobord by default, our custom message will be hidden.
    
    fixes #7331

--- suite-native/helpers/src/hooks/useCopyToClipboard.tsx
+++ suite-native/helpers/src/hooks/useCopyToClipboard.tsx
@@ -1,4 +1,5 @@
 import { useCallback } from 'react';
+import { Platform } from 'react-native';
 import Toast from 'react-native-root-toast'; // TODO: Remove when is our own notification UI ready.
 
 import * as Clipboard from 'expo-clipboard';
@@ -7,8 +8,11 @@ export function useCopyToClipboard() {
     const copyToClipboard = useCallback(async (value: string, toastMessage?: string) => {
         await Clipboard.setStringAsync(value);
 
-        // TODO: Replace with our own notification UI when ready.
-        Toast.show(toastMessage ?? 'Copied to clipboard.');
+        if (Platform.OS === 'ios') {
+            // Android is showing it's own copy-to-clipboard toast message
+            // TODO: Replace with our own notification UI when ready.
+            Toast.show(toastMessage ?? 'Copied to clipboard.');
+        }
     }, []);
     return copyToClipboard;
 }
