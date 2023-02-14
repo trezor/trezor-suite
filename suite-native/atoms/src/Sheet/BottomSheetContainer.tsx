b3a09db066e4e7e1aa09011e18d3dfd115c205a4
Daniel Suchý <suchydan@gmail.com>
2023-02-14 10:04:18 +0100

    chore(mobile): upgrade RN to 0.71 (#7620)
    * chore(mobile): RN upgrade 0.71
    
    * fix: types
    
    * fix: RN tests
    
    * chore(android): remove unused file

--- suite-native/atoms/src/Sheet/BottomSheetContainer.tsx
+++ suite-native/atoms/src/Sheet/BottomSheetContainer.tsx
@@ -13,7 +13,8 @@ type SheetProps = {
  * To fix that, components need to be wrapped with gestureHandlerRootHOC (it's no-op on iOS and web).
  * See more details: https://docs.swmansion.com/react-native-gesture-handler/docs/installation/#usage-with-modals-on-android
  */
-const BottomSheetGestureHandler = gestureHandlerRootHOC(({ children }: { children: ReactNode }) => (
+// @ts-expect-error https://github.com/software-mansion/react-native-gesture-handler/issues/2412
+const BottomSheetGestureHandler = gestureHandlerRootHOC<{ children: ReactNode }>(({ children }) => (
     <>{children}</>
 ));
 
