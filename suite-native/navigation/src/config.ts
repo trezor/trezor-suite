4e67356d0b20517baeeba0a92bbd76cedf83fd77
Daniel Suchý <suchydan@gmail.com>
2023-02-27 08:56:27 +0100

    chore(mobile): fix Adnroid screen transations (#7706)

--- suite-native/navigation/src/config.ts
+++ suite-native/navigation/src/config.ts
@@ -1,5 +1,7 @@
-export const stackNavigationOptionsConfig = {
+import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
+
+export const stackNavigationOptionsConfig: NativeStackNavigationOptions = {
     headerShown: false,
     gestureEnabled: true,
-    animationEnabled: true,
+    animation: 'slide_from_right',
 } as const;
