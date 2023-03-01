4fa07e25a1e8c4dfbcbd8da445dc81037afe5fee
Jan Komarek <jan.komarek@satoshilabs.com>
2023-03-01 12:59:22 +0100

    feat(coinjoin): add max mining fee modifier

--- packages/coinjoin/src/constants.ts
+++ packages/coinjoin/src/constants.ts
@@ -43,9 +43,12 @@ export const PLEBS_DONT_PAY_THRESHOLD = 1000000;
 export const COORDINATOR_FEE_RATE = 0.003;
 export const MIN_ALLOWED_AMOUNT = 5000;
 export const MAX_ALLOWED_AMOUNT = 134375000000;
-export const MAX_MINING_FEE = 1;
+export const MAX_MINING_FEE = 2;
 
 // affiliation flag:
 // - sent coordinator/ready-to-sign request **only** when Alice pays coordination fee
 // - check if Trezor affiliate server is running in status.affiliateInformation.runningAffiliateServers
 export const AFFILIATION_ID = 'trezor';
+
+// modifier applied to a median fee rate to set default max mining fee
+export const MAX_MINING_FEE_MODIFIER = 2.5;
