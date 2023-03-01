7c592f046c01113f601d8e8991d0394be5cfff2e
Petr Knetl <petr.knetl@satoshilabs.com>
2023-03-01 13:55:39 +0100

    refactor(suite-native): deprecated component device action buttons removed

--- suite-native/module-settings/src/index.ts
+++ suite-native/module-settings/src/index.ts
@@ -2,4 +2,3 @@ export * from './navigation/SettingsStackNavigator';
 export * from './slice';
 export * from './components/ColorSchemePicker';
 export * from './components/CoinsSettings';
-export * from './components/DeviceActionButtons';
