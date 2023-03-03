64d99a396902a8d8a76bb804fa8b8bf542dabd9b
juriczech <bohdan.juricek@gmail.com>
2023-03-03 14:33:47 +0100

    feat(suite-native): list tokens with account

--- suite-native/accounts/src/components/AccountListItemInteractive.tsx
+++ suite-native/accounts/src/components/AccountListItemInteractive.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { TouchableOpacity } from 'react-native';
 import { useSelector } from 'react-redux';
 
+import { ListItem, Text, VStack } from '@suite-native/atoms';
 import { selectEthereumAccountsTokens } from '@suite-native/ethereum-tokens';
 import { AccountsRootState } from '@suite-common/wallet-core';
 
@@ -24,6 +25,11 @@ export const AccountListItemInteractive = ({
     return (
         <TouchableOpacity onPress={() => onSelectAccount(account.key)}>
             <AccountListItem key={account.key} account={account} />
+            <VStack>
+                {accountsTokens?.map(token => (
+                    <Text key={token.name}>{token.name}</Text>
+                ))}
+            </VStack>
         </TouchableOpacity>
     );
 };
--- suite-native/ethereum-tokens/src/ethereumTokensSelectors.ts
+++ suite-native/ethereum-tokens/src/ethereumTokensSelectors.ts
@@ -1,11 +1,13 @@
 import { AccountsRootState, selectAccountByKey } from '@suite-common/wallet-core';
 import { TokenInfo } from '@trezor/blockchain-link';
 
+import { filterTokenHasBalance } from './utils';
+
 export const selectEthereumAccountsTokens = (
     state: AccountsRootState,
     ethereumAccountKey: string,
 ): TokenInfo[] | null => {
     const account = selectAccountByKey(state, ethereumAccountKey);
     if (account?.symbol !== 'eth') return null;
-    return account.tokens ?? null;
+    return account.tokens?.filter(filterTokenHasBalance) ?? null;
 };
