190411d1e6cd3ae81bce5af16297e2821381b49c
Matej Kriz <matej@matejkriz.cz>
2023-03-08 12:24:46 +0100

    chore(rollout): remove fw rollout possibility completely
    - Not used at all and complicates the logic a bit.
    - Not properly implemented for intermediary feature.
    - We can put it back whenever needed.

--- packages/connect/src/utils/__tests__/firmwareUtils.test.ts
+++ packages/connect/src/utils/__tests__/firmwareUtils.test.ts
@@ -1,30 +1,4 @@
-import { getScore, isStrictFeatures, isValidReleases } from '../firmwareUtils';
-
-describe('Score Utils', () => {
-    describe('getScore()', () => {
-        const inputs = [
-            '17BdA1CBDE11943C1D48653CD',
-            '27BdA1CBDE11943C1D48653CD',
-            '37BdA1CBDE11943C1D48653CD',
-            '47BdA1CBDE11943C1D48653CD',
-        ];
-
-        inputs.forEach(i => {
-            it(`${i} should return between 0 and 1`, () => {
-                const result = getScore(i);
-                expect(result).toBeLessThanOrEqual(1);
-                expect(result).toBeGreaterThanOrEqual(0);
-            });
-        });
-
-        const ts = Date.now();
-        it(`actually any input should return between 0 and 1, even timestamp ${ts}`, () => {
-            const result = getScore(String(ts));
-            expect(result).toBeLessThanOrEqual(1);
-            expect(result).toBeGreaterThanOrEqual(0);
-        });
-    });
-});
+import { isStrictFeatures, isValidReleases } from '../firmwareUtils';
 
 describe('parse', () => {
     describe('isStrictFeatures()', () => {
