277df4442a2e7e76175435e1efb7bdf5920fd224
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2023-03-15 16:03:34 +0100

    feat(suite-native): upgrade RN, assemble android (#7848)
    * fix(suite-common): remove namespace export
    
    * feat(suite-native): upgrade RN, assemble android
    
    * Revert "Merge branch 'fix/suite-native-staging-errors' into fix/assemble-staging-and-production-build"
    
    This reverts commit 772435833c647b6f45fed41e11d07b782a8c3a19, reversing
    changes made to 66fd08a5f20c146eadb95f6b1ae82eb7ff702181.
    
    * fix(suite-native): add babel plugin
    
    * fix(suite-native): bundle

--- suite-native/app/babel.config.js
+++ suite-native/app/babel.config.js
@@ -1,9 +1,12 @@
 module.exports = {
     env: {
         production: {
-            plugins: ['transform-remove-console'],
+            plugins: ['transform-remove-console', '@babel/plugin-proposal-export-namespace-from'],
         },
     },
     presets: ['module:metro-react-native-babel-preset'],
-    plugins: [['react-native-reanimated/plugin', { globals: ['__scanCodes'] }]],
+    plugins: [
+        ['react-native-reanimated/plugin', { globals: ['__scanCodes'] }],
+        '@babel/plugin-proposal-export-namespace-from',
+    ],
 };
