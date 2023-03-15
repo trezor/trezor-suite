4987897fbe1eeabefaf2265638e745fd65598ff5
Matej Kriz <matej@matejkriz.cz>
2023-03-15 17:19:10 +0100

    feat(dependencies): update all sentry related dependencies

--- packages/suite/src/utils/suite/sentry.ts
+++ packages/suite/src/utils/suite/sentry.ts
@@ -1,4 +1,4 @@
-import * as Sentry from '@sentry/minimal';
+import * as Sentry from '@sentry/core';
 import { Dispatch, GetState } from '@suite-types';
 import { redactDevice, redactDiscovery, getApplicationLog } from '@suite-utils/logsUtils';
 
