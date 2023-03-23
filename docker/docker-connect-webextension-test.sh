c4db8c07a07bae183b7d85a36cd517e4de28ee15
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2023-03-23 14:25:48 +0100

    test(ci): web extension ci tests

---
+++ docker/docker-connect-webextension-test.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+xhost +
+LOCAL_USER_ID="$(id -u "$USER")"
+HEADLESS=false
+export LOCAL_USER_ID
+export HEADLESS
+
+docker-compose -f ./docker/docker-compose.connect-webextension-test.yml up --build --abort-on-container-exit
