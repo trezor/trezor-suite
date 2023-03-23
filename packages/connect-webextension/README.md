6739a4c4feea0d37cbca21ce50e30030fe519ae3
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2023-03-23 14:24:48 +0100

    feat(connect-webextension): create new package

---
+++ packages/connect-webextension/README.md
@@ -0,0 +1,3 @@
+# @trezor/webextension
+
+This package will contain Trezor Connect Web Extension. For now it only has e2e tests.
---
+++ packages/connect-webextension/playwright.config.ts
@@ -0,0 +1,13 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+
+export const config: PlaywrightTestConfig = {
+    testDir: 'e2e',
+    retries: 0,
+    workers: 1, // to disable parallelism between test files
+    timeout: 30000 * 1000,
+    use: {
+        headless: process.env.HEADLESS === 'true',
+        ignoreHTTPSErrors: true,
+        trace: 'retain-on-failure',
+    },
+};
