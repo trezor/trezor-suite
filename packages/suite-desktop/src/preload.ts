4f4aafa8e48c841b0d2a2999f31f798a80ab0c6a
Marek Polak <marek.polak@satoshilabs.com>
2023-03-27 15:57:58 +0200

    fix(suite-desktop): fix excessive memory usage

--- packages/suite-desktop/src/preload.ts
+++ packages/suite-desktop/src/preload.ts
@@ -3,7 +3,7 @@ import { contextBridge, ipcRenderer } from 'electron';
 import { exposeIpcProxy } from '@trezor/ipc-proxy';
 import { getDesktopApi } from '@trezor/suite-desktop-api';
 
-import '@sentry/electron/preload';
+import '@sentry/electron/preload'; // With this only IPCMode.Classic is ever taken into account
 
 contextBridge.exposeInMainWorld(
     ...exposeIpcProxy(ipcRenderer, ['TrezorConnect', 'CoinjoinBackend', 'CoinjoinClient']),
