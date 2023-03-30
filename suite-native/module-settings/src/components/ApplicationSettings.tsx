3f42c938407363a30b656e69b5c2011d55911f16
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2023-03-30 11:01:55 +0100

    feat(suite-native): add dev button for prod as well

--- suite-native/module-settings/src/components/ApplicationSettings.tsx
+++ suite-native/module-settings/src/components/ApplicationSettings.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Pressable } from 'react-native';
 
+import { atom, useAtom } from 'jotai';
 import { useNavigation } from '@react-navigation/core';
 
 import {
@@ -9,12 +11,30 @@ import {
     RootStackParamList,
     RootStackRoutes,
 } from '@suite-native/navigation';
+import { Text } from '@suite-native/atoms';
 import { isDevelopOrDebugEnv } from '@suite-native/config';
+import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
 
 import { SettingsSection } from './SettingsSection';
 import { SettingsSectionItem } from './SettingsSectionItem';
 
+const devButtonStyle = prepareNativeStyle(() => ({
+    position: 'absolute',
+    zIndex: 1,
+    top: 0,
+    right: 0,
+    width: 50,
+    height: 50,
+}));
+
+const isDevButtonVisibleAtom = atom<boolean>(false);
+
+let tapsCount = 0;
+
 export const ApplicationSettings = () => {
+    const { applyStyle } = useNativeStyles();
+    const [isVisible, setIsDevButtonVisible] = useAtom(isDevButtonVisibleAtom);
+
     const navigation =
         useNavigation<
             StackToStackCompositeNavigationProps<
@@ -28,34 +48,50 @@ export const ApplicationSettings = () => {
         navigation.navigate(routeName);
     };
 
+    const handleTapsCount = () => {
+        if (tapsCount < 7) {
+            tapsCount++;
+        }
+        if (tapsCount === 7) {
+            setIsDevButtonVisible(true);
+        }
+    };
+
+    const shouldShowDevButton = isDevelopOrDebugEnv() || isVisible;
+
     return (
-        <SettingsSection title="Application">
-            {isDevelopOrDebugEnv() && (
+        <>
+            <Pressable style={applyStyle(devButtonStyle)} onPress={handleTapsCount}>
+                <Text> </Text>
+            </Pressable>
+            <SettingsSection title="Application">
+                {shouldShowDevButton && (
+                    <SettingsSectionItem
+                        iconName="placeholder"
+                        title="DEV utils"
+                        subtitle="Only for devs and internal testers."
+                        onPress={() => navigation.navigate(RootStackRoutes.DevUtilsStack)}
+                    />
+                )}
                 <SettingsSectionItem
-                    iconName="placeholder"
-                    title="DEV utils"
-                    subtitle="Only for devs and internal testers."
-                    onPress={() => navigation.navigate(RootStackRoutes.DevUtilsStack)}
+                    iconName="flag"
+                    title="Localisation"
+                    subtitle="Currency, Bitcoin units"
+                    onPress={() => handleNavigation(SettingsStackRoutes.SettingsLocalisation)}
                 />
-            )}
-            <SettingsSectionItem
-                iconName="flag"
-                title="Localisation"
-                subtitle="Currency, Bitcoin units"
-                onPress={() => handleNavigation(SettingsStackRoutes.SettingsLocalisation)}
-            />
-            <SettingsSectionItem
-                title="Customization"
-                iconName="palette"
-                subtitle="Color scheme"
-                onPress={() => handleNavigation(SettingsStackRoutes.SettingsCustomization)}
-            />
-            <SettingsSectionItem
-                title="Privacy & Security"
-                iconName="eye"
-                subtitle="Analytics, Discreet mode"
-                onPress={() => handleNavigation(SettingsStackRoutes.SettingsPrivacyAndSecurity)}
-            />
-        </SettingsSection>
+                <SettingsSectionItem
+                    title="Customization"
+                    iconName="palette"
+                    subtitle="Color scheme"
+                    onPress={() => handleNavigation(SettingsStackRoutes.SettingsCustomization)}
+                />
+                <SettingsSectionItem
+                    title="Privacy & Security"
+                    iconName="eye"
+                    subtitle="Analytics, Discreet mode"
+                    onPress={() => handleNavigation(SettingsStackRoutes.SettingsPrivacyAndSecurity)}
+                />
+            </SettingsSection>
+        </>
     );
 };
