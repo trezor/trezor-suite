ed5829e62a62481048b65b5f3f33d9e76633e5a0
Matej Kriz <matej@matejkriz.cz>
2023-04-03 17:30:15 +0200

    fix(node-utils): dir path can't be dasherized as a whole
    There could be existing path that contains spaces and you need to create just a subfolder of that existing folder.
    
    (cherry picked from commit a54c397c8519d07629d5f63250c80db906e5f15d)

--- packages/node-utils/src/ensureDirectoryExists.ts
+++ packages/node-utils/src/ensureDirectoryExists.ts
@@ -5,9 +5,8 @@ const exists = promisify(fs.exists);
 const mkdir = promisify(fs.mkdir);
 
 export const ensureDirectoryExists = async (dir: string) => {
-    const dasherizedDir = dir.replace(' ', '-');
-    if (!(await exists(dasherizedDir))) {
-        await mkdir(dasherizedDir, { recursive: true });
+    if (!(await exists(dir))) {
+        await mkdir(dir, { recursive: true });
     }
-    return dasherizedDir;
+    return dir;
 };
