b14d3e21cfff7c300ff3092ed06ec0d6499f94d9
Petr Knetl <petr.knetl@satoshilabs.com>
2023-04-06 15:55:05 +0200

    fix(suite-native): camera access alert hiden on undetermined status (#8018)

--- suite-native/qr-code/src/components/CameraPermissionError.tsx
+++ suite-native/qr-code/src/components/CameraPermissionError.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 
-import { PermissionStatus } from 'expo-barcode-scanner';
-
 import { Box, Button, Text } from '@suite-native/atoms';
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
 
 type CameraPermissionErrorProps = {
-    permissionStatus: PermissionStatus;
     onPermissionRequest: () => void;
 };
 
@@ -18,21 +15,13 @@ const grantPermissionButtonStyle = prepareNativeStyle(({ spacings }) => ({
     marginTop: spacings.large,
 }));
 
-export const CameraPermissionError = ({
-    permissionStatus,
-    onPermissionRequest,
-}: CameraPermissionErrorProps) => {
+export const CameraPermissionError = ({ onPermissionRequest }: CameraPermissionErrorProps) => {
     const { applyStyle } = useNativeStyles();
 
     return (
         <Box style={applyStyle(permissionTextContainerStyle)}>
-            <Text align="center">Camera access denied. {permissionStatus}</Text>
-            {permissionStatus === PermissionStatus.DENIED && (
-                <Text align="center">Please allow camera access in your device settings.</Text>
-            )}
-            {permissionStatus === PermissionStatus.UNDETERMINED && (
-                <Text align="center">Please allow camera access to your camera. </Text>
-            )}
+            <Text align="center">Camera access denied.</Text>
+            <Text align="center">Please allow camera access in your device settings.</Text>
 
             <Button onPress={onPermissionRequest} style={applyStyle(grantPermissionButtonStyle)}>
                 Grant permission
