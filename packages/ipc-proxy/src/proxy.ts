5ce2769a36c3768402a04b66301af1cf2d1975df
Marek Polak <marek.polak@satoshilabs.com>
2023-04-07 13:02:55 +0200

    fix(ipc-proxy): unsubscribe correct listeners

--- packages/ipc-proxy/src/proxy.ts
+++ packages/ipc-proxy/src/proxy.ts
@@ -1,8 +1,63 @@
-import type { IpcProxyGenerator } from './types';
+import type { IpcProxyApi } from './types';
 
-const getIpcProxy = <T>(proxyName: string): IpcProxyGenerator<T> | undefined =>
+const getIpcProxyApi = (proxyName: string): IpcProxyApi | undefined =>
     // @ts-expect-error this is the only window reference. not worth typing in global scope
     typeof window !== 'undefined' ? window[proxyName] : undefined;
+
+const getIpcMethods = (ipcProxy: IpcProxyApi, channelName: string, instanceId: string) => {
+    let listeners: { eventName: string; listener: (...args: any[]) => any }[] = [];
+    let created = false;
+
+    const create = async (constructorParams: any) => {
+        if (created) return true;
+        // Handshake with electron main
+        await ipcProxy.create(channelName, instanceId, constructorParams);
+        created = true;
+    };
+
+    // It would be easier to use ipcRenderer.invoke and return a promise directly from API[method]
+    // read more in ./proxy-handler
+    const request =
+        (method: string) =>
+        (...args: any[]) =>
+            ipcProxy.request(channelName, instanceId, method, args);
+
+    const invoke =
+        (method: string) =>
+        (...args: any[]) =>
+            ipcProxy.invoke(channelName, instanceId, method, args);
+
+    const setOrClearHandler = (eventName: string) => {
+        const eventListeners = listeners.filter(l => l.eventName === eventName);
+        if (eventListeners.length) {
+            ipcProxy.setHandler(channelName, instanceId, eventName, (event: any) =>
+                eventListeners.forEach(l => l.listener(...event)),
+            );
+        } else {
+            ipcProxy.clearHandler(channelName, instanceId, eventName);
+        }
+    };
+
+    const addListener = (eventName: string, listener: any) => {
+        listeners.push({ eventName, listener });
+        setOrClearHandler(eventName);
+    };
+
+    const removeListener = (eventName: string, listener?: any) => {
+        listeners = listeners.filter(
+            l => l.eventName !== eventName || (listener && l.listener !== listener),
+        );
+        setOrClearHandler(eventName);
+    };
+
+    return {
+        create,
+        request,
+        invoke,
+        addListener,
+        removeListener,
+    };
+};
 
 interface IpcProxyOptions {
     proxyName?: string; // needs to be also set in `exposeIpcProxy` if changed here (see ./proxy-generator)
@@ -11,23 +66,61 @@ interface IpcProxyOptions {
 
 // This should be used only in electron renderer context.
 // `Proxy` object cannot be defined and returned from electron preload native code context.
-export const createIpcProxy = async <T>(
+export const createIpcProxy = async <T extends object>(
     channelName: string, // declared in `exposeIpcProxy` (see ./proxy-generator)
     options: IpcProxyOptions = {},
     ...constructorParams: any[] // constructor of <T> params, could be undefined
 ): Promise<T> => {
     const proxyName = options?.proxyName || 'ipcProxy';
-    const generator = getIpcProxy<T>(proxyName);
-    if (!generator) {
+
+    const ipcProxyApi = getIpcProxyApi(proxyName);
+    if (!ipcProxyApi) {
         throw new Error(`${proxyName} not found`);
     }
-    const { target, proxy } = await generator(channelName, ...constructorParams);
-    return new Proxy(
-        { ...target, ...options.target },
-        {
-            // NOTE: pass only serializable object, params of `ProxyHandler` (target, name) to electron preload context.
-            // `receiver` parameter (3rd) is an instance of Proxy object and could not be serialized
-            get: (t, n) => proxy.get(t, n),
-        },
+
+    const instanceId = (Date.now() + Math.random()).toString(36);
+
+    const { create, request, addListener, removeListener } = getIpcMethods(
+        ipcProxyApi,
+        channelName,
+        instanceId,
     );
+
+    // try to handshake electron main layer (See ./proxy-handler)
+    await create(constructorParams);
+
+    const prefix = `${channelName}/${instanceId}`;
+
+    const target = { _ipcProxy: prefix, ...options.target } as T;
+
+    const proxyHandler: ProxyHandler<T> = {
+        // NOTE: pass only serializable object, params of `ProxyHandler` (target, name) to electron preload context.
+        // `receiver` parameter (3rd) is an instance of Proxy object and could not be serialized
+        get: (proxyTarget: any, name) => {
+            if (typeof name === 'symbol') return proxyTarget[name];
+
+            // exclude promise-like methods. they presence may be checked since the whole process is async
+            if (['then', 'catch', 'finally'].includes(name)) return undefined;
+
+            if (proxyTarget[name]) {
+                return proxyTarget[name];
+            }
+
+            // handle event-emitter-like methods
+            if (name === 'on') {
+                proxyTarget[name] = addListener;
+                return proxyTarget[name];
+            }
+
+            if (name === 'off' || name === 'removeAllListeners') {
+                proxyTarget[name] = removeListener;
+                return proxyTarget[name];
+            }
+
+            proxyTarget[name] = request(name);
+            return proxyTarget[name];
+        },
+    };
+
+    return new Proxy(target, proxyHandler);
 };
--- packages/ipc-proxy/src/types.ts
+++ packages/ipc-proxy/src/types.ts
@@ -9,3 +9,11 @@ export type IpcProxyGenerator<T> = (
         get(target: T, p: string | symbol): any;
     };
 }>;
+
+export type IpcProxyApi = {
+    create: (channelName: string, instanceId: string, constructorParams: any) => Promise<any>;
+    request: (channelName: string, instanceId: string, method: string, args: any[]) => Promise<any>;
+    invoke: (channelName: string, instanceId: string, method: string, args: any[]) => Promise<any>;
+    setHandler: (channelName: string, instanceId: string, eventName: string, handler: any) => void;
+    clearHandler: (channelName: string, instanceId: string, eventName: string) => void;
+};
