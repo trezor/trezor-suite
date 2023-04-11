67fa47661e75e55ae874ca839171b288fb4ef356
Matej Kriz <matej@matejkriz.cz>
2023-04-11 18:12:40 +0200

    fix(suite-desktop): fix codesign app title
    - esbuild define plugin works different and boolean has to be wrapped in "" to keep it as string
    
    (cherry picked from commit e4a3e831fee3ecdff5b437841cc48c0d1f6c3faa)

--- packages/suite-desktop/scripts/build.ts
+++ packages/suite-desktop/scripts/build.ts
@@ -98,7 +98,7 @@ build({
         'process.env.VERSION': JSON.stringify(suiteVersion),
         'process.env.SENTRY_RELEASE': JSON.stringify(sentryRelease),
         'process.env.SUITE_TYPE': JSON.stringify(PROJECT),
-        'process.env.IS_CODESIGN_BUILD': JSON.stringify(isCodesignBuild),
+        'process.env.IS_CODESIGN_BUILD': `"${isCodesignBuild}"`, // to keep it as string "true"/"false" and not boolean
     },
     inject: [path.join(__dirname, 'build-inject.ts')],
     plugins: useMocks ? [mockPlugin] : [],
