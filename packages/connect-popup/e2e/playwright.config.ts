f0637941b47f70930f5d93dfb85082dacf4771e3
Martin Varmuza <mvarmuza@gmail.com>
2023-04-12 23:43:51 +0200

    test(connet-popup): adapt remaining fixtures to fw 2.6.0

--- packages/connect-popup/e2e/playwright.config.ts
+++ packages/connect-popup/e2e/playwright.config.ts
@@ -4,7 +4,7 @@ const config: PlaywrightTestConfig = {
     testDir: 'tests',
     retries: 0,
     workers: 1, // to disable parallelism between test files
-    timeout: 30000,
+    timeout: 40000,
     use: {
         headless: process.env.HEADLESS === 'true',
         ignoreHTTPSErrors: true,
