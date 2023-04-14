5d2255f5abc9d34e730e13510142669007e10b07
Matej Kriz <matej@matejkriz.cz>
2023-04-14 16:50:29 +0200

    chore(ci): ensure that every commit in release branch is in default branch or marked properly

--- ci/scripts/check_release_commit_messages.sh
+++ ci/scripts/check_release_commit_messages.sh
@@ -15,7 +15,7 @@ do
     # shellcheck disable=SC2076
     if [[ $message =~ "(cherry picked from commit" ]]; then
       # remove last ")" and extract commit hash
-      develop_commit=$(echo "${message:0:-1}" | tr ' ' '\n' | tail -1)
+      develop_commit=$($message | tr ' ' '\n' | tail -1 | sed 's/)$//')
       # check if develop really contains this commit hash
       if [[ $(git branch -a --contains "$develop_commit" | grep --only-matching "remotes/origin/develop") == "remotes/origin/develop" ]]; then
         continue
@@ -29,8 +29,13 @@ do
     fi
 
     fail=1
-    echo "FAILURE! Neither 'cherry picked from..' nor '[RELEASE ONLY]' substring found in this commit message."
+    echo "FAILURE! Neither 'cherry picked from..' nor '[RELEASE ONLY]' substring found in commit message of $commit"
 done
 
-echo "ALL OK"
+if (( fail == 0 )); then
+  echo "ALL OK"
+else
+  echo "FAILURE! Some commit messages are not valid."
+fi
+
 exit $fail
