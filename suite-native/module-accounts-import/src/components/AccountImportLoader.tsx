c1717530d63d621fe6906e0a9491d9f1f7c17ccf
Nodonisko <suchydan@gmail.com>
2023-04-24 18:52:15 +0200

    chore(mobile): replace ugly import globe with nice animation

--- suite-native/module-accounts-import/src/components/AccountImportLoader.tsx
+++ suite-native/module-accounts-import/src/components/AccountImportLoader.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Animated, {
     useAnimatedStyle,
     withDelay,
@@ -6,34 +6,53 @@ import Animated, {
     withTiming,
 } from 'react-native-reanimated';
 
-import { Box, Loader, Text, VStack, Image } from '@suite-native/atoms';
+import Lottie from 'lottie-react-native';
+
+import { Box, Text } from '@suite-native/atoms';
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
+
+import spinnerSuccess from '../assets/spinnerSuccess.json';
 
 const LINE_VISIBILITY_DURATION = 1500;
 
-const loaderContainerStyle = prepareNativeStyle(utils => ({
-    flex: 1,
-    justifyContent: 'space-between',
+const loaderContainerStyle = prepareNativeStyle(() => ({
     alignItems: 'center',
-    marginTop: utils.spacings.medium * 10,
+    justifyContent: 'center',
+    flex: 1,
 }));
+
+const successSpinnerStyle = prepareNativeStyle(utils => ({
+    width: 50,
+    height: 50,
+    marginBottom: utils.spacings.large,
+}));
+
+const ANIMATION_SAFETY_MARGIN = 5;
 
 const textContainerStyle = prepareNativeStyle(utils => ({
     overflow: 'hidden',
-    height: utils.typography.titleMedium.lineHeight,
+    height: utils.typography.titleSmall.lineHeight + ANIMATION_SAFETY_MARGIN * 2,
+    paddingHorizontal: ANIMATION_SAFETY_MARGIN,
 }));
+
+const textStyle = prepareNativeStyle(utils => ({
+    lineHeight: utils.typography.titleSmall.lineHeight + ANIMATION_SAFETY_MARGIN * 2,
+    textAlign: 'center',
+}));
+
 export const AccountImportLoader = () => {
-    const { applyStyle, utils } = useNativeStyles();
-    const { lineHeight } = utils.typography.titleMedium;
+    const { applyStyle } = useNativeStyles();
+    const [lineHeight1, setLineHeight1] = useState(0);
+    const [lineHeight2, setLineHeight2] = useState(0);
+    const [lineHeight3, setLineHeight3] = useState(0);
     const animatedTextStyle = useAnimatedStyle(() => ({
-        height: lineHeight * 3,
         transform: [
             {
                 translateY: withSequence(
-                    withTiming(lineHeight),
+                    withTiming(-lineHeight1),
                     withTiming(0),
-                    withDelay(LINE_VISIBILITY_DURATION, withTiming(-lineHeight)),
-                    withDelay(LINE_VISIBILITY_DURATION, withTiming(-2 * lineHeight)),
+                    withDelay(LINE_VISIBILITY_DURATION, withTiming(-lineHeight2)),
+                    withDelay(LINE_VISIBILITY_DURATION, withTiming(-lineHeight3)),
                 ),
             },
         ],
@@ -41,24 +60,45 @@ export const AccountImportLoader = () => {
 
     return (
         <Box style={applyStyle(loaderContainerStyle)}>
-            {/* eslint-disable-next-line global-require */}
-            <Image source={require('../assets/globe.png')} width={283} height={166} />
-            <VStack spacing="large" flex={1} justifyContent="center">
-                <Loader />
-                <Box style={applyStyle(textContainerStyle)}>
-                    <Animated.View style={animatedTextStyle}>
-                        <Text variant="titleMedium" align="center">
-                            Retrieving Balances
-                        </Text>
-                        <Text variant="titleMedium" align="center">
-                            Confirming assets
-                        </Text>
-                        <Text variant="titleMedium" align="center">
-                            Checking transactions
-                        </Text>
-                    </Animated.View>
-                </Box>
-            </VStack>
+            <Box>
+                <Lottie
+                    source={spinnerSuccess}
+                    autoPlay
+                    style={applyStyle(successSpinnerStyle)}
+                    loop={false}
+                />
+            </Box>
+            <Box style={applyStyle(textContainerStyle)}>
+                <Animated.View style={animatedTextStyle}>
+                    <Text
+                        variant="titleSmall"
+                        style={applyStyle(textStyle)}
+                        onLayout={event => {
+                            setLineHeight1(event.nativeEvent.layout.y);
+                        }}
+                    >
+                        Retrieving Balances
+                    </Text>
+                    <Text
+                        variant="titleSmall"
+                        style={applyStyle(textStyle)}
+                        onLayout={event => {
+                            setLineHeight2(event.nativeEvent.layout.y);
+                        }}
+                    >
+                        Confirming assets
+                    </Text>
+                    <Text
+                        variant="titleSmall"
+                        style={applyStyle(textStyle)}
+                        onLayout={event => {
+                            setLineHeight3(event.nativeEvent.layout.y);
+                        }}
+                    >
+                        Checking transactions
+                    </Text>
+                </Animated.View>
+            </Box>
         </Box>
     );
 };
