f3a1fe9a30066a91f4e79904acd4dd94b601b318
Nodonisko <suchydan@gmail.com>
2023-04-24 15:07:01 +0200

    fix(mobile): fix strange fix rates #8198

--- suite-native/assets/src/tests/fixtures/assets.ts
+++ suite-native/assets/src/tests/fixtures/assets.ts
@@ -1,66 +1,25 @@
 import BigNumber from 'bignumber.js';
 
+import { networks } from '@suite-common/wallet-config';
+
 import { AssetType } from '../../assetsSelectors';
 
 export const assetsFixtureZeroBalance: AssetType[] = [
     {
         symbol: 'btc',
-        network: {
-            symbol: 'btc',
-            name: 'Bitcoin',
-            networkType: 'bitcoin',
-            bip43Path: "m/84'/0'/i'",
-            decimals: 8,
-            explorer: {
-                tx: 'https://btc1.trezor.io/tx/',
-                account: 'https://btc1.trezor.io/xpub/',
-                address: 'https://btc1.trezor.io/address/',
-            },
-            features: ['rbf', 'sign-verify', 'amount-unit'],
-            customBackends: ['blockbook', 'electrum'],
-        },
+        network: networks.btc,
         assetBalance: BigNumber(0),
         fiatBalance: '0.00',
     },
     {
         symbol: 'eth',
-        network: {
-            symbol: 'eth',
-            name: 'Ethereum',
-            networkType: 'ethereum',
-            chainId: 1,
-            bip43Path: "m/44'/60'/0'/0/i",
-            decimals: 18,
-            explorer: {
-                tx: 'https://eth1.trezor.io/tx/',
-                account: 'https://eth1.trezor.io/address/',
-                address: 'https://eth1.trezor.io/address/',
-                nft: 'https://eth1.trezor.io/nft/',
-            },
-            features: ['rbf', 'sign-verify', 'tokens'],
-            label: 'TR_NETWORK_ETHEREUM_LABEL',
-            tooltip: 'TR_NETWORK_ETHEREUM_TOOLTIP',
-            customBackends: ['blockbook'],
-        },
+        network: networks.eth,
         assetBalance: BigNumber(0),
         fiatBalance: '0.00',
     },
     {
         symbol: 'ltc',
-        network: {
-            symbol: 'ltc',
-            name: 'Litecoin',
-            networkType: 'bitcoin',
-            bip43Path: "m/84'/2'/i'",
-            decimals: 8,
-            explorer: {
-                tx: 'https://ltc1.trezor.io/tx/',
-                account: 'https://ltc1.trezor.io/xpub/',
-                address: 'https://ltc1.trezor.io/address/',
-            },
-            features: ['sign-verify'],
-            customBackends: ['blockbook'],
-        },
+        network: networks.ltc,
         assetBalance: BigNumber(0),
         fiatBalance: '0.00',
     },
@@ -69,62 +28,19 @@ export const assetsFixtureZeroBalance: AssetType[] = [
 export const assetsFixtureWithBalance: AssetType[] = [
     {
         symbol: 'btc',
-        network: {
-            symbol: 'btc',
-            name: 'Bitcoin',
-            networkType: 'bitcoin',
-            bip43Path: "m/84'/0'/i'",
-            decimals: 8,
-            explorer: {
-                tx: 'https://btc1.trezor.io/tx/',
-                account: 'https://btc1.trezor.io/xpub/',
-                address: 'https://btc1.trezor.io/address/',
-            },
-            features: ['rbf', 'sign-verify', 'amount-unit'],
-            customBackends: ['blockbook', 'electrum'],
-        },
+        network: networks.btc,
         assetBalance: BigNumber(0.00569722),
         fiatBalance: '98.26',
     },
     {
         symbol: 'eth',
-        network: {
-            symbol: 'eth',
-            name: 'Ethereum',
-            networkType: 'ethereum',
-            chainId: 1,
-            bip43Path: "m/44'/60'/0'/0/i",
-            decimals: 18,
-            explorer: {
-                tx: 'https://eth1.trezor.io/tx/',
-                account: 'https://eth1.trezor.io/address/',
-                address: 'https://eth1.trezor.io/address/',
-                nft: 'https://eth1.trezor.io/nft/',
-            },
-            features: ['rbf', 'sign-verify', 'tokens'],
-            label: 'TR_NETWORK_ETHEREUM_LABEL',
-            tooltip: 'TR_NETWORK_ETHEREUM_TOOLTIP',
-            customBackends: ['blockbook'],
-        },
+        network: networks.eth,
         assetBalance: BigNumber(0.02142776807619),
         fiatBalance: '28.25',
     },
     {
         symbol: 'ltc',
-        network: {
-            symbol: 'ltc',
-            name: 'Litecoin',
-            networkType: 'bitcoin',
-            bip43Path: "m/84'/2'/i'",
-            decimals: 8,
-            explorer: {
-                tx: 'https://ltc1.trezor.io/tx/',
-                account: 'https://ltc1.trezor.io/xpub/',
-                address: 'https://ltc1.trezor.io/address/',
-            },
-            features: ['sign-verify'],
-            customBackends: ['blockbook'],
-        },
+        network: networks.ltc,
         assetBalance: BigNumber(0),
         fiatBalance: '0.00',
     },
@@ -133,20 +49,7 @@ export const assetsFixtureWithBalance: AssetType[] = [
 export const assetsFixtureSingleAsset: AssetType[] = [
     {
         symbol: 'btc',
-        network: {
-            symbol: 'btc',
-            name: 'Bitcoin',
-            networkType: 'bitcoin',
-            bip43Path: "m/84'/0'/i'",
-            decimals: 8,
-            explorer: {
-                tx: 'https://btc1.trezor.io/tx/',
-                account: 'https://btc1.trezor.io/xpub/',
-                address: 'https://btc1.trezor.io/address/',
-            },
-            features: ['rbf', 'sign-verify', 'amount-unit'],
-            customBackends: ['blockbook', 'electrum'],
-        },
+        network: networks.btc,
         assetBalance: BigNumber(0.00569722),
         fiatBalance: '98.26',
     },
