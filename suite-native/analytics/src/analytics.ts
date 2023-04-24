91c27a7408d35f19943775a73c8e08efb2f6a203
Petr Knetl <petr.knetl@satoshilabs.com>
2023-04-24 11:12:19 +0200

    fix(suite-native): analytics app version (#8196)

--- suite-native/analytics/src/analytics.ts
+++ suite-native/analytics/src/analytics.ts
@@ -1,9 +1,9 @@
-import { isDebugEnv } from '@suite-native/config';
+import { getAppVersion, isDebugEnv } from '@suite-native/config';
 import { Analytics } from '@trezor/analytics';
 
 import { SuiteNativeAnalyticsEvent } from './events';
 
-export const analytics = new Analytics<SuiteNativeAnalyticsEvent>(process.env.VERSION!, 'mobile');
+export const analytics = new Analytics<SuiteNativeAnalyticsEvent>(getAppVersion(), 'mobile');
 
 if (isDebugEnv()) {
     // Do not send analytics in development
