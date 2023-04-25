867db528cfed2bbc5bcdde654eda1520ea69901d
tomasklim <tomas.klim@gmail.com>
2023-04-25 15:32:26 +0200

    fix(suite): recovery select

--- packages/suite/src/components/recovery/SelectRecoveryWord.tsx
+++ packages/suite/src/components/recovery/SelectRecoveryWord.tsx
@@ -1,33 +1,16 @@
 import React from 'react';
 import { useRecovery } from '@suite-hooks';
-import TrezorConnect, { UI } from '@trezor/connect';
 import { WordInput, WordInputAdvanced } from '@suite-components';
-import { createTimeoutPromise } from '@trezor/utils';
 
 export const SelectRecoveryWord = () => {
     const { wordRequestInputType } = useRecovery();
 
     if (wordRequestInputType === 6 || wordRequestInputType === 9) {
-        return (
-            <WordInputAdvanced
-                count={wordRequestInputType}
-                onSubmit={async value => {
-                    await createTimeoutPromise(600);
-                    TrezorConnect.uiResponse({ type: UI.RECEIVE_WORD, payload: value });
-                }}
-            />
-        );
+        return <WordInputAdvanced count={wordRequestInputType} />;
     }
 
     if (wordRequestInputType === 'plain') {
-        return (
-            <WordInput
-                onSubmit={async value => {
-                    await createTimeoutPromise(600);
-                    TrezorConnect.uiResponse({ type: UI.RECEIVE_WORD, payload: value });
-                }}
-            />
-        );
+        return <WordInput />;
     }
 
     return null;
