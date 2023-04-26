0918b1743c9e3f9cfc90060fe8ce10d45ab6e6ad
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2023-04-26 13:42:23 +0200

    feat(suite-native): style address row

--- suite-native/transactions/src/components/TransactionDetail/TransactionDetailAddressesSheet.tsx
+++ suite-native/transactions/src/components/TransactionDetail/TransactionDetailAddressesSheet.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { AccountKey } from '@suite-common/wallet-types';
-import { BottomSheet, Box, Button, Card, Text, VStack } from '@suite-native/atoms';
+import {
+    BottomSheet,
+    Box,
+    Button,
+    Card,
+    IconButton,
+    Text,
+    Toggle,
+    VStack,
+} from '@suite-native/atoms';
 import { TransactionsRootState } from '@suite-common/wallet-core';
+import { useCopyToClipboard } from '@suite-native/helpers';
 
 import { selectTransactionInputAddresses, selectTransactionOutputAddresses } from '../../selectors';
 
@@ -14,7 +24,27 @@ type TransactionDetailInputsSheetProps = {
     onClose: () => void;
 };
 
+type ToggleAddressessValue = 'inputs' | 'outputs';
+
 const formatCountString = (count: number) => (count > 1 ? ` ·  ${count}` : null);
+
+const AddressRow = ({ address }: { address: string }) => {
+    const copyToClipboard = useCopyToClipboard();
+
+    const handleCopy = () => copyToClipboard(address, 'Address copied to clipboard');
+
+    return (
+        <Box key={address} flexDirection="row" justifyContent="space-between">
+            <Box style={{ maxWidth: '80%' }}>
+                <Text variant="hint">{address}</Text>
+                <Text variant="hint" color="textSubdued">
+                    0.0032453 BTC
+                </Text>
+            </Box>
+            <IconButton iconName="copy" colorScheme="primary" onPress={handleCopy} />
+        </Box>
+    );
+};
 
 export const TransactionDetailAddressesSheet = ({
     isVisible,
@@ -22,6 +52,7 @@ export const TransactionDetailAddressesSheet = ({
     txid,
     accountKey,
 }: TransactionDetailInputsSheetProps) => {
+    const [activeTab, setActiveTab] = useState<ToggleAddressessValue>('inputs');
     const inputAddresses = useSelector((state: TransactionsRootState) =>
         selectTransactionInputAddresses(state, txid, accountKey),
     );
@@ -29,41 +60,35 @@ export const TransactionDetailAddressesSheet = ({
         selectTransactionOutputAddresses(state, txid, accountKey),
     );
 
-    const sheetSubtitle = `Transaction #${txid}`;
-    const formattedInputsTitle = `From${formatCountString(inputAddresses.length)}`;
-    const formattedOutputsTitle = `To${formatCountString(outputAddresses.length)}`;
+    const addressesArray = activeTab === 'inputs' ? inputAddresses : outputAddresses;
 
     return (
         <BottomSheet
             isVisible={isVisible}
             onClose={onClose}
             title="Addresses"
-            subtitle={sheetSubtitle}
+            subtitle={`Transaction #${txid}`}
         >
-            {/* TODO: This is not a final solution. */}
-            {/* Layout need to be changed according to final design when ready. */}
-            <Box marginVertical="medium">
-                <Card>
-                    <VStack spacing="medium">
-                        <Box>
-                            <Text variant="highlight">{formattedInputsTitle}</Text>
-                            {inputAddresses.map(address => (
-                                <Text key={address}>{address}</Text>
+            <Box>
+                <Toggle
+                    isToggled={activeTab === 'outputs'}
+                    onToggle={() => setActiveTab(activeTab === 'inputs' ? 'outputs' : 'inputs')}
+                    leftLabel={`From${formatCountString(inputAddresses.length)}`}
+                    rightLabel={`To${formatCountString(outputAddresses.length)}`}
+                />
+                <Box marginVertical="medium">
+                    <Card>
+                        <VStack spacing="medium">
+                            {addressesArray.map(address => (
+                                <AddressRow key={address} address={address} />
                             ))}
-                        </Box>
-                        <Box>
-                            <Text variant="highlight">{formattedOutputsTitle}</Text>
-                            {outputAddresses.map(address => (
-                                <Text key={address}>{address}</Text>
-                            ))}
-                        </Box>
-                    </VStack>
-                </Card>
-
-                <Box marginTop="large" paddingHorizontal="small">
-                    <Button size="large" onPress={onClose}>
-                        Close
-                    </Button>
+                        </VStack>
+                    </Card>
+                    <Box marginTop="large" paddingHorizontal="small">
+                        <Button size="large" onPress={onClose}>
+                            Close
+                        </Button>
+                    </Box>
                 </Box>
             </Box>
         </BottomSheet>
