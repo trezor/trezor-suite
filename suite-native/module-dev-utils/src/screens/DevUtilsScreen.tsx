7d6f04a05aabd639d3b4b0a0b62ee128bef09171
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2023-04-28 10:24:51 +0200

    chore(suite-native): refactor dev utils screen

--- suite-native/module-dev-utils/src/screens/DevUtilsScreen.tsx
+++ suite-native/module-dev-utils/src/screens/DevUtilsScreen.tsx
@@ -4,7 +4,7 @@ import { Alert } from 'react-native';
 import * as Sentry from '@sentry/react-native';
 
 import { isDebugEnv, isDevelopOrDebugEnv } from '@suite-native/config';
-import { Box, Button, VStack } from '@suite-native/atoms';
+import { Box, Button, Card, VStack } from '@suite-native/atoms';
 import {
     Screen,
     StackProps,
@@ -16,28 +16,37 @@ import { clearStorage } from '@suite-native/storage';
 
 import { BuildInfo } from '../components/BuildInfo';
 import { RenderingUtils } from '../components/RenderingUtils';
-import { ProductionDevInfo, SentryTestErrorButton } from '../components/ProductionDevInfo';
 import { CopyLogsButton } from '../components/CopyLogsButton';
 
 export const DevUtilsScreen = ({
     navigation,
 }: StackProps<DevUtilsStackParamList, DevUtilsStackRoutes.DevUtils>) => (
     <Screen header={<ScreenHeader content="DEV utils" hasGoBackIcon />}>
-        {isDevelopOrDebugEnv() ? (
-            <Box marginBottom="large">
-                <VStack spacing="medium">
-                    {!isDebugEnv() && <BuildInfo />}
-                    <RenderingUtils />
-                    <Button onPress={() => navigation.navigate(DevUtilsStackRoutes.Demo)}>
-                        See Component Demo
-                    </Button>
-                    <Button onPress={clearStorage}>Reset storage</Button>
-                    <SentryTestErrorButton />
-                    <CopyLogsButton />
-                </VStack>
-            </Box>
-        ) : (
-            <ProductionDevInfo />
-        )}
+        <Card>
+            <VStack spacing="medium">
+                {!isDebugEnv() && <BuildInfo />}
+                {isDebugEnv() && (
+                    <>
+                        <Button onPress={() => navigation.navigate(DevUtilsStackRoutes.Demo)}>
+                            See Component Demo
+                        </Button>
+                        <RenderingUtils />
+                    </>
+                )}
+                <Button
+                    onPress={() => {
+                        const errorMessage = `Sentry test error - ${Date.now()}`;
+                        Sentry.captureException(new Error(errorMessage));
+                        Alert.alert('Sentry error thrown', errorMessage);
+                    }}
+                >
+                    Throw Sentry error
+                </Button>
+                <CopyLogsButton />
+                <Button colorScheme="dangerElevation0" onPress={clearStorage}>
+                    Reset storage
+                </Button>
+            </VStack>
+        </Card>
     </Screen>
 );
