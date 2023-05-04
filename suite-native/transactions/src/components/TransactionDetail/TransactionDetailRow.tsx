f49a4d3e3c7e43523247a991684dba843b0d2d29
Petr Knetl <petr.knetl@satoshilabs.com>
2023-05-04 14:14:30 +0200

    fix(suite-native): small screen polishing

--- suite-native/transactions/src/components/TransactionDetail/TransactionDetailRow.tsx
+++ suite-native/transactions/src/components/TransactionDetail/TransactionDetailRow.tsx
@@ -17,12 +17,21 @@ const rowStyle = prepareNativeStyle(utils => ({
     marginVertical: utils.spacings.small,
 }));
 
+const valueContainerStyle = prepareNativeStyle(_ => ({
+    flexShrink: 1,
+}));
+
 export const TransactionDetailRow = ({ title, children }: TransactionDetailRowProps) => {
     const { applyStyle } = useNativeStyles();
     return (
         <Box style={applyStyle(rowStyle)}>
             <Text color="textSubdued">{title}</Text>
-            <Box flexDirection="row" justifyContent="flex-start" alignItems="center">
+            <Box
+                flexDirection="row"
+                justifyContent="flex-end"
+                alignItems="center"
+                style={applyStyle(valueContainerStyle)}
+            >
                 {G.isString(children) || G.isNumber(children) ? <Text>{children}</Text> : children}
             </Box>
         </Box>
