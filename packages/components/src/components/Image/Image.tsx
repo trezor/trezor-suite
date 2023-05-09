d79099bf72028669181ca158a8cb61f41756fc4f
Martin Varmuza <mvarmuza@gmail.com>
2023-05-09 13:50:04 +0200

    chore(components): inline resolveStaticPath in components

--- packages/components/src/components/Image/Image.tsx
+++ packages/components/src/components/Image/Image.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import { resolveStaticPath } from '@suite-common/suite-utils';
 import { PngImage, SvgImage, PNG_IMAGES, SVG_IMAGES } from './images';
 
 const PNG_PATH = 'images/png';
@@ -11,6 +10,15 @@ const StyledImage = styled.img`
     max-width: 100%;
     filter: ${({ theme }) => theme.IMAGE_FILTER};
 `;
+
+// todo: this should be shared with resolveStaticPath in suite-common/suite-utils/src/resolveStaticPath
+// the problem is that @trezor scoped package must not import from @suite-common scoped package
+// followup: create SuiteImage component wrapper that will use resolveStaticPath util and pass it to Image component
+// https://github.com/trezor/trezor-suite/issues/8433
+export const resolveStaticPath = (
+    path: string,
+    pathPrefix: string | undefined = process.env.ASSET_PREFIX,
+) => `${pathPrefix || ''}/static/${path.replace(/^\/+/, '')}`;
 
 const buildSrcSet = <
     BasePath extends string,
