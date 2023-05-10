3ddc65d77ed9cd51b604f3fe1bac2d58b62972dd
Martin Varmuza <mvarmuza@gmail.com>
2023-05-10 09:08:39 +0200

    chore(urls): remove unused

--- packages/urls/tests/urls.test.ts
+++ packages/urls/tests/urls.test.ts
@@ -9,11 +9,9 @@ const excluded = [
     // TODO: it works locally but CI times out, probably cant handle the redirect or something..
     URLS.DATA_TOS_URL,
     // 503 from CI
-    URLS.TREZOR_BLOG_URL,
     URLS.LTC_ADDRESS_INFO_URL,
     // T2B1 urls, they don't exist yet
     URLS.HELP_CENTER_DRY_RUN_TR_URL,
-    URLS.HELP_CENTER_PACKAGING_TR_URL,
     URLS.HELP_CENTER_FW_DOWNGRADE_TR_URL,
 ];
 
