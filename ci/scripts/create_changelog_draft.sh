88d88cc3e73754bc0eaffbdf7cca2488a2e7c9c3
Martin Varmuza <mvarmuza@gmail.com>
2023-05-13 18:09:31 +0200

    chore(connect): improve release scripts

--- ci/scripts/create_changelog_draft.sh
+++ ci/scripts/create_changelog_draft.sh
@@ -3,16 +3,15 @@
 # creates changelog draft for package passed in argument
 
 changelog=""
+# line_break=$'\n'
 
 git log --oneline -- ./packages/"$1" | (
     while read -r commit; do
-        if [[ $commit == *"release"* ]] &&  [[ $commit == *"@trezor/$1 "* ]];
+        if [[ $commit == *"npm-release: @trezor/$1 "* ]];
         then
-            echo "${changelog}"
+            printf "${changelog}"
             exit 0
         fi
-        changelog="${changelog}${commit}'\n'"
+        changelog="${changelog}\n${commit}"
     done
-
-    # "commit message containing something like 'release: @trezor/$1' in 'git log --oneline -- ./packages/$1' was not found  "
 )
