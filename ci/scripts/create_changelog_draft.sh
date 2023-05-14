42ad477e803e2db84c17c6820e0c4b3206fd1ecf
Martin Varmuza <mvarmuza@gmail.com>
2023-05-14 19:49:01 +0200

    works locally

--- ci/scripts/create_changelog_draft.sh
+++ ci/scripts/create_changelog_draft.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
 # creates changelog draft for package passed in argument
+set -x -o pipefail
 
 changelog=""
+line_break="*"
 
 git log --oneline -- ./packages/"$1" | (
     while read -r commit; do
         if [[ $commit == *"npm-release: @trezor/$1 "* ]];
         then
-            printf "%s" "${changelog}"
+            echo "${changelog}"
             exit 0
         fi
-        changelog=$(printf "%s\n%s" "${changelog}" "${commit}")
+        changelog=$(echo "${changelog} ${commit}${line_break}")
     done
 )
