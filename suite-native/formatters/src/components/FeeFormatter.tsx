f4c74f9f7cc06e6d54d8dd7bd58790f492472693
Petr Knetl <petr.knetl@satoshilabs.com>
2023-05-17 12:42:20 +0200

    feat(suite-native): fee formatter (#8418)

---
+++ suite-native/formatters/src/components/FeeFormatter.tsx
@@ -0,0 +1,28 @@
+import React, { useMemo } from 'react';
+
+import { fromWei } from 'web3-utils';
+
+import { Text } from '@suite-native/atoms';
+import { WalletAccountTransaction } from '@suite-common/wallet-types';
+import { getFeeRate, getFeeUnits } from '@suite-common/wallet-utils';
+import { networks } from '@suite-common/wallet-config';
+
+type FeeFormatterProps = {
+    transaction: WalletAccountTransaction;
+};
+
+export const FeeFormatter = ({ transaction }: FeeFormatterProps) => {
+    const { networkType } = networks[transaction.symbol];
+
+    const formattedValue = useMemo(
+        () =>
+            networkType === 'ethereum'
+                ? fromWei(transaction.ethereumSpecific?.gasPrice ?? '0', 'gwei')
+                : getFeeRate(transaction),
+        [networkType, transaction],
+    );
+
+    const formattedUnit = getFeeUnits(networkType);
+
+    return <Text>{`${formattedValue} ${formattedUnit}`}</Text>;
+};
