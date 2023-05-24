56238ceba16a34d02ea9425f7a99f0cfaede6138
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2023-05-24 11:55:50 +0200

    fix(suite-native): hotfix for app dev

---
+++ suite-common/wallet-utils/src/sendFormUtils.native.ts
@@ -0,0 +1,2 @@
+// Note: This is hotfix for sendFormUtils as there are things that make mobile app crash on local development
+// zlib is required in @ethereumjs and is not supported in mobile app. This file will prevent that package from being included in the bundle.
