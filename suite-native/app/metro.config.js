bae4be59bedbe68938e6ab60ab2c446f79dc1e2a
Petr Knetl <petr.knetl@satoshilabs.com>
2023-05-25 15:20:31 +0200

    fix(suite-native): zlib mocked up from node.js implementation (#8581)

--- suite-native/app/metro.config.js
+++ suite-native/app/metro.config.js
@@ -19,6 +19,7 @@ module.exports = makeMetroConfig({
             stream: nodejs.stream,
             https: nodejs.https,
             http: nodejs.http,
+            zlib: nodejs.zlib,
         },
     },
 });
