9c29f5311a6d0d25b0a4f5ea8853c36f98b38ea3
Matej Kriz <matej@matejkriz.cz>
2023-05-26 12:12:01 +0200

    feat(suite): make cancel error message translatable

--- packages/suite/src/components/suite/modals/Word.tsx
+++ packages/suite/src/components/suite/modals/Word.tsx
@@ -2,27 +2,33 @@ import React from 'react';
 import TrezorConnect from '@trezor/connect';
 import { Translation, WordInput, Modal, ModalProps } from '@suite-components';
 import styled from 'styled-components';
+import { useIntl } from 'react-intl';
+import messages from '@suite/support/messages';
 
 const StyledModal = styled(Modal)`
     min-height: 450px;
 `;
 
-export const Word = (props: ModalProps) => (
-    <StyledModal
-        data-test="@recovery/word"
-        heading={<Translation id="TR_FOLLOW_INSTRUCTIONS_ON_DEVICE" />}
-        description={
-            <>
-                <Translation id="TR_ENTER_SEED_WORDS_INSTRUCTION" />{' '}
-                <Translation id="TR_RANDOM_SEED_WORDS_DISCLAIMER" />
-            </>
-        }
-        onCancel={() => TrezorConnect.cancel('cancelled')}
-        isCancelable
-        totalProgressBarSteps={5}
-        currentProgressBarStep={4}
-        {...props}
-    >
-        <WordInput />
-    </StyledModal>
-);
+export const Word = (props: ModalProps) => {
+    const intl = useIntl();
+
+    return (
+        <StyledModal
+            data-test="@recovery/word"
+            heading={<Translation id="TR_FOLLOW_INSTRUCTIONS_ON_DEVICE" />}
+            description={
+                <>
+                    <Translation id="TR_ENTER_SEED_WORDS_INSTRUCTION" />{' '}
+                    <Translation id="TR_RANDOM_SEED_WORDS_DISCLAIMER" />
+                </>
+            }
+            onCancel={() => TrezorConnect.cancel(intl.formatMessage(messages.TR_CANCELLED))}
+            isCancelable
+            totalProgressBarSteps={5}
+            currentProgressBarStep={4}
+            {...props}
+        >
+            <WordInput />
+        </StyledModal>
+    );
+};
--- packages/suite/src/components/suite/modals/WordAdvanced.tsx
+++ packages/suite/src/components/suite/modals/WordAdvanced.tsx
@@ -4,6 +4,8 @@ import TrezorConnect from '@trezor/connect';
 import { P } from '@trezor/components';
 import { HELP_CENTER_ADVANCED_RECOVERY_URL } from '@trezor/urls';
 import { Translation, WordInputAdvanced, TrezorLink, Modal, ModalProps } from '@suite-components';
+import { useIntl } from 'react-intl';
+import messages from '@suite/support/messages';
 
 const ContentWrapper = styled.div`
     display: flex;
@@ -23,26 +25,30 @@ interface WordAdvancedProps extends ModalProps {
     count: 6 | 9;
 }
 
-export const WordAdvanced = ({ count, ...rest }: WordAdvancedProps) => (
-    <StyledModal
-        heading={<Translation id="TR_FOLLOW_INSTRUCTIONS_ON_DEVICE" />}
-        description={<Translation id="TR_ADVANCED_RECOVERY_TEXT" />}
-        onCancel={() => TrezorConnect.cancel('cancelled')}
-        isCancelable
-        totalProgressBarSteps={5}
-        currentProgressBarStep={4}
-        {...rest}
-    >
-        <ContentWrapper>
-            <WordInputAdvanced count={count} />
-            <BottomText>
-                <P size="tiny">
-                    <Translation id="TR_ADVANCED_RECOVERY_NOT_SURE" />{' '}
-                    <TrezorLink size="tiny" href={HELP_CENTER_ADVANCED_RECOVERY_URL}>
-                        <Translation id="TR_LEARN_MORE" />
-                    </TrezorLink>
-                </P>
-            </BottomText>
-        </ContentWrapper>
-    </StyledModal>
-);
+export const WordAdvanced = ({ count, ...rest }: WordAdvancedProps) => {
+    const intl = useIntl();
+
+    return (
+        <StyledModal
+            heading={<Translation id="TR_FOLLOW_INSTRUCTIONS_ON_DEVICE" />}
+            description={<Translation id="TR_ADVANCED_RECOVERY_TEXT" />}
+            onCancel={() => TrezorConnect.cancel(intl.formatMessage(messages.TR_CANCELLED))}
+            isCancelable
+            totalProgressBarSteps={5}
+            currentProgressBarStep={4}
+            {...rest}
+        >
+            <ContentWrapper>
+                <WordInputAdvanced count={count} />
+                <BottomText>
+                    <P size="tiny">
+                        <Translation id="TR_ADVANCED_RECOVERY_NOT_SURE" />{' '}
+                        <TrezorLink size="tiny" href={HELP_CENTER_ADVANCED_RECOVERY_URL}>
+                            <Translation id="TR_LEARN_MORE" />
+                        </TrezorLink>
+                    </P>
+                </BottomText>
+            </ContentWrapper>
+        </StyledModal>
+    );
+};
