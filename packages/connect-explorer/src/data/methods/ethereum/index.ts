e58c41643ab7bc7989b124cebc05540979217d58
Martin Varmuza <mvarmuza@gmail.com>
2023-06-05 22:24:48 +0200

    feat(connect-explorer): update eth examples

--- packages/connect-explorer/src/data/methods/ethereum/index.ts
+++ packages/connect-explorer/src/data/methods/ethereum/index.ts
@@ -2,7 +2,8 @@ import getPublicKey from './getPublicKey';
 import getAddress from './getAddress';
 import getAccountInfo from './getAccountInfo';
 import signTransaction from './signTransaction';
-import signTransactionERC20 from './signTransaction.erc20';
+import signTransactionERC20 from './signTransaction-erc20-known';
+import signTransactionERC20Unknown from './signTransaction-erc20-unknown';
 import pushTransaction from './pushTransaction';
 import signMessage from './signMessage';
 import verifyMessage from './verifyMessage';
@@ -14,6 +15,7 @@ export default [
     ...getAccountInfo,
     ...signTransaction,
     ...signTransactionERC20,
+    ...signTransactionERC20Unknown,
     ...pushTransaction,
     // ...composeTransaction,
     ...signMessage,
--- packages/connect-explorer/src/data/methods/ethereum/signTransaction-erc20-unknown.ts
+++ packages/connect-explorer/src/data/methods/ethereum/signTransaction-erc20-unknown.ts
@@ -5,14 +5,17 @@ const tx = `{
     nonce: '0x0',
     gasPrice: '0x14',
     gasLimit: '0x14',
-    to: '0xd0d6d6c5fe4a677d343cc433536bb717bae167dd',
+    to: '0x26fb86579e371c7AEdc461b2DdEF0A8628c93d3B',
     chainId: 1,
-    value: '1',
+    value: '0x0',
+    data: '0xa9059cbb000000000000000000000000D6971aabeDC7f2A8113679199FE374aE1B1Aea96000000000000000000000000000000000000000000000000000000000097f6b2',
 }`;
 
+// sending erc-20 unknown token (unknown to firmware)
+// https://etherscan.io/token/0x26fb86579e371c7aedc461b2ddef0a8628c93d3b
 export default [
     {
-        url: '/method/ethereumSignTransaction',
+        url: '/method/ethereumSignTransaction-erc20-unknown',
         name,
         docs,
         submitButton: 'Sign transaction',
