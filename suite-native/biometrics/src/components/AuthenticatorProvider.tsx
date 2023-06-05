27e9e95f0a338eddb49d10ae2e3399ee01bf3c97
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2023-06-05 13:56:58 +0200

    chore(suite-native): alerts

--- suite-native/biometrics/src/components/AuthenticatorProvider.tsx
+++ suite-native/biometrics/src/components/AuthenticatorProvider.tsx
@@ -1,19 +1,12 @@
 import React, { ReactNode } from 'react';
 
-import { BiometricOverlay } from './BiometricOverlay';
-import { useBiometrics } from '../useBiometrics';
-
 type AuthenticatorProviderProps = {
     children: ReactNode;
 };
 
-export const AuthenticatorProvider = ({ children }: AuthenticatorProviderProps) => {
-    const { shouldShowBiometricOverlay } = useBiometrics();
-
-    return (
-        <>
-            {children}
-            {shouldShowBiometricOverlay && <BiometricOverlay />}
-        </>
-    );
-};
+export const AuthenticatorProvider = ({ children }: AuthenticatorProviderProps) => (
+    <>
+        {children}
+        {/* {shouldShowBiometricOverlay && isBiometricsOptionEnabled && <BiometricOverlay />} */}
+    </>
+);
--- suite-native/biometrics/src/useBiometrics.tsx
+++ suite-native/biometrics/src/useBiometrics.tsx
@@ -3,6 +3,8 @@ import { AppState, Platform } from 'react-native';
 
 import * as LocalAuthentication from 'expo-local-authentication';
 
+import { Alert, useAlert } from '@suite-native/alerts';
+
 import { useIsBiometricsEnabled } from './biometricsAtoms';
 import { getIsBiometricsFeatureAvailable } from './isBiometricsFeatureAvailable';
 
@@ -17,20 +19,50 @@ const authenticate = async () => {
         const result = await LocalAuthentication.authenticateAsync();
         return result;
     }
-    // TODO else return false and display alert that biometrics is not available
+};
+
+const authenticationCanceledAlert: Alert = {
+    title: 'Authentication canceled',
+    description: 'You will have to try again.',
+    pictogramVariant: 'red',
+    icon: 'warningCircle',
+    onPressPrimaryButton: authenticate,
+    primaryButtonTitle: 'Try again',
+    secondaryButtonTitle: 'Cancel',
+    onPressSecondaryButton: () => null,
+};
+
+const biometricNotAvailableAlert: Alert = {
+    title: 'Biometrics',
+    description:
+        'No security features on your device. Make sure you have biometrics setup on your phone and try again.',
+    primaryButtonTitle: 'Cancel',
+    onPressPrimaryButton: () => null,
+    icon: 'warningCircle',
+    pictogramVariant: 'yellow',
 };
 
 export const useBiometrics = () => {
     const appState = useRef(AppState.currentState);
     const { isBiometricsOptionEnabled, setIsBiometricsOptionEnabled } = useIsBiometricsEnabled();
     const [isUserAuthenticated, setIsUserAuthenticated] = useState(!isBiometricsOptionEnabled);
-    const isBiometricsModalOpen = useRef(false);
-    const [shouldShowBiometricOverlay, setShouldShowBiometricOverlay] = useState(false);
+    const { showAlert } = useAlert();
 
     const toggleBiometricsOption = useCallback(async () => {
         try {
-            const result = await authenticate();
-            console.log('result', result);
+            const isBiometricsAvailable = await getIsBiometricsFeatureAvailable();
+
+            if (!isBiometricsAvailable) {
+                showAlert(biometricNotAvailableAlert);
+                return;
+            }
+
+            const authResult = await authenticate();
+
+            if (!authResult?.success) {
+                return;
+            }
+
             if (isBiometricsOptionEnabled) {
                 setIsBiometricsOptionEnabled(false);
                 setIsUserAuthenticated(false);
@@ -41,41 +73,54 @@ export const useBiometrics = () => {
         } catch (error) {
             console.error(error);
         }
-    }, [isBiometricsOptionEnabled, setIsBiometricsOptionEnabled]);
+    }, [isBiometricsOptionEnabled, setIsBiometricsOptionEnabled, showAlert]);
 
     useEffect(() => {
         const subscription = AppState.addEventListener('change', nextAppState => {
             if (appState.current === 'background' && nextAppState === 'active') {
-                setShouldShowBiometricOverlay(true);
                 if (isBiometricsOptionEnabled && !isUserAuthenticated) {
                     const auth = async () => {
                         try {
                             const result = await authenticate();
 
-                            if (result && result?.success) {
-                                console.log('user authenticated');
+                            const resultHasError = result && !result.success;
 
+                            if (resultHasError && result.error.startsWith('unknown: -1000')) {
+                                // User don't need to authenticate at this point. The library doesn't accept authentication that's been less than few seconds after closing app.
+                                return;
+                            }
+
+                            const authenticationFailedErrors =
+                                resultHasError && result.error === 'user_cancel';
+
+                            if (authenticationFailedErrors) {
+                                showAlert(authenticationCanceledAlert);
+                            }
+
+                            if (result && result?.success) {
                                 setIsUserAuthenticated(true);
                             }
                         } catch (error) {
                             console.error(error);
                         }
-                        setShouldShowBiometricOverlay(false);
                     };
                     auth();
                 }
             }
+
+            if (appState.current === 'active' && nextAppState !== 'active') {
+                setIsUserAuthenticated(false);
+            }
+
             appState.current = nextAppState;
         });
 
         return () => subscription.remove();
-    }, [isBiometricsOptionEnabled, isUserAuthenticated, setIsUserAuthenticated]);
+    }, [isBiometricsOptionEnabled, isUserAuthenticated, setIsUserAuthenticated, showAlert]);
 
     return {
         toggleBiometricsOption,
         isUserAuthenticated,
         isBiometricsOptionEnabled,
-        isBiometricsModalOpen,
-        shouldShowBiometricOverlay,
     };
 };
