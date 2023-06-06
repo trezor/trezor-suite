eebff50a70f90d9755de2c5838d149a52cae3f34
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2023-06-06 13:56:21 +0200

    fix(suite-native): toggle in settings, provider removed

--- suite-native/biometrics/src/useBiometrics.tsx
+++ suite-native/biometrics/src/useBiometrics.tsx
@@ -1,69 +1,44 @@
 import { useCallback, useEffect, useRef } from 'react';
-import { AppState, Platform } from 'react-native';
+import { AppState } from 'react-native';
 
 import * as LocalAuthentication from 'expo-local-authentication';
-
-import { useAlert, Alert } from '@suite-native/alerts';
 
 import { getIsBiometricsFeatureAvailable } from './isBiometricsFeatureAvailable';
 import { useIsBiometricsEnabled, useIsUserAuthenticated } from './biometricsAtoms';
 
-const authenticate = async () => {
-    if (Platform.OS === 'android') {
-        // Cancel any previous authentication attempt. (Only available for android)
-        await LocalAuthentication.cancelAuthenticate();
-    }
-
+export const authenticate = async () => {
     const isBiometricsAvailable = await getIsBiometricsFeatureAvailable();
+
     if (isBiometricsAvailable) {
         const result = await LocalAuthentication.authenticateAsync();
         return result;
     }
 };
 
-const biometricNotAvailableAlert: Alert = {
-    title: 'Biometrics',
-    description:
-        'No security features on your device. Make sure you have biometrics setup on your phone and try again.',
-    primaryButtonTitle: 'Cancel',
-    onPressPrimaryButton: () => null,
-    icon: 'warningCircle',
-    pictogramVariant: 'yellow',
-};
-
 export const useBiometrics = () => {
-    const { isBiometricsOptionEnabled, setIsBiometricsOptionEnabled } = useIsBiometricsEnabled();
+    const { isBiometricsOptionEnabled } = useIsBiometricsEnabled();
     const appState = useRef(AppState.currentState);
     const { isUserAuthenticated, setIsUserAuthenticated } = useIsUserAuthenticated();
-    const { showAlert } = useAlert();
 
-    const toggleBiometricsOption = useCallback(async () => {
-        const isBiometricsAvailable = await getIsBiometricsFeatureAvailable();
+    const handleAuthentication = useCallback(async () => {
+        if (isBiometricsOptionEnabled && !isUserAuthenticated) {
+            const result = await authenticate();
 
-        if (!isBiometricsAvailable) {
-            showAlert(biometricNotAvailableAlert);
-            return;
+            const resultHasError = result && !result.success;
+
+            // In some cases, if auth happens too quickly after closing app, it will fail with unknown error.
+            // User don't need to authenticate at this point. The library doesn't accept authentication that's been less than few seconds after closing app.
+            if (resultHasError && result.error.startsWith('unknown:')) {
+                return;
+            }
+
+            if (result && result?.success) {
+                setIsUserAuthenticated(true);
+            } else {
+                handleAuthentication();
+            }
         }
-
-        const authResult = await authenticate();
-
-        if (!authResult?.success) {
-            return;
-        }
-
-        if (isBiometricsOptionEnabled) {
-            setIsBiometricsOptionEnabled(false);
-            setIsUserAuthenticated(false);
-        } else {
-            setIsBiometricsOptionEnabled(true);
-            setIsUserAuthenticated(true);
-        }
-    }, [
-        isBiometricsOptionEnabled,
-        setIsBiometricsOptionEnabled,
-        setIsUserAuthenticated,
-        showAlert,
-    ]);
+    }, [isBiometricsOptionEnabled, isUserAuthenticated, setIsUserAuthenticated]);
 
     useEffect(() => {
         const subscription = AppState.addEventListener('change', nextAppState => {
@@ -71,33 +46,7 @@ export const useBiometrics = () => {
             if (appState.current === 'background' && nextAppState === 'active') {
                 if (isBiometricsOptionEnabled && !isUserAuthenticated) {
                     const auth = async () => {
-                        const result = await authenticate();
-
-                        const resultHasError = result && !result.success;
-
-                        // In some cases, if auth happens too quickly after closing app, it will fail with unknown error.
-                        // User don't need to authenticate at this point. The library doesn't accept authentication that's been less than few seconds after closing app.
-                        if (resultHasError && result.error.startsWith('unknown:')) {
-                            return;
-                        }
-
-                        const authenticationFailedErrors =
-                            resultHasError && result.error === 'user_cancel';
-
-                        if (authenticationFailedErrors) {
-                            showAlert({
-                                title: 'Authentication canceled',
-                                description: 'You will have to try again.',
-                                pictogramVariant: 'red',
-                                icon: 'warningCircle',
-                                onPressPrimaryButton: auth,
-                                primaryButtonTitle: 'Try again',
-                            });
-                        }
-
-                        if (result && result?.success) {
-                            setIsUserAuthenticated(true);
-                        }
+                        await handleAuthentication();
                     };
                     auth();
                 }
@@ -111,26 +60,21 @@ export const useBiometrics = () => {
         });
 
         return () => subscription.remove();
-    }, [isBiometricsOptionEnabled, isUserAuthenticated, setIsUserAuthenticated, showAlert]);
+    }, [
+        handleAuthentication,
+        isBiometricsOptionEnabled,
+        isUserAuthenticated,
+        setIsUserAuthenticated,
+    ]);
 
     // First authentication after app being opened after being killed (not just backgrounded)
     useEffect(() => {
         const auth = async () => {
-            if (isBiometricsOptionEnabled && !isUserAuthenticated) {
-                const result = await authenticate();
-
-                if (result && result.success) {
-                    setIsUserAuthenticated(true);
-                }
-            }
+            await handleAuthentication();
         };
 
         auth();
         // Only run once on app start from killed state
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
-
-    return {
-        toggleBiometricsOption,
-    };
 };
