01e67c71f7f762835e66f1d8fce3b1488ace9635
tomasklim <tomas.klim@gmail.com>
2023-06-12 16:26:02 +0200

    feat(analytics): requests outlive the app session

--- packages/analytics/src/tests/analytics.test.ts
+++ packages/analytics/src/tests/analytics.test.ts
@@ -48,7 +48,7 @@ describe('analytics', () => {
         expect(global.fetch).toHaveBeenNthCalledWith(
             1,
             `https://data.trezor.io/${app}/log/${environment}/develop.log?c_v=1.18&c_type=${actionType}&c_commit=${commitId}&c_instance_id=${instanceId}&c_session_id=${sessionId}&c_timestamp=${timestamp}&c_message_id=random`,
-            { method: 'GET' },
+            { method: 'GET', keepalive: true },
         );
         expect(global.fetch).toHaveBeenCalledTimes(1);
 
@@ -92,7 +92,7 @@ describe('analytics', () => {
         expect(global.fetch).toHaveBeenNthCalledWith(
             1,
             `https://data.trezor.io/${app}/log/stable.log?c_v=1.18&c_type=${actionType}&c_commit=${commitId}&c_instance_id=${instanceId}&c_session_id=${sessionId}&c_timestamp=${timestamp}&c_message_id=random`,
-            { method: 'GET' },
+            { method: 'GET', keepalive: true },
         );
         expect(global.fetch).toHaveBeenCalledTimes(1);
     });
