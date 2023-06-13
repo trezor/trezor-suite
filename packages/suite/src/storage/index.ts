d6752e1a57cad92f20db53394642997c687fcab2
Marek Polak <marek.polak@satoshilabs.com>
2023-06-13 12:19:15 +0200

    refactor(suite): simplify idb migrations

--- packages/suite/src/storage/index.ts
+++ packages/suite/src/storage/index.ts
@@ -11,87 +11,18 @@ const VERSION = 36; // don't forget to add migration and CHANGELOG when changing
  *  Otherwise runs a migration function that transform the data to new scheme version if necessary
  */
 const onUpgrade: OnUpgradeFunc<SuiteDBSchema> = async (db, oldVersion, newVersion, transaction) => {
-    let shouldInitDB = oldVersion === 0;
     if (oldVersion > 0 && oldVersion < 13) {
         // just delete whole db as migrations from version older than 13 (internal releases) are not implemented
         try {
             await SuiteDB.removeStores(db);
-            shouldInitDB = true;
         } catch (err) {
             console.error('Storage: Error during removing all stores', err);
+            throw err;
         }
     }
 
-    if (shouldInitDB) {
-        // init db
-        // object store for wallet transactions
-        const txsStore = db.createObjectStore('txs', {
-            keyPath: ['tx.deviceState', 'tx.descriptor', 'tx.txid', 'tx.type'],
-        });
-        txsStore.createIndex('txid', 'tx.txid', { unique: false });
-        txsStore.createIndex('order', 'order', { unique: false });
-        txsStore.createIndex('blockTime', 'tx.blockTime', { unique: false });
-        txsStore.createIndex('deviceState', 'tx.deviceState', { unique: false });
-        txsStore.createIndex('accountKey', ['tx.descriptor', 'tx.symbol', 'tx.deviceState'], {
-            unique: false,
-        });
-
-        // object store for settings
-        db.createObjectStore('suiteSettings');
-        db.createObjectStore('walletSettings');
-
-        // object store for devices
-        db.createObjectStore('devices');
-        // object store for accounts
-        const accountsStore = db.createObjectStore('accounts', {
-            keyPath: ['descriptor', 'symbol', 'deviceState'],
-        });
-        accountsStore.createIndex('deviceState', 'deviceState', { unique: false });
-
-        // object store for discovery
-        db.createObjectStore('discovery', { keyPath: 'deviceState' });
-
-        // object store for send form
-        db.createObjectStore('sendFormDrafts');
-
-        db.createObjectStore('fiatRates');
-        db.createObjectStore('analytics');
-
-        // graph
-        const graphStore = db.createObjectStore('graph', {
-            keyPath: ['account.descriptor', 'account.symbol', 'account.deviceState'],
-        });
-
-        graphStore.createIndex('accountKey', [
-            'account.descriptor',
-            'account.symbol',
-            'account.deviceState',
-        ]);
-
-        graphStore.createIndex('deviceState', 'account.deviceState');
-
-        db.createObjectStore('coinmarketTrades', { keyPath: 'key' });
-
-        // metadata
-        db.createObjectStore('metadata');
-
-        db.createObjectStore('messageSystem');
-        db.createObjectStore('formDrafts');
-
-        db.createObjectStore('backendSettings');
-
-        // firmware. added in 28
-        db.createObjectStore('firmware');
-
-        // coinjoin, added in 32
-        db.createObjectStore('coinjoinAccounts');
-
-        // coinjoin settings
-        db.createObjectStore('coinjoinDebugSettings');
-    } else {
-        // migrate functions
-        await migrate(db, oldVersion, newVersion, transaction);
-    }
+    // migrate functions
+    await migrate(db, oldVersion, newVersion, transaction);
 };
 
 export const db = new SuiteDB<SuiteDBSchema>('trezor-suite', VERSION, onUpgrade, reloadApp);
