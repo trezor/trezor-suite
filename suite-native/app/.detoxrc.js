dc66988c61ae5dd440e87ffe1e79e4d77a273532
jakub.ondracek <jakub.ondracek@satoshilabs.com>
2023-06-19 14:56:00 +0200

    adding detox changes

--- suite-native/app/.detoxrc.js
+++ suite-native/app/.detoxrc.js
@@ -22,7 +22,7 @@ module.exports = {
     },
     'android.debug': {
       type: 'android.apk',
-      binaryPath: 'android/app/build/outputs/apk/debug/app-debug.apk',
+      binaryPath: 'android/app/build/outputs/apk/dev/debug/app-dev-debug.apk',
       build: 'cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug',
       reversePorts: [
         8081
@@ -50,7 +50,7 @@ module.exports = {
     emulator: {
       type: 'android.emulator',
       device: {
-        avdName: 'Pixel_3a_API_30_x86'
+        avdName: 'Pixel_5_API_33'
       }
     }
   },
--- suite-native/app/e2e/starter.test.js
+++ suite-native/app/e2e/starter.test.js
@@ -4,13 +4,15 @@ describe('Example', () => {
     });
 
     beforeEach(async () => {
-        // await device.reloadReactNative();
+        await device.reloadReactNative();
     });
 
     it('should have welcome screen', async () => {
         await device.takeScreenshot('first screen');
         // await waitFor(element(by.id('@onboarding/Welcome/nextBtn')))
-        await expect(element(by.text('Trezor'))).toExist();
+        const nextBtn = await element(by.id('@onboarding/Welcome/nextBtn'));
+        await expect(nextBtn).toBeVisible();
+        await nextBtn.tap();
         await device.takeScreenshot('next screen');
     });
 });
