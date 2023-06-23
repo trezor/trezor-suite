6f4ac6e5eedea374ad91edb2aa54bc5c3fb82408
Martin Varmuza <mvarmuza@gmail.com>
2023-06-23 17:39:21 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! ci: check build size between branches

--- ci/scripts/check-build-size.sh
+++ ci/scripts/check-build-size.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-echo "artifact path: $1"
-echo "base artifact to compare with path path $2"
+echo "base artifact to compare with path path $1"
+echo "artifact path: $2"
 
-NEW_SIZE_RES=$(curl $1 -sI | grep -i Content-Length)
-OLD_SIZE_RES=$(curl $2 -sI | grep -i Content-Length)
+NEW_SIZE_RES=$(curl $2 -sI | grep -i Content-Length)
+OLD_SIZE_RES=$(curl $1 -sI | grep -i Content-Length)
 
 NEW_SIZE=${NEW_SIZE_RES/"content-length: "/""}
 OLD_SIZE=${OLD_SIZE_RES/"content-length: "/""}
@@ -32,4 +32,11 @@ else
     echo "suspiciously large build detected!"
     exit 1;
 fi
+
+# https://gitlab.com/satoshilabs/trezor/trezor-suite/-/jobs/4530912073/artifacts/raw/packages/connect-web/build/trezor-connect.js?inline=false
+# https://gitlab.com/api/v4/projects/14570634/jobs/artifacts/check-build-size/raw/packages/connect-web/build/trezor-connect.js?job=connect-web%20build
+# https://gitlab.com/api/v4/projects/<project-id>/jobs/artifacts/main          /raw/review/index.html?job=build
+
+# /projects/:id/jobs/:job_id/artifacts/*artifact_path
+# https://gitlab.com/api/v4/projects/14570634/jobs/artifacts/check-build-size/raw/packages/connect-web/build/trezor-connect.js?job=connect-web%20build
 
