c8d763f7f0b9c4203dcc9e0724ca883fb7c69766
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2023-06-26 14:23:11 +0200

    wip

--- packages/request-manager/src/index.ts
+++ packages/request-manager/src/index.ts
@@ -3,3 +3,5 @@ export { createInterceptor } from './interceptor';
 export type { InterceptedEvent, BootstrapEvent, TorControllerStatus } from './types';
 export { TOR_CONTROLLER_STATUS } from './types';
 export { TorIdentities } from './torIdentities';
+
+console.log('just a change to trigger e2e test and see it works on CI');
