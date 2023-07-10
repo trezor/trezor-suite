f6b570cf0b2a775606649fc856edbd9b72bd8076
Jan Komarek <jan.komarek@satoshilabs.com>
2023-07-10 11:53:19 +0200

    chore(suite-native): remove errors from provider

--- suite-native/forms/src/Form.tsx
+++ suite-native/forms/src/Form.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Control, FieldErrors, FieldValues, UseFormReturn } from 'react-hook-form';
+import { Control, FieldValues, UseFormReturn } from 'react-hook-form';
 
 export interface FormProps<TFieldValues extends FieldValues> {
     children?: React.ReactNode;
@@ -8,7 +8,6 @@ export interface FormProps<TFieldValues extends FieldValues> {
 
 interface FormContextValue<TFieldValues extends FieldValues> {
     control: Control<TFieldValues>;
-    errors: FieldErrors<TFieldValues>;
 }
 
 export const FormContext = React.createContext<FormContextValue<FieldValues>>(
@@ -19,12 +18,8 @@ export const Form = <TFieldValues extends FieldValues>({
     children,
     form,
 }: FormProps<TFieldValues>) => {
-    // TODO: once react-hook-form is upgraded to v7 remove errors, because it will be accessible via useControl.
-    // It will same some unnecessary rerenders. Also FormContext from rhf should't be used because
-    // there is lof of unnecessary stuff that will cause extra rerenders, but we need only control.
     const formContextValue = {
         control: form.control,
-        errors: form.formState.errors,
     };
 
     return (
