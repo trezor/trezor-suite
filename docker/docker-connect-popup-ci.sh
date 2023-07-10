651d0bd3e9716836e175e9734c01a80d49d939e7
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2023-07-10 14:59:44 +0200

    wip

--- docker/docker-connect-popup-ci.sh
+++ docker/docker-connect-popup-ci.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+LOCAL_USER_ID="$(id -u "$USER")"
+export LOCAL_USER_ID
+export TEST_FILE=$1
+
 docker-compose -f ./docker/docker-compose.connect-popup-test.yml up --build --abort-on-container-exit
