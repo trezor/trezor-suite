014f9405ce52bf427aca8871abe0019bd77c31a6
Martin Varmuza <mvarmuza@gmail.com>
2023-07-18 13:32:39 +0200

    ci(connect): couple of fixes in release init script

--- ci/scripts/connect-release-init.js
+++ ci/scripts/connect-release-init.js
@@ -83,12 +83,12 @@ const initConnectRelease = () => {
             const CHANGELOG_PATH = path.join(PACKAGE_PATH, 'CHANGELOG.md');
 
             const newCommits = [];
-            commitsArr.forEach(commit => {
+            for (let commit of commitsArr) {
                 if (commit.includes(`npm-release: @trezor/${packageName}`)) {
-                    return;
+                    break;
                 }
                 newCommits.push(commit.replaceAll('"', ''));
-            });
+            }
 
             if (newCommits.length) {
                 if (!fs.existsSync(CHANGELOG_PATH)) {
@@ -97,7 +97,7 @@ const initConnectRelease = () => {
 
                 let changelog = fs.readFileSync(CHANGELOG_PATH, 'utf-8');
 
-                changelog = `# ${version}\n\n${newCommits.join('')}\n\n${changelog}`;
+                changelog = `# ${version}\n\n${newCommits.join('\n')}\n\n${changelog}`;
                 fs.writeFileSync(CHANGELOG_PATH, changelog, 'utf-8');
 
                 exec('yarn', ['prettier', '--write', CHANGELOG_PATH]);
