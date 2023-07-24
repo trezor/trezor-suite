d704f7edcfd1359325890da5a4b6a8c7a272f472
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2023-07-24 14:32:55 +0100

    chore(suite-common): add comment

--- suite-common/wallet-utils/src/cardanoUtils.ts
+++ suite-common/wallet-utils/src/cardanoUtils.ts
@@ -1,5 +1,8 @@
 import { Network } from '@suite-common/wallet-config';
 
+// NOTE: This was moved from packages/suite/src/utils/wallet/cardanoUtils.ts
+// Reason for that is that we need to use it in @suite-common/ package, but we cannot import
+// @fivebinaries/coin-selection there, because it will break mobile app bundling.
 export const getDerivationType = (accountType: Network['accountType']) => {
     switch (accountType) {
         case 'normal':
