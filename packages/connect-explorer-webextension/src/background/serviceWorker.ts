32e6b4e2bcb88d26e53710d8f1fb4ea02514c257
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2023-07-27 17:13:36 +0200

    feat(connect-explorer-webextension): new package

---
+++ packages/connect-explorer-webextension/src/background/serviceWorker.ts
@@ -0,0 +1,5 @@
+// empty file so there is a service worker loaded in the extension and id can be used for the e2e tests.
+// https://playwright.dev/docs/chrome-extensions#testing
+
+// eslint-disable-next-line no-console
+console.log('service worker loaded');
---
+++ packages/connect-explorer-webextension/src/pages/extension-popup/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { createRoot } from 'react-dom/client';
+
+import { ExtensionPopup } from './ExtensionPopup';
+
+const container = document.getElementById('extension-popup-container');
+const root = createRoot(container!);
+root.render(<ExtensionPopup />);
