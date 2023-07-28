ff3e476b2b5e9f5d918d476630ee88d409841f0e
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2023-07-28 15:45:38 +0200

    feat(connect-examples): mv2 extension in page

--- packages/connect-examples/webextension-mv2/src/background.js
+++ packages/connect-examples/webextension-mv2/src/background.js
@@ -1,34 +1,49 @@
 /**
-When the button's clicked:
+TrezorConnect is loaded in background script but it is triggered from content script.
 - call for TrezorConnect action
 - show a notification with response
 */
 
 const DEFAULT_SRC = 'https://connect.trezor.io/9/';
 
-TrezorConnect.init({
-    manifest: {
-        email: 'email@developer.com',
-        appUrl: 'webextension-app-boilerplate',
-    },
-    connectSrc: DEFAULT_SRC,
-});
-
-function onClick() {
-    TrezorConnect.getAddress({
-        path: "m/49'/0'/0'/0/0",
-    }).then(response => {
-        const message = response.success
-            ? `BTC Address: ${response.payload.address}`
-            : `Error: ${response.payload.error}`;
-
-        chrome.notifications.create(new Date().getTime().toString(), {
-            type: 'basic',
-            iconUrl: 'icon48.png',
-            title: 'TrezorConnect',
-            message,
-        });
+function loadTrezorConnect() {
+    return TrezorConnect.init({
+        manifest: {
+            email: 'email@developer.com',
+            appUrl: 'webextension-app-boilerplate',
+        },
+        connectSrc: DEFAULT_SRC,
     });
 }
 
-chrome.browserAction.onClicked.addListener(onClick);
+function getAddress() {
+    return TrezorConnect.getAddress({
+        path: "m/49'/0'/0'/0/0",
+    });
+}
+
+async function sendMessageToContentScript(tabID, type, data = null) {
+    try {
+        const response = await chrome.tabs.sendMessage(tabID, { type, data });
+        return response;
+    } catch (error) {
+        return null;
+    }
+}
+
+chrome.runtime.onMessage.addListener((message, sender) => {
+    const { tab } = sender;
+    const { type, data } = message;
+    if (type === 'getAddress') {
+        getAddress().then(response => {
+            const message = response.success
+                ? `BTC Address: ${response.payload.address}`
+                : `Error: ${response.payload.error}`;
+            sendMessageToContentScript(tab.id, 'getAddress', message);
+        });
+    } else if (type === 'pageLoaded') {
+        loadTrezorConnect().then(() => {
+            sendMessageToContentScript(tab.id, 'connectLoaded');
+        });
+    }
+});
---
+++ packages/connect-examples/webextension-mv2/src/connect-manager.js
@@ -0,0 +1,31 @@
+async function sendMessageToBackground(type, data = null) {
+    try {
+        const response = await chrome.runtime.sendMessage({ type, data });
+        return response;
+    } catch (error) {
+        console.error('sendMessageToBackground error: ', error);
+        return null;
+    }
+}
+
+const button = document.getElementById('get-address');
+button.addEventListener('click', () => {
+    sendMessageToBackground('getAddress');
+});
+
+chrome.runtime.onMessage.addListener((message, sender) => {
+    const { type, data } = message;
+    if (type === 'getAddress') {
+        document.getElementById('result').innerText = JSON.stringify(data);
+    } else if (type === 'connectLoaded') {
+        const connectLoaded = document.createElement('div');
+        connectLoaded.setAttribute('data-test', 'connect-loaded');
+        connectLoaded.innerText = 'TrezorConnect is loaded';
+        connectLoaded.style.display = 'block';
+        document.body.appendChild(connectLoaded);
+    }
+});
+
+// When page is loaded send message to background script to get reference so when
+// TrezorConnect is loaded we know it and can call it.
+sendMessageToBackground('pageLoaded');
