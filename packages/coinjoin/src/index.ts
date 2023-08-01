0a5d8267c4fee09a78dd7e6dd9ce366c7af63840
Nodonisko <suchydan@gmail.com>
2023-08-01 10:35:13 +0200

    chore: remove some unecessary build:lib

--- packages/coinjoin/src/index.ts
+++ packages/coinjoin/src/index.ts
@@ -2,3 +2,4 @@ export { CoinjoinBackend } from './backend/CoinjoinBackend';
 export { CoinjoinClient } from './client/CoinjoinClient';
 export { getInputSize, getOutputSize } from './utils/coordinatorUtils';
 export * from './types';
+export * from './enums';
