9dc847810877b6cba7ce7d4532d72a7c5cdc7294
Jan Komarek <jan.komarek@satoshilabs.com>
2023-08-03 18:42:07 +0200

    chore(components): rename Button and TooltipButton filenames

--- packages/components/src/components/buttons/TooltipButton/TooltipButton.tsx
+++ packages/components/src/components/buttons/TooltipButton/TooltipButton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Icon } from '../../Icon';
 import { Tooltip } from '../../Tooltip';
-import { Button, ButtonProps } from '../Button';
+import { Button, ButtonProps } from '../Button/Button';
 import { useTheme } from '../../../utils';
 
 const StyledButton = styled(Button)`
