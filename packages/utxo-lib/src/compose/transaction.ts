105bcd7845ca2fa0ddadf1af9433d6d5929f7f02
Szymon Lesisz <sz.lesisz@gmail.com>
2023-08-09 10:44:53 +0200

    chore(utxo-lib): `composeUtils` simplify outputs sorting
    Since all CoinSelectOutputFinal should now contain script represented as Buffer, including changeOutput (see previous commit) it no longer necessary to create those scripts once again to do the sorting.
    `convertOutput` function returns `ComposedTxOutput` interface (without script)

--- packages/utxo-lib/src/compose/transaction.ts
+++ packages/utxo-lib/src/compose/transaction.ts
@@ -1,50 +1,32 @@
 import * as BN from 'bn.js';
-import * as BitcoinJsAddress from '../address';
-import { p2data } from '../payments/embed';
 import {
     ComposeInput,
     ComposeFinalOutput,
-    ComposedTxOutput,
     ComposedTransaction,
     CoinSelectInput,
     CoinSelectOutputFinal,
 } from '../types';
-import type { Network } from '../networks';
-
-function convertOpReturnOutput(opReturnData: string) {
-    const opReturnDataBuffer = Buffer.from(opReturnData, 'hex');
-    const output = {
-        dataHex: opReturnData,
-        amount: undefined,
-    };
-    const script = p2data({ data: [opReturnDataBuffer] }).output as Buffer;
-    return {
-        output,
-        script,
-    };
-}
 
 function convertOutput(
-    address: string,
-    amount: string,
-    network: Network,
-    basePath: number[],
-    changeId: number,
-    isChange: boolean,
+    selectedOutput: CoinSelectOutputFinal,
+    composeOutput: ComposeFinalOutput | { path: number[] },
 ) {
-    const output: ComposedTxOutput = isChange
-        ? {
-              path: [...basePath, 1, changeId],
-              amount,
-          }
-        : {
-              address,
-              amount,
-          };
+    if ('path' in composeOutput) {
+        return {
+            path: composeOutput.path,
+            amount: selectedOutput.value,
+        };
+    }
+
+    if (composeOutput.type === 'opreturn') {
+        return {
+            dataHex: composeOutput.dataHex,
+        };
+    }
 
     return {
-        output,
-        script: BitcoinJsAddress.toOutputScript(address, network),
+        address: composeOutput.address,
+        amount: selectedOutput.value,
     };
 }
 
@@ -52,8 +34,13 @@ function inputComparator(a: ComposeInput, b: ComposeInput) {
     return Buffer.from(a.txid, 'hex').compare(Buffer.from(b.txid, 'hex')) || a.vout - b.vout;
 }
 
-function outputComparator(aScript: Buffer, aValue: string, bScript: Buffer, bValue: string) {
-    return new BN(aValue).cmp(new BN(bValue)) || aScript.compare(bScript);
+function outputComparator(a: CoinSelectOutputFinal, b: CoinSelectOutputFinal) {
+    return (
+        new BN(a.value).cmp(new BN(b.value)) ||
+        (Buffer.isBuffer(a.script) && Buffer.isBuffer(b.script)
+            ? a.script.compare(b.script)
+            : a.script.length - b.script.length)
+    );
 }
 
 export function createTransaction<Input extends ComposeInput>(
@@ -63,45 +50,28 @@ export function createTransaction<Input extends ComposeInput>(
     selectedOutputs: CoinSelectOutputFinal[],
     basePath: number[],
     changeId: number,
-    changeAddress: string,
-    network: Network,
     skipPermutation?: boolean,
 ): ComposedTransaction<Input> {
     const convertedInputs = selectedInputs.map(input => allInputs[input.i]);
-    const convertedOutputs = selectedOutputs.map((output, i) => {
-        // change is always last
-        const isChange = i === allOutputs.length;
-
-        const original = allOutputs[i]; // null if change
-
-        if (original && original.type === 'opreturn') {
-            return convertOpReturnOutput(original.dataHex);
-        }
-        const address = !original ? changeAddress : original.address;
-        const amount = output.value;
-        return convertOutput(address, amount, network, basePath, changeId, isChange);
-    });
+    const convertedOutputs = selectedOutputs.map((output, index) =>
+        convertOutput(output, allOutputs[index] || { path: [...basePath, 1, changeId] }),
+    );
     const defaultPermutation = convertedOutputs.map((_, index) => index);
 
     if (skipPermutation) {
         return {
             inputs: convertedInputs,
             outputs: {
-                sorted: convertedOutputs.map(({ output }) => output),
+                sorted: convertedOutputs,
                 permutation: defaultPermutation,
             },
         };
     }
 
     const permutation = defaultPermutation.sort((a, b) =>
-        outputComparator(
-            convertedOutputs[a].script,
-            convertedOutputs[a].output.amount || '0',
-            convertedOutputs[b].script,
-            convertedOutputs[b].output.amount || '0',
-        ),
+        outputComparator(selectedOutputs[a], selectedOutputs[b]),
     );
-    const sortedOutputs = permutation.map(index => convertedOutputs[index].output);
+    const sortedOutputs = permutation.map(index => convertedOutputs[index]);
 
     return {
         inputs: convertedInputs.sort(inputComparator),
