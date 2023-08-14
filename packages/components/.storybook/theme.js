ab320c426d1ac25a9f9541273026339472c739f5
Jan Komarek <jan.komarek@satoshilabs.com>
2023-08-14 17:24:18 +0200

    chore(components): storybook - remove broken logo link, set title

--- packages/components/.storybook/theme.js
+++ packages/components/.storybook/theme.js
@@ -3,5 +3,5 @@ import { create } from '@storybook/theming/create';
 export default create({
     base: 'light',
     fontBase: 'TT Hoves',
-    brandImage: 'https://trezor.io/static/images/trezor-logo-black.png',
+    brandTitle: 'Trezor Components',
 });
