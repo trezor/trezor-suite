8f50ad44aa17b31f57d5b279da78fc8cee6ac241
dahaca <dahaca17@gmail.com>
2023-08-16 19:24:09 +0200

    feat(components): add the new theme to the theme providers

--- packages/components/src/utils/hooks.ts
+++ packages/components/src/utils/hooks.ts
@@ -1,12 +1,7 @@
-import { useContext } from 'react';
-import { ThemeContext } from '../support/ThemeProvider';
-import { platform } from './env';
 import { useTheme as useSCTheme } from 'styled-components';
 
 export const useTheme = () => {
-    // In react-native it retrieves theme from our own context
-    // On web use Styled-Components hook/context instead
-    const contextTheme = useContext(ThemeContext);
     const scTheme = useSCTheme();
-    return platform === 'web' ? scTheme : contextTheme;
+
+    return scTheme;
 };
