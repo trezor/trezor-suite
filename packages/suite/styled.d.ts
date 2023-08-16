29b0c5e281c5cc77772357bb1a23fffdd28f0e9a
dahaca <dahaca17@gmail.com>
2023-08-16 19:24:09 +0200

    feat(components): add the new theme to the theme providers

--- packages/suite/styled.d.ts
+++ packages/suite/styled.d.ts
@@ -1,7 +1,8 @@
 // import original module declarations
 import 'styled-components';
 import { types } from '@trezor/components';
+import { Colors } from '@trezor/theme';
 
 declare module 'styled-components' {
-    export interface DefaultTheme extends types.SuiteThemeColors {}
+    export interface DefaultTheme extends types.SuiteThemeColors, Colors {}
 }
