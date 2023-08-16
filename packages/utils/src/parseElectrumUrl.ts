61dce520d4e67ca698f752f4b530ab6c79048105
Marek Polak <marek.polak@satoshilabs.com>
2023-08-16 16:10:22 +0200

    feat(utils): add parseElectrumUrl util

---
+++ packages/utils/src/parseElectrumUrl.ts
@@ -0,0 +1,12 @@
+// URL is in format host:port:[t|s] (t for tcp, s for ssl)
+const ELECTRUM_URL_REGEX = /^(?:([a-zA-Z0-9.-]+)|\[([a-f0-9:]+)\]):([0-9]{1,5}):([ts])$/;
+
+export const parseElectrumUrl = (url: string) => {
+    const match = url.match(ELECTRUM_URL_REGEX);
+    if (!match) return undefined;
+    return {
+        host: match[1] ?? match[2],
+        port: Number.parseInt(match[3], 10),
+        protocol: match[4] as 't' | 's',
+    };
+};
