1da02373f135e8c918efa5bc36cac4f91fe86ecd
Marek Polak <marek.polak@satoshilabs.com>
2023-08-16 16:13:38 +0200

    chore(wallet-utils): use parseElectrumUrl

--- suite-common/wallet-utils/src/__tests__/backend.test.ts
+++ suite-common/wallet-utils/src/__tests__/backend.test.ts
@@ -1,4 +1,4 @@
-import { getDefaultBackendType, isElectrumUrl, isTrezorConnectBackendType } from '../backend';
+import { getDefaultBackendType, isTrezorConnectBackendType } from '../backend';
 
 describe('backend utils', () => {
     test('getDefaultBackendType', () => {
@@ -6,16 +6,6 @@ describe('backend utils', () => {
         expect(getDefaultBackendType('ltc')).toBe('blockbook');
         expect(getDefaultBackendType('ada')).toBe('blockfrost');
         expect(getDefaultBackendType('tada')).toBe('blockfrost');
-    });
-
-    test('isElectrumUrl', () => {
-        expect(isElectrumUrl('electrum.example.com:50001:t')).toBe(true);
-        expect(isElectrumUrl('electrum.example.com:50001:s')).toBe(true);
-        expect(isElectrumUrl('electrum.example.onion:50001:t')).toBe(true);
-        expect(isElectrumUrl('electrum.example.com:50001:x')).toBe(false);
-        expect(isElectrumUrl('wss://blockfrost.io')).toBe(false);
-        expect(isElectrumUrl('https://google.com')).toBe(false);
-        expect(isElectrumUrl('')).toBe(false);
     });
 
     test('isTrezorConnectBackendType', () => {
