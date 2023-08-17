02e94f018e23aadb0b84c6e773d402c9f6662d01
dahaca <dahaca17@gmail.com>
2023-08-17 19:07:21 +0200

    feat(components): add the ButtonGroup component

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -12,6 +12,7 @@ export * from './utils/hooks';
 export * from './components/buttons/Button/Button';
 export * from './components/buttons/IconButton/IconButton';
 export * from './components/buttons/TextButton/TextButton';
+export * from './components/buttons/ButtonGroup/ButtonGroup';
 export * from './components/buttons/PinButton/PinButton';
 export * from './components/buttons/TooltipButton/TooltipButton';
 export * from './components/assets/Flag/Flag';
