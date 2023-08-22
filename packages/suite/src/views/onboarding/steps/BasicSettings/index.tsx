0179af55f5d470de35a479e0debe06ba269bcfe4
tomasklim <tomas.klim@gmail.com>
2023-08-22 10:48:47 +0200

    feat(suite): onboarding coins section bitcoin only fw text

--- packages/suite/src/views/onboarding/steps/BasicSettings/index.tsx
+++ packages/suite/src/views/onboarding/steps/BasicSettings/index.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Translation } from 'src/components/suite';
 import { OnboardingButtonCta } from 'src/components/onboarding';
-import { useOnboarding, useSelector } from 'src/hooks/suite';
+import { useDevice, useOnboarding, useSelector } from 'src/hooks/suite';
 import { BasicSettingsStepBox } from './BasicSettingsStepBox';
 import { AdvancedSetup } from './AdvancedSetup';
 import { getIsTorLoading } from 'src/utils/suite/tor';
+import { hasBitcoinOnlyFirmware } from '@trezor/device-utils';
 
 const BasicSettings = () => {
     const enabledNetworks = useSelector(state => state.wallet.settings.enabledNetworks);
     const torStatus = useSelector(state => state.suite.torStatus);
+    const { device } = useDevice();
+
+    const bitcoinOnlyFirmware = hasBitcoinOnlyFirmware(device);
 
     const { goToNextStep } = useOnboarding();
 
@@ -18,7 +22,15 @@ const BasicSettings = () => {
     return (
         <BasicSettingsStepBox
             heading={<Translation id="TR_ONBOARDING_COINS_STEP" />}
-            description={<Translation id="TR_ONBOARDING_COINS_STEP_DESCRIPTION" />}
+            description={
+                <Translation
+                    id={
+                        bitcoinOnlyFirmware
+                            ? 'TR_ONBOARDING_COINS_STEP_DESCRIPTION_BITCOIN_ONLY'
+                            : 'TR_ONBOARDING_COINS_STEP_DESCRIPTION'
+                    }
+                />
+            }
             outerActions={
                 <AdvancedSetup>
                     <OnboardingButtonCta
