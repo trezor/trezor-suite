09d70ee31f80c8c7a573b07fe95f756ae7e4375a
PeterBenc <peter.benc1@gmail.com>
2023-08-23 15:13:01 +0200

    feat(connect): get solana worker

--- packages/connect/src/workers/workers.ts
+++ packages/connect/src/workers/workers.ts
@@ -2,5 +2,6 @@ import BlockbookWorker from '@trezor/blockchain-link/lib/workers/blockbook';
 import RippleWorker from '@trezor/blockchain-link/lib/workers/ripple';
 import BlockfrostWorker from '@trezor/blockchain-link/lib/workers/blockfrost';
 import ElectrumWorker from '@trezor/blockchain-link/lib/workers/electrum';
+import SolanaWorker from '@trezor/blockchain-link/lib/workers/solana';
 
-export { BlockbookWorker, RippleWorker, BlockfrostWorker, ElectrumWorker };
+export { BlockbookWorker, RippleWorker, BlockfrostWorker, ElectrumWorker, SolanaWorker };
