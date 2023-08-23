f8747601aaaa8b4ee55a17d1b1dc4c9045014bbd
dahaca <dahaca17@gmail.com>
2023-08-23 17:54:01 +0200

    chore(components): narrow down Button interface

--- packages/suite/src/components/onboarding/Buttons/OnboardingButtonBack.tsx
+++ packages/suite/src/components/onboarding/Buttons/OnboardingButtonBack.tsx
@@ -2,13 +2,7 @@ import { Button, ButtonProps } from '@trezor/components';
 import { Translation } from 'src/components/suite';
 
 export const OnboardingButtonBack = (props: Omit<ButtonProps, 'children'>) => (
-    <Button
-        data-test="@onboarding/back-button"
-        variant="tertiary"
-        icon="ARROW_LEFT"
-        {...props}
-        style={{ backgroundColor: 'initial' }}
-    >
+    <Button data-test="@onboarding/back-button" variant="tertiary" icon="ARROW_LEFT" {...props}>
         <Translation id="TR_BACK" />
     </Button>
 );
