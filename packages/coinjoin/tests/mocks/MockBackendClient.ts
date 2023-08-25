800733b2be775a7b9a405e2bac9ade7ea5b7e046
Marek Polak <marek.polak@satoshilabs.com>
2023-08-25 10:28:04 +0200

    test(coinjoin): fetch filters from blockbook
    (cherry picked from commit c93bf2c3e1acba81e45f6ea970437f0ec1962ab6)

--- packages/coinjoin/tests/mocks/MockBackendClient.ts
+++ packages/coinjoin/tests/mocks/MockBackendClient.ts
@@ -90,6 +90,9 @@ export class MockBackendClient extends CoinjoinBackendClient {
 
     private getBlockbookProxyHandler(method: string | symbol, ...params: any[]) {
         switch (method) {
+            case 'getServerInfo': {
+                return Promise.resolve({ bestHeight: this.blocks[this.blocks.length - 1].height });
+            }
             case 'getTransaction': {
                 const tx = this.transactions.find(t => t.txid === params[0]);
                 if (tx) return Promise.resolve(tx as any);
@@ -106,6 +109,23 @@ export class MockBackendClient extends CoinjoinBackendClient {
                         this.mempool.map(({ txid, filter }) => [txid, filter]),
                     ),
                 });
+            }
+            case 'getBlockFiltersBatch': {
+                const bestKnownBlockHash = params[0];
+                const count = params[1];
+
+                if (this.blocks[this.blocks.length - 1].hash === bestKnownBlockHash)
+                    return Promise.resolve({ blockFiltersBatch: [] });
+                const from = this.blocks.findIndex(
+                    ({ previousBlockHash }) => previousBlockHash === bestKnownBlockHash,
+                );
+                if (from >= 0)
+                    return Promise.resolve({
+                        blockFiltersBatch: this.blocks
+                            .slice(from, from + count)
+                            .map(({ height, hash, filter }) => `${height}:${hash}:${filter}`),
+                    });
+                return Promise.reject(new Error('Block not found'));
             }
             // no default
         }
