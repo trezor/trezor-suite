d65eae360792a233bc6f19f979adec91611ce823
Petr Knetl <petr.knetl@satoshilabs.com>
2023-08-25 14:47:42 +0200

    fix(suite-native): navigation receive icon
    Closes #8584
    
    Other icons were already fixed in PR #9213

--- suite-common/icons/src/icons.ts
+++ suite-common/icons/src/icons.ts
@@ -8,6 +8,7 @@ export const icons = {
     action: require('../assets/icons/action.svg'),
     actionHorizontal: require('../assets/icons/actionHorizontal.svg'),
     arrowDown: require('../assets/icons/arrowDown.svg'),
+    arrowDownLight: require('../assets/icons/arrowDownLight.svg'),
     arrowURightDown: require('../assets/icons/arrowURightDown.svg'),
     arrowUp: require('../assets/icons/arrowUp.svg'),
     arrowUpRight: require('../assets/icons/arrowUpRight.svg'),
