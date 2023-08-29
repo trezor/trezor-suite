6d8019752398bd92240dc35843cafb6d139c5e42
tomasklim <tomas.klim@gmail.com>
2023-08-29 14:11:28 +0200

    feat(connect-common): copy models.json from submodules

---
+++ packages/connect/src/data/models.ts
@@ -0,0 +1,17 @@
+export const models = {
+    T1B1: {
+        name: 'Trezor Model One',
+        colors: {},
+    },
+    T2T1: {
+        name: 'Trezor Model T',
+        colors: {},
+    },
+    T2B1: {
+        name: 'Trezor Model R',
+        colors: {
+            '0': 'Black',
+            '1': 'White',
+        },
+    },
+};
