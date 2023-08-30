ea2dc882fb84b380ed0cb20b3eee10beae05bcab
Jan Komarek <jan.komarek@satoshilabs.com>
2023-08-30 16:24:43 +0200

    chore(suite): refactor Data

--- packages/suite/src/views/wallet/send/components/Options/components/EthereumOptions/index.tsx
+++ packages/suite/src/views/wallet/send/components/Options/components/EthereumOptions/index.tsx
@@ -5,6 +5,7 @@ import { Translation } from 'src/components/suite';
 import { OnOffSwitcher } from 'src/components/wallet';
 import { useSendFormContext } from 'src/hooks/wallet';
 import { Data } from './Data';
+import { FormOptions } from '@suite-common/wallet-types';
 
 const Wrapper = styled.div`
     display: flex;
@@ -23,27 +24,23 @@ const StyledButton = styled(Button)`
 `;
 
 const EthereumOptions = () => {
-    const { getDefaultValue, toggleOption, composeTransaction, resetDefaultValue } =
-        useSendFormContext();
+    const { getDefaultValue, toggleOption, composeTransaction } = useSendFormContext();
 
     const options = getDefaultValue('options', []);
     const dataEnabled = options.includes('ethereumData');
     const tokenValue = getDefaultValue<string, string | undefined>('outputs.0.token', undefined);
     const broadcastEnabled = options.includes('broadcast');
 
+    const toggle = (option: FormOptions) => {
+        toggleOption(option);
+        composeTransaction();
+    };
+    const toggleData = () => toggle('ethereumData');
+    const toggleBroadcast = () => toggle('broadcast');
+
     return (
         <Wrapper>
-            {dataEnabled && (
-                <Data
-                    close={() => {
-                        resetDefaultValue('ethereumDataAscii');
-                        resetDefaultValue('ethereumDataHex');
-                        // close additional form
-                        toggleOption('ethereumData');
-                        composeTransaction();
-                    }}
-                />
-            )}
+            {dataEnabled && <Data close={toggleData} />}
             <Left>
                 {!dataEnabled && !tokenValue && (
                     <Tooltip content={<Translation id="DATA_ETH_ADD_TOOLTIP" />} cursor="pointer">
@@ -51,11 +48,7 @@ const EthereumOptions = () => {
                             variant="tertiary"
                             icon="DATA"
                             data-test="send/open-ethereum-data"
-                            onClick={() => {
-                                // open additional form
-                                toggleOption('ethereumData');
-                                composeTransaction();
-                            }}
+                            onClick={toggleData}
                         >
                             <Translation id="DATA_ETH_ADD" />
                         </StyledButton>
@@ -66,10 +59,7 @@ const EthereumOptions = () => {
                         variant="tertiary"
                         icon="BROADCAST"
                         data-test="send/broadcast"
-                        onClick={() => {
-                            toggleOption('broadcast');
-                            composeTransaction();
-                        }}
+                        onClick={toggleBroadcast}
                     >
                         <Translation id="BROADCAST" />
                         <OnOffSwitcher isOn={broadcastEnabled} />
