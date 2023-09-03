4a40d3dbf9d5ad62457af76b756b4b83d9514421
dahaca <dahaca17@gmail.com>
2023-09-03 19:20:29 +0200

    feat(icons): add onClick to the web Icon

--- suite-common/icons/src/webComponents/Icon.web.tsx
+++ suite-common/icons/src/webComponents/Icon.web.tsx
@@ -1,6 +1,6 @@
 import { ReactSVG } from 'react-svg';
 
-import styled, { useTheme } from 'styled-components';
+import styled, { css, useTheme } from 'styled-components';
 
 import { icons } from '../icons';
 import { IconProps, getIconSize } from '../config';
@@ -15,27 +15,57 @@ const SVG = styled(ReactSVG)`
         align-items: center;
         justify-content: center;
     }
+
+    ${({ onClick }) =>
+        onClick &&
+        css`
+            cursor: pointer;
+
+            :focus-visible {
+                svg {
+                    transition: opacity 0.2s;
+
+                    opacity: 0.5;
+                }
+            }
+        `}
 `;
 
 type WebIconProps = IconProps & {
+    onClick?: () => void;
     className?: string;
 };
 
-export const Icon = ({ name, size = 'large', color = 'iconDefault', className }: WebIconProps) => {
+export const Icon = ({
+    name,
+    size = 'large',
+    color = 'iconDefault',
+    onClick,
+    className,
+}: WebIconProps) => {
     const theme = useTheme();
 
     const iconSize = getIconSize(size);
 
+    const handleOnKeyDown = (e: React.KeyboardEvent) => {
+        if (e.key === 'Enter' || e.key === ' ') {
+            onClick?.();
+        }
+    };
+
+    const handleInjection = (svg: SVGSVGElement) => {
+        svg.querySelectorAll('path')?.forEach(path => path.setAttribute('stroke', theme[color]));
+        svg.setAttribute('width', `${iconSize}px`);
+        svg.setAttribute('height', `${iconSize}px`);
+    };
+
     return (
         <SVG
+            onClick={onClick}
+            tabIndex={onClick ? 0 : undefined}
+            onKeyDown={handleOnKeyDown}
             src={icons[name]}
-            beforeInjection={svg => {
-                svg.querySelectorAll('path')?.forEach(path =>
-                    path.setAttribute('stroke', theme[color]),
-                );
-                svg.setAttribute('width', `${iconSize}px`);
-                svg.setAttribute('height', `${iconSize}px`);
-            }}
+            beforeInjection={handleInjection}
             className={className}
         />
     );
