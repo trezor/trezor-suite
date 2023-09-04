f21ed3fe769588dd9596eb4312145623440cf4f1
dahaca <dahaca17@gmail.com>
2023-09-04 23:54:09 +0200

    chore(components): rename noHover prop in Dropdown

--- packages/suite/src/components/suite/NavigationBar/components/NavigationActions/components/NavNotifications.tsx
+++ packages/suite/src/components/suite/NavigationBar/components/NavigationActions/components/NavNotifications.tsx
@@ -78,7 +78,7 @@ export const NavNotifications = ({
                                     </NotificationsWrapper>
                                 ),
                                 noPadding: true,
-                                noHover: true, // no hover effect
+                                noHoverEffect: true, // no hover effect
                                 callback: () => false, // don't close Dropdown on mouse click automatically
                             },
                         ],
