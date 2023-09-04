38a4ae9620084826e788ffc168c8511c18005f3a
dahaca <dahaca17@gmail.com>
2023-09-04 16:43:08 +0200

    chore(components): update Dropdown props

--- packages/suite/src/components/suite/Preloader/SuiteLayout/NavigationBar/NavigationActions/NavNotifications.tsx
+++ packages/suite/src/components/suite/Preloader/SuiteLayout/NavigationBar/NavigationActions/NavNotifications.tsx
@@ -66,7 +66,6 @@ export const NavNotifications = ({
                 onToggle={handleToggleChange}
                 ref={dropdownRef}
                 alignMenu="right"
-                offset={4}
                 items={[
                     {
                         key: 'dropdown',
