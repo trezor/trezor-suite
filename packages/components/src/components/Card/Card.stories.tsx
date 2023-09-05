1f14b0774c987e035b538a627691927b71e7916f
dahaca <dahaca17@gmail.com>
2023-09-05 18:05:08 +0200

    feat(components): update the Card component

--- packages/components/src/components/Card/Card.stories.tsx
+++ packages/components/src/components/Card/Card.stories.tsx
@@ -1,42 +1,13 @@
-import styled from 'styled-components';
 import { Card as CardComponent } from './Card';
-import { variables } from '../../index';
-
-const Wrapper = styled.div`
-    margin: 10px 0;
-    background: ${({ theme }) => theme.BG_GREY};
-    padding: 20px;
-`;
-
-const StyledCard = styled(CardComponent)`
-    margin: 20px;
-`;
-
-const Text = styled.div`
-    font-weight: ${variables.FONT_WEIGHT.DEMI_BOLD};
-`;
+import { StoryObj } from '@storybook/react';
 
 export default {
     title: 'Misc/Card',
+    component: CardComponent,
 };
 
-export const Card = {
-    render: () => (
-        <>
-            <Wrapper>
-                <StyledCard>
-                    <Text>basic card</Text>
-                </StyledCard>
-                <StyledCard noPadding>
-                    <Text>Card with noPadding</Text>
-                </StyledCard>
-                <StyledCard largePadding>
-                    <Text>Card with largePadding</Text>
-                </StyledCard>
-                <StyledCard noVerticalPadding>
-                    <Text>Card with noVerticalPadding</Text>
-                </StyledCard>
-            </Wrapper>
-        </>
-    ),
+export const Card: StoryObj = {
+    args: {
+        children: 'Some content',
+    },
 };
