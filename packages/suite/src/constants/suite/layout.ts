80095b551c04ba7a86f78f523a5857122ee5d76f
dahaca <dahaca17@gmail.com>
2023-09-05 18:07:35 +0200

    feat(suite): remove card padding constants from suite

--- packages/suite/src/constants/suite/layout.ts
+++ packages/suite/src/constants/suite/layout.ts
@@ -1,6 +1,4 @@
 export const MAX_WIDTH = '1020px';
 export const MAX_WIDTH_WALLET_CONTENT = '768px';
-export const CARD_PADDING_SIZE = '20px';
-export const CARD_PADDING_SIZE_LARGE = '18px';
 export const MOBILE_HORIZONTAL_PADDINGS = '16px';
 export const DESKTOP_HORIZONTAL_PADDINGS = '32px';
