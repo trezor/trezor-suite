a7c54d8ba9611b41523e36225287e1b2b41e974b
Szymon Lesisz <sz.lesisz@gmail.com>
2023-09-07 19:11:48 +0200

    feat(device-authenticity): create `@suite-common/device-authenticity` package

---
+++ suite-common/device-authenticity/src/deviceAuthenticityThunks.ts
@@ -0,0 +1,54 @@
+import TrezorConnect from '@trezor/connect';
+import { createThunk } from '@suite-common/redux-utils';
+import { notificationsActions } from '@suite-common/toast-notifications';
+
+import { ACTION_PREFIX, deviceAuthenticityActions } from './deviceAuthenticityActions';
+
+export const checkDeviceAuthenticityThunk = createThunk<{ allowDebugKeys: boolean }>(
+    `${ACTION_PREFIX}/checkDeviceAuthenticity`,
+    async ({ allowDebugKeys }, { dispatch, getState, extra }) => {
+        const {
+            selectors: { selectDevice },
+        } = extra;
+        const device = selectDevice(getState());
+        if (!device) {
+            throw new Error('device is not connected');
+        }
+
+        const result = await TrezorConnect.authenticateDevice({
+            device: {
+                path: device.path,
+            },
+            allowDebugKeys,
+        });
+
+        if (result.success) {
+            if (result.payload.valid) {
+                dispatch(notificationsActions.addToast({ type: 'device-authenticity-success' }));
+            } else if (
+                result.payload.configExpired &&
+                result.payload.error === 'CA_PUBKEY_NOT_FOUND'
+            ) {
+                // sanity check result: CA_PUBKEY_NOT_FOUND with configExpired is temporary allowed
+                // it will be logged to sentry nevertheless (see sentryMiddleware)
+                // TODO: try fetch new config
+                // if (result.payload.configOutdated) {}
+            } else {
+                dispatch(
+                    notificationsActions.addToast({
+                        type: 'device-authenticity-error',
+                        error: `Device is not authentic: ${result.payload.error}`,
+                    }),
+                );
+            }
+            dispatch(deviceAuthenticityActions.result({ device, result: result.payload }));
+        } else {
+            dispatch(
+                notificationsActions.addToast({
+                    type: 'error',
+                    error: `Unable to validate device: ${result.payload.error}`,
+                }),
+            );
+        }
+    },
+);
