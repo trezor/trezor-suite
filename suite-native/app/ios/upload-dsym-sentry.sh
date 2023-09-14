bca911f0d1a8ef4a974f0968d156cca047b136fb
Nodonisko <suchydan@gmail.com>
2023-09-14 15:00:20 +0200

    fix(mobile): sentry debug symbols

--- suite-native/app/ios/upload-dsym-sentry.sh
+++ suite-native/app/ios/upload-dsym-sentry.sh
@@ -1,1 +1,7 @@
 ../../../node_modules/@sentry/cli/bin/sentry-cli upload-dsym
+
+export SENTRY_PROPERTIES=sentry.properties
+
+[[ $SENTRY_INCLUDE_NATIVE_SOURCES == "true" ]] && INCLUDE_SOURCES_FLAG="--include-sources" || INCLUDE_SOURCES_FLAG=""
+SENTRY_CLI="../../../node_modules/@sentry/cli/bin/sentry-cli"
+$SENTRY_CLI debug-files upload "$INCLUDE_SOURCES_FLAG" "$DWARF_DSYM_FOLDER_PATH"
