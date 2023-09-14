43039b7f73508ae4677f1a192c09f5e8623607fa
Marek Polak <marek.polak@satoshilabs.com>
2023-09-14 14:07:03 +0200

    test(utils): mock timer in createCooldown

--- packages/utils/tests/createCooldown.test.ts
+++ packages/utils/tests/createCooldown.test.ts
@@ -1,14 +1,15 @@
 import { createCooldown } from '../src/createCooldown';
-import { createTimeoutPromise } from '../src/createTimeoutPromise';
 
-it('createCooldown', async () => {
+it('createCooldown', () => {
+    jest.useFakeTimers('modern');
+
     const cooldown = createCooldown(20);
     expect(cooldown()).toBe(true);
     expect(cooldown()).toBe(false);
-    await createTimeoutPromise(10);
+    jest.advanceTimersByTime(10);
     expect(cooldown()).toBe(false);
-    await createTimeoutPromise(15);
+    jest.advanceTimersByTime(10);
     expect(cooldown()).toBe(true);
-    await createTimeoutPromise(5);
+    jest.advanceTimersByTime(5);
     expect(cooldown()).toBe(false);
 });
