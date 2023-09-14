f04e0a0e552d5ea88eaf1a92c03dea15578fb121
PeterBenc <peter.benc1@gmail.com>
2023-09-14 13:12:36 +0200

    feat(blockchain-link): get solana tx details

--- packages/blockchain-link-utils/src/tests/solana.test.ts
+++ packages/blockchain-link-utils/src/tests/solana.test.ts
@@ -5,6 +5,7 @@ import { Transaction } from '@trezor/blockchain-link-types';
 import {
     extractAccountBalanceDiff,
     getAmount,
+    getDetails,
     getTargets,
     getTransactionEffects,
     getTxType,
@@ -65,6 +66,19 @@ describe('solana/utils', () => {
         fixtures.getAmount.forEach(({ description, input, expectedOutput }) => {
             it(description, () => {
                 const result = getAmount(input.accountEffect, input.txType as Transaction['type']);
+                expect(result).toEqual(expectedOutput);
+            });
+        });
+    });
+
+    describe('getDetails', () => {
+        fixtures.getDetails.forEach(({ description, input, expectedOutput }) => {
+            it(description, () => {
+                const result = getDetails(
+                    input.transaction as ParsedTransactionWithMeta,
+                    input.effects,
+                    input.accountAddress,
+                );
                 expect(result).toEqual(expectedOutput);
             });
         });
