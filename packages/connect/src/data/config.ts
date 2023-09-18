ea14720054e5aff6d3abee12aeb50cb193df8e3c
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2023-09-18 12:21:30 +0200

    chore(connect): remove outdated knownhost

--- packages/connect/src/data/config.ts
+++ packages/connect/src/data/config.ts
@@ -21,12 +21,6 @@ export const config = {
             icon: '',
         },
         { origin: 'niebkpllfhmpfbffbfifagfgoamhpflf', label: 'Trezor Password Manager', icon: '' },
-        { origin: 'trezor-connect@trezor.io', label: 'Trezor Connect FF Extension', icon: '' },
-        {
-            origin: 'efbfhenfhihgdcmnfdkhaphjdnopihlf',
-            label: 'Trezor Connect Chrome Extension',
-            icon: '',
-        },
         {
             origin: 'mnpfhpndmjholfdlhpkjfmjkgppmodaf',
             label: 'MetaMask',
