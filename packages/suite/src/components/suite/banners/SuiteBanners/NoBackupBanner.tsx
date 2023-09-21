bf34e17225462c0294444f8900881c14c4a3d37a
tomasklim <tomas.klim@gmail.com>
2023-09-21 15:09:36 +0200

    fix(suite): no backup banner

--- packages/suite/src/components/suite/banners/SuiteBanners/NoBackupBanner.tsx
+++ packages/suite/src/components/suite/banners/SuiteBanners/NoBackupBanner.tsx
@@ -1,10 +1,11 @@
 import { Translation } from 'src/components/suite';
 import { goto } from 'src/actions/suite/routerActions';
-import { useDispatch } from 'src/hooks/suite';
+import { useDispatch, useTranslation } from 'src/hooks/suite';
 import { Banner } from '../Banner';
 
 export const NoBackup = () => {
     const dispatch = useDispatch();
+    const { translationString } = useTranslation();
 
     const action = {
         label: <Translation id="TR_CREATE_BACKUP" />,
@@ -12,16 +13,9 @@ export const NoBackup = () => {
         'data-test': '@notification/no-backup/button',
     };
 
-    return (
-        <Banner
-            variant="critical"
-            body={
-                <>
-                    <Translation id="TR_YOUR_TREZOR_IS_NOT_BACKED_UP" />{' '}
-                    <Translation id="TR_IF_YOUR_DEVICE_IS_EVER_LOST" />
-                </>
-            }
-            action={action}
-        />
-    );
+    const translation = `${translationString(
+        'TR_YOUR_TREZOR_IS_NOT_BACKED_UP',
+    )} ${translationString('TR_IF_YOUR_DEVICE_IS_EVER_LOST')}`;
+
+    return <Banner variant="critical" body={translation} action={action} />;
 };
