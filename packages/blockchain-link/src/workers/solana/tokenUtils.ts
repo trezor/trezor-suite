7b655997cfb85ace18f7c9fdc136efa8bd4437c8
Samuel Šulovský <samuel.sulovsky@gmail.com>
2023-09-26 17:38:17 +0200

    feat(blockchain-link): fetch Solana token whitelist

--- packages/blockchain-link/src/workers/solana/tokenUtils.ts
+++ packages/blockchain-link/src/workers/solana/tokenUtils.ts
@@ -1,19 +1,25 @@
 import { TokenDetailByMint } from '@trezor/blockchain-link-types/lib/solana';
 
-const TOKEN_WHITELIST = {
-    So11111111111111111111111111111111111111112: {
-        name: 'Wrapped SOL',
-        symbol: 'WSOL',
-    },
-    Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB: {
-        name: 'Tether',
-        symbol: 'USDT',
-    },
-    '4k3Dyjzvzp8eMZWUXbBCjEvwSkkk59S5iCNLY3QrkX6R': {
-        name: 'Raydium',
-        symbol: 'RAY',
-    },
-};
+// https://github.com/viaprotocol/tokenlists
+// Aggregated token list with tokens listed on multiple exchanges
+const solanaTokenListUrl =
+    'https://cdn.jsdelivr.net/gh/viaprotocol/tokenlists/all_tokens/solana.json';
 
-export const getTokenMetadata = async (): Promise<TokenDetailByMint> =>
-    Promise.resolve(TOKEN_WHITELIST);
+export const getTokenMetadata = async (): Promise<TokenDetailByMint> => {
+    const tokenListResult: { address: string; name: string; symbol: string }[] = await (
+        await fetch(solanaTokenListUrl)
+    ).json();
+
+    const tokenMap = tokenListResult.reduce(
+        (acc, token) => ({
+            [token.address]: {
+                name: token.name,
+                symbol: token.symbol,
+            },
+            ...acc,
+        }),
+        {} as TokenDetailByMint,
+    );
+
+    return tokenMap;
+};
