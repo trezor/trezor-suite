27dacbb8bc92913e932caf63faf98f49e7430db0
Marek Polak <marek.polak@satoshilabs.com>
2023-09-26 16:05:51 +0200

    chore(connect): binance api dynamic import

--- packages/connect/src/api/binance/api/binanceGetAddress.ts
+++ packages/connect/src/api/binance/api/binanceGetAddress.ts
@@ -1,11 +1,11 @@
 // origin: https://github.com/trezor/connect/blob/develop/src/js/core/methods/BinanceGetAddress.js
 
-import { AbstractMethod, MethodReturnType } from '../core/AbstractMethod';
-import { validateParams, getFirmwareRange } from './common/paramsValidator';
-import { getMiscNetwork } from '../data/coinInfo';
-import { validatePath, fromHardened, getSerializedPath } from '../utils/pathUtils';
-import { PROTO, ERRORS } from '../constants';
-import { UI, createUiMessage } from '../events';
+import { AbstractMethod, MethodReturnType } from '../../../core/AbstractMethod';
+import { validateParams, getFirmwareRange } from '../../common/paramsValidator';
+import { getMiscNetwork } from '../../../data/coinInfo';
+import { validatePath, fromHardened, getSerializedPath } from '../../../utils/pathUtils';
+import { PROTO, ERRORS } from '../../../constants';
+import { UI, createUiMessage } from '../../../events';
 
 type Params = PROTO.BinanceGetAddress & {
     address?: string;
---
+++ packages/connect/src/api/binance/api/index.ts
@@ -0,0 +1,3 @@
+export { default as binanceGetAddress } from './binanceGetAddress';
+export { default as binanceGetPublicKey } from './binanceGetPublicKey';
+export { default as binanceSignTransaction } from './binanceSignTransaction';
