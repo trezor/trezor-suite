74fd08389d42c2ed4ce67b28d80c0f0199623275
Marek Polak <marek.polak@satoshilabs.com>
2023-09-26 13:52:39 +0200

    chore(connect): bl workers dynamic import

--- packages/connect/src/workers/workers.ts
+++ packages/connect/src/workers/workers.ts
@@ -1,6 +1,22 @@
-import BlockbookWorker from '@trezor/blockchain-link/lib/workers/blockbook';
-import RippleWorker from '@trezor/blockchain-link/lib/workers/ripple';
-import BlockfrostWorker from '@trezor/blockchain-link/lib/workers/blockfrost';
-import ElectrumWorker from '@trezor/blockchain-link/lib/workers/electrum';
+import type { BaseWorker } from '@trezor/blockchain-link/lib/workers/baseWorker';
+
+type WorkerAsyncImporter = () => Promise<BaseWorker<unknown>>;
+
+const BlockbookWorker: WorkerAsyncImporter = () =>
+    import(
+        /* webpackChunkName: "blockbook-worker" */ '@trezor/blockchain-link/lib/workers/blockbook'
+    ).then(w => w.default());
+const RippleWorker: WorkerAsyncImporter = () =>
+    import(
+        /* webpackChunkName: "ripple-worker" */ '@trezor/blockchain-link/lib/workers/ripple'
+    ).then(w => w.default());
+const BlockfrostWorker: WorkerAsyncImporter = () =>
+    import(
+        /* webpackChunkName: "blockfrost-worker" */ '@trezor/blockchain-link/lib/workers/blockfrost'
+    ).then(w => w.default());
+const ElectrumWorker: WorkerAsyncImporter = () =>
+    import(
+        /* webpackChunkName: "electrum-worker" */ '@trezor/blockchain-link/lib/workers/electrum'
+    ).then(w => w.default());
 
 export { BlockbookWorker, RippleWorker, BlockfrostWorker, ElectrumWorker };
