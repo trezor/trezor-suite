6afe7b468813f154769419a51bbf56692f9f7b57
dahaca <dahaca17@gmail.com>
2023-09-27 01:01:56 +0200

    feat(icons): update web icon component

--- packages/theme/src/types.ts
+++ packages/theme/src/types.ts
@@ -3,3 +3,9 @@ export type CSSColor =
     | `rgb(${number}, ${number}, ${number})`
     | `rgba(${number}, ${number}, ${number}, ${number})`
     | 'transparent';
+
+export const isCSSColor = (color: string): color is CSSColor => {
+    const cssColorRegex =
+        /^#([0-9a-fA-F]{3,8})$|^rgba?\((\d{1,3}),\s*(\d{1,3}),\s*(\d{1,3}),?\s*(\d{1,3})\)$/;
+    return cssColorRegex.test(color);
+};
