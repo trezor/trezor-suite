83ed86a334fa022bf24d8ffda589c621bfab1cea
dahaca <dahaca17@gmail.com>
2023-09-27 18:26:02 +0200

    feat(components): update checkbox

--- packages/components/src/components/form/Checkbox/Checkbox.tsx
+++ packages/components/src/components/form/Checkbox/Checkbox.tsx
@@ -1,103 +1,187 @@
-import { HTMLAttributes, EventHandler, SyntheticEvent, KeyboardEvent } from 'react';
-import styled from 'styled-components';
-import { darken } from 'polished';
+import { EventHandler, SyntheticEvent, KeyboardEvent, ReactNode } from 'react';
+import styled, { useTheme } from 'styled-components';
+import { borders, Color, spacingsPx, typography } from '@trezor/theme';
+
 import { KEYBOARD_CODE } from '../../../constants/keyboardEvents';
-import { FONT_SIZE } from '../../../config/variables';
-import { useTheme } from '../../../utils';
 import { Icon } from '../../assets/Icon/Icon';
+import { getFocusShadowStyle } from '../../../utils/utils';
 
-const Wrapper = styled.div<Pick<CheckboxProps, 'isDisabled'>>`
+interface VariantStyles {
+    background: Color;
+    border: Color;
+    backgroundHover: Color;
+    borderHover: Color;
+    backgroundChecked: Color;
+    borderChecked: Color;
+    backgroundDisabled: Color;
+    borderDisabled: Color;
+    backgroundDisabledChecked: Color;
+    borderDisabledChecked: Color;
+}
+
+export const variantStyles: Record<CheckboxVariant, VariantStyles> = {
+    primary: {
+        background: 'backgroundSurfaceElevation1',
+        border: 'borderOnElevation1',
+        backgroundHover: 'backgroundSurfaceElevation0',
+        borderHover: 'borderFocus',
+        backgroundChecked: 'backgroundPrimaryDefault',
+        borderChecked: 'backgroundPrimaryDefault',
+        backgroundDisabled: 'backgroundSurfaceElevationNegative',
+        borderDisabled: 'borderOnElevation0',
+        backgroundDisabledChecked: 'backgroundPrimarySubtleOnElevation0',
+        borderDisabledChecked: 'backgroundPrimarySubtleOnElevation0',
+    },
+    'alert-red': {
+        background: 'backgroundAlertRedSubtleOnElevation0',
+        border: 'borderAlertRed',
+        backgroundHover: 'backgroundSurfaceElevation0',
+        borderHover: 'borderAlertRed',
+        backgroundChecked: 'borderAlertRed',
+        borderChecked: 'borderAlertRed',
+        backgroundDisabled: 'backgroundSurfaceElevationNegative',
+        borderDisabled: 'backgroundAlertRedSubtleOnElevation0',
+        backgroundDisabledChecked: 'backgroundAlertRedSubtleOnElevation1',
+        borderDisabledChecked: 'backgroundAlertRedSubtleOnElevation1',
+    },
+    'alert-yellow': {
+        background: 'backgroundAlertYellowSubtleOnElevation0',
+        border: 'backgroundAlertYellowBold',
+        backgroundHover: 'backgroundSurfaceElevation0',
+        borderHover: 'backgroundAlertYellowBold',
+        backgroundChecked: 'backgroundAlertYellowBold',
+        borderChecked: 'backgroundAlertYellowBold',
+        backgroundDisabled: 'backgroundSurfaceElevationNegative',
+        borderDisabled: 'backgroundAlertYellowSubtleOnElevation0',
+        backgroundDisabledChecked: 'backgroundAlertYellowSubtleOnElevation1',
+        borderDisabledChecked: 'backgroundAlertYellowSubtleOnElevation1',
+    },
+};
+
+export const Container = styled.div<Pick<CheckboxProps, 'isDisabled' | 'labelAlignment'>>`
     display: flex;
+    align-items: center;
+    gap: ${spacingsPx.xs};
+    flex-direction: ${({ labelAlignment }) => labelAlignment === 'left' && 'row-reverse'};
+    pointer-events: ${({ isDisabled }) => isDisabled && 'none'};
     cursor: ${({ isDisabled }) => (isDisabled ? 'default' : 'pointer')};
-    align-items: center;
-
-    :hover,
-    :focus {
-        outline: none;
-    }
 `;
 
-const IconWrapper = styled.div<
-    Pick<CheckboxProps, 'isChecked' | 'isDisabled'> & { $color?: string }
->`
+export const CheckContainer = styled.div<{ variant: CheckboxVariant }>`
     display: flex;
     justify-content: center;
     align-items: center;
-    width: 24px;
-    min-width: 24px;
-    max-width: 24px;
-    height: 24px;
-    border-radius: 4px;
-    background: ${({ $color, isChecked, isDisabled, theme }) => {
-        if (isDisabled) {
-            return theme.BG_GREY;
-        }
-        return isChecked ? $color || theme.BG_GREEN : theme.BG_WHITE;
-    }};
-    border: 2px solid
-        ${({ $color, isChecked, isDisabled, theme }) =>
-            isChecked && !isDisabled ? $color || theme.BG_GREEN : theme.STROKE_GREY};
-    transition: border 0.1s ease-in-out;
+    width: ${spacingsPx.xl};
+    height: ${spacingsPx.xl};
+    border-radius: ${borders.radii.xs};
+    background: ${({ theme, variant }) => theme[variantStyles[variant].background]};
+    border: 2px solid ${({ theme, variant }) => theme[variantStyles[variant].border]};
+    transition: border 0.1s, background 0.1s, box-shadow 0.1s ease-out;
 
-    :hover,
-    :focus {
-        border: ${({ theme, isChecked, isDisabled }) =>
-            !isChecked &&
-            !isDisabled &&
-            `2px solid ${darken(theme.HOVER_DARKEN_FILTER, theme.STROKE_GREY)}`};
+    input:checked + && {
+        background: ${({ theme, variant }) => theme[variantStyles[variant].backgroundChecked]};
+        border-color: ${({ theme, variant }) => theme[variantStyles[variant].borderChecked]};
     }
+
+    input:disabled:not(:checked) + && {
+        background: ${({ theme, variant }) => theme[variantStyles[variant].backgroundDisabled]};
+        border-color: ${({ theme, variant }) => theme[variantStyles[variant].borderDisabled]};
+    }
+
+    input:disabled:checked + && {
+        background: ${({ theme, variant }) =>
+            theme[variantStyles[variant].backgroundDisabledChecked]};
+        border-color: ${({ theme, variant }) =>
+            theme[variantStyles[variant].backgroundDisabledChecked]};
+    }
+
+    ${/* sc-selector */ Container}:hover input:not(:disabled):not(:checked) + && {
+        background: ${({ theme, variant }) => theme[variantStyles[variant].backgroundHover]};
+        border-color: ${({ theme, variant }) => theme[variantStyles[variant].borderHover]};
+    }
+
+    ${getFocusShadowStyle()}
 `;
 
-const Label = styled.div`
+const CheckIcon = styled(Icon)<{ isVisible: boolean }>`
+    opacity: ${({ isVisible }) => (isVisible ? 1 : 0)};
+    transition: opacity 0.1s;
+`;
+
+export const Label = styled.div<{ isRed: boolean }>`
     display: flex;
     justify-content: center;
-    margin-left: 18px;
-    color: ${({ theme }) => theme.TYPE_DARK_GREY};
-    font-size: ${FONT_SIZE.SMALL};
-    line-height: 24px;
     text-align: left;
+    user-select: none;
+    color: ${({ theme, isRed }) => isRed && theme.textAlertRed};
+    ${typography.body}
+    transition: color 0.1s;
+
+    input:disabled ~ & {
+        color: ${({ theme }) => theme.textDisabled};
+    }
 `;
 
-export interface CheckboxProps extends HTMLAttributes<HTMLDivElement> {
-    color?: string;
+export const HiddenInput = styled.input`
+    position: absolute;
+    opacity: 0;
+    width: 0;
+    height: 0;
+`;
+
+export type CheckboxVariant = 'primary' | 'alert-red' | 'alert-yellow';
+export type LabelAlignment = 'left' | 'right';
+
+export interface CheckboxProps {
+    variant?: CheckboxVariant;
     isChecked?: boolean;
     isDisabled?: boolean;
+    labelAlignment?: LabelAlignment;
     onClick: EventHandler<SyntheticEvent>;
+    children?: ReactNode;
 }
 
 export const Checkbox = ({
-    children,
-    color,
+    variant = 'primary',
     isChecked,
     isDisabled,
+    labelAlignment = 'right',
     onClick,
-    ...rest
+    children,
 }: CheckboxProps) => {
     const theme = useTheme();
 
-    const handleClick: CheckboxProps['onClick'] = event => {
-        if (isDisabled) return;
-        onClick(event);
-    };
     const handleKeyUp = (event: KeyboardEvent<HTMLElement>) => {
-        if (event.code === KEYBOARD_CODE.SPACE) {
-            handleClick(event);
+        if (event.code === KEYBOARD_CODE.SPACE || event.code === KEYBOARD_CODE.ENTER) {
+            onClick(event);
         }
     };
 
     return (
-        <Wrapper
+        <Container
             isDisabled={isDisabled}
-            onClick={handleClick}
+            labelAlignment={labelAlignment}
+            onClick={onClick}
             onKeyUp={handleKeyUp}
-            tabIndex={0}
-            {...rest}
         >
-            <IconWrapper $color={color} isChecked={isChecked} isDisabled={isDisabled}>
-                {isChecked && <Icon size={24} color={theme.TYPE_WHITE} icon="CHECK" />}
-            </IconWrapper>
+            <HiddenInput
+                checked={isChecked}
+                disabled={isDisabled}
+                readOnly
+                type="checkbox"
+                tabIndex={-1}
+            />
 
-            <Label>{children}</Label>
-        </Wrapper>
+            <CheckContainer tabIndex={0} variant={variant}>
+                <CheckIcon
+                    isVisible={!!isChecked}
+                    size={24}
+                    color={theme.iconOnPrimary}
+                    icon="CHECK"
+                />
+            </CheckContainer>
+
+            {children && <Label isRed={variant === 'alert-red'}>{children}</Label>}
+        </Container>
     );
 };
