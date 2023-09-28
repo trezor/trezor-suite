f609c7a32be042c1debbefbe94dced3f79d7c9b1
dahaca <dahaca17@gmail.com>
2023-09-28 02:22:35 +0200

    feat(components): update the input label design

--- packages/components/src/components/form/Input/Input.stories.tsx
+++ packages/components/src/components/form/Input/Input.stories.tsx
@@ -10,12 +10,17 @@ export default {
         value: 'Input',
         label: 'Label',
         bottomText: '',
-        placeholder: '',
         isDisabled: false,
         inputState: null,
         variant: null,
     },
     argTypes: {
+        labelRight: {
+            type: 'string',
+        },
+        placeholder: {
+            type: 'string',
+        },
         state: {
             control: {
                 options: {
