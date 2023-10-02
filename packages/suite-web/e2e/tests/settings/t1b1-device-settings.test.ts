ab30860f5be7df3bd594891dbd5398ef055a10d7
Martin Varmuza <mvarmuza@gmail.com>
2023-10-02 22:49:38 +0200

    test(e2e): add pin success; re-enable pin mismatch

--- packages/suite-web/e2e/tests/settings/t1b1-device-settings.test.ts
+++ packages/suite-web/e2e/tests/settings/t1b1-device-settings.test.ts
@@ -1,4 +1,4 @@
-// @group:bounty
+// @group:settings
 // @retry=2
 
 describe('T1B1 - Device settings', () => {
@@ -11,6 +11,33 @@ describe('T1B1 - Device settings', () => {
         cy.task('stopEmu');
     });
 
+    it('enable pin', () => {
+        cy.viewport(1080, 1440).resetDb();
+        cy.prefixedVisit('/settings/device');
+        cy.passThroughInitialRun();
+
+        cy.getTestElement('@settings/device/pin-switch').click({ force: true });
+        cy.getTestElement('@suite/modal/confirm-action-on-device');
+        cy.task('pressYes');
+
+        // todo: this should be converted to a custom cypress command, something like "enterPinOnBlindMatrix(number)"
+        cy.task('getDebugState').then(state => {
+            const index = state.matrix.indexOf('1') + 1;
+            cy.getTestElement(`@pin/input/${index}`).click();
+            cy.getTestElement('@pin/submit-button').click();
+        });
+
+        cy.getTestElement('@pin/input/1');
+
+        cy.task('getDebugState').then(state => {
+            const index = state.matrix.indexOf('1') + 1;
+            cy.getTestElement(`@pin/input/${index}`).click();
+            cy.getTestElement('@pin/submit-button').click();
+        });
+
+        cy.getTestElement('@toast/pin-changed');
+    });
+
     it('pin mismatch', () => {
         cy.viewport(1080, 1440).resetDb();
         cy.prefixedVisit('/settings/device');
@@ -19,14 +46,11 @@ describe('T1B1 - Device settings', () => {
         cy.getTestElement('@settings/device/pin-switch').click({ force: true });
         cy.getTestElement('@suite/modal/confirm-action-on-device');
         cy.task('pressYes');
-        // todo: add support for pin to trezor-user-env. now I may safely test only wrong pin input
-        // cy.getTestElement('@pin').matchImageSnapshot('first pin input');
         cy.getTestElement('@pin/input/1').click();
 
         cy.getTestElement('@pin/submit-button').click();
         cy.log('enter 2 digits instead of 1 in the first entry. This way pin is always wrong');
         cy.getTestElement('@pin/input/1').click();
-        // cy.getTestElement('@pin').matchImageSnapshot('confirm pin input');
         cy.getTestElement('@pin/input/1').click();
 
         cy.getTestElement('@pin/submit-button').click();
@@ -35,6 +59,7 @@ describe('T1B1 - Device settings', () => {
         cy.getTestElement('@suite/modal/confirm-action-on-device');
         cy.task('pressYes');
     });
+
     /*
      * 1. navigate to settings/device screen and wait for it to load
      * 2. Select & click Choose from gallery in Customization section
@@ -42,7 +67,7 @@ describe('T1B1 - Device settings', () => {
      * 4. Confirm on device
      * 5. Wait for success notification Settings applied
      */
-    it('change homescreen', () => {
+    it.skip('change homescreen', () => {
         //
         // Test preparation
         //
@@ -64,7 +89,7 @@ describe('T1B1 - Device settings', () => {
         //
         cy.getTestElement('@toast/settings-applied').should('be.visible');
     });
-    // TODO: pin success
+
     // TODO: pin caching immediately after it is set
     // TODO: keyboard handling
     // TODO: set auto-lock (needs pin)
