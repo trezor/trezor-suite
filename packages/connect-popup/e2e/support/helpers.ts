d0ec5163c50cfe66679c95a41fb76f3a9127278a
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2023-10-03 21:33:11 +0200

    test(connect-popup): helper to test popup with webextension

--- packages/connect-popup/e2e/support/helpers.ts
+++ packages/connect-popup/e2e/support/helpers.ts
@@ -1,6 +1,7 @@
 /* eslint-disable no-await-in-loop */
 
-import { Page } from '@playwright/test';
+import path from 'path';
+import { chromium, Page } from '@playwright/test';
 
 // Waits and clicks for an array on buttons in serial order.
 export const waitAndClick = async (page: Page, buttons: string[]) => {
@@ -19,4 +20,51 @@ export const findElementByDataTest = async (page: Page, dataTest: string, timeou
 
 export const log = (...val: string[]) => {
     console.log(`[===]`, ...val);
+};
+
+export const getExtensionPage = async () => {
+    const pathToExtension = path.join(
+        __dirname,
+        '..',
+        '..',
+        '..',
+        'connect-explorer-webextension',
+        'build',
+    );
+
+    const userDataDir = `/tmp/test-user-data-dir/${new Date().getTime()}`;
+    const browserContext = await chromium.launchPersistentContext(userDataDir, {
+        // https://playwright.dev/docs/chrome-extensions#headless-mode
+        // By default, Chrome's headless mode in Playwright does not support Chrome extensions.
+        // To overcome this limitation, you can run Chrome's persistent context with a new headless mode.
+        // using `--headless=new`
+        headless: false,
+        args: [
+            process.env.HEADLESS === 'true' ? `--headless=new` : '', // the new headless arg for chrome v109+. Use '--headless=chrome' as arg for browsers v94-108.
+            `--disable-extensions-except=${pathToExtension}`,
+            `--load-extension=${pathToExtension}`,
+        ],
+    });
+
+    await browserContext.clearCookies();
+
+    const page = await browserContext.newPage();
+
+    // https://playwright.dev/docs/chrome-extensions#testing
+    // It looks like the only way to get extension ID from a MV3 web extension in playwright is having serviceworker loaded.
+    let [background] = browserContext.serviceWorkers();
+    if (!background) background = await browserContext.waitForEvent('serviceworker');
+
+    // https://github.com/microsoft/playwright/issues/5593#issuecomment-949813218
+    await page.goto('chrome://inspect/#extensions');
+
+    const extensionId = background.url().split('/')[2];
+
+    const url = `chrome-extension://${extensionId}/connect-explorer.html`;
+
+    return {
+        page,
+        url,
+        browserContext,
+    };
 };
