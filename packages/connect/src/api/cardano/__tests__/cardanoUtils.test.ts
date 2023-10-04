f787d4f1c88dbf04d44e861a5965ea99acdddac7
Marek Polak <marek.polak@satoshilabs.com>
2023-10-04 16:05:26 +0200

    test(connect): cardanoUtils tests

---
+++ packages/connect/src/api/cardano/__tests__/cardanoUtils.test.ts
@@ -0,0 +1,58 @@
+import { composeTxPlan, getTtl, transformUtxos, prepareCertificates } from '../cardanoUtils';
+import * as fixtures from '../__fixtures__/cardanoUtils';
+
+jest.mock('@fivebinaries/coin-selection', () => ({
+    __esModule: true,
+    default: () => {},
+    coinSelection: () => {},
+}));
+
+describe('cardano utils', () => {
+    let dateSpy: any;
+    beforeAll(() => {
+        dateSpy = jest.spyOn(Date.prototype, 'getTime').mockReturnValue(1653394389512);
+    });
+
+    afterAll(() => {
+        dateSpy.mockRestore();
+    });
+
+    it('composeTxPlan', () => {
+        expect(
+            composeTxPlan(
+                'descriptor',
+                [],
+                [],
+                [
+                    {
+                        type: 0,
+                    },
+                    {
+                        path: 'path',
+                        pool: 'abc',
+                        type: 2,
+                    },
+                ],
+                [{ amount: '10', stakeAddress: 'stkAddr' }],
+                'addr',
+                true,
+            ),
+        ).toBeUndefined();
+    });
+
+    fixtures.transformUtxos.forEach(f => {
+        it(`transformUtxos: ${f.description}`, () => {
+            expect(transformUtxos(f.utxo)).toMatchObject(f.result);
+        });
+    });
+
+    fixtures.prepareCertificates.forEach(f => {
+        it(`prepareCertificates: ${f.description}`, () => {
+            expect(prepareCertificates(f.certificates)).toMatchObject(f.result);
+        });
+    });
+
+    it(`getTTL`, () => {
+        expect(getTtl(true)).toBe(-13254411);
+    });
+});
