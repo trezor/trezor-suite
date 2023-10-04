85f9ecefc074a77754dd906e007f6a3304efda35
Marek Polak <marek.polak@satoshilabs.com>
2023-10-04 15:23:05 +0200

    feat(connect): cardanoSignTransaction extended

--- packages/connect/src/types/api/cardanoSignTransaction.ts
+++ packages/connect/src/types/api/cardanoSignTransaction.ts
@@ -4,3 +4,12 @@ import type { CardanoSignTransaction, CardanoSignedTxData } from './cardano';
 export declare function cardanoSignTransaction(
     params: Params<CardanoSignTransaction>,
 ): Response<CardanoSignedTxData>;
+
+export declare function cardanoSignTransaction(
+    params: Params<
+        CardanoSignTransaction & {
+            unsignedTx: { body: string; hash: string };
+            testnet: boolean;
+        }
+    >,
+): Response<CardanoSignedTxData & { serializedTx: string }>;
