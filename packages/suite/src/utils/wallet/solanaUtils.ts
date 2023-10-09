0c119f22127af1a2e43c78c64098c2bd87bf968a
Samuel Šulovský <samuel.sulovsky@gmail.com>
2023-10-09 10:48:17 +0200

    feat(suite): create Solana token transfer instructions

--- packages/suite/src/utils/wallet/solanaUtils.ts
+++ packages/suite/src/utils/wallet/solanaUtils.ts
@@ -1,9 +1,36 @@
+import * as BufferLayout from '@solana/buffer-layout';
 import { getLamportsFromSol } from '@suite-common/wallet-utils';
 
 const loadSolanaLib = async () => {
     const lib = await import('@solana/web3.js');
 
     return lib;
+};
+
+export const getPubKeyFromAddress = async (address: string) => {
+    const { PublicKey } = await loadSolanaLib();
+
+    return new PublicKey(address);
+};
+
+// To transfer Solana tokens we need to manually encode a buffer with the correct layout
+// according to the instruction standard.
+const LAYOUT = BufferLayout.union(BufferLayout.u8('instruction'));
+LAYOUT.addVariant(
+    12,
+    BufferLayout.struct([BufferLayout.nu64('amount'), BufferLayout.u8('decimals')]),
+    'transferChecked',
+);
+
+const instructionMaxSpan = Math.max(...Object.values(LAYOUT.registry).map((r: any) => r.span));
+
+const encodeTokenTransferInstructionData = (instruction: {
+    transferChecked: { amount: BigNumber; decimals: number };
+}): Buffer => {
+    const b = Buffer.alloc(instructionMaxSpan);
+    const span = LAYOUT.encode(instruction, b);
+
+    return b.subarray(0, span);
 };
 
 export const buildTransferTransaction = async (
@@ -29,8 +56,30 @@ export const buildTransferTransaction = async (
     return transaction;
 };
 
-export const getPubKeyFromAddress = async (address: string) => {
-    const { PublicKey } = await loadSolanaLib();
+// Construct the transfer instruction for a token transfer
+// exported for testing
+export const buildTokenTransferInstruction = async (
+    from: string,
+    to: string,
+    owner: string,
+    amount: BigNumber,
+    mint: string,
+    decimals: number,
+) => {
+    const { TransactionInstruction, PublicKey } = await loadSolanaLib();
+    // key layout: https://github.com/solana-labs/solana-program-library/blob/master/token/program/src/instruction.rs#L254
+    const keys = [
+        { pubkey: new PublicKey(from), isSigner: false, isWritable: true },
+        { pubkey: new PublicKey(mint), isSigner: false, isWritable: false },
+        { pubkey: new PublicKey(to), isSigner: false, isWritable: true },
+        { pubkey: new PublicKey(owner), isSigner: true, isWritable: false },
+    ];
 
-    return new PublicKey(address);
+    return new TransactionInstruction({
+        keys,
+        data: encodeTokenTransferInstructionData({
+            transferChecked: { amount, decimals },
+        }),
+        programId: new PublicKey(TOKEN_PROGRAM_PUBLIC_KEY),
+    });
 };
