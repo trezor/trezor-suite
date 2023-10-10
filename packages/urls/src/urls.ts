cdc02353eee5cfa678816428437cf77807e3c364
Matej Kriz <matej@matejkriz.cz>
2023-10-10 16:46:42 +0200

    feat(urls): T2B1 update links

--- packages/urls/src/urls.ts
+++ packages/urls/src/urls.ts
@@ -26,16 +26,14 @@ export const HELP_CENTER_DRY_RUN_T1B1_URL =
 export const HELP_CENTER_DRY_RUN_T2T1_URL =
     'https://trezor.io/learn/a/test-recovery-seed-on-trezor-model-t';
 export const HELP_CENTER_DRY_RUN_T2B1_URL =
-    'https://trezor.io/learn/a/test-recovery-seed-on-trezor-model-r';
+    'https://trezor.io/learn/a/check-backup-on-trezor-safe-3';
 export const HELP_CENTER_PASSPHRASE_URL =
     'https://trezor.io/learn/a/passphrases-and-hidden-wallets';
 export const HELP_CENTER_RECOVERY_SEED_URL = 'https://trezor.io/learn/a/how-to-use-a-recovery-seed';
-export const HELP_CENTER_PACKAGING_T1B1_URL =
-    'https://trezor.io/learn/a/authenticate-trezor-model-one';
-export const HELP_CENTER_PACKAGING_T2T1_URL =
-    'https://trezor.io/learn/a/authenticate-trezor-model-t';
+export const HELP_CENTER_PACKAGING_T1B1_URL = 'https://trezor.io/learn/a/authenticate-model-one';
+export const HELP_CENTER_PACKAGING_T2T1_URL = 'https://trezor.io/learn/a/authenticate-model-t';
 export const HELP_CENTER_PACKAGING_T2B1_URL =
-    'https://trezor.io/learn/a/authenticate-trezor-model-r';
+    'https://trezor.io/learn/a/authenticate-trezor-safe-3';
 export const HELP_CENTER_XRP_URL = 'https://trezor.io/learn/a/ripple-xrp-on-trezor-model-t';
 export const HELP_CENTER_CASHADDR_URL = 'https://trezor.io/learn/a/glossary#CashAddr';
 export const HELP_CENTER_QR_CODE_URL = 'https://trezor.io/learn/a/qr-codes-in-trezor-suite-app';
@@ -49,7 +47,7 @@ export const HELP_CENTER_FW_DOWNGRADE_T1B1_URL =
 export const HELP_CENTER_FW_DOWNGRADE_T2T1_URL =
     'https://trezor.io/learn/a/downgrade-firmware-trezor-model-t';
 export const HELP_CENTER_FW_DOWNGRADE_T2B1_URL =
-    'https://trezor.io/learn/a/downgrade-firmware-trezor-model-r';
+    'https://trezor.io/learn/a/downgrade-firmware-trezor-safe-3';
 export const HELP_CENTER_FAILED_BACKUP_URL = 'https://trezor.io/support/a/trezor-recovery-issues';
 export const HELP_CENTER_ADVANCED_RECOVERY_URL =
     'https://trezor.io/learn/a/advanced-recovery-on-trezor-model-one';
@@ -58,7 +56,8 @@ export const HELP_FIRMWARE_TYPE = 'https://trezor.io/learn/a/bitcoin-only-firmwa
 export const HELP_CENTER_ZERO_VALUE_ATTACKS =
     'https://trezor.io/support/a/address-poisoning-attacks';
 export const HELP_CENTER_LABELING = 'https://trezor.io/learn/a/labels-in-trezor-suite-app';
-export const HELP_CENTER_DEVICE_AUTHENTICATION = 'https://trezor.io/learn'; // TODO
+export const HELP_CENTER_DEVICE_AUTHENTICATION =
+    'https://blog.trezor.io/introducing-an-added-element-of-security-for-your-trezor-safe-3-193474ad48be';
 
 export const INVITY_URL = 'https://invity.io/';
 export const INVITY_SCHEDULE_OF_FEES = 'https://blog.invity.io/schedule-of-fees';
