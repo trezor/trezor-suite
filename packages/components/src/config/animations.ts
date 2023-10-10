26e8241af1ae7df96bb570dc472ffacf2ebe1aa7
Jan Václavík <jvaclavik@gmail.com>
2023-10-10 11:13:20 +0200

    fix(suite): Animation cleanup

--- packages/components/src/config/animations.ts
+++ packages/components/src/config/animations.ts
@@ -1,52 +1,5 @@
 import { keyframes } from 'styled-components';
 
-export const DELAYED_SPIN = keyframes`
-    0% {
-        transform: rotate(0deg);
-    }
-    50% {
-        transform: rotate(360deg);
-    }
-    100% {
-        transform: rotate(360deg);
-    }
-`;
-
-export const DASH = keyframes`
-    0% {
-        stroke-dasharray: 1, 200;
-        stroke-dashoffset: 0;
-    }
-    50% {
-        stroke-dasharray: 89, 200;
-        stroke-dashoffset: -35;
-    }
-    100% {
-        stroke-dasharray: 89, 200;
-        stroke-dashoffset: -124;
-    }
-`;
-
-export const SLIDE_UP = keyframes`
-    0% {
-        transform: translateY(150%);
-    }
-    100% {
-        transform: translateY(0%);
-    }
-`;
-
-export const SLIDE_DOWN = keyframes`
-    0% {
-        transform: translateY(0%);
-        opacity: 1;
-    }
-    100% {
-        transform: translateY(150%);
-        opacity: 0;
-    }
-`;
-
 export const FADE_IN = keyframes`
     0% {
         opacity: 0;
@@ -56,7 +9,7 @@ export const FADE_IN = keyframes`
     }
 `;
 
-export const DROPDOWN_MENU = keyframes` 
+export const DROPDOWN_MENU = keyframes`
     0% {
         opacity: 0;
         transform: translateY(-12px);
