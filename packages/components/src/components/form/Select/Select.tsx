379ba9c31545f28f2fccebaa408ad5102185a9a7
dahaca <dahaca17@gmail.com>
2023-10-18 13:05:56 +0200

    feat(components): update select style

--- packages/components/src/components/form/Select/Select.tsx
+++ packages/components/src/components/form/Select/Select.tsx
@@ -1,196 +1,43 @@
 import { useCallback, useRef, ReactNode } from 'react';
 import ReactSelect, { Props as ReactSelectProps, StylesConfig, SelectInstance } from 'react-select';
 import styled, { css, DefaultTheme, useTheme } from 'styled-components';
-import { darken } from 'polished';
-import { borders, zIndices } from '@trezor/theme';
+import { borders, spacingsPx, typography, zIndices } from '@trezor/theme';
 
-import { FONT_WEIGHT, FONT_SIZE } from '../../../config/variables';
-import { animations } from '../../../config';
-
-import {
-    LabelLeft,
-    INPUT_HEIGHTS,
-    INPUT_BORDER_WIDTH,
-    getInputStateTextColor,
-    getInputStateBorderColor,
-} from '../InputStyles';
+import { INPUT_HEIGHTS, baseInputStyle } from '../InputStyles';
 import { BottomText } from '../BottomText';
 import { InputState, InputSize } from '../inputTypes';
 import { Control, GroupHeading, Option } from './customComponents';
 import { useOnKeyDown } from './useOnKeyDown';
 import { useDetectPortalTarget } from './useDetectPortalTarget';
+import { menuStyle } from '../../Dropdown/menuStyle';
 
 const reactSelectClassNamePrefix = 'react-select';
 
-const selectStyle = (
-    isSearchable: boolean,
-    withDropdownIndicator = true,
-    variant: InputSize,
-    hideTextCursor: boolean,
-    isClean: boolean,
-    minWidth: string,
-    theme: DefaultTheme,
-    inputState?: InputState,
-): StylesConfig<Option, boolean> => ({
-    singleValue: base => ({
-        ...base,
-        display: 'flex',
-        alignItems: 'center',
-        width: '100%',
-        maxWidth: 'initial',
-        margin: 0,
-        padding: '0 8px',
-        color: isClean ? theme.TYPE_LIGHT_GREY : theme.TYPE_DARK_GREY,
-        fontSize: FONT_SIZE.SMALL,
-        fontWeight: FONT_WEIGHT.MEDIUM,
-        borderStyle: 'none',
-        justifyContent: isClean ? 'flex-end' : 'flex-start',
-        position: 'static',
-        transform: 'none',
-        '&:hover': {
-            cursor: hideTextCursor || !isSearchable ? 'pointer' : 'text',
-        },
-    }),
-    control: (base, { isDisabled, menuIsOpen }) => {
-        const borderColorBase = menuIsOpen ? theme.TYPE_LIGHT_GREY : theme.STROKE_GREY;
-        const borderColor = inputState
-            ? getInputStateBorderColor(inputState, theme)
-            : borderColorBase;
-
-        return {
-            ...base,
-            display: 'flex',
-            alignItems: 'center',
-            fontSize: FONT_SIZE.SMALL,
-            height: isClean ? 22 : INPUT_HEIGHTS[variant],
-            borderRadius: borders.radii.sm,
-            borderWidth: INPUT_BORDER_WIDTH,
-            borderColor,
-            borderStyle: isClean ? 'none' : 'solid',
-            backgroundColor: isDisabled && !isClean ? theme.BG_GREY : 'transparent',
-            boxShadow: 'none',
-            flexWrap: 'nowrap',
-            cursor: 'pointer',
-            '&:hover': {
-                borderColor: darken(
-                    theme.HOVER_DARKEN_FILTER,
-                    inputState ? getInputStateTextColor(inputState, theme) : borderColorBase,
-                ),
-                [`.${reactSelectClassNamePrefix}__dropdown-indicator`]: {
-                    color: darken(theme.HOVER_DARKEN_FILTER, theme.STROKE_GREY),
-                },
-            },
-            '&:focus-within': {
-                borderColor: inputState
-                    ? darken(theme.HOVER_DARKEN_FILTER, getInputStateTextColor(inputState, theme))
-                    : theme.TYPE_LIGHT_GREY,
-                [`.${reactSelectClassNamePrefix}__dropdown-indicator`]: {
-                    color: theme.TYPE_LIGHT_GREY,
-                    transform: menuIsOpen ? 'rotate(180deg)' : 'none',
-                },
-            },
-        };
-    },
-    valueContainer: base => ({
-        ...base,
-        border: 0,
-        padding: isClean ? '0 3px 0 0' : '2px 8px',
-        fontWeight: isClean ? FONT_WEIGHT.MEDIUM : FONT_WEIGHT.REGULAR,
-        minWidth,
-        display: 'flex',
-        flexWrap: 'nowrap',
-        justifyContent: isClean ? 'flex-end' : 'flex-start',
-    }),
-    indicatorSeparator: () => ({
-        display: 'none',
-    }),
-    dropdownIndicator: (base, { isDisabled }) => ({
-        ...base,
-        display: !withDropdownIndicator || isDisabled ? 'none' : 'flex',
-        alignItems: 'center',
-        color: isClean ? theme.TYPE_LIGHTER_GREY : theme.STROKE_GREY,
-        cursor: 'pointer',
-        path: '',
-        padding: isClean ? 0 : '10px 16px',
-        transition: `transform 0.2s cubic-bezier(0.68, -0.02, 0.21, 1.1)`,
-    }),
-    menu: base => ({
-        ...base,
-        width: 'max-content',
-        minWidth: '100%',
-        background: theme.BG_WHITE_ALT,
-        margin: '5px 0',
-        borderRadius: '4px',
-        zIndex: zIndices.base,
-    }),
+const createSelectStyle = (theme: DefaultTheme): StylesConfig<Option, boolean> => ({
     menuPortal: base => ({
         ...base,
         zIndex: zIndices.modal /* Necessary to be visible inside a Modal */,
     }),
-    menuList: base => ({
-        ...base,
-        boxShadow: '0 1px 2px 0 rgba(0, 0, 0, 0.2)',
-        background: theme.BG_WHITE_ALT,
-        borderRadius: 8,
-        padding: 8,
-    }),
-    groupHeading: base => ({
-        ...base,
-        fontSize: FONT_SIZE.TINY,
-        textTransform: 'initial',
-        margin: 0,
-        padding: 8,
-    }),
-    group: base => ({
-        ...base,
-        padding: 0,
-        '& + &': {
-            paddingTop: 4,
-            marginTop: 4,
-        },
-    }),
     option: (base, { isFocused }) => ({
         ...base,
-        color: theme.TYPE_DARK_GREY,
-        background: isFocused ? theme.BG_WHITE_ALT_HOVER : theme.BG_WHITE_ALT,
-        borderRadius: 5,
-        padding: 8,
-        fontSize: FONT_SIZE.SMALL,
-        fontWeight: FONT_WEIGHT.MEDIUM,
-        '&:hover': {
-            cursor: 'pointer',
-        },
-        '&:active': {
-            background: theme.BG_WHITE_ALT_HOVER,
-        },
-    }),
-    input: base => ({
-        ...base,
-        width: hideTextCursor ? 2 : 'auto',
-        margin: hideTextCursor ? 0 : 2,
-        fontSize: FONT_SIZE.SMALL,
-        fontWeight: FONT_WEIGHT.MEDIUM,
-        padding: '2px 6px',
-        color: hideTextCursor ? 'transparent' : theme.TYPE_DARK_GREY,
-        '& input': {
-            textShadow: hideTextCursor ? `0 0 0 ${theme.TYPE_DARK_GREY} !important` : 'none',
-        },
-    }),
-    placeholder: base => ({
-        ...base,
-        fontWeight: FONT_WEIGHT.MEDIUM,
-        fontSize: FONT_SIZE.SMALL,
-        padding: '0 6px',
-        position: 'absolute',
+        background: isFocused ? theme.backgroundSurfaceElevation0 : 'transparent',
     }),
 });
 
-const Wrapper = styled.div<Pick<SelectProps, 'isClean'>>`
-    width: 100%;
+type WrapperProps = Pick<
+    SelectProps,
+    | 'isClean'
+    | 'hideTextCursor'
+    | 'withDropdownIndicator'
+    | 'isDisabled'
+    | 'minWidth'
+    | 'size'
+    | 'menuIsOpen'
+    | 'isSearchable'
+>;
 
-    .${reactSelectClassNamePrefix}__menu {
-        animation: ${animations.DROPDOWN_MENU} 0.15s ease-in-out;
-    }
+const Wrapper = styled.div<WrapperProps>`
+    width: 100%;
 
     ${({ isClean }) =>
         !isClean &&
@@ -199,14 +46,116 @@ const Wrapper = styled.div<Pick<SelectProps, 'isClean'>>`
             flex-direction: column;
             justify-content: flex-start;
         `}
-`;
 
-const Label = styled.div`
-    display: flex;
-    justify-content: end;
-    align-items: flex-end;
-    min-height: 30px;
-    padding-bottom: 6px;
+    .${reactSelectClassNamePrefix}__dropdown-indicator {
+        display: ${({ withDropdownIndicator }) => (!withDropdownIndicator ? 'none' : 'flex')};
+        align-items: center;
+        color: ${({ theme, isDisabled }) => (isDisabled ? theme.iconDisabled : theme.iconSubdued)};
+        padding: 0;
+        transition: transform 0.2s cubic-bezier(0.68, -0.02, 0.21, 1.1);
+        cursor: pointer;
+    }
+
+    .${reactSelectClassNamePrefix}__control {
+        ${baseInputStyle}
+        display: flex;
+        align-items: center;
+        flex-wrap: nowrap;
+        height: ${({ isClean, size }) => (isClean ? 22 : size && INPUT_HEIGHTS[size])}px;
+        padding: ${({ isClean }) => (isClean ? 0 : `0 ${spacingsPx.md}`)};
+        border-style: ${({ isClean }) => (isClean ? 'none' : 'solid')};
+        background: ${({ isClean }) => isClean && 'transparent !important'};
+        box-shadow: none;
+        ${typography.body};
+        cursor: pointer;
+
+        :hover:not(:focus-within) {
+            border-color: transparent;
+        }
+
+        :focus-within {
+            .${reactSelectClassNamePrefix}__dropdown-indicator {
+                transform: rotate(180deg);
+            }
+        }
+    }
+
+    .${reactSelectClassNamePrefix}__placeholder {
+        color: ${({ theme, isDisabled }) => (isDisabled ? theme.textDisabled : theme.textSubdued)};
+        ${typography.body}
+    }
+
+    .${reactSelectClassNamePrefix}__value-container {
+        display: flex;
+        flex-wrap: nowrap;
+        min-width: ${({ minWidth }) => minWidth};
+        justify-content: ${({ isClean }) => (isClean ? 'flex-end' : 'flex-start')};
+        padding: 0;
+        border: none;
+    }
+
+    .${reactSelectClassNamePrefix}__single-value {
+        position: static;
+        display: flex;
+        align-items: center;
+        justify-content: ${({ isClean }) => (isClean ? 'flex-end' : 'flex-start')};
+        width: 100%;
+        max-width: initial;
+        color: ${({ isDisabled, theme }) => (isDisabled ? theme.textDisabled : theme.textDefault)};
+        border-style: none;
+        transform: none;
+
+        :hover {
+            cursor: ${({ hideTextCursor, isSearchable }) =>
+                hideTextCursor || !isSearchable ? 'pointer' : 'text'};
+        }
+    }
+
+    .${reactSelectClassNamePrefix}__input {
+        width: ${({ hideTextCursor }) => (hideTextCursor ? '2px' : 'auto')};
+        margin: ${({ hideTextCursor }) => (hideTextCursor ? 0 : '2px')};
+        color: ${({ theme }) => theme.textDefault} !important;
+
+        ${typography.body};
+    }
+
+    .${reactSelectClassNamePrefix}__indicator-separator {
+        display: none;
+    }
+
+    .${reactSelectClassNamePrefix}__menu {
+        ${menuStyle}
+        border: none;
+        z-index: ${zIndices.base};
+    }
+
+    .${reactSelectClassNamePrefix}__group-heading {
+        margin: 0;
+        padding: ${spacingsPx.xs};
+        ${typography.label};
+        text-transform: initial;
+    }
+
+    .${reactSelectClassNamePrefix}__group {
+        padding: 0;
+
+        & + & {
+            padding-top: ${spacingsPx.xxs};
+            margin-top: ${spacingsPx.xxs};
+        }
+    }
+
+    .${reactSelectClassNamePrefix}__option {
+        padding: ${spacingsPx.xs} ${spacingsPx.sm};
+        border-radius: ${borders.radii.xs};
+        color: ${({ theme }) => theme.textDefault};
+        ${typography.body};
+        cursor: pointer;
+
+        :active {
+            background: ${({ theme }) => theme.backgroundSurfaceElevation0};
+        }
+    }
 `;
 
 // Prevent closing the menu when scrolling through options.
@@ -255,6 +204,7 @@ export const Select = ({
     inputState,
     components,
     onChange,
+    isDisabled,
     'data-test': dataTest,
     ...props
 }: SelectProps) => {
@@ -279,13 +229,18 @@ export const Select = ({
     );
 
     return (
-        <Wrapper className={className} isClean={isClean} {...wrapperProps}>
-            {label && (
-                <Label>
-                    <LabelLeft>{label}</LabelLeft>
-                </Label>
-            )}
-
+        <Wrapper
+            className={className}
+            isClean={isClean}
+            isSearchable={isSearchable}
+            withDropdownIndicator={withDropdownIndicator}
+            size={size}
+            hideTextCursor={hideTextCursor}
+            minWidth={minWidth}
+            isDisabled={isDisabled}
+            menuIsOpen={menuIsOpen}
+            {...wrapperProps}
+        >
             <ReactSelect
                 ref={selectRef}
                 onKeyDown={onKeyDown}
@@ -294,19 +249,11 @@ export const Select = ({
                 closeMenuOnScroll={closeMenuOnScroll}
                 menuPosition="fixed" // Required for closeMenuOnScroll to work properly when near page bottom
                 menuPortalTarget={menuPortalTarget}
-                styles={selectStyle(
-                    isSearchable,
-                    withDropdownIndicator,
-                    size,
-                    hideTextCursor,
-                    isClean,
-                    minWidth,
-                    theme,
-                    inputState,
-                )}
+                styles={createSelectStyle(theme)}
                 onChange={handleOnChange}
                 isSearchable={isSearchable}
                 menuIsOpen={menuIsOpen}
+                isDisabled={isDisabled}
                 {...props}
                 components={{
                     Control: controlProps => <Control {...controlProps} dataTest={dataTest} />,
