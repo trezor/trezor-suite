9345ffc535bd890e91a413f66b136134a353d50f
dahaca <dahaca17@gmail.com>
2023-10-18 13:05:56 +0200

    feat(components): update select style

--- packages/components/src/components/form/Select/Select.stories.tsx
+++ packages/components/src/components/form/Select/Select.stories.tsx
@@ -17,28 +17,7 @@ const options = Object.keys(values)
 
 export default {
     title: 'Form/Select',
-    argTypes: {
-        option: {
-            control: {
-                disable: true,
-            },
-        },
-        variant: {
-            control: {
-                options: { 'Large (default)': null, Small: 'small' },
-                type: 'radio',
-            },
-        },
-    },
-    args: {
-        option: 'low',
-        variant: null,
-        isSearchable: false,
-        isClearable: false,
-        isClean: false,
-        isDisabled: false,
-        withDropdownIndicator: true,
-    },
+    component: SelectComponent,
 };
 
 export const Select: StoryObj<SelectProps> = {
@@ -47,18 +26,27 @@ export const Select: StoryObj<SelectProps> = {
         const [{ option }, updateArgs] = useArgs();
         const setOption = (option: { label: string; value: 'string' }) => updateArgs({ option });
 
-        return (
-            <SelectComponent
-                isSearchable={args.isSearchable}
-                isClearable={args.isClearable}
-                isClean={args.isClean}
-                isDisabled={args.isDisabled}
-                withDropdownIndicator={args.withDropdownIndicator}
-                size={args.variant}
-                value={option}
-                onChange={setOption}
-                options={options}
-            />
-        );
+        return <SelectComponent {...args} value={option} onChange={setOption} options={options} />;
+    },
+    argTypes: {
+        isSearchable: {
+            control: {
+                type: 'boolean',
+            },
+        },
+        isDisabled: {
+            control: {
+                type: 'boolean',
+            },
+        },
+        size: {
+            control: {
+                options: { 'Large (default)': null, Small: 'small' },
+                type: 'radio',
+            },
+        },
+    },
+    args: {
+        label: 'Label',
     },
 };
--- packages/components/src/config/animations.ts
+++ packages/components/src/config/animations.ts
@@ -8,15 +8,3 @@ export const FADE_IN = keyframes`
         opacity: 1;
     }
 `;
-
-export const DROPDOWN_MENU = keyframes`
-    0% {
-        opacity: 0;
-        transform: translateY(-12px);
-    }
-
-    100% {
-        opacity: 1;
-        transform: translateY(0);
-    }
-`;
