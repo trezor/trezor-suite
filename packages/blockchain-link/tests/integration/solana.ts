b1d03f88e8a61764733cceb194064491cc37de3f
PeterBenc <peter.benc1@gmail.com>
2023-10-20 16:56:50 +0200

    chore(blockchain-link): add integration tests for solana getAccountInfo

--- packages/blockchain-link/tests/integration/solana.ts
+++ packages/blockchain-link/tests/integration/solana.ts
@@ -1,5 +1,115 @@
+import { AccountInfoParams } from '@trezor/blockchain-link-types';
 import BlockchainLink from '../../lib';
 import SolanaWorker, { SolanaAPI } from '../../lib/workers/solana';
+
+const id = 79;
+const descriptor = '2MLmmoKgCrxVEzMeGatnjdABYS5RXsQSNikcWrmnvQna';
+const balance = '1000000000';
+
+const fixtures = {
+    accountInfoRequest: [
+        {
+            name: 'basic',
+            input: {
+                id,
+                descriptor,
+                details: 'basic',
+            },
+            result: {
+                descriptor,
+                balance,
+                availableBalance: balance,
+                empty: false,
+                history: {
+                    total: 1,
+                    unconfirmed: 0,
+                    transactions: undefined,
+                    txids: ['deadbeaf'],
+                },
+                page: undefined,
+            },
+        },
+        {
+            name: 'txIds',
+            input: {
+                id,
+                descriptor,
+                details: 'txIds',
+            },
+            result: {
+                descriptor,
+                balance,
+                availableBalance: balance,
+                empty: false,
+                history: {
+                    total: 1,
+                    unconfirmed: 0,
+                    transactions: undefined,
+                    txids: ['deadbeaf'],
+                },
+                page: undefined,
+            },
+        },
+        {
+            name: 'txs',
+            input: {
+                id,
+                descriptor,
+                details: 'txs',
+            },
+            result: {
+                descriptor,
+                balance,
+                availableBalance: balance,
+                empty: false,
+                history: {
+                    total: 1,
+                    unconfirmed: 0,
+                    transactions: [
+                        {
+                            type: 'self',
+                            txid: 'deadbeaf',
+                            blockTime: 1631753600,
+                            amount: '20',
+                            fee: '20',
+                            targets: [
+                                {
+                                    n: 0,
+                                    addresses: [descriptor],
+                                    isAddress: true,
+                                    amount: '20',
+                                    isAccountTarget: true,
+                                },
+                            ],
+                            tokens: [],
+                            internalTransfers: [],
+                            details: {
+                                size: 0,
+                                totalInput: '20',
+                                totalOutput: '0',
+                                vin: [
+                                    {
+                                        txid: 'deadbeaf',
+                                        version: 'legacy',
+                                        isAddress: true,
+                                        isAccountOwned: true,
+                                        n: 0,
+                                        value: '20',
+                                        addresses: [descriptor],
+                                    },
+                                ],
+                                vout: [],
+                            },
+                            blockHeight: 195138557,
+                        },
+                    ],
+                    txids: ['deadbeaf'],
+                },
+                page: { total: 1, index: 0, size: 1 },
+            },
+        },
+    ],
+};
 
 export const solanaApi = {
     getGenesisHash: () => '5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d',
@@ -15,6 +125,40 @@ export const solanaApi = {
     }),
     getBlockHeight: () => 195138557,
     rpcEndpoint: 'dummyUrl',
+    getAccountInfo: () => ({
+        address: descriptor,
+        lamports: {
+            toString: () => balance,
+        },
+    }),
+    getSignaturesForAddress: () => [
+        {
+            signature: 'deadbeaf',
+            slot: 1,
+            err: null,
+            memo: null,
+            confirmationStatus: 'confirmed',
+        },
+    ],
+    getParsedTransactions: () => [
+        {
+            meta: {
+                preBalances: [200],
+                postBalances: [180],
+                fee: 20,
+            },
+            transaction: {
+                signatures: ['deadbeaf'],
+                message: {
+                    accountKeys: [{ pubkey: { toString: () => descriptor } }],
+                    instructions: [],
+                },
+            },
+            version: 'legacy',
+            blockTime: 1631753600,
+            slot: 5,
+        },
+    ],
 } as unknown as SolanaAPI;
 
 describe(`Solana`, () => {
@@ -24,7 +168,7 @@ describe(`Solana`, () => {
 
     worker.tryConnect = () => Promise.resolve(solanaApi);
 
-    beforeEach(() => {
+    beforeAll(() => {
         blockchain = new BlockchainLink({
             name: 'Solana',
             worker: () => worker,
@@ -33,7 +177,7 @@ describe(`Solana`, () => {
         });
     });
 
-    afterEach(() => {
+    afterAll(() => {
         blockchain.dispose();
     });
 
@@ -50,4 +194,11 @@ describe(`Solana`, () => {
             decimals: 9,
         });
     });
+
+    fixtures.accountInfoRequest.forEach(f =>
+        it(`Get account info ${f.name}`, async () => {
+            const result = await blockchain.getAccountInfo(f.input as AccountInfoParams);
+            expect(result).toEqual(f.result);
+        }),
+    );
 });
