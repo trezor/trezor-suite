bfdbe59fbae4d7f86c97a74b290ae0307bc27b57
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2023-10-24 14:14:21 +0200

    fix(connect-popup): handle when sharedworker is not available

--- packages/connect-popup/src/utils/debug.ts
+++ packages/connect-popup/src/utils/debug.ts
@@ -1,10 +1,19 @@
 import { LogMessage, LogWriter, initLog } from '@trezor/connect/src/utils/debug';
 
 export const initSharedLogger = (workerSrc: string) => {
-    const worker = new SharedWorker(workerSrc);
-    worker.port.start();
-    const logWriter: LogWriter = {
-        add: (message: LogMessage) => worker.port.postMessage({ type: 'add-log', data: message }),
-    };
-    return initLog('@trezor/connect-popup', false, logWriter);
+    try {
+        if (!SharedWorker) {
+            throw new Error('SharedWorker is not supported');
+        }
+        const worker = new SharedWorker(workerSrc);
+        worker.port.start();
+        const logWriter: LogWriter = {
+            add: (message: LogMessage) =>
+                worker.port.postMessage({ type: 'add-log', data: message }),
+        };
+        return initLog('@trezor/connect-popup', false, logWriter);
+    } catch (error) {
+        console.warn('Failed to initialize LogWorker:', error);
+        return initLog('@trezor/connect-popup');
+    }
 };
