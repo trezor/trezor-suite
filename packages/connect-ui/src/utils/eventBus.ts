45c03c21d9e71d51415b8b3f5849b3ee62dcd228
Martin Varmuza <mvarmuza@gmail.com>
2023-10-30 16:55:00 +0100

    chore(connect-ui): simplify state hand over from popup

--- packages/connect-ui/src/utils/eventBus.ts
+++ packages/connect-ui/src/utils/eventBus.ts
@@ -1,23 +1,23 @@
-import { PopupHandshake, UI_REQUEST, Device, PopupMethodInfo } from '@trezor/connect';
+import { UI_REQUEST, Device } from '@trezor/connect';
 
 import { TransportEventProps } from '../views/Transport';
 import { PassphraseEventProps } from '../views/Passphrase';
 import { ErrorViewProps } from '../views/Error';
+import { State } from '../types';
 
 export type ConnectUIEventProps =
     // connect-core events
     | TransportEventProps
     | PassphraseEventProps
     | ErrorViewProps
-    | PopupHandshake
-    | PopupMethodInfo
     | { type: typeof UI_REQUEST.DEVICE_NEEDS_BACKUP; device: Device }
     | { type: typeof UI_REQUEST.FIRMWARE_OUTDATED; device: Device }
     // connect-popup events
     | { type: 'phishing-domain' }
     | { type: 'connect-ui-rendered' }
     | { type: 'waiting-for-iframe-handshake' }
-    | { type: 'waiting-for-iframe-init' };
+    | { type: 'waiting-for-iframe-init' }
+    | { type: 'state-update'; payload: State };
 
 const reactChannel = 'react';
 
