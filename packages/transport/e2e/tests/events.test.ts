fe173a13c7d46f377e504ad213048d12c5f0e12d
Szymon Lesisz <sz.lesisz@gmail.com>
2023-11-06 16:14:07 +0100

    tests(transport): fix imports in e2e

--- packages/transport/e2e/tests/events.test.ts
+++ packages/transport/e2e/tests/events.test.ts
@@ -1,8 +1,8 @@
+import * as messages from '@trezor/protobuf/messages.json';
 import { TrezorUserEnvLink } from '@trezor/trezor-user-env-link';
 
 // testing build. yarn workspace @trezor/transport build:lib is a required step therefore
 import { BridgeTransport, Descriptor } from '../../lib';
-import messages from '../../messages.json';
 
 // todo: introduce global jest config for e2e
 jest.setTimeout(60000);
