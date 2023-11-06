6feadb573f1698648c7a34233145e593e8292ad1
Szymon Lesisz <sz.lesisz@gmail.com>
2023-11-06 16:13:32 +0100

    tests(transport): remove unused script/test

--- packages/transport/jest.config.e2e.js
+++
@@ -1,8 +0,0 @@
-const config = require('../../jest.config.base');
-
-module.exports = {
-    ...config,
-    testEnvironment: 'node',
-    testMatch: ['**/e2e/tests/*.test.ts'],
-    testPathIgnorePatterns: [],
-};
