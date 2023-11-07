7e0584c5112f414afef8c7c7616dd95cf007ba0a
tomasklim <tomas.klim@gmail.com>
2023-11-07 11:56:06 +0100

    feat(deps): update deps without breaking changes

--- packages/suite/styled.d.ts
+++ packages/suite/styled.d.ts
@@ -3,6 +3,5 @@ import 'styled-components';
 import { types } from '@trezor/components';
 
 declare module 'styled-components' {
-    // eslint-disable-next-line @typescript-eslint/no-empty-interface
     export interface DefaultTheme extends types.SuiteThemeColors {}
 }
