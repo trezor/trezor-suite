906a369c65fe46fe6dd7b7fc29a7ec9b4b57350b
Jan Komarek <jan.komarek@satoshilabs.com>
2023-11-09 20:00:42 +0100

    chore(suite): use reducer instead of return value in authenticate device check

--- packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/AuthenticateDeviceModal.tsx
+++ packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/AuthenticateDeviceModal.tsx
@@ -1,30 +1,28 @@
 import { useState } from 'react';
-// useDispatch used directly from react-redux instead of src/hooks/suite because we need unwrap() method
-import { useDispatch } from 'react-redux';
 
 import { checkDeviceAuthenticityThunk } from '@suite-common/device-authenticity';
 import { Button } from '@trezor/components';
 
 import { onCancel, openModal } from 'src/actions/suite/modalActions';
 import { DeviceAuthenticationExplainer, Modal, Translation } from 'src/components/suite';
-import { useSelector } from 'src/hooks/suite';
+import { useDispatch, useSelector } from 'src/hooks/suite';
 import { selectIsDebugModeActive } from 'src/reducers/suite/suiteReducer';
+import { selectDeviceAuthenticity } from '@suite-common/wallet-core';
 
 export const AuthenticateDeviceModal = () => {
     const [isLoading, setIsLoading] = useState(false);
     const dispatch = useDispatch();
     const isDebugModeActive = useSelector(selectIsDebugModeActive);
+    const deviceAuthenticity = useSelector(selectDeviceAuthenticity);
 
     const handleClick = async () => {
         setIsLoading(true);
 
-        const result = await dispatch(
-            checkDeviceAuthenticityThunk({ allowDebugKeys: isDebugModeActive }),
-        ).unwrap();
+        await dispatch(checkDeviceAuthenticityThunk({ allowDebugKeys: isDebugModeActive }));
 
         setIsLoading(false);
 
-        if (result.resolved && !result.valid) {
+        if (deviceAuthenticity?.valid === false) {
             dispatch(openModal({ type: 'authenticate-device-fail' }));
         }
     };
