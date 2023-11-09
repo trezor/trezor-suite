4aba7aacbca27c987854504a3e7a8a081c4fb782
Jan Komarek <jan.komarek@satoshilabs.com>
2023-11-09 20:00:12 +0100

    chore(suite-common): improve authenticate device result stored in reducer

--- suite-common/device-authenticity/src/deviceAuthenticityActions.ts
+++ suite-common/device-authenticity/src/deviceAuthenticityActions.ts
@@ -5,9 +5,15 @@ import { AuthenticateDeviceResult } from '@trezor/connect';
 
 export const ACTION_PREFIX = '@device-authenticity';
 
+export type StoredAuthenticateDeviceResult =
+    | (Omit<Partial<AuthenticateDeviceResult>, 'error'> & {
+          error?: string;
+      })
+    | undefined;
+
 const result = createAction(
     `${ACTION_PREFIX}/result`,
-    (payload: { device: TrezorDevice; result: AuthenticateDeviceResult }) => ({
+    (payload: { device: TrezorDevice; result: StoredAuthenticateDeviceResult }) => ({
         payload,
     }),
 );
--- suite-common/device-authenticity/src/deviceAuthenticityThunks.ts
+++ suite-common/device-authenticity/src/deviceAuthenticityThunks.ts
@@ -1,16 +1,13 @@
-import TrezorConnect from '@trezor/connect';
+import TrezorConnect, { AuthenticateDeviceResult } from '@trezor/connect';
 import { createThunk } from '@suite-common/redux-utils';
-import { notificationsActions } from '@suite-common/toast-notifications';
+import { notificationsActions, ToastPayload } from '@suite-common/toast-notifications';
 
 import { ACTION_PREFIX, deviceAuthenticityActions } from './deviceAuthenticityActions';
 
-export const checkDeviceAuthenticityThunk = createThunk<
-    {
-        allowDebugKeys: boolean;
-        skipSuccessToast?: boolean;
-    },
-    { resolved: boolean; error?: string; valid?: boolean }
->(
+export const checkDeviceAuthenticityThunk = createThunk<{
+    allowDebugKeys: boolean;
+    skipSuccessToast?: boolean;
+}>(
     `${ACTION_PREFIX}/checkDeviceAuthenticity`,
     async ({ allowDebugKeys, skipSuccessToast }, { dispatch, getState, extra }) => {
         const {
@@ -28,45 +25,44 @@ export const checkDeviceAuthenticityThunk = createThunk<
             allowDebugKeys,
         });
 
+        let storedResult:
+            | AuthenticateDeviceResult
+            | { error: string; valid?: boolean }
+            | undefined = result.payload;
+        let toastPayload: ToastPayload | undefined;
+
         if (!result.success) {
-            dispatch(
-                notificationsActions.addToast({
-                    type: 'error',
-                    error: `Unable to validate device: ${result.payload.error}`,
-                }),
-            );
-            if (!device.features?.bootloader_locked) {
-                return {
-                    resolved: true,
-                    valid: false,
-                    error: result.payload.error,
-                };
-            }
-            return { resolved: false };
+            toastPayload = {
+                type: 'error',
+                error: `Unable to validate device: ${result.payload.error}`,
+            };
+            storedResult = device.features?.bootloader_locked
+                ? undefined
+                : {
+                      valid: false,
+                      error: result.payload.error,
+                  };
+        } else if (result.payload.error === 'CA_PUBKEY_NOT_FOUND' && result.payload.configExpired) {
+            // CA_PUBKEY_NOT_FOUND with configExpired is temporarily allowed and just logged to Sentry
+            storedResult = {
+                ...result.payload,
+                valid: true,
+            };
+        } else if (!result.payload.valid) {
+            toastPayload = {
+                type: 'device-authenticity-error',
+                error: `Device is not authentic: ${result.payload.error}`,
+            };
         }
 
-        dispatch(deviceAuthenticityActions.result({ device, result: result.payload }));
-
-        // CA_PUBKEY_NOT_FOUND with configExpired is temporarily allowed and just logged to Sentry
-        const caPubKeyNotFoundInExpiredConfig =
-            result.payload.error === 'CA_PUBKEY_NOT_FOUND' && result.payload.configExpired;
-
-        if (!result.payload.valid && !caPubKeyNotFoundInExpiredConfig) {
-            dispatch(
-                notificationsActions.addToast({
-                    type: 'device-authenticity-error',
-                    error: `Device is not authentic: ${result.payload.error}`,
-                }),
-            );
-            console.warn(result.payload.error);
-        } else if (!skipSuccessToast) {
-            dispatch(notificationsActions.addToast({ type: 'device-authenticity-success' }));
+        if (!skipSuccessToast && storedResult?.valid) {
+            toastPayload = { type: 'device-authenticity-success' };
         }
 
-        return {
-            resolved: true,
-            valid: caPubKeyNotFoundInExpiredConfig ? true : result.payload.valid,
-            error: result.payload.error,
-        };
+        if (toastPayload) {
+            dispatch(notificationsActions.addToast(toastPayload));
+        }
+
+        dispatch(deviceAuthenticityActions.result({ device, result: storedResult }));
     },
 );
