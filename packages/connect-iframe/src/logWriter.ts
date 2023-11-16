35dfb859e325c9b598dfdb9134a2c471aafc0e9c
Carlos Garcia Ortiz karliatto <yo@carlosgo.me>
2023-11-16 16:05:13 +0100

    fix(connect-iframe): load shared logger only when required

--- packages/connect-iframe/src/logWriter.ts
+++ packages/connect-iframe/src/logWriter.ts
@@ -1,31 +1,24 @@
-// @ts-expect-error (typescript does not know this is worker constructor, this is done by webpack)
-import LogWorker from './sharedLoggerWorker';
 import { LogMessage, LogWriter } from '@trezor/connect/src/utils/debug';
 
 let logWorker: SharedWorker | undefined;
 
-// Check for SharedWorker support
-// if (SharedWorker) {
-try {
-    // Initialize LogWorker
-    logWorker = new LogWorker();
-    logWorker?.port?.start();
-} catch (error) {
-    console.warn('Failed to initialize LogWorker:', error);
-}
-// } else {
-//     console.warn('SharedWorker is not supported');
-// }
-
-const logWriterFactory = (): LogWriter | undefined => {
-    if (logWorker) {
-        return {
-            add: (message: LogMessage) => {
-                logWorker?.port.postMessage({ type: 'add-log', data: message });
-            },
-        };
+const initLogWriter = () => {
+    // Check for SharedWorker support
+    try {
+        logWorker = new SharedWorker('./workers/shared-logger-worker.js');
+        logWorker?.port?.start();
+    } catch (error) {
+        console.warn('Failed to initialize LogWorker:', error);
     }
-    console.warn('Failed to initialize LogWorker');
+
+    const logWriterFactory = (): LogWriter => ({
+        add: (message: LogMessage) => {
+            if (logWorker) {
+                logWorker.port.postMessage({ type: 'add-log', data: message });
+            }
+        },
+    });
+    return logWriterFactory;
 };
 
-export { logWriterFactory };
+export { initLogWriter };
