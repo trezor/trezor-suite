6482be6a0e5c4f796ebe0dbf889a4230c1fe0964
tomasklim <tomas.klim@gmail.com>
2023-11-17 14:55:23 +0100

    chore(jest): update in suite-desktop-core package

--- packages/suite-desktop-core/jest.config.js
+++ packages/suite-desktop-core/jest.config.js
@@ -1,29 +1,11 @@
+const { ...baseConfig } = require('../../jest.config.base');
+
 module.exports = {
+    ...baseConfig,
     roots: ['<rootDir>/src'],
-    moduleFileExtensions: ['js', 'ts', 'tsx'],
-    coverageDirectory: './coverage',
-    collectCoverage: true,
-    collectCoverageFrom: ['<rootDir>/src/http.ts'],
-    coverageThreshold: {
-        global: {
-            statements: 38,
-            branches: 0,
-            functions: 50,
-            lines: 40,
-        },
-    },
     modulePathIgnorePatterns: ['node_modules', '<rootDir>/lib', '<rootDir>/libDev'],
     watchPathIgnorePatterns: ['<rootDir>/libDev', '<rootDir>/lib'],
     testPathIgnorePatterns: ['<rootDir>/libDev/', '<rootDir>/lib/'],
     transformIgnorePatterns: ['/node_modules/'],
     testMatch: ['**/*.test.(ts|tsx|js)'],
-    transform: {
-        '^.+\\.(ts|tsx)$': 'ts-jest',
-    },
-    verbose: false,
-    globals: {
-        'ts-jest': {
-            tsconfig: '<rootDir>/tsconfig.json',
-        },
-    },
 };
