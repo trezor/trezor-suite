0b3ae46ada77100c31f5e3657ad2e7628e2cd8d7
tomasklim <tomas.klim@gmail.com>
2023-11-17 15:22:37 +0100

    chore(jest): update in suite-desktop-api package

--- packages/suite-desktop-api/jest.config.js
+++ packages/suite-desktop-api/jest.config.js
@@ -1,19 +1,6 @@
+const { ...baseConfig } = require('../../jest.config.base');
+
 module.exports = {
-    rootDir: './',
-    globals: {
-        'ts-jest': {
-            tsconfig: 'tsconfig.json',
-        },
-    },
-    moduleFileExtensions: ['js', 'ts'],
-    testMatch: ['**/*.test.ts'],
-    coverageDirectory: './coverage/',
-    collectCoverage: true,
-    collectCoverageFrom: ['**/src/**/*.ts', '!**/__tests__/**'],
-    modulePathIgnorePatterns: ['node_modules', '<rootDir>/lib', '<rootDir>/libDev'],
-    watchPathIgnorePatterns: ['<rootDir>/libDev', '<rootDir>/lib'],
-    testPathIgnorePatterns: ['<rootDir>/libDev/', '<rootDir>/lib/'],
-    transform: {
-        '^.+\\.ts$': 'ts-jest',
-    },
+    ...baseConfig,
+    testEnvironment: 'jsdom',
 };
