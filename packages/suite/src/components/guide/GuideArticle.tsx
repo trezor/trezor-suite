6694618d23ac6fd02403fa4ca5a79219d0156131
Jan Václavík <jvaclavik@gmail.com>
2023-11-22 10:50:26 +0100

    refactor(suite): Rename guide (#10018)
    * refactor(suite): GuidePage -> GuideArticle
    
    * refactor(suite): GuidePanel -> GuideRouter

--- packages/suite/src/components/guide/GuideArticle.tsx
+++ packages/suite/src/components/guide/GuideArticle.tsx
@@ -1,13 +1,13 @@
 import { useSelector } from 'src/hooks/suite';
 import { GuideHeader, GuideContent, GuideViewWrapper, GuideMarkdown } from 'src/components/guide';
 import { Translation } from 'src/components/suite';
-import { useGuideLoadPage } from 'src/hooks/guide';
+import { useGuideLoadArticle } from 'src/hooks/guide';
 
-export const GuidePage = () => {
+export const GuideArticle = () => {
     const currentNode = useSelector(state => state.guide.currentNode);
     const language = useSelector(state => state.suite.settings.language);
 
-    const { markdown, hasError } = useGuideLoadPage(currentNode, language);
+    const { markdown, hasError } = useGuideLoadArticle(currentNode, language);
 
     return (
         <GuideViewWrapper>
