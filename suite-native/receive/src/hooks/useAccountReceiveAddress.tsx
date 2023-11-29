a640d4067a98aaf6815c2ff1651b15522950bce3
AdamSchinzel <schinzel.adam@gmail.com>
2023-11-29 13:30:36 +0100

    fix(suite-native): show always in chunks on device

--- suite-native/receive/src/hooks/useAccountReceiveAddress.tsx
+++ suite-native/receive/src/hooks/useAccountReceiveAddress.tsx
@@ -49,6 +49,7 @@ export const useAccountReceiveAddress = (accountKey: AccountKey) => {
                     confirmAddressOnDeviceThunk({
                         accountKey,
                         addressPath: freshAddress.path,
+                        chunkify: true,
                     }),
                 ).unwrap(),
             );
