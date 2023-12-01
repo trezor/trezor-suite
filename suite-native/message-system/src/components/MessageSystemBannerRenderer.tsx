3be9322ba4105343d57e19c54540b2a0b99b2840
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2023-12-01 08:06:15 +0100

    fixup! fix(suite-native): empty space above message banner

--- suite-native/message-system/src/components/MessageSystemBannerRenderer.tsx
+++ suite-native/message-system/src/components/MessageSystemBannerRenderer.tsx
@@ -1,12 +1,38 @@
+import { useSelector } from 'react-redux';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { Platform } from 'react-native';
+
 import { VStack } from '@suite-native/atoms';
+import { selectActiveBannerMessages } from '@suite-common/message-system';
+import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
 
 import { MessageBanner } from './MessageBanner';
+
+const messageBannerContainerStyle = prepareNativeStyle<{ topSafeAreaInset: number }>(
+    (_, { topSafeAreaInset }) => ({
+        extend: {
+            condition: Platform.OS === 'ios',
+            style: {
+                marginTop: topSafeAreaInset,
+            },
+        },
+    }),
+);
 
 export const MessageSystemBannerRenderer = () => {
     const activeBannerMessages = useSelector(selectActiveBannerMessages);
 
+    const { applyStyle } = useNativeStyles();
+
+    const { top: topSafeAreaInset } = useSafeAreaInsets();
+
     return (
-        <VStack spacing={4}>
+        <VStack
+            spacing={4}
+            style={applyStyle(messageBannerContainerStyle, {
+                topSafeAreaInset,
+            })}
+        >
             {activeBannerMessages.map(message => (
                 <MessageBanner key={message.id} message={message} />
             ))}
