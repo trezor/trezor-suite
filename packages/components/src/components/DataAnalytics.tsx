8f9f3e26e4324bf49b76322e6e2ddeee2e13d018
dahaca <dahaca17@gmail.com>
2023-12-04 18:41:00 +0100

    chore(suite): add TT Satoshi to Connect UI

--- packages/components/src/components/DataAnalytics.tsx
+++ packages/components/src/components/DataAnalytics.tsx
@@ -7,7 +7,6 @@ import { Button } from './buttons/Button/Button';
 import { CollapsibleBox } from './CollapsibleBox/CollapsibleBox';
 import { Card } from './Card/Card';
 import { Switch } from './form/Switch/Switch';
-import { H2 } from './typography/Heading/Heading';
 
 const StyledCard = styled(Card)`
     max-width: 550px;
@@ -42,7 +41,7 @@ const Label = styled.span`
     display: flex;
 `;
 
-const Heading = styled(H2)`
+const Heading = styled.h2`
     font-size: ${variables.FONT_SIZE.SMALL};
     font-weight: ${variables.FONT_WEIGHT.DEMI_BOLD};
     color: ${({ theme }) => theme.TYPE_DARK_GREY};
