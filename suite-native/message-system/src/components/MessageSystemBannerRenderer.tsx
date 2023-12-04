8ace9c42d9fa58d859c350bc90bc3f1c4ac8bd4c
Martin Vytick Vytrhlík <vytick@gmail.com>
2023-12-04 12:57:32 +0400

    fix(suite-native): add top offset to messages only if they exist

--- suite-native/message-system/src/components/MessageSystemBannerRenderer.tsx
+++ suite-native/message-system/src/components/MessageSystemBannerRenderer.tsx
@@ -1,6 +1,8 @@
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import { useSelector } from 'react-redux';
 
+import { A } from '@mobily/ts-belt';
+
 import { VStack } from '@suite-native/atoms';
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
 import { selectActiveBannerMessages } from '@suite-common/message-system';
@@ -18,12 +20,12 @@ export const MessageSystemBannerRenderer = () => {
     const { top: topSafeAreaInset } = useSafeAreaInsets();
 
     const activeBannerMessages = useSelector(selectActiveBannerMessages);
-
+    const topInset = A.isNotEmpty(activeBannerMessages) ? topSafeAreaInset : 0;
     return (
         <VStack
             spacing={4}
             style={applyStyle(messageBannerContainerStyle, {
-                topSafeAreaInset,
+                topSafeAreaInset: topInset,
             })}
         >
             {activeBannerMessages.map(message => (
