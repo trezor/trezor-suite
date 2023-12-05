cf78a31eb0267f03f683e0062e5b8d0f561e6e20
Petr Knetl <petr.knetl@satoshilabs.com>
2023-12-05 17:54:58 +0530

    feat(suite-native): connect trezor animation (#10192)

--- suite-native/module-connect-device/src/screens/ConnectAndUnlockDeviceScreen.tsx
+++ suite-native/module-connect-device/src/screens/ConnectAndUnlockDeviceScreen.tsx
@@ -1,22 +1,44 @@
-import { Text, Image, VStack } from '@suite-native/atoms';
-import { useTranslate } from '@suite-native/intl';
+import { Dimensions } from 'react-native';
 
-import { ConnectDeviceBackground } from '../components/ConnectDeviceBackground';
+import { Text, VStack } from '@suite-native/atoms';
+import { useTranslate } from '@suite-native/intl';
+import { ConnectDeviceAnimation } from '@suite-native/device';
+import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
+import { Screen } from '@suite-native/navigation';
+
+import { ConnectDeviceScreenHeader } from '../components/ConnectDeviceScreenHeader';
+
+const ANIMATION_HEIGHT = Dimensions.get('screen').height * 0.6;
+
+const screenContentStyle = prepareNativeStyle(() => ({
+    flex: 1,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingTop: 40,
+}));
+
+const animationStyle = prepareNativeStyle(() => ({
+    height: ANIMATION_HEIGHT,
+}));
 
 export const ConnectAndUnlockDeviceScreen = () => {
     const { translate } = useTranslate();
+    const { applyStyle } = useNativeStyles();
 
     return (
-        <ConnectDeviceBackground>
-            <VStack flex={1} justifyContent="flex-end">
+        <Screen
+            screenHeader={<ConnectDeviceScreenHeader />}
+            customHorizontalPadding={0}
+            customVerticalPadding={0}
+            hasBottomInset={false}
+            isScrollable={false}
+        >
+            <VStack style={applyStyle(screenContentStyle)}>
                 <Text variant="titleMedium" textAlign="center">
                     {translate('moduleConnectDevice.connectAndUnlockScreen.title')}
                 </Text>
-                <Image
-                    source={require('../assets/connectedTrezor.png')}
-                    accessibilityLabel="Connected Trezor"
-                />
+                <ConnectDeviceAnimation style={applyStyle(animationStyle)} />
             </VStack>
-        </ConnectDeviceBackground>
+        </Screen>
     );
 };
