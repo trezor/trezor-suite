9730fe85347496fc4addc00ccfce32bd29f758b5
Marek Polak <marek.polak@satoshilabs.com>
2023-12-06 14:41:48 +0100

    test(blockchain-link): remove `TypedRawTransaction`

--- packages/blockchain-link/tests/unit/fixtures/getTransaction.ts
+++ packages/blockchain-link/tests/unit/fixtures/getTransaction.ts
@@ -1,4 +1,4 @@
-const blockbookTx = {
+const blockbookFixture = {
     txid: '28451eff296d05f25cdb08c8bb7956d4f18d0c1e09fe4a1d337f347afdf9a7a1',
     version: 2,
     vin: [
@@ -23,10 +23,36 @@ const blockbookTx = {
     value: '39165971',
     valueIn: '0',
     fees: '0',
+    size: 122,
+    vsize: 122,
     hex: '02000000010000000000000000000000000000000000000000000000000000000000000000ffffffff2503a4f617041fba365d2f4d696e65642042792048454c4c4f2fffffffffafcb4c0000000000ffffffff0113a05502000000001976a91431938090e8a00edb2a42a8fbcf1a56d99de43bc988ac00000000',
 };
 
-const blockfrostTx = {
+const blockbookTx = {
+    type: 'unknown',
+    txid: blockbookFixture.txid,
+    details: {
+        vin: blockbookFixture.vin,
+        vout: blockbookFixture.vout,
+        size: blockbookFixture.size,
+        totalInput: blockbookFixture.valueIn,
+        totalOutput: blockbookFixture.value,
+    },
+    amount: blockbookFixture.value,
+    blockHash: blockbookFixture.blockHash,
+    blockHeight: blockbookFixture.blockHeight,
+    blockTime: blockbookFixture.blockTime,
+    fee: blockbookFixture.fees,
+    feeRate: '0',
+    vsize: blockbookFixture.vsize,
+    hex: blockbookFixture.hex,
+    internalTransfers: [],
+    targets: [],
+    tokens: [],
+};
+
+const blockfrostFixture = {
+    hash: 'deadbeef',
     block: 'e6369fee087d31192016b1659f1c381e9fc4925339278a4eef6f340c96c1947f',
     block_height: 5040611,
     slot: 15650536,
@@ -50,24 +76,53 @@ const blockfrostTx = {
     pool_retire_count: 0,
 };
 
+const blockfrostTx = {
+    type: 'unknown',
+    txid: 'deadbeef',
+    amount: blockfrostFixture.output_amount[0].quantity,
+    blockHash: blockfrostFixture.block,
+    blockHeight: blockfrostFixture.block_height,
+    fee: blockfrostFixture.fees,
+    details: { vin: [], vout: [], size: blockfrostFixture.size, totalInput: '0', totalOutput: '0' },
+    targets: [],
+    tokens: [],
+    internalTransfers: [],
+    cardanoSpecific: {},
+};
+
+const xrpFixture = {
+    Account: 'rB8Ai21NLgz85T9js2fKAVTVEDZnbTn8Eu',
+    Amount: '10000000',
+    Destination: 'rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh',
+    DestinationTag: 1000000999,
+    Fee: '12',
+    Flags: 2147483648,
+    LastLedgerSequence: 21230993,
+    Sequence: 176,
+    SigningPubKey: '02EEFCFCDC10B7ADAD61C9DF80AEF746A7D2439B52AA5155E6D21B4008CDB6AC43',
+    TransactionType: 'Payment',
+    TxnSignature:
+        '3045022100EE2DC777F71513EEAEC9E6F61F8A83FB0ED4C1278D634B71937142F6934C7BCE02200C33939DA39B5654D67B659265ED3616C9C7B66BE864FDED523FA47B336DF539',
+    date: 617181871,
+    hash: '34E5B5A710421589B868FDFB5BF9AAD2C08352ED0886E17800867A89FBD317D3',
+    inLedger: 21230990,
+    ledger_index: 21230990,
+    validated: true,
+};
+
 const xrpTx = {
-    type: 'payment',
-    address: 'rB8Ai21NLgz85T9js2fKAVTVEDZnbTn8Eu',
-    sequence: 176,
-    id: '34E5B5A710421589B868FDFB5BF9AAD2C08352ED0886E17800867A89FBD317D3',
-    specification: {
-        source: {
-            address: 'rB8Ai21NLgz85T9js2fKAVTVEDZnbTn8Eu',
-            maxAmount: {
-                currency: 'XRP',
-                value: '10',
-            },
-        },
-        destination: {
-            address: 'rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh',
-            tag: 1000000999,
-        },
-    },
+    txid: xrpFixture.hash,
+    type: 'unknown',
+    blockHeight: xrpFixture.ledger_index,
+    blockHash: xrpFixture.hash,
+    blockTime: xrpFixture.date + 946684800,
+    amount: xrpFixture.Amount,
+    fee: xrpFixture.Fee,
+
+    targets: [],
+    tokens: [],
+    internalTransfers: [],
+    details: { size: 0, totalInput: '0', totalOutput: '0', vin: [], vout: [] },
 };
 
 export default {
@@ -79,14 +134,11 @@ export default {
                 {
                     method: 'getTransaction',
                     response: {
-                        data: blockbookTx,
+                        data: blockbookFixture,
                     },
                 },
             ],
-            response: {
-                type: 'blockbook',
-                tx: blockbookTx,
-            },
+            response: blockbookTx,
         },
         {
             description: 'Not found',
@@ -104,48 +156,24 @@ export default {
                     response: {
                         type: 'response',
                         status: 'success',
-                        result: {
-                            Account: 'rB8Ai21NLgz85T9js2fKAVTVEDZnbTn8Eu',
-                            Amount: '10000000',
-                            Destination: 'rHb9CJAWyB4rj91VRWn96DkukG4bwdtyTh',
-                            DestinationTag: 1000000999,
-                            Fee: '12',
-                            Flags: 2147483648,
-                            LastLedgerSequence: 21230993,
-                            Sequence: 176,
-                            SigningPubKey:
-                                '02EEFCFCDC10B7ADAD61C9DF80AEF746A7D2439B52AA5155E6D21B4008CDB6AC43',
-                            TransactionType: 'Payment',
-                            TxnSignature:
-                                '3045022100EE2DC777F71513EEAEC9E6F61F8A83FB0ED4C1278D634B71937142F6934C7BCE02200C33939DA39B5654D67B659265ED3616C9C7B66BE864FDED523FA47B336DF539',
-                            date: 617181871,
-                            hash: '34E5B5A710421589B868FDFB5BF9AAD2C08352ED0886E17800867A89FBD317D3',
-                            inLedger: 21230990,
-                            ledger_index: 21230990,
-                            validated: true,
-                        },
+                        result: xrpFixture,
                     },
                 },
             ],
-            response: {
-                type: 'ripple',
-                tx: xrpTx,
-            },
+            response: xrpTx,
         },
         {
             description: 'Not found',
             params: '6390FE5EE3B22239B2CA403C32DAFCA613B7FEEE55473A50CB8602CE0FE3EB3F',
-            error: '[NotFoundError(Transaction not found)]',
+            error: 'Transaction not found',
         },
     ],
     blockfrost: [
         {
             description: 'Successful',
             params: '28172ea876c3d1e691284e5179fae2feb3e69d7d41e43f8023dc380115741026',
-            response: {
-                type: 'blockfrost',
-                tx: blockfrostTx,
-            },
+            serverFixtures: [{ method: 'GET_TRANSACTION', response: { data: blockfrostFixture } }],
+            response: blockfrostTx,
         },
         {
             description: 'Not found',
