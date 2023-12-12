c698b51b35552a590abfbcbf50c0b19e0cd58d39
vytick <vytick@gmail.com>
2023-12-12 18:19:40 +0100

    chore(suite-native): supported networks refactor (#10241)

--- suite-native/module-accounts-import/src/components/SelectableNetworkList.tsx
+++ suite-native/module-accounts-import/src/components/SelectableNetworkList.tsx
@@ -1,13 +1,6 @@
-import { A } from '@mobily/ts-belt';
-
 import { HeaderedCard, VStack } from '@suite-native/atoms';
 import { Network, NetworkSymbol } from '@suite-common/wallet-config';
-import {
-    mainnetsOrder,
-    testnetsOrder,
-    importEnabledMainnets,
-    importEnabledTestnets,
-} from '@suite-native/config';
+import { portfolioTrackerMainnets, portfolioTrackerTestnets } from '@suite-native/config';
 
 import { SelectableNetworkItem } from './SelectableNetworkItem';
 
@@ -15,23 +8,13 @@ type SelectableAssetListProps = {
     onSelectItem: (networkSymbol: NetworkSymbol) => void;
 };
 
-const sortNetworkItems = (networkItems: Network[], networkOrder: NetworkSymbol[]): Network[] =>
-    A.sort(networkItems, (a, b) => {
-        const aOrder = networkOrder.indexOf(a.symbol) ?? Number.MAX_SAFE_INTEGER;
-        const bOrder = networkOrder.indexOf(b.symbol) ?? Number.MAX_SAFE_INTEGER;
-        return aOrder - bOrder;
-    }) as Network[];
-
-const sortedMainnetsNetworks = sortNetworkItems(importEnabledMainnets, mainnetsOrder);
-const sortedTestnetNetworks = sortNetworkItems(importEnabledTestnets, testnetsOrder);
-
 const NetworkItemSection = ({
     title,
     networks,
     onSelectItem,
 }: {
     title: string;
-    networks: Network[];
+    networks: readonly Network[];
     onSelectItem: SelectableAssetListProps['onSelectItem'];
 }) => (
     <HeaderedCard title={title}>
@@ -52,13 +35,13 @@ export const SelectableNetworkList = ({ onSelectItem }: SelectableAssetListProps
     <VStack spacing="large">
         <NetworkItemSection
             title="Select a coin to sync"
-            networks={sortedMainnetsNetworks}
+            networks={portfolioTrackerMainnets}
             onSelectItem={onSelectItem}
         />
 
         <NetworkItemSection
             title="Testnet coins (have no value – for testing purposes only)"
-            networks={sortedTestnetNetworks}
+            networks={portfolioTrackerTestnets}
             onSelectItem={onSelectItem}
         />
     </VStack>
