876a8a83b89b230d0aca7640a65415da60dc3478
Bohdan Juříček <36101761+juriczech@users.noreply.github.com>
2023-12-12 08:46:18 +0100

    chore(suite-native): discovery duration warn

--- suite-native/discovery/src/useIsDiscoveryDurationTooLong.tsx
+++ suite-native/discovery/src/useIsDiscoveryDurationTooLong.tsx
@@ -1,26 +1,40 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-import { selectIsDeviceDiscoveryActive } from '@suite-common/wallet-core';
+import { selectIsDeviceDiscoveryActive, selectIsPortfolioEmpty } from '@suite-common/wallet-core';
 
 import { selectDiscoveryStartTimeStamp } from './discoveryConfigSlice';
 
-const DISCOVERY_LENGTH_CHECK_INTERVAL = 1000;
-const DISCOVERY_DURATION_TRESHOLD = 20000;
+const DISCOVERY_LENGTH_CHECK_INTERVAL = 1_000;
+const DISCOVERY_DURATION_TRESHOLD = 45_000;
+const FIRST_ASSET_DISCOVERY_DURATION_TRESHOLD = 10_000;
 
 export const useIsDiscoveryDurationTooLong = () => {
     const startDiscoveryTimestamp = useSelector(selectDiscoveryStartTimeStamp);
     const isDiscoveryActive = useSelector(selectIsDeviceDiscoveryActive);
 
+    const isPortfolioEmpty = useSelector(selectIsPortfolioEmpty);
+
     const [loadingTakesLongerThanExpected, setLoadingTakesLongerThanExpected] = useState(false);
+
+    const handleSetLoadingTakesLongerThanExpected = useCallback(
+        (interval: any) => {
+            setLoadingTakesLongerThanExpected(true);
+            clearInterval(interval);
+        },
+        [setLoadingTakesLongerThanExpected],
+    );
 
     useEffect(() => {
         let interval: any;
         if (isDiscoveryActive && startDiscoveryTimestamp) {
             interval = setInterval(() => {
-                if (performance.now() - startDiscoveryTimestamp > DISCOVERY_DURATION_TRESHOLD) {
-                    setLoadingTakesLongerThanExpected(true);
-                    clearInterval(interval);
+                const currentDuration = performance.now() - startDiscoveryTimestamp;
+                if (currentDuration > DISCOVERY_DURATION_TRESHOLD) {
+                    handleSetLoadingTakesLongerThanExpected(interval);
+                }
+                if (isPortfolioEmpty && currentDuration > FIRST_ASSET_DISCOVERY_DURATION_TRESHOLD) {
+                    handleSetLoadingTakesLongerThanExpected(interval);
                 }
             }, DISCOVERY_LENGTH_CHECK_INTERVAL);
         } else {
@@ -32,7 +46,12 @@ export const useIsDiscoveryDurationTooLong = () => {
                 clearInterval(interval);
             }
         };
-    }, [isDiscoveryActive, startDiscoveryTimestamp]);
+    }, [
+        handleSetLoadingTakesLongerThanExpected,
+        isDiscoveryActive,
+        isPortfolioEmpty,
+        startDiscoveryTimestamp,
+    ]);
 
     return loadingTakesLongerThanExpected;
 };
