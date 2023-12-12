7889b550662690fafd804e648429a62957ebc8cb
Martin Varmuza <mvarmuza@gmail.com>
2023-12-12 08:12:41 +0100

    chore(protocol): remove bytebuffer dependency

--- packages/protocol/src/protocol-trzd/decode.ts
+++ packages/protocol/src/protocol-trzd/decode.ts
@@ -1,19 +1,16 @@
-import ByteBuffer from 'bytebuffer';
-
 // Decode `trzd` protocol used for decoding of dynamically loaded `@trezor/protobuf` messages
 // https://github.com/trezor/trezor-firmware/blob/087becd2caa5618eecab37ac3f2ca51172e52eb9/docs/common/ethereum-definitions.md#definition-format
 
 export const decode = (bytes: ArrayBuffer) => {
-    const byteBuffer = ByteBuffer.wrap(bytes);
-    byteBuffer.LE(true); // use little endian byte order
+    const byteBuffer = Buffer.from(bytes);
     // 5 bytes magic `trzd`
-    const magic = byteBuffer.readBytes(5).toUTF8();
+    const magic = byteBuffer.slice(0, 5).toString('utf8');
     // 1 byte
-    const definitionType = byteBuffer.readUint8();
+    const definitionType = byteBuffer.readUInt8(5);
     // 4 bytes
-    const dataVersion = byteBuffer.readUint32();
+    const dataVersion = byteBuffer.readUInt32LE(6);
     // 2 bytes
-    const protobufLength = byteBuffer.readUint16();
+    const protobufLength = byteBuffer.readUInt16LE(10);
     // N bytes
     const protobufPayload = byteBuffer.slice(12, 12 + protobufLength);
 
