6de5cf286f1af7d24958d6b1e74098e32902ab18
Jan Václavík <jvaclavik@gmail.com>
2023-12-12 15:50:24 +0100

    style(suite): Settings redesign (#10050)

--- packages/suite/src/components/suite/ChangeDeviceLabel.tsx
+++ packages/suite/src/components/suite/ChangeDeviceLabel.tsx
@@ -1,50 +1,20 @@
 import { useState, ChangeEventHandler } from 'react';
 import { analytics, EventType } from '@trezor/suite-analytics';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
-import { Button, INPUT_HEIGHTS, Input } from '@trezor/components';
+import { Button, Input } from '@trezor/components';
 import { Translation } from 'src/components/suite';
-import { useDevice, useDispatch, useTranslation, useLayoutSize } from 'src/hooks/suite';
+import { useDevice, useDispatch, useTranslation } from 'src/hooks/suite';
 import { applySettings } from 'src/actions/settings/deviceSettingsActions';
 import { MAX_LABEL_LENGTH } from 'src/constants/suite/device';
 import { isAscii } from '@trezor/utils';
-import { SCREEN_SIZE } from '@trezor/components/src/config/variables';
+import { spacingsPx } from '@trezor/theme';
 
-const StyledInput = styled(Input)<{ isVertical?: boolean; $width?: number }>`
-    width: ${({ $width }) => $width && `${$width}px`};
-
-    ${props =>
-        props.isVertical &&
-        css`
-            input {
-                :not(:first-child) {
-                    margin-left: 8px;
-                }
-            }
-        `};
-`;
-
-const StyledButton = styled(Button)<{ isVertical?: boolean; isDisabled: boolean }>`
-    height: ${INPUT_HEIGHTS.large}px;
-
-    ${props =>
-        props.isVertical &&
-        css`
-            min-width: 170px;
-            margin: 4px 0;
-
-            &:not(:first-child) {
-                margin-left: 8px;
-
-                @media (max-width: ${SCREEN_SIZE.SM}) {
-                    margin-left: 0;
-                }
-            }
-
-            @media (max-width: ${SCREEN_SIZE.SM}) {
-                width: 100%;
-            }
-        `}
+const Container = styled.div<{ isVertical?: boolean }>`
+    display: flex;
+    flex-direction: ${({ isVertical }) => (isVertical ? 'column' : 'row')};
+    align-items: center;
+    gap: ${spacingsPx.sm};
 `;
 
 interface ChangeDeviceLabelProps {
@@ -63,7 +33,6 @@ export const ChangeDeviceLabel = ({
     const { translationString } = useTranslation();
     const { device } = useDevice();
     const dispatch = useDispatch();
-    const { isMobileLayout } = useLayoutSize();
 
     const [label, setLabel] = useState(device?.label === placeholder ? '' : device?.label);
     const [error, setError] = useState<string | null>(null);
@@ -96,25 +65,26 @@ export const ChangeDeviceLabel = ({
         isDeviceLocked || (!placeholder && label === device?.label) || !!error || !label;
 
     return (
-        <>
-            <StyledInput
-                $width={!isMobileLayout ? 170 : undefined}
-                isVertical={isVertical}
+        <Container isVertical={isVertical}>
+            <Input
                 bottomText={error || null}
                 value={label}
                 placeholder={placeholder}
                 inputState={error ? 'error' : undefined}
                 onChange={handleChange}
                 data-test="@settings/device/label-input"
+                hasBottomPadding={false}
+                size={isVertical ? 'small' : 'large'}
             />
-            <StyledButton
-                isVertical={isVertical}
+            <Button
                 onClick={handleClick}
                 isDisabled={isDisabled}
                 data-test="@settings/device/label-submit"
+                size={isVertical ? 'small' : 'large'}
+                isFullWidth
             >
                 <Translation id="TR_DEVICE_SETTINGS_DEVICE_EDIT_LABEL" />
-            </StyledButton>
-        </>
+            </Button>
+        </Container>
     );
 };
