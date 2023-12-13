40ac7f3d67d36b6217220c9b205bac73892faa15
Jan Komarek <jan.komarek@satoshilabs.com>
2023-12-13 18:51:50 +0100

    chore(urls): add Url type

--- packages/urls/src/index.ts
+++ packages/urls/src/index.ts
@@ -1,3 +1,8 @@
+import type * as Urls from './urls';
+import type * as GithubUrls from './github';
+
 export * from './urls';
 export * from './github';
 export * from './tor';
+
+export type Url = (typeof Urls)[keyof typeof Urls] | (typeof GithubUrls)[keyof typeof GithubUrls];
