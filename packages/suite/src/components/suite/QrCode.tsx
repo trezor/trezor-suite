d1924f8456ce3ce53f377aab7264f3fe26c8f255
Peter Sanderson <dierk.deidel@protonmail.com>
2023-12-13 11:38:41 +0100

    fix(suite): fit spanish into QR code description

--- packages/suite/src/components/suite/QrCode.tsx
+++ packages/suite/src/components/suite/QrCode.tsx
@@ -1,7 +1,7 @@
 import { QRCodeSVG } from 'qrcode.react';
 import styled from 'styled-components';
 
-import { Icon, colors, variables } from '@trezor/components';
+import { Icon, Tooltip, colors, variables } from '@trezor/components';
 import { Translation } from './Translation';
 
 export const QRCODE_SIZE = 384;
@@ -16,24 +16,31 @@ const Wrapper = styled.div`
     background: ${colors.BG_WHITE};
     max-width: ${QRCODE_SIZE}px;
     position: relative;
-`;
 
-const MessageWrapper = styled.div`
     display: flex;
-    gap: 6px;
-    position: absolute;
-    left: 50%;
-    bottom: 9px;
-    transform: translate(-50%, 0);
-    width: 100%;
-    align-items: center;
-    justify-content: center;
+    flex-direction: column;
 `;
 
 const Message = styled.div`
     font-size: ${variables.FONT_SIZE.SMALL};
     font-weight: ${variables.FONT_WEIGHT.DEMI_BOLD};
     color: ${({ theme }) => theme.TYPE_LIGHT_GREY};
+    margin-top: 12px;
+    white-space: nowrap;
+`;
+
+const StyledIcon = styled(Icon)`
+    display: inline-block;
+    margin-left: 5px;
+    vertical-align: middle;
+`;
+
+const SpanToResetWhitespace = styled.span`
+    white-space: normal;
+`;
+
+const StyledTooltip = styled(Tooltip)`
+    display: inline-block;
 `;
 
 interface QrCodeProps {
@@ -55,12 +62,16 @@ export const QrCode = ({ value, className, bgColor, fgColor, showMessage }: QrCo
             style={{ width: '100%', height: '100%', objectFit: 'contain' }}
         />
         {showMessage && (
-            <MessageWrapper>
-                <Message>
+            <Message>
+                <SpanToResetWhitespace>
                     <Translation id="TR_QR_RECEIVE_ADDRESS_CONFIRM" />
-                </Message>
-                <Icon icon="INFO" size={12} color={fgColor || colors.TYPE_DARK_GREY} />
-            </MessageWrapper>
+                </SpanToResetWhitespace>
+                <StyledTooltip
+                    content={<Translation id="TR_QR_RECEIVE_ADDRESS_CONFIRM_EXPLANATION" />}
+                >
+                    <StyledIcon icon="INFO" size={12} color={fgColor || colors.TYPE_DARK_GREY} />
+                </StyledTooltip>
+            </Message>
         )}
     </Wrapper>
 );
