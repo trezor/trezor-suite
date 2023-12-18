d1813bd802695bca402b02afe00e7a55d8b6dcca
Martin Vytick Vytrhlík <vytick@gmail.com>
2023-12-19 00:30:08 +0400

    chore(env): different fetch interval for messages in suite and mobile

--- suite-common/message-system/src/messageSystemConstants.ts
+++ suite-common/message-system/src/messageSystemConstants.ts
@@ -9,12 +9,18 @@ export const JWS_CONFIG_FILENAME_REMOTE = `config.v${VERSION}.jws`;
 export const JWS_CONFIG_FILENAME_LOCAL = `config.v${VERSION}.ts`;
 
 /*
- * On the app launch and then about once a minute (FETCH_INTERVAL), a new config tries to fetch.
- * FETCH_CHECK_INTERVAL is interval for checking if FETCH_INTERVAL already expired.
+ * On the app launch and then about once in FETCH_INTERVAL_IN_MS, a new config tries to fetch.
+ * FETCH_CHECK_INTERVAL_IN_MS is interval for checking if FETCH_INTERVAL_IN_MS already expired.
+ * "_MOBILE" variant is used for mobile apps.
  */
-export const FETCH_INTERVAL = 60_000; // 1 minute in milliseconds
-export const FETCH_CHECK_INTERVAL = 30_000;
-export const FETCH_TIMEOUT = 30_000;
+export const FETCH_INTERVAL_IN_MS = 60_000; // 1 minute in milliseconds
+export const FETCH_INTERVAL_IN_MS_MOBILE = 600_000; // 10 min
+
+export const FETCH_CHECK_INTERVAL_IN_MS = 30_000; // 30 sec
+export const FETCH_CHECK_INTERVAL_IN_MS_MOBILE = 180_000; // 3 min
+
+// timeout for fetching remote config
+export const FETCH_TIMEOUT_IN_MS = 30_000; // 30 sec
 
 export const CONFIG_URL_REMOTE_BASE = 'https://data.trezor.io/config';
 export const CONFIG_URL_REMOTE = {
