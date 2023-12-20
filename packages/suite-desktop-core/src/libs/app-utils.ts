e02dbb22b932135db30dda6a569656d32c898770
tomasklim <tomas.klim@gmail.com>
2023-12-20 13:57:36 +0100

    fix(suite-desktop): restart app on Linux

--- packages/suite-desktop-core/src/libs/app-utils.ts
+++ packages/suite-desktop-core/src/libs/app-utils.ts
@@ -5,6 +5,13 @@ export const restartApp = () => {
 
     logger.info('app', `Relaunching app with ${process.argv.slice(1).join(', ')} arguments.`);
 
+    const options: Electron.RelaunchOptions = { args: process.argv };
+    if (process.env.APPIMAGE) {
+        options.execPath = process.env.APPIMAGE;
+        options.args = options.args ?? [];
+        options.args.unshift('--appimage-extract-and-run');
+    }
+
     app.relaunch();
     app.quit();
 };
