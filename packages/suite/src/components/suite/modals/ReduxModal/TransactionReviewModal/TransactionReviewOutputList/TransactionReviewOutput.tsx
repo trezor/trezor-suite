24a486b7e845382747a0dac6ff13d6dedaffe7dc
Peter Sanderson <dierk.deidel@protonmail.com>
2024-01-02 13:15:42 +0100

    fix(suite): add scrolling to every tx-outpup that needs to be confirmed on the device

--- packages/suite/src/components/suite/modals/ReduxModal/TransactionReviewModal/TransactionReviewOutputList/TransactionReviewOutput.tsx
+++ packages/suite/src/components/suite/modals/ReduxModal/TransactionReviewModal/TransactionReviewOutputList/TransactionReviewOutput.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, forwardRef } from 'react';
 import BigNumber from 'bignumber.js';
 import { Translation } from 'src/components/suite';
 import { formatNetworkAmount, formatAmount, isTestnet } from '@suite-common/wallet-utils';
@@ -32,172 +32,175 @@ export type TransactionReviewOutputProps = {
     account: Account;
 } & ReviewOutput;
 
-export const TransactionReviewOutput = (props: TransactionReviewOutputProps) => {
-    const { type, state, label, value, symbol, token, account } = props;
-    let outputLabel: ReactNode = label;
-    const { networkType } = account;
+export const TransactionReviewOutput = forwardRef<HTMLDivElement, TransactionReviewOutputProps>(
+    (props, ref) => {
+        const { type, state, label, value, symbol, token, account } = props;
+        let outputLabel: ReactNode = label;
+        const { networkType } = account;
 
-    if (type === 'locktime') {
-        const isTimestamp = new BigNumber(value).gte(BTC_LOCKTIME_VALUE);
-        outputLabel = (
-            <Translation id={isTimestamp ? 'LOCKTIME_TIMESTAMP' : 'LOCKTIME_BLOCKHEIGHT'} />
-        );
-    }
-    if (type === 'fee') {
-        outputLabel = <Translation id={getFeeLabel(networkType)} />;
-    }
-    if (type === 'contract') {
-        if (networkType === 'solana') {
-            outputLabel = <Translation id="TR_TOKEN" />;
-        } else {
-            outputLabel = <Translation id="TR_CONTRACT" />;
+        if (type === 'locktime') {
+            const isTimestamp = new BigNumber(value).gte(BTC_LOCKTIME_VALUE);
+            outputLabel = (
+                <Translation id={isTimestamp ? 'LOCKTIME_TIMESTAMP' : 'LOCKTIME_BLOCKHEIGHT'} />
+            );
         }
-    }
-    if (type === 'address' || type === 'regular_legacy') {
-        outputLabel = <Translation id="TR_RECIPIENT_ADDRESS" />;
-    }
-    if (type === 'amount') {
-        outputLabel = <Translation id="TR_AMOUNT_SENT" />;
-    }
-    if (type === 'destination-tag') {
-        outputLabel = <Translation id="DESTINATION_TAG" />;
-    }
-    if (type === 'gas') {
-        outputLabel = <Translation id="TR_GAS_PRICE" />;
-    }
+        if (type === 'fee') {
+            outputLabel = <Translation id={getFeeLabel(networkType)} />;
+        }
+        if (type === 'contract') {
+            if (networkType === 'solana') {
+                outputLabel = <Translation id="TR_TOKEN" />;
+            } else {
+                outputLabel = <Translation id="TR_CONTRACT" />;
+            }
+        }
+        if (type === 'address' || type === 'regular_legacy') {
+            outputLabel = <Translation id="TR_RECIPIENT_ADDRESS" />;
+        }
+        if (type === 'amount') {
+            outputLabel = <Translation id="TR_AMOUNT_SENT" />;
+        }
+        if (type === 'destination-tag') {
+            outputLabel = <Translation id="DESTINATION_TAG" />;
+        }
+        if (type === 'gas') {
+            outputLabel = <Translation id="TR_GAS_PRICE" />;
+        }
 
-    let outputValue = value;
-    let outputSymbol;
-    let fiatVisible = false;
-    if (token) {
-        outputValue = formatAmount(value, token.decimals);
-        outputSymbol = token.symbol as NetworkSymbol;
-    } else if (type === 'fee' || type === 'amount') {
-        outputValue = formatNetworkAmount(value, symbol);
-        outputSymbol = symbol;
-        fiatVisible = !isTestnet(symbol);
-    } else if (type === 'gas') {
-        outputSymbol = symbol;
-        fiatVisible = !isTestnet(symbol);
-    }
+        let outputValue = value;
+        let outputSymbol;
+        let fiatVisible = false;
+        if (token) {
+            outputValue = formatAmount(value, token.decimals);
+            outputSymbol = token.symbol as NetworkSymbol;
+        } else if (type === 'fee' || type === 'amount') {
+            outputValue = formatNetworkAmount(value, symbol);
+            outputSymbol = symbol;
+            fiatVisible = !isTestnet(symbol);
+        } else if (type === 'gas') {
+            outputSymbol = symbol;
+            fiatVisible = !isTestnet(symbol);
+        }
 
-    let outputLines: OutputElementLine[];
+        let outputLines: OutputElementLine[];
 
-    if (type === 'fee-replace') {
-        outputLines = [
-            {
-                id: 'increase-fee-by',
-                label: <Translation id="TR_INCREASE_FEE_BY" />,
-                value: formatNetworkAmount(value, symbol),
-            },
-            {
-                id: 'increased-fee',
-                label: <Translation id="TR_INCREASED_FEE" />,
-                value: formatNetworkAmount(props.value2, symbol),
-            },
-        ];
-        outputSymbol = symbol;
-        fiatVisible = !isTestnet(symbol);
-    } else if (type === 'reduce-output') {
-        outputLines = [
-            {
-                id: 'decrease-address',
-                label: <Translation id="TR_RECIPIENT_ADDRESS" />,
-                value: props.label,
-                plainValue: true,
-            },
-            {
-                id: 'decrease-by',
-                label: <Translation id="TR_DECREASE_AMOUNT_BY" />,
-                value: formatNetworkAmount(value, symbol),
-            },
-            {
-                id: 'decreased-amount',
-                label: <Translation id="TR_DECREASED_AMOUNT" />,
-                value: formatNetworkAmount(props.value2, symbol),
-            },
-        ];
-        outputSymbol = symbol;
-        fiatVisible = !isTestnet(symbol);
-    } else if (type === 'txid') {
-        outputLines = [
-            {
-                id: 'txid',
-                label: <Translation id="TR_TXID" />,
-                value: outputValue,
-                plainValue: true,
-            },
-        ];
-    } else if (type === 'data') {
-        outputLines = [
-            {
-                id: 'data',
-                label: <Translation id="DATA_ETH" />,
-                value: outputValue,
-                plainValue: true,
-            },
-        ];
-    } else if (type === 'regular_legacy') {
-        outputLines = [
-            {
-                id: 'regular_legacy',
-                label: outputLabel,
-                value: outputValue,
-                confirmLabel: <Translation id="TR_RECIPIENT_ADDRESS_MATCH" />,
-                plainValue: true,
-            },
-        ];
-    } else if (type === 'contract') {
-        outputLines = [
-            {
-                id: 'contract',
-                label: outputLabel,
-                value: outputValue,
-                plainValue: true,
-            },
-        ];
-    } else if (type === 'address') {
-        outputLines = [
-            {
-                id: 'address',
-                label: outputLabel,
-                value: outputValue,
-                confirmLabel: <Translation id="TR_RECIPIENT_ADDRESS_MATCH" />,
-                plainValue: true,
-            },
-        ];
-    } else if (type === 'opreturn') {
-        outputLines = [
-            {
-                id: 'opreturn',
-                label: <Translation id="OP_RETURN" />,
-                value: outputValue,
-                plainValue: true,
-            },
-        ];
-    } else {
-        outputLines = [
-            {
-                id: 'default',
-                label: outputLabel,
-                value: outputValue,
-            },
-        ];
-    }
+        if (type === 'fee-replace') {
+            outputLines = [
+                {
+                    id: 'increase-fee-by',
+                    label: <Translation id="TR_INCREASE_FEE_BY" />,
+                    value: formatNetworkAmount(value, symbol),
+                },
+                {
+                    id: 'increased-fee',
+                    label: <Translation id="TR_INCREASED_FEE" />,
+                    value: formatNetworkAmount(props.value2, symbol),
+                },
+            ];
+            outputSymbol = symbol;
+            fiatVisible = !isTestnet(symbol);
+        } else if (type === 'reduce-output') {
+            outputLines = [
+                {
+                    id: 'decrease-address',
+                    label: <Translation id="TR_RECIPIENT_ADDRESS" />,
+                    value: props.label,
+                    plainValue: true,
+                },
+                {
+                    id: 'decrease-by',
+                    label: <Translation id="TR_DECREASE_AMOUNT_BY" />,
+                    value: formatNetworkAmount(value, symbol),
+                },
+                {
+                    id: 'decreased-amount',
+                    label: <Translation id="TR_DECREASED_AMOUNT" />,
+                    value: formatNetworkAmount(props.value2, symbol),
+                },
+            ];
+            outputSymbol = symbol;
+            fiatVisible = !isTestnet(symbol);
+        } else if (type === 'txid') {
+            outputLines = [
+                {
+                    id: 'txid',
+                    label: <Translation id="TR_TXID" />,
+                    value: outputValue,
+                    plainValue: true,
+                },
+            ];
+        } else if (type === 'data') {
+            outputLines = [
+                {
+                    id: 'data',
+                    label: <Translation id="DATA_ETH" />,
+                    value: outputValue,
+                    plainValue: true,
+                },
+            ];
+        } else if (type === 'regular_legacy') {
+            outputLines = [
+                {
+                    id: 'regular_legacy',
+                    label: outputLabel,
+                    value: outputValue,
+                    confirmLabel: <Translation id="TR_RECIPIENT_ADDRESS_MATCH" />,
+                    plainValue: true,
+                },
+            ];
+        } else if (type === 'contract') {
+            outputLines = [
+                {
+                    id: 'contract',
+                    label: outputLabel,
+                    value: outputValue,
+                    plainValue: true,
+                },
+            ];
+        } else if (type === 'address') {
+            outputLines = [
+                {
+                    id: 'address',
+                    label: outputLabel,
+                    value: outputValue,
+                    confirmLabel: <Translation id="TR_RECIPIENT_ADDRESS_MATCH" />,
+                    plainValue: true,
+                },
+            ];
+        } else if (type === 'opreturn') {
+            outputLines = [
+                {
+                    id: 'opreturn',
+                    label: <Translation id="OP_RETURN" />,
+                    value: outputValue,
+                    plainValue: true,
+                },
+            ];
+        } else {
+            outputLines = [
+                {
+                    id: 'default',
+                    label: outputLabel,
+                    value: outputValue,
+                },
+            ];
+        }
 
-    const hasExpansion = (type === 'opreturn' || type === 'data') && outputValue.length >= 10;
+        const hasExpansion = (type === 'opreturn' || type === 'data') && outputValue.length >= 10;
 
-    return (
-        <TransactionReviewOutputElement
-            account={account}
-            indicator={<TransactionReviewStepIndicator state={state} size={16} />}
-            lines={outputLines}
-            token={token}
-            state={state}
-            cryptoSymbol={outputSymbol as NetworkSymbol}
-            fiatSymbol={symbol}
-            hasExpansion={hasExpansion}
-            fiatVisible={fiatVisible}
-        />
-    );
-};
+        return (
+            <TransactionReviewOutputElement
+                ref={ref}
+                account={account}
+                indicator={<TransactionReviewStepIndicator state={state} size={16} />}
+                lines={outputLines}
+                token={token}
+                state={state}
+                cryptoSymbol={outputSymbol as NetworkSymbol}
+                fiatSymbol={symbol}
+                hasExpansion={hasExpansion}
+                fiatVisible={fiatVisible}
+            />
+        );
+    },
+);
