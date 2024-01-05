569e316f8ce65ddce0fe57244a3ff9cde0fe2e96
Matej Kriz <matej@matejkriz.cz>
2024-01-05 10:41:44 +0100

    chore(suite-native): remove no longer valid comment
    - getAccountDescriptor is already available in Connect and used here.

--- suite-native/discovery/src/utils.ts
+++ suite-native/discovery/src/utils.ts
@@ -4,8 +4,7 @@ import { DiscoveryItem } from '@suite-common/wallet-types';
 import TrezorConnect from '@trezor/connect';
 
 import { DiscoveryDescriptorItem } from './types';
-// This function will be removed in one of the following PRs, when `TrezorConnect.getAccountDescriptor` method is ready.
-// see more: https://github.com/trezor/trezor-suite/issues/9658
+
 export const fetchBundleDescriptors = async (bundle: DiscoveryItem[]) => {
     const { success, payload } = await TrezorConnect.getAccountDescriptor({
         bundle,
