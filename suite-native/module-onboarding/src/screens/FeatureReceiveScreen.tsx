30c42b5b7ebf6891d6ce00ba990c0adad81059aa
juriczech <bohdan.juricek@gmail.com>
2024-01-09 16:30:22 +0100

    refactor(suite-native): receive screen

--- suite-native/module-onboarding/src/screens/FeatureReceiveScreen.tsx
+++ suite-native/module-onboarding/src/screens/FeatureReceiveScreen.tsx
@@ -1,3 +1,5 @@
+import { ReactNode } from 'react';
+
 import { useNavigation } from '@react-navigation/native';
 
 import { useIsUsbDeviceConnectFeatureEnabled } from '@suite-native/feature-flags';
@@ -7,82 +9,72 @@ import {
     StackNavigationProps,
 } from '@suite-native/navigation';
 import { Box } from '@suite-native/atoms';
-import { useTranslate } from '@suite-native/intl';
+import { TxKeyPath, useTranslate } from '@suite-native/intl';
 
 import { OnboardingFooter } from '../components/OnboardingFooter';
 import { OnboardingScreen } from '../components/OnboardingScreen';
 import { CoinsSvg } from '../components/CoinsSvg';
 
-const FeatureReceiveScreenWithDevice = () => {
-    const { translate } = useTranslate();
+type NavigationProps = StackNavigationProps<
+    OnboardingStackParamList,
+    OnboardingStackRoutes.AboutReceiveCoinsFeature
+>;
 
-    const navigation =
-        useNavigation<
-            StackNavigationProps<
-                OnboardingStackParamList,
-                OnboardingStackRoutes.AboutReceiveCoinsFeature
-            >
-        >();
+type ScreenContent = {
+    title: TxKeyPath;
+    subtitle: TxKeyPath;
+    redirectTarget: OnboardingStackRoutes;
+};
+const receiveScreenContentMap = {
+    device: {
+        title: 'moduleOnboarding.featureReceiveScreen.usb.title',
+        subtitle: 'moduleOnboarding.featureReceiveScreen.usb.subtitle',
+        redirectTarget: OnboardingStackRoutes.TrackBalances,
+    },
+    portfolioTracker: {
+        title: 'moduleOnboarding.featureReceiveScreen.noUsb.title',
+        subtitle: 'moduleOnboarding.featureReceiveScreen.noUsb.subtitle',
+        redirectTarget: OnboardingStackRoutes.AnalyticsConsent,
+    },
+} as const satisfies Record<'device' | 'portfolioTracker', ScreenContent>;
+
+const IconWrapper = ({ children }: { children: ReactNode }) => {
+    const { isUsbDeviceConnectFeatureEnabled } = useIsUsbDeviceConnectFeatureEnabled();
+
+    if (!isUsbDeviceConnectFeatureEnabled) return <>{children}</>;
 
     return (
-        <OnboardingScreen
-            title={translate('moduleOnboarding.featureReceiveScreen.usb.title')}
-            subtitle={translate('moduleOnboarding.featureReceiveScreen.usb.subtitle')}
-            activeStep={2}
-            footer={
-                <OnboardingFooter
-                    redirectTarget={() => navigation.navigate(OnboardingStackRoutes.TrackBalances)}
-                    onBack={navigation.goBack}
-                    backButtonTitle={translate('generic.buttons.back')}
-                    nextButtonTitle={translate('generic.buttons.next')}
-                />
-            }
-        >
-            <Box alignItems="center" flex={1} justifyContent="center">
-                <CoinsSvg />
-            </Box>
-        </OnboardingScreen>
+        <Box alignItems="center" flex={1} justifyContent="center">
+            {children}
+        </Box>
     );
 };
 
-const FeatureReceiveScreenNoDevice = () => {
+export const FeatureReceiveScreen = () => {
     const { translate } = useTranslate();
+    const navigation = useNavigation<NavigationProps>();
+    const { isUsbDeviceConnectFeatureEnabled } = useIsUsbDeviceConnectFeatureEnabled();
 
-    const navigation =
-        useNavigation<
-            StackNavigationProps<
-                OnboardingStackParamList,
-                OnboardingStackRoutes.AboutReceiveCoinsFeature
-            >
-        >();
+    const content =
+        receiveScreenContentMap[isUsbDeviceConnectFeatureEnabled ? 'device' : 'portfolioTracker'];
 
     return (
         <OnboardingScreen
-            title={translate('moduleOnboarding.featureReceiveScreen.noUsb.title')}
-            subtitle={translate('moduleOnboarding.featureReceiveScreen.noUsb.subtitle')}
+            title={translate(content.title)}
+            subtitle={translate(content.subtitle)}
             activeStep={2}
             footer={
                 <OnboardingFooter
-                    redirectTarget={() =>
-                        navigation.navigate(OnboardingStackRoutes.AnalyticsConsent)
-                    }
+                    redirectTarget={() => navigation.navigate(content.redirectTarget)}
                     onBack={navigation.goBack}
                     backButtonTitle={translate('generic.buttons.back')}
                     nextButtonTitle={translate('generic.buttons.continue')}
                 />
             }
         >
-            <CoinsSvg />
+            <IconWrapper>
+                <CoinsSvg />
+            </IconWrapper>
         </OnboardingScreen>
-    );
-};
-
-export const FeatureReceiveScreen = () => {
-    const { isUsbDeviceConnectFeatureEnabled } = useIsUsbDeviceConnectFeatureEnabled();
-
-    return isUsbDeviceConnectFeatureEnabled ? (
-        <FeatureReceiveScreenWithDevice />
-    ) : (
-        <FeatureReceiveScreenNoDevice />
     );
 };
