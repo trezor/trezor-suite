0474f8dfe451d2c20205001565305b078a5b4488
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2024-01-09 15:44:47 +0100

    wip: ci testing webextension

--- docker/docker-connect-popup-test.sh
+++ docker/docker-connect-popup-test.sh
@@ -5,5 +5,6 @@ xhost +
 LOCAL_USER_ID="$(id -u "$USER")"
 export LOCAL_USER_ID
 export TEST_FILE=$1
+export URL=https://suite.corp.sldev.cz/connect/feat/connect-explorer-serviceworker-proxy/
 
 docker-compose -f ./docker/docker-compose.connect-popup-test.yml up --build --abort-on-container-exit
