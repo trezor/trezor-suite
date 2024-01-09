11c74726db3aaea8a29577c358b0e7f8d2edb089
Jan Komarek <jan.komarek@satoshilabs.com>
2024-01-09 15:42:18 +0100

    chore(suite-data): update gitbook revision
    (cherry picked from commit b4327a4bd89aa11080237f72c64152f8cd637979)

--- packages/suite-data/src/guide/constants.ts
+++ packages/suite-data/src/guide/constants.ts
@@ -1,7 +1,7 @@
 import { resolve, join } from 'path';
 
 export const GITBOOK_SOURCE = 'https://github.com/trezor/trezor-suite-guide.git';
-export const GITBOOK_REVISION = 'bfc6b375681c495b302a30bfb26820e5d11404a0';
+export const GITBOOK_REVISION = '18cb3345ccecf979e6f9275af033b7683cea7fb2';
 export const TMP = join(resolve(__dirname, '../..'), 'tmp', 'guide');
 // Path to the GitBook assets. Relative to TMP.
 export const GITBOOK_ASSETS_DIR_PREFIX = '.gitbook';
