683bffe849b8c831f3d098178a5bb4f21f6f757b
Martin Varmuza <mvarmuza@gmail.com>
2024-01-09 10:16:19 +0100

    ci(connect): allow bigger build

--- packages/connect-web/scripts/check-inline-build-size.sh
+++ packages/connect-web/scripts/check-inline-build-size.sh
@@ -12,7 +12,10 @@ SIZE_S=$(du -s "$parent_path/../build/trezor-connect.js" | cut -f1)
 # at time of creating this script size was 320
 # if you have considerably more, there is a chance that you accidentally included
 # parts of code that shouldn't be in this build.
-if [[ "$SIZE_S" -gt 400 ]]
+echo "size: $SIZE_S"
+# todo: size should be smaller, it grew after https://github.com/trezor/trezor-suite/pull/10280 was merged
+# plan is to decresase it https://github.com/trezor/trezor-suite/issues/10554
+if [[ "$SIZE_S" -gt 610 ]]
 then
     echo "suspiciously large build detected!"
     exit 1;
