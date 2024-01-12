723ff11d023ed4793cb7efbfaa7b98ff3293722e
juriczech <bohdan.juricek@gmail.com>
2024-01-12 13:53:41 +0100

    feat(suite-native): different icons

--- suite-native/accounts/src/components/AccountListPlaceholder.tsx
+++ suite-native/accounts/src/components/AccountListPlaceholder.tsx
@@ -1,6 +1,10 @@
+import { useRoute } from '@react-navigation/native';
+
 import { Box, Pictogram } from '@suite-native/atoms';
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
 import { useTranslate } from '@suite-native/intl';
+import { ReceiveStackRoutes, RootStackRoutes } from '@suite-native/navigation';
+import { IconName } from '@suite-common/icons';
 
 const PLACEHOLDER_HEIGHT = 380;
 
@@ -17,12 +21,28 @@ const titleVariant = prepareNativeStyle(_ => ({
 export const AccountListPlaceholder = ({ isFilterEmpty }: AccountListPlaceholderProps) => {
     const { translate } = useTranslate();
     const { applyStyle } = useNativeStyles();
+    const route = useRoute();
+
+    const isReceiveRoute =
+        route.name === ReceiveStackRoutes.ReceiveAccounts ||
+        route.name === RootStackRoutes.ReceiveModal;
+
+    const getIcon = (): IconName => {
+        if (!isFilterEmpty) {
+            return 'searchLight';
+        }
+
+        if (isReceiveRoute) {
+            return 'receive';
+        }
+        return 'discover';
+    };
 
     return (
         <Box style={applyStyle(titleVariant)}>
             <Pictogram
                 variant="yellow"
-                icon="searchLight"
+                icon={getIcon()}
                 title={translate('moduleAccounts.emptyState.title')}
                 subtitle={translate(
                     `moduleAccounts.emptyState.${isFilterEmpty ? 'subtitle' : 'searchAgain'}`,
