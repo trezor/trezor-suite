59a68eda69c35b2b2f543e2c997aef45412cd4e4
Peter Sanderson <dierk.deidel@protonmail.com>
2024-01-12 15:37:24 +0100

    style(suite): use theme instead of the direct borderBox

--- packages/suite/src/views/dashboard/components/AssetsView/components/AssetTableRowGrid.tsx
+++ packages/suite/src/views/dashboard/components/AssetsView/components/AssetTableRowGrid.tsx
@@ -1,18 +1,20 @@
 import styled, { css } from 'styled-components';
 import { ArrowIcon } from './AssetInfo';
+import { borders } from '@trezor/theme';
 
 export const AssetTableRowGrid = styled.div`
     display: grid;
     overflow: hidden;
     grid-template-columns: 2fr 2fr 1fr 1fr 1fr;
-    border-radius: 8px;
+    border-radius: ${borders.radii.xs};
 
-    ${({ onClick }) =>
+    ${({ onClick, theme }) =>
         onClick !== undefined
             ? css`
                   cursor: pointer;
+
                   :hover {
-                      background: #d9d9d933;
+                      background: ${theme.backgroundSurfaceElevation2};
 
                       ${ArrowIcon} {
                           opacity: 1;
