5138988cbacd823cfe7a1e317f65dabcf3e81e0f
Peter Sanderson <dierk.deidel@protonmail.com>
2024-01-16 14:02:21 +0100

    style(suite): make Select work with Elevation

--- packages/components/src/components/form/Select/Select.tsx
+++ packages/components/src/components/form/Select/Select.tsx
@@ -19,11 +19,14 @@ import { useOnKeyDown } from './useOnKeyDown';
 import { useDetectPortalTarget } from './useDetectPortalTarget';
 import { DROPDOWN_MENU, menuStyle } from '../../Dropdown/menuStyle';
 import { useElevation } from '../../ElevationContext/ElevationContext';
-import { Elevation } from '@trezor/theme/src/elevation';
+import { Elevation, mapElevationToBackground, nextElevation } from '@trezor/theme/src/elevation';
 
 const reactSelectClassNamePrefix = 'react-select';
 
-const createSelectStyle = (theme: DefaultTheme): StylesConfig<Option, boolean> => ({
+const createSelectStyle = (
+    theme: DefaultTheme,
+    elevation: Elevation,
+): StylesConfig<Option, boolean> => ({
     menuPortal: base => ({
         ...base,
         zIndex: zIndices.modal /* Necessary to be visible inside a Modal */,
@@ -39,7 +42,7 @@ const createSelectStyle = (theme: DefaultTheme): StylesConfig<Option, boolean> =
         minWidth: 140,
         borderRadius: borders.radii.md,
         background: theme.backgroundSurfaceElevation1,
-        boxShadow: boxShadows.elevation3,
+        boxShadow: theme.boxShadowElevation3,
         zIndex: zIndices.modal,
         animation: `${DROPDOWN_MENU.getName()} 0.15s ease-in-out`,
         listStyleType: 'none',
@@ -71,7 +74,9 @@ const createSelectStyle = (theme: DefaultTheme): StylesConfig<Option, boolean> =
         ...base,
         padding: `${spacingsPx.xs} ${spacingsPx.sm}`,
         borderRadius: borders.radii.xxs,
-        background: isFocused ? theme.backgroundSurfaceElevation0 : 'transparent',
+        background: isFocused
+            ? theme[mapElevationToBackground[nextElevation[elevation]]]
+            : 'transparent',
 
         color: theme.textDefault,
         ...{
@@ -290,7 +295,7 @@ export const Select = ({
                 closeMenuOnScroll={closeMenuOnScroll}
                 menuPosition="fixed" // Required for closeMenuOnScroll to work properly when near page bottom
                 menuPortalTarget={menuPortalTarget}
-                styles={createSelectStyle(theme)}
+                styles={createSelectStyle(theme, elevation)}
                 onChange={handleOnChange}
                 isSearchable={isSearchable}
                 menuIsOpen={menuIsOpen}
