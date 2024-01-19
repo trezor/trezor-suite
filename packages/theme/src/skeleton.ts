00c734a0f6c57ca23c151ad0478f20d6a6ed630f
Peter Sanderson <dierk.deidel@protonmail.com>
2024-01-19 13:24:00 +0100

    style(suite): colors for skeleton

--- packages/theme/src/skeleton.ts
+++ packages/theme/src/skeleton.ts
@@ -1,41 +1,57 @@
+import {
+    standardBackgroundElevationColors as stdBg,
+    darkBackgroundElevationColors as darkBg,
+} from './background';
 import { Elevation } from './elevation';
 
 export const standardSkeletonColors = {
-    skeletonBackgroundElevationNegative: '#eeeeeeff',
-    skeletonForegroundElevationNegative: '#f6f6f6ff',
+    skeletonBackgroundElevationNegative: stdBg.backgroundSurfaceElevationNegative,
+    skeletonForegroundElevationNegative: stdBg.backgroundSurfaceElevation0,
 
-    skeletonBackgroundElevation0: '#f6f6f6',
-    skeletonForegroundElevation0: '#eeeeee',
+    skeletonBackgroundElevation0: stdBg.backgroundSurfaceElevation0,
+    skeletonForegroundElevation0: stdBg.backgroundSurfaceElevationNegative,
 
-    skeletonBackgroundElevation1: '#ffffff',
-    skeletonForegroundElevation1: '#f6f6f6',
+    skeletonBackgroundElevation1: stdBg.backgroundSurfaceElevation1,
+    skeletonForegroundElevation1: stdBg.backgroundSurfaceElevation0,
+
+    skeletonBackgroundElevation2: stdBg.backgroundSurfaceElevation2,
+    skeletonForegroundElevation2: stdBg.backgroundSurfaceElevation1,
+
+    skeletonBackgroundElevation3: stdBg.backgroundSurfaceElevation3,
+    skeletonForegroundElevation3: stdBg.backgroundSurfaceElevation0,
 };
 
 type SkeletonColor = keyof typeof standardSkeletonColors;
 
 export const darkSkeletonColors = {
-    skeletonBackgroundElevationNegative: '#000000ff',
-    skeletonForegroundElevationNegative: '#161716ff',
+    skeletonBackgroundElevationNegative: darkBg.backgroundSurfaceElevationNegative,
+    skeletonForegroundElevationNegative: darkBg.backgroundSurfaceElevation0,
 
-    skeletonBackgroundElevation0: '#161716ff',
-    skeletonForegroundElevation0: '#000000ff',
+    skeletonBackgroundElevation0: darkBg.backgroundSurfaceElevation0,
+    skeletonForegroundElevation0: darkBg.backgroundSurfaceElevationNegative,
 
-    skeletonBackgroundElevation1: '#ffffffff',
-    skeletonForegroundElevation1: '#161716ff',
+    skeletonBackgroundElevation1: darkBg.backgroundSurfaceElevation1,
+    skeletonForegroundElevation1: darkBg.backgroundSurfaceElevation0,
+
+    skeletonBackgroundElevation2: darkBg.backgroundSurfaceElevation2,
+    skeletonForegroundElevation2: darkBg.backgroundSurfaceElevation1,
+
+    skeletonBackgroundElevation3: darkBg.backgroundSurfaceElevation3,
+    skeletonForegroundElevation3: darkBg.backgroundSurfaceElevation0,
 };
 
 export const mapElevationToSkeletonBackground: Record<Elevation, SkeletonColor> = {
     '-1': 'skeletonBackgroundElevationNegative', // For example left menu is negative elevation
     0: 'skeletonBackgroundElevation0',
     1: 'skeletonBackgroundElevation1',
-    2: 'skeletonBackgroundElevation0',
-    3: 'skeletonBackgroundElevation1',
+    2: 'skeletonBackgroundElevation2',
+    3: 'skeletonBackgroundElevation3',
 };
 
 export const mapElevationToSkeletonForeground: Record<Elevation, SkeletonColor> = {
     '-1': 'skeletonForegroundElevationNegative', // For example left menu is negative elevation
     0: 'skeletonForegroundElevation0',
     1: 'skeletonForegroundElevation1',
-    2: 'skeletonForegroundElevation0',
-    3: 'skeletonForegroundElevation1',
+    2: 'skeletonForegroundElevation2',
+    3: 'skeletonForegroundElevation3',
 };
