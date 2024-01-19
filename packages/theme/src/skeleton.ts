f120037f371a51af62bfaeb23dc0829bb622e019
Peter Sanderson <dierk.deidel@protonmail.com>
2024-01-19 11:48:10 +0100

    style(suite): add skeleton colors

---
+++ packages/theme/src/skeleton.ts
@@ -0,0 +1,21 @@
+export const standardSkeletonColors = {
+    skeletonBackgroundElevationNegative: '#eeeeeeff',
+    skeletonForegroundElevationNegative: '#f6f6f6ff',
+
+    skeletonBackgroundElevation0: '#f6f6f6',
+    skeletonForegroundElevation0: '#eeeeee',
+
+    skeletonBackgroundElevation1: '#ffffff',
+    skeletonForegroundElevation1: '#f6f6f6',
+};
+
+export const darkSkeletonColors = {
+    skeletonBackgroundElevationNegative: '#eeeeeeff',
+    skeletonForegroundElevationNegative: '#f6f6f6ff',
+
+    skeletonBackgroundElevation0: '#f6f6f6',
+    skeletonForegroundElevation0: '#eeeeee',
+
+    skeletonBackgroundElevation1: '#ffffff',
+    skeletonForegroundElevation1: '#f6f6f6',
+};
