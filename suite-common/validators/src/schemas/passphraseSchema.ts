9168cdd8a56fbb5a3163816f545bea7b3bc7fc93
juriczech <bohdan.juricek@gmail.com>
2024-01-23 14:33:05 +0100

    ref(suite-native): remove min length

--- suite-common/validators/src/schemas/passphraseSchema.ts
+++ suite-common/validators/src/schemas/passphraseSchema.ts
@@ -2,11 +2,7 @@ import { yup } from '../config';
 import { formInputsMaxLength } from '../inputsLengthConfig';
 
 export const passphraseFormSchema = yup.object({
-    passphrase: yup
-        .string()
-        .required('Empty passphrase.')
-        .max(formInputsMaxLength.passphrase)
-        .min(1),
+    passphrase: yup.string().required('Empty passphrase.').max(formInputsMaxLength.passphrase),
 });
 
 export type PassphraseFormValues = yup.InferType<typeof passphraseFormSchema>;
--- suite-common/validators/src/schemas/pinSchema.ts
+++ suite-common/validators/src/schemas/pinSchema.ts
@@ -2,7 +2,7 @@ import { yup } from '../config';
 import { formInputsMaxLength } from '../inputsLengthConfig';
 
 export const pinFormSchema = yup.object({
-    pin: yup.string().required('Empty pin.').max(formInputsMaxLength.pin).min(1),
+    pin: yup.string().required('Empty pin.').max(formInputsMaxLength.pin),
 });
 
 export type PinFormValues = yup.InferType<typeof pinFormSchema>;
