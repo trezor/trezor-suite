3a8d285b529575054dc0cc394ee6eb4df3548e1f
Peter Sanderson <dierk.deidel@protonmail.com>
2024-01-23 13:15:59 +0100

    style(suite): rework of trade page (menu)

--- packages/suite/src/views/wallet/coinmarket/common/CoinmarketLayout/CoinmarketLayoutNavigation.tsx
+++ packages/suite/src/views/wallet/coinmarket/common/CoinmarketLayout/CoinmarketLayoutNavigation.tsx
@@ -1,25 +1,41 @@
-import styled from 'styled-components';
-import { goto } from 'src/actions/suite/routerActions';
-import { useDevice, useDispatch, useSelector } from 'src/hooks/suite';
-import { WalletLayoutNavigation, WalletLayoutNavLink } from 'src/components/wallet';
+import styled, { css } from 'styled-components';
+import { useDevice, useSelector } from 'src/hooks/suite';
 import { getTitleForNetwork } from '@suite-common/wallet-utils';
 import { Translation } from 'src/components/suite';
 import { selectSelectedAccount } from 'src/reducers/wallet/selectedAccountReducer';
 import { FirmwareType } from '@trezor/connect';
+import { FONT_SIZE, FONT_WEIGHT } from '@trezor/components/src/config/variables';
+import { TranslationKey } from '@suite-common/intl-types';
+import { Route } from '@suite-common/suite-types';
+import { borders, spacingsPx } from '@trezor/theme';
+import { NavigationItem } from '../../../../../components/suite/Preloader/SuiteLayout/Sidebar/NavigationItem';
+import { IconName } from '@suite-common/icons';
 
-const SavingsWalletLayoutNavLinkWrapper = styled.div`
+const List = styled.div`
     display: flex;
-    margin-left: auto;
-    padding-left: 42px;
+    flex-direction: column;
+    width: 171px;
+    gap: ${spacingsPx.xxs};
 `;
 
-const items = [
-    { route: 'wallet-coinmarket-buy', title: 'TR_NAV_BUY' },
-    { route: 'wallet-coinmarket-sell', title: 'TR_NAV_SELL' },
-    { route: 'wallet-coinmarket-exchange', title: 'TR_NAV_EXCHANGE' },
-    { route: 'wallet-coinmarket-spend', title: 'TR_NAV_SPEND' },
-] as const;
-const p2pRoute = 'wallet-coinmarket-p2p';
+const NavListItem = styled(NavigationItem)`
+    white-space: nowrap;
+    font-size: ${FONT_SIZE.NORMAL};
+    font-weight: ${FONT_WEIGHT.MEDIUM};
+
+    ${({ isActive }) =>
+        isActive
+            ? css`
+                  border-radius: ${borders.radii.full};
+              `
+            : ''}
+`;
+
+const HorizontalDivider = styled.div`
+    margin: ${spacingsPx.xxs} 0; /* gap xxs + margin xxs = spacing xs around divider */
+    width: 100%;
+    border-bottom: 1px solid ${({ theme }) => theme.textDefault};
+`;
 
 export const CoinmarketLayoutNavigation = () => {
     const routeName = useSelector(state => state.router.route?.name);
@@ -31,7 +47,6 @@ export const CoinmarketLayoutNavigation = () => {
         state => state.wallet.coinmarket.savings.savingsInfo?.savingsList?.providers,
     );
     const { device } = useDevice();
-    const dispatch = useDispatch();
 
     const showP2pTab = account && p2pSupportedCoins && p2pSupportedCoins.has(account.symbol);
     const showSavingsTab =
@@ -43,59 +58,55 @@ export const CoinmarketLayoutNavigation = () => {
                 savingsProvider.tradedCoins.includes(account.symbol.toUpperCase()),
         );
 
-    const goToP2p = () => dispatch(goto(p2pRoute, { preserveParams: true }));
-    const goToSavingsSetup = () =>
-        dispatch(
-            goto('wallet-coinmarket-savings-setup', {
-                preserveParams: true,
-            }),
-        );
+    const isBitcoinOnly = device?.firmwareType === FirmwareType.BitcoinOnly;
+
+    const Item = ({
+        route,
+        title,
+        icon,
+    }: {
+        route: Route['name'];
+        title: TranslationKey;
+        icon: IconName;
+    }) => (
+        <NavListItem
+            data-test={`@coinmarket/menu/${route}`}
+            nameId={title}
+            isActive={routeName === route}
+            icon={icon}
+            route={route}
+        />
+    );
 
     return (
-        <WalletLayoutNavigation>
-            <>
-                {items
-                    .filter(
-                        item =>
-                            !(
-                                item.route === 'wallet-coinmarket-exchange' &&
-                                device?.firmwareType === FirmwareType.BitcoinOnly
-                            ),
-                    )
-                    .map(({ route, title }) => (
-                        <WalletLayoutNavLink
-                            data-test={`@coinmarket/menu/${route}`}
-                            key={route}
-                            title={title}
-                            active={routeName === route}
-                            onClick={() => dispatch(goto(route, { preserveParams: true }))}
-                        />
-                    ))}
-                {showP2pTab && (
-                    <WalletLayoutNavLink
-                        key={p2pRoute}
-                        title="TR_NAV_P2P"
-                        active={routeName === p2pRoute}
-                        onClick={goToP2p}
-                    />
-                )}
-                {showSavingsTab && (
-                    <SavingsWalletLayoutNavLinkWrapper>
-                        <WalletLayoutNavLink
-                            key="wallet-coinmarket-savings"
-                            title="TR_NAV_SAVINGS"
-                            values={{
-                                cryptoCurrencyName: (
-                                    <Translation id={getTitleForNetwork(account.symbol)} />
-                                ),
-                            }}
-                            badge="TR_NAV_SAVINGS_BADGE"
-                            active={!!routeName?.startsWith('wallet-coinmarket-savings')}
-                            onClick={goToSavingsSetup}
-                        />
-                    </SavingsWalletLayoutNavLinkWrapper>
-                )}
-            </>
-        </WalletLayoutNavigation>
+        <List>
+            <Item route="wallet-coinmarket-buy" title="TR_NAV_BUY" icon="plus" />
+            <Item route="wallet-coinmarket-sell" title="TR_NAV_SELL" icon="minus" />
+
+            {!isBitcoinOnly ? (
+                <Item route="wallet-coinmarket-exchange" title="TR_NAV_EXCHANGE" icon="trade" />
+            ) : null}
+
+            <HorizontalDivider />
+
+            {showSavingsTab && (
+                <NavListItem
+                    key="wallet-coinmarket-savings"
+                    isActive={!!routeName?.startsWith('wallet-coinmarket-savings')}
+                    route="wallet-coinmarket-savings-setup"
+                    icon="clock"
+                    nameId="TR_NAV_SAVINGS"
+                    dataTest="@coinmarket/menu/wallet-coinmarket-savings-setup"
+                    values={{
+                        cryptoCurrencyName: <Translation id={getTitleForNetwork(account.symbol)} />,
+                    }}
+                />
+            )}
+
+            {showP2pTab && (
+                <Item route="wallet-coinmarket-p2p" title="TR_NAV_P2P" icon="userFocus" />
+            )}
+            <Item route="wallet-coinmarket-spend" title="TR_NAV_SPEND" icon="label" />
+        </List>
     );
 };
