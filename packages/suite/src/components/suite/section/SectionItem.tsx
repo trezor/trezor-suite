be9ce0990890c88897bed70bb0c1d7402439ad44
Peter Sanderson <dierk.deidel@protonmail.com>
2024-01-25 15:41:35 +0100

    style(suite): fix z-index for settings selects

--- packages/suite/src/components/suite/section/SectionItem.tsx
+++ packages/suite/src/components/suite/section/SectionItem.tsx
@@ -12,11 +12,6 @@ const Content = styled.div<{ shouldHighlight?: boolean }>`
     margin: -${spacingsPx.md};
     border-radius: ${({ shouldHighlight }) => shouldHighlight && borders.radii.xs};
 
-    /*
-        so that the borders underneath are hidden - used for anchor highlighting
-        @TODO it causes following bug: https://github.com/trezor/trezor-suite/issues/10792
-    */
-    z-index: 1;
     ${anchorOutlineStyles}
 
     @media all and (max-width: ${variables.SCREEN_SIZE.SM}) {
