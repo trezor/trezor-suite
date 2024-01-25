879ca49be8170babbce63e0888c92708be0a7995
Jan Václavík <jvaclavik@gmail.com>
2024-01-25 08:58:07 +0100

    Style/10533 button group device orientation (#10797)
    * style(suite): Use ButtonGroup for device orientation
    
    * style(suite): Use ButtonGroup for image upload

--- packages/suite/src/views/settings/SettingsDevice/DisplayRotation.tsx
+++ packages/suite/src/views/settings/SettingsDevice/DisplayRotation.tsx
@@ -1,15 +1,8 @@
-import styled from 'styled-components';
 import { analytics, EventType } from '@trezor/suite-analytics';
 
-import {
-    ActionButton,
-    ActionColumn,
-    SectionItem,
-    TextColumn,
-    Translation,
-} from 'src/components/suite';
-import { variables } from '@trezor/components';
-import { useDispatch } from 'src/hooks/suite';
+import { ActionColumn, SectionItem, TextColumn, Translation } from 'src/components/suite';
+import { Button, ButtonGroup } from '@trezor/components';
+import { useDevice, useDispatch } from 'src/hooks/suite';
 import { applySettings } from 'src/actions/settings/deviceSettingsActions';
 import { useAnchor } from 'src/hooks/suite/useAnchor';
 import { SettingsAnchor } from 'src/constants/suite/anchors';
@@ -21,17 +14,6 @@ const DISPLAY_ROTATIONS = [
     { label: <Translation id="TR_WEST" />, value: 270 },
 ] as const;
 
-const RotationButton = styled(ActionButton)`
-    min-width: 81px;
-    flex-basis: auto;
-
-    &:not(:first-of-type) {
-        @media (max-width: ${variables.SCREEN_SIZE.SM}) {
-            margin-top: 10px;
-        }
-    }
-`;
-
 interface DisplayRotationProps {
     isDeviceLocked: boolean;
 }
@@ -39,6 +21,8 @@ interface DisplayRotationProps {
 export const DisplayRotation = ({ isDeviceLocked }: DisplayRotationProps) => {
     const dispatch = useDispatch();
     const { anchorRef, shouldHighlight } = useAnchor(SettingsAnchor.DisplayRotation);
+    const { device } = useDevice();
+    const currentRotation = device?.features?.display_rotation;
 
     return (
         <SectionItem
@@ -48,25 +32,26 @@ export const DisplayRotation = ({ isDeviceLocked }: DisplayRotationProps) => {
         >
             <TextColumn title={<Translation id="TR_DEVICE_SETTINGS_DISPLAY_ROTATION" />} />
             <ActionColumn>
-                {DISPLAY_ROTATIONS.map(variant => (
-                    <RotationButton
-                        key={variant.value}
-                        variant="secondary"
-                        onClick={() => {
-                            dispatch(applySettings({ display_rotation: variant.value }));
-                            analytics.report({
-                                type: EventType.SettingsDeviceChangeOrientation,
-                                payload: {
-                                    value: variant.value,
-                                },
-                            });
-                        }}
-                        data-test={`@settings/device/rotation-button/${variant.value}`}
-                        isDisabled={isDeviceLocked}
-                    >
-                        {variant.label}
-                    </RotationButton>
-                ))}
+                <ButtonGroup size="small" isDisabled={isDeviceLocked}>
+                    {DISPLAY_ROTATIONS.map(variant => (
+                        <Button
+                            key={variant.value}
+                            variant={currentRotation === variant.value ? 'primary' : 'secondary'}
+                            onClick={() => {
+                                dispatch(applySettings({ display_rotation: variant.value }));
+                                analytics.report({
+                                    type: EventType.SettingsDeviceChangeOrientation,
+                                    payload: {
+                                        value: variant.value,
+                                    },
+                                });
+                            }}
+                            data-test={`@settings/device/rotation-button/${variant.value}`}
+                        >
+                            {variant.label}
+                        </Button>
+                    ))}
+                </ButtonGroup>
             </ActionColumn>
         </SectionItem>
     );
