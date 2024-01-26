660e868f33ac4cb9839fefc55b9cba9b475c35c2
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2024-01-26 10:28:53 +0100

    feat(connect-explorer): build it as webextension

---
+++ packages/connect-explorer/src-webextension/pages/connect-explorer/index.tsx
@@ -0,0 +1,7 @@
+import { createRoot } from 'react-dom/client';
+
+import App from '../../../src/router';
+
+const container = document.getElementById('connect-explorer-container');
+const root = createRoot(container!);
+root.render(<App />);
