9d5d5e772dc56e5056724e1672813a8e49016f6a
Jan Václavík <jvaclavik@gmail.com>
2024-01-26 16:49:21 +0100

    style(suite): Find trezor button

--- packages/suite/src/components/suite/WebUsbButton.tsx
+++ packages/suite/src/components/suite/WebUsbButton.tsx
@@ -1,24 +1,20 @@
 import TrezorConnect from '@trezor/connect';
-import { ButtonProps, IconButton } from '@trezor/components';
-import { useTranslation } from 'src/hooks/suite';
+import { ButtonProps, Button } from '@trezor/components';
 import { Translation } from './Translation';
 
-export const WebUsbButton = (props: Omit<ButtonProps, 'children'>) => {
-    const { translationString } = useTranslation();
-    return (
-        <div data-test="TR_CHECK_FOR_DEVICES">
-            <IconButton
-                {...props}
-                icon="REFRESH"
-                title={translationString('TR_CHECK_FOR_DEVICES')}
-                variant="tertiary"
-                onClick={e => {
-                    e.stopPropagation();
-                    TrezorConnect.requestWebUSBDevice();
-                }}
-                size="small"
-                label={<Translation id="TR_CHECK_FOR_DEVICES" />}
-            />
-        </div>
-    );
-};
+export const WebUsbButton = (props: Omit<ButtonProps, 'children'>) => (
+    <div data-test="TR_CHECK_FOR_DEVICES">
+        <Button
+            {...props}
+            icon="SEARCH"
+            variant="primary"
+            onClick={e => {
+                e.stopPropagation();
+                TrezorConnect.requestWebUSBDevice();
+            }}
+            size="small"
+        >
+            <Translation id="TR_CHECK_FOR_DEVICES" />
+        </Button>
+    </div>
+);
