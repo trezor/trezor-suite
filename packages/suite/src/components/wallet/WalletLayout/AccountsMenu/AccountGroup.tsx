6832219627a7a60977fa90a41a29cbafdcaf600d
dahaca <dahaca17@gmail.com>
2024-01-26 12:22:51 +0100

    fix(component): fix incorrect focus shadow style

--- packages/suite/src/components/wallet/WalletLayout/AccountsMenu/AccountGroup.tsx
+++ packages/suite/src/components/wallet/WalletLayout/AccountsMenu/AccountGroup.tsx
@@ -13,6 +13,9 @@ const Container = styled.div`
 const HeaderWrapper = styled.div`
     position: sticky;
     top: 0;
+
+    /* to not overlap with the Account's focus shadow */
+    margin-bottom: ${spacingsPx.xxs};
     z-index: 1;
 `;
 
@@ -126,6 +129,7 @@ export const AccountGroup = forwardRef(
                         </Header>
                     )}
                 </HeaderWrapper>
+
                 <AnimationWrapper opened={isOpen} onUpdate={onUpdate}>
                     {children}
                 </AnimationWrapper>
