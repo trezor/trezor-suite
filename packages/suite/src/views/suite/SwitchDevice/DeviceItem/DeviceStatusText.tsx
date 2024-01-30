70fc35b1cd59a620b424d80150a8860d3ef54f5f
0xadams.eth <schinzel.adam@gmail.com>
2024-01-30 16:21:23 +0100

    fix(suite): acquire device (#10880)

--- packages/suite/src/views/suite/SwitchDevice/DeviceItem/DeviceStatusText.tsx
+++ packages/suite/src/views/suite/SwitchDevice/DeviceItem/DeviceStatusText.tsx
@@ -1,13 +1,22 @@
-import styled, { useTheme } from 'styled-components';
+import styled, { css, useTheme } from 'styled-components';
 import { TrezorDevice } from '@suite-common/suite-types';
+import * as deviceUtils from '@suite-common/suite-utils';
 import { Icon } from '@trezor/components';
 import { spacingsPx, typography } from '@trezor/theme';
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import { Translation, WalletLabeling } from 'src/components/suite';
 
-const Container = styled.span<{ color: string }>`
+const Container = styled.span<{ color: string; isAction?: boolean }>`
     ${typography.label}
     color: ${({ color }) => color};
+
+    ${({ isAction }) =>
+        isAction &&
+        css`
+            :hover {
+                opacity: 0.8;
+            }
+        `}
 `;
 const TextRow = styled.div`
     display: flex;
@@ -15,11 +24,33 @@ const TextRow = styled.div`
     gap: ${spacingsPx.xxs};
 `;
 
-type DeviceStatusTextProps = { device: TrezorDevice; walletLabel?: string };
+type DeviceStatusTextProps = {
+    device: TrezorDevice;
+    onRefreshClick?: MouseEventHandler;
+    walletLabel?: string;
+};
 
-export const DeviceStatusText = ({ device, walletLabel }: DeviceStatusTextProps) => {
+export const DeviceStatusText = ({
+    device,
+    onRefreshClick,
+    walletLabel,
+}: DeviceStatusTextProps) => {
     const theme = useTheme();
     const { connected } = device;
+    const deviceStatus = deviceUtils.getStatus(device);
+    const needsAttention = deviceUtils.deviceNeedsAttention(deviceStatus);
+
+    if (connected && needsAttention && onRefreshClick) {
+        return (
+            <Container isAction onClick={onRefreshClick} color={theme.textAlertYellow}>
+                <TextRow>
+                    <Icon icon="REFRESH" size={12} color={theme.textAlertYellow} />
+                    <Translation id="TR_SOLVE_ISSUE" />
+                </TextRow>
+            </Container>
+        );
+    }
+
     return (
         <Container
             color={connected ? theme.textPrimaryDefault : theme.textSubdued}
@@ -29,7 +60,7 @@ export const DeviceStatusText = ({ device, walletLabel }: DeviceStatusTextProps)
                 <Icon
                     icon={connected ? 'LINK' : 'UNLINK'}
                     size={12}
-                    color={connected ? theme.iconPrimaryDefault : theme.iconPrimaryDefault}
+                    color={connected ? theme.textPrimaryDefault : theme.textSubdued}
                 />
                 {walletLabel ? (
                     <WalletLabeling device={device} />
