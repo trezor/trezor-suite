bd59dda5c3e71682e17bd30ae37bcfb07fd12738
dahaca <dahaca17@gmail.com>
2024-01-31 17:53:25 +0100

    fix(suite): coin filter outline

--- packages/suite/src/components/wallet/WalletLayout/AccountsMenu/CoinsFilter.tsx
+++ packages/suite/src/components/wallet/WalletLayout/AccountsMenu/CoinsFilter.tsx
@@ -4,40 +4,35 @@ import { CoinLogo, Tooltip } from '@trezor/components';
 import { selectDevice } from '@suite-common/wallet-core';
 
 import { useSelector, useAccountSearch } from 'src/hooks/suite';
-import { spacingsPx } from '@trezor/theme';
+import { borders, spacingsPx } from '@trezor/theme';
 
 const StyledCoinLogo = styled(CoinLogo)<{ isSelected?: boolean }>`
     display: block;
-    ${({ isSelected }) => !isSelected && `filter: grayscale(100%);`}
+    border-radius: ${borders.radii.full};
+    outline: 2px solid
+        ${({ isSelected, theme }) =>
+            isSelected ? theme.backgroundSecondaryPressed : 'transparent'};
+    transition: outline 0.2s;
+    filter: ${({ isSelected }) => !isSelected && 'grayscale(100%)'};
+    cursor: pointer;
+
+    :hover {
+        outline: 2px solid ${({ theme }) => theme.backgroundSecondaryDefault};
+    }
 `;
 const Container = styled.div`
+    position: relative;
     display: flex;
     flex-wrap: wrap;
     align-items: center;
     gap: ${spacingsPx.xxs};
     margin: ${spacingsPx.xs} ${spacingsPx.xs} ${spacingsPx.xs} 48px;
     z-index: 2;
-    position: relative;
 
     :hover {
         ${StyledCoinLogo} {
             filter: none;
         }
-    }
-`;
-
-const OuterCircle = styled.div<{ isSelected?: boolean }>`
-    height: 19px;
-    width: 19px;
-    border-radius: 50%;
-    border: 2px solid
-        ${({ isSelected, theme }) =>
-            isSelected ? theme.backgroundSecondaryPressed : 'transparent'};
-    transition: all 0.3;
-    cursor: pointer;
-
-    :hover {
-        border: 2px solid ${({ theme }) => theme.backgroundSecondaryDefault};
     }
 `;
 
@@ -61,28 +56,28 @@ export const CoinsFilter = () => {
                 setCoinFilter(undefined);
             }}
         >
-            {supportedNetworks.map(n => {
-                const isSelected = coinFilter === n;
+            {supportedNetworks.map(network => {
+                const isSelected = coinFilter === network;
                 return (
-                    <OuterCircle
-                        key={n}
-                        isSelected={isSelected}
-                        onClick={e => {
-                            e.stopPropagation();
-                            // select the coin or deactivate if it's already selected
-                            setCoinFilter(coinFilter === n ? undefined : n);
-                        }}
+                    <Tooltip
+                        key={network}
+                        content={network.toUpperCase()}
+                        cursor="pointer"
+                        delay={500}
                     >
-                        <Tooltip content={n.toUpperCase()} cursor="pointer" delay={500}>
-                            <StyledCoinLogo
-                                data-test={`@account-menu/filter/${n}`}
-                                symbol={n}
-                                size={16}
-                                data-test-activated={coinFilter === n}
-                                isSelected={isSelected}
-                            />
-                        </Tooltip>
-                    </OuterCircle>
+                        <StyledCoinLogo
+                            data-test={`@account-menu/filter/${network}`}
+                            symbol={network}
+                            size={16}
+                            data-test-activated={coinFilter === network}
+                            isSelected={isSelected}
+                            onClick={e => {
+                                e.stopPropagation();
+                                // select the coin or deactivate if it's already selected
+                                setCoinFilter(coinFilter === network ? undefined : network);
+                            }}
+                        />
+                    </Tooltip>
                 );
             })}
         </Container>
