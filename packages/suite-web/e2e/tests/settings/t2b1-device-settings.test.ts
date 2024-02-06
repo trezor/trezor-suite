5634ec5cb0aa453480a6f1de153da9230f7085eb
Martin Varmuza <mvarmuza@gmail.com>
2024-02-06 13:44:20 +0100

    test(suite): fix settings tests

--- packages/suite-web/e2e/tests/settings/t2b1-device-settings.test.ts
+++ packages/suite-web/e2e/tests/settings/t2b1-device-settings.test.ts
@@ -15,7 +15,7 @@ describe('T2B1 - Device settings', () => {
     };
 
     beforeEach(() => {
-        cy.viewport(1080, 1440).resetDb();
+        cy.viewport(1080, 3200).resetDb();
     });
 
     /*
@@ -30,7 +30,7 @@ describe('T2B1 - Device settings', () => {
      * 8. change the device's background
      * 9. change the device's rotation
      */
-    it('change all possible device settings', () => {
+    it.only('change all possible device settings', () => {
         //
         // Test preparation & constants
         //
