daedecec641b408e3145e0d5c6d7588f7820a6d6
Szymon Lesisz <sz.lesisz@gmail.com>
2024-02-06 20:12:07 +0100

    feat: create `@trezor/transport-bluetooth` package

---
+++ packages/transport-bluetooth/src/client/bluetooth-ipc-proxy.ts
@@ -0,0 +1,218 @@
+import { TypedEmitter } from '@trezor/utils';
+
+import { TrezorBle } from './trezor-ble';
+import type {
+    BluetoothApiEvents,
+    BluetoothDevice,
+    BluetoothIpcApi,
+    BluetoothIpcApiState,
+    DeviceConnectionStatus,
+    TrezorBleSettings,
+} from './types';
+
+const notImplemented = (): any => {
+    throw new Error('Method not implemented.');
+};
+
+// override this object with proxy
+export const bluetoothManager: BluetoothIpcApi = {
+    getAvailability: notImplemented,
+    connectDevice: notImplemented,
+    disconnectDevice: notImplemented,
+    forgetDevice: notImplemented,
+    startScan: notImplemented,
+    stopScan: notImplemented,
+    on: notImplemented,
+    off: notImplemented,
+    removeAllListeners: notImplemented,
+    setState: notImplemented,
+};
+/*
+ * Shared class to be used in @trezor/suite-desktop
+ * Provides ipc-proxy between main and renderer context
+ * Usage:
+ * - [main] just import and use
+ * - [renderer] override
+ */
+// class implemented in electron main context
+// should be overriden in electron renderer context
+export class BluetoothApiImpl extends TypedEmitter<BluetoothApiEvents> implements BluetoothIpcApi {
+    private api: TrezorBle;
+    private state: BluetoothIpcApiState = { knownDevices: [] };
+
+    constructor(settings: TrezorBleSettings) {
+        super();
+        this.api = new TrezorBle(settings);
+    }
+
+    getAvailability() {
+        return Promise.resolve({ success: true, payload: true } as const);
+    }
+
+    disconnectDevice() {
+        return Promise.resolve({ success: true, payload: true } as const);
+    }
+
+    async connectDevice(uuid: string) {
+        try {
+            await this.api.connect();
+        } catch (error) {
+            return { success: false, error: error.message };
+        }
+
+        const emitStatus = (event: DeviceConnectionStatus) => {
+            this.emit('device-connection-status', event);
+        };
+
+        this.api.on('device_connection_status', event =>
+            emitStatus({ uuid: event.uuid, type: event.phase }),
+        );
+        this.api.on('device_pairing', event => {
+            if (!event.paired) {
+                emitStatus({
+                    uuid,
+                    type: 'pairing',
+                    pin: event.pin,
+                });
+            } else {
+                emitStatus({
+                    uuid: event.uuid,
+                    type: 'paired',
+                });
+            }
+        });
+
+        try {
+            const result = await this.api.send('connect_device', uuid);
+            console.warn('Connect result', result);
+        } catch (error) {
+            return { success: false, error: error.message };
+        }
+
+        return { success: true } as const;
+    }
+
+    async forgetDevice(id: string) {
+        try {
+            await this.api.connect();
+        } catch (error) {
+            return { success: false, error: error.message };
+        }
+
+        const result = await this.api
+            .send('forget_device', id)
+            .then(() => ({ success: true }) as const)
+            .catch(error => ({ success: false, error: error.message }));
+        console.warn('Forget result', result);
+
+        return result;
+    }
+
+    async startScan() {
+        try {
+            await this.api.connect();
+        } catch (error) {
+            return { success: false, error: error.message };
+        }
+
+        const connectableDevices = (devs: BluetoothDevice[]) =>
+            devs
+                .filter(
+                    d =>
+                        this.state.knownDevices.find(dev => d.address === dev.address) ||
+                        d.paired ||
+                        (d.data && d.data[0] === 1),
+                )
+                .map(d => {
+                    // TODO: paired device on linux adv. data missing
+                    if (d.paired && d.data.length === 0) {
+                        d.data.push(1, 1, 1);
+                    }
+
+                    return d;
+                });
+        // return devs.filter(d => d.data.length > 0 && (d.paired || d.data[0] === 1));
+        const emitSelect = ({ devices }: { devices: BluetoothDevice[] }) => {
+            this.emit('device-list-update', connectableDevices(devices));
+        };
+        const emitAdapterState = ({ powered }: { powered: boolean }) => {
+            this.emit('adapter-event', powered);
+            if (!powered) {
+                // api.send('stop_scan');
+            } else {
+                this.api.send('start_scan').catch(error => {
+                    console.warn('Start scan error', error);
+                });
+            }
+        };
+
+        this.api.on('device_discovered', emitSelect);
+        this.api.on('device_updated', emitSelect);
+        this.api.on('device_connected', emitSelect);
+        this.api.on('device_disconnected', emitSelect);
+        this.api.on('adapter_state_changed', emitAdapterState);
+
+        try {
+            const info = await this.api.send('get_info');
+            // emit adapter event
+            if (!info.powered) {
+                this.emit('adapter-event', false);
+            }
+
+            const devices = await this.api.send('start_scan');
+            emitSelect({ devices: connectableDevices(devices) });
+        } catch (error) {
+            return { success: false, error: error.message };
+        }
+
+        return { success: true } as const;
+    }
+
+    async stopScan() {
+        try {
+            await this.api.connect();
+            await this.api.send('stop_scan');
+
+            return { success: true } as const;
+        } catch (error) {
+            return { success: false, error: error.message };
+        } finally {
+            console.warn('FINALLY!!!');
+            this.api.removeAllListeners();
+            this.api.disconnect();
+        }
+    }
+
+    async setState(state: BluetoothIpcApiState) {
+        this.state = state;
+        if (state.knownDevices.length > 0) {
+            try {
+                await this.api.connect();
+                await this.api.send('start_scan');
+                const devices = await this.api.send('enumerate');
+
+                const promises = devices.flatMap(d => {
+                    // device is already connected to BT (linux only?)
+                    if (d.connected) {
+                        return this.api.send('connect_device', d.uuid);
+                    }
+                    const isKnown = state.knownDevices.find(dd => dd.address === d.address);
+                    if (isKnown) {
+                        return this.api.send('connect_device', d.uuid);
+                    }
+
+                    return [];
+                });
+                await Promise.all(promises);
+
+                await this.api.send('stop_scan');
+
+                console.warn('inital scan connected', devices);
+            } catch (e) {
+                console.warn('initial scan error', e);
+            }
+        }
+
+        return Promise.resolve({ success: true } as const);
+    }
+}
---
+++ packages/transport-bluetooth/src/client/trezor-ble.ts
@@ -0,0 +1,95 @@
+import { WebsocketClient } from '@trezor/websocket-client';
+
+import {
+    BluetoothDevice,
+    BluetoothInfo,
+    Logger,
+    NotificationEvent,
+    TrezorBleSettings,
+} from './types';
+
+export type TODOReq = {
+    (method: 'get_info', adapter?: boolean): Promise<BluetoothInfo>;
+    (method: 'enumerate'): Promise<BluetoothDevice[]>;
+    (method: 'start_scan'): Promise<BluetoothDevice[]>;
+    (method: 'stop_scan'): Promise<boolean>;
+    (method: 'connect_device', uuid: string): Promise<boolean>;
+    (method: 'disconnect_device', uuid: string): Promise<boolean>;
+    (method: 'forget_device', uuid: string): Promise<boolean>;
+    (method: 'open_device', uuid: string): Promise<boolean>;
+    (method: 'close_device', uuid: string): Promise<boolean>;
+    (method: 'read', uuid: string): Promise<boolean>;
+    (method: 'write', args: [string, number[]]): Promise<boolean>;
+    // req: { method: { name: string; args?: [] } };
+    // resp: { b: 1 };
+};
+
+// Client for trezor-ble websocket server
+export class TrezorBle extends WebsocketClient<NotificationEvent> {
+    readonly settings: TrezorBleSettings;
+    readonly logger: Logger;
+    private devices: BluetoothDevice[] = [];
+
+    constructor(settings: TrezorBleSettings) {
+        super({
+            url: 'ws://127.0.0.1:21327', // TODO: url dynamic, URL NEEDED HERE?
+            // pingTimeout: 5000,
+        });
+        this.settings = Object.freeze(settings);
+        this.logger = settings.logger || {
+            debug: (..._args: string[]) => {},
+            log: (..._args: string[]) => {},
+            warn: (..._args: string[]) => {},
+            error: (..._args: string[]) => {},
+        };
+    }
+
+    createWebsocket() {
+        return this.initWebsocket({
+            url: 'ws://127.0.0.1:21327', // TODO: url dynamic
+            headers: {
+                Origin: 'https://node.trezor.io',
+                'User-Agent': 'Trezor Suite',
+            },
+        });
+    }
+
+    ping() {
+        return Promise.resolve(this.sendRawMessage('PING'));
+    }
+
+    public getDevices() {
+        return this.devices;
+    }
+
+    send(method: 'get_info', adapter?: boolean): Promise<BluetoothInfo>;
+    send(method: 'enumerate'): Promise<BluetoothDevice[]>;
+    send(method: 'start_scan'): Promise<BluetoothDevice[]>;
+    send(method: 'stop_scan'): Promise<boolean>;
+    send(method: 'connect_device', uuid: string): Promise<boolean>;
+    send(method: 'disconnect_device', uuid: string): Promise<boolean>;
+    send(method: 'forget_device', uuid: string): Promise<boolean>;
+    send(method: 'open_device', uuid: string): Promise<boolean>;
+    send(method: 'close_device', uuid: string): Promise<boolean>;
+    send(method: 'read', uuid: string): Promise<boolean>;
+    send(method: 'write', args: [string, number[]]): Promise<boolean>;
+    public send(method: string, args?: any) {
+        return this.sendMessage({ method: { name: method, args: args || [] } });
+    }
+
+    protected onMessage(message: string | Buffer) {
+        super.onMessage(message, data => {
+            console.warn('onBTmessage', data);
+            if (data.event) {
+                this.emit(data.event, data.payload);
+
+                return;
+            }
+            if (data.error) {
+                throw new Error(data.error);
+            }
+
+            return data.payload;
+        });
+    }
+}
