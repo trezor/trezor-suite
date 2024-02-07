4984db6459ac209341b25e9bbf62bea162ef72d2
Peter Sanderson <dierk.deidel@protonmail.com>
2024-02-07 11:49:51 +0100

    fix(suite): fix banner for AnalyzeInExplorer

--- packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/TxDetailModal/AdvancedTxDetails/IODetails/AnalyzeInExplorerBanner.tsx
+++ packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/TxDetailModal/AdvancedTxDetails/IODetails/AnalyzeInExplorerBanner.tsx
@@ -1,21 +1,9 @@
-import styled, { useTheme } from 'styled-components';
-import { darken } from 'polished';
+import styled from 'styled-components';
 
-import { Button, Icon, Link, variables, Warning } from '@trezor/components';
-import { Translation } from 'src/components/suite';
+import { variables } from '@trezor/components';
+import { NotificationCard, Translation } from 'src/components/suite';
 import { useSelector } from 'src/hooks/suite';
-
-const StyledWarning = styled(Warning)`
-    margin-bottom: 16px;
-    display: flex;
-    justify-content: space-between;
-`;
-
-const Wrapper = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 16px;
-`;
+import { typography } from '@trezor/theme';
 
 const TextWrapper = styled.div`
     display: flex;
@@ -28,28 +16,11 @@ const TextWrapper = styled.div`
 `;
 
 const Heading = styled.span`
-    font-weight: ${variables.FONT_WEIGHT.DEMI_BOLD};
+    font-weight: ${typography.highlight};
 `;
 
 const Description = styled.span`
-    color: ${({ theme }) => theme.TYPE_LIGHT_GREY};
-`;
-
-const StyledButton = styled(Button)`
-    color: ${({ theme }) => theme.TYPE_WHITE};
-    gap: 6px;
-    background-color: ${({ theme }) => theme.TYPE_BLUE};
-    font-size: ${variables.FONT_SIZE.SMALL};
-    padding: 6px 10px;
-
-    ${variables.SCREEN_QUERY.MOBILE} {
-        width: 100%;
-    }
-
-    :hover,
-    :focus {
-        background-color: ${({ theme }) => darken(0.05, theme.TYPE_BLUE)};
-    }
+    color: ${({ theme }) => theme.textSubdued};
 `;
 
 interface AnalyzeInExplorerBannerProps {
@@ -57,31 +28,30 @@ interface AnalyzeInExplorerBannerProps {
 }
 
 export const AnalyzeInExplorerBanner = ({ txid }: AnalyzeInExplorerBannerProps) => {
-    const theme = useTheme();
-
     const { selectedAccount } = useSelector(state => state.wallet);
     const { network } = selectedAccount;
     const explorerUrl = network?.explorer.tx;
 
     return (
-        <StyledWarning variant="info">
-            <Wrapper>
-                <Icon icon="CUBE" size={24} color={theme.TYPE_BLUE} />
-                <TextWrapper>
-                    <Heading>
-                        <Translation id="TR_ANALYZE_IN_BLOCKBOOK" />
-                    </Heading>
-                    <Description>
-                        <Translation id="TR_ANALYZE_IN_BLOCKBOOK_DESC" />
-                    </Description>
-                </TextWrapper>
-            </Wrapper>
-            <Link variant="nostyle" href={`${explorerUrl}${txid}`}>
-                <StyledButton variant="primary">
-                    <Translation id="TR_ANALYZE_IN_BLOCKBOOK_OPEN" />
-                    <Icon icon="EXTERNAL_LINK" color={theme.TYPE_WHITE} size={20} />
-                </StyledButton>
-            </Link>
-        </StyledWarning>
+        <NotificationCard
+            variant="info"
+            icon="CUBE"
+            button={{
+                href: `${explorerUrl}${txid}`,
+                children: <Translation id="TR_ANALYZE_IN_BLOCKBOOK_OPEN" />,
+                icon: 'EXTERNAL_LINK',
+                iconAlignment: 'right',
+                size: 'small',
+            }}
+        >
+            <TextWrapper>
+                <Heading>
+                    <Translation id="TR_ANALYZE_IN_BLOCKBOOK" />
+                </Heading>
+                <Description>
+                    <Translation id="TR_ANALYZE_IN_BLOCKBOOK_DESC" />
+                </Description>
+            </TextWrapper>
+        </NotificationCard>
     );
 };
