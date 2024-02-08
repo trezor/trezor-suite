ba03fac3b77732f015cc31d4947ec752fb8e4633
tomasklim <tomas.klim@gmail.com>
2024-02-09 00:39:17 +0100

    fix(suite): fetching fiat rates using eth token definitions
    (cherry picked from commit 24940b756ae1f7cd250e04c0df3d37046546cd4d)

--- suite-common/wallet-config/src/types.ts
+++ suite-common/wallet-config/src/types.ts
@@ -2,3 +2,8 @@ import { networks, NetworkSymbol } from './networksConfig';
 
 export const isNetworkSymbol = (symbol: NetworkSymbol | string): symbol is NetworkSymbol =>
     symbol in networks;
+
+export const isEthereumBasedNetwork = (
+    network: (typeof networks)[NetworkSymbol],
+): network is (typeof networks)[NetworkSymbol] & { chainId: number } =>
+    network.networkType === 'ethereum';
