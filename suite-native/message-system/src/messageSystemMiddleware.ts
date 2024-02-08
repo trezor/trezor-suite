bd63532c04b2bea8ac6fcdd9d68aaff427af1632
Petr Knetl <petr.knetl@satoshilabs.com>
2024-02-08 08:01:08 +0000

    Fix/persisted native message system (#11039)
    * fix(suite-native): persisted message system
    
    * fix(suite-native): messages validated on device actions
    
    * fix(suite-native,message-system): fix local fallback on mobile
    
    - Conditional import for some reason start using wrong base path on mobile. Regular import on top of the file fix the issue.
    - Potential downside is that local config is fully bundled in the code so it can increase bundle size on web.
    
    ---------
    
    Co-authored-by: Matej Kriz <matej@matejkriz.cz>

--- suite-native/message-system/src/messageSystemMiddleware.ts
+++ suite-native/message-system/src/messageSystemMiddleware.ts
@@ -5,18 +5,28 @@ import {
     messageSystemActions,
     categorizeMessages,
     getValidMessages,
+    selectMessageSystemConfig,
 } from '@suite-common/message-system';
-import { selectAccountsSymbols } from '@suite-common/wallet-core';
+import { deviceActions, selectAccountsSymbols, selectDevice } from '@suite-common/wallet-core';
 
-const isAnyOfMessageSystemAffectingActions = isAnyOf(messageSystemActions.fetchSuccessUpdate);
+const isAnyOfMessageSystemAffectingActions = isAnyOf(
+    messageSystemActions.fetchSuccessUpdate,
+    deviceActions.selectDevice,
+    deviceActions.connectDevice,
+);
 
 export const messageSystemMiddleware = createMiddleware((action, { next, dispatch, getState }) => {
-    if (isAnyOfMessageSystemAffectingActions(action)) {
-        const { config } = action.payload;
+    // The action has to be handled by the reducer first to apply its
+    // changes first, because this middleware expects already updated state.
+    next(action);
 
+    if (isAnyOfMessageSystemAffectingActions(action)) {
+        const config = selectMessageSystemConfig(getState());
+        const device = selectDevice(getState());
         const enabledNetworks = selectAccountsSymbols(getState());
 
         const validMessages = getValidMessages(config, {
+            device,
             settings: {
                 tor: false, // not supported in suite-native
                 enabledNetworks,
@@ -28,5 +38,5 @@ export const messageSystemMiddleware = createMiddleware((action, { next, dispatc
         dispatch(messageSystemActions.updateValidMessages(categorizedValidMessages));
     }
 
-    return next(action);
+    return action;
 });
