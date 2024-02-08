5b1050ebb59ccafe1ac13abd9e5ede9985928814
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2024-02-08 09:53:58 +0100

    chore(connect): connect-webextension to bump-version script

--- packages/connect/scripts/bump-version.ts
+++ packages/connect/scripts/bump-version.ts
@@ -28,6 +28,7 @@ const connectPaths = [
     './packages/connect/src/data/version.ts',
     './packages/connect-web/package.json',
     './packages/connect-web/src/webextension/trezor-usb-permissions.js',
+    './packages/connect-webextension/package.json',
 ];
 
 const cmd = ['bump', semver];
