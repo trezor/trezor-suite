935c9d244dcf34d5cd0384c8b23bc1ab26dc043b
Daniel Suchy <suchydan@gmail.com>
2024-02-13 18:30:59 +0100

    chore: remove outdated comment from TypedEmitter

--- packages/utils/src/typedEventEmitter.ts
+++ packages/utils/src/typedEventEmitter.ts
@@ -1,22 +1,3 @@
-/*
-
-This file should not be exported from index to prevent missing dependency/polyfill error.
-EventEmitter is accessible in nodejs but requires polyfills in web builds.
-
-use:
-import { TypedEmitter } from '@trezor/utils';
-
-example:
-type EventMap = {
-    obj: { id: string };
-    primitive: boolean | number | string | symbol;
-    noArgs: undefined;
-    multipleArgs: (a: number, b: string, c: boolean) => void;
-    [type: `dynamic/${string}`]: boolean;
-};
-
-*/
-
 import { EventEmitter } from 'events';
 
 type EventMap = Record<string, any>;
@@ -25,13 +6,14 @@ type EventKey<T extends EventMap> = string & keyof T;
 type IsUnion<T, U extends T = T> = T extends unknown ? ([U] extends [T] ? 0 : 1) : 2;
 
 // NOTE: case 1. looks like case 4. but works differently. the order matters
-type EventReceiver<T> = IsUnion<T> extends 1
-    ? (event: T) => void // 1. use union payload
-    : T extends (...args: any[]) => any
-    ? T // 2. use custom callback
-    : T extends undefined
-    ? () => void // 3. enforce empty params
-    : (event: T) => void; // 4. default
+type EventReceiver<T> =
+    IsUnion<T> extends 1
+        ? (event: T) => void // 1. use union payload
+        : T extends (...args: any[]) => any
+          ? T // 2. use custom callback
+          : T extends undefined
+            ? () => void // 3. enforce empty params
+            : (event: T) => void; // 4. default
 
 export interface TypedEmitter<T extends EventMap> {
     on<K extends EventKey<T>>(eventName: K, fn: EventReceiver<T[K]>): this;
