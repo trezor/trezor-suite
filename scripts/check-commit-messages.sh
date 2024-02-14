b738dc2c16493a985d8ef89729d8b0132166a8af
Jan Komarek <jan.komarek@satoshilabs.com>
2024-02-14 13:50:01 +0100

    ci: create custom fixup commit check
    - The action did not work when there was a merge commit.

--- scripts/check-commit-messages.sh
+++ scripts/check-commit-messages.sh
@@ -3,13 +3,26 @@ for commit in $(git rev-list origin/HEAD..HEAD); do
 
     commit_msg=$(git log --format=%B -n 1 "$commit")
 
-    # Skip validation in case of fixup and revert commits
-    if echo "$commit_msg" | grep -qE "^(Revert|fixup! )"; then
+    # Skip validation in case of revert commits
+    if echo "$commit_msg" | grep -qE "^Revert"; then
     continue
     fi
 
+    # Check for fixup commits
+    if echo "$commit_msg" | grep -qE "^fixup! "; then
+    tput setaf 1
+    echo -e "Fixup commit validation failed for commit $commit:\n$commit_msg"
+    tput sgr0
+    echo -e "To squash fixup commits, run:\ngit rebase -i --autosquash origin/HEAD"
+    exit 1
+    fi
+
+    # Check commit message syntax
     if ! echo "$commit_msg" | grep -qE "^(build|ci|docs|feat|fix|perf|refactor|style|test|chore|revert)(\([a-z, -]+\))?: "; then
-    echo "Conventional Commits validation failed for commit $commit: $commit_msg"
+    tput setaf 1
+    echo -e "Conventional Commits validation failed for commit $commit:\n$commit_msg"
+    tput sgr0
+    echo "Learn more about Conventional Commits at https://www.conventionalcommits.org"
     exit 1
     fi
 done
