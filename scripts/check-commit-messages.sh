7eea75358a0f680696159ee8147eedeb440820fc
Jan Komarek <jan.komarek@satoshilabs.com>
2024-02-20 20:19:24 +0100

    chore(ci): fix commit message check script for CI

--- scripts/check-commit-messages.sh
+++ scripts/check-commit-messages.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
-for commit in $(git rev-list origin/HEAD..HEAD); do
+
+# This script is run by commit-messages-check github action
+
+if ! git rev-list origin/"$BASE_BRANCH_NAME"..HEAD > /dev/null 2>&1; then
+  tput -T linux setaf 1
+  echo "git rev-list command failed"
+  tput -T linux sgr0
+  exit 1
+fi
+
+for commit in $(git rev-list origin/"$BASE_BRANCH_NAME"..HEAD); do
 
     commit_msg=$(git log --format=%B -n 1 "$commit")
 
@@ -10,18 +20,18 @@ for commit in $(git rev-list origin/HEAD..HEAD); do
 
     # Check for fixup commits
     if echo "$commit_msg" | grep -qE "^fixup! "; then
-    tput setaf 1
-    echo -e "Fixup commit validation failed for commit $commit:\n$commit_msg"
-    tput sgr0
+    tput -T linux setaf 1
+    echo "Fixup commit validation failed for commit $commit: $commit_msg"
+    tput -T linux sgr0
     echo -e "To squash fixup commits, run:\ngit rebase -i --autosquash origin/HEAD"
     exit 1
     fi
 
     # Check commit message syntax
     if ! echo "$commit_msg" | grep -qE "^(build|ci|docs|feat|fix|perf|refactor|style|test|chore|revert)(\([a-z, -]+\))?: "; then
-    tput setaf 1
-    echo -e "Conventional Commits validation failed for commit $commit:\n$commit_msg"
-    tput sgr0
+    tput -T linux setaf 1
+    echo "Conventional Commits validation failed for commit $commit: $commit_msg"
+    tput -T linux sgr0
     echo "Learn more about Conventional Commits at https://www.conventionalcommits.org"
     exit 1
     fi
