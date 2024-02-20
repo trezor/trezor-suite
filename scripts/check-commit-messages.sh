bbe0a3ab18b79ef2a0f2bb7c3b535dd9bc1f6928
Jan Komarek <jan.komarek@satoshilabs.com>
2024-02-20 15:57:29 +0100

    fix: wip 6

--- scripts/check-commit-messages.sh
+++ scripts/check-commit-messages.sh
@@ -2,12 +2,14 @@
 
 branch_name=$(echo ${GITHUB_REF#refs/heads/})
 echo "test: $branch_name"
+echo "CURRENT_BRANCH_NAME: $CURRENT_BRANCH_NAME"
+echo "BASE_BRANCH_NAME: $BASE_BRANCH_NAME"
 
 echo "Current branch: $(git rev-parse --abbrev-ref HEAD)"
 
 echo echo "Remote branch: $(git rev-parse --abbrev-ref origin/HEAD)"
 
-for commit in $(git rev-list origin/develop..HEAD); do
+for commit in $(git rev-list $BASE_BRANCH_NAME..$CURRENT_BRANCH_NAME); do
 
     commit_msg=$(git log --format=%B -n 1 "$commit")
 
