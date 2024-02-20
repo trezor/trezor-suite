889307dc48714ba69ae5428007b7152b24b808cf
Jan Komarek <jan.komarek@satoshilabs.com>
2024-02-20 16:44:13 +0100

    fix: wip 12

--- scripts/check-commit-messages.sh
+++ scripts/check-commit-messages.sh
@@ -15,7 +15,7 @@ for commit in $(git rev-list $BASE_BRANCH_NAME..$CURRENT_BRANCH_NAME); do
 
     # Check for fixup commits
     if echo "$commit_msg" | grep -qE "^fixup! "; then
-    tput setaf 1
+    tput -T linux setaf 1
     echo -e "Fixup commit validation failed for commit $commit:\n$commit_msg"
     tput sgr0
     echo -e "To squash fixup commits, run:\ngit rebase -i --autosquash origin/HEAD"
@@ -24,7 +24,7 @@ for commit in $(git rev-list $BASE_BRANCH_NAME..$CURRENT_BRANCH_NAME); do
 
     # Check commit message syntax
     if ! echo "$commit_msg" | grep -qE "^(build|ci|docs|feat|fix|perf|refactor|style|test|chore|revert)(\([a-z, -]+\))?: "; then
-    tput setaf 1
+    tput -T linux setaf 1
     echo -e "Conventional Commits validation failed for commit $commit:\n$commit_msg"
     tput sgr0
     echo "Learn more about Conventional Commits at https://www.conventionalcommits.org"
