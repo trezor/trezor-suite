3576d5a4f0130bdc6948703e4cfc28cc0395e6b2
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2024-02-21 14:15:15 +0100

    chore(docker): add TREZOR_CONNECT_SRC variable

--- docker/docker-connect-popup-test.sh
+++ docker/docker-connect-popup-test.sh
@@ -5,5 +5,6 @@ xhost +
 LOCAL_USER_ID="$(id -u "$USER")"
 export LOCAL_USER_ID
 export TEST_FILE=$1
+export TREZOR_CONNECT_SRC=http://localhost:8088/
 
 docker-compose -f ./docker/docker-compose.connect-popup-test.yml up --build --abort-on-container-exit
