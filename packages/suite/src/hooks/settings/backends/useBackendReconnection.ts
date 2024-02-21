291bccbb8465b73b49c848df7a03fe63f956a29a
Marek Polak <marek.polak@satoshilabs.com>
2024-02-21 13:05:55 +0100

    feat(suite): create useBackendReconnection hook

---
+++ packages/suite/src/hooks/settings/backends/useBackendReconnection.ts
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+import { reconnectBlockchainThunk } from '@suite-common/wallet-core';
+
+import { useDispatch } from 'src/hooks/suite';
+import { NetworkSymbol } from 'src/types/wallet';
+
+export const useBackendReconnection = (
+    coin: NetworkSymbol,
+    identity?: string,
+    resolveTime?: number,
+) => {
+    const [progress, setProgress] = useState(false);
+    const [time, setTime] = useState<number>();
+
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        if (!resolveTime) return;
+        const interval = setInterval(() => {
+            const secToResolve = Math.round((resolveTime - new Date().getTime()) / 1000);
+            setTime(secToResolve);
+        }, 500);
+
+        return () => {
+            clearInterval(interval);
+        };
+    }, [resolveTime]);
+
+    const reconnect = async () => {
+        setProgress(true);
+        const r: any = await dispatch(reconnectBlockchainThunk({ coin, identity }));
+        if (!r.success) {
+            setProgress(false);
+        }
+    };
+
+    const isResolving = typeof time === 'number' && time <= 0;
+    const countdownSeconds = !isResolving ? time : undefined;
+    const isReconnecting = progress || isResolving;
+
+    return { reconnect, isReconnecting, countdownSeconds };
+};
