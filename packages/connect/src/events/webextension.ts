2285138f536595c8c130d7b046b34a99b506824f
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2024-02-21 14:06:41 +0100

    feat(connect): add CHANNEL_HANDSHAKE_CONFIRM to webextension events

--- packages/connect/src/events/webextension.ts
+++ packages/connect/src/events/webextension.ts
@@ -3,4 +3,5 @@ export const WEBEXTENSION = {
     USB_PERMISSIONS_INIT: 'usb-permissions-init',
     USB_PERMISSIONS_CLOSE: 'usb-permissions-close',
     USB_PERMISSIONS_FINISHED: 'usb-permissions-finished',
+    CHANNEL_HANDSHAKE_CONFIRM: 'channel-handshake-confirm',
 };
