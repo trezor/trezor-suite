b1356c8380c24a7c6cfcf4812b1c386f338146c3
Jan Václavík <jvaclavik@gmail.com>
2024-03-01 12:05:35 +0100

    refactor(suite): Use Box instead of custom styled Card (#11264)

--- packages/suite/src/views/settings/SettingsCoins/FirmwareTypeSuggestion.tsx
+++ packages/suite/src/views/settings/SettingsCoins/FirmwareTypeSuggestion.tsx
@@ -5,17 +5,16 @@ import { goto } from 'src/actions/suite/routerActions';
 import { Translation } from 'src/components/suite';
 import { SettingsAnchor } from 'src/constants/suite/anchors';
 import { useDevice, useDispatch } from 'src/hooks/suite';
-import { Button, Card } from '@trezor/components';
+import { Box, Button } from '@trezor/components';
 import { hasBitcoinOnlyFirmware } from '@trezor/device-utils';
 import { spacingsPx, typography } from '@trezor/theme';
 
-const StyledCard = styled(Card)`
+const Row = styled.div`
+    display: flex;
+    flex: 1;
     align-items: center;
-    border-left: ${spacingsPx.xs} solid ${({ theme }) => theme.borderSecondary};
-    box-shadow: 0 2px 5px 0 ${({ theme }) => theme.boxShadowBase};
-    flex-direction: row;
     justify-content: space-between;
-    margin-bottom: ${spacingsPx.lg};
+    gap: ${spacingsPx.sm};
 `;
 
 const StyledButton = styled(Button)`
@@ -27,7 +26,7 @@ const Description = styled.div`
     color: ${({ theme }) => theme.textSubdued};
 `;
 
-export const FirmwareTypeSuggestion = () => {
+const FirmwareTypeSuggestionDescription = () => {
     const dispatch = useDispatch();
     const { device } = useDevice();
 
@@ -35,29 +34,40 @@ export const FirmwareTypeSuggestion = () => {
         ? 'TR_SETTINGS_COINS_REGULAR_FIRMWARE_SUGGESTION'
         : 'TR_SETTINGS_COINS_BITCOIN_ONLY_FIRMWARE_SUGGESTION';
 
-    const handleClose = () => dispatch(setFlag('firmwareTypeBannerClosed', true));
     const goToFirmwareType = () =>
         dispatch(goto('settings-device', { anchor: SettingsAnchor.FirmwareType }));
 
     return (
-        <StyledCard>
-            <Description>
-                <Translation
-                    id={translationId}
-                    values={{
-                        button: chunks => (
-                            <StyledButton variant="tertiary" size="tiny" onClick={goToFirmwareType}>
-                                {chunks}
-                            </StyledButton>
-                        ),
-                        bitcoinOnly: <Translation id="TR_FIRMWARE_TYPE_BITCOIN_ONLY" />,
-                        regular: <Translation id="TR_FIRMWARE_TYPE_REGULAR" />,
-                    }}
-                />
-            </Description>
-            <Button variant="tertiary" size="small" onClick={handleClose}>
-                <Translation id="TR_GOT_IT" />
-            </Button>
-        </StyledCard>
+        <Description>
+            <Translation
+                id={translationId}
+                values={{
+                    button: chunks => (
+                        <StyledButton variant="tertiary" size="tiny" onClick={goToFirmwareType}>
+                            {chunks}
+                        </StyledButton>
+                    ),
+                    bitcoinOnly: <Translation id="TR_FIRMWARE_TYPE_BITCOIN_ONLY" />,
+                    regular: <Translation id="TR_FIRMWARE_TYPE_REGULAR" />,
+                }}
+            />
+        </Description>
+    );
+};
+
+export const FirmwareTypeSuggestion = () => {
+    const dispatch = useDispatch();
+
+    const handleClose = () => dispatch(setFlag('firmwareTypeBannerClosed', true));
+
+    return (
+        <Box variant="primary" margin={{ bottom: 20 }}>
+            <Row>
+                <FirmwareTypeSuggestionDescription />
+                <Button variant="tertiary" size="small" onClick={handleClose}>
+                    <Translation id="TR_GOT_IT" />
+                </Button>
+            </Row>
+        </Box>
     );
 };
