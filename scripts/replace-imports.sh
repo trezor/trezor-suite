661a668b0f697d86011a0ed4a488ef5c726f0a99
Nodonisko <suchydan@gmail.com>
2024-03-05 14:51:25 +0100

    fix(repo): fixed wrong base url

--- scripts/replace-imports.sh
+++ scripts/replace-imports.sh
@@ -4,4 +4,14 @@ set -euxo pipefail
 
 echo "replacing imports in $1"
 
-find "$1" -type f -exec sed -i "s/import(\"packages/import(\"@trezor/g" {} +
+# Determine the operating system
+OS="$(uname)"
+
+# Execute the appropriate command based on the OS
+if [[ "$OS" == "Darwin" ]]; then
+    # macOS command with -i '' for in-place editing without backup and -E for extended regex
+    find "$1" -type f -exec sed -i '' -E "s/import\(\"@trezor\/([^/]+)\/src/import(\"@trezor\/\1\/lib/g" {} +
+else
+    # Linux command with -i for in-place editing without backup (GNU sed syntax)
+    find "$1" -type f -exec sed -i "s/import\(\"@trezor\/([^/]+)\/src/import(\"@trezor\/\1\/lib/g" {} +
+fi
