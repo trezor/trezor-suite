09437d92cfac01c2d46dad0b5e2545e413ca9df1
Jan Komarek <jan.komarek@satoshilabs.com>
2024-03-05 13:35:39 +0100

    chore(suite-data): update tor to 0.4.8.10

--- packages/suite-data/files/bin/tor/update.sh
+++ packages/suite-data/files/bin/tor/update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-CRX_VER=1_0_34
-CRX_LINUX_ARM_VER=1_0_3
+CRX_VER=1_0_35
+CRX_LINUX_ARM_VER=1_0_4
 
 # check whether we have all required commands
 for cmd in 7z curl lipo shasum ; do
