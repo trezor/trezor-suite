b81554677c4365cece5de12a3e4399c8b431437e
Nodonisko <suchydan@gmail.com>
2024-03-06 09:31:28 +0100

    fix: replace imports on Linux

--- scripts/replace-imports.sh
+++ scripts/replace-imports.sh
@@ -2,7 +2,7 @@
 
 set -euxo pipefail
 
-echo "Replacing imports from @trezor/*/src to @trezor/*/lib in folder $1"
+REGEX="s/@trezor\/([^/]+)\/src/@trezor\/\1\/lib/g"
 
 # Determine the operating system
 OS="$(uname)"
@@ -10,10 +10,8 @@ OS="$(uname)"
 # Execute the appropriate command based on the OS
 if [[ "$OS" == "Darwin" ]]; then
     # macOS command with -i '' for in-place editing without backup and -E for extended regex
-    # Removes the "import(" part from the search pattern and replaces @trezor/*/src with @trezor/*/lib
-    find "$1" -type f -exec sed -i '' -E "s/@trezor\/([^/]+)\/src/@trezor\/\1\/lib/g" {} +
+    find "$1" -type f -exec sed -i '' -E "$REGEX" {} +
 else
-    # Linux command with -i for in-place editing without backup (GNU sed syntax)
-    # Similar adjustment as for macOS
-    find "$1" -type f -exec sed -i "s/@trezor\/([^/]+)\/src/@trezor\/\1\/lib/g" {} +
+    # Linux command with -i and -E for in-place editing without backup (GNU sed syntax) and extended regex
+    find "$1" -type f -exec sed -i -E "$REGEX" {} +
 fi
