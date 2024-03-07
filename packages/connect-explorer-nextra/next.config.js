52c101eec63af768c1ba1568b2f4c378a93c2859
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2024-03-07 16:53:39 +0100

    feat(connect-explorer-nextra): pass env variables with webpack

--- packages/connect-explorer-nextra/next.config.js
+++ packages/connect-explorer-nextra/next.config.js
@@ -3,7 +3,11 @@ const withNextra = require('nextra')({
     themeConfig: './theme.config.tsx',
 });
 
+const commitHash = require('child_process').execSync('git rev-parse HEAD').toString().trim();
+
 module.exports = withNextra({
+    basePath: process.env.CONNECT_EXPLORER_BASE_PATH,
+    assetPrefix: process.env.CONNECT_EXPLORER_ASSET_PREFIX,
     output: 'export',
     images: {
         unoptimized: true,
@@ -12,12 +16,22 @@ module.exports = withNextra({
     compiler: {
         styledComponents: true,
     },
-    webpack: config => {
+    webpack: (config, { webpack }) => {
         // Image loader
         config.module.rules.push({
             test: /\.(svg)$/,
             type: 'asset/resource',
         });
+
+        config.plugins.push(
+            new webpack.DefinePlugin({
+                'process.env.__TREZOR_CONNECT_SRC': JSON.stringify(
+                    process.env.__TREZOR_CONNECT_SRC,
+                ),
+                'process.env.COMMIT_HASH': JSON.stringify(commitHash),
+                'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+            }),
+        );
     },
     basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
     typescript: {
