e51dc85ab0ce46af0925b844de9a41d8d4605e7b
Peter Sanderson <dierk.deidel@protonmail.com>
2024-03-15 15:00:54 +0100

    chore(suite): attempt to use elevation for remmember wallet desing

--- packages/suite/src/views/remember-wallet/RememberWalletCard.tsx
+++ packages/suite/src/views/remember-wallet/RememberWalletCard.tsx
@@ -1,4 +1,12 @@
-import { Button, Card, Icon, Image, Text, useElevation } from '@trezor/components';
+import {
+    Button,
+    Card,
+    ElevationContext,
+    Icon,
+    Image,
+    Text,
+    useElevation,
+} from '@trezor/components';
 import { FormattedCryptoAmount, Translation } from '../../components/suite';
 import styled from 'styled-components';
 import { Elevation, borders, mapElevationToBackground, spacings, spacingsPx } from '@trezor/theme';
@@ -60,18 +68,24 @@ const DeviceItem = styled.div`
     align-items: center;
 `;
 
-const WindowGrayTop = styled.div`
+const StyledGrayTop = styled.div<{ $elevation: Elevation }>`
     margin: ${spacingsPx.xxs};
-    background-color: ${({ theme }) => theme.backgroundNeutralSubtleOnElevation1};
+    background-color: ${mapElevationToBackground};
     border-radius: ${borders.radii.xxs};
 `;
 
+const WindowGrayTop = () => {
+    const { elevation } = useElevation();
+
+    return <StyledGrayTop $elevation={elevation} />;
+};
+
 const WindowChart = styled.div``;
 
-const Top = styled.div`
+const StyledTop = styled.div<{ $elevation: Elevation }>`
     display: flex;
     flex-direction: row;
-    background-color: ${({ theme }) => theme.backgroundNeutralSubtleOnElevation1};
+    background-color: ${mapElevationToBackground};
     height: 196px;
     border-radius: ${borders.radii.sm};
     padding-top: ${spacingsPx.sm};
@@ -95,77 +109,92 @@ const ChartText = styled.div`
 const Buttons = styled.div`
     display: flex;
     gap: ${spacingsPx.sm};
-    width: : 100%;
 `;
 
-export const RememberWalletCard = () => (
-    <StyledCard>
-        <Top>
-            <MacWindow>
-                <WindowGrayTop>
-                    <DeviceItem>
-                        <SmallDeviceImage alt="Trezor" image={`TREZOR_T2T1`} />
+const Top = () => {
+    const { elevation } = useElevation();
 
-                        <DeviceDetail label="My Trezor">
-                            <DeviceConnectionText icon="LINK" variant="primary">
-                                <Translation id="TR_CONNECTED" />
-                            </DeviceConnectionText>
-                        </DeviceDetail>
-                    </DeviceItem>
-                </WindowGrayTop>
-                <WindowChart>
-                    <ChartText>
-                        <Text variant="primary" typographyStyle="label">
-                            <Translation id="TR_BALANCE" />
-                        </Text>
-                        <FormattedCryptoAmount value={'0.04223123'} symbol={'BTC'} />
-                    </ChartText>
-                    <PriceChartLine />
-                </WindowChart>
-            </MacWindow>
+    return (
+        <StyledTop $elevation={elevation}>
+            <ElevationContext baseElevation={elevation}>
+                <MacWindow>
+                    <WindowGrayTop>
+                        <DeviceItem>
+                            <SmallDeviceImage alt="Trezor" image={`TREZOR_T2T1`} />
+
+                            <DeviceDetail label="My Trezor">
+                                <DeviceConnectionText icon="LINK" variant="primary">
+                                    <Translation id="TR_CONNECTED" />
+                                </DeviceConnectionText>
+                            </DeviceDetail>
+                        </DeviceItem>
+                    </WindowGrayTop>
+                    <WindowChart>
+                        <ChartText>
+                            <Text variant="primary" typographyStyle="label">
+                                <Translation id="TR_BALANCE" />
+                            </Text>
+                            <FormattedCryptoAmount value={'0.04223123'} symbol={'BTC'} />
+                        </ChartText>
+                        <PriceChartLine />
+                    </WindowChart>
+                </MacWindow>
+            </ElevationContext>
             <LargeDeviceImage alt="Trezor" image={`TREZOR_T2T1`} />
-        </Top>
-        <Bottom>
-            <Text typographyStyle="titleSmall">
-                <Translation
-                    id="TR_REMEMBER_CARD_CALL_TO_ACTION"
-                    values={{
-                        primary: chunks => (
-                            <>
-                                <br />
-                                <Text variant="primary">{chunks}</Text>
-                            </>
-                        ),
-                    }}
-                />
-            </Text>
+        </StyledTop>
+    );
+};
 
-            <Callout>
-                <Circle>
-                    <Icon icon="LINK" size={spacings.xl} />
-                </Circle>
-                <Text variant="subdued">
+export const RememberWalletCard = () => {
+    const { elevation } = useElevation();
+
+    return (
+        <StyledCard>
+            <ElevationContext baseElevation={elevation}>
+                <Top />
+            </ElevationContext>
+            <Bottom>
+                <Text typographyStyle="titleSmall">
                     <Translation
-                        id="TR_REMEMBER_CARD_EXPLANATION"
+                        id="TR_REMEMBER_CARD_CALL_TO_ACTION"
                         values={{
-                            secondLine: chunks => (
+                            primary: chunks => (
                                 <>
                                     <br />
-                                    {chunks}
+                                    <Text variant="primary">{chunks}</Text>
                                 </>
                             ),
                         }}
                     />
                 </Text>
-            </Callout>
-            <Buttons>
-                <Button variant="primary" isFullWidth>
-                    <Translation id="TR_MODAL_REMEMBER_WALLET_YES" />
-                </Button>
-                <Button variant="tertiary" isFullWidth>
-                    <Translation id="TR_MODAL_REMEMBER_WALLET_NOT_NOW" />
-                </Button>
-            </Buttons>
-        </Bottom>
-    </StyledCard>
-);
+
+                <Callout>
+                    <Circle>
+                        <Icon icon="LINK" size={spacings.xl} />
+                    </Circle>
+                    <Text variant="subdued">
+                        <Translation
+                            id="TR_REMEMBER_CARD_EXPLANATION"
+                            values={{
+                                secondLine: chunks => (
+                                    <>
+                                        <br />
+                                        {chunks}
+                                    </>
+                                ),
+                            }}
+                        />
+                    </Text>
+                </Callout>
+                <Buttons>
+                    <Button variant="primary" isFullWidth>
+                        <Translation id="TR_MODAL_REMEMBER_WALLET_YES" />
+                    </Button>
+                    <Button variant="tertiary" isFullWidth>
+                        <Translation id="TR_MODAL_REMEMBER_WALLET_NOT_NOW" />
+                    </Button>
+                </Buttons>
+            </Bottom>
+        </StyledCard>
+    );
+};
