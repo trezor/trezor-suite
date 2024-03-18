43cf43d33098eaceabfac49aab39d69523f2ed53
Peter Sanderson <dierk.deidel@protonmail.com>
2024-03-18 15:32:06 +0100

    chore(components): reuse ExclusiveColorOrVariant type

--- packages/components/src/components/typography/Text/Text.tsx
+++ packages/components/src/components/typography/Text/Text.tsx
@@ -2,11 +2,20 @@ import styled from 'styled-components';
 import { UIVariant } from '../../../config/types';
 import { CSSColor, Color, Colors, TypographyStyle, typography } from '@trezor/theme';
 import { ReactNode } from 'react';
+import { TransientProps } from '../../../utils/transientProps';
 
 export type TextVariant = Extract<
     UIVariant,
     'primary' | 'tertiary' | 'info' | 'warning' | 'destructive'
 >;
+
+type ExclusiveColorOrVariant =
+    | { variant?: TextVariant; color?: undefined }
+    | {
+          variant?: undefined;
+          /** @deprecated Use only is case of absolute desperation. Prefer using `variant`. */
+          color?: string;
+      };
 
 const variantColorMap: Record<TextVariant, Color> = {
     primary: 'textPrimaryDefault',
@@ -17,10 +26,8 @@ const variantColorMap: Record<TextVariant, Color> = {
 };
 
 type ColorProps = {
-    $variant?: TextVariant;
     theme: Colors;
-    $color?: string;
-};
+} & TransientProps<ExclusiveColorOrVariant>;
 
 const getColorForTextVariant = ({
     $variant,
@@ -35,10 +42,8 @@ const getColorForTextVariant = ({
 };
 
 type StyledTextProps = {
-    $variant?: TextVariant;
-    $color?: string;
     $typographyStyle?: TypographyStyle;
-};
+} & ExclusiveColorOrVariant;
 
 const StyledText = styled.span<StyledTextProps>`
     color: ${getColorForTextVariant};
@@ -49,20 +54,12 @@ type TextProps = {
     children: ReactNode;
     className?: string;
     typographyStyle?: TypographyStyle;
-} & (
-    | { variant?: TextVariant; color?: undefined }
-    | {
-          variant?: undefined;
-          /** @deprecated Use only is case of absolute desperation. Prefer using `variant`. */
-          color?: string;
-      }
-);
+} & ExclusiveColorOrVariant;
 
 export const Text = ({ variant, color, children, className, typographyStyle }: TextProps) => {
     return (
         <StyledText
-            $variant={variant}
-            $color={color}
+            {...(variant !== undefined ? { $variant: variant } : { $color: color })}
             className={className}
             $typographyStyle={typographyStyle}
         >
