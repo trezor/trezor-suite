7cf1f02dc418d8638dc7150a858b488ff28d7bf7
Peter Sanderson <dierk.deidel@protonmail.com>
2024-03-18 11:26:02 +0100

    fix(connect): be more defensive in descriptor parsing

--- packages/connect/src/device/resolveDescriptorForTaproot.ts
+++ packages/connect/src/device/resolveDescriptorForTaproot.ts
@@ -11,25 +11,32 @@ export const resolveDescriptorForTaproot = ({ response, publicKey }: Params) => 
         const [xpub, checksum] = publicKey.descriptor.split('#');
 
         // This is here to keep backwards compatibility, suite and blockbooks are still using `'` over `h`
-        const [beforeOpeningSquareBracket, afterOpeningSquareBracket] = xpub.split('[');
-        const [path, afterClosingSquareBracket] = afterOpeningSquareBracket.split(']');
+        const openingSquareBracketSplit = xpub.split('[');
+        if (openingSquareBracketSplit.length === 2) {
+            const [beforeOpeningBracket, afterOpeningBracket] = openingSquareBracketSplit;
 
-        const correctedPath = path.replace(/h/g, "'"); // .replaceAll()
+            const closingSquareBracketSplit = afterOpeningBracket.split(']');
+            if (closingSquareBracketSplit.length === 2) {
+                const [path, afterClosingBracket] = closingSquareBracketSplit;
 
-        return {
-            xpub: `${beforeOpeningSquareBracket}[${correctedPath}]${afterClosingSquareBracket}`,
-            checksum,
-        };
-    } else {
-        // wrap regular xpub into bitcoind native descriptor
-        const fingerprint = Number(publicKey.root_fingerprint || 0)
-            .toString(16)
-            .padStart(8, '0');
-        const descriptorPath = `${fingerprint}${response.serializedPath.substring(1)}`;
+                const correctedPath = path.replace(/h/g, "'"); // .replaceAll()
 
-        return {
-            xpub: `tr([${descriptorPath}]${response.xpub}/<0;1>/*)`,
-            checksum: undefined,
-        };
+                return {
+                    xpub: `${beforeOpeningBracket}[${correctedPath}]${afterClosingBracket}`,
+                    checksum,
+                };
+            }
+        }
     }
+
+    // wrap regular xpub into bitcoind native descriptor
+    const fingerprint = Number(publicKey.root_fingerprint || 0)
+        .toString(16)
+        .padStart(8, '0');
+    const descriptorPath = `${fingerprint}${response.serializedPath.substring(1)}`;
+
+    return {
+        xpub: `tr([${descriptorPath}]${response.xpub}/<0;1>/*)`,
+        checksum: undefined,
+    };
 };
