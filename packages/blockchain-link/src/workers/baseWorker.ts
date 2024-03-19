3a04f0586d73d9fbcb9338bf087c66d382e0daa7
Martin Vytick Vytrhlík <vytick@gmail.com>
2024-03-19 14:12:24 +0100

    fix(blockchain-link): allow close connecting ws

--- packages/blockchain-link/src/workers/baseWorker.ts
+++ packages/blockchain-link/src/workers/baseWorker.ts
@@ -94,13 +94,17 @@ export abstract class BaseWorker<API> {
             }
 
             const endpoints = prioritizeEndpoints(urls);
-            this.connectPromise = this.connectRecursive(endpoints).then(api => {
-                this.debug('Connected');
-                this.api = api;
-                this.connectPromise = undefined;
+            this.connectPromise = this.connectRecursive(endpoints)
+                .then(api => {
+                    this.debug('Connected');
+                    this.api = api;
+                    this.connectPromise = undefined;
 
-                return api;
-            });
+                    return api;
+                })
+                .catch(error => {
+                    throw error;
+                });
         }
 
         return this.connectPromise;
