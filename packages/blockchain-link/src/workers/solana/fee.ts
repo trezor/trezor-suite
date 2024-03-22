91c0f9e9c59fc09c44fe9430f9f832ae397aa792
gabrielkerekes <gabriel.kerekes@vacuumlabs.com>
2024-03-22 11:35:18 +0100

    feat(blockchain-link): add tx simulation to Solana priority fees

--- packages/blockchain-link/src/workers/solana/fee.ts
+++ packages/blockchain-link/src/workers/solana/fee.ts
@@ -1,11 +1,25 @@
-import { Connection, Message, PublicKey } from '@solana/web3.js';
+import { Connection, Message, PublicKey, VersionedTransaction } from '@solana/web3.js';
 import BigNumber from 'bignumber.js';
 
+const COMPUTE_BUDGET_PROGRAM_ID = 'ComputeBudget111111111111111111111111111111';
 const DEFAULT_COMPUTE_UNIT_PRICE = 100_000; // micro-lamports, value taken from other wallets
 const DEFAULT_COMPUTE_UNIT_LIMIT = 50_000; // sending tokens with token account creation requires ~28K units
 
 export const getBaseFee = async (api: Connection, message: Message) => {
-    const result = await api.getFeeForMessage(message);
+    // Remove ComputeBudget instructions from the message when estimating the base fee
+    // since the exact priority fees are computed separately and getFeeForMessage also
+    // considers priority fees.
+    const messageWithoutComputeBudget = new Message({
+        header: message.header,
+        accountKeys: message.accountKeys,
+        recentBlockhash: message.recentBlockhash,
+        instructions: message.instructions.filter(
+            instruction =>
+                message.accountKeys[instruction.programIdIndex].toBase58() !==
+                COMPUTE_BUDGET_PROGRAM_ID,
+        ),
+    });
+    const result = await api.getFeeForMessage(messageWithoutComputeBudget);
     // The result can be null, for example if the transaction blockhash is invalid.
     // In this case, we should fall back to the default fee.
     if (result.value == null) {
@@ -25,6 +39,14 @@ export const getPriorityFee = async (api: Connection, message: Message) => {
         lockedWritableAccounts: affectedAccounts.map(a => new PublicKey(a)),
     });
 
+    // https://solana.com/developers/guides/advanced/how-to-request-optimal-compute#special-considerations
+    const simulationResult = await api.simulateTransaction(new VersionedTransaction(message));
+    const simulatedUnitsConsumed = simulationResult.value.unitsConsumed;
+    const computeUnitLimit =
+        simulatedUnitsConsumed != null
+            ? Math.ceil(simulatedUnitsConsumed * 1.2) // 20% buffer - 10% is recommended by the article above, but we double it just to be sure
+            : DEFAULT_COMPUTE_UNIT_LIMIT;
+
     const networkPriorityFee = recentFees.map(a => a.prioritizationFee).sort((a, b) => b - a)[
         Math.floor(recentFees.length / 4)
     ]; // 25th percentile because many 0 priority fees are expected
@@ -35,8 +57,9 @@ export const getPriorityFee = async (api: Connection, message: Message) => {
 
     const fee = computeUnitPrice
         .times(10 ** -6) // microLamports -> Lamports
-        .times(DEFAULT_COMPUTE_UNIT_LIMIT)
+        .times(computeUnitLimit)
+        .decimalPlaces(0, BigNumber.ROUND_UP)
         .toNumber();
 
-    return { computeUnitPrice, computeUnitLimit: DEFAULT_COMPUTE_UNIT_LIMIT, fee };
+    return { computeUnitPrice: computeUnitPrice.toNumber(), computeUnitLimit, fee };
 };
