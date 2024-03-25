908c6a9af5d573057ea4862810f87c47ee979044
Daniel Suchý <suchydan@gmail.com>
2024-03-25 21:15:15 +0100

    fix(mobile): fix env variables + build (#11742)
    (cherry picked from commit bdfb94a8a6db41c34cd1969f9ff1e1a171e6addf)

--- suite-native/app/babel.config.js
+++ suite-native/app/babel.config.js
@@ -1,3 +1,7 @@
+// We don't use babel-preset-expo which takes care of including environment variables in the build, so we have to include them manually in babel.config.js.
+// This could be removed once we use babel-preset-expo.
+const envVarsToInclude = Object.keys(process.env).filter(key => key.startsWith('EXPO_PUBLIC_'));
+
 module.exports = function (api) {
     api.cache(true);
 
@@ -14,6 +18,12 @@ module.exports = function (api) {
             '@babel/plugin-transform-export-namespace-from',
             ['@babel/plugin-proposal-decorators', { version: '2023-05' }],
             ['@babel/plugin-transform-class-static-block'],
+            [
+                'transform-inline-environment-variables',
+                {
+                    include: ['NODE_ENV', ...envVarsToInclude],
+                },
+            ],
             // react-native-reanimated plugin has to be listed last
             ['react-native-reanimated/plugin', { globals: ['__scanCodes'] }],
         ],
