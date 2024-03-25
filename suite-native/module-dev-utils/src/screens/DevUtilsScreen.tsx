883df751889065133a64fc6f1f19322b9cd82cb3
Daniel Suchý <suchydan@gmail.com>
2024-03-25 19:51:45 +0100

    chore(env): use env directly for debug (#11738)

--- suite-native/module-dev-utils/src/screens/DevUtilsScreen.tsx
+++ suite-native/module-dev-utils/src/screens/DevUtilsScreen.tsx
@@ -2,7 +2,7 @@ import { Alert } from 'react-native';
 
 import * as Sentry from '@sentry/react-native';
 
-import { getEnv, isDebugEnv, isDevelopOrDebugEnv, isProduction } from '@suite-native/config';
+import { isDebugEnv, isDevelopOrDebugEnv, isProduction } from '@suite-native/config';
 import { Button, Card, ListItem, VStack } from '@suite-native/atoms';
 import {
     Screen,
@@ -27,10 +27,16 @@ export const DevUtilsScreen = ({
             <Card>
                 <VStack spacing="medium">
                     {!isDebugEnv() && (
-                        <ListItem
-                            subtitle={`${getEnv()}-${getSuiteVersion()}, commit ${getCommitHash()}`}
-                            title="Build version"
-                        />
+                        <>
+                            <ListItem
+                                subtitle={`${process.env.EXPO_PUBLIC_ENVIRONMENT}-${getSuiteVersion()}, commit ${getCommitHash()}`}
+                                title="Build version"
+                            />
+                            <ListItem
+                                subtitle={process.env.EXPO_PUBLIC_JWS_PUBLIC_KEY}
+                                title="JWT"
+                            />
+                        </>
                     )}
                     {isDebugEnv() && (
                         <Button onPress={() => navigation.navigate(DevUtilsStackRoutes.Demo)}>
