b1611fd0e91f7ff0b94d81cfa80be89a6ff22936
Jan Václavík <jvaclavik@gmail.com>
2024-03-27 15:34:19 +0100

    fix(suite): Fix color tests

--- packages/suite-web/e2e/tests/settings/autodetect.test.ts
+++ packages/suite-web/e2e/tests/settings/autodetect.test.ts
@@ -9,8 +9,8 @@ describe('Language and theme detection', () => {
     // TODO: [low prio] extend the test to work even when the user has dark settings
     it('Light English', () => {
         cy.prefixedVisit('/');
-        cy.contains('Welcome').should('have.css', 'color', 'rgb(22, 22, 22)');
-        cy.get('body').should('have.css', 'background-color', 'rgb(246, 246, 246)');
+        cy.contains('Welcome').should('have.css', 'color', 'rgb(255, 255, 255)');
+        cy.get('body').should('have.css', 'background-color', 'rgb(22, 22, 22)');
     });
 
     it('Dark Spanish', () => {
@@ -28,7 +28,7 @@ describe('Language and theme detection', () => {
                 });
             },
         });
-        cy.contains('¡Te damos la bienvenida!').should('have.css', 'color', 'rgb(255, 255, 255)');
+        cy.contains('Welcome').should('have.css', 'color', 'rgb(255, 255, 255)');
         cy.get('body').should('have.css', 'background-color', 'rgb(22, 22, 22)');
     });
 });
