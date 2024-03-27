9c750cb4d171e2fa23590d025d8715830799b2e9
Petr Knetl <petr.knetl@satoshilabs.com>
2024-03-27 08:49:54 +0100

    fix(suite-native): biometric test ids

--- suite-native/module-home/src/screens/HomeScreen/components/BiometricsBottomSheet.tsx
+++ suite-native/module-home/src/screens/HomeScreen/components/BiometricsBottomSheet.tsx
@@ -159,12 +159,12 @@ export const BiometricsBottomSheet = () => {
             <Box style={applyStyle(buttonWrapperStyle)}>
                 <Button
                     colorScheme="tertiaryElevation0"
-                    data-testID="reject-biometrics"
+                    testID="reject-biometrics"
                     onPress={handleClose}
                 >
                     <Translation id="moduleHome.biometricsModal.button.later" />
                 </Button>
-                <Button data-testID="enable-biometrics" onPress={handleEnable}>
+                <Button testID="enable-biometrics" onPress={handleEnable}>
                     <Translation id="moduleHome.biometricsModal.button.enable" />
                 </Button>
             </Box>
