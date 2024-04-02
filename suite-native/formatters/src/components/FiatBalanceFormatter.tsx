3c278c2463ba765b1b6fbc5b709d5fa309a168bb
juriczech <bohdan.juricek@gmail.com>
2024-04-02 15:31:54 +0200

    fix(suite-native): spacing on graph balance

--- suite-native/formatters/src/components/FiatBalanceFormatter.tsx
+++ suite-native/formatters/src/components/FiatBalanceFormatter.tsx
@@ -13,7 +13,7 @@ type BalanceFormatterProps = FormatterProps<string | null>;
 
 const wholeNumberStyle = prepareNativeStyle(utils => ({
     flexShrink: 1,
-    marginBottom: -utils.spacings.medium,
+    marginBottom: -utils.spacings.small,
     textAlign: 'center',
 }));
 
