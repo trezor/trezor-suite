0064c43acd0912a4cbefac5e1079e0655e23948a
Hilda Stastna <hstastna@redhat.com>
2024-04-02 12:21:34 +0200

    feat(suite): Implement ResizableBox component

---
+++ packages/components/src/components/ResizableBox/ResizableBoxExamples.stories.tsx
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+import { Meta, StoryObj } from '@storybook/react';
+import { ResizableBox } from './ResizableBox';
+
+const Container = styled.div`
+    position: relative;
+    width: 100%;
+    height: 300px;
+`;
+const Wrapper = styled.div`
+    bottom: 0;
+    right: 0;
+    position: absolute;
+`;
+
+const Content = styled.div<{ $color: string }>`
+    background: ${({ $color }) => $color};
+    padding: 10px;
+    border-radius: 8px;
+    font-weight: 900;
+    width: 100%;
+    height: 100%;
+`;
+
+const meta: Meta = {
+    title: 'Misc/ResizableBox',
+} as Meta;
+export default meta;
+
+export const ResizableBoxExamples: StoryObj = {
+    render: () => (
+        <Container>
+            <ResizableBox
+                directions={['bottom', 'right']}
+                width={200}
+                height={100}
+                maxWidth={400}
+                maxHeight={300}
+            >
+                <Content $color="green">Resize me from bottom and/or right</Content>
+            </ResizableBox>
+
+            <Wrapper>
+                <ResizableBox
+                    directions={['top', 'left']}
+                    width={200}
+                    height={100}
+                    maxWidth={400}
+                    maxHeight={300}
+                >
+                    <Content $color="salmon">Resize me from top and/or left</Content>
+                </ResizableBox>
+            </Wrapper>
+        </Container>
+    ),
+};
