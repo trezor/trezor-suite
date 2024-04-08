2c42dc5c5ff804d600f3bedec2c75bbec052046a
Tomas Martykan <t.martykan@gmail.com>
2024-04-08 14:47:38 +0200

    fix(protobuf): clean up type of `backup_type` in schema

--- packages/connect/src/types/api/resetDevice.ts
+++ packages/connect/src/types/api/resetDevice.ts
@@ -2,16 +2,7 @@
  * Performs device setup and generates a new seed.
  */
 
-import { Static, Type } from '@trezor/schema-utils';
 import { PROTO } from '../../constants';
 import type { Params, Response } from '../params';
 
-export type ResetDevice = Static<typeof ResetDevice>;
-export const ResetDevice = Type.Composite([
-    PROTO.ResetDevice,
-    Type.Object({
-        backup_type: Type.Optional(Type.Union([Type.Literal(0), Type.Literal(1)])),
-    }),
-]);
-
-export declare function resetDevice(params: Params<ResetDevice>): Response<PROTO.Success>;
+export declare function resetDevice(params: Params<PROTO.ResetDevice>): Response<PROTO.Success>;
