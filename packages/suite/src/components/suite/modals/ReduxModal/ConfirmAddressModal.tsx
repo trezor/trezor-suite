bd41d8c274550072f20cd81464ddee89fba09475
Martin Homola <adder10@gmail.com>
2024-04-09 16:37:36 +0200

    fix(suite): fix exchange currency

--- packages/suite/src/components/suite/modals/ReduxModal/ConfirmAddressModal.tsx
+++ packages/suite/src/components/suite/modals/ReduxModal/ConfirmAddressModal.tsx
@@ -5,13 +5,23 @@ import { Translation } from 'src/components/suite';
 import { useDisplayMode, useSelector } from 'src/hooks/suite';
 import { selectSelectedAccount } from 'src/reducers/wallet/selectedAccountReducer';
 import { ConfirmValueModal, ConfirmValueModalProps } from './ConfirmValueModal/ConfirmValueModal';
+import {
+    cryptoToCoinSymbol,
+    cryptoToNetworkSymbol,
+} from 'src/utils/wallet/coinmarket/cryptoSymbolUtils';
 
 interface ConfirmAddressModalProps
     extends Pick<ConfirmValueModalProps, 'isConfirmed' | 'onCancel' | 'value'> {
     addressPath: string;
+    receiveSymbol?: CryptoSymbol;
 }
 
-export const ConfirmAddressModal = ({ addressPath, value, ...props }: ConfirmAddressModalProps) => {
+export const ConfirmAddressModal = ({
+    addressPath,
+    value,
+    receiveSymbol,
+    ...props
+}: ConfirmAddressModalProps) => {
     const account = useSelector(selectSelectedAccount);
     const displayMode = useDisplayMode();
 
@@ -22,13 +32,29 @@ export const ConfirmAddressModal = ({ addressPath, value, ...props }: ConfirmAdd
 
     if (!account) return null;
 
-    return (
-        <ConfirmValueModal
-            account={account}
-            heading={
+    const getHeading = () => {
+        if (receiveSymbol) {
+            const coinSymbol = cryptoToCoinSymbol(receiveSymbol).toUpperCase();
+            const networkSymbol = cryptoToNetworkSymbol(receiveSymbol)?.toUpperCase();
+
+            if (networkSymbol && coinSymbol !== networkSymbol) {
+                return (
+                    <Translation
+                        id="TR_ADDRESS_MODAL_TITLE_EXCHANGE"
+                        values={{
+                            networkName: networkSymbol,
+                            networkCurrencyName: coinSymbol,
+                        }}
+                    />
+                );
+            }
+
+            return (
                 <Translation
                     id="TR_ADDRESS_MODAL_TITLE"
-                    values={{ networkName: account.symbol.toUpperCase() }}
+                    values={{
+                        networkName: coinSymbol,
+                    }}
                 />
             }
             stepLabel={<Translation id="TR_RECEIVE_ADDRESS" />}
