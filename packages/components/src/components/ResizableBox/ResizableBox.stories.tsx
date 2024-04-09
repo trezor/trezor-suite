9fa7c68b1965b3efb60a9c5abc9ab93121df0672
Hilda Stastna <hstastna@redhat.com>
2024-04-09 02:27:03 +0200

    feat(suite): Implement ResizableBox component - 1st updates
    Related Issue:
    https://github.com/trezor/trezor-suite/issues/11681

--- packages/components/src/components/ResizableBox/ResizableBox.stories.tsx
+++ packages/components/src/components/ResizableBox/ResizableBox.stories.tsx
@@ -2,18 +2,32 @@ import styled from 'styled-components';
 import { Meta, StoryObj } from '@storybook/react';
 import { ResizableBoxProps, ResizableBox as BoxComponent } from './ResizableBox';
 
+const Wrapper1 = styled.div`
+    position: absolute;
+    right: 0;
+`;
+
 const Content = styled.div`
-    width: 400px;
     border: 1px solid black;
+    width: 100%;
     height: 100%;
 `;
 
 const meta: Meta = {
-    title: 'Misc/Box/ResizableBox',
-    component: BoxComponent,
+    title: 'Misc/ResizableBox',
+    component: props => ( // TODO finish adding multiple boxes!
+        <>
+            <BoxComponent {...props} />
+            {/* <Wrapper1>
+                <BoxComponent {...props} />
+            </Wrapper1> */}
+        </>
+    ),
 } as Meta;
 export default meta;
 
+// TODO make sure it works also for dark mode!!!
+// TODO make sure it's possible to use multiple instances on the same page and independently!!!
 export const ResizableBox: StoryObj<ResizableBoxProps> = {
     args: {
         children: <Content>Some content for the resizable box</Content>,
