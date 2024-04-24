923ca2763306d9d5002cdddd5b6d42260a3240cc
Marek Polak <marek.polak@satoshilabs.com>
2024-04-24 15:41:35 +0200

    refactor(connect): noBackupConfirmation improved

--- packages/connect/src/api/getAddress.ts
+++ packages/connect/src/api/getAddress.ts
@@ -22,6 +22,7 @@ export default class GetAddress extends AbstractMethod<'getAddress', Params[]> {
     confirmed?: boolean;
 
     init() {
+        this.noBackupConfirmationMode = 'always';
         this.requiredPermissions = ['read'];
 
         // create a bundle with only one batch if bundle doesn't exists
@@ -130,25 +131,6 @@ export default class GetAddress extends AbstractMethod<'getAddress', Params[]> {
         this.confirmed = uiResp.payload;
 
         return this.confirmed;
-    }
-
-    async noBackupConfirmation() {
-        // wait for popup window
-        await this.getPopupPromise().promise;
-        // initialize user response promise
-        const uiPromise = this.createUiPromise(UI.RECEIVE_CONFIRMATION);
-
-        // request confirmation view
-        this.postMessage(
-            createUiMessage(UI.REQUEST_CONFIRMATION, {
-                view: 'no-backup',
-            }),
-        );
-
-        // wait for user action
-        const uiResp = await uiPromise.promise;
-
-        return uiResp.payload;
     }
 
     async _call({
