b67ade2551a09473beaee781a1f8b6d7cd75f7fd
Martin Varmuza <mvarmuza@gmail.com>
2024-04-29 18:35:28 +0200

    test(connect-popup): exnted e2e timeout

--- packages/connect-popup/e2e/playwright.config.ts
+++ packages/connect-popup/e2e/playwright.config.ts
@@ -4,7 +4,8 @@ const config: PlaywrightTestConfig = {
     testDir: 'tests',
     retries: 0,
     workers: 1, // to disable parallelism between test files
-    timeout: 40000,
+    // loading core.js takes very long (in webextension). last time I checked it war 3mb. This is some development build or what?
+    timeout: 60000,
     use: {
         headless: process.env.HEADLESS === 'true',
         ignoreHTTPSErrors: true,
