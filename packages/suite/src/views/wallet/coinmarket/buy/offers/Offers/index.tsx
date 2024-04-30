0ece664476faded10f152a21e033f20b05784193
Martin Homola <adder10@gmail.com>
2024-04-30 15:59:37 +0200

    feat(coinmarket): delete alternative buy offers

--- packages/suite/src/views/wallet/coinmarket/buy/offers/Offers/index.tsx
+++ packages/suite/src/views/wallet/coinmarket/buy/offers/Offers/index.tsx
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 import { CoinmarketFooter, NoOffers } from 'src/views/wallet/coinmarket/common';
 import { variables } from '@trezor/components';
-import { Translation } from 'src/components/suite';
 import { useLayout } from 'src/hooks/suite';
 import { useCoinmarketBuyOffersContext } from 'src/hooks/wallet/useCoinmarketBuyOffers';
 import { useCoinmarketNavigation } from 'src/hooks/wallet/useCoinmarketNavigation';
@@ -22,84 +21,29 @@ const Wrapper = styled.div`
     }
 `;
 
-const Divider = styled.div`
-    display: flex;
-    flex: 1;
-    align-items: center;
-    cursor: default;
-    padding: 61px 0;
-`;
-
-const DividerLine = styled.div`
-    height: 1px;
-    flex: 1;
-    background: ${({ theme }) => theme.STROKE_GREY};
-`;
-
-const DividerLeft = styled(DividerLine)``;
-const DividerRight = styled(DividerLine)``;
-
-const Currency = styled.div`
-    color: ${({ theme }) => theme.TYPE_DARK_GREY};
-    text-transform: uppercase;
-    padding-left: 3px;
-`;
-
-const DividerMiddle = styled.div`
-    display: flex;
-    align-items: center;
-    padding: 5px 20px;
-    color: ${({ theme }) => theme.TYPE_LIGHT_GREY};
-    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
-    font-size: ${variables.FONT_SIZE.SMALL};
-    border-radius: 25px;
-    border: 1px solid ${({ theme }) => theme.STROKE_GREY};
-    background: ${({ theme }) => theme.BG_WHITE};
-    text-align: center;
-`;
-
 const Offers = () => {
-    const { account, quotes, alternativeQuotes, selectedQuote, timer, getQuotes } =
-        useCoinmarketBuyOffersContext();
+    const { account, quotes, selectedQuote, timer, getQuotes } = useCoinmarketBuyOffersContext();
     const { navigateToBuyForm } = useCoinmarketNavigation(account);
 
     useLayout('Trezor Suite | Trade', () => <PageHeader backRoute="wallet-coinmarket-buy" />);
 
-    const hasLoadingFailed = !(quotes && alternativeQuotes);
-    const noOffers = hasLoadingFailed || (quotes.length === 0 && alternativeQuotes.length === 0);
+    const hasLoadingFailed = !quotes;
+    const noOffers = hasLoadingFailed || quotes.length === 0;
 
     return (
         <Wrapper>
-            {!selectedQuote && (
-                <>
-                    {noOffers ? (
-                        <NoOffers
-                            coinmarketRefreshTimeIsLoading={timer.isLoading}
-                            coinmarketRefreshTimeSeconds={timer.timeSpend.seconds}
-                            onBackButtonClick={navigateToBuyForm}
-                            onReloadOffersButtonClick={getQuotes}
-                            hasLoadingFailed={hasLoadingFailed}
-                        />
-                    ) : (
-                        <>
-                            {quotes.length > 0 && <BuyQuoteList quotes={quotes} />}
-                            {alternativeQuotes.length > 0 && (
-                                <>
-                                    <Divider>
-                                        <DividerLeft />
-                                        <DividerMiddle>
-                                            <Translation id="TR_BUY_OTHER_OFFERS_IN" />
-                                            <Currency>{alternativeQuotes[0].fiatCurrency}</Currency>
-                                        </DividerMiddle>
-                                        <DividerRight />
-                                    </Divider>
-                                    <BuyQuoteList isAlternative quotes={alternativeQuotes} />
-                                </>
-                            )}
-                        </>
-                    )}
-                </>
-            )}
+            {!selectedQuote &&
+                (noOffers ? (
+                    <NoOffers
+                        coinmarketRefreshTimeIsLoading={timer.isLoading}
+                        coinmarketRefreshTimeSeconds={timer.timeSpend.seconds}
+                        onBackButtonClick={navigateToBuyForm}
+                        onReloadOffersButtonClick={getQuotes}
+                        hasLoadingFailed={hasLoadingFailed}
+                    />
+                ) : (
+                    quotes.length > 0 && <BuyQuoteList quotes={quotes} />
+                ))}
             {selectedQuote && <SelectedOffer />}
             <CoinmarketFooter />
         </Wrapper>
