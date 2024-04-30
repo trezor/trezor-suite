ef670d2ef909b87a14d468ab4abaae031ee63390
Petr Knetl <petr.knetl@satoshilabs.com>
2024-04-30 13:56:08 +0200

    fix(suite-native): web3-validator entry point file fix

--- suite-native/app/metro.config.js
+++ suite-native/app/metro.config.js
@@ -30,6 +30,20 @@ const config = {
             zlib: nodejs.zlib,
         },
         resolveRequest: (context, moduleName, platform) => {
+            // index 0 refers to suite-native/app node_modules directory
+            const rootNodeModulesPath = context.nodeModulesPaths[1];
+
+            // web3-validator package is by default trying to use non-existing minified index file. This fixes that.
+            // Can be removed once web3-validator fixup PR is merged: https://github.com/web3/web3.js/pull/7016.
+            if (moduleName.startsWith('web3-validator')) {
+                return {
+                    filePath: require.resolve(
+                        rootNodeModulesPath + '/web3-validator/lib/commonjs/index.js',
+                    ),
+                    type: 'sourceFile',
+                };
+            }
+
             if (moduleName.startsWith('@emurgo/cardano')) {
                 // Cardano libs doesn't have main field in package.json which will cause error in metro
                 // Also they use WASM which doesn't work in RN so we polyfill it with empty file to build errors
