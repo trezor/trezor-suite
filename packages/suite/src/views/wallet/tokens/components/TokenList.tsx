6fba7ee41b541b55641d3d1678981c94b8a5c679
tomasklim <tomas.klim@gmail.com>
2024-05-08 16:08:14 +0200

    fix(suite): do not show loading state when fiat rate fetch fails

--- packages/suite/src/views/wallet/tokens/components/TokenList.tsx
+++ packages/suite/src/views/wallet/tokens/components/TokenList.tsx
@@ -195,7 +195,7 @@ export const TokenList = ({
                                                                 <LastUpdateTooltip
                                                                     timestamp={timestamp}
                                                                 >
-                                                                    <>{value}</>
+                                                                    {value}
                                                                 </LastUpdateTooltip>
                                                             ) : (
                                                                 <StyledNoRatesTooltip />
