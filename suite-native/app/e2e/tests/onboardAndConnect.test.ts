e7a555408745ae14a236c69fa5b5b120d61f49ba
Martin Vytick Vytrhlík <vytick@gmail.com>
2024-05-17 14:58:32 +0200

    chore(suite-native): longer timeout for test

--- suite-native/app/e2e/tests/onboardAndConnect.test.ts
+++ suite-native/app/e2e/tests/onboardAndConnect.test.ts
@@ -40,7 +40,7 @@ describe('Go through onboarding and connect Trezor.', () => {
 
             await waitFor(element(by.id('skip-view-only-mode')))
                 .toBeVisible()
-                .withTimeout(10000); // communication between connected Trezor and app takes some time.
+                .withTimeout(20000); // communication between connected Trezor and app takes some time.
 
             await element(by.id('skip-view-only-mode')).tap();
 
