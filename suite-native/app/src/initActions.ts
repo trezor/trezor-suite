f4408ff224e991759720931f540a569d4cd0b398
Daniel Suchý <suchydan@gmail.com>
2024-05-21 07:44:17 +0200

    fix(mobile): fix infinite loading of XRP (#12489)

--- suite-native/app/src/initActions.ts
+++ suite-native/app/src/initActions.ts
@@ -11,6 +11,7 @@ import { selectFiatCurrencyCode } from '@suite-native/settings';
 import { initMessageSystemThunk } from '@suite-common/message-system';
 import { setIsAppReady, setIsConnectInitialized } from '@suite-native/state/src/appSlice';
 import { periodicCheckTokenDefinitionsThunk } from '@suite-common/token-definitions';
+import { TransactionCacheEngine } from '@suite-common/transaction-cache-engine';
 
 let isAlreadyInitialized = false;
 
@@ -30,6 +31,8 @@ export const applicationInit = createThunk(
             dispatch(initDevices());
 
             await dispatch(connectInitThunk());
+
+            TransactionCacheEngine.init();
 
             dispatch(setIsConnectInitialized(true));
 
