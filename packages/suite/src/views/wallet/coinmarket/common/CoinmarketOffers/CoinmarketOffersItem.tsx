77105e1e3d2a6aa855373d94fa875a1c8cbb44de
Martin Homola <adder10@gmail.com>
2024-05-23 20:24:46 +0200

    feat(coinmarket): redesign - list of offers

--- packages/suite/src/views/wallet/coinmarket/common/CoinmarketOffers/CoinmarketOffersItem.tsx
+++ packages/suite/src/views/wallet/coinmarket/common/CoinmarketOffers/CoinmarketOffersItem.tsx
@@ -1,45 +1,57 @@
-import styled, { useTheme } from 'styled-components';
-
-import { Button, variables, Icon, Card, H3 } from '@trezor/components';
-import {
-    CoinmarketCryptoAmount,
-    CoinmarketFiatAmount,
-    CoinmarketPaymentType,
-    CoinmarketProviderInfo,
-    CoinmarketTag,
-} from 'src/views/wallet/coinmarket/common';
+import styled from 'styled-components';
+import { Button, variables, Card } from '@trezor/components';
 import { Translation } from 'src/components/suite';
 import { BuyTrade } from 'invity-api';
 import { useCoinmarketBuyOffersContext } from 'src/hooks/wallet/useCoinmarketBuyOffers';
+import { spacings, spacingsPx, typography } from '@trezor/theme';
+import { CoinmarketUtilsProvider } from '../CoinmarketUtils/CoinmarketUtilsProvider';
+import CoinmarketUtilsPrice from '../CoinmarketUtils/CoinmarketUtilsPrice';
 import { getTagAndInfoNote } from 'src/utils/wallet/coinmarket/coinmarketUtils';
-import { cryptoToCoinSymbol } from 'src/utils/wallet/coinmarket/cryptoSymbolUtils';
 
-const Details = styled.div`
-    display: flex;
-    min-height: 20px;
-    flex-wrap: wrap;
-    padding: 10px 30px;
-
-    @media (max-width: ${variables.SCREEN_SIZE.SM}) {
-        flex-direction: column;
-        padding: 10px 20px;
-    }
+const OfferWrap = styled(Card)`
+    min-height: 100px;
 `;
 
-const Column = styled.div`
+const Offer = styled.div`
     display: flex;
-    padding: 10px 0;
-    flex: 1;
+    height: 100px;
+`;
+
+const OfferColumn = styled.div`
+    display: flex;
     flex-direction: column;
-    justify-content: flex-start;
+    flex: none;
 `;
 
-const Heading = styled.div`
+const OfferColumn1 = styled(OfferColumn)`
+    width: 28.75%;
+    justify-content: center;
+`;
+
+const OfferColumn2 = styled(OfferColumn)`
+    width: 100%;
+    flex: auto;
+    justify-content: center;
+`;
+
+const OfferColumn3 = styled(OfferColumn)`
+    justify-content: center;
+`;
+
+const OfferProvider = styled(CoinmarketUtilsProvider)<{ $isMargined?: boolean }>`
+    ${({ $isMargined }) => ($isMargined ? 'margin-top: auto;' : '')}
+`;
+
+const OfferBadgeWrap = styled.div`
     display: flex;
-    text-transform: uppercase;
-    color: ${({ theme }) => theme.TYPE_LIGHT_GREY};
-    font-weight: ${variables.FONT_WEIGHT.DEMI_BOLD};
-    padding-bottom: 9px;
+    align-items: center;
+    width: 100%;
+`;
+
+const OfferBadgeInfo = styled.div`
+    margin-left: ${spacingsPx.xs};
+    ${typography.label};
+    color: ${({ theme }) => theme.textSubdued};
 `;
 
 const StyledButton = styled(Button)`
@@ -50,232 +62,51 @@ const StyledButton = styled(Button)`
     }
 `;
 
-const Value = styled.div`
-    display: flex;
-    align-items: center;
-    color: ${({ theme }) => theme.TYPE_DARK_GREY};
-    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
-`;
-
-const Footer = styled.div`
-    margin: 0 30px;
-    padding: 10px 0;
-    padding-top: 23px;
-    color: ${({ theme }) => theme.TYPE_LIGHT_GREY};
-    border-top: 1px solid ${({ theme }) => theme.STROKE_GREY};
-    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
-    font-size: ${variables.FONT_SIZE.SMALL};
-
-    @media (max-width: ${variables.SCREEN_SIZE.SM}) {
-        margin: 0 20px;
-    }
-`;
-
-const ErrorFooter = styled.div`
-    display: flex;
-    margin: 0 30px;
-    padding: 10px 0;
-    border-top: 1px solid ${({ theme }) => theme.STROKE_GREY};
-    color: ${({ theme }) => theme.TYPE_RED};
-
-    @media (max-width: ${variables.SCREEN_SIZE.SM}) {
-        margin: 0 20px;
-    }
-`;
-
-const StyledIcon = styled(Icon)`
-    padding-top: 8px;
-`;
-
-const IconWrapper = styled.div`
-    padding-right: 3px;
-`;
-
-const ErrorText = styled.div``;
-
-interface CoinmarketOffersItemProps {
-    className?: string;
+export interface CoinmarketOffersItemProps {
     quote: BuyTrade;
     wantCrypto: boolean;
 }
 
-export function getQuoteError(quote: BuyTrade, wantCrypto: boolean) {
-    if (quote.error) {
-        if (wantCrypto) {
-            const cryptoAmount = Number(quote.receiveStringAmount);
-            const symbol = quote.receiveCurrency;
-            if (quote.minCrypto && cryptoAmount < quote.minCrypto) {
-                return (
-                    <Translation
-                        id="TR_OFFER_ERROR_MINIMUM_CRYPTO"
-                        values={{
-                            amount: (
-                                <CoinmarketCryptoAmount
-                                    amount={cryptoAmount}
-                                    symbol={cryptoToCoinSymbol(symbol!)}
-                                />
-                            ),
-                            min: (
-                                <CoinmarketCryptoAmount
-                                    amount={quote.minCrypto}
-                                    symbol={cryptoToCoinSymbol(symbol!)}
-                                />
-                            ),
-                        }}
-                    />
-                );
-            }
-            if (quote.maxCrypto && cryptoAmount > quote.maxCrypto) {
-                return (
-                    <Translation
-                        id="TR_OFFER_ERROR_MAXIMUM_CRYPTO"
-                        values={{
-                            amount: (
-                                <CoinmarketCryptoAmount
-                                    amount={cryptoAmount}
-                                    symbol={cryptoToCoinSymbol(symbol!)}
-                                />
-                            ),
-                            max: (
-                                <CoinmarketCryptoAmount
-                                    amount={quote.maxCrypto}
-                                    symbol={cryptoToCoinSymbol(symbol!)}
-                                />
-                            ),
-                        }}
-                    />
-                );
-            }
-        } else {
-            const fiatAmount = Number(quote.fiatStringAmount);
-            if (quote.minFiat && fiatAmount < quote.minFiat) {
-                return (
-                    <Translation
-                        id="TR_OFFER_ERROR_MINIMUM_FIAT"
-                        values={{
-                            amount: (
-                                <CoinmarketFiatAmount
-                                    amount={fiatAmount}
-                                    currency={quote.fiatCurrency}
-                                />
-                            ),
-                            min: (
-                                <CoinmarketFiatAmount
-                                    amount={quote.minFiat}
-                                    currency={quote.fiatCurrency}
-                                />
-                            ),
-                        }}
-                    />
-                );
-            }
-            if (quote.maxFiat && fiatAmount > quote.maxFiat) {
-                return (
-                    <Translation
-                        id="TR_OFFER_ERROR_MAXIMUM_FIAT"
-                        values={{
-                            amount: (
-                                <CoinmarketFiatAmount
-                                    amount={fiatAmount}
-                                    currency={quote.fiatCurrency}
-                                />
-                            ),
-                            max: (
-                                <CoinmarketFiatAmount
-                                    amount={quote.maxFiat}
-                                    currency={quote.fiatCurrency}
-                                />
-                            ),
-                        }}
-                    />
-                );
-            }
-        }
-
-        return quote.error;
-    }
-
-    return '';
-}
-
-const CoinmarketOffersItem = ({ className, quote, wantCrypto }: CoinmarketOffersItemProps) => {
-    const theme = useTheme();
+const CoinmarketOffersItem = ({ quote, wantCrypto }: CoinmarketOffersItemProps) => {
     const { selectQuote, providersInfo } = useCoinmarketBuyOffersContext();
-    const { tag, infoNote } = getTagAndInfoNote(quote);
-    const { paymentMethod, paymentMethodName, exchange, error } = quote;
+    const { exchange } = quote;
+    const { tag } = getTagAndInfoNote(quote);
+    const tagsExist = !!tag;
 
     return (
-        <Card className={className}>
-            <Details>
-                <Column>
-                    <Value>
-                        {error && <H3>N/A</H3>}
-                        {!error && (
-                            <H3>
-                                {wantCrypto ? (
-                                    <CoinmarketFiatAmount
-                                        amount={quote.fiatStringAmount}
-                                        currency={quote.fiatCurrency}
-                                    />
-                                ) : (
-                                    <CoinmarketCryptoAmount
-                                        amount={quote.receiveStringAmount}
-                                        symbol={cryptoToCoinSymbol(quote.receiveCurrency!)}
-                                    />
-                                )}
-                                <CoinmarketTag tag={tag} />
-                            </H3>
-                        )}
-                    </Value>
-                </Column>
-                <Column>
-                    <Heading>
-                        <Translation id="TR_BUY_PROVIDER" />
-                    </Heading>
-                    <Value>
-                        <CoinmarketProviderInfo exchange={exchange} providers={providersInfo} />
-                    </Value>
-                </Column>
-                <Column>
-                    <Heading>
-                        <Translation id="TR_BUY_PAID_BY" />
-                    </Heading>
-                    <Value>
-                        <CoinmarketPaymentType
-                            method={paymentMethod}
-                            methodName={paymentMethodName}
-                        />
-                    </Value>
-                </Column>
-                <Column>
-                    <Value>
-                        {quote.status === 'LOGIN_REQUEST' ? (
-                            <StyledButton onClick={() => selectQuote(quote)}>
-                                <Translation id="TR_LOGIN_PROCEED" />
-                            </StyledButton>
-                        ) : (
-                            <StyledButton
-                                isDisabled={!!quote.error}
-                                onClick={() => selectQuote(quote)}
-                                data-test="@coinmarket/buy/offers/get-this-deal-button"
-                            >
-                                <Translation id="TR_BUY_GET_THIS_OFFER" />
-                            </StyledButton>
-                        )}
-                    </Value>
-                </Column>
-            </Details>
-            {error && (
-                <ErrorFooter>
-                    <IconWrapper>
-                        <StyledIcon icon="CROSS" size={12} color={theme.TYPE_RED} />
-                    </IconWrapper>
-                    <ErrorText>{getQuoteError(quote, wantCrypto)}</ErrorText>
-                </ErrorFooter>
-            )}
-
-            {infoNote && !error && <Footer>{infoNote}</Footer>}
-        </Card>
+        <OfferWrap margin={{ top: spacings.md }}>
+            <Offer>
+                <OfferColumn1>
+                    <OfferBadgeWrap>
+                        {/*<Badge variant="primary">Limited offer</Badge>*/}
+                        <OfferBadgeInfo>{/*0% fees on all buys*/}</OfferBadgeInfo>
+                    </OfferBadgeWrap>
+                    <OfferProvider
+                        exchange={exchange}
+                        providers={providersInfo}
+                        $isMargined={tagsExist}
+                    />
+                </OfferColumn1>
+                <OfferColumn2>
+                    <CoinmarketUtilsPrice quote={quote} wantCrypto={wantCrypto} />
+                </OfferColumn2>
+                <OfferColumn3>
+                    {quote.status === 'LOGIN_REQUEST' ? (
+                        <StyledButton onClick={() => selectQuote(quote)}>
+                            <Translation id="TR_LOGIN_PROCEED" />
+                        </StyledButton>
+                    ) : (
+                        <StyledButton
+                            isDisabled={!!quote.error}
+                            onClick={() => selectQuote(quote)}
+                            data-test="@coinmarket/buy/offers/get-this-deal-button"
+                        >
+                            <Translation id="TR_BUY_GET_THIS_OFFER" />
+                        </StyledButton>
+                    )}
+                </OfferColumn3>
+            </Offer>
+        </OfferWrap>
     );
 };
 
