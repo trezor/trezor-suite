26c70f48f7e7711282ffa400013c7acb744d86a4
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2024-05-23 14:59:29 +0200

    feat(connect): use full version in URL when beta

--- packages/connect/src/data/version.ts
+++ packages/connect/src/data/version.ts
@@ -2,4 +2,8 @@ export const VERSION = '9.2.4-beta.1';
 
 const versionN = VERSION.split('.').map(s => parseInt(s, 10));
 
-export const DEFAULT_DOMAIN = `https://connect.trezor.io/${versionN[0]}/`;
+const isBeta = VERSION.includes('beta');
+
+export const DEFAULT_DOMAIN = isBeta
+    ? `https://connect.trezor.io/${VERSION}/`
+    : `https://connect.trezor.io/${versionN[0]}/`;
