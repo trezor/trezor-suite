acf19a3294b44002f2f4741d9927cffe02089858
Szymon Lesisz <sz.lesisz@gmail.com>
2024-05-28 15:21:38 +0200

    chore(trezor-bridge): add api argument to bin.js file
    `node ./packages/transport-bridge/dist/bin.js udp`

--- packages/transport-bridge/src/bin.ts
+++ packages/transport-bridge/src/bin.ts
@@ -4,7 +4,7 @@ import { TrezordNode } from './http';
 
 const trezordNode = new TrezordNode({
     port: 21325,
-    api: 'usb',
+    api: process.argv.includes('udp') ? 'udp' : 'usb',
     logger: new Log('@trezor/transport-bridge', true),
 });
 
