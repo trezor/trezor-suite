f66d4c1532b8305ea3837df9cd7da59ccd9c9108
Jan Václavík <jvaclavik@gmail.com>
2024-05-29 20:28:19 +0200

    fix(suite): Add possibility of Tooltip to ButtonGroup

--- packages/components/src/components/buttons/ButtonGroup/ButtonGroup.tsx
+++ packages/components/src/components/buttons/ButtonGroup/ButtonGroup.tsx
@@ -4,13 +4,15 @@ import { borders } from '@trezor/theme';
 import { Button, ButtonProps } from '../Button/Button';
 import { ButtonSize, ButtonVariant } from '../buttonStyleUtils';
 import { IconButton, IconButtonProps } from '../IconButton/IconButton';
+import { Tooltip, TooltipProps } from '../../Tooltip/Tooltip';
 
 const Container = styled.div<{ $variant?: Exclude<ButtonVariant, 'danger'> }>`
     position: relative;
     display: flex;
     align-items: center;
 
-    > button {
+    > button,
+    div > button {
         border-radius: 0;
     }
 
@@ -39,17 +41,28 @@ const Container = styled.div<{ $variant?: Exclude<ButtonVariant, 'danger'> }>`
 `;
 
 const checkChildren = (children: Array<React.ReactNode>) =>
-    children.every(
-        child =>
-            React.isValidElement(child) && (child.type === Button || child.type === IconButton),
-    );
+    children.every(child => {
+        if (React.isValidElement(child)) {
+            if (child.type === Button || child.type === IconButton) {
+                return true;
+            }
+            if (child.type === Tooltip && React.isValidElement(child.props.children)) {
+                const tooltipChild = child.props.children;
+
+                return tooltipChild.type === Button || tooltipChild.type === IconButton;
+            }
+        }
+
+        return false;
+    });
 
 interface ButtonGroupProps {
     variant?: Exclude<ButtonVariant, 'danger'>;
     size?: ButtonSize;
     isDisabled?: boolean;
     className?: string;
-    children: React.ReactElement<ButtonProps | IconButtonProps>[];
+    children: React.ReactElement<ButtonProps | IconButtonProps | TooltipProps>[];
+    withTooltips?: boolean;
 }
 
 export const ButtonGroup = ({
@@ -63,19 +76,41 @@ export const ButtonGroup = ({
 
     if (!areChildrenValid) {
         console.error(
-            'Invalid children passed to ButtonGroup. Only Button and IconButton are allowed.',
+            'Invalid children passed to ButtonGroup. Only Button, IconButton, and Tooltip (containing Button or IconButton) are allowed.',
         );
 
         return null;
     }
 
-    const childrenWithProps = children.map(child =>
-        React.cloneElement(child, {
-            variant: child.props.variant || variant,
-            size: child.props.size || size,
-            isDisabled: child.props.isDisabled || isDisabled,
-        }),
-    );
+    const childrenWithProps = React.Children.map(children, child => {
+        if (React.isValidElement(child)) {
+            if (
+                child.type === Tooltip &&
+                React.isValidElement((child.props as TooltipProps).children)
+            ) {
+                const tooltipProps = child.props as TooltipProps;
+                const tooltipChild = tooltipProps.children as React.ReactElement;
+                const tooltipChildProps: ButtonProps | IconButtonProps = tooltipChild?.props;
+                const childWithProps = React.cloneElement(tooltipChild, {
+                    variant: tooltipChildProps.variant || variant,
+                    size: tooltipChildProps.size || size,
+                    isDisabled: tooltipChildProps.isDisabled || isDisabled,
+                });
+
+                return React.cloneElement(child, {}, childWithProps);
+            }
+
+            const childProps = child.props as ButtonProps | IconButtonProps;
+
+            return React.cloneElement(child, {
+                variant: childProps.variant || variant,
+                size: childProps.size || size,
+                isDisabled: childProps.isDisabled || isDisabled,
+            });
+        }
+
+        return child;
+    });
 
     return (
         <Container $variant={variant} className={className}>
