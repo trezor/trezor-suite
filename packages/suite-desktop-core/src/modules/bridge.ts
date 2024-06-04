90c1f72cf7f0f4c4d393c67f63bf5d3404a1a2e3
Martin Varmuza <mvarmuza@gmail.com>
2024-06-04 15:07:03 +0200

    chore(suite-desktop): change bridge logger options

--- packages/suite-desktop-core/src/modules/bridge.ts
+++ packages/suite-desktop-core/src/modules/bridge.ts
@@ -69,9 +69,18 @@ const getBridgeInstance = (store: Dependencies['store']) => {
         return new BridgeProcess();
     }
 
-    const bridgeLogger = new Logger('info', {
+    /**
+     * We need a different instance from the global logger instance. Because we want to save bridge logs to memory
+     * to make them available from bridge status page.
+     */
+    const bridgeLogger = new Logger('debug', {
         writeToDisk: false,
         writeToMemory: true,
+        // by default, bridge logs are not printed to console to avoid too much noise. The other reason why this is set to false
+        // is that global logger has logic of turning it on and off depending on 'debug' mode (see logger/config message) and we don't have this implemented here
+        // it would require putting bridgeLogger to the global scope which might be perceived as controversial
+        writeToConsole: false,
+        dedupeTimeout: 0,
     });
 
     return new TrezordNode({
