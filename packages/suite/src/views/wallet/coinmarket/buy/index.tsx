b65d0bb4077c4267cc0f7e9449b1f3b5cb309151
Martin Homola <adder10@gmail.com>
2024-06-06 09:43:49 +0200

    feat(coinmarket): buy form - menu

--- packages/suite/src/views/wallet/coinmarket/buy/index.tsx
+++ packages/suite/src/views/wallet/coinmarket/buy/index.tsx
@@ -1,22 +1,14 @@
 import { useCoinmarketBuyForm, BuyFormContext } from 'src/hooks/wallet/useCoinmarketBuyForm';
 import { withSelectedAccountLoaded } from 'src/components/wallet';
-import { CoinmarketLayout } from 'src/views/wallet/coinmarket/common';
 import BuyForm from './components/BuyForm';
 import { UseCoinmarketProps } from 'src/types/coinmarket/coinmarket';
+import CoinmarketLayout from '../common/CoinmarketLayoutNew/CoinmarketLayout';
 
 const CoinmarketBuy = (props: UseCoinmarketProps) => {
     const coinmarketBuyContextValues = useCoinmarketBuyForm(props);
-    const {
-        isDraft,
-        formState: { isDirty },
-        handleClearFormButtonClick,
-    } = coinmarketBuyContextValues;
 
     return (
-        <CoinmarketLayout
-            selectedAccount={props.selectedAccount}
-            onClearFormButtonClick={isDirty || isDraft ? handleClearFormButtonClick : undefined}
-        >
+        <CoinmarketLayout selectedAccount={props.selectedAccount}>
             <BuyFormContext.Provider value={coinmarketBuyContextValues}>
                 <BuyForm />
             </BuyFormContext.Provider>
