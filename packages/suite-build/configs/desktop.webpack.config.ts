d8182b3fa4e3d89e11675d3d973cf8c6ae213e6b
Matej Kriz <matej@matejkriz.cz>
2024-06-07 12:37:46 +0200

    feat(suite-desktop): include release notes in latest*.yml files automatically
    - release-notes.md file is copied automatically during build and used by electron-builder to generate changelog in all latest*.yml files
    - platform specific changelog can be specifed in files release-notes-mac.md release-notes-linux.md or release-notes-windows.md files.
    
    (cherry picked from commit 2eb6522d3264f2d6826de069197e7201f687fd2c)

--- packages/suite-build/configs/desktop.webpack.config.ts
+++ packages/suite-build/configs/desktop.webpack.config.ts
@@ -66,6 +66,12 @@ const config: webpack.Configuration = {
                         from: path.join(__dirname, '../../', 'transport-bridge/dist/ui'),
                         to: path.join(baseDir, 'build/node-bridge/ui'),
                     },
+                ])
+                .concat([
+                    {
+                        from: path.join(__dirname, '../../', 'suite-desktop/releaseNotes'),
+                        to: path.join(baseDir, 'build'),
+                    },
                 ]),
             options: {
                 concurrency: 100,
