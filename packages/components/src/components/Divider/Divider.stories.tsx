2f7fe0d6a7b66bd4b80f7a806a9470a9b46511c7
Jan Václavík <jvaclavik@gmail.com>
2024-06-10 08:38:35 +0200

    refactor(suite): Divider (#12790)

--- packages/components/src/components/Divider/Divider.stories.tsx
+++ packages/components/src/components/Divider/Divider.stories.tsx
@@ -1,5 +1,11 @@
 import { Meta, StoryObj } from '@storybook/react';
 import { Divider as DividerComponent } from '../../index';
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width: 200px;
+    height: 200px;
+`;
 
 const meta: Meta = {
     title: 'Misc/Divider',
@@ -7,4 +13,27 @@ const meta: Meta = {
 } as Meta;
 export default meta;
 
-export const Divider: StoryObj = {};
+export const Divider: StoryObj = {
+    render: props => (
+        <Container>
+            <DividerComponent {...props} />
+        </Container>
+    ),
+    args: {
+        orientation: 'horizontal',
+    },
+    argTypes: {
+        orientation: {
+            control: {
+                type: 'select',
+            },
+            options: ['horizontal', 'vertical'],
+        },
+        strokeWidth: {
+            type: 'number',
+        },
+        color: {
+            type: 'string',
+        },
+    },
+};
