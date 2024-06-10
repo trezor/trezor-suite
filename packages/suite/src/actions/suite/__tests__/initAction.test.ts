5438059a2bb02b051b18c42d72156002a8d121b4
AdamSchinzel <schinzel.adam@gmail.com>
2024-06-10 15:08:13 +0200

    fix(suite): missing fiat rates for txs

--- packages/suite/src/actions/suite/__tests__/initAction.test.ts
+++ packages/suite/src/actions/suite/__tests__/initAction.test.ts
@@ -16,6 +16,7 @@ import {
     preloadFeeInfoThunk,
     initStakeDataThunk,
     periodicCheckStakeDataThunk,
+    updateMissingTxFiatRatesThunk,
 } from '@suite-common/wallet-core';
 import { analyticsActions, prepareAnalyticsReducer } from '@suite-common/analytics';
 import TrezorConnect from '@trezor/connect';
@@ -116,6 +117,8 @@ const fixtures: Fixture[] = [
             fetchFiatRatesThunk.pending.type,
             fetchFiatRatesThunk.fulfilled.type,
             periodicFetchFiatRatesThunk.fulfilled.type,
+            updateMissingTxFiatRatesThunk.pending.type,
+            updateMissingTxFiatRatesThunk.fulfilled.type,
             periodicCheckStakeDataThunk.pending.type,
             initStakeDataThunk.pending.type,
             SUITE.READY,
@@ -158,6 +161,8 @@ const fixtures: Fixture[] = [
             fetchFiatRatesThunk.pending.type,
             fetchFiatRatesThunk.fulfilled.type,
             periodicFetchFiatRatesThunk.fulfilled.type,
+            updateMissingTxFiatRatesThunk.pending.type,
+            updateMissingTxFiatRatesThunk.fulfilled.type,
             appChanged.type,
             ROUTER.LOCATION_CHANGE,
             periodicCheckStakeDataThunk.pending.type,
@@ -201,6 +206,8 @@ const fixtures: Fixture[] = [
             fetchFiatRatesThunk.pending.type,
             fetchFiatRatesThunk.fulfilled.type,
             periodicFetchFiatRatesThunk.fulfilled.type,
+            updateMissingTxFiatRatesThunk.pending.type,
+            updateMissingTxFiatRatesThunk.fulfilled.type,
             ROUTER.LOCATION_CHANGE,
             periodicCheckStakeDataThunk.pending.type,
             initStakeDataThunk.pending.type,
