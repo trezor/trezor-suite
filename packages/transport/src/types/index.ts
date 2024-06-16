e3dbe855bb1c18a3056f0d5d880d04d1e38772ff
Martin Varmuza <mvarmuza@gmail.com>
2024-06-16 20:29:08 +0200

    feat(transport): make descriptor.type required

--- packages/transport/src/types/index.ts
+++ packages/transport/src/types/index.ts
@@ -7,7 +7,7 @@ export type Session = `${number}`;
 export type DescriptorApiLevel = {
     path: string;
     /** only used in status page */
-    type?: DEVICE_TYPE;
+    type: DEVICE_TYPE;
     /** only important for T1 over old bridge (trezord-go), defacto part of 'path'. More explanation in https://github.com/trezor/trezor-suite/compare/transport-descriptor-product */
     product?: number;
 };
