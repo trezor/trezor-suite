49e7d81f4f2eab915e1e714b448fbf6f84d03d08
tomasklim <tomas.klim@gmail.com>
2024-06-19 14:17:57 +0200

    feat(suite): add BNB

--- suite-common/wallet-utils/src/accountUtils.ts
+++ suite-common/wallet-utils/src/accountUtils.ts
@@ -149,6 +149,8 @@ export const getTitleForNetwork = (symbol: NetworkSymbol) => {
             return 'TR_NETWORK_ZCASH';
         case 'eth':
             return 'TR_NETWORK_ETHEREUM';
+        case 'bsc':
+            return 'TR_NETWORK_BSC';
         case 'tsep':
             return 'TR_NETWORK_ETHEREUM_SEPOLIA';
         case 'thol':
