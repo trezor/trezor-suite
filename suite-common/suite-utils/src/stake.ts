d6a060ed1efddd521b3630307711a30dc953d3e3
Pavlo Syrotyna <p.syrotyna@everstake.one>
2024-06-20 17:18:57 +0300

    fix(staking): correct message for replace transaction

--- suite-common/suite-utils/src/stake.ts
+++ suite-common/suite-utils/src/stake.ts
@@ -1,3 +1,5 @@
+import { StakeType } from '@suite-common/wallet-types';
+
 export const isStakeTx = (signature: string | undefined) => {
     if (!signature) return false;
 
@@ -23,4 +25,23 @@ export const isStakeTypeTx = (signature: string | undefined) => {
     if (!signature) return false;
 
     return isStakeTx(signature) || isUnstakeTx(signature) || isClaimTx(signature);
+};
+
+export const signatureToStakeNameMap: { [key: string]: StakeType } = {
+    '0x3a29dbae': 'stake',
+    '0x76ec871c': 'unstake',
+    '0x33986ffa': 'claim',
+};
+
+export const getSignatureByEthereumDataHex = (dataHex: string) => {
+    const signature = '0x' + dataHex.slice(0, 8);
+
+    return signature;
+};
+
+export const selectTxStakeNameByDataHex = (dataHex: string | undefined): StakeType | null => {
+    if (!dataHex) return null;
+    const signature = getSignatureByEthereumDataHex(dataHex);
+
+    return isStakeTypeTx(signature) ? signatureToStakeNameMap[signature] : null;
 };
