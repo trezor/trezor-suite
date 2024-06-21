e0e51c70b67313f5cfbc0fa4d5b6fe87805fc07f
Tomas Martykan <t.martykan@gmail.com>
2024-06-21 13:33:46 +0200

    fix(connect): race condition when closing and opening popup subsequently

--- packages/connect-web/src/popup/index.ts
+++ packages/connect-web/src/popup/index.ts
@@ -376,6 +376,7 @@ export class PopupManager extends EventEmitter {
                 });
             });
         } else if (data.type === POPUP.CANCEL_POPUP_REQUEST) {
+            clearTimeout(this.requestTimeout);
             if (this.popupPromise) {
                 this.close();
             }
