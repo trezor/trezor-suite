be3c8698262b9c26e2ea74fa09a96078869069b9
Martin Homola <adder10@gmail.com>
2024-06-21 11:10:49 +0200

    refactor(coinmarket): buy form - dropdown separate logic

--- packages/suite/src/views/wallet/coinmarket/common/CoinmarketSelectedOffer/CoinmarketSelectedOfferVerifyOptions.tsx
+++ packages/suite/src/views/wallet/coinmarket/common/CoinmarketSelectedOffer/CoinmarketSelectedOfferVerifyOptions.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from 'react';
 import styled, { useTheme } from 'styled-components';
-import useTimeoutFn from 'react-use/lib/useTimeoutFn';
 import { variables, CoinLogo, Select, Icon } from '@trezor/components';
 import {
     FiatValue,
@@ -8,17 +6,10 @@ import {
     AccountLabeling,
     FormattedCryptoAmount,
 } from 'src/components/suite';
-import { useDispatch, useSelector } from 'src/hooks/suite';
-import { openModal } from 'src/actions/suite/modalActions';
-import { getUnusedAddressFromAccount } from 'src/utils/wallet/coinmarket/coinmarketUtils';
-import type { UseFormReturn } from 'react-hook-form';
-import type { Account } from 'src/types/wallet';
-import { useCoinmarketFormContext } from 'src/hooks/wallet/coinmarket/form/useCoinmarketCommonForm';
-import { cryptoToNetworkSymbol } from 'src/utils/wallet/coinmarket/cryptoSymbolUtils';
-import { CoinmarketTradeBuyType } from 'src/types/coinmarket/coinmarket';
-import { selectIsDebugModeActive } from 'src/reducers/suite/suiteReducer';
-import { AppState } from 'src/reducers/store';
-import { networksCompatibility } from '@suite-common/wallet-config';
+import {
+    CoinmarketSelectedOfferVerifyOptionsProps,
+    CoinmarketVerifyFormAccountOptionProps,
+} from 'src/types/coinmarket/coinmarketVerify';
 
 const LogoWrapper = styled.div`
     display: flex;
@@ -62,116 +53,24 @@ const AccountType = styled.span`
     padding-left: 5px;
 `;
 
-export type AccountSelectOption = {
-    type: 'SUITE' | 'ADD_SUITE' | 'NON_SUITE';
-    account?: Account;
-};
-
-type FormState = {
-    address?: string;
-};
-
-type CoinmarketSelectedOfferVerifyOptionsProps = Pick<UseFormReturn<FormState>, 'setValue'> & {
-    selectedAccountOption?: AccountSelectOption;
-    setSelectedAccountOption: (o: AccountSelectOption) => void;
-};
-
-const CoinmarketSelectedOfferVerifyOptions = (props: CoinmarketSelectedOfferVerifyOptionsProps) => {
+const CoinmarketSelectedOfferVerifyOptions = ({
+    receiveNetwork,
+    selectAccountOptions,
+    selectedAccountOption,
+    onChangeAccount,
+}: CoinmarketSelectedOfferVerifyOptionsProps) => {
     const theme = useTheme();
-    const dispatch = useDispatch();
-    const { device, selectedQuote } = useCoinmarketFormContext<CoinmarketTradeBuyType>();
-    const [suiteReceiveAccounts, setSuiteReceiveAccounts] =
-        useState<AppState['wallet']['accounts']>();
-    const isDebug = useSelector(selectIsDebugModeActive);
-
-    const accounts = useSelector(state => state.wallet.accounts);
-
-    const { selectedAccountOption, setSelectedAccountOption, setValue } = props;
-    const receiveNetwork =
-        selectedQuote?.receiveCurrency && cryptoToNetworkSymbol(selectedQuote?.receiveCurrency);
-    const selectAccountOptions: AccountSelectOption[] = [];
-
-    if (suiteReceiveAccounts) {
-        suiteReceiveAccounts.forEach(account => {
-            selectAccountOptions.push({ type: 'SUITE', account });
-        });
-        selectAccountOptions.push({ type: 'ADD_SUITE' });
-    }
-    selectAccountOptions.push({ type: 'NON_SUITE' });
-
-    const selectAccountOption = (option: AccountSelectOption) => {
-        setSelectedAccountOption(option);
-        // setReceiveAccount(option.account);
-        if (option.account) {
-            const { address } = getUnusedAddressFromAccount(option.account);
-            setValue('address', address, { shouldValidate: true });
-        } else {
-            setValue('address', '', { shouldValidate: false });
-        }
-    };
-
-    const onChangeAccount = (account: AccountSelectOption) => {
-        if (account.type === 'ADD_SUITE') {
-            if (device) {
-                dispatch(
-                    openModal({
-                        type: 'add-account',
-                        device: device!,
-                        symbol: receiveNetwork as Account['symbol'],
-                        noRedirect: true,
-                    }),
-                );
-            }
-        } else {
-            selectAccountOption(account);
-        }
-    };
-
-    // preselect the account after everything is loaded
-    useTimeoutFn(() => {
-        if (selectAccountOptions.length > 0 && selectAccountOptions[0].type !== 'ADD_SUITE') {
-            selectAccountOption(selectAccountOptions[0]);
-        }
-    }, 100);
-
-    useEffect(() => {
-        // exchangeStep === 'RECEIVING_ADDRESS'
-        if (selectedQuote) {
-            const unavailableCapabilities = device?.unavailableCapabilities ?? {};
-            // is the symbol supported by the suite and the device natively
-            const receiveNetworks = networksCompatibility.filter(
-                n =>
-                    n.symbol === receiveNetwork &&
-                    !unavailableCapabilities[n.symbol] &&
-                    ((n.isDebugOnly && isDebug) || !n.isDebugOnly),
-            );
-            if (receiveNetworks.length > 0) {
-                // get accounts of the current symbol belonging to the current device
-                setSuiteReceiveAccounts(
-                    accounts.filter(
-                        a =>
-                            a.deviceState === device?.state &&
-                            a.symbol === receiveNetwork &&
-                            (!a.empty ||
-                                a.visible ||
-                                (a.accountType === 'normal' && a.index === 0)),
-                    ),
-                );
-
-                return;
-            }
-        }
-        setSuiteReceiveAccounts(undefined);
-    }, [accounts, device, receiveNetwork, selectedQuote, isDebug]);
 
     return (
         <Select
-            onChange={(selected: any) => onChangeAccount(selected)}
+            onChange={(selected: CoinmarketVerifyFormAccountOptionProps) =>
+                onChangeAccount(selected)
+            }
             value={selectedAccountOption}
             isClearable={false}
             options={selectAccountOptions}
             minValueWidth="70px"
-            formatOptionLabel={(option: AccountSelectOption) => {
+            formatOptionLabel={(option: CoinmarketVerifyFormAccountOptionProps) => {
                 switch (option.type) {
                     case 'SUITE': {
                         if (!option.account) return null;
