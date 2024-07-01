e2b7fc7ea5ba330a9563cec2e75603c413abc8e9
Tomas Martykan <t.martykan@gmail.com>
2024-07-01 17:25:35 +0200

    fix(connect-explorer): bad styled-components prop

--- packages/connect-explorer/src/components/ZoomableIllustration.tsx
+++ packages/connect-explorer/src/components/ZoomableIllustration.tsx
@@ -7,37 +7,37 @@ import { createGlobalStyle, useTheme, styled } from 'styled-components';
 import 'react-medium-image-zoom/dist/styles.css';
 
 export type ZoomableIllustrationProps = React.ImgHTMLAttributes<HTMLImageElement> & {
-    darkMode?: boolean;
+    $darkMode?: boolean;
 };
 
 // Automatically invert colors of the image when darkMode is enabled
-const DarkModeImg = styled.img<{ darkMode?: boolean }>`
-    filter: ${({ darkMode }) => (darkMode ? 'invert(1) hue-rotate(180deg)' : 'invert(0)')};
+const DarkModeImg = styled.img<{ $darkMode?: boolean }>`
+    filter: ${({ $darkMode }) => ($darkMode ? 'invert(1) hue-rotate(180deg)' : 'invert(0)')};
 `;
 
 // Handle dark mode styles in react-medium-image-zoom
-const ReactMediumImageZoomStyle = createGlobalStyle<{ darkMode?: boolean }>`
+const ReactMediumImageZoomStyle = createGlobalStyle<{ $darkMode?: boolean }>`
   html.dark [data-rmiz-modal-overlay="visible"] {
     background-color: rgba(0, 0, 0, 1);
   }
 
   [data-rmiz-modal-img] {
-    filter: ${({ darkMode }) => (darkMode ? 'invert(1) hue-rotate(180deg)' : 'invert(0)')};
+    filter: ${({ $darkMode }) => ($darkMode ? 'invert(1) hue-rotate(180deg)' : 'invert(0)')};
   }
 `;
 
 export const ZoomableIllustration = (props: ZoomableIllustrationProps) => {
     const router = useRouter();
     const { THEME } = useTheme();
-    const darkMode = props.darkMode && THEME === 'dark';
+    const darkMode = props.$darkMode && THEME === 'dark';
 
     // Automatic absolute path handling
     const src = props?.src?.startsWith('/') ? router.basePath + props.src : props.src;
 
     return (
         <Zoom>
-            <ReactMediumImageZoomStyle darkMode={darkMode} />
-            <DarkModeImg {...props} src={src} darkMode={darkMode} />
+            <ReactMediumImageZoomStyle $darkMode={darkMode} />
+            <DarkModeImg {...props} src={src} $darkMode={darkMode} />
         </Zoom>
     );
 };
