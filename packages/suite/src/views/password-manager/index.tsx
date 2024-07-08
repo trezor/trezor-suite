646fcbe05ceee6912bca155de4f8ad76c531a590
Marek Polak <marek.polak@satoshilabs.com>
2024-07-08 13:56:12 +0200

    feat(suite): add password manager page header

--- packages/suite/src/views/password-manager/index.tsx
+++ packages/suite/src/views/password-manager/index.tsx
@@ -1,7 +1,16 @@
 import { useLayout } from 'src/hooks/suite';
 import { PasswordManager } from './PasswordManager/PasswordManager';
+import { PageHeader } from 'src/components/suite/layouts/SuiteLayout';
+import { BasicName } from 'src/components/suite/layouts/SuiteLayout/PageHeader/PageNames/BasicName';
 
-export default () => {
-    useLayout('Password manager');
+const PasswordManagerView = () => {
+    useLayout('Password manager', () => (
+        <PageHeader>
+            <BasicName nameId="TR_PASSWORD_MANAGER" />
+        </PageHeader>
+    ));
+
     return <PasswordManager />;
 };
+
+export default PasswordManagerView;
