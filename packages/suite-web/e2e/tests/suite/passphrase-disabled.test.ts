322ccc98182532dff4b6cb292bcf86e11a058345
Jakub Ondracek <ondracekjakub@gmail.com>
2024-07-11 20:50:51 +0200

    fix: adding obsolete tests that are now skipped for further review

---
+++ packages/suite-web/e2e/tests/suite/passphrase-disabled.test.ts
@@ -0,0 +1,69 @@
+// @group_passphrase
+// @retry=2
+
+const DEFAULT_STANDARD_WALLET_LABEL = 'Standard wallet';
+const DEFAULT_HIDDEN_WALLET_LABEL = 'Hidden wallet #';
+
+// TODO: review this test if it's indeed obsolete
+describe.skip('Suite switch wallet modal', () => {
+    beforeEach(() => {
+        cy.viewport(1440, 2560).resetDb();
+        cy.task('startBridge');
+    });
+
+    it('passphrase_protection: false', () => {
+        cy.task('startEmu', { wipe: true });
+        cy.task('setupEmu', { passphrase_protection: false });
+
+        cy.prefixedVisit('/');
+
+        cy.passThroughInitialRun();
+        cy.discoveryShouldFinish();
+        cy.wait(501);
+        cy.prefixedVisit('/settings');
+
+        cy.getTestElement('@menu/switch-device').click();
+
+        // device does not have state yet, there is only one button to discovery the first wallet
+        cy.getTestElement('@switch-device/add-wallet-button').click();
+        cy.getTestElement('@dashboard/loading', { timeout: 30000 });
+        cy.getTestElement('@dashboard/loading', { timeout: 30000 }).should('not.exist');
+        cy.getTestElement('@menu/switch-device').should('contain', DEFAULT_STANDARD_WALLET_LABEL);
+
+        cy.getTestElement('@menu/switch-device').click();
+
+        cy.getTestElement('@switch-device/wallet-on-index/0').should(
+            'contain',
+            DEFAULT_STANDARD_WALLET_LABEL,
+        );
+        cy.getTestElement('@switch-device/add-hidden-wallet-button').click();
+
+        cy.getTestElement('@suite/modal/confirm-action-on-device');
+        cy.task('pressYes');
+
+        const passphaseToType = 'taxation is theft';
+        cy.getTestElement('@passphrase/input').type(passphaseToType);
+        cy.getTestElement('@passphrase/hidden/submit-button').click();
+
+        cy.task('pressYes');
+        cy.task('pressYes');
+
+        cy.getTestElement('@modal');
+        cy.getTestElement('@passphrase/input', { timeout: 10000 }).type(passphaseToType);
+        cy.getTestElement('@passphrase/confirm-checkbox', { timeout: 20000 }).click();
+        cy.getTestElement('@passphrase/hidden/submit-button').click();
+
+        cy.task('pressYes');
+        cy.task('pressYes');
+
+        cy.getTestElement('@modal').should('not.exist');
+
+        cy.getTestElement('@menu/switch-device').should(
+            'contain',
+            `${DEFAULT_HIDDEN_WALLET_LABEL}1`,
+        );
+        cy.getTestElement('@menu/switch-device').click();
+    });
+});
+
+export {};
