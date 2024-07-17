b8681d92ea47afc29777e0e37f4c4754ce59b13f
Jan Václavík <jvaclavik@gmail.com>
2024-07-17 10:57:55 +0200

    refactor(suite): Afuera with legacy components

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -53,7 +53,6 @@ export * from './components/modals/Modal/Backdrop';
 export * from './components/modals/Modal/Modal';
 export * from './components/Note/Note';
 export * from './components/Passphrase/PassphraseTypeCard';
-export * from './components/Passphrase/PassphraseTypeCardLegacy';
 export * from './components/ResizableBox/ResizableBox';
 export * from './components/skeletons/SkeletonCircle';
 export * from './components/skeletons/SkeletonRectangle';
