6473886843e29dc3696505eaaba8c71bc768d8d0
Peter Sanderson <dierk.deidel@protonmail.com>
2024-07-18 14:51:25 +0200

    feat: add support for changelog_bitcoinonly into release.json

--- packages/connect/src/types/firmware.ts
+++ packages/connect/src/types/firmware.ts
@@ -15,14 +15,14 @@ export type FirmwareRelease = {
     required: boolean;
     url: string;
     fingerprint: string;
-    changelog: string;
+    changelog: string | string[];
+    changelog_bitcoinonly?: string | string[]; // Added later, may not be there for older releases
     version: VersionArray;
     min_firmware_version: VersionArray;
     min_bootloader_version: VersionArray;
     bootloader_version?: VersionArray;
     url_bitcoinonly?: string;
     fingerprint_bitcoinonly?: string;
-    notes?: string;
     channel?: string;
     translations?: string[];
 };
