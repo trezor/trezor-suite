86a9e764a495c4a6f04bba9337256ee00b8fb1a0
Tomas Martykan <t.martykan@gmail.com>
2024-07-19 13:57:57 +0200

    feat(suite): rework bridge modal

--- packages/suite-web/e2e/tests/suite/bridge.test.ts
+++ packages/suite-web/e2e/tests/suite/bridge.test.ts
@@ -1,15 +1,6 @@
 // @group_suite
 // @retry=2
 
-const systems = [
-    'Linux 64-bit (deb)',
-    'Linux 64-bit (rpm)',
-    'Linux 32-bit (deb)',
-    'Linux 32-bit (rpm)',
-    'macOS',
-    'Windows',
-];
-
 describe('Bridge page', () => {
     beforeEach(() => {
         cy.viewport(1440, 2560).resetDb();
@@ -18,17 +9,7 @@ describe('Bridge page', () => {
     it('/bridge', () => {
         cy.prefixedVisit('/bridge');
 
-        // it correctly preselects installer by system
-        cy.getTestElement('@bridge/installers/input').should('contain', systems[0]);
-
-        // there is a dropdown with installers
-        cy.getTestElement('@bridge/installers/input').click();
-
-        // select listens to keyboard events
-        cy.get('body').type('{downarrow}{downarrow}{downarrow}{downarrow}{downarrow}{enter}');
-        cy.getTestElement('@bridge/installers/input').should('contain', 'Windows');
-
-        cy.getTestElement('@modal/bridge').matchImageSnapshot('bridge-modal', {
+        cy.getTestElement('@modal/bridge').matchImageSnapshot('bridge-modal-new', {
             blackout: ['[data-test="@bridge/download-button"]'],
         });
 
@@ -37,15 +18,6 @@ describe('Bridge page', () => {
 
         // connect device prompt with webusb enabled appears
         cy.getTestElement('@connect-device-prompt');
-
-        // linux platforms show udev rules link also
-        cy.getTestElement('@connect-device-prompt/no-device-detected').click();
-
-        cy.getTestElement('@goto/udev').click();
-        cy.getTestElement('@modal/udev').matchImageSnapshot('udev-modal');
-
-        // udev rules modal is closable via close button
-        cy.getTestElement('@modal/close-button').click();
     });
 });
 
