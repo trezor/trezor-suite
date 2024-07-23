bd2aaee0ccab6368ee19afe4ac8924678560fc9d
Jan Václavík <jvaclavik@gmail.com>
2024-07-23 09:02:25 +0200

    feat(suite): Add shortcut for settings

--- packages/suite/src/components/suite/layouts/SuiteLayout/useAppShortcuts.tsx
+++ packages/suite/src/components/suite/layouts/SuiteLayout/useAppShortcuts.tsx
@@ -3,23 +3,30 @@ import { useEvent } from 'react-use';
 import { closeModalApp, goto } from 'src/actions/suite/routerActions';
 import { addWalletThunk } from 'src/actions/wallet/addWalletThunk';
 import { useDispatch, useSelector } from 'src/hooks/suite';
+import { KEYBOARD_CODE } from '@trezor/components';
 
 export const useAppShortcuts = () => {
     const selectedDevice = useSelector(selectDevice);
     const dispatch = useDispatch();
 
     useEvent('keydown', e => {
-        const { altKey } = e;
+        const { altKey, metaKey } = e;
+        const isDeviceSelected = selectedDevice !== undefined;
 
         // press ALT + P to show PassphraseModal
-        if (selectedDevice?.connected && altKey && e.code === 'KeyP' && selectedDevice) {
+        if (
+            selectedDevice?.connected &&
+            altKey &&
+            e.code === KEYBOARD_CODE.KEY_P &&
+            isDeviceSelected
+        ) {
             dispatch(addWalletThunk({ walletType: 'passphrase', device: selectedDevice }));
             dispatch(closeModalApp());
             e.preventDefault();
         }
 
         // press ALT + D to show SwitchDevice
-        if (altKey && e.code === 'KeyD' && selectedDevice) {
+        if (altKey && e.code === KEYBOARD_CODE.KEY_D && isDeviceSelected) {
             dispatch(
                 goto('suite-switch-device', {
                     params: {
@@ -27,6 +34,12 @@ export const useAppShortcuts = () => {
                     },
                 }),
             );
+            e.preventDefault();
+        }
+
+        // press CMD + , to show Settings
+        if (metaKey && e.code === KEYBOARD_CODE.COMMA && isDeviceSelected) {
+            dispatch(goto('settings-index'));
             e.preventDefault();
         }
     });
