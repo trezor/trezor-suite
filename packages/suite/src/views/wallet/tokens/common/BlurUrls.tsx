ac7601a9b3aea098540de9c0e2950a6e375056e5
tomasklim <tomas.klim@gmail.com>
2024-07-30 16:07:05 +0200

    fix(suite): blur url 2.0

--- packages/suite/src/views/wallet/tokens/common/BlurUrls.tsx
+++ packages/suite/src/views/wallet/tokens/common/BlurUrls.tsx
@@ -1,9 +1,9 @@
 import { Fragment } from 'react';
 import styled from 'styled-components';
 import { Tooltip } from '@trezor/components';
-
 import { BlurWrapper } from 'src/components/wallet/TransactionItem/TransactionItemBlurWrapper';
 import { Translation } from 'src/components/suite';
+import { extractUrlsFromText } from '@trezor/utils';
 
 const StyledTooltip = styled(Tooltip)`
     display: inline-block;
@@ -16,21 +16,14 @@ interface BlurUrlsProps {
 export const BlurUrls = ({ text }: BlurUrlsProps) => {
     if (!text) return null;
 
-    // Regex to match URLs like https://example.com, http://example.com, www.example.com, example.com
-    const urlRegex =
-        /\b(?:https?:\/\/|www\.)[a-zA-Z0-9-._~:/?#[\]@!$&'()*+,;=]+(?:\.[a-zA-Z]{2,})\b|[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+/gi;
+    const { textParts, urls } = extractUrlsFromText(text);
 
-    // Split text into parts excluding URLs and find all URL matches
-    const parts = text.split(urlRegex);
-    const matches = [...text.matchAll(urlRegex)];
-
-    // Map each part to a Fragment, blurring matched URLs
-    return parts.map((part, index) => (
+    return textParts.map((part, index) => (
         <Fragment key={index}>
             {part}
-            {matches[index] && (
+            {index < urls.length && (
                 <StyledTooltip content={<Translation id="TR_URL_IN_TOKEN" />}>
-                    <BlurWrapper $isBlurred>{matches[index][0]}</BlurWrapper>
+                    <BlurWrapper $isBlurred>{urls[index]}</BlurWrapper>
                 </StyledTooltip>
             )}
         </Fragment>
