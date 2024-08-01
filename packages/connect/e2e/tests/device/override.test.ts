2dc9ed48677c04287ab6020eb7ece46557b18767
Marek Polak <marek.polak@satoshilabs.com>
2024-08-01 16:48:14 +0200

    test(connect): override test polished a bit

--- packages/connect/e2e/tests/device/override.test.ts
+++ packages/connect/e2e/tests/device/override.test.ts
@@ -13,21 +13,22 @@ describe('TrezorConnect override param', () => {
         await initTrezorConnect(controller);
     });
 
+    afterEach(async () => {
+        await new Promise(resolve => setTimeout(resolve, 1000));
+    });
+
     afterAll(async () => {
         controller.dispose();
         await TrezorConnect.dispose();
     });
 
-    for (const delay of [1, 10, 100, 300, 500, 1000, 1500]) {
+    [1, 10, 100, 300, 500, 1000, 1500].forEach(delay => {
         it(`override previous call after ${delay}ms`, async () => {
             TrezorConnect.removeAllListeners();
 
-            TrezorConnect.getAddress({
+            const overriden = TrezorConnect.getAddress({
                 path: "m/44'/1'/0'/0/0",
                 showOnTrezor: true,
-            }).then(response => {
-                expect(response.success).toBe(false);
-                expect(response.payload).toMatchObject({ code: 'Method_Override' });
             });
 
             await new Promise(resolve => setTimeout(resolve, delay));
@@ -38,7 +39,10 @@ describe('TrezorConnect override param', () => {
                 showOnTrezor: false,
             });
             expect(address.success).toBe(true);
-            await new Promise(resolve => setTimeout(resolve, 1000));
+
+            const response = await overriden;
+            expect(response.success).toBe(false);
+            expect(response.payload).toMatchObject({ code: 'Method_Override' });
         });
-    }
+    });
 });
