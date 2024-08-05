80c72f3a205cd9b9b658361dca7f358944014848
Tomas Martykan <t.martykan@gmail.com>
2024-08-05 14:24:49 +0200

    chore(connect-explorer): clean up coins select

--- packages/connect-explorer/src/data/methods/bitcoin/common.ts
+++ packages/connect-explorer/src/data/methods/bitcoin/common.ts
@@ -1,17 +1,4 @@
-export const select = [
-    { value: 'btc', label: 'Bitcoin', affectedValue: `m/49'/0'/0'` },
-    { value: 'test', label: 'Bitcoin Testnet', affectedValue: `m/49'/1'/0'` },
-    { value: 'bch', label: 'Bitcoin Cash', affectedValue: `m/44'/145'/0'` },
-    { value: 'btg', label: 'Bitcoin Gold', affectedValue: `m/49'/156'/0'` },
-    { value: 'ltc', label: 'Litecoin', affectedValue: `m/49'/2'/0'` },
-    { value: 'dash', label: 'Dash', affectedValue: `m/44'/5'/0'` },
-    { value: 'zcash', label: 'Zcash', affectedValue: `m/44'/133'/0'` },
-    { value: 'doge', label: 'Dogecoin', affectedValue: `m/44'/3'/0'` },
-    { value: 'nmc', label: 'Namecoin', affectedValue: `m/44'/7'/0'` },
-    { value: 'vtc', label: 'Vertcoin', affectedValue: `m/49'/28'/0'` },
-    { value: 'cpc', label: 'Capricoin', affectedValue: `m/44'/289'/0'` },
-    { value: 'kmd', label: 'Komodo', affectedValue: `m/44'/289'/0'` },
-];
+export { coinsSelect as select } from '../../../constants/coins';
 
 export const xpubs = [
     'ypub6XKbB5DSkq8Royg8isNtGktj6bmEfGJXDs83Ad5CZ5tpDV8QofwSWQFTWP2Pv24vNdrPhquehL7vRMvSTj2GpKv6UaTQCBKZALm6RJAmxG6',
