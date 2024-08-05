9cef961d671d9e5af727facc0df73b697ea51584
Tomas Martykan <t.martykan@gmail.com>
2024-08-05 14:24:49 +0200

    chore(connect-explorer): clean up coins select

--- packages/connect-explorer/src/constants/coins.ts
+++ packages/connect-explorer/src/constants/coins.ts
@@ -7,8 +7,4 @@ export const coinsSelect = [
     { value: 'dash', label: 'Dash', affectedValue: `m/44'/5'/0'` },
     { value: 'zcash', label: 'Zcash', affectedValue: `m/44'/133'/0'` },
     { value: 'doge', label: 'Dogecoin', affectedValue: `m/44'/3'/0'` },
-    { value: 'nmc', label: 'Namecoin', affectedValue: `m/44'/7'/0'` },
-    { value: 'vtc', label: 'Vertcoin', affectedValue: `m/49'/28'/0'` },
-    { value: 'cpc', label: 'Capricoin', affectedValue: `m/44'/289'/0'` },
-    { value: 'kmd', label: 'Komodo', affectedValue: `m/44'/289'/0'` },
 ];
