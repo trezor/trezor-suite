7116740aa6bba393c37ade622b4adc32d1069359
MiroslavProchazka <miroslav.prochazka9@gmail.com>
2024-08-15 22:32:05 +0200

    chore(message-system): adjusting validation schema and tests to new lngs

--- suite-common/suite-types/src/messageSystem.ts
+++ suite-common/suite-types/src/messageSystem.ts
@@ -8,7 +8,7 @@
  */
 export type DateTime = string;
 export type Version = string | string[];
-export type Model = '1' | 'T' | 'T1B1' | 'T2T1' | 'T2B1' | 'Safe 3' | 'T3T1' | '';
+export type Model = '1' | 'T' | 'T1B1' | 'T2T1' | 'T2B1' | 'Safe 3' | 'T3T1' | 'T3B1' | '';
 export type FirmwareRevision = string;
 export type FirmwareVariant = '*' | 'bitcoin-only' | 'regular';
 /**
@@ -121,6 +121,13 @@ export interface Localization {
     cs: string;
     ru: string;
     ja: string;
+    de: string;
+    fr: string;
+    it: string;
+    pt: string;
+    tr: string;
+    uk: string;
+    hu: string;
     [k: string]: string;
 }
 /**
