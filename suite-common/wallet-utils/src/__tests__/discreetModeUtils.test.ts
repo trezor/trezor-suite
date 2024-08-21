91d2afa510b650910142c07d32ccfac9d3df7243
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2024-08-21 14:59:34 +0200

    fix(suite): Refactor constant placeholder to variable length

--- suite-common/wallet-utils/src/__tests__/discreetModeUtils.test.ts
+++ suite-common/wallet-utils/src/__tests__/discreetModeUtils.test.ts
@@ -1,17 +1,25 @@
-import { DISCREET_PLACEHOLDER, redactNumericalSubstring } from '../discreetModeUtils';
+import { redactNumericalSubstring } from '../discreetModeUtils';
+
+const MAX_PLACEHOLDER = '####'; // placeholder with maximal length of 4 characters
 
 describe('redactNumericalSubstring', () => {
     it('replaces sole stringified number with placeholder', () => {
-        expect(redactNumericalSubstring('123')).toBe(DISCREET_PLACEHOLDER);
-        expect(redactNumericalSubstring('0.001234')).toBe(DISCREET_PLACEHOLDER);
-        expect(redactNumericalSubstring('-9,876,543.210001')).toBe(DISCREET_PLACEHOLDER);
-        expect(redactNumericalSubstring('-.1')).toBe(DISCREET_PLACEHOLDER);
+        expect(redactNumericalSubstring('123')).toBe('###');
+        expect(redactNumericalSubstring('0.001234')).toBe(MAX_PLACEHOLDER);
+        expect(redactNumericalSubstring('-9,876,543.210001')).toBe(MAX_PLACEHOLDER);
+        expect(redactNumericalSubstring('-.1')).toBe('###');
     });
 
     it('redacts only the number, not an accompanying symboll', () => {
-        expect(redactNumericalSubstring('CZK 123')).toBe(`CZK ${DISCREET_PLACEHOLDER}`);
-        expect(redactNumericalSubstring('0.001234 BTC')).toBe(`${DISCREET_PLACEHOLDER} BTC`);
-        expect(redactNumericalSubstring('-9,876,543.210001€')).toBe(`${DISCREET_PLACEHOLDER}€`);
+        expect(redactNumericalSubstring('CZK 123')).toBe(`CZK ###`);
+        expect(redactNumericalSubstring('0.001234 BTC')).toBe(`${MAX_PLACEHOLDER} BTC`);
+        expect(redactNumericalSubstring('-9,876,543.210001€')).toBe(`${MAX_PLACEHOLDER}€`);
+    });
+
+    it('redacts all number occurrences', () => {
+        expect(redactNumericalSubstring('CZK 123 is 12345 satoshi')).toBe(
+            `CZK ### is ${MAX_PLACEHOLDER} satoshi`,
+        );
     });
 
     it('does not replace non-numerical strings', () => {
--- suite-common/wallet-utils/src/discreetModeUtils.ts
+++ suite-common/wallet-utils/src/discreetModeUtils.ts
@@ -1,15 +1,24 @@
 import { createContext, useContext } from 'react';
 
-export const DISCREET_PLACEHOLDER = '###';
-
 const numericalSubstringRegex = /[\d\-\.,]*\d+[\.\d]*/g;
 /**
  * Search the input for a numerical substring and replace it with DISCREET_PLACEHOLDER.
  * @param value whole string value, usually number with symbol
  * @returns redacted string
  */
-export const redactNumericalSubstring = (value: string): string =>
-    value.replace(numericalSubstringRegex, DISCREET_PLACEHOLDER);
+export const redactNumericalSubstring = (value: string): string => {
+    const PLACEHOLDER_CHAR = '#';
+    const MAX_PLACEHOLDER_LENGTH = 4;
+
+    const matches = value.match(numericalSubstringRegex);
+    if (!matches) return value;
+
+    return matches.reduce((redactedValue, match) => {
+        const newLength = Math.min(MAX_PLACEHOLDER_LENGTH, match.length);
+
+        return redactedValue.replace(match, PLACEHOLDER_CHAR.repeat(newLength));
+    }, value);
+};
 
 type RedactNumbersContextData = { shouldRedactNumbers: boolean };
 export const RedactNumbersContext = createContext<RedactNumbersContextData>({
