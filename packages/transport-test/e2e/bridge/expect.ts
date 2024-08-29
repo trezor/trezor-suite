20b8ba593bd48b0b749901536a7d67cc9a7828a7
Martin Varmuza <mvarmuza@gmail.com>
2024-08-29 14:34:53 +0200

    test(transport-test): add a new scenario: test acquire - read - acquire (steal) write read"

--- packages/transport-test/e2e/bridge/expect.ts
+++ packages/transport-test/e2e/bridge/expect.ts
@@ -24,3 +24,10 @@ const type =
 export const pathLength = USE_HW && USE_NODE_BRIDGE ? 24 : !USE_HW && USE_NODE_BRIDGE ? 15 : 1;
 
 export const descriptor = { debug, debugSession, path, product, vendor, type };
+
+export const errorCase1 =
+    (USE_NODE_BRIDGE && USE_HW) || (!USE_NODE_BRIDGE && !USE_HW)
+        ? 'unexpected error'
+        : USE_HW
+          ? 'device disconnected during action'
+          : 'Network request failed';
