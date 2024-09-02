9d63f2dae2c25edd5ed9199a22f3925891aa0df9
Adam Havel <adam.havel@satoshilabs.com>
2024-09-02 13:57:23 +0200

    feat(suite): update device check opt-out modals to use new component from design system

--- packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/DeviceAuthenticityOptOutModal.tsx
+++ packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/DeviceAuthenticityOptOutModal.tsx
@@ -1,36 +1,67 @@
-import { Column, Paragraph } from '@trezor/components';
-import { Translation } from 'src/components/suite';
+import { useState } from 'react';
+
+import { Column, NewModal, Card, Banner, H3, Paragraph } from '@trezor/components';
+import { Translation, CheckItem } from 'src/components/suite';
 import { deviceAuthenticityOptOut } from 'src/actions/suite/suiteActions';
 import { useDispatch } from 'src/hooks/suite';
-import { OptOutModal } from './OptOutModal';
+import { spacings } from '@trezor/theme';
 
 type DeviceAuthenticityOptOutModalProps = {
     onCancel: () => void;
 };
 
 export const DeviceAuthenticityOptOutModal = ({ onCancel }: DeviceAuthenticityOptOutModalProps) => {
+    const [isConfirmed, setIsConfirmed] = useState(false);
     const dispatch = useDispatch();
 
+    const handleTurningOffRevisionCheck = () => {
+        dispatch(deviceAuthenticityOptOut(true));
+        onCancel();
+    };
+
     return (
-        <OptOutModal
-            headingKey="TR_DEVICE_AUTHENTICITY_OPT_OUT_MODAL_HEADING"
-            checkboxLabelKey="TR_DEVICE_AUTHENTICITY_OPT_OUT_MODAL_CHECKBOX_TITLE"
-            confirmKey="TR_DEVICE_AUTHENTICITY_OPT_OUT_MODAL_BUTTON"
-            data-testid="@device-authenticity"
-            onChange={({ isDisabled }) => dispatch(deviceAuthenticityOptOut(isDisabled))}
+        <NewModal
             onCancel={onCancel}
+            icon="shieldWarning"
+            size="small"
+            bottomContent={
+                <>
+                    <NewModal.Button
+                        onClick={handleTurningOffRevisionCheck}
+                        isDisabled={!isConfirmed}
+                        data-testid="@device-authenticity/opt-out-button"
+                    >
+                        <Translation id="TR_DEVICE_AUTHENTICITY_OPT_OUT_MODAL_BUTTON" />
+                    </NewModal.Button>
+                    <NewModal.Button variant="tertiary" onClick={onCancel}>
+                        <Translation id="TR_CANCEL" />
+                    </NewModal.Button>
+                </>
+            }
+            variant="warning"
         >
-            <Column gap={16}>
-                <Paragraph>
+            <H3>
+                <Translation id="TR_DEVICE_AUTHENTICITY_OPT_OUT_TITLE" />
+            </H3>
+            <Paragraph variant="tertiary" typographyStyle="hint">
+                <Translation id="TR_DEVICE_AUTHENTICITY_OPT_OUT_MODAL_DESCRIPTION_3" />
+            </Paragraph>
+            <Column gap={spacings.sm} margin={{ top: spacings.xl }}>
+                <Banner icon="questionFilled">
                     <Translation id="TR_DEVICE_AUTHENTICITY_OPT_OUT_MODAL_DESCRIPTION_1" />
-                </Paragraph>
-                <Paragraph>
+                </Banner>
+                <Banner icon="warningFilled">
                     <Translation id="TR_DEVICE_AUTHENTICITY_OPT_OUT_MODAL_DESCRIPTION_2" />
-                </Paragraph>
-                <Paragraph>
-                    <Translation id="TR_DEVICE_AUTHENTICITY_OPT_OUT_MODAL_DESCRIPTION_3" />
-                </Paragraph>
+                </Banner>
             </Column>
-        </OptOutModal>
+            <Card margin={{ top: spacings.lg }}>
+                <CheckItem
+                    title={<Translation id="TR_DEVICE_AUTHENTICITY_OPT_OUT_MODAL_CHECKBOX_TITLE" />}
+                    isChecked={isConfirmed}
+                    onClick={() => setIsConfirmed(!isConfirmed)}
+                    data-testid="@device-authenticity/checkbox"
+                />
+            </Card>
+        </NewModal>
     );
 };
