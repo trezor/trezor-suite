165fd0235a7f203d0883c5187042ac361acd970e
Martin Varmuza <mvarmuza@gmail.com>
2024-09-02 08:39:21 +0200

    fix(suite): change misleading copy mentioning browser in desktop debug tips

--- packages/suite/src/components/suite/ConnectDevicePrompt.tsx
+++ packages/suite/src/components/suite/ConnectDevicePrompt.tsx
@@ -9,6 +9,7 @@ import {
     ElevationUp,
     Icon,
 } from '@trezor/components';
+import { isDesktop } from '@trezor/env-utils';
 import { Translation } from 'src/components/suite';
 import { useDevice, useDispatch } from 'src/hooks/suite';
 import { goto } from 'src/actions/suite/routerActions';
@@ -72,7 +73,7 @@ const getMessageId = ({
 }) => {
     switch (prerequisite) {
         case 'transport-bridge':
-            return 'TR_NO_TRANSPORT';
+            return isDesktop() ? 'TR_NO_TRANSPORT_DESKTOP' : 'TR_NO_TRANSPORT';
         case 'device-bootloader':
             return 'TR_DEVICE_CONNECTED_BOOTLOADER';
         default: {
