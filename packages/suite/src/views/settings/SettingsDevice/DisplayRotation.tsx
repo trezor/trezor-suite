60eefa6d7d3fcde3e8cbc203564c250c6487c913
Jan Václavík <jvaclavik@gmail.com>
2024-09-02 13:55:59 +0200

    fix(suite): Display rotation (#14047)

--- packages/suite/src/views/settings/SettingsDevice/DisplayRotation.tsx
+++ packages/suite/src/views/settings/SettingsDevice/DisplayRotation.tsx
@@ -3,17 +3,20 @@ import { analytics, EventType } from '@trezor/suite-analytics';
 
 import { SettingsSectionItem } from 'src/components/settings';
 import { ActionColumn, TextColumn, Translation } from 'src/components/suite';
-import { Button, ButtonGroup } from '@trezor/components';
+import { SelectBar } from '@trezor/components';
 import { useDevice, useDispatch } from 'src/hooks/suite';
 import { applySettings } from 'src/actions/settings/deviceSettingsActions';
 import { SettingsAnchor } from 'src/constants/suite/anchors';
 
-const DISPLAY_ROTATIONS = [
+type RotationValue = 0 | 90 | 180 | 270;
+type Rotation = { label: JSX.Element; value: RotationValue };
+
+const DISPLAY_ROTATIONS: Array<Rotation> = [
     { label: <Translation id="TR_NORTH" />, value: 0 },
     { label: <Translation id="TR_EAST" />, value: 90 },
     { label: <Translation id="TR_SOUTH" />, value: 180 },
     { label: <Translation id="TR_WEST" />, value: 270 },
-] as const;
+];
 
 // features.display_rotation cannot be used to determine support because can be defined for devices not supporting rotation (e.g. T2B1).
 const DEVICES_SUPPORTING_ROTATION = [DeviceModelInternal.T2T1, DeviceModelInternal.T3T1];
@@ -40,26 +43,21 @@ export const DisplayRotation = ({ isDeviceLocked }: DisplayRotationProps) => {
         <SettingsSectionItem anchorId={SettingsAnchor.DisplayRotation}>
             <TextColumn title={<Translation id="TR_DEVICE_SETTINGS_DISPLAY_ROTATION" />} />
             <ActionColumn>
-                <ButtonGroup size="small" isDisabled={isDeviceLocked}>
-                    {DISPLAY_ROTATIONS.map(variant => (
-                        <Button
-                            key={variant.value}
-                            variant={currentRotation === variant.value ? 'primary' : 'tertiary'}
-                            onClick={() => {
-                                dispatch(applySettings({ display_rotation: variant.value }));
-                                analytics.report({
-                                    type: EventType.SettingsDeviceChangeOrientation,
-                                    payload: {
-                                        value: variant.value,
-                                    },
-                                });
-                            }}
-                            data-testid={`@settings/device/rotation-button/${variant.value}`}
-                        >
-                            {variant.label}
-                        </Button>
-                    ))}
-                </ButtonGroup>
+                <SelectBar
+                    isDisabled={isDeviceLocked}
+                    data-testid="@settings/device/rotation-button"
+                    selectedOption={currentRotation ?? undefined}
+                    options={DISPLAY_ROTATIONS}
+                    onChange={(value: number) => {
+                        dispatch(applySettings({ display_rotation: value }));
+                        analytics.report({
+                            type: EventType.SettingsDeviceChangeOrientation,
+                            payload: {
+                                value: value as RotationValue,
+                            },
+                        });
+                    }}
+                />
             </ActionColumn>
         </SettingsSectionItem>
     );
