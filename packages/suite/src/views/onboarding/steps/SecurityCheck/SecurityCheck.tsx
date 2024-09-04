5e6071801df2315155f307ca705f99e00bca521c
Jan Komarek <jan.komarek@satoshilabs.com>
2024-09-04 20:00:08 +0200

    chore(suite): refactor SecurityCheck styles

--- packages/suite/src/views/onboarding/steps/SecurityCheck/SecurityCheck.tsx
+++ packages/suite/src/views/onboarding/steps/SecurityCheck/SecurityCheck.tsx
@@ -4,8 +4,8 @@ import styled, { useTheme } from 'styled-components';
 import { getConnectedDeviceStatus } from '@suite-common/suite-utils';
 import { AcquiredDevice } from '@suite-common/suite-types';
 import { deviceActions, selectDevice, selectDevices } from '@suite-common/wallet-core';
-import { Icon, Tooltip, variables, H2, useElevation } from '@trezor/components';
-import { Elevation, mapElevationToBorder, typography } from '@trezor/theme';
+import { Column, Icon, H2, Text, Tooltip, useElevation } from '@trezor/components';
+import { Elevation, mapElevationToBorder, spacingsPx, typography } from '@trezor/theme';
 import { TREZOR_RESELLERS_URL, TREZOR_URL } from '@trezor/urls';
 
 import { goto } from 'src/actions/suite/routerActions';
@@ -24,33 +24,20 @@ import { DeviceAuthenticity } from './DeviceAuthenticity';
 
 const StyledCard = styled(CollapsibleOnboardingCard)`
     max-width: 840px;
-    padding: 16px;
-`;
-
-const Content = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
+    padding: ${spacingsPx.md};
 `;
 
 const DeviceNameSection = styled.div<{ $elevation: Elevation }>`
     border-bottom: 1px solid ${mapElevationToBorder};
-    margin-top: 8px;
-    padding-bottom: 24px;
+    margin: ${spacingsPx.xs} 0 ${spacingsPx.xl};
+    padding-bottom: ${spacingsPx.xl};
     width: 100%;
 `;
 
 const DeviceName = styled.div`
     ${typography.titleMedium}
     color: ${({ theme }) => theme.backgroundPrimaryDefault};
-    margin-top: 12px;
-`;
-
-// eslint-disable-next-line local-rules/no-override-ds-component
-const StyledH2 = styled(H2)`
-    font-size: 28px;
-    font-weight: ${variables.FONT_WEIGHT.DEMI_BOLD};
-    margin: 24px 0 0;
+    margin-top: ${spacingsPx.sm};
 `;
 
 const Underline = styled.span`
@@ -60,34 +47,30 @@ const Underline = styled.span`
     &::after {
         content: '';
         position: absolute;
-        bottom: 4px;
+        bottom: ${spacingsPx.xxs};
         left: 0;
         right: 0;
-        border-bottom: 1px dashed ${({ theme }) => theme.legacy.TYPE_LIGHT_GREY};
+        border-bottom: 1px dashed ${({ theme }) => theme.textSubdued};
         width: 100%;
     }
 `;
 
 const TimeEstimateWrapper = styled.div`
-    font-size: ${variables.FONT_SIZE.TINY};
-    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
+    ${typography.label}
     display: flex;
     justify-content: center;
     align-items: center;
+    gap: ${spacingsPx.xxxs};
     opacity: 0.66;
     margin-top: 1px;
-`;
-
-const IconWrapper = styled.div`
-    margin: 0 1px 2px 6px;
 `;
 
 const Buttons = styled.div<{ $elevation: Elevation }>`
     display: flex;
     flex-wrap: wrap;
-    gap: 24px;
+    gap: ${spacingsPx.xl};
     justify-content: space-between;
-    padding-top: 24px;
+    padding-top: ${spacingsPx.xl};
     border-top: 2px solid ${mapElevationToBorder};
     width: 100%;
 `;
@@ -100,19 +83,13 @@ const SecurityCheckButtonWithSecondLine = styled(StyledSecurityCheckButton)`
     flex-direction: column;
 `;
 
-const Text = styled.div`
-    color: ${({ theme }) => theme.legacy.TYPE_LIGHT_GREY};
-    font-size: ${variables.FONT_SIZE.NORMAL};
-    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
-`;
-
 const StyledTrezorLink = styled(TrezorLink)`
-    color: ${({ theme }) => theme.legacy.TYPE_LIGHT_GREY};
+    color: inherit;
 `;
 
-// eslint-disable-next-line local-rules/no-override-ds-component
-const StyledTooltip = styled(Tooltip)`
+const TooltipWrapper = styled.span`
     display: inline-block;
+    position: relative;
 `;
 
 const firmwareInstalledChecklist = [
@@ -151,13 +128,15 @@ const getNoFirmwareChecklist = (isMobileLayout: boolean) =>
                     id="TR_ONBOARDING_DEVICE_CHECK_1"
                     values={{
                         strong: chunks => (
-                            <StyledTooltip
-                                placement={isMobileLayout ? 'top' : 'left'}
-                                title={<Translation id="TR_HOLOGRAM_STEP_HEADING" />}
-                                content={<Hologram />}
-                            >
-                                <Underline>{chunks}</Underline>
-                            </StyledTooltip>
+                            <TooltipWrapper>
+                                <Tooltip
+                                    placement={isMobileLayout ? 'top' : 'left'}
+                                    title={<Translation id="TR_HOLOGRAM_STEP_HEADING" />}
+                                    content={<Hologram />}
+                                >
+                                    <Underline>{chunks}</Underline>
+                                </Tooltip>
+                            </TooltipWrapper>
                         ),
                     }}
                 />
@@ -236,9 +215,9 @@ const SecurityCheckContent = ({
         <SecurityCheckFail useSoftMessaging goBack={toggleView} />
     ) : (
         <SecurityCheckLayout>
-            <Content>
+            <Column alignItems="flex-start">
                 <DeviceNameSection $elevation={elevation}>
-                    <Text>
+                    <Text variant="tertiary">
                         <Translation id="TR_YOU_HAVE_CONNECTED" />
                     </Text>
                     <DeviceName>{device?.name}</DeviceName>
@@ -246,11 +225,11 @@ const SecurityCheckContent = ({
                         <Translation id="TR_CONNECTED_DIFFERENT_DEVICE" />
                     </OnboardingButtonSkip>
                 </DeviceNameSection>
-                <StyledH2>
+                <H2>
                     <Translation id={headingText} />
-                </StyledH2>
+                </H2>
                 <SecurityChecklist items={checklistItems} />
-            </Content>
+            </Column>
             <Buttons $elevation={elevation}>
                 <StyledSecurityCheckButton variant="tertiary" onClick={toggleView}>
                     <Translation id={secondaryButtonText} />
@@ -269,9 +248,7 @@ const SecurityCheckContent = ({
                     >
                         <Translation id={primaryButtonTopText} />
                         <TimeEstimateWrapper>
-                            <IconWrapper>
-                                <Icon size={12} name="clock" color={theme.iconOnPrimary} />
-                            </IconWrapper>
+                            <Icon size={12} name="clock" color={theme.iconOnPrimary} />
                             <Translation id="TR_TAKES_N_MINUTES" />
                         </TimeEstimateWrapper>
                     </SecurityCheckButtonWithSecondLine>
