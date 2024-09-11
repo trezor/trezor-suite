1a7151936ab82ed9d55a81459591f8eed6fa8e6c
Martin Vytick Vytrhlík <vytick@gmail.com>
2024-09-11 22:26:12 +0200

    fix(suite-native): no loading when coin enabling is set but no coin is found

--- suite-native/device/src/index.ts
+++ suite-native/device/src/index.ts
@@ -2,7 +2,6 @@ export * from './middlewares/deviceMiddleware';
 export * from './middlewares/buttonRequestMiddleware';
 export * from './hooks/useHandleDeviceConnection';
 export * from './hooks/useDetectDeviceError';
-export * from './hooks/useDelayedNavigation';
 export * from './hooks/useReportDeviceConnectToAnalytics';
 export * from './screens/DeviceInfoModalScreen';
 export * from './components/ConnectDeviceAnimation';
