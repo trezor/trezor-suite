fe63f2e6335693714fc2c3482884439620fd60b9
Matej Kriz <matej@matejkriz.cz>
2024-09-12 15:37:18 +0200

    fix(suite-native): prevent extra line break on fiat and crypto amounts on android
    - Resetting letter spacing on Android for amount values looks like ultimate workaround until the issue is fixed on RN side.

--- suite-native/formatters/src/components/AmountText.tsx
+++ suite-native/formatters/src/components/AmountText.tsx
@@ -1,14 +1,31 @@
 import { DiscreetText, Text, TextProps } from '@suite-native/atoms';
+import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
+import { isAndroid } from '@trezor/env-utils';
 
 type AmountTextProps = {
     isDiscreetText?: boolean;
     value: string | null;
 } & TextProps;
 
-export const AmountText = ({ value, isDiscreetText = true, ...textProps }: AmountTextProps) => {
-    if (isDiscreetText) {
-        return <DiscreetText {...textProps}>{value}</DiscreetText>;
-    }
+const amountTextStyle = prepareNativeStyle(_ => ({
+    // Because of this RN issue https://github.com/facebook/react-native/issues/46436
+    // turning off custom letter spacing for amounts on Android.
+    extend: {
+        condition: isAndroid(),
+        style: {
+            letterSpacing: 0,
+        },
+    },
+}));
 
-    return <Text {...textProps}>{value}</Text>;
+export const AmountText = ({ value, isDiscreetText = true, ...textProps }: AmountTextProps) => {
+    const { applyStyle } = useNativeStyles();
+
+    const TextComponent = isDiscreetText ? DiscreetText : Text;
+
+    return (
+        <TextComponent style={applyStyle(amountTextStyle)} {...textProps}>
+            {value}
+        </TextComponent>
+    );
 };
