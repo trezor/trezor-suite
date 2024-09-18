8c53a1df854bd43a22c50521e4bdb478d111f216
Adam Havel <adam.havel@satoshilabs.com>
2024-09-18 10:31:22 +0200

    chore(suite): update DiscoveryLoader and PassphraseModal to use NewModal component

--- packages/suite/src/components/suite/modals/ReduxModal/DeviceContextModal/PassphraseModal.tsx
+++ packages/suite/src/components/suite/modals/ReduxModal/DeviceContextModal/PassphraseModal.tsx
@@ -1,6 +1,6 @@
 import { useCallback } from 'react';
 
-import { Text } from '@trezor/components';
+import { Text, NewModal } from '@trezor/components';
 import {
     selectIsDiscoveryAuthConfirmationRequired,
     onPassphraseSubmit,
@@ -10,7 +10,6 @@ import { useSelector, useDispatch } from 'src/hooks/suite';
 import { Translation } from 'src/components/suite';
 import type { TrezorDevice } from 'src/types/suite';
 import { OpenGuideFromTooltip } from 'src/components/guide';
-import { SwitchDeviceRenderer } from 'src/views/suite/SwitchDevice/SwitchDeviceRenderer';
 import { CardWithDevice } from 'src/views/suite/SwitchDevice/CardWithDevice';
 import { PassphraseDescription } from './PassphraseDescription';
 import { PassphraseWalletConfirmation } from './PassphraseWalletConfirmation';
@@ -19,6 +18,7 @@ import TrezorConnect from '@trezor/connect';
 import { spacingsPx } from '@trezor/theme';
 import styled from 'styled-components';
 import { PassphraseTypeCard } from '@trezor/product-components';
+import { SwitchDeviceModal } from 'src/views/suite/SwitchDevice/SwitchDeviceModal';
 
 const MarginContainer = styled.div`
     margin: 0 ${spacingsPx.sm};
@@ -80,75 +80,77 @@ export const PassphraseModal = ({ device }: PassphraseModalProps) => {
         );
     }
 
-    // "view-only" is active, device is reconnected and you fired an action that needs passphrase (e.g. add coin, show receive address)
-    if (hasDeviceState) {
-        return (
-            <SwitchDeviceRenderer isCancelable onCancel={onEnterPassphraseDialogCancel}>
-                <CardWithDevice
-                    onCancel={onEnterPassphraseDialogCancel}
-                    device={device}
-                    isFullHeaderVisible
-                    icon="x"
-                >
-                    <MarginContainer>
-                        <PassphraseHeading>
-                            <Translation id="TR_CONFIRM_PASSPHRASE" />
-                        </PassphraseHeading>
-
-                        <Text>
-                            <Translation id="TR_CONFIRM_PASSPHRASE_WITHOUT_ADVICE_DESCRIPTION" />
-                        </Text>
-                    </MarginContainer>
-                    <PassphraseTypeCard
-                        submitLabel={<Translation id="TR_CONFIRM" />}
-                        type="hidden"
-                        singleColModal
-                        offerPassphraseOnDevice={onDeviceOffer}
-                        onSubmit={onSubmit}
-                        deviceModel={deviceModel ?? undefined}
-                        learnMoreTooltipOnClick={
-                            <OpenGuideFromTooltip
-                                data-testid="@tooltip/guideAnchor"
-                                id="/1_initialize-and-secure-your-trezor/6_passphrase.md"
-                            />
-                        }
-                    />
-                </CardWithDevice>
-            </SwitchDeviceRenderer>
-        );
-    }
-
-    // first step of adding passphrase wallet from switch device modal
     return (
-        <SwitchDeviceRenderer isCancelable onCancel={onEnterPassphraseDialogCancel}>
-            <CardWithDevice
-                onCancel={onEnterPassphraseDialogCancel}
-                device={device}
-                onBackButtonClick={onEnterPassphraseDialogBack}
-                isFullHeaderVisible
-            >
-                <MarginContainer>
-                    <PassphraseHeading>
-                        <Translation id="TR_PASSPHRASE_HIDDEN_WALLET" />
-                    </PassphraseHeading>
+        <NewModal.Backdrop
+            onClick={onEnterPassphraseDialogCancel}
+            alignment={{ x: 'left', y: 'top' }}
+            padding={5}
+        >
+            <SwitchDeviceModal isAnimationEnabled onCancel={onEnterPassphraseDialogCancel}>
+                {hasDeviceState ? (
+                    // "view-only" is active, device is reconnected and you fired an action that needs passphrase (e.g. add coin, show receive address)
+                    <CardWithDevice
+                        onCancel={onEnterPassphraseDialogCancel}
+                        device={device}
+                        isFullHeaderVisible
+                        icon="x"
+                    >
+                        <MarginContainer>
+                            <PassphraseHeading>
+                                <Translation id="TR_CONFIRM_PASSPHRASE" />
+                            </PassphraseHeading>
 
-                    <PassphraseDescription />
-                </MarginContainer>
-                <PassphraseTypeCard
-                    submitLabel={<Translation id="TR_ACCESS_HIDDEN_WALLET" />}
-                    type="hidden"
-                    singleColModal
-                    offerPassphraseOnDevice={onDeviceOffer}
-                    onSubmit={onSubmit}
-                    deviceModel={deviceModel ?? undefined}
-                    learnMoreTooltipOnClick={
-                        <OpenGuideFromTooltip
-                            data-testid="@tooltip/guideAnchor"
-                            id="/1_initialize-and-secure-your-trezor/6_passphrase.md"
+                            <Text>
+                                <Translation id="TR_CONFIRM_PASSPHRASE_WITHOUT_ADVICE_DESCRIPTION" />
+                            </Text>
+                        </MarginContainer>
+                        <PassphraseTypeCard
+                            submitLabel={<Translation id="TR_CONFIRM" />}
+                            type="hidden"
+                            singleColModal
+                            offerPassphraseOnDevice={onDeviceOffer}
+                            onSubmit={onSubmit}
+                            deviceModel={deviceModel ?? undefined}
+                            learnMoreTooltipOnClick={
+                                <OpenGuideFromTooltip
+                                    data-testid="@tooltip/guideAnchor"
+                                    id="/1_initialize-and-secure-your-trezor/6_passphrase.md"
+                                />
+                            }
                         />
-                    }
-                />
-            </CardWithDevice>
-        </SwitchDeviceRenderer>
+                    </CardWithDevice>
+                ) : (
+                    // first step of adding passphrase wallet from switch device modal
+                    <CardWithDevice
+                        onCancel={onEnterPassphraseDialogCancel}
+                        device={device}
+                        onBackButtonClick={onEnterPassphraseDialogBack}
+                        isFullHeaderVisible
+                    >
+                        <MarginContainer>
+                            <PassphraseHeading>
+                                <Translation id="TR_PASSPHRASE_HIDDEN_WALLET" />
+                            </PassphraseHeading>
+
+                            <PassphraseDescription />
+                        </MarginContainer>
+                        <PassphraseTypeCard
+                            submitLabel={<Translation id="TR_ACCESS_HIDDEN_WALLET" />}
+                            type="hidden"
+                            singleColModal
+                            offerPassphraseOnDevice={onDeviceOffer}
+                            onSubmit={onSubmit}
+                            deviceModel={deviceModel ?? undefined}
+                            learnMoreTooltipOnClick={
+                                <OpenGuideFromTooltip
+                                    data-testid="@tooltip/guideAnchor"
+                                    id="/1_initialize-and-secure-your-trezor/6_passphrase.md"
+                                />
+                            }
+                        />
+                    </CardWithDevice>
+                )}
+            </SwitchDeviceModal>
+        </NewModal.Backdrop>
     );
 };
