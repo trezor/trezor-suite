8f4a8905ac6e7d82fbbc64f7e1fdb825403db4cc
Adam Havel <adam.havel@satoshilabs.com>
2024-09-18 10:31:22 +0200

    chore(suite): update DiscoveryLoader and PassphraseModal to use NewModal component

--- packages/suite/src/components/suite/modals/ModalSwitcher/DiscoveryLoader.tsx
+++ packages/suite/src/components/suite/modals/ModalSwitcher/DiscoveryLoader.tsx
@@ -1,35 +1,30 @@
-import styled from 'styled-components';
-import { H3, Spinner } from '@trezor/components';
+import { H3, Spinner, NewModal, Column } from '@trezor/components';
 import { Translation } from 'src/components/suite';
 import { CardWithDevice } from 'src/views/suite/SwitchDevice/CardWithDevice';
-import { SwitchDeviceRenderer } from 'src/views/suite/SwitchDevice/SwitchDeviceRenderer';
+import { SwitchDeviceModal } from 'src/views/suite/SwitchDevice/SwitchDeviceModal';
 import { useSelector } from 'src/hooks/suite';
 import { selectDevice } from '@suite-common/wallet-core';
-import { spacingsPx } from '@trezor/theme';
-
-const Expand = styled.div`
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    justify-content: center;
-    align-items: center;
-    margin: ${spacingsPx.xxxl} 0 ${spacingsPx.xl};
-`;
+import { spacings } from '@trezor/theme';
 
 export const DiscoveryLoader = () => {
     const device = useSelector(selectDevice);
     if (!device) return null;
 
     return (
-        <SwitchDeviceRenderer isCancelable={false} data-testid="@discovery/loader">
-            <CardWithDevice device={device} isFullHeaderVisible={false}>
-                <Expand>
-                    <Spinner size={80} isGrey={false} margin={{ bottom: 48 }} />
-                    <H3 align="center">
-                        <Translation id="TR_COIN_DISCOVERY_LOADER_DESCRIPTION" />
-                    </H3>
-                </Expand>
-            </CardWithDevice>
-        </SwitchDeviceRenderer>
+        <NewModal.Backdrop alignment={{ x: 'left', y: 'top' }} padding={5}>
+            <SwitchDeviceModal data-testid="@discovery/loader">
+                <CardWithDevice device={device} isFullHeaderVisible={false}>
+                    <Column
+                        justifyContent="center"
+                        margin={{ top: spacings.xxxl, bottom: spacings.xl }}
+                    >
+                        <Spinner size={80} isGrey={false} />
+                        <H3 align="center" margin={{ top: spacings.xxxxl }}>
+                            <Translation id="TR_COIN_DISCOVERY_LOADER_DESCRIPTION" />
+                        </H3>
+                    </Column>
+                </CardWithDevice>
+            </SwitchDeviceModal>
+        </NewModal.Backdrop>
     );
 };
