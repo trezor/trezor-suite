d456aef31bce9e762ceeeabca7e79b894d88923f
Marek Polak <marek.polak@satoshilabs.com>
2024-09-19 16:02:55 +0200

    feat(utils): add `lockId` to `getSynchronize`

--- packages/utils/src/getSynchronize.ts
+++ packages/utils/src/getSynchronize.ts
@@ -1,6 +1,8 @@
 /**
  * Ensures that all async actions passed to the returned function are called
  * immediately one after another, without interfering with each other.
+ * Optionally, it also takes `lockId` param, in which case only actions with
+ * the same lock id are blocking each other.
  *
  * Example:
  *
@@ -8,23 +10,28 @@
  * const synchronize = getSynchronize();
  * synchronize(() => asyncAction1());
  * synchronize(() => asyncAction2());
+ * synchronize(() => asyncAction3(), 'differentLockId');
  * ```
  */
 export const getSynchronize = () => {
-    let lock: Promise<unknown> | undefined;
+    const DEFAULT_ID = Symbol();
+    const locks: Record<keyof any, Promise<unknown>> = {};
 
-    return <T>(action: () => T): T extends Promise<unknown> ? T : Promise<T> => {
-        const newLock = (lock ?? Promise.resolve())
+    return <T>(
+        action: () => T,
+        lockId: keyof any = DEFAULT_ID,
+    ): T extends Promise<unknown> ? T : Promise<T> => {
+        const newLock = (locks[lockId] ?? Promise.resolve())
             .catch(() => {})
             .then(action)
             .finally(() => {
-                if (lock === newLock) {
-                    lock = undefined;
+                if (locks[lockId] === newLock) {
+                    delete locks[lockId];
                 }
             });
-        lock = newLock;
+        locks[lockId] = newLock;
 
-        return lock as any;
+        return newLock as any;
     };
 };
 
