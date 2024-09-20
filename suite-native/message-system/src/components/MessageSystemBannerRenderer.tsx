21615c13c2a7a98d29162be1d8adad38f3dff01d
Martin Vytick Vytrhlík <vytick@gmail.com>
2024-09-20 12:59:05 +0200

    tst

--- suite-native/message-system/src/components/MessageSystemBannerRenderer.tsx
+++ suite-native/message-system/src/components/MessageSystemBannerRenderer.tsx
@@ -4,8 +4,13 @@ import { A } from '@mobily/ts-belt';
 
 import { VStack } from '@suite-native/atoms';
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
-import { selectActiveBannerMessages } from '@suite-common/message-system';
+import {
+    selectActiveBannerMessages,
+    selectMessageSystemConfig,
+} from '@suite-common/message-system';
 import { useOfflineBannerAwareSafeAreaInsets } from '@suite-native/connection-status';
+import { selectDeviceEnabledDiscoveryNetworkSymbols } from '@suite-native/discovery';
+import { Message } from '@suite-common/suite-types';
 
 import { MessageBanner } from './MessageBanner';
 
@@ -20,7 +25,59 @@ export const MessageSystemBannerRenderer = () => {
     const { top: topSafeAreaInset } = useOfflineBannerAwareSafeAreaInsets();
 
     const activeBannerMessages = useSelector(selectActiveBannerMessages);
+    const enabledNetworks = useSelector(selectDeviceEnabledDiscoveryNetworkSymbols);
     const topInset = A.isNotEmpty(activeBannerMessages) ? topSafeAreaInset : 0;
+    const configFull = useSelector(selectMessageSystemConfig);
+
+    const config = `sequence: ${configFull?.sequence}, t: ${configFull?.timestamp}, content: ${configFull?.actions.map(a => `| ${a.message.content.en.substring(0, 25)}|`)}`;
+
+    let messages: Message[] = [
+        {
+            id: 'xbfce56e-6b1b-4f88-8acc-38b9bc5dabeb',
+            priority: 93,
+            dismissible: true,
+            variant: 'critical',
+            category: 'banner',
+            content: {
+                'en-GB': `[${enabledNetworks}]`,
+                en: `[${enabledNetworks}]`,
+                es: `[${enabledNetworks}]`,
+                cs: `[${enabledNetworks}]`,
+                ru: `[${enabledNetworks}]`,
+                ja: `[${enabledNetworks}]`,
+                de: `[${enabledNetworks}]`,
+                fr: `[${enabledNetworks}]`,
+                it: `[${enabledNetworks}]`,
+                pt: `[${enabledNetworks}]`,
+                tr: `[${enabledNetworks}]`,
+                uk: `[${enabledNetworks}]`,
+                hu: `[${enabledNetworks}]`,
+            },
+        },
+        {
+            id: 'ddfce37e-6b9b-4f88-8acc-38b9bc5dabeb',
+            priority: 94,
+            dismissible: true,
+            variant: 'info',
+            category: 'banner',
+            content: {
+                'en-GB': `(${config})`,
+                en: `(${config})`,
+                es: `(${config})`,
+                cs: `(${config})`,
+                ru: `(${config})`,
+                ja: `(${config})`,
+                de: `(${config})`,
+                fr: `(${config})`,
+                it: `(${config})`,
+                pt: `(${config})`,
+                tr: `(${config})`,
+                uk: `(${config})`,
+                hu: `(${config})`,
+            },
+        },
+    ];
+    activeBannerMessages.map(m => messages.push(m));
 
     return (
         <VStack
@@ -29,7 +86,7 @@ export const MessageSystemBannerRenderer = () => {
                 topSafeAreaInset: topInset,
             })}
         >
-            {activeBannerMessages.map(message => (
+            {messages.map(message => (
                 <MessageBanner key={message.id} message={message} />
             ))}
         </VStack>
