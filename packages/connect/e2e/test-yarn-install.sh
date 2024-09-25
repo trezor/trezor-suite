d67aec480904f67bedc2e675d9e912c6fba65b3b
Martin Varmuza <mvarmuza@gmail.com>
2024-09-25 09:17:17 +0200

    test(connect): install test: extend with beta channel

--- packages/connect/e2e/test-yarn-install.sh
+++ packages/connect/e2e/test-yarn-install.sh
@@ -16,9 +16,9 @@ npm init -y
 touch yarn.lock
 
 # install connect package
-yarn add @trezor/connect
+yarn add @trezor/connect@"$1"
 # prepare minimal typescript implementation
-echo import TrezorConnect from \"@trezor/connect\" > index.ts
+echo import TrezorConnect from \"@trezor/connect\" >index.ts
 
 # compile with typescript
 yarn add typescript@5.3.2
