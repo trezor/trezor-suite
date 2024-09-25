acb63e0163fec203965392e871c1c98c889303d8
Martin Varmuza <mvarmuza@gmail.com>
2024-09-25 13:09:50 +0200

    chore(transport): get rid of duplicated code by moving it to parent class

--- packages/transport/src/transports/nodeusb.ts
+++ packages/transport/src/transports/nodeusb.ts
@@ -1,7 +1,6 @@
 import { WebUSB } from 'usb';
 import { AbstractTransportParams } from './abstract';
 import { AbstractApiTransport } from './abstractApi';
-import { SessionsBackground } from '../sessions/background';
 import { UsbApi } from '../api/usb';
 
 // notes:
@@ -10,8 +9,6 @@ import { UsbApi } from '../api/usb';
 
 export class NodeUsbTransport extends AbstractApiTransport {
     public name = 'NodeUsbTransport' as const;
-
-    private readonly sessionsBackground = new SessionsBackground();
 
     constructor(params: AbstractTransportParams) {
         const { messages, logger, debugLink } = params;
@@ -28,34 +25,9 @@ export class NodeUsbTransport extends AbstractApiTransport {
         });
     }
 
-    public init() {
-        return this.scheduleAction(async () => {
-            // in nodeusb there is no synchronization yet. this is a followup and needs to be decided
-            // so far, sessionsClient has direct access to sessionBackground
-            this.sessionsClient.init({
-                requestFn: args => this.sessionsBackground.handleMessage(args),
-                registerBackgroundCallbacks: () => {},
-            });
-
-            this.sessionsBackground.on('descriptors', descriptors => {
-                this.sessionsClient.emit('descriptors', descriptors);
-            });
-
-            const handshakeRes = await this.sessionsClient.handshake();
-            this.stopped = !handshakeRes.success;
-
-            return handshakeRes;
-        });
-    }
-
     public listen() {
         this.api.listen();
 
         return super.listen();
-    }
-
-    public stop() {
-        super.stop();
-        this.sessionsBackground.dispose();
     }
 }
--- packages/transport/src/transports/udp.ts
+++ packages/transport/src/transports/udp.ts
@@ -3,11 +3,9 @@ import { AbstractTransportParams } from './abstract';
 import { UdpApi } from '../api/udp';
 import { AbstractApiTransport } from './abstractApi';
 
-import { SessionsBackground } from '../sessions/background';
 export class UdpTransport extends AbstractApiTransport {
     public name = 'UdpTransport' as const;
     private enumerateTimeout: ReturnType<typeof setTimeout> | undefined;
-    private readonly sessionsBackground = new SessionsBackground();
 
     constructor(params: AbstractTransportParams) {
         const { messages, logger, debugLink } = params;
@@ -16,25 +14,6 @@ export class UdpTransport extends AbstractApiTransport {
             messages,
             api: new UdpApi({ logger, debugLink }),
             logger,
-        });
-    }
-
-    public init() {
-        return this.scheduleAction(async () => {
-            // in udp there is no synchronization.
-            this.sessionsClient.init({
-                requestFn: args => this.sessionsBackground.handleMessage(args),
-                registerBackgroundCallbacks: () => {},
-            });
-
-            this.sessionsBackground.on('descriptors', descriptors => {
-                this.sessionsClient.emit('descriptors', descriptors);
-            });
-
-            const handshakeRes = await this.sessionsClient.handshake();
-            this.stopped = !handshakeRes.success;
-
-            return handshakeRes;
         });
     }
 
@@ -49,7 +28,6 @@ export class UdpTransport extends AbstractApiTransport {
             clearTimeout(this.enumerateTimeout);
             this.enumerateTimeout = undefined;
         }
-        this.sessionsBackground.dispose();
 
         return super.stop();
     }
