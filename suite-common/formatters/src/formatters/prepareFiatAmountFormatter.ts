1a0f48b915e3b5f1ac728794a3387658c40ba6fc
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2024-09-26 15:50:40 +0200

    chore(formatters): extract functions to remove let

--- suite-common/formatters/src/formatters/prepareFiatAmountFormatter.ts
+++ suite-common/formatters/src/formatters/prepareFiatAmountFormatter.ts
@@ -10,34 +10,41 @@ export type FiatAmountFormatterDataContext<T> = {
     [K in keyof T]: T[K];
 };
 
+const handleBigNumberFormatting = (
+    value: string | number,
+    dataContext: FiatAmountFormatterDataContext<FormatNumberOptions>,
+    config: FormatterConfig,
+) => {
+    const { intl, fiatCurrency } = config;
+    const { style, currency, minimumFractionDigits, maximumFractionDigits } = dataContext;
+    const fiatValue = new BigNumber(value);
+    const currencyForDisplay = currency ?? fiatCurrency;
+
+    if (fiatValue.gt(Number.MAX_SAFE_INTEGER)) {
+        return `${value} ${currencyForDisplay}`;
+    }
+
+    return intl.formatNumber(fiatValue.toNumber(), {
+        ...dataContext,
+        style: style || 'currency',
+        currency: currencyForDisplay,
+        minimumFractionDigits: minimumFractionDigits ?? 2,
+        maximumFractionDigits: maximumFractionDigits ?? 2,
+    });
+};
+
 export const prepareFiatAmountFormatter = (config: FormatterConfig) =>
     makeFormatter<
         string | number,
         string | null,
         FiatAmountFormatterDataContext<FormatNumberOptions>
     >((value, dataContext, shouldRedactNumbers) => {
-        const { intl, fiatCurrency } = config;
-        const { style, currency, minimumFractionDigits, maximumFractionDigits } = dataContext;
         const fiatValue = new BigNumber(value);
-        const currencyForDisplay = currency ?? fiatCurrency;
-
         if (fiatValue.isNaN()) {
             return null;
         }
 
-        let formattedValue: string = '';
-
-        if (fiatValue.gt(Number.MAX_SAFE_INTEGER)) {
-            formattedValue = `${value} ${currencyForDisplay}`;
-        } else {
-            formattedValue = intl.formatNumber(fiatValue.toNumber(), {
-                ...dataContext,
-                style: style || 'currency',
-                currency: currencyForDisplay,
-                minimumFractionDigits: minimumFractionDigits ?? 2,
-                maximumFractionDigits: maximumFractionDigits ?? 2,
-            });
-        }
+        const formattedValue = handleBigNumberFormatting(value, dataContext, config);
 
         return shouldRedactNumbers ? redactNumericalSubstring(formattedValue) : formattedValue;
     }, 'FiatAmountFormatter');
