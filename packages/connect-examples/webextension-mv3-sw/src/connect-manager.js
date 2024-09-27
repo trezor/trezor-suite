11a81fc80466a3e429a9e36b02b1de1ed97330cd
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2024-09-27 08:48:19 +0200

    fix(connect-example): handle new tab button

--- packages/connect-examples/webextension-mv3-sw/src/connect-manager.js
+++ packages/connect-examples/webextension-mv3-sw/src/connect-manager.js
@@ -34,3 +34,8 @@ document.getElementById('get-features').addEventListener('click', () => {
         }
     });
 });
+
+const newTabButton = document.getElementById('tab');
+newTabButton.addEventListener('click', () => {
+    chrome.tabs.create({ url: 'connect-manager.html' });
+});
