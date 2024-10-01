33cc06a50c9f8082508c9558c456bfbb3891b7df
Adam Havel <adam.havel@protonmail.com>
2024-10-01 10:52:44 +0200

    chore(suite): update AuthenticateDeviceModal to use NewModal component

--- packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/AuthenticateDeviceModal.tsx
+++ packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/AuthenticateDeviceModal.tsx
@@ -1,13 +1,21 @@
 import { useState } from 'react';
 
 import { checkDeviceAuthenticityThunk } from '@suite-common/device-authenticity';
-import { Button } from '@trezor/components';
+import { NewModal, List, IconName, Paragraph, Icon } from '@trezor/components';
+import { selectSelectedDeviceAuthenticity } from '@suite-common/wallet-core';
+import { TranslationKey } from '@suite-common/intl-types';
+import { spacings } from '@trezor/theme';
 
 import { onCancel, openModal } from 'src/actions/suite/modalActions';
-import { DeviceAuthenticationExplainer, Modal, Translation } from 'src/components/suite';
+import { Translation } from 'src/components/suite';
 import { useDispatch, useSelector } from 'src/hooks/suite';
 import { selectIsDebugModeActive } from 'src/reducers/suite/suiteReducer';
-import { selectSelectedDeviceAuthenticity } from '@suite-common/wallet-core';
+
+const items: Array<{ icon: IconName; text: TranslationKey }> = [
+    { icon: 'shieldCheck', text: 'TR_DEVICE_AUTHENTICITY_ITEM_1' },
+    { icon: 'chip', text: 'TR_DEVICE_AUTHENTICITY_ITEM_2' },
+    { icon: 'checklist', text: 'TR_DEVICE_AUTHENTICITY_ITEM_3' },
+];
 
 export const AuthenticateDeviceModal = () => {
     const [isLoading, setIsLoading] = useState(false);
@@ -30,17 +38,40 @@ export const AuthenticateDeviceModal = () => {
     const handleClose = () => dispatch(onCancel());
 
     return (
-        <Modal
-            isCancelable
+        <NewModal
             onCancel={handleClose}
             heading={<Translation id="TR_LETS_CHECK_YOUR_DEVICE" />}
-            bottomBarComponents={
-                <Button onClick={handleClick} isDisabled={isLoading} isLoading={isLoading}>
-                    <Translation id="TR_START_CHECK" />
-                </Button>
+            bottomContent={
+                <>
+                    <NewModal.Button
+                        onClick={handleClick}
+                        isDisabled={isLoading}
+                        isLoading={isLoading}
+                    >
+                        <Translation id="TR_START_CHECK" />
+                    </NewModal.Button>
+                    <NewModal.Button variant="tertiary" onClick={handleClose}>
+                        <Translation id="TR_CANCEL" />
+                    </NewModal.Button>
+                </>
             }
         >
-            <DeviceAuthenticationExplainer />
-        </Modal>
+            <List
+                gap={spacings.xl}
+                bulletGap={spacings.xl}
+                margin={{ top: spacings.xs, bottom: spacings.sm }}
+            >
+                {items.map(({ icon, text }) => (
+                    <List.Item
+                        key={icon}
+                        bulletComponent={<Icon name={icon} size="extraLarge" variant="primary" />}
+                    >
+                        <Paragraph variant="tertiary">
+                            <Translation id={text} />
+                        </Paragraph>
+                    </List.Item>
+                ))}
+            </List>
+        </NewModal>
     );
 };
