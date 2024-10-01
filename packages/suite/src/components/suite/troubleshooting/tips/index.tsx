21d9e5057bb733ad28037bb74cb2ea01542af069
Martin Varmuza <mvarmuza@gmail.com>
2024-10-01 17:53:49 +0200

    chore(suite): move 'reconnect troubleshooting tip' to a shared file

--- packages/suite/src/components/suite/troubleshooting/tips/index.tsx
+++ packages/suite/src/components/suite/troubleshooting/tips/index.tsx
@@ -1,4 +1,4 @@
-import { isWeb, isLinux, isAndroid } from '@trezor/env-utils';
+import { isWeb, isDesktop, isLinux, isAndroid } from '@trezor/env-utils';
 
 import { Translation } from 'src/components/suite/Translation';
 
@@ -56,4 +56,18 @@ export const TROUBLESHOOTING_TIP_UDEV = {
     heading: <Translation id="TR_UDEV_DOWNLOAD_TITLE" />,
     description: <UdevDescription />,
     hide: !isLinux(),
+};
+
+export const TROUBLESHOOTING_TIP_RECONNECT = {
+    key: 'device-reconnect',
+    heading: <Translation id="TR_RECONNECT_YOUR_DEVICE" />,
+    description: (
+        <Translation
+            id={
+                isDesktop()
+                    ? 'TR_RECONNECT_DEVICE_DESCRIPTION_DESKTOP'
+                    : 'TR_RECONNECT_DEVICE_DESCRIPTION'
+            }
+        />
+    ),
 };
