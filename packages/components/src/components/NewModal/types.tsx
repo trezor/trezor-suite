20df59fe00b27a34e60c706f4b4033b8f845223d
Peter Sanderson <dierk.deidel@protonmail.com>
2024-10-02 12:22:24 +0200

    feat: NewModal can have color (to override the variant), also CSSColor is userd for Icon color instead of a string

--- packages/components/src/components/NewModal/types.tsx
+++ packages/components/src/components/NewModal/types.tsx
@@ -1,7 +1,16 @@
 import { UIVariant, UISize, UIHorizontalAlignment, UIVerticalAlignment } from '../../config/types';
+import { CSSColor } from '@trezor/theme';
 
 export type NewModalVariant = Extract<UIVariant, 'primary' | 'warning' | 'destructive'> | 'purple';
 
 export type NewModalSize = Extract<UISize, 'large' | 'medium' | 'small' | 'tiny'>;
 
 export type NewModalAlignment = { x: UIHorizontalAlignment; y: UIVerticalAlignment };
+
+export type NewModalExclusiveColorOrVariant =
+    | { variant?: NewModalVariant; color?: undefined }
+    | {
+          variant?: undefined;
+          /** @deprecated Use only is case of absolute desperation. Prefer using `variant`. */
+          color?: { icon: CSSColor; background: CSSColor };
+      };
