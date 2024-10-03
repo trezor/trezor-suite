9443c19914af1e7baa6149c1fcacdbd398389e6b
MiroslavProchazka <miroslav.prochazka9@gmail.com>
2024-10-03 15:42:40 +0200

    feat(connect): add t3w1 placeholders for auth. keys

--- packages/connect/src/data/deviceAuthenticityConfig.ts
+++ packages/connect/src/data/deviceAuthenticityConfig.ts
@@ -112,4 +112,12 @@ export const deviceAuthenticityConfig: DeviceAuthenticityConfig = {
             ],
         },
     },
+    T3W1: {
+        rootPubKeys: ['you shall not pass'],
+        caPubKeys: ['you shall not pass'],
+        debug: {
+            rootPubKeys: ['you shall not pass'],
+            caPubKeys: ['you shall not pass'],
+        },
+    },
 };
