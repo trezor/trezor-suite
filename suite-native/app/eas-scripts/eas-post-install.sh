db95a5cbd0739f365d68787609d16305f161c4b5
Matej Kriz <matej@matejkriz.cz>
2024-10-03 17:24:38 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! chore(suite-native): add github workflow for preview builds and updates

--- suite-native/app/eas-scripts/eas-post-install.sh
+++ suite-native/app/eas-scripts/eas-post-install.sh
@@ -2,7 +2,9 @@
 
 yarn workspace @suite-common/message-system sign-config
 
-EXPO_PUBLIC_RUNTIME_VERSION=$(npx @expo/fingerprint ../. | jq -r '.hash')
+echo $PWD
+FINGERPRINT=$(npx @expo/fingerprint .)
+EXPO_PUBLIC_RUNTIME_VERSION=$($FINGERPRINT | grep -o '"hash":"[^"]*"' | sed 's/"hash":"//;s/"//')
 set-env EXPO_PUBLIC_RUNTIME_VERSION $EXPO_PUBLIC_RUNTIME_VERSION
 
 echo "EXPO_PUBLIC_RUNTIME_VERSION $EXPO_PUBLIC_RUNTIME_VERSION"
