2e0192d784e46dfaca090fd5600e78ff126bfe41
Peter Sanderson <dierk.deidel@protonmail.com>
2024-10-08 17:26:07 +0200

    feat: remove suport for BIP-69, keep the default order of the inputs and outputs (part1: do not sort inputs)

--- packages/utxo-lib/src/coinselect/index.ts
+++ packages/utxo-lib/src/coinselect/index.ts
@@ -10,8 +10,28 @@ export function coinselect({ inputs, outputs, feeRate, ...options }: CoinSelectR
         return split(inputs, outputs, feeRate, options);
     }
 
-    const sortedInputs = options.skipPermutation ? inputs : inputs.sort(sortByScore(feeRate));
+    const sortedInputs = inputs.sort(sortByScore(feeRate));
     const algorithm = tryConfirmed(anyOf([bnb, accumulative]), options);
+    const result = algorithm(sortedInputs, outputs, feeRate, options);
 
-    return algorithm(sortedInputs, outputs, feeRate, options);
+    if (result.inputs === undefined) {
+        return { fee: result.fee };
+    }
+
+    return {
+        fee: result.fee,
+        inputs: result.inputs.sort((a, b) => {
+            const originalA = inputs.findIndex(({ i }) => i === a.i);
+            const originalB = inputs.findIndex(({ i }) => i === b.i);
+
+            if (originalA === -1 || originalB === -1) {
+                throw new Error(
+                    `Input not found in original inputs (originalA: ${originalA}, originalB: ${originalB})`,
+                );
+            }
+
+            return originalA - originalB;
+        }),
+        outputs: result.outputs,
+    };
 }
