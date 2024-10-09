2e75912ea54f589f19d740372d67f9059694dc53
Szymon Lesisz <sz.lesisz@gmail.com>
2024-10-09 12:45:51 +0200

    chore(protobuf): build types script as module

---
+++ packages/protobuf/scripts/protobuf-patches/MessageType.ts
@@ -0,0 +1,14 @@
+// @ts-nocheck
+
+export type MessageKey = keyof MessageType;
+
+export type MessageResponse<T extends MessageKey> = {
+    type: T;
+    message: MessageType[T];
+};
+
+export type TypedCall = <T extends MessageKey, R extends MessageKey>(
+    type: T,
+    resType: R,
+    message?: MessageType[T],
+) => Promise<MessageResponse<R>>;
