324ba3092182505061dd826f78e37cb6995699b7
Szymon Lesisz <sz.lesisz@gmail.com>
2024-10-09 12:45:51 +0200

    chore(protobuf): build types script as module

--- packages/protobuf/scripts/protobuf-types.ts
+++ packages/protobuf/scripts/protobuf-types.ts
@@ -12,6 +12,7 @@ import {
     FIELD_TYPES,
     ENUM_KEYS,
     ORDER,
+    readPatch,
 } from './protobuf-patches';
 
 const CONSOLE_RED = '\x1b[31m';
@@ -32,15 +33,18 @@ type TypeItem = {
 type Definition = {
     options?: Record<string, unknown>;
     rule?: string;
-    type: string;
+    type?: string;
     nested?: Record<string, Definition>;
     fields?: Record<string, Definition>;
     values?: Record<string, unknown>;
 };
 
-const types: TypeItem[] = [];
+type ParseMessageOptions = {
+    defaultRule?: 'required' | 'optional';
+    fixOrder?: boolean;
+};
 
-const parseEnum = (itemName: string, item: Definition) => {
+const parseEnum = (itemName: string, item: Definition): TypeItem => {
     const IS_KEY = ENUM_KEYS.includes(itemName);
 
     // declare enum
@@ -59,155 +63,164 @@ const parseEnum = (itemName: string, item: Definition) => {
         value.push(`export type ${itemName} = keyof typeof ${enumName};`, '');
     }
 
-    types.push({
+    return {
         type: 'enum',
         name: itemName,
         value: value.join('\n'),
-    });
+    };
 };
 
-const parseMessage = (messageName: string, message: Definition, depth = 0) => {
-    const value: string[] = [];
-    // add comment line
-    if (!depth) value.push(`// ${messageName}`);
-    // declare nested enums
-
-    // declare nested values
+const parseMessage = (
+    messageName: string,
+    message: Definition,
+    options?: ParseMessageOptions,
+): TypeItem | TypeItem[] => {
+    const lines: string[] = [];
+    // parse nested values
     const { nested, fields } = message;
+    let nestedTypes;
     if (nested) {
-        Object.keys(nested).map(item => parseMessage(item, nested[item], depth + 1));
+        nestedTypes = Object.keys(nested).flatMap(item =>
+            parseMessage(item, nested[item], options),
+        );
     }
 
+    // parse enum
     if (message.values) {
         return parseEnum(messageName, message);
     }
-    if (!fields || !Object.keys(fields).length) {
+
+    if (DEFINITION_PATCH[messageName]) {
+        // replace whole declaration with patch
+        lines.push(DEFINITION_PATCH[messageName]());
+    } else if (!fields || !Object.keys(fields).length) {
         // few types are just empty objects, make it one line
-        value.push(`export type ${messageName} = {};`);
-        value.push('');
+        lines.push(`export type ${messageName} = {};`, '');
     } else {
-        // find patch
-        const definition = DEFINITION_PATCH[messageName];
-        if (definition) {
-            // replace whole declaration
-            value.push(definition());
-        } else {
-            // declare type
-            value.push(`export type ${messageName} = {`);
-            Object.keys(fields).forEach(fieldName => {
-                const field = fields[fieldName];
-                const fieldKey = `${messageName}.${fieldName}`;
-                // find patch for "rule"
-                const fieldRule = RULE_PATCH[fieldKey] || field.rule;
-                const rule = fieldRule === 'required' || fieldRule === 'repeated' ? ': ' : '?: ';
-                // find patch for "type"
-                let type = TYPE_PATCH[fieldKey] || FIELD_TYPES[field.type] || field.type;
-                // automatically convert all amount and fee fields to UINT_TYPE
-                if (['amount', 'fee'].includes(fieldName)) {
-                    type = UINT_TYPE;
-                }
-                // array
-                if (field.rule === 'repeated') {
-                    type = type.split('|').length > 1 ? `Array<${type}>` : `${type}[]`;
-                }
-                value.push(`    ${fieldName}${rule}${type};`);
-            });
-            // close type declaration
-            value.push('};');
-            // empty line
-            value.push('');
-        }
+        // declare type
+        lines.push(`export type ${messageName} = {`);
+        Object.keys(fields).forEach(fieldName => {
+            const field = fields[fieldName];
+            const fieldKey = `${messageName}.${fieldName}`;
+            // find patch for "rule"
+            const fieldRule = RULE_PATCH[fieldKey] || field.rule || options?.defaultRule;
+            const rule = fieldRule === 'required' || fieldRule === 'repeated' ? ': ' : '?: ';
+            // find patch for "type"
+            let type = TYPE_PATCH[fieldKey] || FIELD_TYPES[field.type || ''] || field.type;
+            // automatically convert all amount and fee fields to UINT_TYPE
+            if (['amount', 'fee'].includes(fieldName)) {
+                type = UINT_TYPE;
+            }
+            // array
+            if (field.rule === 'repeated') {
+                type = type.split('|').length > 1 ? `Array<${type}>` : `${type}[]`;
+            }
+            lines.push(`    ${fieldName}${rule}${type};`);
+        });
+        // close type declaration
+        lines.push('};', '');
     }
-    types.push({
-        type: 'message',
-        name: messageName,
-        value: value.join('\n'),
-    });
+
+    return [
+        ...(nestedTypes || []),
+        {
+            type: 'message',
+            name: messageName,
+            value: lines.join('\n'),
+        },
+    ];
 };
 
-// top level messages and nested messages
-Object.keys(json.nested).map(e => parseMessage(e, json.nested[e]));
+const fixOrder = (types: TypeItem[]) => {
+    Object.keys(ORDER).forEach(key => {
+        if (key === ORDER[key]) {
+            logError(`ORDER map cannot have key=value`);
+        }
 
-Object.keys(ORDER).forEach(key => {
-    if (key === ORDER[key]) {
-        logError(`ORDER map cannot have key=value`);
-    }
+        // find indexes
+        const indexOfDependency = types.findIndex(t => t && t.name === key);
+        if (indexOfDependency === -1) {
+            logError(`Type from key: '${key}' not found in the 'types' variable!`);
+        }
 
-    // find indexes
-    const indexOfDependency = types.findIndex(t => t && t.name === key);
-    if (indexOfDependency === -1) {
-        logError(`Type from key: '${key}' not found in the 'types' variable!`);
-    }
+        const indexOfDependant = types.findIndex(t => t && t.name === ORDER[key]);
+        if (indexOfDependant === -1) {
+            logError(`Type from value: '${ORDER[key]}' not found in the 'types' variable!`);
+        }
 
-    const indexOfDependant = types.findIndex(t => t && t.name === ORDER[key]);
-    if (indexOfDependant === -1) {
-        logError(`Type from value: '${ORDER[key]}' not found in the 'types' variable!`);
-    }
-
-    const dependency = types[indexOfDependency];
-    // replace values
-    delete types[indexOfDependency];
-    types.splice(indexOfDependant, 0, dependency);
-});
+        const dependency = types[indexOfDependency];
+        // replace values
+        delete types[indexOfDependency];
+        types.splice(indexOfDependant, 0, dependency);
+    });
+};
 
 // skip not needed types
-SKIP.forEach(key => {
-    const index = types.findIndex(t => t && t.name === key);
-    delete types[index];
-});
-
-// create content from types
-const content = types.flatMap(t => (t ? [t.value] : [])).join('\n');
-
-const lines: string[] = [];
-
-lines.push('// This file is auto generated from data/messages/message.json', '');
-lines.push('// custom type uint32/64 may be represented as string');
-lines.push(`export type ${UINT_TYPE} = string | number;`, '');
-lines.push('// custom type sint32/64');
-lines.push(`export type ${SINT_TYPE} = string | number;`, '');
-lines.push(
-    `export enum DeviceModelInternal {
-    T1B1 = 'T1B1',
-    T2T1 = 'T2T1',
-    T2B1 = 'T2B1',
-    T3B1 = 'T3B1',
-    T3T1 = 'T3T1',
-}`,
-    '',
-);
-lines.push(content);
-
-// create custom definition
-
-lines.push('// custom connect definitions');
-lines.push('export type MessageType = {');
-types
-    .flatMap(t => (t && t.type === 'message' ? [t] : []))
-    .forEach(t => {
-        lines.push(`    ${t.name}: ${t.name};`);
+const skipTypes = (types: TypeItem[]) => {
+    SKIP.forEach(key => {
+        const index = types.findIndex(t => t && t.name === key);
+        delete types[index];
     });
-lines.push('};');
-
-// additional types utilities
-lines.push(`
-export type MessageKey = keyof MessageType;
-
-export type MessageResponse<T extends MessageKey> = {
-    type: T;
-    message: MessageType[T];
 };
 
-export type TypedCall = <T extends MessageKey, R extends MessageKey>(
-    type: T,
-    resType: R,
-    message?: MessageType[T],
-) => Promise<MessageResponse<R>>;
-`);
+export const createTypes = (
+    def: Definition | Record<string, Definition>,
+    options?: ParseMessageOptions,
+) => {
+    const root = def.nested ? def.nested : def;
+    const types = Object.keys(root).flatMap(key => {
+        return parseMessage(key, root[key], options);
+    });
+    skipTypes(types);
+    if (options?.fixOrder) {
+        fixOrder(types);
+    }
 
-// save to file
-const filePath = path.join(__dirname, '../src/messages.ts');
+    return types;
+};
 
-fs.writeFile(filePath, lines.join('\n'), err => {
-    if (err) return logError(err.message);
-});
+const createCustomTypes = () => {
+    const lines: string[] = [];
+    lines.push('// This file is auto generated by @trezor/protobuf package', '');
+    lines.push('// custom type uint32/64 may be represented as string');
+    lines.push(`export type ${UINT_TYPE} = string | number;`, '');
+    lines.push('// custom type sint32/64');
+    lines.push(`export type ${SINT_TYPE} = string | number;`, '');
+    lines.push(readPatch('customTypes.ts'), '');
+
+    return lines;
+};
+
+const createMessageType = (types: TypeItem[]) => {
+    const lines: string[] = [];
+    lines.push('// custom connect definitions');
+    lines.push('export type MessageType = {');
+    types
+        .flatMap(t => (t && t.type === 'message' ? t : []))
+        .forEach(t => {
+            lines.push(`    ${t.name}: ${t.name};`);
+        });
+    lines.push('};');
+
+    lines.push(readPatch('MessageType.ts'));
+
+    return lines;
+};
+
+if (require.main === module) {
+    // called directly, otherwise required as a module
+
+    const types = createTypes(json, { fixOrder: true });
+    const lines = createCustomTypes();
+    // create content from types
+    const content = types.flatMap(t => (t ? t.value : [])).join('\n');
+    lines.push(content);
+    // create custom MessageType definition
+    lines.push(...createMessageType(types));
+
+    // save to file
+    const filePath = path.join(__dirname, '../src/messages.ts');
+    fs.writeFile(filePath, lines.join('\n'), err => {
+        if (err) return logError(err.message);
+    });
+}
