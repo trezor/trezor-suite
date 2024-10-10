0003d24c7b0c4d2246dcb1860d7552fedda527aa
yanas <yanas@yanas.cz>
2024-10-10 15:12:04 +0200

    refactor(suite-native): extract layout component for device settings card

---
+++ suite-native/device/src/components/DeviceSettingsCard.tsx
@@ -0,0 +1,44 @@
+import { ReactNode } from 'react';
+
+import { Icon, IconName } from '@suite-common/icons-deprecated';
+import { AlertBox, AlertBoxProps, Box, Card, HStack, Text, VStack } from '@suite-native/atoms';
+import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
+
+const contentStyle = prepareNativeStyle(() => ({
+    flexGrow: 1,
+}));
+
+export type DeviceSettingsCardProps = {
+    icon: IconName;
+    title: ReactNode;
+    children: ReactNode;
+    alertBoxProps?: Omit<AlertBoxProps, 'borderRadius'>;
+};
+
+export const DeviceSettingsCard = ({
+    icon,
+    title,
+    children,
+    alertBoxProps,
+}: DeviceSettingsCardProps) => {
+    const { applyStyle } = useNativeStyles();
+
+    return (
+        <Card noPadding>
+            <HStack margin="sp16" spacing="sp12">
+                <Box marginVertical="sp2">
+                    <Icon name={icon} size="mediumLarge" />
+                </Box>
+                <VStack spacing={0} style={applyStyle(contentStyle)}>
+                    <Text variant="highlight">{title}</Text>
+                    {children}
+                </VStack>
+            </HStack>
+            {alertBoxProps && (
+                <Box margin="sp4">
+                    <AlertBox {...alertBoxProps} borderRadius="r12" />
+                </Box>
+            )}
+        </Card>
+    );
+};
