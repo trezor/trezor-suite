3d893b73215242591df01ae0bf99badca5d0a231
yanas <yanas@yanas.cz>
2024-10-10 15:12:04 +0200

    refactor(suite-native): extract layout component for device settings card

--- suite-native/device/src/components/DeviceFirmwareCard.tsx
+++ suite-native/device/src/components/DeviceFirmwareCard.tsx
@@ -4,18 +4,20 @@ import { useSelector } from 'react-redux';
 import { G } from '@mobily/ts-belt';
 
 import { getFwUpdateVersion } from '@suite-common/suite-utils';
-import { DeviceModelIcon } from '@suite-common/icons-deprecated';
+import { deviceModelToIconName } from '@suite-common/icons-deprecated';
 import {
     selectDevice,
     selectDeviceModel,
     selectDeviceReleaseInfo,
 } from '@suite-common/wallet-core';
-import { AlertBox, Box, Card, HStack, Text, VStack } from '@suite-native/atoms';
+import { HStack, Text, VStack } from '@suite-native/atoms';
 import { Translation } from '@suite-native/intl';
 import { getFirmwareVersion, hasBitcoinOnlyFirmware } from '@trezor/device-utils';
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
 
-const vStackStyle = prepareNativeStyle(() => ({
+import { DeviceSettingsCard } from './DeviceSettingsCard';
+
+const firmwareInfoStyle = prepareNativeStyle(() => ({
     flexGrow: 1,
 }));
 
@@ -28,7 +30,7 @@ const FirmwareInfo = ({ label, value }: DeviceInfoProps) => {
     const { applyStyle } = useNativeStyles();
 
     return (
-        <VStack spacing="sp2" style={applyStyle(vStackStyle)}>
+        <VStack spacing="sp2" style={applyStyle(firmwareInfoStyle)}>
             <Text variant="hint" color="textSubdued">
                 {label}
             </Text>
@@ -38,8 +40,6 @@ const FirmwareInfo = ({ label, value }: DeviceInfoProps) => {
 };
 
 export const DeviceFirmwareCard = () => {
-    const { applyStyle } = useNativeStyles();
-
     const device = useSelector(selectDevice);
     const deviceModel = useSelector(selectDeviceModel);
     const deviceReleaseInfo = useSelector(selectDeviceReleaseInfo);
@@ -79,32 +79,21 @@ export const DeviceFirmwareCard = () => {
     })();
 
     return (
-        <Card noPadding>
-            <HStack margin="sp16" spacing="sp12">
-                <Box marginVertical="sp2">
-                    <DeviceModelIcon deviceModel={deviceModel} size="mediumLarge" />
-                </Box>
-                <VStack spacing="sp12" style={applyStyle(vStackStyle)}>
-                    <Text variant="highlight">
-                        <Translation id="deviceSettings.firmware.title" />
-                    </Text>
-                    <HStack spacing="sp2">
-                        <FirmwareInfo
-                            label={<Translation id="deviceSettings.firmware.version" />}
-                            value={firmwareVersion}
-                        />
-                        <FirmwareInfo
-                            label={<Translation id="deviceSettings.firmware.type" />}
-                            value={<Translation id={firmwareTypeTranslationId} />}
-                        />
-                    </HStack>
-                </VStack>
+        <DeviceSettingsCard
+            icon={deviceModelToIconName(deviceModel)}
+            title={<Translation id="deviceSettings.firmware.title" />}
+            alertBoxProps={firmwareUpdateProps}
+        >
+            <HStack marginTop="sp12" spacing="sp2">
+                <FirmwareInfo
+                    label={<Translation id="deviceSettings.firmware.version" />}
+                    value={firmwareVersion}
+                />
+                <FirmwareInfo
+                    label={<Translation id="deviceSettings.firmware.type" />}
+                    value={<Translation id={firmwareTypeTranslationId} />}
+                />
             </HStack>
-            {firmwareUpdateProps && (
-                <Box margin="sp4">
-                    <AlertBox {...firmwareUpdateProps} borderRadius="r12" />
-                </Box>
-            )}
-        </Card>
+        </DeviceSettingsCard>
     );
 };
