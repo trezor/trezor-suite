80ec7a70b4325eaa98cf3e3d91925291ade2bd3c
Adam Havel <adam.havel@protonmail.com>
2024-10-11 10:19:45 +0200

    feat(suite): add coin selection prompt to empty passphrase wallet

--- packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/PassphraseMismatchModal.tsx
+++ packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/PassphraseMismatchModal.tsx
@@ -1,7 +1,7 @@
 import { Button, H3, Column, Text } from '@trezor/components';
+import { spacings } from '@trezor/theme';
 
 import { useDevice, useDispatch } from 'src/hooks/suite';
-import { SwitchDeviceRenderer } from 'src/views/suite/SwitchDevice/SwitchDeviceRenderer';
 import { CardWithDevice } from 'src/views/suite/SwitchDevice/CardWithDevice';
 import { Translation } from '../../../Translation';
 import { passwordMismatchResetThunk } from '@suite-common/wallet-core';
@@ -10,6 +10,7 @@ import {
     redirectAfterWalletSelectedThunk,
 } from 'src/actions/wallet/addWalletThunk';
 import { WalletType } from '@suite-common/wallet-types';
+import { SwitchDeviceModal } from 'src/views/suite/SwitchDevice/SwitchDeviceModal';
 
 export const PassphraseMismatchModal = ({ onCancel }: { onCancel: () => void }) => {
     const { isLocked, device: selectDevice } = useDevice();
@@ -29,17 +30,20 @@ export const PassphraseMismatchModal = ({ onCancel }: { onCancel: () => void }) 
     };
 
     return (
-        <SwitchDeviceRenderer isCancelable={false} data-testid="@passphrase-mismatch">
+        <SwitchDeviceModal data-testid="@passphrase-mismatch">
             <CardWithDevice device={selectDevice} isFullHeaderVisible={false}>
-                <Column gap={8} margin={{ bottom: 32 }} alignItems="center">
-                    <H3 margin={{ top: 12 }}>
+                <Column
+                    gap={spacings.xs}
+                    margin={{ top: spacings.xxs, bottom: spacings.lg }}
+                    alignItems="stretch"
+                >
+                    <H3>
                         <Translation id="TR_PASSPHRASE_MISMATCH" />
                     </H3>
                     <Text variant="tertiary">
                         <Translation id="TR_PASSPHRASE_MISMATCH_DESCRIPTION" />
                     </Text>
                 </Column>
-
                 <Button
                     variant="primary"
                     onClick={onStartOver}
@@ -50,6 +54,6 @@ export const PassphraseMismatchModal = ({ onCancel }: { onCancel: () => void }) 
                     <Translation id="TR_PASSPHRASE_MISMATCH_START_OVER" />
                 </Button>
             </CardWithDevice>
-        </SwitchDeviceRenderer>
+        </SwitchDeviceModal>
     );
 };
