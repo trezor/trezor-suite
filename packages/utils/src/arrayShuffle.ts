45f2273e816f0f4716d375709c905554a573ac84
Peter Sanderson <dierk.deidel@protonmail.com>
2024-10-11 09:52:34 +0200

    feat: shufle inputs of TX randomly

--- packages/utils/src/arrayShuffle.ts
+++ packages/utils/src/arrayShuffle.ts
@@ -1,8 +1,11 @@
 /**
- * Randomly shuffles the elements in an array. This method
- * does not mutate the original array.
+ * Implementation of the Fisher-Yates shuffle algorithm.
+ * The algorithm produces an unbiased permutation: every permutation is equally likely.
+ * @link https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+ *
+ * This method does not mutate the original array.
  */
-export const arrayShuffle = <T>(array: readonly T[]) => {
+export const arrayShuffle = <T>(array: readonly T[]): T[] => {
     const shuffled = array.slice();
     for (let i = shuffled.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
