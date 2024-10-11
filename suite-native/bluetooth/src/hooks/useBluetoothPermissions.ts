34043ec797c302987f89f537d3dfd9ace8a41267
Nodonisko <suchydan@gmail.com>
2024-10-11 15:56:18 +0200

    chore: add hook deps

--- suite-native/bluetooth/src/hooks/useBluetoothPermissions.ts
+++ suite-native/bluetooth/src/hooks/useBluetoothPermissions.ts
@@ -1,4 +1,4 @@
-import { useState, useRef, useLayoutEffect } from 'react';
+import { useState, useRef, useLayoutEffect, useCallback } from 'react';
 import { AppState, AppStateStatus, Platform } from 'react-native';
 import {
     PERMISSIONS,
@@ -22,83 +22,92 @@ export const useBluetoothPermissions = () => {
     const [hasBluetoothPermissions, setHasBluetoothPermissions] = useState<boolean>(false);
     const [bluetoothPermissionError, setBluetoothPermissionError] =
         useState<BluetoothPermissionErrors>();
-    const deviceOSVersion = Constants.systemVersion || 0;
 
-    const requestIosPermission = async ({ checkOnly = false }: { checkOnly?: boolean } = {}) => {
-        const permissionFn = checkOnly ? check : request;
-        const bluetoothPermissionStatus = await permissionFn(PERMISSIONS.IOS.BLUETOOTH);
-        const bluetoothAllowed = bluetoothPermissionStatus === RESULTS.GRANTED;
-
-        if (bluetoothAllowed) {
-            setHasBluetoothPermissions(true);
-            setBluetoothPermissionError(undefined);
-        } else {
-            setBluetoothPermissionError(BluetoothPermissionErrors.BluetoothAccessBlocked);
-        }
-    };
-
-    const requestAndroidPermission = async ({
-        checkOnly = false,
-    }: { checkOnly?: boolean } = {}) => {
-        let hasError = false;
-
-        if (deviceOSVersion >= 12) {
-            let result: {
-                [PERMISSIONS.ANDROID.BLUETOOTH_CONNECT]: PermissionStatus;
-                [PERMISSIONS.ANDROID.BLUETOOTH_SCAN]: PermissionStatus;
-            } = {
-                [PERMISSIONS.ANDROID.BLUETOOTH_CONNECT]: RESULTS.DENIED,
-                [PERMISSIONS.ANDROID.BLUETOOTH_SCAN]: RESULTS.DENIED,
-            };
-            if (checkOnly) {
-                result[PERMISSIONS.ANDROID.BLUETOOTH_CONNECT] = await check(
-                    PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
-                );
-                result[PERMISSIONS.ANDROID.BLUETOOTH_SCAN] = await check(
-                    PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
-                );
-            } else {
-                result = await requestMultiple([
-                    PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
-                    PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
-                ]);
-            }
-
-            if (
-                result[PERMISSIONS.ANDROID.BLUETOOTH_CONNECT] !== RESULTS.GRANTED ||
-                result[PERMISSIONS.ANDROID.BLUETOOTH_SCAN] !== RESULTS.GRANTED
-            ) {
-                setBluetoothPermissionError(BluetoothPermissionErrors.NearbyDevicesAccessBlocked);
-                hasError = true;
-            }
-        } else {
+    const requestIosPermission = useCallback(
+        async ({ checkOnly = false }: { checkOnly?: boolean } = {}) => {
             const permissionFn = checkOnly ? check : request;
-            const bluetoothPermissionStatus = await permissionFn(
-                PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
-            );
+            const bluetoothPermissionStatus = await permissionFn(PERMISSIONS.IOS.BLUETOOTH);
+            const bluetoothAllowed = bluetoothPermissionStatus === RESULTS.GRANTED;
 
-            if (bluetoothPermissionStatus !== RESULTS.GRANTED) {
-                setBluetoothPermissionError(BluetoothPermissionErrors.LocationAccessBlocked);
-                hasError = true;
+            if (bluetoothAllowed) {
+                setHasBluetoothPermissions(true);
+                setBluetoothPermissionError(undefined);
+            } else {
+                setBluetoothPermissionError(BluetoothPermissionErrors.BluetoothAccessBlocked);
             }
-        }
+        },
+        [],
+    );
 
-        if (!hasError) {
-            setHasBluetoothPermissions(true);
-            setBluetoothPermissionError(undefined);
-        }
-    };
+    const requestAndroidPermission = useCallback(
+        async ({ checkOnly = false }: { checkOnly?: boolean } = {}) => {
+            let hasError = false;
+            const deviceOSVersion = Constants.systemVersion || 0;
+
+            if (deviceOSVersion >= 12) {
+                let result: {
+                    [PERMISSIONS.ANDROID.BLUETOOTH_CONNECT]: PermissionStatus;
+                    [PERMISSIONS.ANDROID.BLUETOOTH_SCAN]: PermissionStatus;
+                } = {
+                    [PERMISSIONS.ANDROID.BLUETOOTH_CONNECT]: RESULTS.DENIED,
+                    [PERMISSIONS.ANDROID.BLUETOOTH_SCAN]: RESULTS.DENIED,
+                };
+                if (checkOnly) {
+                    result[PERMISSIONS.ANDROID.BLUETOOTH_CONNECT] = await check(
+                        PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
+                    );
+                    result[PERMISSIONS.ANDROID.BLUETOOTH_SCAN] = await check(
+                        PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
+                    );
+                } else {
+                    result = await requestMultiple([
+                        PERMISSIONS.ANDROID.BLUETOOTH_CONNECT,
+                        PERMISSIONS.ANDROID.BLUETOOTH_SCAN,
+                    ]);
+                }
+
+                if (
+                    result[PERMISSIONS.ANDROID.BLUETOOTH_CONNECT] !== RESULTS.GRANTED ||
+                    result[PERMISSIONS.ANDROID.BLUETOOTH_SCAN] !== RESULTS.GRANTED
+                ) {
+                    setBluetoothPermissionError(
+                        BluetoothPermissionErrors.NearbyDevicesAccessBlocked,
+                    );
+                    hasError = true;
+                }
+            } else {
+                const permissionFn = checkOnly ? check : request;
+                const bluetoothPermissionStatus = await permissionFn(
+                    PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION,
+                );
+
+                if (bluetoothPermissionStatus !== RESULTS.GRANTED) {
+                    setBluetoothPermissionError(BluetoothPermissionErrors.LocationAccessBlocked);
+                    hasError = true;
+                }
+            }
+
+            if (!hasError) {
+                setHasBluetoothPermissions(true);
+                setBluetoothPermissionError(undefined);
+            }
+        },
+        [],
+    );
 
     // Checking if app has required permissions every time the app becomes active
-    const requestPermissions = async ({ checkOnly = false }: { checkOnly?: boolean } = {}) => {
-        if (Platform.OS === 'ios') {
-            await requestIosPermission({ checkOnly });
-        }
+    const requestPermissions = useCallback(
+        async ({ checkOnly = false }: { checkOnly?: boolean } = {}) => {
+            if (Platform.OS === 'ios') {
+                await requestIosPermission({ checkOnly });
+            }
 
-        if (Platform.OS === 'android') {
-            await requestAndroidPermission({ checkOnly });
-        }
-    };
+            if (Platform.OS === 'android') {
+                await requestAndroidPermission({ checkOnly });
+            }
+        },
+        [requestIosPermission, requestAndroidPermission],
+    );
 
     // External permission changes must be picked up by the app by tracking the app state
     useLayoutEffect(() => {
@@ -117,8 +126,7 @@ export const useBluetoothPermissions = () => {
         return () => {
             subscription.remove();
         };
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+    }, [requestPermissions]);
 
     return {
         hasBluetoothPermissions,
