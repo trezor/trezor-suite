0c0945dbeb08974675852d5a2d5da5df109cbb95
Peter Sanderson <dierk.deidel@protonmail.com>
2024-10-14 14:14:24 +0200

    chore: code improvement for arrayShuffle

--- packages/utils/tests/arrayShuffle.test.ts
+++ packages/utils/tests/arrayShuffle.test.ts
@@ -5,19 +5,24 @@ const SAMPLES = 10000;
 const TOLERANCE = 0.1;
 const EXPECTED = SAMPLES / KEYS.length;
 
-describe('arrayShuffle', () => {
+const LOWER_BOUND = (1 - TOLERANCE) * EXPECTED;
+const UPPER_BOUND = (1 + TOLERANCE) * EXPECTED;
+
+describe(arrayShuffle.name, () => {
     it('shuffles randomly', () => {
         const samples = Object.fromEntries(KEYS.map(key => [key, new Array(KEYS.length).fill(0)]));
+
         for (let sample = 0; sample < SAMPLES; ++sample) {
             const shuffled = arrayShuffle(KEYS);
             for (let i = 0; i < shuffled.length; ++i) {
                 samples[shuffled[i]][i]++;
             }
         }
+
         KEYS.forEach(key =>
             samples[key].forEach(count => {
-                expect(count).toBeGreaterThanOrEqual((1 - TOLERANCE) * EXPECTED);
-                expect(count).toBeLessThanOrEqual((1 + TOLERANCE) * EXPECTED);
+                expect(count).toBeGreaterThanOrEqual(LOWER_BOUND);
+                expect(count).toBeLessThanOrEqual(UPPER_BOUND);
             }),
         );
     });
