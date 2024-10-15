b711480e7beaa1d7dabf33f835b214b332ae3943
Daniel Suchý <suchydan@gmail.com>
2024-10-15 13:04:29 +0200

    feat(mobile): migrate BottomSheetFlashList to new lib (#14854)

--- suite-native/atoms/src/Sheet/BottomSheetFlashList.tsx
+++ suite-native/atoms/src/Sheet/BottomSheetFlashList.tsx
@@ -1,17 +1,15 @@
-import { useEffect, useRef, ReactNode } from 'react';
+import { ReactNode, useCallback, useEffect, useMemo, useRef } from 'react';
+import { Dimensions, StyleSheet, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import Animated from 'react-native-reanimated';
-import { PanGestureHandler } from 'react-native-gesture-handler';
-import { GestureResponderEvent, Pressable, Dimensions } from 'react-native';
 
-import { FlashList, FlashListProps } from '@shopify/flash-list';
+import {
+    BottomSheetBackdrop,
+    BottomSheetModal,
+    BottomSheetFlashList as FlashList,
+} from '@gorhom/bottom-sheet';
+import { FlashListProps } from '@shopify/flash-list';
 
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
-
-import { Box } from '../Box';
-import { BottomSheetContainer } from './BottomSheetContainer';
-import { useBottomSheetAnimation } from './useBottomSheetAnimation';
-import { BottomSheetHeader } from './BottomSheetHeader';
 
 export type BottomSheetFlashListProps<TItem> = {
     isVisible: boolean;
@@ -19,17 +17,16 @@ export type BottomSheetFlashListProps<TItem> = {
     onClose: (isVisible: boolean) => void;
     title?: ReactNode;
     subtitle?: ReactNode;
-    ExtraProvider?: React.ComponentType;
     estimatedListHeight?: number;
 } & FlashListProps<TItem>;
 
-const DEFAULT_INSET_BOTTOM = 50;
+const DEFAULT_INSET_BOTTOM = 25;
 
-const sheetWrapperStyle = prepareNativeStyle(utils => ({
+const bottomSheetStyle = prepareNativeStyle(utils => ({
     backgroundColor: utils.colors.backgroundSurfaceElevation0,
+
     borderTopLeftRadius: utils.borders.radii.r20,
     borderTopRightRadius: utils.borders.radii.r20,
-    maxHeight: '80%',
 }));
 
 const sheetContentContainerStyle = prepareNativeStyle<{
@@ -39,10 +36,13 @@ const sheetContentContainerStyle = prepareNativeStyle<{
     paddingHorizontal: utils.spacings.sp16,
 }));
 
-const sheetWithOverlayStyle = prepareNativeStyle(_ => ({
-    flex: 1,
-    justifyContent: 'flex-end',
+const handleStyle = prepareNativeStyle(utils => ({
+    backgroundColor: utils.colors.borderDashed,
 }));
+
+const WindowOverlay = ({ children }: { children: ReactNode }) => {
+    return <View style={StyleSheet.absoluteFill}>{children}</View>;
+};
 
 export const BottomSheetFlashList = <TItem,>({
     isVisible,
@@ -50,96 +50,69 @@ export const BottomSheetFlashList = <TItem,>({
     onClose,
     title,
     subtitle,
-    ExtraProvider,
     estimatedListHeight = 0,
     ...flashListProps
 }: BottomSheetFlashListProps<TItem>) => {
     const { applyStyle } = useNativeStyles();
     const insets = useSafeAreaInsets();
-    const {
-        animatedSheetWithOverlayStyle,
-        animatedSheetWrapperStyle,
-        closeSheetAnimated,
-        openSheetAnimated,
-        panGestureEvent,
-        scrollEvent,
-    } = useBottomSheetAnimation({
-        onClose,
-        isVisible,
-    });
-    const panGestureRef = useRef(null);
-    const scrollViewRef = useRef(null);
 
-    useEffect(() => {
-        if (isVisible) {
-            openSheetAnimated();
-        }
-    }, [isVisible, openSheetAnimated]);
+    const bottomSheetModalRef = useRef<BottomSheetModal>(null);
 
-    const handlePressOutside = (event: GestureResponderEvent) => {
-        if (event.target === event.currentTarget) closeSheetAnimated();
-    };
+    const handleClose = useCallback(() => {
+        bottomSheetModalRef.current?.dismiss();
+        onClose(false);
+    }, [onClose]);
 
     const insetBottom = Math.max(insets.bottom, DEFAULT_INSET_BOTTOM);
 
-    return (
-        <BottomSheetContainer
-            isVisible={isVisible}
-            onClose={closeSheetAnimated}
-            ExtraProvider={ExtraProvider}
-        >
-            <Animated.View
-                style={[animatedSheetWithOverlayStyle, applyStyle(sheetWithOverlayStyle)]}
-            >
-                <Pressable style={applyStyle(sheetWithOverlayStyle)} onPress={handlePressOutside}>
-                    <PanGestureHandler
-                        ref={panGestureRef}
-                        activeOffsetY={5}
-                        failOffsetY={-5}
-                        onGestureEvent={panGestureEvent}
-                    >
-                        <Animated.View
-                            style={[
-                                animatedSheetWrapperStyle,
-                                applyStyle(sheetWrapperStyle, {
-                                    insetBottom,
-                                }),
-                            ]}
-                        >
-                            <BottomSheetHeader
-                                title={title}
-                                subtitle={subtitle}
-                                isCloseDisplayed={isCloseDisplayed}
-                                onCloseSheet={closeSheetAnimated}
-                            />
+    const maxHeight = Dimensions.get('window').height * 0.9;
+    const minHeight = Math.max(Dimensions.get('window').height * 0.4, estimatedListHeight);
+    // minHeight can be higher than maxHeight because of estimatedListHeight, but it must be capped by maxHeight
+    const snapPoints = useMemo(() => [Math.min(minHeight, maxHeight)], [minHeight, maxHeight]);
 
-                            <Box
-                                style={{
-                                    // only estimated height is used, so we need to add some buffer to prevent unnecessary scrolling, when list is shorter than estimated
-                                    height: Math.max(
-                                        estimatedListHeight * 1.1,
-                                        // We use this because minHeight doesn't work with `height set
-                                        Dimensions.get('window').height * 0.35,
-                                    ),
-                                    maxHeight: '100%',
-                                }}
-                            >
-                                <FlashList
-                                    {...flashListProps}
-                                    overrideProps={{
-                                        simultaneousHandlers: panGestureRef,
-                                    }}
-                                    contentContainerStyle={applyStyle(sheetContentContainerStyle, {
-                                        insetBottom,
-                                    })}
-                                    ref={scrollViewRef}
-                                    onScroll={scrollEvent}
-                                />
-                            </Box>
-                        </Animated.View>
-                    </PanGestureHandler>
-                </Pressable>
-            </Animated.View>
-        </BottomSheetContainer>
+    const handleSheetChanges = useCallback(
+        (index: number) => {
+            if (index === -1) {
+                handleClose();
+            }
+        },
+        [handleClose],
+    );
+
+    useEffect(() => {
+        if (isVisible) {
+            bottomSheetModalRef.current?.present();
+        } else {
+            bottomSheetModalRef.current?.dismiss();
+        }
+    }, [isVisible]);
+
+    return (
+        <BottomSheetModal
+            ref={bottomSheetModalRef}
+            snapPoints={snapPoints}
+            maxDynamicContentSize={maxHeight}
+            enableDynamicSizing={false}
+            onChange={handleSheetChanges}
+            backdropComponent={props => (
+                <BottomSheetBackdrop
+                    {...props}
+                    onPress={handleClose}
+                    appearsOnIndex={0}
+                    disappearsOnIndex={-1}
+                />
+            )}
+            backgroundStyle={applyStyle(bottomSheetStyle)}
+            handleIndicatorStyle={applyStyle(handleStyle)}
+            // @ts-expect-error wrong type, doesn't expect children
+            containerComponent={WindowOverlay}
+        >
+            <FlashList
+                {...flashListProps}
+                contentContainerStyle={applyStyle(sheetContentContainerStyle, {
+                    insetBottom,
+                })}
+            />
+        </BottomSheetModal>
     );
 };
