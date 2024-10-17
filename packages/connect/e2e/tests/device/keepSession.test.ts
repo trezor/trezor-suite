b59a2852cec306007e1d1683c0a89fb758b30b76
Szymon Lesisz <sz.lesisz@gmail.com>
2024-10-17 20:18:07 +0200

    feat(connect): try to reuse initialized session
    DeviceState contains all the information to call `Initialize` message

--- packages/connect/e2e/tests/device/keepSession.test.ts
+++ packages/connect/e2e/tests/device/keepSession.test.ts
@@ -1,4 +1,4 @@
-import TrezorConnect from '../../../src';
+import TrezorConnect, { StaticSessionId } from '../../../src';
 
 import { getController, setup, conditionalTest, initTrezorConnect } from '../../common.setup';
 
@@ -9,6 +9,7 @@ describe('keepSession common param', () => {
         await TrezorConnect.dispose();
         await setup(controller, {
             mnemonic: 'mnemonic_all',
+            passphrase_protection: true,
         });
         await initTrezorConnect(controller);
     });
@@ -19,6 +20,10 @@ describe('keepSession common param', () => {
     });
 
     conditionalTest(['1', '<2.3.2'], 'keepSession with changing useCardanoDerivation', async () => {
+        TrezorConnect.on('ui-request_passphrase', () => {
+            TrezorConnect.uiResponse({ type: 'ui-receive_passphrase', payload: { value: 'a' } });
+        });
+
         const noDerivation = await TrezorConnect.getAccountDescriptor({
             coin: 'ada',
             path: "m/1852'/1815'/0'/0/0",
@@ -38,5 +43,35 @@ describe('keepSession common param', () => {
         });
         if (!enableDerivation.success) throw new Error(enableDerivation.payload.error);
         expect(enableDerivation.payload.descriptor).toBeDefined();
+
+        const { device } = enableDerivation;
+        if (!device || !device.state) throw new Error('Device not found');
+
+        // change device instance to simulate app reload
+        // passphrase request should not be called
+        TrezorConnect.removeAllListeners('ui-request_passphrase');
+        // modify instance in staticSessionId
+        const staticSessionId = device.state.staticSessionId?.replace(
+            ':0',
+            ':1',
+        ) as StaticSessionId;
+        const keepCardanoDerivation = await TrezorConnect.getAccountDescriptor({
+            coin: 'ada',
+            path: "m/1852'/1815'/0'/0/0",
+            device: {
+                // change instance to new but use already initialized state
+                instance: 1,
+                state: {
+                    ...device.state,
+                    staticSessionId,
+                },
+                path: device.path,
+            },
+            // useCardanoDerivation: true, // NOTE: not required, its in the state
+        });
+        if (!keepCardanoDerivation.success) throw new Error(keepCardanoDerivation.payload.error);
+        expect(keepCardanoDerivation.payload.descriptor).toEqual(
+            enableDerivation.payload.descriptor,
+        );
     });
 });
