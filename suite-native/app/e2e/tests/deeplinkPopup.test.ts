26288fe4b3b01c9cbc29af87f553ff6948f6c6f1
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2024-10-17 09:55:24 +0200

    wip

--- suite-native/app/e2e/tests/deeplinkPopup.test.ts
+++ suite-native/app/e2e/tests/deeplinkPopup.test.ts
@@ -1,25 +1,29 @@
 import http from 'http';
 import { exec } from 'child_process';
-// `expect` keyword is already used by jest.
-import { expect as detoxExpect } from 'detox';
 
-import { TrezorUserEnvLink, MNEMONICS } from '@trezor/trezor-user-env-link';
+import { TrezorUserEnvLink } from '@trezor/trezor-user-env-link';
 import TrezorConnect from '@trezor/connect-mobile';
 
-import { openApp } from '../utils';
+import {
+    appIsFullyLoaded,
+    disconnectTrezorUserEnv,
+    openApp,
+    prepareTrezorEmulator,
+    restartApp,
+} from '../utils';
 import { onOnboarding } from '../pageObjects/onboardingActions';
 import { onCoinEnablingInit } from '../pageObjects/coinEnablingActions';
+import { onHome } from '../pageObjects/homeActions';
 
-const TREZOR_DEVICE_LABEL = 'Trezor T - Tester';
-const platform = device.getPlatform();
 const SERVER_PORT = 8080;
-const SERVER_URL = `http://10.0.2.2:${SERVER_PORT}`;
+const SERVER_URL = `http://localhost:${SERVER_PORT}`;
 
 let server: http.Server | undefined;
 
-function openUriScheme(url: string, platformToOpen: 'android') {
+const openUriScheme = (url: string, platformToOpen: 'android') => {
     const command = `npx uri-scheme open '${url.replace(/'/g, '')}' --${platformToOpen}`;
 
+    // TODO: make it asynchronous
     exec(command, (err, stdout, stderr) => {
         if (err) {
             console.error(err);
@@ -30,11 +34,31 @@ function openUriScheme(url: string, platformToOpen: 'android') {
         console.info(stdout);
         console.error(stderr);
     });
-}
+};
+
+const reverseServerPort = (port: number) => {
+    return new Promise((resolve, reject) => {
+        exec(
+            `/usr/local/bin/platform-tools/adb reverse tcp:${port} tcp:${port}`,
+            (err, stdout, stderr) => {
+                if (err) {
+                    console.error(err);
+                    return reject(err);
+                }
+                console.info(stdout);
+                console.error(stderr);
+                resolve(stdout);
+            },
+        );
+    });
+};
 
 describe('Deeplink connect popup.', () => {
     beforeAll(async () => {
+        console.log('beforeAll');
         server = http.createServer((req, res) => {
+            console.log('req', req);
+            console.log('req.url', req.url);
             if (req.url) {
                 const url = new URL(req.url, SERVER_URL);
                 TrezorConnect.handleDeeplink(url.href);
@@ -49,36 +73,62 @@ describe('Deeplink connect popup.', () => {
             console.info(`Server running at ${SERVER_URL}`);
         });
 
-        if (platform === 'android') {
-            // Prepare Trezor device for test scenario and turn it off.
-            await TrezorUserEnvLink.disconnect();
-            await TrezorUserEnvLink.connect();
-            await TrezorUserEnvLink.startEmu({ wipe: true });
-            await TrezorUserEnvLink.setupEmu({
-                label: TREZOR_DEVICE_LABEL,
-                mnemonic: MNEMONICS.mnemonic_immune,
-            });
-            await TrezorUserEnvLink.startBridge();
-            await TrezorUserEnvLink.stopEmu();
-            await TrezorConnect.init({
-                manifest: {
-                    email: 'developer@xyz.com',
-                    appUrl: 'http://your.application.com',
-                },
-                deeplinkOpen: url => {
-                    openUriScheme(url, 'android');
-                },
-                deeplinkCallbackUrl: `${SERVER_URL}/connect/`,
-            });
-        }
-
+        await prepareTrezorEmulator();
         await openApp({ newInstance: true });
+        await onOnboarding.finishOnboarding();
+
+        await onCoinEnablingInit.waitForScreen();
+        await onCoinEnablingInit.enableNetwork('regtest');
+        await onCoinEnablingInit.clickOnConfirmButton();
+
+        await waitFor(element(by.id('skip-view-only-mode')))
+            .toBeVisible()
+            .withTimeout(10000); // communication between connected Trezor and app takes some time.
+
+        await element(by.id('skip-view-only-mode')).tap();
+
+        // This `TrezorConnect` instance here is pretending to be the integrator or @trezor/connect-mobile
+        await TrezorConnect.init({
+            manifest: {
+                email: 'developer@xyz.com',
+                appUrl: 'http://your.application.com',
+            },
+            deeplinkOpen: url => {
+                openUriScheme(url, 'android');
+            },
+            deeplinkCallbackUrl: `${SERVER_URL}/connect/`,
+            connectSrc: 'https://dev.suite.sldev.cz/connect/9.1.2/',
+        });
+    });
+
+    beforeEach(async () => {
+        console.log('beforeEach');
+        await prepareTrezorEmulator();
+        await restartApp();
+
+        console.log('after restartApp');
+
+        await reverseServerPort(SERVER_PORT);
+
+        console.log('after reverse ports');
+        await appIsFullyLoaded();
+        console.log('after appIsFullyLoaded');
+
+        await waitFor(element(by.id('@screen/ConnectingDevice')))
+            .toBeVisible()
+            .withTimeout(10000);
+
+        await onHome.waitForScreen();
+        console.log('after onHome.waitForScreen');
+        // await onTabBar.navigateToMyAssets();
+        // console.log('after onTabBar.navigateToMyAssets');
+
+        // await onMyAssets.openAccountDetail({ accountName: 'Bitcoin Regtest #1' });
+        // console.log('after onMyAssets.openAccountDetail');
     });
 
     afterAll(async () => {
-        if (platform === 'android') {
-            await TrezorUserEnvLink.stopEmu();
-        }
+        disconnectTrezorUserEnv();
         await new Promise(resolve => {
             if (server) {
                 server.close(() => {
@@ -89,64 +139,46 @@ describe('Deeplink connect popup.', () => {
         await device.terminateApp();
     });
 
-    it('Navigate to dashboard', async () => {
-        await onOnboarding.finishOnboarding();
+    it('Handle deeplink', async () => {
+        console.log('starting navigate to dashboard');
+        // await onOnboarding.finishOnboarding();
 
-        if (platform === 'android') {
-            await TrezorUserEnvLink.startEmu();
+        console.log('calling TrezorConnect');
+        const promise = TrezorConnect.getAddress({
+            path: "m/49'/0'/0'/0/0",
+            coin: 'btc',
+        });
 
-            await waitFor(element(by.id('@screen/CoinEnablingInit')))
-                .toBeVisible()
-                .withTimeout(10000);
+        console.log('waiting for @popup/deeplink-info');
+        await element(by.id('@popup/deeplink-info'));
 
-            await onCoinEnablingInit.waitForBtcToBeVisible();
+        console.log('waiting for @popup/call-device');
+        await waitFor(element(by.id('@popup/call-device')))
+            .toBeVisible()
+            .withTimeout(10 * 1000);
+        console.log('tapping on @popup/call-device');
+        await element(by.id('@popup/call-device')).tap();
 
-            await onCoinEnablingInit.enableNetwork('btc');
-            // TODO: I don't understand why without eth the part after does not complete.
-            await onCoinEnablingInit.enableNetwork('eth');
+        console.log('pressYes');
+        await TrezorUserEnvLink.pressYes();
 
-            await onCoinEnablingInit.save();
+        const response = await promise;
+        console.log('response', response);
+        const expectedResponse = {
+            id: 1,
+            payload: {
+                path: [49, 0, 0, 0, 0],
+                serializedPath: 'm/49/0/0/0/0',
+                address: '3J7UQSLAaFh1nkUomVdm8ArifPEvYfNr1o',
+            },
+            success: true,
+        };
 
-            await waitFor(element(by.id('skip-view-only-mode')))
-                .toBeVisible()
-                .withTimeout(60000); // communication between connected Trezor and app takes some time.
-
-            await element(by.id('skip-view-only-mode')).tap();
-
-            await detoxExpect(element(by.id('@home/portfolio/graph'))); // discovery finished and graph is visible
-
-            const promise = TrezorConnect.getAddress({
-                path: "m/49'/0'/0'/0/0",
-                coin: 'btc',
-            });
-
-            await element(by.id('@popup/deeplink-info'));
-
-            await waitFor(element(by.id('@popup/call-device')))
-                .toBeVisible()
-                .withTimeout(10 * 1000);
-            await element(by.id('@popup/call-device')).tap();
-
-            await TrezorUserEnvLink.pressYes();
-
-            const response = await promise;
-
-            const expectedResponse = {
-                id: 1,
-                payload: {
-                    path: [49, 0, 0, 0, 0],
-                    serializedPath: 'm/49/0/0/0/0',
-                    address: '3J7UQSLAaFh1nkUomVdm8ArifPEvYfNr1o',
-                },
-                success: true,
-            };
-
-            await new Promise((resolve, reject) => {
-                if (JSON.stringify(response) === JSON.stringify(expectedResponse)) {
-                    return resolve(null);
-                }
-                reject(new Error('Result does not match expected.'));
-            });
-        }
+        await new Promise((resolve, reject) => {
+            if (JSON.stringify(response) === JSON.stringify(expectedResponse)) {
+                return resolve(null);
+            }
+            reject(new Error('Result does not match expected.'));
+        });
     });
 });
