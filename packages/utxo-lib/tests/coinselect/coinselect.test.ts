a39752d6f989581fe7fbe94f5123ef9022bb9314
Peter Sanderson <dierk.deidel@protonmail.com>
2024-10-18 13:30:45 +0200

    chore: default-export code improvement
    chore: refactor defualt exports to named

--- packages/utxo-lib/tests/coinselect/coinselect.test.ts
+++ packages/utxo-lib/tests/coinselect/coinselect.test.ts
@@ -1,10 +1,10 @@
 import { coinselect } from '../../src/coinselect';
-import fixtures from '../__fixtures__/coinselect/coinselect-index';
-import fixturesErrors from '../__fixtures__/coinselect/coinselect-errors';
+import { coinselectIndexFixture } from '../__fixtures__/coinselect/coinselect-index';
+import { coinselectFixturesErrors } from '../__fixtures__/coinselect/coinselect-errors';
 import * as utils from './test.utils';
 
 describe('coinselect errors', () => {
-    fixturesErrors.forEach(f => {
+    coinselectFixturesErrors.forEach(f => {
         it(f.description, () => {
             const inputs = utils.expand(f.inputs, true);
             const outputs = utils.expand(f.outputs, false);
@@ -24,7 +24,7 @@ describe('coinselect errors', () => {
 });
 
 describe('coinselect index', () => {
-    fixtures.forEach(f => {
+    coinselectIndexFixture.forEach(f => {
         it(f.description, () => {
             const inputs = utils.expand(f.inputs as any, true);
             const outputs = utils.expand(f.outputs as any, false);
