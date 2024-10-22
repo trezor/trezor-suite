936c2a99cc54ee362645d81e60b65d590f6ef239
Jan Komarek <jan.komarek@satoshilabs.com>
2024-10-22 18:29:47 +0200

    chore(scripts): default to main branch when updating protobuf

--- scripts/update-protobuf.sh
+++ scripts/update-protobuf.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-FROM_FW_BRANCH=$1
+# Set FROM_FW_BRANCH to the first argument or default to 'main' if no argument is provided
+FROM_FW_BRANCH=${1:-main}
 
 yarn workspace @trezor/protobuf update:protobuf "$FROM_FW_BRANCH"
 yarn workspace @trezor/protobuf update:schema
