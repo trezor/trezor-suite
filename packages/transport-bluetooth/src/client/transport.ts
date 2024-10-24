94032ed13e5ea6f3f2ef5a3bdb136c414f8cc776
Szymon Lesisz <sz.lesisz@gmail.com>
2024-10-24 17:39:59 +0200

    fixup! feat: create `@trezor/transport-bluetooth` package

--- packages/transport-bluetooth/src/client/transport.ts
+++ packages/transport-bluetooth/src/client/transport.ts
@@ -1,9 +1,4 @@
-import {
-    Transport as AbstractTransport,
-    AbstractApiTransport,
-    SessionsClient,
-    SessionsBackground,
-} from '@trezor/transport';
+import { Transport as AbstractTransport, AbstractApiTransport } from '@trezor/transport';
 
 import { BluetoothApi } from './bluetooth-api';
 
@@ -14,25 +9,12 @@ export class BluetoothTransport extends AbstractApiTransport {
 
     constructor(params?: ConstructorParameters<typeof AbstractTransport>[0]) {
         const { messages, logger } = params || {};
-        const abortController = new AbortController();
-        const sessionsBackground = new SessionsBackground({ signal: abortController.signal });
-
-        const sessionsClient = new SessionsClient({
-            requestFn: args => sessionsBackground.handleMessage(args),
-            registerBackgroundCallbacks: () => {},
-        });
-
-        sessionsBackground.on('descriptors', descriptors => {
-            sessionsClient.emit('descriptors', descriptors);
-        });
 
         const api = new BluetoothApi({ logger }) as any;
 
         super({
             messages,
             api,
-            sessionsClient,
-            signal: abortController.signal,
         });
 
         this.wsApi = api;
@@ -41,7 +23,7 @@ export class BluetoothTransport extends AbstractApiTransport {
     public init() {
         return this.scheduleAction(async () => {
             await this.wsApi.init();
-            await super.init().promise;
+            await super.init();
 
             return this.success(undefined);
         });
