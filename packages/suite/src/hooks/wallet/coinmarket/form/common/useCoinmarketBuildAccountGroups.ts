7f5cf8cce795863eebc0663cd95a9df5da329caa
Martin Homola <adder10@gmail.com>
2024-10-24 14:22:20 +0200

    perf(trade): swap/sell timer optimalization

---
+++ packages/suite/src/hooks/wallet/coinmarket/form/common/useCoinmarketBuildAccountGroups.ts
@@ -0,0 +1,47 @@
+import { selectAccounts, selectDevice } from '@suite-common/wallet-core';
+import { useMemo } from 'react';
+import { useDefaultAccountLabel, useSelector } from 'src/hooks/suite';
+import { selectAccountLabels } from 'src/reducers/suite/metadataReducer';
+import {
+    CoinmarketAccountsOptionsGroupProps,
+    CoinmarketTradeSellExchangeType,
+} from 'src/types/coinmarket/coinmarket';
+import { coinmarketBuildAccountOptions } from 'src/utils/wallet/coinmarket/coinmarketUtils';
+
+export const useCoinmarketBuildAccountGroups = (
+    type: CoinmarketTradeSellExchangeType,
+): CoinmarketAccountsOptionsGroupProps[] => {
+    const accounts = useSelector(selectAccounts);
+    const accountLabels = useSelector(selectAccountLabels);
+    const device = useSelector(selectDevice);
+    const { getDefaultAccountLabel } = useDefaultAccountLabel();
+    const { tokenDefinitions } = useSelector(state => state);
+    const supportedSymbols = useSelector(state =>
+        type === 'sell'
+            ? state.wallet.coinmarket.sell.sellInfo?.supportedCryptoCurrencies
+            : state.wallet.coinmarket.exchange.exchangeInfo?.sellSymbols,
+    );
+
+    const groups = useMemo(
+        () =>
+            coinmarketBuildAccountOptions({
+                accounts,
+                deviceState: device?.state,
+                accountLabels,
+                tokenDefinitions,
+                supportedCryptoIds: supportedSymbols,
+                getDefaultAccountLabel,
+            }),
+
+        [
+            accounts,
+            device?.state,
+            accountLabels,
+            tokenDefinitions,
+            supportedSymbols,
+            getDefaultAccountLabel,
+        ],
+    );
+
+    return groups;
+};
--- packages/suite/src/hooks/wallet/coinmarket/form/useCoinmarketSellFormDefaultValues.ts
+++ packages/suite/src/hooks/wallet/coinmarket/form/useCoinmarketSellFormDefaultValues.ts
@@ -3,62 +3,18 @@ import { SellInfo } from 'src/actions/wallet/coinmarketSellActions';
 import { DEFAULT_PAYMENT, DEFAULT_VALUES } from '@suite-common/wallet-constants';
 import {
     buildFiatOption,
-    coinmarketBuildAccountOptions,
     cryptoIdToNetworkSymbol,
     getDefaultCountry,
 } from 'src/utils/wallet/coinmarket/coinmarketUtils';
 import { Account } from 'src/types/wallet';
-import {
-    CoinmarketAccountsOptionsGroupProps,
-    CoinmarketPaymentMethodListProps,
-    CoinmarketTradeSellExchangeType,
-} from 'src/types/coinmarket/coinmarket';
+import { CoinmarketPaymentMethodListProps } from 'src/types/coinmarket/coinmarket';
 import { CoinmarketSellFormDefaultValuesProps } from 'src/types/coinmarket/coinmarketForm';
 import { FormState, Output } from '@suite-common/wallet-types';
-import { selectAccountLabels } from 'src/reducers/suite/metadataReducer';
-import { useSelector, useDefaultAccountLabel } from 'src/hooks/suite';
-import { selectAccounts, selectDevice } from '@suite-common/wallet-core';
 import {
     FORM_DEFAULT_FIAT_CURRENCY,
     FORM_DEFAULT_PAYMENT_METHOD,
 } from 'src/constants/wallet/coinmarket/form';
-
-export const useCoinmarketBuildAccountGroups = (
-    type: CoinmarketTradeSellExchangeType,
-): CoinmarketAccountsOptionsGroupProps[] => {
-    const accounts = useSelector(selectAccounts);
-    const accountLabels = useSelector(selectAccountLabels);
-    const device = useSelector(selectDevice);
-    const { getDefaultAccountLabel } = useDefaultAccountLabel();
-    const { tokenDefinitions } = useSelector(state => state);
-    const supportedSymbols = useSelector(state =>
-        type === 'sell'
-            ? state.wallet.coinmarket.sell.sellInfo?.supportedCryptoCurrencies
-            : state.wallet.coinmarket.exchange.exchangeInfo?.sellSymbols,
-    );
-
-    const groups = useMemo(
-        () =>
-            coinmarketBuildAccountOptions({
-                accounts,
-                deviceState: device?.state,
-                accountLabels,
-                tokenDefinitions,
-                supportedCryptoIds: supportedSymbols,
-                getDefaultAccountLabel,
-            }),
-        [
-            accounts,
-            supportedSymbols,
-            accountLabels,
-            device,
-            tokenDefinitions,
-            getDefaultAccountLabel,
-        ],
-    );
-
-    return groups;
-};
+import { useCoinmarketBuildAccountGroups } from 'src/hooks/wallet/coinmarket/form/common/useCoinmarketBuildAccountGroups';
 
 export const useCoinmarketSellFormDefaultValues = (
     account: Account,
@@ -66,17 +22,21 @@ export const useCoinmarketSellFormDefaultValues = (
 ): CoinmarketSellFormDefaultValuesProps => {
     const country = sellInfo?.sellList?.country;
     const cryptoGroups = useCoinmarketBuildAccountGroups('sell');
-    const cryptoOptions = cryptoGroups.flatMap(group => group.options);
+    const cryptoOptions = useMemo(
+        () => cryptoGroups.flatMap(group => group.options),
+        [cryptoGroups],
+    );
     const defaultSendCryptoSelect = useMemo(
         () =>
             cryptoOptions.find(
                 option =>
                     option.descriptor === account.descriptor &&
-                    cryptoIdToNetworkSymbol(option.value) === account.symbol,
+                    account.symbol === cryptoIdToNetworkSymbol(option.value),
             ),
         [account.descriptor, account.symbol, cryptoOptions],
     );
     const defaultCountry = useMemo(() => getDefaultCountry(country), [country]);
+
     const defaultPaymentMethod: CoinmarketPaymentMethodListProps = useMemo(
         () => ({
             value: FORM_DEFAULT_PAYMENT_METHOD,
