debaaff312e2fb9bb1540a984ca5a0a4534dd198
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2024-10-28 09:30:52 +0100

    fix(blockchain-link): export socks-prosy-agent for browser

--- packages/blockchain-link/src/utils/socks-proxy-agent.ts
+++ packages/blockchain-link/src/utils/socks-proxy-agent.ts
@@ -1,6 +1,4 @@
 // socks-proxy-agent is not supported in browser
 // use fallback module. see package.json "browser" field
 
-const createSocksProxyAgent = (_opts: string) => ({});
-
-export default createSocksProxyAgent;
+export const SocksProxyAgent = (_opts: string) => ({});
