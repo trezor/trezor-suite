86c7fd506d94b9b90ab2cc5f2556c7ec67896739
tomasklim <tomas.klim@gmail.com>
2024-10-30 17:44:10 +0100

    chore: tests add text encoder, decoder polyfill

--- packages/suite/jest.setup.js
+++ packages/suite/jest.setup.js
@@ -1,3 +1,7 @@
+const { TextEncoder, TextDecoder } = require('util');
+
+Object.assign(global, { TextDecoder, TextEncoder });
+
 // Fixes issues with Buffer instanceof Uint8Array checks relevant for Solana tests.
 // See: https://github.com/solana-labs/solana-pay/issues/106#issuecomment-1713217913
 const originalHasInstance = Uint8Array[Symbol.hasInstance];
