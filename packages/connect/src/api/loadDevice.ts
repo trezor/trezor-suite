2e1fe6846ca4594bb3f06e1727f881d55d6f0cd3
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2024-10-31 16:43:27 +0100

    fix(connect): only device mode seedless for LoadDevice

--- packages/connect/src/api/loadDevice.ts
+++ packages/connect/src/api/loadDevice.ts
@@ -6,7 +6,7 @@ import { Assert } from '@trezor/schema-utils';
 
 export default class LoadDevice extends AbstractMethod<'loadDevice', PROTO.LoadDevice> {
     init() {
-        this.allowDeviceMode = [UI.INITIALIZE, UI.SEEDLESS];
+        this.allowDeviceMode = [UI.SEEDLESS];
         this.useDeviceState = false;
         this.requiredPermissions = ['management'];
         this.firmwareRange = getFirmwareRange(this.name, null, this.firmwareRange);
