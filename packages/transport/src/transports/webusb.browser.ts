18511812044ce4ffedc221f511f17e57172e0d56
Martin Varmuza <mvarmuza@gmail.com>
2024-11-04 18:56:48 +0100

    fix(connect): webusb sessions sync in all deployments

--- packages/transport/src/transports/webusb.browser.ts
+++ packages/transport/src/transports/webusb.browser.ts
@@ -5,10 +5,7 @@ import { UsbApi } from '../api/usb';
 import { BrowserSessionsBackground } from '../sessions/background-browser';
 
 const defaultSessionsBackgroundUrl =
-    window.location.origin +
-    `${process.env.ASSET_PREFIX || ''}/workers/sessions-background-sharedworker.js`
-        // just in case so that whoever defines ASSET_PREFIX does not need to worry about trailing slashes
-        .replace(/\/+/g, '/');
+    'https://connect.trezor.io/9/workers/sessions-background-sharedworker.js';
 
 type WebUsbTransportParams = AbstractTransportParams & { sessionsBackgroundUrl?: string };
 
@@ -20,7 +17,7 @@ type WebUsbTransportParams = AbstractTransportParams & { sessionsBackgroundUrl?:
 export class WebUsbTransport extends AbstractApiTransport {
     public name = 'WebUsbTransport' as const;
 
-    private readonly sessionsBackgroundUrl;
+    private readonly sessionsBackgroundUrl: string | null = defaultSessionsBackgroundUrl;
 
     constructor({ logger, sessionsBackgroundUrl, ...rest }: WebUsbTransportParams) {
         super({
@@ -28,10 +25,19 @@ export class WebUsbTransport extends AbstractApiTransport {
             logger,
             ...rest,
         });
-        this.sessionsBackgroundUrl = sessionsBackgroundUrl ?? defaultSessionsBackgroundUrl;
+        if (sessionsBackgroundUrl || sessionsBackgroundUrl === null) {
+            this.sessionsBackgroundUrl = sessionsBackgroundUrl;
+        }
     }
 
     private async trySetSessionsBackground() {
+        if (!this.sessionsBackgroundUrl) {
+            this.logger?.log(
+                'No sessionsBackgroundUrl provided. Falling back to use local module.',
+            );
+
+            return;
+        }
         try {
             const response = await fetch(this.sessionsBackgroundUrl, { method: 'HEAD' });
             if (!response.ok) {
