413b7e35e80bd71515f36555970b212d9e955e06
Peter Sanderson <dierk.deidel@protonmail.com>
2024-11-11 14:21:28 +0100

    chore: better comment

--- packages/eslint/eslint.config.mjs
+++ packages/eslint/eslint.config.mjs
@@ -1,5 +1,7 @@
 import { eslint } from './src/index.mjs';
 
+// This config is just for this package.
+// The exported config, that shall be used in other packages, is in src/index.mjs.
 export default [
     ...eslint,
     {
