8bd89763283fb789be5619f751c5427dab21678d
Peter Sanderson <dierk.deidel@protonmail.com>
2024-11-12 11:30:44 +0100

    fix: typo

--- packages/address-validator/eslint.config.mjs
+++ packages/address-validator/eslint.config.mjs
@@ -4,7 +4,7 @@ export default [
     ...eslint,
     {
         ignores: [
-            // This is a JS only codebase written in JS, wo we are not checking it
+            // This is a JS only codebase written in JS, so we are not checking it
             // Todo: reconsider this decision as most of the ESLint issues are auto-fixable
             'src/**/*.js',
             'tests/**/*.js',
