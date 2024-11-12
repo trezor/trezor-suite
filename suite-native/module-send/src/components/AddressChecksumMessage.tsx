3f880e98aeb97fdfe3c15455aa15241c742bef03
Petr Knetl <petr.knetl@satoshilabs.com>
2024-11-12 09:01:05 +0100

    feat(suite-native): checksum message animation

--- suite-native/module-send/src/components/AddressChecksumMessage.tsx
+++ suite-native/module-send/src/components/AddressChecksumMessage.tsx
@@ -1,3 +1,5 @@
+import Animated, { FadeIn, FadeOut } from 'react-native-reanimated';
+
 import { Link } from '@suite-native/link';
 import { HStack, Text } from '@suite-native/atoms';
 import { Icon } from '@suite-native/icons';
@@ -6,23 +8,25 @@ import { Translation } from '@suite-native/intl';
 const LINK_URL = 'https://trezor.io/learn/a/evm-address-checksum-in-trezor-suite';
 
 export const AddressChecksumMessage = () => (
-    <HStack>
-        <Icon name="info" size="medium" color="iconSubdued" />
-        <Text variant="label" color="textSubdued">
-            <Translation
-                id="moduleSend.outputs.recipients.checksum.label"
-                values={{
-                    link: linkChunk => (
-                        <Link
-                            href={LINK_URL}
-                            label={linkChunk}
-                            textVariant="label"
-                            isUnderlined
-                            textColor="textSubdued"
-                        />
-                    ),
-                }}
-            />
-        </Text>
-    </HStack>
+    <Animated.View entering={FadeIn} exiting={FadeOut}>
+        <HStack>
+            <Icon name="info" size="medium" color="iconSubdued" />
+            <Text variant="label" color="textSubdued">
+                <Translation
+                    id="moduleSend.outputs.recipients.checksum.label"
+                    values={{
+                        link: linkChunk => (
+                            <Link
+                                href={LINK_URL}
+                                label={linkChunk}
+                                textVariant="label"
+                                isUnderlined
+                                textColor="textSubdued"
+                            />
+                        ),
+                    }}
+                />
+            </Text>
+        </HStack>
+    </Animated.View>
 );
