7d16751dfced64f71c65eb4888f98c72b667cbdc
Daniel Suchy <suchydan@gmail.com>
2024-11-14 00:24:46 +0100

    fix: android build

--- suite-native/app/app.config.ts
+++ suite-native/app/app.config.ts
@@ -100,6 +100,8 @@ const getPlugins = (): ExpoPlugins => {
             {
                 android: {
                     minSdkVersion: 28,
+                    // this fixes expo-updates build error
+                    kotlinVersion: '1.9.24',
                 },
                 ios: {
                     deploymentTarget: '15.1',
