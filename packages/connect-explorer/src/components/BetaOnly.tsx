3fb9c197866c3d91a796a6469699f99779932634
Martin Varmuza <mvarmuza@gmail.com>
2024-11-19 17:23:18 +0100

    fix(connect-explorer): fix betaOnly condition

--- packages/connect-explorer/src/components/BetaOnly.tsx
+++ packages/connect-explorer/src/components/BetaOnly.tsx
@@ -1,6 +1,4 @@
-export const isBetaOnly = !process.env.CONNECT_EXPLORER_FULL_URL?.startsWith(
-    'https://connect.trezor.io/9/',
-);
+export const isBetaOnly = !window.location.href.startsWith('https://connect.trezor.io/9/');
 
 export const BetaOnly = (props: React.PropsWithChildren) => {
     if (isBetaOnly) {
