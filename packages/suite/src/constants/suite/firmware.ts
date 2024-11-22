8f1cd4a4d8be485792a50d5ac9d32f9b2ca010bd
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2024-11-22 13:36:17 +0100

    feat(suite): ignore FW hash check other-error

--- packages/suite/src/constants/suite/firmware.ts
+++ packages/suite/src/constants/suite/firmware.ts
@@ -8,6 +8,8 @@ export const skippedRevisionCheckErrors = [
 export const skippedHashCheckErrors = [
     'check-skipped',
     'check-unsupported',
+    // TODO this shouldn't be ignored -> investigate false positives
+    'other-error',
     // this could be serious, but it's also caught by revision check, which handles edge-cases better, so it's skipped here
     'unknown-release',
 ] satisfies FirmwareHashCheckError[];
