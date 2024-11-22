c3bdb4866b453e00ee54aba6547eaceafe06005f
Tomas Martykan <t.martykan@gmail.com>
2024-11-22 10:40:21 +0100

    chore(suite-native): use method info instead of core api in connect popup

--- suite-native/module-connect-popup/src/hooks/useConnectMethod.ts
+++ suite-native/module-connect-popup/src/hooks/useConnectMethod.ts
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'react';
 
-import { getMethod } from '@trezor/connect/src/core/method';
-import { AbstractMethod } from '@trezor/connect/src/core/AbstractMethod';
-import { isDevelopOrDebugEnv } from '@suite-native/config';
+import TrezorConnect from '@trezor/connect';
+import type { MethodInfo } from '@trezor/connect/src/core/AbstractMethod';
 
 // Load AbstractMethod from @trezor/connect based on the URL parameters
 export const useConnectMethod = (popupOptions?: { method: string; params: any }) => {
-    const [method, setMethod] = useState<AbstractMethod<any> | undefined>();
+    const [method, setMethod] = useState<MethodInfo | undefined>();
     const [methodError, setMethodError] = useState<string | undefined>();
 
     useEffect(() => {
@@ -20,35 +19,30 @@ export const useConnectMethod = (popupOptions?: { method: string; params: any })
 
         setMethod(undefined);
         setMethodError(undefined);
-        getMethod({
-            id: 0,
-            type: 'iframe-call',
-            payload: {
-                method: popupOptions?.method,
-                ...popupOptions?.params,
-            },
-        })
-            .then(async _method => {
-                _method.init();
-                await _method.initAsync?.();
 
+        const run = async () => {
+            try {
+                // @ts-expect-error method is dynamic
+                const methodInfo = await TrezorConnect[popupOptions.method]({
+                    ...popupOptions.params,
+                    __info: true,
+                });
                 if (
-                    _method.requiredPermissions.includes('management') ||
-                    _method.requiredPermissions.includes('push_tx') ||
-                    (!isDevelopOrDebugEnv() && _method.requiredPermissions.includes('write'))
+                    methodInfo.payload.requiredPermissions.includes('management') ||
+                    methodInfo.payload.requiredPermissions.includes('push_tx')
                 ) {
                     setMethodError('Method requires unsafe permissions');
 
                     return;
                 }
-
-                setMethod(_method);
-            })
-            .catch(e => {
-                console.error('Error while getting method', e);
+                setMethod(methodInfo.payload);
+            } catch (error) {
+                console.error('Error while getting method', error);
                 setMethod(undefined);
-                setMethodError(e.message);
-            });
+                setMethodError(error.message);
+            }
+        };
+        run();
     }, [popupOptions?.method, popupOptions?.params]);
 
     return { method, methodError };
