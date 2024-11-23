ac8da7833d0a8a67b7d77174054b52843a2028d3
Steven Luscher <steven.luscher@anza.xyz>
2024-11-22 23:52:31 -0800

    chore: Update the Solana types in `@trezor/blockchain-link-types`
    This change attempts to largely replicate the old account and transaction shapes, but also represents a breaking change as some of the old types (like `PublicKey`) no longer exist in `@solana/web3.js` version 2.
    
    Some type utilties were added so that we could source the _actual_ data shapes from the API calls themselves. For instance, we choose the return type that results from calling `getTransaction` with the config `"encoding": "jsonParsed"` _in particular_.

--- packages/blockchain-link-types/src/solana.ts
+++ packages/blockchain-link-types/src/solana.ts
@@ -1,10 +1,58 @@
-import type { ParsedTransactionWithMeta } from '@solana/web3.js';
+import type {
+    AccountInfoBase,
+    AccountInfoWithBase58EncodedData,
+    AccountInfoWithBase64EncodedData,
+    AccountInfoWithBase64EncodedZStdCompressedData,
+    AccountInfoWithJsonData,
+    GetTransactionApi,
+    Signature,
+} from '@solana/web3.js';
 
-export type SolanaValidParsedTxWithMeta = ParsedTransactionWithMeta & {
-    meta: Required<NonNullable<ParsedTransactionWithMeta['meta']>>;
-    transaction: Required<ParsedTransactionWithMeta['transaction']>;
-    blockTime: Required<NonNullable<ParsedTransactionWithMeta['blockTime']>>;
+import type {
+    GetObjectWithKey,
+    GetObjectWithoutKey,
+    ObjectsOnly,
+    Overloads,
+} from '@trezor/type-utils';
+
+type GetTransactionApiOverloads = Overloads<GetTransactionApi['getTransaction']>;
+type GetJsonParsedTransactionApiOverloads = {
+    [P in keyof GetTransactionApiOverloads]: GetTransactionApiOverloads[P] extends (
+        ...args: [
+            signature: Signature,
+            config: Readonly<{
+                encoding: 'jsonParsed';
+            }>,
+        ]
+    ) => infer R
+        ? R
+        : never;
 };
+
+export type ParsedTransactionWithMeta = Pick<
+    SolanaValidParsedTxWithMeta,
+    'slot' | 'transaction' | 'meta'
+> &
+    Readonly<{
+        blockTime?: SolanaValidParsedTxWithMeta['blockTime'];
+        version?: SolanaValidParsedTxWithMeta['version'];
+    }>;
+
+export type PartiallyDecodedInstruction = GetObjectWithoutKey<
+    SolanaValidParsedTxWithMeta['transaction']['message']['instructions'][number],
+    'parsed'
+>;
+
+export type ParsedAccountData = ObjectsOnly<AccountInfoWithJsonData['data']>;
+
+export type ParsedInstruction = GetObjectWithKey<
+    SolanaValidParsedTxWithMeta['transaction']['message']['instructions'][number],
+    'parsed'
+>;
+
+export type SolanaValidParsedTxWithMeta = NonNullable<
+    ObjectsOnly<GetJsonParsedTransactionApiOverloads[keyof GetTransactionApiOverloads]>
+>;
 
 export type SolanaTokenAccountInfo = {
     address: string;
@@ -12,13 +60,14 @@ export type SolanaTokenAccountInfo = {
     decimals: number | undefined;
 };
 
-export type {
-    ParsedInstruction,
-    ParsedTransactionWithMeta,
-    PartiallyDecodedInstruction,
-    AccountInfo,
-    ParsedAccountData,
-    PublicKey,
-} from '@solana/web3.js';
+export type AccountInfo<
+    TData extends
+        | AccountInfoWithBase58EncodedData['data']
+        | AccountInfoWithBase64EncodedData['data']
+        | AccountInfoWithBase64EncodedZStdCompressedData['data']
+        | AccountInfoWithJsonData['data'],
+> = AccountInfoBase & Readonly<{ data: TData }>;
+
+export type { Address } from '@solana/web3.js';
 
 export type TokenDetailByMint = { [mint: string]: { name: string; symbol: string } };
