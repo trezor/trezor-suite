ca5d7d92c1e249de3ae4fbfddbf7fdec0e1c5303
trezor-ci <trezor-ci@trezor.io>
2024-11-26 10:29:08 +0000

    npm-release: @trezor/connect 9.4.4

--- packages/connect-web/src/webextension/trezor-usb-permissions.js
+++ packages/connect-web/src/webextension/trezor-usb-permissions.js
@@ -1,4 +1,4 @@
-const VERSION = '9.4.4-beta.1';
+const VERSION = '9.4.4';
 const versionN = VERSION.split('.').map(s => parseInt(s, 10));
 
 const isBeta = VERSION.includes('beta');
--- packages/connect/src/data/version.ts
+++ packages/connect/src/data/version.ts
@@ -1,4 +1,4 @@
-export const VERSION = '9.4.4-beta.1';
+export const VERSION = '9.4.4';
 
 const versionN = VERSION.split('.').map(s => parseInt(s, 10));
 
