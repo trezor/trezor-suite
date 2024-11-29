0ccf71e55e067d287a3b4763a6f5aa18dc4ef26e
Pavlo Syrotyna <p.syrotyna@everstake.one>
2024-11-29 12:51:08 +0200

    fix(suite-native): remove wallet-sdk mock

--- suite-native/test-utils/src/walletSdkMock.js
+++
@@ -1,8 +0,0 @@
-jest.mock('@everstake/wallet-sdk/ethereum', () => ({
-    selectNetwork: jest.fn().mockImplementation(network => {
-        return {
-            address_pool: `mocked_pool_${network}`,
-            address_withdraw_treasury: `mocked_treasury_${network}`,
-        };
-    }),
-}));
