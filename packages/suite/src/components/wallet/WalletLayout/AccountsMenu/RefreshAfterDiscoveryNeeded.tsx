16c80a8beff06f8466055dd68f54846cb9540931
Jan Václavík <jvaclavik@gmail.com>
2024-12-02 15:48:52 +0100

    fix(suite): Fix refresh button in collapsed sidebar

--- packages/suite/src/components/wallet/WalletLayout/AccountsMenu/RefreshAfterDiscoveryNeeded.tsx
+++ packages/suite/src/components/wallet/WalletLayout/AccountsMenu/RefreshAfterDiscoveryNeeded.tsx
@@ -3,14 +3,15 @@ import React from 'react';
 import styled from 'styled-components';
 import { AnimatePresence, MotionProps, motion } from 'framer-motion';
 
-import { Button, motionEasing } from '@trezor/components';
-import { spacingsPx, typography } from '@trezor/theme';
+import { Button, IconButton, motionEasing, Row, Tooltip } from '@trezor/components';
+import { spacings, spacingsPx, typography } from '@trezor/theme';
 import { selectDevice, startDiscoveryThunk } from '@suite-common/wallet-core';
 
 import { useRediscoveryNeeded, useDispatch, useSelector } from 'src/hooks/suite';
 import { Translation } from 'src/components/suite';
 
 import { AccountsMenuNotice } from './AccountsMenuNotice';
+import { useIsSidebarCollapsed } from '../../../suite/layouts/SuiteLayout/Sidebar/utils';
 
 const DiscoveryButtonContainer = styled(motion.div)`
     display: flex;
@@ -42,7 +43,7 @@ export const RefreshAfterDiscoveryNeeded = () => {
     const dispatch = useDispatch();
     const isDiscoveryButtonVisible = useRediscoveryNeeded();
     const selectedDevice = useSelector(selectDevice);
-
+    const isSidebarCollapsed = useIsSidebarCollapsed();
     if (!selectedDevice?.connected) {
         return null;
     }
@@ -54,15 +55,38 @@ export const RefreshAfterDiscoveryNeeded = () => {
     return (
         <AnimatePresence>
             {isDiscoveryButtonVisible && (
-                <DiscoveryButtonContainer {...animationConfig}>
-                    <AccountsMenuNotice>
-                        <Translation id="TR_DISCOVERY_NEW_COINS_TEXT" isNested={false} />
-                    </AccountsMenuNotice>
+                <>
+                    {isSidebarCollapsed ? (
+                        <Row
+                            justifyContent="center"
+                            margin={{ top: spacings.xs, bottom: spacings.lg }}
+                        >
+                            <Tooltip content={<Translation id="REFRESH" />}>
+                                <IconButton
+                                    variant="tertiary"
+                                    size="tiny"
+                                    icon="refresh"
+                                    onClick={startDiscovery}
+                                />
+                            </Tooltip>
+                        </Row>
+                    ) : (
+                        <DiscoveryButtonContainer {...animationConfig}>
+                            <AccountsMenuNotice>
+                                <Translation id="TR_DISCOVERY_NEW_COINS_TEXT" isNested={false} />
+                            </AccountsMenuNotice>
 
-                    <Button variant="tertiary" size="tiny" icon="refresh" onClick={startDiscovery}>
-                        <Translation id="REFRESH" />
-                    </Button>
-                </DiscoveryButtonContainer>
+                            <Button
+                                variant="tertiary"
+                                size="tiny"
+                                icon="refresh"
+                                onClick={startDiscovery}
+                            >
+                                <Translation id="REFRESH" />
+                            </Button>
+                        </DiscoveryButtonContainer>
+                    )}
+                </>
             )}
         </AnimatePresence>
     );
