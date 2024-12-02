c8dd18a955fc35af5548518d92ee9624e0bce0f7
Martin Vytick Vytrhlík <vytick@gmail.com>
2024-12-02 12:09:00 +0100

    chore(suite-native): polyfills for solana web3 2_0

--- suite-native/app/globalPolyfills.js
+++ suite-native/app/globalPolyfills.js
@@ -1,5 +1,14 @@
 import { install } from 'react-native-quick-crypto';
 
+import { EventTarget, Event } from 'event-target-shim'; // to work with Solana/web3.js
+import 'abortcontroller-polyfill/dist/polyfill-patch-fetch'; // to work with Solana/web3.js
+import { CustomEvent } from '@whatwg-node/events'; // to work with Solana/web3.js
+
+// Event, EventTarget and CustomEvent are needed for Solana/web3.js to work
+globalThis.Event = Event;
+globalThis.EventTarget = EventTarget;
+globalThis.CustomEvent = CustomEvent;
+
 // Ensures that crypto functions required by Solana and device authenticity check are available.
 install();
 
