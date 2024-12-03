a27b93330f97813e2916dbcfc23058d063b3c2ac
Prochmi99 <152851523+MiroslavProchazka@users.noreply.github.com>
2024-12-03 16:49:58 +0100

    fix(suite): change kb url for tor snowflake

--- packages/suite/src/constants/suite/experimental.ts
+++ packages/suite/src/constants/suite/experimental.ts
@@ -1,6 +1,6 @@
 import { TranslationKey } from '@suite-common/intl-types';
 import { desktopApi } from '@trezor/suite-desktop-api';
-import { EXPERIMENTAL_PASSWORD_MANAGER_KB_URL, TOR_SNOWFLAKE_PROJECT_URL, Url } from '@trezor/urls';
+import { EXPERIMENTAL_PASSWORD_MANAGER_KB_URL, TOR_SNOWFLAKE_KB_URL, Url } from '@trezor/urls';
 import { Route } from '@suite-common/suite-types';
 
 import { Dispatch } from '../../types/suite';
@@ -26,7 +26,7 @@ export const EXPERIMENTAL_FEATURES: Record<ExperimentalFeature, ExperimentalFeat
     'tor-snowflake': {
         title: 'TR_EXPERIMENTAL_TOR_SNOWFLAKE',
         description: 'TR_EXPERIMENTAL_TOR_SNOWFLAKE_DESCRIPTION',
-        knowledgeBaseUrl: TOR_SNOWFLAKE_PROJECT_URL,
+        knowledgeBaseUrl: TOR_SNOWFLAKE_KB_URL,
         onToggle: async ({ newValue }) => {
             if (!newValue) {
                 const result = await desktopApi.getTorSettings();
