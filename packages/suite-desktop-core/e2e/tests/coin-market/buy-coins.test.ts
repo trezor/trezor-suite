9ead4d43ca0dd7d7a4e5fef093658b3f70116a30
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2024-12-19 20:16:36 +0100

    fix(e2e): Wrong test group

--- packages/suite-desktop-core/e2e/tests/coin-market/buy-coins.test.ts
+++ packages/suite-desktop-core/e2e/tests/coin-market/buy-coins.test.ts
@@ -2,7 +2,7 @@ import { test, expect } from '../../support/fixtures';
 
 const regexpBtcValue = /^\d+(\.\d+)? BTC$/;
 
-test.describe('Coin market buy', { tag: ['@group_other'] }, () => {
+test.describe('Coin market buy', { tag: ['@group=other'] }, () => {
     test.use({ emulatorStartConf: { wipe: true } });
     test.beforeEach(async ({ onboardingPage, dashboardPage, marketPage }) => {
         await onboardingPage.completeOnboarding();
