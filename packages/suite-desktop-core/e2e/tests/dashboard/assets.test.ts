89bd896eae4fd8f92d7a97d7500220d64279a04d
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2024-12-28 21:53:09 +0100

    fix(e2e): adds missing await

--- packages/suite-desktop-core/e2e/tests/dashboard/assets.test.ts
+++ packages/suite-desktop-core/e2e/tests/dashboard/assets.test.ts
@@ -13,13 +13,13 @@ test.describe('Assets', { tag: ['@group=suite'] }, () => {
     test('User can initiate buy from Assets in table view', async ({ assetsPage, marketPage }) => {
         await assetsPage.tableIcon.click();
         await assetsPage.buyAssetButton('btc').click();
-        expect(marketPage.section).toBeVisible();
+        await expect(marketPage.section).toBeVisible();
     });
 
     test('User can initiate buy from Assets in grid view', async ({ assetsPage, marketPage }) => {
         await assetsPage.gridIcon.click();
         await assetsPage.buyAssetButton('btc').click();
-        expect(marketPage.section).toBeVisible();
+        await expect(marketPage.section).toBeVisible();
     });
 
     test('New asset is shown in both grid and row', async ({
