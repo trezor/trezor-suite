b10e45b0295dbd157cd497163b31981082eb2414
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2025-01-02 19:23:46 +0100

    fix(e2e): Fix dry backup run test by switching to 12x all mnemonics

--- packages/suite-desktop-core/e2e/tests/recovery/t1b1-dry-run.test.ts
+++ packages/suite-desktop-core/e2e/tests/recovery/t1b1-dry-run.test.ts
@@ -1,13 +1,13 @@
+import { MNEMONICS } from '@trezor/trezor-user-env-link';
+
 import { test, expect } from '../../support/fixtures';
 
-const mnemonic =
-    'nasty answer gentle inform unaware abandon regret supreme dragon gravity behind lava dose pilot garden into dynamic outer hard speed luxury run truly armed';
 const pin = '1';
 
 test.describe('Recovery T1B1 - dry run', { tag: ['@group=device-management'] }, () => {
     test.use({
         emulatorStartConf: { model: 'T1B1', version: '1-latest', wipe: true },
-        emulatorSetupConf: { mnemonic, pin },
+        emulatorSetupConf: { mnemonic: MNEMONICS.mnemonic_all, pin },
     });
 
     test.beforeEach(async ({ onboardingPage, settingsPage }) => {
@@ -23,9 +23,9 @@ test.describe('Recovery T1B1 - dry run', { tag: ['@group=device-management'] }, 
         trezorUserEnvLink,
     }) => {
         await settingsPage.checkSeedButton.click();
-        await recoveryPage.initDryCheck('basic', 24);
+        await recoveryPage.initDryCheck('basic', 12);
         await trezorInput.enterPinOnBlindMatrix(pin);
-        await trezorInput.inputMnemonicT1B1(mnemonic);
+        await trezorInput.inputMnemonicT1B1(MNEMONICS.mnemonic_all);
         await trezorUserEnvLink.pressYes();
         await expect(recoveryPage.successTitle).toHaveText('Wallet backup checked successfully');
     });
