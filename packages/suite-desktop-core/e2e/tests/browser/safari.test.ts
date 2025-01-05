a9e0439c09b7a17c6e28aa560cc1d2778f7542fe
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2025-01-05 13:17:23 +0100

    feat(e2e): Convert to pw browser tests
    Unfortunatelly, PW does not support Firefox + Mobile combination
    It is also not recommended to run automation on outdated browsers

---
+++ packages/suite-desktop-core/e2e/tests/browser/safari.test.ts
@@ -0,0 +1,16 @@
+import { test, expect } from '../../support/fixtures';
+
+test.use({ emulatorStartConf: { wipe: true }, browserName: 'webkit' });
+test.describe('Safari', { tag: ['@group=other', '@webOnly'] }, () => {
+    test('Should display unsupported browsers page', async ({ page }) => {
+        await expect(
+            page.getByRole('heading', { name: 'Your browser is not supported' }),
+        ).toBeVisible();
+        await expect(page.getByRole('link', { name: 'Desktop App Download' })).toBeVisible();
+        await expect(page.getByRole('link', { name: /Chrome \d+\+? Download/ })).toBeVisible();
+        //TODO:  #16073 add visual regression test
+        await expect(page.getByTestId('@continue-to-suite')).toHaveText('Continue at my own risk');
+        await page.getByTestId('@continue-to-suite').click();
+        await expect(page.getByTestId('@welcome/title')).toBeVisible();
+    });
+});
