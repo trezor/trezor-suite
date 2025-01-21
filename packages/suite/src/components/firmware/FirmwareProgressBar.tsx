2db40a5dfed7c8743da2ff2cce78e86e557a0aaa
Jan Václavík <jvaclavik@gmail.com>
2025-01-21 13:50:27 +0100

    fix(suite): Fix firmware update progress bar (#16417)
    (cherry picked from commit df170dc9600e643ba0b7424169007d063df03b09)

--- packages/suite/src/components/firmware/FirmwareProgressBar.tsx
+++ packages/suite/src/components/firmware/FirmwareProgressBar.tsx
@@ -1,54 +1,15 @@
 import styled, { useTheme } from 'styled-components';
 
-import { Icon, ProgressBar, variables } from '@trezor/components';
-import { borders, spacingsPx } from '@trezor/theme';
+import { Box, Column, Icon, ProgressBar, Row, Text } from '@trezor/components';
+import { spacings } from '@trezor/theme';
 import { TranslationKey } from '@suite-common/intl-types';
 import { FirmwareOperationStatus, useFirmwareInstallation } from '@suite-common/firmware';
 
 import { Translation } from '../suite';
 
-const Wrapper = styled.div`
-    display: flex;
-    border-radius: ${borders.radii.xs};
-    padding: ${spacingsPx.lg} ${spacingsPx.xl};
-    width: 100%;
-    font-size: ${variables.FONT_SIZE.SMALL};
-    color: ${({ theme }) => theme.legacy.TYPE_LIGHT_GREY};
-    align-items: center;
-
-    ${variables.SCREEN_QUERY.ABOVE_LAPTOP} {
-        &:last-child {
-            border-radius: 0 0 ${borders.radii.md} ${borders.radii.md};
-        }
-    }
-`;
-
-const Label = styled.div`
-    display: flex;
-    margin-right: ${spacingsPx.lg};
-    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
-`;
-
-// eslint-disable-next-line local-rules/no-override-ds-component
-const StyledProgressBar = styled(ProgressBar)`
-    display: flex;
-    margin-right: ${spacingsPx.lg};
-    border-radius: ${borders.radii.xxs};
-    background: ${({ theme }) => theme.legacy.STROKE_GREY_ALT};
-    flex: 1;
-
-    ${ProgressBar.Value} {
-        height: 3px;
-        position: relative;
-        border-radius: ${borders.radii.xxs};
-    }
-`;
 const Percentage = styled.div`
-    display: flex;
-    margin-left: ${spacingsPx.sm};
-    font-weight: ${variables.FONT_WEIGHT.DEMI_BOLD};
     font-variant-numeric: tabular-nums;
-    height: ${spacingsPx.xl};
+    width: 30px;
 `;
 
 const mapOperationToTransaltionId: Record<
@@ -68,20 +29,29 @@ export const FirmwareProgressBar = () => {
     const isDone = progress === 100;
 
     return (
-        <Wrapper>
-            {operation && (
-                <Label>
-                    <Translation id={mapOperationToTransaltionId[operation]} />
-                </Label>
-            )}
-            <StyledProgressBar value={progress} />
-            <Percentage>
-                {isDone ? (
-                    <Icon name="check" color={theme.legacy.TYPE_GREEN} size={24} />
-                ) : (
-                    `${progress} %`
-                )}
-            </Percentage>
-        </Wrapper>
+        <Box width="100%">
+            <Column margin={{ vertical: spacings.md, horizontal: spacings.lg }}>
+                <Text typographyStyle="hint" variant="tertiary">
+                    {operation ? <Translation id={mapOperationToTransaltionId[operation]} /> : ' '}
+                </Text>
+
+                <Row gap={spacings.lg} justifyContent="space-between">
+                    <ProgressBar
+                        value={progress}
+                        backgroundColor={theme.backgroundNeutralSubtleOnElevationNegative}
+                    />
+                    <Percentage>
+                        {isDone ? (
+                            <Icon name="check" variant="primary" size={24} />
+                        ) : (
+                            <Text typographyStyle="highlight">
+                                {progress}
+                                {'\u00A0'}%
+                            </Text>
+                        )}
+                    </Percentage>
+                </Row>
+            </Column>
+        </Box>
     );
 };
