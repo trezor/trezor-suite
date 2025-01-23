2d40e1a8570d0c8ed8ffd7502fe418d6a46d0608
Martin Varmuza <mvarmuza@gmail.com>
2025-01-23 16:49:59 +0100

    fixup! chore(deps): remove postcss loaders

--- packages/suite-data/browser-detection.webpack.ts
+++ packages/suite-data/browser-detection.webpack.ts
@@ -43,7 +43,6 @@ module.exports = {
                             },
                         },
                     },
-                    'postcss-loader',
                 ],
             },
             {
