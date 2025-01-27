9e6c1833c2fa48a5fd0911a5dec7f5a5c3c20e59
Petr Knetl <petr.knetl@satoshilabs.com>
2025-01-27 06:51:03 +0000

    refactor(suite-native): icon list item used in analytics screen

--- suite-native/module-onboarding/src/components/AnalyticsInfoRow.tsx
+++ suite-native/module-onboarding/src/components/AnalyticsInfoRow.tsx
@@ -1,8 +1,7 @@
 import { ReactNode } from 'react';
 
-import { Box, HStack, Text, VStack } from '@suite-native/atoms';
-import { Icon, IconName } from '@suite-native/icons';
-import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
+import { IconListItem, Text, VStack } from '@suite-native/atoms';
+import { IconName } from '@suite-native/icons';
 
 type AnalyticsInfoRowProps = {
     iconName: IconName;
@@ -10,33 +9,13 @@ type AnalyticsInfoRowProps = {
     description: ReactNode;
 };
 
-const WRAPPER_SIZE = 36;
-
-const iconWrapper = prepareNativeStyle(utils => ({
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: WRAPPER_SIZE,
-    height: WRAPPER_SIZE,
-    backgroundColor: utils.colors.backgroundSurfaceElevation2,
-    borderRadius: utils.borders.radii.r12,
-    borderWidth: 1,
-    borderColor: utils.colors.backgroundTertiaryDefaultOnElevation0,
-}));
-
-export const AnalyticsInfoRow = ({ iconName, title, description }: AnalyticsInfoRowProps) => {
-    const { applyStyle } = useNativeStyles();
-
-    return (
-        <HStack spacing="sp12" flexDirection="row" alignItems="center">
-            <Box style={applyStyle(iconWrapper)}>
-                <Icon name={iconName} size="mediumLarge" />
-            </Box>
-            <VStack spacing="sp4" flex={1}>
-                <Text variant="highlight">{title}</Text>
-                <Text variant="hint" color="textSubdued">
-                    {description}
-                </Text>
-            </VStack>
-        </HStack>
-    );
-};
+export const AnalyticsInfoRow = ({ iconName, title, description }: AnalyticsInfoRowProps) => (
+    <IconListItem icon={iconName} iconSize="mediumLarge">
+        <VStack spacing="sp4" flex={1}>
+            <Text variant="highlight">{title}</Text>
+            <Text variant="hint" color="textSubdued">
+                {description}
+            </Text>
+        </VStack>
+    </IconListItem>
+);
