0bb8881ec1684c4e2a202293eb39c0e465dd0311
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2025-01-30 12:23:53 +0000

    wip

--- scripts/replace-imports.sh
+++ scripts/replace-imports.sh
@@ -15,22 +15,28 @@ else
     REGEX="s/@trezor\/([^/]+)\/src/@trezor\/\1\/lib/g"
 fi
 
-if [[ "$LIB_TYPE" == "libESM" ]]; then
-    REGEX="s/@trezor\/([^/]+)\/src/@trezor\/\1\/libESM/g"
-else
-    REGEX="s/@trezor\/([^/]+)\/src/@trezor\/\1\/lib/g"
-fi
-
 # Determine the operating system
 OS="$(uname)"
+
+echo "Changing files in directory: $1"
 
 # Execute the appropriate command based on the OS
 if [[ "$OS" == "Darwin" ]]; then
     # macOS command with -i '' for in-place editing without backup and -E for extended regex
-    find "$1" -type f -exec sed -i '' -E "$REGEX" {} +
+    find "$1" -type f | while read -r file; do
+        if grep -q "@trezor/[^/]+/src" "$file"; then
+            echo "Changing: $file" >> "$LOG_FILE"
+            sed -i '' -E "$REGEX" "$file"
+        fi
+    done
 else
     # Linux command with -i and -E for in-place editing without backup (GNU sed syntax) and extended regex
-    find "$1" -type f -exec sed -i -E "$REGEX" {} +
+    find "$1" -type f | while read -r file; do
+        if grep -q "@trezor/[^/]+/src" "$file"; then
+            echo "Changing: $file" >> "$LOG_FILE"
+            sed -i -E "$REGEX" "$file"
+        fi
+    done
 fi
 
 # Safety check to ensure that all occurrences of '@trezor/*/src' have been replaced
