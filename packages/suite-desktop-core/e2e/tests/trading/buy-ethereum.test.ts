1e968067338abd09511dc1ce2e00805e24c291b1
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2025-02-03 10:04:08 +0100

    refactor(e2e): File renaming of coinMarket section

---
+++ packages/suite-desktop-core/e2e/tests/trading/buy-ethereum.test.ts
@@ -0,0 +1,5 @@
+import { test } from '../../support/fixtures';
+
+test.describe('Trading - Buy Solana', { tag: ['@group=other', '@webOnly'] }, () => {
+    test.skip('Buy Ethereum that is not discovered on wallet yet', () => {});
+});
