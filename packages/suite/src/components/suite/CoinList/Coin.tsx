ab1d90c8ceff880f3857d9e12ad46f98454a2a68
tomasklim <tomas.klim@gmail.com>
2025-02-04 17:38:44 +0100

    fix(suite): network/token icons
    (cherry picked from commit 2ec93d9a2db2bca7580f6353c893e4f3c18f5736)

--- packages/suite/src/components/suite/CoinList/Coin.tsx
+++ packages/suite/src/components/suite/CoinList/Coin.tsx
@@ -201,7 +201,7 @@ export const Coin = ({
             data-active={toggled}
         >
             <ImageWrapper>
-                <CoinLogo size={24} symbol={symbol} type="network" />
+                <CoinLogo size={24} symbol={symbol} type="coin" />
                 <Check $visible={toggled}>
                     <Icon size={8} color={theme.legacy.TYPE_WHITE} name="check" />
                 </Check>
