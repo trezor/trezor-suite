5e1811fb1163db192251e9d65ab471f60b2a3293
Jan Komarek <jan.komarek@satoshilabs.com>
2025-02-04 14:31:33 +0100

    chore(suite-data): update tor binaries

--- packages/suite-data/files/bin/tor/update.sh
+++ packages/suite-data/files/bin/tor/update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-CRX_VER=1_0_36
-CRX_LINUX_ARM_VER=1_0_5
+CRX_VER=1_0_37
+CRX_LINUX_ARM_VER=1_0_6
 
 # check whether we have all required commands
 for cmd in 7z curl lipo shasum ; do
