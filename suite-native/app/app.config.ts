a748ce9f856b3bdb07edf84200333d15f2a5a213
Matej Kriz <matej@matejkriz.cz>
2025-02-07 16:07:59 +0100

    chore(suite-native): bump runtimeVersion 27 for testing

--- suite-native/app/app.config.ts
+++ suite-native/app/app.config.ts
@@ -168,7 +168,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         slug: appSlugs[buildType],
         owner: appOwners[buildType],
         version: suiteNativeVersion,
-        runtimeVersion: '26',
+        runtimeVersion: '27',
         ...(buildType === 'production'
             ? {}
             : {
