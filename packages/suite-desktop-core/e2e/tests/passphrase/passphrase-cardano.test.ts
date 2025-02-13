78c96bdf42ea5eba80a8ca6b9f500770869ff9c3
Ondřej Hájek <ondrej.hajek@satoshilabs.com>
2025-02-13 21:21:24 +0100

    feat(e2e): Migrated all passphrase tests (#16967)
    * feat(e2e): Migrated all passphrase tests
    
    * fix(e2e): Fixed adding passphrase wallet in sell bitcoin test

---
+++ packages/suite-desktop-core/e2e/tests/passphrase/passphrase-cardano.test.ts
@@ -0,0 +1,81 @@
+import { splitStringEveryNCharacters } from '@trezor/utils';
+
+import { expect, test } from '../../support/fixtures';
+
+const correctPassphraseAddr =
+    'addr1qx3ufjpwcx30ee73a7r29surauze6yt0jvr7c3rnahw0hnppg7qp5xvslcfucsqqayrtjhm4u66x';
+const passphrase = 'secret passphrase A';
+
+test.describe('Passphrase with cardano', { tag: ['@group=passphrase'] }, () => {
+    test.use({ emulatorSetupConf: { mnemonic: 'mnemonic_all', passphrase_protection: true } });
+    test.beforeEach(async ({ onboardingPage, dashboardPage }) => {
+        await onboardingPage.completeOnboarding();
+        await dashboardPage.discoveryShouldFinish();
+    });
+
+    test('verify cardano address behind passphrase', async ({
+        page,
+        settingsPage,
+        dashboardPage,
+        walletPage,
+        devicePrompt,
+        trezorUserEnvLink,
+        emulatorStartConf,
+    }) => {
+        async function restartEmulator() {
+            await trezorUserEnvLink.stopEmu();
+            await expect(page.getByTestId('@deviceStatus-disconnected')).toBeVisible();
+            await trezorUserEnvLink.startEmu({ model: emulatorStartConf.model, wipe: false });
+            await expect(page.getByTestId('@deviceStatus-connected')).toBeVisible();
+        }
+
+        await settingsPage.navigateTo('coins');
+        await settingsPage.coins.enableNetwork('ada');
+
+        // starting discovery triggers passphrase dialogue
+        await dashboardPage.dashboardMenuButton.click();
+        await dashboardPage.discoveryShouldFinish();
+        await dashboardPage.openDeviceSwitcher();
+        await dashboardPage.addUnusedHiddenWallet(passphrase);
+
+        // turn on view-only on the hidden wallet
+        await dashboardPage.openDeviceSwitcher();
+        await dashboardPage.setViewOnlyForWallet(1, 'enabled');
+        await dashboardPage.deviceSwitchingCloseButton.click();
+
+        // restart device
+        await restartEmulator();
+
+        // reveal cardano address
+        await walletPage.accountButton({ symbol: 'ada', type: 'normal', atIndex: 0 }).click();
+        await walletPage.receiveButton.click();
+        await walletPage.revealAddressButton.click();
+
+        // device after reset asks for passphrase again, enter correct passphrase associated with this account
+        await dashboardPage.passphraseInput.fill(passphrase);
+        await dashboardPage.passphraseSubmitButton.click();
+        await devicePrompt.waitForPromptAndConfirm(); // Confirm next screen shows your passphrase
+        await devicePrompt.waitForPromptAndConfirm(); // Confirm passphrase
+
+        await expect(page.getByTestId('@modal/output-value')).toContainText(
+            splitStringEveryNCharacters(correctPassphraseAddr, 4).join(' '),
+        );
+        await devicePrompt.waitForPromptAndConfirm(); // Confirm receive address
+
+        await expect(page.getByTestId('@metadata/copy-address-button')).toBeVisible();
+        await page.getByTestId('@modal/close-button').click();
+        await expect(walletPage.revealAddressButton).toBeVisible();
+
+        // restart device again
+        await restartEmulator();
+
+        // reveal cardano address, now enter wrong passphrase
+        await walletPage.revealAddressButton.click();
+        await dashboardPage.passphraseInput.fill('wrong passphrase');
+        await dashboardPage.passphraseSubmitButton.click();
+        await devicePrompt.waitForPromptAndConfirm(); // Confirm next screen shows your passphrase
+        await devicePrompt.waitForPromptAndConfirm(); // Confirm passphrase
+
+        await expect(page.getByTestId('@toast/verify-address-error')).toBeVisible();
+    });
+});
---
+++ packages/suite-desktop-core/e2e/tests/passphrase/passphrase-duplicate.test.ts
@@ -0,0 +1,28 @@
+import { expect, test } from '../../support/fixtures';
+
+test.describe('Passphrase duplicate', { tag: ['@group=passphrase'] }, () => {
+    test.use({ emulatorSetupConf: { passphrase_protection: true } });
+    test.beforeEach(async ({ onboardingPage, dashboardPage, trezorUserEnvLink }) => {
+        await trezorUserEnvLink.applySettings({ passphrase_always_on_device: false });
+        await onboardingPage.completeOnboarding();
+        await dashboardPage.discoveryShouldFinish();
+    });
+
+    test('attempt to add the same hidden wallet twice results in warning', async ({
+        page,
+        dashboardPage,
+    }) => {
+        const passphraseToType = 'taxation is theft';
+
+        // enter passphrase A for the first time
+        await dashboardPage.openDeviceSwitcher();
+        await dashboardPage.addUnusedHiddenWallet(passphraseToType);
+
+        // try to add another wallet with the same passphrase
+        await dashboardPage.openDeviceSwitcher();
+        await dashboardPage.addHiddenWallet(passphraseToType, { skipDiscovery: true });
+
+        // duplicate passphrase modal appears;
+        await expect(page.getByTestId('@passphrase-duplicate-header')).toBeVisible();
+    });
+});
