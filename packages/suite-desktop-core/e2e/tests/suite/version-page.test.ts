6a9f79b173b315a7e6f34b9c354548af83b3e333
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2025-02-14 12:17:44 +0100

    fix(e2e): version-page test refactored adn skipped
    The test was unstable and falsely passing

--- packages/suite-desktop-core/e2e/tests/suite/version-page.test.ts
+++ packages/suite-desktop-core/e2e/tests/suite/version-page.test.ts
@@ -1,8 +1,9 @@
-import { getSuiteVersion } from '@trezor/env-utils';
+import { getCommitHash, getSuiteVersion } from '@trezor/env-utils';
 
 import { expect, test } from '../../support/fixtures';
 
-test.describe('Hidden version page', { tag: ['@group=suite', '@webOnly'] }, () => {
+//TODO: Fix #17001
+test.describe.skip('Hidden version page', { tag: ['@group=suite', '@webOnly'] }, () => {
     test.beforeEach(async ({ trezorUserEnvLink }) => {
         await trezorUserEnvLink.stopEmu();
         await trezorUserEnvLink.stopBridge();
@@ -11,8 +12,15 @@ test.describe('Hidden version page', { tag: ['@group=suite', '@webOnly'] }, () =
 
     test('is accessible via route', async ({ url, page }) => {
         const suiteVersion = getSuiteVersion();
+        const suiteCommitHash = getCommitHash();
         await page.goto(url + 'version');
-        await expect(page.getByTestId('@version/number')).toContainText(suiteVersion);
-        await page.getByTestId('@modal/version').screenshot({ path: 'version-modal.png' });
+        await expect(page.getByTestId('@version/number')).toHaveText(suiteVersion, {
+            timeout: 30_000,
+        });
+        await expect(page.getByTestId('@version/commit-hash-link')).toHaveText(suiteCommitHash);
+        const hashLink = await page.getByTestId('@version/commit-hash-link').getAttribute('href');
+        expect(hashLink).toContain(
+            `https://github.com/trezor/trezor-suite/commits/${suiteCommitHash}`,
+        );
     });
 });
