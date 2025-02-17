a7a696647982cab583a6697f0654cb9521a6b01d
Martin Vytick Vytrhlík <vytick@gmail.com>
2025-02-17 21:31:13 +0100

    fix(suite-native): staking icon size

--- suite-native/accounts/src/components/AccountsList/AccountsListStakingItem.tsx
+++ suite-native/accounts/src/components/AccountsList/AccountsListStakingItem.tsx
@@ -2,6 +2,7 @@ import { Account } from '@suite-common/wallet-types';
 import { RoundedIcon } from '@suite-native/atoms';
 import { CryptoAmountFormatter, CryptoToFiatAmountFormatter } from '@suite-native/formatters';
 import { Translation } from '@suite-native/intl';
+import { useNativeStyles } from '@trezor/styles';
 
 import { AccountsListItemBase } from './AccountsListItemBase';
 
@@ -20,27 +21,37 @@ export const AccountsListStakingItem = ({
     stakingCryptoBalance,
     isLast,
     ...props
-}: AccountsListStakingItemProps) => (
-    <AccountsListItemBase
-        {...props}
-        isLast={isLast}
-        showDivider={!isLast}
-        icon={<RoundedIcon name="piggyBankFilled" color="iconSubdued" />}
-        title={<Translation id="accountList.staking" />}
-        mainValue={
-            <CryptoToFiatAmountFormatter
-                value={stakingCryptoBalance}
-                symbol={account.symbol}
-                isBalance
-            />
-        }
-        secondaryValue={
-            <CryptoAmountFormatter
-                value={stakingCryptoBalance}
-                symbol={account.symbol}
-                numberOfLines={1}
-                adjustsFontSizeToFit
-            />
-        }
-    />
-);
+}: AccountsListStakingItemProps) => {
+    const { utils } = useNativeStyles();
+
+    return (
+        <AccountsListItemBase
+            {...props}
+            isLast={isLast}
+            showDivider={!isLast}
+            icon={
+                <RoundedIcon
+                    name="piggyBankFilled"
+                    color="iconSubdued"
+                    containerSize={utils.spacings.sp32}
+                />
+            }
+            title={<Translation id="accountList.staking" />}
+            mainValue={
+                <CryptoToFiatAmountFormatter
+                    value={stakingCryptoBalance}
+                    symbol={account.symbol}
+                    isBalance
+                />
+            }
+            secondaryValue={
+                <CryptoAmountFormatter
+                    value={stakingCryptoBalance}
+                    symbol={account.symbol}
+                    numberOfLines={1}
+                    adjustsFontSizeToFit
+                />
+            }
+        />
+    );
+};
