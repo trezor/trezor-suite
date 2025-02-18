953918465cc15f1f31b85fb0ebad8b0ab3a1128d
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-02-18 14:43:53 +0100

    fix(suite-desktop-core): remove obsolete option from notarize call

--- packages/suite-desktop-core/scripts/notarize.ts
+++ packages/suite-desktop-core/scripts/notarize.ts
@@ -19,13 +19,11 @@ const notarizeAfterSignHook: Hooks['afterSign'] = context => {
 
     return notarize({
         tool: 'notarytool',
-        appBundleId: 'io.trezor.TrezorSuite',
         appPath,
         appleId: process.env.APPLEID,
         appleIdPassword: process.env.APPLEIDPASS,
         teamId: process.env.APPLETEAMID,
-        // TODO fix: notarize tool is known to be working, but TS fails: no overload matches this call
-    } as any);
+    });
 };
 
 export default notarizeAfterSignHook;
