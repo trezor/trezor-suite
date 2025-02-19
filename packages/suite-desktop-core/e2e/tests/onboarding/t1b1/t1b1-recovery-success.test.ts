aa7fa7b0bf76b15272b0f552cb749e34879bd4a1
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2025-02-19 16:12:21 +0100

    fix(e2e): Enable t1b1 e2e tests

--- packages/suite-desktop-core/e2e/tests/onboarding/t1b1/t1b1-recovery-success.test.ts
+++ packages/suite-desktop-core/e2e/tests/onboarding/t1b1/t1b1-recovery-success.test.ts
@@ -3,48 +3,44 @@ import { expect, test } from '../../../support/fixtures';
 const mnemonic =
     'nasty answer gentle inform unaware abandon regret supreme dragon gravity behind lava dose pilot garden into dynamic outer hard speed luxury run truly armed';
 
-test.describe.skip(
-    'Onboarding - recover wallet T1B1',
-    { tag: ['@group=device-management'] },
-    () => {
-        test.use({
-            emulatorStartConf: { model: 'T1B1', version: '1-latest', wipe: true },
-            setupEmulator: false,
-        });
+test.describe('Onboarding - recover wallet T1B1', { tag: ['@group=device-management'] }, () => {
+    test.use({
+        emulatorStartConf: { model: 'T1B1', version: '1-latest', wipe: true },
+        setupEmulator: false,
+    });
 
-        test.beforeEach(async ({ onboardingPage }) => {
-            await onboardingPage.disableFirmwareHashCheck();
-        });
+    test.beforeEach(async ({ onboardingPage }) => {
+        await onboardingPage.disableFirmwareHashCheck();
+    });
 
-        test('Successfully recovers wallet from mnemonic', async ({
-            page,
-            onboardingPage,
-            analyticsPage,
-            devicePrompt,
-            recoveryPage,
-            trezorInput,
-            trezorUserEnvLink,
-        }) => {
-            await analyticsPage.passThroughAnalytics();
+    test('Successfully recovers wallet from mnemonic', async ({
+        page,
+        onboardingPage,
+        analyticsPage,
+        devicePrompt,
+        recoveryPage,
+        trezorInput,
+        trezorUserEnvLink,
+    }) => {
+        await analyticsPage.passThroughAnalytics();
 
-            // Start wallet recovery process
-            await onboardingPage.firmware.continueButton.click();
-            await onboardingPage.recoverWalletButton.click();
-            await recoveryPage.selectWordCount(24);
-            await recoveryPage.selectBasicRecoveryButton.click();
-            await devicePrompt.confirmOnDevicePromptIsShown();
-            await page.waitForTimeout(1000);
-            await trezorUserEnvLink.pressYes();
+        // Start wallet recovery process
+        await onboardingPage.firmware.continueButton.click();
+        await onboardingPage.recoverWalletButton.click();
+        await recoveryPage.selectWordCount(24);
+        await recoveryPage.selectBasicRecoveryButton.click();
+        await devicePrompt.confirmOnDevicePromptIsShown();
+        await page.waitForTimeout(1000);
+        await trezorUserEnvLink.pressYes();
 
-            // Input mnemonic
-            await trezorInput.inputMnemonicT1B1(mnemonic);
+        // Input mnemonic
+        await trezorInput.inputMnemonicT1B1(mnemonic);
 
-            // Finalize recovery, skip pin, and verify success
-            await onboardingPage.continueRecoveryButton.click();
-            await onboardingPage.pin.skip();
-            await onboardingPage.continueCoinsButton.click();
-            await expect(onboardingPage.finalTitle).toBeVisible();
-            await expect(onboardingPage.finalTitle).toContainText('Setup complete!');
-        });
-    },
-);
+        // Finalize recovery, skip pin, and verify success
+        await onboardingPage.continueRecoveryButton.click();
+        await onboardingPage.pin.skip();
+        await onboardingPage.continueCoinsButton.click();
+        await expect(onboardingPage.finalTitle).toBeVisible();
+        await expect(onboardingPage.finalTitle).toContainText('Setup complete!');
+    });
+});
