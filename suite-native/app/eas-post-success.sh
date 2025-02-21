f8c7bc5c6921952acde5e1f2c69dd6513e2c647c
Matej Kriz <matej@matejkriz.cz>
2025-02-21 16:13:31 +0100

    feat(suite-native): distribute preview and adhoc build same as develop

--- suite-native/app/eas-post-success.sh
+++ suite-native/app/eas-post-success.sh
@@ -2,22 +2,32 @@
 
 FIREBASE_VERSION="${FIREBASE_VERSION:-v13.16.0}"
 
-distribute_develop_apk() {
+distribute_apk() {
     # Install Firebase CLI
     curl -sL https://firebase.tools | sed "s/latest/$FIREBASE_VERSION/" | bash
 
     release_notes="Last commit hash: $EAS_BUILD_GIT_COMMIT_HASH"
     echo "$EAS_BUILD_GIT_COMMIT_HASH"
+
+    # Set app ID based on profile
+    if [[ "$EAS_BUILD_PROFILE" == "develop" ]]; then
+        FIREBASE_APP_ID="1:191883890128:android:625bcdab76b3b3a644bdd5"
+    else
+        FIREBASE_APP_ID="1:191883890128:android:a7fd34594f97aa6444bdd5"
+    fi
+
     # Distribute APK to develop-testers group using Firebase CLI
     firebase appdistribution:distribute "$EAS_BUILD_WORKINGDIR"/suite-native/app/android/app/build/outputs/apk/release/app-release.apk \
         --project pc-api-4710771878548015996-769 \
-        --app 1:191883890128:android:625bcdab76b3b3a644bdd5 \
+        --app "$FIREBASE_APP_ID" \
         --groups "develop-testers" \
         --release-notes "$release_notes"
 }
 
-if [[ "$EAS_BUILD_PLATFORM" == "android" && "$EAS_BUILD_PROFILE" == "develop" ]]; then
-    distribute_develop_apk
-elif [[ "$EAS_BUILD_PLATFORM" == "ios" ]]; then
+if [[ "$EAS_BUILD_PLATFORM" == "ios" ]]; then
     exit 0
+fi
+
+if [[ "$EAS_BUILD_PROFILE" =~ ^(develop|preview|adhoc)$ ]]; then
+    distribute_apk
 fi
