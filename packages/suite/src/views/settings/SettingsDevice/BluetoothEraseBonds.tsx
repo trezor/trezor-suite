eb34bdba41922e2b1389f66e435a85696ddb7c1e
Peter Sanderson <dierk.deidel@protonmail.com>
2025-02-21 13:42:29 +0100

    fixup! feat: implement suite-common bluetooth code into Suite

--- packages/suite/src/views/settings/SettingsDevice/BluetoothEraseBonds.tsx
+++ packages/suite/src/views/settings/SettingsDevice/BluetoothEraseBonds.tsx
@@ -13,7 +13,7 @@ export const BluetoothEraseBonds = () => {
 
     const [inProgress, setInProgress] = useState(false);
 
-    const onCheckFirmwareAuthenticity = async () => {
+    const onEraseClick = async () => {
         setInProgress(true);
         // TODO: missing button request in FW
         const result = await TrezorConnect.eraseBonds({ device });
@@ -33,11 +33,7 @@ export const BluetoothEraseBonds = () => {
                 description="Forget pairing credentials. Trezor will no longer be paired with this computer (require bluetooth module restart? or device disconnect? forget your device in system UI?)"
             />
             <ActionColumn>
-                <Button
-                    onClick={onCheckFirmwareAuthenticity}
-                    isLoading={inProgress}
-                    isDisabled={inProgress}
-                >
+                <Button onClick={onEraseClick} isLoading={inProgress} isDisabled={inProgress}>
                     Erase
                 </Button>
             </ActionColumn>
