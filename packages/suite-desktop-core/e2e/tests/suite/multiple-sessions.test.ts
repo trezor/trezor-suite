389b783dc7a6bcdf9aad2878d9213e56acb6c51a
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2025-02-28 19:37:51 +0100

    fix(e2e): fix Multiple session test - right page object usage

--- packages/suite-desktop-core/e2e/tests/suite/multiple-sessions.test.ts
+++ packages/suite-desktop-core/e2e/tests/suite/multiple-sessions.test.ts
@@ -2,7 +2,9 @@ import * as messages from '@trezor/protobuf/src/messages';
 import { BridgeTransport } from '@trezor/transport';
 
 import { expect, test } from '../../support/fixtures';
+import { AnalyticsActions } from '../../support/pageActions/analyticsActions';
 import { DashboardActions } from '../../support/pageActions/dashboardActions';
+import { DevicePromptActions } from '../../support/pageActions/devicePromptActions';
 import { OnboardingActions } from '../../support/pageActions/onboarding/onboardingActions';
 
 const stealBridgeSession = async () => {
@@ -85,10 +87,7 @@ test.describe('Multiple sessions', { tag: ['@group=suite'] }, () => {
     test(
         'Overtake session by opening suite new tab',
         { tag: ['@webOnly'] },
-        async (
-            { context, onboardingPage, dashboardPage, devicePrompt, analyticsPage },
-            testInfo,
-        ) => {
+        async ({ context, onboardingPage, dashboardPage }, testInfo) => {
             await onboardingPage.completeOnboarding();
             await dashboardPage.discoveryShouldFinish();
 
@@ -97,15 +96,17 @@ test.describe('Multiple sessions', { tag: ['@group=suite'] }, () => {
                 window.Playwright = true;
             });
             await pageTwo.goto('./');
+            const analyticsPageTwo = new AnalyticsActions(pageTwo);
+            const devicePromptTwo = new DevicePromptActions(pageTwo);
             const onboardingPageTwo = new OnboardingActions(
                 pageTwo,
                 onboardingPage.model,
                 testInfo,
-                devicePrompt,
-                analyticsPage,
+                devicePromptTwo,
+                analyticsPageTwo,
             );
             await onboardingPageTwo.completeOnboarding();
-            const dashboardPageTwo = new DashboardActions(pageTwo, devicePrompt);
+            const dashboardPageTwo = new DashboardActions(pageTwo, devicePromptTwo);
             await dashboardPageTwo.discoveryShouldFinish();
             await expect(dashboardPageTwo.deviceStatus).toHaveText('Connected');
             await expect(dashboardPage.deviceStatus).toHaveText('Refresh');
