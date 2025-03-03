2bd1f24c6577ee633cc49f8fd87d22b967c158d3
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-03-03 14:55:38 +0100

    feat(scripts): add a script to generate UUID

---
+++ suite-common/message-system/scripts/get-uuid.ts
@@ -0,0 +1,6 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { v4 } from 'uuid';
+
+const uuidv4 = v4();
+// eslint-disable-next-line no-console
+console.log(`Generated a new UUID (v4):\n${uuidv4}`);
