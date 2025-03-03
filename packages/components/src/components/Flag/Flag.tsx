dcf7bbb8f92ae099391e71ccce1b00b588b7c54b
Vojtěch Tranta <vojta.tranta@gmail.com>
2025-03-03 14:02:52 +0100

    fix(suite): make the dynamic import of the flag svg actaully dynamic

--- packages/components/src/components/Flag/Flag.tsx
+++ packages/components/src/components/Flag/Flag.tsx
@@ -1,6 +1,9 @@
+import { useEffect, useState } from 'react';
+
 import styled from 'styled-components';
 
 import { FLAGS } from './flags';
+import { SkeletonRectangle } from '../skeletons/SkeletonRectangle';
 
 export type FlagType = keyof typeof FLAGS;
 
@@ -15,13 +18,26 @@ export interface FlagProps {
     size?: number;
 }
 
-export const Flag = ({ size = 24, country, className }: FlagProps) => (
-    <Wrapper>
-        <img
-            src={require(`../../images/flags/${country.toLowerCase()}.svg`)}
-            width={`${size}px`}
-            alt={`flag-${country}`}
-            className={className}
-        />
-    </Wrapper>
-);
+export const Flag = ({ size = 24, country, className }: FlagProps) => {
+    const [src, setSrc] = useState('');
+    useEffect(() => {
+        import(`../../images/flags/${country.toLowerCase()}.svg`)
+            .then(module => {
+                setSrc(module.default);
+            })
+            .catch(err => {
+                // NOTE: keep error here as this is not a critical issue
+                console.error('Flag image loading error: ', err);
+            });
+    }, [country]);
+
+    return (
+        <Wrapper>
+            {src ? (
+                <img src={src} width={`${size}px`} alt={`flag-${country}`} className={className} />
+            ) : (
+                <SkeletonRectangle width={size} height={size} />
+            )}
+        </Wrapper>
+    );
+};
