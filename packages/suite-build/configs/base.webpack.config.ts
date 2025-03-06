a2af562e838a7899696c94d71cd3746bd8e307b9
Vojtěch Tranta <vojta.tranta@gmail.com>
2025-03-06 12:33:39 +0100

    prioritize main file load

--- packages/suite-build/configs/base.webpack.config.ts
+++ packages/suite-build/configs/base.webpack.config.ts
@@ -63,10 +63,20 @@ const config: webpack.Configuration = {
     optimization: {
         splitChunks: {
             cacheGroups: {
+                react: {
+                    chunks: 'initial',
+                    name: 'react',
+                    test: /[\\/]node_modules[\\/]react/,
+                },
                 vendors: {
                     chunks: 'initial',
                     name: 'vendors',
                     test: /[\\/]node_modules[\\/](?!react)/,
+                },
+                components: {
+                    chunks: 'initial',
+                    name: 'components',
+                    test: /[\\/]packages[\\/]components[\\/]/,
                 },
             },
         },
