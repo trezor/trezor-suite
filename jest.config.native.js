7d7fb22c3f8ec66a2498b0201302ff2b35f8a950
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-03-07 14:03:06 +0100

    chore(suite-native): Polyfill setImmediate and clearImmediate for suite-native jest tests

--- jest.config.native.js
+++ jest.config.native.js
@@ -29,6 +29,7 @@ module.exports = {
     setupFiles: [
         '<rootDir>/../../node_modules/@shopify/react-native-skia/jestSetup.js',
         '<rootDir>/../../node_modules/react-native-gesture-handler/jestSetup.js',
+        '<rootDir>/../../suite-native/test-utils/src/everstakeJestSetup.js',
         '<rootDir>/../../suite-native/test-utils/src/expoMock.js',
         '<rootDir>/../../suite-native/firmware/src/jestSetup.js',
         '<rootDir>/../../suite-native/connection-status/src/jestSetup.js',
