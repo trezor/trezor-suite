ccfae7fbb78e9236233cb20a1c04fdc61b3bd506
Ondrej Hajek <ondrej.hajek@satoshilabs.com>
2025-03-07 14:47:13 +0100

    fix(ci): final formatting

--- packages/suite-desktop-core/e2e/tests/analytics/events.test.ts
+++ packages/suite-desktop-core/e2e/tests/analytics/events.test.ts
@@ -83,7 +83,6 @@ test.describe('Analytics Events', { tag: ['@group=suite', '@webOnly'] }, () => {
         onboardingPage,
         trezorUserEnvLink,
     }) => {
-        expect(false).toBe(true);
         await trezorUserEnvLink.startEmu({ wipe: true, model: 'T3T1' });
         await trezorUserEnvLink.setupEmu({
             passphrase_protection: true,
