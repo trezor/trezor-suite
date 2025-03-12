41e39a21e62b535b355089b12156ebb9ed61a1d1
Jan Václavík <jvaclavik@gmail.com>
2025-03-12 19:59:46 +0100

    fix(suite): Fix shrinking animation issue

--- packages/suite/src/views/dashboard/banner-animations.ts
+++ packages/suite/src/views/dashboard/banner-animations.ts
@@ -2,7 +2,7 @@ import { motionEasing } from '@trezor/components';
 
 export const bannerAnimationConfig = {
     initial: { opacity: 1, transform: 'scale(1)' },
-    exit: { opacity: 0, transform: 'scale(0.7)', marginBottom: -184 },
+    exit: { opacity: 0, transform: 'scale(0.7)', height: 0 },
     transition: {
         duration: 0.33,
         ease: motionEasing.transition,
