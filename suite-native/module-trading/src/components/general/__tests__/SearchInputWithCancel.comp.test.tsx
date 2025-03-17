1abeaa42e95d5e6dee161eb241f16287c88a5749
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-03-17 12:17:52 +0100

    chore(suite-native): add SearchInputWithCancel component tests

---
+++ suite-native/module-trading/src/components/general/__tests__/SearchInputWithCancel.comp.test.tsx
@@ -0,0 +1,35 @@
+import { fireEvent, render } from '@suite-native/test-utils';
+
+import { SearchInputWithCancel, SearchInputWithCancelProps } from '../SearchInputWithCancel';
+
+describe('SearchInputWithCancel', () => {
+    const renderSearchInputWithCancel = (props: Partial<SearchInputWithCancelProps>) =>
+        render(<SearchInputWithCancel onChange={jest.fn()} {...props} />);
+
+    it('should render without "Cancel" button by default', () => {
+        const { queryByText } = renderSearchInputWithCancel({});
+
+        expect(queryByText('Cancel')).toBeNull();
+    });
+
+    it('should call onChange callback when text is typed', () => {
+        const changeMock = jest.fn();
+        const { getByPlaceholderText } = renderSearchInputWithCancel({ onChange: changeMock });
+
+        fireEvent.changeText(getByPlaceholderText('Search'), 'test');
+
+        expect(changeMock).toHaveBeenCalledTimes(1);
+    });
+
+    it('should call onFocus and display "Cancel" button on input focus', () => {
+        const focusMock = jest.fn();
+        const { getByText, getByPlaceholderText } = renderSearchInputWithCancel({
+            onFocus: focusMock,
+        });
+
+        fireEvent(getByPlaceholderText('Search'), 'focus');
+
+        expect(getByText('Cancel')).toBeDefined();
+        expect(focusMock).toHaveBeenCalledTimes(1);
+    });
+});
