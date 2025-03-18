b99bb4cb5477955edf3ce176043fb73aafb697ad
Vojtěch Tranta <vojta.tranta@gmail.com>
2025-03-18 14:33:57 +0100

    chore(suite): react-intl@7.1.6

--- suite-native/intl/src/hooks.ts
+++ suite-native/intl/src/hooks.ts
@@ -1,7 +1,9 @@
 import { useCallback } from 'react';
-import { useIntl } from 'react-intl';
+import { PrimitiveType, useIntl } from 'react-intl';
 
 import { TxKeyPath } from './types';
+
+type FormatXMLElementFn<T, R = string | T | (string | T)[]> = (parts: Array<string | T>) => R;
 
 export const useTranslate = () => {
     const { formatMessage } = useIntl();
@@ -9,9 +11,9 @@ export const useTranslate = () => {
     const translate = useCallback(
         (
             id: TxKeyPath,
-            values?: Parameters<typeof formatMessage>[1],
+            values?: Record<string, PrimitiveType | FormatXMLElementFn<string, string>>,
             options?: Parameters<typeof formatMessage>[2],
-        ) => formatMessage({ id }, values, options) as string,
+        ) => formatMessage({ id }, values, options),
         [formatMessage],
     );
 
