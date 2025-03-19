34976fc4d81eb51886abbbc82561284b54623310
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-03-19 14:29:40 +0100

    fix(suite-desktop-core): Adapt ShellSpawnPlugin to Windows

--- packages/suite-build/plugins/shell-spawn-plugin.ts
+++ packages/suite-build/plugins/shell-spawn-plugin.ts
@@ -1,6 +1,8 @@
 import { spawn, spawnSync } from 'child_process';
 import webpack from 'webpack';
 
+import { isWindows } from '@trezor/env-utils';
+
 interface Command {
     command: string;
     args: string[];
@@ -31,10 +33,20 @@ class ShellSpawnPlugin {
         compiler.hooks.afterEmit.tap('ShellSpawnPlugin', (_: webpack.Compilation) => {
             const execute = ({ command, args, isSync }: Command) => {
                 if (isSync) {
-                    return spawnSync(command, args, { stdio: 'inherit', cwd: this.options.cwd });
+                    return spawnSync(command, args, {
+                        stdio: 'inherit',
+                        cwd: this.options.cwd,
+                        // on Unix systems, the compile process doesn't need direct shell access, as it recognizes `yarn` executable in PATH and can call it directly
+                        // but it's a common issue on Windows that nodeJS can't access `yarn` and needs to use shell at CWD to run commands through it
+                        shell: isWindows(),
+                    });
                 }
 
-                return spawn(command, args, { stdio: 'inherit', cwd: this.options.cwd });
+                return spawn(command, args, {
+                    stdio: 'inherit',
+                    cwd: this.options.cwd,
+                    shell: isWindows(), // see spawnSync above
+                });
             };
 
             if (!this.initialRun) {
