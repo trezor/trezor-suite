81eb3461b5b0a00f4faaaed983315832b086826e
Martin Varmuza <mvarmuza@gmail.com>
2025-03-21 08:42:20 +0100

    chore(suite-web): TrezorConnect.disableWebusb removed from useOpenSuiteDesktop hook

--- packages/suite/src/hooks/suite/useOpenSuiteDesktop.ts
+++ packages/suite/src/hooks/suite/useOpenSuiteDesktop.ts
@@ -1,19 +1,9 @@
-import TrezorConnect from '@trezor/connect';
-import type TrezorConnectWeb from '@trezor/connect-web';
 import { useWindowFocus } from '@trezor/react-utils';
 import { SUITE_BRIDGE_DEEPLINK, SUITE_URL } from '@trezor/urls';
 
-import { useSelector } from 'src/hooks/suite';
-import { selectHasTransportOfType } from 'src/reducers/suite/suiteReducer';
-
 export const useOpenSuiteDesktop = () => {
-    const isWebUsbTransport = useSelector(selectHasTransportOfType('WebUsbTransport'));
     const windowFocused = useWindowFocus();
     const handleOpenSuite = () => {
-        if (isWebUsbTransport) {
-            (TrezorConnect as typeof TrezorConnectWeb).disableWebUSB();
-        }
-
         location.href = SUITE_BRIDGE_DEEPLINK;
 
         // fallback in case deeplink does not work
