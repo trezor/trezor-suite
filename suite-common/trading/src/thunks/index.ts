774035467fa3f0cf6cf22240cfd80c4075ef83d7
Martin Homola <adder10@gmail.com>
2025-03-24 16:14:20 +0100

    refactor(trading): exchange handleRequestThunk

--- suite-common/trading/src/thunks/index.ts
+++ suite-common/trading/src/thunks/index.ts
@@ -1,10 +1,11 @@
 import { confirmTradeThunk } from './buy/confirmTradeThunk';
-import { handleRequestThunk } from './buy/handleRequestThunk';
+import { handleRequestThunk as handleBuyRequestThunk } from './buy/handleRequestThunk';
 import { loadInfoThunk } from './buy/loadInfoThunk';
 import { selectQuoteThunk } from './buy/selectQuoteThunk';
 import { loadInitialDataThunk } from './common/loadInitialDataThunk';
 import { verifyAddressThunk } from './common/verifyAddressThunk';
 import { watchTradeThunk } from './common/watchTradeThunk';
+import { handleRequestThunk as handleExchangeRequestThunk } from './exchange/handleRequestThunk';
 
 export { type HandleRequestThunkProps } from './buy/handleRequestThunk';
 
@@ -16,7 +17,11 @@ export const tradingThunks = {
 
 export const buyThunks = {
     loadInfoThunk,
-    handleRequestThunk,
+    handleRequestThunk: handleBuyRequestThunk,
     selectQuoteThunk,
     confirmTradeThunk,
+};
+
+export const exchangeThunks = {
+    handleRequestThunk: handleExchangeRequestThunk,
 };
