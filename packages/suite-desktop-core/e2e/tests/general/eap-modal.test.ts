aa791bf05d7015cbde7f9a384352ff238794d3e2
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2025-03-25 15:52:13 +0100

    fix(e2e): fix groupping tag of eap test

--- packages/suite-desktop-core/e2e/tests/general/eap-modal.test.ts
+++ packages/suite-desktop-core/e2e/tests/general/eap-modal.test.ts
@@ -2,8 +2,12 @@ import { expect, test } from '../../support/fixtures';
 
 test.use({ startEmulator: false });
 
-test('Join early access', { tag: '@settings' }, async ({ settingsPage }) => {
-    await settingsPage.navigateTo('application');
-    await settingsPage.joinEarlyAccessProgram();
-    await expect(settingsPage.earlyAccessJoinButton).toHaveText('Opt out');
-});
+test(
+    'Join early access',
+    { tag: ['@group=settings', '@desktopOnly'] },
+    async ({ settingsPage }) => {
+        await settingsPage.navigateTo('application');
+        await settingsPage.joinEarlyAccessProgram();
+        await expect(settingsPage.earlyAccessJoinButton).toHaveText('Opt out');
+    },
+);
