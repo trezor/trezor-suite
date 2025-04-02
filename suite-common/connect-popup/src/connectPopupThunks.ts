4da71fc8e87c4cf516f2edb411d4a4a0a0d98f53
Martin Varmuza <mvarmuza@gmail.com>
2025-04-02 15:33:35 +0200

    fix(suite-common): remove early return in handling of non permitted methods

--- suite-common/connect-popup/src/connectPopupThunks.ts
+++ suite-common/connect-popup/src/connectPopupThunks.ts
@@ -61,7 +61,7 @@ export const connectPopupCallThunkInner = createThunk<
                     }),
                 );
 
-                return;
+                throw TypedError('Method_NotAllowed');
             }
 
             // Check if permission remembered
