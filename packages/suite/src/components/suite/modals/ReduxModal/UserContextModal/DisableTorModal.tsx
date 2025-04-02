08117d2126bfe6e68360abef4cbc754c000da265
Seibei Iguchi <2836193+seibei-iguchi@users.noreply.github.com>
2025-04-02 11:23:29 +0200

    refactor(suite): update DisableTorModal

--- packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/DisableTorModal.tsx
+++ packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/DisableTorModal.tsx
@@ -1,85 +1,17 @@
 import { useState } from 'react';
 
-import styled from 'styled-components';
-
 import { UserContextPayload } from '@suite-common/suite-types';
 import { type NetworkSymbol, getNetwork } from '@suite-common/wallet-config';
 import { blockchainActions } from '@suite-common/wallet-core';
-import { Button, H3, Paragraph, variables } from '@trezor/components';
+import { Banner, Button, Card, Column, H3, NewModal, Paragraph, Row } from '@trezor/components';
 import { CoinLogo } from '@trezor/product-components';
+import { spacings } from '@trezor/theme';
 
-import { Modal, Translation } from 'src/components/suite';
+import { Translation } from 'src/components/suite';
 import { AdvancedCoinSettingsModal } from 'src/components/suite/modals';
 import { useCustomBackends } from 'src/hooks/settings/backends';
 import { useDispatch } from 'src/hooks/suite';
 import { isOnionUrl } from 'src/utils/suite/tor';
-
-const BackendRowWrapper = styled.div`
-    display: flex;
-    width: 100%;
-    align-items: center;
-    padding: 12px 0;
-
-    & + & {
-        border-top: 1px solid ${({ theme }) => theme.legacy.STROKE_GREY};
-    }
-`;
-
-const CoinDescription = styled.div`
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-    align-items: start;
-    margin: 0 16px;
-    overflow: hidden;
-`;
-
-const CoinTitle = styled.span`
-    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
-    font-size: ${variables.FONT_SIZE.NORMAL};
-`;
-
-const CoinUrls = styled.span`
-    font-weight: ${variables.FONT_WEIGHT.MEDIUM};
-    font-size: ${variables.FONT_SIZE.SMALL};
-    color: ${({ theme }) => theme.legacy.TYPE_LIGHT_GREY};
-    white-space: nowrap;
-    overflow: hidden;
-    width: 100%;
-    text-overflow: ellipsis;
-    text-align: start;
-`;
-
-interface BackendRowProps {
-    symbol: NetworkSymbol;
-    urls: string[];
-    onSettings: () => void;
-}
-
-const BackendRow = ({ symbol, urls, onSettings }: BackendRowProps) => (
-    <BackendRowWrapper>
-        <CoinLogo symbol={symbol} />
-        <CoinDescription>
-            <CoinTitle>{getNetwork(symbol).name}</CoinTitle>
-            <CoinUrls>{urls.join(', ')}</CoinUrls>
-        </CoinDescription>
-        <Button variant="tertiary" onClick={onSettings} icon="settings">
-            <Translation id="TR_GO_TO_SETTINGS" />
-        </Button>
-    </BackendRowWrapper>
-);
-
-// eslint-disable-next-line local-rules/no-override-ds-component
-const Title = styled(H3)`
-    text-align: left;
-    margin-bottom: 12px;
-`;
-
-// eslint-disable-next-line local-rules/no-override-ds-component
-const Description = styled(Paragraph)`
-    text-align: left;
-    margin-bottom: 16px;
-`;
 
 type DisableTorModalProps = Omit<Extract<UserContextPayload, { type: 'disable-tor' }>, 'type'> & {
     onCancel: () => void;
@@ -104,23 +36,20 @@ export const DisableTorModal = ({ onCancel, decision }: DisableTorModalProps) =>
         onCancel();
     };
 
-    return (
-        <>
-            <Modal
-                isCancelable
-                onCancel={onCancel}
-                heading={
-                    onionBackends.length ? (
-                        <Translation id="TR_TOR_DISABLE_ONIONS_ONLY" />
-                    ) : (
-                        <Translation id="TR_TOR_DISABLE_ONIONS_ONLY_RESOLVED" />
-                    )
-                }
-                bottomBarComponents={
-                    <Button
-                        variant={onionBackends.length ? 'tertiary' : 'primary'}
-                        onClick={onDisableTor}
-                    >
+    return symbol ? (
+        <AdvancedCoinSettingsModal symbol={symbol} onCancel={() => setSymbol(undefined)} />
+    ) : (
+        <NewModal
+            onCancel={onCancel}
+            variant={onionBackends.length ? 'warning' : 'primary'}
+            size="small"
+            iconName={onionBackends.length ? undefined : 'tor'}
+            heading={
+                onionBackends.length ? <Translation id="TR_TOR_DISABLE_ONIONS_ONLY" /> : undefined
+            }
+            bottomContent={
+                <>
+                    <NewModal.Button onClick={onDisableTor}>
                         <Translation
                             id={
                                 onionBackends.length
@@ -128,40 +57,58 @@ export const DisableTorModal = ({ onCancel, decision }: DisableTorModalProps) =>
                                     : 'TR_TOR_DISABLE'
                             }
                         />
-                    </Button>
-                }
-            >
-                {onionBackends.length ? (
-                    <>
-                        <Title>
-                            <Translation id="TR_TOR_DISABLE_ONIONS_ONLY_TITLE" />
-                        </Title>
-                        <Description>
-                            <Translation id="TR_TOR_DISABLE_ONIONS_ONLY_DESCRIPTION" />
-                        </Description>
-                        {onionBackends.map(({ symbol, urls }) => (
-                            <BackendRow
-                                key={symbol}
-                                symbol={symbol}
-                                urls={urls}
-                                onSettings={() => setSymbol(symbol)}
-                            />
-                        ))}
-                    </>
-                ) : (
-                    <>
-                        <Title>
-                            <Translation id="TR_TOR_DISABLE_ONIONS_ONLY_NO_MORE_TITLE" />
-                        </Title>
-                        <Description>
-                            <Translation id="TR_TOR_DISABLE_ONIONS_ONLY_NO_MORE_DESCRIPTION" />
-                        </Description>
-                    </>
-                )}
-            </Modal>
-            {symbol && (
-                <AdvancedCoinSettingsModal symbol={symbol} onCancel={() => setSymbol(undefined)} />
+                    </NewModal.Button>
+                    <NewModal.Button onClick={onCancel} variant="tertiary">
+                        <Translation id="TR_CANCEL" />
+                    </NewModal.Button>
+                </>
+            }
+        >
+            {onionBackends.length ? (
+                <Column gap={spacings.md}>
+                    <Banner variant="warning" icon="tor">
+                        <Translation id="TR_TOR_DISABLE_ONIONS_ONLY_TITLE" />{' '}
+                        <Translation id="TR_TOR_DISABLE_ONIONS_ONLY_DESCRIPTION" />
+                    </Banner>
+                    <Card>
+                        <Column gap={spacings.xxl} hasDivider>
+                            {onionBackends.map(({ symbol, urls }) => (
+                                <Row key={symbol} gap={spacings.md}>
+                                    <CoinLogo symbol={symbol} />
+                                    <Column>
+                                        <Paragraph>{getNetwork(symbol).name}</Paragraph>
+                                        <Paragraph
+                                            variant="tertiary"
+                                            typographyStyle="hint"
+                                            ellipsisLineCount={1}
+                                        >
+                                            {urls.join(', ')}
+                                        </Paragraph>
+                                    </Column>
+                                    <Button
+                                        variant="tertiary"
+                                        onClick={() => setSymbol(symbol)}
+                                        icon="settings"
+                                        size="small"
+                                        margin={{ left: 'auto' }}
+                                    >
+                                        <Translation id="TR_GO_TO_SETTINGS" />
+                                    </Button>
+                                </Row>
+                            ))}
+                        </Column>
+                    </Card>
+                </Column>
+            ) : (
+                <Column gap={spacings.xxs}>
+                    <H3>
+                        <Translation id="TR_TOR_DISABLE_ONIONS_ONLY_NO_MORE_DESCRIPTION" />
+                    </H3>
+                    <Paragraph variant="tertiary">
+                        <Translation id="TR_TOR_DISABLE_ONIONS_ONLY_NO_MORE_TITLE" />
+                    </Paragraph>
+                </Column>
             )}
-        </>
+        </NewModal>
     );
 };
