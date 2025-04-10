b50810aa81836029bb9c5e8bf6f246d66b584ba3
Ondrej Hajek <ondrej.hajek@satoshilabs.com>
2025-04-10 15:51:22 +0200

    fix(e2e): Fix formatting

--- packages/suite-desktop-core/e2e/support/testExtends/currentsFixture.ts
+++ packages/suite-desktop-core/e2e/support/testExtends/currentsFixture.ts
@@ -1,14 +1,10 @@
-import { 
-  CurrentsFixtures,
-  CurrentsWorkerFixtures,
-  fixtures
-} from '@currents/playwright';
+import { CurrentsFixtures, CurrentsWorkerFixtures, fixtures } from '@currents/playwright';
 import { test as baseTest } from '@playwright/test';
 
 const currentsTest = baseTest.extend<CurrentsFixtures, CurrentsWorkerFixtures>({
-  ...fixtures.baseFixtures,
-  ...fixtures.coverageFixtures,
-  ...fixtures.actionFixtures,
+    ...fixtures.baseFixtures,
+    ...fixtures.coverageFixtures,
+    ...fixtures.actionFixtures,
 });
 
 export { currentsTest };
