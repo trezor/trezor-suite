c5bbdf923a9de595d8dc0e64c1ca8fdf8bf643cd
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2025-04-14 08:21:15 +0000

    fix(e2e): Fixing rebased new test

--- packages/suite-desktop-core/e2e/tests/trezor-connect/error.test.ts
+++ packages/suite-desktop-core/e2e/tests/trezor-connect/error.test.ts
@@ -4,9 +4,8 @@ import { expect, test } from '../../support/fixtures';
 
 test.describe('TrezorConnect', { tag: ['@group=suite', '@desktopOnly'] }, () => {
     test.use({ electronConf: { exposeConnectWs: true } });
-    test.beforeEach(async ({ onboardingPage, dashboardPage }) => {
+    test.beforeEach(async ({ onboardingPage }) => {
         await onboardingPage.completeOnboarding();
-        await dashboardPage.discoveryShouldFinish();
         await test.step('Initialize TrezorConnect', async () => {
             await TrezorConnect.init({
                 manifest: {
