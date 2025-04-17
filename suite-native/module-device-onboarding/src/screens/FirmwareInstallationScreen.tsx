ed68a7112b373d313076153cec7b502dfc1da3a0
Petr Knetl <petr.knetl@satoshilabs.com>
2025-04-17 08:00:47 +0200

    fix(suite-native): device onboarding firmware fail recovery

--- suite-native/module-device-onboarding/src/screens/FirmwareInstallationScreen.tsx
+++ suite-native/module-device-onboarding/src/screens/FirmwareInstallationScreen.tsx
@@ -1,3 +1,7 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+
+import { firmwareActions } from '@suite-common/firmware';
 import { FirmwareInstallationScreenContent } from '@suite-native/firmware';
 import {
     DeviceOnboardingStackParamList,
@@ -13,6 +17,14 @@ export const FirmwareInstallationScreen = ({
     DeviceOnboardingStackParamList,
     DeviceOnboardingStackRoutes.FirmwareInstallation
 >) => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        // On first render, set the firmware installation status to 'initial'. Some previous failed firmware updates might
+        // happened before and we want do avoid showing the "firmware update failed" UI when user returns to this screen.
+        dispatch(firmwareActions.setStatus('initial'));
+    }, [dispatch]);
+
     const handleFirmwareInstallationSuccess = () => {
         navigation.navigate(DeviceOnboardingStackRoutes.DeviceTutorial);
     };
