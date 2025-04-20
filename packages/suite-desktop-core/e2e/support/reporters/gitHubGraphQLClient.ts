489a01c0611c02fd4251aeccfef9bad08a6cd2b1
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2025-04-20 14:53:46 +0000

    fix(e2e): Clean up retry logic

--- packages/suite-desktop-core/e2e/support/reporters/gitHubGraphQLClient.ts
+++ packages/suite-desktop-core/e2e/support/reporters/gitHubGraphQLClient.ts
@@ -23,31 +23,26 @@ export class GitHubGraphQLClient {
     async createProject(ownerId: string, projectName: string): Promise<string> {
         this.logger.log(`Creating project with owner ID: ${ownerId}`);
 
-        try {
-            const mutation = `
-                mutation {
-                    createProjectV2(
-                    input: {
-                        ownerId: "${ownerId}"
-                        title: "${projectName}"
-                    }) {
-                        projectV2 {
-                            id
-                        }
+        const mutation = `
+            mutation {
+                createProjectV2(
+                input: {
+                    ownerId: "${ownerId}"
+                    title: "${projectName}"
+                }) {
+                    projectV2 {
+                        id
                     }
                 }
-            `;
+            }
+        `;
 
-            const response = await this.octokit.graphql<CreateProjectMutation>(mutation);
-            const projectId = response.createProjectV2.projectV2.id;
+        const response = await this.octokit.graphql<CreateProjectMutation>(mutation);
+        const projectId = response.createProjectV2.projectV2.id;
 
-            this.logger.log(`Created GitHub project with ID: ${projectId}`);
+        this.logger.log(`Created GitHub project with ID: ${projectId}`);
 
-            return projectId;
-        } catch (error) {
-            this.logger.logError(`Failed to create project for owner ${ownerId}:`);
-            throw error;
-        }
+        return projectId;
     }
 
     async createProjectField(
@@ -64,42 +59,37 @@ export class GitHubGraphQLClient {
             }...`,
         );
 
-        try {
-            if (isSelectField && options) {
-                optionsString = options
-                    .map(
-                        opt =>
-                            `{ name: "${opt.value}", description: "${opt.value}", color: ${opt.color} }`,
-                    )
-                    .join(',\n');
-            }
-
-            const mutation = `
-          mutation {
-            createProjectV2Field(
-            input: {
-              projectId: "${projectId}"
-              name: "${fieldName}"
-              dataType: ${fieldType}
-              ${isSelectField ? `singleSelectOptions: [${optionsString}]` : ''}
-            }) {
-                projectV2Field {
-                  ... on ProjectV2SingleSelectField {
-                    id
-                  }
-                }
-            } 
-          }`;
-
-            const response = await this.octokit.graphql<CreateFieldResponse>(mutation);
-            const fieldId = response.createProjectV2Field.projectV2Field.id;
-            this.logger.log(`Created ${fieldType} field "${fieldName}" with ID: ${fieldId}`);
-
-            return fieldId;
-        } catch (error) {
-            this.logger.logError(`Failed to create ${fieldType} field "${fieldName}".`);
-            throw error;
+        if (isSelectField && options) {
+            optionsString = options
+                .map(
+                    opt =>
+                        `{ name: "${opt.value}", description: "${opt.value}", color: ${opt.color} }`,
+                )
+                .join(',\n');
         }
+
+        const mutation = `
+      mutation {
+        createProjectV2Field(
+        input: {
+          projectId: "${projectId}"
+          name: "${fieldName}"
+          dataType: ${fieldType}
+          ${isSelectField ? `singleSelectOptions: [${optionsString}]` : ''}
+        }) {
+            projectV2Field {
+              ... on ProjectV2SingleSelectField {
+                id
+              }
+            }
+        } 
+      }`;
+
+        const response = await this.octokit.graphql<CreateFieldResponse>(mutation);
+        const fieldId = response.createProjectV2Field.projectV2Field.id;
+        this.logger.log(`Created ${fieldType} field "${fieldName}" with ID: ${fieldId}`);
+
+        return fieldId;
     }
 
     async getProjectFromOrganization(
@@ -108,120 +98,105 @@ export class GitHubGraphQLClient {
     ): Promise<Project[]> {
         this.logger.log(`Fetching projects for organization: ${organization}`);
 
-        try {
-            const query = `
-                query {
-                  organization(login: "${organization}") {
-                    projectsV2(first: 10, query: "${projectName}") {
-                      nodes {
-                        id
-                        title
-                      }
-                    }
+        const query = `
+            query {
+              organization(login: "${organization}") {
+                projectsV2(first: 10, query: "${projectName}") {
+                  nodes {
+                    id
+                    title
                   }
                 }
-            `;
+              }
+            }
+        `;
 
-            const response = await this.octokit.graphql<ProjectQueryResponse>(query);
-            this.logger.log('Successfully retrieved organization projects');
+        const response = await this.octokit.graphql<ProjectQueryResponse>(query);
+        this.logger.log('Successfully retrieved organization projects');
 
-            return response.organization.projectsV2.nodes;
-        } catch (error) {
-            this.logger.logError(`Failed to get projects for organization ${organization}:`);
-            throw error;
-        }
+        return response.organization.projectsV2.nodes;
     }
 
     async getProjectFromUser(ownerLogin: string, projectName: string): Promise<Project[]> {
         this.logger.log(`Fetching projects for user: ${ownerLogin}`);
 
-        try {
-            const query = `
-                query {
-                  user(login: "${ownerLogin}") {
-                    projectsV2(first: 10, query: "${projectName}") {
-                      nodes {
-                        id
-                        title
-                      }
-                    }
+        const query = `
+            query {
+              user(login: "${ownerLogin}") {
+                projectsV2(first: 10, query: "${projectName}") {
+                  nodes {
+                    id
+                    title
                   }
                 }
-            `;
+              }
+            }
+        `;
 
-            const response = await this.octokit.graphql<ProjectQueryResponseUser>(query);
-            this.logger.log('Successfully retrieved user projects');
+        const response = await this.octokit.graphql<ProjectQueryResponseUser>(query);
+        this.logger.log('Successfully retrieved user projects');
 
-            return response.user.projectsV2.nodes;
-        } catch (error) {
-            this.logger.logError(`Failed to get projects for user ${ownerLogin}:`);
-            throw error;
-        }
+        return response.user.projectsV2.nodes;
     }
 
     async addIssueToProject(projectId: string, issueNodeId: string): Promise<string> {
         this.logger.log(`Adding issue ${issueNodeId} to project ${projectId}...`);
 
-        try {
-            const mutation = `
-                mutation {
-                  addProjectV2ItemById(
-                  input: {
-                    projectId: "${projectId}"
-                    contentId: "${issueNodeId}"
-                  }) {
-                    item {
-                      id
-                    }
-                  }
+        const mutation = `
+            mutation {
+              addProjectV2ItemById(
+              input: {
+                projectId: "${projectId}"
+                contentId: "${issueNodeId}"
+              }) {
+                item {
+                  id
                 }
-            `;
+              }
+            }
+        `;
 
-            const response = await this.octokit.graphql<AddIssueToProjectResponse>(mutation);
+        const response = await this.octokit.graphql<AddIssueToProjectResponse>(mutation);
+        this.logger.log(
+            `Successfully added issue to project with item ID: ${response.addProjectV2ItemById.item.id}`,
+        );
 
-            return response.addProjectV2ItemById.item.id;
-        } catch (error) {
-            this.logger.logError('Failed to add issue to project:');
-            throw error;
-        }
+        return response.addProjectV2ItemById.item.id;
     }
 
     async getProjectFields(projectId: string): Promise<ProjectField[]> {
         this.logger.log(`Fetching fields for project ${projectId}...`);
 
-        try {
-            const response = await this.octokit.graphql<ProjectFieldsResponse>(`
-              query {
-                node(id: "${projectId}") {
-                  ... on ProjectV2 {
-                    fields(first: 20) {
-                      nodes {
-                        ... on ProjectV2FieldCommon {
-                          id
-                          name
-                          dataType
-                        }
-                        ... on ProjectV2SingleSelectField {
-                          id
-                          name
-                          dataType
-                          options {
-                            id
-                            name
-                          }
-                        }
+        const response = await this.octokit.graphql<ProjectFieldsResponse>(`
+          query {
+            node(id: "${projectId}") {
+              ... on ProjectV2 {
+                fields(first: 20) {
+                  nodes {
+                    ... on ProjectV2FieldCommon {
+                      id
+                      name
+                      dataType
+                    }
+                    ... on ProjectV2SingleSelectField {
+                      id
+                      name
+                      dataType
+                      options {
+                        id
+                        name
                       }
                     }
                   }
                 }
               }
-          `);
+            }
+          }
+      `);
 
-            return response.node.fields.nodes;
-        } catch (error) {
-            this.logger.logError('Failed to get project fields:');
-            throw error;
-        }
+        this.logger.log(`Successfully retrieved fields for project ${projectId}`);
+
+        return response.node.fields.nodes;
     }
 
     async setItemValue(
@@ -234,29 +209,24 @@ export class GitHubGraphQLClient {
             `Updating field ${fieldId} for item ${itemId} with value: ${JSON.stringify(valueOrOptionId)}`,
         );
 
-        try {
-            const mutation = `
-              mutation {
-                updateProjectV2ItemFieldValue(
-                input: {
-                  projectId: "${projectId}"
-                  itemId: "${itemId}"
-                  fieldId: "${fieldId}"
-                  value: ${valueOrOptionId}
-                }) {
-                  projectV2Item {
-                    id
-                  }
-                }
+        const mutation = `
+          mutation {
+            updateProjectV2ItemFieldValue(
+            input: {
+              projectId: "${projectId}"
+              itemId: "${itemId}"
+              fieldId: "${fieldId}"
+              value: ${valueOrOptionId}
+            }) {
+              projectV2Item {
+                id
               }
-          `;
+            }
+          }
+      `;
 
-            await this.octokit.graphql<UpdateProjectItemFieldResponse>(mutation);
-            this.logger.log(`Successfully updated field for item ${itemId}`);
-        } catch (error) {
-            this.logger.logError(`Failed to update field for item ${itemId}:`);
-            throw error;
-        }
+        await this.octokit.graphql<UpdateProjectItemFieldResponse>(mutation);
+        this.logger.log(`Successfully updated field for item ${itemId}`);
     }
 
     async updateFieldOptions(
@@ -264,37 +234,32 @@ export class GitHubGraphQLClient {
         options: { value: string; color: string }[],
     ): Promise<void> {
         this.logger.log(`Updating all options for field ${fieldId}`);
-        try {
-            const optionsString = options
-                .map(
-                    opt =>
-                        `{ name: "${opt.value}", description: "${opt.value}", color: ${opt.color} }`,
-                )
-                .join(',\n');
 
-            const mutation = `
-            mutation {
-              updateProjectV2Field(
-              input: {
-                fieldId: "${fieldId}"
-                singleSelectOptions: [
-                  ${optionsString}
-                ]
-              }) {
-                projectV2Field {
-                  ... on ProjectV2SingleSelectField {
-                    id
-                  }
-                }
-              } 
+        const optionsString = options
+            .map(
+                opt => `{ name: "${opt.value}", description: "${opt.value}", color: ${opt.color} }`,
+            )
+            .join(',\n');
+
+        const mutation = `
+        mutation {
+          updateProjectV2Field(
+          input: {
+            fieldId: "${fieldId}"
+            singleSelectOptions: [
+              ${optionsString}
+            ]
+          }) {
+            projectV2Field {
+              ... on ProjectV2SingleSelectField {
+                id
+              }
             }
-          `;
-
-            await this.octokit.graphql(mutation);
-            this.logger.log(`Successfully replaced all options for field ${fieldId}`);
-        } catch (error) {
-            this.logger.logError(`Failed to update field options.`);
-            throw error;
+          } 
         }
+      `;
+
+        await this.octokit.graphql(mutation);
+        this.logger.log(`Successfully replaced all options for field ${fieldId}`);
     }
 }
