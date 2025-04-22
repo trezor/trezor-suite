bc60955b0a20d6915820732f5a9afc6da27ee671
Martin Varmuza <mvarmuza@gmail.com>
2025-04-22 10:33:57 +0200

    fixup! chore(node-utils): rename findProcessOnIncomingPort.ts to process.ts

--- packages/node-utils/src/tests/process.test.ts
+++ packages/node-utils/src/tests/process.test.ts
@@ -3,39 +3,41 @@ import net from 'net';
 import { findFullPathByPid, findProcessFromIncomingPort } from '../process';
 import { getFreePort } from '../getFreePort';
 
-describe('findProcessFromIncomingPort', () => {
-    test('start a server on a random free port and try to detect it', async () => {
-        const port = await getFreePort();
+describe('process', () => {
+    describe('findProcessFromIncomingPort', () => {
+        test('start a server on a random free port and try to detect it', async () => {
+            const port = await getFreePort();
 
-        const server = net.createServer().listen(port);
-        try {
-            // wait for listening
-            await new Promise(resolve => {
-                server.on('listening', () => {
-                    resolve(undefined);
+            const server = net.createServer().listen(port);
+            try {
+                // wait for listening
+                await new Promise(resolve => {
+                    server.on('listening', () => {
+                        resolve(undefined);
+                    });
                 });
-            });
 
-            const processInfo = await findProcessFromIncomingPort(port);
-            expect(processInfo).toBeDefined();
+                const processInfo = await findProcessFromIncomingPort(port);
+                expect(processInfo).toBeDefined();
 
-            if (process.platform === 'win32') {
-                expect(processInfo?.name).toEqual('node.exe');
-            } else {
-                expect(processInfo?.name).toEqual('node');
+                if (process.platform === 'win32') {
+                    expect(processInfo?.name).toEqual('node.exe');
+                } else {
+                    expect(processInfo?.name).toEqual('node');
+                }
+
+                const executablePath = await findFullPathByPid(processInfo!.pid);
+                console.log('executablePath', executablePath);
+            } finally {
+                server.close();
             }
+        });
 
-            const executablePath = await findFullPathByPid(processInfo!.pid);
-            console.log('executablePath', executablePath);
-        } finally {
-            server.close();
-        }
-    });
-
-    test('if there is nothing running on the port, findProcessFromIncomingPort throws', async () => {
-        const port = await getFreePort();
-        await expect(findProcessFromIncomingPort(port)).rejects.toThrow(
-            'Command failed with code 1: ',
-        );
+        test('if there is nothing running on the port, findProcessFromIncomingPort throws', async () => {
+            const port = await getFreePort();
+            await expect(findProcessFromIncomingPort(port)).rejects.toThrow(
+                'Command failed with code 1: ',
+            );
+        });
     });
 });
