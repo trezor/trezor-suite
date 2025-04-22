6cdc9e11786dc9761bf582912ab630edbc06d386
Jan Komarek <jan.komarek@satoshilabs.com>
2025-04-22 17:21:17 +0200

    fix(components): omit as prop from Paragraph

--- packages/components/src/components/typography/Paragraph/Paragraph.tsx
+++ packages/components/src/components/typography/Paragraph/Paragraph.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
+import { Without } from '@trezor/type-utils';
+
 import { Text, TextProps } from '../Text/Text';
 
-export const Paragraph = ({ children, ...rest }: TextProps) => (
+export const Paragraph = ({ children, ...rest }: Without<TextProps, 'as'>) => (
     <Text {...rest} as="p">
         {children}
     </Text>
