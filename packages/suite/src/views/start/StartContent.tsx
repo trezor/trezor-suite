0546fb2d5455eba0dff0255ad99d547281bec2ee
Peter Sanderson <dierk.deidel@protonmail.com>
2025-04-23 15:51:20 +0200

    fix: modal on start screen

--- packages/suite/src/views/start/StartContent.tsx
+++ packages/suite/src/views/start/StartContent.tsx
@@ -12,6 +12,7 @@ import { PrerequisitesGuide, TrezorLink } from 'src/components/suite';
 import { useDispatch, useSelector } from 'src/hooks/suite';
 import { selectPrerequisite } from 'src/reducers/suite/suiteReducer';
 
+import { ModalSwitcher } from '../../components/suite/modals/ModalSwitcher/ModalSwitcher';
 import { SecurityCheck } from '../onboarding/steps/SecurityCheck/SecurityCheck';
 
 export const StartContent = () => {
@@ -66,8 +67,15 @@ export const StartContent = () => {
         prerequisite &&
         !['device-initialize', 'firmware-missing', 'device-recovery-mode'].includes(prerequisite)
     ) {
-        return <PrerequisitesGuide />;
+        return (
+            <>
+                <ModalSwitcher />
+                <PrerequisitesGuide />
+            </>
+        );
     }
 
+    // Security check has to be without <ModalSwitcher /> as it handles the
+    // button request without it. Its terrible, but it is what it is.
     return <SecurityCheck />;
 };
