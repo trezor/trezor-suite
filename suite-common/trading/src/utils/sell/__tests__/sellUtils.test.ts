76ad0e7c71810133de0f70e579fa85da4aa18d5b
Martin Homola <adder10@gmail.com>
2025-04-24 11:42:50 +0200

    refactor(trading): move verify bank account util

--- suite-common/trading/src/utils/sell/__tests__/sellUtils.test.ts
+++ suite-common/trading/src/utils/sell/__tests__/sellUtils.test.ts
@@ -1,3 +1,6 @@
+import { CryptoId, SellFiatTrade } from 'invity-api';
+
+import { TradingSellInfoSelector } from '../../../selectors/tradingSelectors';
 import { sellUtilsFixtures } from '../__fixtures__/sellUtils';
 import { sellUtils } from '../sellUtils';
 
@@ -88,6 +91,135 @@ describe('sellUtils', () => {
             ['SITE_ACTION_REQUEST' as const, 'TR_SELL_STATUS_PENDING'],
         ])('should return %s for %s', (status, expected) => {
             expect(sellUtils.getStatusMessage(status)).toBe(expected);
+        });
+    });
+
+    describe('needToRegisterOrVerifyBankAccount', () => {
+        const sellInfo: TradingSellInfoSelector = {
+            providerInfos: {
+                test: {
+                    name: 'test',
+                    companyName: 'Test',
+                    logo: 'test.jpg',
+                    isActive: true,
+                    statusUrl: 'https://test.io/sell/txs/{{orderId}}',
+                    supportUrl: 'https://support.test.io',
+                    tradedCoins: ['bitcoin' as CryptoId],
+                    tradedFiatCurrencies: ['CZK', 'USD'],
+                    type: 'Fiat',
+                    supportedCountries: ['CZ'],
+                    flow: 'BANK_ACCOUNT',
+                },
+            },
+            supportedFiatCurrencies: new Set(),
+            supportedCryptoCurrencies: new Set(),
+        };
+
+        const quote: SellFiatTrade = {
+            ...sellUtilsFixtures.MIN_MAX_QUOTES_OK[0],
+            exchange: 'test',
+            orderId: 'orderId',
+            cryptoStringAmount: '0.01',
+            destinationAddress: 'destinationAddress',
+            destinationPaymentExtraId: 'extraId',
+        };
+
+        it('should return false when is not match between exchange and quote', () => {
+            expect(
+                sellUtils.needToRegisterOrVerifyBankAccount({
+                    sellInfo,
+                    quote: {
+                        ...quote,
+                        exchange: 'test-different',
+                    },
+                }),
+            ).toBe(false);
+        });
+
+        it('should return false when quote exchange is undefined', () => {
+            expect(
+                sellUtils.needToRegisterOrVerifyBankAccount({
+                    sellInfo,
+                    quote: {
+                        ...quote,
+                        exchange: undefined,
+                    },
+                }),
+            ).toBe(false);
+        });
+
+        it('should return false when provider flow is not not BANK_ACCOUNT', () => {
+            expect(
+                sellUtils.needToRegisterOrVerifyBankAccount({
+                    sellInfo: {
+                        ...sellInfo,
+                        providerInfos: {
+                            test: { ...sellInfo.providerInfos.test, flow: 'PAYMENT_GATE' },
+                        },
+                    },
+                    quote,
+                }),
+            ).toBe(false);
+        });
+
+        it('should return false when quoteId is not defined', () => {
+            expect(
+                sellUtils.needToRegisterOrVerifyBankAccount({
+                    sellInfo,
+                    quote: {
+                        ...quote,
+                        quoteId: undefined,
+                    },
+                }),
+            ).toBe(false);
+        });
+
+        it('should return false when there are some verified bank accounts', () => {
+            expect(
+                sellUtils.needToRegisterOrVerifyBankAccount({
+                    sellInfo,
+                    quote: {
+                        ...quote,
+                        bankAccounts: [
+                            {
+                                bankAccount: 'bankAccount',
+                                holder: 'holder',
+                                verified: true,
+                            },
+                        ],
+                    },
+                }),
+            ).toBe(false);
+        });
+
+        it('should return true when there are not any bank accounts', () => {
+            expect(
+                sellUtils.needToRegisterOrVerifyBankAccount({
+                    sellInfo,
+                    quote: {
+                        ...quote,
+                        bankAccounts: undefined,
+                    },
+                }),
+            ).toBe(true);
+        });
+
+        it('should return true when account is verified', () => {
+            expect(
+                sellUtils.needToRegisterOrVerifyBankAccount({
+                    sellInfo,
+                    quote: {
+                        ...quote,
+                        bankAccounts: [
+                            {
+                                bankAccount: 'bankAccount',
+                                holder: 'holder',
+                                verified: false,
+                            },
+                        ],
+                    },
+                }),
+            ).toBe(true);
         });
     });
 });
