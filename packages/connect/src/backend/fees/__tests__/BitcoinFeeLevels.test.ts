3964ae8afc62a3a1a471cc87464a27db42f992ed
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-04-24 20:57:00 +0200

    test(connect): cleanup BitcoinFeeLevels test

--- packages/connect/src/backend/fees/__tests__/BitcoinFeeLevels.test.ts
+++ packages/connect/src/backend/fees/__tests__/BitcoinFeeLevels.test.ts
@@ -4,7 +4,7 @@ import coinsJSON from '@trezor/connect-common/files/coins.json';
 
 import { getBitcoinNetwork, parseCoinsJson } from '../../../data/coinInfo';
 import { FeeLevel } from '../../../types';
-import { initBlockchain } from '../../BlockchainLink';
+import { dispose, initBlockchain } from '../../BlockchainLink';
 import { BitcoinFeeLevels } from '../BitcoinFeeLevels';
 
 // simple linear mock for decreasing fees per requested block number
@@ -17,157 +17,113 @@ const defaultFeesMock: FeeLevel[] = [
     { label: 'economy', blocks: 80, feePerUnit: '22' },
 ];
 
+const estimateFeeMockOK: typeof BlockchainLink.prototype.estimateFee = params =>
+    Promise.resolve(
+        (params.blocks ?? []).map(block => ({
+            feePerUnit: mockFeeValuePerBlock(block).toString(),
+        })),
+    );
+
+const estimateFeeMockIncomplete: typeof BlockchainLink.prototype.estimateFee = params =>
+    Promise.resolve(
+        (params.blocks ?? []).map(block =>
+            // no data for the 'economy' fee level
+            block === 80
+                ? { feePerUnit: '-1' }
+                : { feePerUnit: mockFeeValuePerBlock(block).toString() },
+        ),
+    );
+
 describe('BitcoinFeeLevels', () => {
     // load coin definitions
     parseCoinsJson({ ...coinsJSON, ...coinsJSONEth });
 
-    it('should fetch Bitcoin smart FeeLevels with exact match', async () => {
+    afterAll(() => {
+        dispose();
+        jest.resetAllMocks();
+    });
+
+    it('fetches Bitcoin smart FeeLevels with exact match', async () => {
         const coinInfo = getBitcoinNetwork('Bitcoin');
         if (!coinInfo) throw new Error('coinInfo is missing');
         const coinInfoMock = { ...coinInfo, defaultFees: defaultFeesMock };
 
-        const spy = jest
-            .spyOn(BlockchainLink.prototype, 'estimateFee')
-            .mockImplementation(params => {
-                if (!params.blocks) return Promise.resolve([]);
-                const response = params.blocks.map(block => ({
-                    feePerUnit: mockFeeValuePerBlock(block).toString(),
-                }));
-
-                return Promise.resolve(response);
-            });
+        jest.spyOn(BlockchainLink.prototype, 'estimateFee').mockImplementation(estimateFeeMockOK);
 
         const backend = await initBlockchain(coinInfoMock, () => {});
-        const feeLevels = new BitcoinFeeLevels(coinInfoMock);
+        const feeLevelsInstance = new BitcoinFeeLevels(coinInfoMock);
 
-        expect(feeLevels.levels.length).toEqual(4);
+        expect(feeLevelsInstance.levels.length).toEqual(4);
         // returns preloaded values from coins.json
-        expect(feeLevels.levels.map(l => l.feePerUnit)).toEqual(['222', '111', '77', '22']);
+        expect(feeLevelsInstance.levels.map(l => l.feePerUnit)).toEqual(['222', '111', '77', '22']);
 
-        const smartFeeLevels = await feeLevels.load(backend);
+        const result = await feeLevelsInstance.load(backend);
         // linear mock of requested blocks
-        expect(smartFeeLevels.map(l => l.feePerUnit)).toEqual(['9.9', '9', '6', '2']);
-
-        backend.disconnect();
-        spy.mockReset();
+        expect(result.map(l => l.feePerUnit)).toEqual(['9.9', '9', '6', '2']);
     });
 
-    it('should fetch Bitcoin smart FeeLevels with some unknown results in response', async () => {
+    it('fetches Bitcoin smart FeeLevels with some unknown results in response', async () => {
         const coinInfo = getBitcoinNetwork('Bitcoin');
         if (!coinInfo) throw new Error('coinInfo is missing');
         const coinInfoMock = { ...coinInfo, defaultFees: defaultFeesMock };
 
-        const spy = jest
-            .spyOn(BlockchainLink.prototype, 'estimateFee')
-            .mockImplementation(params => {
-                if (!params.blocks) return Promise.resolve([]);
-                const response = params.blocks.map(block =>
-                    block === 80
-                        ? // no data for economy fee level
-                          { feePerUnit: '-1' }
-                        : { feePerUnit: mockFeeValuePerBlock(block).toString() },
-                );
-
-                return Promise.resolve(response);
-            });
+        jest.spyOn(BlockchainLink.prototype, 'estimateFee').mockImplementation(
+            estimateFeeMockIncomplete,
+        );
 
         const backend = await initBlockchain(coinInfoMock, () => {});
-        const feeLevels = new BitcoinFeeLevels(coinInfoMock);
+        const feeLevelsInstance = new BitcoinFeeLevels(coinInfoMock);
 
-        const smartFeeLevels = await feeLevels.load(backend);
+        const result = await feeLevelsInstance.load(backend);
         // linear mock of requested blocks, or preloaded values if not fetched successfully
-        expect(smartFeeLevels.map(l => l.feePerUnit)).toEqual(['9.9', '9', '6', '22']);
-
-        backend.disconnect();
-        spy.mockReset();
+        expect(result.map(l => l.feePerUnit)).toEqual(['9.9', '9', '6', '22']);
     });
 
-    it('should fetch Bitcoin smart FeeLevels with custom fee level', async () => {
+    it('fetches Bitcoin smart FeeLevels with custom fee level', async () => {
         const coinInfo = getBitcoinNetwork('Bitcoin');
         if (!coinInfo) throw new Error('coinInfo is missing');
         const coinInfoMock = { ...coinInfo, defaultFees: defaultFeesMock };
 
         const spy = jest
             .spyOn(BlockchainLink.prototype, 'estimateFee')
-            .mockImplementation(params => {
-                if (!params.blocks) return Promise.resolve([]);
-                const response = params.blocks.map(block => ({
-                    feePerUnit: mockFeeValuePerBlock(block).toString(),
-                }));
-
-                return Promise.resolve(response);
-            });
+            .mockImplementation(estimateFeeMockOK);
 
         const backend = await initBlockchain(coinInfoMock, () => {});
-        const feeLevels = new BitcoinFeeLevels(coinInfoMock);
+        const feeLevelsInstance = new BitcoinFeeLevels(coinInfoMock);
 
         // 'custom' level is appended at the end
-        feeLevels.updateBitcoinCustomFee('7.6');
-        expect(feeLevels.levels.at(-1)).toMatchObject({ label: 'custom', feePerUnit: '7.6' });
+        feeLevelsInstance.updateBitcoinCustomFee('7.6');
+        expect(feeLevelsInstance.levels.at(-1)).toMatchObject({
+            label: 'custom',
+            feePerUnit: '7.6',
+        });
 
-        const smartFeeLevels = await feeLevels.load(backend);
+        const result = await feeLevelsInstance.load(backend);
         // 'custom' level is excluded from the query
         expect(spy).toHaveBeenCalledWith({ blocks: [1, 10, 40, 80] });
 
         // but the 'custom' level is still present, unchanged
-        expect(smartFeeLevels.map(l => l.feePerUnit)).toEqual(['9.9', '9', '6', '2', '7.6']);
-
-        backend.disconnect();
-        spy.mockReset();
+        expect(result.map(l => l.feePerUnit)).toEqual(['9.9', '9', '6', '2', '7.6']);
     });
 
-    it('should fetch Testnet smart FeeLevels with some unknown results in response', async () => {
+    it('fetches Testnet smart FeeLevels with some unknown results in response', async () => {
         const coinInfo = getBitcoinNetwork('Testnet');
         if (!coinInfo) throw new Error('coinInfo is missing');
         // testnet has only one fee level 'normal'
         const coinInfoMock = { ...coinInfo, defaultFees: [defaultFeesMock[1]] };
 
-        const spy = jest
-            .spyOn(BlockchainLink.prototype, 'estimateFee')
-            .mockImplementation(params => {
-                if (!params.blocks) return Promise.resolve([]);
-                const response = params.blocks.map(block =>
-                    block < 10
-                        ? // no data for high fee level (but that one is not queried now)
-                          { feePerUnit: '-1' }
-                        : { feePerUnit: mockFeeValuePerBlock(block).toString() },
-                );
-
-                return Promise.resolve(response);
-            });
+        jest.spyOn(BlockchainLink.prototype, 'estimateFee').mockImplementation(
+            estimateFeeMockIncomplete,
+        );
 
         const backend = await initBlockchain(coinInfoMock, () => {});
-        const feeLevels = new BitcoinFeeLevels(coinInfoMock);
+        const feeLevelsInstance = new BitcoinFeeLevels(coinInfoMock);
 
-        expect(feeLevels.levels.length).toEqual(1);
+        expect(feeLevelsInstance.levels.length).toEqual(1);
         // returns preloaded values from coins.json
-        expect(feeLevels.levels.map(l => l.feePerUnit)).toEqual(['111']);
+        expect(feeLevelsInstance.levels.map(l => l.feePerUnit)).toEqual(['111']);
 
-        const smartFeeLevels = await feeLevels.load(backend);
-        expect(smartFeeLevels?.map(l => l.feePerUnit)).toEqual(['9']);
-
-        backend.disconnect();
-        spy.mockReset();
+        const result = await feeLevelsInstance.load(backend);
+        expect(result?.map(l => l.feePerUnit)).toEqual(['9']);
     });
-
-    // This block is useful to manually observe fee rates from real backends (supported in suite)
-    // How to: uncomment test below
-    // Note: currently broken! The "test" runs, but all you can see is the defaults from json.
-    // Because all API responses are -1. Maybe websocket issue in jest?
-
-    // const e2eNetworks = ['BTC', 'TEST', 'BCH', 'DASH', 'DOGE', 'LTC'];
-    // e2eNetworks.forEach(network => {
-    // eslint-disable-next-line jest/no-commented-out-tests
-    //     it.only(`${network} e2e smart FeeLevels`, async () => {
-    //         const coinInfo = getBitcoinNetwork(network)!;
-    //         if (!coinInfo) throw new Error('coinInfo is missing');
-    //
-    //         const backend = await initBlockchain(coinInfo, () => {});
-    //         const feeLevels = new BitcoinFeeLevels(coinInfo);
-    //         const smartFeeLevels = await feeLevels.load(backend);
-    //         console.info(`${network} FeeLevels`, smartFeeLevels);
-    //         console.info(`${network} longTermFeeRate`, feeLevels.longTermFeeRate);
-    //         backend.disconnect();
-    //     });
-    // });
 });
