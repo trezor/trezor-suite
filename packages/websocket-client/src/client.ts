9f5c81450890b6b4b8d112925e37a3a68d0aea71
Szymon Lesisz <sz.lesisz@gmail.com>
2025-04-25 14:58:17 +0200

    fix(websocket-client): sendMessage timeout handler

--- packages/websocket-client/src/client.ts
+++ packages/websocket-client/src/client.ts
@@ -51,7 +51,7 @@ export class WebsocketClient<Events extends Record<string, any>> extends TypedEm
         this.options = options;
         this.messages = createDeferredManager({
             timeout: this.options.timeout || DEFAULT_TIMEOUT,
-            onTimeout: this.onTimeout.bind(this),
+            onTimeout: () => this.onMessageTimeout(),
         });
     }
 
@@ -93,11 +93,11 @@ export class WebsocketClient<Events extends Record<string, any>> extends TypedEm
         return this.ping();
     }
 
-    private onTimeout() {
+    private onMessageTimeout() {
         const { ws } = this;
         if (!ws) return;
-        this.messages.rejectAll(new WebsocketError('websocket_timeout'));
-        ws.close();
+        this.messages.rejectAll(new WebsocketError('websocket_timeout')); // <- this rejects all(!) pending messages, but only one is failed
+        // ws.close(); // <- this is wrong, if sendMessage throws timeout it disconnect whole client
     }
 
     private onError() {
