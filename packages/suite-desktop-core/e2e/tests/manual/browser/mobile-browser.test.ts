949919858f92d0b82dc8dcf2852b168655e4926c
Tomas Bosansky <bosomt@Tomass-MacBook-Air.local>
2025-04-29 15:19:43 +0200

    feat(e2e): add mobile browser to sanity tests

---
+++ packages/suite-desktop-core/e2e/tests/manual/browser/mobile-browser.test.ts
@@ -0,0 +1,30 @@
+import { TestCategory, TestPriority } from '../../../support/enums/testAnnotations';
+import { test } from '../../../support/fixtures';
+import { createTestAnnotation } from '../../../support/reporters/annotations';
+
+test.describe.skip('Mobile browser', { tag: ['@group=manual'] }, () => {
+    test(
+        'Suite web version mobile browser support',
+        {
+            annotation: createTestAnnotation({
+                testCase:
+                    'Verifies that a user can connect and use a device via supported mobile browser on Android phone.',
+                prerequisites: [
+                    'Seeded Trezor device',
+                    'Supported mobile browser on Android phone',
+                    'Access to Staging version of Trezor Suite',
+                ],
+                steps: [
+                    'Start Google Chrome browser',
+                    'Navigate to https://staging-suite.trezor.io/web/ and connect and unlock device',
+                    'Connect device via webUSB dialogue',
+                    'Perform discovery and generate receive address',
+                    'Send a transaction',
+                ],
+                category: TestCategory.Wallets,
+                priority: TestPriority.Critical,
+            }),
+        },
+        async () => {},
+    );
+});
