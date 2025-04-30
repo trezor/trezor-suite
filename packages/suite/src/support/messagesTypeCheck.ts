81e2dccdc0f448cbd71789391fe9c3d944f4c3bd
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-04-30 13:58:11 +0200

    chore(suite): finish typecheck of translations

--- packages/suite/src/support/messagesTypeCheck.ts
+++ packages/suite/src/support/messagesTypeCheck.ts
@@ -8,8 +8,10 @@ type Messages = typeof messages;
 type TranslationKey = keyof Messages;
 type TranslationId = Messages[keyof Messages]['id'];
 
-const functionJustForTypeCheck = (k: TranslationId): TranslationKey => k;
+// check if one type is assignable to the other, and then do the same in reverse (i.e. if types exactly match)
+const functionJustForTypeCheck1 = (k: TranslationId): TranslationKey => k;
+const functionJustForTypeCheck2 = (k: TranslationKey): TranslationId => k;
 
 // @ts-expect-error TS6133: Suppress unused variable error;
 // can't do this over the function so that it does not suppress the type check itself
-const _suppressUnusedVariableError = functionJustForTypeCheck;
+const _suppressUnusedVariableError = functionJustForTypeCheck1 || functionJustForTypeCheck2;
