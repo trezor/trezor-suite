b58821666cddb94535e2bef5941a19d5905ba8e5
Martin Varmuza <mvarmuza@gmail.com>
2025-05-05 07:54:28 +0200

    enable debug

--- packages/connect/e2e/common.setup.ts
+++ packages/connect/e2e/common.setup.ts
@@ -177,7 +177,7 @@ export const initTrezorConnect = async (
             email: 'tests@connect.trezor.io',
         },
         transports: ['BridgeTransport'],
-        debug: false,
+        debug: true,
         popup: false,
         pendingTransportEvent: true,
         transportReconnect: false,
