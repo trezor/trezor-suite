5e1cbc8e960a1c281d4364dd9b48062d581c0560
Jun Luo <4catcode@gmail.com>
2025-05-08 20:29:18 +0800

    refactor(suite-native): use `@exodus/patch-broken-hermes-typed-arrays` to fix broken Hermes engine TypedArray implementation.

--- suite-native/app/globalPolyfills.js
+++ suite-native/app/globalPolyfills.js
@@ -1,3 +1,6 @@
+// Attempt to fix broken Hermes engine TypedArray implementation for React Native
+// See https://github.com/ExodusMovement/patch-broken-hermes-typed-arrays
+import '@exodus/patch-broken-hermes-typed-arrays';
 import { install } from 'react-native-quick-crypto';
 
 import { CustomEvent } from '@whatwg-node/events'; // to work with @solana/kit
@@ -19,36 +22,6 @@ install();
 
 // The Buffer implementation from react-native-quick-crypto is not compatible with Trezor Connect.
 global.Buffer = require('buffer').Buffer;
-// There is bug in buffer polyfill that when you call subarray it returns Uint8Array instead of Buffer, this fixes it
-// It's basically copy pasted slice method from buffer polyfill with one change in line `const newBuf...`
-Buffer.prototype.subarray = function subarray(start, end) {
-    const len = this.length;
-    start = ~~start;
-    end = end === undefined ? len : ~~end;
-
-    if (start < 0) {
-        start += len;
-        if (start < 0) start = 0;
-    } else if (start > len) {
-        start = len;
-    }
-
-    if (end < 0) {
-        end += len;
-        if (end < 0) end = 0;
-    } else if (end > len) {
-        end = len;
-    }
-
-    if (end < start) end = start;
-
-    const newBuf = Uint8Array.prototype.subarray.call(this, start, end);
-
-    // Return an augmented `Uint8Array` instance
-    Object.setPrototypeOf(newBuf, Buffer.prototype);
-
-    return newBuf;
-};
 
 global.process = {
     ...require('process'),
