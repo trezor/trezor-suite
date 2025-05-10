b4695728d8afdb522787bc11aa5529f5f3c00d34
brantalikp <brantalikp@gmail.com>
2025-05-10 15:32:41 +0200

    fix(suite-native): fix fw navigation when device is disconnected

--- suite-native/module-device-settings/src/hooks/useDeviceConnectionGuard.ts
+++ suite-native/module-device-settings/src/hooks/useDeviceConnectionGuard.ts
@@ -1,7 +1,7 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
 
-import { useNavigation } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 
 import { selectIsDeviceConnected } from '@suite-common/wallet-core';
 import {
@@ -36,10 +36,13 @@ export const useDeviceConnectionGuard = () => {
         });
     }, [navigation]);
 
-    useEffect(() => {
-        if (!isDeviceConnected) return redirectToConnectAndUnlockScreen();
-        // The check should be conducted only once when the screen is shown
-    }, [redirectToConnectAndUnlockScreen]); // eslint-disable-line react-hooks/exhaustive-deps
+    useFocusEffect(
+        useCallback(() => {
+            if (!isDeviceConnected) {
+                redirectToConnectAndUnlockScreen();
+            }
+        }, [isDeviceConnected, redirectToConnectAndUnlockScreen]),
+    );
 
     return { isDeviceConnected };
 };
