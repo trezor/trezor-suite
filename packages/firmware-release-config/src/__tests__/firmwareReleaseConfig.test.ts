66a34aa022e30a641bf8cfe71014145ae81633fa
Carlos Garcia Ortiz karliatto <yo@karliatto.com>
2025-05-19 17:58:39 +0200

    test(firmware-release-config): change test file name

--- packages/firmware-release-config/src/__tests__/firmwareReleaseConfig.test.ts
+++ packages/firmware-release-config/src/__tests__/firmwareReleaseConfig.test.ts
@@ -1,7 +1,7 @@
 import { jws as releasesJwsLocal } from '../../files/releases.v1';
 import { getFirmwareReleaseConfig } from '../index';
 
-describe('getFirmwareReleaseConfig returns releases signed file', () => {
+describe('getFirmwareReleaseConfig returns releases signed file correctly', () => {
     it('should return local JWS', async () => {
         const result = await getFirmwareReleaseConfig();
         expect(result).toEqual({
