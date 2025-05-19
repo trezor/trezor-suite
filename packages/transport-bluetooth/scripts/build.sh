841ea43877c39febc4f53b62d25a54bbc2b77545
vdovhanych <dovhanych@me.com>
2025-05-19 16:26:52 +0200

    feat(ci): add build of transport-bluetooth binary

--- packages/transport-bluetooth/scripts/build.sh
+++ packages/transport-bluetooth/scripts/build.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-docker build . -f ./scripts/Dockerfile -t trezor-bluetooth-image
+# This has to be built using x86 docker image arm image is not working. For building on arm mac use --platform linux/amd64 for the build command.
+docker build . -f ./scripts/Dockerfile -t trezor-bluetooth-image .
 docker create --name trezor-bluetooth-container trezor-bluetooth-image
 docker cp trezor-bluetooth-container:/output/. ../suite-data/files/bin/bluetooth
 docker rm trezor-bluetooth-container
