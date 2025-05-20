341455817b4f718d6a9e2bec4812af5ac18e41c3
Vojtěch Tranta <vojta.tranta@gmail.com>
2025-05-20 14:00:33 +0200

    fix(suite-native): use initial screen navigation when passphrase flow finishes

--- suite-native/module-authorize-device/src/useRedirectOnPassphraseCompletion.ts
+++ suite-native/module-authorize-device/src/useRedirectOnPassphraseCompletion.ts
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector, useStore } from 'react-redux';
 
-import { useNavigation, useRoute } from '@react-navigation/native';
+import { useRoute } from '@react-navigation/native';
 
 import {
     DiscoveryRootState,
@@ -15,21 +15,8 @@ import {
     selectHasVerificationCancelledError,
     selectPassphraseError,
 } from '@suite-native/device-authorization';
-import {
-    AppTabsRoutes,
-    AuthorizeDeviceStackParamList,
-    AuthorizeDeviceStackRoutes,
-    HomeStackRoutes,
-    RootStackParamList,
-    RootStackRoutes,
-    StackToStackCompositeNavigationProps,
-} from '@suite-native/navigation';
+import { useNavigateToInitialScreen } from '@suite-native/navigation';
 
-type NavigationProp = StackToStackCompositeNavigationProps<
-    AuthorizeDeviceStackParamList,
-    AuthorizeDeviceStackRoutes.PassphraseConfirmOnTrezor,
-    RootStackParamList
->;
 export const useRedirectOnPassphraseCompletion = () => {
     const device = useSelector(selectSelectedDevice);
 
@@ -39,19 +26,14 @@ export const useRedirectOnPassphraseCompletion = () => {
 
     const dispatch = useDispatch();
     const store = useStore();
-    const navigation = useNavigation<NavigationProp>();
+    const navigateToInitialScreen = useNavigateToInitialScreen();
 
     const route = useRoute();
 
     useEffect(() => {
         // If there is passphrase error, we don't want to go back, but handle errors through alerts within the flow
         if (passphraseDiscoveryCompleted && !hasPassphraseError) {
-            navigation.navigate(RootStackRoutes.AppTabs, {
-                screen: AppTabsRoutes.HomeStack,
-                params: {
-                    screen: HomeStackRoutes.Home,
-                },
-            });
+            navigateToInitialScreen();
 
             const discovery = selectDiscoveryByDevicePath(
                 store.getState() as DiscoveryRootState,
@@ -62,7 +44,13 @@ export const useRedirectOnPassphraseCompletion = () => {
                 payload: { isEmptyWallet: Boolean(discovery?.emptyWallet) },
             });
         }
-    }, [passphraseDiscoveryCompleted, hasPassphraseError, navigation, store, device?.path]);
+    }, [
+        passphraseDiscoveryCompleted,
+        hasPassphraseError,
+        navigateToInitialScreen,
+        store,
+        device?.path,
+    ]);
 
     useEffect(() => {
         // User has canceled the authorization process on device (authorizeDeviceThunk rejects with auth-failed error)
