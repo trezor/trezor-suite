07fea0ddab3653b099a6e59e0ea975f48e7d2bd2
Vojtěch Tranta <vojta.tranta@gmail.com>
2025-05-21 16:48:22 +0200

    feat(suite-native): remove native discovery-thunks

--- suite-native/discovery/src/discoveryMiddleware.ts
+++ suite-native/discovery/src/discoveryMiddleware.ts
@@ -1,23 +1,13 @@
 import { createMiddlewareWithExtraDeps } from '@suite-common/redux-utils';
 import { periodicCheckTokenDefinitionsThunk } from '@suite-common/token-definitions';
 import {
-    accountsActions,
     changeCoinVisibility,
     changeNetworks,
     deviceActions,
     discoveryActions,
-    selectDeviceFirmwareVersion,
-    selectDeviceModel,
 } from '@suite-common/wallet-core';
-import { isFirmwareVersionSupported } from '@suite-native/device';
-import {
-    selectAreTestnetsEnabled,
-    selectIsCoinEnablingInitFinished,
-    toggleAreTestnetsEnabled,
-} from '@suite-native/settings';
+import { selectIsCoinEnablingInitFinished } from '@suite-native/settings';
 import { hasBitcoinOnlyFirmware } from '@trezor/device-utils';
-
-import { discoveryCheckThunk, startDescriptorPreloadedDiscoveryThunk } from './discoveryThunks';
 
 export const prepareDiscoveryMiddleware = createMiddlewareWithExtraDeps(
     (action, { dispatch, next, getState }) => {
@@ -28,29 +18,7 @@ export const prepareDiscoveryMiddleware = createMiddlewareWithExtraDeps(
             dispatch(discoveryActions.deleteDiscovery(action.payload.device.path));
         }
 
-        const deviceModel = selectDeviceModel(getState());
-        const deviceFwVersion = selectDeviceFirmwareVersion(getState());
-        const areTestnetsEnabled = selectAreTestnetsEnabled(getState());
         const isCoinEnablingInitFinished = selectIsCoinEnablingInitFinished(getState());
-
-        const isDeviceFirmwareVersionSupported = isFirmwareVersionSupported(
-            deviceFwVersion,
-            deviceModel,
-        );
-
-        // If user enables testnets discovery, run discovery with testnets enabled.
-        if (
-            toggleAreTestnetsEnabled.match(action) &&
-            !areTestnetsEnabled &&
-            isDeviceFirmwareVersionSupported &&
-            isCoinEnablingInitFinished
-        ) {
-            dispatch(
-                startDescriptorPreloadedDiscoveryThunk({
-                    forcedAreTestnetsEnabled: areTestnetsEnabled,
-                }),
-            );
-        }
 
         // We need to wait until `authorizeDeviceThunk` action is fulfilled, because we need
         // to know the device state when starting discovery of newly authorized device.
@@ -63,14 +31,6 @@ export const prepareDiscoveryMiddleware = createMiddlewareWithExtraDeps(
             if (device?.connected && hasBitcoinOnlyFirmware(device)) {
                 dispatch(changeCoinVisibility({ symbol: 'btc', shouldBeVisible: true }));
             }
-        }
-
-        // for further continual discovery check for various other reasons
-        if (
-            deviceActions.selectDevice.match(action) || // user switched device
-            accountsActions.changeAccountVisibility.match(action) // account visibility changed - e.g. when incoming txn to hidden account
-        ) {
-            dispatch(discoveryCheckThunk());
         }
 
         // if we changed enabled networks, check for token definitions right away
