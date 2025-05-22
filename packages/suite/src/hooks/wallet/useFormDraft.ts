ad75ef643be370e3958e3c4e2157cae01697755c
Martin Homola <adder10@gmail.com>
2025-05-22 18:19:31 +0200

    chore(suite): delete useActions from useFormDraft

--- packages/suite/src/hooks/wallet/useFormDraft.ts
+++ packages/suite/src/hooks/wallet/useFormDraft.ts
@@ -1,13 +1,32 @@
+import { useCallback } from 'react';
 import { FieldValues } from 'react-hook-form';
 
 import { FormDraftKeyPrefix } from '@suite-common/wallet-types';
 
 import * as formDraftActions from 'src/actions/wallet/formDraftActions';
-import { useActions } from 'src/hooks/suite';
+import { useDispatch } from 'src/hooks/suite';
 
-export const useFormDraft = <T extends FieldValues>(keyPrefix: FormDraftKeyPrefix) =>
-    useActions({
-        getDraft: formDraftActions.getDraft<T>(keyPrefix),
-        saveDraft: formDraftActions.saveDraft<T>(keyPrefix),
-        removeDraft: formDraftActions.removeDraft(keyPrefix),
-    });
+export const useFormDraft = <T extends FieldValues>(keyPrefix: FormDraftKeyPrefix) => {
+    const dispatch = useDispatch();
+
+    const getDraft = useCallback(
+        (key: string) => dispatch(formDraftActions.getDraft<T>(keyPrefix)(key)),
+        [dispatch, keyPrefix],
+    );
+
+    const saveDraft = useCallback(
+        (key: string, data: T) => dispatch(formDraftActions.saveDraft<T>(keyPrefix)(key, data)),
+        [dispatch, keyPrefix],
+    );
+
+    const removeDraft = useCallback(
+        (key: string) => dispatch(formDraftActions.removeDraft(keyPrefix)(key)),
+        [dispatch, keyPrefix],
+    );
+
+    return {
+        getDraft,
+        saveDraft,
+        removeDraft,
+    };
+};
