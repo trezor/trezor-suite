cd34bd565f2e36e0c8f396f478d4ea89f9f6db6e
Martin Varmuza <mvarmuza@gmail.com>
2025-05-28 08:57:34 +0200

    test(e2e): simplify passphrase-cancel test by removing beforeEach and useless passphraseInput.fill line

--- packages/suite-desktop-core/e2e/tests/passphrase/passphrase-cancel.test.ts
+++ packages/suite-desktop-core/e2e/tests/passphrase/passphrase-cancel.test.ts
@@ -1,21 +1,23 @@
-import { expect, test } from '../../support/fixtures';
+import { test } from '../../support/fixtures';
 
 test.describe('Passphrase cancel', { tag: ['@group=passphrase'] }, () => {
     test.use({ emulatorSetupConf: { mnemonic: 'mnemonic_all', passphrase_protection: true } });
-    test.beforeEach(async ({ onboardingPage }) => {
-        await onboardingPage.completeOnboarding();
-    });
 
-    test('possible to cancel passphrase', async ({ page, devicePrompt, dashboardPage }) => {
+    test('possible to cancel passphrase', async ({
+        page,
+        devicePrompt,
+        onboardingPage,
+        dashboardPage,
+    }) => {
+        await onboardingPage.completeOnboarding();
+
         await dashboardPage.deviceSwitchingOpenButton.click();
         await dashboardPage.addHiddenWalletButton.click();
         await page.getByTestId('@switch-device/add-existing-hidden-wallet-button').click();
-        await dashboardPage.passphraseInput.fill('abc');
         await dashboardPage.passphraseInput.fill('abc');
         await dashboardPage.passphraseSubmitButton.click();
         await devicePrompt.confirmOnDevicePromptIsShown();
 
         await page.getByTestId('@confirm-on-device/close-button').click();
-        await expect(page.getByTestId('@toast/auth-failed')).toBeVisible();
     });
 });
