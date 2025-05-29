afe47f81db0551717d97866dcd7cf0dcd0dec13d
Vojtěch Tranta <vojta.tranta@gmail.com>
2025-05-29 14:11:55 +0200

    feat(suite): add bio guard overaly

--- packages/suite/src/actions/suite/bioAuthThunks.ts
+++ packages/suite/src/actions/suite/bioAuthThunks.ts
@@ -1,9 +1,20 @@
 import { createThunk } from '@suite-common/redux-utils';
+import { notificationsActions } from '@suite-common/toast-notifications';
 import { desktopApi } from '@trezor/suite-desktop-api';
 
-import { selectBioAuthEnabled, selectIsRequestingBioAuthChange } from 'src/reducers/desktop';
+import {
+    selectBioAuthEnabled,
+    selectIsBioAuthValidationRequested,
+    selectIsRequestingBioAuthChange,
+} from 'src/reducers/desktop';
 
-import { requestBioAuthChange, requestBioAuthChangeEnd, setBioAuthEnabled } from './suiteActions';
+import {
+    bioAuthValidated,
+    requestBioAuthChange,
+    requestBioAuthChangeEnd,
+    setBioAuthEnabled,
+    toggleBioAuthValidationRequested,
+} from './suiteActions';
 
 const BIO_AUTH_PREFIX = '@suite/bioAuth';
 
@@ -14,20 +25,56 @@ export const requestBioAuthChangeThunk = createThunk(
         const isRequestingChange = selectIsRequestingBioAuthChange(getState());
         const nextBioEnabled = !prevBioEnabled;
 
-        if (isRequestingChange) {
+        if (isRequestingChange || selectIsBioAuthValidationRequested(getState())) {
             return;
         }
 
         dispatch(requestBioAuthChange(nextBioEnabled));
 
-        // NOTE: async stuff here
-
         try {
             await desktopApi.validateBioAuth();
 
             dispatch(setBioAuthEnabled(nextBioEnabled));
+            dispatch(bioAuthValidated(new Date()));
+        } catch (error) {
+            dispatch(bioAuthValidated(null));
+            console.error(error);
+            dispatch(
+                notificationsActions.addToast({
+                    type: 'error',
+                    error: 'Biometric authentication failed',
+                }),
+            );
         } finally {
             dispatch(requestBioAuthChangeEnd());
+        }
+    },
+);
+
+export const requestBioAuthValidationThunk = createThunk(
+    `${BIO_AUTH_PREFIX}/validateAuth`,
+    async (_, { dispatch, getState }) => {
+        const isRequestingValidation = selectIsBioAuthValidationRequested(getState());
+
+        if (isRequestingValidation) {
+            return;
+        }
+
+        dispatch(toggleBioAuthValidationRequested(true));
+        try {
+            await desktopApi.validateBioAuth();
+            dispatch(bioAuthValidated(new Date()));
+        } catch (error) {
+            dispatch(bioAuthValidated(null));
+            console.error(error);
+            dispatch(
+                notificationsActions.addToast({
+                    type: 'error',
+                    error: 'Biometric authentication failed',
+                }),
+            );
+        } finally {
+            dispatch(toggleBioAuthValidationRequested(false));
         }
     },
 );
--- packages/suite/src/reducers/desktop/index.ts
+++ packages/suite/src/reducers/desktop/index.ts
@@ -1,3 +1,5 @@
+import { differenceInMinutes } from 'date-fns';
+
 import type { HandshakeElectron } from '@trezor/suite-desktop-api';
 
 import { SUITE } from 'src/actions/suite/constants';
@@ -6,11 +8,19 @@ import { Action, AppState } from 'src/types/suite';
 export type DesktopState = {
     bioAuthEnabled: boolean;
     bioAuthEnabledNextValue: null | boolean;
+    lastBioAuthValidatedTimestamp: null | string;
+    lastWindowBlurTimestamp: null | string;
+    windowBlurred: boolean;
+    bioAuthValidationRequested: boolean;
 } & Partial<Pick<HandshakeElectron, 'paths' | 'urls'>>;
 
 const initialState: DesktopState = {
     bioAuthEnabled: false,
     bioAuthEnabledNextValue: null,
+    lastBioAuthValidatedTimestamp: null,
+    lastWindowBlurTimestamp: null,
+    windowBlurred: false,
+    bioAuthValidationRequested: false,
 };
 
 export const desktopReducer = (
@@ -40,6 +50,33 @@ export const desktopReducer = (
                 ...state,
                 bioAuthEnabledNextValue: null,
             };
+        case SUITE.REQUEST_BIO_AUTH_VALIDATED:
+            return {
+                ...state,
+                lastBioAuthValidatedTimestamp: action.payload?.toUTCString() ?? null,
+                bioAuthValidationRequested: false,
+                lastWindowBlurTimestamp: null,
+            };
+
+        case SUITE.BIO_AUTH_WINDOW_BLUR:
+            return {
+                ...state,
+                lastWindowBlurTimestamp: action.payload.toUTCString(),
+                windowBlurred: true,
+            };
+
+        case SUITE.BIO_AUTH_WINDOW_FOCUS:
+            return {
+                ...state,
+                windowBlurred: false,
+                lastWindowBlurTimestamp: null,
+            };
+
+        case SUITE.TOGGLE_BIO_AUTH_VALIDATION_REQUESTED:
+            return {
+                ...state,
+                bioAuthValidationRequested: action.payload,
+            };
 
         default:
             return state;
@@ -53,3 +90,41 @@ export const selectBioAuthChangeNextValue = (state: AppState) =>
 
 export const selectIsRequestingBioAuthChange = (state: AppState) =>
     state.desktop.bioAuthEnabledNextValue !== null;
+
+export const selectLastBioAuthValidatedTimestamp = (state: AppState) =>
+    state.desktop.lastBioAuthValidatedTimestamp;
+
+export const selectIsBioAuthValidationRequired = (
+    state: AppState,
+    timestamp: Date = new Date(),
+) => {
+    if (!selectBioAuthEnabled(state)) {
+        return false;
+    }
+
+    const lastBioAuthValidatedTimestamp = selectLastBioAuthValidatedTimestamp(state);
+    if (!lastBioAuthValidatedTimestamp) {
+        return true;
+    }
+
+    const dayInMinutes = 24 * 60;
+    const isValidationExpired =
+        differenceInMinutes(timestamp, new Date(lastBioAuthValidatedTimestamp)) > dayInMinutes;
+
+    const fiveMinutesInMinutes = 5;
+    const isBlurredTooLong =
+        state.desktop.lastWindowBlurTimestamp &&
+        differenceInMinutes(timestamp, new Date(state.desktop.lastWindowBlurTimestamp)) >
+            fiveMinutesInMinutes;
+
+    return Boolean(isValidationExpired || isBlurredTooLong);
+};
+
+export const selectIsBioAuthValidationRequested = (state: AppState) =>
+    state.desktop.bioAuthValidationRequested;
+
+export const selectIsAppUiHidden = (state: AppState) =>
+    (state.desktop.windowBlurred && selectBioAuthEnabled(state)) ||
+    selectIsBioAuthValidationRequested(state);
+
+export const selectIsWindowFocused = (state: AppState) => !state.desktop.windowBlurred;
