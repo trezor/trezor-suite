d136fed9fcc4e1a9ffd30ed9c384459f2f4befbe
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-05-29 13:19:30 +0200

    chore(suite-native): Mobile Trade: refactor useConvertFormValueToBaseUnit hook

--- suite-native/module-trading/src/hooks/general/useConvertFormValueToBaseUnit.ts
+++ suite-native/module-trading/src/hooks/general/useConvertFormValueToBaseUnit.ts
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
 
 import { NetworkSymbol, NetworkSymbolExtended, getNetwork } from '@suite-common/wallet-config';
@@ -7,33 +8,33 @@ import { satoshiAmountToBtc } from '@suite-common/wallet-utils';
 export const useConvertFormValueToBaseUnit = () => {
     const areSatsAmountUnit = useSelector(selectAreSatsAmountUnit);
 
-    const getIsAmountInSats = (symbol: NetworkSymbolExtended) => {
-        // this is copy of selectIsAmountInSats logic
-        const network = getNetwork(symbol as NetworkSymbol);
-        const isAmountUnitSupported = !!network && network.features.includes('amount-unit');
+    const getIsAmountInSats = useCallback(
+        (symbol: NetworkSymbolExtended) => {
+            // this is copy of selectIsAmountInSats logic
+            const network = getNetwork(symbol as NetworkSymbol);
+            const isAmountUnitSupported = !!network && network.features.includes('amount-unit');
 
-        return isAmountUnitSupported && areSatsAmountUnit;
-    };
+            return isAmountUnitSupported && areSatsAmountUnit;
+        },
+        [areSatsAmountUnit],
+    );
+
+    const convertStrToBaseUnit = useCallback(
+        (amount: string | undefined, symbol: NetworkSymbolExtended) =>
+            amount !== undefined && getIsAmountInSats(symbol) ? satoshiAmountToBtc(amount) : amount,
+        [getIsAmountInSats],
+    );
+
+    const convertNumberToBaseUnit = useCallback(
+        (amount: number | undefined, symbol: NetworkSymbolExtended) =>
+            amount !== undefined && getIsAmountInSats(symbol)
+                ? parseFloat(satoshiAmountToBtc(amount.toString()))
+                : amount,
+        [getIsAmountInSats],
+    );
 
     return {
-        convertStrToBaseUnit: (amount: string | undefined, symbol: NetworkSymbolExtended) => {
-            if (amount === undefined) {
-                return undefined;
-            }
-
-            return getIsAmountInSats(symbol) ? satoshiAmountToBtc(amount) : amount;
-        },
-
-        convertNumberToBaseUnit: (amount: number | undefined, symbol: NetworkSymbolExtended) => {
-            if (amount === undefined) {
-                return undefined;
-            }
-
-            if (getIsAmountInSats(symbol)) {
-                return parseFloat(satoshiAmountToBtc(amount.toString()));
-            }
-
-            return amount;
-        },
+        convertStrToBaseUnit,
+        convertNumberToBaseUnit,
     };
 };
