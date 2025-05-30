a0f314fce72e27adbda718c455c7d4ed32ae3e07
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-05-30 18:58:08 +0200

    feat(suite): min fee loader duration to emphasize

--- packages/suite/src/components/wallet/Fees/useRefetchFees.ts
+++ packages/suite/src/components/wallet/Fees/useRefetchFees.ts
@@ -6,27 +6,39 @@ import { FEES_UPDATE_INTERVAL, updateFeeInfoThunk } from '@suite-common/wallet-c
 
 import { useDispatch } from 'src/hooks/suite';
 
+const MIN_LOADER_LIFETIME = 1000; // [ms]
+
 type UseRefetchFeesProps = { networkSymbol: NetworkSymbol };
 
 export const useRefetchFees = ({ networkSymbol }: UseRefetchFeesProps) => {
     const dispatch = useDispatch();
     const [areFeesLoading, setAreFeesLoading] = useState(false);
 
-    const refetchFees = async () => {
+    const refetchFees = async (minLoaderLifetime: number) => {
         if (areFeesLoading) return;
         setAreFeesLoading(true);
-        await dispatch(updateFeeInfoThunk({ networkSymbol }));
+
+        /*
+         Backend request is usually very quick, so ensure that the loader is visible for a bit longer,
+         to draw users attention to the fees which are changing.
+         It also prevents flickering of loading state.
+        */
+        await Promise.all([
+            dispatch(updateFeeInfoThunk({ networkSymbol })),
+            new Promise(resolve => setTimeout(resolve, minLoaderLifetime)),
+        ]);
+
         setAreFeesLoading(false);
     };
 
     // Initial fetch when component mounts
     useEffect(() => {
-        refetchFees();
+        refetchFees(0);
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     // Refetch fees periodically
-    useInterval(refetchFees, FEES_UPDATE_INTERVAL);
+    useInterval(() => refetchFees(MIN_LOADER_LIFETIME), FEES_UPDATE_INTERVAL);
 
     return { areFeesLoading };
 };
