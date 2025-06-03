4ccaccee6072cfe9ea31f1f57f6b8e1fa9945110
juriczech <bohdan.juricek@gmail.com>
2025-06-03 10:18:42 +0200

    feat(suite-native): disable wipe for view only

--- suite-native/module-device-settings/src/navigation/WipeDeviceStackNavigator.tsx
+++ suite-native/module-device-settings/src/navigation/WipeDeviceStackNavigator.tsx
@@ -6,28 +6,33 @@ import {
     stackNavigationOptionsConfig,
 } from '@suite-native/navigation';
 
+import { useDeviceConnectionGuard } from '../hooks/useDeviceConnectionGuard';
 import { WipeDeviceContinueOnTrezorScreen } from '../screens/WipeDeviceContinueOnTrezorScreen';
 import { WipeDeviceLoadingScreen } from '../screens/WipeDeviceLoadingScreen';
 import { WipeDeviceScreen } from '../screens/WipeDeviceScreen';
 
 const WipeDeviceStack = createNativeStackNavigator<WipeDeviceStackParamList>();
 
-export const WipeDeviceStackNavigator = () => (
-    <WipeDeviceStack.Navigator
-        initialRouteName={WipeDeviceStackRoutes.WipeDevice}
-        screenOptions={stackNavigationOptionsConfig}
-    >
-        <WipeDeviceStack.Screen
-            name={WipeDeviceStackRoutes.WipeDevice}
-            component={WipeDeviceScreen}
-        />
-        <WipeDeviceStack.Screen
-            name={WipeDeviceStackRoutes.ContinueOnTrezor}
-            component={WipeDeviceContinueOnTrezorScreen}
-        />
-        <WipeDeviceStack.Screen
-            name={WipeDeviceStackRoutes.WipeDeviceLoadingScreen}
-            component={WipeDeviceLoadingScreen}
-        />
-    </WipeDeviceStack.Navigator>
-);
+export const WipeDeviceStackNavigator = () => {
+    useDeviceConnectionGuard();
+
+    return (
+        <WipeDeviceStack.Navigator
+            initialRouteName={WipeDeviceStackRoutes.WipeDevice}
+            screenOptions={stackNavigationOptionsConfig}
+        >
+            <WipeDeviceStack.Screen
+                name={WipeDeviceStackRoutes.WipeDevice}
+                component={WipeDeviceScreen}
+            />
+            <WipeDeviceStack.Screen
+                name={WipeDeviceStackRoutes.ContinueOnTrezor}
+                component={WipeDeviceContinueOnTrezorScreen}
+            />
+            <WipeDeviceStack.Screen
+                name={WipeDeviceStackRoutes.WipeDeviceLoadingScreen}
+                component={WipeDeviceLoadingScreen}
+            />
+        </WipeDeviceStack.Navigator>
+    );
+};
