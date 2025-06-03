679f5ee1649e63df2d2786f80178d268895d7c9a
Tomas Martykan <t.martykan@gmail.com>
2025-06-03 12:26:19 +0200

    fix(components): revert increase depth in useDetectPortalTarget

--- packages/components/src/components/form/Select/useDetectPortalTarget.ts
+++ packages/components/src/components/form/Select/useDetectPortalTarget.ts
@@ -10,9 +10,6 @@ export const useDetectPortalTarget = (selectRef: RefObject<SelectInstance<Option
     // The inputRef is deeply nested so we iterate with while loop until wee reach the parentElement or max depth.
     // The depth has a limit so that it does not iterate over the entire DOM.
     // The depth limit has a buffer of 2 iterations in case the select is wrapped. (Minimum depth is 4.)
-
-    // UPDATE (2025-06-02): There is a lot more wrapping now, just in the modal itself there's 4 layers of wrapping.
-    // So I increased the depth limit to 15 iterations.
     useEffect(() => {
         let parent = selectRef.current?.inputRef?.parentElement;
         let count = 0;
@@ -21,7 +18,7 @@ export const useDetectPortalTarget = (selectRef: RefObject<SelectInstance<Option
                 setMenuPortalTarget(document.body);
                 break;
             }
-            if (count > 15) {
+            if (count > 5) {
                 break;
             }
             parent = parent.parentElement;
