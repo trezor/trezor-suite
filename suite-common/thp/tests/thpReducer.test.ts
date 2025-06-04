e5318b80d6428618a6bbeaa6cd9dc36601ad6235
Peter Sanderson <dierk.deidel@protonmail.com>
2025-06-04 14:19:36 +0200

    feat: THP common

--- suite-common/thp/tests/thpReducer.test.ts
+++ suite-common/thp/tests/thpReducer.test.ts
@@ -1,5 +1,6 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
+import { ThpSuiteCredentials } from '@suite-common/suite-types';
 import { configureMockStore, extraDependenciesMock } from '@suite-common/test-utils';
 
 import { prepareThpReducer, thpActions } from '../src';
@@ -13,6 +14,13 @@ const initialState: ThpState = {
     credentials: [],
 };
 
+const createCredential = (credential: string): ThpSuiteCredentials => ({
+    credential,
+    connectionCounter: 0,
+    trezor_static_pubkey: '',
+    autoconnect: false,
+});
+
 describe('thpReducer', () => {
     it('sets the lastThpCode', () => {
         const store = configureMockStore({
@@ -24,5 +32,26 @@ describe('thpReducer', () => {
         expect(store.getState().thp.lastThpCode).toEqual(undefined);
         store.dispatch(thpActions.setLastThpCode({ code: '123456' }));
         expect(store.getState().thp.lastThpCode).toEqual('123456');
+    });
+
+    it('filters out the credentials to be removed', () => {
+        const store = configureMockStore({
+            extra: {},
+            reducer: combineReducers({ thp: thpReduce }),
+            preloadedState: {
+                thp: {
+                    ...initialState,
+                    credentials: [createCredential('1'), createCredential('2')],
+                },
+            },
+        });
+
+        expect(store.getState().thp.credentials.map(it => it.credential)).toEqual(['1', '2']);
+
+        store.dispatch(thpActions.removeCredentials({ credentials: [createCredential('3')] }));
+        expect(store.getState().thp.credentials.map(it => it.credential)).toEqual(['1', '2']);
+
+        store.dispatch(thpActions.removeCredentials({ credentials: [createCredential('1')] }));
+        expect(store.getState().thp.credentials.map(it => it.credential)).toEqual(['2']);
     });
 });
