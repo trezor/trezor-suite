0926facc2a143638f81b0f14945ebbc8c50b6bb4
Peter Sanderson <dierk.deidel@protonmail.com>
2025-06-09 12:03:52 +0200

    chore: enable connect methdos calls after connect THP merge

--- packages/suite/src/actions/thp/startThpAutoconnectThunk.ts
+++ packages/suite/src/actions/thp/startThpAutoconnectThunk.ts
@@ -2,8 +2,7 @@ import { createThunk } from '@suite-common/redux-utils/';
 import { thpActions } from '@suite-common/thp';
 import { notificationsActions } from '@suite-common/toast-notifications';
 import { selectSelectedDevice } from '@suite-common/wallet-core';
-// TODO thp-post-fixes
-// import TrezorConnect from '@trezor/connect';
+import TrezorConnect from '@trezor/connect';
 
 import { THP_PREFIX } from './thpActions';
 
@@ -16,12 +15,7 @@ export const startThpAutoconnectThunk = createThunk<void, void, void>(
             return;
         }
 
-        // TODO thp-post-fixes
-        // const response = await TrezorConnect.thpGetCredentials({ device });
-        const response: any = await Promise.resolve({
-            success: false,
-            payload: { error: 'Implement TrezorConnect.thpGetCredentials' },
-        });
+        const response = await TrezorConnect.thpGetCredentials({ device });
 
         if (response.success) {
             dispatch(thpActions.addCredential({ credential: response.payload }));
