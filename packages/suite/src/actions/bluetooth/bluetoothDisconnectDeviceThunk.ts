1cbbb905c24ab054b81bd6b154a8d7b9eb05f5a0
Peter Sanderson <dierk.deidel@protonmail.com>
2025-06-10 12:33:18 +0200

    Revert "fix: remove logs, intentionally in separate commit to easy revert for debugging"
    This reverts commit 51ac173ef05e053eef22ff6dc2847819a9205214.

--- packages/suite/src/actions/bluetooth/bluetoothDisconnectDeviceThunk.ts
+++ packages/suite/src/actions/bluetooth/bluetoothDisconnectDeviceThunk.ts
@@ -14,9 +14,11 @@ export const bluetoothDisconnectDeviceThunk = createThunk<
 >(
     `${BLUETOOTH_PREFIX}/bluetoothDisconnectDeviceThunk`,
     async ({ id }, { fulfillWithValue, dispatch }) => {
+        console.log('_____calling: bluetoothIpc.disconnectDevice(id)', id);
         const result = await bluetoothIpc.disconnectDevice(id);
 
         if (!result.success) {
+            console.log('__FAIL!!!: bluetoothIpc.disconnectDevice(id)', id, result.error);
             dispatch(
                 bluetoothActions.updateDeviceConnectionStatus({
                     deviceId: id,
@@ -30,6 +32,8 @@ export const bluetoothDisconnectDeviceThunk = createThunk<
                     error: result.error,
                 }),
             );
+        } else {
+            console.log('_____SUCCESS!!!: bluetoothIpc.disconnectDevice(id)', id);
         }
 
         return fulfillWithValue({ success: result.success });
