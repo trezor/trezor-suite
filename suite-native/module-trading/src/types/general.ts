ddc0be5d3b3160f301454a1ead8b8d59cc3e1810
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-06-11 17:11:06 +0200

    chore(suite-native): Mobile Trade: remove unused type

--- suite-native/module-trading/src/types/general.ts
+++ suite-native/module-trading/src/types/general.ts
@@ -5,9 +5,6 @@ import { NetworkSymbolExtended } from '@suite-common/wallet-config';
 import { Account, TokenAddress } from '@suite-common/wallet-types';
 import { Address } from '@trezor/blockchain-link-types';
 
-import { BuyFormType } from './buy';
-import { ExchangeFormType } from './exchange';
-
 export type TradeableAsset = {
     symbol: NetworkSymbolExtended;
     contractAddress?: TokenAddress | undefined;
@@ -33,5 +30,3 @@ export type BaseFormValues<FocusedValue extends string, Quote extends TradingTra
     generalAlert: string | undefined;
     focusedValue: FocusedValue | undefined;
 } & Record<FocusedValue, string | undefined>;
-
-export type GeneralFormType = BuyFormType | ExchangeFormType;
