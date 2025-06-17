fb05e4ea16e696d57a6f9eb17372c9bfb8e536b8
Matěj Husák <matej.husak@icloud.com>
2025-06-17 10:31:18 +0200

    fix(mobile): created NoBackupWarningBottomSheet

---
+++ suite-native/module-device-onboarding/src/components/WalletBackupNotSetWarningBottomSheet.tsx
@@ -0,0 +1,73 @@
+import { forwardRef, useCallback } from 'react';
+
+import { BottomSheetModalMethods } from '@gorhom/bottom-sheet/lib/typescript/types';
+import { useNavigation } from '@react-navigation/native';
+
+import { BottomSheetModal, Button, Text, TitleHeader, VStack } from '@suite-native/atoms';
+import { Translation } from '@suite-native/intl';
+import {
+    DeviceOnboardingStackParamList,
+    DeviceOnboardingStackRoutes,
+    RootStackParamList,
+    RootStackRoutes,
+    StackToStackCompositeNavigationProps,
+} from '@suite-native/navigation';
+import TrezorConnect, { UI } from '@trezor/connect';
+
+type WalletBackupNotSetWarningBottomSheetProps = {
+    onConfirm?: () => void;
+    onClose?: () => void;
+};
+
+type NavigationProps = StackToStackCompositeNavigationProps<
+    DeviceOnboardingStackParamList,
+    RootStackRoutes.DeviceOnboardingStack,
+    RootStackParamList
+>;
+
+export const WalletBackupNotSetWarningBottomSheet = forwardRef<
+    BottomSheetModalMethods,
+    WalletBackupNotSetWarningBottomSheetProps
+>(({ onConfirm, onClose }, ref) => {
+    const navigation = useNavigation<NavigationProps>();
+
+    const handleContinueAnyway = useCallback(() => {
+        // When Trezor has no backup, it awaits confirmation to continue.
+        TrezorConnect.uiResponse({
+            type: UI.RECEIVE_CONFIRMATION,
+            payload: true,
+        });
+        onConfirm?.();
+    }, [onConfirm]);
+
+    const handleCreateBackup = useCallback(() => {
+        navigation.navigate(RootStackRoutes.DeviceOnboardingStack, {
+            screen: DeviceOnboardingStackRoutes.WalletBackupTutorial,
+        });
+        onClose?.();
+    }, [navigation, onClose]);
+
+    return (
+        <BottomSheetModal ref={ref}>
+            <VStack spacing="sp24" paddingHorizontal="sp8" paddingBottom="sp24">
+                <TitleHeader
+                    textAlign="center"
+                    title={<Translation id="moduleDevice.noBackupModal.title" />}
+                />
+
+                <Text textAlign="center">
+                    <Translation id="moduleDevice.noBackupModal.subtitle" />
+                </Text>
+
+                <VStack spacing="sp12">
+                    <Button colorScheme='yellowElevation0' onPress={handleContinueAnyway}>
+                        <Translation id="moduleDevice.noBackupModal.continue" />
+                    </Button>
+                    <Button colorScheme='yellowBold' onPress={handleCreateBackup}>
+                        <Translation id="moduleDevice.noBackupModal.cta" />
+                    </Button>
+                </VStack>
+            </VStack>
+        </BottomSheetModal>
+    );
+});
