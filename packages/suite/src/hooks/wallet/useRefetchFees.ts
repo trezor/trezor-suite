c956709c65b8bdfa593cfb84293a1bc5d4b45fd7
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-06-24 19:49:45 +0200

    fix(suite): don't refetch fees when any modal is open

--- packages/suite/src/hooks/wallet/useRefetchFees.ts
+++ packages/suite/src/hooks/wallet/useRefetchFees.ts
@@ -7,8 +7,10 @@ import {
     FEE_UPDATE_DELAY_MILLISECONDS,
     updateFeeInfoThunk,
 } from '@suite-common/wallet-core';
+import { UI } from '@trezor/connect';
 
-import { useDispatch } from 'src/hooks/suite';
+import { MODAL } from 'src/actions/suite/constants';
+import { useDispatch, useSelector } from 'src/hooks/suite';
 
 type UseRefetchFeesProps = { networkSymbol: NetworkSymbol; isDisabled?: boolean };
 
@@ -22,12 +24,28 @@ export const useFetchFeesOnce = ({ networkSymbol, isDisabled }: UseRefetchFeesPr
     }, [dispatch, networkSymbol, isDisabled]);
 };
 
+// Don't refetch when specific critical modals are open
+const excludedModalWindowTypes = [
+    UI.REQUEST_PASSPHRASE,
+    // both are TransactionReviewModal, which should be final and not be subject to sudden change!
+    'ButtonRequest_ConfirmOutput',
+    'ButtonRequest_SignTx',
+];
+
 // Refetch fees periodically, incl. loading behavior
 export const useRefetchFees = ({ networkSymbol, isDisabled }: UseRefetchFeesProps) => {
     const dispatch = useDispatch();
 
+    const modal = useSelector(state => state.modal);
+
+    const isExcludedModal =
+        modal.context === MODAL.CONTEXT_DEVICE &&
+        modal.windowType !== undefined &&
+        excludedModalWindowTypes.includes(modal.windowType);
+
     useInterval(() => {
         if (isDisabled === true) return;
+        if (isExcludedModal) return;
         dispatch(
             updateFeeInfoThunk({ networkSymbol, artificialDelay: FEE_UPDATE_DELAY_MILLISECONDS }),
         );
