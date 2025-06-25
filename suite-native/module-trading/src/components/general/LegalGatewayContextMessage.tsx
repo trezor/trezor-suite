fc1ddecacd23626eb4ee144ac71cc655a3dcbf02
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-06-25 15:35:40 +0200

    feat(suite-native): Mobile Trade: Gateway 21 footer

---
+++ suite-native/module-trading/src/components/general/LegalGatewayContextMessage.tsx
@@ -0,0 +1,8 @@
+import { Context } from '@suite-common/message-system';
+import { ContextMessage, ContextMessageProps } from '@suite-native/message-system';
+
+export type LegalGatewayContextMessageProps = Omit<ContextMessageProps, 'context'>;
+
+export const LegalGatewayContextMessage = (props: LegalGatewayContextMessageProps) => (
+    <ContextMessage context={Context.getLegal('gateway')} {...props} />
+);
