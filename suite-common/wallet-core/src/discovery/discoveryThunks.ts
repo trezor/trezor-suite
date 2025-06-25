e759b6bd5c81eec5eb22852a91ab98f735f60a7a
Martin Varmuza <mvarmuza@gmail.com>
2025-06-25 13:18:43 +0200

    chore(suite): remove unneeded asset in runDiscoveryThunk

--- suite-common/wallet-core/src/discovery/discoveryThunks.ts
+++ suite-common/wallet-core/src/discovery/discoveryThunks.ts
@@ -364,7 +364,6 @@ export const runDiscoveryThunk = createThunk(
 
             const { isAddingHiddenWallet } = discovery;
 
-            assertDeviceIsAcquired(device);
             if (isAddingHiddenWallet && device.features && !device.features.passphrase_protection) {
                 dispatch(
                     discoveryActions.updateDiscovery(
