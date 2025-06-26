685c748692168cc2eacfff4319cadf355d660169
Matěj Husák <matej.husak@icloud.com>
2025-06-26 03:14:12 +0200

    feat(mobile): added unit tests for coin control in module-send

---
+++ suite-native/module-send/src/components/CoinControl/__tests__/SendUtxoScreenFooter.comp.test.tsx
@@ -0,0 +1,46 @@
+import { renderWithStoreProviderAsync } from '@suite-native/test-utils';
+
+import { SendUtxoScreenFooter } from '../SendUtxoScreenFooter';
+
+jest.mock('../../../hooks/useUtxoSelection', () => ({
+    useUtxoSelection: jest.fn(),
+}));
+
+describe('SendUtxosScreenFooter', () => {
+    it('should render footer with selected total and continue button', async () => {
+        const { getByText } = await renderWithStoreProviderAsync(
+            <SendUtxoScreenFooter symbol="btc" selectedTotal="800000000" onSubmit={jest.fn()} />,
+        );
+
+        expect(getByText('Selected')).toBeTruthy();
+        expect(getByText('8 BTC')).toBeTruthy();
+        expect(getByText('Confirm selection')).toBeTruthy();
+    });
+
+    it('should show remaining amount when amount is provided and selected total is less than amount', async () => {
+        const { getByText } = await renderWithStoreProviderAsync(
+            <SendUtxoScreenFooter
+                symbol="btc"
+                selectedTotal="500000000"
+                onSubmit={jest.fn()}
+                amount="800000000"
+            />,
+        );
+
+        expect(getByText('Remaining to select')).toBeTruthy();
+        expect(getByText('3 BTC')).toBeTruthy(); // 800000000 - 500000000 = 300000000 (3 BTC)
+    });
+
+    it('should not show remaining amount when selected total is equal to or greater than amount', async () => {
+        const { queryByText } = await renderWithStoreProviderAsync(
+            <SendUtxoScreenFooter
+                symbol="btc"
+                selectedTotal="800000000"
+                onSubmit={jest.fn()}
+                amount="800000000"
+            />,
+        );
+
+        expect(queryByText('Remaining to select')).toBeNull();
+    });
+});
---
+++ suite-native/module-send/src/components/CoinControl/__tests__/SwitchCoinControlButton.comp.test.tsx
@@ -0,0 +1,89 @@
+import {
+    BasicProviderForTests,
+    renderHook,
+    renderWithBasicProvider,
+} from '@suite-native/test-utils';
+import { NativeStyleUtils, useNativeStyles } from '@trezor/styles';
+import { BigNumber } from '@trezor/utils';
+
+import { useUtxoSelection } from '../../../hooks/useUxtoSelection';
+import { SwitchCoinControlButton } from '../SwitchCoinControlButton';
+
+jest.mock('../../../hooks/useUtxoSelection', () => ({
+    useUtxoSelection: jest.fn(),
+}));
+
+describe('renders button with correct color scheme', () => {
+    let colors: NativeStyleUtils['colors'];
+
+    beforeAll(() => {
+        const { result } = renderHook(() => useNativeStyles(), { wrapper: BasicProviderForTests });
+        ({ colors } = result.current.utils);
+    });
+
+    it('should render button with yellowBold color scheme when selected utxos are not enough', () => {
+        (useUtxoSelection as jest.Mock).mockReturnValue({
+            isCoinControlEnabled: true,
+            selectedUtxos: [
+                {
+                    txid: 'txid1',
+                    vout: 0,
+                    amount: '500',
+                    blockHeight: 123456,
+                    address: 'address1',
+                    path: 'm/44/0/0/0',
+                    confirmations: 10,
+                },
+            ],
+            totalSelectedAmount: BigNumber(500),
+        });
+
+        const { getByTestId } = renderWithBasicProvider(
+            <SwitchCoinControlButton accountKey="account-key" amount="1000" />,
+        );
+
+        const button = getByTestId('switch-coin-control-button');
+        expect(button.props.style[1].backgroundColor).toBe(colors.backgroundAlertYellowBold);
+    });
+
+    it('should render button with primary color scheme when utxos are selected and amount is sufficient', () => {
+        (useUtxoSelection as jest.Mock).mockReturnValue({
+            isCoinControlEnabled: true,
+            selectedUtxos: [
+                {
+                    txid: 'txid1',
+                    vout: 0,
+                    amount: '1000',
+                    blockHeight: 123456,
+                    address: 'address1',
+                    path: 'm/44/0/0/0',
+                    confirmations: 10,
+                },
+            ],
+            totalSelectedAmount: BigNumber(1000),
+        });
+        const { getByTestId } = renderWithBasicProvider(
+            <SwitchCoinControlButton accountKey="account-key" amount="1000" />,
+        );
+
+        const button = getByTestId('switch-coin-control-button');
+        expect(button.props.style[1].backgroundColor).toBe(colors.backgroundPrimaryDefault);
+    });
+
+    it('should render a button with gray color scheme when no utxos are selected and no amount is provided', () => {
+        (useUtxoSelection as jest.Mock).mockReturnValue({
+            isCoinControlEnabled: false,
+            selectedUtxos: [],
+            totalSelectedAmount: BigNumber(1000),
+        });
+
+        const { getByTestId } = renderWithBasicProvider(
+            <SwitchCoinControlButton accountKey="account-key" />,
+        );
+
+        const button = getByTestId('switch-coin-control-button');
+        expect(button.props.style[1].backgroundColor).toBe(
+            colors.backgroundTertiaryDefaultOnElevation0,
+        );
+    });
+});
