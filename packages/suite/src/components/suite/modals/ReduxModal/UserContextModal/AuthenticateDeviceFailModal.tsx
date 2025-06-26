bfe6508fd7a10325d4511038ebc20b5d23b4b04b
Jan Komarek <jan.komarek@satoshilabs.com>
2025-06-26 15:03:06 +0200

    feat(suite): make AuthenticateDeviceFailModal closable

--- packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/AuthenticateDeviceFailModal.tsx
+++ packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/AuthenticateDeviceFailModal.tsx
@@ -1,15 +1,30 @@
-import { Card, Modal } from '@trezor/components';
+import { useDispatch } from 'react-redux';
 
+import { Button, Modal } from '@trezor/components';
+
+import { onCancel } from 'src/actions/suite/modalActions';
 import { SecurityCheckFail } from 'src/components/suite/SecurityCheck/SecurityCheckFail';
 import { AuthenticateDeviceSupportButton } from 'src/components/suite/SecurityCheck/deviceCompromisedCtas';
+import { Translation } from 'src/components/suite/Translation';
 
-export const AuthenticateDeviceFailModal = () => (
-    <Modal>
-        <Card>
+export const AuthenticateDeviceFailModal = () => {
+    const dispatch = useDispatch();
+
+    const close = () => dispatch(onCancel());
+
+    return (
+        <Modal>
             <SecurityCheckFail
-                ctaSection={<AuthenticateDeviceSupportButton />}
+                ctaSection={
+                    <>
+                        <AuthenticateDeviceSupportButton />
+                        <Button variant="tertiary" onClick={close}>
+                            <Translation id="TR_DISMISS" />
+                        </Button>
+                    </>
+                }
                 text="TR_DEVICE_COMPROMISED_DEVICE_AUTHENTICITY_TEXT"
             />
-        </Card>
-    </Modal>
-);
+        </Modal>
+    );
+};
