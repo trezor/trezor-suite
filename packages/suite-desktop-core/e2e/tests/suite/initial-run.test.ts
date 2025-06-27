750d870b17124affafa0ce07e327a5124433780c
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2025-06-27 17:15:33 +0200

    refactor(e2e): Revert optionallyDismissFwHashCheckError
    while working, the new version had confusing sideeffect in logs

--- packages/suite-desktop-core/e2e/tests/suite/initial-run.test.ts
+++ packages/suite-desktop-core/e2e/tests/suite/initial-run.test.ts
@@ -1,4 +1,17 @@
+import { Page } from '@playwright/test';
+
 import { expect, test } from '../../support/fixtures';
+
+const optionallyDismissFwHashCheckError = (page: Page) => {
+    // dismisses the error modal only if it appears (handle it async in parallel, not necessary to block the rest of the flow)
+    // eslint-disable-next-line playwright/no-wait-for-selector
+    void page
+        .waitForSelector('[data-testid="@device-compromised/dismiss-button"]', {
+            timeout: 10_000,
+        })
+        .then(async button => await button.click())
+        .catch(() => {}); // Intentionally ignore timeout errors - means the modal was not shown
+};
 
 test.describe('Suite initial run', { tag: ['@group=suite'] }, () => {
     test('Until user passed through initial run, it will be there after reload', async ({
@@ -6,19 +19,21 @@ test.describe('Suite initial run', { tag: ['@group=suite'] }, () => {
         analyticsSection,
         onboardingPage,
     }) => {
-        await onboardingPage.disableNecessaryFirmwareChecks();
-        await onboardingPage.optionallyDismissFwHashCheckError();
+        await onboardingPage.verifySuiteIsLoaded();
+        optionallyDismissFwHashCheckError(page);
         await expect(analyticsSection.toggleSwitch).toBeVisible();
 
         await page.reload();
-        await onboardingPage.optionallyDismissFwHashCheckError();
+        await onboardingPage.verifySuiteIsLoaded();
+        optionallyDismissFwHashCheckError(page);
         // analytics screen is there until user confirms his choice
         await expect(analyticsSection.toggleSwitch).toBeVisible();
         await analyticsSection.continueButton.click();
         await expect(page.getByTestId('@onboarding/exit-app-button')).toBeVisible();
 
         await page.reload();
-        await onboardingPage.optionallyDismissFwHashCheckError();
+        await onboardingPage.verifySuiteIsLoaded();
+        optionallyDismissFwHashCheckError(page);
         await expect(analyticsSection.toggleSwitch).toBeHidden();
         await expect(onboardingPage.onboardingContinueButton).toBeVisible();
     });
