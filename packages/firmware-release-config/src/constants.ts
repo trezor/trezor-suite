444fdf10c4de40a2da55611f9dd0224559761685
karliatto <yo@karliatto.com>
2025-07-01 10:02:30 +0200

    chore(firmware-release-config): delete firmware-release-config package

--- packages/firmware-release-config/src/constants.ts
+++
@@ -1,9 +0,0 @@
-export const JWS_SIGN_ALGORITHM = 'ES256';
-export const VERSION = 1;
-export const JWS_RELEASES_FILENAME_REMOTE = `releases.v${VERSION}.jws`;
-
-export const RELEASES_URL_REMOTE_BASE = 'https://data.trezor.io/releases';
-export const RELEASES_URL_REMOTE = {
-    stable: `${RELEASES_URL_REMOTE_BASE}/stable/${JWS_RELEASES_FILENAME_REMOTE}`,
-    develop: `${RELEASES_URL_REMOTE_BASE}/develop/${JWS_RELEASES_FILENAME_REMOTE}`,
-};
