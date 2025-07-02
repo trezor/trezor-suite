e6d62e9fc2486161d3eae8753cd93567e7de5584
Ondrej Hajek <ondrej.hajek@satoshilabs.com>
2025-07-02 14:10:35 +0200

    fix: Fixed passphrase reconnection test

--- packages/suite-desktop-core/e2e/support/pageObjects/devicePrompt.ts
+++ packages/suite-desktop-core/e2e/support/pageObjects/devicePrompt.ts
@@ -78,6 +78,11 @@ export class DevicePrompt {
     }
 
     @step()
+    async confirmOnDevicePromptIsHiddenOnModal() {
+        await expect(this.confirmOnDevicePrompt).toHaveCSS('opacity', '0'); // On some modal dialogs, the prompt is hidden via opacity but still present in the DOM
+    }
+
+    @step()
     async connectDevicePromptIsShown() {
         await expect(this.connectDevicePrompt).toBeVisible();
     }
