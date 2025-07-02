94530fcd1d74dad9c695c1fec3626e6ec25bdb20
Seibei Iguchi <2836193+seibei-iguchi@users.noreply.github.com>
2025-07-02 11:39:04 +0200

    fix(suite): auto eject settings persistence

--- packages/suite/src/middlewares/wallet/storageMiddleware.ts
+++ packages/suite/src/middlewares/wallet/storageMiddleware.ts
@@ -251,6 +251,7 @@ const storageMiddleware = (api: MiddlewareAPI<Dispatch, AppState>) => {
                 case SUITE.SET_ADDRESS_DISPLAY_TYPE:
                 case SUITE.SET_DEFAULT_WALLET_LOADING:
                 case SUITE.SET_AUTODETECT:
+                case SUITE.SET_AUTO_EJECT:
                 case SUITE.SET_SIDEBAR_WIDTH:
                 case SUITE.TOGGLE_DEVICE_AUTHENTICITY_CHECK:
                 case SUITE.TOGGLE_FIRMWARE_REVISION_CHECK:
