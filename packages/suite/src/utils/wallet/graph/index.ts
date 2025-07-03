e73342b980a5605e9a80e2743d5fb685debd72b7
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-07-03 14:23:56 +0200

    chore(suite): refactor DashboardGraph from worker to async fn

--- packages/suite/src/utils/wallet/graph/index.ts
+++ packages/suite/src/utils/wallet/graph/index.ts
@@ -1,8 +1,9 @@
 /*
-    The Graph utilities are organized into three distinct files. 
-    This structure is implemented to prevent the '@trezor/connect' from being bundled into the worker file. 
+    TODO reorganize, because worker is no more
+    The Graph utilities are organized into three distinct files.
+    This structure is implemented to prevent the '@trezor/connect' from being bundled into the worker file.
 
-    When working within Suite, you can import from this file. 
+    When working within Suite, you can import from this file.
     However, it's important to note that these utilities should not be imported from the worker (graph.ts file).
 */
 
