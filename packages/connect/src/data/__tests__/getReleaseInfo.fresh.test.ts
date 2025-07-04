be48eb9ac1261e6cdd28db45274a54d6ff1882a0
karliatto <yo@karliatto.com>
2025-07-04 11:13:24 +0200

    test(connect): update tests to firmware release config

---
+++ packages/connect/src/data/__tests__/getReleaseInfo.fresh.test.ts
@@ -0,0 +1,76 @@
+import {
+    ConditionalRelease,
+    DeviceModelInternal,
+    FirmwareType,
+    VersionArray,
+} from '@trezor/device-utils';
+
+import { getReleaseInfo } from '../firmwareInfo';
+
+const { getReleaseData, getDeviceFeatures } = global.JestMocks;
+
+const fixtures = [
+    {
+        desc: 'it should respect bootloader and offer intermediary',
+        features: getDeviceFeatures({
+            bootloader_mode: true,
+            major_version: 1,
+            minor_version: 0,
+            patch_version: 0,
+            internal_model: DeviceModelInternal.T1B1,
+            firmware_present: false,
+        }),
+        release: getReleaseData(),
+        conditions: {
+            environment: { min_suite_version: '25.2.1' },
+            rollout_probability: 100,
+        },
+        isBitcoinOnlyAvailable: true,
+        firmwareType: FirmwareType.Regular,
+        intermediary: {
+            min_firmware_version: [1, 6, 2] as VersionArray,
+            min_bootloader_version: [1, 8, 0] as VersionArray,
+            firmware_revision: '592590cf66a9b62dfeee7e4d2afb6e01005e5b2c',
+            url: '/some/path.bin',
+            version: 1,
+        },
+        result: {
+            releaseConditions: {
+                environment: { min_suite_version: '25.2.1' },
+                rollout_probability: 100,
+                shouldBeOffered: true,
+            },
+            release: getReleaseData(),
+            intermediary: {
+                min_firmware_version: [1, 6, 2] as VersionArray,
+                min_bootloader_version: [1, 8, 0] as VersionArray,
+                firmware_revision: '592590cf66a9b62dfeee7e4d2afb6e01005e5b2c',
+                url: '/some/path.bin',
+                version: 1,
+            },
+            isRequired: false,
+            isNewer: true,
+            translations: getReleaseData().translations,
+        },
+    },
+];
+
+describe('getReleaseInfo() for fresh device', () => {
+    fixtures.forEach(f => {
+        it(f.desc, () => {
+            const result = getReleaseInfo({
+                features: f.features,
+                release: f.release as ConditionalRelease['release'],
+                conditions: f.conditions,
+                intermediary: f.intermediary,
+                firmwareType: f.firmwareType,
+                isBitcoinOnlyAvailable: f.isBitcoinOnlyAvailable,
+            });
+            if (f.result) {
+                expect(result).toMatchObject(f.result);
+            } else if (f.result === null) {
+                expect(result).toBeNull();
+            }
+        });
+    });
+});
