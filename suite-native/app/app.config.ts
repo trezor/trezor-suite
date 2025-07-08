6c84d9391807dc1bccecb40f0eb4721660f24af5
brantalikp <brantalikp@gmail.com>
2025-07-08 11:37:43 +0200

    chore(suite-native): bump runtime version

--- suite-native/app/app.config.ts
+++ suite-native/app/app.config.ts
@@ -188,7 +188,7 @@ export default ({ config }: ConfigContext): ExpoConfig => {
         slug: appSlugs[buildType],
         owner: appOwners[buildType],
         version: suiteNativeVersion,
-        runtimeVersion: '35',
+        runtimeVersion: '36',
         ...(buildType === 'production'
             ? {}
             : {
