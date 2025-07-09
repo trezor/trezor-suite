7c2fb87943ff51c025fa9048d1f87ec54555229f
Martin Varmuza <mvarmuza@gmail.com>
2025-07-09 11:45:01 +0200

    chore(node-utils): prepare getFreePort utility to support multiple free ports

--- packages/node-utils/src/getFreePort.ts
+++ packages/node-utils/src/getFreePort.ts
@@ -1,14 +1,26 @@
 import net from 'net';
 
-export const getFreePort = (): Promise<number> =>
-    new Promise<number>((resolve, reject) => {
-        const server = net.createServer();
-        server.unref();
-        server.on('error', reject);
-        server.listen(0, () => {
-            const { port } = server.address() as net.AddressInfo;
-            server.close(() => {
-                resolve(port);
+export const getFreePort = async (count = 1): Promise<number> => {
+    const servers = Array.from({ length: count }, () => net.createServer());
+
+    const ports = await Promise.all(
+        servers.map(server => {
+            return new Promise<number>((resolve, reject) => {
+                server.unref();
+                server.on('error', reject);
+                server.listen(0, () => {
+                    const { port } = server.address() as net.AddressInfo;
+                    resolve(port);
+                });
             });
-        });
-    });
+        }),
+    );
+
+    await Promise.all(
+        servers.map(server => {
+            return server.close();
+        }),
+    );
+
+    return ports[0];
+};
