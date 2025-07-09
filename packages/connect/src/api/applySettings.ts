7ce1ffb372ce3981e16514508c41e9e1a4a8e917
Szymon Lesisz <sz.lesisz@gmail.com>
2025-07-09 13:24:34 +0200

    feat(connect): applySettings homescreen streaming
    following https://github.com/trezor/trezor-firmware/pull/5015

--- packages/connect/src/api/applySettings.ts
+++ packages/connect/src/api/applySettings.ts
@@ -34,7 +34,33 @@ export default class ApplySettings extends AbstractMethod<'applySettings', PROTO
 
     async run() {
         const cmd = this.device.getCommands();
-        const response = await cmd.typedCall('ApplySettings', 'Success', this.params);
+
+        // https://github.com/trezor/trezor-firmware/pull/5015
+        // homescreen bytes are streamed in smaller data chunks
+        const homescreenBytes = this.params.homescreen
+            ? Buffer.from(this.params.homescreen, 'hex')
+            : undefined;
+        if (this.device.atLeast('2.9.0') && homescreenBytes) {
+            // cannot use both. Failure: Mutually exclusive settings
+            this.params.homescreen = undefined;
+            this.params.homescreen_length = homescreenBytes.length;
+        }
+
+        let response = await cmd.typedCall(
+            'ApplySettings',
+            ['DataChunkRequest', 'Success'],
+            this.params,
+        );
+
+        while (response.type !== 'Success') {
+            const start = response.message.data_offset;
+            const end = start + response.message.data_length;
+            const data_chunk = homescreenBytes?.subarray(start, end).toString('hex') || '';
+
+            response = await cmd.typedCall('DataChunkAck', ['DataChunkRequest', 'Success'], {
+                data_chunk,
+            });
+        }
 
         return response.message;
     }
