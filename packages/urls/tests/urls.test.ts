a6dda2ab917dd89797e6be24dc130144c258ea8c
Martin Varmuza <mvarmuza@gmail.com>
2025-07-10 08:31:27 +0200

    test(urls): adjust skipped urls in test

--- packages/urls/tests/urls.test.ts
+++ packages/urls/tests/urls.test.ts
@@ -14,8 +14,8 @@ const excluded = [
     URLS.HELP_CENTER_DRY_RUN_T3W1_URL,
     URLS.HELP_CENTER_PACKAGING_T3W1_URL,
     URLS.HELP_CENTER_FW_DOWNGRADE_T3W1_URL,
-    URLS.HELP_CENTER_XLM_URL,
     URLS.HELP_CENTER_SOLANA_HELP_URL,
+    URLS.IMAGE_PROXY_API_URL, // returns 'unauthorized'
 ];
 
 describe('Test that all external links are alive', () => {
