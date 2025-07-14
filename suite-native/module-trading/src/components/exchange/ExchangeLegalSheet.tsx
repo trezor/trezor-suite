e5cd0b6444f64be1a3ecd50f41988258afb2e674
Martin Vytick Vytrhlík <vytick@gmail.com>
2025-07-14 11:31:12 +0200

    chore(suite-native): refactored ExchangeLegalSheet

--- suite-native/module-trading/src/components/exchange/ExchangeLegalSheet.tsx
+++ suite-native/module-trading/src/components/exchange/ExchangeLegalSheet.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, memo, useEffect, useMemo } from 'react';
+import { ReactNode, memo, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import { CryptoId } from 'invity-api';
@@ -30,6 +30,27 @@ export type ExchangeLegalSheetProps = TradingExchangeUserConsentProps & {
 
 const CONFIRM_BUTTON_TEST_ID = '@trading/exchange/confirm-button';
 
+const useLegalSheetTradeInfoData = ({
+    provider,
+    send,
+    receive,
+}: Omit<TradingExchangeUserConsentProps, 'isDex'>) => {
+    const { cryptoIdToSymbolAndContractAddress } = useTradingInfo();
+    const providers = useSelector(selectTradingExchangeProviders);
+    const { companyName } = providers?.[provider] ?? {};
+
+    const { coinSymbol: sendCoinSymbol, contractAddress: sendContractAddress } =
+        cryptoIdToSymbolAndContractAddress(send as CryptoId);
+    const sendSymbol = sendCoinSymbol && getDisplaySymbol(sendCoinSymbol, sendContractAddress);
+
+    const { coinSymbol: receiveCoinSymbol, contractAddress: receiveContractAddress } =
+        cryptoIdToSymbolAndContractAddress(receive as CryptoId);
+    const receiveSymbol =
+        receiveCoinSymbol && getDisplaySymbol(receiveCoinSymbol, receiveContractAddress);
+
+    return { sendSymbol, receiveSymbol, companyName };
+};
+
 const Subheader = ({ children }: { children: ReactNode }) => (
     <Text variant="highlight" color="textDefault">
         {children}
@@ -40,6 +61,93 @@ const Info = ({ children }: { children: ReactNode }) => (
     <BulletListItem variant="hint" color="textDefault">
         {children}
     </BulletListItem>
+);
+
+const DexInfo = ({ companyName }: { companyName: string | undefined }) => (
+    <>
+        <VStack>
+            <Subheader>
+                <Translation id="moduleTrading.legalSheet.exchange.dex.security.title" />
+            </Subheader>
+
+            <Info>
+                <Translation
+                    id="moduleTrading.legalSheet.exchange.dex.security.line1"
+                    values={{ companyName }}
+                />
+            </Info>
+            <Info>
+                <Translation id="moduleTrading.legalSheet.exchange.dex.security.line2" />
+            </Info>
+            <Info>
+                <Translation id="moduleTrading.legalSheet.exchange.dex.security.line3" />
+            </Info>
+        </VStack>
+        <VStack>
+            <Subheader>
+                <Translation id="moduleTrading.legalSheet.exchange.dex.verifiedPartners.title" />
+            </Subheader>
+            <Info>
+                <Translation
+                    id="moduleTrading.legalSheet.exchange.dex.verifiedPartners.line1"
+                    values={{ companyName }}
+                />
+            </Info>
+        </VStack>
+        <VStack>
+            <Subheader>
+                <Translation id="moduleTrading.legalSheet.exchange.dex.legal.title" />
+            </Subheader>
+            <Info>
+                <Translation id="moduleTrading.legalSheet.exchange.dex.legal.line1" />
+            </Info>
+            <Info>
+                <Translation id="moduleTrading.legalSheet.exchange.dex.legal.line2" />
+            </Info>
+        </VStack>
+    </>
+);
+
+const CexInfo = ({ companyName }: { companyName: string | undefined }) => (
+    <>
+        <VStack>
+            <Subheader>
+                <Translation id="moduleTrading.legalSheet.exchange.cex.security.title" />
+            </Subheader>
+
+            <Info>
+                <Translation id="moduleTrading.legalSheet.exchange.cex.security.line1" />
+            </Info>
+            <Info>
+                <Translation id="moduleTrading.legalSheet.exchange.cex.security.line2" />
+            </Info>
+            <Info>
+                <Translation id="moduleTrading.legalSheet.exchange.cex.security.line3" />
+            </Info>
+        </VStack>
+        <VStack>
+            <Subheader>
+                <Translation id="moduleTrading.legalSheet.exchange.cex.verifiedPartners.title" />
+            </Subheader>
+            <Info>
+                <Translation
+                    id="moduleTrading.legalSheet.exchange.cex.verifiedPartners.line1"
+                    values={{ companyName }}
+                />
+            </Info>
+        </VStack>
+        <VStack>
+            <Subheader>
+                <Translation id="moduleTrading.legalSheet.exchange.cex.legal.title" />
+            </Subheader>
+            <Info>
+                <Translation id="moduleTrading.legalSheet.exchange.cex.legal.line1" />
+            </Info>
+            <Info>
+                <Translation id="moduleTrading.legalSheet.exchange.cex.legal.line2" />
+            </Info>
+        </VStack>
+    </>
 );
 
 export const ExchangeLegalSheet = memo(
@@ -53,109 +161,11 @@ export const ExchangeLegalSheet = memo(
         isDex,
     }: ExchangeLegalSheetProps) => {
         const { bottomSheetRef, openModal, closeModal } = useBottomSheetModal();
-        const { cryptoIdToSymbolAndContractAddress } = useTradingInfo();
-        const providers = useSelector(selectTradingExchangeProviders);
-        const { companyName } = providers?.[provider] ?? {};
-
-        const { coinSymbol: sendCoinSymbol, contractAddress: sendContractAddress } =
-            cryptoIdToSymbolAndContractAddress(send as CryptoId);
-        const sendSymbol = sendCoinSymbol && getDisplaySymbol(sendCoinSymbol, sendContractAddress);
-
-        const { coinSymbol: receiveCoinSymbol, contractAddress: receiveContractAddress } =
-            cryptoIdToSymbolAndContractAddress(receive as CryptoId);
-        const receiveSymbol =
-            receiveCoinSymbol && getDisplaySymbol(receiveCoinSymbol, receiveContractAddress);
-
-        const translations = useMemo(() => {
-            if (isDex) {
-                return {
-                    title: (
-                        <Translation
-                            id="moduleTrading.legalSheet.exchange.dex.title"
-                            values={{ send: sendSymbol, receive: receiveSymbol, companyName }}
-                        />
-                    ),
-                    security: {
-                        title: (
-                            <Translation id="moduleTrading.legalSheet.exchange.dex.security.title" />
-                        ),
-                        line1: (
-                            <Translation
-                                id="moduleTrading.legalSheet.exchange.dex.security.line1"
-                                values={{ companyName }}
-                            />
-                        ),
-                        line2: (
-                            <Translation id="moduleTrading.legalSheet.exchange.dex.security.line2" />
-                        ),
-                        line3: (
-                            <Translation id="moduleTrading.legalSheet.exchange.dex.security.line3" />
-                        ),
-                    },
-                    verifiedPartners: {
-                        title: (
-                            <Translation id="moduleTrading.legalSheet.exchange.dex.verifiedPartners.title" />
-                        ),
-                        line1: (
-                            <Translation
-                                id="moduleTrading.legalSheet.exchange.dex.verifiedPartners.line1"
-                                values={{ companyName }}
-                            />
-                        ),
-                    },
-                    legal: {
-                        title: (
-                            <Translation id="moduleTrading.legalSheet.exchange.dex.legal.title" />
-                        ),
-                        line1: (
-                            <Translation id="moduleTrading.legalSheet.exchange.dex.legal.line1" />
-                        ),
-                        line2: (
-                            <Translation id="moduleTrading.legalSheet.exchange.dex.legal.line2" />
-                        ),
-                    },
-                };
-            }
-
-            return {
-                title: (
-                    <Translation
-                        id="moduleTrading.legalSheet.exchange.cex.title"
-                        values={{ send: sendSymbol, receive: receiveSymbol, companyName }}
-                    />
-                ),
-                security: {
-                    title: (
-                        <Translation id="moduleTrading.legalSheet.exchange.cex.security.title" />
-                    ),
-                    line1: (
-                        <Translation id="moduleTrading.legalSheet.exchange.cex.security.line1" />
-                    ),
-                    line2: (
-                        <Translation id="moduleTrading.legalSheet.exchange.cex.security.line2" />
-                    ),
-                    line3: (
-                        <Translation id="moduleTrading.legalSheet.exchange.dex.security.line3" />
-                    ),
-                },
-                verifiedPartners: {
-                    title: (
-                        <Translation id="moduleTrading.legalSheet.exchange.cex.verifiedPartners.title" />
-                    ),
-                    line1: (
-                        <Translation
-                            id="moduleTrading.legalSheet.exchange.cex.verifiedPartners.line1"
-                            values={{ companyName }}
-                        />
-                    ),
-                },
-                legal: {
-                    title: <Translation id="moduleTrading.legalSheet.exchange.cex.legal.title" />,
-                    line1: <Translation id="moduleTrading.legalSheet.exchange.cex.legal.line1" />,
-                    line2: <Translation id="moduleTrading.legalSheet.exchange.cex.legal.line2" />,
-                },
-            };
-        }, [isDex, sendSymbol, receiveSymbol, companyName]);
+        const { sendSymbol, receiveSymbol, companyName } = useLegalSheetTradeInfoData({
+            provider,
+            send,
+            receive,
+        });
 
         useEffect(() => {
             if (isVisible) {
@@ -170,26 +180,25 @@ export const ExchangeLegalSheet = memo(
         return (
             <BottomSheetModal
                 ref={bottomSheetRef}
-                title={translations.title}
+                title={
+                    <Translation
+                        id={
+                            isDex
+                                ? 'moduleTrading.legalSheet.exchange.dex.title'
+                                : 'moduleTrading.legalSheet.exchange.cex.title'
+                        }
+                        values={{ send: sendSymbol, receive: receiveSymbol, companyName }}
+                    />
+                }
                 onDismiss={onDismiss}
                 isCloseDisplayed
             >
                 <VStack spacing="sp12" paddingHorizontal="sp12">
-                    <VStack>
-                        <Subheader>{translations.security.title}</Subheader>
-                        <Info>{translations.security.line1}</Info>
-                        <Info>{translations.security.line2}</Info>
-                        <Info>{translations.security.line3}</Info>
-                    </VStack>
-                    <VStack>
-                        <Subheader>{translations.verifiedPartners.title}</Subheader>
-                        <Info>{translations.verifiedPartners.line1}</Info>
-                    </VStack>
-                    <VStack>
-                        <Subheader>{translations.legal.title}</Subheader>
-                        <Info>{translations.legal.line1}</Info>
-                        <Info>{translations.legal.line2}</Info>
-                    </VStack>
+                    {isDex ? (
+                        <DexInfo companyName={companyName} />
+                    ) : (
+                        <CexInfo companyName={companyName} />
+                    )}
                     {
                         // Keep this condition here to simplify testing.
                         // Otherwise, due to the way the Gorhom bottom sheet is mocked,
