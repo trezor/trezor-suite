621943b1e15b03e13c7e48e897d6eb69fa02fc39
Szymon Lesisz <sz.lesisz@gmail.com>
2025-07-14 16:58:52 +0200

    feat(suite-desktop): init `BluetoothTransport` on TrezorConnect init and setTransports

--- packages/suite-desktop-core/src/modules/trezor-connect.ts
+++ packages/suite-desktop-core/src/modules/trezor-connect.ts
@@ -1,9 +1,10 @@
 import { ipcMain } from 'electron';
 
-import TrezorConnect, { LocalFirmwares, UI, UI_EVENT } from '@trezor/connect';
+import TrezorConnect, { ConnectSettings, LocalFirmwares, UI, UI_EVENT } from '@trezor/connect';
 import { IpcProxyHandlerOptions, createIpcProxyHandler } from '@trezor/ipc-proxy';
 import { parseElectrumUrl } from '@trezor/utils';
 
+import { bluetoothModuleState } from './bluetooth';
 import { getStoredFirmwares } from './firmware';
 import { APP_NAME } from '../libs/constants';
 import { getComputerName } from '../libs/info';
@@ -42,6 +43,21 @@ const emitOnSetCustomBackendToMainThreadToAllowDomains = ({
     }
 };
 
+// override TrezorConnect.init and TrezorConnect.setTransports param
+// add BluetoothTransport if bluetooth module is enabled
+const getTransportsParam = (
+    transports?: ConnectSettings['transports'],
+): ConnectSettings['transports'] => {
+    const bluetooth = bluetoothModuleState.getTransport();
+    if (!bluetooth) return transports;
+
+    if (transports && transports.length > 0) {
+        return [...transports, bluetooth];
+    }
+
+    return [bluetooth, 'BridgeTransport'];
+};
+
 export const initBackground: ModuleInitBackground = ({ mainThreadEmitter, store }) => {
     const { logger } = global;
     logger.info(SERVICE_NAME, `Starting service`);
@@ -72,6 +88,7 @@ export const initBackground: ModuleInitBackground = ({ mainThreadEmitter, store 
                     if (localFirmwares.success) {
                         settings.localFirmwares = localFirmwares.payload;
                     }
+                    settings.transports = getTransportsParam(settings.transports);
 
                     const response = await TrezorConnect.init(settings);
                     await setProxy();
@@ -90,6 +107,10 @@ export const initBackground: ModuleInitBackground = ({ mainThreadEmitter, store 
                     powerSaveBlocker.stopBlockingPowerSave();
 
                     return response;
+                }
+
+                if (method === 'setTransports') {
+                    params[0].transports = getTransportsParam(params[0].transports);
                 }
 
                 return (TrezorConnect[method] as any)(...params);
