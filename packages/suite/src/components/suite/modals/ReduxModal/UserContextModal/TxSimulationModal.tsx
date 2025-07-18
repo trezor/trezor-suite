24680702efb299a2985d695b734e1db7904edf43
Tomas Martykan <t.martykan@gmail.com>
2025-07-18 10:19:20 +0200

    fix(suite): alignment in tx simulation modal

--- packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/TxSimulationModal.tsx
+++ packages/suite/src/components/suite/modals/ReduxModal/UserContextModal/TxSimulationModal.tsx
@@ -543,27 +543,31 @@ export const TxSimulationModal = () => {
                         />
                     )}
 
-                    <Text variant="tertiary" margin={{ left: spacings.xs }}>
-                        <Translation
-                            id="TR_SIMULATION_POWERED_BY"
-                            values={{ provider: <Link href="https://blockaid.io">Blockaid</Link> }}
-                        />
-                    </Text>
+                    <Column margin={{ left: spacings.xs }} gap={spacings.md}>
+                        <Text variant="tertiary">
+                            <Translation
+                                id="TR_SIMULATION_POWERED_BY"
+                                values={{
+                                    provider: <Link href="https://blockaid.io">Blockaid</Link>,
+                                }}
+                            />
+                        </Text>
 
-                    {isSigningTransaction && account && (
-                        <Fees
-                            account={account}
-                            feeInfo={feeInfo}
-                            control={control}
-                            register={register}
-                            setValue={setValue}
-                            getValues={getValues}
-                            errors={errors}
-                            isDirty={isDirty}
-                            changeFeeLevel={changeFeeLevel}
-                            trigger={trigger}
-                        />
-                    )}
+                        {isSigningTransaction && account && (
+                            <Fees
+                                account={account}
+                                feeInfo={feeInfo}
+                                control={control}
+                                register={register}
+                                setValue={setValue}
+                                getValues={getValues}
+                                errors={errors}
+                                isDirty={isDirty}
+                                changeFeeLevel={changeFeeLevel}
+                                trigger={trigger}
+                            />
+                        )}
+                    </Column>
                 </Column>
             </Modal.ModalBase>
         </Modal.Backdrop>
