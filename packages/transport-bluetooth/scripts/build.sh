86af47c48c817afe60aede3314543b20c7f63e49
Szymon Lesisz <sz.lesisz@gmail.com>
2025-07-21 18:59:14 +0200

    fix(transport-bluetooth): CI build cargo edition 2024

--- packages/transport-bluetooth/scripts/build.sh
+++ packages/transport-bluetooth/scripts/build.sh
@@ -2,7 +2,7 @@
 set -e
 
 # This has to be built using x86 docker image arm image is not working. For building on arm mac use --platform linux/amd64 for the build command.
-docker build . -f ./scripts/Dockerfile -t trezor-bluetooth-image .
+docker build . -f ./scripts/Dockerfile -t trezor-bluetooth-image
 docker create --name trezor-bluetooth-container trezor-bluetooth-image
 docker cp trezor-bluetooth-container:/output/. ../suite-data/files/bin/bluetooth
 docker rm trezor-bluetooth-container
