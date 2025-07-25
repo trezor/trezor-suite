67279a18bdd4787d39b2b3e8e3eba9d9b4275b51
Matěj Husák <matej.husak@icloud.com>
2025-07-25 12:13:50 +0200

    chore(suite): extended db-migration with language test

--- packages/suite/src/storage/migrations/versions/25.8.0.ts
+++ packages/suite/src/storage/migrations/versions/25.8.0.ts
@@ -1,7 +1,7 @@
 import { createMigration } from '@suite/idb-migration-utils';
+import { typedObjectKeys } from '@trezor/utils';
 
 import { LANGUAGES } from 'src/config/suite';
-import type { Locale } from 'src/config/suite/languages';
 import { SuiteDBSchema } from 'src/storage/definitions';
 
 import { updateAll } from '../utils';
@@ -9,27 +9,27 @@ import { updateAll } from '../utils';
 export default createMigration<SuiteDBSchema>('25.8.0', async (db, tx) => {
     if (db.objectStoreNames.contains('suiteSettings')) {
         const store = tx.objectStore('suiteSettings');
-        const settings = await store.get('suite');
+        const suiteSettings = await store.get('suite');
 
-        if (settings && settings.settings) {
+        if (suiteSettings && suiteSettings.settings) {
             if (
-                typeof settings.settings.language === 'string' &&
-                settings.settings.language.length === 2
+                typeof suiteSettings.settings.language === 'string' &&
+                suiteSettings.settings.language.length === 2
             ) {
-                const languageKeys = Object.keys(LANGUAGES) as Locale[];
+                const languageKeys = typedObjectKeys(LANGUAGES);
                 for (const key of languageKeys) {
-                    if (key.startsWith(settings.settings.language)) {
-                        settings.settings.language = key;
+                    if (key.startsWith(suiteSettings.settings.language)) {
+                        suiteSettings.settings.language = key;
                         break;
                     }
                 }
             }
 
-            await store.put(settings, 'suite');
+            await store.put(suiteSettings, 'suite');
         }
     }
 
-    await updateAll(transaction, 'devices', device => {
+    await updateAll(tx, 'devices', device => {
         if (device.authenticityChecks) return;
         device.authenticityChecks = {
             firmwareRevision: null,
