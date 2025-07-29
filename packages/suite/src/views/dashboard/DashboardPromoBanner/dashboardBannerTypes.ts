bc51b1061536dd57395d6bdf24e5ff2e0eb5bd50
Jan Václavík <jvaclavik@gmail.com>
2025-07-29 12:48:44 +0200

    fix(suite): Fix accidental replace

--- packages/suite/src/views/dashboard/DashboardPromoBanner/dashboardBannerTypes.ts
+++ packages/suite/src/views/dashboard/DashboardPromoBanner/dashboardBannerTypes.ts
@@ -1,2 +1,8 @@
+import { isArrayMember } from '@trezor/utils';
+
 export const dashboardBannerTypes = ['tex'] as const;
 export type DashboardBannerType = (typeof dashboardBannerTypes)[number] | null;
+
+export const isDashboardBannerType = (bannerType: unknown): bannerType is DashboardBannerType =>
+    bannerType === null ||
+    (typeof bannerType === 'string' && isArrayMember(bannerType, dashboardBannerTypes));
