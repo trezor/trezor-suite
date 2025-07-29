7b59ed45f28ba949ff85c1d03c7c97f037a34f98
Jaroslav Hrách <me@jaroslavhrach.cz>
2025-07-29 17:51:27 +0200

    refactor(suite): revert send max switch in send form

---
+++ packages/suite/src/views/wallet/send/Outputs/Amount/SendMaxSwitch.tsx
@@ -0,0 +1,24 @@
+import { Switch } from '@trezor/components';
+
+import { Translation } from 'src/components/suite';
+
+type SendMaxSwitchProps = {
+    isSetMaxActive: boolean;
+    'data-testid'?: string;
+    onChange: () => void;
+};
+
+export const SendMaxSwitch = ({
+    isSetMaxActive,
+    'data-testid': dataTest,
+    onChange,
+}: SendMaxSwitchProps) => (
+    <Switch
+        labelPosition="start"
+        isChecked={isSetMaxActive}
+        data-testid={dataTest}
+        size="small"
+        onChange={onChange}
+        label={<Translation id="AMOUNT_SEND_MAX" />}
+    />
+);
