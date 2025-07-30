ac87335076126bec3758a1667f95e8bfc4add1c2
Tomas Martykan <t.martykan@gmail.com>
2025-07-30 16:56:20 +0200

    feat(suite-native): add walletconnect deeplink

--- suite-common/walletconnect/src/walletConnectConstants.ts
+++ suite-common/walletconnect/src/walletConnectConstants.ts
@@ -1,3 +1,5 @@
+import { isNative } from '@trezor/env-utils';
+
 export const WALLETCONNECT_MODULE = '@suite/walletconnect';
 
 export const PROJECT_ID = '203549d0480d0f24d994780f34889b03';
@@ -6,5 +8,11 @@ export const WALLETCONNECT_METADATA = {
     name: 'Trezor Suite',
     description: 'Manage your Trezor device',
     url: 'https://suite.trezor.io',
-    icons: ['https://trezor.io/favicon/apple-touch-icon.png'],
+    icons: ['https://trezor.io/images/suite/appIcon.png'],
+    redirect: isNative()
+        ? {
+              native: 'trezorsuitelite://walletconnect/',
+              linkMode: true,
+          }
+        : {},
 };
