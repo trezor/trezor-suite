32340be48e7f98d84525ebdb93098ecad3ec7480
Ondrej Hajek <ondrej.hajek@satoshilabs.com>
2025-07-30 18:55:37 +0200

    refactor(e2e): Added GH reporter base class

--- packages/e2e-utils/src/index.ts
+++ packages/e2e-utils/src/index.ts
@@ -2,6 +2,7 @@ export { BackendWebsocketServerMock } from './mocks/backendServer';
 export { TrezorBridgeMock } from './mocks/bridge';
 export { DropboxMock } from './mocks/dropbox';
 export { GoogleMock } from './mocks/google';
+export { GitHubReporterBase, InitializationState } from './githubReporter/gitHubReporterBase';
 export { GitHubProject } from './githubReporter/gitHubProject';
 export { IssueRequests } from './githubReporter/issueRequests';
 export * from './githubReporter/types';
