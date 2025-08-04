70919b7f69f7286789703ada07ebf2583b365273
juriczech <bohdan.juricek@gmail.com>
2025-08-04 11:25:28 +0200

    feat(suite-native): default navigation animation

--- suite-native/navigation/src/config.ts
+++ suite-native/navigation/src/config.ts
@@ -3,5 +3,4 @@ import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
 export const stackNavigationOptionsConfig: NativeStackNavigationOptions = {
     headerShown: false,
     gestureEnabled: true,
-    animation: 'slide_from_right',
 } as const;
