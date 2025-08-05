2ab9d60170d01721d510c9ee455c2928e7c06dd9
Vojtěch Tranta <vojta.tranta@gmail.com>
2025-08-05 10:23:49 +0200

    fix(suite): e2e test

--- packages/suite/src/components/suite/layouts/SuiteLayout/PageHeader/PageNames/AccountName/AccountSubpageName.tsx
+++ packages/suite/src/components/suite/layouts/SuiteLayout/PageHeader/PageNames/AccountName/AccountSubpageName.tsx
@@ -16,6 +16,7 @@ const Container = styled.div`
     gap: ${spacingsPx.md};
 `;
 
+// NOTE: change for E2E
 interface AccountSubpageNameProps {
     selectedAccount: Account;
     backRoute?: Route['name'];
