37b2477c23b011bfb2a01c78e6c3b081b8d1f89f
brantalikp <brantalikp@gmail.com>
2025-08-06 06:51:42 +0200

    fix(suite-native): fix buy flow

--- suite-native/app/e2e/fixtures/walletWithBtcAcc.ts
+++ suite-native/app/e2e/fixtures/walletWithBtcAcc.ts
@@ -1,3 +1,5 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-nocheck — Although some fields don't satisfy the expected types, this is the bare minimum required for the trading buy flow to render and display the address correctly.
 import { PreloadedState } from '@suite-native/state';
 
 export const btcWalletPreloaded: PreloadedState = {
@@ -6,6 +8,7 @@ export const btcWalletPreloaded: PreloadedState = {
             {
                 accountLabel: 'BTC SegWit',
                 accountType: 'normal',
+                addresses: [],
                 availableBalance: '0',
                 balance: '0',
                 descriptor:
@@ -13,10 +16,14 @@ export const btcWalletPreloaded: PreloadedState = {
                 deviceState: 'state@hiddenDeviceWithImportedAccounts:1',
                 empty: false,
                 formattedBalance: '0',
+                history: [],
                 imported: true,
                 index: 0,
                 key: 'zpub6rszzdAK6RuafeRwyN8z1cgWcXCuKbLmjjfnrW4fWKtcoXQ8787214pNJjnBG5UATyghuNzjn6Lfp5k5xymrLFJnCy46bMYJPyZsbpFGagT-btc-state@hiddenDeviceWithImportedAccounts:1',
+                metadata: [],
                 networkType: 'bitcoin',
+                page: [],
+                path: '',
                 symbol: 'btc',
                 tokens: [],
                 ts: 1753692438941,
