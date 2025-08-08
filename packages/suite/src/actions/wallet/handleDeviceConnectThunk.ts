2fb7be21bdf927d42f933383d110f7e9efeb8031
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-08-08 14:27:33 +0200

    fix(suite): don't show wallet-switcher when connecting physical over a view-only device

--- packages/suite/src/actions/wallet/handleDeviceConnectThunk.ts
+++ packages/suite/src/actions/wallet/handleDeviceConnectThunk.ts
@@ -8,14 +8,23 @@ import { Device } from '@trezor/connect';
 
 import { openSwitchDeviceDialog } from './addWalletThunk';
 
+/**
+ * Triggered by @trezor/connect events DEVICE.CONNECT or DEVICE.CONNECT_UNACQUIRED.
+ * @param device physical device that was just now connected (raw Connect device, not yet enriched to TrezorDevice type)
+ */
 export const handleDeviceConnect = createThunk(
     `${DEVICE_MODULE_PREFIX}/handleDeviceConnect`,
     (device: Device, { dispatch, getState }) => {
         const selectedDevice = selectSelectedDevice(getState());
 
-        // Select automatically when it is the first known device,
-        // or when we connected physical device corresponding to a selected remembered wallet.
-        const shouldSelectDevice = !selectedDevice || device.id === selectedDevice.id;
+        const shouldSelectDevice =
+            // Select automatically when it is the first encountered device,
+            !selectedDevice ||
+            // or when we connected physical device corresponding to a selected remembered wallet,
+            device.id === selectedDevice.id ||
+            // or when currently selected device is remembered, and a different device is physically connected.
+            !selectedDevice.connected;
+
         if (shouldSelectDevice) {
             dispatch(selectDeviceThunk({ device }));
         } else {
