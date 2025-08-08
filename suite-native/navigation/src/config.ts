4d5055e620dd8810b4ad8bef3f47431387636f00
yanas <yanas@yanas.cz>
2025-08-08 14:44:25 +0200

    fix(suite-native): use ios_from_right as default navigation animation

--- suite-native/navigation/src/config.ts
+++ suite-native/navigation/src/config.ts
@@ -3,4 +3,5 @@ import { NativeStackNavigationOptions } from '@react-navigation/native-stack';
 export const stackNavigationOptionsConfig: NativeStackNavigationOptions = {
     headerShown: false,
     gestureEnabled: true,
+    animation: 'ios_from_right',
 } as const;
