9b4025ad844ed22a93e144e88790919375b5b607
Martin Vytick Vytrhlík <vytick@gmail.com>
2025-08-14 21:19:56 +0200

    feat(suite-native): fetch trade status update while on trade tab
    (cherry picked from commit ee8c9bca7b2872f8a59be282d29474e8db9f109d)

---
+++ suite-native/module-trading/src/hooks/general/useWatchAllTrades.ts
@@ -0,0 +1,58 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import {
+    TradingRootStateWithDeviceAndAccounts,
+    selectDeviceTradingTrades,
+} from '@suite-common/trading';
+
+import { useAllTradesReloadTimer } from './useAllTradesReloadTimer';
+import { useTransactionStateChangeAnalyticsReporting } from './useTransactionStateChangeAnalyticsReporting';
+
+export const useWatchAllTrades = () => {
+    const deviceTrades = useSelector((state: TradingRootStateWithDeviceAndAccounts) =>
+        selectDeviceTradingTrades(state),
+    );
+
+    // Use the analytics reporting hook
+    useTransactionStateChangeAnalyticsReporting(deviceTrades);
+
+    // Use the all trades reload timer hook
+    const {
+        refreshAllTrades,
+        shouldReload,
+        hasFetchedInitialTrades,
+        isFetching,
+        setIsFetching,
+        tradesToWatch,
+    } = useAllTradesReloadTimer();
+
+    // Refresh all relevant trades when they need refreshing
+    useEffect(() => {
+        if ((!hasFetchedInitialTrades || shouldReload) && !isFetching) {
+            setIsFetching(true);
+            const refreshTrades = async () => {
+                await refreshAllTrades();
+                setIsFetching(false);
+            };
+            refreshTrades();
+        }
+    }, [
+        tradesToWatch,
+        shouldReload,
+        hasFetchedInitialTrades,
+        refreshAllTrades,
+        isFetching,
+        setIsFetching,
+    ]);
+
+    return {
+        allTrades: deviceTrades,
+        tradesToWatch,
+
+        totalTrades: deviceTrades.length,
+        tradesWatching: tradesToWatch.length,
+        // Manual refresh function
+        manualRefresh: refreshAllTrades,
+    };
+};
