1fe9a75f6ebcaaf24d5664b6d896ecde6fccf5dd
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-08-18 09:07:53 +0200

    chore(connect): suppress warning for recoverDevice when uninitialized

--- packages/connect/src/device/DeviceCurrentSession.ts
+++ packages/connect/src/device/DeviceCurrentSession.ts
@@ -40,6 +40,8 @@ const allowedCallsBeforeInitialize: Messages.MessageKey[] = [
     'RebootToBootloader',
     'FirmwareErase',
     'FirmwareUpload',
+    // Wallet recovery may also be called before Initialize
+    'RecoveryDevice',
     // There are other, which are allowed by firmware (ApplySettings,...) but we do not use them this way in connect.
 ];
 
