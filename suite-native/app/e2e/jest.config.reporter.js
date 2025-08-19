e1688326cefe0ea49b78ccd9e5c69a01aca4dceb
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2025-08-19 15:28:48 +0200

    fix(e2e-report): switch native reporter from detox to jest only

---
+++ suite-native/app/e2e/jest.config.reporter.js
@@ -0,0 +1,22 @@
+const dotenv = require('dotenv');
+const path = require('path');
+
+const nativeJestConfig = require('../../../jest.config.native');
+
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+module.exports = {
+    rootDir: '..',
+    preset: 'jest-expo',
+    testEnvironment: 'node',
+    transformIgnorePatterns: [
+        'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|@shopify/react-native-skia)',
+    ],
+    transform: {
+        ...nativeJestConfig.transform,
+        '^.+\\.ts$': ['ts-jest', { isolatedModules: true }],
+    },
+    verbose: true,
+    reporters: ['<rootDir>/e2e/support/reporter/index.js'],
+    testMatch: ['<rootDir>/e2e/tests/manual/**/*.test.ts'],
+};
