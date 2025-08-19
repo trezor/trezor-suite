e3ae239ab56fae94b6285826fda29bad6743e1b5
Szymon Lesisz <sz.lesisz@gmail.com>
2025-08-19 18:51:32 +0200

    test(connect): THP pairing with autoconnect credentials

--- packages/connect/e2e/tests/device/thpPairing.test.ts
+++ packages/connect/e2e/tests/device/thpPairing.test.ts
@@ -28,8 +28,8 @@ describe('THP pairing', () => {
 
         return new Promise<Device>((resolve, reject) => {
             const onDeviceConnected = (device: Device) => {
-                TrezorConnect.removeAllListeners('device-connect');
-                TrezorConnect.removeAllListeners('device-connect_unacquired');
+                TrezorConnect.off('device-connect', onDeviceConnected);
+                TrezorConnect.off('device-connect_unacquired', onDeviceConnected);
                 if (device.type === 'unreadable') {
                     reject(new Error('Device unreadable'));
                 } else {
@@ -84,5 +84,89 @@ describe('THP pairing', () => {
 
         // TODO: expect(device.type).toEqual('unreadable');
         expect(device.type).toEqual('unacquired');
+    });
+
+    it('ThpPairing with credentials (autoconnect: false)', async () => {
+        const device = await waitForDevice({
+            pairingMethods: ['CodeEntry'],
+            knownCredentials: [
+                {
+                    trezor_static_public_key:
+                        'f60b84cdb80a2139f80489c811dc129937a4f4f75ca7710c7570c5085f1ffe68',
+                    credential:
+                        '0a1c0a0974657374733a65326510001a0d5472657a6f72436f6e6e656374122098822d64a482198c9654295ee690f7bf0e8ecf1aac50473b0e805c5614a3bef9',
+                    autoconnect: false,
+                },
+            ],
+        });
+
+        const pairingSpy = typeof jest !== 'undefined' ? jest.fn() : jasmine.createSpy('pairing');
+        TrezorConnect.on('ui-request_thp_pairing', pairingSpy);
+
+        const address = await TrezorConnect.getAddress({
+            device,
+            path: "m/44'/0'/0'/1/1",
+            showOnTrezor: true,
+        });
+        expect(address).toMatchObject({ success: true });
+        expect(pairingSpy).toHaveBeenCalledTimes(0);
+    });
+
+    it('ThpPairing with credentials (autoconnect: true)', async () => {
+        const device = await waitForDevice({ pairingMethods: ['CodeEntry'] });
+
+        const credentialsSpy =
+            typeof jest !== 'undefined' ? jest.fn() : jasmine.createSpy('credentials');
+        TrezorConnect.on('device-thp_credentials_changed', credentialsSpy);
+
+        TrezorConnect.on('ui-request_thp_pairing', async ({ nfcData }) => {
+            const state = await controller.getPairingInfo(device.thp!.channel, nfcData);
+            TrezorConnect.uiResponse({
+                type: 'ui-receive_thp_pairing_tag',
+                payload: { source: 'code-entry', tag: state.code_entry_code },
+            });
+        });
+
+        // start pairing
+        await TrezorConnect.getFeatures({ device });
+        // autoconnect: false credentials obtained
+        expect(credentialsSpy).toHaveBeenCalledTimes(1);
+
+        // generate autoconnect credentials
+        await TrezorConnect.thpGetCredentials({ device });
+        // autoconnect: true credentials obtained
+        expect(credentialsSpy).toHaveBeenCalledTimes(2);
+
+        // expect no pairing or button request from now on
+        TrezorConnect.removeAllListeners('ui-request_thp_pairing');
+        TrezorConnect.removeAllListeners('ui-button');
+
+        // restart the device
+        await controller.stopEmu();
+        await new Promise<void>(resolve => {
+            const onDeviceDisconnected = () => {
+                TrezorConnect.off('device-disconnect', onDeviceDisconnected);
+                resolve();
+            };
+            TrezorConnect.on('device-disconnect', onDeviceDisconnected);
+        });
+        await controller.startEmu({
+            model: 'T3W1',
+            version: '2-main',
+        });
+        await new Promise<void>(resolve => {
+            const onDeviceConnected = () => {
+                TrezorConnect.off('device-connect', onDeviceConnected);
+                resolve();
+            };
+            TrezorConnect.on('device-connect', onDeviceConnected);
+        });
+
+        const address = await TrezorConnect.getAddress({
+            device,
+            path: "m/44'/0'/0'/1/1",
+            showOnTrezor: false,
+        });
+        expect(address).toMatchObject({ success: true });
     });
 });
