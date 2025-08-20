b9f391d58fe4cb13a5e5b837f79146ef2b6e7cf8
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-08-20 16:00:53 +0200

    fix(suite): missing device image on ThpConnectionModal during acquisition

--- packages/suite/src/components/suite/DeviceConfirmImage.tsx
+++ packages/suite/src/components/suite/DeviceConfirmImage.tsx
@@ -1,17 +1,14 @@
 import { getDeviceInternalModel } from '@suite-common/suite-utils';
 import { Image, ImageKey, ImageProps } from '@trezor/components';
 import { Device } from '@trezor/connect';
+import { DeviceModelInternal } from '@trezor/device-utils';
 
 type DeviceConfirmImageProps = Omit<ImageProps, 'image'> & {
     device: Pick<Device, 'features' | 'thp'>;
 };
 
 export const DeviceConfirmImage = ({ device }: DeviceConfirmImageProps) => {
-    const deviceModelInternal = getDeviceInternalModel(device);
-
-    if (!deviceModelInternal) {
-        return null;
-    }
+    const deviceModelInternal = getDeviceInternalModel(device) ?? DeviceModelInternal.UNKNOWN;
 
     const imgName: ImageKey = `DEVICE_CONFIRM_TREZOR_${deviceModelInternal}`;
 
