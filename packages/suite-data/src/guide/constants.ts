e88aed2ff7bc22e6c46b1dcf7b991b97c8853319
Daniel Skýpala <daniel@honza.info>
2025-08-21 14:50:14 +0200

    chore(suite-data): update guide revision

--- packages/suite-data/src/guide/constants.ts
+++ packages/suite-data/src/guide/constants.ts
@@ -1,7 +1,7 @@
 import { join, resolve } from 'path';
 
 export const GITBOOK_SOURCE = 'https://github.com/trezor/trezor-suite-guide.git';
-export const GITBOOK_REVISION = '8677add56d5524b1eef325c631b9c61a965d9c01';
+export const GITBOOK_REVISION = '68762d92980c192001a703b11a1bd4f4046ab360';
 export const TMP = join(resolve(__dirname, '../..'), 'tmp', 'guide');
 // Path to the GitBook assets. Relative to TMP.
 export const GITBOOK_ASSETS_DIR_PREFIX = '.gitbook';
