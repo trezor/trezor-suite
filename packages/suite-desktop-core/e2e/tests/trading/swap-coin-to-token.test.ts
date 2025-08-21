e503e69df9a13917c841a699b171619a166d9b03
Matus Balascak <balascakmatus@gmail.com>
2025-08-21 09:19:40 +0200

    chore(suite-desktop-core): update swap tests with correct toast text

--- packages/suite-desktop-core/e2e/tests/trading/swap-coin-to-token.test.ts
+++ packages/suite-desktop-core/e2e/tests/trading/swap-coin-to-token.test.ts
@@ -17,7 +17,7 @@ const formattedSendAmount = `${localizeNumber(sendAmount)} SOL`;
 const formattedReceiveAmount = `${localizeNumber(swapQuotesSolanaUSDC[0].receiveStringAmount)} USDC`;
 const { sendAddress, receiveAddress, receive: usdcMint } = swapTradeSolanaUSDC;
 const formattedSendAddress = formatAddress(sendAddress);
-const toastText = `${formattedSendAmount} sent from Solana #1`;
+const toastText = `Swap transaction of ${formattedSendAmount} (Solana #1) to ${formattedReceiveAmount} (Ethereum #1) was broadcasted`;
 
 test.describe('Trading - Swap coin to token', { tag: ['@group=trading', '@webOnly'] }, () => {
     test.use({ emulatorSetupConf: { mnemonic: 'mnemonic_academic', passphrase_protection: true } });
@@ -90,7 +90,7 @@ test.describe('Trading - Swap coin to token', { tag: ['@group=trading', '@webOnl
         // Thanks to our mocked responses, the crypto is actually not send.
         await test.step('Send crypto to provider', async () => {
             await devicePrompt.sendButton.click();
-            await expect(page.getByTestId('@toast/tx-sent')).toContainText(toastText);
+            await expect(page.getByTestId('@toast/tx-exchange')).toContainText(toastText);
             await expect(tradingPage.transactionDetailStatus).toHaveText(
                 messages['TR_EXCHANGE_DETAIL_SUCCESS_TITLE'].defaultMessage,
             );
