5b2cc4c3c0d1ec6e3008720eeae149909a0f18cd
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-08-21 16:45:48 +0200

    feat(suite-native): Mobile Sell: sell form input interactions

--- suite-native/module-trading/src/hooks/sell/useSellForm.tsx
+++ suite-native/module-trading/src/hooks/sell/useSellForm.tsx
@@ -1,10 +1,14 @@
-import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { CryptoId, FiatCurrencyCode } from 'invity-api';
 
 import { useFormatters } from '@suite-common/formatters';
+import { EventType, analytics } from '@suite-native/analytics';
 import { useForm } from '@suite-native/forms';
 import { useTranslate } from '@suite-native/intl';
 
+import { sellActions } from '../../reducers';
 import {
     selectSellAmountLimits,
     selectSellFormDefaultValues,
@@ -20,6 +24,74 @@ const useSendAccountChangeEffect = ({ setValue }: SellFormType) => {
     useEffect(() => {
         setValue('sendAccount', sendAccount);
     }, [sendAccount, setValue]);
+};
+
+const useAmountAndCurrencyFieldsChangeEffect = ({ setValue, getValues, watch }: SellFormType) => {
+    const dispatch = useDispatch();
+    const prevCryptoId = useRef<CryptoId | undefined>(undefined);
+    const prevFiatCurrency = useRef<FiatCurrencyCode | undefined>(getValues('fiatCurrency'));
+
+    useEffect(() => {
+        const { unsubscribe } = watch(
+            ({ fiatCurrency, sendAsset, amountInCrypto, focusedValue }, { name, type }) => {
+                switch (name) {
+                    case 'fiatStringAmount':
+                        if (type === 'change' && focusedValue === 'fiatStringAmount') {
+                            setValue('cryptoStringAmount', undefined, { shouldValidate: true });
+                            if (amountInCrypto) {
+                                setValue('amountInCrypto', false);
+                            }
+                        }
+                        break;
+
+                    case 'cryptoStringAmount':
+                        if (type === 'change' && focusedValue === 'cryptoStringAmount') {
+                            setValue('fiatStringAmount', undefined, { shouldValidate: true });
+                            if (!amountInCrypto) {
+                                setValue('amountInCrypto', true);
+                            }
+                        }
+                        break;
+
+                    case 'sendAsset':
+                        if (sendAsset?.cryptoId !== prevCryptoId.current) {
+                            analytics.report({
+                                type: EventType.TradingParameterChanged,
+                                payload: {
+                                    type: 'sell',
+                                    parameter: 'cryptoFrom',
+                                },
+                            });
+                            prevCryptoId.current = sendAsset?.cryptoId as CryptoId | undefined;
+                            setValue('cryptoStringAmount', undefined, { shouldValidate: true });
+                            dispatch(sellActions.sendAssetChanged());
+                        }
+                        break;
+
+                    case 'fiatCurrency':
+                        if (fiatCurrency !== prevFiatCurrency.current) {
+                            analytics.report({
+                                type: EventType.TradingParameterChanged,
+                                payload: {
+                                    type: 'sell',
+                                    parameter: 'fiat',
+                                },
+                            });
+                            prevFiatCurrency.current = fiatCurrency;
+                            setValue('fiatStringAmount', undefined, { shouldValidate: true });
+                            dispatch(sellActions.fiatCurrencyChanged());
+                        }
+                        break;
+
+                    default:
+                        // do nothing
+                        break;
+                }
+            },
+        );
+
+        return unsubscribe;
+    }, [setValue, watch, dispatch]);
 };
 
 export const useSellForm = (): SellFormType => {
@@ -44,6 +116,7 @@ export const useSellForm = (): SellFormType => {
     });
 
     useSendAccountChangeEffect(form);
+    useAmountAndCurrencyFieldsChangeEffect(form);
 
     return form;
 };
