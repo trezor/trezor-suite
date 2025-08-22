edd6d4e67f49367e725b088ef3af6562e72f6835
yanas <yanas@yanas.cz>
2025-08-22 14:22:27 +0200

    feat(suite-native): prepare T3W1 device color images s

---
+++ suite-native/bluetooth/src/components/DeviceColorImage.tsx
@@ -0,0 +1,15 @@
+import { Image } from '@suite-native/atoms';
+
+const deviceImageMap: Record<number, string> = {
+    1: require('../assets/t3w1-color-1.webp'),
+    2: require('../assets/t3w1-color-2.webp'),
+    3: require('../assets/t3w1-color-3.webp'),
+};
+
+type DeviceColorImageProps = {
+    color: number;
+};
+
+export const DeviceColorImage = ({ color }: DeviceColorImageProps) => (
+    <Image source={deviceImageMap[color] ?? deviceImageMap[1]} width={68} height={112} />
+);
