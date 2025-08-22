5f50d0d6a68c29d478b9bf9e2037c78464d6ef6c
Matěj Husák <matej.husak@icloud.com>
2025-08-22 16:30:17 +0200

    feat(suite): added bluetooth adapter status modal and no trezor found modal

---
+++ packages/suite/src/components/suite/modals/ReduxModal/ConnectDeviceModal/DontSeeYourTrezorModal.tsx
@@ -0,0 +1,99 @@
+import { useMemo } from 'react';
+
+import { Button, Modal } from '@trezor/components';
+import { isDesktop } from '@trezor/env-utils';
+import { TREZOR_SUPPORT_URL } from '@trezor/urls';
+
+import { Translation } from 'src/components/suite/Translation';
+import { TroubleshootingTipsItem } from 'src/components/suite/troubleshooting/TroubleshootingTips';
+import { TroubleshootingTipsList } from 'src/components/suite/troubleshooting/TroubleshootingTipsList';
+import {
+    TROUBLESHOOTING_ALL_BLUETOOTH_TIPS,
+    TROUBLESHOOTING_TIP_BRIDGE_STATUS,
+    TROUBLESHOOTING_TIP_CABLE,
+    TROUBLESHOOTING_TIP_DIFFERENT_COMPUTER,
+    TROUBLESHOOTING_TIP_SUITE_DESKTOP,
+    TROUBLESHOOTING_TIP_SUITE_DESKTOP_TOGGLE_BRIDGE,
+    TROUBLESHOOTING_TIP_UDEV,
+    TROUBLESHOOTING_TIP_USB,
+} from 'src/components/suite/troubleshooting/tips';
+import { useSelector } from 'src/hooks/suite';
+import { useBridgeDesktopApi } from 'src/hooks/suite/useBridgeDesktopApi';
+import { selectHasTransportOfType } from 'src/selectors/suite/suiteSelectors';
+
+type DontSeeYourTrezorModalProps = {
+    isBluetoothMode: boolean;
+    onGoBack?: () => void;
+};
+
+const commonCableTips = [
+    TROUBLESHOOTING_TIP_UDEV,
+    TROUBLESHOOTING_TIP_CABLE,
+    TROUBLESHOOTING_TIP_USB,
+];
+
+export const DontSeeYourTrezorModal = ({
+    onGoBack,
+    isBluetoothMode,
+}: DontSeeYourTrezorModalProps) => {
+    const isWebUsbTransport = useSelector(selectHasTransportOfType('WebUsbTransport'));
+
+    const bridgeDesktopApi = useBridgeDesktopApi();
+
+    const openTrezorSupport = () => {
+        window.open(TREZOR_SUPPORT_URL, '_blank');
+        onGoBack?.();
+    };
+
+    const cableItem: TroubleshootingTipsItem[] = useMemo(() => {
+        const items = isWebUsbTransport
+            ? [...commonCableTips, TROUBLESHOOTING_TIP_SUITE_DESKTOP]
+            : [
+                  TROUBLESHOOTING_TIP_BRIDGE_STATUS,
+                  ...commonCableTips,
+                  TROUBLESHOOTING_TIP_DIFFERENT_COMPUTER,
+              ];
+
+        if (bridgeDesktopApi?.bridgeProcess?.process) {
+            items.push(TROUBLESHOOTING_TIP_SUITE_DESKTOP_TOGGLE_BRIDGE);
+        } else {
+            // TODO: here we are going to put instruction to uninstall standalone bridge
+        }
+
+        return items;
+    }, [isWebUsbTransport, bridgeDesktopApi]);
+
+    const tipItems = useMemo(() => {
+        if (isBluetoothMode && isDesktop()) {
+            return TROUBLESHOOTING_ALL_BLUETOOTH_TIPS;
+        }
+
+        return cableItem;
+    }, [isBluetoothMode, cableItem]);
+
+    return (
+        <Modal
+            bottomContent={
+                <>
+                    <Button onClick={onGoBack} variant="info">
+                        <Translation
+                            id={isBluetoothMode ? 'TR_BLUETOOTH_SCAN_AGAIN' : 'TR_GOT_IT'}
+                        />
+                    </Button>
+                    <Button
+                        variant="tertiary"
+                        onClick={isBluetoothMode ? onGoBack : openTrezorSupport}
+                    >
+                        <Translation
+                            id={isBluetoothMode ? 'TR_CANCEL' : 'TR_CONTACT_TREZOR_SUPPORT'}
+                        />
+                    </Button>
+                </>
+            }
+            heading={<Translation id="TR_STILL_DONT_SEE_YOUR_TREZOR" />}
+            onCancel={onGoBack}
+        >
+            <TroubleshootingTipsList items={tipItems} />
+        </Modal>
+    );
+};
