431b63d13ffbe66a4240923c2b2d0db0ad696a54
Matěj Husák <matej.husak@icloud.com>
2025-08-25 14:53:32 +0200

    feat(suite): redesign BT components, added new modals

---
+++ packages/suite/src/components/connection/BluetoothConnectionModal.tsx
@@ -0,0 +1,111 @@
+import { Flex, H3, Modal, Text } from '@trezor/components';
+
+import { DesktopBluetoothDevice } from 'src/actions/bluetooth/DesktopBluetoothDevice';
+import { BluetoothPairingPin } from 'src/components/suite/bluetooth/BluetoothPairingPin';
+import { BluetoothScanningList } from 'src/components/suite/bluetooth/BluetoothScanningList';
+import { BluetoothSelectedDevice } from 'src/components/suite/bluetooth/BluetoothSelectedDevice';
+
+type BluetoothConnectionModalProps = {
+    devices: DesktopBluetoothDevice[];
+    selectedDevice: DesktopBluetoothDevice | undefined;
+    onPairingCancel: (deviceId: string) => Promise<void>;
+    onRescanClick: () => void;
+    onConnect: (deviceId: string) => Promise<void>;
+    onCancel: () => void;
+};
+
+type BluetoothModalConnectionHeadingProps = {
+    heading: string;
+    description: string;
+};
+
+const BluetoothModalConnectionHeading = ({
+    heading,
+    description,
+}: BluetoothModalConnectionHeadingProps) => (
+    <Flex direction="column">
+        <H3>{heading}</H3>
+        <Text typographyStyle="hint" variant="tertiary">
+            {description}
+        </Text>
+    </Flex>
+);
+
+export const BluetoothConnectionModal = ({
+    devices,
+    selectedDevice,
+    onPairingCancel,
+    onRescanClick,
+    onConnect,
+    onCancel,
+}: BluetoothConnectionModalProps) => {
+    if (
+        selectedDevice !== undefined &&
+        selectedDevice !== null &&
+        selectedDevice.connectionStatus.type === 'pairing' &&
+        (selectedDevice.connectionStatus?.pin?.length ?? 0) > 0
+    ) {
+        return (
+            <Modal
+                onCancel={() => onPairingCancel(selectedDevice.id)}
+                heading={
+                    <BluetoothModalConnectionHeading
+                        heading="Connect your Trezor"
+                        description="Please select a device to connect."
+                    />
+                }
+            >
+                <BluetoothPairingPin
+                    device={selectedDevice}
+                    pairingPin={selectedDevice.connectionStatus.pin}
+                />
+            </Modal>
+        );
+    }
+
+    if (selectedDevice !== undefined) {
+        return (
+            <Modal
+                onCancel={onCancel}
+                heading={
+                    <BluetoothModalConnectionHeading
+                        heading="Connect your Trezor"
+                        description="Please select a device to connect."
+                    />
+                }
+            >
+                <BluetoothSelectedDevice
+                    device={selectedDevice}
+                    onReScanClick={onRescanClick}
+                    onCancel={onPairingCancel}
+                />
+            </Modal>
+        );
+    }
+
+    if (devices.length > 0 && !selectedDevice) {
+        return (
+            <Modal
+                onCancel={onCancel}
+                heading={
+                    <BluetoothModalConnectionHeading
+                        heading="Connect your Trezor"
+                        description="Please select a device to connect."
+                    />
+                }
+            >
+                <BluetoothScanningList
+                    devices={devices}
+                    uiMode="spatial"
+                    onConnect={onConnect}
+                    onReScanClick={onRescanClick}
+                    onClose={() => {
+                        alert('close!!');
+                    }}
+                />
+            </Modal>
+        );
+    }
+
+    return null;
+};
--- packages/suite/src/components/suite/bluetooth/BluetoothPairingPin.tsx
+++ packages/suite/src/components/suite/bluetooth/BluetoothPairingPin.tsx
@@ -1,11 +1,10 @@
 import styled from 'styled-components';
 
-import { Card, Link, Modal, Row, Text } from '@trezor/components';
+import { Card, Row } from '@trezor/components';
 import { spacings, spacingsPx, typography } from '@trezor/theme';
 
 import { BluetoothDeviceComponent } from './BluetoothDeviceComponent';
 import { DesktopBluetoothDevice } from '../../../actions/bluetooth/DesktopBluetoothDevice';
-import { Translation } from '../Translation';
 
 const Pin = styled.div`
     display: flex;
@@ -17,41 +16,23 @@ const Pin = styled.div`
 `;
 
 type BluetoothPairingPinProps = {
-    onCancel: (deviceId: string) => void;
     pairingPin?: string;
     device: DesktopBluetoothDevice;
 };
 
-export const BluetoothPairingPin = ({ onCancel, pairingPin, device }: BluetoothPairingPinProps) => {
-    const handleOnCancel = () => onCancel(device.id);
-
-    return (
-        <Modal
-            heading={<Translation id="TR_BLUETOOTH_PIN_CODE" />}
-            onCancel={handleOnCancel}
-            variant="primary"
-            bottomContent={
-                <Link onClick={handleOnCancel} typographyStyle="hint" variant="underline">
-                    <Text variant="tertiary">
-                        <Translation id="TR_BLUETOOTH_PIN_CODE_DONT_MATCH" />
-                    </Text>
-                </Link>
-            }
+export const BluetoothPairingPin = ({ pairingPin, device }: BluetoothPairingPinProps) => (
+    <Card paddingType="none" overflow="hidden">
+        <Row
+            alignItems="center"
+            gap={spacings.xs}
+            justifyContent="space-between"
+            margin={{ vertical: spacings.xxl, horizontal: spacings.xxl }}
         >
-            <Card paddingType="none" overflow="hidden">
-                <Row
-                    alignItems="center"
-                    gap={spacings.xs}
-                    justifyContent="space-between"
-                    margin={{ vertical: spacings.xxl, horizontal: spacings.xxl }}
-                >
-                    <Pin>{pairingPin}</Pin>
-                    <BluetoothDeviceComponent
-                        device={device}
-                        margin={{ vertical: spacings.xxs, horizontal: spacings.xxs }}
-                    />
-                </Row>
-            </Card>
-        </Modal>
-    );
-};
+            <Pin>{pairingPin}</Pin>
+            <BluetoothDeviceComponent
+                device={device}
+                margin={{ vertical: spacings.xxs, horizontal: spacings.xxs }}
+            />
+        </Row>
+    </Card>
+);
