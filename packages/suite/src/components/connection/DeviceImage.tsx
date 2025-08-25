adc7b9759f2e4ea4875895790f0dcbe47b343664
Matěj Husák <matej.husak@icloud.com>
2025-08-25 13:32:59 +0200

    feat(suite): added DeviceImage component, connection animation

---
+++ packages/suite/src/components/connection/DeviceImage.tsx
@@ -0,0 +1,60 @@
+import styled from 'styled-components';
+
+import { resolveStaticPath } from '@suite-common/suite-utils';
+import { Image, PNG_IMAGES, PNG_PATH } from '@trezor/components';
+import { DeviceModelInternal } from '@trezor/device-utils';
+
+// component taken from native part of app, edit for desktop & web usage only for one model
+type SupportedDeviceModels = DeviceModelInternal.T3W1;
+
+const deviceImageMap: Record<SupportedDeviceModels, string> = {
+    [DeviceModelInternal.T3W1]: resolveStaticPath(`${PNG_PATH}/${PNG_IMAGES.TREZOR_T3W1_LARGE_2x}`),
+};
+
+type DeviceImageSize = 'small' | 'normal' | 'large';
+type DeviceImageDimensions = {
+    width: number;
+    height: number;
+};
+
+export const deviceImageSizeToDimensionsMap = {
+    small: {
+        width: 68,
+        height: 112,
+    },
+    normal: {
+        width: 92,
+        height: 151,
+    },
+    large: {
+        width: 243,
+        height: 400,
+    },
+} as const satisfies Record<DeviceImageSize, DeviceImageDimensions>;
+
+const ImageWrapper = styled.div<{
+    $size: DeviceImageSize;
+    $maxHeight?: number;
+}>`
+    width: ${({ $size }) => deviceImageSizeToDimensionsMap[$size].width}px;
+    height: ${({ $size }) => deviceImageSizeToDimensionsMap[$size].height}px;
+    max-height: ${({ $maxHeight }) => ($maxHeight ? `${$maxHeight}px` : 'none')};
+
+    img {
+        width: 100%;
+        height: 100%;
+        object-fit: contain;
+    }
+`;
+
+export type DeviceImageProps = {
+    deviceModel: SupportedDeviceModels;
+    size?: DeviceImageSize;
+    maxHeight?: number;
+};
+
+export const DeviceImage = ({ deviceModel, size = 'normal', maxHeight }: DeviceImageProps) => (
+    <ImageWrapper $size={size} $maxHeight={maxHeight}>
+        <Image imageSrc={deviceImageMap[deviceModel]} />
+    </ImageWrapper>
+);
