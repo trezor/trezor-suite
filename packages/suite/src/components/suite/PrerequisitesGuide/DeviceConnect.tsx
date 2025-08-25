a9f3a82ae7601dcf83201efc4273980409ead398
Matěj Husák <matej.husak@icloud.com>
2025-08-25 15:01:54 +0200

    feat(suite): created ConnectionGlobalModal, updated entry points

--- packages/suite/src/components/suite/PrerequisitesGuide/DeviceConnect.tsx
+++ packages/suite/src/components/suite/PrerequisitesGuide/DeviceConnect.tsx
@@ -1,110 +1,15 @@
-import { ReactNode } from 'react';
-
+import { deviceActions } from '@suite-common/wallet-core';
 import { Button } from '@trezor/components';
-import { isDesktop } from '@trezor/env-utils';
 
-import { Translation, TroubleshootingTips, WebUsbButton } from 'src/components/suite';
-import {
-    TROUBLESHOOTING_ALL_BLUETOOTH_TIPS,
-    TROUBLESHOOTING_TIP_BRIDGE_STATUS,
-    TROUBLESHOOTING_TIP_CABLE,
-    TROUBLESHOOTING_TIP_DIFFERENT_COMPUTER,
-    TROUBLESHOOTING_TIP_SUITE_DESKTOP,
-    TROUBLESHOOTING_TIP_SUITE_DESKTOP_TOGGLE_BRIDGE,
-    TROUBLESHOOTING_TIP_UDEV,
-    TROUBLESHOOTING_TIP_USB,
-} from 'src/components/suite/troubleshooting/tips';
-import { useBridgeDesktopApi } from 'src/hooks/suite/useBridgeDesktopApi';
-import { selectHasTransportOfType, selectSuiteFlags } from 'src/selectors/suite/suiteSelectors';
+import { Translation } from 'src/components/suite';
+import { useDispatch } from 'src/hooks/suite';
 
-import { selectConnectingDevices } from '../../../actions/bluetooth/desktopBluetoothSelectors';
-import { useSelector } from '../../../hooks/suite';
-import {
-    TroubleshootingTipsItem,
-    TroubleshootingTipsWithSections,
-} from '../troubleshooting/TroubleshootingTips';
-
-type DeviceConnectProps = {
-    setIsBluetoothConnectOpen: (value: true) => void;
-};
-
-export const DeviceConnect = ({ setIsBluetoothConnectOpen }: DeviceConnectProps) => {
-    const isWebUsbTransport = useSelector(selectHasTransportOfType('WebUsbTransport'));
-    const { isBluetoothEnabled } = useSelector(selectSuiteFlags);
-    const autoConnectingDeviceIds = useSelector(selectConnectingDevices);
-
-    const commonCableTips = [
-        TROUBLESHOOTING_TIP_UDEV,
-        TROUBLESHOOTING_TIP_CABLE,
-        TROUBLESHOOTING_TIP_USB,
-    ];
-    const cableItem: TroubleshootingTipsItem[] = isWebUsbTransport
-        ? [...commonCableTips, TROUBLESHOOTING_TIP_SUITE_DESKTOP]
-        : [
-              TROUBLESHOOTING_TIP_BRIDGE_STATUS,
-              ...commonCableTips,
-              TROUBLESHOOTING_TIP_DIFFERENT_COMPUTER,
-          ];
-
-    if (useBridgeDesktopApi()?.bridgeProcess?.process) {
-        cableItem.push(TROUBLESHOOTING_TIP_SUITE_DESKTOP_TOGGLE_BRIDGE);
-    } else {
-        // todo: here we are going to put instruction to uninstall standalone bridge
-    }
-
-    const getCallToActionButton = (): ReactNode => {
-        if (isWebUsbTransport) {
-            return (
-                <WebUsbButton data-testid="@webusb-button" translationId="TR_CHECK_FOR_DEVICES" />
-            );
-        }
-
-        if (isBluetoothEnabled && isDesktop()) {
-            const isAutoConnecting = autoConnectingDeviceIds.length > 0;
-
-            return (
-                <Button
-                    onClick={() => setIsBluetoothConnectOpen(true)}
-                    icon="bluetooth"
-                    variant="tertiary"
-                    size="small"
-                    isLoading={isAutoConnecting}
-                >
-                    <Translation id="TR_PAIR_NEW_BLUETOOTH_DEVICE" />
-                </Button>
-            );
-        }
-
-        return null;
-    };
-
-    if (isBluetoothEnabled && isDesktop()) {
-        return (
-            <TroubleshootingTipsWithSections
-                label={<Translation id="TR_CONNECTION_TYPE" />}
-                ctaLabel={<Translation id="TR_TREZOR_SAFE_7" />}
-                items={{
-                    cable: { items: cableItem, label: <Translation id="TR_CABLE" /> },
-                    bluetooth: {
-                        items: TROUBLESHOOTING_ALL_BLUETOOTH_TIPS,
-                        label: <Translation id="TR_BLUETOOTH" />,
-                    },
-                }}
-                defaultSection="cable"
-                cta={getCallToActionButton()}
-                data-testid="@connect-device-prompt/no-device-detected"
-                toggleText={<Translation id="TR_STILL_DONT_SEE_YOUR_TREZOR" />}
-            />
-        );
-    }
+export const DeviceConnect = () => {
+    const dispatch = useDispatch();
 
     return (
-        <TroubleshootingTips
-            cta={getCallToActionButton()}
-            ctaLabel={<Translation id="TR_TREZOR_NOT_DETECTED" />}
-            items={cableItem}
-            data-testid="@connect-device-prompt/no-device-detected"
-            toggleText={<Translation id="TR_STILL_DONT_SEE_YOUR_TREZOR" />}
-        />
+        <Button onClick={() => dispatch(deviceActions.toggleConnectionModal())}>
+            <Translation id="TR_CONNECT" />
+        </Button>
     );
 };
