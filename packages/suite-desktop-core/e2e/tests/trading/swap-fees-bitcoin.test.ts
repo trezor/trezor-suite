4fe186198d01d5921236af2df32a575bef22b219
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2025-08-25 20:19:14 +0200

    refactor(e2e): Introruces awaited expects for Redux store
    eliminates set of page.waitForTimeout

--- packages/suite-desktop-core/e2e/tests/trading/swap-fees-bitcoin.test.ts
+++ packages/suite-desktop-core/e2e/tests/trading/swap-fees-bitcoin.test.ts
@@ -44,7 +44,7 @@ test.describe('Trading - Swap fees Bitcoin', { tag: ['@group=trading', '@webOnly
             await tradingPage.swapBestOfferButton.click();
             await tradingPage.confirmTrade('Ethereum #1');
             await tradingPage.finishTransactionButton.click();
-            await page.waitForTimeout(3000); // wait for compound data to be ready
+            await page.expectReduxObjectNotToBeEmpty('wallet.tradingNew.composedTransactionInfo');
             await tradingPage.openConfirmAndSendModal();
             await expect(devicePrompt.headerParagraph).toContainText('Bitcoin #1');
             await devicePrompt.waitForPromptAndClick();
