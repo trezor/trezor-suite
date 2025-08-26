beb9b79cf4c8d2bb94f194d524c6ff47fed98112
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-08-26 17:44:10 +0200

    feat(suite-native): Mobile Sell: display badge about actual amount to be traded

--- suite-native/module-trading/src/components/sell/SellFormFieldErrorBadge.tsx
+++ suite-native/module-trading/src/components/sell/SellFormFieldErrorBadge.tsx
@@ -1,20 +1,33 @@
 import { useSelector } from 'react-redux';
 
 import { useFormatters } from '@suite-common/formatters';
+import { invariant } from '@suite-common/suite-utils';
 import { selectTradingSellIsLoading } from '@suite-common/trading';
+import { FiatRatesRootState, WalletSettingsRootState } from '@suite-common/wallet-core';
 import { asBaseCurrencyAmount } from '@suite-common/wallet-utils';
 import { Badge } from '@suite-native/atoms';
 import { useField } from '@suite-native/forms';
 import { useTranslate } from '@suite-native/intl';
 import { BigNumber } from '@trezor/utils';
 
-import { MAX_FIAT_DECIMALS } from '../../consts/general/consts';
+import { MAX_CRYPTO_DECIMALS, MAX_FIAT_DECIMALS } from '../../consts/general/consts';
+import { useConvertFormValueToBaseUnit } from '../../hooks/general/useConvertFormValueToBaseUnit';
 import { useSellFormContext } from '../../hooks/sell/useSellFormContext';
+import { TradingRootState } from '../../reducers';
+import { selectAmountInBaseFiatCurrency } from '../../selectors/commonSelectors';
+import { TradeableAsset } from '../../types/general';
 import { SellFormValues } from '../../types/sell';
 import { truncateDecimals } from '../../utils/general/amountUtils';
+import { getSymbolFromTradeableAsset } from '../../utils/general/tradeableAssetUtils';
+import { FiatAmountBadge } from '../general/FiatAmountBadge';
 
 export type SellFormFieldErrorBadgeProps = {
     fieldName: keyof SellFormValues;
+};
+
+type SellSendFiatAmountBadgeProps = {
+    amount: string;
+    asset: TradeableAsset;
 };
 
 const asNonEmptyStringValue = (value: unknown): string => (value as string) ?? '0';
@@ -22,19 +35,44 @@ const asNonEmptyStringValue = (value: unknown): string => (value as string) ?? '
 const useMismatchedAmountMessage = (fieldName: keyof SellFormValues) => {
     const { watch } = useSellFormContext();
     const { translate } = useTranslate();
-    const { BaseCurrencyAmountFormatter } = useFormatters();
+    const { CryptoAmountFormatter, BaseCurrencyAmountFormatter } = useFormatters();
+    const { convertStrToBaseUnit } = useConvertFormValueToBaseUnit();
 
-    const [quote, amountInCrypto, value] = watch(['quote', 'amountInCrypto', fieldName]);
+    const [asset, quote, amountInCrypto, value] = watch([
+        'sendAsset',
+        'quote',
+        'amountInCrypto',
+        fieldName,
+    ]);
+    const symbol = getSymbolFromTradeableAsset(asset);
 
     if (!quote) {
         return undefined;
     }
 
-    const { fiatStringAmount, fiatCurrency } = quote;
+    const { fiatStringAmount, cryptoStringAmount, fiatCurrency } = quote;
     let requestedAmount: string | null = null;
     let quoteAmount: string | null = null;
 
-    if (!amountInCrypto && fieldName === 'fiatStringAmount' && fiatStringAmount && fiatCurrency) {
+    if (amountInCrypto && fieldName === 'cryptoStringAmount' && cryptoStringAmount && symbol) {
+        const nonEmptyValue = asNonEmptyStringValue(value);
+        const convertedRequestedAmount = convertStrToBaseUnit(nonEmptyValue, symbol) as string;
+        requestedAmount = CryptoAmountFormatter.format(convertedRequestedAmount, {
+            symbol,
+            isBalance: true,
+        });
+
+        const truncatedCryptoAmount = truncateDecimals(cryptoStringAmount, MAX_CRYPTO_DECIMALS);
+        quoteAmount = CryptoAmountFormatter.format(truncatedCryptoAmount, {
+            symbol,
+            isBalance: true,
+        });
+    } else if (
+        !amountInCrypto &&
+        fieldName === 'fiatStringAmount' &&
+        fiatStringAmount &&
+        fiatCurrency
+    ) {
         requestedAmount = BaseCurrencyAmountFormatter.format(
             asBaseCurrencyAmount(new BigNumber(asNonEmptyStringValue(value))),
             { currency: fiatCurrency },
@@ -56,22 +94,46 @@ const useMismatchedAmountMessage = (fieldName: keyof SellFormValues) => {
     return undefined;
 };
 
+const SellSendFiatAmountBadge = ({ amount, asset }: SellSendFiatAmountBadgeProps) => {
+    const { convertStrToBaseUnit } = useConvertFormValueToBaseUnit();
+    const symbol = getSymbolFromTradeableAsset(asset);
+    invariant(symbol, 'Asset symbol is undefined');
+
+    const convertedAmount = convertStrToBaseUnit(amount, symbol);
+    invariant(convertedAmount, 'Amount could not be converted to base unit');
+
+    const fiatAmount = useSelector(
+        (state: FiatRatesRootState & WalletSettingsRootState & TradingRootState) =>
+            selectAmountInBaseFiatCurrency(state, asset, convertedAmount),
+    );
+
+    return <FiatAmountBadge amount={fiatAmount} />;
+};
+
 export const SellFormFieldErrorBadge = ({ fieldName }: SellFormFieldErrorBadgeProps) => {
     const isLoading = useSelector(selectTradingSellIsLoading);
+    const { watch } = useSellFormContext();
 
-    const { errorMessage, hasError } = useField({ name: fieldName });
+    const { errorMessage, hasError, value } = useField({ name: fieldName });
     const mismatchedAmountMessage = useMismatchedAmountMessage(fieldName);
 
-    if (isLoading) {
-        return null;
+    if (!isLoading) {
+        if (hasError) {
+            return <Badge label={errorMessage} variant="red" size="small" />;
+        }
+
+        if (mismatchedAmountMessage) {
+            return <Badge label={mismatchedAmountMessage} variant="neutral" size="small" />;
+        }
     }
 
-    if (hasError) {
-        return <Badge label={errorMessage} variant="red" size="small" />;
-    }
+    if (fieldName === 'cryptoStringAmount') {
+        const asset = watch('sendAsset');
+        if (!asset || !value) {
+            return null;
+        }
 
-    if (mismatchedAmountMessage) {
-        return <Badge label={mismatchedAmountMessage} variant="neutral" size="small" />;
+        return <SellSendFiatAmountBadge amount={value} asset={asset} />;
     }
 
     return null;
