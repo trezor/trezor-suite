5b10d1cd53b3c94ae03fc8099b2e3a0bf40318b6
Peter Sanderson <dierk.deidel@protonmail.com>
2025-08-27 13:00:09 +0200

    feat: upgrade FW stuff for getEvoluNode

--- packages/connect/src/types/api/evoluGetKeys.ts
+++
@@ -1,4 +0,0 @@
-import { PROTO } from '../../constants';
-import { Params, Response } from '../params';
-
-export declare function evoluGetKeys(params: Params<PROTO.EvoluGetKeys>): Response<PROTO.EvoluKeys>;
