9cf82dbdf0c857e9ed5668a73328201f496b3168
Jan Václavík <jvaclavik@gmail.com>
2025-08-27 13:50:53 +0200

    feat(suite): Add front color resolution

---
+++ packages/product-components/src/components/DeviceAnimation/Video.tsx
@@ -0,0 +1,34 @@
+import { MouseEventHandler } from 'react';
+
+import styled from 'styled-components';
+
+import { resolveStaticPath } from '../../utils/resolveStaticPath';
+const StyledVideo = styled.video`
+    max-width: 100%;
+    max-height: 100%;
+`;
+
+type VideoProps = {
+    src: string;
+    loop: boolean;
+    videoRef: React.Ref<HTMLVideoElement>;
+    onMouseOver?: MouseEventHandler<HTMLVideoElement>;
+    key: string;
+};
+
+export const Video = ({ src, loop, videoRef, onMouseOver, key }: VideoProps) => {
+    const commonProps = {
+        loop,
+        autoPlay: true,
+        muted: true,
+        ref: videoRef,
+        onMouseOver,
+        key,
+    };
+
+    return (
+        <StyledVideo {...commonProps}>
+            <source src={resolveStaticPath(src)} type="video/webm" />
+        </StyledVideo>
+    );
+};
