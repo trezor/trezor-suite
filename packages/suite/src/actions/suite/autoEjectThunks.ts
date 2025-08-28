4d4b7fe999190e6f7f57cf1fe813a1330d5c8239
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-08-28 14:38:40 +0200

    chore(suite): don't needlessly save auto-eject to storage

--- packages/suite/src/actions/suite/autoEjectThunks.ts
+++ packages/suite/src/actions/suite/autoEjectThunks.ts
@@ -38,8 +38,6 @@ export const setAutoEjectEnabledThunk = createThunk(
             }
         });
 
-        dispatch(storageActions.saveSuiteSettings());
-
         dispatch(setAutoEject(true));
 
         const currentDevices = selectDevices(getState());
