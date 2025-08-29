441a9dc694bbdac791488e18ab9b0dcbf63a5e74
Tomas Martykan <t.martykan@gmail.com>
2025-08-29 09:11:52 +0200

    fix(suite): remove background for OnboardingLayout

--- packages/suite/src/components/onboarding/OnboardingLayout.tsx
+++ packages/suite/src/components/onboarding/OnboardingLayout.tsx
@@ -27,7 +27,6 @@ const Wrapper = styled.div`
     height: 100%;
     flex-direction: column;
     align-items: center;
-    background: ${({ theme }) => theme.backgroundSurfaceElevation2};
 `;
 
 const Body = styled.div`
@@ -61,9 +60,6 @@ const Header = styled.div`
     align-items: center;
     flex-direction: column;
     max-width: ${MAX_ONBOARDING_WIDTH}px;
-    background: ${({ theme }) => theme.backgroundSurfaceElevation2};
-    box-shadow: 0 ${spacingsPx.md} ${spacingsPx.sm} ${spacingsPx.xxs}
-        ${({ theme }) => theme.backgroundSurfaceElevation2};
     margin-bottom: ${spacingsPx.md};
     z-index: ${zIndices.base};
 
