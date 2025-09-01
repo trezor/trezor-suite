f9e839566ebda06f645077c656bfac556d97d747
Martin Varmuza <mvarmuza@gmail.com>
2025-09-01 10:02:10 +0200

    fix(suite-native): prevent TrezorConnect.getFeatures from getting scheduled multiple times

--- suite-native/device/src/hooks/useRetryFwAuthenticityChecks.ts
+++ suite-native/device/src/hooks/useRetryFwAuthenticityChecks.ts
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import { requestDeviceAccess } from '@suite-native/device-mutex';
+import { deviceAccessMutex } from '@suite-native/device-mutex/src/DeviceAccessMutex';
 import TrezorConnect, { FIRMWARE } from '@trezor/connect';
 import { TimerId } from '@trezor/type-utils';
 import { isArrayMember } from '@trezor/utils';
@@ -21,11 +22,15 @@ export const useRetryFwAuthenticityChecks = () => {
         let timeoutHandle: TimerId;
         const recheckFwRevision = () => {
             if (isRetriableError) {
-                // any device call will cause the tests to be rerun, so getFeatures is used as the most basic one
-                // it'd be useless to await the result; what interests us is the Device state that updates, and gets propagated into redux
-                requestDeviceAccess({
-                    deviceCallback: () => TrezorConnect.getFeatures({ useEmptyPassphrase: true }),
-                });
+                // refrain from scheduling multiple tasks (since this runs in a loop)
+                if (deviceAccessMutex.taskQueue.length === 0) {
+                    // any device call will cause the tests to be rerun, so getFeatures is used as the most basic one
+                    // it'd be useless to await the result; what interests us is the Device state that updates, and gets propagated into redux
+                    requestDeviceAccess({
+                        deviceCallback: () =>
+                            TrezorConnect.getFeatures({ useEmptyPassphrase: true }),
+                    });
+                }
                 timeoutHandle = setTimeout(recheckFwRevision, REFRESH_INTERVAL);
             }
         };
