f2c317ac665865f200ac82d01725fbc597aa6b97
Martin Varmuza <mvarmuza@gmail.com>
2025-09-01 10:01:23 +0200

    fix(suite-native): wrap TrezorConnect call into requestDeviceAccess in useRetryFwAuthenticityChecks

--- suite-native/device/src/hooks/useRetryFwAuthenticityChecks.ts
+++ suite-native/device/src/hooks/useRetryFwAuthenticityChecks.ts
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
+import { requestDeviceAccess } from '@suite-native/device-mutex';
 import TrezorConnect, { FIRMWARE } from '@trezor/connect';
 import { TimerId } from '@trezor/type-utils';
 import { isArrayMember } from '@trezor/utils';
@@ -22,7 +23,9 @@ export const useRetryFwAuthenticityChecks = () => {
             if (isRetriableError) {
                 // any device call will cause the tests to be rerun, so getFeatures is used as the most basic one
                 // it'd be useless to await the result; what interests us is the Device state that updates, and gets propagated into redux
-                TrezorConnect.getFeatures({ useEmptyPassphrase: true });
+                requestDeviceAccess({
+                    deviceCallback: () => TrezorConnect.getFeatures({ useEmptyPassphrase: true }),
+                });
                 timeoutHandle = setTimeout(recheckFwRevision, REFRESH_INTERVAL);
             }
         };
