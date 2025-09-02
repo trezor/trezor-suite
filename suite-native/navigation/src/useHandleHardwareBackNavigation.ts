5e2c368156263fd291a0bb4c73be318e39af0aee
juriczech <bohdan.juricek@gmail.com>
2025-09-02 14:00:24 +0200

    fix(suite-native): fix useHandleHardwareBackNavigation

--- suite-native/navigation/src/useHandleHardwareBackNavigation.ts
+++ suite-native/navigation/src/useHandleHardwareBackNavigation.ts
@@ -1,15 +1,19 @@
-import { useEffect } from 'react';
+import { useCallback } from 'react';
 import { BackHandler } from 'react-native';
 
+import { useFocusEffect } from '@react-navigation/native';
+
 export const useHandleHardwareBackNavigation = (onPress?: () => void) => {
-    useEffect(() => {
-        // do nothing unless onPress has some custom handling
-        const subscription = BackHandler.addEventListener('hardwareBackPress', () => {
-            onPress?.();
+    useFocusEffect(
+        useCallback(() => {
+            // do nothing unless onPress has some custom handling
+            const subscription = BackHandler.addEventListener('hardwareBackPress', () => {
+                onPress?.();
 
-            return true;
-        });
+                return true;
+            });
 
-        return () => subscription.remove();
-    }, [onPress]);
+            return () => subscription.remove();
+        }, [onPress]),
+    );
 };
