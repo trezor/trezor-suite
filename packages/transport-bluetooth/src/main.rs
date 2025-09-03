41bb4d946eb58228cd01573eacb8a80fee2c413a
Szymon Lesisz <sz.lesisz@gmail.com>
2025-09-03 16:25:00 +0200

    chore(transport-bluetooth): server remove structopt, get port from env variable

--- packages/transport-bluetooth/src/main.rs
+++ packages/transport-bluetooth/src/main.rs
@@ -1,20 +1,14 @@
-use structopt::StructOpt;
-
 mod server;
-
-#[derive(StructOpt, Debug)]
-#[structopt(name = "trezor-bluetooth")]
-struct Opts {
-    #[structopt(short, long, default_value = "21327")]
-    port: u16,
-}
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
+    let port = match std::env::var("TREZOR_BLUETOOTH_PORT") {
+        Ok(port) => port,
+        Err(_) => "21327".to_string(),
+    };
 
-    let opt = Opts::from_args();
-    let addr = format!("127.0.0.1:{}", opt.port);
+    let addr = format!("127.0.0.1:{}", port);
 
     match server::start_server(&addr).await {
         // Ok should not happen as start_server runs indefinitely unless there's an error
