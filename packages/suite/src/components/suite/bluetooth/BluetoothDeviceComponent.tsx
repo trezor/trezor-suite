eb94f393472b19d95790a2d763d032aa15133bf9
Vojtěch Tranta <vojta.tranta@gmail.com>
2025-09-03 14:22:48 +0200

    fix(suite): display backs of TS7 devices in the connect device list

--- packages/suite/src/components/suite/bluetooth/BluetoothDeviceComponent.tsx
+++ packages/suite/src/components/suite/bluetooth/BluetoothDeviceComponent.tsx
@@ -1,6 +1,6 @@
-import { Column, InfoSegments, Row, Text } from '@trezor/components';
+import { resolveStaticPath } from '@suite-common/suite-utils';
+import { Column, Image, InfoSegments, Row,Text } from '@trezor/components';
 import { models } from '@trezor/device-utils';
-import { RotateDeviceImage } from '@trezor/product-components';
 
 import { selectSuiteFlags } from 'src/selectors/suite/suiteSelectors';
 
@@ -12,23 +12,29 @@ type BluetoothDeviceProps = {
     device: DesktopBluetoothDevice;
 };
 
+const getTHPVideoColor = (
+    device: DesktopBluetoothDevice,
+): keyof (typeof models)['T3W1']['colors'] => String(device.manufacturerData.deviceColor || 1); // NOTE: Default color to 1 as 0 doesnt' really exist
+
 export const BluetoothDeviceComponent = ({ device }: BluetoothDeviceProps) => {
     const internalModel = device.manufacturerData.deviceModel;
     const modelConfig = models[internalModel];
     const modelName = modelConfig.name;
-    const color = device.manufacturerData.deviceColor;
+    const color = getTHPVideoColor(device);
     const colorName = modelConfig.colors[color.toString()];
 
     const { showBluetoothDebugInfo } = useSelector(selectSuiteFlags);
 
     return (
         <Row gap={12} alignItems="stretch">
-            <RotateDeviceImage
-                deviceModel={internalModel}
-                deviceColor={color}
-                animationHeight="44px"
-                animationWidth="44px"
-            />
+            <Column alignItems="center" justifyContent="center">
+                <Image
+                    height="36px"
+                    imageSrc={resolveStaticPath(
+                        `images/t3w1-backs//${internalModel.toLocaleLowerCase()}-back-color-${color}.webp`,
+                    )}
+                />
+            </Column>
             <Column justifyContent="start" alignItems="start" flex="1">
                 <Text typographyStyle="body">{modelName}</Text>
                 {showBluetoothDebugInfo && <BluetoothDebugInfo device={device} />}
