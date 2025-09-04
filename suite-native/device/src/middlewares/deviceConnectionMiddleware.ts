5ff807e512ad2088ad299ef4f120d516b7c3598e
juriczech <bohdan.juricek@gmail.com>
2025-09-04 16:04:02 +0200

    fix(suite-native): remembered thp device connection

--- suite-native/device/src/middlewares/deviceConnectionMiddleware.ts
+++ suite-native/device/src/middlewares/deviceConnectionMiddleware.ts
@@ -7,6 +7,7 @@ import {
 
 import {
     deviceActions,
+    deviceConnectThunks,
     selectIsDeviceConnectedAndAuthorized,
     selectIsDeviceInitialized,
     selectIsDeviceRemembered,
@@ -60,7 +61,7 @@ deviceConnectionMiddleware.startListening({
         // If device is not initialized and is compromised, we display the modal (reason why this condition is here) and then want to redirect to uninitialized device landing.
         (selectIsDeviceInitialized(currentState) || selectIsDeviceCompromised(currentState)),
     effect: (
-        _action: UnknownAction,
+        action: UnknownAction,
         { getState }: ListenerEffectAPI<NativeDeviceRootState, Dispatch<UnknownAction>>,
     ) => {
         const shouldNavigateToDeviceCompromisedModal = selectIsDeviceCompromised(getState());
@@ -85,7 +86,14 @@ deviceConnectionMiddleware.startListening({
         // The passphrase flow handles connection differently and redirect to connecting screen is not wanted.
         const isDeviceUsingPassphrase = selectIsDeviceUsingPassphrase(getState());
 
-        if (isDeviceUsingPassphrase || isDeviceConnectedAndAuthorized) return;
+        if (isDeviceUsingPassphrase) return;
+
+        const isUsbRememberedDeviceConnectAction =
+            isDeviceConnectedAndAuthorized &&
+            deviceConnectThunks.fulfilled.match(action) &&
+            action.meta.arg.device.thp === undefined;
+
+        if (isUsbRememberedDeviceConnectAction) return;
 
         handleDeviceConnectNavigation({ isCoinEnablingInitFinished });
     },
