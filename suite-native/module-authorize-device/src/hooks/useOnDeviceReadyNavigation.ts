586b80d36020d10bfbd44ac317766c8e4ba1335c
juriczech <bohdan.juricek@gmail.com>
2025-09-04 11:16:52 +0200

    chore(suite-native): navigation.popTo on connecting screen
    - implement useFocusEffect instead of isFocused
    - remove firmware installation check because it shouldn't be needed since we navigate only from device connection middleware where it's checked already
    - navigation.popTo to homescreen so we cannot swipe back from dashboard to connecting screen

--- suite-native/module-authorize-device/src/hooks/useOnDeviceReadyNavigation.ts
+++ suite-native/module-authorize-device/src/hooks/useOnDeviceReadyNavigation.ts
@@ -1,9 +1,8 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 
-import { useIsFocused, useNavigation } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 
-import { selectIsNoPhysicalDeviceConnected } from '@suite-common/wallet-core';
 import { selectIsDeviceReadyToUseAndAuthorized } from '@suite-native/device';
 import { selectDeviceEnabledDiscoveryNetworkSymbols } from '@suite-native/discovery';
 import { selectIsFirmwareInstallationRunning } from '@suite-native/firmware';
@@ -15,25 +14,23 @@ import {
     HomeStackRoutes,
     RootStackParamList,
     RootStackRoutes,
-    StackToTabCompositeProps,
+    StackToStackCompositeNavigationProps,
 } from '@suite-native/navigation';
 import { selectIsCoinEnablingInitFinished } from '@suite-native/settings';
 
 const LOADING_TIMEOUT = 2500;
 
-type NavigationProp = StackToTabCompositeProps<
+type NavigationProp = StackToStackCompositeNavigationProps<
     AuthorizeDeviceStackParamList,
-    AuthorizeDeviceStackRoutes.PinMatrix,
+    AuthorizeDeviceStackRoutes.ConnectingDevice,
     RootStackParamList
 >;
 
 export const useOnDeviceReadyNavigation = () => {
-    const isFocused = useIsFocused();
     const [isTimeoutFinished, setIsTimeoutFinished] = useState(false);
     const navigation = useNavigation<NavigationProp>();
 
     const isDeviceReadyToUseAndAuthorized = useSelector(selectIsDeviceReadyToUseAndAuthorized);
-    const isNoPhysicalDeviceConnected = useSelector(selectIsNoPhysicalDeviceConnected);
     const deviceEnabledDiscoveryNetworkSymbols = useSelector(
         selectDeviceEnabledDiscoveryNetworkSymbols,
     );
@@ -54,35 +51,35 @@ export const useOnDeviceReadyNavigation = () => {
     // If connected device has no supported networks and coin enabling init was finished,
     // also redirect to the Home screen otherwise user would be blocked forever.
     // This can happen if user enabled only COIN on device A and then connects device B which does not support COIN.
-    useEffect(() => {
-        if (isFirmwareInstallationRunning || !isFocused) return;
+    useFocusEffect(
+        useCallback(() => {
+            if (isFirmwareInstallationRunning) return;
 
-        if (
-            (isDeviceReadyToUseAndAuthorized && isTimeoutFinished) ||
-            (deviceEnabledDiscoveryNetworkSymbols.length === 0 && isCoinEnablingInitFinished)
-        ) {
-            if (navigation.canGoBack() && isConnectPopupOpened) {
-                // NOTE: we don't want to go back to the home screen if Connect Popup is opened
-                navigation.goBack();
+            if (
+                (isDeviceReadyToUseAndAuthorized && isTimeoutFinished) ||
+                (deviceEnabledDiscoveryNetworkSymbols.length === 0 && isCoinEnablingInitFinished)
+            ) {
+                if (navigation.canGoBack() && isConnectPopupOpened) {
+                    // NOTE: we don't want to go back to the home screen if Connect Popup is opened
+                    navigation.goBack();
 
-                return;
+                    return;
+                }
+                navigation.popTo(RootStackRoutes.AppTabs, {
+                    screen: AppTabsRoutes.HomeStack,
+                    params: {
+                        screen: HomeStackRoutes.Home,
+                    },
+                });
             }
-            navigation.navigate(RootStackRoutes.AppTabs, {
-                screen: AppTabsRoutes.HomeStack,
-                params: {
-                    screen: HomeStackRoutes.Home,
-                },
-            });
-        }
-    }, [
-        isDeviceReadyToUseAndAuthorized,
-        isNoPhysicalDeviceConnected,
-        isTimeoutFinished,
-        navigation,
-        deviceEnabledDiscoveryNetworkSymbols,
-        isCoinEnablingInitFinished,
-        isConnectPopupOpened,
-        isFirmwareInstallationRunning,
-        isFocused,
-    ]);
+        }, [
+            deviceEnabledDiscoveryNetworkSymbols.length,
+            isCoinEnablingInitFinished,
+            isConnectPopupOpened,
+            isDeviceReadyToUseAndAuthorized,
+            isFirmwareInstallationRunning,
+            isTimeoutFinished,
+            navigation,
+        ]),
+    );
 };
