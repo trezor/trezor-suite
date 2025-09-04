1a0c4d92d4d976076add7285a041384f5f50432d
Matěj Husák <matej.husak@icloud.com>
2025-09-04 10:19:39 +0200

    feat(native): simplified auto-connect bottom sheet

--- suite-native/thp/src/hooks/useThpAlert.tsx
+++ suite-native/thp/src/hooks/useThpAlert.tsx
@@ -3,7 +3,6 @@ import { useDispatch } from 'react-redux';
 
 import { startThpAutoconnectThunk, thpActions } from '@suite-common/thp';
 import { useAlert } from '@suite-native/alerts';
-import { Box, BulletListItem, Text, VStack } from '@suite-native/atoms';
 import { Translation } from '@suite-native/intl';
 
 export const useThpAlerts = () => {
@@ -20,27 +19,8 @@ export const useThpAlerts = () => {
 
     const showThpAutoconnectAlert = useCallback(() => {
         showAlert({
-            textAlign: 'left',
             title: <Translation id="thp.autoconnect.title" />,
             description: <Translation id="thp.autoconnect.description" />,
-            appendix: (
-                <VStack spacing="sp4">
-                    <Text variant="highlight">
-                        <Translation id="thp.autoconnect.appendix.highlight" />
-                    </Text>
-                    <Box>
-                        <BulletListItem color="textSubdued">
-                            <Translation id="thp.autoconnect.appendix.bullet1" />
-                        </BulletListItem>
-                        <BulletListItem color="textSubdued">
-                            <Translation id="thp.autoconnect.appendix.bullet2" />
-                        </BulletListItem>
-                        <BulletListItem color="textSubdued">
-                            <Translation id="thp.autoconnect.appendix.bullet3" />
-                        </BulletListItem>
-                    </Box>
-                </VStack>
-            ),
             primaryButtonTitle: <Translation id="thp.autoconnect.turnOnButton" />,
             onPressPrimaryButton: turnOnAutoconnect,
             secondaryButtonTitle: <Translation id="thp.autoconnect.noThanksButton" />,
