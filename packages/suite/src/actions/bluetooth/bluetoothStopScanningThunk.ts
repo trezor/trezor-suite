193fa212313b58a53d3b1fc21c17857b6d2d9f86
Tomas Martykan <t.martykan@gmail.com>
2025-09-04 14:26:04 +0200

    fix(suite): bluetooth scanning stops when not intended

--- packages/suite/src/actions/bluetooth/bluetoothStopScanningThunk.ts
+++ packages/suite/src/actions/bluetooth/bluetoothStopScanningThunk.ts
@@ -2,11 +2,24 @@ import { BLUETOOTH_PREFIX, bluetoothActions } from '@suite-common/bluetooth';
 import { createThunk } from '@suite-common/redux-utils';
 import { bluetoothIpc } from '@trezor/transport-bluetooth';
 
+import {
+    selectDeviceDefaultConnectionMode,
+    selectIsConnectionModalOpen,
+} from '../device/deviceSelectors';
+
 export const bluetoothStopScanningThunk = createThunk<void, void, void>(
     `${BLUETOOTH_PREFIX}/bluetoothStopScanningThunk`,
-    (_, { dispatch }) => {
+    (_, { dispatch, getState }) => {
+        const defaultConnectionMode = selectDeviceDefaultConnectionMode(getState());
+        const isModalOpen = selectIsConnectionModalOpen(getState());
+        if (defaultConnectionMode === 'bluetooth' && isModalOpen) {
+            // Don't actually stop scanning
+            return;
+        }
+
         dispatch(bluetoothActions.scanStatusAction({ status: 'idle' }));
         // This can fail, but there is nothing we can do about it
+        console.log('_____BT: scanning - STOP');
         bluetoothIpc.stopScan();
     },
 );
