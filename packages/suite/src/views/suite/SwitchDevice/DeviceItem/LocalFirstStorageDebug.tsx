9bd2eebe31605357a9bc3b7032b67029931af719
Peter Sanderson <dierk.deidel@protonmail.com>
2025-09-04 15:11:41 +0200

    feat: rework of co-existance of the Legacy & Evolu labeling - now they are mutually exclusive

--- packages/suite/src/views/suite/SwitchDevice/DeviceItem/LocalFirstStorageDebug.tsx
+++ packages/suite/src/views/suite/SwitchDevice/DeviceItem/LocalFirstStorageDebug.tsx
@@ -2,34 +2,48 @@ import { AcquiredDevice } from '@suite-common/suite-types';
 import { Banner, Code, Row, Text, Tooltip } from '@trezor/components';
 import { spacings } from '@trezor/theme';
 
-import { selectSuiteFlags } from 'src/selectors/suite/suiteSelectors';
-
-import { useSelector } from '../../../../hooks/suite';
+import { useLabelingCombined } from '../../../../hooks/suite/useLabelingCombined';
 
 export const LocalFirstStorageDebug = ({ device }: { device: AcquiredDevice }) => {
-    const { isLocalFirstStorageDebugEnabled } = useSelector(selectSuiteFlags);
+    const { legacyMetadataState, isLocalFirstStorageEnabled, isLocalFirstStorageDebugEnabled } =
+        useLabelingCombined();
+
+    if (!isLocalFirstStorageDebugEnabled || !device.state?.staticSessionId) {
+        return;
+    }
 
     if (device.unavailableCapabilities.evolu) {
         // Todo: better UX to cover this
         return <Banner variant="warning">Upgrade Firmware for Evolu Labeling.</Banner>;
     }
 
-    return isLocalFirstStorageDebugEnabled && device.state?.staticSessionId ? (
+    return isLocalFirstStorageEnabled ? (
         <Row gap={spacings.xxs}>
-            <Text typographyStyle="hint" variant="warning">
-                <Code>{device.state.staticSessionId.split('@')[0].slice(-8)}</Code>
-            </Text>
-            @
-            <Text typographyStyle="hint" variant="purple">
-                <Code>{device.state.staticSessionId.slice(-8)}</Code>
-            </Text>
-            <Tooltip
-                content={<Code>{JSON.stringify(device.localFirstStorageSecret, null, 2)}</Code>}
-            >
-                <Text typographyStyle="hint" variant="purple">
-                    E: <Code>{device.localFirstStorageSecret?.evoluKeys?.ownerId.slice(-8)}</Code>
-                </Text>
-            </Tooltip>
+            🐞
+            {legacyMetadataState.enabled && <Text variant="purple">[Legacy]</Text>}
+            {isLocalFirstStorageEnabled && (
+                <>
+                    <Text typographyStyle="hint" variant="warning">
+                        <Code>{device.state.staticSessionId.split('@')[0].slice(-8)}</Code>
+                    </Text>
+                    @
+                    <Text typographyStyle="hint" variant="purple">
+                        <Code>{device.state.staticSessionId.slice(-8)}</Code>
+                    </Text>
+                    <Tooltip
+                        content={
+                            <Code>{JSON.stringify(device.localFirstStorageSecret, null, 2)}</Code>
+                        }
+                    >
+                        <Text typographyStyle="hint" variant="purple">
+                            E:{' '}
+                            <Code>
+                                {device.localFirstStorageSecret?.evoluKeys?.ownerId.slice(-8)}
+                            </Code>
+                        </Text>
+                    </Tooltip>
+                </>
+            )}
         </Row>
     ) : null;
 };
