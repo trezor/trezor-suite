8f0c4dfe4b10c95461fca72f95b1de1fb6d72eb5
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-09-08 17:34:38 +0200

    chore(suite-native): Mobile Trade: do not log missing currency name

--- suite-native/module-trading/src/utils/general/__tests__/currencyUtils.test.ts
+++ suite-native/module-trading/src/utils/general/__tests__/currencyUtils.test.ts
@@ -2,23 +2,8 @@ import { getCurrencyLabel } from '../currencyUtils';
 
 describe('currencyUtils', () => {
     describe('getCurrencyLabel', () => {
-        let consoleErrorSpy: jest.SpyInstance;
-
-        beforeAll(() => {
-            consoleErrorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
-        });
-
-        beforeEach(() => {
-            consoleErrorSpy.mockClear();
-        });
-
-        afterAll(() => {
-            consoleErrorSpy.mockRestore();
-        });
-
         it('should return label from supported currencies map', () => {
             expect(getCurrencyLabel('czk')).toBe('Czech Koruna');
-            expect(consoleErrorSpy).not.toHaveBeenCalled();
         });
 
         it('should return label from other currencies map', () => {
@@ -28,17 +13,5 @@ describe('currencyUtils', () => {
         it('should be uppercase code otherwise', () => {
             expect(getCurrencyLabel('xyz')).toBe('XYZ');
         });
-
-        it.each(['xof', 'xyz'])(
-            'should log warning when currency is not supported, case %#',
-            fiatCurrency => {
-                getCurrencyLabel(fiatCurrency);
-
-                expect(consoleErrorSpy).toHaveBeenCalledTimes(1);
-                expect(consoleErrorSpy).toHaveBeenCalledWith(
-                    `Trading: Currency [${fiatCurrency}] is not in supportedFiatCurrenciesMap`,
-                );
-            },
-        );
     });
 });
