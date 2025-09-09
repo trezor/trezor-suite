93de628c49ea8fcd93942f3cc7d97e34b6ed6d57
juriczech <bohdan.juricek@gmail.com>
2025-09-09 16:41:51 +0200

    feat(suite-native): device onboarding slice

--- suite-native/device/src/hooks/useHandleDeviceConnection.ts
+++ suite-native/device/src/hooks/useHandleDeviceConnection.ts
@@ -2,9 +2,9 @@ import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 
 import { useNavigation, useNavigationState } from '@react-navigation/native';
-import { useAtomValue } from 'jotai';
 
 import { selectDeviceRequestedPin } from '@suite-native/device-authorization';
+import { selectIsOnboardingDeviceDisconnectedAlertDisplayed } from '@suite-native/device-onboarding';
 import {
     AuthorizeDeviceStackRoutes,
     DeviceOnboardingStackRoutes,
@@ -15,11 +15,6 @@ import {
     useNavigationRouteMatch,
 } from '@suite-native/navigation';
 import { selectIsOnboardingFinished } from '@suite-native/settings';
-
-import {
-    isOnboardingDeviceDisconnectedAlertDisplayedAtom,
-    wasDeviceOnboardingCancelledAtom,
-} from '../deviceAtoms';
 
 type NavigationProp = StackNavigationProps<RootStackParamList, RootStackRoutes>;
 
@@ -32,11 +27,9 @@ export const useHandleDeviceConnection = () => {
     const isOnboardingFinished = useSelector(selectIsOnboardingFinished);
     const hasDeviceRequestedPin = useSelector(selectDeviceRequestedPin);
 
-    const isOnboardingDeviceDisconnectedAlertDisplayed = useAtomValue(
-        isOnboardingDeviceDisconnectedAlertDisplayedAtom,
+    const isOnboardingDeviceDisconnectedAlertDisplayed = useSelector(
+        selectIsOnboardingDeviceDisconnectedAlertDisplayed,
     );
-
-    const wasDeviceOnboardingCancelled = useAtomValue(wasDeviceOnboardingCancelledAtom);
 
     const navigation = useNavigation<NavigationProp>();
 
@@ -54,8 +47,7 @@ export const useHandleDeviceConnection = () => {
     useEffect(() => {
         if (
             !isOnboardingDeviceDisconnectedAlertDisplayed &&
-            (!isDeviceOnboardingStackFocused || isDeviceOnboardingConnectAndUnlockScreenFocused) &&
-            !wasDeviceOnboardingCancelled
+            (!isDeviceOnboardingStackFocused || isDeviceOnboardingConnectAndUnlockScreenFocused)
         ) {
             // If THP confirmation screen was shown, we want to prevent swiping/navigating back to
             // that THP confirmation screen. Swiping/navigating back shall lead to the Home screen.
@@ -82,7 +74,6 @@ export const useHandleDeviceConnection = () => {
         isDeviceOnboardingStackFocused,
         isOnboardingDeviceDisconnectedAlertDisplayed,
         navigation,
-        wasDeviceOnboardingCancelled,
     ]);
 
     // When trezor gets locked, it is necessary to display a PIN matrix for T1 so that it can be unlocked
