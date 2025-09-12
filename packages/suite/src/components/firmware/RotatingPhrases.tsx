93d5eb8c52e75706804597aff32a315c81641ea1
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-09-12 16:04:27 +0200

    chore(suite): replace some dynamic translation Ids with explicit

--- packages/suite/src/components/firmware/RotatingPhrases.tsx
+++ packages/suite/src/components/firmware/RotatingPhrases.tsx
@@ -7,7 +7,20 @@ import { Card, H4, Paragraph, motionEasing } from '@trezor/components';
 
 import { Translation } from 'src/components/suite';
 
-const PHRASES_COUNT = 12;
+const PHRASES: TranslationKey[] = [
+    'TR_DYK_ITEM_1',
+    'TR_DYK_ITEM_2',
+    'TR_DYK_ITEM_3',
+    'TR_DYK_ITEM_4',
+    'TR_DYK_ITEM_5',
+    'TR_DYK_ITEM_6',
+    'TR_DYK_ITEM_7',
+    'TR_DYK_ITEM_8',
+    'TR_DYK_ITEM_9',
+    'TR_DYK_ITEM_10',
+    'TR_DYK_ITEM_11',
+    'TR_DYK_ITEM_12',
+];
 
 type RotatingPhrasesProps = {
     interval?: number;
@@ -18,7 +31,7 @@ export const RotatingPhrases = ({ interval = 10000 }: RotatingPhrasesProps) => {
 
     useEffect(() => {
         const id = setInterval(() => {
-            setIndex(prev => (prev + 1) % PHRASES_COUNT);
+            setIndex(prev => (prev + 1) % PHRASES.length);
         }, interval);
 
         return () => clearInterval(id);
@@ -39,7 +52,7 @@ export const RotatingPhrases = ({ interval = 10000 }: RotatingPhrasesProps) => {
                         transition={{ duration: 0.3, ease: motionEasing.enter }}
                     >
                         <Paragraph align="center">
-                            <Translation id={`TR_DYK_ITEM_${index + 1}` as TranslationKey} />
+                            <Translation id={PHRASES[index]} />
                         </Paragraph>
                     </motion.div>
                 </AnimatePresence>
