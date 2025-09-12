6cf29db470f218e042b16e55f86817fd17be56e4
Tomáš Boďa <tominoboda@gmail.com>
2025-09-12 12:41:57 +0200

    chore(suite): hide MEV settings when no supported coins are enabled
    (cherry picked from commit c5b01466032772e621a22355ccee6ca7abbeddf6)

--- packages/suite/src/views/settings/SettingsGeneral/SettingsGeneral.tsx
+++ packages/suite/src/views/settings/SettingsGeneral/SettingsGeneral.tsx
@@ -2,7 +2,7 @@ import { Context } from '@suite-common/message-system';
 import { getNetwork } from '@suite-common/wallet-config';
 import {
     selectEnabledNetworks,
-    selectIsMevProtectionFeatureEnabled,
+    selectIsMevProtectionSettingsVisible,
 } from '@suite-common/wallet-core';
 import { isDesktop, isLinux, isWeb } from '@trezor/env-utils';
 
@@ -68,7 +68,7 @@ export const SettingsGeneral = () => {
 
     const isMetadataEnabled = metadata.enabled && !metadata.initiating;
     const isProviderConnected = useSelector(selectSelectedProviderForLabels);
-    const isMevProtectionFeatureEnabled = useSelector(selectIsMevProtectionFeatureEnabled);
+    const isMevProtectionSettingsVisible = useSelector(selectIsMevProtectionSettingsVisible);
 
     return (
         <SettingsLayout data-testid="@settings/index">
@@ -120,7 +120,7 @@ export const SettingsGeneral = () => {
                 {isDesktop() && !isLinux() && <BioAuthSettings />}
             </SettingsSection>
 
-            {isMevProtectionFeatureEnabled && (
+            {isMevProtectionSettingsVisible && (
                 <SettingsSection title={<Translation id="TR_SECURITY" />} icon="shield">
                     <MevProtection />
                 </SettingsSection>
