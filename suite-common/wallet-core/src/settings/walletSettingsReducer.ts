c5b01466032772e621a22355ccee6ca7abbeddf6
Tomáš Boďa <tominoboda@gmail.com>
2025-09-12 12:41:57 +0200

    chore(suite): hide MEV settings when no supported coins are enabled

--- suite-common/wallet-core/src/settings/walletSettingsReducer.ts
+++ suite-common/wallet-core/src/settings/walletSettingsReducer.ts
@@ -3,8 +3,17 @@ import {
     Feature,
     MessageSystemRootState,
 } from '@suite-common/message-system/src/messageSystemTypes';
-import { createReducerWithExtraDeps, returnStableArrayIfEmpty } from '@suite-common/redux-utils';
-import { NetworkSymbol, getNetwork, networkSymbolCollection } from '@suite-common/wallet-config';
+import {
+    createReducerWithExtraDeps,
+    createWeakMapSelector,
+    returnStableArrayIfEmpty,
+} from '@suite-common/redux-utils';
+import {
+    NetworkSymbol,
+    getNetwork,
+    networkSymbolCollection,
+    networksCollection,
+} from '@suite-common/wallet-config';
 import type { WalletSettings } from '@suite-common/wallet-types';
 import { isBaseCurrencyWithSats } from '@suite-common/wallet-utils';
 import { PROTO } from '@trezor/connect';
@@ -20,6 +29,10 @@ export type WalletSettingsRootState = {
         settings: State;
     };
 };
+
+export const createMemoizedSelector = createWeakMapSelector.withTypes<
+    WalletSettingsRootState & MessageSystemRootState
+>();
 
 const initialState: State = {
     localCurrency: 'usd',
@@ -138,3 +151,16 @@ export const selectIsMevProtectionEnabled = (state: WalletSettingsRootState) =>
 
 export const selectIsMevProtectionFeatureEnabled = (state: MessageSystemRootState) =>
     selectIsFeatureEnabled(state, Feature.mevProtection, true);
+
+const MEV_PROTECTION_SUPPORTED_NETWORK_SYMBOLS = networksCollection
+    .filter(network => network.features.includes('mev-protection'))
+    .map(network => network.symbol);
+
+export const selectIsMevProtectionSettingsVisible = createMemoizedSelector(
+    [selectIsMevProtectionFeatureEnabled, selectEnabledNetworks],
+    (isFeatureEnabled, enabledNetworks) =>
+        isFeatureEnabled &&
+        enabledNetworks.some(enabledNetwork =>
+            MEV_PROTECTION_SUPPORTED_NETWORK_SYMBOLS.includes(enabledNetwork),
+        ),
+);
