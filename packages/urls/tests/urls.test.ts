5449c1c4174210d9b81be5fb4d8b653590a2581b
Matej Kriz <matej@matejkriz.cz>
2025-09-15 16:03:09 +0200

    test(urls): unskip test for external links

--- packages/urls/tests/urls.test.ts
+++ packages/urls/tests/urls.test.ts
@@ -18,7 +18,7 @@ const excluded = [
     URLS.IMAGE_PROXY_API_URL, // returns 'unauthorized'
 ];
 
-describe.skip('Test that all external links are alive', () => {
+describe('Test that all external links are alive', () => {
     beforeEach(() => {
         jest.setTimeout(30000);
     });
