768a91db16b233f9af2f3cdbcfe91710bcd975a6
Vojtěch Tranta <vojta.tranta@gmail.com>
2025-09-16 13:51:12 +0200

    fix(suite): wrong typing of the return type for color

--- packages/product-components/src/utils/getModelFrontColor.tsx
+++ packages/product-components/src/utils/getModelFrontColor.tsx
@@ -4,12 +4,16 @@ import { DeviceModelInternal, models } from '@trezor/device-utils';
 export const getModelFrontColor = (
     deviceModelInternal?: DeviceModelInternal,
     deviceUnitColor?: number,
-) => (deviceModelInternal && models[deviceModelInternal].frontColors?.[`${deviceUnitColor}`]) ?? 1;
+): number =>
+    Number(
+        (deviceModelInternal && models[deviceModelInternal].frontColors?.[`${deviceUnitColor}`]) ??
+            1,
+    );
 
 export const getLargeModelImagePath = (
     deviceModelInternal?: DeviceModelInternal,
     deviceUnitColor?: number,
-) => {
+): ImageKey => {
     const frontColor = getModelFrontColor(deviceModelInternal, deviceUnitColor);
 
     return `TREZOR_${deviceModelInternal}${frontColor === 1 ? '' : `_FRONTCOLOR_${frontColor}`}_LARGE` as ImageKey;
