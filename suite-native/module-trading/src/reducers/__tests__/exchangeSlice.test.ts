a391aadb241f7c807f28f9514ecfd314ffd93860
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-09-16 22:34:33 +0200

    chore(suite-native): Mobile Trade: fix c&p errors in test names

--- suite-native/module-trading/src/reducers/__tests__/exchangeSlice.test.ts
+++ suite-native/module-trading/src/reducers/__tests__/exchangeSlice.test.ts
@@ -21,14 +21,14 @@ describe('exchangeSlice', () => {
     });
 
     describe('setReceiveAddress', () => {
-        it('should set buy receive address', () => {
+        it('should set exchange receive address', () => {
             const address = 'bc1qxyz';
             const state = exchangeReducer(undefined, exchangeActions.setReceiveAddress(address));
 
             expect(state.receiveAddress).toEqual(address);
         });
 
-        it('should set buy receive address to undefined', () => {
+        it('should set exchange receive address to undefined', () => {
             const state = exchangeReducer(undefined, exchangeActions.setReceiveAddress(undefined));
 
             expect(state.receiveAddress).toBeUndefined();
