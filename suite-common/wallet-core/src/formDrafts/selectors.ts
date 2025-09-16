8621d20fb9b9e272fc25938800a4022e015407ec
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-09-16 11:36:58 +0200

    chore(suite-common): Introduce formDrafts to wallet-core

---
+++ suite-common/wallet-core/src/formDrafts/selectors.ts
@@ -0,0 +1,16 @@
+import type { FieldValues } from 'react-hook-form';
+
+import { createWeakMapSelector } from '@suite-common/redux-utils';
+
+import { FormDraftRootState } from './formDraftSlice';
+
+const createMemoizedSelector = createWeakMapSelector.withTypes<FormDraftRootState>();
+
+export const selectFormDraft = <T extends FieldValues>(
+    { wallet }: FormDraftRootState,
+    formDraftKey: string,
+) => wallet.formDrafts[formDraftKey] as T | undefined;
+
+export const selectDeepCopyOfFormDraft = createMemoizedSelector([selectFormDraft], formDraft =>
+    formDraft ? JSON.parse(JSON.stringify(formDraft)) : undefined,
+);
---
+++ suite-common/wallet-core/src/formDrafts/useFormDraft.ts
@@ -0,0 +1,33 @@
+import { useCallback } from 'react';
+import type { FieldValues } from 'react-hook-form';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { FormDraftKeyPrefix } from '@suite-common/wallet-types';
+import { getFormDraftKey } from '@suite-common/wallet-utils';
+
+import { FormDraftRootState, formDraftActions } from './formDraftSlice';
+import { selectDeepCopyOfFormDraft } from './selectors';
+
+export const useFormDraft = <T extends FieldValues>(keyPrefix: FormDraftKeyPrefix, key: string) => {
+    const dispatch = useDispatch();
+
+    const formDraftKey = getFormDraftKey(keyPrefix, key);
+
+    const draft = useSelector(
+        (state: FormDraftRootState) =>
+            selectDeepCopyOfFormDraft(state, formDraftKey) as T | undefined,
+    );
+
+    const saveDraft = useCallback(
+        (formDraft: T) => {
+            dispatch(formDraftActions.storeDraft({ key: formDraftKey, formDraft }));
+        },
+        [dispatch, formDraftKey],
+    );
+
+    const removeDraft = useCallback(() => {
+        dispatch(formDraftActions.removeDraft({ key: formDraftKey }));
+    }, [dispatch, formDraftKey]);
+
+    return { draft, saveDraft, removeDraft };
+};
