ee1996b60878badf46ebe5841c266ff99aaa9617
Matus Balascak <balascakmatus@gmail.com>
2025-09-19 09:10:39 +0200

    chore(suite): show sent tokens in tx heading

--- packages/suite/src/components/wallet/TransactionItem/TransactionItem.tsx
+++ packages/suite/src/components/wallet/TransactionItem/TransactionItem.tsx
@@ -135,9 +135,9 @@ export const TransactionItem = memo(
             | { type: 'internal'; payload: (typeof filteredInternalTransfers)[number] }
             | { type: 'target'; payload: WalletAccountTransaction['targets'][number] }
         )[] = [
+            ...tokens.map(t => ({ type: 'token' as const, payload: t })),
             ...targets.map(t => ({ type: 'target' as const, payload: t })),
             ...filteredInternalTransfers.map(t => ({ type: 'internal' as const, payload: t })),
-            ...tokens.map(t => ({ type: 'token' as const, payload: t })),
         ];
 
         const isExpandable = allOutputs.length - DEFAULT_LIMIT > 0;
