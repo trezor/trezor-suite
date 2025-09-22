39ef8fa6b2fb86f7575c8c935f4cc2f9fe79f279
Tomas Martykan <t.martykan@gmail.com>
2025-09-22 18:22:56 +0200

    feat(suite): kill searchpartyuseragent on mac bt pairing

--- packages/suite-desktop-core/src/modules/bluetooth.ts
+++ packages/suite-desktop-core/src/modules/bluetooth.ts
@@ -1,7 +1,10 @@
+import { exec } from 'child_process';
 import { ipcMain } from 'electron';
 
+import { isMacOs } from '@trezor/env-utils';
 import { IpcProxyHandlerOptions, createIpcProxyHandler } from '@trezor/ipc-proxy';
 import { getFreePort } from '@trezor/node-utils';
+import { InvokeResult } from '@trezor/suite-desktop-api';
 import { BluetoothIpc, BluetoothIpcApi, BluetoothTransport } from '@trezor/transport-bluetooth';
 
 import { BluetoothProcess } from '../libs/processes/BluetoothProcess';
@@ -87,6 +90,32 @@ export const init: ModuleInit = () => {
 
                     if (method === 'dispose') {
                         killBluetoothProcess();
+                    }
+
+                    // Workaround for MacOS pairing flow:
+                    // Unfortunately the "searchpartyuseragent" process interferes with the pairing PIN dialog and causes it to not show up.
+                    // This is related to Apple's Find My feature and happens to users with Bluetooth devices such as AirPods paired to their Mac.
+                    // We previously tried using NAPI to execute pairing inside the foreground Electron app process to handle MacOS security restrictions,
+                    // but it still did not work in 100% of cases. So now we simply kill the process before attempting to connect to a device.
+                    // The process will be automatically restarted by MacOS if needed and doesn't seem to cause any issues with Find My functionality.
+                    if (method === 'connectDevice' && isMacOs()) {
+                        try {
+                            const devices = await api.enumerateDevices();
+                            const isPaired = devices.find(d => d.id === params[0])?.paired;
+                            if (!isPaired) {
+                                await new Promise<InvokeResult>(resolve => {
+                                    exec('pkill searchpartyuseragent', {}, error => {
+                                        if (error) {
+                                            resolve({ success: false, error: error.toString() });
+                                        } else {
+                                            resolve({ success: true });
+                                        }
+                                    });
+                                });
+                            }
+                        } catch (error) {
+                            logger.warn(SERVICE_NAME, `macos pairing workaround failed: ${error}`);
+                        }
                     }
 
                     return (api[method] as any)(...params);
