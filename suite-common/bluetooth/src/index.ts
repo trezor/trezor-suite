f42845cc1fedcea9da453eb8dfd1db6ad9070593
Matěj Husák <matej.husak@icloud.com>
2025-09-23 08:09:05 +0200

    chore(suite): created Bluetooth constants

--- suite-common/bluetooth/src/index.ts
+++ suite-common/bluetooth/src/index.ts
@@ -22,3 +22,4 @@ export {
 export { filterOutOldDuplicatesByName } from './filterOutOldDuplicatesByName';
 
 export { parseManufacturerData, serializeManufacturerData } from './manufacturerDataUtils';
+export { SCAN_TIMEOUT, UNPAIRED_DEVICES_LAST_UPDATED_LIMIT } from './bluetoothConstants';
