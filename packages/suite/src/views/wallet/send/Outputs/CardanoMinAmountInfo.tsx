4af987734c1d9876396da0889850e372622d712a
Matus Balascak <balascakmatus@gmail.com>
2025-09-23 17:27:55 +0200

    chore(suite): show min amount needed for send for cardano

---
+++ packages/suite/src/views/wallet/send/Outputs/CardanoMinAmountInfo.tsx
@@ -0,0 +1,89 @@
+import { getNetworkDisplaySymbol } from '@suite-common/wallet-config';
+import { MIN_CARDANO_AMOUNT_FOR_SEND } from '@suite-common/wallet-constants';
+import {
+    asAmountSubunit,
+    asAmountUnit,
+    subunitsToUnits,
+    unitsToSubunits,
+} from '@suite-common/wallet-utils';
+import { Banner, InfoItem, Text, Tooltip } from '@trezor/components';
+
+import { FormattedCryptoAmount } from 'src/components/suite';
+import { Translation } from 'src/components/suite/Translation';
+import { useSendFormContext } from 'src/hooks/wallet';
+
+export const CardanoMinAmountInfo = () => {
+    const {
+        account: { symbol, networkType, balance },
+        composedLevels,
+        getValues,
+    } = useSendFormContext();
+
+    const formOutputs = getValues().outputs;
+    const selectedFee = getValues().selectedFee || 'normal';
+
+    const transactionInfo = composedLevels ? composedLevels[selectedFee] : undefined;
+    const hasTransactionInfo = transactionInfo !== undefined && transactionInfo.type !== 'error';
+
+    if (networkType !== 'cardano') return null;
+
+    const totalAdaAmount = formOutputs.reduce((acc, output) => {
+        if (!output.token && !!output.amount) {
+            return acc.plus(output.amount ?? 0);
+        }
+
+        return acc;
+    }, new BigNumber(0));
+
+    const minAdaAmount = new BigNumber(
+        hasTransactionInfo
+            ? transactionInfo.totalSpent
+            : new BigNumber(MIN_CARDANO_AMOUNT_FOR_SEND)
+                  .times(formOutputs.length)
+                  .plus(unitsToSubunits({ symbol, value: asAmountUnit(totalAdaAmount) })),
+    );
+
+    const hasEnoughADA = new BigNumber(balance).minus(minAdaAmount).gt(0);
+    const networkDisplaySymbol = getNetworkDisplaySymbol(symbol);
+
+    return (
+        <>
+            <InfoItem
+                direction="row"
+                label={
+                    <Tooltip
+                        hasIcon
+                        maxWidth={328}
+                        content={
+                            <Translation
+                                id="TR_SEND_MIN_ADA_AMOUNT_TOOLTIP"
+                                values={{ networkDisplaySymbol }}
+                            />
+                        }
+                    >
+                        <Translation
+                            id="TR_SEND_MIN_ADA_AMOUNT_TITLE"
+                            values={{ networkDisplaySymbol }}
+                        />
+                    </Tooltip>
+                }
+            >
+                <Text variant="tertiary">
+                    <FormattedCryptoAmount
+                        disableHiddenPlaceholder
+                        value={subunitsToUnits({
+                            symbol,
+                            value: asAmountSubunit(minAdaAmount),
+                        })}
+                        symbol={symbol}
+                    />
+                </Text>
+            </InfoItem>
+            {!hasEnoughADA && (
+                <Banner variant="destructive" icon>
+                    <Translation id="TR_SEND_MIN_ADA_AMOUNT" values={{ networkDisplaySymbol }} />
+                </Banner>
+            )}
+        </>
+    );
+};
--- suite-common/wallet-constants/src/cardanoConstants.ts
+++ suite-common/wallet-constants/src/cardanoConstants.ts
@@ -17,3 +17,5 @@ export const CARDANO_EVERSTAKE_STAKING_POOL = {
     hex: '8120831fd871a6023c2099d05f21b76bdbb1e4a37464c0cd56f743c7',
     bech32: 'pool1sysgx87cwxnqy0pqn8g97gdhd0dmre9rw3jvpn2k7apuwa7cgkn',
 };
+
+export const MIN_CARDANO_AMOUNT_FOR_SEND = new BigNumber(1_000_000);
