33225a045a5144a71155d9f12dc339dfe80c9ff4
karliatto <yo@karliatto.com>
2025-09-24 13:21:47 +0200

    feat(connect): subscribe to battery-level changes

--- packages/connect/src/device/Device.ts
+++ packages/connect/src/device/Device.ts
@@ -336,6 +336,7 @@ export class Device extends TypedEmitter<DeviceEvents> {
                     channels: ['battery-level'],
                 })
                 .then(result => {
+                    console.log('result', result);
                     if (result.success && this.bluetoothProps) {
                         this.bluetoothProps.channels = result.payload;
                         this.lifecycle.emit(DEVICE.CHANGED);
