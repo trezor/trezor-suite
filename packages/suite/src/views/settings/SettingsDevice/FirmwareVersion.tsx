05d891e31e3478815437b5550602b1597c348eef
karliatto <yo@karliatto.com>
2025-09-24 13:22:38 +0200

    fix(suite): improve check in battery level updates

--- packages/suite/src/views/settings/SettingsDevice/FirmwareVersion.tsx
+++ packages/suite/src/views/settings/SettingsDevice/FirmwareVersion.tsx
@@ -78,8 +78,8 @@ export const FirmwareVersion = ({ isDeviceLocked }: FirmwareVersionProps) => {
     const handleUpdate = () => {
         if (
             isDeviceConnectedViaBluetooth &&
-            device.features?.soc &&
-            device.features?.soc < DEVICE_LOW_BATTERY_PERCENTAGE_THRESHOLD
+            typeof device?.features.soc === 'number' &&
+            device.features.soc < DEVICE_LOW_BATTERY_PERCENTAGE_THRESHOLD
         ) {
             setLowBatteryWarning(true);
 
