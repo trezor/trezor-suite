78d8a597e53f596d7d4500e78ad7c684b32f9490
Peter Sanderson <dierk.deidel@protonmail.com>
2025-09-25 12:35:00 +0200

    fix: use getDeviceInternalModel to get model correctly even in THP scenario

--- packages/suite/src/components/suite/SecurityCheck/DeviceCompromised.tsx
+++ packages/suite/src/components/suite/SecurityCheck/DeviceCompromised.tsx
@@ -96,7 +96,7 @@ const DeviceCompromisedContent = () => {
 };
 
 export const DeviceCompromised = () => (
-    <WelcomeLayout showDeviceSwitcher={true} showAccounts={false}>
+    <WelcomeLayout showAccounts={false}>
         <Card data-testid="@device-compromised">
             <DeviceCompromisedContent />
         </Card>
