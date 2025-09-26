6a28a375c5ce4716b21a05e488a572b5414aa72b
karliatto <yo@karliatto.com>
2025-09-26 11:05:19 +0200

    feat(protocol): trezor-push-notification decode

--- packages/protocol/src/errors.ts
+++ packages/protocol/src/errors.ts
@@ -1,1 +1,2 @@
 export const PROTOCOL_MALFORMED = 'Malformed protocol format' as const;
+export const PROTOCOL_MISSMATCH_VERSION = 'Protocol missmatch version' as const;
---
+++ packages/protocol/src/protocol-tpn/constants.ts
@@ -0,0 +1,2 @@
+export const MESSAGE_LENGTH = 3;
+export const TPN_VERSION = 1;
