387fbea4fa095863fc2ed1ceff2918bae4471ab2
Martin Varmuza <mvarmuza@gmail.com>
2025-09-29 14:24:42 +0200

    chore(transport): all non version 3+ bridge backends are now considered outdated

--- packages/transport/src/transports/bridge.ts
+++ packages/transport/src/transports/bridge.ts
@@ -98,14 +98,9 @@ export class BridgeTransport extends AbstractTransport {
 
                 this.version = response.payload.version;
 
-                if (this.version.startsWith('3')) {
-                    this.isOutdated = false;
-                } else {
-                    this.isOutdated =
-                        // as for trezord-go only 2.0.33 (suite-desktop bundled) is supported
-                        !['2.0.33'].includes(this.version);
+                if (!this.version.startsWith('3')) {
+                    this.isOutdated = true;
                 }
-
                 this.useProtocolMessages = !!response.payload.protocolMessages;
 
                 this.stopped = false;
