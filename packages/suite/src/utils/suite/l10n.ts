79814de380ecff006bedba1c115c4f0148d63148
Jan Komarek <jan.komarek@satoshilabs.com>
2025-09-30 18:47:18 +0200

    fix(suite): fall back to language variant of system language

--- packages/suite/src/utils/suite/l10n.ts
+++ packages/suite/src/utils/suite/l10n.ts
@@ -1,18 +1,31 @@
 import { LANGUAGES, Locale } from '@suite-common/suite-types';
 import { getPlatformLanguages } from '@trezor/env-utils';
 
-const DEFAULT_LOCALE = 'en-US';
-
-export const isLocale = (lang: string): lang is Locale => lang in LANGUAGES;
+export const DEFAULT_LOCALE = 'en-US';
 
 /**
  * Finds and returns first of languages preferred by user's environment
  * which is implemented and completed in Suite, or defaultLocale.
  */
-export const getOsLocale = (defaultLocale: Locale = DEFAULT_LOCALE): Locale => {
-    const languages = getPlatformLanguages() || [];
+export const getOsLocale = (): Locale => {
+    const platformLanguages = getPlatformLanguages() || [];
 
-    return languages.find(isLocale) || defaultLocale;
+    const isLocale = (lang: string): lang is Locale => lang in LANGUAGES;
+
+    for (const platformLanguage of platformLanguages) {
+        if (isLocale(platformLanguage)) {
+            return platformLanguage;
+        }
+        // If your locale variant (e.g. en-GB) is not available, try to find a variant of your locale that is supported (e.g. en-US)
+        const languageVariant = (Object.keys(LANGUAGES) as Locale[]).find(
+            language => language.slice(0, 2) === platformLanguage.slice(0, 2),
+        );
+        if (languageVariant) {
+            return languageVariant;
+        }
+    }
+
+    return DEFAULT_LOCALE;
 };
 
 export const watchOsLocale = (callback: (loc: Locale) => void) => {
