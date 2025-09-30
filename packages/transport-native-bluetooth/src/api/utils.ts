f59b99d7eac17d75a6ae77737be03371a7a2f4a6
yanas <yanas@yanas.cz>
2025-09-30 17:34:13 +0200

    chore(transport-native-bluetooth): introduce Bluetooth utils and tests

---
+++ packages/transport-native-bluetooth/src/api/utils.ts
@@ -0,0 +1,14 @@
+import { Device } from 'react-native-ble-plx';
+
+import { BluetoothDevice } from './types';
+
+export const base64ToByteArray = (value: string) => Array.from(Buffer.from(value, 'base64'));
+
+export const toBluetoothDevice = (device: Device): BluetoothDevice => ({
+    id: device.id,
+    name: device.name ?? 'Unknown',
+    // @suite-common utils expect the Bluetooth company identifier (first two bytes) to be trimmed
+    manufacturerData: base64ToByteArray(device.manufacturerData ?? '').slice(2),
+    lastUpdatedTimestamp: Date.now(),
+    connectionStatus: { type: 'disconnected' },
+});
