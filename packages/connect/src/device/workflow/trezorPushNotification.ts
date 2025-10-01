ce6963a0c65df801dffce562f2789862454dda78
Jan Komarek <jan.komarek@satoshilabs.com>
2025-10-01 14:56:05 +0200

    feat(connect): propagate soft lock/unlock push notifications

--- packages/connect/src/device/workflow/trezorPushNotification.ts
+++ packages/connect/src/device/workflow/trezorPushNotification.ts
@@ -87,6 +87,14 @@ export const trezorPushNotificationHandler = async ({ device, message }: TpnWork
         case TrezorPushNotificationType.UNLOCK:
             await setupDeviceMode(device, decoded.mode);
             break;
+        case TrezorPushNotificationType.SOFTLOCK:
+            device.setBusy('pin-locked');
+            device.lifecycle.emit(DEVICE.CHANGED);
+            break;
+        case TrezorPushNotificationType.SOFTUNLOCK:
+            device.setBusy(device.features ? undefined : 'thp-locked');
+            device.lifecycle.emit(DEVICE.CHANGED);
+            break;
         default:
             break;
     }
