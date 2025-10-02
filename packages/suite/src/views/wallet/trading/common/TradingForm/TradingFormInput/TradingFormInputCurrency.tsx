cbd5cb13ed456f934a892eae255af022c6d811d8
Jiří Čermák <jiri.cermak@satoshilabs.com>
2025-10-02 11:03:04 +0200

    fix(suite): prevent default value overwriting user amount input

--- packages/suite/src/views/wallet/trading/common/TradingForm/TradingFormInput/TradingFormInputCurrency.tsx
+++ packages/suite/src/views/wallet/trading/common/TradingForm/TradingFormInput/TradingFormInputCurrency.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react';
+import { useMemo } from 'react';
 import { Control, Controller } from 'react-hook-form';
 
 import {
@@ -64,17 +64,6 @@ export const TradingFormInputCurrency = ({
             context.refreshQuotes();
         }
     };
-
-    // update defaultCurrency in select only on mount
-    useEffect(() => {
-        if (isTradingBuyContext(context)) {
-            context.setValue(TRADING_FORM_FIAT_CURRENCY_SELECT, defaultCurrency);
-            context.setValue(
-                TRADING_FORM_FIAT_INPUT,
-                fiatCurrencies?.defaultAmountsOfFiatCurrencies?.get(defaultCurrency.value) ?? '',
-            );
-        }
-    }, [fiatCurrencies?.defaultAmountsOfFiatCurrencies]); // eslint-disable-line react-hooks/exhaustive-deps
 
     return (
         <Controller
