ab998c4ab81c27513abf52765e6f667fa021101f
Martin Varmuza <mvarmuza@gmail.com>
2025-10-02 14:41:49 +0200

    fix(transport): fix broken usb synchronziation after page reload

--- packages/transport/src/transports/abstractApi.ts
+++ packages/transport/src/transports/abstractApi.ts
@@ -461,7 +461,9 @@ export abstract class AbstractApiTransport extends AbstractTransport {
             });
         }
         super.stop();
-        this.sessionsClient.dispose();
+        // note:
+        // not disposing sessionClient on purpose. on window reload, transport.stop is called. we do not want to clear sessions background data in this case because
+        // there might be another client connected to it. When the last client disconnects, the background disposes itself.
         this.api.dispose();
     }
 }
