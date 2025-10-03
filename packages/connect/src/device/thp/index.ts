7ebfe1c5f7d44a5b46991702e50c57605e189c0f
Marek Polak <marek.polak@satoshilabs.com>
2025-10-03 14:15:46 +0200

    feat(connect): retry thp handshake when locked

--- packages/connect/src/device/thp/index.ts
+++ packages/connect/src/device/thp/index.ts
@@ -12,7 +12,18 @@ export const getThpChannel = async (device: Device, withInteraction?: boolean) =
     try {
         if (thpState?.phase === 'handshake') {
             await createThpChannel(device);
-            await thpHandshake(device);
+            try {
+                await thpHandshake(device);
+            } catch (error) {
+                if (error.code === 'ThpDeviceLocked') {
+                    // Device is pin-locked, retry handshake with tryToUnlock param
+                    thpState.resetState();
+                    await createThpChannel(device);
+                    await thpHandshake(device, true);
+                } else {
+                    throw error;
+                }
+            }
         }
         if (thpState?.phase === 'pairing' && withInteraction) {
             // start pairing with UI interaction
