06194faffa8ee5a0bd8a238b59ebe2520d08c73e
brantalikp <brantalikp@gmail.com>
2025-10-03 12:53:24 +0200

    fix(suite-native): continue on trezor: default bottom offset

--- suite-native/device/src/components/ConfirmOnTrezor/hooks/useConfirmOnTrezorController.ts
+++ suite-native/device/src/components/ConfirmOnTrezor/hooks/useConfirmOnTrezorController.ts
@@ -1,10 +1,16 @@
-import { useCallback, useRef, useState } from 'react';
+import { useCallback, useMemo, useRef, useState } from 'react';
+
+import { useBannerAwareSafeAreaInsets } from '@suite-native/atoms';
+import { useNativeStyles } from '@trezor/styles';
 
 import { BottomSheetControlProps } from './useConfirmOnTrezorSheet';
 
 export const useConfirmOnTrezorController = () => {
     const [isSheetOpen, setIsSheetOpen] = useState(false);
     const confirmOnTrezorRef = useRef<BottomSheetControlProps>(null);
+    const { bottom } = useBannerAwareSafeAreaInsets();
+    const { utils } = useNativeStyles();
+    const currentHeaderHeight = confirmOnTrezorRef?.current?.currentHeaderHeight || 0;
 
     const revealConfirmOnTrezorSheet = useCallback(() => {
         confirmOnTrezorRef?.current?.revealConfirmOnTrezorSheet();
@@ -16,11 +22,17 @@ export const useConfirmOnTrezorController = () => {
         setIsSheetOpen(false);
     }, []);
 
+    const defaultBottomInset = useMemo(
+        () => (isSheetOpen ? currentHeaderHeight : 0) + bottom + utils.spacings.sp16,
+        [isSheetOpen, currentHeaderHeight, bottom, utils.spacings.sp16],
+    );
+
     return {
         revealConfirmOnTrezorSheet,
         confirmOnTrezorRef,
         isSheetOpen,
         closeSheet,
-        currentHeaderHeight: confirmOnTrezorRef?.current?.currentHeaderHeight,
+        currentHeaderHeight,
+        defaultBottomInset,
     };
 };
