d975d90bfefd54f5a9012fc833149dc092e9ed31
karliatto <yo@karliatto.com>
2025-10-05 17:11:46 +0200

    feat(connect): new firmware env unsigned-stable

--- packages/connect/src/types/firmware.ts
+++ packages/connect/src/types/firmware.ts
@@ -51,6 +51,7 @@ export type FirmwareUpdateFlowType =
 export type FirmwareUpdateSource =
     | 'production'
     | 'test-unsigned'
+    | 'test-unsigned-stable'
     | 'test-signed'
     | 'localhost-unsigned'
     | 'localhost-signed';
