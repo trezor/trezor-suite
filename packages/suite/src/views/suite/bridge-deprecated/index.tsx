b9d2eb9e79afd0e3a5be89a685636d96845a1dfb
Jan Václavík <jvaclavik@gmail.com>
2025-10-07 19:52:04 +0200

    fix(suite): buttom replaced by link

--- packages/suite/src/views/suite/bridge-deprecated/index.tsx
+++ packages/suite/src/views/suite/bridge-deprecated/index.tsx
@@ -1,28 +1,24 @@
-import styled from 'styled-components';
-
-import { Column, H3, Modal, Paragraph } from '@trezor/components';
+import { Column, H3, Link, Modal, Paragraph } from '@trezor/components';
 import { spacings } from '@trezor/theme';
 import { UNINSTALL_BRIDGE_URL } from '@trezor/urls';
 
 import { goto } from 'src/actions/suite/routerActions';
 import { Metadata } from 'src/components/suite';
-import { LearnMoreButton } from 'src/components/suite/LearnMoreButton';
 import { Translation } from 'src/components/suite/Translation';
-import { useDispatch, useLayout } from 'src/hooks/suite';
-
-const LinkWrapper = styled.span`
-    display: inline-block;
-`;
+import { useDispatch, useExternalLink, useLayout } from 'src/hooks/suite';
 
 /**
  * modal that should show users basic information how to uninstall old standalone bridge
  */
 export const BridgeDeprecated = () => {
     const dispatch = useDispatch();
-
-    const goToWallet = () => dispatch(goto('wallet-index'));
+    const uninstallBridgeUrl = useExternalLink(UNINSTALL_BRIDGE_URL);
 
     useLayout('Bridge');
+
+    const onClose = () => {
+        dispatch(goto('wallet-index'));
+    };
 
     return (
         <Modal
@@ -30,7 +26,7 @@ export const BridgeDeprecated = () => {
                 <>
                     <Modal.Button
                         variant="tertiary"
-                        onClick={() => goToWallet()}
+                        onClick={onClose}
                         data-testid="@bridge/goto/wallet-index"
                     >
                         <Translation id="TR_TAKE_ME_BACK_TO_WALLET" />
@@ -40,6 +36,8 @@ export const BridgeDeprecated = () => {
             size="small"
             variant="warning"
             iconName="warning"
+            onCancel={onClose}
+            isBackdropCancelable
         >
             <Metadata title="Bridge | Trezor Suite" />
             <Column gap={spacings.xxs}>
@@ -51,11 +49,9 @@ export const BridgeDeprecated = () => {
                         id="TR_STANDALONE_BRIDGE_DEPRECATED_DESCRIPTION"
                         values={{
                             a: chunks => (
-                                <LinkWrapper>
-                                    <LearnMoreButton url={UNINSTALL_BRIDGE_URL}>
-                                        {chunks}
-                                    </LearnMoreButton>
-                                </LinkWrapper>
+                                <Link href={uninstallBridgeUrl} icon="arrowUpRight">
+                                    {chunks}
+                                </Link>
                             ),
                         }}
                     />
