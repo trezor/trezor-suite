21795dfd4451d4ad6d3920a5777a3bc77140554e
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-10-08 13:08:27 +0200

    chore: bump react-hook-form + @hookform/resolvers

--- suite-native/module-trading/src/hooks/general/useSheetControls.ts
+++ suite-native/module-trading/src/hooks/general/useSheetControls.ts
@@ -1,19 +1,27 @@
-import { useCallback } from 'react';
+import { Dispatch, useCallback } from 'react';
 
-import type { Path, UseFormReturn } from '@suite-native/forms';
+import type { FieldPathValue, Path, UseFormReturn } from '@suite-native/forms';
 
 import { useBottomSheetControls } from './useBottomSheetControls';
 import { BuyFormValues } from '../../types/buy';
 import { ExchangeFormValues } from '../../types/exchange';
 import { SellFormValues } from '../../types/sell';
 
-export const useSheetControls = <
-    FormValues extends BuyFormValues | ExchangeFormValues | SellFormValues,
+type BottomSheetControls = ReturnType<typeof useBottomSheetControls>;
+type FormUnion = BuyFormValues | ExchangeFormValues | SellFormValues;
+// explicit return type declaration, because typescript reaches its limits when trying to infer type
+type SheetControls<
+    FormValues extends FormUnion,
     Key extends Path<FormValues>,
->(
+> = BottomSheetControls & {
+    selectedValue: FieldPathValue<FormValues, Key>;
+    setSelectedValue: Dispatch<FieldPathValue<FormValues, Key>>;
+};
+
+export const useSheetControls = <FormValues extends FormUnion, Key extends Path<FormValues>>(
     { setValue, watch }: UseFormReturn<FormValues>,
     key: Key,
-) => {
+): SheetControls<FormValues, Key> => {
     const bottomSheetControls = useBottomSheetControls();
 
     const selectedValue = watch(key);
