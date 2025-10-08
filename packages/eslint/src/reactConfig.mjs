e36dd1448faa041348bb7ab4c976de32d66023da
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-10-08 12:23:49 +0200

    chore(ci): bump eslint-related deps

--- packages/eslint/src/reactConfig.mjs
+++ packages/eslint/src/reactConfig.mjs
@@ -23,10 +23,10 @@ export const reactConfig = [
     },
 
     // React Hooks
+    ...pluginReactHooks.configs.recommended,
     {
         plugins: { 'react-hooks': pluginReactHooks },
         rules: {
-            ...pluginReactHooks.configs.recommended.rules,
             'react-hooks/rules-of-hooks': 'error',
             'react-hooks/exhaustive-deps': 'error',
         },
