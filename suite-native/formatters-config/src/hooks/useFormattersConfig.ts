f814965991f561a4cc3125edade60da6d4876fb8
Petr Knetl <petr.knetl@satoshilabs.com>
2025-10-09 12:02:19 +0200

    refactor(suite-native): pass locale to formatters config

--- suite-native/formatters-config/src/hooks/useFormattersConfig.ts
+++ suite-native/formatters-config/src/hooks/useFormattersConfig.ts
@@ -5,20 +5,22 @@ import { getCalendars } from 'expo-localization';
 
 import { FormatterProviderConfig } from '@suite-common/formatters';
 import { selectBaseCurrency, selectBitcoinAmountUnit } from '@suite-common/wallet-core';
+import { useLocale } from '@suite-native/intl';
 
 const is24HourFormat = getCalendars()[0].uses24hourClock ?? true;
 
 export const useFormattersConfig = (): FormatterProviderConfig => {
     const baseCurrencyCode = useSelector(selectBaseCurrency);
     const bitcoinAmountUnit = useSelector(selectBitcoinAmountUnit);
+    const locale = useLocale();
 
     return useMemo(
         () => ({
-            locale: 'en',
-            baseCurrency: baseCurrencyCode,
+            locale,
             bitcoinAmountUnit,
             is24HourFormat,
+            baseCurrency: baseCurrencyCode,
         }),
-        [baseCurrencyCode, bitcoinAmountUnit],
+        [baseCurrencyCode, bitcoinAmountUnit, locale],
     );
 };
