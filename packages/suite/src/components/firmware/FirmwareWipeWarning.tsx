a08b9354aaa10d8567c9b64c46e375459e466fa8
Seibei Iguchi <2836193+seibei-iguchi@users.noreply.github.com>
2025-10-10 11:21:40 +0200

    feat(suite): add FirmwareWipeWarning component for firmware switch alerts

---
+++ packages/suite/src/components/firmware/FirmwareWipeWarning.tsx
@@ -0,0 +1,18 @@
+import { Banner, Paragraph, Text } from '@trezor/components';
+
+import { ExtendedMessageDescriptor, Translation } from 'src/components/suite/Translation';
+
+export const FirmwareWipeWarning = () => {
+    const warningTranslationValues: ExtendedMessageDescriptor['values'] = {
+        b: chunks => <Text typographyStyle="callout">{chunks}</Text>,
+    };
+
+    return (
+        <Banner variant="destructive" icon="warning">
+            <Paragraph>
+                <Translation id="TR_FIRMWARE_SWITCH_WARNING_1" values={warningTranslationValues} />{' '}
+                <Translation id="TR_FIRMWARE_SWITCH_WARNING_2" values={warningTranslationValues} />
+            </Paragraph>
+        </Banner>
+    );
+};
