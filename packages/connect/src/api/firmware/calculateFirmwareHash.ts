23a5d931021244aa0de53f6886d420695ebfd57e
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-10-15 14:54:11 +0200

    fix(connect): FW hash check for T3W1 2.9.3

--- packages/connect/src/api/firmware/calculateFirmwareHash.ts
+++ packages/connect/src/api/firmware/calculateFirmwareHash.ts
@@ -1,6 +1,7 @@
 import { blake2sHex } from 'blakejs';
 
-import { DeviceModelInternal } from '@trezor/device-utils';
+import { DeviceModelInternal, VersionArray } from '@trezor/device-utils';
+import { versionUtils } from '@trezor/utils';
 
 // Size values are taken from
 // https://github.com/trezor/trezor-firmware/blob/56f9490c01b40e99d94fe5d8a283955800d86562/core/embed/models/T3T1/memory.h#L62
@@ -22,13 +23,41 @@ const firmwareSizeMap: Partial<Record<DeviceModelInternal, number>> = {
     [DeviceModelInternal.T3W1]: SIZE_T3W1,
 };
 
-export const calculateFirmwareHash = (
-    internal_model: DeviceModelInternal,
-    fw: ArrayBuffer,
-    key?: Buffer,
-) => {
+type CalculateFirmwareHashParams = {
+    internal_model: DeviceModelInternal;
+    fw: ArrayBuffer;
+    firmwareVersion: VersionArray;
+    key?: Buffer;
+};
+
+const getSizeForModel = ({
+    internal_model,
+    firmwareVersion,
+}: Pick<CalculateFirmwareHashParams, 'firmwareVersion' | 'internal_model'>) => {
     // Default is SIZE_T1B1 since some old T1 do not report internal_model.
     const size = firmwareSizeMap[internal_model] ?? SIZE_T1B1;
+
+    // Fix for T3W1 2.9.3, which had a bug causing the calculated area to be 16kB smaller.
+    // This can be removed once 2.9.3 is no longer supported.
+    if (
+        firmwareVersion !== undefined &&
+        internal_model === DeviceModelInternal.T3W1 &&
+        versionUtils.isEqual(firmwareVersion, [2, 9, 3])
+    ) {
+        return size - 16 * 1024;
+    }
+
+    return size;
+};
+
+export const calculateFirmwareHash = ({
+    internal_model,
+    firmwareVersion,
+    fw,
+    key,
+}: CalculateFirmwareHashParams) => {
+    // Default is SIZE_T1B1 since some old T1 do not report internal_model.
+    const size = getSizeForModel({ internal_model, firmwareVersion });
 
     const padding = size - fw.byteLength;
     if (padding < 0) {
