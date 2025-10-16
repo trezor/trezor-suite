8d24c71779af244e34d9ff7d835e6ab1df6f5bc1
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-10-16 15:19:35 +0200

    feat(suite-native): Mobile Trade: Introduce trading-residence module with state

---
+++ suite-native/trading-residence/src/selectors/residenceSelectors.ts
@@ -0,0 +1,7 @@
+import { TradingResidenceRootState } from '../reducers/residenceSlice';
+
+export const selectTradingResidenceCountry = (state: TradingResidenceRootState) =>
+    state.wallet.trading.residence.country;
+
+export const selectWasTradingResidenceOnboardingVisited = (state: TradingResidenceRootState) =>
+    state.wallet.trading.residence.wasOnboardingVisited;
