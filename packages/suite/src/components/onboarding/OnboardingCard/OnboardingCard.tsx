43f481a045b22a6705f1e2910b071711084a125d
Seibei Iguchi <2836193+seibei-iguchi@users.noreply.github.com>
2025-10-16 11:28:07 +0200

    fix(suite): onboarding card width

--- packages/suite/src/components/onboarding/OnboardingCard/OnboardingCard.tsx
+++ packages/suite/src/components/onboarding/OnboardingCard/OnboardingCard.tsx
@@ -76,6 +76,7 @@ export const OnboardingCard = ({
                 gap={20}
                 position={{ type: 'relative' }}
                 padding={{ top: iconName ? 48 : 0 }}
+                width="100%"
             >
                 {isBackDropVisible && (
                     <Box
