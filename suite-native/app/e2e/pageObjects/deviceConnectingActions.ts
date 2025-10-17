2c65b0417ec71e76c2a09b3aba031e5675f48070
juriczech <bohdan.juricek@gmail.com>
2025-10-17 09:28:27 +0200

    fix(suite-native): detox connecting device async

--- suite-native/app/e2e/pageObjects/deviceConnectingActions.ts
+++ suite-native/app/e2e/pageObjects/deviceConnectingActions.ts
@@ -1,8 +1,11 @@
 class DeviceConnectingActions {
     async waitForDeviceConnectingScreen() {
+        // Connecting screen is rendered when discovery is running in the background so we need to disable synchronization to reliably test its presence
+        await device.disableSynchronization();
         await waitFor(element(by.id('@screen/ConnectingDevice')))
             .toBeVisible()
             .withTimeout(10000);
+        await device.enableSynchronization();
     }
 }
 
