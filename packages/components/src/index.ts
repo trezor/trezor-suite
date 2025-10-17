a5b3c4d06b693ce6bde64cde96940bdb7061092d
Seibei Iguchi <2836193+seibei-iguchi@users.noreply.github.com>
2025-10-17 18:44:51 +0200

    feat(components): add NewButtonGroup component

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -21,6 +21,7 @@ export * from './components/Badge/Badge';
 export * from './components/buttons/Button/Button';
 export { type ButtonVariant } from './components/buttons/buttonStyleUtils';
 export * from './components/buttons/ButtonGroup/ButtonGroup';
+export * from './components/buttons/NewButtonGroup/NewButtonGroup';
 export * from './components/buttons/IconButton/IconButton';
 export { NewButton, type NewButtonProps } from './components/buttons/NewButton/NewButton';
 export * from './components/Icon/Icon';
