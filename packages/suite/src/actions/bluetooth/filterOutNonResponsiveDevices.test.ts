76bc04823564adc7232cd8c3374b67b36d05924a
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-10-17 10:12:34 +0200

    chore(desktop): extract getLastUpdatedLimitForDevice

--- packages/suite/src/actions/bluetooth/filterOutNonResponsiveDevices.test.ts
+++ packages/suite/src/actions/bluetooth/filterOutNonResponsiveDevices.test.ts
@@ -3,11 +3,16 @@ import * as envUtils from '@trezor/env-utils';
 
 import { DesktopBluetoothDevice } from './DesktopBluetoothDevice';
 import { createMockedBluetoothDevice } from './__tests__/createMockedBluetoothDevice';
-import { filterOutNonResponsiveDevices } from './filterOutNonResponsiveDevices';
+import {
+    filterOutNonResponsiveDevices,
+    getLastUpdatedLimitForDevice,
+} from './filterOutNonResponsiveDevices';
+
+const NOW = 8_000;
 
 describe(filterOutNonResponsiveDevices.name, () => {
     beforeAll(() => {
-        jest.spyOn(Date, 'now').mockReturnValue(8_000);
+        jest.spyOn(Date, 'now').mockReturnValue(NOW);
         jest.spyOn(envUtils, 'isLinux').mockReturnValue(false);
     });
 
@@ -20,13 +25,13 @@ describe(filterOutNonResponsiveDevices.name, () => {
             createMockedBluetoothDevice({
                 id: asBluetoothDeviceId('1'),
                 name: 'DeviceA',
-                lastUpdatedTimestamp: 4_000,
+                lastUpdatedTimestamp: NOW - 4_000,
                 connectionStatus: { type: 'pairing' },
             }),
             createMockedBluetoothDevice({
                 id: asBluetoothDeviceId('2'),
                 name: 'DeviceA',
-                lastUpdatedTimestamp: 4_000,
+                lastUpdatedTimestamp: NOW - 4_000,
                 connectionStatus: { type: 'pairing' },
             }),
         ];
@@ -40,17 +45,17 @@ describe(filterOutNonResponsiveDevices.name, () => {
             createMockedBluetoothDevice({
                 id: asBluetoothDeviceId('1'),
                 name: 'DeviceA',
-                lastUpdatedTimestamp: 4_000,
+                lastUpdatedTimestamp: NOW - 4_000,
             }),
             createMockedBluetoothDevice({
                 id: asBluetoothDeviceId('2'),
                 name: 'DeviceA',
-                lastUpdatedTimestamp: 4_000,
+                lastUpdatedTimestamp: NOW - 4_000,
             }),
             createMockedBluetoothDevice({
                 id: asBluetoothDeviceId('3'),
                 name: 'DeviceA',
-                lastUpdatedTimestamp: 9_000,
+                lastUpdatedTimestamp: NOW + 1_000,
             }),
         ];
 
@@ -63,12 +68,12 @@ describe(filterOutNonResponsiveDevices.name, () => {
             createMockedBluetoothDevice({
                 id: asBluetoothDeviceId('1'),
                 name: 'DeviceA',
-                lastUpdatedTimestamp: 7_500,
+                lastUpdatedTimestamp: NOW - 500,
             }),
             createMockedBluetoothDevice({
                 id: asBluetoothDeviceId('2'),
                 name: 'DeviceA',
-                lastUpdatedTimestamp: 7_000,
+                lastUpdatedTimestamp: NOW - 1_000,
             }),
         ];
 
@@ -81,23 +86,58 @@ describe(filterOutNonResponsiveDevices.name, () => {
             createMockedBluetoothDevice({
                 id: asBluetoothDeviceId('1'),
                 name: 'DeviceA',
-                lastUpdatedTimestamp: 6_000,
+                lastUpdatedTimestamp: NOW - 2_000,
                 rssi: -90,
             }),
             createMockedBluetoothDevice({
                 id: asBluetoothDeviceId('2'),
                 name: 'DeviceB',
-                lastUpdatedTimestamp: 5_000,
+                lastUpdatedTimestamp: NOW - 5_000,
+                rssi: -100,
+            }),
+            createMockedBluetoothDevice({
+                id: asBluetoothDeviceId('2'),
+                name: 'DeviceB',
+                lastUpdatedTimestamp: NOW - 5_001,
                 rssi: -100,
             }),
             createMockedBluetoothDevice({
                 id: asBluetoothDeviceId('3'),
                 name: 'DeviceC',
-                lastUpdatedTimestamp: 1_000,
+                lastUpdatedTimestamp: NOW - 7000,
             }),
         ];
 
         const result = filterOutNonResponsiveDevices(weakSignalDevices);
         expect(result).toHaveLength(2);
+    });
+});
+
+describe(getLastUpdatedLimitForDevice.name, () => {
+    afterAll(() => {
+        jest.restoreAllMocks();
+    });
+    it('calculates limit as per rssi for platforms other than linux', () => {
+        jest.spyOn(envUtils, 'isLinux').mockReturnValue(false);
+        expect(getLastUpdatedLimitForDevice(undefined)).toBe(3_000);
+        expect(getLastUpdatedLimitForDevice(-35)).toBe(3_000);
+        expect(getLastUpdatedLimitForDevice(-70)).toBe(3_000);
+        expect(getLastUpdatedLimitForDevice(-80)).toBe(3_000);
+        expect(getLastUpdatedLimitForDevice(-81)).toBe(4_000);
+        expect(getLastUpdatedLimitForDevice(-89)).toBe(4_000);
+        expect(getLastUpdatedLimitForDevice(-91)).toBe(5_000);
+        expect(getLastUpdatedLimitForDevice(-111)).toBe(7_000);
+    });
+
+    it('calculates limit as per rssi for linux', () => {
+        jest.spyOn(envUtils, 'isLinux').mockReturnValue(true);
+        expect(getLastUpdatedLimitForDevice(undefined)).toBe(5_000);
+        expect(getLastUpdatedLimitForDevice(-35)).toBe(5_000);
+        expect(getLastUpdatedLimitForDevice(-70)).toBe(5_000);
+        expect(getLastUpdatedLimitForDevice(-80)).toBe(5_000);
+        expect(getLastUpdatedLimitForDevice(-81)).toBe(6_000);
+        expect(getLastUpdatedLimitForDevice(-89)).toBe(6_000);
+        expect(getLastUpdatedLimitForDevice(-91)).toBe(7_000);
+        expect(getLastUpdatedLimitForDevice(-111)).toBe(9_000);
     });
 });
--- packages/suite/src/actions/bluetooth/filterOutNonResponsiveDevices.ts
+++ packages/suite/src/actions/bluetooth/filterOutNonResponsiveDevices.ts
@@ -8,13 +8,33 @@ export const NEARBY_DEVICES_LAST_UPDATED_LIMIT_LINUX = 5_000;
 
 const NEARBY_DEVICE_WEAK_SIGNAL_LIMIT = -80; // dBm
 
-const NEARBY_DEVICE_INCREASE_PER = 10; // dBm
-const NEARBY_DEVICE_INCREASE = 1_000; // ms
+const NEARBY_DEVICE_DECREASE_PER = 10; // dBm
+const NEARBY_DEVICE_DECREASE = -1_000; // ms
 
 const CONNECTION_STATUSES_TO_KEEP: DeviceBluetoothConnectionStatusType[] = [
     'connecting',
     'pairing',
 ] as const;
+
+/**
+ * For each additional 10 dBm below the weak signal limit, increase the limit by 2 seconds
+ * e.g. -90 dBm => 2 seconds increase, -100 dBm => 4 seconds increase, ...
+ */
+export const getLastUpdatedLimitForDevice = (rssi?: number) => {
+    const baseLimit = isLinux()
+        ? NEARBY_DEVICES_LAST_UPDATED_LIMIT_LINUX
+        : NEARBY_DEVICES_LAST_UPDATED_LIMIT;
+
+    if (rssi === undefined) return baseLimit;
+
+    const limit =
+        baseLimit +
+        Math.floor((rssi - NEARBY_DEVICE_WEAK_SIGNAL_LIMIT) / NEARBY_DEVICE_DECREASE_PER) *
+            NEARBY_DEVICE_DECREASE;
+
+    // if above NEARBY_DEVICE_WEAK_SIGNAL_LIMIT, return always base limit
+    return Math.max(limit, baseLimit);
+};
 
 /**
  * Filters out devices that have not been responsive for a certain period of time.
@@ -24,25 +44,10 @@ const CONNECTION_STATUSES_TO_KEEP: DeviceBluetoothConnectionStatusType[] = [
  */
 export const filterOutNonResponsiveDevices = (devices: DesktopBluetoothDevice[]) => {
     const now = Date.now();
-    const baseLimit = isLinux()
-        ? NEARBY_DEVICES_LAST_UPDATED_LIMIT_LINUX
-        : NEARBY_DEVICES_LAST_UPDATED_LIMIT;
 
-    return devices.filter(device => {
-        let limit = baseLimit;
-
-        if (device.rssi && device.rssi <= NEARBY_DEVICE_WEAK_SIGNAL_LIMIT) {
-            // for each additional 10 dBm below the weak signal limit, increase the limit by 2 seconds
-            // e.g. -90 dBm => 4 seconds increase, -100 dBm => 6 seconds increase, ...
-            limit +=
-                Math.floor(
-                    (NEARBY_DEVICE_WEAK_SIGNAL_LIMIT - device.rssi) / NEARBY_DEVICE_INCREASE_PER,
-                ) * NEARBY_DEVICE_INCREASE;
-        }
-
-        return (
-            device.lastUpdatedTimestamp >= now - limit ||
-            CONNECTION_STATUSES_TO_KEEP.includes(device.connectionStatus.type)
-        );
-    });
+    return devices.filter(
+        device =>
+            device.lastUpdatedTimestamp >= now - getLastUpdatedLimitForDevice(device.rssi) ||
+            CONNECTION_STATUSES_TO_KEEP.includes(device.connectionStatus.type),
+    );
 };
