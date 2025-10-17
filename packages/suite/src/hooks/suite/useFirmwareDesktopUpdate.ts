9d2988172a2f8fa58814c555088079f46d07baf6
Vojtěch Tranta <vojta.tranta@gmail.com>
2025-10-17 17:21:35 +0200

    feat(suite): display the device reconnect modal when after FW install device expects entering the pin

--- packages/suite/src/hooks/suite/useFirmwareDesktopUpdate.ts
+++ packages/suite/src/hooks/suite/useFirmwareDesktopUpdate.ts
@@ -20,7 +20,7 @@ export const useFirmwareDesktopUpdate = (
         selectIsDeviceConnectedViaBluetoothLowOnBattery,
     );
 
-    const { firmwareUpdate, reconnectEvent, operation, progress, ...rest } =
+    const { firmwareUpdate, originalDevice, reconnectEvent, operation, progress, ...rest } =
         useFirmwareInstallation({
             shouldSwitchFirmwareType,
         });
@@ -36,6 +36,14 @@ export const useFirmwareDesktopUpdate = (
 
     const restartingToBootloader = reconnectEvent && reconnectEvent.target === 'bootloader';
 
+    // NOTE: Asume that when the device is restarting back to normal mode and is PIN protected, the PIN will be requested and hence display "device modal"
+    const restartingToNormalWithPinProtection =
+        reconnectEvent &&
+        reconnectEvent.target === 'normal' &&
+        originalDevice?.features?.pin_protection &&
+        // NOTE: when the device is wiped, the PIN is also wiped
+        !rest.deviceWillBeWiped;
+
     const updateOperation: FirmwareOperationStatus =
         operation === 'restarting'
             ? {
@@ -49,11 +57,13 @@ export const useFirmwareDesktopUpdate = (
 
     return {
         ...rest,
+        originalDevice,
         firmwareUpdate: desktopFirmwareUpdate,
         toggleLowBatteryModal: useCallback(() => setShowLowBatteryModal(prev => !prev), []),
         showLowBatteryModal,
         reconnectEvent,
         // NOTE: on desktop, set the progress during restart to 0, when device is just going to the bootloader
         ...updateOperation,
+        showReconnectPrompt: rest.showReconnectPrompt || restartingToNormalWithPinProtection,
     };
 };
