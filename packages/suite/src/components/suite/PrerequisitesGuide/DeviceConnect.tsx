a92c1c0141bc7bf5d45be46d878b2ed061ee36bd
Jan Václavík <jvaclavik@gmail.com>
2025-10-22 14:02:49 +0200

    fix(suite): Fix find trezor button with webusb
    (cherry picked from commit 902dd6319973e61f346ff8b9551f52cec6d6e5a6)

--- packages/suite/src/components/suite/PrerequisitesGuide/DeviceConnect.tsx
+++ packages/suite/src/components/suite/PrerequisitesGuide/DeviceConnect.tsx
@@ -1,24 +1,11 @@
 import { Button } from '@trezor/components';
 
 import { toggleConnectionModal } from 'src/actions/device/deviceSlice';
-import { WebUsbButton } from 'src/components/suite';
 import { Translation } from 'src/components/suite/Translation';
-import { useDispatch, useSelector } from 'src/hooks/suite';
-import { selectHasTransportOfType } from 'src/selectors/suite/suiteSelectors';
+import { useDispatch } from 'src/hooks/suite';
 
 export const DeviceConnect = () => {
     const dispatch = useDispatch();
-    const isWebUsbTransport = useSelector(selectHasTransportOfType('WebUsbTransport'));
-
-    if (isWebUsbTransport) {
-        return (
-            <WebUsbButton
-                size="medium"
-                data-testid="@webusb-button"
-                translationId="TR_CHECK_FOR_DEVICES"
-            />
-        );
-    }
 
     return (
         <Button minWidth={240} size="large" onClick={() => dispatch(toggleConnectionModal())}>
--- packages/suite/src/views/suite/SwitchDevice/DeviceItem/DeviceHeader.tsx
+++ packages/suite/src/views/suite/SwitchDevice/DeviceItem/DeviceHeader.tsx
@@ -1,23 +1,17 @@
 import { ReactNode } from 'react';
 
 import { getDeviceInternalModel } from '@suite-common/suite-utils';
-import { selectSelectedDevice } from '@suite-common/wallet-core';
 import { Box, IconButton, Row, TOOLTIP_DELAY_LONG, Tooltip } from '@trezor/components';
 import { spacings } from '@trezor/theme';
 
-import { WebUsbButton } from 'src/components/suite';
 import { Translation } from 'src/components/suite/Translation';
-import { WebUsbIconButton } from 'src/components/suite/WebUsbButton';
 import { DeviceStatus } from 'src/components/suite/layouts/SuiteLayout/DeviceSelector/DeviceStatus';
-import { useSelector } from 'src/hooks/suite';
-import { selectHasTransportOfType } from 'src/selectors/suite/suiteSelectors';
 import { ForegroundAppProps, TrezorDevice } from 'src/types/suite';
 
 type DeviceHeaderProps = {
     device: TrezorDevice;
     onCancel?: ForegroundAppProps['onCancel'];
     onBackButtonClick?: () => void;
-    isFindTrezorVisible?: boolean;
     isDeviceStatusVisible?: boolean;
     actions?: ReactNode | null;
 };
@@ -26,13 +20,9 @@ export const DeviceHeader = ({
     onCancel,
     device,
     onBackButtonClick,
-    isFindTrezorVisible = false,
     isDeviceStatusVisible = true,
     actions,
 }: DeviceHeaderProps) => {
-    const selectedDevice = useSelector(selectSelectedDevice);
-    const isWebUsbTransport = useSelector(selectHasTransportOfType('WebUsbTransport'));
-    const isDeviceConnected = selectedDevice?.connected === true;
     const deviceModelInternal = getDeviceInternalModel(device);
 
     const isDefaultCancelVisible = !actions && actions !== null && onCancel;
@@ -77,13 +67,6 @@ export const DeviceHeader = ({
             )}
 
             <Row gap={spacings.xxs} margin={{ left: 'auto' }}>
-                {isWebUsbTransport &&
-                    isFindTrezorVisible &&
-                    (isDeviceConnected ? (
-                        <WebUsbIconButton variant="tertiary" size="small" />
-                    ) : (
-                        <WebUsbButton variant="primary" size="tiny" />
-                    ))}
                 {isDefaultCancelVisible && (
                     <Tooltip delayShow={TOOLTIP_DELAY_LONG} content={<Translation id="TR_CLOSE" />}>
                         <IconButton
