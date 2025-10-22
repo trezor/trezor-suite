8745d51261bbca05b9ab37e1d80573b9c334a0b2
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-10-22 12:13:37 +0200

    feat(suite-native): Mobile Trade: do not display Country of residence in trading form

---
+++ suite-native/module-trading/src/components/general/__tests__/TradingCountryOfResidencePicker.comp.test.tsx
@@ -0,0 +1,73 @@
+import { TradingCountryOption } from '@suite-common/trading';
+import { yup } from '@suite-common/validators';
+import { EventType, analytics } from '@suite-native/analytics';
+import { FeatureFlag } from '@suite-native/feature-flags';
+import { Form, useForm } from '@suite-native/forms';
+import type { UseFormReturn } from '@suite-native/forms';
+import {
+    PreloadedState,
+    act,
+    renderHookWithStoreProviderAsync,
+    renderWithStoreProviderAsync,
+    userEvent,
+} from '@suite-native/test-utils';
+
+import { TradingCountryOfResidencePicker } from '../TradingCountryOfResidencePicker';
+
+describe('TradingCountryOfResidencePicker', () => {
+    let form: UseFormReturn<{ country: TradingCountryOption }>;
+
+    const renderForm = () =>
+        renderHookWithStoreProviderAsync(() =>
+            useForm<{ country: TradingCountryOption }>({ validation: yup.object() }),
+        );
+
+    const renderCountryOfResidencePicker = (preloadedState: PreloadedState) =>
+        renderWithStoreProviderAsync(
+            <TradingCountryOfResidencePicker testID="testID" context="buy" />,
+            {
+                wrapper: ({ children }) => <Form form={form}>{children}</Form>,
+                preloadedState,
+            },
+        );
+
+    beforeEach(async () => {
+        const { result } = await renderForm();
+        form = result.current;
+    });
+
+    it('should use country from form', async () => {
+        act(() => {
+            form.setValue('country', { value: 'US', label: 'United States' });
+        });
+
+        const { getByTestId } = await renderCountryOfResidencePicker({});
+
+        expect(getByTestId('testID/value')).toHaveTextContent('United States');
+    });
+
+    it('should call analytics on country change', async () => {
+        const reportSpy = jest.spyOn(analytics, 'report');
+
+        const { getByText } = await renderCountryOfResidencePicker({});
+
+        await userEvent.press(getByText('Country of residence'));
+        await userEvent.press(getByText(/Algeria/));
+
+        expect(reportSpy).toHaveBeenCalledWith({
+            type: EventType.TradingParameterChanged,
+            payload: {
+                type: 'buy',
+                parameter: 'country',
+            },
+        });
+    });
+
+    it('should render nothing when isTradingResidenceCheckEnabled FF is true', async () => {
+        const { toJSON } = await renderCountryOfResidencePicker({
+            featureFlags: { [FeatureFlag.IsTradingResidenceCheckEnabled]: true },
+        });
+
+        expect(toJSON()).toBeNull();
+    });
+});
--- suite-native/module-trading/src/components/sell/payment/SellCountryOfResidencePicker.tsx
+++
@@ -1,8 +0,0 @@
-import { CountryOfResidencePicker } from '@suite-native/trading-residence';
-
-const COUNTRY_PICKER_TEST_ID = '@trading/sell/country';
-
-export const SellCountryOfResidencePicker = () => 
-    // TODO 22469 FF switch
-     <CountryOfResidencePicker testID={COUNTRY_PICKER_TEST_ID} context="sell" />
-;
