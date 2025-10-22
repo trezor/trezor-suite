159fb9a30b253264f04375f2158b4a62b626234e
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-10-22 12:13:37 +0200

    feat(suite-native): Mobile Trade: do not display Country of residence in trading form

--- suite-native/module-trading/src/components/buy/BuyCountryOfResidencePicker.tsx
+++
@@ -1,8 +0,0 @@
-import { CountryOfResidencePicker } from '@suite-native/trading-residence';
-
-const COUNTRY_PICKER_TEST_ID = '@trading/buy/country';
-
-export const BuyCountryOfResidencePicker = () => 
-    // TODO 22469 FF switch
-     <CountryOfResidencePicker testID={COUNTRY_PICKER_TEST_ID} context="buy" />
-;
