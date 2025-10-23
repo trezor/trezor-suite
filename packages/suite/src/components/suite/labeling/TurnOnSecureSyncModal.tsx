7cebded478ddb7f90598c5ebe7ce27b702860dd8
Matěj Husák <matej.husak@icloud.com>
2025-10-24 01:52:43 +0200

    feat(suite): added labeling translations

--- packages/suite/src/components/suite/labeling/TurnOnSecureSyncModal.tsx
+++ packages/suite/src/components/suite/labeling/TurnOnSecureSyncModal.tsx
@@ -1,47 +1,51 @@
-import { labelingActions } from '@suite-common/local-first-storage';
-import { Card, List, Modal, Paragraph } from '@trezor/components';
+import { Card, Icon, List, Modal, Paragraph } from '@trezor/components';
 
-import { useDispatch } from 'src/hooks/suite';
 import { useLabelingCombined } from 'src/hooks/suite/useLabelingCombined';
+
+import { Translation } from '../Translation';
 
 type TurnOnSecureSyncModalProps = {
     onClose: () => void;
 };
 
-// TODO add translations
 export const TurnOnSecureSyncModal = ({ onClose }: TurnOnSecureSyncModalProps) => {
-    const dispatch = useDispatch();
     const { localFirstEnableIfNeeded } = useLabelingCombined({ deviceStaticSessionId: undefined });
 
     const onSwitch = () => {
         localFirstEnableIfNeeded();
-        dispatch(labelingActions.updateshowLocalFirstStorage({ isShownInSettings: false }));
+        onClose();
     };
 
     return (
         <Modal
-            heading="Turn on secure sync to use labels"
-            description="Secure sync keeps your data consistent across all of your devices."
+            heading={<Translation id="TR_TURN_ON_SECURE_SYNC_LABELS_MODAL_HEADING" />}
+            description={<Translation id="TR_TURN_ON_SECURE_SYNC_LABELS_MODAL_DESCRIPTION" />}
             onCancel={onClose}
             bottomContent={
                 <>
-                    <Modal.Button onClick={onSwitch}>Turn on</Modal.Button>
+                    <Modal.Button onClick={onSwitch}>
+                        <Translation id="TR_TURN_ON_SECURE_SYNC" />
+                    </Modal.Button>
                     <Modal.Button onClick={onClose} variant="tertiary">
-                        Cancel
+                        <Translation id="TR_CANCEL" />
                     </Modal.Button>
                 </>
             }
         >
             <Card>
-                <List>
+                <List
+                    bulletGap={0}
+                    gap={0}
+                    bulletComponent={<Icon name="dot" variant="tertiary" size={32} />}
+                >
                     <List.Item>
-                        <Paragraph>
-                            Data is stored locally and synced only with devices you authorize.
+                        <Paragraph variant="tertiary">
+                            <Translation id="TR_TURN_ON_SECURE_SYNC_DATA_STORED_LOCALLY" />
                         </Paragraph>
                     </List.Item>
                     <List.Item>
-                        <Paragraph>
-                            Only devices you authorize with your Trezor can decrypt the data.
+                        <Paragraph variant="tertiary">
+                            <Translation id="TR_TURN_ON_SECURE_SYNC_ONLY_AUTHORIZED_DEVICES" />
                         </Paragraph>
                     </List.Item>
                 </List>
