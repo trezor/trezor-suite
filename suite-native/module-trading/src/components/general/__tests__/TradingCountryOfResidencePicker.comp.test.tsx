43887135db7c9963de6c52c9ccd4d08f7ab003a4
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-10-23 22:10:53 +0200

    fixup! fixup! feat(suite-native): Mobile Trade: allow to set Trading location in onboarding

--- suite-native/module-trading/src/components/general/__tests__/TradingCountryOfResidencePicker.comp.test.tsx
+++ suite-native/module-trading/src/components/general/__tests__/TradingCountryOfResidencePicker.comp.test.tsx
@@ -1,7 +1,6 @@
 import { TradingCountryOption } from '@suite-common/trading';
 import { yup } from '@suite-common/validators';
 import { EventType, analytics } from '@suite-native/analytics';
-import { FeatureFlag } from '@suite-native/feature-flags';
 import { Form, useForm } from '@suite-native/forms';
 import type { UseFormReturn } from '@suite-native/forms';
 import {
@@ -12,14 +11,19 @@ import {
     userEvent,
 } from '@suite-native/test-utils';
 
+import {
+    residenceCheckDisabledState,
+    residenceCheckEnabledState,
+} from '../../../__fixtures__/residenceCheckState';
 import { TradingCountryOfResidencePicker } from '../TradingCountryOfResidencePicker';
 
 describe('TradingCountryOfResidencePicker', () => {
     let form: UseFormReturn<{ country: TradingCountryOption }>;
 
     const renderForm = () =>
-        renderHookWithStoreProviderAsync(() =>
-            useForm<{ country: TradingCountryOption }>({ validation: yup.object() }),
+        renderHookWithStoreProviderAsync(
+            () => useForm<{ country: TradingCountryOption }>({ validation: yup.object() }),
+            { preloadedState: residenceCheckDisabledState },
         );
 
     const renderCountryOfResidencePicker = (preloadedState: PreloadedState) =>
@@ -41,7 +45,7 @@ describe('TradingCountryOfResidencePicker', () => {
             form.setValue('country', { value: 'US', label: 'United States' });
         });
 
-        const { getByTestId } = await renderCountryOfResidencePicker({});
+        const { getByTestId } = await renderCountryOfResidencePicker(residenceCheckDisabledState);
 
         expect(getByTestId('testID/value')).toHaveTextContent('United States');
     });
@@ -49,7 +53,7 @@ describe('TradingCountryOfResidencePicker', () => {
     it('should call analytics on country change', async () => {
         const reportSpy = jest.spyOn(analytics, 'report');
 
-        const { getByText } = await renderCountryOfResidencePicker({});
+        const { getByText } = await renderCountryOfResidencePicker(residenceCheckDisabledState);
 
         await userEvent.press(getByText('Country of residence'));
         await userEvent.press(getByText(/Algeria/));
@@ -64,9 +68,7 @@ describe('TradingCountryOfResidencePicker', () => {
     });
 
     it('should render nothing when isTradingResidenceCheckEnabled FF is true', async () => {
-        const { toJSON } = await renderCountryOfResidencePicker({
-            featureFlags: { [FeatureFlag.IsTradingResidenceCheckEnabled]: true },
-        });
+        const { toJSON } = await renderCountryOfResidencePicker(residenceCheckEnabledState);
 
         expect(toJSON()).toBeNull();
     });
