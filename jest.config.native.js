a125ec32aa5c88538a8a859788190a36d30c05b7
Petr Knetl <petr.knetl@satoshilabs.com>
2025-10-24 09:09:31 +0200

    chore(suite-native): bump react native related dependencies (2025-10)

--- jest.config.native.js
+++ jest.config.native.js
@@ -24,7 +24,7 @@ module.exports = {
         '\\.(js|jsx|ts|tsx)$': ['babel-jest', babelConfig],
     },
     transformIgnorePatterns: [
-        'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|@shopify/react-native-skia|@noble|@evolu|nanoid|msgpackr)',
+        'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|@shopify/react-native-skia|@noble|@evolu|nanoid|msgpackr|@gorhom)',
     ],
     setupFiles: [
         '<rootDir>/../../node_modules/@shopify/react-native-skia/jestSetup.js',
