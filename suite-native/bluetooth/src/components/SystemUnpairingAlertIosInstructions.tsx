c773fb04116305a29dadd357fae8ce7bf795f762
Matej Kriz <matej@matejkriz.cz>
2025-10-24 14:53:58 +0200

    fix(suite-native): never try to use device name in BLE pairing instructions
    - Prevent wrong device name to be used (i.e. "Portfolio tracker")

--- suite-native/bluetooth/src/components/SystemUnpairingAlertIosInstructions.tsx
+++ suite-native/bluetooth/src/components/SystemUnpairingAlertIosInstructions.tsx
@@ -1,41 +1,30 @@
 import { BottomSheetListItem, Box, Text, VStack } from '@suite-native/atoms';
-import { Translation, TxKeyPath, useTranslate } from '@suite-native/intl';
+import { Translation, TxKeyPath } from '@suite-native/intl';
 
 type SystemUnpairingAlertIosInstructionsProps = {
     translationKey: TxKeyPath;
-    deviceName?: string;
 };
 
 export const SystemUnpairingAlertIosInstructions = ({
     translationKey,
-    deviceName,
-}: SystemUnpairingAlertIosInstructionsProps) => {
-    const { translate } = useTranslate();
-
-    return (
-        <Box>
-            <Text>
-                <Translation id={translationKey} />
-            </Text>
-            <VStack paddingTop="sp24">
-                <BottomSheetListItem
-                    iconNumber={1}
-                    translationKey="bluetooth.alerts.pairingInstructions.step1"
-                />
-                <BottomSheetListItem
-                    iconNumber={2}
-                    translationKey="bluetooth.alerts.pairingInstructions.step2"
-                    translationValues={{
-                        deviceName:
-                            deviceName ??
-                            translate('bluetooth.alerts.pairingFailed.deviceNamePlaceholder'),
-                    }}
-                />
-                <BottomSheetListItem
-                    iconNumber={3}
-                    translationKey="bluetooth.alerts.pairingInstructions.step3"
-                />
-            </VStack>
-        </Box>
-    );
-};
+}: SystemUnpairingAlertIosInstructionsProps) => (
+    <Box>
+        <Text>
+            <Translation id={translationKey} />
+        </Text>
+        <VStack paddingTop="sp24">
+            <BottomSheetListItem
+                iconNumber={1}
+                translationKey="bluetooth.alerts.pairingInstructions.step1"
+            />
+            <BottomSheetListItem
+                iconNumber={2}
+                translationKey="bluetooth.alerts.pairingInstructions.step2"
+            />
+            <BottomSheetListItem
+                iconNumber={3}
+                translationKey="bluetooth.alerts.pairingInstructions.step3"
+            />
+        </VStack>
+    </Box>
+);
