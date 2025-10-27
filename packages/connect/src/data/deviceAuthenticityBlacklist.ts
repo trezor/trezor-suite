4f1e8b5f2c278975b7efed27e6e3ed2a03ad79f3
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-10-27 08:08:18 +0100

    chore(device-authenticity): create new package @trezor/device-authenticity

--- packages/connect/src/data/deviceAuthenticityBlacklist.ts
+++
@@ -1,9 +0,0 @@
-import { DeviceAuthenticityBlacklistConfig } from './deviceAuthenticityBlacklistConfig';
-
-export const deviceAuthenticityBlacklist: DeviceAuthenticityBlacklistConfig = {
-    version: 1,
-    blacklistedCaPubKeys: [],
-    debug: {
-        blacklistedCaPubKeys: [],
-    },
-};
--- packages/device-authenticity/src/tests/verifyAuthenticityProof.test.ts
+++ packages/device-authenticity/src/tests/verifyAuthenticityProof.test.ts
@@ -1,6 +1,6 @@
-import { DeviceAuthenticityBlacklistConfig } from '../../../data/deviceAuthenticityBlacklistConfig';
-import { DeviceAuthenticityConfig } from '../../../data/deviceAuthenticityConfigTypes';
-import { VerifyAuthenticityProofParams } from '../verifyAuthenticity/types';
+import type { DeviceAuthenticityBlacklistConfig } from '../config/deviceAuthenticityBlacklistConfigTypes';
+import type { DeviceAuthenticityConfig } from '../config/deviceAuthenticityConfigTypes';
+import { VerifyAuthenticityProofParams } from '../types';
 import { verifyAuthenticityProof } from '../verifyAuthenticityProof';
 
 const CA_CERT_OPTIGA =
