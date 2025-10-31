83957c2007dc9873da82e9f0e0d65a63dbc7a3c1
Tomas Martykan <t.martykan@gmail.com>
2025-10-31 09:52:45 +0100

    feat(connect-web): enable suite-web instead of old popup

--- packages/connect-web/src/index.ts
+++ packages/connect-web/src/index.ts
@@ -53,7 +53,7 @@ const impl = new TrezorConnectDynamic<
                 console.warn(`Invalid coreMode: ${settings.coreMode}`);
             }
 
-            return 'iframe';
+            return 'core-in-suite-web';
         }
     },
     handleBeforeCall: async () => {
@@ -77,7 +77,7 @@ const impl = new TrezorConnectDynamic<
             impl.getTargetType() === 'core-in-suite-desktop' &&
             (errorCode === 'Desktop_ConnectionMissing' || errorCode === 'Method_Unsupported')
         ) {
-            await impl.switchTarget('iframe');
+            await impl.switchTarget('core-in-suite-web');
 
             return true;
         }
