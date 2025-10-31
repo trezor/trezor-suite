a1c2d5bb428cc314ae925a3546012d45c65f23db
Peter Sanderson <dierk.deidel@protonmail.com>
2025-10-31 14:14:26 +0100

    fix: allow blob img-src for CSP

--- packages/suite-desktop-core/src/modules/csp.ts
+++ packages/suite-desktop-core/src/modules/csp.ts
@@ -20,7 +20,7 @@ const createCspRules = ({ nonce }: CreateCspRulesParams) => [
     // Allow all API calls (Can't be restricted bc of custom backends)
     'connect-src *',
     // Allow images from trezor.io
-    "img-src 'self' *.trezor.io",
+    "img-src 'self' blob: *.trezor.io",
 ];
 
 export const init: ModuleInit = ({ cspNonce }) => {
