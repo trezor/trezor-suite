d1f6c30c50444ba356ead8a7aa4b3ddf0540779c
yanas <yanas@yanas.cz>
2025-11-03 14:36:09 +0100

    fix(suite-native): scan for Bluetooth devices only when the app is active

---
+++ suite-native/bluetooth/src/hooks/useBluetoothScanner.ts
@@ -0,0 +1,25 @@
+import { useCallback } from 'react';
+import { AppState } from 'react-native';
+
+import { BleError, bluetoothManager } from '@trezor/transport-native-bluetooth';
+
+export const useBluetoothScanner = () => {
+    const startDeviceScan = useCallback((errorHandler?: (error: BleError) => void) => {
+        bluetoothManager.startDeviceScan(errorHandler);
+
+        const subscription = AppState.addEventListener('change', nextAppState => {
+            if (nextAppState === 'active') {
+                bluetoothManager.startDeviceScan(errorHandler);
+            } else {
+                bluetoothManager.stopDeviceScan();
+            }
+        });
+
+        return () => {
+            subscription.remove();
+            bluetoothManager.stopDeviceScan();
+        };
+    }, []);
+
+    return { startDeviceScan };
+};
