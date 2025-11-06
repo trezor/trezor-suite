a246b981361b364d2b4c7b215920e2b230e8fac5
Martin Varmuza <mvarmuza@gmail.com>
2025-11-06 08:26:09 +0100

    chore(components): remove unused getSafeWindowSize

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -117,7 +117,6 @@ export * from './constants/keyboardEvents';
 export * from './utils/useScrollShadow';
 export * from './utils/transientProps';
 export { useMediaQuery } from './utils/useMediaQuery';
-export { getSafeWindowSize } from './utils/getSafeWindowSize';
 
 export { intermediaryTheme } from './config/colors';
 export type { SuiteThemeColors } from './config/colors';
