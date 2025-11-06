42097707dcf8ca21bd5b396649735278ccb84dce
Jan Václavík <jvaclavik@gmail.com>
2025-11-06 19:21:54 +0100

    refactor(ds): Refactor flags

--- packages/components/src/components/Flag/Flag.tsx
+++ packages/components/src/components/Flag/Flag.tsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from 'react';
-
 import styled from 'styled-components';
 
 import { FLAGS } from './flags';
-import { SkeletonRectangle } from '../skeletons/SkeletonRectangle';
+import { resolveStaticPath } from '../../utils/resolveStaticPath';
+import { IMAGES_PATH } from '../Image/Image';
 
 export type FlagType = keyof typeof FLAGS;
 
@@ -18,26 +17,12 @@ export interface FlagProps {
     size?: number;
 }
 
-export const Flag = ({ size = 24, country, className }: FlagProps) => {
-    const [src, setSrc] = useState('');
-    useEffect(() => {
-        import(`../../images/flags/${country.toLowerCase()}.svg`)
-            .then(module => {
-                setSrc(module.default);
-            })
-            .catch(err => {
-                // NOTE: keep error here as this is not a critical issue
-                console.error('Flag image loading error: ', err);
-            });
-    }, [country]);
-
-    return (
-        <Wrapper>
-            {src ? (
-                <img src={src} width={`${size}px`} alt={`flag-${country}`} className={className} />
-            ) : (
-                <SkeletonRectangle width={size} height={size} />
-            )}
-        </Wrapper>
-    );
-};
+export const Flag = ({ size = 24, country }: FlagProps) => (
+    <Wrapper>
+        <img
+            src={resolveStaticPath(`${IMAGES_PATH}/flags/${country.toLowerCase()}.svg`)}
+            width={`${size}px`}
+            alt={`flag-${country}`}
+        />
+    </Wrapper>
+);
