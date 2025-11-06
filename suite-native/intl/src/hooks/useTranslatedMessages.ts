1bfad1bee5cae65286f85bc401edf86fa5a97115
Petr Knetl <petr.knetl@satoshilabs.com>
2025-11-06 14:38:39 +0100

    feat(suite-native): czech language as community (beta)

--- suite-native/intl/src/hooks/useTranslatedMessages.ts
+++ suite-native/intl/src/hooks/useTranslatedMessages.ts
@@ -1,31 +1,35 @@
 import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 
+import { LocaleSliceRootState, selectIsLanguageLocaleSupported } from '../localeSlice';
 import { messages as defaultMessages } from '../messages';
-import { NativeLocale, isOfficiallySupportedNativeLocale } from '../types';
+import { SupportedLanguage } from '../types';
 import { flatten } from '../utils';
+import { useLocale } from './useLocale';
 
 const LANGUAGE_TRANSLATIONS_MAP = {
     'en-US': require('../../translations/en-US.json'),
     'cs-CZ': require('../../translations/cs-CZ.json'),
-} as const satisfies Record<NativeLocale, any>;
+} as const satisfies Record<SupportedLanguage, any>;
 
 // default values defined during the development.
 const englishFallback = flatten(defaultMessages);
 
-type UseTranslatedMessagesProps = {
-    locale: string;
-};
-
-export const useTranslatedMessages = ({ locale }: UseTranslatedMessagesProps) => {
+export const useTranslatedMessages = () => {
     const [messages, setMessages] = useState<{ [key: string]: string }>({});
+    const locale = useLocale();
+
+    const isLanguageLocaleSupported = useSelector((state: LocaleSliceRootState) =>
+        selectIsLanguageLocaleSupported(state, locale),
+    );
 
     useEffect(() => {
-        const localizedMessages = isOfficiallySupportedNativeLocale(locale)
-            ? LANGUAGE_TRANSLATIONS_MAP[locale]
+        const localizedMessages = isLanguageLocaleSupported
+            ? LANGUAGE_TRANSLATIONS_MAP[locale as SupportedLanguage]
             : {};
 
         setMessages({ ...englishFallback, ...localizedMessages });
-    }, [locale]);
+    }, [locale, isLanguageLocaleSupported]);
 
     return messages;
 };
