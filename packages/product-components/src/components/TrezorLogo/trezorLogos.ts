eef1d1f5cff44b1b0d50fa953fcd0eb7a30e78e7
Matej Kriz <matej@matejkriz.cz>
2025-11-07 12:26:27 +0100

    feat(analytics): reuse settings/device/wipe event on mobile

--- packages/product-components/src/components/TrezorLogo/trezorLogos.ts
+++ packages/product-components/src/components/TrezorLogo/trezorLogos.ts
@@ -1,8 +1,9 @@
+import { IMAGES_PATH } from '@trezor/components';
+
+import { resolveStaticPath } from '../../utils/resolveStaticPath';
+
 export const LOGOS: { [key: string]: any } = {
-    HORIZONTAL: require('../../../../components/src/images/trezor_logo_horizontal.svg'),
-    VERTICAL: require('../../../../components/src/images/trezor_logo_vertical.svg'),
-    SYMBOL: require('../../../../components/src/images/trezor_logo_symbol.svg'),
-    SUITE: require('../../../../components/src/images/trezor_logo_suite.svg'),
-    SUITE_SQUARE: require('../../../../components/src/images/trezor_logo_suite_square.svg'),
-    SUITE_COMPACT: require('../../../../components/src/images/trezor_logo_suite_compact.svg'),
+    HORIZONTAL: resolveStaticPath(`${IMAGES_PATH}/logos/trezor_logo_horizontal.svg`),
+    VERTICAL: resolveStaticPath(`${IMAGES_PATH}/logos/trezor_logo_vertical.svg`),
+    SYMBOL: resolveStaticPath(`${IMAGES_PATH}/logos/trezor_logo_symbol.svg`),
 };
