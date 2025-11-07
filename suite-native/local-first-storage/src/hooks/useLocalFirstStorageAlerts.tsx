2e89ba8be4cd175ce576146a00b819170b9ca04b
Matěj Husák <matej.husak@icloud.com>
2025-11-07 02:12:35 +0100

    feat(native): added localFirstStorageAlerts and tweaks

---
+++ suite-native/local-first-storage/src/hooks/useLocalFirstStorageAlerts.tsx
@@ -0,0 +1,46 @@
+import { useSelector } from 'react-redux';
+
+import { evoluReactNativeDeps } from '@evolu/react-native/expo-sqlite';
+
+import { useLocalFirstStorage } from '@suite-common/local-first-storage';
+import { selectSelectedDevice } from '@suite-common/wallet-core';
+import { useAlert } from '@suite-native/alerts';
+import { Translation } from '@suite-native/intl';
+
+export const useLocalFirstStorageAlerts = () => {
+    const { showAlert } = useAlert();
+
+    const device = useSelector(selectSelectedDevice);
+
+    const { disableLocalFirstStorageIfNeeded, enableLocalFirstStorageIfNeeded } =
+        useLocalFirstStorage({
+            device,
+        });
+
+    const showLocalFirstStorageDisableConfirmationAlert = () => {
+        showAlert({
+            title: <Translation id="labeling.disableAlert.title" />,
+            description: <Translation id="labeling.disableAlert.description" />,
+            primaryButtonTitle: <Translation id="labeling.disableAlert.cta" />,
+            onPressPrimaryButton: disableLocalFirstStorageIfNeeded,
+            secondaryButtonTitle: <Translation id="generic.buttons.cancel" />,
+        });
+    };
+
+    const showLocalFirstStorageEnableConfirmationAlert = () => {
+        showAlert({
+            title: <Translation id="labeling.enableAlert.title" />,
+            description: <Translation id="labeling.enableAlert.description" />,
+            primaryButtonTitle: <Translation id="labeling.enableAlert.cta" />,
+            onPressPrimaryButton: () => {
+                enableLocalFirstStorageIfNeeded(evoluReactNativeDeps);
+            },
+            secondaryButtonTitle: <Translation id="generic.buttons.cancel" />,
+        });
+    };
+
+    return {
+        showLocalFirstStorageDisableConfirmationAlert,
+        showLocalFirstStorageEnableConfirmationAlert,
+    };
+};
