0df9ef0733bbfca4b43e9f9f44cb6b0731397d6f
Jiří Čermák <jiri.cermak@satoshilabs.com>
2025-11-13 12:08:36 +0100

    fix(suite): fix perf issue of GlobalSendModal with a lot of items

--- packages/suite/src/hooks/general/useCurrentRef.ts
+++ packages/suite/src/hooks/general/useCurrentRef.ts
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 
 export function useCurrentRef<T>(value: T) {
-    const ref = useRef<T>(null);
+    const ref = useRef<T>(value);
 
     useEffect(() => {
         ref.current = value;
