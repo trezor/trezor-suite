585fc4f865ef037890681f3d184cc6ac51927421
Vojtěch Tranta <vojta.tranta@gmail.com>
2025-11-14 10:22:15 +0100

    fix(suite): add debug option to delete all evolu keys

--- packages/suite/src/views/suite/SwitchDevice/DeviceItem/LocalFirstStorageDebug.tsx
+++ packages/suite/src/views/suite/SwitchDevice/DeviceItem/LocalFirstStorageDebug.tsx
@@ -1,11 +1,16 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { AcquiredDevice } from '@suite-common/suite-types';
+import { deviceActions } from '@suite-common/wallet-core';
 import { parseDeviceStaticSessionId } from '@suite-common/wallet-utils';
 import { Code, Row, Text, Tooltip } from '@trezor/components';
 import { spacings } from '@trezor/theme';
 
+import { useDispatch } from 'src/hooks/suite';
+
 import { useLabelingCombined } from '../../../../hooks/suite/useLabelingCombined';
 
 export const LocalFirstStorageDebug = ({ device }: { device: AcquiredDevice }) => {
+    const dispatch = useDispatch();
     const {
         legacyMetadataState,
         isLocalFirstStorageEnabled,
@@ -22,6 +27,18 @@ export const LocalFirstStorageDebug = ({ device }: { device: AcquiredDevice }) =
     }
 
     const { walletDescriptor, deviceId } = parseDeviceStaticSessionId(device.state.staticSessionId);
+
+    const handleResetKeysRequest = () => {
+        if (!device?.id) return;
+
+        dispatch(deviceActions.setLocalFirstStorageSecret({ device, evoluKeys: undefined }));
+        dispatch(
+            deviceActions.setLocalFirstDelegatedKey({
+                deviceId: device.id,
+                delegatedKey: null,
+            }),
+        );
+    };
 
     return isLocalFirstStorageEnabled ? (
         <Row gap={spacings.xxs}>
@@ -50,6 +67,17 @@ export const LocalFirstStorageDebug = ({ device }: { device: AcquiredDevice }) =
                     </Tooltip>
                 </>
             )}
+            <span
+                role="button"
+                tabIndex={0}
+                onClick={(event: React.MouseEvent<HTMLSpanElement>) => {
+                    event.stopPropagation();
+                    event.preventDefault();
+                    handleResetKeysRequest();
+                }}
+            >
+                ❌
+            </span>
         </Row>
     ) : null;
 };
