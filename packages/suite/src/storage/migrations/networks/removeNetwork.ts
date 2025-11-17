7d60bffbee213b0b8b7f00c30930244ec1185691
Marek Polak <marek.polak@satoshilabs.com>
2025-11-17 16:40:27 +0100

    fix(suite): fix removeNetwork migration util

--- packages/suite/src/storage/migrations/networks/removeNetwork.ts
+++ packages/suite/src/storage/migrations/networks/removeNetwork.ts
@@ -7,37 +7,23 @@ export async function removeNetwork(
     tx: IDBPTransaction<SuiteDBSchema, StoreNames<SuiteDBSchema>[], 'versionchange'>,
     removedNetworkSymbol: string,
 ) {
-    // remove a network transactions, change token address to contract
+    // remove a network transactions
     if (tx.objectStoreNames.contains('txs')) {
         await updateAll(tx, 'txs', tx => {
             if (tx.tx.symbol === removedNetworkSymbol) {
                 return null;
             }
 
-            tx.tx.tokens.forEach(token => {
-                // @ts-expect-error
-                token.contract = token.address;
-                // @ts-expect-error
-                delete token.address;
-            });
-
             return tx;
         });
     }
 
-    // remove a network accounts, change token address to contract
+    // remove a network accounts
     if (tx.objectStoreNames.contains('accounts')) {
         await updateAll(tx, 'accounts', account => {
             if (account.symbol === removedNetworkSymbol) {
                 return null;
             }
-
-            account.tokens?.forEach(token => {
-                // @ts-expect-error
-                token.contract = token.address;
-                // @ts-expect-error
-                delete token.address;
-            });
 
             return account;
         });
