636f861bdfc18ce5c3f82c8621895bef39faae40
juriczech <bohdan.juricek@gmail.com>
2025-11-19 16:19:47 +0100

    feat(suite-native): handleBiometricsAppStateChangeThunk

--- suite-native/biometrics/src/index.ts
+++ suite-native/biometrics/src/index.ts
@@ -1,6 +1,5 @@
 export * from './biometricsUtils';
 export * from './biometricsSlice';
-export * from './useBiometrics';
 export * from './components/BiometricsModalRenderer';
 export * from './components/BiometricsIcon';
 export * from './components/BiometricsSvg';
