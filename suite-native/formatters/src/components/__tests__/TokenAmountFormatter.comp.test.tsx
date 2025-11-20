810e313c8a41c303d4a384a3fe8d4820fd821f0c
Jirka Bažant <jiri.bazant@satoshilabs.com>
2025-11-20 10:46:08 +0100

    chore(suite-native): Add tests for TokenAmountFormatter

---
+++ suite-native/formatters/src/components/__tests__/TokenAmountFormatter.comp.test.tsx
@@ -0,0 +1,26 @@
+import { TokenSymbol } from '@suite-common/wallet-types';
+import { renderWithStoreProviderAsync } from '@suite-native/test-utils';
+
+import { TokenAmountFormatter, TokenAmountFormatterProps } from '../TokenAmountFormatter';
+
+describe('TokenAmountFormatter', () => {
+    const renderTokenAmountFormatter = (props: Partial<TokenAmountFormatterProps>) =>
+        renderWithStoreProviderAsync(
+            <TokenAmountFormatter tokenSymbol={'USDC' as TokenSymbol} value="1234.56" {...props} />,
+        );
+
+    it('should render formatted value', async () => {
+        const { getByTestId } = await renderTokenAmountFormatter({});
+
+        expect(getByTestId('plain-text')).toHaveTextContent('1,234.56 USDC');
+    });
+
+    it('should render phishing transaction with empty value as discreet text', async () => {
+        const { getByTestId } = await renderTokenAmountFormatter({
+            value: '',
+            isPhishingTransaction: true,
+        });
+
+        expect(getByTestId('discreet-text')).toHaveTextContent('0 USDC');
+    });
+});
