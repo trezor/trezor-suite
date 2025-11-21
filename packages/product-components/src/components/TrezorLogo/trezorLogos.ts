6849b0dd9c25a1dccfd28b3b1e3067b52e33bc6a
Tomas Martykan <t.martykan@gmail.com>
2025-11-21 17:41:04 +0100

    fix(product-components): bundle TrezorLogo SVGs again

--- packages/product-components/src/components/TrezorLogo/trezorLogos.ts
+++ packages/product-components/src/components/TrezorLogo/trezorLogos.ts
@@ -1,8 +1,5 @@
-import { IMAGES_PATH } from '@trezor/components';
-import { resolveStaticPath } from '@trezor/env-utils';
-
 export const LOGOS: { [key: string]: any } = {
-    HORIZONTAL: resolveStaticPath(`${IMAGES_PATH}/logos/trezor_logo_horizontal.svg`),
-    VERTICAL: resolveStaticPath(`${IMAGES_PATH}/logos/trezor_logo_vertical.svg`),
-    SYMBOL: resolveStaticPath(`${IMAGES_PATH}/logos/trezor_logo_symbol.svg`),
+    HORIZONTAL: require(`../../images/logos/trezor_logo_horizontal.svg`),
+    VERTICAL: require(`../../images/logos/trezor_logo_vertical.svg`),
+    SYMBOL: require(`../../images/logos/trezor_logo_symbol.svg`),
 };
