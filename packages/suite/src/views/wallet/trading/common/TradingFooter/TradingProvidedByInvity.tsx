2e00d3d0abaf4b4bd716441007479610e26c812f
Jan Václavík <jvaclavik@gmail.com>
2025-11-24 07:26:27 +0100

    fix(suite): Make invity footer responsive

--- packages/suite/src/views/wallet/trading/common/TradingFooter/TradingProvidedByInvity.tsx
+++ packages/suite/src/views/wallet/trading/common/TradingFooter/TradingProvidedByInvity.tsx
@@ -1,4 +1,4 @@
-import { Center, Image, Paragraph } from '@trezor/components';
+import { Image, Paragraph, Row } from '@trezor/components';
 import { INVITY_URL } from '@trezor/urls';
 
 import { Translation } from 'src/components/suite/Translation';
@@ -6,12 +6,12 @@ import { Translation } from 'src/components/suite/Translation';
 import { TrezorLink } from '../../../../../components/suite';
 
 export const TradingProvidedByInvity = () => (
-    <Center>
+    <Row>
         <Paragraph variant="tertiary" typographyStyle="label">
             <Translation id="TR_BUY_PROVIDED_BY_INVITY" />
         </Paragraph>
         <TrezorLink href={INVITY_URL} variant="nostyle">
             <Image width={70} image="INVITY_LOGO" />
         </TrezorLink>
-    </Center>
+    </Row>
 );
