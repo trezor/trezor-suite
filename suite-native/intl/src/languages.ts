a19d325a430856875729b8004c55bb280617ffcd
Petr Knetl <petr.knetl@satoshilabs.com>
2025-11-26 10:28:57 +0000

    feat(suite-native): remove czech beta label

--- suite-native/intl/src/languages.ts
+++ suite-native/intl/src/languages.ts
@@ -4,7 +4,7 @@ export const DEFAULT_LOCALE = 'en-US';
 
 export const LANGUAGES = {
     'en-US': { name: 'English', en: 'English', type: 'official' },
-    'cs-CZ': { name: 'Čeština (beta)', en: 'Czech (beta)', type: 'community' },
+    'cs-CZ': { name: 'Čeština', en: 'Czech', type: 'official' },
     'de-DE': { name: 'Deutsch', en: 'German', type: 'official' },
     'pt-BR': { name: 'Português (BR)', en: 'Portuguese (BR)', type: 'official' },
     'ja-JP': { name: '日本語', en: 'Japanese', type: 'official' },
