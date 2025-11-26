1a472b3e591f055df963c7f07a364ce6956686f3
karliatto <yo@karliatto.com>
2025-11-26 16:38:49 +0100

    feat(metadata-types): BIP329 type

--- suite-common/metadata-types/src/metadataTypes.ts
+++ suite-common/metadata-types/src/metadataTypes.ts
@@ -291,3 +291,10 @@ export type PasswordManagerState = {
     extVersion?: string;
     tags: Record<number, PasswordTag>;
 };
+
+export type Bip329Label = {
+    type: 'tx' | 'addr' | 'wallet' | 'xpub' | 'pubkey' | 'input' | 'output';
+    ref?: string; // The identifier for the object being labeled (e.g., txid, address, txid:vout)
+    label: string; // The label text
+    spendable?: boolean;
+};
