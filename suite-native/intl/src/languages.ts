5f5f887cd5d384512e9452787f0d794d4c57da5b
Petr Knetl <petr.knetl@satoshilabs.com>
2025-11-26 10:27:38 +0000

    feat(suite-native): german japanese and portuguese added

--- suite-native/intl/src/languages.ts
+++ suite-native/intl/src/languages.ts
@@ -5,4 +5,7 @@ export const DEFAULT_LOCALE = 'en-US';
 export const LANGUAGES = {
     'en-US': { name: 'English', en: 'English', type: 'official' },
     'cs-CZ': { name: 'Čeština (beta)', en: 'Czech (beta)', type: 'community' },
+    'de-DE': { name: 'Deutsch', en: 'German', type: 'official' },
+    'pt-BR': { name: 'Português (BR)', en: 'Portuguese (BR)', type: 'official' },
+    'ja-JP': { name: '日本語', en: 'Japanese', type: 'official' },
 } as const satisfies Record<string, LocaleInfo>;
