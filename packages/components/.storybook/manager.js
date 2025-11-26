2f5421c6d1dc32918dd94e5bcb5fcdc3542fbfbb
Jan Václavík <jvaclavik@gmail.com>
2025-11-26 17:17:23 +0100

    refactor(suite): Refactor storybook theming

--- packages/components/.storybook/manager.js
+++ packages/components/.storybook/manager.js
@@ -1,6 +1,7 @@
 import { addons } from 'storybook/manager-api';
 
 import theme from './theme';
+import './theme-toggle';
 
 addons.setConfig({
     theme,
---
+++ packages/components/.storybook/preview.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import { StoryContext, StoryFn } from '@storybook/react';
+import { ThemeProvider } from 'styled-components';
+
+import { intermediaryTheme } from '../src/config/colors';
+
+export const globalTypes = {
+    mode: {
+        defaultValue: 'light',
+    },
+};
+export const decorators = [
+    (Story: StoryFn, context: StoryContext) => {
+        const mode = context.globals.mode ?? 'light';
+
+        const theme =
+            mode === 'light'
+                ? { ...intermediaryTheme.light, variant: 'light' }
+                : { ...intermediaryTheme.dark, variant: 'dark' };
+
+        return (
+            <ThemeProvider theme={theme}>
+                <div
+                    style={{
+                        padding: 32,
+                        background: theme.backgroundSurfaceElevation0,
+                        color: theme.textDefault,
+                        boxSizing: 'border-box',
+                        // height: '100vh',
+                        // overflow: 'auto',
+                        // nic víc!
+                    }}
+                >
+                    {Story(context.args, context)}
+                </div>
+            </ThemeProvider>
+        );
+    },
+];
+
+export const parameters = {
+    layout: 'fullscreen',
+    options: {
+        showPanel: true,
+        showInfo: true,
+        panelPosition: 'right',
+        storySort: {
+            order: ['🎨 Colors', '🫵 Buttons', '✏️ Form', '🅰️ Typography', '*'],
+            method: 'alphabetical',
+            locales: 'cs',
+        },
+    },
+    theme: {
+        base: 'light',
+    },
+};
