f9ed567fb0957194e8bd9bb37879176a7087435c
Martin Varmuza <mvarmuza@gmail.com>
2025-11-27 10:39:46 +0100

    chore(components): remove unused re-exports

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -12,8 +12,6 @@ export {
 export { Checkbox, type CheckboxProps } from './components/form/Checkbox/Checkbox';
 export * from './components/animations/LottieAnimation';
 export { recolorLottieAnimation } from './components/animations/recolorLottieAnimation';
-export { hexToRgbaArray } from '../../utils/src/hexToRgbaArray';
-export { hexToRgba } from '../../utils/src/hexToRgba';
 export * from './components/Flag/Flag';
 export * from './components/AutoScalingInput/AutoScalingInput';
 export { Badge, type BadgeProps, type BadgeSize, type BadgeIntent } from './components/Badge/Badge';
