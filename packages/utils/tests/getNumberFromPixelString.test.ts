ed9f97dbcf1378c7fd03afd98e124c41c42f9622
Martin Varmuza <mvarmuza@gmail.com>
2025-11-27 10:38:50 +0100

    chore(utils): remove unused code

--- packages/utils/tests/getNumberFromPixelString.test.ts
+++
@@ -1,6 +0,0 @@
-import { getNumberFromPixelString } from '../src/getNumberFromPixelString';
-
-it('getNumberFromPixelString', () => {
-    expect(getNumberFromPixelString('1px')).toBe(1);
-    expect(getNumberFromPixelString('1')).toBe(1);
-});
