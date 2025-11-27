d95194c95eacbfc6f7d41a212f565c6f8024dfc5
tomasklim <tomas.klim@gmail.com>
2025-11-27 10:18:05 +0100

    chore: reduce number of txs per page to 8 for Cardano

--- suite-common/suite-utils/src/txsPerPage.ts
+++ suite-common/suite-utils/src/txsPerPage.ts
@@ -2,7 +2,7 @@ import { settingsCommonConfig } from '@suite-common/suite-config';
 import type { Account } from '@suite-common/wallet-types';
 
 export const getTxsPerPage = (networkType: Account['networkType']) => {
-    if (networkType === 'solana') {
+    if (networkType === 'solana' || networkType === 'cardano') {
         return 8;
     }
 
