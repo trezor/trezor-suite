faa61225e40d9bcc3352d9ab437fa4e12ab39fe9
tomasklim <tomas.klim@gmail.com>
2025-11-30 22:03:55 +0100

    feat(suite): cardano staking debug info

---
+++ packages/suite/src/views/wallet/staking/components/StakingDashboard/components/DebugOnlyCardanoStakingCard.tsx
@@ -0,0 +1,102 @@
+import styled from 'styled-components';
+
+import { selectCardanoPoolsInfo } from '@suite-common/wallet-core';
+import { Account } from '@suite-common/wallet-types';
+import {
+    isCardanoStakedWithEverstake,
+    isCardanoStakedWithFiveBinaries,
+} from '@suite-common/wallet-utils';
+import { Card, Column, Icon, InfoItem, Paragraph, Row } from '@trezor/components';
+import { spacings } from '@trezor/theme';
+
+import { DebugOnlyBadge } from 'src/components/suite/DebugOnlyBadge';
+import { useSelector } from 'src/hooks/suite';
+import { selectIsDebugModeActive } from 'src/selectors/suite/suiteSelectors';
+
+const ParagraphWrapper = styled.div`
+    white-space: pre-wrap;
+    overflow-wrap: anywhere;
+`;
+
+interface DebugOnlyCardanoStakingCardProps {
+    account: Account;
+}
+
+export const DebugOnlyCardanoStakingCard = ({ account }: DebugOnlyCardanoStakingCardProps) => {
+    const isDebugModeActive = useSelector(selectIsDebugModeActive);
+    const cardanoStakingPools = useSelector(selectCardanoPoolsInfo);
+    const isStakedWithEverstake = isCardanoStakedWithEverstake(account, cardanoStakingPools);
+    const isStakedWithFiveBinaries = isCardanoStakedWithFiveBinaries(account);
+
+    if (!isDebugModeActive || account.networkType !== 'cardano') return null;
+
+    return (
+        <Card paddingType="small" flex="1">
+            <Column alignItems="flex-start" flex="1" gap={spacings.lg}>
+                <Row columnGap={spacings.xxs}>
+                    <Icon name="info" variant="tertiary" />
+
+                    <DebugOnlyBadge />
+                </Row>
+
+                <Column rowGap={spacings.xs}>
+                    <InfoItem
+                        label="Provider"
+                        direction="row"
+                        labelWidth={90}
+                        verticalAlignment="start"
+                    >
+                        <ParagraphWrapper>
+                            <Paragraph typographyStyle="label">
+                                {isStakedWithEverstake ? 'Everstake' : ''}
+                                {isStakedWithFiveBinaries ? 'FiveBinaries' : ''}
+                                {!isStakedWithEverstake && !isStakedWithFiveBinaries
+                                    ? 'Unknown'
+                                    : ''}
+                            </Paragraph>
+                        </ParagraphWrapper>
+                    </InfoItem>
+
+                    <InfoItem
+                        label="Pool ID"
+                        direction="row"
+                        labelWidth={90}
+                        verticalAlignment="start"
+                    >
+                        <ParagraphWrapper>
+                            <Paragraph typographyStyle="label">
+                                {account?.misc?.staking.poolId}
+                            </Paragraph>
+                        </ParagraphWrapper>
+                    </InfoItem>
+
+                    <InfoItem
+                        label="Stake address"
+                        direction="row"
+                        labelWidth={90}
+                        verticalAlignment="start"
+                    >
+                        <ParagraphWrapper>
+                            <Paragraph typographyStyle="label">
+                                {account?.misc?.staking.address}
+                            </Paragraph>
+                        </ParagraphWrapper>
+                    </InfoItem>
+
+                    <InfoItem
+                        label="DRep"
+                        direction="row"
+                        labelWidth={90}
+                        verticalAlignment="start"
+                    >
+                        <ParagraphWrapper>
+                            <Paragraph typographyStyle="label">
+                                {account?.misc?.staking.drep?.drep_id}
+                            </Paragraph>
+                        </ParagraphWrapper>
+                    </InfoItem>
+                </Column>
+            </Column>
+        </Card>
+    );
+};
