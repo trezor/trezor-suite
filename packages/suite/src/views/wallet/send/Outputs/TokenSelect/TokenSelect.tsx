de374fe98c278dbb4f621892da308c0ea30054e2
Jun Luo <4catcode@gmail.com>
2025-12-01 22:47:23 +0800

    chore(suite): enable stellar tokens feature in non-debug mode.

--- packages/suite/src/views/wallet/send/Outputs/TokenSelect/TokenSelect.tsx
+++ packages/suite/src/views/wallet/send/Outputs/TokenSelect/TokenSelect.tsx
@@ -193,7 +193,7 @@ export const TokenSelect = ({ outputId }: TokenSelectProps) => {
                             )}
                         </Column>
                     </Row>
-                    {!hasNoStandardTokens && hasNetworkFeatures(account, 'tokens') && (
+                    {!hasNoStandardTokens && (
                         <IconButton icon="caretDown" intent="neutral" priority="secondary" />
                     )}
                 </Row>
