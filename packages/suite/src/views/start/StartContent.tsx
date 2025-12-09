5beb44857f352b727ec8f214fb026e238dce4b8c
Martin Varmuza <mvarmuza@gmail.com>
2025-12-09 09:05:40 +0100

    feat(suite): remove security check step

--- packages/suite/src/views/start/StartContent.tsx
+++ packages/suite/src/views/start/StartContent.tsx
@@ -1,32 +1,12 @@
 import { Card } from '@trezor/components';
 
 import { PrerequisitesGuide } from 'src/components/suite';
-import { useSelector } from 'src/hooks/suite';
-import { selectPrerequisite } from 'src/selectors/suite/suiteSelectors';
-import { PrerequisiteType } from 'src/utils/suite/prerequisites';
 
 import { ModalSwitcher } from '../../components/suite/modals/ModalSwitcher/ModalSwitcher';
-import { SecurityCheck } from '../onboarding/steps/DeviceAuthenticityStep/SecurityCheck';
 
-const startAppExcludedPrerequisites: PrerequisiteType[] = [
-    'device-initialize',
-    'firmware-missing',
-    'device-recovery-mode',
-];
-
-export const StartContent = () => {
-    const prerequisite = useSelector(selectPrerequisite);
-
-    if (prerequisite !== null && !startAppExcludedPrerequisites.includes(prerequisite)) {
-        return (
-            <Card>
-                <ModalSwitcher />
-                <PrerequisitesGuide />
-            </Card>
-        );
-    }
-
-    // Security check has to be without <ModalSwitcher /> as it handles the
-    // button request without it. Its terrible, but it is what it is.
-    return <SecurityCheck />;
-};
+export const StartContent = () => (
+    <Card>
+        <ModalSwitcher />
+        <PrerequisitesGuide />
+    </Card>
+);
