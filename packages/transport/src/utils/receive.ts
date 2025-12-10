bf41dc128f2ae526502b9b1767b955c4862c6bc7
Szymon Lesisz <sz.lesisz@gmail.com>
2025-12-10 11:58:04 +0100

    fix(transport): wrap errors from `receive` utility

--- packages/transport/src/utils/receive.ts
+++ packages/transport/src/utils/receive.ts
@@ -1,7 +1,7 @@
 import { decodeMessage } from '@trezor/protobuf';
-import { TransportProtocol } from '@trezor/protocol';
+import { PROTOCOL_MALFORMED, TransportProtocol } from '@trezor/protocol';
 
-import { success } from './result';
+import { error, success } from './result';
 import { AbstractApi } from '../api/abstract';
 
 type Receiver = () => ReturnType<AbstractApi['read']>;
@@ -11,31 +11,35 @@ export async function receive<T extends Receiver>(receiver: T, protocol: Transpo
     if (!readResult.success) {
         return readResult;
     }
-    const data = readResult.payload;
-    const { length, messageType, payload, header } = protocol.decode(data);
-    const result = Buffer.alloc(length);
-    const [, chunkHeader] = protocol.getHeaders(Buffer.from(data));
 
-    payload.copy(result);
-    let offset = payload.length;
-
-    while (offset < length) {
-        const readResult = await receiver();
-
-        if (!readResult.success) {
-            return readResult;
-        }
+    try {
         const data = readResult.payload;
-        const dataChunkHeader = data.subarray(0, chunkHeader.length);
-        if (dataChunkHeader.compare(chunkHeader) !== 0) {
-            throw new Error(`Unexpected chunkHeader ${dataChunkHeader.toString('hex')}`);
+        const { length, messageType, payload, header } = protocol.decode(readResult.payload);
+        const [, chunkHeader] = protocol.getHeaders(data);
+
+        const result = Buffer.alloc(length);
+        payload.copy(result);
+
+        let offset = payload.length;
+        while (offset < length) {
+            const readResult = await receiver();
+            if (!readResult.success) {
+                return readResult;
+            }
+            const data = readResult.payload;
+            const dataChunkHeader = data.subarray(0, chunkHeader.length);
+            if (dataChunkHeader.compare(chunkHeader) !== 0) {
+                throw new Error(`Unexpected chunkHeader ${dataChunkHeader.toString('hex')}`);
+            }
+
+            Buffer.from(data).copy(result, offset, chunkHeader.byteLength);
+            offset += data.byteLength - chunkHeader.byteLength;
         }
 
-        Buffer.from(data).copy(result, offset, chunkHeader.byteLength);
-        offset += data.byteLength - chunkHeader.byteLength;
+        return success({ messageType, payload: result, header, length });
+    } catch (e) {
+        return error({ error: PROTOCOL_MALFORMED, message: e.message });
     }
-
-    return success({ messageType, payload: result, header, length });
 }
 
 export async function receiveAndParse<T extends Receiver>(
