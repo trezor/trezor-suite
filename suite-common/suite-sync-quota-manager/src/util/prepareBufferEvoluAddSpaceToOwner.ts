524e853ba9d3c603de62927da19244a219117db1
Matěj Husák <matej.husak@icloud.com>
2025-12-14 23:45:33 +0100

    feat(suite): prepare QM buffers, improved getProofOfDelegatedIdentity

---
+++ suite-common/suite-sync-quota-manager/src/util/prepareBufferEvoluAddSpaceToOwner.ts
@@ -0,0 +1,29 @@
+import { bufferUtils } from '@trezor/utils';
+
+type PrepareBufferEvoluAddSpaceToOwnerParams = {
+    publicKey: string;
+    ownerId: string;
+    challenge: string;
+    size: number;
+};
+
+export const prepareBufferEvoluAddSpaceToOwner = ({
+    publicKey,
+    ownerId,
+    challenge,
+    size,
+}: PrepareBufferEvoluAddSpaceToOwnerParams) => {
+    const sizeBuffer = Buffer.alloc(4, 0, 'binary');
+    sizeBuffer.writeUInt32BE(size, 0);
+
+    return Buffer.concat([
+        bufferUtils.getChunkSize(Buffer.from(publicKey, 'hex').byteLength),
+        Buffer.from(publicKey, 'hex'),
+        bufferUtils.getChunkSize(Buffer.from(ownerId, 'utf8').byteLength),
+        Buffer.from(ownerId, 'utf8'),
+        bufferUtils.getChunkSize(Buffer.from(challenge, 'hex').byteLength),
+        Buffer.from(challenge, 'hex'),
+        bufferUtils.getChunkSize(sizeBuffer.length),
+        sizeBuffer,
+    ]);
+};
