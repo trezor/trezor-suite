552eea264cbc04abd1e0b16be5266fab5c164c36
juriczech <bohdan.juricek@gmail.com>
2025-12-15 13:55:07 +0100

    chore(suite-sync): get slip21 after discovery success
    After successful wallet creation and discovery, suite will ask for owner ID to enable labelling for device
    
    # Conflicts:
    #	suite-common/suite-sync/src/suiteSyncMiddleware.ts

--- suite-common/suite-sync/src/suiteSyncMiddleware.ts
+++ suite-common/suite-sync/src/suiteSyncMiddleware.ts
@@ -1,21 +1,27 @@
 import { isAnyOf } from '@reduxjs/toolkit';
 
 import { createMiddlewareWithExtraDeps } from '@suite-common/redux-utils';
-import { applyDeviceStatesThunk, selectDeviceThunk } from '@suite-common/wallet-core';
+import { deviceActions, selectDeviceThunk } from '@suite-common/wallet-core';
 import { isTrezorDeviceWithState } from '@suite-common/wallet-utils';
 
 import { selectIsSuiteSyncEnabled } from './suiteSyncSelectors';
 
 // Thunk triggers for which we want to turn on Suite Sync for the currently selected wallet
 const suiteSyncTurnOnTriggers = [
-    // New wallet created
-    applyDeviceStatesThunk.fulfilled,
     // Wallet selected
     selectDeviceThunk.fulfilled,
 ] as const;
 
 export const prepareSuiteSyncMiddleware = createMiddlewareWithExtraDeps(
     (action, { next, getState, extra }) => {
+        if (selectIsSuiteSyncEnabled(getState()) && deviceActions.setDiscovered.match(action)) {
+            if (action.payload.success) {
+                extra.services.suiteSync.turnOnSuiteSyncForWallet({
+                    deviceStaticSessionId: action.payload.staticSessionId,
+                });
+            }
+        }
+
         if (selectIsSuiteSyncEnabled(getState()) && isAnyOf(...suiteSyncTurnOnTriggers)(action)) {
             const { payload } = action as ReturnType<(typeof suiteSyncTurnOnTriggers)[number]>;
 
