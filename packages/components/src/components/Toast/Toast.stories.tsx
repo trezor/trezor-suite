d5a80fe1558a54a2026052e5ce3ee0cf65c6c75e
Jaroslav Hrách <me@jaroslavhrach.cz>
2025-12-16 01:04:09 +0100

    feat(components): create UI component Toast

---
+++ packages/components/src/components/Toast/Toast.stories.tsx
@@ -0,0 +1,59 @@
+import { ArgTypes, Meta, StoryObj } from '@storybook/react';
+
+import { Toast as ToastComponent, ToastProps } from './Toast';
+import { variables } from '../../config';
+
+const meta: Meta<typeof ToastComponent> = {
+    title: 'Toast',
+    component: ToastComponent,
+};
+export default meta;
+
+const args: Partial<ToastProps> | undefined = {
+    content: 'This is a toast message.',
+    intent: 'info',
+    icon: undefined,
+    dismissible: true,
+    onDismiss: () => alert('Toast closed'),
+};
+
+const argTypes: Partial<ArgTypes<ToastProps>> | undefined = {
+    content: {
+        type: 'string',
+    },
+    icon: {
+        options: [undefined, ...variables.ICONS],
+        control: {
+            type: 'select',
+        },
+    },
+    dismissible: {
+        control: {
+            type: 'boolean',
+        },
+    },
+};
+
+export const Default: StoryObj<ToastProps> = {
+    args,
+    argTypes,
+};
+
+export const WithActions: StoryObj<ToastProps> = {
+    args: {
+        ...args,
+        actions: [
+            {
+                label: 'Action A',
+                onClick: () => alert('Action A clicked'),
+                position: 'right',
+            },
+            {
+                label: 'Action B',
+                onClick: () => alert('Action B clicked'),
+                position: 'right',
+            },
+        ],
+    },
+    argTypes,
+};
---
+++ packages/components/src/components/Toast/utils.ts
@@ -0,0 +1,63 @@
+import { DefaultTheme } from 'styled-components';
+
+import { CSSColor } from '@trezor/theme';
+
+import { ToastAction, ToastIconVariant, ToastIntent } from './types';
+
+export const mapToastIntentToIcon = (intent: ToastIntent) => {
+    const iconMap: Record<ToastIntent, ToastIconVariant> = {
+        brand: 'check',
+        info: 'info',
+        warning: 'warning',
+        critical: 'warning',
+        neutral: 'info',
+    };
+
+    return iconMap[intent];
+};
+
+export const mapToastVariantToColor = (variant: ToastIntent, theme: DefaultTheme) => {
+    const colorMap: Record<ToastIntent, CSSColor> = {
+        brand: theme.textPrimaryDefault,
+        info: theme.textAlertBlue,
+        warning: theme.textAlertYellow,
+        critical: theme.textAlertRed,
+        neutral: theme.textSubdued,
+    };
+
+    return colorMap[variant];
+};
+
+export const normalizeToastActions = (
+    actions: ToastAction | ToastAction[] | undefined,
+    toastIntent: ToastIntent,
+) => {
+    let list: ToastAction[] = [];
+
+    if (actions) {
+        list = Array.isArray(actions) ? actions : [actions];
+    }
+
+    return list.reduce(
+        (acc, action) => {
+            const normalized = {
+                ...action,
+                intent: action.intent ?? toastIntent,
+                priority: action.priority ?? 'secondary',
+                position: action.position ?? 'right',
+            };
+
+            if (normalized.position === 'bottom') {
+                acc.bottomActions.push(normalized);
+            } else {
+                acc.rightActions.push(normalized);
+            }
+
+            return acc;
+        },
+        {
+            bottomActions: [] as typeof list,
+            rightActions: [] as typeof list,
+        },
+    );
+};
