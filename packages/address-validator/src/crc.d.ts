bf655a0b4cc46ea32b5315ac55d5b10c955f2e2a
Martin Varmuza <mvarmuza@gmail.com>
2025-12-16 21:04:31 +0100

    fixup! chore(address-validator): rewrite to ts

--- packages/address-validator/src/crc.d.ts
+++
@@ -1,9 +0,0 @@
-declare module 'crc' {
-    export function crc16xmodem(payload: ArrayLike<number> | Buffer): number;
-    export function crc16(payload: ArrayLike<number> | Buffer): number;
-    export function crc32(payload: ArrayLike<number> | Buffer): number;
-    export function crcjam(payload: ArrayLike<number> | Buffer): number;
-    export function ccitt(payload: ArrayLike<number> | Buffer): number;
-    export function modbus(payload: ArrayLike<number> | Buffer): number;
-    export function xmodem(payload: ArrayLike<number> | Buffer): number;
-}
