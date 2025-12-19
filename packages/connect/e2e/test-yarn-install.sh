aca435d3bb96076caa91171ce219668761a86b9c
karliatto <yo@karliatto.com>
2025-12-19 13:52:31 +0100

    fix(connect): using @types/node@22.13.10 in check
    The definitions for @types/node have changed in latest version and the
    tests test-yarn-insatll.sh is using the latest one since it it not fixed
    in the script.
    
    So fixing it to @types/node@22.13.10 makes the test successful.

--- packages/connect/e2e/test-yarn-install.sh
+++ packages/connect/e2e/test-yarn-install.sh
@@ -23,5 +23,5 @@ yarn add @trezor/connect@"$1"
 echo import TrezorConnect from \"@trezor/connect\" >index.ts
 
 # compile with typescript
-yarn add typescript@5.8.2
-yarn tsc ./index.ts --types node,w3c-web-usb --esModuleInterop --target ES2022 --module commonjs
+yarn add typescript@5.8.3 @types/node@22.13.10
+yarn tsc ./index.ts --types node,w3c-web-usb --esModuleInterop --target ES2024 --module commonjs
