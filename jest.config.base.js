3f002de2ad97c6968632a24c5a2ec9c11e735f50
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2025-12-22 20:53:30 +0100

    chore(suite): bump react-intl

--- jest.config.base.js
+++ jest.config.base.js
@@ -36,7 +36,7 @@ module.exports = {
         '\\.(js|jsx|ts|tsx)$': ['babel-jest', babelConfig],
     },
 
-    transformIgnorePatterns: ['node_modules/?!(uuid)/'],
+    transformIgnorePatterns: ['node_modules/?!(uuid|react-intl|@formatjs/*|intl-messageformat)/'],
 
     // An array of regexp patterns that are matched against all source file paths before re-running tests in watch mode
     watchPathIgnorePatterns: ['libDev', 'lib'],
