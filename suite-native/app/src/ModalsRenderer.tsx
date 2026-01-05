a8b561e512d99611343846ffcafd505c35846616
Matej Kriz <matej@matejkriz.cz>
2026-01-05 16:45:38 +0100

    fix(suite-native): RIP snow
    - There was a bug, it only snowed on half of the screen, not everywhere. It doesn't make sense to fix it now – we might as well come up with something new.

--- suite-native/app/src/ModalsRenderer.tsx
+++ suite-native/app/src/ModalsRenderer.tsx
@@ -2,13 +2,10 @@ import { AlertRenderer } from '@suite-native/alerts';
 import { BiometricsModalRenderer } from '@suite-native/biometrics';
 import { ToastRenderer } from '@suite-native/toasts';
 
-import { Snow } from './snow/Snow';
-
 export const ModalsRenderer = () => (
     <>
         <AlertRenderer />
         <ToastRenderer />
-        <Snow />
         <BiometricsModalRenderer />
     </>
 );
