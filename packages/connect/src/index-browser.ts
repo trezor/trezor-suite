2f3389c6615e82dfecb34934a1ab87c1d15d091f
Marek Polak <marek.polak@satoshilabs.com>
2026-01-06 11:24:12 +0100

    refactor(connect): use CoreInModule directly

--- packages/connect/src/index-browser.ts
+++ packages/connect/src/index-browser.ts
@@ -1,62 +1,11 @@
-import { ERRORS } from './constants';
 import { config } from './data/config';
-import { CallMethodPayload, TRANSPORT, createErrorMessage } from './events';
+import { TRANSPORT } from './events';
 import { factory } from './factory';
 import { CoreInModule } from './impl/core-in-module';
-import type { ConnectSettingsPublic, Manifest } from './types';
-import { type InitFullSettings } from './types/api/init';
-import { SetTransports } from './types/api/setTransports';
 
-let lastSettings: InitFullSettings<ConnectSettingsPublic> | undefined;
 const impl = new CoreInModule();
 
-const manifest = (m: Manifest) => {
-    lastSettings = { ...lastSettings, m } as typeof lastSettings;
-
-    impl.manifest(m);
-};
-
-const dispose = () => {
-    impl.eventEmitter.removeAllListeners();
-
-    return impl.dispose();
-};
-
-const init = async (settings: InitFullSettings<ConnectSettingsPublic>) => {
-    if (!settings?.manifest) {
-        throw ERRORS.TypedError('Init_ManifestMissing');
-    }
-    // Save settings for later use
-    lastSettings = settings;
-
-    return await impl.init(lastSettings);
-};
-
-const call = async (params: CallMethodPayload) => {
-    try {
-        const response = await impl.call(params);
-
-        return response;
-    } catch (error) {
-        // Don't throw but return error payload
-        return createErrorMessage(error);
-    }
-};
-
-const setTransports = ({ transports }: SetTransports) => {
-    lastSettings = { ...lastSettings, transports } as typeof lastSettings;
-    impl.setTransports({ transports });
-};
-
-const uiResponse = (params: any) => impl.uiResponse(params);
-
-const cancel = (error?: string) => impl.cancel(error);
-
 const disableWebUSB = () => {
-    if (!lastSettings) {
-        throw ERRORS.TypedError('Init_NotInitialized');
-    }
-
     impl.handleCoreMessage({ type: TRANSPORT.DISABLE_WEBUSB });
 };
 
@@ -69,16 +18,17 @@ const requestWebUSBDevice = async () => {
     }
 };
 
+// Exported to enable using directly
 const TrezorConnect = factory(
     {
         eventEmitter: impl.eventEmitter,
-        init,
-        call,
-        setTransports,
-        manifest,
-        uiResponse,
-        cancel,
-        dispose,
+        manifest: impl.manifest.bind(impl),
+        init: impl.init.bind(impl),
+        call: impl.call.bind(impl),
+        setTransports: impl.setTransports.bind(impl),
+        uiResponse: impl.uiResponse.bind(impl),
+        cancel: impl.cancel.bind(impl),
+        dispose: impl.dispose.bind(impl),
     },
     {
         disableWebUSB,
@@ -93,5 +43,7 @@ export default TrezorConnect;
 export * from './exports';
 
 if (typeof window !== 'undefined') {
-    window.addEventListener('beforeunload', dispose);
+    window.addEventListener('beforeunload', () => {
+        impl.dispose();
+    });
 }
