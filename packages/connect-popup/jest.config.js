ee302ebf5596bcd1df66d6b986bfc02e92e9d5aa
Martin Varmuza <mvarmuza@gmail.com>
2026-01-07 15:17:00 +0100

    chore: remove connect-popup

--- packages/connect-popup/jest.config.js
+++
@@ -1,7 +0,0 @@
-const { testPathIgnorePatterns, ...baseConfig } = require('../../jest.config.base');
-
-module.exports = {
-    ...baseConfig,
-    collectCoverage: true,
-    testPathIgnorePatterns: [...testPathIgnorePatterns, 'e2e'],
-};
--- packages/connect-popup/src/types/eth-phishing-detect.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'eth-phishing-detect' {
-    // eslint-disable-next-line import/no-default-export
-    export default function (domain: string): boolean;
-}
