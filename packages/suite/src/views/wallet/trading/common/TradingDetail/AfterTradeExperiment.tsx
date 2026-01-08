a01f33fc2a291ca4054ed89e5add6acde496e8bc
vytick <vytick@gmail.com>
2026-01-08 12:41:12 +0100

    feat(trade): enhance tradingFeedbackForm experiment with TradingDetailSurvey

---
+++ packages/suite/src/views/wallet/trading/common/TradingDetail/AfterTradeExperiment.tsx
@@ -0,0 +1,57 @@
+import {
+    BuyTradeStatus,
+    ExchangeProviderInfo,
+    ExchangeTradeStatus,
+    SellTradeStatus,
+} from 'invity-api';
+
+import { ExperimentId } from '@suite-common/message-system';
+import { TradingType } from '@suite-common/trading';
+
+import { ExperimentWrapper } from 'src/components/suite/Experiment/ExperimentWrapper';
+import { TradingGetCryptoQuoteAmountProps } from 'src/types/trading/trading';
+
+import { TradingDetailFeedback } from './TradingDetailFeedback';
+import { TradingDetailSurvey } from './TradingDetailSurvey';
+
+interface AfterTradeExperimentProps {
+    status: ExchangeTradeStatus | SellTradeStatus | BuyTradeStatus | undefined;
+    type: TradingType;
+    provider?: ExchangeProviderInfo['name'];
+    id?: string;
+    quoteAmounts: TradingGetCryptoQuoteAmountProps;
+    country?: string;
+}
+
+export const AfterTradeExperiment = ({
+    status,
+    type,
+    provider,
+    id,
+    quoteAmounts,
+    country,
+}: AfterTradeExperimentProps) => (
+    <ExperimentWrapper
+        id={ExperimentId.tradingFeedbackForm}
+        components={[
+            { variant: 'A', element: <></> },
+            {
+                variant: 'B',
+                element: (
+                    <TradingDetailFeedback
+                        status={status}
+                        type={type}
+                        provider={provider}
+                        id={id}
+                        quoteAmounts={quoteAmounts}
+                        country={country}
+                    />
+                ),
+            },
+            {
+                variant: 'C',
+                element: <TradingDetailSurvey />,
+            },
+        ]}
+    />
+);
