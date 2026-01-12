99233bb8d7eb9aa42f25ebaf3368fcb3672c0dbc
Peter Sanderson <dierk.deidel@protonmail.com>
2026-01-12 13:42:17 +0100

    chore: improve ResultType

--- packages/type-utils/src/result.ts
+++ packages/type-utils/src/result.ts
@@ -3,14 +3,14 @@
  * @see https://www.evolu.dev/docs/api-reference/common/Result/type-aliases/Result
  */
 
-export type Result<T, E> = Ok<T> | Err<E>;
+export type Result<T, E = never> = Ok<T> | Err<E>;
 
-export interface Ok<T> {
+export interface Ok<out T> {
     readonly ok: true;
     readonly value: T;
 }
 
-export interface Err<E> {
+export interface Err<out E> {
     readonly ok: false;
     readonly error: E;
 }
@@ -18,10 +18,10 @@ export interface Err<E> {
 export type InferOk<R extends Result<any, any>> = R extends Ok<infer T> ? T : never;
 export type InferErr<R extends Result<any, any>> = R extends Err<infer E> ? E : never;
 
-export function ok(): Ok<void>;
-export function ok<T>(value: T): Ok<T>;
-export function ok<T>(value = undefined): Ok<T> {
+export function ok(): Result<void>;
+export function ok<T>(value: T): Result<T>;
+export function ok<T>(value?: T): Result<T> {
     return { ok: true, value: value as T };
 }
 
-export const err = <E>(error: E): Err<E> => ({ ok: false, error });
+export const err = <E>(error: E): Result<never, E> => ({ ok: false, error });
