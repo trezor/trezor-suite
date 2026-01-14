22827413f14a07bac7e19208ecb1e368e205a391
juriczech <bohdan.juricek@gmail.com>
2026-01-14 15:34:06 +0100

    feat(suite-native): passphrase enter on trezor
    ^ Conflicts:
    ^	suite-native/passphrase/src/PassphraseEnterOnTrezorScreen.tsx

---
+++ suite-native/module-authorize-device/src/screens/passphrase/PassphraseEnterOnTrezorScreen.tsx
@@ -0,0 +1,55 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
+import { EventType, analytics } from '@suite-native/analytics';
+import { Text } from '@suite-native/atoms';
+import {
+    selectInputPassphraseOnDevice,
+    selectIsCreatingNewPassphraseWallet,
+} from '@suite-native/device-authorization';
+import { Translation } from '@suite-native/intl';
+import { AuthorizeDeviceStackRoutes, useNavigateToInitialScreen } from '@suite-native/navigation';
+import {
+    PassphraseContentScreenWrapper,
+    PassphraseEnterOnTrezorScreenContent,
+} from '@suite-native/passphrase';
+import TrezorConnect from '@trezor/connect';
+
+export const PassphraseEnterOnTrezorScreen = () => {
+    const isCreatingNewPassphraseWallet = useSelector(selectIsCreatingNewPassphraseWallet);
+    const inputPassphraseOnDevice = useSelector(selectInputPassphraseOnDevice);
+    const navigateToInitialScreen = useNavigateToInitialScreen();
+
+    useEffect(() => {
+        if (!isCreatingNewPassphraseWallet && !inputPassphraseOnDevice) {
+            // NOTE: this means that the device passphrase request was fulfilled either success or not,
+            // TrezorConnect will trigger proper events globally and this will be reopened if needed
+            navigateToInitialScreen();
+        }
+    }, [isCreatingNewPassphraseWallet, inputPassphraseOnDevice, navigateToInitialScreen]);
+
+    const handleCancel = () => {
+        analytics.report({
+            type: EventType.PassphraseExit,
+            payload: { screen: AuthorizeDeviceStackRoutes.PassphraseEnterOnTrezor },
+        });
+        TrezorConnect.cancel();
+        navigateToInitialScreen();
+    };
+
+    return (
+        <PassphraseContentScreenWrapper
+            title={<Translation id="modulePassphrase.title" />}
+            subtitle={
+                <Translation
+                    id="modulePassphrase.subtitle"
+                    values={{
+                        bold: chunks => <Text variant="highlight">{chunks}</Text>,
+                    }}
+                />
+            }
+        >
+            <PassphraseEnterOnTrezorScreenContent onCancel={handleCancel} />
+        </PassphraseContentScreenWrapper>
+    );
+};
---
+++ suite-native/passphrase/src/PassphraseEnterOnTrezorScreenContent.tsx
@@ -0,0 +1,68 @@
+import { useDispatch } from 'react-redux';
+
+import { Box, Button, Card, CenteredTitleHeader, VStack } from '@suite-native/atoms';
+import { ConfirmOnTrezorAnimation } from '@suite-native/device';
+import {
+    DeviceAuthorizationStep,
+    changeDeviceAuthorizationStep,
+} from '@suite-native/device-authorization';
+import { Translation } from '@suite-native/intl';
+import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
+
+import { PassphraseMismatchAlert } from './PassphraseMismatchAlert';
+import { useRedirectOnPassphraseCompletion } from './useRedirectOnPassphraseCompletion';
+
+const buttonWrapperStyle = prepareNativeStyle(_ => ({
+    width: '100%',
+}));
+
+const cardStyle = prepareNativeStyle(_ => ({
+    paddingTop: 28,
+}));
+
+type PassphraseEnterOnTrezorScreenContentProps = {
+    onCancel: () => void;
+};
+
+export const PassphraseEnterOnTrezorScreenContent = ({
+    onCancel,
+}: PassphraseEnterOnTrezorScreenContentProps) => {
+    const dispatch = useDispatch();
+
+    const { applyStyle } = useNativeStyles();
+
+    // If this screen was present during authorizing device with passphrase for some feature,
+    // on success, this hook will close the stack and go back
+    useRedirectOnPassphraseCompletion();
+
+    const handleCancel = () => {
+        dispatch(changeDeviceAuthorizationStep(DeviceAuthorizationStep.Idle));
+        onCancel();
+    };
+
+    return (
+        <>
+            <Card style={applyStyle(cardStyle)}>
+                <VStack spacing={28}>
+                    <VStack justifyContent="center" alignItems="center" spacing="sp24">
+                        <ConfirmOnTrezorAnimation />
+                        <CenteredTitleHeader
+                            title={
+                                <Translation id="modulePassphrase.enterPassphraseOnTrezor.title" />
+                            }
+                            subtitle={
+                                <Translation id="modulePassphrase.enterPassphraseOnTrezor.subtitle" />
+                            }
+                        />
+                    </VStack>
+                    <Box style={applyStyle(buttonWrapperStyle)}>
+                        <Button onPress={handleCancel} colorScheme="redElevation1">
+                            <Translation id="generic.buttons.cancel" />
+                        </Button>
+                    </Box>
+                </VStack>
+            </Card>
+            <PassphraseMismatchAlert />
+        </>
+    );
+};
