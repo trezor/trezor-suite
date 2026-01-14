040fe2216f4dccc86775a16c68c841eb63954d0a
Ondrej Hajek <ondrej.hajek@satoshilabs.com>
2026-01-14 15:35:14 +0100

    refactor(ci): Cleaned e2e test yarn scripts, PW config selected by pipelines

--- suite/e2e/playwright-web-pr.config.ts
+++ suite/e2e/playwright-web-pr.config.ts
@@ -9,37 +9,37 @@ const config = defineConfig({
             use: {
                 model: 'T3W1',
             },
-            grep: /(?=.*@T3W1)/,
+            grep: /(?=.*@T3W1)(?=.*@webOnly)/,
         }),
         createProject('T3T1_smoke', baseWebProject, {
             use: {
                 model: 'T3T1',
             },
-            grep: /(?=.*@T3T1)(?=.*@smoke)/,
+            grep: /(?=.*@T3T1)(?=.*@smoke)(?=.*@webOnly)/,
         }),
         createProject('T3B1', baseWebProject, {
             use: {
                 model: 'T3B1',
             },
-            grep: /(?=.*@T3B1)/,
+            grep: /(?=.*@T3B1)(?=.*@webOnly)/,
         }),
         createProject('T2T1', baseWebProject, {
             use: {
                 model: 'T2T1',
             },
-            grep: /(?=.*@T2T1)/,
+            grep: /(?=.*@T2T1)(?=.*@webOnly)/,
         }),
         createProject('T1B1', baseWebProject, {
             use: {
                 model: 'T1B1',
             },
-            grep: /(?=.*@T1B1)/,
+            grep: /(?=.*@T1B1)(?=.*@webOnly)/,
         }),
         createProject('no_device', baseWebProject, {
             use: {
                 model: 'T1B1', // model has to be set even when not used
             },
-            grep: /(?=.*@noDevice)/,
+            grep: /(?=.*@noDevice)(?=.*@webOnly)/,
         }),
     ],
 });
