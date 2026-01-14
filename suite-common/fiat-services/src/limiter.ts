b88c864cc0e7da30baab999fb9ab04751a087cd6
yanas <yanas@yanas.cz>
2026-01-14 10:53:31 +0100

    refactor(suite-common): simplify RateLimiter implementation

--- suite-common/fiat-services/src/limiter.ts
+++ suite-common/fiat-services/src/limiter.ts
@@ -1,45 +1,26 @@
-import { differenceInMilliseconds } from 'date-fns';
-
 import { scheduleAction } from '@trezor/utils';
 
+// Poor man's rate limiter that slows down requests so there is a `delayMs` gap between them.
 export class RateLimiter {
-    // Poor man's rate limiter
-    // Slows down requests so there is `delayMs` gap between them
-    private delayMs: number; // gap between each request
-    private timeoutMs: number;
-    private lastFetchTimestamp: number;
-    private queued: number; // how many requests are waiting to be fired
-    private totalDelay: number; // by how much time are we gonna slown down next request
+    private readonly delayMs: number; // gap between each request
+    private readonly timeoutMs: number;
+
+    private queue = Promise.resolve();
 
     constructor(delayMs: number, timeoutMs: number) {
         this.delayMs = delayMs;
         this.timeoutMs = timeoutMs;
-
-        this.lastFetchTimestamp = 0;
-        this.queued = 0;
-        this.totalDelay = 0;
     }
 
-    async limit<T>(fn: (signal?: AbortSignal) => Promise<T>): Promise<T> {
-        const msSinceLastFetch = differenceInMilliseconds(
-            new Date().getTime(),
-            this.lastFetchTimestamp,
+    limit<T>(fn: (signal?: AbortSignal) => Promise<T>): Promise<T> {
+        const resultPromise = this.queue.then(
+            async () => await scheduleAction(signal => fn(signal), { timeout: this.timeoutMs }),
         );
-        if (msSinceLastFetch < this.delayMs) {
-            this.queued += 1;
-            this.totalDelay += this.delayMs;
-            // dummy wait for this.totalDelay before we fire next request
-            await new Promise(resolve => setTimeout(resolve, this.totalDelay)); // slow down firing next request
-        }
 
-        this.lastFetchTimestamp = new Date().getTime();
-        const results = await scheduleAction(signal => fn(signal), { timeout: this.timeoutMs });
-        this.queued -= 1;
-        if (this.queued === 0) {
-            // if all queued requests were fired, we need to reset totalDelay to properly delay next batch of requests
-            this.totalDelay = 0;
-        }
+        this.queue = resultPromise
+            .catch(error => console.error(error)) // ensure errors don't stop the queue
+            .then(() => new Promise(res => setTimeout(res, this.delayMs)));
 
-        return results;
+        return resultPromise;
     }
 }
