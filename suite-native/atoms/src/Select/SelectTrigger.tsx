4083755b3ed16dffc40eda0972207542d4dd429b
yanas <yanas@yanas.cz>
2026-01-14 13:36:02 +0100

    feat(suite-native): improve Select and SelectTrigger atoms

--- suite-native/atoms/src/Select/SelectTrigger.tsx
+++ suite-native/atoms/src/Select/SelectTrigger.tsx
@@ -3,11 +3,13 @@ import { ReactNode } from 'react';
 import { Icon } from '@suite-native/icons';
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
 
+import { Box } from '../Box';
 import { PressableOpacity } from '../Pressable';
 import { HStack } from '../Stack';
 import { ACCESSIBILITY_FONTSIZE_MULTIPLIER, Text } from '../Text';
 
 type SelectTriggerProps = {
+    label?: ReactNode;
     value: string | null;
     icon?: ReactNode;
     handlePress: () => void;
@@ -22,25 +24,32 @@ const selectStyle = prepareNativeStyle(utils => ({
     alignItems: 'center',
     backgroundColor: utils.colors.backgroundNeutralSubtleOnElevation1,
     borderWidth: utils.borders.widths.small,
-    borderRadius: utils.borders.radii.r8,
-    borderColor: utils.colors.backgroundNeutralSubtleOnElevation1,
+    borderRadius: utils.borders.radii.r12,
+    borderColor: utils.colors.borderInputDefault,
     color: utils.colors.textSubdued,
     paddingLeft: utils.spacings.sp12,
     paddingRight: 23.25,
     height: SELECT_HEIGHT,
 }));
 
-export const SelectTrigger = ({ value, icon, handlePress, testID }: SelectTriggerProps) => {
+export const SelectTrigger = ({ label, value, icon, handlePress, testID }: SelectTriggerProps) => {
     const { applyStyle } = useNativeStyles();
 
     return (
         <PressableOpacity onPress={handlePress} style={applyStyle(selectStyle)} testID={testID}>
-            <HStack alignItems="center">
-                {icon}
-                <Text numberOfLines={1} ellipsizeMode="tail">
-                    {value}
-                </Text>
-            </HStack>
+            <Box>
+                {label && (
+                    <Text variant="label" color="textSubdued">
+                        {label}
+                    </Text>
+                )}
+                <HStack alignItems="center">
+                    {icon}
+                    <Text numberOfLines={1} ellipsizeMode="tail">
+                        {value}
+                    </Text>
+                </HStack>
+            </Box>
             <Icon size="large" color="iconSubdued" name="caretDown" />
         </PressableOpacity>
     );
