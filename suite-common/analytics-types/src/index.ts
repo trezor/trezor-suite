8fbc106067a819ebac25dc47b2fb20f95d48ee7f
Jan Václavík <jvaclavik@gmail.com>
2026-01-17 17:24:58 +0100

    fix(suite): Unify imports

--- suite-common/analytics-types/src/index.ts
+++ suite-common/analytics-types/src/index.ts
@@ -1,7 +1,6 @@
 export { EventType } from './constants';
 export { type SuiteSharedLegacyAnalyticsEvents } from './types';
 export { type AnalyticsSharedEvents } from './analyticsEvents';
-
-// do not use this directly, it should be used for generating docs
-export * as sharedEventsData from './events';
 export type { AttributeDef, EventDef, EventInstance } from './eventDefinition';
+
+export * as events from './events';
