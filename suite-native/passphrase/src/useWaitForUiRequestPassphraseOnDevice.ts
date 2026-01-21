12e72160d225e2a57dcde692b17320559575db65
juriczech <bohdan.juricek@gmail.com>
2026-01-21 15:35:34 +0100

    fix(suite-native): handle TS7 pin

--- suite-native/passphrase/src/useWaitForUiRequestPassphraseOnDevice.ts
+++ suite-native/passphrase/src/useWaitForUiRequestPassphraseOnDevice.ts
@@ -1,15 +1,11 @@
-import { useCallback } from 'react';
-
-import { useFocusEffect } from '@react-navigation/native';
+import { useEffect } from 'react';
 
 import TrezorConnect, { UI } from '@trezor/connect';
 
 export const useWaitForUiRequestPassphraseOnDevice = (callback: () => void) => {
-    useFocusEffect(
-        useCallback(() => {
-            TrezorConnect.on(UI.REQUEST_PASSPHRASE_ON_DEVICE, callback);
+    useEffect(() => {
+        TrezorConnect.on(UI.REQUEST_PASSPHRASE_ON_DEVICE, callback);
 
-            return () => TrezorConnect.off(UI.REQUEST_PASSPHRASE_ON_DEVICE, callback);
-        }, [callback]),
-    );
+        return () => TrezorConnect.off(UI.REQUEST_PASSPHRASE_ON_DEVICE, callback);
+    }, [callback]);
 };
