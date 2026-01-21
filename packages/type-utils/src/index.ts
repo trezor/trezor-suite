6fa00c8c29b610186c87e903bdad92a7768092b0
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-01-21 10:07:53 +0100

    fix(ci): disable Nx tsc cache again

--- packages/type-utils/src/index.ts
+++ packages/type-utils/src/index.ts
@@ -6,6 +6,3 @@ export * from './overloads';
 export * from './result';
 export * from './timeout';
 export * from './utils';
-
-// no-op change to trigger Nx cache invalidation
-// TODO remove this after 1st February 2026
