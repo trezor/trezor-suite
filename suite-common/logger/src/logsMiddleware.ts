cb90ad55c96bfbe5390fc09089257e89242fdfa6
Petr Knetl <petr.knetl@satoshilabs.com>
2026-01-22 12:54:04 +0100

    refactor(suite): enhance common logger middleware

--- suite-common/logger/src/logsMiddleware.ts
+++ suite-common/logger/src/logsMiddleware.ts
@@ -1,14 +1,88 @@
 import { isAnyOf } from '@reduxjs/toolkit';
 
+import { analyticsActions } from '@suite-common/analytics';
 import { createMiddleware } from '@suite-common/redux-utils';
-import { accountsActions, blockchainActions } from '@suite-common/wallet-core';
+import {
+    WALLET_SETTINGS,
+    accountsActions,
+    blockchainActions,
+    changeNetworks,
+    deviceActions,
+    setBaseCurrency,
+} from '@suite-common/wallet-core';
 import { getAccountIdentifier } from '@suite-common/wallet-utils';
+import { DEVICE, TRANSPORT } from '@trezor/connect';
 
 import { addLog } from './logsSlice';
 
 export const logsMiddleware = createMiddleware((action, { next, dispatch }) => {
+    if (changeNetworks.match(action)) {
+        dispatch(
+            addLog({
+                action,
+                type: action.type,
+            }),
+        );
+    }
+
+    if (deviceActions.addButtonRequest.match(action)) {
+        if (action.payload.buttonRequest) {
+            dispatch(
+                addLog({
+                    type: action.type,
+                    payload: {
+                        code: action.payload.buttonRequest.code,
+                    },
+                }),
+            );
+        }
+    }
+
+    if (
+        isAnyOf(
+            setBaseCurrency,
+            analyticsActions.enableAnalytics,
+            analyticsActions.disableAnalytics,
+        )(action) ||
+        action.type === WALLET_SETTINGS.SET_HIDE_BALANCE
+    ) {
+        dispatch(addLog({ type: action.type, payload: { ...action, type: undefined } }));
+    }
+
+    if (
+        isAnyOf(deviceActions.updateSelectedDevice, deviceActions.setRememberDevice)(action) ||
+        action.type === DEVICE.CONNECT ||
+        action.type === DEVICE.DISCONNECT
+    ) {
+        dispatch(
+            addLog({
+                type: action.type,
+                payload: {
+                    ...action.payload,
+                    firmwareReleaseConfigInfo: undefined,
+                    unavailableCapabilities: undefined,
+                },
+            }),
+        );
+    }
+
     if (isAnyOf(accountsActions.createAccount, accountsActions.updateAccount)(action)) {
         dispatch(addLog({ type: action.type, payload: { ...action.payload } }));
+    }
+
+    if (action.type === TRANSPORT.START) {
+        dispatch(
+            addLog({
+                type: action.type,
+                payload: {
+                    type: action.payload.type,
+                    version: action.payload.version,
+                },
+            }),
+        );
+    }
+    if (action.type === TRANSPORT.ERROR) {
+        dispatch(addLog({ type: action.type, payload: { error: action.payload.error } }));
     }
 
     if (isAnyOf(accountsActions.updateSelectedAccount)(action)) {
