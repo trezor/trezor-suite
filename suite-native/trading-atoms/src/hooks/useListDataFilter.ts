ffc06bd8404ecafa95f323cd25ec2ef0c473ba07
Jirka Bažant <jiri.bazant@satoshilabs.com>
2026-01-22 10:08:01 +0100

    feat(suite-native): Improve country of residence search and sorting

--- suite-native/trading-atoms/src/hooks/useListDataFilter.ts
+++ suite-native/trading-atoms/src/hooks/useListDataFilter.ts
@@ -3,6 +3,7 @@ import { useMemo, useState } from 'react';
 export const useListDataFilter = <T>(
     rawData: T[],
     filterCallback: (item: T, filterValue: string) => boolean,
+    sortCallback?: (a: T, b: T, filterValue: string) => number,
 ): {
     filteredData: T[];
     filterValue: string;
@@ -11,12 +12,18 @@ export const useListDataFilter = <T>(
     const [filterValue, setFilterValue] = useState('');
 
     const filteredData = useMemo(() => {
+        let data = [...rawData];
+
         if (filterValue?.length > 0) {
-            return rawData.filter(item => filterCallback(item, filterValue));
+            data = rawData.filter(item => filterCallback(item, filterValue));
         }
 
-        return rawData;
-    }, [rawData, filterValue, filterCallback]);
+        if (sortCallback) {
+            data.sort((a, b) => sortCallback(a, b, filterValue));
+        }
+
+        return data;
+    }, [rawData, filterValue, filterCallback, sortCallback]);
 
     return { filteredData, filterValue, setFilterValue };
 };
--- suite-native/trading-residence/src/hooks/__tests__/useCountryFilteredData.test.ts
+++ suite-native/trading-residence/src/hooks/__tests__/useCountryFilteredData.test.ts
@@ -34,7 +34,9 @@ describe('useCountryFilteredData', () => {
             result.current.setFilterValue('CZEch');
         });
 
-        expect(result.current.filteredData).toEqual([expect.objectContaining({ value: 'CZ' })]);
+        expect(result.current.filteredData).toEqual([
+            expect.objectContaining({ value: 'CZ', name: 'Czechia' }),
+        ]);
     });
 
     it('should filter by value case-insensitive', () => {
@@ -45,7 +47,101 @@ describe('useCountryFilteredData', () => {
         });
 
         expect(result.current.filteredData).toEqual(
-            expect.arrayContaining([expect.objectContaining({ value: 'US' })]),
+            expect.arrayContaining([
+                expect.objectContaining({ value: 'US', name: 'United States of America' }),
+            ]),
         );
+    });
+
+    it('should filter by codeAlpha3 case-insensitive', () => {
+        const { result } = renderUseCountryFilteredData();
+
+        act(() => {
+            result.current.setFilterValue('uSa');
+        });
+
+        expect(result.current.filteredData).toEqual([
+            expect.objectContaining({ codeAlpha3: 'USA', name: 'United States of America' }),
+        ]);
+    });
+
+    describe('sorting', () => {
+        it('should sort by name when no filter is applied', () => {
+            const { result } = renderUseCountryFilteredData();
+            expect(result.current.filteredData[0].name).toBe('Åland Islands'); // Åland Islands
+        });
+
+        it('should place exact match on name before other results', () => {
+            const { result } = renderUseCountryFilteredData();
+
+            act(() => {
+                result.current.setFilterValue('guinea');
+            });
+
+            expect(result.current.filteredData[0]).toEqual(
+                expect.objectContaining({ name: 'Guinea' }),
+            );
+        });
+
+        it('should place exact match on 2 digit code before other results', () => {
+            const { result } = renderUseCountryFilteredData();
+
+            act(() => {
+                result.current.setFilterValue('uS');
+            });
+
+            expect(result.current.filteredData[0]).toEqual(
+                expect.objectContaining({ value: 'US', name: 'United States of America' }),
+            );
+            expect(result.current.filteredData[1]).toEqual(
+                expect.objectContaining({ name: 'Australia' }),
+            );
+        });
+
+        it('should place exact match on 3 digit code before other results', () => {
+            const { result } = renderUseCountryFilteredData();
+
+            act(() => {
+                result.current.setFilterValue('pol');
+            });
+
+            expect(result.current.filteredData).toEqual([
+                expect.objectContaining({ name: 'Poland', codeAlpha3: 'POL' }),
+                expect.objectContaining({ name: 'French Polynesia' }),
+            ]);
+        });
+
+        it('should put matches on beginning before other matches - filter "sa"', () => {
+            const { result } = renderUseCountryFilteredData();
+
+            act(() => {
+                result.current.setFilterValue('sa');
+            });
+
+            expect(result.current.filteredData[0]).toEqual(
+                expect.objectContaining({ name: 'Saudi Arabia', value: 'SA' }), // exact match on value
+            );
+            expect(result.current.filteredData[1]).toEqual(
+                expect.objectContaining({ name: 'Saint Barthélemy' }), // starts with match on name
+            );
+            expect(result.current.filteredData[11]).toEqual(
+                expect.objectContaining({ name: 'American Samoa' }), // other match (that starts with a)
+            );
+        });
+
+        it('should put matches on beginning before other matches - filter "Guinea"', () => {
+            const { result } = renderUseCountryFilteredData();
+
+            act(() => {
+                result.current.setFilterValue('Guinea');
+            });
+
+            expect(result.current.filteredData).toEqual([
+                expect.objectContaining({ name: 'Guinea' }), // exact match on name
+                expect.objectContaining({ name: 'Guinea-Bissau' }), // starts with match on name
+                expect.objectContaining({ name: 'Equatorial Guinea' }), // other match
+                expect.objectContaining({ name: 'Papua New Guinea' }), // other match
+            ]);
+        });
     });
 });
