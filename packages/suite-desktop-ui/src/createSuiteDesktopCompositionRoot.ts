0df95b0d94727150a0390c0afc4b02632d70c755
Vojtěch Tranta <vojta.tranta@gmail.com>
2026-01-23 14:30:26 +0100

    fix: unused code

--- packages/suite-desktop-ui/src/createSuiteDesktopCompositionRoot.ts
+++ packages/suite-desktop-ui/src/createSuiteDesktopCompositionRoot.ts
@@ -7,7 +7,7 @@ import { PreloadStoreAction } from 'src/support/suite/preloadStore';
 
 export const createSuiteDesktopCompositionRoot = (
     preloadStoreAction?: PreloadStoreAction,
-    statePatch: Record<string, any> = {},
+    statePatch: Record<string, any> = undefined,
 ) => {
     const history = createMemoryHistory();
     const flushSuiteSyncStorage = () => {
