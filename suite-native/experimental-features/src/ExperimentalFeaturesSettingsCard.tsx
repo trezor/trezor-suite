ac2ed7b95ca3427bbe061d7c0856600aa797f2bd
Matej Kriz <matej@matejkriz.cz>
2026-01-23 16:58:07 +0100

    feat(suite-sync): delete redundant debug settings for Suite Sync replaced by experimantal features

--- suite-native/experimental-features/src/ExperimentalFeaturesSettingsCard.tsx
+++ suite-native/experimental-features/src/ExperimentalFeaturesSettingsCard.tsx
@@ -64,7 +64,6 @@ export const ExperimentalFeaturesSettingsCard = () => {
         enabledFeatures?.forEach(featureKey =>
             EXPERIMENTAL_FEATURES[featureKey]?.onToggle?.({
                 newValue: !areExperimentalFeaturesAllowed,
-                dispatch,
                 services,
             }),
         );
--- suite-native/experimental-features/src/experimentalFeatures.ts
+++ suite-native/experimental-features/src/experimentalFeatures.ts
@@ -1,6 +1,3 @@
-import { Dispatch } from '@reduxjs/toolkit';
-
-import { suiteSyncActions } from '@suite-common/suite-sync';
 import { TxKeyPath } from '@suite-native/intl';
 import { NativeServices } from '@suite-native/services';
 import { ExperimentalFeature } from '@suite-native/settings';
@@ -8,22 +5,18 @@ import { ExperimentalFeature } from '@suite-native/settings';
 export type ExperimentalFeatureConfig = {
     titleKey: TxKeyPath;
     descriptionKey: TxKeyPath;
-    onToggle?: ({ newValue, dispatch, services }: { newValue: boolean; dispatch: Dispatch; services: NativeServices }) => void;
+    onToggle?: ({ newValue, services }: { newValue: boolean; services: NativeServices }) => void;
 };
 
 export const EXPERIMENTAL_FEATURES: Record<ExperimentalFeature, ExperimentalFeatureConfig> = {
     'suite-sync': {
         titleKey: 'moduleSettings.advanced.experimentalFeatures.suiteSync.title',
         descriptionKey: 'moduleSettings.advanced.experimentalFeatures.suiteSync.description',
-        onToggle: ({ newValue, dispatch, services }) => {
+        onToggle: ({ newValue, services }) => {
             if (!newValue) {
+                // Turn off Suite Sync
                 services.suiteSync.turnOffSuiteSync();
             }
-            dispatch(
-                suiteSyncActions.updateIsFeatureSuiteSyncAvailable({
-                    isShownInSettings: newValue,
-                }),
-            );
         },
     },
 };
