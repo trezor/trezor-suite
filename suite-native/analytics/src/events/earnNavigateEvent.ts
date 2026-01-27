d0cd556fee7b2acf46593f3d58c3670d99cbdb49
Jan Václavík <jvaclavik@gmail.com>
2026-01-27 20:55:55 +0100

    fix(suite): fix other events

--- suite-native/analytics/src/events/earnNavigateEvent.ts
+++ suite-native/analytics/src/events/earnNavigateEvent.ts
@@ -6,5 +6,4 @@ export const earnNavigateEvent: EventDef<undefined, EventType.EearnNavigate> = {
     name: EventType.EearnNavigate,
     descriptionTrigger: 'On Earn Page opened',
     changelog: [{ version: '26.1.2', notes: 'added' }],
-    attributes: undefined,
 };
