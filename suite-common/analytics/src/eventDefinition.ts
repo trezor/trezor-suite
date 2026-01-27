8b4b6203279d11434dc074ea2b1ca87b5f4001d3
Jan Václavík <jvaclavik@gmail.com>
2026-01-27 19:21:59 +0100

    feat(suite): Support analytics event payload like Record<string, number>

--- suite-common/analytics/src/eventDefinition.ts
+++ suite-common/analytics/src/eventDefinition.ts
@@ -21,24 +21,43 @@ export type AttributeDef<T> = AnalyticsBaseAttribute & {
 
 type Domain = string;
 type EventName = `${Domain}/${string}` | `${string}`;
-type HasAttributes<A> = keyof A extends never ? false : true;
 
-export type EventDef<A, N extends EventName = EventName> = AnalyticsBaseEvent & {
-    name: N;
-    attributes: A;
-};
+type HasKeys<T> = keyof T extends never ? false : true;
+type IsAttributeMap<T> = T extends Record<string, AttributeDef<any>> ? true : false;
 
 type AttributePayload<T> = NonNullable<T> extends AttributeDef<infer V> ? V : never;
+
+type AttributeEventInstance<A, N> =
+    HasKeys<A> extends true
+        ? {
+              type: N;
+              payload: {
+                  [K in keyof A]: AttributePayload<A[K]>;
+              };
+          }
+        : {
+              type: N;
+          };
+
+type PayloadEventInstance<A, N> = {
+    type: N;
+    payload: A;
+};
+
+export type EventDef<A, N extends EventName = EventName> =
+    IsAttributeMap<A> extends true
+        ? AnalyticsBaseEvent & {
+              name: N;
+              attributes: A;
+          }
+        : AnalyticsBaseEvent & {
+              name: N;
+              payloadType?: A;
+          };
+
 export type EventInstance<E extends EventDef<any, any>> =
     E extends EventDef<infer A, infer N>
-        ? HasAttributes<A> extends true
-            ? {
-                  type: N;
-                  payload: {
-                      [K in keyof A]: AttributePayload<A[K]>;
-                  };
-              }
-            : {
-                  type: N;
-              }
+        ? IsAttributeMap<A> extends true
+            ? AttributeEventInstance<A, N>
+            : PayloadEventInstance<A, N>
         : never;
