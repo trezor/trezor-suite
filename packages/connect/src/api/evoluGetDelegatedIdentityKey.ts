aeae4fa8c6e658842c6042752ce004c4bf5d4298
Peter Sanderson <dierk.deidel@protonmail.com>
2026-01-27 22:31:08 +0100

    fix: do not send host_static_public_key as it is not needed
    # Conflicts:
    #	packages/connect/src/api/evoluGetDelegatedIdentityKey.ts

--- packages/connect/src/api/evoluGetDelegatedIdentityKey.ts
+++ packages/connect/src/api/evoluGetDelegatedIdentityKey.ts
@@ -1,4 +1,5 @@
 import type { MessagesSchema as PROTO } from '@trezor/protobuf';
+import { versionUtils } from '@trezor/utils';
 
 import type { MethodMessage, MethodPermission } from '../core/AbstractMethod';
 import { AbstractMethod } from '../core/AbstractMethod';
@@ -28,11 +29,17 @@ export default class EvoluGetDelegatedIdentityKey extends AbstractMethod<
 
     async run() {
         const thpState = this.getDevice().getThpState();
+        const version = this.device.getVersion();
         if (thpState) {
             this.params = {
                 thp_credential: thpState.pairingCredentials[0].credential,
-                host_static_public_key:
-                    thpState.handshakeCredentials?.hostStaticPublicKey.toString('hex'),
+
+                ...(version !== undefined && versionUtils.isNewer(version, [2, 10, 0])
+                    ? {} // The `host_static_public_key` is no longer required
+                    : {
+                          host_static_public_key:
+                              thpState.handshakeCredentials?.hostStaticPublicKey.toString('hex'),
+                      }),
             };
         }
 
