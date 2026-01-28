16a98eac69fca20598da886af1ee1245ab75f04f
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-01-28 14:08:12 +0100

    feat(sentry): hard limit on sentry events per session

--- suite-common/sentry/src/constants.ts
+++ suite-common/sentry/src/constants.ts
@@ -1,3 +1,5 @@
 export const ALLOW_REPORT_TAG = 'allowReport';
 export const COINJOIN_REPORT_TAG = 'coinjoinReport';
 export const COINJOIN_NETWORK_TAG = 'coinjoinNetwork';
+export const MAX_EVENTS_PER_INTERVAL = 100;
+export const MAX_EVENTS_INTERVAL_LENGTH = 3_600_000; // [ms]
