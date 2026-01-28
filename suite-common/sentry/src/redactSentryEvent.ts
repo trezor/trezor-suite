8a763884bac2fc3d7af1366f6d173faa108bd264
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-01-28 10:16:53 +0100

    chore(sentry): rename consts to uppercase

--- suite-common/sentry/src/redactSentryEvent.ts
+++ suite-common/sentry/src/redactSentryEvent.ts
@@ -1,13 +1,13 @@
 import type { ErrorEvent } from '@sentry/core';
 
-import { allowReportTag } from './constants';
+import { ALLOW_REPORT_TAG } from './constants';
 
 /**
  * Common function in all Sentry application to redact a Sentry event or filter it out completely (then returns null).
  */
 export const redactSentryEvent = (event: ErrorEvent): ErrorEvent | null => {
     // sentry events are skipped until user confirm analytics reporting
-    const allowReport = event.tags?.[allowReportTag];
+    const allowReport = event.tags?.[ALLOW_REPORT_TAG];
 
     if (allowReport === false) {
         return null;
