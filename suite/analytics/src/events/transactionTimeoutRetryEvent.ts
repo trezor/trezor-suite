4e341bc45a8dc36c473546ee3eae7cb3cd46ed4f
Jan Václavík <jvaclavik@gmail.com>
2026-01-28 06:40:35 +0100

    refactor(suite): Migrate transactionTimeoutRetryEvent

---
+++ suite/analytics/src/events/transactionTimeoutRetryEvent.ts
@@ -0,0 +1,21 @@
+import type { AttributeDef, EventDef } from '@suite-common/analytics-types';
+
+import { EventType } from '../constants';
+
+type Attributes = {
+    url: AttributeDef<string>;
+};
+
+export const transactionTimeoutRetryEvent: EventDef<Attributes, EventType.TransactionRetry> = {
+    name: EventType.TransactionRetry,
+    descriptionTrigger:
+        'fired when a transaction has timed out and the user clicks on the retry button',
+    changelog: [{ version: '25.4.0', notes: 'added' }],
+
+    attributes: {
+        url: {
+            changelog: [{ version: '25.4.0', notes: 'added' }],
+            description: 'current URL where the retry button has been clicked',
+        },
+    },
+};
