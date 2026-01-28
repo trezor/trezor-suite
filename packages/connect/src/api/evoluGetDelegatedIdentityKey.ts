4acd26a93f887e4b5f941024afb186964326da24
Peter Sanderson <dierk.deidel@protonmail.com>
2026-01-28 18:26:14 +0100

    fix: increse the required FW for Evolu, to a version where host_static_public_key is no longer required

--- packages/connect/src/api/evoluGetDelegatedIdentityKey.ts
+++ packages/connect/src/api/evoluGetDelegatedIdentityKey.ts
@@ -1,5 +1,4 @@
 import type { MessagesSchema as PROTO } from '@trezor/protobuf';
-import { versionUtils } from '@trezor/utils';
 
 import type { MethodMessage, MethodPermission } from '../core/AbstractMethod';
 import { AbstractMethod } from '../core/AbstractMethod';
@@ -29,17 +28,9 @@ export default class EvoluGetDelegatedIdentityKey extends AbstractMethod<
 
     async run() {
         const thpState = this.getDevice().getThpState();
-        const version = this.device.getVersion();
         if (thpState) {
             this.params = {
                 thp_credential: thpState.pairingCredentials[0].credential,
-
-                ...(version !== undefined && versionUtils.isNewer(version, [2, 10, 0])
-                    ? {} // The `host_static_public_key` is no longer required
-                    : {
-                          host_static_public_key:
-                              thpState.handshakeCredentials?.hostStaticPublicKey.toString('hex'),
-                      }),
             };
         }
 
