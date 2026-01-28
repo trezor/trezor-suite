400195c96e8ad57bf95175ba86561e56a8c4a5c1
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-01-28 18:47:48 +0100

    chore(sentry): copy common errors to outbound filter from ingest inbound filter

--- suite-common/sentry/src/ignoreErrors.ts
+++ suite-common/sentry/src/ignoreErrors.ts
@@ -3,15 +3,58 @@ import type { Options } from '@sentry/core';
 /**
  * List of Sentry error messages to be filtered client-side, which are either irrelevant or out of our control.
  * Note that when you add it here, inbound filters in Sentry ingest are still needed for older Suite versions.
+ *
+ * This is a good place to add commonly occurring lifecycle errors that may happen during normal use (not preventable).
+ * Do not add here errors that are now definitively fixed (those belong in Sentry ingest inbound filters).
  */
-export const ignoreErrors = [
-    'ERR_INTERNET_DISCONNECTED',
-    'ERR_NETWORK_IO_SUSPENDED',
-    'ERR_NETWORK_CHANGED',
-    'Error: HTTP Error',
-    'ResizeObserver loop limit exceeded',
+const ignoreErrorsCommon = [
+    /.*Sequence of config.*is older than the current one.*/,
+
+    // Various kinds of network connectivity problems
+    /.*ERR_INTERNET_DISCONNECTED.*/,
+    /.*ERR_NETWORK_IO_SUSPENDED.*/,
+    /.*ERR_NETWORK_CHANGED.*/,
+    /.*ERR_CONNECTION_TIMED_OUT.*/,
+    /.*HTTP Error.*/,
+    /.*ERR_NAME_NOT_RESOLVED.*/,
+    /.*Websocket timeout.*/,
+    /.*Socket is closed.*/,
+    /.*websocket was closed.*/,
+    /.*Timeout for request.*/,
+    /.*Websocket closed unexpectedly.*/,
+    /.*Failed to fetch.*/,
+    /.*Aborted by timeout.*/,
+    /.*Client network socket disconnected before secure TLS connection was established.*/,
+    /.*Failed to open URL.*/,
+    /.*Fetching of remote JWS config failed: Error: Aborted by timeout.*/,
+    /.*failed to get recent blockhash: TypeError: fetch failed.*/,
+
+    // Common transport or Connect lifecycle errors
+    /.*Transport stopped.*/,
+    /.*Response of unexpected type.*/,
     // comes from bridge originally, we allowed user to init another connect call. should now be wrapped however and not thrown on transport layer
-    'other call in progress',
-    'Action canceled by user',
-    'device disconnected during action', // the same as with 'other call in progress'
+    /.*other call in progress.*/,
+    /.*session not found.*/,
+    /.*Action canceled by user.*/,
+    /.*device disconnected during action.*/, // the same as with 'other call in progress'
+] satisfies Options['ignoreErrors'];
+
+export const ignoreErrorsSuite = [
+    ...ignoreErrorsCommon,
+    /.*ResizeObserver loop limit exceeded.*/,
+    /.*Timeout waiting for TOR control port.*/,
+    /.*write EPIPE.*/,
+
+    // Common IDB lifecycle errors
+    /.*The database connection is closing.*/,
+    /.*Error: InvalidStateError: Failed to execute 'transaction' on 'IDBDatabase'.*/,
+
+    // Common Electron lifecycle errors
+    /.*Frame property was accessed after it navigated or was destroyed.*/, // Renderer process already closed while main is still responding to its IPC
+] satisfies Options['ignoreErrors'];
+
+export const ignoreErrorsSuiteMobile = [
+    ...ignoreErrorsCommon,
+    /.*Websocket closed.*/,
+    /.*Network request failed.*/,
 ] satisfies Options['ignoreErrors'];
--- suite-common/sentry/src/index.ts
+++ suite-common/sentry/src/index.ts
@@ -1,4 +1,4 @@
 export * from './constants';
-export { ignoreErrors } from './ignoreErrors';
+export { ignoreErrorsSuite, ignoreErrorsSuiteMobile } from './ignoreErrors';
 export { SENTRY_CONFIG } from './suiteConfig';
 export { redactSentryEvent } from './redactSentryEvent';
