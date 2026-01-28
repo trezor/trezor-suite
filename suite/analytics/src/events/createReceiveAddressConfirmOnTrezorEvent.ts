51ea7c22429de883a5d11b928cc989f226b03956
Jan Václavík <jvaclavik@gmail.com>
2026-01-28 06:49:49 +0100

    refactor(suite): Migrate createReceiveAddressConfirmOnTrezorEvent

---
+++ suite/analytics/src/events/createReceiveAddressConfirmOnTrezorEvent.ts
@@ -0,0 +1,23 @@
+import type { AttributeDef, EventDef } from '@suite-common/analytics-types';
+import { NetworkSymbol } from '@suite-common/wallet-config';
+
+import { EventType } from '../constants';
+
+type Attributes = {
+    assetSymbol: AttributeDef<NetworkSymbol>;
+};
+
+export const createReceiveAddressConfirmOnTrezorEvent: EventDef<
+    Attributes,
+    EventType.CreateReceiveAddressConfirmOnTrezor
+> = {
+    name: EventType.CreateReceiveAddressConfirmOnTrezor,
+    descriptionTrigger: 'Desktop: Receive → Show full address → Confirm on Trezor',
+    changelog: [{ version: '25.4.1', notes: 'added' }],
+
+    attributes: {
+        assetSymbol: {
+            changelog: [{ version: '25.4.1', notes: 'added' }],
+        },
+    },
+};
