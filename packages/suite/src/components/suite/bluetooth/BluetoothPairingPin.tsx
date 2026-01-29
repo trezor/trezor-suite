a74b35d6aa1af9a7fa9380e8d978bf2d3ae74f46
Vojtěch Tranta <vojta.tranta@gmail.com>
2026-01-29 09:37:58 +0100

    fix: proper wording of the connection states

--- packages/suite/src/components/suite/bluetooth/BluetoothPairingPin.tsx
+++ packages/suite/src/components/suite/bluetooth/BluetoothPairingPin.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
-import { Card, Row, Text } from '@trezor/components';
+import { Translation } from '@suite/intl';
+import { Card, Modal, Row, Text } from '@trezor/components';
 import { spacingsPx } from '@trezor/theme';
 
 import { BluetoothDeviceComponent } from './BluetoothDeviceComponent';
@@ -13,15 +14,22 @@ const Pin = styled.span`
 type BluetoothPairingPinProps = {
     pairingPin?: string;
     device: DesktopBluetoothDevice;
+    onCancel: () => void;
 };
 
-export const BluetoothPairingPin = ({ pairingPin, device }: BluetoothPairingPinProps) => (
-    <Card overflow="hidden" paddingType="large">
-        <Row gap={8} justifyContent="space-between" padding={{ horizontal: 8, vertical: 4 }}>
-            <Text typographyStyle="titleLarge">
-                <Pin>{pairingPin}</Pin>
-            </Text>
-            <BluetoothDeviceComponent device={device} />
-        </Row>
-    </Card>
+export const BluetoothPairingPin = ({ pairingPin, device, onCancel }: BluetoothPairingPinProps) => (
+    <Modal
+        onCancel={onCancel}
+        heading={<Translation id="TR_CONFIRM_PAIRING_TREZOR" />}
+        description={<Translation id="TR_CONFIRM_PAIRING_TREZOR_DESCRIPTION" />}
+    >
+        <Card overflow="hidden" paddingType="large">
+            <Row gap={8} justifyContent="space-between" padding={{ horizontal: 8, vertical: 4 }}>
+                <Text typographyStyle="titleLarge">
+                    <Pin>{pairingPin}</Pin>
+                </Text>
+                <BluetoothDeviceComponent device={device} />
+            </Row>
+        </Card>
+    </Modal>
 );
