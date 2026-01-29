1f5b73b961336de49f03ddc3f73843702693b5dd
Matěj Husák <matej.husak@icloud.com>
2026-01-29 15:41:18 +0100

    feat(common): erase QM data on certain events

--- suite-common/suite-sync-quota-manager/src/quotaManagerReducer.ts
+++ suite-common/suite-sync-quota-manager/src/quotaManagerReducer.ts
@@ -31,6 +31,10 @@ export const suiteSyncQuotaManagerReducer = createReducer<SuiteSyncQuotaManagerS
         builder
             .addCase(updateQuotaManagerBaseUrl, (state, { payload }) => {
                 state.baseUrl = payload.baseUrl;
+
+                // we clear the fetched data when the base URL changes
+                state.registeredDevices = [];
+                state.ownersAllowance = [];
             })
             .addCase(eraseFetchedDataDebug, state => {
                 state.registeredDevices = [];
@@ -38,6 +42,12 @@ export const suiteSyncQuotaManagerReducer = createReducer<SuiteSyncQuotaManagerS
             })
             .addCase(quotaManagerEnabledUpdated, (state, { payload }) => {
                 state.enabled = payload.isEnabled;
+
+                // we clear the data when the QM is disabled
+                if (payload.isEnabled === false) {
+                    state.registeredDevices = [];
+                    state.ownersAllowance = [];
+                }
             })
             .addCase(quotaManagerDeviceFetched, (state, { payload }) => {
                 const existingDevice = state.registeredDevices.find(
