d236d6d886904b09f712665970940f7bb671cdd1
Szymon Lesisz <sz.lesisz@gmail.com>
2026-01-29 18:28:46 +0100

    fix(suite): onboarding THP pairing/connection modal

--- packages/suite/src/views/onboarding/steps/ThpPairingConfirmStep.tsx
+++ packages/suite/src/views/onboarding/steps/ThpPairingConfirmStep.tsx
@@ -1,9 +1,4 @@
-import { useCallback } from 'react';
-import { useIntl } from 'react-intl';
-
-import { messages } from '@suite/intl';
 import { TrezorDevice } from '@suite-common/suite-types';
-import TrezorConnect from '@trezor/connect';
 
 import { ConfirmActionModal } from '../../../components/suite/modals/ReduxModal/DeviceContextModal/ConfirmActionModal';
 
@@ -11,19 +6,10 @@ type ThpPairingConfirmStepParams = {
     device: TrezorDevice;
 };
 
-export const ThpPairingConfirmStep = ({ device }: ThpPairingConfirmStepParams) => {
-    const intl = useIntl();
-
-    const abort = useCallback(
-        () => TrezorConnect.cancel(intl.formatMessage(messages.TR_CANCELLED)),
-        [intl],
-    );
-
-    return (
-        <ConfirmActionModal
-            device={device}
-            title="TR_THP_SECURELY_CONNECT_WITH_TREZOR"
-            onCancel={abort}
-        />
-    );
-};
+export const ThpPairingConfirmStep = ({ device }: ThpPairingConfirmStepParams) => (
+    <ConfirmActionModal
+        device={device}
+        title="TR_THP_SECURELY_CONNECT_WITH_TREZOR"
+        enableBackdropClick={false}
+    />
+);
