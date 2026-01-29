a945c62d36167a761004592c2e932cdffab5590d
Jiří Čermák <jiri.cermak@satoshilabs.com>
2026-01-29 15:56:56 +0100

    feat: add security headers for web build preview
    - Define security headers for local testing of production web build.
    - Use Vite to run the preview local server.

---
+++ packages/suite-web/constants/webSecurityHeaders.ts
@@ -0,0 +1,72 @@
+import type { SecurityHeaders } from '../types/securityHeaders';
+import { formatContentSecurityPolicy } from '../utils/csp';
+import { createReportingHeaders } from '../utils/reportingHeaders';
+
+const SENTRY_REPORT_URL =
+    'https://o117836.ingest.us.sentry.io/api/5193825/security/?sentry_key=6d91ca6e6a5d4de7b47989455858b5f6';
+
+/**
+ * Response security headers applied during `yarn build:preview` / `yarn preview` command.
+ * - There's equal to the ones deployed in production (https://suite.trezor.io).
+ */
+const PRODUCTION_SECURITY_HEADERS = {
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+     */
+    'strict-transport-security': 'max-age=31536000',
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Upgrade-Insecure-Requests
+     */
+    'upgrade-insecure-requests': '1',
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+     */
+    'x-content-type-options': 'nosniff',
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+     * Prevent non-https://suite.trezor.io websites from embedding our website in their iframe.
+     */
+    'x-frame-options': 'SAMEORIGIN',
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Referrer-Policy
+     * This header prevents URL data leakage for foreign sites.
+     * If user clicks on a link redirecting to another (non suite.trezor.io) site, the site receives only `https://suite.trezor.io` in the referrer response header instead of whole URL
+     * It won't receive the referrer header if the target site doesn't use https.
+     */
+    'referrer-policy': 'strict-origin-when-cross-origin',
+
+    /**
+     * @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cross-Origin-Opener-Policy
+     * The `same-origin` header ensures the `window.opener` is `null` even without explicitly using `noopener` unless it opens website of same origin.
+     * So foreign sites can't get reference and potentially exploit the Window object.
+     */
+    'cross-origin-opener-policy': 'same-origin',
+
+    'content-security-policy': {
+        'default-src': ['self'],
+        'script-src-attr': ['none'],
+        'style-src': ['self', 'unsafe-inline'],
+        'style-src-elem': ['self', 'unsafe-inline'],
+        'img-src': ['self', 'blob:', 'data:', 'https://*.trezor.io'],
+        'connect-src': ['data:', '*'],
+        'upgrade-insecure-requests': true,
+        'script-src': ['self', 'unsafe-eval'],
+        'report-uri': SENTRY_REPORT_URL,
+        'report-to': 'csp-endpoint',
+    },
+
+    ...createReportingHeaders('csp-endpoint', SENTRY_REPORT_URL),
+} as const satisfies Partial<SecurityHeaders>;
+
+export function getSecurityHeaders() {
+    return {
+        ...PRODUCTION_SECURITY_HEADERS,
+        'content-security-policy': formatContentSecurityPolicy(
+            PRODUCTION_SECURITY_HEADERS['content-security-policy'],
+        ),
+    } as const;
+}
---
+++ packages/suite-web/types/securityHeaders.ts
@@ -0,0 +1,32 @@
+export type ContentSecurityPolicyRules = {
+    'default-src': string[];
+    'script-src-attr': string[];
+    'script-src': string[];
+    'style-src': string[];
+    'style-src-elem': string[];
+    'img-src': string[];
+    'connect-src': string[];
+    'upgrade-insecure-requests': true;
+    'report-uri': string;
+    'report-to': string;
+};
+
+/**
+ * Allow only stricter values from available options
+ */
+export type SecurityHeaders = {
+    'strict-transport-security': string;
+    'upgrade-insecure-requests': '1';
+    'x-content-type-options': 'nosniff';
+    'x-frame-options': 'SAMEORIGIN' | 'DENY';
+    'referrer-policy':
+        | 'strict-origin-when-cross-origin'
+        | 'no-referrer'
+        | 'no-referrer-when-downgrade'
+        | 'same-origin'
+        | 'strict-origin';
+    'cross-origin-opener-policy': 'same-origin';
+    'content-security-policy': ContentSecurityPolicyRules;
+    'report-to': string;
+    'reporting-endpoints': string;
+};
