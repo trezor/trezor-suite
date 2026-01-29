554bb8dabe87b246e64a11ea80131b35733cd969
Jan Václavík <jvaclavik@gmail.com>
2026-01-29 20:02:01 +0100

    refactor(suite): Migrate coinjoinAnonymityGainEvent

---
+++ suite/analytics/src/events/coinjoinAnonymityGainEvent.ts
@@ -0,0 +1,25 @@
+import type { AttributeDef, EventDef } from '@suite-common/analytics';
+
+import { EventType } from '../constants';
+
+type Attributes = {
+    networkSymbol: AttributeDef<string>;
+    value: AttributeDef<number>;
+};
+
+export const coinjoinAnonymityGainEvent: EventDef<Attributes, EventType.CoinjoinAnonymityGain> = {
+    name: EventType.CoinjoinAnonymityGain,
+    descriptionTrigger:
+        'Coinjoin is paused/stopped and there is at least one coinjoin transaction that has not been reported yet',
+    changelog: [{ version: '23.4.1', notes: 'added' }],
+
+    attributes: {
+        networkSymbol: {
+            changelog: [{ version: '23.4.1', notes: 'added' }],
+        },
+        value: {
+            changelog: [{ version: '23.4.1', notes: 'added' }],
+            description: 'number (rounded to 3 decimals)',
+        },
+    },
+};
