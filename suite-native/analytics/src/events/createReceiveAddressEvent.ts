e044969b20b707cb84e0e222db6ebbed24ba5686
Petr Knetl <petr.knetl@satoshilabs.com>
2026-01-30 14:42:44 +0100

    refactor(suite-native): create_receive_address analytics event

---
+++ suite-native/analytics/src/events/createReceiveAddressEvent.ts
@@ -0,0 +1,21 @@
+import type { AttributeDef, EventDef } from '@suite-common/analytics';
+import type { NetworkSymbol } from '@suite-common/wallet-config';
+
+import { EventType } from '../constants';
+
+type Attributes = {
+    assetSymbol: AttributeDef<NetworkSymbol>;
+};
+
+export const createReceiveAddressEvent: EventDef<Attributes, EventType.CreateReceiveAddress> = {
+    name: EventType.CreateReceiveAddress,
+    descriptionTrigger: 'On receive address creation',
+    changelog: [{ version: '23.4.1', notes: 'added' }],
+
+    attributes: {
+        assetSymbol: {
+            changelog: [{ version: '23.4.1', notes: 'added' }],
+            description: 'Describes the network of the account (`btc`, `eth`,…)',
+        },
+    },
+};
