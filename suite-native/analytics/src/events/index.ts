1368d6f119c91a3da942e34312a861bf88e98225
Petr Knetl <petr.knetl@satoshilabs.com>
2026-01-30 13:49:51 +0100

    refactor(suite-native): onboarding_completed analytics event

--- suite-native/analytics/src/events/index.ts
+++ suite-native/analytics/src/events/index.ts
@@ -2,4 +2,5 @@ export { appReadyEvent } from './AppReadyEvent';
 export { assetDetailEvent } from './assetDetailEvent';
 export { earnNavigateEvent } from './earnNavigateEvent';
 export { earnStakeTilePressedEvent } from './earnStakeTilePressedEvent';
+export { onboardingCompletedEvent } from './OnboardingCompletedEvent';
 export { settingsChangeCoinBackendEvent } from './settingsChangeCoinBackendEvent';
