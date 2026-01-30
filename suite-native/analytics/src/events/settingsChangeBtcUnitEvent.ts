94e5c9efc55c4fd687f3bdc43df5dd3800fbf4ae
Petr Knetl <petr.knetl@satoshilabs.com>
2026-01-30 14:49:45 +0100

    refactor(suite-native): settings/change_btc_unit analytics event

---
+++ suite-native/analytics/src/events/settingsChangeBtcUnitEvent.ts
@@ -0,0 +1,21 @@
+import type { AttributeDef, EventDef } from '@suite-common/analytics';
+import { UNIT_ABBREVIATION } from '@suite-common/suite-constants';
+
+import { EventType } from '../constants';
+
+type Attributes = {
+    bitcoinUnit: AttributeDef<UNIT_ABBREVIATION>;
+};
+
+export const settingsChangeBtcUnitEvent: EventDef<Attributes, EventType.SettingsChangeBtcUnit> = {
+    name: EventType.SettingsChangeBtcUnit,
+    descriptionTrigger: 'On Bitcoin unit change in settings',
+    changelog: [{ version: '23.4.1', notes: 'added' }],
+
+    attributes: {
+        bitcoinUnit: {
+            changelog: [{ version: '23.4.1', notes: 'added' }],
+            description: 'The selected Bitcoin unit (`BTC`, `sat`)',
+        },
+    },
+};
