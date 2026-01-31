99fab3777ab27cad12139249b060a7d6520ac575
Peter Sanderson <dierk.deidel@protonmail.com>
2026-01-31 12:39:55 +0100

    chore: reduce  in the typedObject utils

--- packages/utils/src/typedObject.ts
+++ packages/utils/src/typedObject.ts
@@ -4,16 +4,15 @@ export const typedObjectEntries = <T extends Record<string, unknown>>(
     obj: T,
 ): [keyof T, T[keyof T]][] => Object.entries(obj) as [keyof T, T[keyof T]][];
 
-export function typedObjectFromEntries<T extends readonly (readonly [string, any])[]>(
+export const typedObjectFromEntries = <T extends readonly (readonly [string, unknown])[]>(
     entries: T,
-): { [K in T[number] as K[0]]: K[1] } {
-    return Object.fromEntries(entries) as any;
-}
+): { [K in T[number] as K[0]]: K[1] } =>
+    Object.fromEntries(entries) as { [K in T[number] as K[0]]: K[1] };
 
-export const typedObjectKeys = <T extends Record<any, any>>(obj: T): Array<keyof T> =>
+export const typedObjectKeys = <T extends Record<string, unknown>>(obj: T): Array<keyof T> =>
     Object.keys(obj) as Array<keyof T>;
 
-export const typedObjectValues = <T extends Record<any, any>>(obj: T): Array<T[keyof T]> =>
+export const typedObjectValues = <T extends Record<string, unknown>>(obj: T): Array<T[keyof T]> =>
     Object.values(obj) as Array<T[keyof T]>;
 
 /**
