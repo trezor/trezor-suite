ece829749ace03c5d5d027ace96bf2f33f7b8fdc
Jirka Bažant <jiri.bazant@satoshilabs.com>
2026-02-02 18:13:35 +0100

    chore(suite-native): Trading: Improve country list

--- suite-native/trading-residence/src/components/CountrySheet/CountryListItem.tsx
+++ suite-native/trading-residence/src/components/CountrySheet/CountryListItem.tsx
@@ -1,22 +1,31 @@
-import { ReactNode } from 'react';
 import { Platform, Pressable } from 'react-native';
 import { FadeIn, FadeOut } from 'react-native-reanimated';
 
+import { TradingCountryOption } from '@suite-common/trading';
 import { AnimatedBox, Box, Card, HStack, Radio, Text } from '@suite-native/atoms';
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
 
 export type CountryListItemProps = {
-    value: string;
-    label: ReactNode;
     isSelected: boolean;
     onPress: () => void;
-};
+} & TradingCountryOption;
 
 const wrapperStyle = prepareNativeStyle(({ spacings }) => ({
     marginVertical: spacings.sp4,
 }));
 
-export const CountryListItem = ({ label, onPress, value, isSelected }: CountryListItemProps) => {
+const flagStyle = prepareNativeStyle(() => ({
+    fontSize: 20,
+    lineHeight: 24,
+}));
+
+export const CountryListItem = ({
+    name,
+    flag,
+    onPress,
+    value,
+    isSelected,
+}: CountryListItemProps) => {
     const { applyStyle } = useNativeStyles();
     // on android fade animation looks ugly on view with shadows, but without animation it looks even worse
     // do not display shadow on android and keep animating
@@ -27,9 +36,14 @@ export const CountryListItem = ({ label, onPress, value, isSelected }: CountryLi
             <Pressable onPress={onPress} style={applyStyle(wrapperStyle)}>
                 <Card noShadow={noShadow}>
                     <HStack alignItems="center" justifyContent="space-between">
+                        <Box flex={0}>
+                            <Text variant="body" color="textDefault" style={applyStyle(flagStyle)}>
+                                {flag}
+                            </Text>
+                        </Box>
                         <Box flex={1}>
                             <Text variant="body" color="textDefault">
-                                {label}
+                                {name}
                             </Text>
                         </Box>
                         <Box flex={0}>
--- suite-native/trading-residence/src/components/CountrySheet/__tests__/CountryListItem.comp.test.tsx
+++ suite-native/trading-residence/src/components/CountrySheet/__tests__/CountryListItem.comp.test.tsx
@@ -1,29 +1,27 @@
+import { nonSanctionedRegional } from '@suite-common/trading';
 import { fireEvent, renderWithBasicProvider } from '@suite-native/test-utils';
 
 import { CountryListItem, CountryListItemProps } from '../CountryListItem';
 
 describe('CountryListItem', () => {
+    const usData = nonSanctionedRegional.countriesOptionsMap.get('US')!;
+
     const renderCountryListItem = (props: Partial<CountryListItemProps>) =>
         renderWithBasicProvider(
-            <CountryListItem
-                value="US"
-                isSelected={false}
-                label="🇺🇸 United States of America"
-                onPress={jest.fn()}
-                {...props}
-            />,
+            <CountryListItem isSelected={false} onPress={jest.fn()} {...usData} {...props} />,
         );
-    it('should render given name', () => {
-        const { getByText } = renderCountryListItem({ label: '🇺🇸 United States of America' });
+    it('should render flag and name', () => {
+        const { getByText } = renderCountryListItem({});
 
-        expect(getByText('🇺🇸 United States of America')).toBeTruthy();
+        expect(getByText('🇺🇸')).toBeOnTheScreen();
+        expect(getByText('United States of America')).toBeOnTheScreen();
     });
 
     it('should call onPress callback on item press', () => {
         const onPress = jest.fn();
         const { getByText } = renderCountryListItem({ onPress });
 
-        fireEvent.press(getByText('🇺🇸 United States of America'));
+        fireEvent.press(getByText('🇺🇸'));
 
         expect(onPress).toHaveBeenCalledTimes(1);
     });
