4c434b9af03f563f919f5a6c1ab4a133b69e8fa9
Martin Varmuza <mvarmuza@gmail.com>
2026-02-02 17:35:51 -0300

    chore: filter icons in explorer to decrease build size

---
+++ suite-common/icons/src/icons-types.ts
@@ -0,0 +1,3 @@
+// Type-only export to avoid bundling assets in consumers.
+// This file intentionally re-exports the IconName type from the full icon set.
+export type IconName = import('./icons').IconName;
