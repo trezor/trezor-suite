cc94e9c1dcbff47f1551a578de67703bff258813
Petr Knetl <petr.knetl@satoshilabs.com>
2026-02-02 13:17:57 +0100

    refactor(suite-native): referral_button_press analytics event

--- suite-native/analytics/src/events/referralButtonPressEvent.ts
+++ suite-native/analytics/src/events/referralButtonPressEvent.ts
@@ -2,7 +2,9 @@ import type { EventDef } from '@suite-common/analytics';
 
 import { EventType } from '../constants';
 
-export const referralButtonPressEvent: EventDef<{}, EventType.ReferralButtonPress> = {
+type Attributes = {};
+
+export const referralButtonPressEvent: EventDef<Attributes, EventType.ReferralButtonPress> = {
     name: EventType.ReferralButtonPress,
     descriptionTrigger: 'Press referral button in homescreen',
     changelog: [{ version: '25.11.1', notes: 'added' }],
