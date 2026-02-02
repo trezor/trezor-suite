028e349a9ceb0ec1ee7dd72d02f2a12a830f07e3
Lukas 'Sherpa' Werner <sherpa.psx@gmail.com>
2026-02-02 11:38:36 +0100

    fix(tests): update swap quotes for Solana tokens and USDC

--- suite/e2e/tests/trading/swap-coin-to-token.test.ts
+++ suite/e2e/tests/trading/swap-coin-to-token.test.ts
@@ -13,9 +13,9 @@ import { formatAddressWithNewlines } from '../../support/common';
 import { expect, test } from '../../support/fixtures';
 
 // Expected values based on our mocked responses
-const sendAmount = swapQuotesSolanaUSDC[1].sendStringAmount;
-const receiveAmount = localizeNumber(swapQuotesSolanaUSDC[1].receiveStringAmount);
-const provider = getCompanyNameFromList(swapQuotesSolanaUSDC[1].exchange, 'swapList');
+const sendAmount = swapQuotesSolanaUSDC[0].sendStringAmount;
+const receiveAmount = localizeNumber(swapQuotesSolanaUSDC[0].receiveStringAmount);
+const provider = getCompanyNameFromList(swapQuotesSolanaUSDC[0].exchange, 'swapList');
 const formattedSendAmount = `${localizeNumber(sendAmount)} SOL`;
 const formattedReceiveAmount = `${receiveAmount} USDC`;
 const { sendAddress, receive: usdcMint, receiveAddress } = swapTradeSolanaUSDC;
