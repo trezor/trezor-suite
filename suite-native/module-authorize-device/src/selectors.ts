22e0994b2e7daf34d896996d34e722007432546b
yanas <yanas@yanas.cz>
2026-02-03 13:38:52 +0100

    refactor(suite-common): rename selectIsDeviceThpRequired to selectIsDeviceThpLocked

--- suite-native/module-authorize-device/src/selectors.ts
+++ suite-native/module-authorize-device/src/selectors.ts
@@ -1,11 +1,11 @@
 import { createWeakMapSelector } from '@suite-common/redux-utils';
 import { ThpRootState, selectThpLastResult, selectThpStep } from '@suite-common/thp';
-import { DeviceRootState, selectIsDeviceThpRequired } from '@suite-common/wallet-core';
+import { DeviceRootState, selectIsDeviceThpLocked } from '@suite-common/wallet-core';
 
 const createMemoizedSelector = createWeakMapSelector.withTypes<ThpRootState & DeviceRootState>();
 
 export const selectIsThpScreenDismissable = createMemoizedSelector(
-    [selectThpStep, selectThpLastResult, selectIsDeviceThpRequired],
-    (thpStep, thpLastResult, isDeviceThpRequired) =>
-        thpStep === null && (thpLastResult === 'canceled' || !isDeviceThpRequired),
+    [selectThpStep, selectThpLastResult, selectIsDeviceThpLocked],
+    (thpStep, thpLastResult, isDeviceThpLocked) =>
+        thpStep === null && (thpLastResult === 'canceled' || !isDeviceThpLocked),
 );
