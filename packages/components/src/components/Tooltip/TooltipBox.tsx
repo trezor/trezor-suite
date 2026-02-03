f7430c55f50a1a4398bacd07702f7ffd52f1650f
Seibei Iguchi <2836193+seibei-iguchi@users.noreply.github.com>
2026-02-03 13:20:50 +0100

    fix(components): update TooltipBox text overflow handling

--- packages/components/src/components/Tooltip/TooltipBox.tsx
+++ packages/components/src/components/Tooltip/TooltipBox.tsx
@@ -38,7 +38,7 @@ export const TooltipBox = ({ addon, tooltipMaxWidth, content, title }: TooltipBo
                     </Row>
                 )}
 
-                <Text typographyStyle="hint" as="div" variant="default" overflowWrap="break-word">
+                <Text typographyStyle="hint" as="div" variant="default" overflowWrap="anywhere">
                     {content}
                 </Text>
             </Column>
