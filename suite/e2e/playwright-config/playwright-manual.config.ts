2e3738e98bb0992c48b27c783c3bbb5c0fcf63fe
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2026-02-04 14:07:05 +0100

    fix(e2e-reporter): corrects path from package script to PW config
    fixes the manual config

--- suite/e2e/playwright-config/playwright-manual.config.ts
+++ suite/e2e/playwright-config/playwright-manual.config.ts
@@ -1,18 +1,15 @@
 import { defineConfig } from '@playwright/test';
 
 import { baseConfig } from './playwright-base.config';
-import {
-    PlaywrightProjectBuilder,
-    PlaywrightProjectDefinition,
-} from './playwright-project-builder';
-import { PlaywrightTarget } from '../support/testExtends/suiteTestOptions';
-
-const target = PlaywrightTarget.Web;
-const definition: PlaywrightProjectDefinition[] = [{ name: 'manual', grep: /@group=manual/ }];
 
 const config = defineConfig({
     ...baseConfig,
-    projects: PlaywrightProjectBuilder.buildFromDefinitions(target, definition),
+    projects: [
+        {
+            name: 'manual',
+            grep: /@group=manual/,
+        },
+    ],
 });
 
 /* eslint-disable-next-line import/no-default-export */
