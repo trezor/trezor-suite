fbc9034f72da6d997530648e4d36bbd7e676315b
Jan Václavík <jvaclavik@gmail.com>
2026-02-05 06:02:05 +0100

    refactor(suite-native): update analytics reporting to new service and remove deprecated PassphraseTryAgain event type

---
+++ suite-native/analytics/src/events/passphraseTryAgainEvent.ts
@@ -0,0 +1,13 @@
+import type { EventDef } from '@suite-common/analytics';
+
+import { EventType } from '../constants';
+
+type Attributes = {};
+
+export const passphraseTryAgainEvent: EventDef<Attributes, EventType.PassphraseTryAgain> = {
+    name: EventType.PassphraseTryAgain,
+    descriptionTrigger:
+        'User enters passphrase to empty wallet and presses Try again to return to initial form.',
+    changelog: [{ version: '24.7.2', notes: 'added' }],
+    attributes: {},
+};
