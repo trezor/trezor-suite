877a8ffd8528c7164f1304dbe6ddcfc815084ead
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-02-06 18:04:32 +0100

    chore(test): move defaultDevicePersistentData

--- packages/suite/src/components/suite/SecurityCheck/__tests__/DeviceCompromised.test.tsx
+++ packages/suite/src/components/suite/SecurityCheck/__tests__/DeviceCompromised.test.tsx
@@ -1,10 +1,9 @@
 import '@suite-common/test-utils/src/globalOverrides';
 
 import { TranslationKey } from '@suite/intl';
-import { mockSuiteDevice } from '@suite-common/suite-types/mocks';
+import { defaultDevicePersistentData, mockSuiteDevice } from '@suite-common/suite-types/mocks';
 import * as deviceUtils from '@suite-common/suite-utils';
 import { DeviceReducerState, deviceInitialState } from '@suite-common/wallet-core';
-import { defaultDevicePersistentData } from '@suite-common/wallet-core/src/support/deviceMocks';
 import { DeviceModelInternal } from '@trezor/device-utils';
 
 import { AppState } from 'src/reducers/store';
