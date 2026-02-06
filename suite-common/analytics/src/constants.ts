88c6c1da0d5a9da699e1a93e7475d61d3dda5b43
Jan Václavík <jvaclavik@gmail.com>
2026-02-06 08:47:45 +0100

    refactor(suite): Sort consts in analytics

--- suite-common/analytics/src/constants.ts
+++ suite-common/analytics/src/constants.ts
@@ -1,21 +1,18 @@
 export enum EventType {
+    ConnectPopupCall = 'connect-popup/call',
+    ConnectPopupError = 'connect-popup/error',
+    ConnectPopupInit = 'connect-popup/init',
+    ConnectPopupPermissions = 'connect-popup/permissions',
+    DeviceConnectionDeviceConfirmation = 'device-connection/device-confirmation',
+    DeviceConnectionDeviceFound = 'device-connection/device-found',
+    DeviceConnectionDevicePaired = 'device-connection/device-paired',
+    SettingsDeviceChangeLabel = 'settings/device/change-label',
+    SettingsDeviceWipe = 'settings/device/wipe',
+    SettingsGeneralLabeling = 'settings/general/labeling',
     WalletConnectInit = 'wallet-connect/init',
     WalletConnectPaired = 'wallet-connect/paired',
     WalletConnectProposal = 'wallet-connect/proposal',
     WalletConnectProposalApproved = 'wallet-connect/proposal-approved',
     WalletConnectProposalRejected = 'wallet-connect/proposal-rejected',
     WalletConnectSessionRequest = 'wallet-connect/session-request',
-
-    SettingsDeviceChangeLabel = 'settings/device/change-label',
-    SettingsDeviceWipe = 'settings/device/wipe',
-    SettingsGeneralLabeling = 'settings/general/labeling',
-
-    ConnectPopupInit = 'connect-popup/init',
-    ConnectPopupPermissions = 'connect-popup/permissions',
-    ConnectPopupCall = 'connect-popup/call',
-    ConnectPopupError = 'connect-popup/error',
-
-    DeviceConnectionDeviceFound = 'device-connection/device-found',
-    DeviceConnectionDevicePaired = 'device-connection/device-paired',
-    DeviceConnectionDeviceConfirmation = 'device-connection/device-confirmation',
 }
