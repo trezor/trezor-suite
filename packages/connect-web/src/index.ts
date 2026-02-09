bf40c7f5090b4f1bd1a0980de967f57bd85bb528
Marek Polak <marek.polak@satoshilabs.com>
2026-02-09 16:38:38 +0100

    chore(connect): unify both dynamic implementations

--- packages/connect-web/src/index.ts
+++ packages/connect-web/src/index.ts
@@ -1,51 +1,13 @@
 import { factory } from '@trezor/connect/src/factory';
-import { ConnectDynamicSettings, TrezorConnectDynamic } from '@trezor/connect/src/impl/dynamic';
+import { TrezorConnectDynamic } from '@trezor/connect/src/impl/dynamic';
 
 import { CoreInSuiteDesktop } from './impl/core-in-suite-desktop';
 import { CoreInSuiteWeb } from './impl/core-in-suite-web';
 
-const impl = new TrezorConnectDynamic<'core-in-suite-desktop' | 'core-in-suite-web'>({
-    implementations: [
-        {
-            type: 'core-in-suite-desktop',
-            impl: new CoreInSuiteDesktop(),
-        },
-        {
-            type: 'core-in-suite-web',
-            impl: new CoreInSuiteWeb(),
-        },
-    ],
-    getInitTarget: (coreMode: ConnectDynamicSettings['coreMode']) => {
-        if (coreMode === 'suite-desktop') {
-            return 'core-in-suite-desktop';
-        } else if (coreMode === 'suite-web') {
-            return 'core-in-suite-web';
-        } else {
-            if (coreMode && coreMode !== 'auto') {
-                console.warn(`Invalid coreMode: ${coreMode}`);
-            }
-
-            return 'core-in-suite-desktop';
-        }
-    },
-    handleBeforeCall: async (coreMode: ConnectDynamicSettings['coreMode']) => {
-        // Always try if desktop is available again
-        if (coreMode === 'suite-desktop' || coreMode === 'auto' || coreMode === undefined) {
-            await impl.switchTarget('core-in-suite-desktop');
-        }
-    },
-    handleErrorFallback: async (errorCode: string) => {
-        // Handle desktop errors
-        if (
-            impl.getTargetType() === 'core-in-suite-desktop' &&
-            (errorCode === 'Desktop_ConnectionMissing' || errorCode === 'Method_Unsupported')
-        ) {
-            await impl.switchTarget('core-in-suite-web');
-
-            return true;
-        }
-
-        return false;
+const impl = new TrezorConnectDynamic({
+    implementations: {
+        'core-in-suite-desktop': new CoreInSuiteDesktop(),
+        'core-in-suite-web': new CoreInSuiteWeb(),
     },
 });
 
--- packages/connect-webextension/src/index.ts
+++ packages/connect-webextension/src/index.ts
@@ -7,42 +7,10 @@ import { ServiceWorkerWindowChannel } from '@trezor/connect-common/src/messageCh
 import { CoreInSuiteDesktop } from '@trezor/connect-web/src/impl/core-in-suite-desktop';
 import { CoreInSuiteWeb } from '@trezor/connect-web/src/impl/core-in-suite-web';
 
-const impl = new TrezorConnectDynamic<'core-in-suite-desktop' | 'core-in-suite-web'>({
-    implementations: [
-        {
-            type: 'core-in-suite-desktop',
-            impl: new CoreInSuiteDesktop(),
-        },
-        {
-            type: 'core-in-suite-web',
-            impl: new CoreInSuiteWeb(),
-        },
-    ],
-    getInitTarget: (coreMode: ConnectDynamicSettings['coreMode']) => {
-        if (coreMode === 'suite-desktop') {
-            return 'core-in-suite-desktop';
-        } else {
-            return 'core-in-suite-web';
-        }
-    },
-    handleBeforeCall: async (coreMode: ConnectDynamicSettings['coreMode']) => {
-        // Always try if desktop is available again
-        if (coreMode === 'suite-desktop' || coreMode === 'auto' || coreMode === undefined) {
-            await impl.switchTarget('core-in-suite-desktop');
-        }
-    },
-    handleErrorFallback: async errorCode => {
-        // Handle desktop errors
-        if (
-            impl.getTargetType() === 'core-in-suite-desktop' &&
-            errorCode === 'Desktop_ConnectionMissing'
-        ) {
-            await impl.switchTarget('core-in-suite-web');
-
-            return true;
-        }
-
-        return false;
+const impl = new TrezorConnectDynamic({
+    implementations: {
+        'core-in-suite-desktop': new CoreInSuiteDesktop(),
+        'core-in-suite-web': new CoreInSuiteWeb(),
     },
 });
 
