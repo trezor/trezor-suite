c870f7a3da89d7f9dad59fa182aba775a4948d3a
Petr Knetl <petr.knetl@satoshilabs.com>
2026-02-09 11:45:05 +0100

    fix(suite-native): compact card atom shadow

--- suite-native/module-settings/src/components/__tests__/GeneralSettings.comp.test.tsx
+++ suite-native/module-settings/src/components/__tests__/GeneralSettings.comp.test.tsx
@@ -1,20 +1,11 @@
-import { act, renderWithStoreProviderAsync, userEvent } from '@suite-native/test-utils';
+import { renderWithStoreProviderAsync } from '@suite-native/test-utils';
 
 import { GeneralSettings } from '../GeneralSettings';
-
-const mockNavigate = jest.fn();
 
 jest.mock('@suite-native/trading-state', () => ({
     ...jest.requireActual('@suite-native/trading-state'),
     selectIsTradingCountrySet: () => true,
     selectIsTradingResidenceCheckEnabled: () => true,
-}));
-
-jest.mock('@react-navigation/native', () => ({
-    ...jest.requireActual('@react-navigation/native'),
-    useNavigation: () => ({
-        navigate: mockNavigate,
-    }),
 }));
 
 describe('GeneralSettings', () => {
@@ -28,14 +19,5 @@ describe('GeneralSettings', () => {
         const { getByTestId } = await renderGeneralSettings();
 
         expect(getByTestId('@settings/trading')).toBeOnTheScreen();
-
-        await act(async () => {
-            await userEvent.press(getByTestId('@settings/trading'));
-        });
-
-        expect(mockNavigate).toHaveBeenCalledTimes(1);
-        expect(mockNavigate).toHaveBeenCalledWith('SettingsScreenStack', {
-            screen: 'SettingsTradingLocation',
-        });
     });
 });
