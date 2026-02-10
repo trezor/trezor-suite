a950327b9987c7cc6cdf15291e54daf13748e15f
Tomas Martykan <t.martykan@gmail.com>
2026-02-10 18:50:19 +0100

    fix(suite-native): passphrase ui action type guards

--- suite-native/device-authorization/src/utils.ts
+++ suite-native/device-authorization/src/utils.ts
@@ -1,20 +1,22 @@
 import { G } from '@mobily/ts-belt';
 import { UnknownAction } from '@reduxjs/toolkit';
 
-import { UI } from '@trezor/connect';
+import { UI, UiRequestDeviceAction } from '@trezor/connect';
 
 export const pinButtonRequestCodes = [
     'ButtonRequest_PinEntry',
     'PinMatrixRequestType_Current',
 ] as const;
 
-export const isPinButtonRequestCode = (action: UnknownAction) =>
+export const isPinButtonRequestCode = (action: UnknownAction): action is UiRequestDeviceAction =>
     action.type === UI.REQUEST_BUTTON &&
     G.isNotNullable(action.payload) &&
     'code' in action.payload &&
     pinButtonRequestCodes.includes(action.payload.code as (typeof pinButtonRequestCodes)[number]);
 
-export const isPassphraseButtonRequestCode = (action: UnknownAction) =>
+export const isPassphraseButtonRequestCode = (
+    action: UnknownAction,
+): action is UiRequestDeviceAction =>
     action.type === UI.REQUEST_PASSPHRASE_ON_DEVICE ||
     (action.type === UI.REQUEST_BUTTON &&
         G.isNotNullable(action.payload) &&
@@ -22,6 +24,9 @@ export const isPassphraseButtonRequestCode = (action: UnknownAction) =>
         action.payload.code === 'ButtonRequest_Other' &&
         'name' in action.payload &&
         action.payload.name === 'passphrase_host1');
+
+export const isPassphraseRequest = (action: UnknownAction): action is UiRequestDeviceAction =>
+    action.type === UI.REQUEST_PASSPHRASE;
 
 export const flowEndingButtonRequests = [
     'ButtonRequest_ConfirmOutput',
