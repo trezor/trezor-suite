995d88922d2617db2989aae023094b1db6fecbdc
Matěj Husák <matej.husak@icloud.com>
2026-02-13 01:33:46 +0100

    fix(native): show xpub at correct moment

--- suite-native/qr-code/src/components/XpubQRCodeBottomSheet.tsx
+++ suite-native/qr-code/src/components/XpubQRCodeBottomSheet.tsx
@@ -1,10 +1,15 @@
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
+import { selectSelectedDevice } from '@suite-common/device';
 import { type NetworkSymbol, getNetworkType } from '@suite-common/wallet-config';
+import { AccountsRootState, selectAccountByKey, showXpubOnDevice } from '@suite-common/wallet-core';
+import { AccountKey } from '@suite-common/wallet-types';
 import { isAddressBasedNetwork } from '@suite-common/wallet-utils';
 import { BottomSheetModal, BottomSheetModalRef, Box, Button, VStack } from '@suite-native/atoms';
 import { useCopyToClipboard } from '@suite-native/clipboard';
 import { Translation, useTranslate } from '@suite-native/intl';
+import TrezorConnect from '@trezor/connect';
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
 
 import { XpubQRCodeCard } from './XpubQRCodeCard';
@@ -14,6 +19,7 @@ type XpubQRCodeBottomSheetProps = {
     qrCodeData?: string;
     symbol: NetworkSymbol;
     ref: BottomSheetModalRef;
+    accountKey: AccountKey;
 };
 
 const buttonStyle = prepareNativeStyle(utils => ({
@@ -25,13 +31,21 @@ export const XpubQRCodeBottomSheet = ({
     qrCodeData,
     symbol,
     ref,
+    accountKey,
 }: XpubQRCodeBottomSheetProps) => {
+    const [confirmationInProgress, setConfirmationInProgress] = useState(false);
     const { translate } = useTranslate();
     const networkType = getNetworkType(symbol);
     const isAddressBased = isAddressBasedNetwork(networkType);
     const { applyStyle } = useNativeStyles();
     const copyToClipboard = useCopyToClipboard();
     const [isXpubShown, setIsXpubShown] = useState(isAddressBased);
+
+    const account = useSelector((state: AccountsRootState) =>
+        selectAccountByKey(state, accountKey),
+    );
+
+    const device = useSelector(selectSelectedDevice);
 
     if (!qrCodeData) return null;
 
@@ -60,8 +74,26 @@ export const XpubQRCodeBottomSheet = ({
         />
     );
 
-    const handleShowXpub = () => {
-        setIsXpubShown(true);
+    const handleCancelConfirmation = () => {
+        if (!confirmationInProgress) return;
+
+        setConfirmationInProgress(false);
+        TrezorConnect.cancel();
+    };
+
+    const handleShowXpub = async () => {
+        if (!device || !account) return;
+
+        setConfirmationInProgress(true);
+        const xpubResponse = await showXpubOnDevice(device, account);
+
+        if (xpubResponse.success) {
+            setIsXpubShown(true);
+        } else {
+            onClose();
+        }
+
+        setConfirmationInProgress(false);
     };
 
     const handleCopyXpub = async () => {
@@ -70,7 +102,12 @@ export const XpubQRCodeBottomSheet = ({
     };
 
     return (
-        <BottomSheetModal ref={ref} isCloseDisplayed title={sheetTitle}>
+        <BottomSheetModal
+            ref={ref}
+            isCloseDisplayed
+            title={sheetTitle}
+            onClose={handleCancelConfirmation}
+        >
             <VStack spacing="sp24">
                 <XpubQRCodeCard isXpubShown={isXpubShown} qrCodeData={qrCodeData} />
 
