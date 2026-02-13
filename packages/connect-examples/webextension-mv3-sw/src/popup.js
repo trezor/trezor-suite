732fe0080f1c6b2caa434954a3194ecc4166184d
Martin Varmuza <mvarmuza@gmail.com>
2026-02-13 08:04:29 +0100

    chore: remove webextension-mv3-sw example

--- packages/connect-examples/webextension-mv3-sw/src/popup.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const newTabButton = document.getElementById('tab');
-    if (newTabButton) {
-        newTabButton.addEventListener('click', () => {
-            chrome.tabs.create({ url: 'connect-manager.html' });
-        });
-    }
-});
