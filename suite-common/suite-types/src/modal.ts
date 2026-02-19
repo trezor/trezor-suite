555fc87c81d152a36fdaa9db6e633416a73ada64
Jaroslav Hrách <me@jaroslavhrach.cz>
2026-02-20 00:22:52 +0100

    refactor(suite-types): add earn account ref and extend modal payload

--- suite-common/suite-types/src/modal.ts
+++ suite-common/suite-types/src/modal.ts
@@ -5,7 +5,7 @@ import { UI } from '@trezor/connect';
 import { Deferred } from '@trezor/utils';
 
 import { TrezorDevice } from './device';
-import { EarnFlow, EarnProvider } from './staking';
+import { EarnAccountRef, EarnFlow, EarnProvider } from './staking';
 
 export type UserContextPayload =
     | {
@@ -133,21 +133,43 @@ export type UserContextPayload =
           type: 'earn-in-a-nutshell';
           flow: EarnFlow;
           provider: EarnProvider;
+          account?: EarnAccountRef;
+          yieldId?: string;
+          tokenContractAddress?: string;
+      }
+    | {
+          type: 'supply';
+          flow: EarnFlow;
+          account?: EarnAccountRef;
+          yieldId?: string;
+          tokenContractAddress?: string;
       }
     | {
           type: 'stake';
           flow: EarnFlow;
+          account?: EarnAccountRef;
+          yieldId?: string;
+          tokenContractAddress?: string;
+      }
+    | {
+          type: 'withdraw';
+          account?: EarnAccountRef;
       }
     | {
           type: 'unstake';
+          account?: EarnAccountRef;
       }
     | {
           type: 'claim';
+          account?: EarnAccountRef;
       }
     | {
           type: 'earn-provider-consent';
           flow: EarnFlow;
           provider: EarnProvider;
+          account?: EarnAccountRef;
+          yieldId?: string;
+          tokenContractAddress?: string;
       }
     | {
           type: 'change-delegate';
--- suite-common/suite-types/src/staking.ts
+++ suite-common/suite-types/src/staking.ts
@@ -1,3 +1,5 @@
+import { Account } from '@suite-common/wallet-types';
+
 export enum EarnFlow {
     Stake = 'stake',
     Yield = 'yield',
@@ -8,3 +10,17 @@ export enum EarnProvider {
     Everstake = 'everstake',
     YieldXyz = 'yield-xyz',
 }
+
+export type EarnAccountRef = {
+    descriptor: Account['descriptor'];
+    symbol: Account['symbol'];
+    deviceStaticSessionId: Account['deviceState'];
+};
+
+export const createEarnAccountRef = (
+    account: Pick<Account, 'descriptor' | 'symbol' | 'deviceState'>,
+): EarnAccountRef => ({
+    descriptor: account.descriptor,
+    symbol: account.symbol,
+    deviceStaticSessionId: account.deviceState,
+});
