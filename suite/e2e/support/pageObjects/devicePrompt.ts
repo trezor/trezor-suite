Bertrand256/dash-trezor-suite

feat(e2e): introduce live swap test

suite/e2e/support/pageObjects/devicePrompt.ts
@@ -124,6 +124,13 @@ export class DevicePrompt {
     }
 
     @step()
+    async compareAddressesOnDeviceAndSuite() {
+        const addressFromSuite = await this.outputValueOf('address').innerText();
+        const addressFromDevice = await this.getAddress();
+        expect(addressFromSuite.replace(/\s/g, '')).toBe(addressFromDevice.replace(/\s/g, ''));
+    }
+
+    @step()
     private async getPaginatedTextSeparator(): Promise<string | false> {
         const isSeparatorVisible = await this.paginatedTextSeparator.isVisible();
         if (!isSeparatorVisible) {

trezor/trezor-suite

suite/e2e/support/pageObjects/devicePrompt.ts
@@ -124,6 +124,13 @@ export class DevicePrompt {
     }
 
     @step()
+    async compareAddressesOnDeviceAndSuite() {
+        const addressFromSuite = await this.outputValueOf('address').innerText();
+        const addressFromDevice = await this.getAddressFromDisplay();
+        expect(addressFromSuite.replace(/\s/g, '')).toBe(addressFromDevice.replace(/\s/g, ''));
+    }
+
+    @step()
     private async getPaginatedTextSeparator(): Promise<string | false> {
         const isSeparatorVisible = await this.paginatedTextSeparator.isVisible();
         if (!isSeparatorVisible) {