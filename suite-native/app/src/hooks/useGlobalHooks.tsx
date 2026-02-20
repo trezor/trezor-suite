3b20035bf3871f5605c9eee8417672c4924b2ebf
karliatto <yo@karliatto.com>
2026-02-20 12:53:12 +0100

    chore(suite-native): add selectAllowPrerelease for types

--- suite-native/app/src/hooks/useGlobalHooks.tsx
+++ suite-native/app/src/hooks/useGlobalHooks.tsx
@@ -25,7 +25,7 @@ export const useGlobalHooks = () => {
 
     useDetectDeviceError();
     useHandleDeviceAuthorization();
-    useReportDeviceCompromised({ device });
+    useReportDeviceCompromised({ device, selectAllowPrerelease: () => false });
     useRenderDeviceDangerBanner();
     useDeviceCompromisedNotification();
 
