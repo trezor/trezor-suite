96ca60593bf4d56022a3ee5c0018ff90146b4711
juriczech <bohdan.juricek@gmail.com>
2026-02-20 09:24:06 +0100

    feat(suite-native): Suite sync FW alert

--- suite-native/module-home/src/screens/HomeScreen/components/HomescreenAlerts.tsx
+++ suite-native/module-home/src/screens/HomeScreen/components/HomescreenAlerts.tsx
@@ -5,18 +5,27 @@ import { selectShouldDisplayOutOfQuotaAlert } from '@suite-common/suite-sync-quo
 import { SuiteSyncKeysAlert } from './SuiteSyncKeysAlert';
 import {
     selectShouldDisplaySuiteSyncAlert,
+    selectShouldDisplaySuiteSyncFirmwareUpdateAlert,
     selectShouldDisplayUpgradeFirmwareAlert,
 } from '../homescreenSelectors';
 import { FirmwareUpdateAlert } from './FirmwareUpdateAlert';
 import { OutOfQuotaAlert } from './OutOfQuotaAlert';
+import { SuiteSyncFirmwareUpdateAlert } from './SuiteSyncFirmwareUpdateAlert';
 
 export const HomescreenAlerts = () => {
     const shouldDisplayOutOfQuotaAlert = useSelector(selectShouldDisplayOutOfQuotaAlert);
     const shouldDisplaySuiteSyncAlert = useSelector(selectShouldDisplaySuiteSyncAlert);
+    const shouldDisplaySuiteSyncFirmwareUpdateAlert = useSelector(
+        selectShouldDisplaySuiteSyncFirmwareUpdateAlert,
+    );
     const shouldDisplayFirmwareUpdateAlert = useSelector(selectShouldDisplayUpgradeFirmwareAlert);
 
     if (shouldDisplaySuiteSyncAlert) {
         return <SuiteSyncKeysAlert />;
+    }
+
+    if (shouldDisplaySuiteSyncFirmwareUpdateAlert) {
+        return <SuiteSyncFirmwareUpdateAlert />;
     }
 
     if (shouldDisplayFirmwareUpdateAlert) {
