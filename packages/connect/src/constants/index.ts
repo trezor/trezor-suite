d50c34fe8e7a862101faca5722fecbe194e62cea
Tomas Boda <tominoboda@gmail.com>
2026-02-23 14:37:26 +0100

    chore: unify txs per page across suite and connect

--- packages/connect/src/constants/index.ts
+++ packages/connect/src/constants/index.ts
@@ -1,5 +1,6 @@
 export * as NETWORK from './network';
 export * as CARDANO from './cardano';
 export * as FIRMWARE from './firmware';
+export * as PAGING from './paging';
 export { DEFAULT_SORTING_STRATEGY } from './utxo';
 export { MessagesSchema as PROTO } from '@trezor/protobuf';
---
+++ packages/connect/src/constants/paging.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_TXS_PER_PAGE = 25;
+export const CARDANO_TXS_PER_PAGE = 8;
+export const SOLANA_TXS_PER_PAGE = 8;
