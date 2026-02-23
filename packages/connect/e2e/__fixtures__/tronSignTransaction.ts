fa4c19b98d0ab866295e44e8b54622740f1e1b7f
Tomas Martykan <t.martykan@gmail.com>
2026-02-23 16:10:06 +0100

    feat(connect): tron freeze, unfreeze
    (cherry picked from commit f563d7c871d9350affffbb2c092917a3caca74b1)

--- packages/connect/e2e/__fixtures__/tronSignTransaction.ts
+++ packages/connect/e2e/__fixtures__/tronSignTransaction.ts
@@ -12,7 +12,7 @@ export default {
     },
     tests: [
         {
-            description: 'TronTransferContract',
+            description: 'Transfer TRX',
             params: {
                 path: "m/44'/195'/0'/0/0",
                 ref_block_bytes: 'e942',
@@ -39,7 +39,7 @@ export default {
             legacyResults,
         },
         {
-            description: 'TronTriggerSmartContract',
+            description: 'Trigger smart contract',
             params: {
                 path: "m/44'/195'/0'/0/0",
                 ref_block_bytes: 'dae0',
@@ -63,6 +63,82 @@ export default {
             result: {
                 signature:
                     'f99e2f704c78b07d49fcfa4cc7cd032635807f9a1f09b57adc2232cac38d0d42428008bd9953944e0276123ee6851a266078df9c68e5c4db158be05377dadd7b1c',
+            },
+            legacyResults,
+        },
+        {
+            description: 'Freeze for energy',
+            params: {
+                path: "m/44'/195'/0'/0/0",
+                ref_block_bytes: '7e0b',
+                ref_block_hash: 'ed0f599cb230d512',
+                expiration: 1770551739000,
+                timestamp: 1770551679000,
+                contract: [
+                    {
+                        type: 'FreezeBalanceV2Contract',
+                        parameter: {
+                            value: {
+                                owner_address: '41f2cd810c48c401d392ead3c6e1e1cb9f57750a58',
+                                balance: 50000000,
+                                resource: 'ENERGY',
+                            },
+                        },
+                    },
+                ],
+            },
+            result: {
+                signature:
+                    '39114ab6d33aafc741057c7245272b286d71fd4242052445cd058170073bbee1534e6752f2d1a305916fbac06fb0803641df604c713ee927e872a0ad972d18d01c',
+            },
+            legacyResults,
+        },
+        {
+            description: 'Unfreeze for bandwidth',
+            params: {
+                path: "m/44'/195'/0'/0/0",
+                ref_block_bytes: 'f2b7',
+                ref_block_hash: 'fb75c38816f843ef',
+                expiration: 1771437825000,
+                timestamp: 1771436565000,
+                contract: [
+                    {
+                        type: 'UnfreezeBalanceV2Contract',
+                        parameter: {
+                            value: {
+                                owner_address: '41f2cd810c48c401d392ead3c6e1e1cb9f57750a58',
+                                balance: 10000000,
+                            },
+                        },
+                    },
+                ],
+            },
+            result: {
+                signature:
+                    'b4d10dbd35e1c4e925f45c38809e648df9936f6e5a9fa49ffd90f6ca81abc0595aa388f797114963a5a2be71b982832112846dfdd5b8125c0bcedb2822047a591b',
+            },
+            legacyResults,
+        },
+        {
+            description: 'Claim unfrozen balance',
+            params: {
+                path: "m/44'/195'/0'/0/0",
+                ref_block_bytes: 'f55c',
+                ref_block_hash: 'e53dfa7c93866297',
+                expiration: 1771439874000,
+                timestamp: 1771438614000,
+                contract: [
+                    {
+                        type: 'WithdrawExpireUnfreezeContract',
+                        parameter: {
+                            value: { owner_address: '41f2cd810c48c401d392ead3c6e1e1cb9f57750a58' },
+                        },
+                    },
+                ],
+            },
+            result: {
+                signature:
+                    '833f6ba5c449a28ee6c58d98298f2f7cce8be1b7f70396e763ec216ca1d396b152bcefe9c7dde9970db2f1d38ecfba5aa20023f966f685da0048944415cb56f31c',
             },
             legacyResults,
         },
