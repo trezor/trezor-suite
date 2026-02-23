dd1f461659523d591c03b92243c73943e10cac50
karliatto <yo@karliatto.com>
2026-02-23 10:40:03 +0100

    feat(conenct): remove host_static_public_key from EvoluGetDelegatedIdentityKey

--- packages/connect/src/api/evoluGetDelegatedIdentityKey.ts
+++ packages/connect/src/api/evoluGetDelegatedIdentityKey.ts
@@ -25,8 +25,6 @@ export default class EvoluGetDelegatedIdentityKey extends AbstractMethod<
         if (thpState) {
             this.params = {
                 thp_credential: thpState.pairingCredentials[0].credential,
-                host_static_public_key:
-                    thpState.handshakeCredentials?.hostStaticPublicKey.toString('hex'),
             };
         }
 
