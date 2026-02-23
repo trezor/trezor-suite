67b8ae46dc465d4eabb992b5b2ca287826f94a91
Jan Václavík <jvaclavik@gmail.com>
2026-02-23 18:19:06 +0100

    fix(suite): Update analytics version

--- suite/analytics/src/events/appFormPercentButtonsEvent.ts
+++ suite/analytics/src/events/appFormPercentButtonsEvent.ts
@@ -11,15 +11,15 @@ export const appFormPercentButtonsEvent: EventDef<Attributes, EventType.AppFormP
     name: EventType.AppFormPercentButtons,
     descriptionTrigger:
         'Fired when user clicks 10%, 25%, 50% or MAX in staking/sell/swap/send form',
-    changelog: [{ version: '26.0.0', notes: 'added' }],
+    changelog: [{ version: '26.2.0', notes: 'added' }],
 
     attributes: {
         type: {
-            changelog: [{ version: '26.3.0', notes: 'added' }],
+            changelog: [{ version: '26.2.0', notes: 'added' }],
             description: 'Specifies place in the app where the button was clicked',
         },
         value: {
-            changelog: [{ version: '26.3.0', notes: 'added' }],
+            changelog: [{ version: '26.2.0', notes: 'added' }],
             description: 'Specifies the value of the button that was clicked (percentage or max)',
         },
     },
