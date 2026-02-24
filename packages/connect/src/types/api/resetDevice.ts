aa24e662a635d502685c843aac8fe1a98407b695
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-02-24 08:18:24 +0100

    feat(connect): return xpubs hashes from successful entropy check

--- packages/connect/src/types/api/resetDevice.ts
+++ packages/connect/src/types/api/resetDevice.ts
@@ -3,6 +3,9 @@
  */
 
 import { PROTO } from '../../constants';
+import type { XPubHashesPerBip43Path } from '../device';
 import type { Params, Response } from '../params';
 
-export declare function resetDevice(params: Params<PROTO.ResetDevice>): Response<PROTO.Success>;
+export declare function resetDevice(
+    params: Params<PROTO.ResetDevice>,
+): Response<PROTO.Success & { xpubHashes?: XPubHashesPerBip43Path }>;
