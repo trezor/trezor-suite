c37148afdf897d2548b500c7c92ac5159b7b4c82
vytick <vytick@gmail.com>
2026-02-25 23:35:44 +0400

    chore(suite-native): remove universal links from trade
    (cherry picked from commit 7490805a85cfdf0b8bea90f2a80caa3bb506d387)

--- suite-native/trading-browser-auth/src/consts.ts
+++ suite-native/trading-browser-auth/src/consts.ts
@@ -1,9 +1,2 @@
-import { isProduction } from '@suite-native/config';
-
-const TRADING_URL_BASE_PRODUCTION = 'https://trezor.io/suite/deeplinks/trade';
-const TRADING_URL_BASE_DEV = 'trezorsuite://trading';
-
-// we don't want to expose custom scheme in production,
-// but we want to use custom scheme for develop as dev app has different app package
-export const TRADING_URL_BASE = isProduction() ? TRADING_URL_BASE_PRODUCTION : TRADING_URL_BASE_DEV;
+export const TRADING_URL_BASE = 'trezorsuite://trading';
 export const TRADING_URL_DEFAULT_BACK = `${TRADING_URL_BASE}/back`;
