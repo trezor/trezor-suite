25015baa2e76dd77997042f43b681849eb3a7aa6
Matěj Husák <matej.husak@icloud.com>
2026-02-25 14:26:05 +0100

    feat(intl): add translations for experimental feedback

--- suite/intl/src/messages.ts
+++ suite/intl/src/messages.ts
@@ -10730,4 +10730,29 @@ export const messages = defineMessages({
         id: 'TR_NON_TRADABLE_TOKENS',
         defaultMessage: 'Non-tradable tokens',
     },
+    TR_EXPERIMENTAL_FEEDBACK_MODAL_HEADING: {
+        id: 'TR_EXPERIMENTAL_FEEDBACK_MODAL_HEADING',
+        defaultMessage: 'Rate your {feature} experience',
+    },
+    TR_EXPERIMENTAL_FEEDBACK_CARD_HEADING: {
+        id: 'TR_EXPERIMENTAL_FEEDBACK_CARD_HEADING',
+        defaultMessage: 'Please rate your {feature} experience',
+    },
+    TR_EXPERIMENTAL_FEEDBACK_CARD_DESCRIPTION: {
+        id: 'TR_EXPERIMENTAL_FEEDBACK_CARD_DESCRIPTION',
+        defaultMessage:
+            'It takes just a few seconds. Your feedback helps us improve Suite Sync for you and others. ',
+    },
+    TR_EXPERIMENTAL_FEEDBACK_MODAL_DESCRIPTION: {
+        id: 'TR_EXPERIMENTAL_FEEDBACK_MODAL_DESCRIPTION',
+        defaultMessage: 'Your feedback helps improve-tell us what you think.',
+    },
+    TR_EXPERIMENTAL_FEEDBACK_MODAL_SUBMIT: {
+        id: 'TR_EXPERIMENTAL_FEEDBACK_MODAL_SUBMIT',
+        defaultMessage: 'Submit',
+    },
+    TR_EXPERIMENTAL_FEEDBACK_CARD_RATE_BUTTON: {
+        id: 'TR_EXPERIMENTAL_FEEDBACK_CARD_RATE_BUTTON',
+        defaultMessage: 'Sure thing',
+    },
 } as const);
