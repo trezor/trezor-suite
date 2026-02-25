f8bad14361a5e93e4a1573488976567b5f12044a
Tomas Martykan <t.martykan@gmail.com>
2026-02-25 10:53:24 +0100

    fix(suite): walletconnect max fee calculation

---
+++ packages/suite/src/hooks/wallet/form/useComposedLevelsPlaceholder.tsx
@@ -0,0 +1,45 @@
+import { useMemo } from 'react';
+
+import { FeeInfo, PrecomposedLevels } from '@suite-common/wallet-types';
+import { FeeLevel } from '@trezor/connect';
+
+export const useComposedLevelsPlaceholder = ({
+    feeInfo,
+    selectedFee,
+    feePerUnit,
+    maxFeePerGas,
+    feeTotalCalculation,
+}: {
+    feeInfo: FeeInfo;
+    selectedFee?: FeeLevel['label'];
+    feePerUnit: string;
+    maxFeePerGas?: string;
+    feeTotalCalculation?: (feeValue: string) => string;
+}) => {
+    const composedLevels = useMemo(() => {
+        const levels: PrecomposedLevels = {};
+
+        const createComposedTx = (feeValue: string) => ({
+            type: 'final' as const,
+            totalSpent: '0',
+            fee: feeTotalCalculation ? feeTotalCalculation(feeValue) : feeValue,
+            feePerByte: feeValue,
+            bytes: 0,
+            inputs: [],
+            outputs: [],
+            outputsPermutation: [],
+        });
+
+        feeInfo.levels.forEach(level => {
+            levels[level.label] = createComposedTx(level.maxFeePerGas ?? level.feePerUnit);
+        });
+
+        if (selectedFee === 'custom' && feePerUnit) {
+            levels.custom = createComposedTx(maxFeePerGas ?? feePerUnit);
+        }
+
+        return levels;
+    }, [feeInfo.levels, feePerUnit, maxFeePerGas, selectedFee, feeTotalCalculation]);
+
+    return composedLevels;
+};
