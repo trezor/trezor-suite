f4498c0d4d95e498f2c07f3eaae49910b735e37a
Martin Varmuza <mvarmuza@gmail.com>
2026-02-27 13:52:03 +0100

    chore(connect): remove Lisk mentions

--- packages/connect/e2e/__fixtures__/getFeatures.ts
+++ packages/connect/e2e/__fixtures__/getFeatures.ts
@@ -84,33 +84,7 @@ const tests = [
             display_rotation: 'North',
             experimental_features: expect.any(Boolean),
         },
-        legacyResults: [
-            {
-                rules: ['<2.4.2'], // 2.4.2 removed Lisk capability
-                success: true,
-                payload: {
-                    capabilities: expect.arrayContaining([
-                        'Capability_Bitcoin',
-                        'Capability_Bitcoin_like',
-                        'Capability_Binance',
-                        'Capability_Cardano',
-                        'Capability_Crypto',
-                        'Capability_Ethereum',
-                        'Capability_Lisk',
-                        'Capability_Monero',
-                        'Capability_Ripple',
-                        'Capability_Stellar',
-                        'Capability_Tezos',
-                        'Capability_U2F',
-                        'Capability_Shamir',
-                        'Capability_ShamirGroups',
-                        'Capability_PassphraseEntry',
-                    ]),
-                    session_id: expect.any(String),
-                    passphrase_always_on_device: false,
-                },
-            },
-        ],
+        legacyResults: [],
     },
     {
         description: 'T1B1 features',
@@ -166,37 +140,7 @@ const tests = [
             experimental_features: expect.any(Boolean),
             backup_type: 'Bip39',
         },
-        legacyResults: [
-            {
-                rules: ['<1.10.3'], // 1.10.3 removed Lisk capability
-                success: true,
-                payload: {
-                    capabilities: expect.arrayContaining([
-                        'Capability_Bitcoin',
-                        'Capability_Bitcoin_like',
-                        'Capability_Crypto',
-                        'Capability_Ethereum',
-                        'Capability_Lisk',
-                        'Capability_Stellar',
-                        'Capability_U2F',
-                    ]),
-                },
-            },
-            {
-                rules: ['<1.8.3'], // 1.8.3 added Lisk capability
-                success: true,
-                payload: {
-                    capabilities: expect.arrayContaining([
-                        'Capability_Bitcoin',
-                        'Capability_Bitcoin_like',
-                        'Capability_Crypto',
-                        'Capability_Ethereum',
-                        'Capability_Stellar',
-                        'Capability_U2F',
-                    ]),
-                },
-            },
-        ],
+        legacyResults: [],
     },
 ];
 
