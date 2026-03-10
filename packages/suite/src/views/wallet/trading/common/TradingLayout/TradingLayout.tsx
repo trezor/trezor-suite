b8a892671263de314cfa9fd055760188da373a8c
Seibei Iguchi <2836193+seibei-iguchi@users.noreply.github.com>
2026-03-10 09:08:10 +0100

    fix(suite): trading navigation active state

--- packages/suite/src/views/wallet/trading/common/TradingLayout/TradingLayout.tsx
+++ packages/suite/src/views/wallet/trading/common/TradingLayout/TradingLayout.tsx
@@ -2,11 +2,17 @@ import { PropsWithChildren } from 'react';
 
 import { Column } from '@trezor/components';
 
+import { useSelector } from 'src/hooks/suite';
+import { selectRouteName } from 'src/reducers/suite/routerReducer';
 import { TradingLayoutNavigation } from 'src/views/wallet/trading/common/TradingLayout/TradingLayoutNavigation';
 
-export const TradingLayout = ({ children }: PropsWithChildren) => (
-    <Column data-testid="@trading" gap={24}>
-        <TradingLayoutNavigation />
-        {children}
-    </Column>
-);
+export const TradingLayout = ({ children }: PropsWithChildren) => {
+    const routeName = useSelector(selectRouteName);
+
+    return (
+        <Column data-testid="@trading" gap={24}>
+            <TradingLayoutNavigation route={routeName} />
+            {children}
+        </Column>
+    );
+};
