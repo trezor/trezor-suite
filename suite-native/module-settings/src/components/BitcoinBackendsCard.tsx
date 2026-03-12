85d7019f16370e7d93d683348e5b6d753354c616
yanas <yanas@yanas.cz>
2026-03-12 14:49:31 +0100

    fix(suite-native): use correct settings card for Bitcoin backends

--- suite-native/module-settings/src/components/BitcoinBackendsCard.tsx
+++ suite-native/module-settings/src/components/BitcoinBackendsCard.tsx
@@ -1,17 +1,17 @@
+import { PressableCardWithIconLayout } from '@suite-native/atoms';
 import { Translation } from '@suite-native/intl';
 import { SettingsStackRoutes } from '@suite-native/navigation';
 
-import { AppSettingsCardWithIconLayout } from './AppSettingsCardWithIconLayout';
 import { useSettingsNavigateTo } from '../navigation/useSettingsNavigateTo';
 
 export const BitcoinBackendsCard = () => {
     const navigateTo = useSettingsNavigateTo();
 
     return (
-        <AppSettingsCardWithIconLayout
+        <PressableCardWithIconLayout
             icon="database"
             title={<Translation id="moduleSettings.advanced.bitcoinBackends.title" />}
-            subtitle={<Translation id="moduleSettings.advanced.bitcoinBackends.subtitle" />}
+            description={<Translation id="moduleSettings.advanced.bitcoinBackends.subtitle" />}
             onPress={() => navigateTo(SettingsStackRoutes.BitcoinBackends)}
         />
     );
