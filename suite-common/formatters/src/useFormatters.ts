ceae6874d58f794b1cef5f8aef3309a620377fa9
Jirka Bažant <jiri.bazant@satoshilabs.com>
2026-03-13 07:00:50 +0100

    chore(suite-common): Use type imports in formatters

--- suite-common/formatters/src/useFormatters.ts
+++ suite-common/formatters/src/useFormatters.ts
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
 
-import { FormatterProviderContext, Formatters } from './FormatterProvider';
+import { FormatterProviderContext, type Formatters } from './FormatterProvider';
 
 export const useFormatters = (): Formatters => useContext(FormatterProviderContext);
