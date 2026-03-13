457f61858f47b9b4c842dedd53a30415e2c97125
YashejShah <YashejShah@users.noreply.github.com>
2026-03-13 22:11:29 +0530

    document csp connect-src dependency on request-filter

--- packages/suite-desktop-core/src/modules/csp.ts
+++ packages/suite-desktop-core/src/modules/csp.ts
@@ -17,7 +17,8 @@ const createCspRules = ({ nonce }: CreateCspRulesParams) => [
     "default-src 'self'",
     "script-src 'self'",
     `style-src 'self' 'nonce-${nonce}'`,
-    // Allow all API calls (Can't be restricted bc of custom backends)
+    // connect-src is permissive because custom backends need arbitrary domains.
+    // The request-filter module provides additional domain-level allowlisting.
     'connect-src data: *',
     // Allow images from trezor.io
     "img-src 'self' blob: data: https://*.trezor.io",
