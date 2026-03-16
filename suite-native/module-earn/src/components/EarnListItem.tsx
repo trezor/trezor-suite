4d28ff76cd86d2786cdb41452d8e1901ac27354e
brantalikp <brantalikp@gmail.com>
2026-03-16 11:02:44 +0100

    fix(suite-native): fix earn modal

--- suite-native/module-earn/src/components/EarnListItem.tsx
+++ suite-native/module-earn/src/components/EarnListItem.tsx
@@ -1,10 +1,7 @@
-import { events } from '@suite-native/analytics';
-import { PressableOpacity, useBottomSheetModal } from '@suite-native/atoms';
-import { useAnalytics } from '@suite-native/services';
+import { PressableOpacity } from '@suite-native/atoms';
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles';
 
 import { type EarnPromoItem } from '../types';
-import { EarnItemInfoModal } from './EarnItemInfoModal';
 import { EarnItemOverviewSection } from './EarnItemOverviewSection';
 
 const promoItemStyle = prepareNativeStyle(utils => ({
@@ -12,31 +9,21 @@ const promoItemStyle = prepareNativeStyle(utils => ({
     minHeight: 70,
 }));
 
-export type EarnListItemProps = EarnPromoItem;
+type EarnListItemProps = EarnPromoItem & {
+    onPress: (type: EarnPromoItem['type']) => void;
+};
 
 export const EarnListItem = (item: EarnListItemProps) => {
+    const { onPress, type } = item;
     const { applyStyle } = useNativeStyles();
-    const { bottomSheetRef, openModal } = useBottomSheetModal();
-    const analytics = useAnalytics();
 
     const handlePress = () => {
-        openModal();
-
-        analytics.report({
-            type:
-                item.type === 'staking'
-                    ? events.earnStakeTilePressedEvent.name
-                    : events.earnStablecoinYieldTilePressedEvent.name,
-        });
+        onPress(type);
     };
 
     return (
-        <>
-            <PressableOpacity style={applyStyle(promoItemStyle)} onPress={handlePress}>
-                <EarnItemOverviewSection {...item} />
-            </PressableOpacity>
-
-            <EarnItemInfoModal ref={bottomSheetRef} type={item.type} />
-        </>
+        <PressableOpacity style={applyStyle(promoItemStyle)} onPress={handlePress}>
+            <EarnItemOverviewSection {...item} />
+        </PressableOpacity>
     );
 };
