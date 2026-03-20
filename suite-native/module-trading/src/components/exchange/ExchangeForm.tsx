4e0c34bd7c9144465f68b1a6f663e286ffc8f3b8
vytick <vytick@gmail.com>
2026-03-20 21:13:16 +0100

    fix(suite-native): fix swap entering animation

--- suite-native/module-trading/src/components/exchange/ExchangeForm.tsx
+++ suite-native/module-trading/src/components/exchange/ExchangeForm.tsx
@@ -1,5 +1,5 @@
 import { memo } from 'react';
-import { LinearTransition } from 'react-native-reanimated';
+import { FadeIn, FadeInDown, LinearTransition } from 'react-native-reanimated';
 
 import { AnimatedBox, Card, VStack } from '@suite-native/atoms';
 import { AmountEditingDoneButton } from '@suite-native/trading-atoms';
@@ -13,6 +13,7 @@ import { ExchangeSendCard } from './send/ExchangeSendCard';
 import { useExchangeFormContext } from '../../hooks/exchange/useExchangeFormContext';
 import { useExchangeQuotes } from '../../hooks/exchange/useExchangeQuotes';
 import { useFocusedValueWatch } from '../../hooks/general/useFocusedValueWatch';
+import { useMountedRecentlyFlag } from '../../hooks/general/useMountedRecentlyFlag';
 
 type ExchangeFormProps = {
     shouldAnimateEntering?: boolean;
@@ -26,26 +27,45 @@ type ExchangeFormMemoizedProps = {
 const EXCHANGE_FORM_TEST_ID = '@trading/exchange/form';
 const AMOUNT_EDITING_DONE_BUTTON_TEST_ID = '@trading/exchange/amount-editing-done-button';
 
-const ExchangeFormMemoized = memo(({ isAmountInputActive }: ExchangeFormMemoizedProps) => (
-    <AnimatedBox layout={LinearTransition}>
-        <VStack spacing="sp16" testID={EXCHANGE_FORM_TEST_ID}>
-            <ExchangeAlert />
-            <ExchangeSendCard isAmountInputActive={isAmountInputActive} />
-            <ExchangeReceiveCard />
-            {isAmountInputActive ? (
-                <AmountEditingDoneButton testID={AMOUNT_EDITING_DONE_BUTTON_TEST_ID} />
-            ) : (
-                <>
-                    <Card noPadding>
-                        <ExchangeReceiveAccountPicker />
-                        <ExchangeRateAndProviderPicker />
-                    </Card>
-                    <ExchangeConfirmation enteringAnimation={LinearTransition} />
-                </>
-            )}
-        </VStack>
-    </AnimatedBox>
-));
+const getEnteringAnimation = (isFormMountedRecently?: boolean, shouldAnimateEntering?: boolean) => {
+    if (!isFormMountedRecently) {
+        return FadeInDown;
+    }
+
+    return shouldAnimateEntering ? FadeIn : undefined;
+};
+
+const ExchangeFormMemoized = memo(
+    ({ isAmountInputActive, shouldAnimateEntering }: ExchangeFormMemoizedProps) => {
+        const isFormMountedRecently = useMountedRecentlyFlag();
+
+        const enteringAnimation = getEnteringAnimation(
+            isFormMountedRecently,
+            shouldAnimateEntering,
+        );
+
+        return (
+            <AnimatedBox layout={LinearTransition}>
+                <VStack spacing="sp16" testID={EXCHANGE_FORM_TEST_ID}>
+                    <ExchangeAlert />
+                    <ExchangeSendCard isAmountInputActive={isAmountInputActive} />
+                    <ExchangeReceiveCard />
+                    {isAmountInputActive ? (
+                        <AmountEditingDoneButton testID={AMOUNT_EDITING_DONE_BUTTON_TEST_ID} />
+                    ) : (
+                        <>
+                            <Card noPadding>
+                                <ExchangeReceiveAccountPicker />
+                                <ExchangeRateAndProviderPicker />
+                            </Card>
+                            <ExchangeConfirmation enteringAnimation={enteringAnimation} />
+                        </>
+                    )}
+                </VStack>
+            </AnimatedBox>
+        );
+    },
+);
 
 export const ExchangeForm = ({ shouldAnimateEntering }: ExchangeFormProps) => {
     const exchangeForm = useExchangeFormContext();
