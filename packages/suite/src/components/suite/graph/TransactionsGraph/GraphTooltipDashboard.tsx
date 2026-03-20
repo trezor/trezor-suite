febd34144bd0d725d862390c65915b7e8aa0fdf4
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-03-20 10:53:27 +0100

    refactor(suite): fix circulars in component props

--- packages/suite/src/components/suite/graph/TransactionsGraph/GraphTooltipDashboard.tsx
+++ packages/suite/src/components/suite/graph/TransactionsGraph/GraphTooltipDashboard.tsx
@@ -3,10 +3,10 @@ import { type TooltipProps } from 'recharts';
 import { useFormatters } from '@suite-common/formatters';
 import { BASE_CURRENCY_ZERO } from '@suite-common/wallet-utils';
 
+import type { FiatGraphProps } from 'src/components/suite/graph/types';
 import { type CommonAggregatedHistory, type GraphRange } from 'src/types/wallet/graph';
 
 import { GraphTooltipBase } from './GraphTooltipBase';
-import type { FiatGraphProps } from './TransactionsGraph';
 
 interface GraphTooltipDashboardProps extends TooltipProps<number, any> {
     selectedRange: GraphRange;
