62da4a186ad4c5050b619637c248ba9b60c58e6a
Lukas 'Sherpa' Werner <sherpa.psx@gmail.com>
2026-03-23 08:54:24 +0100

    feat(trading): show non-production trading environment warning

--- packages/suite/src/views/wallet/trading/common/TradingForm/TradingFormLayout.tsx
+++ packages/suite/src/views/wallet/trading/common/TradingForm/TradingFormLayout.tsx
@@ -1,10 +1,13 @@
 import { type ReactNode } from 'react';
 
+import { selectInvityServerEnvironment } from '@suite/settings';
+import { TradingEnvironmentWarning } from '@suite/trading';
 import { Context } from '@suite-common/message-system';
 import { Box, Card, Column } from '@trezor/components';
 import { breakpoints, spacings } from '@trezor/theme';
 
 import { ContextMessage } from 'src/components/wallet/WalletLayout/AccountBanners/ContextMessage';
+import { useSelector } from 'src/hooks/suite';
 import { useTradingDeviceDisconnected } from 'src/hooks/wallet/trading/form/common/useTradingDeviceDisconnected';
 import { ConnectDeviceGenericPromo } from 'src/views/wallet/receive/components/ConnectDevicePromo';
 import { TradingFeaturedOffers } from 'src/views/wallet/trading/common/TradingFeaturedOffers/TradingFeaturedOffers';
@@ -19,10 +22,12 @@ interface TradingFormLayoutProps {
 
 export const TradingFormLayout = ({ children }: TradingFormLayoutProps) => {
     const { tradingDeviceDisconnected } = useTradingDeviceDisconnected();
+    const invityServerEnvironment = useSelector(selectInvityServerEnvironment);
 
     return (
         <Column gap={spacings.md} data-testid="@trading/form">
             {tradingDeviceDisconnected && <ConnectDeviceGenericPromo />}
+            <TradingEnvironmentWarning tradingEnvironment={invityServerEnvironment} />
 
             {/* If clicking on disabled input, the click propagates to the form and submits it (some form values are then pushed to URL search params) */}
             <form onSubmit={e => e.preventDefault()}>
