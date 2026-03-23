48a075233e55f815375766aba81d703e5e44bb6c
juriczech <bohdan.juricek@gmail.com>
2026-03-23 14:45:29 +0100

    chore(suite): remove unused files

--- packages/suite/src/actions/onboarding/onboardingActions.ts
+++
@@ -1,9 +0,0 @@
-export type { OnboardingFlowRootState } from '@suite/onboarding';
-export {
-    beginOnboardingTutorial,
-    goToNextStep,
-    goToPreviousStep,
-    goToSuite,
-    recoveryRerun,
-    selectResolvedNextStepAfterSkipped,
-} from '@suite/onboarding';
