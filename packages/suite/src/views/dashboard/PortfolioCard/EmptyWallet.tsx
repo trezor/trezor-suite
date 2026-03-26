1658e1bc82845abd5d1adf31912574b236ab51a7
Seibei Iguchi <2836193+seibei-iguchi@users.noreply.github.com>
2026-03-26 11:12:38 +0100

    feat(suite): add option to add networks to empty wallet dashboard

--- packages/suite/src/views/dashboard/PortfolioCard/EmptyWallet.tsx
+++ packages/suite/src/views/dashboard/PortfolioCard/EmptyWallet.tsx
@@ -1,17 +1,14 @@
 import { Translation } from '@suite/intl';
 import { goto } from '@suite/router';
-import { selectIsDeviceUsingPassphrase } from '@suite-common/device';
 import { selectEnabledNetworks } from '@suite-common/wallet-core';
 import { Button, Column, H3, IconCircle, Paragraph, Row } from '@trezor/components';
 import { CoinLogo } from '@trezor/product-components';
-import { spacings } from '@trezor/theme';
 
 import { useNetworkSupport } from 'src/hooks/settings/useNetworkSupport';
 import { useDispatch, useSelector } from 'src/hooks/suite';
 
 export const EmptyWallet = () => {
     const { supportedMainnets } = useNetworkSupport();
-    const isPassphraseType = useSelector(selectIsDeviceUsingPassphrase);
     const enabledNetworks = useSelector(selectEnabledNetworks);
     const dispatch = useDispatch();
 
@@ -20,24 +17,23 @@ export const EmptyWallet = () => {
     );
 
     return (
-        <Column gap={spacings.xxs} data-testid="@dashboard/wallet-ready" alignItems="center">
+        <Column gap={4} data-testid="@dashboard/wallet-ready" alignItems="center">
             <IconCircle name="check" size={96} intent="brand" />
-            <H3 margin={{ top: spacings.md }}>
+            <H3 margin={16}>
                 <Translation id="TR_YOUR_WALLET_IS_READY_WHAT" />
             </H3>
-            {isPassphraseType && !areAllNetworksEnabled && (
-                <Row gap={spacings.xs} flexWrap="wrap">
+            {!areAllNetworksEnabled && (
+                <Row gap={8} flexWrap="wrap">
                     <Paragraph intent="neutral" priority="secondary" typographyStyle="body-sm">
                         <Translation id="TR_CHECKED_BALANCES_ON" />:
                     </Paragraph>
-                    <Row gap={spacings.xxs} flexWrap="wrap">
+                    <Row gap={4} flexWrap="wrap">
                         {enabledNetworks.map(network => (
                             <CoinLogo key={network} symbol={network} size={16} />
                         ))}
                     </Row>
                     <Button
-                        intent="neutral"
-                        priority="secondary"
+                        intent="brand"
                         iconLeft="plus"
                         size="small"
                         onClick={() => {
