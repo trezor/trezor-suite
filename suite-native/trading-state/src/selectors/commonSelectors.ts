539bb0c572cd237950af86305c28c5b2cd1b15b5
brantalikp <brantalikp@gmail.com>
2026-03-26 16:25:18 +0100

    fix(suite-native): trade - filter accounts without trade id

--- suite-native/trading-state/src/selectors/commonSelectors.ts
+++ suite-native/trading-state/src/selectors/commonSelectors.ts
@@ -231,6 +231,7 @@ export const selectAccountsWithTokensToSellSectionListByTradingType =
             // TODO: Remove this filter when Cardano send is implemented (#15068)
             // Currently filtering out Cardano accounts and tokens from trading until Cardano send is supported
             const filteredAccounts = accounts.filter(account => {
+                if (!getNetwork(account.symbol).tradeCryptoId) return false;
                 const networkType = getNetworkType(account.symbol);
 
                 return networkType !== 'cardano' || isCardanoSendEnabled;
