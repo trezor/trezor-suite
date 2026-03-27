c230f0baf14d8d41df3831f3a148f37a0c73b2ef
Tomas Martykan <t.martykan@gmail.com>
2026-03-27 10:22:27 +0100

    fix(suite): ensure descriptor.apiType with migration

---
+++ packages/suite/src/storage/migrations/versions/26.4.0.1.ts
@@ -0,0 +1,23 @@
+import { createMigration } from '@suite/idb-migration-utils';
+
+import { type SuiteDBSchema } from 'src/storage/definitions';
+
+import { updateAll } from '../utils';
+
+export default createMigration<SuiteDBSchema>('26.4.0.1', async (_, tx) => {
+    await updateAll(tx, 'devices', device => {
+        // Ensure apiType is set for remembered devices from old versions of Suite.
+        // Bluetooth didn't exist before, so defaulting to 'usb' is safe.
+        if (!device.descriptor) {
+            device.descriptor = { apiType: 'usb' };
+
+            return device;
+        }
+
+        if (!device.descriptor.apiType) {
+            device.descriptor.apiType = 'usb';
+
+            return device;
+        }
+    });
+});
