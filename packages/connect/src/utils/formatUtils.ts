6ce8bb78810aa18dd45c31c3d428d092731deebe
Marek Polak <marek.polak@satoshilabs.com>
2026-03-27 16:38:00 +0100

    refactor(connect): small `formatUtils` cleanup

--- packages/connect/src/utils/formatUtils.ts
+++ packages/connect/src/utils/formatUtils.ts
@@ -14,44 +14,32 @@ export const addHexPrefix = (str: string): `0x${string}` =>
     str !== undefined && !hasHexPrefix(str) ? `0x${str}` : (str as `0x${string}`);
 
 // from (isHexString) https://github.com/ethjs/ethjs-util/blob/master/src/index.js
-export const isHexString = (value: string, length?: number) => {
-    if (typeof value !== 'string' || !value.match(/^(0x|0X)?[0-9A-Fa-f]*$/)) {
-        return false;
-    }
-    if (length && value.length !== 2 + 2 * length) {
-        return false;
-    }
-
-    return true;
-};
+export const isHexString = (value: string) =>
+    typeof value === 'string' && value.match(/^(0x|0X)?[0-9A-Fa-f]*$/);
 
 // from (toBuffer) https://github.com/ethereumjs/ethereumjs-util/blob/master/index.js
-export const messageToHex = (message: string) => {
-    let buffer: Buffer;
+export const messageToBuffer = (message: string) => {
     if (isHexString(message)) {
         let clean = stripHexPrefix(message);
         // pad left even
         if (clean.length % 2 !== 0) {
             clean = `0${clean}`;
         }
-        buffer = Buffer.from(clean, 'hex');
-    } else {
-        buffer = Buffer.from(message);
-    }
 
-    return buffer.toString('hex');
+        return Buffer.from(clean, 'hex');
+    } else {
+        return Buffer.from(message, 'utf8');
+    }
 };
 
-export const hexToText = (hex: string) => {
-    const clean = messageToHex(hex);
+export const messageToHex = (message: string) => messageToBuffer(message).toString('hex');
 
-    const text = Buffer.from(clean, 'hex').toString('utf8');
+export const hexToText = (hex: string) => {
+    const text = messageToBuffer(hex).toString('utf8');
 
     // U+FFFD is the replacement character for invalid UTF-8 sequences
     // If we find it, return the hex original string
-    if (/[\uFFFD]/.test(text)) return hex;
-
-    return text;
+    return /[\uFFFD]/.test(text) ? hex : text;
 };
 
 export const deepTransform = <V>(transform: (str: string) => V) => {
