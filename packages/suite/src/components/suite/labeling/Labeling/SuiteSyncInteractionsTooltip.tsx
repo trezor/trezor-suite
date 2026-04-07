cd7ef25d7f473b03ac1766f3e197c2cca42adf31
Peter Sanderson <dierk.deidel@protonmail.com>
2026-04-07 13:27:02 +0200

    fix: correct error message when device is unsuported for Suite Sync

--- packages/suite/src/components/suite/labeling/Labeling/SuiteSyncInteractionsTooltip.tsx
+++ packages/suite/src/components/suite/labeling/Labeling/SuiteSyncInteractionsTooltip.tsx
@@ -1,35 +1,19 @@
 import { type ReactNode } from 'react';
 
 import { Translation, type TranslationKey } from '@suite/intl';
-import { selectDeviceByStaticSessionId, selectDeviceLabelOrNameById } from '@suite-common/device';
 import { type SuiteSyncInteraction } from '@suite-common/suite-sync';
 import { Text, Tooltip } from '@trezor/components';
-import { type StaticSessionId } from '@trezor/connect';
 import { exhaustive } from '@trezor/type-utils';
-
-import { useSelector } from '../../../../hooks/suite';
 
 type LabelingDisabledTooltipProps = {
     suiteSyncInteraction: SuiteSyncInteraction | null;
     children: ReactNode;
-    deviceStaticSessionId: StaticSessionId;
 };
 
 export const SuiteSyncInteractionsTooltip = ({
     suiteSyncInteraction,
     children,
-    deviceStaticSessionId,
 }: LabelingDisabledTooltipProps) => {
-    const device = useSelector(state =>
-        deviceStaticSessionId !== null
-            ? selectDeviceByStaticSessionId(state, deviceStaticSessionId)
-            : undefined,
-    );
-
-    const deviceLabel = useSelector(state =>
-        device !== undefined ? selectDeviceLabelOrNameById(state, device.id) : null,
-    );
-
     if (suiteSyncInteraction === null) {
         return children;
     }
@@ -53,10 +37,7 @@ export const SuiteSyncInteractionsTooltip = ({
                 <Tooltip
                     content={
                         <Text>
-                            <Translation
-                                id={translationMap[suiteSyncInteraction]}
-                                values={{ name: deviceLabel }}
-                            />
+                            <Translation id={translationMap[suiteSyncInteraction]} />
                         </Text>
                     }
                 >
