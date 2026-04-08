5fb98091c6a1d37ababbb897621e591c85605109
Szymon Lesisz <sz.lesisz@gmail.com>
2026-04-08 14:28:41 +0200

    chore(protobuf): remove obsolete code

--- packages/protobuf/scripts/protobuf-thp-definitions.ts
+++ packages/protobuf/scripts/protobuf-thp-definitions.ts
@@ -5,7 +5,6 @@ import path from 'path';
 
 import { buildDefinitions } from './protobuf-definitions';
 import { DISCLAIMER } from './protobuf-patches';
-import { createTypes } from './protobuf-types';
 
 const DIST = path.join(__dirname, '../../protocol/src/protocol-thp/messages');
 
@@ -17,34 +16,6 @@ const buildThpDefinitions = (root: ReturnType<typeof buildDefinitions>) => {
     lines.push(`export const getProtobufDefinitions = () => (`, defs, `);`, '');
 
     fs.writeFileSync(`${DIST}/protobufDefinitions.ts`, lines.join('\n'));
-};
-
-const createMessageType = (types: ReturnType<typeof createTypes>) => {
-    const lines: string[] = [];
-    lines.push('export type ThpProtobufMessageType = {');
-    types
-        .flatMap(t => (t && t.type === 'message' ? t : []))
-        .forEach(t => {
-            lines.push(`    ${t.name}: ${t.name};`);
-        });
-    lines.push('};', '');
-
-    return lines;
-};
-
-const buildThpTypes = (json: ReturnType<typeof buildDefinitions>) => {
-    const types = createTypes(json, { defaultRule: 'required', fixOrder: false });
-    const lines: string[] = [];
-    lines.push(DISCLAIMER, '');
-
-    const content = lines
-        .concat(
-            types.flatMap(t => (t ? t.value : [])),
-            createMessageType(types),
-        )
-        .join('\n');
-
-    fs.writeFileSync(`${DIST}/protobufTypes.ts`, content);
 };
 
 // merge ThpMessageType into MessageType
@@ -79,7 +50,6 @@ const run = () => {
 
     modifyMessageType(defs);
     buildThpDefinitions(defs);
-    buildThpTypes(defs);
 
     const filePath = `${DIST}/protobuf*`;
     const cmd = `yarn workspace @trezor/protocol`;
