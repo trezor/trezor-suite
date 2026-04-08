798d76ecde9816a27dc5ddae8e22b88d9433b41b
Mattew Husak <matej.husak@icloud.com>
2026-04-08 23:14:49 +0200

    chore(migration): split feature feedback into 26.4.0.2.ts

--- packages/suite/src/storage/migrations/versions/26.3.0.1.ts
+++ packages/suite/src/storage/migrations/versions/26.3.0.1.ts
@@ -3,6 +3,6 @@ import { createMigration } from '@suite/idb-migration-utils';
 import { type SuiteDBSchema } from 'src/storage/definitions';
 
 export default createMigration<SuiteDBSchema>('26.3.0.1', db => {
-    // @ts-expect-error experimentalFeedback was renamed to featureFeedback in 26.4.0.1
+    // @ts-expect-error experimentalFeedback was renamed to featureFeedback in 26.4.0.2
     db.createObjectStore('experimentalFeedback');
 });
