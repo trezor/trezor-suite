15b1cbbee535447c91a670cffbdef8cf5304beb8
Marek Polak <marek.polak@satoshilabs.com>
2026-04-08 13:37:39 +0200

    fix(connect): use proper `GetAddress` schema

--- packages/connect/src/api/getAddress.ts
+++ packages/connect/src/api/getAddress.ts
@@ -1,12 +1,9 @@
 // origin: https://github.com/trezor/connect/blob/develop/src/js/core/methods/GetAddress.js
 
 import { type BitcoinNetworkInfo, Bundle, type PROTO } from '@trezor/connect-common';
-import {
-    GetAddress as GetAddressSchema,
-    UI_REQUEST,
-    createUiMessage,
-} from '@trezor/connect-common';
+import { UI_REQUEST, createUiMessage } from '@trezor/connect-common';
 import { ERRORS } from '@trezor/connect-common/src/constants';
+import { GetAddress as GetAddressSchema } from '@trezor/connect-common/src/types/api/getAddress';
 import { Assert } from '@trezor/schema-utils';
 
 import { bundlify, getFirmwareRange, validateCoinPath } from './common/paramsValidator';
