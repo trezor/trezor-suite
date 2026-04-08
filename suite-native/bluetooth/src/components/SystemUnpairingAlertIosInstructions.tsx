d4a2052e4147878b6c6dac293bdd6b194158f38b
yanas <yanas@yanas.cz>
2026-04-08 16:47:43 +0200

    fix(suite-native): use correct layout in iOS system unpairing alert

--- suite-native/bluetooth/src/components/SystemUnpairingAlertIosInstructions.tsx
+++ suite-native/bluetooth/src/components/SystemUnpairingAlertIosInstructions.tsx
@@ -1,30 +1,18 @@
-import { BottomSheetListItem, Box, Text, VStack } from '@suite-native/atoms';
-import { Translation, type TxKeyPath } from '@suite-native/intl';
+import { BottomSheetListItem, VStack } from '@suite-native/atoms';
 
-type SystemUnpairingAlertIosInstructionsProps = {
-    translationKey: TxKeyPath;
-};
-
-export const SystemUnpairingAlertIosInstructions = ({
-    translationKey,
-}: SystemUnpairingAlertIosInstructionsProps) => (
-    <Box>
-        <Text>
-            <Translation id={translationKey} />
-        </Text>
-        <VStack paddingTop="sp24">
-            <BottomSheetListItem
-                iconNumber={1}
-                translationKey="bluetooth.alerts.unpairingInstructions.step1"
-            />
-            <BottomSheetListItem
-                iconNumber={2}
-                translationKey="bluetooth.alerts.unpairingInstructions.step2"
-            />
-            <BottomSheetListItem
-                iconNumber={3}
-                translationKey="bluetooth.alerts.unpairingInstructions.step3"
-            />
-        </VStack>
-    </Box>
+export const SystemUnpairingAlertIosInstructions = () => (
+    <VStack>
+        <BottomSheetListItem
+            iconNumber={1}
+            translationKey="bluetooth.alerts.unpairingInstructions.step1"
+        />
+        <BottomSheetListItem
+            iconNumber={2}
+            translationKey="bluetooth.alerts.unpairingInstructions.step2"
+        />
+        <BottomSheetListItem
+            iconNumber={3}
+            translationKey="bluetooth.alerts.unpairingInstructions.step3"
+        />
+    </VStack>
 );
