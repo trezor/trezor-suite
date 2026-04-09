3ebaf119e2bada8c9d193d20bb27795dede2a32c
brantalikp <brantalikp@gmail.com>
2026-04-09 13:55:29 +0200

    feat(suite-native): staking - thod support enable

--- suite-native/module-earn/src/constants.ts
+++ suite-native/module-earn/src/constants.ts
@@ -1,9 +1,10 @@
-import { type NetworkSymbol } from '@suite-common/wallet-config';
+import { type NetworkSymbol, getNetworkFeatures } from '@suite-common/wallet-config';
 
-export const MOBILE_SUPPORTED_STAKING_NETWORKS: readonly NetworkSymbol[] = ['eth'] as const;
+export const MOBILE_SUPPORTED_STAKING_NETWORKS: readonly NetworkSymbol[] = ['eth', 'thod'] as const;
 
 export const isMobileSupportedStakingNetwork = (symbol: NetworkSymbol): boolean =>
-    MOBILE_SUPPORTED_STAKING_NETWORKS.includes(symbol);
+    MOBILE_SUPPORTED_STAKING_NETWORKS.includes(symbol) &&
+    getNetworkFeatures(symbol).includes('staking');
 
 export const CRYPTO_BALANCE_DECIMALS = 5;
 export const NETWORK_FEE_WARNING_MULTIPLIER = 4;
