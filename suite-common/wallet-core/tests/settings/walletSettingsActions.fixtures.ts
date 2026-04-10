1a554fc40d35ee32f4511cdb1c88d349cafad347
Jan Václavík <jvaclavik@gmail.com>
2026-04-10 16:37:25 +0200

    fix(suite): Enable ETH after onboarding
    (cherry picked from commit a487894ad7b71ff7e3564e77e7892a43a7751a4c)

--- suite-common/wallet-core/tests/settings/walletSettingsActions.fixtures.ts
+++ suite-common/wallet-core/tests/settings/walletSettingsActions.fixtures.ts
@@ -3,11 +3,11 @@ import { changeCoinVisibility } from '../../src/settings/walletSettingsThunks';
 
 export const walletSettingsFixtures = [
     {
-        description: 'Btc should be visible as a default if no initial state provided',
+        description: 'Btc and eth should be visible as a default if no initial state provided',
         initialState: undefined,
         action: () => changeCoinVisibility({ symbol: 'ltc', shouldBeVisible: true }),
         result: {
-            enabledNetworks: ['btc', 'ltc'],
+            enabledNetworks: ['btc', 'eth', 'ltc'],
         },
     },
     {
