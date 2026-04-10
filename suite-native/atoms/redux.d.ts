f49d9db5c91383d01b8a2655c1d584b06deb6929
Peter Sanderson <dierk.deidel@protonmail.com>
2026-04-10 14:26:34 +0200

    fix: redux.d.ts, this package shall not have redux at all, but for now, at least correct typings

--- suite-native/atoms/redux.d.ts
+++ suite-native/atoms/redux.d.ts
@@ -1,7 +1,10 @@
 import { AsyncThunkAction, ThunkAction } from '@reduxjs/toolkit';
+import type { Action, AnyAction } from 'redux';
 
 declare module 'redux' {
     export interface Dispatch<A extends Action = AnyAction> {
+        <T extends A>(action: T, ...extraArgs: any[]): T;
+
         <TThunk extends AsyncThunkAction<any, any, any>>(thunk: TThunk): ReturnType<TThunk>;
 
         <ReturnType = any, State = any, ExtraThunkArg = any>(
