cc235619b30d9623fda9b988509dcc72fc9737cf
Jiří Čermák <jiri.cermak@satoshilabs.com>
2026-04-13 11:20:39 +0200

    fix: add prod url for earn staking api

--- suite-common/earn-staking-api/src/constants/index.ts
+++ suite-common/earn-staking-api/src/constants/index.ts
@@ -1,1 +1,5 @@
-export const EARN_API_BASE_URL = 'https://dev-earn.suite.sldev.cz/staking/v1';
+import { isCodesignBuild } from '@trezor/env-utils';
+
+export const EARN_API_BASE_URL = isCodesignBuild()
+    ? 'https://earn.trezor.io/staking/v1'
+    : 'https://dev-earn.suite.sldev.cz/staking/v1';
