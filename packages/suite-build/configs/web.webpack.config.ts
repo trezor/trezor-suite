4e4b37b36264d2afde770626fcacbc83b9354ef5
Szymon Lesisz <sz.lesisz@gmail.com>
2026-04-14 14:39:59 +0200

    feat(suite-web): add `connect-popup/bootstrap.html` endpoint to build

--- packages/suite-build/configs/web.webpack.config.ts
+++ packages/suite-build/configs/web.webpack.config.ts
@@ -25,6 +25,10 @@ const config: webpack.Configuration = {
             // Use importScripts-based chunk loading so vendor/runtime chunks load in a worker context
             chunkLoading: 'import-scripts',
         },
+        'connect-popup-bootstrap': {
+            filename: 'connect-popup/bootstrap.[hash].js',
+            import: path.resolve(__dirname, '../../connect-web/src/bootstrap/index.ts'),
+        },
     },
     output: {
         path: path.join(baseDir, 'build'),
@@ -91,6 +95,18 @@ const config: webpack.Configuration = {
                     filename: path.join(baseDir, 'build', route.pattern, 'index.html'),
                 }),
         ),
+        new HtmlWebpackPlugin({
+            chunks: ['connect-popup-bootstrap'],
+            minify: false,
+            templateParameters: {
+                assetPrefix,
+                isOnionLocation: FLAGS.ONION_LOCATION_META,
+            },
+            inject: 'body' as const,
+            scriptLoading: 'blocking' as const,
+            template: path.resolve(__dirname, '../../connect-web/src/bootstrap/bootstrap.html'),
+            filename: path.join(baseDir, 'build/connect-popup/bootstrap.html'),
+        }),
         ...(!isDev ? [new CssMinimizerPlugin()] : []),
     ],
 };
