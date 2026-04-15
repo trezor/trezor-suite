1e09280ce90fe23a08be69cf819a2890c661fed8
Szymon Lesisz <sz.lesisz@gmail.com>
2026-04-15 12:13:34 +0200

    feat(connect-web): implement bootstrap in web popup

--- packages/connect-web/src/popup/web.ts
+++ packages/connect-web/src/popup/web.ts
@@ -1,43 +1,116 @@
+import * as ERRORS from '@trezor/connect-common/src/constants/errors';
 import { type CoreEventMessage } from '@trezor/connect-common/src/events';
 import { type AbstractMessageChannel } from '@trezor/connect-common/src/messageChannel/abstract';
 import { WindowWindowChannel } from '@trezor/connect-common/src/messageChannel/window-window';
+import { getOrigin } from '@trezor/connect-common/src/utils/urlUtils';
+import { getWeakRandomId } from '@trezor/utils/src/getWeakRandomId';
 
 import { Popup } from './abstract';
+import { getIframeInstance } from './iframe';
+import { BootstrapError } from '../bootstrap/bootstrap-errors';
 
 export class WebPopup extends Popup {
     private popupWindow: Window | undefined;
+    private iframe = getIframeInstance();
+    private channelId = getWeakRandomId(16);
 
-    protected createChannel(origin: string): AbstractMessageChannel<CoreEventMessage> {
+    protected createChannel(): AbstractMessageChannel<CoreEventMessage> {
         return new WindowWindowChannel<CoreEventMessage>({
             windowHere: window,
-            windowPeer: () => this.popupWindow,
+            windowPeer: () => this.iframe.get()?.contentWindow || undefined,
             channel: {
                 here: '@trezor/connect-web',
                 peer: '@trezor/connect-popup',
             },
             logger: this.logger,
-            origin,
+            origin: getOrigin(this.popupSrc),
         });
     }
 
-    protected open(): void {
+    protected async open(): Promise<void> {
         const url = this.buildPopupUrl(this.popupSrc);
+        const query = `connect-popup-req=${this.channelId}`;
+        const debug = this.logger.enabled ? '&debug=1' : '';
+        const popupUrl = `${url}/bootstrap.html?${query}${debug}`;
+        const popupOrigin = getOrigin(this.popupSrc);
 
-        const windowResult = window.open(url, 'modal');
-
+        const windowResult = window.open('about:blank', '_blank');
         if (!windowResult) {
-            this.handleOpenFailure('Popup window blocked by browser');
+            const error = ERRORS.TypedError('Handshake_Error', 'popup-blocked');
+            this.handleOpenFailure(error.message);
 
-            return;
+            return Promise.reject(error);
         }
 
-        this.popupWindow = windowResult;
+        try {
+            await this.iframe.create(popupUrl);
+        } catch (error) {
+            windowResult.close();
+            this.handleOpenFailure(error.message);
+
+            return Promise.reject(error);
+        }
+
+        // Open bootstrap.html. From now on we cant control the window.
+        // Communication via postMessage is limited to the same origin or localhost.
+        windowResult.location.href = popupUrl;
+
+        // TODO: popupWindow assign conditionally when we have limited control over the window.
+        // this.popupWindow = windowResult;
+
+        const iframeWindowChannel = new WindowWindowChannel<
+            CoreEventMessage | { type: 'channel-handshake-error'; error: string }
+        >({
+            windowHere: window,
+            windowPeer: () => this.iframe.get()?.contentWindow || undefined,
+            channel: {
+                here: '@trezor/connect-web',
+                peer: '@trezor/connect-bootstrap-iframe',
+            },
+            logger: this.logger,
+            origin: popupOrigin,
+        });
+
+        iframeWindowChannel.on('message', message => {
+            if (message.type === 'channel-handshake-error') {
+                // This works only on limited setups: same origin or localhost.
+                // Handled by ../bootstrap.ts popup handshake
+                windowResult.postMessage(
+                    {
+                        type: 'channel-handshake-error',
+                        error: message.error,
+                    },
+                    popupOrigin,
+                );
+
+                this.handleOpenFailure(message.error);
+                iframeWindowChannel.abortHandshake(message.error);
+            }
+        });
+
+        try {
+            await iframeWindowChannel.init();
+            iframeWindowChannel.disconnect();
+        } catch (error) {
+            this.handleOpenFailure(error.message);
+            iframeWindowChannel.disconnect();
+
+            const isBootstrapError = Object.values(BootstrapError).includes(error.message);
+            if (isBootstrapError) {
+                // transform bootstrap-specific error
+                return Promise.reject(ERRORS.TypedError('Handshake_Error', error.message));
+            }
+
+            return Promise.reject(error);
+        }
 
         if (!this.channel.isConnected) {
             this.channel.connect();
         }
 
-        this.startCloseMonitoring();
+        // TODO: implement ping-pong this.startCloseMonitoring();
+
+        return Promise.resolve();
     }
 
     protected focusPopup(): void {
@@ -50,7 +123,7 @@ export class WebPopup extends Popup {
     }
 
     protected isOpen(): Promise<boolean> {
-        return Promise.resolve(this.popupWindow !== undefined && !this.popupWindow.closed);
+        return Promise.resolve(this.popupWindow !== undefined);
     }
 
     protected onReset(): void {}
