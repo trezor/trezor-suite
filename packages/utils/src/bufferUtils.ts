2da5c923c515d78716d45d4dc6ba366490b10fc8
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-04-20 13:19:00 +0200

    feat(utils): add bufferToBytes, toNonSharedBuffer

--- packages/utils/src/bufferUtils.ts
+++ packages/utils/src/bufferUtils.ts
@@ -17,3 +17,20 @@ export const getChunkSize = (n: number) => {
 
     return buf;
 };
+
+export const bufferToBytes = (buffer: Buffer<ArrayBuffer>): ArrayBuffer =>
+    buffer.buffer.slice(buffer.byteOffset, buffer.byteOffset + buffer.byteLength);
+
+/**
+ * Forces the broader ArrayBufferLike buffer (a shared or non-shared buffer) to the plain non-shared buffer if needed,
+ * though a non-shared buffer is left unchanged.
+ * Our codebase should generally work with the broader type, but conversion is needed to interface with APIs
+ * that only accept non-shared buffers (e.g. `crypto.subtle.digest`).
+ */
+export const toNonSharedBuffer = (view: Buffer<ArrayBufferLike>): Buffer<ArrayBuffer> => {
+    if (view.buffer instanceof ArrayBuffer) {
+        return view as Buffer<ArrayBuffer>;
+    }
+
+    return Buffer.from(view); // byte-copy of original buffer
+};
