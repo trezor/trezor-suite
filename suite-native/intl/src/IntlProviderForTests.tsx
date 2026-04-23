59eb646e2025abb9aa4ca4128da93239392d51cc
juriczech <bohdan.juricek@gmail.com>
2026-04-23 14:09:26 +0200

    chore(suite-native): host flatten messages

--- suite-native/intl/src/IntlProviderForTests.tsx
+++ suite-native/intl/src/IntlProviderForTests.tsx
@@ -3,13 +3,11 @@ import { IntlProvider as ReactIntlProvider } from 'react-intl';
 import { messages } from './messages';
 import { flatten } from './utils';
 
-// For uni test we always expect the messages to be in english, so the language selection logic can be omitted here.
-export const IntlProviderForTests = ({ children }: { children: React.ReactNode }) => {
-    const x = flatten(messages);
+const flatMessages = flatten(messages);
 
-    return (
-        <ReactIntlProvider locale="en-US" messages={x}>
-            {children}
-        </ReactIntlProvider>
-    );
-};
+// For uni test we always expect the messages to be in english, so the language selection logic can be omitted here.
+export const IntlProviderForTests = ({ children }: { children: React.ReactNode }) => (
+    <ReactIntlProvider locale="en-US" messages={flatMessages}>
+        {children}
+    </ReactIntlProvider>
+);
