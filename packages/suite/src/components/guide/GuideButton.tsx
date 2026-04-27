ae50c55612840071428bd89660744b95ecb46ba9
Jan Václavík <jvaclavik@gmail.com>
2026-04-27 09:22:08 +0200

    feat(suite): Hide guide button sooner

--- packages/suite/src/components/guide/GuideButton.tsx
+++ packages/suite/src/components/guide/GuideButton.tsx
@@ -3,7 +3,7 @@ import { FreeFocusInside } from 'react-focus-lock';
 import styled from 'styled-components';
 
 import { Translation } from '@suite/intl';
-import { IconButton, Tooltip } from '@trezor/components';
+import { IconButton, TOOLTIP_DELAY_LONG, Tooltip } from '@trezor/components';
 import { zIndices } from '@trezor/theme';
 import { hexToRgba } from '@trezor/utils';
 
@@ -16,8 +16,8 @@ const Wrapper = styled.div<{ $isGuideOpen: boolean }>`
     right: 15px;
     border-radius: 12px;
     backdrop-filter: blur(10px);
-    transition: all 0.3s ease 0.3s;
-    visibility: ${({ $isGuideOpen }) => ($isGuideOpen ? 'hidden' : 'visible')};
+    transition: ${({ $isGuideOpen }) => ($isGuideOpen ? 'none' : 'all 0.3s ease 0.3s')};
+    opacity: ${({ $isGuideOpen }) => ($isGuideOpen ? '0' : '1')};
     background: ${({ theme }) => hexToRgba(theme.surfaceFillModeless, 0.7)};
 `;
 
@@ -29,7 +29,8 @@ export const GuideButton = () => {
             <Wrapper $isGuideOpen={isGuideOpen}>
                 <Tooltip
                     content={<Translation id="TR_GUIDE_SUPPORT_AND_FEEDBACK" />}
-                    placement="left"
+                    placement="top"
+                    delayShow={TOOLTIP_DELAY_LONG}
                 >
                     <IconButton
                         data-testid="@guide/button-open"
