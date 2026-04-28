ae9d2efafd91f3b42e97f819b6e741bdd96086d2
Martin Šmídl <smidl.martin1@gmail.com>
2026-04-28 13:15:47 +0200

    feat(suite-native): align list design with figma

--- suite-native/trading-residence/src/components/CountrySheet/CountryListItem.tsx
+++ suite-native/trading-residence/src/components/CountrySheet/CountryListItem.tsx
@@ -1,46 +1,32 @@
-import { Platform, Pressable } from 'react-native';
-import { FadeIn, FadeOut } from 'react-native-reanimated';
+import { Pressable } from 'react-native';
 
 import { getCountryFlag } from '@suite-common/flags';
 import { type TradingCountryOption } from '@suite-common/trading';
-import { AnimatedBox, Box, Card, Flag, HStack, Radio, Text } from '@suite-native/atoms';
-import { prepareNativeStyle, useNativeStyles } from '@trezor/styles-native';
+import { Box, Flag, HStack, Text } from '@suite-native/atoms';
 
 export type CountryListItemProps = {
     isSelected: boolean;
     onPress: () => void;
 } & TradingCountryOption;
 
-const wrapperStyle = prepareNativeStyle(({ spacings }) => ({
-    marginVertical: spacings.sp4,
-}));
-
-export const CountryListItem = ({ name, onPress, value, isSelected }: CountryListItemProps) => {
-    const { applyStyle } = useNativeStyles();
+export const CountryListItem = ({ name, onPress, value }: CountryListItemProps) => {
     const countryFlag = getCountryFlag(value);
-    // on android fade animation looks ugly on view with shadows, but without animation it looks even worse
-    // do not display shadow on android and keep animating
-    const noShadow = Platform.OS === 'android';
 
     return (
-        <AnimatedBox entering={FadeIn} exiting={FadeOut}>
-            <Pressable onPress={onPress} style={applyStyle(wrapperStyle)}>
-                <Card noShadow={noShadow}>
-                    <HStack alignItems="center" justifyContent="space-between">
-                        <Box flex={0}>
-                            {countryFlag && <Flag country={countryFlag} size={20} />}
-                        </Box>
-                        <Box flex={1}>
-                            <Text variant="body-md" color="contentPrimary">
-                                {name}
-                            </Text>
-                        </Box>
-                        <Box flex={0}>
-                            <Radio value={value} onPress={onPress} isChecked={isSelected} />
-                        </Box>
-                    </HStack>
-                </Card>
-            </Pressable>
-        </AnimatedBox>
+        <Pressable onPress={onPress} accessibilityRole="button" accessibilityLabel={name}>
+            <HStack
+                alignItems="center"
+                spacing="sp12"
+                paddingVertical="sp20"
+                paddingHorizontal="sp6"
+            >
+                <Box flex={0}>{countryFlag && <Flag country={countryFlag} size={20} />}</Box>
+                <Box flex={1}>
+                    <Text variant="body-md" color="contentPrimary">
+                        {name}
+                    </Text>
+                </Box>
+            </HStack>
+        </Pressable>
     );
 };
--- suite-native/trading-residence/src/components/CountrySheet/CountrySubdivisionListItem.tsx
+++ suite-native/trading-residence/src/components/CountrySheet/CountrySubdivisionListItem.tsx
@@ -1,44 +1,19 @@
-import { Platform, Pressable } from 'react-native';
-import { FadeIn, FadeOut } from 'react-native-reanimated';
+import { Pressable } from 'react-native';
 
 import { type TradingCountrySubdivisionOption } from '@suite-common/trading';
-import { AnimatedBox, Box, Card, HStack, Radio, Text } from '@suite-native/atoms';
-import { prepareNativeStyle, useNativeStyles } from '@trezor/styles-native';
+import { HStack, Text } from '@suite-native/atoms';
 
 export type CountrySubdivisionListItemProps = {
     isSelected: boolean;
     onPress: () => void;
 } & TradingCountrySubdivisionOption;
 
-const wrapperStyle = prepareNativeStyle(({ spacings }) => ({
-    marginVertical: spacings.sp4,
-}));
-
-export const CountrySubdivisionListItem = ({
-    name,
-    onPress,
-    value,
-    isSelected,
-}: CountrySubdivisionListItemProps) => {
-    const { applyStyle } = useNativeStyles();
-    const noShadow = Platform.OS === 'android';
-
-    return (
-        <AnimatedBox entering={FadeIn} exiting={FadeOut}>
-            <Pressable onPress={onPress} style={applyStyle(wrapperStyle)}>
-                <Card noShadow={noShadow}>
-                    <HStack alignItems="center" justifyContent="space-between">
-                        <Box flex={1}>
-                            <Text variant="body-md" color="contentPrimary">
-                                {name}
-                            </Text>
-                        </Box>
-                        <Box flex={0}>
-                            <Radio value={value} onPress={onPress} isChecked={isSelected} />
-                        </Box>
-                    </HStack>
-                </Card>
-            </Pressable>
-        </AnimatedBox>
-    );
-};
+export const CountrySubdivisionListItem = ({ name, onPress }: CountrySubdivisionListItemProps) => (
+    <Pressable onPress={onPress} accessibilityRole="button" accessibilityLabel={name}>
+        <HStack alignItems="center" spacing="sp12" paddingVertical="sp20" paddingHorizontal="sp6">
+            <Text variant="body-md" color="contentPrimary">
+                {name}
+            </Text>
+        </HStack>
+    </Pressable>
+);
