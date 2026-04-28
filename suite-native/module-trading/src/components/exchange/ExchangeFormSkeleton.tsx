985663c4cae0712c7305367fe7de42162cf246fe
Jirka Bažant <jiri.bazant@satoshilabs.com>
2026-04-28 15:05:25 +0200

    chore(suite-native): adjust ExchangeFormSkeleton for improved layout consistency

--- suite-native/module-trading/src/components/exchange/ExchangeFormSkeleton.tsx
+++ suite-native/module-trading/src/components/exchange/ExchangeFormSkeleton.tsx
@@ -4,15 +4,15 @@ import { SkeletonLargeRow, SkeletonSmall } from '@suite-native/trading-atoms';
 export const ExchangeFormSkeleton = () => (
     <>
         <Card>
-            <VStack>
-                <SkeletonSmall widthPercentage={0.2} />
-                <SkeletonLargeRow leftWidthPercentage={0.4} rightWidthPercentage={0.35} />
-            </VStack>
-        </Card>
-        <Card>
-            <VStack>
-                <SkeletonSmall widthPercentage={0.2} />
-                <SkeletonLargeRow leftWidthPercentage={0.4} rightWidthPercentage={0.3} />
+            <VStack spacing="sp16">
+                <VStack>
+                    <SkeletonSmall widthPercentage={0.2} />
+                    <SkeletonLargeRow leftWidthPercentage={0.4} rightWidthPercentage={0.35} />
+                </VStack>
+                <VStack>
+                    <SkeletonSmall widthPercentage={0.2} />
+                    <SkeletonLargeRow leftWidthPercentage={0.4} rightWidthPercentage={0.3} />
+                </VStack>
             </VStack>
         </Card>
     </>
