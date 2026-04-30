93e3feda1b8c26f5a21309b5eeb882ff0264bac2
Jiří Čermák <jiri.cermak@satoshilabs.com>
2026-04-30 16:10:22 +0200

    feat: add tx simulation for stablecoin yield

--- suite-common/tx-simulation/src/index.ts
+++ suite-common/tx-simulation/src/index.ts
@@ -4,9 +4,11 @@ export { getSimulationErrorRiskLevel, areTxSimulationMethods } from './utils';
 export { useDappScan } from './hooks/useDappScan';
 export {
     useNetworkTxSimulation,
+    isTxSimulationResultWithMethods,
     type NetworkTxSimulationResult,
     type TxSimulationEVMResult,
 } from './hooks/useNetworkTxSimulation';
 export { useTxSimulation } from './hooks/useTxSimulation';
 
 export type { AccountSummary, TransactionSimulation } from '@blockaid/client/resources/evm';
+export { TX_METHODS_WITH_FEES } from './config';
