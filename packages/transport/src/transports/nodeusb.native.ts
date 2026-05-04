550d6d497681e9bd5be33f2b29df3dbf1dab1288
Marek Polak <marek.polak@satoshilabs.com>
2026-05-04 13:52:24 +0200

    build: ignore `.browser.` files for commonjs

---
+++ packages/transport/src/transports/nodeusb.native.ts
@@ -0,0 +1,26 @@
+import { AbstractTransport, type AbstractTransportParams } from './abstract';
+import { WRONG_ENVIRONMENT } from '../errors';
+import { empty, emptySync } from '../utils/resultEmpty';
+
+// this class loads in browser environment only in case of accidental use of NodeUsbTransport
+
+export class NodeUsbTransport extends AbstractTransport {
+    public name = 'NodeUsbTransport' as const;
+
+    constructor(params: AbstractTransportParams) {
+        super(params);
+        console.error(WRONG_ENVIRONMENT);
+    }
+
+    init = empty;
+    acquire = empty;
+    enumerate = empty;
+    call = empty;
+    receive = empty;
+    send = empty;
+    release = empty;
+    stop = empty;
+    releaseDevice = empty;
+    releaseSync = emptySync;
+    listen = emptySync;
+}
