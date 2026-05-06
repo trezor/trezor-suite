b0c1734f0323b866b9176236fa9f1dde9e731581
Marek Polak <marek.polak@satoshilabs.com>
2026-05-06 17:05:29 +0200

    chore(utxo-lib): separate module declarations

--- packages/utxo-lib/src/types/pushdata-bitcoin.d.ts
+++ packages/utxo-lib/src/types/pushdata-bitcoin.d.ts
@@ -1,13 +1,3 @@
-declare module 'bip66' {
-    function check(buffer: Buffer): boolean;
-    function decode(buffer: Buffer): { r: Buffer; s: Buffer };
-    function encode(r: Buffer, s: Buffer): Buffer;
-}
-
-declare module 'bitcoin-ops';
-
-declare module 'minimaldata';
-
 declare module 'pushdata-bitcoin' {
     function encodingLength(len: number): number;
     function encode(buffer: Buffer, number: number, offset: number): number;
@@ -16,5 +6,3 @@ declare module 'pushdata-bitcoin' {
         offset: number,
     ): { opcode: number; number: number; size: number };
 }
-
-declare module 'blake-hash';
