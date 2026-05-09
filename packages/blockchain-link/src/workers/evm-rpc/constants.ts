d0f67ccbff699e8a22c1686ab044ee1f7c3c7ead
Martin Varmuza <mvarmuza@gmail.com>
2026-05-09 16:55:17 +0200

    chore(blockchain-link): remove dead TOKEN_DISCOVERY constant

--- packages/blockchain-link/src/workers/evm-rpc/constants.ts
+++ packages/blockchain-link/src/workers/evm-rpc/constants.ts
@@ -1,8 +1,3 @@
-export const TOKEN_DISCOVERY = {
-    CHUNK_SIZE: 10000,
-    MAX_BLOCKS_TO_CHECK: 1000000,
-} as const;
-
 export const BLOCK_SUBSCRIPTION = {
     POLL_INTERVAL_MS: 10000,
 } as const;
