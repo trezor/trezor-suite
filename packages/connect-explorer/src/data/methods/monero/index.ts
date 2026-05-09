3d948e9d963c93b5027924e087872053b17f00df
Martin Varmuza <mvarmuza@gmail.com>
2026-05-09 10:22:06 +0200

    chore(connect-explorer): remove orphaned modules and dead module-level exports
    Removes orphaned files and module-level dead code that had no inbound references:
    - 11 per-method config files never registered in any barrel (e.g. solana/getAccountInfo, ethereum/pushTransaction, management/{wipeDevice,getFeatures,firmwareUpdate,backupDevice,recoverDevice}, ripple/getAccountInfo, ripple/pushTransaction)
    - 12 unused index.ts barrels (data/methods/{bitcoin,blockchain,cardano,ethereum,management,monero,other,ripple,solana,stellar,tezos}/index.ts and data/methods/index.ts)
    - src/actions/index.ts barrel
    - src/utils/windowUtils.ts utility
    - src/data/methods/ripple/common.ts orphan
    - bitcoin/common.ts: remove unused 13-element xpubs array
    - global.d.ts: remove unused __TREZOR_CONNECT_SRC Window augmentation

--- packages/connect-explorer/src/data/methods/monero/index.ts
+++
@@ -1,6 +0,0 @@
-import getAddress from './getAddress';
-import getWatchKey from './getWatchKey';
-import keyImageSync from './keyImageSync';
-import signTransaction from './signTransaction';
-
-export default [...getAddress, ...getWatchKey, ...keyImageSync, ...signTransaction];
