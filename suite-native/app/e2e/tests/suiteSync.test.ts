c7642f9558255d2787d8563699ebd803289e9e3b
Ondrej Hajek <ondrej.hajek@satoshilabs.com>
2026-05-11 15:02:26 +0200

    fix(e2e): Fully skipping Suite sync native test to prevent freezes

--- suite-native/app/e2e/tests/suiteSync.test.ts
+++ suite-native/app/e2e/tests/suiteSync.test.ts
@@ -75,7 +75,7 @@ const preloadedState = preparePreloadedReduxState(
     deviceChecksDisabledState,
 );
 
-describe('Suite Sync - Labelling [@androidOnly @T3T1 @smoke]', () => {
+describe.skip('Suite Sync - Labelling [@androidOnly @T3T1 @smoke]', () => {
     let evoluClient: NativeEvoluClient;
 
     beforeEach(async () => {
