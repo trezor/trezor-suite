7a45537f6320126eef5f98523652a0f0f62793ae
karliatto <yo@karliatto.com>
2026-05-12 20:23:25 +0200

    feat(connect-common): new event CORE_CALL_CANCEL
    The event CORE_CALL_CANCEL is the substitute for POPUP.CLOSED when it was not actually a popup closing the reason for it.

--- packages/connect-common/src/events/core-call.ts
+++ packages/connect-common/src/events/core-call.ts
@@ -1,2 +1,5 @@
 // Request to execute a Connect method in Core
 export const CORE_CALL = 'iframe-call' as const; // Keep old name for backward compatibility.
+
+// Request to cancel an in-progress Core method call
+export const CORE_CALL_CANCEL = 'core-call-cancel' as const;
