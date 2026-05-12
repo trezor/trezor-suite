0355f749f21b3141d6d6caceb992067f848f9480
Martin Varmuza <mvarmuza@gmail.com>
2026-05-12 07:54:47 +0200

    gnhf 106: Tightened `Record<string, Deferred<any>>` to `Record<string, Deferred<unknown>>` in @suite-common/fiat-services' ParallelRequestsCache, eliminating the file's lone `any` annotation with a single contained `as Promise<T>` cast at the cache-hit return site.

--- suite-common/fiat-services/src/cache.ts
+++ suite-common/fiat-services/src/cache.ts
@@ -3,16 +3,16 @@ import { type Deferred, createDeferred } from '@trezor/utils';
 // Cache for parallel requests
 // It's used to prevent multiple requests for the same data
 export class ParallelRequestsCache {
-    private promises: Record<string, Deferred<any>> = {};
+    private promises: Record<string, Deferred<unknown>> = {};
 
     async cache<T>(keys: (string | number | undefined)[], fn: () => Promise<T>): Promise<T> {
         const key = keys.join('-');
         if (this.promises[key]) {
             // Cache hit
-            return this.promises[key].promise;
+            return this.promises[key].promise as Promise<T>;
         }
 
-        this.promises[key] = createDeferred();
+        this.promises[key] = createDeferred<unknown>();
         try {
             const res = await fn();
             this.promises[key].resolve(res);
