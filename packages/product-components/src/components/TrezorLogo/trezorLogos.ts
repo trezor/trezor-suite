70bc306a08c5ddd443fd184f72ab21dd8936c538
Martin Varmuza <mvarmuza@gmail.com>
2026-05-12 10:49:04 +0200

    gnhf 139: Tightened the `LOGOS` constant in @trezor/product-components from `{ [key: string]: any }` to `Record<string, string>`, eliminating one `any` annotation in an internal SVG lookup map.

--- packages/product-components/src/components/TrezorLogo/trezorLogos.ts
+++ packages/product-components/src/components/TrezorLogo/trezorLogos.ts
@@ -1,4 +1,4 @@
-export const LOGOS: { [key: string]: any } = {
+export const LOGOS: Record<string, string> = {
     HORIZONTAL: require(`../../images/logos/trezor_logo_horizontal.svg`),
     VERTICAL: require(`../../images/logos/trezor_logo_vertical.svg`),
     SYMBOL: require(`../../images/logos/trezor_logo_symbol.svg`),
