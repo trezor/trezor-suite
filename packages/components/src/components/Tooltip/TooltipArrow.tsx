66b91bd0f48d962c513da423bf5967f6e25d840d
Jan Václavík <jvaclavik@gmail.com>
2026-05-12 09:48:21 +0200

    fix(suite): Fix arrows in Tooltip and enable them by default

--- packages/components/src/components/Tooltip/TooltipArrow.tsx
+++ packages/components/src/components/Tooltip/TooltipArrow.tsx
@@ -2,7 +2,6 @@ import { FloatingArrow } from '@floating-ui/react';
 
 import { paletteV2 } from '@trezor/theme';
 
-import { TOOLTIP_BORDER_RADIUS } from './TooltipBox';
 import { type ArrowProps } from './TooltipFloatingUi';
 
 export const TooltipArrow = ({ ref, context }: ArrowProps) => (
@@ -10,11 +9,7 @@ export const TooltipArrow = ({ ref, context }: ArrowProps) => (
         ref={ref}
         context={context}
         fill={paletteV2.darkCoolGrey50}
-        staticOffset={TOOLTIP_BORDER_RADIUS}
         strokeWidth={0}
         tipRadius={1}
-        style={{
-            transform: 'translateY(-2px)',
-        }}
     />
 );
