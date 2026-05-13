e196e012b6bb4a6c654a283da1538dca99b075e2
Jiří Čermák <jiri.cermak@satoshilabs.com>
2026-05-13 10:30:28 +0200

    fix: correct YIELD_VAULTS_URL path

--- suite-common/token-definitions/scripts/constants/index.ts
+++ suite-common/token-definitions/scripts/constants/index.ts
@@ -18,4 +18,4 @@ export const STELLAR_EXPERT_URL = 'https://api.stellar.expert/explorer/public';
 
 export const NFTS_PER_PAGE = 250;
 
-export const YIELD_VAULTS_URL = 'https://earn.trezor.io/yield/v1/vaults';
+export const YIELD_VAULTS_URL = 'https://earn.trezor.io/yield/vaults/v1';
