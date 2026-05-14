f1567f4240de89c4e753d27865987dd894d85584
Peter Sanderson <dierk.deidel@protonmail.com>
2026-05-14 20:04:59 +0200

    refactor(suite): move SuiteSyncInteractionsTooltip into suite-sync

--- suite/suite-sync/src/index.ts
+++ suite/suite-sync/src/index.ts
@@ -1,5 +1,6 @@
 export { createSuiteSyncDesktopCompositionRoot } from './createSuiteSyncDesktopCompositionRoot';
 export { SelectSuiteSyncServer } from './SelectSuiteSyncServer';
+export { SuiteSyncInteractionsTooltip } from './SuiteSyncInteractionsTooltip';
 export { SuiteSyncServers } from './SuiteSyncServers';
 export { SuiteSyncSettings } from './SuiteSyncSettings';
 export { SuiteSyncWalletDebug } from './SuiteSyncWalletDebug';
