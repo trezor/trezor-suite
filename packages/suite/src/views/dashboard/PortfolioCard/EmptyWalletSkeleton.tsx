4f082f8e1401d080366eaf7e963710fa83c1c967
Jan Václavík <jvaclavik@gmail.com>
2026-05-15 07:07:29 +0200

    fix(suite): Fix empty wallet skeleton

--- packages/suite/src/views/dashboard/PortfolioCard/EmptyWalletSkeleton.tsx
+++ packages/suite/src/views/dashboard/PortfolioCard/EmptyWalletSkeleton.tsx
@@ -1,4 +1,4 @@
-import { Column, Row, SkeletonCircle, SkeletonRectangle } from '@trezor/components';
+import { Column, SkeletonCircle, SkeletonRectangle } from '@trezor/components';
 
 import { useLoadingSkeleton } from 'src/hooks/suite';
 
@@ -6,24 +6,10 @@ export const EmptyWalletSkeleton = () => {
     const { shouldAnimate } = useLoadingSkeleton();
 
     return (
-        <Column gap={4} data-testid="@dashboard/empty-wallet-skeleton" alignItems="center">
+        <Column gap={24} data-testid="@dashboard/empty-wallet-skeleton" alignItems="center">
             <SkeletonCircle size={96} animate={shouldAnimate} />
-            <SkeletonRectangle width={180} height={20} animate={shouldAnimate} borderRadius={6} />
-            <SkeletonRectangle width={280} height={14} animate={shouldAnimate} borderRadius={4} />
-            <Row gap={12} margin={{ top: 24 }}>
-                <SkeletonRectangle
-                    width={120}
-                    height={44}
-                    animate={shouldAnimate}
-                    borderRadius={12}
-                />
-                <SkeletonRectangle
-                    width={120}
-                    height={44}
-                    animate={shouldAnimate}
-                    borderRadius={12}
-                />
-            </Row>
+            <SkeletonRectangle width={180} height={32} animate={shouldAnimate} borderRadius={4} />
+            <SkeletonRectangle width={280} height={28} animate={shouldAnimate} borderRadius={4} />
         </Column>
     );
 };
