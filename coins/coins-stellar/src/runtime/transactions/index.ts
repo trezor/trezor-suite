16d49f8e15bb8ca3f24e2348ddf204c94fcf346b
Marek Polak <marek.polak@satoshilabs.com>
2026-05-15 10:17:18 +0200

    chore(stellar): add code to @trezor/coins-stellar

---
+++ coins/coins-stellar/src/runtime/transactions/index.ts
@@ -0,0 +1,4 @@
+export * from './build';
+export * from './identify';
+export * from './parse';
+export * from './transform';
--- coins/coins-stellar/src/types/index.ts
+++ coins/coins-stellar/src/types/index.ts
@@ -1,1 +1,11 @@
-export {};
+import type { Horizon, Transaction } from '@stellar/stellar-sdk';
+
+import type { identifyTransaction } from '../runtime/transactions/identify';
+
+export type StellarAPI = Horizon.Server;
+
+export type StellarTransaction = Transaction;
+
+export type RawStellarTransaction = Horizon.ServerApi.TransactionRecord;
+
+export type IdentifiedTransaction = ReturnType<typeof identifyTransaction>;
