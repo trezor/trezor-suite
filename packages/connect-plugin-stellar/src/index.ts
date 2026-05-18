a3c23d93366d002938342df47be4c288a0ec56aa
Marek Polak <marek.polak@satoshilabs.com>
2026-05-18 14:57:59 +0200

    chore(stellar): deprecate @trezor/connect-plugin-stellar

--- packages/connect-plugin-stellar/src/index.ts
+++ packages/connect-plugin-stellar/src/index.ts
@@ -1,1 +1,18 @@
-// TODO Scheduled for deletion
+type __TREZOR_CONNECT_PLUGIN_STELLAR_DEPRECATED__ = {
+    readonly __deprecated: '@trezor/connect-plugin-stellar is deprecated. See README.md for migration info';
+};
+
+/**
+ * @deprecated `@trezor/connect-plugin-stellar` is deprecated as of `@trezor/connect@10`.
+ */
+export const transformTransaction = (
+    _path?: unknown,
+    _transaction?: unknown,
+): __TREZOR_CONNECT_PLUGIN_STELLAR_DEPRECATED__ => {
+    throw new Error(
+        '@trezor/connect-plugin-stellar is deprecated. See README.md for migration info.',
+    );
+};
+
+// eslint-disable-next-line import/no-default-export
+export default transformTransaction;
