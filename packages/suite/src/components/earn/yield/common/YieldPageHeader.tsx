12ab3589e059f1c380699881f2692a03aa0ca47b
Jaroslav Hrách <me@jaroslavhrach.cz>
2026-05-20 16:29:32 +0200

    fix(suite): show account type badge in yield claim account rows
    (cherry picked from commit 5e45dec62cf33f97c9beb13c55cad148ad83e752)

--- packages/suite/src/components/earn/yield/common/YieldPageHeader.tsx
+++ packages/suite/src/components/earn/yield/common/YieldPageHeader.tsx
@@ -113,6 +113,8 @@ export const YieldPageHeader = ({ analyticsStep, account, routeParams }: YieldPa
                             {account && (
                                 <AccountLabel
                                     account={account}
+                                    showAccountTypeBadge
+                                    accountTypeBadgeSize="small"
                                     intent="neutral"
                                     priority="secondary"
                                     typographyStyle="body-sm"
