4ff11469028a7bbffad36e740e391c9ffb5a1b89
Jaroslav Hrách <me@jaroslavhrach.cz>
2026-05-20 17:47:17 +0200

    fix(suite): validate earn yield route params and stabilize claim loading
    (cherry picked from commit 004211ef84515714eadc18d6183d16035791ddbb)

--- packages/suite/src/components/earn/yield/claim/YieldClaim.tsx
+++ packages/suite/src/components/earn/yield/claim/YieldClaim.tsx
@@ -49,9 +49,10 @@ export const YieldClaim = ({ account }: YieldClaimProps) => {
     const isClaiming = isClaimSubmitting || !!claimSession.action.pendingTransaction;
     const isDeviceConnected = !!device?.connected && device.available;
 
-    const { merkleRewardsQuery } = useMerkleRewards(account);
+    const { merkleRewardsQuery, missingRateTickersQuery } = useMerkleRewards(account);
     const accountRewards: YieldAccountRewards | undefined =
         merkleRewardsQuery.data?.accountsRewards[0];
+    const isRewardsLoading = merkleRewardsQuery.isLoading || missingRateTickersQuery.isFetching;
 
     useEffect(() => {
         dispatch(stablecoinYieldActions.initSession({ flowType: 'claim', flowKey }));
@@ -153,7 +154,7 @@ export const YieldClaim = ({ account }: YieldClaimProps) => {
 
                                 <YieldRewardsList
                                     accountRewards={accountRewards}
-                                    isLoading={merkleRewardsQuery.isLoading}
+                                    isLoading={isRewardsLoading}
                                 />
                             </Column>
                         </Card>
@@ -174,9 +175,7 @@ export const YieldClaim = ({ account }: YieldClaimProps) => {
                             size="large"
                             width="100%"
                             isDisabled={
-                                merkleRewardsQuery.isLoading ||
-                                !accountRewards?.rewards.length ||
-                                isClaiming
+                                isRewardsLoading || !accountRewards?.rewards.length || isClaiming
                             }
                             isLoading={isClaimSubmitting}
                             onClick={handleClaim}
