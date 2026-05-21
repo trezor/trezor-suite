cfc5bc31de89ffabdbd7eca895df880398049bcf
Marek Polak <marek.polak@satoshilabs.com>
2026-05-21 14:46:03 +0200

    chore(xrpl): add code to @trezor/coins-xrpl

--- coins/coins-xrpl/src/runtime/exports.ts
+++ coins/coins-xrpl/src/runtime/exports.ts
@@ -1,1 +1,11 @@
-export {};
+import { Client, type ClientOptions, type ErrorResponse, XrplError } from 'xrpl';
+
+export { xrpToDrops, TimeoutError } from 'xrpl';
+
+export const getXrplApi = (server: string, options: ClientOptions) => new Client(server, options);
+
+export const asXrplError = (error: unknown) => {
+    if (error instanceof XrplError) {
+        return error as XrplError & { data: ErrorResponse | undefined };
+    }
+};
