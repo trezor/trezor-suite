0d7ef4af367e198d5cd689b2622d17de6829c379
karliatto <yo@karliatto.com>
2026-05-22 16:59:53 +0200

    feat(connect-cli): add nostr methods
    Nostr methods nostr-get-public-key and nostr-sign-event can be called from connect-cli like yarn workspace @trezor/connect-cli cli --udp  --method=nostr-get-public-key --params={__experimental:true} or yarn workspace @trezor/connect-cli cli --udp  --method=nostr-sign-event --params={__experimental:true}

--- packages/connect-cli/src/index.ts
+++ packages/connect-cli/src/index.ts
@@ -142,6 +142,26 @@ const runTestCase = async (device: Device) => {
                 allowDebugKeys: true,
             });
             break;
+        case 'nostr-get-public-key':
+            result = await TrezorConnect.nostrGetPublicKey({
+                device,
+                __experimental: true,
+                path: "m/44'/1237'/0'/0/0",
+                ...params,
+            });
+            break;
+        case 'nostr-sign-event':
+            result = await TrezorConnect.nostrSignEvent({
+                device,
+                __experimental: true,
+                path: "m/44'/1237'/0'/0/0",
+                created_at: Math.floor(Date.now() / 1000),
+                kind: 1,
+                tags: [],
+                content: 'Hello from @trezor/connect-cli',
+                ...params,
+            });
+            break;
         default:
             result = await TrezorConnect.getAddress({ device, path: "m/44'/0'/0'/0/0", ...params });
     }
