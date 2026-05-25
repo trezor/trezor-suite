3f47a695294b92f4e599a422916e3198ebe0be77
Jiří Čermák <jiri.cermak@satoshilabs.com>
2026-05-25 16:56:20 +0200

    fix: resolve yield flow

--- packages/suite/src/components/earn/yield/hooks/useYieldPendingTransactionTracking.ts
+++ packages/suite/src/components/earn/yield/hooks/useYieldPendingTransactionTracking.ts
@@ -263,7 +263,7 @@ export const useYieldPendingTransactionTracking = ({
             const completeAction = () => {
                 dispatch(
                     stablecoinYieldActions.completeAction({
-                        flowType: 'claim',
+                        flowType,
                         flowKey,
                         amount: pendingTransaction.amount,
                     }),
