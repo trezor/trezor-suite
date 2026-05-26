cc87a438d999a38c9dcfd443273ee51d6cea0e7e
Marek Polak <marek.polak@satoshilabs.com>
2026-05-26 13:50:39 +0200

    chore(suite): remove unused updateFee action

--- suite-common/wallet-core/src/fees/feesActions.ts
+++ suite-common/wallet-core/src/fees/feesActions.ts
@@ -1,17 +1,8 @@
 import { createAction } from '@reduxjs/toolkit';
 
-import { type NetworkSymbol } from '@suite-common/wallet-config';
-import { type FeeInfo, type FeesState, type FeesStatus } from '@suite-common/wallet-types';
+import { type FeesState } from '@suite-common/wallet-types';
 
 export const FEES_MODULE_PREFIX = '@common/wallet-core/fees';
-
-const updateFee = createAction(
-    `${FEES_MODULE_PREFIX}/updateFee`,
-    // note: status is handled automatically by reducer cases for `updateFeeInfoThunk`
-    (payload: { symbol: NetworkSymbol; data: FeeInfo; status?: FeesStatus }) => ({
-        payload,
-    }),
-);
 
 const updateMultipleFees = createAction(
     `${FEES_MODULE_PREFIX}/updateMultipleFees`,
@@ -20,7 +11,4 @@ const updateMultipleFees = createAction(
     }),
 );
 
-export const feesActions = {
-    updateFee,
-    updateMultipleFees,
-};
+export const feesActions = { updateMultipleFees };
