f9ea8aaa1258e5d074ccc93eb8e05fa329bc45d5
Matus Balascak <balascakmatus@gmail.com>
2026-05-28 15:09:12 +0200

    fix(suite-common): skip generic fee estimation for tron

--- suite-common/wallet-core/src/fees/feesUtils.ts
+++ suite-common/wallet-core/src/fees/feesUtils.ts
@@ -42,6 +42,11 @@ export const getNewFeeInfo = async ({
     device,
 }: GetNewFeeInfoProps): Promise<BlockchainEstimatedFeeLevel | undefined> => {
     const { symbol } = network;
+
+    if (network.networkType === 'tron') {
+        return;
+    }
+
     if (network.networkType === 'ethereum') {
         const result = await TrezorConnect.blockchainEstimateFee({
             coin: symbol,
