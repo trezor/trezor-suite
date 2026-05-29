ee5b345868a32284456b9846bb8b991ccd88483a
yanas <yanas@yanas.cz>
2026-05-29 09:25:50 +0200

    feat(suite-native): unify network settings screen for all firmware types

--- suite-native/coin-enabling/src/index.ts
+++ suite-native/coin-enabling/src/index.ts
@@ -1,4 +1,3 @@
-export * from './components/BtcOnlyCoinEnablingContent';
 export * from './components/CoinEnablingForm';
 export * from './components/DiscoveryCoinsFilter';
 export * from './screens/CoinEnablingInitScreen';
