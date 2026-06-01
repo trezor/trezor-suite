efdfbd41b4569fc4d1b01d3a6c9760c8ae032b4c
Martin Šmídl <smidl.martin1@gmail.com>
2026-06-01 08:19:00 +0200

    feat(suite-native): replace Dimensions with useWindowDimensions to not crash app in review screen, adjust skeleton height based on current content impl

--- suite-native/trading-atoms/src/components/Skeleton/SkeletonLarge.tsx
+++ suite-native/trading-atoms/src/components/Skeleton/SkeletonLarge.tsx
@@ -1,17 +1,19 @@
-import { Dimensions } from 'react-native';
+import { useWindowDimensions } from 'react-native';
 
 import { BoxSkeleton } from '@suite-native/atoms';
 
 export type SkeletonLargeProps = {
     widthPercentage: number;
+    height?: number;
 };
 
 const SKELETON_LARGE_HEIGHT = 60;
 
-export const SkeletonLarge = ({ widthPercentage }: SkeletonLargeProps) => (
-    <BoxSkeleton
-        width={Dimensions.get('window').width * widthPercentage}
-        height={SKELETON_LARGE_HEIGHT}
-        borderRadius="r16"
-    />
-);
+export const SkeletonLarge = ({
+    widthPercentage,
+    height = SKELETON_LARGE_HEIGHT,
+}: SkeletonLargeProps) => {
+    const { width } = useWindowDimensions();
+
+    return <BoxSkeleton width={width * widthPercentage} height={height} borderRadius="r16" />;
+};
