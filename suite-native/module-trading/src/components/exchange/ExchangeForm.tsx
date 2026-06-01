1053db4abeaf6692a1b31f3075b79522475cfeed
Martin Šmídl <smidl.martin1@gmail.com>
2026-06-01 10:28:11 +0200

    feat(suite-native): extract exchange pickers to remove gap when no content is displayed

--- suite-native/module-trading/src/components/exchange/ExchangeForm.tsx
+++ suite-native/module-trading/src/components/exchange/ExchangeForm.tsx
@@ -1,22 +1,14 @@
 import { memo } from 'react';
-import { Platform } from 'react-native';
-import {
-    FadeInUp,
-    FadeOutUp,
-    LinearTransition,
-    StretchInY,
-    StretchOutY,
-} from 'react-native-reanimated';
+import { LinearTransition } from 'react-native-reanimated';
 
-import { AnimatedBox, Card, VStack } from '@suite-native/atoms';
+import { AnimatedBox, VStack } from '@suite-native/atoms';
 import { AmountEditingDoneButton } from '@suite-native/trading-atoms';
 
 import { ExchangeAlert } from './ExchangeAlert';
 import { ExchangeCard } from './ExchangeCard';
 import { ExchangeConfirmation } from './ExchangeConfirmation';
 import { ExchangeFormQuoteDebugView } from './ExchangeFormQuoteDebugView';
-import { ExchangeRateAndProviderPicker } from './ExchangeRateAndProviderPicker';
-import { ExchangeReceiveAccountPicker } from './receive/ExchangeReceiveAccountPicker';
+import { ExchangePickersCard } from './ExchangePickersCard';
 import { useExchangeFormContext } from '../../hooks/exchange/useExchangeFormContext';
 import { useExchangeQuotes } from '../../hooks/exchange/useExchangeQuotes';
 import { useFocusedValueWatch } from '../../hooks/general/useFocusedValueWatch';
@@ -28,9 +20,6 @@ type ExchangeFormMemoizedProps = {
 const EXCHANGE_FORM_TEST_ID = '@trading/exchange/form';
 const AMOUNT_EDITING_DONE_BUTTON_TEST_ID = '@trading/exchange/amount-editing-done-button';
 
-const cardEnteringAnimation = Platform.OS === 'android' ? StretchInY : FadeInUp;
-const cardExitingAnimation = Platform.OS === 'android' ? StretchOutY : FadeOutUp;
-
 const ExchangeFormMemoized = memo(({ isAmountInputActive }: ExchangeFormMemoizedProps) => (
     <AnimatedBox layout={LinearTransition}>
         <VStack spacing="sp16" testID={EXCHANGE_FORM_TEST_ID}>
@@ -41,16 +30,7 @@ const ExchangeFormMemoized = memo(({ isAmountInputActive }: ExchangeFormMemoized
                 <AmountEditingDoneButton testID={AMOUNT_EDITING_DONE_BUTTON_TEST_ID} />
             ) : (
                 <>
-                    <AnimatedBox
-                        layout={LinearTransition}
-                        entering={cardEnteringAnimation}
-                        exiting={cardExitingAnimation}
-                    >
-                        <Card noPadding>
-                            <ExchangeReceiveAccountPicker />
-                            <ExchangeRateAndProviderPicker />
-                        </Card>
-                    </AnimatedBox>
+                    <ExchangePickersCard />
                     <ExchangeConfirmation />
                 </>
             )}
