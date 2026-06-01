46d8eb7a07a8fe3c41590bbb6458be81ce28ab08
Ondrej Hajek <ondrej.hajek@satoshilabs.com>
2026-06-01 12:52:09 +0200

    fix(e2e): Fixed bridge in db migration tests

--- packages/trezor-user-env-link/src/api.ts
+++ packages/trezor-user-env-link/src/api.ts
@@ -82,7 +82,7 @@ export interface ReadAndConfirmAtomicShamirMnemonicEmu {
     threshold: number;
 }
 
-type StartBridgeVersion = 'node-bridge';
+type StartBridgeVersion = 'node-bridge' | '2.0.33' | '2.0.32';
 
 export const MNEMONICS = {
     mnemonic_all: 'all all all all all all all all all all all all',
