3a0cc26c679fe29e7792713a95b32ac4a599c77f
Mattew Husak <matej.husak@icloud.com>
2026-06-01 13:23:06 +0200

    refactor(suite-common): rename shouldDisplayExportBip329Labels to shouldDisplayExportImportBip329Labels

--- suite-common/bip329/src/bip329Utils.ts
+++ suite-common/bip329/src/bip329Utils.ts
@@ -1,14 +1,14 @@
 import { type Account } from '@suite-common/wallet-types';
 
-type ShouldDisplayExportBip329LabelsParams = {
+type ShouldDisplayExportImportBip329LabelsParams = {
     account: Account | null;
     isSuiteSyncEnabled: boolean;
     isMetadataEnabled?: boolean;
 };
 
-export const shouldDisplayExportBip329Labels = ({
+export const shouldDisplayExportImportBip329Labels = ({
     account,
     isSuiteSyncEnabled,
     isMetadataEnabled = false,
-}: ShouldDisplayExportBip329LabelsParams): boolean =>
+}: ShouldDisplayExportImportBip329LabelsParams): boolean =>
     account?.networkType === 'bitcoin' && (isSuiteSyncEnabled || isMetadataEnabled);
