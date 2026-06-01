99c077395c39b0d414be364f493e65abe5448e56
Martin Šmídl <smidl.martin1@gmail.com>
2026-06-01 08:29:38 +0200

    feat(suite-native): remove re-renders on changing tabs by adding new selector, make the header full width with gradient on right side

--- suite-native/trading-debug/src/components/TradingEnvironmentWarning.tsx
+++ suite-native/trading-debug/src/components/TradingEnvironmentWarning.tsx
@@ -1,6 +1,6 @@
 import { useSelector } from 'react-redux';
 
-import { InlineAlertBox } from '@suite-native/atoms';
+import { InlineAlertBox, VStack } from '@suite-native/atoms';
 import { selectTradingEnvironment } from '@suite-native/trading-state';
 
 export const TradingEnvironmentWarning = () => {
@@ -11,6 +11,11 @@ export const TradingEnvironmentWarning = () => {
     }
 
     return (
-        <InlineAlertBox title={`Trading environment: ${tradingEnvironment}`} variant="warning" />
+        <VStack paddingHorizontal="sp16">
+            <InlineAlertBox
+                title={`Trading environment: ${tradingEnvironment}`}
+                variant="warning"
+            />
+        </VStack>
     );
 };
