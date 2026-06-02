76cb2db32109a18a95e3d59ef7489c90660c3114
Marek Polak <marek.polak@satoshilabs.com>
2026-06-02 17:28:28 +0200

    chore(utils): extend isHex util

--- packages/utils/src/isHex.ts
+++ packages/utils/src/isHex.ts
@@ -1,5 +1,19 @@
-export const isHex = (str: string): boolean => {
-    const regExp = /^(0x|0X)?[0-9A-Fa-f]+$/g;
+type Options = { prefix?: 'required' | 'optional' | 'prohibited'; allowEmpty?: boolean };
 
-    return regExp.test(str);
-};
+export function isHex(
+    value: unknown,
+    options?: Options & { prefix?: 'required' },
+): value is `0x${string}`;
+export function isHex(
+    value: unknown,
+    options: Options & { prefix: 'optional' | 'prohibited' },
+): value is string;
+
+export function isHex(value: unknown, { prefix = 'required', allowEmpty = true }: Options = {}) {
+    const p = prefix === 'prohibited' ? '' : '(0x)';
+    const o = prefix === 'optional' ? '?' : '';
+    const e = allowEmpty ? '*' : '+';
+    const regex = `^${p}${o}[0-9a-fA-F]${e}$`;
+
+    return typeof value === 'string' && new RegExp(regex).test(value);
+}
