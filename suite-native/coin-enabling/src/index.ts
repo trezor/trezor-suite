c356220ea56da6257fce8153e9d065113696c96f
yanas <yanas@yanas.cz>
2026-06-03 20:51:32 +0200

    feat(suite-native): use network icons on the add account screen

--- suite-native/coin-enabling/src/index.ts
+++ suite-native/coin-enabling/src/index.ts
@@ -1,3 +1,4 @@
 export * from './components/CoinEnablingForm';
 export * from './components/DiscoveryCoinsFilter';
+export * from './components/NetworkListItem';
 export * from './screens/CoinEnablingInitScreen';
