b548fcc408fc08530223875c969d450bbe140b1f
Jirka Bažant <jiri.bazant@satoshilabs.com>
2026-06-05 09:56:48 +0200

    chore(suite-native): Refactor format callback to separate `useFormatCryptoValue` hook

---
+++ suite-native/module-trading/src/hooks/general/useFormatCryptoValue.ts
@@ -0,0 +1,41 @@
+import { useCallback } from 'react';
+
+import type { CryptoId } from 'invity-api';
+
+import { useFormatters } from '@suite-common/formatters';
+import { useTradingUtils } from '@suite-common/trading';
+import { type NetworkSymbol, getNetworkDecimals } from '@suite-common/wallet-config';
+
+const TOKEN_DECIMALS_LENGTH = 16;
+
+export const useFormatCryptoValue = () => {
+    const { CryptoAmountFormatter } = useFormatters();
+    const { cryptoIdToCoinSymbol } = useTradingUtils();
+
+    return useCallback(
+        (
+            value: string | undefined,
+            cryptoId: CryptoId | undefined,
+            smallestUnitsOverride?: boolean,
+        ): string | undefined => {
+            if (value === undefined || cryptoId === undefined) {
+                return undefined;
+            }
+
+            const coinSymbol = cryptoIdToCoinSymbol(cryptoId);
+            if (!coinSymbol) {
+                return undefined;
+            }
+            const networkDecimals = coinSymbol ? getNetworkDecimals(coinSymbol) : undefined;
+
+            return CryptoAmountFormatter.format(value, {
+                maxDisplayedDecimals: networkDecimals ?? TOKEN_DECIMALS_LENGTH,
+                isBalance: true,
+                symbol: coinSymbol as NetworkSymbol,
+                isEllipsisAppended: false,
+                smallestUnitsOverride,
+            });
+        },
+        [cryptoIdToCoinSymbol, CryptoAmountFormatter],
+    );
+};
