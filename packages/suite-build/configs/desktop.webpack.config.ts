9571d293315e7063a4cee2a7d800984b1389b06b
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-06-08 22:08:31 +0200

    chore(suite-desktop): bump electron packages

--- packages/suite-build/configs/desktop.webpack.config.ts
+++ packages/suite-build/configs/desktop.webpack.config.ts
@@ -33,8 +33,8 @@ const messageSystemMockFile = path.join(
 );
 
 const config: webpack.Configuration = {
-    // Electron 41 runs on Chromium 146 https://www.electronjs.org/blog/electron-41-0
-    target: 'browserslist:Chrome >= 146',
+    // Electron 42 runs on Chromium 148 https://www.electronjs.org/blog/electron-42-0
+    target: 'browserslist:Chrome >= 148',
     entry: { main: [path.join(baseDirUI, 'src', 'index.tsx')] },
     output: {
         path: path.join(baseDir, 'build'),
