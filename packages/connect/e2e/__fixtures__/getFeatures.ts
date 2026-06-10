3aa50dfdb7e4b9f652bc848156c832d58737d792
Szymon Lesisz <sz.lesisz@gmail.com>
2026-06-10 12:26:17 +0200

    test(connect): adjust `getFeatures` T1B1 fixtures

--- packages/connect/e2e/__fixtures__/getFeatures.ts
+++ packages/connect/e2e/__fixtures__/getFeatures.ts
@@ -1,27 +1,4 @@
-// firmware should be always set. This tests actually tests the fact that
-
 import { DeviceModelInternal } from '@trezor/device-utils';
-
-const emulatorStartOpts = process.env.EMULATOR_START_OPTS
-    ? JSON.parse(process.env.EMULATOR_START_OPTS)
-    : {};
-const firmware = emulatorStartOpts.version;
-
-let major;
-let minor;
-let patch;
-
-if (firmware) {
-    [major, minor, patch] = firmware.split('.');
-}
-
-// if custom build is used, we ignore firmware version numbers
-const customFirmwareBuild =
-    !firmware ||
-    process.env.TESTS_CUSTOM_FIRMWARE_BUILD ||
-    process.env.TESTS_FIRMWARE?.includes('master') ||
-    // integration tests in trezor-firmware repo use 2.99.99 version
-    process.env.TESTS_FIRMWARE === '2.99.99';
 
 const baseFeatures = {
     device_id: expect.any(String),
@@ -30,6 +7,7 @@ const baseFeatures = {
     minor_version: expect.any(Number),
     patch_version: expect.any(Number),
     bootloader_mode: null,
+    bootloader_hash: null,
     pin_protection: expect.any(Boolean),
     passphrase_protection: expect.any(Boolean),
     language: 'en-US',
@@ -38,12 +16,35 @@ const baseFeatures = {
     revision: expect.any(String),
     imported: null,
     unlocked: expect.any(Boolean),
+    // flags: expect.any(Number), // flags may be changed by applyFlags test
     firmware_present: null,
     fw_major: null,
     fw_minor: null,
     fw_patch: null,
+    fw_vendor: expect.any(String),
     unfinished_backup: expect.any(Boolean),
     no_backup: expect.any(Boolean),
+    backup_availability: expect.any(String),
+    wipe_code_protection: false,
+    // session_id: expect.any(String), // in all except of T3W1
+    safety_checks: expect.any(String),
+    auto_lock_delay_ms: expect.any(Number),
+};
+
+const baseT1B1Features = {
+    ...baseFeatures,
+    bootloader_hash: expect.any(String), // difference between T2T1
+    imported: true, // difference between T2T1
+    sd_card_present: null, // no sdcard in T1B1
+    sd_protection: null, // no sdcard in T1B1
+    passphrase_always_on_device: null, // no support
+    display_rotation: null, // no support
+    experimental_features: null, // no support
+    model: '1',
+    internal_model: DeviceModelInternal.T1B1,
+    backup_type: null, // no support
+    recovery_status: null, // no support
+    recovery_type: null, // no support
 };
 
 const tests = [
@@ -53,12 +54,8 @@ const tests = [
         params: {},
         result: {
             ...baseFeatures,
-            bootloader_hash: null,
-            backup_availability: expect.any(String),
             model: expect.any(String), // "T" | "R"
             internal_model: expect.any(String),
-            unfinished_backup: expect.any(Boolean),
-            no_backup: expect.any(Boolean),
             recovery_status: 'Nothing',
             capabilities: expect.arrayContaining([
                 'Capability_Bitcoin',
@@ -79,12 +76,8 @@ const tests = [
             backup_type: 'Bip39',
             sd_card_present: expect.any(Boolean),
             sd_protection: false,
-            wipe_code_protection: false,
             passphrase_always_on_device: false,
             flags: expect.any(Number),
-            fw_vendor: expect.any(String),
-            safety_checks: expect.any(String),
-            auto_lock_delay_ms: expect.any(Number),
             display_rotation: 'North',
             experimental_features: expect.any(Boolean),
         },
@@ -103,33 +96,8 @@ const tests = [
         skip: ['2'],
         params: {},
         result: {
-            device_id: expect.any(String),
-            vendor: 'trezor.io',
-            major_version: customFirmwareBuild ? expect.any(Number) : Number(major),
-            minor_version: customFirmwareBuild ? expect.any(Number) : Number(minor),
-            patch_version: customFirmwareBuild ? expect.any(Number) : Number(patch),
-            bootloader_mode: null,
-            pin_protection: expect.any(Boolean),
-            passphrase_protection: expect.any(Boolean),
-            language: 'en-US',
-            label: expect.any(String),
-            initialized: true,
-            revision: expect.any(String),
-            bootloader_hash: expect.any(String), // difference between T2T1
-            imported: true, // difference between T2T1
-            unlocked: expect.any(Boolean),
-            firmware_present: null,
-            backup_availability: expect.any(String),
-            // flags: null, // flags may be changed by applyFlags test
-            model: '1',
-            internal_model: DeviceModelInternal.T1B1,
-            fw_major: null,
-            fw_minor: null,
-            fw_patch: null,
-            // fw_vendor: null, // "EMULATOR" since 1.11.1
-            unfinished_backup: expect.any(Boolean),
-            no_backup: expect.any(Boolean),
-            recovery_status: 'Nothing', // difference between T2T1
+            ...baseT1B1Features,
+
             capabilities: expect.arrayContaining([
                 'Capability_Bitcoin',
                 'Capability_Bitcoin_like',
@@ -138,26 +106,23 @@ const tests = [
                 'Capability_Stellar',
                 'Capability_U2F',
             ]),
-            recovery_type: undefined, // difference between T2T1
-            sd_card_present: null, // no sdcard in T1B1
-            sd_protection: null, // no sdcard in T1B1
-            wipe_code_protection: false,
-            session_id: expect.any(String),
-            passphrase_always_on_device: null,
-            flags: expect.any(Number),
-            fw_vendor: expect.any(String),
-            safety_checks: expect.any(String),
-            auto_lock_delay_ms: expect.any(Number),
-            display_rotation: 'North',
-            experimental_features: expect.any(Boolean),
-            backup_type: 'Bip39',
         },
         legacyResults: [
+            {
+                rules: ['<1.11.1'],
+                success: true,
+                payload: {
+                    ...baseT1B1Features,
+                    fw_vendor: null,
+                },
+            },
             {
                 rules: ['<1.10.3'],
                 success: true,
                 payload: {
-                    ...baseFeatures,
+                    ...baseT1B1Features,
+                    fw_vendor: null,
+                    safety_checks: null,
                 },
             },
         ],
