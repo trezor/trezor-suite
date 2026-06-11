1b62faad57abddf6a0fa8ec83ae35b7df882236f
Vojtěch Tranta <vojta.tranta@gmail.com>
2026-06-11 14:58:12 +0200

    fixup! feat(suite): drive HomescreenGallery applySettings through connect-flow

--- packages/suite/src/hooks/suite/useConnect.ts
+++ packages/suite/src/hooks/suite/useConnect.ts
@@ -1,8 +1,10 @@
 import { useCallback, useMemo } from 'react';
 
-import { type UiEventAction } from '@suite-common/wallet-core';
+import { type UiEventAction, defaultTrezorUIEventHandlerThunk } from '@suite-common/wallet-core';
 import TrezorConnect from '@trezor/connect';
 import { createConnect } from '@trezor/connect-flow';
+
+import { useDispatch } from './useDispatch';
 
 type Connect = ReturnType<typeof createConnect>;
 
@@ -14,13 +16,17 @@ export const useConnect = (): {
     connect: Connect;
     run: (proc: DrivableProcess) => Promise<void>;
 } => {
+    const dispatch = useDispatch();
     const connect = useMemo(() => createConnect({ trezorConnect: TrezorConnect }), []);
 
-    const run = useCallback(async (proc: DrivableProcess) => {
-        for await (const subprocess of proc.run()) {
-            void subprocess;
-        }
-    }, []);
+    const run = useCallback(
+        async (proc: DrivableProcess) => {
+            for await (const subprocess of proc.run()) {
+                dispatch(defaultTrezorUIEventHandlerThunk(subprocess.originalEvent));
+            }
+        },
+        [dispatch],
+    );
 
     return { connect, run };
 };
