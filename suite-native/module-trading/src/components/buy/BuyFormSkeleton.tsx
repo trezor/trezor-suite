e578cd76fadbca2cbd4068ad828faa0486017da1
Jirka Bažant <jiri.bazant@satoshilabs.com>
2026-06-11 10:16:36 +0200

    feat(suite-native): Buy form: move receive account picker to second section

--- suite-native/module-trading/src/components/buy/BuyFormSkeleton.tsx
+++ suite-native/module-trading/src/components/buy/BuyFormSkeleton.tsx
@@ -1,18 +1,27 @@
+import { useSelector } from 'react-redux';
+
 import { Card, VStack } from '@suite-native/atoms';
 import { SkeletonLargeRow, SkeletonSmall } from '@suite-native/trading-atoms';
+import { selectIsTradingResidenceCheckEnabled } from '@suite-native/trading-state';
 
-export const BuyFormSkeleton = () => (
-    <VStack spacing="sp16">
-        <Card>
-            <VStack>
-                <SkeletonSmall widthPercentage={0.2} />
-                <SkeletonLargeRow leftWidthPercentage={0.3} rightWidthPercentage={0.35} />
-                <SkeletonSmall widthPercentage={0.25} />
-                <SkeletonLargeRow leftWidthPercentage={0.4} rightWidthPercentage={0.3} />
-            </VStack>
-        </Card>
-        <Card>
-            <SkeletonLargeRow leftWidthPercentage={0.35} rightWidthPercentage={0.35} />
-        </Card>
-    </VStack>
-);
+export const BuyFormSkeleton = () => {
+    const isTradingResidenceCheckEnabled = useSelector(selectIsTradingResidenceCheckEnabled);
+
+    return (
+        <VStack spacing="sp16">
+            <Card>
+                <VStack>
+                    <SkeletonSmall widthPercentage={0.2} />
+                    <SkeletonLargeRow leftWidthPercentage={0.3} rightWidthPercentage={0.35} />
+                    <SkeletonSmall widthPercentage={0.25} />
+                    <SkeletonLargeRow leftWidthPercentage={0.4} rightWidthPercentage={0.3} />
+                </VStack>
+            </Card>
+            {!isTradingResidenceCheckEnabled && (
+                <Card>
+                    <SkeletonLargeRow leftWidthPercentage={0.35} rightWidthPercentage={0.35} />
+                </Card>
+            )}
+        </VStack>
+    );
+};
