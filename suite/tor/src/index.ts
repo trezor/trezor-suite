3f1c2729e90b9f9e289bc95b1368346418e76e45
Martin Varmuza <mvarmuza@gmail.com>
2026-06-11 15:51:14 +0200

    chore(suite): remove dead tor selectors and unexport torSlice
    Remove unused selectTorStatus/selectTorBootstrap selectors and unexport intra-file TorState and torSlice in suite/tor.
    
    Part of the dead-code elimination sweep tracked in #28463.

--- suite/tor/src/index.ts
+++ suite/tor/src/index.ts
@@ -1,9 +1,4 @@
-export { type TorBootstrap, type TorState, TorStatus } from './torSlice';
+export { type TorBootstrap, TorStatus } from './torSlice';
 export { getIsTorDomain, getIsTorEnabled, getIsTorLoading, isOnionUrl } from './torUtils';
-export {
-    selectIsTorEnabled,
-    selectTorBootstrap,
-    selectTorState,
-    selectTorStatus,
-} from './torSelectors';
-export { type TorRootState, torActions, torReducer, torSlice } from './torSlice';
+export { selectIsTorEnabled, selectTorState } from './torSelectors';
+export { type TorRootState, torActions, torReducer } from './torSlice';
