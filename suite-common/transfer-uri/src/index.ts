f7ab3c79c763442e76ec2c63c61c2085417a66ea
Mattew Husak <matej.husak@icloud.com>
2026-06-12 00:01:37 +0200

    refactor(transfer-uri): add parseTransferUri facade over format parsers

--- suite-common/transfer-uri/src/index.ts
+++ suite-common/transfer-uri/src/index.ts
@@ -1,4 +1,11 @@
 export { parseTransferUri } from './parseTransferUri';
-export type { TransferUriInfo, TransferUriError, TransferUriResult } from './parseTransferUri';
+export { parseBip321Uri } from './parseBip321Uri';
 export { parseErc681TransferUri } from './parseErc681TransferUri';
 export type { Erc681TransferInfo } from './parseErc681TransferUri';
+export type {
+    BipTransferUriInfo,
+    ErcTransferUriInfo,
+    TransferUriInfo,
+    TransferUriError,
+    TransferUriResult,
+} from './transferUtils';
