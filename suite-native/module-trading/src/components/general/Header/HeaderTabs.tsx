8cc2e5654e3b6c75473b5c6cdff6ecacb0a2d522
Jirka Bažant <jiri.bazant@satoshilabs.com>
2026-06-12 13:30:04 +0200

    feat(suite-native): Trade Remove slippage settings from

--- suite-native/module-trading/src/components/general/Header/HeaderTabs.tsx
+++ suite-native/module-trading/src/components/general/Header/HeaderTabs.tsx
@@ -1,14 +1,12 @@
-import { useEffect, useMemo, useRef, useState } from 'react';
-import { type NativeScrollEvent, type NativeSyntheticEvent } from 'react-native';
+import { useEffect, useMemo, useRef } from 'react';
 import { FlatList } from 'react-native-gesture-handler';
-import { FadeIn, FadeOut } from 'react-native-reanimated';
 
 import { LinearGradient } from 'expo-linear-gradient';
 
 import { useServices } from '@suite-common/dependency-injection';
 import { type TradingTypeWithConcierge } from '@suite-common/trading';
 import { events, selectNativeAnalyticsDep } from '@suite-native/analytics';
-import { AnimatedBox, HStack, IconButton, useBottomSheetModal } from '@suite-native/atoms';
+import { HStack } from '@suite-native/atoms';
 import { type IconName } from '@suite-native/icons';
 import { useTranslate } from '@suite-native/intl';
 import { prepareNativeStyle, useNativeStyles } from '@trezor/styles-native';
@@ -16,7 +14,6 @@ import { hexToRgba } from '@trezor/utils';
 
 import { HeaderTab } from './HeaderTab';
 import { useTradingTabs } from '../../../hooks/general/useTradingTabs';
-import { AdvancedSettingsSheet } from '../../settings/AdvancedSettingsSheet';
 
 const useTabsData = () => {
     const { translate } = useTranslate();
@@ -58,25 +55,36 @@ const tabsStyle = prepareNativeStyle(({ spacings }) => ({
     paddingHorizontal: spacings.sp16,
 }));
 
-const tabsEdgeGradientStyle = prepareNativeStyle(({ spacings }) => ({
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    bottom: 0,
-    width: spacings.sp40,
-    pointerEvents: 'none',
-}));
+const tabsEdgeGradientStyle = prepareNativeStyle<{ position: 'left' | 'right' }>(
+    ({ spacings }, { position }) => ({
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        width: spacings.sp20,
+        pointerEvents: 'none',
+        extend: [
+            {
+                condition: position === 'left',
+                style: {
+                    left: 0,
+                },
+            },
+            {
+                condition: position === 'right',
+                style: {
+                    right: 0,
+                },
+            },
+        ],
+    }),
+);
 
 export const HeaderTabs = () => {
     const listRef = useRef<FlatList>(null);
     const { applyStyle, utils } = useNativeStyles();
     const { activeTab, setActiveTab } = useTradingTabs();
     const data = useTabsData();
-    const { translate } = useTranslate();
-    const { bottomSheetRef, openModal, closeModal } = useBottomSheetModal();
     const { analytics } = useServices(selectNativeAnalyticsDep);
-
-    const [isGradientEnabled, setIsGradientEnabled] = useState(true);
 
     const tabsEdgeGradientColors = useMemo<[string, string, string, string]>(() => {
         const backgroundColor = utils.colors.surfaceFillPage;
@@ -106,19 +114,6 @@ export const HeaderTabs = () => {
         });
     }, [activeTabIndex]);
 
-    const handleScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-        const { contentOffset, layoutMeasurement, contentSize } = event.nativeEvent;
-
-        const { x } = contentOffset;
-        const { width: visibleWidth } = layoutMeasurement;
-        const { width: totalWidth } = contentSize;
-
-        const threshold = 8;
-        const isEnabled = x + visibleWidth < totalWidth - threshold;
-
-        setIsGradientEnabled(isEnabled);
-    };
-
     const onTabPress = (tab: TradingTypeWithConcierge) => {
         if (tab === activeTab) {
             return;
@@ -137,13 +132,12 @@ export const HeaderTabs = () => {
 
     return (
         <>
-            <HStack spacing={0} paddingRight="sp16">
+            <HStack spacing={0}>
                 <FlatList
                     ref={listRef}
                     horizontal
                     showsHorizontalScrollIndicator={false}
                     accessible={true}
-                    onScroll={handleScroll}
                     contentContainerStyle={applyStyle(tabsStyle)}
                     renderItem={({ item }) => (
                         <HeaderTab
@@ -163,27 +157,21 @@ export const HeaderTabs = () => {
                         });
                     }}
                 />
-                {isGradientEnabled && (
-                    <AnimatedBox entering={FadeIn.duration(200)} exiting={FadeOut}>
-                        <LinearGradient
-                            start={{ x: 0, y: 0.5 }}
-                            end={{ x: 1, y: 0.5 }}
-                            colors={tabsEdgeGradientColors}
-                            locations={[0, 0.45, 0.8, 1]}
-                            style={applyStyle(tabsEdgeGradientStyle)}
-                        />
-                    </AnimatedBox>
-                )}
-                <IconButton
-                    iconName="gear"
-                    size="medium"
-                    intent="neutral"
-                    priority="secondary"
-                    accessibilityLabel={translate('moduleTrading.tradingScreen.tabs.settings')}
-                    onPress={openModal}
+                <LinearGradient
+                    start={{ x: 1, y: 0.5 }}
+                    end={{ x: 0, y: 0.5 }}
+                    colors={tabsEdgeGradientColors}
+                    locations={[0, 0.45, 0.8, 1]}
+                    style={applyStyle(tabsEdgeGradientStyle, { position: 'left' })}
+                />
+                <LinearGradient
+                    start={{ x: 0, y: 0.5 }}
+                    end={{ x: 1, y: 0.5 }}
+                    colors={tabsEdgeGradientColors}
+                    locations={[0, 0.45, 0.8, 1]}
+                    style={applyStyle(tabsEdgeGradientStyle, { position: 'right' })}
                 />
             </HStack>
-            <AdvancedSettingsSheet ref={bottomSheetRef} closeModal={closeModal} />
         </>
     );
 };
