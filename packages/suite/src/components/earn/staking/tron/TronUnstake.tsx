a3f45c1579269c83b9f432a8bd4549f017aa1a93
Matus Balascak <balascakmatus@gmail.com>
2026-06-12 14:11:59 +0200

    feat(suite): add Tron unstake success screen

--- packages/suite/src/components/earn/staking/tron/TronUnstake.tsx
+++ packages/suite/src/components/earn/staking/tron/TronUnstake.tsx
@@ -1,7 +1,10 @@
+import { Translation } from '@suite/intl';
 import { type Account } from '@suite-common/wallet-types';
 import { Column } from '@trezor/components';
 
 import { TronStakeContext } from './TronStakeContext';
+import { TronStakeComplete } from './complete/TronStakeComplete';
+import { TronUnstakeSummaryCard } from './complete/TronUnstakeSummaryCard';
 import { useTronStakeFlow } from './hooks/useTronStakeFlow';
 import { TronUnstakeForm } from './unstake/TronUnstakeForm';
 
@@ -11,12 +14,22 @@ interface TronUnstakeProps {
 
 export const TronUnstake = ({ account }: TronUnstakeProps) => {
     const context = useTronStakeFlow({ account, flow: 'unstake' });
+    const { step } = context.actions;
 
     return (
         <TronStakeContext.Provider value={context}>
             <Column alignItems="center">
                 <Column gap={24} width="100%" maxWidth={500}>
-                    <TronUnstakeForm />
+                    {step === 'complete' ? (
+                        <TronStakeComplete
+                            heading={<Translation id="TR_EARN_TRON_UNSTAKE_COMPLETE" />}
+                            description={<Translation id="TR_EARN_TRON_UNSTAKE_DESCRIPTION" />}
+                        >
+                            <TronUnstakeSummaryCard />
+                        </TronStakeComplete>
+                    ) : (
+                        <TronUnstakeForm />
+                    )}
                 </Column>
             </Column>
         </TronStakeContext.Provider>
