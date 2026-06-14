60d91de2e34fa7f2913536baaecd24e0696d4d85
Mattew Husak <matej.husak@icloud.com>
2026-06-15 01:49:49 +0200

    refactor(suite-native): extract useShowSuiteSyncEnabledToast hook

--- suite-native/module-settings/src/components/ToggleSuiteSyncCard.tsx
+++ suite-native/module-settings/src/components/ToggleSuiteSyncCard.tsx
@@ -14,14 +14,13 @@ import { events as nativeEvents, selectNativeAnalyticsDep } from '@suite-native/
 import { TouchableSwitchRow } from '@suite-native/atoms';
 import { Translation } from '@suite-native/intl';
 import { StorageContext } from '@suite-native/storage';
-import { useSuiteSyncErrorHandler } from '@suite-native/suite-sync';
-import { useToast } from '@suite-native/toasts';
+import { useShowSuiteSyncEnabledToast, useSuiteSyncErrorHandler } from '@suite-native/suite-sync';
 
 export const ToggleSuiteSyncCard = () => {
     const { analytics } = useServices(selectNativeAnalyticsDep);
     const storageContext = useContext(StorageContext);
     const { showAlert } = useAlert();
-    const { showToast } = useToast();
+    const { showSuiteSyncEnabledToast } = useShowSuiteSyncEnabledToast();
 
     const { turnOffSuiteSync, turnOnSuiteSync } = useServices(
         selectTurnOffSuiteSyncDep,
@@ -69,11 +68,7 @@ export const ToggleSuiteSyncCard = () => {
             });
 
             if (result.success) {
-                showToast({
-                    intent: 'neutral',
-                    icon: 'check',
-                    message: <Translation id="suiteSync.enabledToast" />,
-                });
+                showSuiteSyncEnabledToast();
             } else {
                 handleSuiteSyncError(result.error);
             }
