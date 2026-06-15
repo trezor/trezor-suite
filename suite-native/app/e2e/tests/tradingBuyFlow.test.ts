ca917a89d244602c7903cb5f73239f1226131791
Ondrej Hajek <ondrej.hajek@satoshilabs.com>
2026-06-15 16:12:45 +0200

    fix(e2e): Fixed suite native trading buy flow test

--- suite-native/app/e2e/tests/tradingBuyFlow.test.ts
+++ suite-native/app/e2e/tests/tradingBuyFlow.test.ts
@@ -24,8 +24,8 @@ describe('Trade Buy [@noDevice]', () => {
         await tradingBuyActions.selectReceiveAsset('BTC', undefined, 'Bitcoin');
         await tradingBuyActions.selectBtcReceiveAccount('BTC SegWit', "m/84'/0'/0'/0/0");
         await tradingBuyActions.selectFiatCurrency('PLN');
-        await tradingBuyActions.selectCountry('Polan', 'Poland', 'POL');
         await tradingBuyActions.setFiatAmount('100');
+        await tradingBuyActions.selectCountry('Polan', 'Poland', 'POL');
 
         await tradingBuyActions.viewHowTradingWorks();
         await tradingBuyActions.viewPaymentMethods();
