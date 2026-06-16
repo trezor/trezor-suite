241160af9dbaf599a4ba918889515c6df776fe88
Marek Polak <marek.polak@satoshilabs.com>
2026-06-16 10:56:40 +0200

    refactor(connect): move solana method definitions to corresponding folder

--- packages/connect-common/src/types/api/solanaSignTransaction.ts
+++
@@ -1,6 +0,0 @@
-import type { Params, Response } from '../params';
-import type { SolanaSignTransaction, SolanaSignedTransaction } from './solana';
-
-export declare function solanaSignTransaction(
-    params: Params<SolanaSignTransaction>,
-): Response<SolanaSignedTransaction>;
