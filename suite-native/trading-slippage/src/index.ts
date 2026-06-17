bb5e9114d5fb3338f442c8335f8436056aef00ad
Jirka Bažant <jiri.bazant@satoshilabs.com>
2026-06-17 09:42:35 +0200

    chore(suite-native): Mobile Swap: Refactor slippage logic to trading-slippage module

---
+++ suite-native/trading-slippage/src/index.ts
@@ -0,0 +1,4 @@
+export { SlippageBottomSheet } from './components/SlippageBottomSheet';
+export { SlippagePicker, SLIPPAGE_PICKER_TEST_ID } from './components/SlippagePicker';
+export { SlippageSummary } from './components/SlippageSummary';
+export { useSlippageForm } from './hooks/useSlippageForm';
