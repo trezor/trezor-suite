93c22ca0a4347c33fb41ef7a2db6e47d6c34129a
Jan Václavík <jvaclavik@gmail.com>
2026-06-18 15:55:00 +0200

    feat: Add flags editor in debug

--- packages/suite/src/views/settings/SettingsDebug/PreField.tsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-const Pre = styled.pre`
-    line-break: anywhere;
-    white-space: break-spaces;
-`;
-
-export const PreField = ({ children }: { children: string }) => <Pre>{children}</Pre>;
