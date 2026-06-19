4363a24e6654fc9cf5ef60386e1da0154e663468
sherpaPSX <sherpa.psx@gmail.com>
2026-06-19 13:05:58 +0200

    chore(deps): bump @types/invity-api to 1.1.19

--- suite-common/trading/src/thunks/common/__tests__/loadInitialDataThunk.test.ts
+++ suite-common/trading/src/thunks/common/__tests__/loadInitialDataThunk.test.ts
@@ -86,6 +86,7 @@ const testUpdatedInfoData = async (type: 'outdated' | 'account-changed') => {
         Promise.resolve({
             coins: {},
             platforms: {},
+            config: {},
         });
 
     const getCurrentAccountDescriptorMock = jest.spyOn(invityAPI, 'getCurrentAccountDescriptor');
