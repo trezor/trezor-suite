580a454e4acb758c7e7c74dac339cd589a9f8b2e
Jirka Bažant <jiri.bazant@satoshilabs.com>
2026-06-20 20:50:35 +0200

    chore(suite-native): Mobile DEX: remove selectTradingMaxSlippagePercentage usage

--- suite-native/trading-quote-utils/src/hooks/useReceiveAmountMultiplier.ts
+++ suite-native/trading-quote-utils/src/hooks/useReceiveAmountMultiplier.ts
@@ -1,18 +1,23 @@
 import { useCallback } from 'react';
 import { useSelector } from 'react-redux';
 
-import { selectTradingMaxSlippagePercentage } from '@suite-common/trading';
+import {
+    TRADING_SETTINGS_MAX_SLIPPAGE_PERCENTAGE_DEFAULT,
+    selectTradingExchangeSelectedQuoteSwapSlippage,
+} from '@suite-common/trading';
 import { BigNumber } from '@trezor/utils';
 
 export const useReceiveAmountMultiplier = () => {
-    const maxSlippage = useSelector(selectTradingMaxSlippagePercentage);
+    const swapSlippage =
+        useSelector(selectTradingExchangeSelectedQuoteSwapSlippage) ??
+        TRADING_SETTINGS_MAX_SLIPPAGE_PERCENTAGE_DEFAULT;
 
     return useCallback(
         (amount: string) => {
-            const receiveAmountMultiplier = (100 - Number(maxSlippage)) / 100;
+            const receiveAmountMultiplier = (100 - Number(swapSlippage)) / 100;
 
             return BigNumber(amount).multipliedBy(receiveAmountMultiplier).toString();
         },
-        [maxSlippage],
+        [swapSlippage],
     );
 };
