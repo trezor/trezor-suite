10360084442970709968815bc283696429ba4aae
tomasklim <tomas.klim@gmail.com>
2026-06-22 11:51:51 +0200

    fix(suite): do not show address verification error toast on modal close

--- suite/receive/src/showAddressThunk.ts
+++ suite/receive/src/showAddressThunk.ts
@@ -87,7 +87,11 @@ export const showAddressThunk =
                 // Special case: device no-backup permissions not granted
                 response.error.code === 'Method_PermissionsNotGranted' ||
                 // User action: address cancelled on device
-                response.error.code === 'Failure_ActionCancelled'
+                response.error.code === 'Failure_ActionCancelled' ||
+                // User action: receive modal closed, cancelling the request
+                response.error.code === 'Method_Cancel' ||
+                // User action: connect popup closed, interrupting the request
+                response.error.code === 'Method_Interrupted'
             )
                 return;
 
