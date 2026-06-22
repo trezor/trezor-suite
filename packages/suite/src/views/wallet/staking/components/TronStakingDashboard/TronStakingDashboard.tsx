bcb8001a1eede94df76623dac4c9861a01511fa1
Tomas Boda <tominoboda@gmail.com>
2026-06-22 16:22:13 +0200

    feat(suite): add Tron staking guide link

--- packages/suite/src/views/wallet/staking/components/TronStakingDashboard/TronStakingDashboard.tsx
+++ packages/suite/src/views/wallet/staking/components/TronStakingDashboard/TronStakingDashboard.tsx
@@ -3,6 +3,7 @@ import { type SelectedAccountLoaded } from '@suite-common/wallet-types';
 import { Column, Row, TextButton } from '@trezor/components';
 
 import { WalletLayout } from 'src/components/wallet';
+import { useGuideOpenNode } from 'src/hooks/guide';
 
 import { TronResourcesCard } from './TronResourcesCard/TronResourcesCard';
 import { TronStakedCard } from './TronStakedCard';
@@ -10,12 +11,15 @@ import { TronUnstakingCard } from './TronUnstakingCard';
 import { TronVotingRewardsCard } from './TronVotingRewardsCard';
 import { TronWithdrawReadyBanner } from './TronWithdrawReadyBanner';
 
+const TRON_STAKING_GUIDE_PATH = '/earn/staking/tron-trx-staking.md';
+
 interface TronStakingDashboardProps {
     selectedAccount: SelectedAccountLoaded;
 }
 
 export const TronStakingDashboard = ({ selectedAccount }: TronStakingDashboardProps) => {
     const { account } = selectedAccount;
+    const { openNodeById } = useGuideOpenNode();
 
     return (
         <WalletLayout title="TR_NAV_STAKING" account={selectedAccount}>
@@ -32,7 +36,12 @@ export const TronStakingDashboard = ({ selectedAccount }: TronStakingDashboardPr
                 <TronStakedCard account={account} />
                 <TronResourcesCard account={account} />
                 <Row justifyContent="center" padding={{ vertical: 20 }}>
-                    <TextButton size="small" iconLeft="question" isUnderlined>
+                    <TextButton
+                        size="small"
+                        iconLeft="question"
+                        isUnderlined
+                        onClick={() => openNodeById(TRON_STAKING_GUIDE_PATH)}
+                    >
                         <Translation id="TR_EARN_TRON_HOW_IT_WORKS" />
                     </TextButton>
                 </Row>
