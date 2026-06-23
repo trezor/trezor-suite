32962a6973440091ac92f66e768c161aea3b6a6e
Tomas Boda <tominoboda@gmail.com>
2026-06-23 13:41:35 +0200

    chore(suite): move Tron staking out of debug mode

--- packages/suite/src/views/earn/tron/EarnTronUnstake.tsx
+++ packages/suite/src/views/earn/tron/EarnTronUnstake.tsx
@@ -1,29 +1,12 @@
-import { useEffect } from 'react';
-
-import { selectIsDebugModeActive } from '@suite/debug';
-import { goto } from '@suite/router';
-
 import { TronStakePageHeader, TronUnstake } from 'src/components/earn';
 import { useEarnRouteAccount } from 'src/components/earn/utils/useEarnRouteAccount';
 import { AccountNotExists } from 'src/components/wallet/WalletLayout/AccountException/AccountNotExists';
-import { useDispatch, useLayout, useSelector } from 'src/hooks/suite';
+import { useLayout } from 'src/hooks/suite';
 
 export const EarnTronUnstake = () => {
-    const dispatch = useDispatch();
-    const isDebugModeActive = useSelector(selectIsDebugModeActive);
     const { account } = useEarnRouteAccount();
 
     useLayout('Earn', <TronStakePageHeader account={account} />);
-
-    useEffect(() => {
-        if (!isDebugModeActive) {
-            dispatch(goto({ routeName: 'suite-earn' }));
-        }
-    }, [isDebugModeActive, dispatch]);
-
-    if (!isDebugModeActive) {
-        return null;
-    }
 
     if (!account) {
         return <AccountNotExists />;
