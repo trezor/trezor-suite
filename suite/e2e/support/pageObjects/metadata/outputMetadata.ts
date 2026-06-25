5f25b406a121c02b28d40afcea17113a5946d3b1
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2026-06-25 15:43:47 +0200

    test(e2e): fix label tests
    - stabilize output lablel change
    - add missing trigger for obtaining keys in migration test

--- suite/e2e/support/pageObjects/metadata/outputMetadata.ts
+++ suite/e2e/support/pageObjects/metadata/outputMetadata.ts
@@ -21,6 +21,7 @@ export class OutputMetadata extends MetadataBase {
     async clickAddLabelButton(outputId: string, txNumber: number) {
         await this.page.resetMousePosition();
         await expect(this.outputLabel(outputId, txNumber)).toHaveText(/[A-Za-z]+/);
+        await this.outputLabel(outputId, txNumber).scrollIntoViewIfNeeded();
         await this.outputLabel(outputId, txNumber).hover();
         await this.page.waitForTimeout(500); // edit button is unstable without this wait
         await this.outputLabel(outputId, txNumber).getByTestId(this.editButtonId).click();
