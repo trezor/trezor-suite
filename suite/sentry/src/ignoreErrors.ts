61a83d2bbad18735d96a1ff4ba4d911778c60d9d
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-06-26 14:59:24 +0200

    chore(sentry): ignore unhandledrejection [object Object] events

--- suite/sentry/src/ignoreErrors.ts
+++ suite/sentry/src/ignoreErrors.ts
@@ -17,4 +17,7 @@ export const ignoreErrors = [
 
     // nodeJS deprecation errors
     /.*DEP0040.*punycode.*/, // used deep within tech stack, not much we can do about it atm
+
+    // browser-only Sentry global handler sends duplicated errors (in event.extra it wraps an error message that is also sent as a separate error event)
+    /.*unhandledrejection*object Object.*/,
 ] satisfies Options['ignoreErrors'];
