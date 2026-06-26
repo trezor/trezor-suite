1ef107cb09a918b19e0288a92557d2593200284f
yanas <yanas@yanas.cz>
2026-06-26 12:46:51 +0200

    refactor(suite): use server address example & validation from suite-common

--- packages/suite/src/hooks/settings/backends/index.ts
+++ packages/suite/src/hooks/settings/backends/index.ts
@@ -2,4 +2,3 @@ export { useDefaultUrls } from './useDefaultUrls';
 export { useBackendsForm } from './useBackendsForm';
 export { useCustomBackends } from './useCustomBackends';
 export { useBackendReconnection } from './useBackendReconnection';
-export type { BackendOption } from './useBackendsForm';
