e67bf241a1d948948b822cfb46bcd664459ae008
Tomas Boda <tominoboda@gmail.com>
2026-06-30 13:04:52 +0200

    fix(suite): redirect back to account staking detail on flow complete

--- packages/suite/src/components/earn/staking/tron/TronUnstake.tsx
+++ packages/suite/src/components/earn/staking/tron/TronUnstake.tsx
@@ -22,6 +22,7 @@ export const TronUnstake = ({ account }: TronUnstakeProps) => {
                 <Column gap={24} width="100%" maxWidth={500}>
                     {step === 'complete' ? (
                         <TronStakeComplete
+                            account={account}
                             heading={<Translation id="TR_EARN_TRON_UNSTAKE_COMPLETE" />}
                             description={<Translation id="TR_EARN_TRON_UNSTAKE_DESCRIPTION" />}
                         >
--- packages/suite/src/components/earn/staking/tron/TronWithdraw.tsx
+++ packages/suite/src/components/earn/staking/tron/TronWithdraw.tsx
@@ -22,6 +22,7 @@ export const TronWithdraw = ({ account }: TronWithdrawProps) => {
                 <Column gap={24} width="100%" maxWidth={500}>
                     {step === 'complete' ? (
                         <TronStakeComplete
+                            account={account}
                             heading={<Translation id="TR_EARN_TRON_WITHDRAW_COMPLETE" />}
                             description={<Translation id="TR_EARN_TRON_WITHDRAW_DESCRIPTION" />}
                         >
