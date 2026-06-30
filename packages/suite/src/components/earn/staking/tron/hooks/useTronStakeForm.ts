a1035b948c3f1d4bef861ed6ee8eef1d1222352e
Tomas Boda <tominoboda@gmail.com>
2026-06-30 16:18:18 +0200

    feat(suite): add currency switcher to amount input in tron freezing

--- packages/suite/src/components/earn/staking/tron/hooks/useTronStakeForm.ts
+++ packages/suite/src/components/earn/staking/tron/hooks/useTronStakeForm.ts
@@ -2,17 +2,9 @@ import { useForm } from 'react-hook-form';
 
 import { useTranslation } from '@suite/intl';
 import { isAddressValid } from '@suite-common/address';
-import { getNetwork } from '@suite-common/wallet-config';
 import { type TronFlow } from '@suite-common/wallet-core';
 import { type Account, type TronResourceType } from '@suite-common/wallet-types';
-import { getStakingLimitsByNetworkSymbol } from '@suite-common/wallet-utils';
 import { type FeeLevel } from '@trezor/connect';
-
-import {
-    validateDecimals,
-    validateMin,
-    validateReserveOrBalance,
-} from 'src/utils/suite/validation';
 
 import { getStakedBalance } from '../unstake/unstakeUtils';
 import { CUSTOM_REPRESENTATIVE } from '../vote/constants';
@@ -61,30 +53,6 @@ export const useTronStakeForm = ({ account, flow }: UseTronStakeFormProps) => {
         },
     });
 
-    const stakingLimits = getStakingLimitsByNetworkSymbol(account.symbol);
-    const minStakingAmount = stakingLimits?.MIN_AMOUNT_FOR_STAKING;
-
-    const { displaySymbol } = getNetwork(account.symbol);
-
-    const amountRules = {
-        required: translationString('AMOUNT_IS_NOT_SET'),
-        validate: {
-            min: validateMin(translationString),
-            minStakingAmount: (value: string) => {
-                if (value && minStakingAmount?.isGreaterThan(value)) {
-                    return translationString('TR_EARN_STAKING_DASHBOARD_MINIMUM_STAKE', {
-                        amount: minStakingAmount.toString(),
-                        displaySymbol,
-                    });
-                }
-            },
-            decimals: validateDecimals(translationString, {
-                decimals: getNetwork(account.symbol).decimals,
-            }),
-            reserveOrBalance: validateReserveOrBalance(translationString, { account }),
-        },
-    };
-
     const customRepresentativeRules = {
         validate: {
             validAddress: (value: string, { representative }: TronStakeFormValues) => {
@@ -102,7 +70,6 @@ export const useTronStakeForm = ({ account, flow }: UseTronStakeFormProps) => {
 
     return {
         methods,
-        amountRules,
         customRepresentativeRules,
     };
 };
