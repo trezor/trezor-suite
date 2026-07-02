5d3f362ce86dd8747299c0972a3ede8511d91d56
Jan Václavík <jvaclavik@gmail.com>
2026-07-02 08:20:19 +0200

    feat(suite): Update URL for feedback form

--- suite-common/analytics/src/events/index.ts
+++ suite-common/analytics/src/events/index.ts
@@ -1,4 +1,3 @@
-export { accountsInfoEvent } from './accountsInfoEvent';
 export { connectPopupCallEvent } from './connectPopupCallEvent';
 export { connectPopupErrorEvent } from './connectPopupErrorEvent';
 export { connectPopupInitEvent } from './connectPopupInitEvent';
@@ -6,28 +5,29 @@ export { connectPopupPermissionsEvent } from './connectPopupPermissionsEvent';
 export { deviceConnectionDeviceConfirmationEvent } from './deviceConnectionDeviceConfirmationEvent';
 export { deviceConnectionDeviceFoundEvent } from './deviceConnectionDeviceFoundEvent';
 export { deviceConnectionDevicePairedEvent } from './deviceConnectionDevicePairedEvent';
-export {
-    onboardingFeedbackBannerClickedEvent,
-    type OnboardingFeedbackBannerOrigin,
-} from './onboardingFeedbackBannerClickedEvent';
-export {
-    onboardingStepViewedEvent,
-    type DeviceOnboardingStepName,
-} from './onboardingStepViewedEvent';
-export {
-    promoNoDeviceEshopCtaEvent,
-    type NoDeviceEshopCtaOrigin,
-} from './promoNoDeviceEshopCtaEvent';
 export { settingsAppLogExportedEvent } from './settingsAppLogExportedEvent';
 export { settingsDeviceChangeLabelEvent } from './settingsDeviceChangeLabelEvent';
 export { settingsDeviceWipeEvent } from './settingsDeviceWipeEvent';
 export { settingsGeneralLabelingEvent } from './settingsGeneralLabelingEvent';
 export { settingsNetworkSearchUsedEvent } from './settingsNetworkSearchUsedEvent';
-export { suiteSyncLabelCreatedEvent } from './suiteSyncLabelCreatedEvent';
-export { walletBalanceEvent } from './walletBalanceEvent';
 export { walletConnectInitEvent } from './walletConnectInitEvent';
 export { walletConnectPairedEvent } from './walletConnectPairedEvent';
 export { walletConnectProposalApprovedEvent } from './walletConnectProposalApprovedEvent';
 export { walletConnectProposalEvent } from './walletConnectProposalEvent';
 export { walletConnectProposalRejectedEvent } from './walletConnectProposalRejectedEvent';
 export { walletConnectSessionRequestEvent } from './walletConnectSessionRequestEvent';
+export { accountsInfoEvent } from './accountsInfoEvent';
+export {
+    onboardingStepViewedEvent,
+    type DeviceOnboardingStepName,
+} from './onboardingStepViewedEvent';
+export {
+    onboardingFeedbackBannerClickedEvent,
+    type OnboardingFeedbackBannerOrigin,
+} from './onboardingFeedbackBannerClickedEvent';
+export { suiteSyncLabelCreatedEvent } from './suiteSyncLabelCreatedEvent';
+export { walletBalanceEvent } from './walletBalanceEvent';
+export {
+    promoNoDeviceEshopCtaEvent,
+    type NoDeviceEshopCtaOrigin,
+} from './promoNoDeviceEshopCtaEvent';
