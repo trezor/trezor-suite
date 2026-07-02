071a585bba3e1d135f547dedf463088467021fb5
yanas <yanas@yanas.cz>
2026-07-02 12:30:55 +0200

    feat(suite-native): report settings/testnet-networks-toggle analytics event

--- suite-native/module-settings/src/components/ToggleTestnetsCard.tsx
+++ suite-native/module-settings/src/components/ToggleTestnetsCard.tsx
@@ -1,15 +1,25 @@
 import { useDispatch, useSelector } from 'react-redux';
 
+import { events } from '@suite-common/analytics';
+import { useServices } from '@suite-common/dependency-injection';
+import { selectNativeAnalyticsDep } from '@suite-native/analytics';
 import { TouchableSwitchRow } from '@suite-native/atoms';
 import { Translation } from '@suite-native/intl';
 import { selectAreTestnetsEnabled, toggleAreTestnetsEnabled } from '@suite-native/settings';
 
 export const ToggleTestnetsCard = () => {
     const dispatch = useDispatch();
+    const { analytics } = useServices(selectNativeAnalyticsDep);
+
     const areTestnetsEnabled = useSelector(selectAreTestnetsEnabled);
 
-    const handleToggleTestnets = () => {
+    const handleToggleTestnets = (value: boolean) => {
         dispatch(toggleAreTestnetsEnabled());
+
+        analytics.report({
+            type: events.settingsTestnetNetworksToggleEvent.name,
+            payload: { enabled: value },
+        });
     };
 
     return (
