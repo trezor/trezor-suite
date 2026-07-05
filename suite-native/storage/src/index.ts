4673537369e6945cbaa3907401eda5c412cd6b11
tomasklim <tomas.klim@gmail.com>
2026-07-05 12:49:51 +0200

    feat(suite-native): sort persisted accounts by coin via migration

--- suite-native/storage/src/index.ts
+++ suite-native/storage/src/index.ts
@@ -9,6 +9,7 @@ export * from './atomWithUnecryptedStorage';
 
 export * from './migrations/account/v2';
 export * from './migrations/account/v3';
+export * from './migrations/account/v4';
 export * from './migrations/device/v2';
 export * from './migrations/device/v3';
 export * from './migrations/device/v4';
