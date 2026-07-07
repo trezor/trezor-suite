ce0708b616c42b807bab3177864d5332f33e2ad0
Tomas Boda <tominoboda@gmail.com>
2026-07-07 12:43:04 +0200

    feat(suite-native): move account settings to account assets screen

--- suite-native/module-accounts-management/src/components/AccountAssets/AccountAssetsScreenHeader.tsx
+++ suite-native/module-accounts-management/src/components/AccountAssets/AccountAssetsScreenHeader.tsx
@@ -1,5 +1,7 @@
 import { useSelector } from 'react-redux';
 
+import { useNavigation } from '@react-navigation/native';
+
 import { type AccountsRootState, selectAccountByKey } from '@suite-common/wallet-core';
 import { type AccountKey } from '@suite-common/wallet-types';
 import {
@@ -7,12 +9,24 @@ import {
     type NativeAccountsRootState,
     selectAccountFiatBalance,
 } from '@suite-native/accounts';
-import { HStack, VStack } from '@suite-native/atoms';
+import { HStack, IconButton, VStack } from '@suite-native/atoms';
 import { BaseCurrencyAmountFormatter } from '@suite-native/formatters';
 import { CryptoIcon } from '@suite-native/icons';
-import { ScreenHeader } from '@suite-native/navigation';
+import {
+    type AccountsStackParamList,
+    type RootStackParamList,
+    RootStackRoutes,
+    ScreenHeader,
+    type StackToStackCompositeNavigationProps,
+} from '@suite-native/navigation';
 
 import { type AccountAssetsFlow } from './types';
+
+type AccountDetailNavigationProps = StackToStackCompositeNavigationProps<
+    AccountsStackParamList,
+    RootStackRoutes.AccountDetail,
+    RootStackParamList
+>;
 
 type Props = { accountKey: AccountKey; flowType?: AccountAssetsFlow };
 
@@ -48,9 +62,29 @@ const AccountAssetsScreenHeaderContent = ({ accountKey }: Omit<Props, 'flowType'
     );
 };
 
-export const AccountAssetsScreenHeader = ({ accountKey, flowType }: Props) => (
-    <ScreenHeader
-        customContent={<AccountAssetsScreenHeaderContent accountKey={accountKey} />}
-        closeActionType={flowType === 'send' ? 'back' : 'close'}
-    />
-);
+export const AccountAssetsScreenHeader = ({ accountKey, flowType }: Props) => {
+    const navigation = useNavigation<AccountDetailNavigationProps>();
+
+    const handleSettingsNavigation = () => {
+        navigation.navigate(RootStackRoutes.AccountSettings, {
+            accountKey,
+        });
+    };
+
+    return (
+        <ScreenHeader
+            customContent={<AccountAssetsScreenHeaderContent accountKey={accountKey} />}
+            closeActionType={flowType === 'send' ? 'back' : 'close'}
+            rightIcon={
+                <IconButton
+                    intent="neutral"
+                    priority="secondary"
+                    size="medium"
+                    iconName="gear"
+                    onPress={handleSettingsNavigation}
+                    testID="@account-assets/settings-button"
+                />
+            }
+        />
+    );
+};
