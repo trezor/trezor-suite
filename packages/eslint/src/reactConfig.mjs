cb34d25e828d0c8400334d911c732633214aedc8
Marek Polak <marek.polak@satoshilabs.com>
2026-07-08 17:36:13 +0200

    chore(react-utils): change useAsyncMemo implementation

--- packages/eslint/src/reactConfig.mjs
+++ packages/eslint/src/reactConfig.mjs
@@ -29,7 +29,7 @@ export const reactConfig = [
         plugins: { 'react-hooks': pluginReactHooks },
         rules: {
             'react-hooks/rules-of-hooks': 'error',
-            'react-hooks/exhaustive-deps': 'error',
+            'react-hooks/exhaustive-deps': ['error', { additionalHooks: '(useAsyncMemo)' }],
             'react-hooks/static-components': 'off', // TODO fix & reenable
             'react-hooks/set-state-in-effect': 'off', // TODO fix & reenable, though this anti-pattern is unfortunately quite widespread
             'react-hooks/refs': 'off', // Too restrictive. Reading ref in render is often desired, though must be carefully considered
