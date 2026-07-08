945b4d1a99593a42359c99103cc794d70ed48d87
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-07-08 13:36:15 +0200

    chore(sentry): cleanup commentary on Sentry integrations

--- packages/suite-web/src/sentry.ts
+++ packages/suite-web/src/sentry.ts
@@ -4,6 +4,7 @@ import { SENTRY_BROWSER_CONFIG, getCommonBrowserIntegrations } from '@suite/sent
 
 const BROWSER_SENTRY_CONFIG: BrowserOptions = {
     ...SENTRY_BROWSER_CONFIG,
+    // Important: must be a function to keep default Sentry integrations; an array would mean ONLY those specific integrations.
     integrations: defaults => [
         ...defaults.filter(i => i.name !== 'BrowserSession'),
         ...getCommonBrowserIntegrations(),
