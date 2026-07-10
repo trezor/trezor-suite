c5c892304c095929ae4dd08f1e6b15b6761a31dc
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-07-10 13:10:14 +0200

    chore: bump foundation eslint deps

--- packages/connect-explorer-theme/src/components/highlight-matches.tsx
+++ packages/connect-explorer-theme/src/components/highlight-matches.tsx
@@ -8,40 +8,41 @@ type MatchArgs = {
     match: string;
 };
 
-export const HighlightMatches = memo<MatchArgs>(function HighlightMatches({
-    value,
-    match,
-}: MatchArgs): ReactElement | null {
-    if (!value) {
-        return null;
-    }
-    const splitText = value.split('');
-    const escapedSearch = escapeStringRegexp(match.trim());
-    const regexp = new RegExp(escapedSearch.replaceAll(/\s+/g, '|'), 'ig');
-    let result;
-    let index = 0;
-    const content: (string | ReactNode)[] = [];
-
-    while ((result = regexp.exec(value))) {
-        if (result.index === regexp.lastIndex) {
-            regexp.lastIndex++;
-        } else {
-            const before = splitText.splice(0, result.index - index).join('');
-            const after = splitText.splice(0, regexp.lastIndex - result.index).join('');
-            content.push(
-                before,
-                <span key={result.index} className="nx-text-primary-600">
-                    {after}
-                </span>,
-            );
-            index = regexp.lastIndex;
+export const HighlightMatches = memo<MatchArgs>(
+    ({ value, match }: MatchArgs): ReactElement | null => {
+        if (!value) {
+            return null;
         }
-    }
+        const splitText = value.split('');
+        const escapedSearch = escapeStringRegexp(match.trim());
+        const regexp = new RegExp(escapedSearch.replaceAll(/\s+/g, '|'), 'ig');
+        let result;
+        let index = 0;
+        const content: (string | ReactNode)[] = [];
 
-    return (
-        <>
-            {content}
-            {splitText.join('')}
-        </>
-    );
-});
+        while ((result = regexp.exec(value))) {
+            if (result.index === regexp.lastIndex) {
+                regexp.lastIndex++;
+            } else {
+                const before = splitText.splice(0, result.index - index).join('');
+                const after = splitText.splice(0, regexp.lastIndex - result.index).join('');
+                content.push(
+                    before,
+                    <span key={result.index} className="nx-text-primary-600">
+                        {after}
+                    </span>,
+                );
+                index = regexp.lastIndex;
+            }
+        }
+
+        return (
+            <>
+                {content}
+                {splitText.join('')}
+            </>
+        );
+    },
+);
+
+HighlightMatches.displayName = 'HighlightMatches';
