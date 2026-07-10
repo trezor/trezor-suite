27a2ceffdb5e5ce1ce33efe5b757e861119c85d0
STew790 <stepan.janda@satoshilabs.com>
2026-07-10 10:41:09 +0200

    test(e2e): add test for banner after onboarding

--- suite/e2e/tests/onboarding/t3w1/t3w1-create-wallet.test.ts
+++ suite/e2e/tests/onboarding/t3w1/t3w1-create-wallet.test.ts
@@ -64,11 +64,14 @@ test.describe('Onboarding - create wallet', { tag: ['@T3W1'] }, () => {
 
             await test.step('Finish wallet creation', async () => {
                 await onboardingPage.finalButton.click();
+                await expect(onboardingPage.onboardingFeedbackBanner).toBeVisible();
+                await onboardingPage.onboardingFeedbackBannerCTAButton.click();
                 await dashboardPage.discoveryEmptyPrimaryButton.click();
                 await assetsSection.enableNetworkViaActivateAssetsModal(['btc', 'eth']);
 
                 await expect(onboardingPage.suiteLoadedIndicator).toBeVisible({ timeout: 30_000 });
                 await expect(dashboardPage.walletReady).toBeVisible({ timeout: 30_000 });
+                await expect(onboardingPage.onboardingFeedbackBanner).toBeHidden();
             });
         },
     );
