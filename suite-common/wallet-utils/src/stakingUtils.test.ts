1ecf189f535e09648b89cc67a924c6c0ebc1da56
yasiuram <yasiura-github@everstake.one>
2026-07-12 09:17:36 +0200

    fix(suite-common): add shared getMaxStakeAmount reserve policy

--- suite-common/wallet-utils/src/stakingUtils.test.ts
+++ suite-common/wallet-utils/src/stakingUtils.test.ts
@@ -1,5 +1,8 @@
-import { getUnstakingPeriodInDaysFixture } from './__fixtures__/stakingUtils';
-import { getUnstakingPeriodInDays } from './stakingUtils';
+import {
+    getMaxStakeAmountFixture,
+    getUnstakingPeriodInDaysFixture,
+} from './__fixtures__/stakingUtils';
+import { getMaxStakeAmount, getUnstakingPeriodInDays } from './stakingUtils';
 
 describe('getUnstakingPeriodInDays', () => {
     getUnstakingPeriodInDaysFixture.forEach(test => {
@@ -7,6 +10,18 @@ describe('getUnstakingPeriodInDays', () => {
             const result = getUnstakingPeriodInDays(test.args.networkType, {
                 withdrawTime: test.args.withdrawTime,
                 exitTime: test.args.exitTime,
+            });
+            expect(result).toEqual(test.result);
+        });
+    });
+});
+
+describe('getMaxStakeAmount', () => {
+    getMaxStakeAmountFixture.forEach(test => {
+        it(test.description, () => {
+            const result = getMaxStakeAmount({
+                balance: test.args.balance,
+                symbol: test.args.symbol,
             });
             expect(result).toEqual(test.result);
         });
