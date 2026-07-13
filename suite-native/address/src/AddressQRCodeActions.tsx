1d56284a1bd075f788a4d58977de2e62ab136155
juriczech <bohdan.juricek@gmail.com>
2026-07-13 15:30:46 +0200

    feat(suite-native): redesign receive address layout

---
+++ suite-native/address/src/AddressQRCodeActions.tsx
@@ -0,0 +1,45 @@
+import { Alert, Share } from 'react-native';
+
+import { Button, VStack } from '@suite-native/atoms';
+import { useCopyToClipboard } from '@suite-native/clipboard';
+import { Translation, useTranslate } from '@suite-native/intl';
+
+type AddressQRCodeActionsProps = {
+    address: string;
+};
+
+export const AddressQRCodeActions = ({ address }: AddressQRCodeActionsProps) => {
+    const copyToClipboard = useCopyToClipboard();
+    const { translate } = useTranslate();
+
+    const handleCopyAddress = async () => {
+        await copyToClipboard(address, translate('qrCode.addressCopied'));
+    };
+
+    const handleShareData = async () => {
+        try {
+            await Share.share({
+                message: address,
+            });
+        } catch (error) {
+            Alert.alert('Something went wrong.', error.message);
+        }
+    };
+
+    return (
+        <VStack spacing="sp8">
+            <Button iconLeft="copy" onPress={handleCopyAddress} isFullWidth>
+                <Translation id="qrCode.copyButton" />
+            </Button>
+            <Button
+                iconLeft="shareNetwork"
+                intent="neutral"
+                priority="secondary"
+                onPress={handleShareData}
+                isFullWidth
+            >
+                <Translation id="qrCode.shareButton" />
+            </Button>
+        </VStack>
+    );
+};
