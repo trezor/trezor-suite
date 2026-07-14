bc666cc1f4e8763ab55f019195f2b3f6a8e07b63
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2026-07-14 14:19:07 +0200

    chore(docker-files): removes unused dockerfiles and scripts

--- docker/docker-regtest-electrum.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-LOCAL_USER_ID="$(id -u "$USER")"
-export LOCAL_USER_ID
-
-docker compose -f ./docker/docker-compose.regtest-electrum.yml up --build --abort-on-container-exit --force-recreate
