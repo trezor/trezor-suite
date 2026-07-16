b86b32f46fff10f930edbb234e52dae1b18d7632
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2026-07-16 15:45:53 +0200

    fix(e2e): replace ts-node/register with tsx/cjs in Jest reporter to fix ERR_REQUIRE_ESM

--- suite-native/app/e2e/support/reporter/index.js
+++ suite-native/app/e2e/support/reporter/index.js
@@ -1,3 +1,4 @@
 // JEST supports only commonJS modules https://jestjs.io/docs/code-transformation
-require('ts-node/register');
+// tsx/cjs is used instead of ts-node/register to handle ESM packages (e.g. @trezor/utils with "type": "module")
+require('tsx/cjs');
 module.exports = require('./gitHubReporter.ts');
