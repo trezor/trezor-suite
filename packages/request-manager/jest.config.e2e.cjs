a26b29574c674c5f91e686ad1370867626f66e6a
Szymon Lesisz <sz.lesisz@gmail.com>
2026-07-16 12:16:45 +0200

    test(request-manager): fix testing scripts

--- packages/request-manager/jest.config.e2e.cjs
+++ packages/request-manager/jest.config.e2e.cjs
@@ -2,5 +2,8 @@ const baseConfig = require('../../jest.config.base.swc');
 
 module.exports = {
     ...baseConfig,
+    maxWorkers: 1, // runInBand
+    verbose: true,
     testEnvironment: '../../JestCustomEnv.js',
+    testMatch: ['**/e2e/**/*.test.ts'],
 };
