29feeeae2c6ac0773d368fa8601796d21612d6db
Peter Sanderson <dierk.deidel@protonmail.com>
2026-07-16 12:53:29 +0200

    refactor(connect-data): shrink firmware asset declaration

--- packages/connect-data/src/index.ts
+++ packages/connect-data/src/index.ts
@@ -1,15 +1,2 @@
-import type { FirmwareRelease } from '@trezor/device-utils';
-
-import { firmwareAssets as _firmwareAssets } from './map-releases';
-
 export * from './firmware-jws';
-
-type FirmwareAssetsMap = {
-    [device: string]: {
-        [type: string]: {
-            [file: string]: FirmwareRelease;
-        };
-    };
-};
-
-export const firmwareAssets = _firmwareAssets as unknown as FirmwareAssetsMap;
+export { firmwareAssets } from './map-releases';
