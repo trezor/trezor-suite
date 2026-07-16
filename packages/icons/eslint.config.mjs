cb3be087ebd45ae1b7ad38dce3beb1fdf6c239c3
Peter Sanderson <dierk.deidel@protonmail.com>
2026-07-16 14:35:49 +0200

    refactor(icons): reduce generated type graph
    Refs #29904
    
    fix(icons): restore required lint script
    
    fix(eslint): allow ignored staged files
    
    chore(icons): drop unrelated generated change

--- packages/icons/eslint.config.mjs
+++ packages/icons/eslint.config.mjs
@@ -1,6 +1,9 @@
 import { eslint } from '@trezor/eslint';
 
 export default [
+    {
+        ignores: ['src/generated/icons/**'],
+    },
     ...eslint,
     {
         files: ['icon-index-template.mjs', 'svgr.web.config.mjs'],
