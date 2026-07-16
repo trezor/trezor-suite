8041924deb936a756ffd719f02a533624ab4da03
Peter Sanderson <dierk.deidel@protonmail.com>
2026-07-16 21:36:26 +0200

    refactor(suite): simplify router test state overrides

--- packages/type-utils/test/object.type-test.ts
+++ packages/type-utils/test/object.type-test.ts
@@ -1,4 +1,4 @@
-import { NullablePropsRecursive } from '../src';
+import { NullablePropsRecursive, type OptionalKey } from '../src';
 
 type X = {
     a: 'A';
@@ -21,7 +21,21 @@ const y4fail: Y = {};
 // @ts-expect-error This expects and error as b is missing
 const y3fail: Y = { a: null };
 
+type DiscriminatedUnion = { type: 'string'; value: string } | { type: 'number'; value: number };
+
+type PartialDiscriminatedUnion = OptionalKey<DiscriminatedUnion, keyof DiscriminatedUnion>;
+
+const nonDistributivePartial: PartialDiscriminatedUnion = {
+    type: 'string',
+    value: 1,
+};
+
+// @ts-expect-error The value remains constrained to the original property's value types.
+const invalidNonDistributivePartial: PartialDiscriminatedUnion = { value: false };
+
 void y1ok;
 void y2ok;
-void y4fail;
 void y3fail;
+void y4fail;
+void nonDistributivePartial;
+void invalidNonDistributivePartial;
