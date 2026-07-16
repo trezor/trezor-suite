94fc3e994f8537ec59ed33afa946a980f51b3852
Matus Balascak <balascakmatus@gmail.com>
2026-07-16 10:30:35 +0200

    fix(suite-native): fix vault token placeholder in yield benefits message
    (cherry picked from commit 19b99a5a9bb7708fd96c0ec81ea2c780bba0b7ed)

--- suite-native/intl/src/messages.ts
+++ suite-native/intl/src/messages.ts
@@ -2820,7 +2820,7 @@ export const messages = {
                         'Most rewards compound automatically—some must be claimed manually.',
                 },
                 third: {
-                    title: 'Deposit {tokenSymbol} to receive {vaultTokenName}',
+                    title: 'Deposit {tokenSymbol} to receive {vaultTokenSymbol}',
                     description: 'This is your vault position.',
                 },
                 fourth: {
