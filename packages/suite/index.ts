e1995f58964e18dd2ef43079c95420aeb780dd6d
Peter Sanderson <dierk.deidel@protonmail.com>
2026-07-17 13:05:30 +0200

    refactor(suite): remove test mock from package entry point

--- packages/suite/index.ts
+++ packages/suite/index.ts
@@ -1,5 +1,4 @@
 export { ConnectedThemeProvider } from './src/support/suite/ConnectedThemeProvider';
 export { ResponsiveContextProvider } from './src/support/suite/ResponsiveContext';
-export { extraDependenciesDesktopMock } from './src/support/tests/extraDependenciesDesktop.mock';
 export { initStore, type PreloadedState } from './src/reducers/store';
 export { type Store } from './src/types/suite';
