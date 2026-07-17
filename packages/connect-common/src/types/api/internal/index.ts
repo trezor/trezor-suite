8c5d5ce17ceba16ca3aaa27110f23d930a08ed66
Peter Sanderson <dierk.deidel@protonmail.com>
2026-07-17 22:30:25 +0200

    refactor(connect): shrink internal API declaration

--- packages/connect-common/src/types/api/internal/index.ts
+++ packages/connect-common/src/types/api/internal/index.ts
@@ -1,4 +1,4 @@
-import type { Static } from '@trezor/schema-utils';
+import type { Static, TUnsafe } from '@trezor/schema-utils';
 import { Type } from '@trezor/schema-utils';
 
 import type { off } from './off';
@@ -8,7 +8,15 @@ import type { uiResponse } from './uiResponse';
 import type { updateConnectSettings } from './updateConnectSettings';
 
 // Initialization, lifecycle, events, and settings
-export const TrezorConnectInternal = Type.Object({
+type TrezorConnectInternalSchema = {
+    on: typeof on;
+    off: typeof off;
+    removeAllListeners: typeof removeAllListeners;
+    uiResponse: typeof uiResponse;
+    updateConnectSettings: typeof updateConnectSettings;
+};
+
+export const TrezorConnectInternal: TUnsafe<TrezorConnectInternalSchema> = Type.Object({
     on: Type.Unsafe<typeof on>(),
     off: Type.Unsafe<typeof off>(),
     removeAllListeners: Type.Unsafe<typeof removeAllListeners>(),
