5ae51deba04ec5579ff52aaf654e64926eb6447c
Peter Sanderson <dierk.deidel@protonmail.com>
2026-07-19 18:30:58 +0200

    test(connect): avoid e2e package self-imports

--- packages/connect/e2e/common.setup.ts
+++ packages/connect/e2e/common.setup.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import TrezorConnect from '@trezor/connect';
 import { UI_REQUEST, UI_RESPONSE } from '@trezor/connect-common';
 import type { ApplySettings } from '@trezor/protobuf/src/definitions';
 import { BridgeTransport } from '@trezor/transport-common';
@@ -9,7 +7,7 @@ import { versionUtils } from '@trezor/utils';
 
 import { THP_CREDENTIALS_AUTOCONNECT } from './common-thp-credentials';
 
-// import TrezorConnect from '../src';
+import TrezorConnect from '../src';
 
 // Read emulator start options from EMULATOR_START_OPTS env var (JSON string set by run.ts).
 // In browser mode, Vite's `define` replaces process.env.EMULATOR_START_OPTS at build time.
--- packages/connect/e2e/tests/device/authenticateDevice.test.ts
+++ packages/connect/e2e/tests/device/authenticateDevice.test.ts
@@ -1,9 +1,8 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
-import TrezorConnect from '@trezor/connect';
 import type { DeviceAuthenticityConfig } from '@trezor/device-authenticity';
 import { deviceAuthenticityConfig } from '@trezor/device-authenticity';
 import { DeviceModelInternal } from '@trezor/device-utils';
 
+import TrezorConnect from '../../../src';
 import { conditionalTest, getController, initTrezorConnect, setup } from '../../common.setup';
 
 const controller = getController();
