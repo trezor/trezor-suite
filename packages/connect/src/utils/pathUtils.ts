c853571d1969a8628a01865b7e211aea30755ac8
Peter Sanderson <dierk.deidel@protonmail.com>
2026-07-20 14:52:56 +0200

    fix(packages): declare development type entries

--- packages/connect/src/utils/pathUtils.ts
+++ packages/connect/src/utils/pathUtils.ts
@@ -1,3 +1,3 @@
 // pathUtils moved to @trezor/connect-common so it can be shared without a deep import into
 // @trezor/connect (see #27376). Re-exported here to keep connect's internal call sites unchanged.
-export * from '@trezor/connect-common/src/utils/pathUtils';
+export * from '@trezor/connect-common/pathUtils';
