ec9dfb7994bd92fccf1fe9032c8fa629358ed6a7
yanas <yanas@yanas.cz>
2026-07-20 15:40:56 +0200

    feat(suite-native): swap firmware and device authenticity checks

--- suite-native/module-settings/src/screens/SettingsAdvancedScreen.tsx
+++ suite-native/module-settings/src/screens/SettingsAdvancedScreen.tsx
@@ -21,8 +21,8 @@ export const SettingsAdvancedScreen = () => {
             }
         >
             <VStack spacing="sp16">
-                <ToggleFirmwareAuthenticityCheckCard />
                 <ToggleDeviceAuthenticityCheckCard />
+                <ToggleFirmwareAuthenticityCheckCard />
                 <ToggleAddressDisplayCard />
                 <ToggleTestnetsCard />
                 {isNetworkReserveSettingsVisible && <ToggleNetworkReserveCheckCard />}
