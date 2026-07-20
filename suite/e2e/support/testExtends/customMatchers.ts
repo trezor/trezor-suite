37f3ae187c11a5d5df66c48d913680f34c0427a1
Matej Kriz <matej@matejkriz.cz>
2026-07-20 11:19:18 +0200

    fixup! test(e2e): fix custom matcher typing

--- suite/e2e/support/testExtends/customMatchers.ts
+++ suite/e2e/support/testExtends/customMatchers.ts
@@ -116,7 +116,7 @@ export const transformAddress = (address: string, lineFormat: LineFormats = 'fou
     }
 };
 
-export const expect: typeof baseExpect = baseExpect.extend({
+export const expect = baseExpect.extend({
     async toHaveTextGreaterThan(locator: Locator, expectedValue: number) {
         return await compareTextAndNumber(locator, expectedValue, (a, b) => a > b, 'greater');
     },
@@ -353,4 +353,4 @@ export const expect: typeof baseExpect = baseExpect.extend({
             pass: true,
         };
     },
-});
+}) satisfies typeof baseExpect;
