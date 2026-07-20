f675af203d37479dae00ed878f77cf010da038fc
Prochmi99 <152851523+MiroslavProchazka@users.noreply.github.com>
2026-07-20 11:27:10 +0200

    feat(blockchain-link): add Robinhood Chain support
    (cherry picked from commit 0e3a6962f2d92ae05f16f442360eda9c928a2627)

--- packages/blockchain-link/src/ui/config.ts
+++ packages/blockchain-link/src/ui/config.ts
@@ -222,6 +222,28 @@ export default [
     },
     {
         blockchain: {
+            name: 'Robinhood Chain',
+            worker: 'js/blockbook-worker.js',
+            server: ['https://rhc.trezor.io'],
+            debug: true,
+        },
+        data: {
+            address: '',
+            accountInfoOptions: {
+                page: 1,
+                pageSize: 25,
+                contractFilter: undefined,
+            },
+            estimateFeeOptions: {
+                blocks: [1, 2, 10],
+            },
+            txid: '',
+            tx: '',
+            subscribe: '',
+        },
+    },
+    {
+        blockchain: {
             name: 'Base',
             worker: 'js/blockbook-worker.js',
             server: ['https://base.trezor.io'],
