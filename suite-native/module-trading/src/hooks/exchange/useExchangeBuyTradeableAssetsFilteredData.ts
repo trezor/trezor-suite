cb75b7f19f4b3d64a2a10e4c1fb65a186884c031
Martin Šmídl <smidl.martin1@gmail.com>
2026-07-22 14:39:47 +0200

    fix(suite-native): fix same assets replacement in pickders, add missing dep

--- suite-native/module-trading/src/hooks/exchange/useExchangeBuyTradeableAssetsFilteredData.ts
+++ suite-native/module-trading/src/hooks/exchange/useExchangeBuyTradeableAssetsFilteredData.ts
@@ -1,20 +1,16 @@
 import { useSelector } from 'react-redux';
 
 import type { FeatureFlagsRootState } from '@suite-native/feature-flags';
-import { useWatch } from '@suite-native/forms';
 import {
     type TradingRootState,
     selectExchangeBuyTradeableAssets,
 } from '@suite-native/trading-state';
 
-import { useExchangeFormContext } from './useExchangeFormContext';
 import { useTradeableAssetsFilteredData } from '../general/useTradeableAssetsFilteredData';
 
 export const useExchangeBuyTradeableAssetsFilteredData = () => {
-    const { control } = useExchangeFormContext();
-    const sendAsset = useWatch({ control, name: 'sendAsset' });
     const assets = useSelector((state: TradingRootState & FeatureFlagsRootState) =>
-        selectExchangeBuyTradeableAssets(state, sendAsset?.cryptoId),
+        selectExchangeBuyTradeableAssets(state),
     );
 
     return useTradeableAssetsFilteredData({ assets });
