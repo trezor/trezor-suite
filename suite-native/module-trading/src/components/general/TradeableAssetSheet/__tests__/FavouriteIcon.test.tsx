88033441ef6ae8c7025d6a38bb130b4d2be275df
Jirka Bažant <jiri.bazant@satoshilabs.com>
2026-07-22 12:36:03 +0200

    fix(suite-native): fix broken tests after crowdin update

--- suite-native/module-trading/src/components/general/TradeableAssetSheet/__tests__/FavouriteIcon.test.tsx
+++ suite-native/module-trading/src/components/general/TradeableAssetSheet/__tests__/FavouriteIcon.test.tsx
@@ -1,3 +1,4 @@
+import { getTranslation } from '@suite-native/intl';
 import { fireEvent, renderWithBasicProvider } from '@suite-native/test-utils';
 
 import { FavouriteIcon } from '../FavouriteIcon';
@@ -7,14 +8,18 @@ describe('FavouriteIcon', () => {
         const { getByA11yHint } = renderWithBasicProvider(
             <FavouriteIcon isFavourite={true} onPress={jest.fn()} />,
         );
-        expect(getByA11yHint('Remove from favourites')).toBeTruthy();
+        expect(
+            getByA11yHint(getTranslation('moduleTrading.tradeableAssetsSheet.favouritesRemove')),
+        ).toBeTruthy();
     });
 
     it('should have correct hint when not marked as favourite', () => {
         const { getByA11yHint } = renderWithBasicProvider(
             <FavouriteIcon isFavourite={false} onPress={jest.fn()} />,
         );
-        expect(getByA11yHint('Add to favourites')).toBeTruthy();
+        expect(
+            getByA11yHint(getTranslation('moduleTrading.tradeableAssetsSheet.favouritesAdd')),
+        ).toBeTruthy();
     });
 
     it('should call onPress callback', () => {
@@ -23,7 +28,9 @@ describe('FavouriteIcon', () => {
             <FavouriteIcon isFavourite={false} onPress={pressSpy} />,
         );
 
-        const button = getByA11yHint('Add to favourites');
+        const button = getByA11yHint(
+            getTranslation('moduleTrading.tradeableAssetsSheet.favouritesAdd'),
+        );
         fireEvent.press(button);
 
         expect(pressSpy).toHaveBeenCalledWith();
