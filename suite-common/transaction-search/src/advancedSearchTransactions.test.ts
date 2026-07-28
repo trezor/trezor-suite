53826fbb58bc3154d5c3ac98f2ddd69f1299ec1d
Peter Sanderson <dierk.deidel@protonmail.com>
2026-07-28 16:53:38 +0200

    test(transaction-search): co-locate tests

--- suite-common/transaction-search/src/advancedSearchTransactions.test.ts
+++ suite-common/transaction-search/src/advancedSearchTransactions.test.ts
@@ -1,10 +1,10 @@
 import { type WalletAccountTransaction } from '@suite-common/wallet-types';
 import { typedObjectEntries } from '@trezor/utils';
 
-import { searchTransactionsFixture } from '../__fixtures__/searchTransactions.fixture';
-import stMock from '../__fixtures__/searchTransactions.json';
-import { advancedSearchTransactions } from '../advancedSearchTransactions';
-import { type SearchAccountLabels, type SearchOutputLabels } from '../searchLabels';
+import { searchTransactionsFixture } from './__fixtures__/searchTransactions.fixture';
+import stMock from './__fixtures__/searchTransactions.json';
+import { advancedSearchTransactions } from './advancedSearchTransactions';
+import { type SearchAccountLabels, type SearchOutputLabels } from './searchLabels';
 
 // Original Fixtures were create with legacy metadata structure,
 // so we need to transform them to fit the new SearchAccountLabels structure used in the tests
