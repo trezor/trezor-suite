a64f67ff2f3758614efcf0dfcb89c80e4ab5490b
Peter Sanderson <dierk.deidel@protonmail.com>
2026-07-29 14:12:37 +0200

    test(intl): co-locate translation tests

--- suite/intl/src/types.test.ts
+++ suite/intl/src/types.test.ts
@@ -1,5 +1,6 @@
-import { isTranslationKey } from '@suite/intl';
 import { SUITE_PRECOMPOSE_ERRORS } from '@suite-common/wallet-types';
+
+import { isTranslationKey } from './types';
 
 it.each(Object.values(SUITE_PRECOMPOSE_ERRORS))(
     'precomposed error %s should be translatable',
