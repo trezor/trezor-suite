0cfdf89261377ad953f5c314d3c73bfadf14cafa
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-07-29 16:59:15 +0200

    chore(suite): extract @suite/discovery

--- packages/suite/src/middlewares/wallet/index.ts
+++ packages/suite/src/middlewares/wallet/index.ts
@@ -1,6 +1,7 @@
 import type { MiddlewareAPI } from 'redux';
 
 import { coinjoinMiddleware } from '@suite/coinjoin';
+import { prepareDiscoveryMiddleware } from '@suite/discovery';
 import { prepareConnectPopupMiddleware } from '@suite-common/connect-popup';
 import type { ExtraDependencies } from '@suite-common/redux-utils';
 import { prepareSuiteSyncMiddleware } from '@suite-common/suite-sync';
@@ -13,7 +14,6 @@ import {
 } from '@suite-common/wallet-core';
 import { prepareWalletConnectMiddleware } from '@suite-common/walletconnect';
 
-import { prepareDiscoveryMiddleware } from './discoveryMiddleware';
 import graphMiddleware from './graphMiddleware';
 import { replaceByFeeErrorMiddleware } from './replaceByFeeErrorMiddleware';
 import storageMiddleware from './storageMiddleware';
