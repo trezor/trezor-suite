101cdac028ef2758f4661bde38282a3b317a0181
Peter Sanderson <dierk.deidel@protonmail.com>
2026-07-29 13:49:00 +0200

    test(utxo-lib): co-locate tests

---
+++ packages/utxo-lib/src/crypto.test.ts
@@ -0,0 +1,21 @@
+import { fixtures } from './__fixtures__/crypto';
+import * as bcrypto from './crypto';
+
+describe('crypto', () => {
+    const a = ['hash160', 'hash256', 'ripemd160', 'sha1', 'sha256'] as const;
+    a.forEach(algorithm => {
+        describe(algorithm, () => {
+            fixtures.forEach(f => {
+                const fn = bcrypto[algorithm];
+                const expected = f[algorithm];
+
+                it(`returns ${expected} for ${f.hex}`, () => {
+                    const data = Buffer.from(f.hex, 'hex');
+                    const actual = fn(data).toString('hex');
+
+                    expect(actual).toEqual(expected);
+                });
+            });
+        });
+    });
+});
