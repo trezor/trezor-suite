d056083e4bda13a1e1cdb132e9182d1f704e51ed
Marek Polak <marek.polak@satoshilabs.com>
2026-07-29 14:05:10 +0200

    chore(tor): memoize selectTorState selector

--- suite/tor/src/torSelectors.ts
+++ suite/tor/src/torSelectors.ts
@@ -1,20 +1,23 @@
+import { createWeakMapSelector } from '@suite-common/redux-utils';
+
 import { type TorRootState, TorStatus } from './torSlice';
 import { getIsTorEnabled, getIsTorLoading } from './torUtils';
+
+const createMemoizedSelector = createWeakMapSelector.withTypes<TorRootState>();
 
 export const selectIsTorEnabled = (state: TorRootState) =>
     state.tor.torStatus === TorStatus.Enabled || state.tor.torStatus === TorStatus.Slow;
 
-export const selectTorState = (state: TorRootState) => {
-    const { torStatus, torBootstrap } = state.tor;
-
-    return {
+export const selectTorState = createMemoizedSelector(
+    [(state: TorRootState) => state.tor],
+    ({ torStatus, torBootstrap }) => ({
         torStatus,
+        torBootstrap,
         isTorEnabled: getIsTorEnabled(torStatus),
         isTorLoading: getIsTorLoading(torStatus),
         isTorError: torStatus === TorStatus.Error,
         isTorDisabling: torStatus === TorStatus.Disabling,
         isTorDisabled: torStatus === TorStatus.Disabled,
         isTorEnabling: torStatus === TorStatus.Enabling,
-        torBootstrap,
-    };
-};
+    }),
+);
