76c01c558b066c8965c2e159dbc3b35383cb2e88
Peter Sanderson <dierk.deidel@protonmail.com>
2026-07-29 14:17:45 +0200

    test(modal): co-locate reducer tests

--- suite/modal/src/modalReducer.test.ts
+++ suite/modal/src/modalReducer.test.ts
@@ -1,15 +1,12 @@
-import { type State, modalReducer } from '@suite/modal';
-
-import { type Action } from 'src/types/suite';
-
-import fixtures from '../__fixtures__/modalReducer';
+import fixtures from './__fixtures__/modalReducer';
+import { type State, modalReducer } from './modalReducer';
 
 describe('modalReducer', () => {
     fixtures.forEach(f => {
         it(f.description, () => {
             let state: State = f.initialState as State;
             f.actions.forEach(a => {
-                state = modalReducer(state, a as Action);
+                state = modalReducer(state, a);
             });
             expect(state).toEqual(f.result);
         });
