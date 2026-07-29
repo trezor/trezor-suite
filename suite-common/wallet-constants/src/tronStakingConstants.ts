b19c8accea9e029078d323170c7e1769bcea19d3
Matus Balascak <balascakmatus@gmail.com>
2026-07-29 11:51:13 +0200

    fix(suite): keep a TRX reserve for the Tron voting fee
    (cherry picked from commit 07fae52faf1576c089e3274aacb63eb8ca9e0551)

--- suite-common/wallet-constants/src/tronStakingConstants.ts
+++ suite-common/wallet-constants/src/tronStakingConstants.ts
@@ -4,6 +4,7 @@ export const MIN_TRON_AMOUNT_FOR_STAKING = new BigNumber(1);
 export const MAX_TRON_AMOUNT_FOR_STAKING = new BigNumber(1_000_000_000);
 export const MIN_TRON_FOR_WITHDRAWALS = new BigNumber(0.01);
 export const MIN_TRON_BALANCE_FOR_FEE_BUFFER = new BigNumber(5);
+export const TRON_STAKING_RESERVE = new BigNumber(0.5);
 export const MIN_TRON_BALANCE_FOR_STAKING =
     MIN_TRON_AMOUNT_FOR_STAKING.plus(MIN_TRON_FOR_WITHDRAWALS);
 
