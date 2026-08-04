9410375aa10dbfe6b72f5f94931d174a749d1594
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2026-08-04 09:21:00 +0200

    test(suite): narrow db-migration tests to not be run on canary test runs

--- suite/e2e/tests/suite/db-migration.test.ts
+++ suite/e2e/tests/suite/db-migration.test.ts
@@ -11,10 +11,10 @@ const suiteDevInstance = 'https://dev.suite.sldev.cz/suite-web';
 test.describe(
     'Database migration',
     // This test is run only on web nightly builds, it works with web instances of 25.10 and develop branch
-    // On PR and release CI run it would provide no value and potentially false failures
+    // On PR and release CI run it would provide no value and potentially false failures, same goes for canary firmware runs
     // Note: Trezor user env doesn't support legacy bridge versions on macOs, which is needed to connect the device to the old Suite version. Use linux or only run in CI.
     // Additionally, 25.10 does not support T3W1 yet
-    { tag: ['@webOnly', '@nightlyOnly', '@T3T1'] },
+    { tag: ['@webOnly', '@nightlyOnly', '@T3T1', '@specificFirmware'] },
     () => {
         test.use({
             deviceSetup: { passphrase_protection: true, mnemonic: 'mnemonic_all' },
