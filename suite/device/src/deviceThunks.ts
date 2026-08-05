df7a89783650c959f216f89db0350144f5dfd5fc
Szymon Lesisz <sz.lesisz@gmail.com>
2026-08-05 15:30:26 +0200

    fix(suite): remove misleading code

--- suite/device/src/deviceThunks.ts
+++ suite/device/src/deviceThunks.ts
@@ -1,8 +1,7 @@
-import { selectRouterApp } from '@suite/router';
 import { selectSelectedDevice } from '@suite-common/device';
 import { createThunk } from '@suite-common/redux-utils';
 import type { TrezorDevice } from '@suite-common/suite-types';
-import { handleDeviceDisconnect, selectDeviceThunk } from '@suite-common/wallet-core';
+import { handleDeviceDisconnect } from '@suite-common/wallet-core';
 import type { Device } from '@trezor/connect';
 
 const DEVICE_MODULE_PREFIX = '@suite';
@@ -13,18 +12,6 @@ export const disconnectDeviceThunk = createThunk(
         const selectedDevice = selectSelectedDevice(getState());
         if (!selectedDevice) return;
         if (selectedDevice.path !== device.path) return;
-
-        const routerApp = selectRouterApp(getState());
-
-        /**
-         * Under normal circumstances, after device is disconnected we want suite to select another existing device (either remembered or physically connected)
-         * This is not the case in firmware update and onboarding; In this case we simply wan't suite.device to be empty until user reconnects a device again
-         */
-        if (['onboarding', 'firmware', 'firmware-type'].includes(routerApp)) {
-            dispatch(selectDeviceThunk({ device: undefined }));
-
-            return;
-        }
 
         dispatch(handleDeviceDisconnect(device));
     },
