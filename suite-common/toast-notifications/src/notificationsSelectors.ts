aad219420d9af94c21115a4ef5078c695a2f8821
Petr Knetl <p.knetlml@gmail.com>
2026-08-05 10:04:47 +0200

    refactor(toast-notifications): extract selectHasUnseenNotifications to shared selector

--- suite-common/toast-notifications/src/notificationsSelectors.ts
+++ suite-common/toast-notifications/src/notificationsSelectors.ts
@@ -1,10 +1,14 @@
 import { createWeakMapSelector, returnStableArrayIfEmpty } from '@suite-common/redux-utils';
 
+import { isTransactionNotification } from './notificationsUtils';
 import { type NotificationsRootState, type ToastPayload } from './types';
 
 const createMemoizedSelector = createWeakMapSelector.withTypes<NotificationsRootState>();
 
 export const selectNotifications = (state: NotificationsRootState) => state.notifications;
+
+export const selectHasUnseenNotifications = (state: NotificationsRootState): boolean =>
+    state.notifications.some(n => !n.seen && isTransactionNotification(n));
 
 export const selectVisibleNotificationsByType = createMemoizedSelector(
     [
