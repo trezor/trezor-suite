7a33675ebb0806ea11154cb09f4328c4c5cc442c
M1nd3r <petrsedlacek.km@seznam.cz>
2026-08-06 17:35:26 +0200

    chore(message-system): add a warning about ES256 deviation
    - In the signing of message-system JWS, `secp256k1` keys are used instead of `P-256`. This is a deviation from the ES256 specification. Using `secp256k1` keys has no security implications. However, it prevents replacing `jws` lib by e.g. `jose`, as `jose` does not allow `secp256k1` keys in ES256.

--- packages/env-utils/src/jws.ts
+++ packages/env-utils/src/jws.ts
@@ -1,3 +1,9 @@
+// Public keys for message-system JWS verification
+//
+// Warning: Keys are secp256k1 instead of P-256, which is a deviation from the ES256 specification (RFC 7518). This issue is known and it has been accepted,
+// as it has no security implications.
+// Impact: It is not possible to easily replace `jws` library, as most alternatives verify that the used keys are P-256.
+
 export const publicKey = {
     dev: `-----BEGIN PUBLIC KEY-----
 MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAEbSUHJlr17+NywPS/w+xMkp3dSD8eWXSuAfFKwonZPe5fL63kISipJC+eJP7Mad0WxgyJoiMsZCV6BZPK2jIFdg==
