030be129ed6879bb18c00071f772afd9ae6a62e4
Tomas Boda <tominoboda@gmail.com>
2026-08-07 13:20:41 +0200

    fix(suite-native): change apy to rate in deposit timeline bottom sheet

--- suite-native/intl/src/messages.ts
+++ suite-native/intl/src/messages.ts
@@ -3002,7 +3002,7 @@ export const messages = {
                 },
                 third: {
                     title: 'Earn rewards as your assets grow in value',
-                    description: '~{apy}% APY',
+                    description: '~{apy}% Rate',
                 },
             },
             withdrawTimelineTitle: 'Withdraw',
