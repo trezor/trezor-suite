b920f8503e9ba6f4cac0ce7d867fefd99e146f3f
tomasklim <tomas.klim@gmail.com>
2026-08-11 09:36:32 +0200

    fix(suite): make missing exchange rate tooltip generic

--- suite/intl/src/messages.ts
+++ suite/intl/src/messages.ts
@@ -2568,9 +2568,9 @@ export const messages = defineMessages({
         defaultMessage: 'Rate not available',
         id: 'TR_FIAT_RATES_NOT_AVAILABLE',
     },
-    TR_FIAT_RATES_NOT_AVAILABLE_TOOLTIP: {
-        defaultMessage: 'The 7D change rate is currently not available.',
-        id: 'TR_FIAT_RATES_NOT_AVAILABLE_TOOLTIP',
+    TR_EXCHANGE_RATE_NOT_AVAILABLE_TOOLTIP: {
+        defaultMessage: 'The exchange rate is currently not available.',
+        id: 'TR_EXCHANGE_RATE_NOT_AVAILABLE_TOOLTIP',
     },
     TR_FIRMWARE: {
         defaultMessage: 'Firmware',
