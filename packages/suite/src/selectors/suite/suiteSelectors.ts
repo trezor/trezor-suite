44cc6ed3015ae0f76e5cceb4f7b1bc9787b99e91
Matej Kriz <matej@matejkriz.cz>
2026-08-12 16:23:59 +0200

    fix(suite): stabilize empty selectActiveTransports selector

--- packages/suite/src/selectors/suite/suiteSelectors.ts
+++ packages/suite/src/selectors/suite/suiteSelectors.ts
@@ -1,5 +1,6 @@
 import { type RouterRootState, selectRouter } from '@suite/router';
 import { type DeviceRootState, selectSelectedDevice } from '@suite-common/device';
+import { returnStableArrayIfEmpty } from '@suite-common/redux-utils';
 import { type TransportInfo } from '@trezor/connect';
 
 import { type SuiteRootState } from 'src/reducers/suite/suiteReducer';
@@ -12,7 +13,7 @@ export const selectSuiteTransports = (state: SuiteRootState) =>
     state.suite.transport?.transports.map(({ type, version }) => ({ type, version }));
 export const selectIsTransportInitialized = (state: SuiteRootState) => !!state.suite.transport;
 export const selectActiveTransports = (state: SuiteRootState) =>
-    state.suite.transport?.transports ?? [];
+    returnStableArrayIfEmpty(state.suite.transport?.transports);
 export const selectHasActiveTransport = (state: SuiteRootState) =>
     !!state.suite.transport?.transports.length;
 export const selectHasTransportOfType = (type: TransportInfo['type']) => (state: SuiteRootState) =>
