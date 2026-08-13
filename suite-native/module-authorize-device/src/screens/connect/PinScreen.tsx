58df3722d074c3e4a6e1662c3c937ae83c3c9bff
juriczech <bohdan.juricek@gmail.com>
2026-08-13 15:06:57 +0200

    fix(suite-native): handle authorization after PIN

--- suite-native/module-authorize-device/src/screens/connect/PinScreen.tsx
+++ suite-native/module-authorize-device/src/screens/connect/PinScreen.tsx
@@ -9,16 +9,26 @@ import {
     PinOnKeypad,
     selectDeviceAuthorizationStep,
 } from '@suite-native/device-authorization';
-import { useNavigateToInitialScreen } from '@suite-native/navigation';
+import {
+    type AuthorizeDeviceStackParamList,
+    AuthorizeDeviceStackRoutes,
+    type StackNavigationProps,
+    useNavigateToInitialScreen,
+} from '@suite-native/navigation';
 import { DeviceModelInternal } from '@trezor/device-utils';
 
 import { ConnectDeviceScreenView } from '../../components/connect/ConnectDeviceScreenView';
 import { PinOnDevice } from '../../components/connect/PinOnDevice';
 
+type NavigationProp = StackNavigationProps<
+    AuthorizeDeviceStackParamList,
+    AuthorizeDeviceStackRoutes.PinMatrix
+>;
+
 export const PinScreen = () => {
     const navigateToInitialScreen = useNavigateToInitialScreen();
     const deviceAuthorizationStep = useSelector(selectDeviceAuthorizationStep);
-    const navigation = useNavigation();
+    const navigation = useNavigation<NavigationProp>();
     const deviceModel = useSelector(selectDeviceModel);
 
     const onSuccess = useCallback(() => {
@@ -29,6 +39,12 @@ export const PinScreen = () => {
         useCallback(() => {
             if (deviceAuthorizationStep === DeviceAuthorizationStep.Idle) {
                 navigation.goBack();
+            } else if (deviceAuthorizationStep === DeviceAuthorizationStep.PassphraseRequested) {
+                navigation.popTo(AuthorizeDeviceStackRoutes.PassphraseForm);
+            } else if (
+                deviceAuthorizationStep === DeviceAuthorizationStep.ContinueOnTrezorRequested
+            ) {
+                navigation.popTo(AuthorizeDeviceStackRoutes.ContinueOnTrezor);
             }
         }, [deviceAuthorizationStep, navigation]),
     );
