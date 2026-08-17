48b94884a5f1a9bfa74261735a94a3cd2e28e771
Martin Vere Cihlar <martin.cihlar@satoshilabs.com>
2026-08-17 16:02:58 +0200

    test(suite): fix strings in swap tests

--- suite/e2e/tests/trading/swap-sol-token-to-eth.test.ts
+++ suite/e2e/tests/trading/swap-sol-token-to-eth.test.ts
@@ -101,7 +101,7 @@ test.describe('Trading - Swap', { tag: ['@T3W1', '@T3T1'] }, () => {
                 T3W1: {
                     header: { title: 'Send' },
                     body: [
-                        ['Amount:'],
+                        ['Amount'],
                         [formattedSendAmount],
                         ['Max fees and rent'],
                         device.wrapText(`${reviewFee} SOL`, { wrapByWords: true }),
