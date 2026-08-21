7b5fb990ad9b595c5ca927127b96ae620f23c532
Jiří Čermák <jiri.cermak@satoshilabs.com>
2026-08-21 12:05:02 +0200

    refactor(suite): memoize accounts filter, remove obsolete props
    - remove unused props
    - remove inline use of `getTokens` in `Accounts` render method -> re-use it from parent
    - memoize mapping/filtering accounts

--- packages/suite/src/components/wallet/WalletLayout/AccountsMenu/AccountsList.tsx
+++ packages/suite/src/components/wallet/WalletLayout/AccountsMenu/AccountsList.tsx
@@ -1,3 +1,5 @@
+import { memo, useMemo } from 'react';
+
 import { getDefaultAccountLabel } from '@suite/account';
 import { selectCoinjoinIsPreloading } from '@suite/coinjoin';
 import { Translation, useTranslation } from '@suite/intl';
@@ -6,40 +8,37 @@ import { type RouteParams, selectRouterParams } from '@suite/router';
 import { selectSelectedDevice } from '@suite-common/device';
 import { selectAccountsWithSuiteSyncLabel } from '@suite-common/suite-sync';
 import { selectTokenDefinitions } from '@suite-common/token-definitions';
-import { getTokens, selectAllAccountsToList } from '@suite-common/wallet-core';
+import {
+    type GetTokensOutputType,
+    getTokens,
+    selectAllAccountsToList,
+} from '@suite-common/wallet-core';
 import { type Account } from '@suite-common/wallet-types';
 import { accountSearchFn, getAccountTypeName } from '@suite-common/wallet-utils';
+import { type TokenInfo } from '@trezor/blockchain-link-types';
 import { Column } from '@trezor/components';
 
 import { useAccountSearch, useSelector } from 'src/hooks/suite';
 import { useResponsiveContext } from 'src/support/suite/ResponsiveContext';
-import { type AccountItemType } from 'src/types/wallet';
 import { selectDiscoveryOverallStatus } from 'src/utils/wallet/selectDiscoveryOverallStatus';
 
 import { AccountItemSkeleton } from './AccountItemSkeleton';
 import { AccountSection } from './AccountSection';
 import { AccountsMenuNotice } from './AccountsMenuNotice';
 
-interface AccountListProps {
-    forceOnlyItemClick?: boolean;
-    hideStaking?: boolean;
-    onItemClick?: (account: Account, type: AccountItemType) => void;
-}
-
 type AccountsProps = {
-    accounts: Account[];
-    hideStaking?: boolean;
-    // NOTE: this is to disable completely default click behavior of the item
-    forceOnlyItemClick?: boolean;
-    onItemClick?: (account: Account, type: AccountItemType) => void;
+    accountsWithTokens: {
+        account: Account;
+        tokens: GetTokensOutputType<TokenInfo>;
+    }[];
 };
 
-const Accounts = ({ accounts, forceOnlyItemClick, hideStaking, onItemClick }: AccountsProps) => {
+const Accounts = memo(({ accountsWithTokens }: AccountsProps) => {
     const params = useSelector(selectRouterParams) as RouteParams;
 
     return (
         <>
-            {accounts.map(account => {
+            {accountsWithTokens.map(({ account, tokens }) => {
                 const selected =
                     account.symbol === params?.symbol &&
                     account.accountType === params.accountType &&
@@ -48,23 +47,17 @@ const Accounts = ({ accounts, forceOnlyItemClick, hideStaking, onItemClick }: Ac
                 return (
                     <AccountSection
                         key={account.key}
-                        forceOnlyItemClick={forceOnlyItemClick}
-                        hideStaking={hideStaking}
                         account={account}
+                        tokens={tokens.shownWithBalance}
                         selected={selected}
-                        onItemClick={onItemClick}
                     />
                 );
             })}
         </>
     );
-};
+});
 
-export const AccountsList = ({
-    forceOnlyItemClick,
-    hideStaking,
-    onItemClick,
-}: AccountListProps) => {
+export const AccountsList = memo(() => {
     const device = useSelector(selectSelectedDevice);
     const baseAccounts = useSelector(selectAllAccountsToList);
 
@@ -86,58 +79,69 @@ export const AccountsList = ({
     const discoveryInProgress = discoveryStatus?.status === 'loading';
     const tokenDefinitions = useSelector(selectTokenDefinitions);
 
+    const filteredAccounts = useMemo(
+        () =>
+            accounts
+                .map(account => {
+                    const tokens = getTokens({
+                        tokens: account.tokens ?? [],
+                        symbol: account.symbol,
+                        tokenDefinitions: tokenDefinitions[account.symbol]?.coin,
+                    });
+
+                    return { account, tokens };
+                })
+                .filter(({ account, tokens }) => {
+                    const { key } = account;
+
+                    if (!searchString && !coinFilter) {
+                        return true;
+                    }
+
+                    const accountLabel =
+                        account.label ??
+                        (Object.hasOwn(accountLegacyLabels, key)
+                            ? accountLegacyLabels[key]
+                            : getDefaultAccountLabel(translationString, account)) ??
+                        '';
+
+                    // Mirror the account type badge, which is hidden for normal accounts.
+                    const accountTypeTranslationId =
+                        account.accountType === 'normal'
+                            ? null
+                            : getAccountTypeName({
+                                  path: account.path,
+                                  accountType: account.accountType,
+                                  networkType: account.networkType,
+                              });
+
+                    return accountSearchFn(account, searchString, {
+                        coinsFilter: coinFilter,
+                        accountLabel,
+                        searchableTokens: tokens.shownWithBalance,
+                        accountTypeName: accountTypeTranslationId
+                            ? translationString(accountTypeTranslationId)
+                            : undefined,
+                    });
+                }),
+        [
+            accounts,
+            searchString,
+            coinFilter,
+            accountLegacyLabels,
+            tokenDefinitions,
+            translationString,
+        ],
+    );
+
     if (!device) {
         return null;
     }
 
-    const filteredAccounts =
-        searchString || coinFilter
-            ? accounts.filter(account => {
-                  const { key } = account;
-
-                  const accountLabel =
-                      account.label ??
-                      (Object.prototype.hasOwnProperty.call(accountLegacyLabels, key)
-                          ? accountLegacyLabels[key]
-                          : getDefaultAccountLabel(translationString, account)) ??
-                      '';
-
-                  const { shownWithBalance } = getTokens({
-                      tokens: account.tokens ?? [],
-                      symbol: account.symbol,
-                      tokenDefinitions: tokenDefinitions[account.symbol]?.coin,
-                  });
-
-                  // Mirror the account type badge, which is hidden for normal accounts.
-                  const accountTypeTranslationId =
-                      account.accountType === 'normal'
-                          ? null
-                          : getAccountTypeName({
-                                path: account.path,
-                                accountType: account.accountType,
-                                networkType: account.networkType,
-                            });
-
-                  return accountSearchFn(account, searchString, {
-                      coinsFilter: coinFilter,
-                      accountLabel,
-                      searchableTokens: shownWithBalance,
-                      accountTypeName: accountTypeTranslationId
-                          ? translationString(accountTypeTranslationId)
-                          : undefined,
-                  });
-              })
-            : accounts;
-
     if (filteredAccounts.length > 0) {
         return (
             <Column gap={4} margin={{ bottom: 20, left: 8, right: 8 }}>
-                <Accounts
-                    accounts={filteredAccounts}
-                    hideStaking={hideStaking}
-                    forceOnlyItemClick={forceOnlyItemClick}
-                    onItemClick={onItemClick}
-                />
+                <Accounts accountsWithTokens={filteredAccounts} />
                 {coinjoinIsPreloading && !searchString && !coinFilter && <AccountItemSkeleton />}
             </Column>
         );
@@ -156,4 +160,4 @@ export const AccountsList = ({
             <Translation id="TR_ACCOUNT_SEARCH_NO_RESULTS" />
         </AccountsMenuNotice>
     );
-};
+});
