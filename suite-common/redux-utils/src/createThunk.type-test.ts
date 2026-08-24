610b1731997a1877fe3edc97da63f75e055b36ef
Peter Sanderson <dierk.deidel@protonmail.com>
2026-08-24 16:27:54 +0200

    test(redux-utils): cover parent thunk contracts

--- suite-common/redux-utils/src/createThunk.type-test.ts
+++ suite-common/redux-utils/src/createThunk.type-test.ts
@@ -1,3 +1,5 @@
+import { type ThunkDispatch, type UnknownAction } from '@reduxjs/toolkit';
+
 import { createThunk } from './createThunk';
 
 type SelectedExtraDependencies = {
@@ -77,3 +79,63 @@ createThunk<void, void, { state: SelectedState; extra: SelectedExtraDependencies
         void unselectedAnalytics;
     },
 );
+
+type ChildThunkState = {
+    child: {
+        value: number;
+    };
+};
+
+type ChildThunkDeps = {
+    services: {
+        childDependency: () => void;
+    };
+};
+
+const selectChildValue = (state: ChildThunkState) => state.child.value;
+
+const childThunk = createThunk<void, void, { state: ChildThunkState; extra: ChildThunkDeps }>(
+    'test/child',
+    (_, { extra, getState }) => {
+        const childValue: number = selectChildValue(getState());
+        extra.services.childDependency();
+
+        void childValue;
+    },
+);
+
+type ParentThunkState = SelectedState & ChildThunkState;
+type ParentThunkDeps = SelectedExtraDependencies & ChildThunkDeps;
+
+const parentThunk = createThunk<void, void, { state: ParentThunkState; extra: ParentThunkDeps }>(
+    'test/parent',
+    (_, { dispatch, extra, getState }) => {
+        const selectedValue: string = selectSelectedValue(getState());
+        const childValue: number = selectChildValue(getState());
+        extra.services.selectedDependency();
+        extra.services.childDependency();
+        dispatch(childThunk());
+
+        void selectedValue;
+        void childValue;
+    },
+);
+
+declare const compatibleDispatch: ThunkDispatch<ParentThunkState, ParentThunkDeps, UnknownAction>;
+compatibleDispatch(parentThunk());
+
+declare const dispatchWithoutChildState: ThunkDispatch<
+    SelectedState,
+    ParentThunkDeps,
+    UnknownAction
+>;
+// @ts-expect-error The store does not contain the state required by the dispatched child thunk.
+dispatchWithoutChildState(parentThunk());
+
+declare const dispatchWithoutChildDependencies: ThunkDispatch<
+    ParentThunkState,
+    SelectedExtraDependencies,
+    UnknownAction
+>;
+// @ts-expect-error The store does not provide the dependency required by the dispatched child thunk.
+dispatchWithoutChildDependencies(parentThunk());
