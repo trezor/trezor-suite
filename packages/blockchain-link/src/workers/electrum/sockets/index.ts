82ab992f3eccfc7355110a43669d85d66014db6d
Marek Polak <marek.polak@satoshilabs.com>
2026-08-26 16:05:51 +0200

    chore(blockchain-link): remove deprecated TorSocket for electrum backend

--- packages/blockchain-link/src/workers/electrum/sockets/index.ts
+++ packages/blockchain-link/src/workers/electrum/sockets/index.ts
@@ -4,22 +4,11 @@ import { parseElectrumUrl, throwError } from '@trezor/utils';
 import type { SocketBase, SocketOptions } from './base';
 import { TcpSocket } from './tcp';
 import { TlsSocket } from './tls';
-import { TorSocket } from './tor';
 
 export const createSocket = (url: string, options?: SocketOptions): SocketBase => {
     const { host, port, protocol } =
         parseElectrumUrl(url) ?? throwError(new CustomError('Invalid electrum url'));
-    const { timeout, keepAlive, proxyAgent } = options || {};
-    // Onion address is TCP over Tor
-    if (proxyAgent /* host.endsWith('.onion') */) {
-        return new TorSocket({
-            host,
-            port,
-            timeout,
-            keepAlive,
-            proxyAgent,
-        });
-    }
+    const { timeout, keepAlive } = options || {};
     switch (protocol) {
         case 't': // TCP socket
             return new TcpSocket({ host, port, timeout, keepAlive });
