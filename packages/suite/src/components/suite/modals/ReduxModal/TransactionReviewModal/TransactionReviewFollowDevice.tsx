69fc36ceb374c52c39e2d4ad8be2576e4596b763
Tomas Boda <tominoboda@gmail.com>
2026-08-27 13:36:48 +0200

    feat(suite): show follow-device review for solana split unstakes

---
+++ packages/suite/src/components/suite/modals/ReduxModal/TransactionReviewModal/TransactionReviewFollowDevice.tsx
@@ -0,0 +1,26 @@
+import { Translation } from '@suite/intl';
+import { selectSelectedDevice } from '@suite-common/device';
+import { Column, H2 } from '@trezor/components';
+
+import { DeviceConfirmImage } from 'src/components/suite/DeviceConfirmImage';
+import { useSelector } from 'src/hooks/suite';
+
+interface TransactionReviewFollowDeviceProps {
+    isSigned: boolean;
+}
+
+export const TransactionReviewFollowDevice = ({ isSigned }: TransactionReviewFollowDeviceProps) => {
+    const device = useSelector(selectSelectedDevice);
+
+    return (
+        <Column alignItems="center" gap={16} data-testid="@modal/review/follow-device">
+            <DeviceConfirmImage device={device} />
+
+            {!isSigned && (
+                <H2 align="center" margin={{ left: 16, right: 16, bottom: 16 }}>
+                    <Translation id="TR_CONFIRM_ACTION_ON_YOUR" />
+                </H2>
+            )}
+        </Column>
+    );
+};
