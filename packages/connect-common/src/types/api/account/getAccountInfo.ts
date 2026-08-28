26d63edca076e5a9607236e51ac44ebd53dcb584
Marek Polak <marek.polak@satoshilabs.com>
2026-08-28 15:19:01 +0200

    chore(connect): remove device flow from getAccountInfo

--- packages/connect-common/src/types/api/account/getAccountInfo.ts
+++ packages/connect-common/src/types/api/account/getAccountInfo.ts
@@ -1,18 +1,13 @@
 import type { BlockchainLinkParams } from '@trezor/blockchain-link';
-import type { MessagesSchema as PROTO } from '@trezor/protobuf';
 
-import type { AccountInfo, DiscoveryAccountType } from '../../account';
+import type { AccountInfo } from '../../account';
 import type { CoinSymbol } from '../../coinInfo';
 import type { BundledParams, Params, Response } from '../../params';
 
 export interface GetAccountInfo extends Omit<BlockchainLinkParams<'getAccountInfo'>, 'descriptor'> {
     coin: CoinSymbol;
     identity?: string;
-    path?: string;
-    descriptor?: string;
-    defaultAccountType?: DiscoveryAccountType;
-    derivationType?: PROTO.CardanoDerivationType;
-    suppressBackupWarning?: boolean;
+    descriptor: string;
 }
 
 export declare function getAccountInfo(params: Params<GetAccountInfo>): Response<AccountInfo>;
