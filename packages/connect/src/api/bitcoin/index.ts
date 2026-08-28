59fa93604d33755e28a89dfcad57c8f98edd9b42
Marek Polak <marek.polak@satoshilabs.com>
2026-08-28 16:29:03 +0200

    chore(connect): remove signTxLegacy

--- packages/connect/src/api/bitcoin/index.ts
+++ packages/connect/src/api/bitcoin/index.ts
@@ -4,6 +4,5 @@ export * from './inputs';
 export * from './outputs';
 export * from './refTx';
 export * from './signtx';
-export * from './signtxLegacy';
 export * from './signtxVerify';
 export * from './TransactionComposer';
