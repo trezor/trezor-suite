84449df88279ddf1091f8c10ba6149ffffd38017
sneurlax <sneurlax@gmail.com>
2026-09-02 16:38:39 -0500

    test(connect): expect NU6.3 branch_id fallback for zcash

--- packages/connect/src/api/bitcoin/enhanceSignTx.test.ts
+++ packages/connect/src/api/bitcoin/enhanceSignTx.test.ts
@@ -9,21 +9,21 @@ describe('api/bitcoin/enhanceSignTx', () => {
                 overwintered: true,
                 version: 5,
                 version_group_id: 0x26a7270a,
-                branch_id: 0x5437f330,
+                branch_id: 0x37a5165b,
             });
 
             expect(enhanceSignTx({ version: 4 }, coinInfo)).toEqual({
                 overwintered: true,
                 version: 4,
                 version_group_id: 0x26a7270a,
-                branch_id: 0x5437f330,
+                branch_id: 0x37a5165b,
             });
 
             expect(enhanceSignTx({ version: 4, version_group_id: 1 }, coinInfo)).toEqual({
                 overwintered: true,
                 version: 4,
                 version_group_id: 1,
-                branch_id: 0x5437f330,
+                branch_id: 0x37a5165b,
             });
 
             expect(
