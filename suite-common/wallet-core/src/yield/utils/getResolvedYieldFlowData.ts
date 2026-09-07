1a333c237b77e4cdbee7aa8b6275f71f0a4c3453
Tomas Boda <tominoboda@gmail.com>
2026-09-07 16:06:13 +0200

    fix: mobile yield fiat amount + desktop yield token icon

--- suite-common/wallet-core/src/yield/utils/getResolvedYieldFlowData.ts
+++ suite-common/wallet-core/src/yield/utils/getResolvedYieldFlowData.ts
@@ -193,19 +193,12 @@ export const getResolvedYieldFlowData = ({
         token: vault.outputToken,
     });
 
-    const matchedTokenContract = matchedToken
-        ? getContractAddressForNetworkSymbol(account.symbol, matchedToken.contract)
-        : null;
-    const matchedReceiptTokenContract = matchedReceiptToken
-        ? getContractAddressForNetworkSymbol(account.symbol, matchedReceiptToken.contract)
-        : null;
-
     const token: YieldFlowToken = {
         networkSymbol: account.symbol,
         symbol:
             matchedToken?.symbol ?? vault.token.symbol ?? getNetworkDisplaySymbol(account.symbol),
         decimals: matchedToken?.decimals ?? vault.token.decimals,
-        contractAddress: matchedTokenContract ?? underlyingTokenContract,
+        contractAddress: matchedToken?.contract ?? underlyingTokenContract,
         balance: matchedToken?.balance ?? '0',
     };
 
@@ -213,7 +206,7 @@ export const getResolvedYieldFlowData = ({
         networkSymbol: account.symbol,
         symbol: matchedReceiptToken?.symbol ?? vault.outputToken.symbol,
         decimals: matchedReceiptToken?.decimals ?? vault.outputToken.decimals,
-        contractAddress: matchedReceiptTokenContract ?? receiptTokenContract,
+        contractAddress: matchedReceiptToken?.contract ?? receiptTokenContract,
     };
 
     const flowData: YieldFlowResolvedData = { account, vault, token, receiptToken };
