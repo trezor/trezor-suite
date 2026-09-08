553aeb720d0dd37988b42e3bb3856675372dabfb
Jiri Zbytovsky <jiri.zbytovsky@satoshilabs.com>
2026-09-08 15:12:37 +0200

    feat(suite-desktop): enable devtools also when debug mode is on

--- suite/desktop-app-main/src/modules/shortcuts.ts
+++ suite/desktop-app-main/src/modules/shortcuts.ts
@@ -5,34 +5,33 @@ import { isCodesignBuild } from '@trezor/env-utils';
 import type { ModuleInit } from './module';
 import { restartApp } from '../libs/app-utils';
 import { hasSwitch } from '../libs/process-switches';
+import { Store } from '../libs/store';
 
 export const SERVICE_NAME = 'shortcuts';
 
-// DevTools are only available in development, or in production when explicitly enabled via CLI flag.
-const isDevToolsEnabled = !isCodesignBuild() || hasSwitch('open-devtools');
-
 export const init: ModuleInit = ({ mainWindowProxy }) => {
     const { logger } = global;
+    const store = Store.getStore();
+
+    // DevTools are disabled by default, to avoid inadvertent self-XSS for users. But for development & testing convenience, it's available either:
+    // always on a dev build, or if Suite was executed with --open-devtools arg, or with Debug settings on (the only way to toggle it during runtime).
+    const getIsDevToolsEnabled = () =>
+        !isCodesignBuild() || hasSwitch('open-devtools') || store.getIsDebugModeActive();
 
     mainWindowProxy.on('init', mainWindow => {
-        if (isDevToolsEnabled) {
-            const openDevToolsShortcuts = [
-                'F12',
-                'CommandOrControl+Shift+I',
-                'CommandOrControl+Alt+I',
-            ];
-            openDevToolsShortcuts.forEach(shortcut => {
-                electronLocalshortcut.register(mainWindow, shortcut, () => {
-                    logger.info(SERVICE_NAME, `${shortcut} pressed to open/close DevTools`);
+        const openDevToolsShortcuts = ['F12', 'CommandOrControl+Shift+I', 'CommandOrControl+Alt+I'];
+        openDevToolsShortcuts.forEach(shortcut => {
+            electronLocalshortcut.register(mainWindow, shortcut, () => {
+                if (!getIsDevToolsEnabled()) return;
+                logger.info(SERVICE_NAME, `${shortcut} pressed to open/close DevTools`);
 
-                    if (mainWindow.webContents.isDevToolsOpened()) {
-                        mainWindow.webContents.closeDevTools();
-                    } else {
-                        mainWindow.webContents.openDevTools();
-                    }
-                });
+                if (mainWindow.webContents.isDevToolsOpened()) {
+                    mainWindow.webContents.closeDevTools();
+                } else {
+                    mainWindow.webContents.openDevTools();
+                }
             });
-        }
+        });
 
         const reloadAppShortcuts = ['F5', 'CommandOrControl+R'];
         reloadAppShortcuts.forEach(shortcut => {
