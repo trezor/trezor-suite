a4bd37ff7d609477369384aec3b56784fd46e112
yanas <yanas@yanas.cz>
2026-09-09 13:34:15 +0200

    fix(suite-native): prevent navigating back during FW installation

--- suite-native/module-device-onboarding/src/screens/FirmwareInstallationScreen.tsx
+++ suite-native/module-device-onboarding/src/screens/FirmwareInstallationScreen.tsx
@@ -1,25 +1,15 @@
 import { FirmwareInstallationScreenContent } from '@suite-native/firmware';
-import { ScreenHeader, useOverrideBackNavigation } from '@suite-native/navigation';
 
 import { useExitAlert } from '../hooks/useExitAlert';
 import { useNavigateToNextScreenAfterFirmwareInstallation } from '../hooks/useNavigateToNextScreenAfterFirmwareInstallation';
-
-const FirmwareInstallationScreenHeader = () => {
-    const { handleExitButtonPress } = useExitAlert();
-    useOverrideBackNavigation({ onNavigateBack: handleExitButtonPress });
-
-    return <ScreenHeader closeActionType="close" closeAction={handleExitButtonPress} />;
-};
 
 export const FirmwareInstallationScreen = () => {
     const { handleExitButtonPress } = useExitAlert();
     const { navigateToNextScreenAfterFirmwareInstallation } =
         useNavigateToNextScreenAfterFirmwareInstallation();
-    useOverrideBackNavigation();
 
     return (
         <FirmwareInstallationScreenContent
-            customHeader={<FirmwareInstallationScreenHeader />}
             onCancelAction={handleExitButtonPress}
             onFirmwareInstallationSuccess={navigateToNextScreenAfterFirmwareInstallation}
             isTemporaryRememeberAllowed={false}
