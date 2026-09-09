a7bb7556aa140d3b130f72a7b27a2c40ad309050
Martin Šmídl <smidl.martin1@gmail.com>
2026-09-09 10:12:33 +0200

    fix(suite): fix overlfowing text

--- suite-native/trading-history/src/components/TradingHistoryDetail/TradingHistoryDetailMinimumReceivedRow.tsx
+++ suite-native/trading-history/src/components/TradingHistoryDetail/TradingHistoryDetailMinimumReceivedRow.tsx
@@ -1,6 +1,12 @@
 import { Text } from '@suite-native/atoms';
 import { Translation } from '@suite-native/intl';
 import { TradeInfoRow } from '@suite-native/trading-atoms';
+import { prepareNativeStyle, useNativeStyles } from '@trezor/styles-native';
+
+const amountStyle = prepareNativeStyle(() => ({
+    flexGrow: 1,
+    flexShrink: 1,
+}));
 
 type TradingHistoryDetailMinimumReceivedRowProps = {
     formattedMinimumReceived: string;
@@ -8,11 +14,22 @@ type TradingHistoryDetailMinimumReceivedRowProps = {
 
 export const TradingHistoryDetailMinimumReceivedRow = ({
     formattedMinimumReceived,
-}: TradingHistoryDetailMinimumReceivedRowProps) => (
-    <TradeInfoRow>
-        <Text color="contentSecondary" variant="body-sm">
-            <Translation id="moduleTrading.tradeHistory.detail.info.minimumReceivedAmount" />
-        </Text>
-        <Text variant="body-sm">{formattedMinimumReceived}</Text>
-    </TradeInfoRow>
-);
+}: TradingHistoryDetailMinimumReceivedRowProps) => {
+    const { applyStyle } = useNativeStyles();
+
+    return (
+        <TradeInfoRow flexWrap="wrap">
+            <Text color="contentSecondary" variant="body-sm">
+                <Translation id="moduleTrading.tradeHistory.detail.info.minimumReceivedAmount" />
+            </Text>
+            <Text
+                variant="body-sm"
+                textAlign="right"
+
+                style={applyStyle(amountStyle)}
+            >
+                {formattedMinimumReceived}
+            </Text>
+        </TradeInfoRow>
+    );
+};
