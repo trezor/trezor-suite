aaaf62752636c284007df89885a14a872a7d495a
Marek Polak <marek.polak@satoshilabs.com>
2026-09-10 15:35:10 +0200

    refactor(connect): move `firmwareReleaseConfigAssets` to connect-data

--- packages/connect-data/src/index.ts
+++ packages/connect-data/src/index.ts
@@ -1,2 +1,8 @@
+import type { FirmwareReleaseConfig } from '@trezor/device-utils';
+
+import firmwareReleaseConfigAssetsJson from '../files/firmware/release/releases.v1.json';
+
 export * from './firmware-jws';
 export { firmwareAssets } from './map-releases';
+
+export const firmwareReleaseConfigAssets = firmwareReleaseConfigAssetsJson as FirmwareReleaseConfig;
