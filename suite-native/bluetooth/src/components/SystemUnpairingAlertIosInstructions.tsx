ac95c79b6701dd6db2d89f860a7a4589c06d4953
yanas <yanas@yanas.cz>
2026-09-15 14:46:34 +0200

    refactor(suite-native): replace BottomSheetListItem with IconListTextItem

--- suite-native/bluetooth/src/components/SystemUnpairingAlertIosInstructions.tsx
+++ suite-native/bluetooth/src/components/SystemUnpairingAlertIosInstructions.tsx
@@ -1,18 +1,16 @@
-import { BottomSheetListItem, VStack } from '@suite-native/atoms';
+import { IconList, IconListTextItem } from '@suite-native/atoms';
+import { Translation } from '@suite-native/intl';
 
 export const SystemUnpairingAlertIosInstructions = () => (
-    <VStack>
-        <BottomSheetListItem
-            icon={1}
-            translationKey="bluetooth.alerts.unpairingInstructions.step1"
-        />
-        <BottomSheetListItem
-            icon={2}
-            translationKey="bluetooth.alerts.unpairingInstructions.step2"
-        />
-        <BottomSheetListItem
-            icon={3}
-            translationKey="bluetooth.alerts.unpairingInstructions.step3"
-        />
-    </VStack>
+    <IconList textVariant="body-md">
+        <IconListTextItem icon={1}>
+            <Translation id="bluetooth.alerts.unpairingInstructions.step1" />
+        </IconListTextItem>
+        <IconListTextItem icon={2}>
+            <Translation id="bluetooth.alerts.unpairingInstructions.step2" />
+        </IconListTextItem>
+        <IconListTextItem icon={3}>
+            <Translation id="bluetooth.alerts.unpairingInstructions.step3" />
+        </IconListTextItem>
+    </IconList>
 );
