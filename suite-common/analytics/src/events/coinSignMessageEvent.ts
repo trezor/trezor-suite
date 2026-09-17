5e5b5b1d0f6eda2965e9bc19f75ccabe24dd371d
yanas <yanas@yanas.cz>
2026-09-17 10:40:24 +0200

    refactor(suite): move sign and verify analytics events to suite-common

--- suite-common/analytics/src/events/coinSignMessageEvent.ts
+++ suite-common/analytics/src/events/coinSignMessageEvent.ts
@@ -1,6 +1,5 @@
-import type { AttributeDef, EventDef } from '@suite-common/analytics';
-
 import { EventType } from '../constants';
+import type { AttributeDef, EventDef } from '../eventDefinition';
 
 type Attributes = {
     status: AttributeDef<'success' | 'error' | 'cancelled'>;
