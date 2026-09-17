0d1e7cca6a734c30c4cff4f8a6c305876b8df494
Szymon Lesisz <sz.lesisz@gmail.com>
2026-09-17 16:14:29 +0200

    fix(transport-bluetooth): do not call `discover_services` on disconnected peripheral

--- packages/transport-bluetooth/src/server/adapter_manager.rs
+++ packages/transport-bluetooth/src/server/adapter_manager.rs
@@ -452,23 +452,18 @@ impl AdapterManager {
     }
 
     pub async fn update_serviceless_device(&self, id: &PeripheralId) -> Result<(), AdapterError> {
-        let device = self.serviceless_peripherals.get(&id.to_string());
-        if device.is_none() {
-            return Ok(());
-        }
+        let id_string = id.to_string();
+        let update_count = match self.serviceless_peripherals.remove(&id_string) {
+            Some((_, device)) => device.update_count,
+            None => return Ok(()),
+        };
 
-        let update_count = device.unwrap().update_count;
-        self.serviceless_peripherals.remove(&id.to_string());
         if self.is_discovered(id) {
             return Ok(());
         }
 
-        let peripheral = self.get_peripheral_or_die(&id.to_string()).await?;
-        if peripheral.services().is_empty() {
-            let _ = peripheral.discover_services().await;
-        }
-
         let adapter = self.get_adapter_or_die().await?;
+        let peripheral = self.get_peripheral_or_die(&id_string).await?;
         if let Some(_device) = utils::scan_filter(&adapter, id).await {
             if let Ok(_device) = self.add_device(id).await {
                 let devices = self.get_devices().await;
