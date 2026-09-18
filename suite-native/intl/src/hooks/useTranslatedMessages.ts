925e45ab3326c77cafaba5ea7e2c1fb6701e679d
Petr Knetl <p.knetlml@gmail.com>
2026-09-18 13:56:00 +0200

    perf(suite-native): load only the active translation catalog
    Closes #32454

--- suite-native/intl/src/hooks/useTranslatedMessages.ts
+++ suite-native/intl/src/hooks/useTranslatedMessages.ts
@@ -1,27 +1,10 @@
 import { useSelector } from 'react-redux';
 
+import { getMessagesForLocale } from '../getMessagesForLocale';
 import { selectSupportedLanguageLocale } from '../localeSlice';
-import { messages as defaultMessages } from '../messages';
-import { type SupportedLocaleCode } from '../types';
-import { flatten } from '../utils';
-
-const LANGUAGE_TRANSLATIONS_MAP = {
-    'en-US': require('../../translations/en-US.json'),
-    'cs-CZ': require('../../translations/cs-CZ.json'),
-    'de-DE': require('../../translations/de-DE.json'),
-    'es-ES': require('../../translations/es-ES.json'),
-    'fr-FR': require('../../translations/fr-FR.json'),
-    'id-ID': require('../../translations/id-ID.json'),
-    'pt-BR': require('../../translations/pt-BR.json'),
-    'ja-JP': require('../../translations/ja-JP.json'),
-    'zh-CN': require('../../translations/zh-CN.json'),
-} as const satisfies Record<SupportedLocaleCode, any>;
-
-// default values defined during the development.
-const englishFallback = flatten(defaultMessages);
 
 export const useTranslatedMessages = (): Record<string, string> => {
     const supportedLanguageLocale = useSelector(selectSupportedLanguageLocale);
 
-    return { ...englishFallback, ...LANGUAGE_TRANSLATIONS_MAP[supportedLanguageLocale] };
+    return getMessagesForLocale(supportedLanguageLocale);
 };
