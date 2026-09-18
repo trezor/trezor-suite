a39913526d041cd29f4feaa2fa92cfa0f865f0ed
Martin Varmuza <mvarmuza@gmail.com>
2026-09-18 12:05:49 +0200

    refactor(suite): remove unused files and exports

--- packages/suite/src/actions/wallet/constants/receiveConstants.ts
+++ packages/suite/src/actions/wallet/constants/receiveConstants.ts
@@ -1,3 +1,1 @@
 export const DISPOSE = '@receive/dispose' as const;
-export const SHOW_ADDRESS = '@receive/show-address' as const;
-export const SHOW_UNVERIFIED_ADDRESS = '@receive/show-unverified-address' as const;
--- packages/suite/src/types/trading/tradingVerify.ts
+++ packages/suite/src/types/trading/tradingVerify.ts
@@ -1,11 +1,6 @@
-import { type UseFormReturn } from 'react-hook-form';
-
 import { type CryptoId } from 'invity-api';
 
 import { type ExtendedMessageDescriptor } from '@suite/intl';
-import { type AccountAddress } from '@trezor/connect';
-
-import type { Account } from 'src/types/wallet';
 
 export interface TradingVerifyFormProps {
     address?: string;
@@ -20,21 +15,4 @@ export interface TradingVerifyAccountProps {
 export interface TradingGetTranslationIdsProps {
     accountTooltipTranslationId: ExtendedMessageDescriptor['id'];
     addressTooltipTranslationId: ExtendedMessageDescriptor['id'];
-}
-
-export interface TradingVerifyAccountReturnProps {
-    form: UseFormReturn<TradingVerifyFormProps>;
-    accountAddress: AccountAddress | Pick<AccountAddress, 'path' | 'address'> | undefined;
-    receiveNetwork: CryptoId | undefined;
-    suiteReceiveAccounts: Account[] | undefined;
-    selectedAccount: Account | null | undefined;
-    canAddSuiteAccount: boolean;
-    canUseNonSuiteAccount: boolean;
-    isMenuOpen: boolean | undefined;
-    getTranslationIds: (
-        selectedAccount: Account | null | undefined,
-    ) => TradingGetTranslationIdsProps;
-    onChangeAccount: (account: Account) => void;
-    selectNonSuiteAddress: (address?: string) => void;
-    openAddSuiteAccount: () => void;
 }
