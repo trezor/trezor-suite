a05142a9b178435aec8e86ee3aeccd59378d3365
Matej Kriz <matej@matejkriz.cz>
2026-09-23 13:54:56 +0200

    fix(suite-native): transform kysely in native Jest tests

--- jest.config.native.js
+++ jest.config.native.js
@@ -39,7 +39,7 @@ module.exports = {
         '\\.(js|jsx)$': ['babel-jest', babelConfig],
     },
     transformIgnorePatterns: [
-        'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|@shopify/react-native-skia|@shopify/flash-list|@noble|@scure|@evolu|random|nanoid|msgpackr|@gorhom|uuid|react-intl|@formatjs/*|intl-messageformat)',
+        'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@sentry/react-native|native-base|react-native-svg|@shopify/react-native-skia|@shopify/flash-list|@noble|@scure|@evolu|kysely|random|nanoid|msgpackr|@gorhom|uuid|react-intl|@formatjs/*|intl-messageformat)',
     ],
     setupFiles: [
         '<rootDir>/../../suite-native/test-utils/src/mocks/reanimatedMock.js',
